9399cfe625ebe415216a1f4b7fdac2b491e38c87
Matthew Wilson <mtwilson@quicinc.com>
2010-04-12 10:50:49 -0500

    bluetooth: add Bluetooth drivers for MSM
    Add Bluetooth power switch driver.
    Add Bluetooth UART low power mode drivers.
    
    Change-Id: I509b39af7c32292a8f9c84f20aa633d0654ad358
    Signed-off-by: Matthew Wilson <mtwilson@quicinc.com>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -574,6 +574,9 @@ static int __init hci_uart_init(void)
 #ifdef CONFIG_BT_HCIUART_ATH3K
 	ath_init();
 #endif
+#ifdef CONFIG_BT_HCIUART_IBS
+	ibs_init();
+#endif
 
 	return 0;
 }
@@ -593,6 +596,9 @@ static void __exit hci_uart_exit(void)
 #endif
 #ifdef CONFIG_BT_HCIUART_ATH3K
 	ath_deinit();
+#endif
+#ifdef CONFIG_BT_HCIUART_IBS
+	ibs_deinit();
 #endif
 
 	/* Release tty registration of line discipline */
