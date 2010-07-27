ab131d7e3c4a74cf5ea068c418fc184292d25be1
Israel Schlesinger <israels@codeaurora.org>
2010-07-27 10:53:32 -0700

    checkpatch: fix regex for catching mdelay()
    upstream prefers this change for checkpatch:
    http://lkml.org/lkml/2010/7/27/254
    
    Signed-off-by: Israel Schlesinger <israels@codeaurora.org>
    Change-Id: I61f8c1ff622994b37a79fed4b811115da2c108ad
    (cherry picked from commit 76cc4ad08fe1d8912ce44ee2cc1f10373c2974d3)

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -3256,7 +3256,7 @@ sub process {
 		}
 
 # check the patch for use of mdelay
-		if ($line =~ /\bmdelay\(/) {
+		if ($line =~ /\bmdelay\s*\(/) {
 			WARN("use of mdelay() found: msleep() is the preferred API.\n" . $line );
 		}
 
