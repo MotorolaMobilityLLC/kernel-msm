53b1546ec3d785f429937fe79632a4c0bb98f2cc
John Michelau <john.michelau@motorola.com>
2010-11-08 18:05:37 -0600

    usb: gadget: Multiple ACM gadget instances
    - Added multiple ACM instance support in Android gadget
    - Fixed multiple instance naming issue in ACM function
    - Increased max instances from 4 to 8
    
    Change-Id: I65f1b0be94da859bab7ec0ad7cd804b896c7c4c5
    Signed-off-by: John Michelau <john.michelau@motorola.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/usb/gadget/f_acm.c
+++ drivers/usb/gadget/f_acm.c
@@ -889,6 +889,7 @@ acm_unbind(struct usb_configuration *c, struct usb_function *f)
 		usb_free_descriptors(f->ss_descriptors);
 	usb_free_descriptors(f->descriptors);
 	gs_free_req(acm->notify, acm->notify_req);
+	kfree(acm->port.func.name);
 	kfree(acm);
 }
 
@@ -962,7 +963,11 @@ int acm_bind_config(struct usb_configuration *c, u8 port_num)
 	acm->port.send_break = acm_send_break;
 	acm->port.send_modem_ctrl_bits = acm_send_modem_ctrl_bits;
 
-	acm->port.func.name = "acm";
+	acm->port.func.name = kasprintf(GFP_KERNEL, "acm%u", port_num + 1);
+	if (!acm->port.func.name) {
+		kfree(acm);
+		return -ENOMEM;
+	}
 	acm->port.func.strings = acm_strings;
 	/* descriptors are per-instance copies */
 	acm->port.func.bind = acm_bind;
