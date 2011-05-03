10438a9b7e3b8320dee1554030cc679d2e595007
Pavankumar Kondeti <pkondeti@codeaurora.org>
2011-05-03 14:39:42 +0530

    USB: gadget: Implement pullup gadget method in ci13xxx_udc
    Implement pullup gadget method for providing enumeration control
    to function drivers.
    
    Change-Id: I8ed73280646902003888cd9e19959f3b23ed31ed
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.h
+++ drivers/usb/gadget/ci13xxx_udc.h
@@ -137,6 +137,7 @@ struct ci13xxx {
 	struct usb_gadget_driver  *driver;     /* 3rd party gadget driver */
 	struct ci13xxx_udc_driver *udc_driver; /* device controller driver */
 	int                        vbus_active; /* is VBUS active */
+	int                        softconnect; /* is pull-up enable allowed */
 	struct usb_phy            *transceiver; /* Transceiver struct */
 };
 
