1ae58158644a486f11c5af502065fe9b5d22f7d7
Pratik Patel <pratikp@codeaurora.org>
2011-05-29 18:24:18 -0700

    power: fix lcd resume taking long time with an ongoing file copy
    This fixes the issue where LCD takes a long time to come back up
    since the execution of backlight on and late_resume works by the
    suspend worker thread is delayed due to one (or more) of the
    sys_sync calls in early_suspend and suspend paths taking a long
    time (sometimes 15sec or more) for the below reported scenario(s):
    
    Scenario 1 (copy with usb connected):
    1. plug usb
    2. adb shell
    3. busybox cp /sdcard/file1 /sdcard/file2 (copy >= 100MB file1
       in sdcard/emmc to file2 in sdcard/emmc)
    4. press end key to suspend
    5. press end key again and it takes a long time for LCD to come
       back up
    
    Scenario 2 (background copy):
    1. plug usb
    2. adb shell
    3. busybox cp /sdcard/file1 /sdcard/file2 & (copy >= 100MB file1
       in sdcard/emmc to file2 in sdcard/emmc)
    4. disconnect usb
    5. press end key to suspend
    6. press end key again and it takes a long time for LCD to come
       back up
    
    A more common form of Scenario 2 is for the user to just use the
    copy function on the UI to copy large file(s).
    
    We address this by moving sys_sync calls to a separate workqueue
    and having a timeout polling based mechanism to bail out of suspend
    in case of user invoking a wakeup event (like end key press) while
    we are waiting for the sys_sync completion at the synchronization
    point in suspend worker thread context.
    
    CRs-Fixed: 283994
    Change-Id: I6b54af8432e58fd5442817b7388ce2e0b83354b6
    Signed-off-by: Pratik Patel <pratikp@codeaurora.org>
    (cherry picked from commit 8564b5ebeafa2be276e0004d7d32bd101642fb3d)
    
    Conflicts:
    
    	kernel/power/process.c

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -44,6 +44,8 @@ static DEFINE_SPINLOCK(list_lock);
 static LIST_HEAD(inactive_locks);
 static struct list_head active_wake_locks[WAKE_LOCK_TYPE_COUNT];
 static int current_event_num;
+static struct workqueue_struct *suspend_sys_sync_work_queue;
+static DECLARE_COMPLETION(suspend_sys_sync_comp);
 struct workqueue_struct *suspend_work_queue;
 struct wake_lock main_wake_lock;
 suspend_state_t requested_suspend_state = PM_SUSPEND_MEM;
@@ -266,6 +268,62 @@ long has_wake_lock(int type)
 	return ret;
 }
 
+
+static void suspend_sys_sync(struct work_struct *work)
+{
+	if (debug_mask & DEBUG_SUSPEND)
+		pr_info("PM: Syncing filesystems...\n");
+
+	sys_sync();
+
+	if (debug_mask & DEBUG_SUSPEND)
+		pr_info("sync done.\n");
+}
+static DECLARE_WORK(suspend_sys_sync_work, suspend_sys_sync);
+
+void suspend_sys_sync_queue(void)
+{
+	queue_work(suspend_sys_sync_work_queue, &suspend_sys_sync_work);
+}
+
+static bool suspend_sys_sync_abort;
+static void suspend_sys_sync_handler(unsigned long);
+static DEFINE_TIMER(suspend_sys_sync_timer, suspend_sys_sync_handler, 0, 0);
+/* value should be less then half of input event wake lock timeout value
+ * which is currently set to 5*HZ (see drivers/input/evdev.c)
+ */
+#define SUSPEND_SYS_SYNC_TIMEOUT (HZ/4)
+static void suspend_sys_sync_handler(unsigned long arg)
+{
+	if (workqueue_empty(suspend_sys_sync_work_queue)) {
+		del_timer(&suspend_sys_sync_timer);
+		complete(&suspend_sys_sync_comp);
+	} else if (has_wake_lock(WAKE_LOCK_SUSPEND)) {
+		suspend_sys_sync_abort = true;
+		del_timer(&suspend_sys_sync_timer);
+		complete(&suspend_sys_sync_comp);
+	} else {
+		mod_timer(&suspend_sys_sync_timer, jiffies +
+				SUSPEND_SYS_SYNC_TIMEOUT);
+	}
+}
+
+int suspend_sys_sync_wait(void)
+{
+	suspend_sys_sync_abort = false;
+	if (!workqueue_empty(suspend_sys_sync_work_queue)) {
+		mod_timer(&suspend_sys_sync_timer, jiffies +
+				SUSPEND_SYS_SYNC_TIMEOUT);
+		wait_for_completion(&suspend_sys_sync_comp);
+	}
+	if (suspend_sys_sync_abort) {
+		pr_info("suspend aborted....while waiting for sys_sync\n");
+		return -EAGAIN;
+	}
+
+	return 0;
+}
+
 static void suspend(struct work_struct *work)
 {
 	int ret;
@@ -278,7 +336,7 @@ static void suspend(struct work_struct *work)
 	}
 
 	entry_event_num = current_event_num;
-	sys_sync();
+	suspend_sys_sync_queue();
 	if (debug_mask & DEBUG_SUSPEND)
 		pr_info("suspend: enter suspend\n");
 	ret = pm_suspend(requested_suspend_state);
@@ -557,6 +615,14 @@ static int __init wakelocks_init(void)
 		goto err_platform_driver_register;
 	}
 
+	INIT_COMPLETION(suspend_sys_sync_comp);
+	suspend_sys_sync_work_queue =
+		create_singlethread_workqueue("suspend_sys_sync");
+	if (suspend_sys_sync_work_queue == NULL) {
+		ret = -ENOMEM;
+		goto err_suspend_sys_sync_work_queue;
+	}
+
 	suspend_work_queue = create_singlethread_workqueue("suspend");
 	if (suspend_work_queue == NULL) {
 		ret = -ENOMEM;
@@ -570,6 +636,7 @@ static int __init wakelocks_init(void)
 
 	return 0;
 
+err_suspend_sys_sync_work_queue:
 err_suspend_work_queue:
 	platform_driver_unregister(&power_driver);
 err_platform_driver_register:
@@ -589,6 +656,7 @@ static void  __exit wakelocks_exit(void)
 	remove_proc_entry("wakelocks", NULL);
 #endif
 	destroy_workqueue(suspend_work_queue);
+	destroy_workqueue(suspend_sys_sync_work_queue);
 	platform_driver_unregister(&power_driver);
 	platform_device_unregister(&power_device);
 	wake_lock_destroy(&unknown_wakeup);
