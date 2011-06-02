255d011443a6ca0743b06456450a9afebefb6bbe
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2011-06-02 10:05:21 -0700

    mfd: pm8921-core: Use SINGLE_IRQ_RESOURCE wherever possible
    The charger cell introduced SINGLE_IRQ_RESOURCE. Use it for other cells
    too.
    
    Change-Id: I3e53810b792216135453baef229101f8fed98ea9
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/mfd/pm8921-core.c
+++ drivers/mfd/pm8921-core.c
@@ -124,11 +124,7 @@ static struct mfd_cell mpp_cell __devinitdata = {
 };
 
 static const struct resource rtc_cell_resources[] __devinitconst = {
-	[0] = {
-		.start  = PM8921_RTC_ALARM_IRQ,
-		.end    = PM8921_RTC_ALARM_IRQ,
-		.flags  = IORESOURCE_IRQ,
-	},
+	[0] = SINGLE_IRQ_RESOURCE(NULL, PM8921_RTC_ALARM_IRQ),
 	[1] = {
 		.name   = "pmic_rtc_base",
 		.start  = PM8921_RTC_BASE,
@@ -145,16 +141,8 @@ static struct mfd_cell rtc_cell __devinitdata = {
 };
 
 static const struct resource resources_pwrkey[] __devinitconst = {
-	{
-		.start  = PM8921_PWRKEY_REL_IRQ,
-		.end    = PM8921_PWRKEY_REL_IRQ,
-		.flags  = IORESOURCE_IRQ,
-	},
-	{
-		.start  = PM8921_PWRKEY_PRESS_IRQ,
-		.end    = PM8921_PWRKEY_PRESS_IRQ,
-		.flags  = IORESOURCE_IRQ,
-	},
+	SINGLE_IRQ_RESOURCE(NULL, PM8921_PWRKEY_REL_IRQ),
+	SINGLE_IRQ_RESOURCE(NULL, PM8921_PWRKEY_PRESS_IRQ),
 };
 
 static struct mfd_cell pwrkey_cell __devinitdata = {
@@ -165,16 +153,8 @@ static struct mfd_cell pwrkey_cell __devinitdata = {
 };
 
 static const struct resource resources_keypad[] = {
-	{
-		.start  = PM8921_KEYPAD_IRQ,
-		.end    = PM8921_KEYPAD_IRQ,
-		.flags  = IORESOURCE_IRQ,
-	},
-	{
-		.start  = PM8921_KEYSTUCK_IRQ,
-		.end    = PM8921_KEYSTUCK_IRQ,
-		.flags  = IORESOURCE_IRQ,
-	},
+	SINGLE_IRQ_RESOURCE(NULL, PM8921_KEYPAD_IRQ),
+	SINGLE_IRQ_RESOURCE(NULL, PM8921_KEYSTUCK_IRQ),
 };
 
 static struct mfd_cell keypad_cell __devinitdata = {
