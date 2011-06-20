5058cac360372a6e4bc368dac946d81265f65773
Peter Krystad <pkrystad@codeaurora.org>
2011-06-20 13:42:10 -0700

    Bluetooth: Set A2MP CID as before calling kernel_connect()
    Changes made for BT 4.0 mean the A2MP CID must be set before
    kernel_bind() and kernel_connect() are used to create the A2MP
    kernel socket.
    
    Change-Id: Ie0d471701f0f323bd589b1f92b3044ab91537ec8
    Signed-off-by: Peter Krystad <pkrystad@codeaurora.org>

--- include/net/bluetooth/l2cap.h
+++ include/net/bluetooth/l2cap.h
@@ -696,7 +696,7 @@ void l2cap_ertm_shutdown(struct sock *sk);
 void l2cap_ertm_recv_done(struct sock *sk);
 
 void l2cap_fixed_channel_config(struct sock *sk, struct l2cap_options *opt,
-				u16 cid, u16 mps);
+				u16 mps);
 
 void l2cap_recv_deferred_frame(struct sock *sk, struct sk_buff *skb);
 
