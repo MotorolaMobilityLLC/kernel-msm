b5bd0a49b05a8bbdce30998221a3ebcd34971c06
Rohit Vaswani <rvaswani@codeaurora.org>
2012-01-05 20:26:40 -0800

    arm: gic: Configure the GIC to run in secure mode
    Configure the GIC to run in secure mode and handle
    secure as well as non-secure interrupts. This patch
    adds an API to configure an IRQ as a secure IRQ so that
    it can be treated as an FIQ in the secure mode.
    
    Change-Id: Ic3321e76c95a4c10d6287ba418e84623e7004cb1
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>
    Signed-off-by: Sathish Ambley <sambley@codeaurora.org>
    (cherry picked from commit 26e44869e1e730ec7434e899dfd5857530b63415)
    
    Conflicts:
    
    	arch/arm/common/gic.c
    	arch/arm/include/asm/hardware/gic.h

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -22,6 +22,7 @@
 
 #define GIC_DIST_CTRL			0x000
 #define GIC_DIST_CTR			0x004
+#define GIC_DIST_ISR			0x080
 #define GIC_DIST_ENABLE_SET		0x100
 #define GIC_DIST_ENABLE_CLEAR		0x180
 #define GIC_DIST_PENDING_SET		0x200
@@ -45,6 +46,8 @@ void gic_secondary_init(unsigned int);
 void gic_handle_irq(struct pt_regs *regs);
 void gic_cascade_irq(unsigned int gic_nr, unsigned int irq);
 void gic_raise_softirq(const struct cpumask *mask, unsigned int irq);
+void gic_set_irq_secure(unsigned int irq);
+
 static inline void gic_init(unsigned int nr, int start,
 			    void __iomem *dist , void __iomem *cpu)
 {
