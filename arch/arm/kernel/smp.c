b1bf8788709136d36544febe912fdf54cf4b79fa
Marc Zyngier <marc.zyngier@arm.com>
2012-01-11 17:25:17 +0000

    ARM: local timers: Add A15 architected timer support-MSM
    Add support for the A15 generic timer and clocksource.
    As the timer generates interrupts on a different PPI depending
    on the execution mode (normal or secure), it is possible to
    register two different PPIs.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    
    Conflicts:
            [Integrate to the recent patch which has changes to
             local timer registration mechanism.
    
             This fixes the crash seen during hotplug operations
             where after a secondary CPU is brought back online,
             the clock event device setup was happening as part
             of the online notification mechanism which was too
             late. With this change in the local timer mechanims,
             the clock event device is now setup as part of the
             secondary CPU boot initialization making it available
             early enough for use.
    
             Update the board file with the appropriate changes in
             the argument for timer registration.]
    	arch/arm/Kconfig
    	arch/arm/include/asm/arch_timer.h
    	arch/arm/kernel/arch_timer.c
    	arch/arm/kernel/smp.c
    
    Change-Id: I0bc80097c145fb2aac2150db0c5dff3c5e215a58
    Signed-off-by: Sathish Ambley <sambley@codeaurora.org>
    (cherry picked from commit df590ccd9d8210cc3e059671efad06dab7e70d4c)
    
    Change-Id: Iedac55e097b8d983c42799b98f2d11f7b1a95f04
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -241,8 +241,6 @@ static void __cpuinit smp_store_cpu_info(unsigned int cpuid)
 	store_cpu_topology(cpuid);
 }
 
-static void percpu_timer_setup(void);
-
 /*
  * This is the secondary CPU boot entry.  We're using this CPUs
  * idle thread stack, but a set of temporary page tables.
@@ -464,7 +462,7 @@ int local_timer_register(struct local_timer_ops *ops)
 }
 #endif
 
-static void __cpuinit percpu_timer_setup(void)
+void __cpuinit percpu_timer_setup(void)
 {
 	unsigned int cpu = smp_processor_id();
 	struct clock_event_device *evt = &per_cpu(percpu_clockevent, cpu);
