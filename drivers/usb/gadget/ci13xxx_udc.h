5cf762fb2a22d0ad38f2aab1cfdfa111a3d2e590
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2012-02-25 14:35:26 +0530

    USB: gadget: Add OTG support for ci13xxx_udc
    Add support for processing OTG feature requests like b_hnp_enable,
    a_hnp_support, test mode features like otg_srp_reqd, otg_hnp_reqd and
    HNP polling.
    
    Change-Id: Icb2a8c32c3b566638e48d89bac1aeef453d0e8fa
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.h
+++ drivers/usb/gadget/ci13xxx_udc.h
@@ -109,6 +109,7 @@ struct ci13xxx_udc_driver {
 #define CI13XXX_PULLUP_ON_VBUS		BIT(2)
 #define CI13XXX_DISABLE_STREAMING	BIT(3)
 #define CI13XXX_ZERO_ITC		BIT(4)
+#define CI13XXX_IS_OTG			BIT(5)
 
 #define CI13XXX_CONTROLLER_RESET_EVENT			0
 	void	(*notify_event) (struct ci13xxx *udc, unsigned event);
