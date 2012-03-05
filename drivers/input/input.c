4ed6ffe55c85d266abd6c5038f3eda91f4c4413e
lianwei.wang <lian-wei.wang@motorola.com>
2012-03-05 15:12:50 +0800

    input: avoid sending sync_report when no key event passed PROPAGATED_from: (CR)
    Change-Id: Idb2afe7df767fe00668bb76944513a52da9eda19
    Reviewed-on: http://gerrit.pcs.mot.com/618020
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -614,15 +614,18 @@ EXPORT_SYMBOL(input_close_device);
 static void input_dev_release_keys(struct input_dev *dev)
 {
 	int code;
+	int pass = false;
 
 	if (is_event_supported(EV_KEY, dev->evbit, EV_MAX)) {
 		for (code = 0; code <= KEY_MAX; code++) {
 			if (is_event_supported(code, dev->keybit, KEY_MAX) &&
 			    __test_and_clear_bit(code, dev->key)) {
 				input_pass_event(dev, EV_KEY, code, 0);
+				pass = true;
 			}
 		}
-		input_pass_event(dev, EV_SYN, SYN_REPORT, 1);
+		if (pass)
+			input_pass_event(dev, EV_SYN, SYN_REPORT, 1);
 	}
 }
 
