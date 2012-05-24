a2fc65508912792255df4b40eabc2bdb6b2fe9ee
Maya Erez <merez@codeaurora.org>
2012-05-24 23:46:29 +0300

    mmc: card: Adding support for sanitize in eMMC 4.5
    This feature delete the unmap memory region of the eMMC card,
    by writing to a specific register in the EXT_CSD
    unmap region is the memory region that were previously deleted
    (by erase, trim or discard operation)
    
    Change-Id: I7f1df8e87fb782979fd90e064eca00704caca54b
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -244,6 +244,7 @@ struct mmc_host {
 #define MMC_CAP2_PACKED_WR	(1 << 11)	/* Allow packed write */
 #define MMC_CAP2_PACKED_CMD	(MMC_CAP2_PACKED_RD | \
 				 MMC_CAP2_PACKED_WR) /* Allow packed commands */
+#define MMC_CAP2_SANITIZE	(1 << 13)		/* Support Sanitize */
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 	unsigned int        power_notify_type;
 #define MMC_HOST_PW_NOTIFY_NONE		0
