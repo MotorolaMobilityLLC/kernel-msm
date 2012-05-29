d3bcae5f8d8d712e6ddf89d5aa2a3f83f43571b3
Yong Zhang <yong.zhang0@gmail.com>
2012-05-29 15:16:05 +0800

    smp: Remove ipi_call_lock[_irq]()/ipi_call_unlock[_irq]()
    There is no user of those APIs anymore, just remove it.
    
    Signed-off-by: Yong Zhang <yong.zhang0@gmail.com>
    Cc: ralf@linux-mips.org
    Cc: sshtylyov@mvista.com
    Cc: david.daney@cavium.com
    Cc: nikunj@linux.vnet.ibm.com
    Cc: paulmck@linux.vnet.ibm.com
    Cc: axboe@kernel.dk
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1338275765-3217-11-git-send-email-yong.zhang0@gmail.com
    Acked-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Change-Id: I66367a00ea0f0be74817e238116762ba8f89d84c
    Reviewed-on: http://gerrit.pcs.mot.com/537987
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Guo-Jian Chen <a21757@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <cik009@motorola.com>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -89,10 +89,6 @@ int smp_call_function_any(const struct cpumask *mask,
 void __init call_function_init(void);
 void generic_smp_call_function_single_interrupt(void);
 void generic_smp_call_function_interrupt(void);
-void ipi_call_lock(void);
-void ipi_call_unlock(void);
-void ipi_call_lock_irq(void);
-void ipi_call_unlock_irq(void);
 #else
 static inline void call_function_init(void) { }
 #endif
