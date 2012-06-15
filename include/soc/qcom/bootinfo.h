b864b5662f27932ee39196da4aeafdcf45d50484
Ali Zhang <a21152@motorola.com>
2012-06-15 10:52:17 -0500

    IKHSS7-41225 Display BL build info in /proc/bootinfo
    Change-Id: I81c1f460077c7a06d0c72d39debde88e00f41275
    Signed-off-by: Ali Zhang <a21152@motorola.com>
    Change-Id: Ie2fad86945bfbaf1018532de02b8a265a6d1f346
    Reviewed-on: http://gerrit.pcs.mot.com/441003
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>
    Reviewed-by: Qing Chang <a21550@motorola.com>

--- include/soc/qcom/bootinfo.h
+++ include/soc/qcom/bootinfo.h
@@ -97,6 +97,8 @@ void bi_set_battery_status_at_boot(u16 battery_status_at_boot);
 u32  bi_cid_recover_boot(void);
 void bi_set_cid_recover_boot(u32 cid_recover_boot);
 
+void bi_add_bl_build_sig(char *bld_sig);
+
 #endif /* defined(__KERNEL__) && defined(CONFIG_BOOTINFO) */
 
 #endif
