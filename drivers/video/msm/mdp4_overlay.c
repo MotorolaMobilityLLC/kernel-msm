06ce948e5ca46635536b539942fe3891e124ca7d
Devin Kim <dojip.kim@lge.com>
2012-06-14 18:32:32 -0700

    mako: temoprarily blocked mdp error message
    blocked until enabling LCD. if not, too much errors in console
    
    Change-Id: I38bc1bec4559f9d4d830d8f6e024cb5a121abf35

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3452,6 +3452,14 @@ static struct msm_iommu_ctx msm_iommu_split_ctx_names[] = {
 	},
 };
 
+static int mdp_iommu_fault_handler(struct iommu_domain *domain,
+	struct device *dev, unsigned long iova, int flags)
+{
+	/* FIXME: temporarily blocked until enabling LCD */
+	/* pr_err("MDP IOMMU page fault: iova 0x%lx", iova); */
+	return 0;
+}
+
 void mdp4_iommu_attach(void)
 {
 	static int done;
