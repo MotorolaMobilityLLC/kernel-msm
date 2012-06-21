6a2109d1ca1956395658d99e20f8636f0cf83631
Devin Kim <dojip.kim@lge.com>
2012-06-21 14:09:34 -0700

    vibrator: add immersion touchsense vibrator
    Change-Id: I543987642578f35a3a923a2d1368bee0997fdec1

---
+++ drivers/misc/tspdrv/VibeOSKernelLinuxTime.c
@@ -0,0 +1,237 @@
+/*
+** =========================================================================
+** File:
+**     VibeOSKernelLinuxTime.c
+**
+** Description:
+**     Time helper functions for Linux.
+**
+** Portions Copyright (c) 2008-2011 Immersion Corporation. All Rights Reserved.
+**
+** This file contains Original Code and/or Modifications of Original Code
+** as defined in and that are subject to the GNU Public License v2 -
+** (the 'License'). You may not use this file except in compliance with the
+** License. You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA or contact
+** TouchSenseSales@immersion.com.
+**
+** The Original Code and all software distributed under the License are
+** distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+** EXPRESS OR IMPLIED, AND IMMERSION HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+** INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS
+** FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT. Please see
+** the License for the specific language governing rights and limitations
+** under the License.
+** =========================================================================
+*/
+
+#error "Please read the following statement"
+/*
+** Kernel standard software timer is used as an example but another type
+** of timer (such as HW timer or high-resolution software timer) might be used
+** to achieve the 5ms required rate.
+*/
+#error "End of statement"
+
+#if (HZ != 1000)
+#error The Kernel timer is not configured at 1ms. Please update TIMER_INCR to generate a proper 5ms timer.
+#endif
+
+#include <linux/mutex.h>
+
+#define TIMER_INCR              5       /* run timer at 5 jiffies (== 5ms) */
+#define WATCHDOG_TIMEOUT        10      /* 10 timer cycles = 50ms */
+
+/* For compatibility with older Kernels */
+#ifndef DEFINE_SEMAPHORE
+#define DEFINE_SEMAPHORE(name) \
+	struct semaphore name = __SEMAPHORE_INITIALIZER(name, 1)
+#endif
+
+/* Global variables */
+static bool g_bTimerStarted = false;
+static struct timer_list g_timerList;
+static int g_nWatchdogCounter = 0;
+
+DEFINE_SEMAPHORE(g_hMutex);
+
+/* Forward declarations */
+static void VibeOSKernelLinuxStartTimer(void);
+static void VibeOSKernelLinuxStopTimer(void);
+static int VibeOSKernelProcessData(void* data);
+#define VIBEOSKERNELPROCESSDATA
+
+static inline int VibeSemIsLocked(struct semaphore *lock)
+{
+#if ((LINUX_VERSION_CODE & 0xFFFFFF) < KERNEL_VERSION(2,6,27))
+	return atomic_read(&lock->count) != 1;
+#else
+	return (lock->count) != 1;
+#endif
+}
+
+static void tsp_timer_interrupt(unsigned long param)
+{
+	/* Scheduling next timeout value right away */
+	mod_timer(&g_timerList, jiffies + TIMER_INCR);
+
+	if(g_bTimerStarted) {
+		if (VibeSemIsLocked(&g_hMutex))
+			up(&g_hMutex);
+	}
+}
+
+static int VibeOSKernelProcessData(void* data)
+{
+	int i;
+	int nActuatorNotPlaying = 0;
+
+	for (i = 0; i < NUM_ACTUATORS; i++) {
+		actuator_samples_buffer *pCurrentActuatorSample = &(g_SamplesBuffer[i]);
+
+		if (-1 == pCurrentActuatorSample->nIndexPlayingBuffer) {
+			nActuatorNotPlaying++;
+			if ((NUM_ACTUATORS == nActuatorNotPlaying) &&
+					((++g_nWatchdogCounter) > WATCHDOG_TIMEOUT)) {
+				VibeInt8 cZero[1] = {0};
+
+			/* Nothing to play for all actuators,
+			turn off the timer when we reach the watchdog tick count limit */
+				ImmVibeSPI_ForceOut_SetSamples(i, 8, 1, cZero);
+				ImmVibeSPI_ForceOut_AmpDisable(i);
+				VibeOSKernelLinuxStopTimer();
+
+				/* Reset watchdog counter */
+				g_nWatchdogCounter = 0;
+			}
+		} else {
+			/* Play the current buffer */
+			if (VIBE_E_FAIL == ImmVibeSPI_ForceOut_SetSamples(
+				pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nActuatorIndex,
+				pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nBitDepth,
+				pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nBufferSize,
+				pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].dataBuffer)) {
+				/* VIBE_E_FAIL means NAK has been handled.
+					Schedule timer to restart 5 ms from now */
+				mod_timer(&g_timerList, jiffies + TIMER_INCR);
+			}
+
+			pCurrentActuatorSample->nIndexOutputValue += pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nBufferSize;
+
+			if (pCurrentActuatorSample->nIndexOutputValue >= pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nBufferSize) {
+				/* Reach the end of the current buffer */
+				pCurrentActuatorSample->actuatorSamples[(int)pCurrentActuatorSample->nIndexPlayingBuffer].nBufferSize = 0;
+
+				/* Switch buffer */
+				(pCurrentActuatorSample->nIndexPlayingBuffer) ^= 1;
+				pCurrentActuatorSample->nIndexOutputValue = 0;
+
+				/* Finished playing, disable amp for actuator (i) */
+				if (g_bStopRequested) {
+					pCurrentActuatorSample->nIndexPlayingBuffer = -1;
+
+					ImmVibeSPI_ForceOut_AmpDisable(i);
+				}
+			}
+		}
+	}
+
+	/* If finished playing, stop timer */
+	if (g_bStopRequested) {
+		VibeOSKernelLinuxStopTimer();
+
+		/* Reset watchdog counter */
+		g_nWatchdogCounter = 0;
+
+		if (VibeSemIsLocked(&g_hMutex))
+			up(&g_hMutex);
+		return 1;   /* tell the caller this is the last iteration */
+	}
+
+	return 0;
+}
+
+static void VibeOSKernelLinuxInitTimer(void)
+{
+	/* Initialize a 5ms-timer with VibeOSKernelTimerProc as timer callback */
+	init_timer(&g_timerList);
+	g_timerList.function = tsp_timer_interrupt;
+}
+
+static void VibeOSKernelLinuxStartTimer(void)
+{
+	int i;
+	int res;
+
+	/* Reset watchdog counter */
+	g_nWatchdogCounter = 0;
+
+	if (!g_bTimerStarted) {
+		if (!VibeSemIsLocked(&g_hMutex))
+			res = down_interruptible(&g_hMutex); /* start locked */
+
+		g_bTimerStarted = true;
+
+		/* Start the timer */
+		g_timerList.expires = jiffies + TIMER_INCR;
+		add_timer(&g_timerList);
+
+		/* Don't block the write() function
+		 * after the first sample to allow the host sending
+		 * the next samples with no delay
+		 */
+		for (i = 0; i < NUM_ACTUATORS; i++) {
+			if ((g_SamplesBuffer[i].actuatorSamples[0].nBufferSize) ||
+				(g_SamplesBuffer[i].actuatorSamples[1].nBufferSize)) {
+				g_SamplesBuffer[i].nIndexOutputValue = 0;
+				return;
+			}
+		}
+	}
+
+	if (0 != VibeOSKernelProcessData(NULL))
+		return;
+
+	/*
+	** Use interruptible version of down to be safe
+	** (try to not being stuck here if the mutex is not freed for any reason)
+	*/
+	res = down_interruptible(&g_hMutex); /* wait for the mutex to be freed by the timer */
+	if (res != 0) {
+		DbgOut((KERN_INFO "VibeOSKernelLinuxStartTimer: down_interruptible interrupted by a signal.\n"));
+	}
+}
+
+static void VibeOSKernelLinuxStopTimer(void)
+{
+	int i;
+
+	if (g_bTimerStarted) {
+		g_bTimerStarted = false;
+
+		/*
+		** Stop the timer.
+		** Use del_timer vs. del_timer_sync
+		** del_timer_sync may cause a Kernel "soft lockup" on multi-CPU platforms
+		** as VibeOSKernelLinuxStopTimer is called from the timer tick handler.
+		*/
+		del_timer(&g_timerList);
+	}
+
+	/* Reset samples buffers */
+	for (i = 0; i < NUM_ACTUATORS; i++) {
+		g_SamplesBuffer[i].nIndexPlayingBuffer = -1;
+		g_SamplesBuffer[i].actuatorSamples[0].nBufferSize = 0;
+		g_SamplesBuffer[i].actuatorSamples[1].nBufferSize = 0;
+	}
+	g_bStopRequested = false;
+	g_bIsPlaying = false;
+}
+
+static void VibeOSKernelLinuxTerminateTimer(void)
+{
+	VibeOSKernelLinuxStopTimer();
+	if (VibeSemIsLocked(&g_hMutex))
+		up(&g_hMutex);
+}
---
+++ drivers/misc/tspdrv/tspdrv.h
@@ -0,0 +1,106 @@
+/*
+** =========================================================================
+** File:
+**     tspdrv.h
+**
+** Description:
+**     Constants and type definitions for the TouchSense Kernel Module.
+**
+** Portions Copyright (c) 2008-2010 Immersion Corporation. All Rights Reserved.
+**
+** This file contains Original Code and/or Modifications of Original Code
+** as defined in and that are subject to the GNU Public License v2 -
+** (the 'License'). You may not use this file except in compliance with the
+** License. You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA or contact
+** TouchSenseSales@immersion.com.
+**
+** The Original Code and all software distributed under the License are
+** distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+** EXPRESS OR IMPLIED, AND IMMERSION HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+** INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS
+** FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT. Please see
+** the License for the specific language governing rights and limitations
+** under the License.
+** =========================================================================
+*/
+
+#ifndef _TSPDRV_H
+#define _TSPDRV_H
+
+/* Constants */
+#define MODULE_NAME                         "tspdrv"
+#define TSPDRV                              "/dev/"MODULE_NAME
+#define TSPDRV_MAGIC_NUMBER                 0x494D4D52
+#define TSPDRV_STOP_KERNEL_TIMER            _IO(TSPDRV_MAGIC_NUMBER & 0xFF, 1)
+/*
+** Obsolete IOCTL command
+** #define TSPDRV_IDENTIFY_CALLER           _IO(TSPDRV_MAGIC_NUMBER & 0xFF, 2)
+*/
+#define TSPDRV_ENABLE_AMP                   _IO(TSPDRV_MAGIC_NUMBER & 0xFF, 3)
+#define TSPDRV_DISABLE_AMP                  _IO(TSPDRV_MAGIC_NUMBER & 0xFF, 4)
+#define TSPDRV_GET_NUM_ACTUATORS            _IO(TSPDRV_MAGIC_NUMBER & 0xFF, 5)
+#define VIBE_MAX_DEVICE_NAME_LENGTH			64
+#define SPI_HEADER_SIZE                     3   /* DO NOT CHANGE - SPI buffer header size */
+#define VIBE_OUTPUT_SAMPLE_SIZE             50  /* DO NOT CHANGE - maximum number of samples */
+
+/* Type definitions */
+#ifdef __KERNEL__
+typedef int8_t		VibeInt8;
+typedef u_int8_t	VibeUInt8;
+typedef int16_t		VibeInt16;
+typedef u_int16_t	VibeUInt16;
+typedef int32_t		VibeInt32;
+typedef u_int32_t	VibeUInt32;
+typedef u_int8_t	VibeBool;
+typedef VibeInt32	VibeStatus;
+
+typedef struct {
+	VibeUInt8 nActuatorIndex;  /* 1st byte is actuator index */
+	VibeUInt8 nBitDepth;       /* 2nd byte is bit depth */
+	VibeUInt8 nBufferSize;     /* 3rd byte is data size */
+	VibeUInt8 dataBuffer[VIBE_OUTPUT_SAMPLE_SIZE];
+} samples_buffer;
+
+typedef struct {
+	VibeInt8 nIndexPlayingBuffer;
+	VibeUInt8 nIndexOutputValue;
+	samples_buffer actuatorSamples[2]; /* Use 2 buffers to receive samples from user mode */
+} actuator_samples_buffer;
+
+#endif
+
+/* Error and Return value codes */
+#define VIBE_S_SUCCESS                      0   /* Success */
+#define VIBE_E_FAIL                         -4  /* Generic error */
+
+#if defined(VIBE_RECORD) && defined(VIBE_DEBUG)
+void _RecorderInit(void);
+void _RecorderTerminate(void);
+void _RecorderReset(int nActuator);
+void _Record(int actuatorIndex, const char *format,...);
+#endif
+
+/* Kernel Debug Macros */
+#ifdef __KERNEL__
+    #if 1 // def VIBE_DEBUG
+        #define DbgOut(_x_) printk _x_
+    #else   /* VIBE_DEBUG */
+        #define DbgOut(_x_)
+    #endif  /* VIBE_DEBUG */
+
+    #if defined(VIBE_RECORD) && defined(VIBE_DEBUG)
+        #define DbgRecorderInit(_x_) _RecorderInit _x_
+        #define DbgRecorderTerminate(_x_) _RecorderTerminate _x_
+        #define DbgRecorderReset(_x_) _RecorderReset _x_
+        #define DbgRecord(_x_) _Record _x_
+    #else /* defined(VIBE_RECORD) && defined(VIBE_DEBUG) */
+        #define DbgRecorderInit(_x_)
+        #define DbgRecorderTerminate(_x_)
+        #define DbgRecorderReset(_x_)
+        #define DbgRecord(_x_)
+    #endif /* defined(VIBE_RECORD) && defined(VIBE_DEBUG) */
+#endif  /* __KERNEL__ */
+
+#endif  /* _TSPDRV_H */
