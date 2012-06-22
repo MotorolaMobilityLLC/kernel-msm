d7cac050384541b07d070abfdb5376d799f52da4
Peter Zijlstra <a.p.zijlstra@chello.nl>
2012-06-22 15:52:09 +0200

    sched/nohz: Rewrite and fix load-avg computation -- again
    commit 5167e8d5417bf5c322a703d2927daec727ea40dd upstream.
    
    Thanks to Charles Wang for spotting the defects in the current code:
    
     - If we go idle during the sample window -- after sampling, we get a
       negative bias because we can negate our own sample.
    
     - If we wake up during the sample window we get a positive bias
       because we push the sample to a known active period.
    
    So rewrite the entire nohz load-avg muck once again, now adding
    copious documentation to the code.
    
    Reported-and-tested-by: Doug Smythies <dsmythies@telus.net>
    Reported-and-tested-by: Charles Wang <muming.wq@gmail.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1340373782.18025.74.camel@twins
    [ minor edits ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 7490d0a4cfefa16f9d8ce636eb5b2e13d2432db3)
    
    Conflicts:
    
    	include/linux/sched.h
    
    Change-Id: I3fb4c56b4adb7a51694c844ec11694593e136306
    Reviewed-on: http://gerrit.pcs.mot.com/519219
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    (cherry picked from commit cf62fb1fc64b3a82bdea9c66f38a1c35cfac567d)

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -407,6 +407,7 @@ static void tick_nohz_stop_sched_tick(struct tick_sched *ts)
 		 */
 		if (!ts->tick_stopped) {
 			select_nohz_load_balancer(1);
+			calc_load_enter_idle();
 
 			ts->idle_tick = hrtimer_get_expires(&ts->sched_timer);
 			ts->tick_stopped = 1;
@@ -596,6 +597,7 @@ void tick_nohz_idle_exit(void)
 		account_idle_ticks(ticks);
 #endif
 
+	calc_load_exit_idle();
 	touch_softlockup_watchdog();
 	/*
 	 * Cancel the scheduled timer and restore the tick
