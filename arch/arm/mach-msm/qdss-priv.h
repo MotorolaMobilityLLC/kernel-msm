fac0b6244233e042a58a7c51d1fae747e8b7c22f
Devin Kim <dojip.kim@lge.com>
2012-07-02 10:20:42 -0700

    Revert "[ARM] msm: fix the build breakage when unselected HAVE_HW_BREAKPOINT"
    This reverts commit ba9f6df0f93d227ac46dd45fe4ba532126b5b203.

--- arch/arm/mach-msm/qdss-priv.h
+++ arch/arm/mach-msm/qdss-priv.h
@@ -48,9 +48,6 @@
 #define ARM_DEBUG_ARCH_V7B	(0x3)
 /* DBGv7 with all CP14 registers implemented */
 #define ARM_DEBUG_ARCH_V7	(0x4)
-#ifndef CONFIG_HAVE_HW_BREAKPOINT
-#define ARM_DEBUG_ARCH_V7_1	(0x5)
-#endif
 #define ETM_ARCH_V3_3		(0x23)
 #define PFT_ARCH_V1_1		(0x31)
 
