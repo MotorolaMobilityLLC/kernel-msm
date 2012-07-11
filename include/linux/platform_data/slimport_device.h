9b3c4f5823bcd4b0a2036dbd2444b0d60aca0328
choongryeol.lee <choongryeol.lee@lge.com>
2012-07-11 16:35:09 -0700

    slimport: add slimport anx7808 driver
    add slimport anx7808 driver
    
    Change-Id: I133d5272172532a52e27e08779b97789c104173a

---
+++ include/linux/platform_data/slimport_device.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright(c) 2012, LG Electronics Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __SLIMPORT_DEVICE
+#define __SLIMPORT_DEVICE
+
+struct anx7808_platform_data
+{
+	int gpio_p_dwn;
+	int gpio_reset;
+	int gpio_int;
+	int gpio_cbl_det;
+
+	int (*dvdd_power)(bool on);
+	int (*avdd_power)(bool on);
+};
+
+#endif
