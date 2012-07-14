32e320515d9c4b02af14b88ba7200562c1111709
choongryeol.lee <choongryeol.lee@lge.com>
2012-07-13 17:53:47 -0700

    Revert "mako: temoprarily blocked mdp error message"
    This reverts commit 0fca8a4db86917c28b9c8161b1ae5ee73743605d.

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3213,8 +3213,7 @@ static struct {
 static int mdp_iommu_fault_handler(struct iommu_domain *domain,
 	struct device *dev, unsigned long iova, int flags)
 {
-	/* FIXME: temporarily blocked until enabling LCD */
-	/* pr_err("MDP IOMMU page fault: iova 0x%lx", iova); */
+	pr_err("MDP IOMMU page fault: iova 0x%lx", iova);
 	return 0;
 }
 
