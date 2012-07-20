476bbd73f8efd47c2856be4310cf10800bb01b89
Devin Kim <dojip.kim@lge.com>
2012-07-19 18:11:11 -0700

    msm: reduce verbose messages from runtime suspend/resume
    change the message level from info into debug
    
    Change-Id: Ic71c11a653c53bf65b73c1e4ee639a83ea7588b6

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -674,7 +674,7 @@ static int msm_hsic_suspend(struct msm_hsic_hcd *mehci)
 
 	wake_unlock(&mehci->wlock);
 
-	dev_info(mehci->dev, "HSIC-USB in low power mode\n");
+	dev_dbg(mehci->dev, "HSIC-USB in low power mode\n");
 
 	return 0;
 }
@@ -765,7 +765,7 @@ skip_phy_resume:
 		pm_runtime_put_noidle(mehci->dev);
 	}
 
-	dev_info(mehci->dev, "HSIC-USB exited from low power mode\n");
+	dev_dbg(mehci->dev, "HSIC-USB exited from low power mode\n");
 
 	return 0;
 }
