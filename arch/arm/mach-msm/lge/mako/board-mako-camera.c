02f9c2ae042d57550e47e71f58c7d847769accb3
Devin Kim <dojip.kim@lge.com>
2012-07-31 10:51:16 -0700

    mako: msm: camera: Add live snapshot entry to bus vector table.
    msm: camera: Add live snapshot entry to bus vector table.
    commit	1d1d3075e4f5be4d63c2f0271bc9a99d82804a7b
    
    The current camera bus vector table does not have an entry
    for Live snapshot usecase. Add this entry into the bus vector
    table. When user takes a live snapshot during video recording,
    just switch to this new entry and when its done, switch back
    to video entry. Also, correct the .ab value for video usecase
    since the current value is not sufficient for all the usecases.
    
    Change-Id: I0894e3e6554a9d3910d300c8ac17156d21b00a51

--- arch/arm/mach-msm/lge/mako/board-mako-camera.c
+++ arch/arm/mach-msm/lge/mako/board-mako-camera.c
@@ -216,7 +216,7 @@ static struct msm_bus_vectors cam_video_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_VFE,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab  = 140451840,
+		.ab  = 274406400,
 		.ib  = 561807360,
 	},
 	{
@@ -275,6 +275,27 @@ static struct msm_bus_vectors cam_zsl_vectors[] = {
 	},
 };
 
+static struct msm_bus_vectors cam_video_ls_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_VFE,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab  = 348192000,
+		.ib  = 617103360,
+	},
+	{
+		.src = MSM_BUS_MASTER_VPE,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab  = 206807040,
+		.ib  = 488816640,
+	},
+	{
+		.src = MSM_BUS_MASTER_JPEG_ENC,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab  = 540000000,
+		.ib  = 1350000000,
+	},
+};
+
 static struct msm_bus_paths cam_bus_client_config[] = {
 	{
 		ARRAY_SIZE(cam_init_vectors),
@@ -295,6 +316,10 @@ static struct msm_bus_paths cam_bus_client_config[] = {
 	{
 		ARRAY_SIZE(cam_zsl_vectors),
 		cam_zsl_vectors,
+	},
+	{
+		ARRAY_SIZE(cam_video_ls_vectors),
+		cam_video_ls_vectors,
 	},
 };
 
