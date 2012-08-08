f6da488cea158df99f4e48df73e9ecfd5ba5bd88
Jason Hrycay <jason.hrycay@motorola.com>
2012-08-07 23:36:41 -0500

    input: touch: add melfas driver implementation
    Port Melfas touch driver from 3.0 kernel. Resolves the following
    commits:
    
      a330c7c (CR): add firmware upload on demand
      450e5d3 (CR): input: touch: melfas: fail gracefully...
      a6e8459 (CR) add device attribute sysfs file for IC...
      15f57cb (CR) Use gpio toggling to suspend/resume
      0adca7f (CR) Only upgrade if the firmware on the...
      b54a57b (CR): msm8960: touch: melfas: error handling fixups
      c977317 (CR) : instrument touch for performance metrics
      c244830 (CR) adjust driver for to work in Mot environment
      db6f893 (CR) Use open source driver (includes fw dwnld)
      7785a20 (CR) replace smbus functions with i2c function ...
      7955c99 (CR): Fix the Vanquish touch can not use
      8d5da28 (CR) Melfas touch sysfs interfaces for recovery ...
      70c9a454 (CR): ICS Merge for Touch Drivers
      770017f (CR): drivers: melfas-touch: enable/disable inte...
      5ddd5cc (CR): add melfas touch driver
    
    Conflicts:
    
    	drivers/input/touchscreen/Makefile
    
    Change-Id: I02752d8e5d6617393e7c04245a025a5eda9e35ae
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/482589
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    (cherry picked from commit e50a64c0a284f0b7d83a6f9b2fc029627d53997f)
    (cherry picked from commit 53997552f8fbf7b40bd0c8fef62d272204115944)
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

---
+++ include/linux/melfas100_ts.h
@@ -0,0 +1,55 @@
+/*
+ * include/linux/melfas100_ts.h
+ *
+ * Copyright (C) 2011 Motorola Mobility, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_MELFAS100_TS_H
+#define _LINUX_MELFAS100_TS_H
+
+#define MELFAS_TS_NAME "melfas-ts"
+
+#define TS_FLIP_X           (1 << 0)
+#define TS_FLIP_Y           (1 << 1)
+
+#define MELFAS_IRQ_ENABLED_FLAG      0
+#define MELFAS_WAITING_FOR_FW_FLAG   1
+#define MELFAS_CHECKSUM_FAILED       2
+#define MELFAS_IGNORE_CHECKSUM       3
+
+#define MELFAS_TOUCH_SCL_GPIO	17
+#define MELFAS_TOUCH_SDA_GPIO	16
+#define MELFAS_TOUCH_INT_GPIO       46
+#define MELFAS_TOUCH_RESET_GPIO     50
+
+struct melfas_ts_version_info {
+	uint8_t panel_ver;
+	uint8_t hw_ver;
+	uint8_t hw_comp_grp;
+	uint8_t core_fw_ver;
+	uint8_t priv_fw_ver;
+	uint8_t pub_fw_ver;
+};
+
+
+struct melfas_ts_data {
+	struct i2c_client *client;
+	struct device *dev;
+	struct input_dev *input_dev;
+	struct touch_platform_data *pdata;
+	struct early_suspend early_suspend;
+	struct melfas_ts_version_info version_info;
+	atomic_t irq_enabled;
+	uint16_t	status;
+};
+#endif /* _LINUX_MELFAS_TS100_H */
