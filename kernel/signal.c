a997718e259de362c78d146bbd738b02b2530573
Colin Cross <ccross@android.com>
2012-08-15 13:10:04 -0700

    HACK: ARM: disable sleeping while atomic warning in do_signal
    ARM disables interrupts in do_signal, which triggers a warning in
    try_to_freeze, see details at https://lkml.org/lkml/2011/8/23/221.
    To prevent the warnings, add try_to_freeze_nowarn and call it from
    do_signal.
    
    Change-Id: If7482de21c386adc705fa1ac4ecb8c7ece5bb356
    Signed-off-by: Colin Cross <ccross@android.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2211,7 +2211,7 @@ relock:
 	 * Now that we woke up, it's crucial if we're supposed to be
 	 * frozen that we freeze now before running anything substantial.
 	 */
-	try_to_freeze();
+	try_to_freeze_nowarn();
 
 	spin_lock_irq(&sighand->siglock);
 	/*
