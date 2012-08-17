0db2d8bd7868bb72756e4247b2e6582b11e5b7ad
Devin Kim <dojip.kim@lge.com>
2012-08-17 11:53:11 -0700

    mako: clean up codes
    clean up unused codes from mako
    
    Change-Id: Ia3b811c30bf7a608293f469ecd864b919fd9a477

--- arch/arm/mach-msm/include/mach/board_lge.h
+++ arch/arm/mach-msm/include/mach/board_lge.h
@@ -50,50 +50,6 @@ typedef enum {
 
 hw_rev_type lge_get_board_revno(void);
 
-#ifdef CONFIG_LGE_PM
-/*Classified the ADC value for cable detection */
-typedef enum {
-	NO_INIT_CABLE = 0,
-	CABLE_MHL_1K,
-	CABLE_U_28P7K,
-	CABLE_28P7K,
-	CABLE_56K,
-	CABLE_100K,
-	CABLE_130K,
-	CABLE_180K,
-	CABLE_200K,
-	CABLE_220K,
-	CABLE_270K,
-	CABLE_330K,
-	CABLE_620K,
-	CABLE_910K,
-	CABLE_NONE
-} acc_cable_type;
-
-struct chg_cable_info {
-	acc_cable_type cable_type;
-	unsigned ta_ma;
-	unsigned usb_ma;
-};
-
-int lge_pm_get_cable_info(struct chg_cable_info *);
-void lge_pm_read_cable_info(void);
-acc_cable_type lge_pm_get_cable_type(void);
-unsigned lge_pm_get_ta_current(void);
-unsigned lge_pm_get_usb_current(void);
-#endif
-
-#ifdef CONFIG_LGE_PM_BATTERY_ID_CHECKER
-enum {
-	BATT_UNKNOWN,
-	BATT_DS2704_N = 17,
-	BATT_DS2704_L = 32,
-	BATT_ISL6296_N = 73,
-	BATT_ISL6296_L = 94,
-};
-extern int lge_battery_info;
-#endif
-
 #ifdef CONFIG_LCD_KCAL
 struct kcal_data {
 	int red;
@@ -108,17 +64,6 @@ struct kcal_platform_data {
 };
 #endif
 
-#ifdef CONFIG_LGE_PM
-struct pseudo_batt_info_type {
-	int mode;
-	int id;
-	int therm;
-	int temp;
-	int volt;
-	int capacity;
-	int charging;
-};
-#endif
 int __init lge_get_uart_mode(void);
 
 /* from androidboot.mode */
