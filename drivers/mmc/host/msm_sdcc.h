f9eb87ab2fcd478a88da4676e0fdd5548be7fd2d
Subhash Jadavani <subhashj@codeaurora.org>
2012-08-28 16:31:48 +0530

    mmc: msm_sdcc: add support for AUTO_CMD19 control
    SDR104 (supported by SD3.0 spec compliant SD cards) bus speed mode
    requires DLL (Delay Locked Loop for sampling clock generation) HW
    block to be tuned if clock rate is >100MHz. To help the sampling clock
    tuning SD3.0 specification has added CMD19.
    
    During card initialization, driver uses these tuning commands to search
    for optimal sampling point and then programs the DLL HW with this optimal
    sampling point.
    
    In addition, our SDCC controller's DLL HW has a mechansim of data
    tracking (CDR) which can be enabled during read transaction so the host
    will not lose the sampling point as a result of voltage and, especially,
    temperature variations. Tuning pattern commands (CMD19) can be sent
    automatically by HW (by using AUTO_CMD19 bit) before any RX transaction
    for helping the CDR to track after the correct sampling point.
    
    Although sending CMD19 automatically before every read commands is
    overhead and reduces read performance by ~1 MB/s. SD spec does
    not mandate to send these commands before every read operations.
    
    This change keeps AUTO_CMD19 disabled by default but provides sysfs
    attribute to enable or disable AUTO_CMD19 as per need.
    
    Change-Id: I9ef99a8dde9b9733ea99639cdab2b6714ac50d76
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.h
+++ drivers/mmc/host/msm_sdcc.h
@@ -400,6 +400,7 @@ struct msmsdcc_host {
 	bool io_pad_pwr_switch;
 	bool tuning_in_progress;
 	bool tuning_needed;
+	bool en_auto_cmd19;
 	bool sdio_gpio_lpm;
 	bool irq_wake_enabled;
 	struct pm_qos_request pm_qos_req_dma;
@@ -415,6 +416,7 @@ struct msmsdcc_host {
 	struct device_attribute	max_bus_bw;
 	struct device_attribute	polling;
 	struct device_attribute idle_timeout;
+	struct device_attribute auto_cmd19_attr;
 };
 
 #define MSMSDCC_VERSION_STEP_MASK	0x0000FFFF
@@ -429,6 +431,7 @@ struct msmsdcc_host {
 #define MSMSDCC_SW_RST_CFG	(1 << 6)
 #define MSMSDCC_WAIT_FOR_TX_RX	(1 << 7)
 #define MSMSDCC_IO_PAD_PWR_SWITCH	(1 << 8)
+#define MSMSDCC_AUTO_CMD19	(1 << 9)
 
 #define set_hw_caps(h, val)		((h)->hw_caps |= val)
 #define is_sps_mode(h)			((h)->hw_caps & MSMSDCC_SPS_BAM_SUP)
@@ -440,6 +443,7 @@ struct msmsdcc_host {
 #define is_sw_reset_save_config(h)	((h)->hw_caps & MSMSDCC_SW_RST_CFG)
 #define is_wait_for_tx_rx_active(h)	((h)->hw_caps & MSMSDCC_WAIT_FOR_TX_RX)
 #define is_io_pad_pwr_switch(h)	((h)->hw_caps & MSMSDCC_IO_PAD_PWR_SWITCH)
+#define is_auto_cmd19(h)		((h)->hw_caps & MSMSDCC_AUTO_CMD19)
 
 /* Set controller capabilities based on version */
 static inline void set_default_hw_caps(struct msmsdcc_host *host)
@@ -464,7 +468,8 @@ static inline void set_default_hw_caps(struct msmsdcc_host *host)
 	if (version) /* SDCC v4 and greater */
 		host->hw_caps |= MSMSDCC_AUTO_PROG_DONE |
 			MSMSDCC_SOFT_RESET | MSMSDCC_REG_WR_ACTIVE
-			| MSMSDCC_WAIT_FOR_TX_RX | MSMSDCC_IO_PAD_PWR_SWITCH;
+			| MSMSDCC_WAIT_FOR_TX_RX | MSMSDCC_IO_PAD_PWR_SWITCH
+			| MSMSDCC_AUTO_CMD19;
 
 	if ((step == 0x18) && (minor >= 3))
 		/* Version 0x06000018 need hard reset on errors */
