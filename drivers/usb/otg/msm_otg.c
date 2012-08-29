fae0afd7be3adcb910ea44d772c09d69d0d31375
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2012-08-28 19:33:49 -0700

    msm: power: update power drivers to msm-3.4 tip
    This branch is currently very outdated. Cherry pick the following
    changes in order to bring recent fixes to this branch.
    
    Change-Ids
    Ib99cd49623698d0295bc3c3cf880225495d530cb
    Ifba2bd1248e8bb47a0700dec4d321d32db4f2066
    I0fc1e116c0f5a5648f99df967bdbb41fe5a07cbc
    I8e3e9068e7b9a3bfd24c07a00bb3953180ce0741
    I2bf31a23199f942a9ac843b9940a0f7377256238
    I96b47c8603a291c41c5769fab9483d83fefb6084
    If3c5f00f4b75d03838ee556235697369fd2e47dd
    I32d50caf7b7a84db2654b69baafb40a441f46bf0
    Ibd1020113de05e8214d41ffb975d4c95d3bb1ccf
    I67c2ac422b0ba1e5eca11ffccfe7e112c2e65a3d
    I2c149e6edd7c194e7f31ece95e2003ffde935806
    I1f4b65cba149480fb4a8f91667ce45a876dff2f0
    I2605afc2d6f9f3569ec783b2d23881f31cc9038a
    I77b40f9cb8295cff1d4a24dfc7d85b0e57262f6e
    I8ec5a68cad573218a0b0bd0073277b76978417ff
    I9eaf7f21c73dd1ceaf97589cbf82b722706c48c2
    I089725e20ae20956530e570d918a87b72e59626e
    Ifa70a4daf01414dfafed7830e3a6a682c42e9364
    Iaace5bc1d0611619f11b760ea6986eadb7346da1
    I415d7831f15c0d690fd118ff190cd710a31368c6
    I1e55bbfe37c7cc6f62581af11280a565ed648c06
    I11938c569a25b026ee6b7b5125cec930676ec01d
    I07577d1aa57bfa4bee40b52c18b5bfcb1cdb7070
    Icdb0a28b5f9456135c60badf726c367b02f6d762
    I4f14b7419f7a90813650601638f6563422c13ae4
    I016a33c358be2e7a2d26202f2e9b168dd54b6d34
    I43e26a2932ab3e3d9d79bb5af7daf2364ca133b7
    I333c810568cdc656d2e13f0eeb9511857335b834
    I830bf2e679e76efe7ed9d8bb30a8d459d8b9a09a
    I08d8d319d096c050ae53d90eeac293c9b1bab2bf
    I445676d558e05a6c5e9dd1e76e80ba2f5e8cd6c1
    I9f38515f35fcddfbdc34ae4f59ffcfc6682033d4
    Ia32e581c11954163bc8ff20a7b32432a843a17b2
    I07c346475fb4a294bc8eb8da8c2048cc23394e20
    Ic3f2c4e5bafd3a1556412005564fc0b549e6813c
    Ic68717f4410170814d3e3b141de77b35eca12f7b
    Ifad79991298e9d779b38a33a270af5197d2fc060
    I3060c5ba31b59666aedfafad430e21329c4479d4
    I9c87282ba5819a76a6f6697a5e9de6c92d74a665
    I8144e2b83748a96ab83ba14894bfa5bd59995895
    I8cac84296ddb1e323f28e2476f78d0190f4a1b73
    Icef2b7fa70f274f671bd1b9469eb6a333ab462f9
    Idc4ceb7973d1a424834f8c2a4bc5916ef7b1e961
    I1bc779e5890f6994442c562ee5abf37c06dc6d3d
    Ib1161d3d8c4a9378eabf44f9de1eae9556d82643
    I11a17fc105dea398140abc1a59e6355e9e55b322
    I0aa523321f1eed4a0a5461b1a894452f77e6ff99
    I827bd74e9892969bb4891b31af1515789fa76c1c
    I1bb658339e5a6f0adb07fda3ee0bc6c03670cc09
    I135261c851067a25b20b30482764b5a61145f186
    I4971d605c3475949be64372dc6bd3355ed258527
    I08f6a7830c4b9ad44fb96f6d304ce257a899e287
    I96886f8d0f3f03905163d93c4f8f3e637d98127f
    Idf2cf7f40780cf46ba95228764790b818bc72db5
    Ib046fd7149d71fc62c8f2df7a9747d6b54e95138
    I546ff57d76e50ad86eb5853782fdcc1f348c9f0e
    I1293ecc2974bae4ad530ecb775cd19072e413c72
    I8db5a202a2e63ca0a3e355a6083d426acb7342cb
    I71482f9a4430a8bbcd02c954a415ff3fab2a0c8f
    I0c5656798444764b52f4d9d8889f831d674a1610
    I2ac4ccd967a7a47284573c9b645055ebeca4635c
    I93b830d42e7d44435c790832c11d5d38861015e0
    If24985f47cc9fcd6c2eb84a73165878559008120
    I39b908451f4b678ed63b872ffc46590e2c728b2c
    If7e5622067c77c4c8d64ee466d1c62e4dcb0e53f
    If58279329f6c76978223369a2d08d47a7b1e3579
    I3621a0a204360343ba684e2217a68a6ef51a7850
    I358909c38b0771fe0283386841b6123ddb2931a6
    I2e8fcb4a15af3a66331a921d378bfd5d2dfe4533
    I0bbad22c5fed8c32dd7de387194e94c8d4b9ad75
    I34f51c6202cbaa805c496360e8dbd391890decc6
    Iaab82ea254444bba233cd9ebad93d38426154ca4
    Ica2565031546a8281820c17edc95d258ad31ba65
    Ic282fff7167a8224ec1a1e0f7c247a9c8d73f390
    Icdc707a9438adae5c8bb2d9b97af95e65c4bc2a1
    I26c4e1fb971f22ec721e1dddcce6b301f5db6608
    I8882575084eefa6a23bd59b8d2a54a53d793298e
    Ic1e7cf4da4d2bf73d889aee7bac6cf260b26d532
    I472b9da10e9f45b3c254f8440526f3f9d5116931
    I1560e0df1c89be1db1e1d1aeaaeef2e6da5f85c1
    Ie2842b28648084d8970329d7c76fcf0d0666a847
    I2dccaedb559fc5e9865b137f980024e068bdafa3
    I61a3d44f81d829f1ef22fd3044c65e4b3518b92b
    I0d657c2e0d47af32cc35cd66b902be72952e5438
    I22d2dbdae5c9c859d4baca2bdb7bea71da09b85b
    I8c99fcbbf2f7229728236ae1059f4c9cb8dbb065
    I1bb841e463658c8db42eb15ecc1687262df86a2f
    
    Change-Id: Ib04f801b0085e7d5da3d73594bbc41c281b06026
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -1079,7 +1079,7 @@ static int msm_otg_notify_chg_type(struct msm_otg *motg)
 		motg->chg_type == USB_ACA_C_CHARGER))
 		charger_type = POWER_SUPPLY_TYPE_USB_ACA;
 	else
-		charger_type = POWER_SUPPLY_TYPE_BATTERY;
+		charger_type = POWER_SUPPLY_TYPE_UNKNOWN;
 
 	return pm8921_set_usb_power_supply_type(charger_type);
 }
