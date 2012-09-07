ed05ecfd878363dfb3757999111fcb7df9b48921
Kaushal Kumar <kaushalk@codeaurora.org>
2012-09-07 16:34:02 +0530

    msm: cpr: Add boot parameter option to disable CPR
    CPR should have an option to be disabled at boot if required.
    Usage: Append "msm-cpr.enable=0" to the kernel commandline to
    disable CPR. Append "msm-cpr.enable=1" to enable CPR.
    
    Change-Id: Idaac721063184abcfb37ae1ea33870f2464d70f0
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>
    (cherry picked from commit 5d83a9e81a449fd8eab5071226fcbdfdfe6b9857)
    
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>

--- arch/arm/mach-msm/msm_cpr.c
+++ arch/arm/mach-msm/msm_cpr.c
@@ -15,6 +15,7 @@
 #define pr_fmt(fmt) "%s: " fmt, __func__
 
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 #include <linux/io.h>
 #include <linux/irq.h>
@@ -38,6 +39,10 @@
 
 /* Need platform device handle for suspend and resume APIs */
 static struct platform_device *cpr_pdev;
+
+static bool enable = 1;
+module_param(enable, bool, 0644);
+MODULE_PARM_DESC(enable, "CPR Enable");
 
 struct msm_cpr {
 	int curr_osc;
@@ -700,6 +705,9 @@ static int __devinit msm_cpr_probe(struct platform_device *pdev)
 	const struct msm_cpr_config *pdata = pdev->dev.platform_data;
 	void __iomem *base;
 	struct resource *mem;
+
+	if (!enable)
+		return -EPERM;
 
 	if (!pdata) {
 		pr_err("CPR: Platform data is not available\n");
