4bdc71fbb55f944f659942916737c097304afb77
Devin Kim <dojip.kim@lge.com>
2012-09-17 21:15:02 -0700

    msm: clock: Fix HDMI compatibility of 720p TV.
    hdmi clock should be re-set when changing HDMI resolution.
    
    Change-Id: Ieb7c3e5570ca42a79300281daa2ccb305af926b8

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -4054,6 +4054,7 @@ void set_rate_tv(struct rcg_clk *rcg, struct clk_freq_tbl *nf)
 #ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
 	unsigned long pll_rate = (unsigned long)nf->extra_freq_data;
 	if (pll_rate) {
+		hdmi_pll_set_rate(pll_rate);
 		hdmi_pll_clk.rate = pll_rate;
 	}
 #endif
