f4f2d845482e5c36440b8b0bb787071326e647b9
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-19 07:50:20 -0500

    IKQCOMSD1-1650 arm: msm: add gpio mux installation
    Add custom handler for hooking into the OEM function
    table and read out the GPIO MUX from the device tree.
    This re-uses functionality from previous code base:
    
       * 76a0898 msm/gpiomux: set gpiomux by devtree
    
    PROPOGATED_from: IKHSS7-22185
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    
    Change-Id: I09d6162ac5b5e7beb3848e5b589baae2c2a97516
    Reviewed-on: http://gerrit.pcs.mot.com/480102
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit 548f37a389f41155bb7cbb7b222239e56a9f1e92)

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -20,6 +20,7 @@
 #include <linux/of_fdt.h>
 #include <linux/of.h>
 
+#include <mach/gpiomux.h>
 #include <mach/restart.h>
 
 #include "board-8960.h"
@@ -27,6 +28,52 @@
 
 static u32 fdt_start_address; /* flattened device tree address */
 static u32 fdt_size;
+
+struct dt_gpiomux {
+	u16 gpio;
+	u8 setting;
+	u8 func;
+	u8 pull;
+	u8 drv;
+	u8 dir;
+} __attribute__ ((__packed__));
+
+#define DT_PATH_MUX		"/System@0/IOMUX@0"
+#define DT_PROP_MUX_SETTINGS	"settings"
+
+static void __init mmi_gpiomux_init(void)
+{
+	struct device_node *node;
+	const struct dt_gpiomux *prop;
+	int i;
+	int size = 0;
+	struct gpiomux_setting setting;
+
+	/* Override the default setting by devtree.  Do not manually
+	 * install via msm_gpiomux_install hardcoded values.
+	 */
+	node = of_find_node_by_path(DT_PATH_MUX);
+	if (!node) {
+		pr_info("%s: no node found: %s\n", __func__, DT_PATH_MUX);
+		return;
+	}
+	prop = (const void *)of_get_property(node, DT_PROP_MUX_SETTINGS, &size);
+	if (prop && ((size % sizeof(struct dt_gpiomux)) == 0)) {
+		for (i = 0; i < size / sizeof(struct dt_gpiomux); i++) {
+			setting.func = prop->func;
+			setting.drv = prop->drv;
+			setting.pull = prop->pull;
+			setting.dir = prop->dir;
+			if (msm_gpiomux_write(prop->gpio, prop->setting,
+						&setting, NULL))
+				pr_err("%s: gpio%d mux setting %d failed\n",
+					__func__, prop->gpio, prop->setting);
+			prop++;
+		}
+	}
+
+	of_node_put(node);
+}
 
 static void __init mmi_msm8960_init_early(void)
 {
@@ -41,6 +88,9 @@ static void __init mmi_msm8960_init_early(void)
 
 		unflatten_device_tree();
 	}
+
+	/* Initialize OEM initialization overrides */
+	msm8960_oem_funcs.msm_gpio_init = mmi_gpiomux_init;
 }
 
 static int __init parse_tag_flat_dev_tree_address(const struct tag *tag)
