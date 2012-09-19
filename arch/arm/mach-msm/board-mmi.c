192b0414b8d5bfcc9ae6c9311f453c97f96a52f2
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-18 21:48:00 -0500

    arm: msm: allow late parsing of dev tree
    Utilize the device tree ATAG to parse the flattened device
    tree later on during the boot sequence.
    
    PROPOGATED_from: (CR), (CR)
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    
    Change-Id: Id15c3b339c7cfca85cbc06c5cadabba023ab3fb9
    Reviewed-on: http://gerrit.pcs.mot.com/480100
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit 0f371c26c652dd663233aa749f945f5ee61c61c9)

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -16,10 +16,49 @@
 #include <asm/mach/arch.h>
 #include <asm/setup.h>
 
+#include <linux/bootmem.h>
+#include <linux/of_fdt.h>
+#include <linux/of.h>
+
 #include <mach/restart.h>
 
 #include "board-8960.h"
 #include "timer.h"
+
+static u32 fdt_start_address; /* flattened device tree address */
+static u32 fdt_size;
+
+static void __init mmi_msm8960_init_early(void)
+{
+	msm8960_allocate_memory_regions();
+	if (fdt_start_address) {
+		void *mem;
+		mem = __alloc_bootmem(fdt_size, __alignof__(int), 0);
+		BUG_ON(!mem);
+		memcpy(mem, (const void *)fdt_start_address, fdt_size);
+		initial_boot_params = (struct boot_param_header *)mem;
+		pr_info("Unflattening device tree: 0x%08x\n", (u32)mem);
+
+		unflatten_device_tree();
+	}
+}
+
+static int __init parse_tag_flat_dev_tree_address(const struct tag *tag)
+{
+	struct tag_flat_dev_tree_address *fdt_addr =
+		(struct tag_flat_dev_tree_address *)&tag->u.fdt_addr;
+
+	if (fdt_addr->size) {
+		fdt_start_address = (u32)phys_to_virt(fdt_addr->address);
+		fdt_size = fdt_addr->size;
+	}
+
+	pr_info("flat_dev_tree_address=0x%08x, flat_dev_tree_size == 0x%08X\n",
+		fdt_addr->address, fdt_addr->size);
+
+	return 0;
+}
+__tagtable(ATAG_FLAT_DEV_TREE_ADDRESS, parse_tag_flat_dev_tree_address);
 
 MACHINE_START(VANQUISH, "Vanquish")
 	.map_io = msm8960_map_io,
@@ -28,7 +67,7 @@ MACHINE_START(VANQUISH, "Vanquish")
 	.handle_irq = gic_handle_irq,
 	.timer = &msm_timer,
 	.init_machine = msm8960_cdp_init,
-	.init_early = msm8960_allocate_memory_regions,
+	.init_early = mmi_msm8960_init_early,
 	.init_very_early = msm8960_early_memory,
 	.restart = msm_restart,
 MACHINE_END
