893c77f83b78a611ab43e7321a2d918fcbbb5c30
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-09-18 19:30:48 -0700

    wlan: Request wake up on idle cpu for RX_Thread, TX_Thread
    In high throughput usecases RX_Thread and TX_Thread must run as quickly
    as possible and also in parallel with other tasks when possible.
    
    Change-Id: I8af13e4416850da2262ba91bd39ad5e5b9cf18c0
    CRs-Fixed: 372351

--- CORE/VOSS/src/vos_sched.c
+++ CORE/VOSS/src/vos_sched.c
@@ -849,6 +849,7 @@ static int VosTXThread ( void * Arg )
   v_CONTEXT_t pVosContext        = NULL;
 
   set_user_nice(current, -1);
+  set_wake_up_idle(true);
 
   if (Arg == NULL)
   {
@@ -1072,6 +1073,7 @@ static int VosRXThread ( void * Arg )
   VOS_STATUS       vStatus       = VOS_STATUS_SUCCESS;
 
   set_user_nice(current, -1);
+  set_wake_up_idle(true);
 
   if (Arg == NULL)
   {
