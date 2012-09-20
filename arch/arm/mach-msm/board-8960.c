7153aa9e318f016fbfae2c251cf3f27bbaeb6533
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-20 17:40:33 -0500

    arm: msm: add pm8921 gpio/mpp init override
    Add new OEM function override option for initializing the
    configuration of PM8921 GPIO and MPPs
    
    Change-Id: I0e640d747db07e77b474a65ac6dfddde60e5d31d
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/480642
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    (cherry picked from commit 8249a39b5a2dce44bbbf0cea138808818e5356fa)

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -3211,12 +3211,18 @@ void __init msm8960_cdp_init(void)
 	platform_add_devices(common_devices, ARRAY_SIZE(common_devices));
 	msm8960_add_vidc_device();
 
-	msm8960_pm8921_gpio_mpp_init();
+	/* PM8921 GPIO and MPP MUX Configurations */
+	if (msm8960_oem_funcs.msm_gpio_mpp_init)
+		msm8960_oem_funcs.msm_gpio_mpp_init(&msm8960_oem_funcs);
+	else
+		msm8960_pm8921_gpio_mpp_init();
+
 	/* Don't add modem devices on APQ targets */
 	if (socinfo_get_id() != 124) {
 		platform_device_register(&msm_8960_q6_mss_fw);
 		platform_device_register(&msm_8960_q6_mss_sw);
 	}
+
 	platform_add_devices(cdp_devices, ARRAY_SIZE(cdp_devices));
 	msm8960_init_smsc_hub();
 	msm8960_init_hsic();
