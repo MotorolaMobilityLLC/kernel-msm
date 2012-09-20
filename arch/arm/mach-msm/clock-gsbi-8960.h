a659e4086fb1a16e41b1ee09c24a9965ca2d125c
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-20 09:07:02 -0500

    IKQCOMSD1-1650 arm: msm: export all GSBI related clocks
    Remove static definitions from the GSBI clocks in order to
    expose them to OEMs who may use different configurations than
    the standard definitions.
    
    PROPOGATED_from: IKFERMI-237
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    
    Change-Id: I584fe480dfbf1d1828af39aedef21fac96bc99e5
    Reviewed-on: http://gerrit.pcs.mot.com/480359
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit 970c4e208cad9d46ebca34aeeb39d51494b1251a)

---
+++ arch/arm/mach-msm/clock-gsbi-8960.h
@@ -0,0 +1,57 @@
+/* Copyright (c) 2012, Motorola Mobility. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __ARCH_ARM_MACH_MSM_CLOCK_GSBI_8960
+#define __ARCH_ARM_MACH_MSM_CLOCK_GSBI_8960
+
+#include "clock-local.h"
+
+extern struct branch_clk gsbi1_p_clk;
+extern struct branch_clk gsbi2_p_clk;
+extern struct branch_clk gsbi3_p_clk;
+extern struct branch_clk gsbi4_p_clk;
+extern struct branch_clk gsbi5_p_clk;
+extern struct branch_clk gsbi6_p_clk;
+extern struct branch_clk gsbi7_p_clk;
+extern struct branch_clk gsbi8_p_clk;
+extern struct branch_clk gsbi9_p_clk;
+extern struct branch_clk gsbi10_p_clk;
+extern struct branch_clk gsbi11_p_clk;
+extern struct branch_clk gsbi12_p_clk;
+
+extern struct rcg_clk gsbi1_uart_clk;
+extern struct rcg_clk gsbi2_uart_clk;
+extern struct rcg_clk gsbi3_uart_clk;
+extern struct rcg_clk gsbi4_uart_clk;
+extern struct rcg_clk gsbi5_uart_clk;
+extern struct rcg_clk gsbi6_uart_clk;
+extern struct rcg_clk gsbi7_uart_clk;
+extern struct rcg_clk gsbi8_uart_clk;
+extern struct rcg_clk gsbi9_uart_clk;
+extern struct rcg_clk gsbi10_uart_clk;
+extern struct rcg_clk gsbi11_uart_clk;
+extern struct rcg_clk gsbi12_uart_clk;
+
+extern struct rcg_clk gsbi1_qup_clk;
+extern struct rcg_clk gsbi2_qup_clk;
+extern struct rcg_clk gsbi3_qup_clk;
+extern struct rcg_clk gsbi4_qup_clk;
+extern struct rcg_clk gsbi5_qup_clk;
+extern struct rcg_clk gsbi6_qup_clk;
+extern struct rcg_clk gsbi7_qup_clk;
+extern struct rcg_clk gsbi8_qup_clk;
+extern struct rcg_clk gsbi9_qup_clk;
+extern struct rcg_clk gsbi10_qup_clk;
+extern struct rcg_clk gsbi11_qup_clk;
+extern struct rcg_clk gsbi12_qup_clk;
+
+#endif
