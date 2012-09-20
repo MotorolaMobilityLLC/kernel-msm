aebb3260656279720ed0d9956d30d1d10629b92c
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-20 18:09:29 -0500

    IKQCOMSD1-1650 arm: msm: devtree utilities to own file
    Move the device tree parsing utilities into a separate file so they
    can be accessed from multiple locations.
    
    Change-Id: Ic50d80208b3187912ce1c4396329e373ea2a00e9
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/480641
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    (cherry picked from commit 3b1fd25be6ee7d9c19a5b1fcb706c12c79f3c683)

---
+++ arch/arm/mach-msm/devtree_util.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2012 Motorola Mobility, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+#include <linux/of.h>
+
+__init u8 dt_get_u8_or_die(struct device_node *node, const char *name)
+{
+	int len = 0;
+	const void *prop;
+
+	prop = of_get_property(node, name, &len);
+	BUG_ON(!prop || (len != sizeof(u8)));
+
+	return *(u8 *)prop;
+}
+
+__init u16 dt_get_u16_or_die(struct device_node *node, const char *name)
+{
+	int len = 0;
+	const void *prop;
+
+	prop = of_get_property(node, name, &len);
+	BUG_ON(!prop || (len != sizeof(u16)));
+
+	return *(u16 *)prop;
+}
+
+__init u32 dt_get_u32_or_die(struct device_node *node, const char *name)
+{
+	int len = 0;
+	const void *prop;
+
+	prop = of_get_property(node, name, &len);
+	BUG_ON(!prop || (len != sizeof(u32)));
+
+	return *(u32 *)prop;
+}
+
---
+++ arch/arm/mach-msm/include/mach/devtree_util.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 Motorola, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ */
+
+#ifndef __DEVTREE_UTIL_H__
+#define __DEVTREE_UTIL_H__
+
+#include <linux/of.h>
+#include <asm/mach-types.h>
+
+__init u8  dt_get_u8_or_die(struct device_node *node, const char *name);
+__init u16 dt_get_u16_or_die(struct device_node *node, const char *name);
+__init u32 dt_get_u32_or_die(struct device_node *node, const char *name);
+
+#endif
+
