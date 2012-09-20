9f9380131a990657ff3229c6b95909b2bc031144
James Wylder <jwylder1@motorola.com>
2012-09-20 10:36:36 -0500

    arm: msm: Add msm8960dt machine
    Add support for a generic machine msm8960dt, to
    eventually replace the current vanquish stanza.
    
    Change-Id: Ia8097ea9566db3e90d00527d7eccec0ae1b2ad42
    Signed-off-by: James Wylder <jwylder1@motorola.com>
    
    Conflicts:
    	arch/arm/mach-msm/board-8960.c
    Change-Id: Iab93111c69bf5ad73d29bd96f3b6cc8a7ddfac4e
    Reviewed-on: http://gerrit.pcs.mot.com/480408
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit b52de7131f7136502a336c44d499372a7651bfc5)

--- arch/arm/mach-msm/board-mmi.c
+++ arch/arm/mach-msm/board-mmi.c
@@ -128,3 +128,15 @@ MACHINE_START(VANQUISH, "Vanquish")
 	.restart = msm_restart,
 MACHINE_END
 
+MACHINE_START(MSM8960DT, "msm8960dt")
+	.map_io = msm8960_map_io,
+	.reserve = msm8960_reserve,
+	.init_irq = msm8960_init_irq,
+	.handle_irq = gic_handle_irq,
+	.timer = &msm_timer,
+	.init_machine = msm8960_cdp_init,
+	.init_early = mmi_msm8960_init_early,
+	.init_very_early = msm8960_early_memory,
+	.restart = msm_restart,
+MACHINE_END
+
