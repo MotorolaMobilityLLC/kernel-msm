7b8e056c4809b0639805874fbd4f05d5669725b2
Jason Hrycay <jason.hrycay@motorola.com>
2012-09-23 22:24:13 -0500

    IKQCOMSD1-1650 arm: msm: add custom pm8921 oem overrides
    Add new functions to override pm8921 platform data initialization
    with ability to override charging and keypad related data.
    
    Change-Id: Ic55e94aad9fdd76cc4d4f71acc322a3ca31444ec
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/481457
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    (cherry picked from commit f7f4145587a9e41df505080bb227f1ab490841b2)

--- arch/arm/mach-msm/board-mmi.h
+++ arch/arm/mach-msm/board-mmi.h
@@ -22,6 +22,10 @@ extern void mmi_register_i2c_devices_from_dt(void);
 
 /* from board-mmi-pmic.c */
 extern void mmi_init_pm8921_gpio_mpp(void);
+extern void mmi_pm8921_init(void *);
+
+/* from board-mmi-keypad.c */
+extern void mmi_pm8921_keypad_init(void *);
 
 #endif
 
