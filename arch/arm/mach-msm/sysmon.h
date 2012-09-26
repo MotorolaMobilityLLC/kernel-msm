613e3b6f4cc62bba78f1c60d76bb62861b9cbc61
agathon.jung <agathon.jung@lge.com>
2012-09-26 11:36:51 -0700

    msm: mdm: Add shutdown ioctl and send poweroff request to the mdm
    When the whole phone is being powered off, a poweroff request needs
    to be sent to the external modem so that it can shutdown gracefully.
    This request needs to be triggered from userspace before kernel drivers
    start unloading so that other drivers needed to send the request are
    still available. The shutdown ioctl is provided for this purpose.
    The request is sent over system monitor.
    
    Crs-Fixed: 401598
    Signed-off-by: Joel King <joelking@codeaurora.org>

--- arch/arm/mach-msm/sysmon.h
+++ arch/arm/mach-msm/sysmon.h
@@ -38,6 +38,7 @@ enum subsys_id {
 int sysmon_send_event(enum subsys_id dest_ss, const char *event_ss,
 		      enum subsys_notif_type notif);
 int sysmon_get_reason(enum subsys_id dest_ss, char *buf, size_t len);
+int sysmon_send_shutdown(enum subsys_id dest_ss);
 #else
 static inline int sysmon_send_event(enum subsys_id dest_ss,
 				    const char *event_ss,
