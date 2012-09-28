3352adb9810a8dc503a920029c75c1155fb9cee3
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-09-28 14:57:24 -0700

    wlan: Fixed problem of dangling pointers to freed memory
    Make sure pointers to freed memory are reset to NULL in case they will
    be used again after re-init of the driver (e.g. SSR). This is not an
    issue for DLKM but for statically linked driver
    
    Change-Id: I6d89b7f25822a578bc99fd8f0f2d72f7e7e43518
    CR-Fixed: 402677

--- CORE/WDI/CP/src/wlan_qct_wdi.c
+++ CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -1390,6 +1390,8 @@ WDI_Stop
   /* Free the global variables */
   wpalMemoryFree(gpHostWlanFeatCaps);
   wpalMemoryFree(gpFwWlanFeatCaps);
+  gpHostWlanFeatCaps = NULL;
+  gpFwWlanFeatCaps = NULL;
 
   /*------------------------------------------------------------------------
     Fill in Event data and post to the Main FSM
@@ -1645,6 +1647,8 @@ WDI_Shutdown
    /* Free the global variables */
    wpalMemoryFree(gpHostWlanFeatCaps);
    wpalMemoryFree(gpFwWlanFeatCaps);
+   gpHostWlanFeatCaps = NULL;
+   gpFwWlanFeatCaps = NULL;
    /*Clear control block.  note that this will clear the "magic"
      which will inhibit all asynchronous callbacks*/
    WDI_CleanCB(&gWDICb);
