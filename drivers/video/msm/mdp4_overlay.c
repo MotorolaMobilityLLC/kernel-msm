7bf0610f438a2f91f6d7e0759c64d400e5ab6369
Naseer Ahmed <naseer@codeaurora.org>
2012-10-01 15:54:25 -0400

    msm_fb: display: Send vsync events using sysfs for MDP4 targets
    Replace sending of vsync timestamp using uevents with sysfs entry
    as uevent result in increase in power numbers due to broadcast in
    nature. Also, for smart panel, wait4vsync is removed.
    
    Change-Id: I5219e85b837b8bab9d00a9f40c1048d2a2355194
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -1595,6 +1595,8 @@ void mdp4_mixer_stage_commit(int mixer)
 		data |= stage;
 	}
 
+	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
+	mdp_clk_ctrl(1);
 	mdp4_mixer_blend_setup(mixer);
 
 	off = 0;
@@ -1614,7 +1616,6 @@ void mdp4_mixer_stage_commit(int mixer)
 				mixer, data, ctrl->flush[mixer], current->pid);
 	}
 
-	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 	local_irq_save(flags);
 	if (off)
 		outpdw(MDP_BASE + off, data);
@@ -1625,6 +1626,7 @@ void mdp4_mixer_stage_commit(int mixer)
 	}
 	local_irq_restore(flags);
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
+	mdp_clk_ctrl(0);
 }
 
 
