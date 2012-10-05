992cd44a747cbc7c9e89e9b3f9dfbcca86b50837
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-10-05 10:56:17 -0700

    wlan: Inform TL about pending packets whenever Queue is filling up
    Currently, HDD informs TL about pending packets only when it goes empty
    to non-empty. If TL misses this signal, it will not hearback again from
    HDD.So, avoid this, we inform TL whenever the queue is getting filled
    
    Change-Id: I282742e5a234d7d204a9c8e80f3245dd0e71dbb3
    CR-Fixed: 384996

--- CORE/HDD/src/wlan_hdd_tx_rx.c
+++ CORE/HDD/src/wlan_hdd_tx_rx.c
@@ -543,7 +543,14 @@ int hdd_hard_start_xmit(struct sk_buff *skb, struct net_device *dev)
 #endif // HDD_WMM_DEBUG
 
    spin_lock(&pAdapter->wmm_tx_queue[ac].lock);
-
+   /*For every increment of 10 pkts in the queue, we inform TL about pending pkts.
+    * We check for +1 in the logic,to take care of Zero count which 
+    * occurs very frequently in low traffic cases */
+   if((pAdapter->wmm_tx_queue[ac].count + 1) % 10 == 0)
+   {
+           VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,"%s:Queue is Filling up.Inform TL again about pending packets", __FUNCTION__);
+           WLANTL_STAPktPending( (WLAN_HDD_GET_CTX(pAdapter))->pvosContext, pHddStaCtx->conn_info.staId[0], ac );
+   }
    //If we have already reached the max queue size, disable the TX queue
    if ( pAdapter->wmm_tx_queue[ac].count == pAdapter->wmm_tx_queue[ac].max_size)
    {
