0db33e98b98560b3dd4dc67138f3ae709c659463
James Wylder <jwylder1@motorola.com>
2012-10-08 12:10:05 -0500

    arm: msm: ram_console: define device
    Add definition for 128K of pram to be used by the android
    ram console.
    
    Change-Id: I6fc18e670cfddc045599087517e68abce5854fb0
    Signed-off-by: James Wylder <jwylder1@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/485525
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit cc3df1e7bfd85edff044fe7a72069824678447b8)

--- arch/arm/mach-msm/devices-mmi.h
+++ arch/arm/mach-msm/devices-mmi.h
@@ -22,4 +22,6 @@ extern struct platform_device mmi_msm8960_device_uart_gsbi8;
 
 extern struct platform_device mmi_w1_gpio_device;
 
+extern struct platform_device mmi_ram_console_device;
+extern struct persistent_ram mmi_ram_console_pram;
 #endif
