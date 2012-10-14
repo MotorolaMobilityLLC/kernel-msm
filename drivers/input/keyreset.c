6fd46ae4b682b15e11e4f265111eae4a5075d54d
Dima Zavin <dima@android.com>
2012-10-14 00:14:41 -0700

    input: keyreset: move init to subsys to be ready early
    Bug: 7344361
    Change-Id: I3564a354fddd69f352704c9e819ea4538096264a
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/input/keyreset.c
+++ drivers/input/keyreset.c
@@ -242,5 +242,5 @@ static void __exit keyreset_exit(void)
 	return platform_driver_unregister(&keyreset_driver);
 }
 
-module_init(keyreset_init);
+subsys_initcall(keyreset_init);
 module_exit(keyreset_exit);
