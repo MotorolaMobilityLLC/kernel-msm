d40296288736f9216c561420fe82c40cbff5df44
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-10-18 20:30:17 -0700

    wlan: Fix for Klockwork errors.
    Change-Id: I1c075b46afb8f1f43b9d528e425763852895d0a6
    CR-Fixed: 408813

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -6124,9 +6124,8 @@ eHalStatus sme_ReceiveFilterSetFilter(tHalHandle hHal, tpSirRcvPktFilterCfgType 
                "filterId = %d", __FUNCTION__,
                pRcvPktFilterCfg->filterType, pRcvPktFilterCfg->filterId);
   
-    allocSize = sizeof(tSirRcvPktFilterCfgType) + 
-                      ((pRcvPktFilterCfg->numFieldParams - 1) * 
-                      sizeof(tSirRcvPktFilterFieldParams));
+    allocSize = sizeof(tSirRcvPktFilterCfgType);
+    
     pRequestBuf = vos_mem_malloc(allocSize);
 
     if (NULL == pRequestBuf)
