0f667e81c030e9bef6b75dfb315c8d49a406c80c
Iliyan Malchev <malchev@google.com>
2012-10-19 12:27:48 -0700

    Revert "base: sync: signal a sync pt when not adding to the active_list"
    This reverts commit bd483da2f7a76290369bffb7e5171336d653c134.

--- drivers/base/sync.c
+++ drivers/base/sync.c
@@ -229,13 +229,12 @@ static void sync_pt_activate(struct sync_pt *pt)
 	spin_lock_irqsave(&obj->active_list_lock, flags);
 
 	err = _sync_pt_has_signaled(pt);
-	if (err != 0) {
-		spin_unlock_irqrestore(&obj->active_list_lock, flags);
-		sync_fence_signal_pt(pt);
-		return;
-	}
+	if (err != 0)
+		goto out;
 
 	list_add_tail(&pt->active_list, &obj->active_list_head);
+
+out:
 	spin_unlock_irqrestore(&obj->active_list_lock, flags);
 }
 
