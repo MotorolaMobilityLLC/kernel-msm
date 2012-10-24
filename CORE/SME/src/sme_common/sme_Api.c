ab4ab0d1af5815f23232cbfdbf73e461be3eb742
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-10-24 14:26:50 -0700

    wlan: Dump neighbor state info when scan is not allowed.
    This commit only adds a debug statement and supplements the changes made
    in CL651697.
    
    Change-Id: I621e2a7aaf8cba6cf900cc8827790a87f4f09e65
    CR-Fixed: NA

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -1924,8 +1924,11 @@ eHalStatus sme_ScanRequest(tHalHandle hHal, tANI_U8 sessionId, tCsrScanRequest *
                                                      pScanRequestID, callback, pContext );
 #ifdef FEATURE_WLAN_LFR
                     } else {
-                            /*HandOff is in progress. So schedule this scan later*/
-                            status = eHAL_STATUS_RESOURCES;
+                        smsLog(pMac, LOGE, FL("Scan denied in state %d (sub-state %d)"),
+                                pMac->roam.neighborRoamInfo.neighborRoamState,
+                                pMac->roam.curSubState[sessionId]);
+                        /*HandOff is in progress. So schedule this scan later*/
+                        status = eHAL_STATUS_RESOURCES;
                     }
 #endif
                 }
