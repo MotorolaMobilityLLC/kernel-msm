03e09a91f43905b6f2db71f9ccd4080a927c6c0a
Dmitry Ornatsky <cdo021c@motorola.com>
2012-10-25 15:29:18 -0500

    arm: msm: Register RGB led device
    Add platform data and device to board files
    
    Change-Id: Iab322497c68d4f74836ce2acf39ac1b654b84e9a
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/491593
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    (cherry picked from commit 5ec136422b65dc8d59b222cc7dc67a4a0e4a807e)

--- arch/arm/mach-msm/devices-mmi.h
+++ arch/arm/mach-msm/devices-mmi.h
@@ -23,6 +23,7 @@ extern struct platform_device mmi_msm8960_device_uart_gsbi8;
 extern struct platform_device mmi_w1_gpio_device;
 
 extern struct platform_device mmi_ram_console_device;
+extern struct platform_device mmi_pm8xxx_rgb_leds_device;
 extern struct persistent_ram mmi_ram_console_pram;
 extern struct platform_device mmi_factory_device;
 #endif
