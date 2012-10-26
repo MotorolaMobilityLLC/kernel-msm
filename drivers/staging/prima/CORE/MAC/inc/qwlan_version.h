1a0c9cb596ab650ce45183f80e75e9cea4f5f207
Iliyan Malchev <malchev@google.com>
2012-10-26 11:54:08 -0700

    prima: release 3.2.1.10e
        git://codeaurora.org/external/wlan/prima.git
    
        4c8540b wlan: Release 3.2.1.10e
        c8c7c53 Add dummy handlers for Android PNO commands
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
@@ -39,8 +39,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "d"
+#define QWLAN_VERSION_EXTRA            "e"
 #define QWLAN_VERSION_BUILD            10
-#define QWLAN_VERSIONSTR               "3.2.1.10d"
+#define QWLAN_VERSIONSTR               "3.2.1.10e"
 
 #endif /* QWLAN_VERSION_H */
