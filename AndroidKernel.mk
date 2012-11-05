a5a78d1e5da45ec64f8c2b09b30dfcfb7d5f554c
Jason Hrycay <jason.hrycay@motorola.com>
2012-11-04 18:43:33 -0600

    make: compile DTSs during kernel compile
    Build the dtbs target during normal kernel compilation to generate
    all of the .dtb.  These can then be used to generate a combined
    device tree binary and appended to the boot images.
    
    Change-Id: I41dc253e0743c61330d6cc47428728182e06c2a3
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/494008
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <e50276@motorola.com>
    (cherry picked from commit 1a086b9f345f63a066c9b62254f4893c3014537d)

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -69,6 +69,7 @@ $(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
 
 $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- dtbs
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_INSTALL) ARCH=arm CROSS_COMPILE=arm-eabi- modules_install
 	$(mv-modules)
