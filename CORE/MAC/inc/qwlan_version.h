bcbe94e80a08215a459e451b4323771352b75355
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-11-06 19:47:02 -0800

    wlan: cs release 3.2.0.24
    CL 657549 on 2012/11/02 by c_sunilr 'DUT P2P client sends disassoc f'
    CL 657555 on 2012/11/02 by karmuchi 'Ignore Channel 165 for country '
    CL 657564 on 2012/11/03 by c_skoha 'IOCTL to support MCC config  Ne'
    CL 657584 on 2012/11/04 by ssuryade 'wlan: Fix for passing valid rea'
    CL 657589 on 2012/11/04 by smiryala 'Fix for not closing the smd cha'
    CL 657598 on 2012/11/05 by kganesh 'wlan: NoA not being set in Beac'
    CL 657603 on 2012/11/05 by yathishh 'Removed hard tabs in CL657471 :'
    CL 657611 on 2012/11/05 by karmuchi 'Teardown Link with AP if riva i'
    CL 657615 on 2012/11/05 by asinghal 'More checkpatch fixes for WLAN '
    CL 657639 on 2012/11/05 by asinghal 'Fix (-1) return values to confo'
    CL 657728 on 2012/11/06 by karmuchi 'Update missing review comments '
    CL 657756 on 2012/11/06 by skuppa 'Post proper response to SME whe'
    CL 657832 on 2012/11/06 by c_lreddy 'Indicating reassoc event to sup'
    CL 657847 on 2012/11/07 by c_vyetur 'Fixes Compilation Error '
    CL 657919 on 2012/11/07 by datre 'Fix  Log level in TL  New Featu'
    CL 657925 on 2012/11/07 by sameert 'Notify the Kernel suspend to WC'
    CL 657948 on 2012/11/07 by jjohnson 'Conditionally invoke driver res'
    CL 657951 on 2012/11/07 by schang 'handle DXE CHs transfer error c'
    
    CR-Fixed: 416741, 417938, 415199, 416967, 416542, 389161, 417923,
    417938, 418508, 407823, 412031, 418487, 402874, 415791
    
    Change-Id: I1040de2ec7f157f725f9efc093594c1f0cc4baa5

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,8 +40,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            23
+#define QWLAN_VERSION_BUILD            24
 
-#define QWLAN_VERSIONSTR               "3.2.0.23"
+#define QWLAN_VERSIONSTR               "3.2.0.24"
 
 #endif /* QWLAN_VERSION_H */
