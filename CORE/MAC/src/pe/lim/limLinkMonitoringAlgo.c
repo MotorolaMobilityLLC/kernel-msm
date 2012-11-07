49a7c0f0879a918da3f6c5a17892c3ee6562e7aa
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-11-06 18:57:59 -0800

    wlan: Teardown Link with AP if riva indicates del sta reason keep alive.
    There are two problems associated with this bug.
    1) When Riva sends the delete station context indication to the Host
    with reason code HAL_DEL_STA_REASON_CODE_KEEP_ALIVE and the assocID
    other than the value 1 then PE does not indicate the disconnect
    indication to SME.
    The fix is incase of STA mode, RA deals with peer STA which happens to
    be a BSS (STA Type = Peer), so we should be ignoring the assoc ID as it
    has no meaning.  It has meaning only for STA Type = SELF.
    2) BTC module dynamically enable/disable HB monitoring for specific BT
    events but PE does not reactivate the Heart beat timer when the BTC
    module enable the Heart beat.
    
    Added fix to reactivate the Heart beat timer.
    
    Change-Id: I6a77a47c0f17c4b465dc53537199401ba22e2a02
    CR-Fixed: 417923

--- CORE/MAC/src/pe/lim/limLinkMonitoringAlgo.c
+++ CORE/MAC/src/pe/lim/limLinkMonitoringAlgo.c
@@ -172,11 +172,19 @@ limDeleteStaContext(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
             case HAL_DEL_STA_REASON_CODE_KEEP_ALIVE:
             case HAL_DEL_STA_REASON_CODE_TIM_BASED:
                 PELOGE(limLog(pMac, LOGE, FL(" Deleting station: staId = %d, reasonCode = %d\n"), pMsg->staId, pMsg->reasonCode);)
-#endif        
-                pStaDs = dphGetHashEntry(pMac, pMsg->assocId, &psessionEntry->dph.dphHashTable);
+#endif
+                if((eLIM_BT_AMP_AP_ROLE == psessionEntry->limSystemRole) ||
+                   (eLIM_AP_ROLE == psessionEntry->limSystemRole))
+                {
+                    pStaDs = dphGetHashEntry(pMac, pMsg->assocId, &psessionEntry->dph.dphHashTable);
+                }
+                else
+                {
+                    pStaDs = dphGetHashEntry(pMac, DPH_STA_HASH_INDEX_PEER, &psessionEntry->dph.dphHashTable);
+                }
                 if (! pStaDs)
                 {
-                   PELOGW(limLog(pMac, LOGW, FL("Skip STA deletion (invalid STA)\n"));)
+                   PELOGE(limLog(pMac, LOGE, FL("Skip STA deletion (invalid STA) limSystemRole=%d\n"),psessionEntry->limSystemRole);)
                    palFreeMemory(pMac->hHdd, pMsg);
                    return;
                 }
@@ -186,7 +194,7 @@ limDeleteStaContext(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
                 */
                 if (pStaDs->staIndex != pMsg->staId)
                 {
-                    PELOGW(limLog(pMac, LOGW, FL("staid mismatch: %d vs %d \n"), pStaDs->staIndex, pMsg->staId);)
+                    PELOGE(limLog(pMac, LOGE, FL("staid mismatch: %d vs %d \n"), pStaDs->staIndex, pMsg->staId);)
                     palFreeMemory(pMac->hHdd, pMsg);
                     return;
                 }
@@ -202,7 +210,7 @@ limDeleteStaContext(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
                 {
                     //TearDownLink with AP
                     tLimMlmDeauthInd  mlmDeauthInd;
-                    PELOG1(limLog(pMac, LOG1, FL("lim Delete Station Context (staId: %d, assocId: %d) \n"),
+                    PELOGW(limLog(pMac, LOGW, FL("lim Delete Station Context (staId: %d, assocId: %d) \n"),
                                   pMsg->staId, pMsg->assocId);)
 
                     pStaDs->mlmStaContext.disassocReason = eSIR_MAC_UNSPEC_FAILURE_REASON;
