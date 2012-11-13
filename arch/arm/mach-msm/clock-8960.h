1df923644af8d80417ac4ead957fbb9da213376b
James Wilson <james.wilson@motorola.com>
2012-11-12 19:03:38 -0600

    IKQCOMSD1-1746: arm: msm: Expose camera clocks
    Expose camera clocks in clock-8960.c and add new
    clock-8960.h for external declarations.
    
    Change-Id: I1f8d82cedea642b379c9759689398347b28ce06c
    Signed-off-by: James Wilson <james.wilson@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/496576
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <e50276@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-by: James Wylder <James.Wylder@motorola.com>
    (cherry picked from commit d9970922294691500d7aace802ebc501bd87d1c5)

---
+++ arch/arm/mach-msm/clock-8960.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2012, Motorola Mobility. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __ARCH_ARM_MACH_MSM_CLOCK_8960_H
+#define __ARCH_ARM_MACH_MSM_CLOCK_8960_H
+
+#include "clock-local.h"
+
+extern struct rcg_clk cam0_clk;
+extern struct rcg_clk cam1_clk;
+extern struct rcg_clk cam2_clk;
+
+#endif
