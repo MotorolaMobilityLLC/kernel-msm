673efd033f1e528332b14a75a75c9b2277377630
prabhu annabathula <prabhu.annabathula@motorola.com>
2012-11-15 20:57:28 -0600

    IKQCOMSD1-1958: arm: msm: board file to enable primary i2s tx
    This will enable TI DSP C55->MSM digital audio interface for low
    power audio.
    
    Change-Id: I92b53fdbb4009e56756daa8ca597a54d0fc1644e
    Signed-off-by: prabhu annabathula <prabhu.annabathula@motorola.com>
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-on: http://gerrit.pcs.mot.com/506849
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

--- arch/arm/mach-msm/board-mmi-i2s.c
+++ arch/arm/mach-msm/board-mmi-i2s.c
@@ -15,12 +15,14 @@
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <sound/apr_audio.h>
 #include <sound/msm-dai-q6.h>
 
 /* Device tree node */
-#define DT_PATH_I2S			"/System@0/I2S@0"
+#define DT_I2S_NODE_NAME		"I2S"
 #define DT_PROP_I2S_TYPE		"type"
 #define DT_TYPE_MI2S		0x001E0016
+#define DT_TYPE_PRI_I2S		0x001E001C
 
 
 static struct gpio mmi_mi2s_gpios[] = {
@@ -56,37 +58,77 @@ struct platform_device mmi_msmcpudai_mi2s_rx = {
 	},
 };
 
+static struct gpio mmi_pri_i2s_gpios[] = {
+	{
+		.label = "gpio_ws",
+		.gpio = -1,
+	},
+	{
+		.label = "gpio_sck",
+		.gpio = -1,
+	},
+	{
+		.label = "gpio_din",
+		.gpio = -1,
+	},
+};
+
+static struct msm_primary_i2s_pdata mmi_msm_pri_i2s_tx_data = {
+	.gpios = mmi_pri_i2s_gpios,
+	.num_gpios = ARRAY_SIZE(mmi_pri_i2s_gpios),
+};
+
+struct platform_device mmi_msmcpudai_pri_tx = {
+	.name   = "msm-dai-q6",
+	.id     = PRIMARY_I2S_TX,
+	.dev = {
+		.platform_data = &mmi_msm_pri_i2s_tx_data,
+	},
+};
+
 void __init mmi_i2s_dai_init(void)
 {
 	struct device_node *node;
 	int type;
 	int i;
 
-	node = of_find_node_by_path(DT_PATH_I2S);
-	if (!node)
-		goto exit;
+	/* Read I2S node from device tree */
+	for_each_node_by_name(node, DT_I2S_NODE_NAME) {
+		if (of_property_read_u32(node, DT_PROP_I2S_TYPE, &type))
+			continue;
 
-	if (of_property_read_u32(node, DT_PROP_I2S_TYPE, &type)) {
-		pr_err("DT I2S type read error\n");
-		goto exit;
-	}
-
-	if (type != DT_TYPE_MI2S) {
-		pr_err("%s: DT node wrong type: 0x%08X.\n", __func__, type);
-		goto exit;
-	}
-
-	for (i = 0; i < ARRAY_SIZE(mmi_mi2s_gpios); ++i) {
-		if (of_property_read_u32(node, mmi_mi2s_gpios[i].label,
-					&mmi_mi2s_gpios[i].gpio)) {
-			pr_err("%s: Missing %s\n", __func__, mmi_mi2s_gpios[i].label);
-			goto exit;
+		switch (type) {
+		case DT_TYPE_MI2S:
+			for (i = 0; i < ARRAY_SIZE(mmi_mi2s_gpios); ++i) {
+				if (of_property_read_u32(node,
+						mmi_mi2s_gpios[i].label,
+						&mmi_mi2s_gpios[i].gpio)) {
+					pr_err("%s: Missing %s\n", __func__,
+						mmi_mi2s_gpios[i].label);
+					break;
+				}
+			}
+			break;
+		case DT_TYPE_PRI_I2S:
+			for (i = 0; i < ARRAY_SIZE(mmi_pri_i2s_gpios); ++i) {
+				if (of_property_read_u32(node,
+						mmi_pri_i2s_gpios[i].label,
+						&mmi_pri_i2s_gpios[i].gpio)) {
+					pr_err("%s: Missing %s\n", __func__,
+						mmi_pri_i2s_gpios[i].label);
+					break;
+				}
+			}
+			break;
+		default:
+			pr_err("%s: Invalid I2S node type", __func__);
+			break;
 		}
 	}
 
-exit:
 	of_node_put(node);
-	/* Always register the device and let driver do the
+	/* Always register the devices and let driver do the
 		platform data validation */
 	platform_device_register(&mmi_msmcpudai_mi2s_rx);
+	platform_device_register(&mmi_msmcpudai_pri_tx);
 }
