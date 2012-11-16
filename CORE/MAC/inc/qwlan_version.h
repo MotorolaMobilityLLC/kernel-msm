bc84fd8dfb60c7857240680e7cc595df65757868
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-11-15 17:51:58 -0800

    wlan: cs release 3.2.0.27
    CL 658415 on 2012/11/13 by c_rvaish 'Updating VHT cap IE for SU Beam'
    CL 658450 on 2012/11/13 by c_sgirig 'Add NULL check for session entr'
    CL 658466 on 2012/11/13 by c_sunilr@ ' p2p find takes long time when '
    CL 658483 on 2012/11/14 by ganeshk 'Wait for ack and issue delsta a'
    CL 658506 on 2012/11/14 by skuppa 'De-register lookup UP indicatio'
    CL 658690 on 2012/11/15 by npullela 'Back out CLlist 657132  New'
    CL 658697 on 2012/11/15 by smihir 'Pass correct pointers to wpalMe'
    CL 658723 on 2012/11/15 by mkalikot 'Fix for a crash in WLAN startup'
    
    CR-Fixed: 420622, 420571, 420836, 414558, 411684, 420456, 419963,
    417196, 400073
    
    Change-Id: I0567faac855955a24814ec01a0148f448e98419a

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,8 +40,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            26
+#define QWLAN_VERSION_BUILD            27
 
-#define QWLAN_VERSIONSTR               "3.2.0.26"
+#define QWLAN_VERSIONSTR               "3.2.0.27"
 
 #endif /* QWLAN_VERSION_H */
