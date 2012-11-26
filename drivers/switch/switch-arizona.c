dd9bf31bc7a2b91af27879320207a740a1437bb9
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-11-26 11:05:56 +0000

    switch: arizona: Only set GPIO if requested
    Change-Id: I5b9482d80f3df1ee8c475faf09ccdcfe3dbf2209
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -82,8 +82,9 @@ static void arizona_switch_set_mode(struct arizona_switch_info *info, int mode)
 {
 	struct arizona *arizona = info->arizona;
 
-	gpio_set_value_cansleep(arizona->pdata.micd_pol_gpio,
-				info->micd_modes[mode].gpio);
+	if (arizona->pdata.micd_pol_gpio > 0)
+		gpio_set_value_cansleep(arizona->pdata.micd_pol_gpio,
+					info->micd_modes[mode].gpio);
 	regmap_update_bits(arizona->regmap, ARIZONA_MIC_DETECT_1,
 			   ARIZONA_MICD_BIAS_SRC_MASK,
 			   info->micd_modes[mode].bias);
