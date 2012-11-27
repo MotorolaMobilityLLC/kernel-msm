113c45d9e692dffb162ae20ee13b1a4d68b9800e
Neha Pandey <nehap@codeaurora.org>
2012-11-27 12:18:24 -0800

    Revert "msm: acpuclock-krait: Fix PTE efuse address for acpuclock-8974."
    This reverts commit 3f7590f2a66ecfdc902e8ca37785dba462ad995f.
    
    Signed-off-by: Neha Pandey <nehap@codeaurora.org>

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -219,7 +219,7 @@ static struct acpuclk_krait_params acpuclk_8064_params __initdata = {
 	.l2_freq_tbl = l2_freq_tbl,
 	.l2_freq_tbl_size = sizeof(l2_freq_tbl),
 	.bus_scale = &bus_scale_data,
-	.pte_efuse_phys = 0x007000C0,
+	.qfprom_phys_base = 0x00700000,
 	.stby_khz = 384000,
 };
 
