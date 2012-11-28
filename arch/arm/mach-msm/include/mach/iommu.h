c390a33e2d90612e0b2e3ada73a1fe4be408ba01
Jeremy Gebben <jgebben@codeaurora.org>
2012-11-28 09:32:48 -0700

    iommu/msm: fix the include guard in iommu.h
    msm_soc_version_supports_iommu_v1() was defined outside the guard.
    
    Change-Id: I8db106908b08b89e267550d81d031cdb028b92a2
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- arch/arm/mach-msm/include/mach/iommu.h
+++ arch/arm/mach-msm/include/mach/iommu.h
@@ -138,7 +138,6 @@ static inline struct device *msm_iommu_get_ctx(const char *ctx_name)
 }
 #endif
 
-#endif
 
 static inline int msm_soc_version_supports_iommu_v1(void)
 {
@@ -162,3 +161,4 @@ static inline int msm_soc_version_supports_iommu_v1(void)
 	}
 	return 1;
 }
+#endif
