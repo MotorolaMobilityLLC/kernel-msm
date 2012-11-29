44cb600207b97d6332d2736a3dcd81518d1e4333
Alexander Filonenko <vtp684@motorola.com>
2012-11-29 10:04:16 -0600

    dm-crypt: fix missing deallocation
    Add deallocation missing from the following commit on QC baseline
    "fbd06f9 dm-crypt: move static cipher data out of per-cpu structure".
    The original commit SHA is fd2d231 at
    git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    Change-Id: Ib2f51555174f3483a745183cfbd58e0807483aaa
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/500836
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    (cherry picked from commit 4a00d7d46d593a04a0c96ccd72c5ae5780526fdf)

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1262,6 +1262,9 @@ static void crypt_free_tfms(struct crypt_config *cc)
 			crypto_free_ablkcipher(cc->tfms[i]);
 			cc->tfms[i] = NULL;
 		}
+
+	kfree(cc->tfms);
+	cc->tfms = NULL;
 }
 
 static int crypt_alloc_tfms(struct crypt_config *cc, char *ciphermode)
