6951553f8710ecf9eb328606f7c13f84d1cce13a
Limei Wang <e12499@motorola.com>
2012-11-30 18:17:52 -0600

    msm_fb: display: turn off display panel power supply
    panel_power_save(0) is needed to pull panel reset pin and shut down
    panel supplies in mipi_dsi_off, but it was missed in below commit when
    merge from main-jb-qcpro to la23-bringup.
    
    commit da936051886177eff8330921fdd97d632d6b8c83
    
    Change-Id: I6c618fbe86865d2a6d6c16b51ffeced7df9a422d
    Signed-off-by: Limei Wang <e12499@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/501616
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Bang Nguyen <BNGUYEN1@motorola.com>
    Reviewed-by: Dong Zhao <kevin.d.zhao@motorola.com>
    Reviewed-by: Jun-Xiao Chang <w20081@motorola.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -138,6 +138,10 @@ static int mipi_dsi_off(struct platform_device *pdev)
 	spin_unlock_bh(&dsi_clk_lock);
 
 	mipi_dsi_unprepare_clocks();
+
+	if (mipi_dsi_pdata && mipi_dsi_pdata->panel_power_save)
+		mipi_dsi_pdata->panel_power_save(0);
+
 	if (mipi_dsi_pdata && mipi_dsi_pdata->dsi_power_save)
 		mipi_dsi_pdata->dsi_power_save(0);
 
