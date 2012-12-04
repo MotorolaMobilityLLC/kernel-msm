64b1756a1e457c8013e694c944e7c874d520cb85
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-12-03 19:06:32 -0800

    wlan: cs release 3.2.0.34
    CL 660889 on 2012/11/30 by kiranv 'Set Protect frame bit for TDLS '
    CL 660898 on 2012/11/30 by lhoonki 'wlan: Fix conn_state stuck conn'
    CL 660916 on 2012/12/01 by c_sgirig 'When RoamRssiDiff=0, Roam to an'
    CL 660918 on 2012/12/01 by c_sgirig 'Increase max number of BSS in t'
    CL 660994 on 2012/12/03 by skuppa 'Roam if RSSI(candidate) > RSSI('
    CL 661009 on 2012/12/03 by sgolti 'wlan: Added FATAL prints so tha'
    CL 661032 on 2012/12/03 by coresvc' Add support for RTS/CTS scan
    
    Change-Id: I3eadeaff8300ccd2dbf68b07ccbcf9b7c459545f
    CR-Fixed: 426290, 422187, 427041, 425972, 427062, 423337, 423394
    (cherry picked from commit 0295295c49a2a1c451591a1553299034e8a31a47)

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,8 +40,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            33
+#define QWLAN_VERSION_BUILD            34
 
-#define QWLAN_VERSIONSTR               "3.2.0.33"
+#define QWLAN_VERSIONSTR               "3.2.0.34"
 
 #endif /* QWLAN_VERSION_H */
