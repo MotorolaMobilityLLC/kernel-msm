2881832bc1ac317ba9cc0a1496311cd4e848e76a
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-12-07 14:38:02 -0800

    wlan: cs release 3.2.0.38
    CL 661634 on 2012/12/06 by ramalraj   CLs to include uBSP configuration
    item to the INI file
    CL 661669 on 2012/12/06 by mkamalak 'Add a cfg item to enable/disabl'
    wlan: cp prima/riva/inc to wcnss/inc
    
    Change-Id: I0139318321b50418466e970eb5906a9194079379
    CR-Fixed: 428109, 428109

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,8 +40,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            37
+#define QWLAN_VERSION_BUILD            38
 
-#define QWLAN_VERSIONSTR               "3.2.0.37"
+#define QWLAN_VERSIONSTR               "3.2.0.38"
 
 #endif /* QWLAN_VERSION_H */
