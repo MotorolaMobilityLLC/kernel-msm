af45ae48027fafa9a33b2697c48b6f1a3a49a220
Andrew Wheeler <thf876@motorola.com>
2012-12-09 12:05:08 -0600

    IKQCOMSD1-1885 msm_fb: display: Disable bordefill (WAR la23-merge-hack)
    After merging AU18 the display does not always wake after
    pressing the power key: you must also press the volume
    buttons or the touchscreen.   When this issue is reproduced, user space
    might get null borderill pipe, so it doesn't update screen when we press power
    button. So mdp doesn't turn on display. When we press volume down
    button, there will be a overlay volume dialog in UI which will call
    overlay play, screen will be turned on this time because mdp thinks this
    is first update. Touching screen could also turn on display, it is
    similar - it drags lock, so there will be overlay update(play) to mdp.
    
    Disable borderfill pipe as a temporary work-around.
    
    Change-Id: Ifbb495ab8c3a858e02dbd63d18becf9e9e5764d8

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -346,7 +346,11 @@ uint32 mdp4_overlay_panel_list(void)
 
 int mdp4_overlay_borderfill_supported(void)
 {
-	return (mdp_rev >= MDP_REV_42);
+    // la23-merge-hack: borderfill pipe is sometimes null, which
+    // causes the display to not wake up on power-button press.
+    // Disable borderfill until we can resolve the issue.
+//	return (mdp_rev >= MDP_REV_42);
+    return 0;
 }
 
 void mdp4_overlay_dmae_cfg(struct msm_fb_data_type *mfd, int atv)
