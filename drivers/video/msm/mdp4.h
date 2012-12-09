096acdc96a8d9c4c3ba4c19e6829d3b7860ba8f0
Andrew Wheeler <thf876@motorola.com>
2012-12-08 21:50:27 -0600

    Revert "msm_fb: display: Consolidate commit ioctls" (la23-merge-hack)
    This commit causes SMQ display to not come up.  Reverting until we
    can analyze and provide a real fix.
    
    Note: Keeping the MDP_DISPLAY_COMMIT_OVERLAY #define because
          it's used by the user code.
    
    This reverts commit 309c0ca6eb74c5137650fa6419b63642a081d690.
    
    Conflicts:
    	drivers/video/msm/mdp4.h
    	drivers/video/msm/mdp4_overlay.c
    	include/linux/msm_mdp.h
    
    Change-Id: I98b9df980731285874ed83867c456b7415cc30c8

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -569,7 +569,7 @@ void mdp4_overlay0_done_dsi_video(int cndx);
 void mdp4_overlay0_done_dsi_cmd(int cndx);
 void mdp4_primary_rdptr(void);
 void mdp4_dsi_cmd_overlay(struct msm_fb_data_type *mfd);
-int mdp4_overlay_commit(struct fb_info *info);
+int mdp4_overlay_commit(struct fb_info *info, int mixer);
 int mdp4_dsi_video_pipe_commit(int cndx, int wait);
 int mdp4_dsi_cmd_pipe_commit(int cndx, int wait);
 int mdp4_lcdc_pipe_commit(int cndx, int wait);
