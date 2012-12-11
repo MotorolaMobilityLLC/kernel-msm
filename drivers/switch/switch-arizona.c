2de2c8a7cd3a8d5092fb60d57b142cc583493955
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-12-11 15:07:32 +0900

    switch: arizona: Allow configuration of MICBIAS rise time
    Allow configuration of the rise time for MICBIAS via platform data, the
    delay required depends on things like the external component selection.
    
    Change-Id: Ia3ff203f0ae0d3556acefcfda10b79ed214e19ae
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -382,6 +382,12 @@ static int __devinit arizona_switch_probe(struct platform_device *pdev)
 		}
 	}
 
+	if (arizona->pdata.micd_bias_start_time)
+		regmap_update_bits(arizona->regmap, ARIZONA_MIC_DETECT_1,
+				   ARIZONA_MICD_BIAS_STARTTIME_MASK,
+				   arizona->pdata.micd_bias_start_time
+				   << ARIZONA_MICD_BIAS_STARTTIME_SHIFT);
+
 	arizona_switch_set_mode(info, 0);
 
 	info->input = input_allocate_device();
@@ -439,9 +445,7 @@ static int __devinit arizona_switch_probe(struct platform_device *pdev)
 	}
 
 	regmap_update_bits(arizona->regmap, ARIZONA_MIC_DETECT_1,
-			   ARIZONA_MICD_BIAS_STARTTIME_MASK |
 			   ARIZONA_MICD_RATE_MASK,
-			   7 << ARIZONA_MICD_BIAS_STARTTIME_SHIFT |
 			   8 << ARIZONA_MICD_RATE_SHIFT);
 
 	arizona_clk32k_enable(arizona);
