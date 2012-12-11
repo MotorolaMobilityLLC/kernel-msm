23b04db439002a300603689e6397027661d0b753
Laura Abbott <lauraa@codeaurora.org>
2012-12-11 15:09:03 -0800

    gpu: ion: Restrict access to CP heap
    On certain targets, the CP heap should only be used
    for secure allocations. Add a check to determine which
    targets are allowed to make non-secure allocations from
    the CP heap type. Targets with this restriction will
    fall back to an alternate heap.
    
    Change-Id: Ieaa9e76cbf2dc3ea1da6f4e75a4de903c39a3077
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- arch/arm/mach-msm/board-8930.c
+++ arch/arm/mach-msm/board-8930.c
@@ -344,6 +344,7 @@ static struct ion_cp_heap_pdata cp_mm_msm8930_ion_pdata = {
 	.mem_is_fmem = FMEM_ENABLED,
 	.fixed_position = FIXED_MIDDLE,
 	.is_cma	= 1,
+	.no_nonsecure_alloc = 1,
 };
 
 static struct ion_cp_heap_pdata cp_mfc_msm8930_ion_pdata = {
@@ -352,6 +353,7 @@ static struct ion_cp_heap_pdata cp_mfc_msm8930_ion_pdata = {
 	.reusable = 0,
 	.mem_is_fmem = FMEM_ENABLED,
 	.fixed_position = FIXED_HIGH,
+	.no_nonsecure_alloc = 1,
 };
 
 static struct ion_co_heap_pdata co_msm8930_ion_pdata = {
--- drivers/gpu/ion/ion_cp_heap.c
+++ drivers/gpu/ion/ion_cp_heap.c
@@ -2,7 +2,7 @@
  * drivers/gpu/ion/ion_cp_heap.c
  *
  * Copyright (C) 2011 Google, Inc.
- * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -104,6 +104,7 @@ struct ion_cp_heap {
 	size_t heap_size;
 	dma_addr_t handle;
 	int cma;
+	int disallow_non_secure_allocation;
 };
 
 enum {
@@ -318,6 +319,13 @@ ion_phys_addr_t ion_cp_allocate(struct ion_heap *heap,
 		mutex_unlock(&cp_heap->lock);
 		pr_err("ION cannot allocate un-secure memory from protected"
 			" heap %s\n", heap->name);
+		return ION_CP_ALLOCATE_FAIL;
+	}
+
+	if (!secure_allocation && cp_heap->disallow_non_secure_allocation) {
+		mutex_unlock(&cp_heap->lock);
+		pr_debug("%s: non-secure allocation disallowed from this heap\n",
+			__func__);
 		return ION_CP_ALLOCATE_FAIL;
 	}
 
@@ -1065,6 +1073,8 @@ struct ion_heap *ion_cp_heap_create(struct ion_platform_heap *heap_data)
 		cp_heap->iommu_2x_map_domain =
 				extra_data->iommu_2x_map_domain;
 		cp_heap->cma = extra_data->is_cma;
+		cp_heap->disallow_non_secure_allocation =
+			extra_data->no_nonsecure_alloc;
 
 	}
 
