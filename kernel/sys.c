9bdd4dd352f824dd6ffdf4b05545d15ec546b322
Tianshui Shi <kfp634@motorola.com>
2012-12-12 08:53:49 -0500

    kernel: sys : print task info on reboot or poweroff
    The current task name(pid), and its parent task name(pid)
    are printed out to console on reboot or power off. This
    patch is to indicate which task has triggered the reboot or
    power off.
    
    Change-Id: I6f2ef8fcd39952e0423b79b2567323860c63de10
    Signed-off-by: Tianshui Shi <kfp634@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/504575
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    (cherry picked from commit 3cc8a4804ab19fc4d850b476edd134e8c22a771f)
    
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Change-Id: I58df570e0588b92fe4676dfd2d7b63221f78450b
    Reviewed-on: http://gerrit.pcs.mot.com/621836
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -402,6 +402,10 @@ void kernel_restart(char *cmd)
 		printk(KERN_EMERG "Restarting system.\n");
 	else
 		printk(KERN_EMERG "Restarting system with command '%s'.\n", cmd);
+	printk(KERN_EMERG "Current task:%s(%d) Parent task:%s(%d)\n",
+		current->comm, current->pid,
+		current->real_parent->comm,
+		current->real_parent->pid);
 	kmsg_dump(KMSG_DUMP_RESTART);
 	machine_restart(cmd);
 }
@@ -445,6 +449,10 @@ void kernel_power_off(void)
 	migrate_to_reboot_cpu();
 	syscore_shutdown();
 	printk(KERN_EMERG "Power down.\n");
+	printk(KERN_EMERG "Current task:%s(%d) Parent task:%s(%d)\n",
+		current->comm, current->pid,
+		current->real_parent->comm,
+		current->real_parent->pid);
 	kmsg_dump(KMSG_DUMP_POWEROFF);
 	machine_power_off();
 }
