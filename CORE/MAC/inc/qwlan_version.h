9e6dbe6d7ac7461f7b71dff15484212167b2a0b2
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-12-13 15:41:05 -0800

    wlan: cs release 3.2.0.40
    CL 661809 on 2012/12/07 by c_sgirig 'LFR does not bgscan on channel '
    CL 662492 on 2012/12/10 by c_sgirig 'Fix compile error if LFR is tur'
    CL 662490 on 2012/12/10 by sanoopk   Rerun DPD if divide by zero
    CL 662503 on 2012/12/10 by c_vkuntu 'Added the Initilization before '
    CL 662534 on 2012/12/10 by npullela 'Added logs in WDI and DXE to en'
    CL 662535 on 2012/12/10 by jjohnson 'Remove obsolete macro WNI_POLAR'
    CL 662589 on 2012/12/11 by datre 'Roaming: Fix to Not Send two Po'
    CL 662594 on 2012/12/11 by kiranv 'Add QCA copyright information i'
    CL 662613 on 2012/12/11 by kanand   Remove new Riva 1.2 compiler
    CL 662691 on 2012/12/11 by c_sgirig  Send RSSI value along with
    CL 662702 on 2012/12/11 by c_sgirig 'Send RSSI value along with thre'
    CL 662827 on 2012/12/12 by c_sgirig 'Fix compile errors on top of CL'
    CL 662743 on 2012/12/11 by ganeshk 'Add module name as "wlan:" in l'
    CL 662817 on 2012/12/12 by jjohnson 'Fix misspellings in prima code '
    CL 662776 on 2012/12/12 by spuligil 'Memory Leak in csrApiRoam.c  Fi'
    CL 663016 on 2012/12/12 by spuligil 'Moved code related to LFR under'
    CL 662921 on 2012/12/12 by mkamalak 'Add a cfg item to enable/disabl'
    CL 662990 on 2012/12/12 by ashishka 'Changing  log level for CL#6620'
    CL 662994 on 2012/12/12 by yathishh '[32STA] P2P Go crash on Scan Wi'
    CL 663011 on 2012/12/12 by kganesh 'RemovedHard Tabs introduced bec'
    CL 663025 on 2012/12/13 by c_gowri 'Fix for AP sending wrong regula'
    CL 663028 on 2012/12/13 by c_pavan 'Fix for clock work error  New F'
    
    CR-Fixed: 429923, 423554, 428416, 430317, 429978, 428151, 426223,
    429459, 429934, 431183, 428302, 429978, 428302, 428109, 419544, 431521,
    362084, 423293
    
    Change-Id: I701d25b7dcf28d6bb1f2ab894357160bd2822fe6

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,8 +40,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            39
+#define QWLAN_VERSION_BUILD            40
 
-#define QWLAN_VERSIONSTR               "3.2.0.39"
+#define QWLAN_VERSIONSTR               "3.2.0.40"
 
 #endif /* QWLAN_VERSION_H */
