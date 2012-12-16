91a420ab523f2a09772d15521524d668b20cf018
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-12-16 11:16:18 -0800

    wlan: add support for pronto

--- Android.mk
+++ Android.mk
@@ -1,7 +1,19 @@
-# Android makefile for the WLAN WCNSS/Prima Module
+# Android makefile for the WLAN Module
 
-# Build/Package only in case of 8960 target
+# Build/Package options for 8960 target
 ifeq ($(call is-board-platform,msm8960),true)
+WLAN_CHIPSET := prima
+WLAN_SELECT := CONFIG_PRIMA_WLAN=m
+endif
+
+# Build/Package options for 8974 target
+ifeq ($(call is-board-platform,msm8974),true)
+WLAN_CHIPSET := pronto
+WLAN_SELECT := CONFIG_PRONTO_WLAN=m
+endif
+
+# Build/Package only in case of supported target
+ifneq ($(WLAN_CHIPSET),)
 
 LOCAL_PATH := $(call my-dir)
 
@@ -17,32 +29,33 @@ else
 endif
 
 
-# Build prima_wlan.ko
+# Build wlan.ko as either prima_wlan.ko or pronto_wlan.ko
 ###########################################################
 
 # This is set once per LOCAL_PATH, not per (kernel) module
-KBUILD_OPTIONS := WLAN_PRIMA=../$(WLAN_BLD_DIR)/prima
+KBUILD_OPTIONS := WLAN_ROOT=../$(WLAN_BLD_DIR)/prima
 # We are actually building wlan.ko here, as per the
-# requirement we are specifying prima_wlan.ko as LOCAL_MODULE.
-# This means we need to rename the module to prima_wlan.ko
+# requirement we are specifying <chipset>_wlan.ko as LOCAL_MODULE.
+# This means we need to rename the module to <chipset>_wlan.ko
 # after wlan.ko is built.
 KBUILD_OPTIONS += MODNAME=wlan
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
+KBUILD_OPTIONS += $(WLAN_SELECT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE              := prima_wlan.ko
+LOCAL_MODULE              := $(WLAN_CHIPSET)_wlan.ko
 LOCAL_MODULE_KBUILD_NAME  := wlan.ko
 LOCAL_MODULE_TAGS         := debug
 LOCAL_MODULE_DEBUG_ENABLE := true
-LOCAL_MODULE_PATH         := $(TARGET_OUT)/lib/modules/prima
+LOCAL_MODULE_PATH         := $(TARGET_OUT)/lib/modules/$(WLAN_CHIPSET)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
 
 #Create symbolic link
 $(shell mkdir -p $(TARGET_OUT)/lib/modules; \
-        ln -sf /system/lib/modules/prima/prima_wlan.ko \
+        ln -sf /system/lib/modules/$(WLAN_CHIPSET)/$(WLAN_CHIPSET)_wlan.ko \
                $(TARGET_OUT)/lib/modules/wlan.ko)
 
-endif
+endif # DLKM check
 
-endif # 8960 target check
+endif # supported target check
