78ba3ec4b435c0553ababc3c78afdec652b57578
Yi-wei Zhao <gbjc64@motorola.com>
2012-12-20 18:06:21 -0600

    IKQCOMSD1-1961 mm: page_alloc: retry direct compaction
    When system load is heavy, memory commpaction may encounter
    transient failure; add retry mechanism. And add the following
    statistic variable "compact_retry_success" in /proc/vmstat
    to see how many compaction success due to the retry.
    
    And __zone_watermark_ok() will easily return false when system
    is busy, which will make compaction failure as well. Amend some
    to save margin cases.
    
    This patch is ported from the patch:
     * IKJBREL1-8789 mem compaction enhancement
       Reviewed-on: http://gerrit.pcs.mot.com/494962
    
    Change-Id: I5ad08364678c7e68993f66e0c0d43d97712a99b6
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/507230
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1616,10 +1616,20 @@ static bool __zone_watermark_ok(struct zone *z, int order, unsigned long mark,
 		/* At the next order, this order's pages become unavailable */
 		free_pages -= z->free_area[o].nr_free << o;
 
+		/*
+		 * z->free_area[o].nr_free is changing. It may increase if
+		 * there are newly freed pages. So free_pages may become
+		 * negative (then always < min). But it should not block
+		 * memory allocation due to new free pages added in lower
+		 * order. Just amend some to save some margin case...
+		 */
+		if (free_pages < 0)
+			free_pages = 0;
+
 		/* Require fewer higher order pages to be free */
 		min >>= min_free_order_shift;
 
-		if (free_pages <= min)
+		if (free_pages < min)
 			return false;
 	}
 	return true;
@@ -2064,6 +2074,7 @@ out:
 }
 
 #ifdef CONFIG_COMPACTION
+#define COMPACTION_RETRY_TIMES 2
 /* Try memory compaction for high-order allocations before reclaim */
 static struct page *
 __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
@@ -2074,6 +2085,7 @@ __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 	unsigned long *did_some_progress)
 {
 	struct page *page;
+	int retry_times = 0, order_adj = order;
 
 	if (!order)
 		return NULL;
@@ -2083,8 +2095,9 @@ __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 		return NULL;
 	}
 
+retry_compact:
 	current->flags |= PF_MEMALLOC;
-	*did_some_progress = try_to_compact_pages(zonelist, order, gfp_mask,
+	*did_some_progress = try_to_compact_pages(zonelist, order_adj, gfp_mask,
 						nodemask, sync_migration);
 	current->flags &= ~PF_MEMALLOC;
 	if (*did_some_progress != COMPACT_SKIPPED) {
@@ -2103,7 +2116,20 @@ __alloc_pages_direct_compact(gfp_t gfp_mask, unsigned int order,
 			if (order >= preferred_zone->compact_order_failed)
 				preferred_zone->compact_order_failed = order + 1;
 			count_vm_event(COMPACTSUCCESS);
+
+			if (retry_times)
+				count_vm_event(COMPACTSUCCESS_RETRY);
+
 			return page;
+		}
+
+		if (retry_times++ < COMPACTION_RETRY_TIMES) {
+
+			order_adj++;
+			if (order_adj >= MAX_ORDER)
+				order_adj = -1;
+
+			goto retry_compact;
 		}
 
 		/*
