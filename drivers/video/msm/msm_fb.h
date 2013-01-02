d614c3611ee055136d5c2e03a86fda9e50cbc9ac
Ken Zhang <kenz@codeaurora.org>
2013-01-02 17:01:48 -0500

    msm: display: sync point clean up
    Remove unused fence input in mdp_display_commit
    structure
    Use different timeline name for different fb
    
    Change-Id: I4edd335b36dce057ff3e54302af6088a9cc470ec
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2008-2013, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -231,8 +231,9 @@ int msm_fb_writeback_stop(struct fb_info *info);
 int msm_fb_writeback_terminate(struct fb_info *info);
 int msm_fb_detect_client(const char *name);
 int calc_fb_offset(struct msm_fb_data_type *mfd, struct fb_info *fbi, int bpp);
-int msm_fb_wait_for_fence(struct msm_fb_data_type *mfd);
+void msm_fb_wait_for_fence(struct msm_fb_data_type *mfd);
 int msm_fb_signal_timeline(struct msm_fb_data_type *mfd);
+void msm_fb_release_timeline(struct msm_fb_data_type *mfd);
 #ifdef CONFIG_FB_BACKLIGHT
 void msm_fb_config_backlight(struct msm_fb_data_type *mfd);
 #endif
