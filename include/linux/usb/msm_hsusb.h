2b0197988f6b1fd953e9c296237f5f4a73a6c7eb
Pavankumar Kondeti <pkondeti@codeaurora.org>
2013-01-04 12:28:10 +0530

    USB: msm_otg: Increase data contact detection timeout
    Battery charging specification allows data contact detection (DCD)
    timeout to be any value between 300 and 900 msec.  As DCD is reliably
    working, increase the timeout for detecting slow cable insertion cases.
    DCD would fail with ACA-dock and ACA-A + HS/FS device.  Use 750 msec
    as timeout.  The 150 msec (900 - 750) safe guard time compensates the
    time taken from session valid to DCD enable.  Decrese the DCD polling
    period from 100 msec to 50 msec to exit DCD state quickly.
    
    The 150 msec safe guard time compensates the time taken from session valid
    to starting charger detection procedure.
    
    CRs-Fixed: 437484
    (cherry picked from commit ebb4a2d613128a2db288402bc834622a36e71956)
    
    Conflicts:
    
    	drivers/usb/otg/msm_otg.c
    
    Change-Id: I0e014d2ae0f9e00eaba179af2244924cf4561c90
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -331,7 +331,7 @@ struct msm_otg {
 	struct delayed_work check_ta_work;
 	enum usb_chg_state chg_state;
 	enum usb_chg_type chg_type;
-	u8 dcd_retries;
+	unsigned dcd_time;
 	struct wake_lock wlock;
 	struct notifier_block usbdev_nb;
 	unsigned mA_port;
