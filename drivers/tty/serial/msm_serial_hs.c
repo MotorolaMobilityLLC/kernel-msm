311c9ed1840c82598b95e862f6deb436e4326aa9
Mayank Rana <mrana@codeaurora.org>
2013-01-08 10:15:56 +0530

    msm_serial_hs: consider flush buffer request only when DMA is in progress
    TTY Core uses flush_buffer to clear out any output data before closing
    port, whereas UART application can always request to do flush output data
    before starting data transfer using tty supported ioctl().
    receive_tty_flush is used to make decision on completion of any DMA Tx
    data transfer. Using same approach when there is no DMA Tx transfer is
    happening, it is breaking next DMA Tx transfer functionality. Hence
    update receive_tty_flush only when there is any DMA TX transfer active.
    
    (cherry picked from commit 53069734628ebce341cf1ef778c91dbcbbcf67db)
    
    Change-Id: I26f4ceece422674e1f7647e5dfadb476cec1ef92
    CRs-Fixed: 438057
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -1261,11 +1261,12 @@ static void msm_hs_enable_ms_locked(struct uart_port *uport)
 
 }
 
-static void msm_hs_flush_buffer(struct uart_port *uport)
+static void msm_hs_flush_buffer_locked(struct uart_port *uport)
 {
 	struct msm_hs_port *msm_uport = UARTDM_TO_MSM(uport);
 
-	msm_uport->tty_flush_receive = true;
+	if (msm_uport->tx.dma_in_flight)
+		msm_uport->tty_flush_receive = true;
 }
 
 /*
@@ -2244,7 +2245,7 @@ static struct uart_ops msm_hs_ops = {
 	.config_port = msm_hs_config_port,
 	.release_port = msm_hs_release_port,
 	.request_port = msm_hs_request_port,
-	.flush_buffer = msm_hs_flush_buffer,
+	.flush_buffer = msm_hs_flush_buffer_locked,
 };
 
 module_init(msm_serial_hs_init);
