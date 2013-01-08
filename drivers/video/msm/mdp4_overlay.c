027518be3aef7ce9c5467f9d2757e013bef539cc
Siddhartha Agrawal <agrawals@codeaurora.org>
2013-01-07 17:28:13 -0800

    msm_fb:display: Enable LVDS panel support
    The JB MR1 update was not recognizing the LVDS panel and hence
    throwing an invalid panel error. Adding check for LVDS panel to
    enable display on 8064 CDP.
    
    Change-Id: Ib125cc797b288ca027f91778d9892acfa00a3214
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3523,6 +3523,7 @@ int mdp4_overlay_commit(struct fb_info *info)
 	case MIPI_VIDEO_PANEL:
 		mdp4_dsi_video_pipe_commit(0, 1);
 		break;
+	case LVDS_PANEL:
 	case LCDC_PANEL:
 		mdp4_lcdc_pipe_commit(0, 1);
 		break;
