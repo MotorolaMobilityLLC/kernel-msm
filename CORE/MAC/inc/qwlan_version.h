add701a0b3aa16222eadfe8e318fd6d7a4c61c22
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-01-09 13:26:21 -0800

    wlan: cs release 3.2.0.44
    CL 664233 on 2012/12/21 by skuppa 'LFR: invalid channel in INI cau'
    CL 664313 on 2012/12/21 by schang 'TX_Thread Spinng problem fix  N'
    CL 664119 on 2012/12/20 by c_sgirig 'Support to configure Roaming va'
    CL 664122 on 2012/12/20 by c_sgirig 'Remove FTRssiFilterPeriod  New '
    CL 664179 on 2012/12/20 by sgolti 'Fixed "Out of VOS MSG wrapper i'
    CL 664182 on 2012/12/20 by sgolti 'Fixed Incorrect device mode aft'
    CL 664187 on 2012/12/20 by c_aravik ' Feature to record KMSG in circ'
    CL 664419 on 2012/12/23 by kganesh 'RemovedHard Tabs introduced bec'
    CL 664613 on 2012/12/26 by tushnimb 'SME code cleanup  Added extra d'
    CL 664698 on 2012/12/27 by sgolti 'wlan: Cancel existing remain_on'
    CL 664719 on 2012/12/27 by kiranv 'CL AID pool to peerIndex po'
    CL 664823 on 2012/12/29 by sgolti 'wlan: Fixed SME queue stuck iss'

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            43
+#define QWLAN_VERSION_BUILD            44
 
-#define QWLAN_VERSIONSTR               "3.2.0.43"
+#define QWLAN_VERSIONSTR               "3.2.0.44"
 
 #endif /* QWLAN_VERSION_H */
 
