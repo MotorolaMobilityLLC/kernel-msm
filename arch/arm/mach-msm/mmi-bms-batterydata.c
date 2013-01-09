fd28d60161523dea41fbcb073ceaa29999bebca3
Kenny Kessler <kenny.kessler@motorola.com>
2013-01-09 11:15:36 -0600

    arm: msm: Initial EX34 BMS Entry
    This adds the EX34 to the BMS list so that it can
    be identified.  The final BMS tables are not done
    so the closest match was the EG30 LG Tables. The
    proper eprom values for the initial samples are
    include as well.
    
    Change-Id: I00a4bf1ab6a358c6065cff8f41811808ab578581
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/510067
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Jason Knopsnyder <w19069@motorola.com>

--- arch/arm/mach-msm/mmi-bms-batterydata.c
+++ arch/arm/mach-msm/mmi-bms-batterydata.c
@@ -153,6 +153,39 @@ static struct mmi_battery_cell mmi_eu20_lg_cell_data = {
 	.cell_id = 0x4250, /* Cell code BP */
 };
 
+/* Initial EX34 LG Tables and Charging Parameters */
+static struct pm8921_bms_battery_data  mmi_ex34_lg_metering_data = {
+	.fcc			= 2120,
+	.fcc_temp_lut		= &mmi_eg30_lg_fcc_temp,
+	.fcc_sf_lut		= &mmi_eg30_lg_fcc_sf,
+	.pc_temp_ocv_lut	= &mmi_eg30_lg_pc_temp_ocv,
+	.pc_sf_lut		= &mmi_eg30_lg_pc_sf,
+	.default_rbatt_mohm	= 196,
+};
+
+static struct pm8921_charger_battery_data mmi_ex34_lg_charging_data = {
+	.max_voltage			= 4350,
+	.min_voltage			= 3200,
+	.resume_voltage_delta		= 100,
+	.term_current			= 179,
+	.cool_temp			= 0,
+	.warm_temp			= 45,
+	.max_bat_chg_current		= 2014,
+	.cool_bat_chg_current		= 0,
+	.warm_bat_chg_current		= 0,
+	.cool_bat_voltage		= 4000,
+	.warm_bat_voltage		= 4000,
+	.step_charge_current		= 1600,
+	.step_charge_voltage		= 4200,
+};
+
+static struct mmi_battery_cell mmi_ex34_lg_cell_data = {
+	.capacity = 0xD4,
+	.peak_voltage = 0xB9,
+	.dc_impedance = 0x62,
+	.cell_id = 0x4254, /* Cell code BT */
+};
+
 /* EB40 SDI Tables and Charging Parameters */
 static struct single_row_lut mmi_eb40_lg_fcc_temp = {
 	.x	= {-10, 0, 23, 60},
@@ -942,6 +975,7 @@ struct mmi_battery_list mmi_batts = {
 		&mmi_eb40_lg_cell_data,
 		&mmi_eg30_lg_cell_data,
 		&mmi_eu20_lg_cell_data,
+		&mmi_ex34_lg_cell_data,
 	},
 	.bms_list = {
 		 &mmi_df_metering_data,
@@ -955,6 +989,7 @@ struct mmi_battery_list mmi_batts = {
 		 &mmi_eb40_lg_metering_data,
 		 &mmi_eg30_lg_metering_data,
 		 &mmi_eu20_lg_metering_data,
+		 &mmi_ex34_lg_metering_data,
 	 },
 	.chrg_list = {
 		 &mmi_df_charging_data,
@@ -968,5 +1003,6 @@ struct mmi_battery_list mmi_batts = {
 		 &mmi_eb40_lg_charging_data,
 		 &mmi_eg30_lg_charging_data,
 		 &mmi_eu20_lg_charging_data,
+		 &mmi_ex34_lg_charging_data,
 	 },
 };
