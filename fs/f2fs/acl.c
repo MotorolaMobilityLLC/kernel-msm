03cb8c402c0e7e1b5d1bd93efd8b1c8d83a2a2f7
Chris Fries <C.Fries@motorola.com>
2013-01-10 13:59:16 -0600

    fs: f2fs: Backport to K3.4
    Backporting F2FS from K3.8-rc2 to K3.4
    
    - Adjust for missing e_uid/e_gid in acl entries
    - mismatch in posix_acl_to_xattr/posix_acl_from_xattr
    - missing sb_start_pagefault/sb_end_pagefault
    - request_list in request_queue is rq instead of root_rl
    - missing i_uid_write, i_uid_read
    - clear_inode renamed from end_writeback
    - Missing QSTR_INIT definition
    - f2fs_lookup prototype mismatch
    - f2fs_create prototype mismatch
    
    Change-Id: I6c2dd145822271d69f2a6fed0586179f7fcd6fbe
    Reviewed-on: http://gerrit.pcs.mot.com/510430
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>
    Submit-Approved: Jira Key <JIRAKEY@motorola.com>
    (cherry picked from commit 9c3783f4b8292c07395aae61dbca21e2fc4725b4)

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -87,16 +87,8 @@ static struct posix_acl *f2fs_acl_from_disk(const char *value, size_t size)
 			break;
 
 		case ACL_USER:
-			acl->a_entries[i].e_uid =
-				make_kuid(&init_user_ns,
-						le32_to_cpu(entry->e_id));
-			entry = (struct f2fs_acl_entry *)((char *)entry +
-					sizeof(struct f2fs_acl_entry));
-			break;
 		case ACL_GROUP:
-			acl->a_entries[i].e_gid =
-				make_kgid(&init_user_ns,
-						le32_to_cpu(entry->e_id));
+			acl->a_entries[i].e_id = le32_to_cpu(entry->e_id);
 			entry = (struct f2fs_acl_entry *)((char *)entry +
 					sizeof(struct f2fs_acl_entry));
 			break;
@@ -133,16 +125,8 @@ static void *f2fs_acl_to_disk(const struct posix_acl *acl, size_t *size)
 
 		switch (acl->a_entries[i].e_tag) {
 		case ACL_USER:
-			entry->e_id = cpu_to_le32(
-					from_kuid(&init_user_ns,
-						acl->a_entries[i].e_uid));
-			entry = (struct f2fs_acl_entry *)((char *)entry +
-					sizeof(struct f2fs_acl_entry));
-			break;
 		case ACL_GROUP:
-			entry->e_id = cpu_to_le32(
-					from_kgid(&init_user_ns,
-						acl->a_entries[i].e_gid));
+			entry->e_id = cpu_to_le32(acl->a_entries[i].e_id);
 			entry = (struct f2fs_acl_entry *)((char *)entry +
 					sizeof(struct f2fs_acl_entry));
 			break;
@@ -355,7 +339,7 @@ static int f2fs_xattr_get_acl(struct dentry *dentry, const char *name,
 		return PTR_ERR(acl);
 	if (!acl)
 		return -ENODATA;
-	error = posix_acl_to_xattr(&init_user_ns, acl, buffer, size);
+	error = posix_acl_to_xattr(acl, buffer, size);
 	posix_acl_release(acl);
 
 	return error;
@@ -377,7 +361,7 @@ static int f2fs_xattr_set_acl(struct dentry *dentry, const char *name,
 		return -EPERM;
 
 	if (value) {
-		acl = posix_acl_from_xattr(&init_user_ns, value, size);
+		acl = posix_acl_from_xattr(value, size);
 		if (IS_ERR(acl))
 			return PTR_ERR(acl);
 		if (acl) {
