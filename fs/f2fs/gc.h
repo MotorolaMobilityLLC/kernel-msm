60ef352c321439a3d74c50e56e594b2c4269b91c
Chris Fries <C.Fries@motorola.com>
2013-01-10 13:59:16 -0600

    fs: f2fs: Backport to K3.4
    Backporting F2FS from K3.8-rc2 to K3.4
    
    - Adjust for missing e_uid/e_gid in acl entries
    - mismatch in posix_acl_to_xattr/posix_acl_from_xattr
    - missing sb_start_pagefault/sb_end_pagefault
    - request_list in request_queue is rq instead of root_rl
    - missing i_uid_write, i_uid_read
    - clear_inode renamed from end_writeback
    - Missing QSTR_INIT definition
    - f2fs_lookup prototype mismatch
    - f2fs_create prototype mismatch
    
    Change-Id: I6c2dd145822271d69f2a6fed0586179f7fcd6fbe
    Reviewed-on: http://gerrit.pcs.mot.com/510430
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>
    Submit-Approved: Jira Key <JIRAKEY@motorola.com>

--- fs/f2fs/gc.h
+++ fs/f2fs/gc.h
@@ -101,7 +101,7 @@ static inline int is_idle(struct f2fs_sb_info *sbi)
 {
 	struct block_device *bdev = sbi->sb->s_bdev;
 	struct request_queue *q = bdev_get_queue(bdev);
-	struct request_list *rl = &q->root_rl;
+	struct request_list *rl = &q->rq;
 	return !(rl->count[BLK_RW_SYNC]) && !(rl->count[BLK_RW_ASYNC]);
 }
 
