4606b322ff6bc36f91c51628ca56f618a5d01e47
Hans de Goede <hdegoede@redhat.com>
2013-01-11 13:19:27 +0100

    Revert "(CR) cgroup: check cgrp's count before removing"
    This fix is replaced with it's upstream equivalend.
    
    This reverts commit 15da2c4b6af64f343ae137f88853aba1a2b9f3b0.
    
    Signed-off-by: Chris Fries <cfries@motorola.com>
    Change-Id: I78d69526ee91ae1452f5f82cfaaefe4320688459
    Reviewed-on: http://gerrit.mot.com/697284
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -4025,8 +4025,7 @@ again:
 
 	mutex_lock(&cgroup_mutex);
 	parent = cgrp->parent;
-	if (!cgroup_css_sets_empty(cgrp) || !list_empty(&cgrp->children) ||
-		atomic_read(&cgrp->count)) {
+	if (!cgroup_css_sets_empty(cgrp) || !list_empty(&cgrp->children)) {
 		clear_bit(CGRP_WAIT_ON_RMDIR, &cgrp->flags);
 		mutex_unlock(&cgroup_mutex);
 		return -EBUSY;
