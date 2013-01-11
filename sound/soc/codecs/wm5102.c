7b81cdb639419357925051e389c8d07927fd20da
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-01-11 22:59:35 +0000

    ASoC: wm5102: Add controls for firmware selection
    Change-Id: I832def2152696f7ffc411c925404cfa268197d84
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm5102.c
+++ sound/soc/codecs/wm5102.c
@@ -1546,6 +1546,10 @@ static int wm5102_codec_probe(struct snd_soc_codec *codec)
 
 	arizona_init_spk(codec);
 
+	ret = snd_soc_add_codec_controls(codec, wm_adsp_fw_controls, 1);
+	if (ret != 0)
+		return ret;
+
 	snd_soc_dapm_disable_pin(&codec->dapm, "HAPTICS");
 
 	priv->core.arizona->dapm = &codec->dapm;
