3a5ee5bfd68dc7acb7e54f4cba1d665fc6d90772
Dong-Dong Yang <gdb746@motorola.com>
2013-01-14 13:00:23 -0500

    staging: android: logger: Add kernel console log driver
    1. Fix the known drawback from /proc/kmsg kernel log source.
    2. Provide the unified timestamps for main, system, radio and kernel
    offline logs.
    
    Change-Id: I2534df1903ea0ac9a11748abf6546309e4882ac1
    Signed-off-by: Dong-Dong Yang <gdb746@motorola.com>
    Cherry-picked-by: Chris Fries <cfries@motorola.com>

--- drivers/staging/android/logger.h
+++ drivers/staging/android/logger.h
@@ -74,6 +74,8 @@ struct logger_entry {
 #define LOGGER_LOG_EVENTS	"log_events"	/* system/hardware events */
 #define LOGGER_LOG_SYSTEM	"log_system"	/* system/framework messages */
 #define LOGGER_LOG_MAIN		"log_main"	/* everything else */
+#define LOGGER_LOG_KERNEL      "log_kernel"    /* kernel */
+#define LOGGER_LOG_KERNEL_BOT  "log_kernel_bottom" /* kernel bottom */
 
 #define LOGGER_ENTRY_MAX_PAYLOAD	4076
 
