134318f0c9a539611423a22a94290bf8f339a112
Limei Wang <e12499@motorola.com>
2013-01-15 16:09:45 -0600

    msm_fb: display: enable panel CABC for ultra M
    This CR is to enable ultra M panel CABC output by programming 0x53 and
    0x55. ultra M Backlight IC will use it as PWM input in backlight driver.
    
    Change-Id: I6669d74e08629bd3b6e603e924d4d500ea9121a2
    Signed-off-by: Limei Wang <e12499@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/511514
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Bang Nguyen <bnguyen1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mipi_mot_cmd_jdi_hd_430.c
+++ drivers/video/msm/mipi_mot_cmd_jdi_hd_430.c
@@ -37,11 +37,8 @@ static struct mipi_dsi_phy_ctrl dsi_cmd_mode_phy_db = {
 static char enter_sleep[2] = {DCS_CMD_ENTER_SLEEP_MODE, 0x00};
 static char exit_sleep[2] = {DCS_CMD_EXIT_SLEEP_MODE, 0x00};
 static char display_off[2] = {DCS_CMD_SET_DISPLAY_OFF, 0x00};
-/* TODO
-static char led_pwm1[2] = {DCS_CMD_SET_BRIGHTNESS, 0xFF};
-static char led_pwm2[2] = {DCS_CMD_SET_CTRL_DISP, 0x2C};
-static char led_pwm3[2] = {DCS_CMD_SET_CABC, 0x03};
-*/
+static char led_pwm1[2] = {DCS_CMD_SET_CTRL_DISP, 0x2C};
+static char led_pwm2[2] = {DCS_CMD_SET_CABC, 0x03};
 
 /* Set scan line to 2/3 of the screen */
 /* TODO
@@ -50,6 +47,8 @@ static char set_scanline[3] = {DCS_CMD_SET_SCAN_LINE, 0x03, 0x55};
 
 static struct dsi_cmd_desc mot_cmd_on_cmds[] = {
 	{DTYPE_DCS_WRITE, 1, 0, 0, 120, sizeof(exit_sleep), exit_sleep},
+	{DTYPE_DCS_WRITE1, 1, 0, 0, 1, sizeof(led_pwm1), led_pwm1},
+	{DTYPE_DCS_WRITE1, 1, 0, 0, 1, sizeof(led_pwm2), led_pwm2},
 };
 
 static struct dsi_cmd_desc mot_display_off_cmds[] = {
