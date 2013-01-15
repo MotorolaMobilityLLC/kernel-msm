f1d0bde9f483ad27b9f6e7ab500a92fc575a7304
Jay Chokshi <jchokshi@codeaurora.org>
2013-01-15 13:06:27 -0800

    msm: socinfo: Add support for APQ8064AA
    Add the SoC ID and cpu_is wrapper for identifying the
    APQ8064AA SoC.
    
    Change-Id: Ia038088045e1170fe897f8ba0a9ab725e5acab20
    Signed-off-by: Jay Chokshi <jchokshi@codeaurora.org>
    (cherry picked from commit 38ccbee9a1f823640542f1e9d73a041d18337b2e)
    
    Conflicts:
    
    	arch/arm/mach-msm/socinfo.c
    
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- arch/arm/mach-msm/socinfo.c
+++ arch/arm/mach-msm/socinfo.c
@@ -293,7 +293,10 @@ static enum msm_cpu cpu_of_id[] = {
 	[154] = MSM_CPU_8930AB,
 	[155] = MSM_CPU_8930AB,
 	[156] = MSM_CPU_8930AB,
-	[157] = MSM_CPU_8930AB
+	[157] = MSM_CPU_8930AB,
+
+	/* 8064AA IDs */
+	[172] = MSM_CPU_8064AA,
 
 	/* Uninitialized IDs are not known to run Linux.
 	   MSM_CPU_UNKNOWN is set to 0 to ensure these IDs are
