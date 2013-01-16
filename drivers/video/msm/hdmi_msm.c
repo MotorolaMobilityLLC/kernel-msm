9d60ed7349677d14497ac2171eb128ad94251a54
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-01-16 17:59:04 +0530

    msm_fb: hdmi: update data on resolution change
    On changing resolution, all information related to display
    needs to be updated which helps in calculating the right mdp
    clock for each resolution.
    
    CRs-Fixed: 436151
    Change-Id: I1ab6e5859491936a7fa8c62247da9ef5a0ff45ef
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -4539,10 +4539,20 @@ void hdmi_msm_config_hdcp_feature(void)
 			hdmi_msm_state->hdcp_enable ? "Enabled" : "Disabled");
 }
 
+static void hdmi_msm_update_panel_info(struct msm_fb_data_type *mfd)
+{
+	if (!mfd)
+		return;
+
+	if (hdmi_common_get_video_format_from_drv_data(mfd))
+		hdmi_common_init_panel_info(&mfd->panel_info);
+}
+
 static int __devinit hdmi_msm_probe(struct platform_device *pdev)
 {
 	int rc;
 	struct platform_device *fb_dev;
+	struct msm_fb_data_type *mfd = NULL;
 
 	if (!hdmi_msm_state) {
 		pr_err("%s: hdmi_msm_state is NULL\n", __func__);
@@ -4663,6 +4673,9 @@ static int __devinit hdmi_msm_probe(struct platform_device *pdev)
 		}
 	} else
 		DEV_ERR("Init FAILED: failed to add fb device\n");
+
+	mfd = platform_get_drvdata(fb_dev);
+	mfd->update_panel_info = hdmi_msm_update_panel_info;
 
 	if (hdmi_prim_display) {
 		rc = hdmi_msm_hpd_on();
