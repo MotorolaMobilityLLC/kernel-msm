3e58cb5a84f9a58028f54eefe47ceff5bf895d87
Lianwei Wang <lian-wei.wang@motorola.com>
2013-01-17 16:06:11 +0800

    power: main: Disable VT switch on suspend/resume
    The vt_move_to_console may hang because the console switch complete
    event is never received for some cases. This mainly happened when
    the console is managed by the userspace application.
    
    Change-Id: Ifbfb36569a7d842a70e5f3ff6322489f0a7895e1
    Signed-off-by: Lianwei Wang <lian-wei.wang@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/512068
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Guo-Jian Chen <a21757@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Ke Lv <a2435c@motorola.com>
    Reviewed-by: Srinivas Sripathi <a14759@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <JIRAKEY@motorola.com>
    (cherry picked from commit a2dfeb56f110009d4b4acaa68fe05388883ee26e)
    
    [jason.hrycay@motorola.com: move to main init]
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -666,6 +666,9 @@ static int __init pm_init(void)
 	tc_ev_timer.function = &tc_ev_stop;
 	tc_ev_processed = 1;
 
+	/* Disable VT switch */
+	pm_set_vt_switch(0);
+
 	power_kobj = kobject_create_and_add("power", NULL);
 	if (!power_kobj)
 		return -ENOMEM;
