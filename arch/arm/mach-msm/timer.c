1e53b3ad99b386539794f92611a5354661a6c21b
Greg Meiste <w30289@motorola.com>
2013-01-17 12:26:09 +0800

    IKJB42MAIN-1031 [ARM] msm: Implement read_persistent_clock
    Change-Id: I843faf1f69655c00989faf2f79b60dd2f964b1e3
    Signed-off-by: Greg Meiste <w30289@motorola.com>
    Signed-off-by: Ravi Chebolu <Ravi.Chebolu@motorola.com>
    Signed-off-by: Lianwei Wang <lian-wei.wang@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/512042
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Guo-Jian Chen <a21757@motorola.com>
    Reviewed-by: Ke Lv <a2435c@motorola.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-by: Srinivas Sripathi <a14759@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>

--- arch/arm/mach-msm/timer.c
+++ arch/arm/mach-msm/timer.c
@@ -81,6 +81,10 @@ enum {
 static int global_timer_offset;
 static int msm_global_timer;
 
+static struct timespec persistent_ts;
+static u64 persistent_ns;
+static u64 last_persistent_ns;
+
 #define NR_TIMERS ARRAY_SIZE(msm_clocks)
 
 unsigned int gpt_hz = 32768;
@@ -1009,6 +1013,24 @@ static struct local_timer_ops msm_lt_ops = {
 	local_timer_stop,
 };
 #endif /* CONFIG_LOCAL_TIMERS */
+
+void read_persistent_clock(struct timespec *ts)
+{
+	int64_t delta;
+	int64_t sclk_max;
+	struct timespec *tsp = &persistent_ts;
+
+	last_persistent_ns = persistent_ns;
+	persistent_ns = msm_timer_get_sclk_time(&sclk_max);
+
+	if (persistent_ns < last_persistent_ns)
+		delta = sclk_max - last_persistent_ns + persistent_ns;
+	else
+		delta = persistent_ns - last_persistent_ns;
+
+	timespec_add_ns(tsp, delta);
+	*ts = *tsp;
+}
 
 static void __init msm_timer_init(void)
 {
