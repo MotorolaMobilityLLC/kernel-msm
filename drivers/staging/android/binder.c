75e7d58ca2882197fe1c920c07bacd3d71cabc5f
GAO LIANG-TAO <w20048@motorola.com>
2013-01-22 00:31:43 +0800

    staging: android: binder: Debugging FD leak
    Signed-off-by: GAO LIANG-TAO <w20048@motorola.com>
    (cherry picked from commit cdae4b7951c96c28030daf3d801dcc322989c946)
    
    Change-Id: Ic876b03f82cc8d400c9bd5901001ff0852afef84

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -34,6 +34,7 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/security.h>
+#include <linux/fdleak_dbg.h>
 
 #include "binder.h"
 
@@ -447,6 +448,7 @@ static void task_fd_install(
 	BUG_ON(fdt->fd[fd] != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
 	spin_unlock(&files->file_lock);
+	warn_if_big_fd(fd, proc->tsk);
 }
 
 /*
