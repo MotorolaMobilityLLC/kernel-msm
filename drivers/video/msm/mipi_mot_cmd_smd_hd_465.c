9fca0f183083cb1cab621257d7920396e627102a
Limei Wang <e12499@motorola.com>
2013-01-23 13:29:44 -0600

    msm_fb: display: update dsi_esc_clk ratio.
    Configure pinfo->mipi.esc_byte_ratio = 2 for all panels because
    dsi_esc_clk has to be within the range of 2/3 and 3/2 of panel esc clk.
    
    Change-Id: Ife3e4fc4b50e2c27c319337b36e43942dc50fb03
    Signed-off-by: Limei Wang <e12499@motorola.com>
    Change-Id: I08f371e3fe87bd80793b7c1dabcba81c2b245ccf
    Reviewed-on: http://gerrit.pcs.mot.com/513253
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>

--- drivers/video/msm/mipi_mot_cmd_smd_hd_465.c
+++ drivers/video/msm/mipi_mot_cmd_smd_hd_465.c
@@ -422,7 +422,7 @@ static int __init mipi_mot_cmd_smd_hd_465_init(void)
 	pinfo->mipi.wr_mem_continue = 0x3c;
 	pinfo->mipi.wr_mem_start = 0x3c;
 	pinfo->mipi.dsi_phy_db = &dsi_cmd_mode_phy_db;
-	pinfo->mipi.esc_byte_ratio = 4;
+	pinfo->mipi.esc_byte_ratio = 2;
 	pinfo->mipi.tx_eot_append = 0x01;
 	pinfo->mipi.rx_eot_ignore = 0;
 
