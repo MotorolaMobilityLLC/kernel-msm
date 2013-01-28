9ec3cb6ed87b0a30c8ff78bb442c647f8cbeefda
Dong-Dong Yang <gdb746@motorola.com>
2013-01-28 04:42:51 -0500

    kernel: watchdogtest: add debugfs item to fire watchdog reset.
    Add the debugfs item, fire_watchdog_reset, to verify
    the hardware watchdog feature, bug fix or bug report.
    
    Change-Id: Ib954e664e6322336c0d044b52e5f29f9be78e6a5
    Signed-off-by: Dong-Dong Yang <gdb746@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/514726
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 8a58279c9cc1a7621d3b9d4b0967e740263c86a6)
    
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Change-Id: I40537d6bef6641c8e49e81a5429284d6c1681ab5
    Reviewed-on: http://gerrit.pcs.mot.com/621619
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/watchdogtest.c
+++ kernel/watchdogtest.c
@@ -9,10 +9,37 @@
  * of the License.
  */
 
+#include <linux/completion.h>
+#include <linux/workqueue.h>
+#include <linux/irq.h>
 #include <linux/preempt.h>
 #include <linux/module.h>
 #include <linux/debugfs.h>
 #include <soc/qcom/mmi_watchdog.h>
+
+struct completion wdt_timeout_complete;
+static void wdt_timeout_work(struct work_struct *work)
+{
+	local_irq_disable();
+	while (1)
+	;
+	local_irq_enable();
+	complete(&wdt_timeout_complete);
+}
+static DECLARE_WORK(wdt_timeout_work_struct, wdt_timeout_work);
+
+static int fire_watchdog_reset_set(void *data, u64 val)
+{
+	init_completion(&wdt_timeout_complete);
+	printk(KERN_WARNING "Fire hardware watchdog reset.\n");
+	printk(KERN_WARNING "Please wait ...\n");
+	schedule_work_on(0, &wdt_timeout_work_struct);
+	wait_for_completion(&wdt_timeout_complete);
+
+	return 0;
+}
+DEFINE_SIMPLE_ATTRIBUTE(fire_watchdog_reset_fops,
+	NULL, fire_watchdog_reset_set, "%llu\n");
 
 static int fire_softlockup_reset_set(void *data, u64 val)
 {
@@ -33,6 +60,8 @@ static int watchdog_test_init(void)
 {
 	debugfs_create_file("fire_softlockup_reset", 0200,
 		NULL, NULL, &fire_softlockup_reset_fops);
+	debugfs_create_file("fire_watchdog_reset", 0200,
+		NULL, NULL, &fire_watchdog_reset_fops);
 	return 0;
 }
 
