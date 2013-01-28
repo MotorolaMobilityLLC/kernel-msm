12e12aa51c45351e6c1e075d7a17b8fff7e4e74d
Kenny Kessler <kenny.kessler@motorola.com>
2013-01-28 15:03:50 -0600

    arm: msm: Disable adjust_soc
    The adjust_soc funciton is causing Meter issues.
    This change disables the adjustment.
    
    Change-Id: Ie59f45e94ac8181d87c92391700458830c9f15aa
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/515233
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jason Knopsnyder <w19069@motorola.com>
    Reviewed-by: Vidyadhar Gurram <rnh786@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-mmi-pmic.c
+++ arch/arm/mach-msm/board-mmi-pmic.c
@@ -1194,6 +1194,7 @@ static struct pm8921_bms_platform_data pm8921_bms_pdata __devinitdata = {
 	.i_test			= 0,
 	.v_cutoff		= 3200,
 	.max_voltage_uv		= MAX_VOLTAGE_MV * 1000,
+	.adjust_soc_low_threshold = -1,
 #ifdef CONFIG_PM8921_EXTENDED_INFO
 	.get_batt_info=		read_mmi_battery_bms,
 #endif
