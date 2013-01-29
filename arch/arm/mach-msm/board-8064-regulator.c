6eb6ae3880e4f19621c9881e94188e9b3f5b22a1
Ajay Dudani <adudani@codeaurora.org>
2013-01-28 21:22:50 -0800

    revert "msm: 8064: Add regulator supply for DDR3 memory"
    This reverts commit 994ff121f0c2e3ff00fa38ae2f8dfde4bda1b332.
    PM8921 MPP7 no longer must be kept high at all times for targets
    using PCDDR3 memory because newer hardware ensures that memory is
    always powered when necessary.
    
    Change-Id: I4919abceb875b9ea22e6b2c0bd617280deb7e65b
    Signed-off-by: David Collins <collinsd@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- arch/arm/mach-msm/board-8064-regulator.c
+++ arch/arm/mach-msm/board-8064-regulator.c
@@ -284,7 +284,6 @@ VREG_CONSUMERS(NCP) = {
 };
 VREG_CONSUMERS(EXT_5V) = {
 	REGULATOR_SUPPLY("ext_5v",		NULL),
-	REGULATOR_SUPPLY("ext_ddr3",		NULL),
 	REGULATOR_SUPPLY("vbus",		"msm_ehci_host.0"),
 };
 
@@ -316,7 +315,6 @@ VREG_CONSUMERS(L36) = {
 };
 VREG_CONSUMERS(BOOST) = {
 	REGULATOR_SUPPLY("8917_boost",		NULL),
-	REGULATOR_SUPPLY("ext_ddr3",		NULL),
 	REGULATOR_SUPPLY("vbus",		"msm_ehci_host.0"),
 	REGULATOR_SUPPLY("hdmi_mvs",		"hdmi_msm.0"),
 };
