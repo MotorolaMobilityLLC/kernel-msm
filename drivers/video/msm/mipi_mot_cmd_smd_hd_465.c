9b01f5c804b4a7efb8a6f386d6a6a7b7586aef58
Limei Wang <e12499@motorola.com>
2013-01-30 22:02:26 -0600

    msm_fb: display: fix display tearing
    On some Droid m and xFon panels, see tearing on the top of screen when
    scrolling up and down or run GLbenchmark,it is because on those panels,
    MDP triggered frame tx too late after TE signal is received.
    
    This CR is to tune vertical porches and ref clk to force MDP always
    start frame data tx as soon as TE signal is received.
    
    Lower panel ref clk to make MDP read ptr counting slower than real
    panel refresh, this way will force read ptr reset only when TE is
    received. Along with programming start_pos just 1 line after TE, able
    to achieve the goal.
    
    Change-Id: Ic5c5bcd701366b6ce350c8249e3abf6c060f66c2
    Signed-off-by: Limei Wang <e12499@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/515431
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jun-Xiao Chang <w20081@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mipi_mot_cmd_smd_hd_465.c
+++ drivers/video/msm/mipi_mot_cmd_smd_hd_465.c
@@ -402,10 +402,10 @@ static int __init mipi_mot_cmd_smd_hd_465_init(void)
 
 	pinfo->lcd.vsync_enable = TRUE;
 	pinfo->lcd.hw_vsync_mode = TRUE;
-	pinfo->lcd.v_back_porch = 36;
-	pinfo->lcd.v_front_porch = 30;
+	pinfo->lcd.v_back_porch = 2;
+	pinfo->lcd.v_front_porch = 2;
 	pinfo->lcd.v_pulse_width = 2;
-	pinfo->lcd.refx100 = 6000; /* adjust refx100 to prevent tearing */
+	pinfo->lcd.refx100 = 5800; /* adjust refx100 to prevent tearing */
 
 	pinfo->mipi.mode = DSI_CMD_MODE;
 	pinfo->mipi.dst_format = DSI_CMD_DST_FORMAT_RGB888;
