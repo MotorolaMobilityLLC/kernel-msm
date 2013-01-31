d519f60146a39af88726153d9eb991304a7d3796
Limei Wang <e12499@motorola.com>
2013-01-31 17:41:28 -0600

    msm_fb: display increase dsi cmd tx buf size
    Right now dsi cmd tx buffer is 64 bytes, but some of the panel init cmd
    is longer than that.
    
    Max command length on 8960 is up to 24 bits, this CR is to increase the
    buffer size to 256 bytes to meet our need. If the cmd longer than 512
    bytes(DSI TX FIFO),the controller will stop fetching when the fifo is
    full and will resume fetching when there’s room in the fifo.
    
    Change-Id: I96f5e3b69f9322a3fb109e6b0c3dbb1ac394992a
    Signed-off-by: Limei Wang <e12499@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/515655
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zachary Crosby <azc095@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>

--- drivers/video/msm/mipi_dsi.h
+++ drivers/video/msm/mipi_dsi.h
@@ -190,7 +190,7 @@ struct dsi_clk_desc {
 #define DSI_HDR_DATA1(data)	((data) & 0x0ff)
 #define DSI_HDR_WC(wc)		((wc) & 0x0ffff)
 
-#define DSI_BUF_SIZE	64
+#define DSI_BUF_SIZE	256	/* Bytes */
 #define MIPI_DSI_MRPS	0x04	/* Maximum Return Packet Size */
 
 #define MIPI_DSI_LEN 8 /* 4 x 4 - 6 - 2, bytes dcs header+crc-align  */
