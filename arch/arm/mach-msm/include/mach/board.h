335e43dc4b745398014ba448bdbc7102a39d41f6
Deepak Verma <dverma@codeaurora.org>
2013-02-01 22:47:49 +0530

    msm: Add indicator for separate metadata buffers support
    Add variable to check if separate metabuffers can be
    enabled in video driver for specific targets.
    
    Change-Id: I0ab3be7077d7d8286bfaa37be63705992939b72d
    Signed-off-by: Deepak Verma <dverma@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -1,7 +1,7 @@
 /* arch/arm/mach-msm/include/mach/board.h
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2008-2012, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2013, The Linux Foundation. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -536,6 +536,7 @@ struct msm_vidc_platform_data {
 	int disable_fullhd;
 	u32 cp_enabled;
 	u32 secure_wb_heap;
+	u32 enable_sec_metadata;
 #ifdef CONFIG_MSM_BUS_SCALING
 	struct msm_bus_scale_pdata *vidc_bus_client_pdata;
 #endif
