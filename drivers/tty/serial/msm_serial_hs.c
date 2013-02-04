5329dd867871e9f013c12911b33660d035cb8bb4
Jiangli Yuan <a6808c@motorola.com>
2013-02-04 10:55:16 -0600

    msm_serial_hs:pm: Add timeout for wait_event in msm_hs_shutdown()
     Port (CR) to avoid msm_hs_shutdown hang there forever, when TX stall.
     This commit is from http://gerrit.pcs.mot.com/#/c/501250/
    
    Change-Id: I37aad69900facf4f956ff6e3549acf18e68a97e9
    Signed-off-by: Jiangli Yuan <a6808c@motorola.com>
    Change-Id: I6eb836d5a4cf9f54858089986312d07921b9bf53
    Reviewed-on: http://gerrit.pcs.mot.com/516033
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: David Ding <dding@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    (cherry picked from commit 3ed0773ebdaf0bdb36a73c055db67262e9035ab9)

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -3309,7 +3309,11 @@ static void msm_hs_shutdown(struct uart_port *uport)
 	}
 	tasklet_kill(&msm_uport->tx.tlet);
 	BUG_ON(msm_uport->rx.flush < FLUSH_STOP);
-	wait_event(msm_uport->rx.wait, msm_uport->rx.flush == FLUSH_SHUTDOWN);
+	ret = wait_event_timeout(msm_uport->rx.wait,
+			msm_uport->rx.flush == FLUSH_SHUTDOWN, HZ);
+	if (!ret)
+		pr_err("%s(): HSUART RX Stall.\n", __func__);
+
 	tasklet_kill(&msm_uport->rx.tlet);
 	cancel_delayed_work_sync(&msm_uport->rx.flip_insert_work);
 	flush_workqueue(msm_uport->hsuart_wq);
