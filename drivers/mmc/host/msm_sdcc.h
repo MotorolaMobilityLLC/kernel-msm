0b9dd349d7206e17dedc30bb37b72b86afbcf760
Maya Erez <merez@codeaurora.org>
2013-02-05 13:19:52 +0200

    mmc: msm_sdcc: Reset SDCC controller if check for Tx/Rx active fails
    Reset SDCC controller if the check for TX/RX ACTIVE bit fails while
    carrying out DPSM reset after the end of current transaction
    instead of taking the entire system down by doing BUG().
    
    CRs-Fixed: 423356
    Change-Id: I9e2e945eb9a31d12e0a435df29a8215ae634724b
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    (cherry picked from commit 0a29588be6d3d6bd9122191e7e850c687bb1b166)
    
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.h
+++ drivers/mmc/host/msm_sdcc.h
@@ -411,7 +411,6 @@ struct msmsdcc_host {
 	struct mutex clk_mutex;
 	bool pending_resume;
 	unsigned int idle_tout_ms;			/* Timeout in msecs */
-	bool pending_dpsm_reset;
 	struct msmsdcc_msm_bus_vote msm_bus_vote;
 	struct device_attribute	max_bus_bw;
 	struct device_attribute	polling;
