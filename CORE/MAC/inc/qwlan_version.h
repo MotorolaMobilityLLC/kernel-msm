cd4ca93bd867d56153d24c049baa6526264975b8
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2013-02-12 12:54:06 -0800

    wlan: cs release 3.2.0.55

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            54
+#define QWLAN_VERSION_BUILD            55
 
-#define QWLAN_VERSIONSTR               "3.2.0.54"
+#define QWLAN_VERSIONSTR               "3.2.0.55"
 
 #endif /* QWLAN_VERSION_H */
 
