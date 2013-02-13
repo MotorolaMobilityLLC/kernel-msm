5107d0c84ac095c246e6451e515bf6c0fe46da29
Kuirong Wang <kuirongw@codeaurora.org>
2013-02-12 18:45:49 -0800

    msm: audio: qdsp6v2:  Check return code from audio_aio_open()
    When audio_aio_open() is triggered in blocking fashion, it  frees audio
    resouces and returns error code. In the current implementation,  callers
    did not check the error code which can lead to dereference to freed
    resources and cause system crash.  Update the callers to check return
    code and then return error back to application to avoid the issue. In
    the meanwhile, update the audio_aio_open not to free memory for caller
    and leave the caller to free the memory in error condition.
    
    CRs-fixed: 436653
    Change-Id: If4dda158cb988c5837517817b1afa0572c30696e
    Signed-off-by: Kuirong Wang <kuirongw@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
+++ arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
@@ -1166,9 +1166,6 @@ int audio_aio_open(struct q6audio_aio *audio, struct file *file)
 	return 0;
 
 fail:
-	q6asm_audio_client_free(audio->ac);
-	kfree(audio->codec_cfg);
-	kfree(audio);
 	return rc;
 }
 
