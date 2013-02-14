1c37efe89a09cef04838e9034bc3d6440a9b3ae8
Tarun Karra <tkarra@codeaurora.org>
2013-02-14 12:12:22 -0800

    msm: kgsl: Debugfs entity to switch on/off userspace FT policy control
    When debugfs entity "ft_user_control" is set, it enables userspace to
    control GPU fault tolerance policy. By default it is disabled.
    
    Change-Id: Ia93ccf6d9595f6e7cda71863f9067819aaa0a68d
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2231,6 +2231,9 @@ static int adreno_setproperty(struct kgsl_device *device,
 	case KGSL_PROP_FAULT_TOLERANCE: {
 			struct kgsl_ft_config ftd;
 
+			if (adreno_dev->ft_user_control == 0)
+				break;
+
 			if (sizebytes != sizeof(ftd))
 				break;
 
