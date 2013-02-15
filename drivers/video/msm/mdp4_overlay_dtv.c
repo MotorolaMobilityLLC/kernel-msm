b1432ed224f45230fe0cd715e4cc3f81320f8daf
Ken Zhang <kenz@codeaurora.org>
2013-02-14 20:25:21 -0500

    msm: display: reset vsync state when display is turned on
    In the case that vsync ctrl is unbalanced, vsync IRQ is cleared
    in suspend,  vsync cannot be enabled after resume.
    Clear the status when display is turned on
    
    CRs-fixed: 449155
    Change-Id: I5ecf23053b60d1fbd91f042758d67d39c83ab444
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -588,6 +588,7 @@ int mdp4_dtv_on(struct platform_device *pdev)
 		return -EINVAL;
 
 	vctrl->dev = mfd->fbi->dev;
+	vctrl->vsync_irq_enabled = 0;
 
 	mdp_footswitch_ctrl(TRUE);
 	/* Mdp clock enable */
