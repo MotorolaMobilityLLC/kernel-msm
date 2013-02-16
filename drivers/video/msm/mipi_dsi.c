86b14c9e2130fc50a5d33e35addf1065a684603d
Kuogee Hsieh <khsieh@codeaurora.org>
2013-02-16 08:46:03 -0800

    msm_fb: display: add early_off during panel off
    Some panels need to send display_off command before
    dsi controller turned off. This patch add early_off
    feature for this purpose.
    
    Change-Id: Id377120e9d6972c5f773760369ec056be37e93ff
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -332,6 +332,12 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	return ret;
 }
 
+static int mipi_dsi_early_off(struct platform_device *pdev)
+{
+	return panel_next_early_off(pdev);
+}
+
+
 static int mipi_dsi_late_init(struct platform_device *pdev)
 {
 	return panel_next_late_init(pdev);
@@ -486,6 +492,7 @@ static int mipi_dsi_probe(struct platform_device *pdev)
 	pdata->off = mipi_dsi_off;
 	pdata->fps_level_change = mipi_dsi_fps_level_change;
 	pdata->late_init = mipi_dsi_late_init;
+	pdata->early_off = mipi_dsi_early_off;
 	pdata->next = pdev;
 
 	/*
