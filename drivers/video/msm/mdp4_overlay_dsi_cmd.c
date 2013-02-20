57a90774a0ed3fa9ff1e9196f02ff6bcd9bac9b5
Jared Suttles <jsuttles@motorola.com>
2013-02-20 11:20:08 -0600

    Revert "(CR): Fix cmd mode panel dynamic clock management"
    Official CAF patch provided
    
    This reverts commit c51f893cdd0ecdd8ad01bddfe8992ebff2190ed8.
    
    Change-Id: Id98d4de64143f74f67d48995b22254d76af814b9
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/520252
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Patrick Auchter <wpa722@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -314,10 +314,6 @@ int mdp4_dsi_cmd_pipe_commit(int cndx, int wait)
 	if (mdp4_dsi_cmd_clk_check(vctrl) < 0)
 		return 0;
 
-	spin_lock_irqsave(&vctrl->spin_lock, flags);
-	vctrl->pan_display++;
-	spin_unlock_irqrestore(&vctrl->spin_lock, flags);
-
 	/* free previous committed iommu back to pool */
 	mdp4_overlay_iommu_unmap_freelist(mixer);
 
@@ -1182,6 +1178,7 @@ static int mdp4_dsi_cmd_clk_check(struct vsycn_ctrl *vctrl)
 
 	spin_lock_irqsave(&vctrl->spin_lock, flags);
 	vctrl->clk_control = 0;
+	vctrl->pan_display++;
 	if (!vctrl->clk_enabled) {
 		clk_set_on = 1;
 		vctrl->clk_enabled = 1;
