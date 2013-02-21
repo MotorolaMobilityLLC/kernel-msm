86a0fb7c398dfbc452f341a6ae4886f323636d60
Mahesh Palivela <maheshp@posedge.com>
2013-02-21 06:22:19 +0530

    mac80211: include VHT capability IE in probe requests
    Insert the VHT capability IE into probe requests.
    
    Change-Id: I75620090969b46701f1c760baf9c6559a4a1ac2c
    CRs-fixed: 452921
    
    Signed-off-by: Mahesh Palivela <maheshp@posedge.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -1475,6 +1475,8 @@ u8 *ieee80211_ie_build_ht_info(u8 *pos,
 				struct ieee80211_channel *channel,
 				enum nl80211_channel_type channel_type);
 
+u8 *ieee80211_ie_build_vht_cap(u8 *pos, struct ieee80211_sta_vht_cap *vht_cap,
+			       u32 cap);
 /* internal work items */
 void ieee80211_work_init(struct ieee80211_local *local);
 void ieee80211_add_work(struct ieee80211_work *wk);
