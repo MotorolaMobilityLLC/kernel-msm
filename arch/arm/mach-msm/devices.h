6920b3ec3f412f8c98c56fc74ee2799078b42585
Mayank Rana <mrana@codeaurora.org>
2013-02-22 19:58:59 +0530

    msm: 8064: Add support for GSBI4 based UARTDM
    GSBI4 based UARTDM is used for primary IPC between APPS
    and QSC on 8064 SGLTE2 platform. Hence add required resources
    and configuration (i.e. GSBI4 UARTDM as platform device, clocks,
    interrupts, DMA channels) for GSBI4 based UARTDM and enable the
    same for 8064 SGLTE2 platform.
    
    Change-Id: I3623e4b09d6d3e2d344f1fb7fbbbce75fe008ff4
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -85,6 +85,7 @@ extern struct platform_device msm8960_device_ebi1_ch1_erp;
 extern struct platform_device apq8064_device_uart_gsbi1;
 extern struct platform_device apq8064_device_uart_gsbi3;
 extern struct platform_device apq8064_device_uart_gsbi4;
+extern struct platform_device apq8064_device_uartdm_gsbi4;
 extern struct platform_device apq8064_device_uart_gsbi7;
 extern struct platform_device apq8064_device_qup_i2c_gsbi1;
 extern struct platform_device apq8064_device_qup_i2c_gsbi3;
