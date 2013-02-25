wingrime/android_kernel_swift

Bluetooth: When an AMP channel move fails, restart data transmission.
Data transmission is stopped during an AMP channel move.  If the channel
move fails due to a problem setting up the physical link and there is
data in the transmit queue, data transmission needs to be triggered.
It was possible for data transfer to stall if there was data in the
transmit queue but no further data was added to the queue.

Change-Id: I554ccf477f4abae39b96c8d716b380d14d1abb74
Signed-off-by: Mat Martineau <mathewm@codeaurora.org>

net/bluetooth/l2cap_core.c
@@ -5795,6 +5795,9 @@ void l2cap_amp_physical_complete(int result, u8 local_id, u8 remote_id,
 			l2cap_rmem_available(sk))
 			l2cap_ertm_tx(sk, 0, 0,
 					L2CAP_ERTM_EVENT_LOCAL_BUSY_CLEAR);
+
+		/* Restart data transmission */
+		l2cap_ertm_send_txq(sk);
 	}
 
 	release_sock(sk);

MotorolaMobilityLLC/kernel-msm

net/bluetooth/l2cap_core.c
@@ -4819,6 +4819,9 @@ void l2cap_amp_physical_complete(int result, u8 local_id, u8 remote_id,
 			l2cap_rmem_available(sk))
 			l2cap_ertm_tx(sk, 0, 0,
 					L2CAP_ERTM_EVENT_LOCAL_BUSY_CLEAR);
+
+		/* Restart data transmission */
+		l2cap_ertm_send(sk);
 	}
 
 	release_sock(sk);