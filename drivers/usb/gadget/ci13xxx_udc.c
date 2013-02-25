MiCode/Xiaomi_Kernel_OpenSource

usb: gadget: Remove dependency between HSIC and MSM_OTG.
MSM_OTG is used with the HSUSB core and not with the HSIC core.
Therefore, HSIC do not need to call any api of the otg.

This fix solves 2 problems:
1. Power management: Since the HSIC was registered to the msm_otg,
   then the msm_otg notified the HSIC about cable disconnect, which caused
   to the HSIC to enter to low power mode.
2. In the future, when both HSIC and HSUSB cores will work
   concurrently, then a race could happen without this fix. The race is
   which one of the two will register to the otg as the gadget driver.

Change-Id: I9f6ec5660a63fcf47733a9b20a31513f2c14e725
Signed-off-by: Ido Shayevitz <idos@codeaurora.org>

drivers/usb/gadget/ci13xxx_udc.c
@@ -3306,9 +3306,8 @@ static int udc_probe(struct ci13xxx_udc_driver *driver, struct device *dev,
 	udc->gadget.dev.parent   = dev;
 	udc->gadget.dev.release  = udc_release;
 
-	udc->transceiver = otg_get_transceiver();
-
 	if (udc->udc_driver->flags & CI13XXX_REQUIRE_TRANSCEIVER) {
+		udc->transceiver = otg_get_transceiver();
 		if (udc->transceiver == NULL) {
 			retval = -ENODEV;
 			goto free_udc;

MotorolaMobilityLLC/kernel-msm

drivers/usb/gadget/ci13xxx_udc.c
@@ -3329,9 +3329,8 @@ static int udc_probe(struct ci13xxx_udc_driver *driver, struct device *dev,
 	udc->gadget.dev.parent   = dev;
 	udc->gadget.dev.release  = udc_release;
 
-	udc->transceiver = usb_get_transceiver();
-
 	if (udc->udc_driver->flags & CI13XXX_REQUIRE_TRANSCEIVER) {
+		udc->transceiver = usb_get_transceiver();
 		if (udc->transceiver == NULL) {
 			retval = -ENODEV;
 			goto free_udc;