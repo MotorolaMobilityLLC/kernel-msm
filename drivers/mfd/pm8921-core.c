RonGokhale/codeaurora_kernel

mfd: pm8xxx: add pm8xxx debugfs read/write interface
This driver will provide a means to read and write arbitrary PMIC
PM8XXX SSBI registers via debugfs.  It will first be used on the
PM8921 chip for MSM 8960.

Change-Id: I71c8b98cafec088ad864a89fa39e7ca14d212c7f
Signed-off-by: David Collins <collinsd@codeaurora.org>

drivers/mfd/pm8921-core.c
@@ -176,6 +176,13 @@ static struct mfd_cell keypad_cell __devinitdata = {
 	.resources	= resources_keypad,
 };
 
+static struct mfd_cell debugfs_cell __devinitdata = {
+	.name		= "pm8xxx-debug",
+	.id		= -1,
+	.platform_data	= "pm8921-dbg",
+	.data_size	= sizeof("pm8921-dbg"),
+};
+
 static int __devinit pm8921_add_subdevices(const struct pm8921_platform_data
 					   *pdata,
 					   struct pm8921 *pmic,
@@ -289,6 +296,12 @@ static int __devinit pm8921_add_subdevices(const struct pm8921_platform_data
 			goto bail;
 		}
 		pmic->mfd_regulators = mfd_regulators;
+	}
+
+	ret = mfd_add_devices(pmic->dev, 0, &debugfs_cell, 1, NULL, irq_base);
+	if (ret) {
+		pr_err("Failed to add debugfs subdevice ret=%d\n", ret);
+		goto bail;
 	}
 
 	return 0;

MotorolaMobilityLLC/kernel-msm

drivers/mfd/pm8921-core.c
@@ -176,6 +176,13 @@ static struct mfd_cell keypad_cell __devinitdata = {
 	.resources	= resources_keypad,
 };
 
+static struct mfd_cell debugfs_cell __devinitdata = {
+	.name		= "pm8xxx-debug",
+	.id		= -1,
+	.platform_data	= "pm8921-dbg",
+	.pdata_size	= sizeof("pm8921-dbg"),
+};
+
 static int __devinit pm8921_add_subdevices(const struct pm8921_platform_data
 					   *pdata,
 					   struct pm8921 *pmic,
@@ -289,6 +296,12 @@ static int __devinit pm8921_add_subdevices(const struct pm8921_platform_data
 			goto bail;
 		}
 		pmic->mfd_regulators = mfd_regulators;
+	}
+
+	ret = mfd_add_devices(pmic->dev, 0, &debugfs_cell, 1, NULL, irq_base);
+	if (ret) {
+		pr_err("Failed to add debugfs subdevice ret=%d\n", ret);
+		goto bail;
 	}
 
 	return 0;