RonGokhale/codeaurora_kernel

mfd: pm8921: Add cell data for pm8921 vibrator
Change-Id: I0ddf2555d80ff8575ae85250b13aed46e21a0f0a
Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

drivers/mfd/pm8921-core.c
@@ -338,6 +338,11 @@ static struct mfd_cell batt_alarm_cell __devinitdata = {
 	.data_size	= sizeof(struct pm8xxx_batt_alarm_core_data),
 };
 
+static struct mfd_cell vibrator_cell __devinitdata = {
+	.name           = PM8XXX_VIBRATOR_DEV_NAME,
+	.id             = -1,
+};
+
 static int __devinit
 pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		      struct pm8921 *pmic)
@@ -547,6 +552,18 @@ pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		pr_err("Failed to add battery alarm subdevice ret=%d\n",
 			ret);
 		goto bail;
+	}
+
+	if (pdata->vibrator_pdata) {
+		vibrator_cell.platform_data = pdata->vibrator_pdata;
+		vibrator_cell.data_size =
+				sizeof(struct pm8xxx_vibrator_platform_data);
+		ret = mfd_add_devices(pmic->dev, 0, &vibrator_cell, 1, NULL, 0);
+		if (ret) {
+			pr_err("Failed to add vibrator subdevice ret=%d\n",
+									ret);
+			goto bail;
+		}
 	}
 
 	return 0;

MotorolaMobilityLLC/kernel-msm

drivers/mfd/pm8921-core.c
@@ -338,6 +338,11 @@ static struct mfd_cell batt_alarm_cell __devinitdata = {
 	.pdata_size	= sizeof(struct pm8xxx_batt_alarm_core_data),
 };
 
+static struct mfd_cell vibrator_cell __devinitdata = {
+	.name           = PM8XXX_VIBRATOR_DEV_NAME,
+	.id             = -1,
+};
+
 static int __devinit
 pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		      struct pm8921 *pmic)
@@ -543,6 +548,18 @@ pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		pr_err("Failed to add battery alarm subdevice ret=%d\n",
 			ret);
 		goto bail;
+	}
+
+	if (pdata->vibrator_pdata) {
+		vibrator_cell.platform_data = pdata->vibrator_pdata;
+		vibrator_cell.pdata_size =
+				sizeof(struct pm8xxx_vibrator_platform_data);
+		ret = mfd_add_devices(pmic->dev, 0, &vibrator_cell, 1, NULL, 0);
+		if (ret) {
+			pr_err("Failed to add vibrator subdevice ret=%d\n",
+									ret);
+			goto bail;
+		}
 	}
 
 	return 0;