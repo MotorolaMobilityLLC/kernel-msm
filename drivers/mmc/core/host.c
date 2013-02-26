b0f1923057909551bb8104d6c0cb091ca6aea0ef
Vladimir Karpovich <cvk011c@motorola.com>
2013-02-26 17:05:56 -0600

    driver: mmc: Support for HFF hardware reset
    Add reboot notifier to suspend MMC before HW reset.
    
    Change-Id: I6d76d92e4c0bc81456167c6ed2c50fc3a778e6f2
    Signed-off-by: Vladimir Karpovich <cvk011c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/521141
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Russell Knize <rknize2@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -20,6 +20,7 @@
 #include <linux/leds.h>
 #include <linux/slab.h>
 #include <linux/suspend.h>
+#include <linux/reboot.h>
 
 #include <linux/mmc/host.h>
 #include <linux/mmc/card.h>
@@ -339,7 +340,7 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 #ifdef CONFIG_PM
 	host->pm_notify.notifier_call = mmc_pm_notify;
 #endif
-
+	host->reboot_notify.notifier_call = mmc_reboot_notify;
 	/*
 	 * By default, hosts do not support SGIO or large requests.
 	 * They have to set these according to their abilities.
@@ -455,6 +456,8 @@ int mmc_add_host(struct mmc_host *host)
 	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
 		register_pm_notifier(&host->pm_notify);
 
+	register_reboot_notifier(&host->reboot_notify);
+
 	return 0;
 }
 
@@ -472,7 +475,7 @@ void mmc_remove_host(struct mmc_host *host)
 {
 	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
 		unregister_pm_notifier(&host->pm_notify);
-
+	unregister_reboot_notifier(&host->reboot_notify);
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
