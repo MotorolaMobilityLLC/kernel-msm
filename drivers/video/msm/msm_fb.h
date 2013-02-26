96134ff667407ae52a91e43066d6b6cdcc1a4f92
Huaibin Yang <huaibiny@codeaurora.org>
2013-02-26 15:27:49 -0800

    msm_fb: display: balance secure map and unmap in each mfd
    Make sure secure map and unmap get called in each mfd and balanced.
    Otherwise, secure contents can not be played for both primary and
    hdmi.
    
    CRs-fixed: 450427
    Change-Id: I5399be64fbb104dc3cbe2c5c65b3f4406f47473c
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -215,6 +215,8 @@ struct msm_fb_data_type {
 	int vsync_sysfs_created;
 	void *copy_splash_buf;
 	unsigned char *copy_splash_phys;
+	uint32 sec_mapped;
+	uint32 sec_active;
 };
 struct msm_fb_backup_type {
 	struct fb_info info;
