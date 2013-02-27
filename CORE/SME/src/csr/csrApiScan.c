b2cb77cf5232d061b6e15fbd4246a428149cf869
Venkata Prathyusha Kuntupalli <c_vkuntu@qca.qualcomm.com>
2013-02-26 22:16:52 -0800

    wlan: Updating the scanlist based on the 11d info from AP
    Updating the channel list from nv.bin based on the changed country code
    
    CRs-Fixed: 449915
    Change-Id: Ie29a278c52edc2d3448814459d41e58f4a288b02

--- CORE/SME/src/csr/csrApiScan.c
+++ CORE/SME/src/csr/csrApiScan.c
@@ -3508,7 +3508,6 @@ void csrSetOppositeBandChannelInfo( tpAniSirGlobal pMac )
         {
             // else, we found channel info in the 2.4 GHz band.  If the 5.0 band is empty
             // set the 5.0 band info from the 2.4 country code.
-            if ( !csrLLIsListEmpty( &pMac->scan.channelPowerInfoList5G, LL_ACCESS_LOCK ) ) break;
             fPopulate5GBand = TRUE;
         }
         csrSaveChannelPowerForBand( pMac, fPopulate5GBand );
@@ -3734,6 +3733,12 @@ tANI_BOOLEAN csrLearnCountryInformation( tpAniSirGlobal pMac, tSirBssDescription
 
         // set the indicator of the channel where the country IE was found...
         pMac->scan.channelOf11dInfo = pSirBssDesc->channelId;
+        csrGetRegulatoryDomainForCountry(pMac, pIesLocal->Country.country, &domainId );
+        if ( domainId != pMac->scan.domainIdCurrent )
+        {
+            WDA_SetRegDomain(pMac, domainId);
+            csrGet5GChannels(pMac );
+        }
         // Populate both band channel lists based on what we found in the country information...
         csrSetOppositeBandChannelInfo( pMac );
         bMaxNumChn = WNI_CFG_VALID_CHANNEL_LIST_LEN;
