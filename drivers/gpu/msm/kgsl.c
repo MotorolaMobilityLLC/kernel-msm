fc3f11a3aa995ad9500ba32d76be86b709c5024e
Sakshi Agrawal <sakshia@codeaurora.org>
2013-02-28 16:59:57 -0700

    msm: kgsl: Remove invalid context_id logging
    Remove printing of "invalid context_id" messages and
    replace those with simply returning with -EINVAL. The
    messages serve little purpose and their removal alleviates
    kernel log spamming.
    
    CRs-fixed: 449050
    Change-Id: Id0d6538d228e7dc928a628bdffde8c952ec5d6fe
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    Signed-off-by: Sakshi Agrawal <sakshia@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -1065,11 +1065,8 @@ static long kgsl_ioctl_device_waittimestamp_ctxtid(struct kgsl_device_private
 	int result;
 
 	context = kgsl_find_context(dev_priv, param->context_id);
-	if (context == NULL) {
-		KGSL_DRV_ERR(dev_priv->device, "invalid context_id %d\n",
-			param->context_id);
+	if (context == NULL)
 		return -EINVAL;
-	}
 	/*
 	 * A reference count is needed here, because waittimestamp may
 	 * block with the device mutex unlocked and userspace could
@@ -1093,9 +1090,6 @@ static long kgsl_ioctl_rb_issueibcmds(struct kgsl_device_private *dev_priv,
 	context = kgsl_find_context(dev_priv, param->drawctxt_id);
 	if (context == NULL) {
 		result = -EINVAL;
-		KGSL_DRV_ERR(dev_priv->device,
-			"invalid context_id %d\n",
-			param->drawctxt_id);
 		goto done;
 	}
 
@@ -1206,11 +1200,9 @@ static long kgsl_ioctl_cmdstream_readtimestamp_ctxtid(struct kgsl_device_private
 	struct kgsl_context *context;
 
 	context = kgsl_find_context(dev_priv, param->context_id);
-	if (context == NULL) {
-		KGSL_DRV_ERR(dev_priv->device, "invalid context_id %d\n",
-			param->context_id);
+	if (context == NULL)
 		return -EINVAL;
-	}
+
 
 	return _cmdstream_readtimestamp(dev_priv, context,
 			param->type, &param->timestamp);
