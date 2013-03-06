5c6224c8ffcb8c69d49dad790781221bf4e7e9a0
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-03-06 23:31:42 +0530

    wlan: wda: remove obsolete "WLAN_FEATURE_P2P" featurization
    The current driver has evolved over time from a driver that originally
    only supported "station" mode.  When the initial support for "P2P"
    mode was added, the code was added with conditional compilation so
    that the "P2P" feature could be included or excluded based upon
    the target needs.  Now, however, there is an expectation that all
    drivers will support both "station" mode and "P2P" mode.
    Therefore remove the conditional compilation since "P2P" mode code
    must always be present.
    
    Change-Id: I8889e9fe127f4a666b71e85bd18ce9a56e89736b
    CRs-fixed: 459417

--- CORE/WDA/inc/wlan_qct_wda.h
+++ CORE/WDA/inc/wlan_qct_wda.h
@@ -966,9 +966,7 @@ tSirRetStatus uMacPostCtrlMsg(void* pSirGlobal, tSirMbMsg* pMb);
 #define WDA_ADD_STA_SELF_REQ           SIR_HAL_ADD_STA_SELF_REQ
 #define WDA_DEL_STA_SELF_REQ           SIR_HAL_DEL_STA_SELF_REQ
 
-#ifdef WLAN_FEATURE_P2P
 #define WDA_SET_P2P_GO_NOA_REQ         SIR_HAL_SET_P2P_GO_NOA_REQ
-#endif
 
 #define WDA_TX_COMPLETE_TIMEOUT_IND  (WDA_MSG_TYPES_END - 1)
 #define WDA_WLAN_SUSPEND_IND           SIR_HAL_WLAN_SUSPEND_IND
