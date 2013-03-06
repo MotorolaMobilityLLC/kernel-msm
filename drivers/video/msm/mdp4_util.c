e269292b35fd677789abf3ea8f4f257e4814c947
Praveen AC <praveenac@codeaurora.org>
2013-03-06 12:59:09 -0800

    msm: mdp: Revert MDP Port split changes
    Revert MDP changes to avoid bus overflow
    seen in camera usecases.
    
    Change-Id: I1c687750bd22c8f64ab28a500562cbfbcb6a6048
    Signed-off-by: Kiran Kumar H N <hurlisal@codeaurora.org>
    Signed-off-by: Praveen Ac <praveenac@codeaurora.org>

--- drivers/video/msm/mdp4_util.c
+++ drivers/video/msm/mdp4_util.c
@@ -445,8 +445,6 @@ void mdp4_hw_init(void)
 
 	/* max read pending cmd config */
 	outpdw(MDP_BASE + 0x004c, 0x02222);	/* 3 pending requests */
-	outpdw(MDP_BASE + 0x0400, 0x7FF);
-	outpdw(MDP_BASE + 0x0404, 0x30050);
 
 #ifndef CONFIG_FB_MSM_OVERLAY
 	/* both REFRESH_MODE and DIRECT_OUT are ignored at BLT mode */
