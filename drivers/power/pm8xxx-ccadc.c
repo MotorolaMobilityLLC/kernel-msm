dd3732651598f347c00dd42306a0248fe63a2828
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2013-03-10 06:55:43 -0700

    power: pm8xxx-ccadc: implement quick calibration
    There are certain systems that wakeup every so often to do some
    housekeeping work. We don't want to run a full calibration
    when the system wakeups.
    
    For such systems runs a quick calibration with just two samples.
    Use the periodic_wakeup flag to indicate such a system.
    
    CRs-Fixed: 454221
    Change-Id: Ifc86414f7fab4d0921e3289d6e52b9e077a1f625
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Reviewed-on: http://gerrit.pcs.mot.com/543375
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Kenneth Kessler <w65789@motorola.com>
    Reviewed-by: Srinivas Sripathi <a14759@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>
    Reviewed-by: Jason Knopsnyder <w19069@motorola.com>
    SLT-Approved: Connie Zhao <czhao1@motorola.com>

--- drivers/power/pm8xxx-ccadc.c
+++ drivers/power/pm8xxx-ccadc.c
@@ -82,6 +82,7 @@ struct pm8xxx_ccadc_chip {
 	int			r_sense_uohm;
 	struct delayed_work	calib_ccadc_work;
 	struct mutex		calib_mutex;
+	bool			periodic_wakeup;
 };
 
 static struct pm8xxx_ccadc_chip *the_chip;
@@ -373,7 +374,7 @@ static int get_current_time(unsigned long *now_tm_sec)
 	return 0;
 }
 
-void pm8xxx_calib_ccadc(void)
+static void __pm8xxx_calib_ccadc(int sample_count)
 {
 	u8 data_msb, data_lsb, sec_cntrl;
 	int result_offset, result_gain;
@@ -384,6 +385,8 @@ void pm8xxx_calib_ccadc(void)
 		pr_err("chip not initialized\n");
 		return;
 	}
+
+	pr_debug("sample_count = %d\n", sample_count);
 
 	mutex_lock(&the_chip->calib_mutex);
 	rc = pm8xxx_readb(the_chip->dev->parent,
@@ -411,7 +414,7 @@ void pm8xxx_calib_ccadc(void)
 	}
 
 	result_offset = 0;
-	for (i = 0; i < SAMPLE_COUNT; i++) {
+	for (i = 0; i < sample_count; i++) {
 		/* Short analog inputs to CCADC internally to ground */
 		rc = pm8xxx_writeb(the_chip->dev->parent, ADC_ARB_SECP_RSV,
 							CCADC_CALIB_RSV_GND);
@@ -437,7 +440,7 @@ void pm8xxx_calib_ccadc(void)
 		result_offset += result;
 	}
 
-	result_offset = result_offset / SAMPLE_COUNT;
+	result_offset = result_offset / sample_count;
 
 
 	pr_debug("offset result_offset = 0x%x, voltage = %llduV\n",
@@ -476,7 +479,7 @@ void pm8xxx_calib_ccadc(void)
 	}
 
 	result_gain = 0;
-	for (i = 0; i < SAMPLE_COUNT; i++) {
+	for (i = 0; i < sample_count; i++) {
 		rc = pm8xxx_writeb(the_chip->dev->parent,
 					ADC_ARB_SECP_RSV, CCADC_CALIB_RSV_25MV);
 		if (rc < 0) {
@@ -500,7 +503,7 @@ void pm8xxx_calib_ccadc(void)
 
 		result_gain += result;
 	}
-	result_gain = result_gain / SAMPLE_COUNT;
+	result_gain = result_gain / sample_count;
 
 	/*
 	 * result_offset includes INTRINSIC OFFSET
@@ -524,6 +527,16 @@ bail:
 	pm8xxx_writeb(the_chip->dev->parent, ADC_ARB_SECP_CNTRL, sec_cntrl);
 calibration_unlock:
 	mutex_unlock(&the_chip->calib_mutex);
+}
+
+static void pm8xxx_calib_ccadc_quick(void)
+{
+	__pm8xxx_calib_ccadc(2);
+}
+
+void pm8xxx_calib_ccadc(void)
+{
+	__pm8xxx_calib_ccadc(SAMPLE_COUNT);
 }
 EXPORT_SYMBOL(pm8xxx_calib_ccadc);
 
@@ -743,6 +756,7 @@ static int __devinit pm8xxx_ccadc_probe(struct platform_device *pdev)
 	chip->r_sense_uohm = pdata->r_sense_uohm;
 	chip->calib_delay_ms = pdata->calib_delay_ms;
 	chip->batt_temp_channel = pdata->ccadc_cdata.batt_temp_channel;
+	chip->periodic_wakeup = pdata->periodic_wakeup;
 	mutex_init(&chip->calib_mutex);
 
 	calib_ccadc_read_offset_and_gain(chip,
@@ -799,6 +813,12 @@ static int pm8xxx_ccadc_resume(struct device *dev)
 		pr_err("unable to get current time: %d\n", rc);
 		return 0;
 	}
+
+	if (the_chip->periodic_wakeup) {
+		pm8xxx_calib_ccadc_quick();
+		return 0;
+	}
+
 	if (current_time_sec > the_chip->last_calib_time) {
 		time_since_last_calib = current_time_sec -
 					the_chip->last_calib_time;
@@ -809,9 +829,11 @@ static int pm8xxx_ccadc_resume(struct device *dev)
 				|| delta_temp > CCADC_CALIB_TEMP_THRESH) {
 			the_chip->last_calib_time = current_time_sec;
 			the_chip->last_calib_temp = batt_temp;
-			pm8xxx_calib_ccadc();
+			cancel_delayed_work(&the_chip->calib_ccadc_work);
+			schedule_delayed_work(&the_chip->calib_ccadc_work, 0);
 		}
 	}
+
 	return 0;
 }
 
