c4789d50285366d1052f6cfd84956c1d519562cb
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-03-11 13:24:53 -0700

    wlan: cs release 3.2.0.81

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            80
+#define QWLAN_VERSION_BUILD            81
 
-#define QWLAN_VERSIONSTR               "3.2.0.80"
+#define QWLAN_VERSIONSTR               "3.2.0.81"
 
 #endif /* QWLAN_VERSION_H */
 
