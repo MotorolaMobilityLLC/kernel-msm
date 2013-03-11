fa659a117a8d1311d97ac8491f5b55fda4ca2da8
Kiran Gunda <kgunda@codeaurora.org>
2013-03-11 19:14:44 +0530

    msm: 8930: i2c gpio changes for QRD SGLTE
    I2C camera which uses the gpios 20, 21 (connected to gsbi4)
    is moved to gpios 36, 37 (connected to gsbi8).
    
    Also gpios 95, 96 (connected to gsbi9) which were used for
    touch screen I2C communication, are now used for IPC
    communication (over UART) on SGLTE platform.
    
    Hence, the gsbi4 and gsbi9 are removed from i2c device init
    and gsbi8 is added for I2C camera.
    
    Change-Id: I84de4871a00015bc79bd256e7c71848ef84d71df
    Signed-off-by: Kiran Gunda <kgunda@codeaurora.org>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -1734,6 +1734,34 @@ struct platform_device msm8960_device_qup_i2c_gsbi4 = {
 	.resource	= resources_qup_i2c_gsbi4,
 };
 
+static struct resource resources_qup_i2c_gsbi8[] = {
+	{
+		.name	= "gsbi_qup_i2c_addr",
+		.start	= MSM_GSBI8_PHYS,
+		.end	= MSM_GSBI8_PHYS + 4 - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.name	= "qup_phys_addr",
+		.start	= MSM_GSBI8_QUP_PHYS,
+		.end	= MSM_GSBI8_QUP_PHYS + MSM_QUP_SIZE - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.name	= "qup_err_intr",
+		.start	= GSBI8_QUP_IRQ,
+		.end	= GSBI8_QUP_IRQ,
+		.flags	= IORESOURCE_IRQ,
+	},
+};
+
+struct platform_device msm8960_device_qup_i2c_gsbi8 = {
+	.name		= "qup_i2c",
+	.id		= 8,
+	.num_resources	= ARRAY_SIZE(resources_qup_i2c_gsbi8),
+	.resource	= resources_qup_i2c_gsbi8,
+};
+
 static struct resource resources_qup_i2c_gsbi3[] = {
 	{
 		.name	= "gsbi_qup_i2c_addr",
