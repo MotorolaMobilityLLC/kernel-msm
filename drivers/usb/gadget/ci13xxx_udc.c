MiCode/Xiaomi_Kernel_OpenSource

usb: gadget: Add remote wakeup event to ci13xxx UDC
When the gadget initiates remote-wakeup, raise an event to
the component that handles power management, so it can trigger
a runtime resume, if needed.

Change-Id: Ia3ed500b96ed09347e594d1e74209fdfef554263
Signed-off-by: Amit Blay <ablay@codeaurora.org>

drivers/usb/gadget/ci13xxx_udc.c
@@ -1476,6 +1476,15 @@ static int ci13xxx_wakeup(struct usb_gadget *_gadget)
 		dbg_trace("remote wakeup feature is not enabled\n");
 		goto out;
 	}
+	spin_unlock_irqrestore(udc->lock, flags);
+
+	udc->udc_driver->notify_event(udc,
+		CI13XXX_CONTROLLER_REMOTE_WAKEUP_EVENT);
+
+	if (udc->transceiver)
+		otg_set_suspend(udc->transceiver, 0);
+
+	spin_lock_irqsave(udc->lock, flags);
 	if (!hw_cread(CAP_PORTSC, PORTSC_SUSP)) {
 		ret = -EINVAL;
 		dbg_trace("port is not suspended\n");

MotorolaMobilityLLC/kernel-msm

drivers/usb/gadget/ci13xxx_udc.c
@@ -1499,6 +1499,15 @@ static int ci13xxx_wakeup(struct usb_gadget *_gadget)
 		dbg_trace("remote wakeup feature is not enabled\n");
 		goto out;
 	}
+	spin_unlock_irqrestore(udc->lock, flags);
+
+	udc->udc_driver->notify_event(udc,
+		CI13XXX_CONTROLLER_REMOTE_WAKEUP_EVENT);
+
+	if (udc->transceiver)
+		usb_phy_set_suspend(udc->transceiver, 0);
+
+	spin_lock_irqsave(udc->lock, flags);
 	if (!hw_cread(CAP_PORTSC, PORTSC_SUSP)) {
 		ret = -EINVAL;
 		dbg_trace("port is not suspended\n");