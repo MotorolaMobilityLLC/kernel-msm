RonGokhale/codeaurora_kernel

mfd: pm8921-core: Add mfd cell for pm8xxx-misc device into core
Add an mfd cell in the pm8921-core for a pm8xxx-misc device.

Change-Id: I0bc955c118aa427e45a4d2ce80065ca1d7c146a9
Signed-off-by: David Collins <collinsd@codeaurora.org>

drivers/mfd/pm8921-core.c
@@ -245,6 +245,11 @@ static struct mfd_cell charger_cell __devinitdata = {
 	.num_resources	= ARRAY_SIZE(charger_cell_resources),
 };
 
+static struct mfd_cell misc_cell __devinitdata = {
+	.name           = PM8XXX_MISC_DEV_NAME,
+	.id             = -1,
+};
+
 static int __devinit
 pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		      struct pm8921 *pmic)
@@ -380,6 +385,17 @@ pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 	if (ret) {
 		pr_err("Failed to add pwm subdevice ret=%d\n", ret);
 		goto bail;
+	}
+
+	if (pdata->misc_pdata) {
+		misc_cell.platform_data = pdata->misc_pdata;
+		misc_cell.data_size = sizeof(struct pm8xxx_misc_platform_data);
+		ret = mfd_add_devices(pmic->dev, 0, &misc_cell, 1, NULL,
+				      irq_base);
+		if (ret) {
+			pr_err("Failed to add  misc subdevice ret=%d\n", ret);
+			goto bail;
+		}
 	}
 
 	return 0;

MotorolaMobilityLLC/kernel-msm

drivers/mfd/pm8921-core.c
@@ -245,6 +245,11 @@ static struct mfd_cell charger_cell __devinitdata = {
 	.num_resources	= ARRAY_SIZE(charger_cell_resources),
 };
 
+static struct mfd_cell misc_cell __devinitdata = {
+	.name           = PM8XXX_MISC_DEV_NAME,
+	.id             = -1,
+};
+
 static int __devinit
 pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 		      struct pm8921 *pmic)
@@ -380,6 +385,17 @@ pm8921_add_subdevices(const struct pm8921_platform_data *pdata,
 	if (ret) {
 		pr_err("Failed to add pwm subdevice ret=%d\n", ret);
 		goto bail;
+	}
+
+	if (pdata->misc_pdata) {
+		misc_cell.platform_data = pdata->misc_pdata;
+		misc_cell.pdata_size = sizeof(struct pm8xxx_misc_platform_data);
+		ret = mfd_add_devices(pmic->dev, 0, &misc_cell, 1, NULL,
+				      irq_base);
+		if (ret) {
+			pr_err("Failed to add  misc subdevice ret=%d\n", ret);
+			goto bail;
+		}
 	}
 
 	return 0;