AliceOps/android_kernel_samsung_msm8660-common

usb: ci13xxx_udc: stop charging upon reset
USB CV tool may send reset to measure the unconfigure current.
Hence stop charging by reducing the charging current to 0mA to meet the
compliance requirement.

Change-Id: Ia4e0d6471bd21a632625753da99d5f0b3aad9480
CRs-fixed: 313277
Signed-off-by: Anji jonnala <anjir@codeaurora.org>

drivers/usb/gadget/ci13xxx_udc.c
@@ -1857,6 +1857,11 @@ __acquires(udc->lock)
 	dbg_event(0xFF, "BUS RST", 0);
 
 	spin_unlock(udc->lock);
+
+	/*stop charging upon reset */
+	if (udc->transceiver)
+		otg_set_power(udc->transceiver, 0);
+
 	retval = _gadget_stop_activity(&udc->gadget);
 	if (retval)
 		goto done;

MotorolaMobilityLLC/kernel-msm

drivers/usb/gadget/ci13xxx_udc.c
@@ -1884,6 +1884,11 @@ __acquires(udc->lock)
 	dbg_event(0xFF, "BUS RST", 0);
 
 	spin_unlock(udc->lock);
+
+	/*stop charging upon reset */
+	if (udc->transceiver)
+		usb_phy_set_power(udc->transceiver, 0);
+
 	retval = _gadget_stop_activity(&udc->gadget);
 	if (retval)
 		goto done;