40d023c653f81981ef35815bd4f47ceb02bc6940
Bang Nguyen <bnguyen1@motorola.com>
2013-03-14 09:26:19 -0500

    arm: msm: Added disable_splash() func
    - Add a disable_splash() func in mipi_dsi_platform_data
      structure to disable the cont_splash_enabled display feature
    
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>
    Change-Id: I9c1aa366a8c85b860fdd8d920b90fdedd8886ff0
    Reviewed-on: http://gerrit.pcs.mot.com/524748
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Zachary Crosby <azc095@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Limei Wang <e12499@motorola.com>
    SLT-Approved: Gerrit Code Review <gerrit-blurdev@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -454,6 +454,7 @@ struct mipi_dsi_platform_data {
 	int (*dsi_client_reset)(void);
 	int (*get_lane_config)(void);
 	char (*splash_is_enabled)(void);
+	void (*disable_splash)(void);
 	int (*is_factory_mode)(void);
 	int target_type;
 };
