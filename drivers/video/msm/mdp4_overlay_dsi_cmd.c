8c99659c3518ee67d0ba054cd3b22e6ef591074d
Yinjun Chen <a7301c@motorola.com>
2013-03-18 22:13:52 -0500

    Revert "(CR) msm_fb: display: Fix panic in mdp4_dsi_cmd_pipe_commit"
    The fix is duplicated with the commit as below in qcom drop AU 04.02.02.69.011:
    
    4257a94d: msm_fb: display: add mutex to video_off
    
    This reverts commit 7e9711cbc295be8d07dca85bf6df432591ecf1c7.

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -1071,13 +1071,11 @@ int mdp4_dsi_cmd_off(struct platform_device *pdev)
 	pr_debug("%s+: pid=%d\n", __func__, current->pid);
 
 	mfd = (struct msm_fb_data_type *)platform_get_drvdata(pdev);
-	mutex_lock(&mfd->dma->ov_mutex);
 
 	vctrl = &vsync_ctrl_db[cndx];
 	pipe = vctrl->base_pipe;
 	if (pipe == NULL) {
 		pr_err("%s: NO base pipe\n", __func__);
-		mutex_unlock(&mfd->dma->ov_mutex);
 		return ret;
 	}
 
@@ -1141,7 +1139,6 @@ int mdp4_dsi_cmd_off(struct platform_device *pdev)
 		mdp4_dsi_cmd_pipe_clean(vp);
 	}
 
-	mutex_unlock(&mfd->dma->ov_mutex);
 	pr_debug("%s-:\n", __func__);
 	return ret;
 }
