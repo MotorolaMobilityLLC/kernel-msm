96fbf74221addf62634366c0d097cfa9d486a777
Carter Cooper <ccooper@codeaurora.org>
2013-03-21 13:26:35 -0700

    msm: kgsl: Add support for coresight
    Adding support for Coresight debug bus to work with the GPU,
    including registering graphics core with Coresight and a coresight
    interface to GPU through sysfs.
    
    Change-Id: I9508659ca7d7d67e8a8becba41d06be76360c570
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -18,6 +18,8 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/msm_kgsl.h>
+#include <linux/delay.h>
+#include <linux/of_coresight.h>
 
 #include <mach/socinfo.h>
 #include <mach/msm_bus_board.h>
@@ -1509,6 +1511,9 @@ static int adreno_of_get_pdata(struct platform_device *pdev)
 	if (ret)
 		goto err;
 
+	pdata->coresight_pdata = of_get_coresight_platform_data(&pdev->dev,
+			pdev->dev.of_node);
+
 	pdev->dev.platform_data = pdata;
 	return 0;
 
@@ -1582,6 +1587,7 @@ static int __devinit
 adreno_probe(struct platform_device *pdev)
 {
 	struct kgsl_device *device;
+	struct kgsl_device_platform_data *pdata = NULL;
 	struct adreno_device *adreno_dev;
 	int status = -EINVAL;
 	bool is_dt;
@@ -1616,6 +1622,10 @@ adreno_probe(struct platform_device *pdev)
 	kgsl_pwrscale_attach_policy(device, ADRENO_DEFAULT_PWRSCALE_POLICY);
 
 	device->flags &= ~KGSL_FLAGS_SOFT_RESET;
+	pdata = kgsl_device_get_drvdata(device);
+
+	adreno_coresight_init(pdev);
+
 	return 0;
 
 error_close_device:
@@ -1635,6 +1645,8 @@ static int __devexit adreno_remove(struct platform_device *pdev)
 
 	device = (struct kgsl_device *)pdev->id_entry->driver_data;
 	adreno_dev = ADRENO_DEVICE(device);
+
+	adreno_coresight_remove(pdev);
 
 	kgsl_pwrscale_detach_policy(device);
 	kgsl_pwrscale_close(device);
