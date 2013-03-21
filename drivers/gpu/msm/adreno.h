db99a677f3c9e8061a7852f703a8258c4bd08dcc
Carter Cooper <ccooper@codeaurora.org>
2013-03-21 13:26:35 -0700

    msm: kgsl: Add support for coresight
    Adding support for Coresight debug bus to work with the GPU,
    including registering graphics core with Coresight and a coresight
    interface to GPU through sysfs.
    
    Change-Id: I9508659ca7d7d67e8a8becba41d06be76360c570
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -124,6 +124,15 @@ struct adreno_dispatcher {
 	struct kobject kobj;
 };
 
+enum coresight_debug_reg {
+	DEBUG_BUS_CTL,
+	TRACE_STOP_CNT,
+	TRACE_START_CNT,
+	TRACE_PERIOD_CNT,
+	TRACE_CMD,
+	TRACE_BUS_CTL,
+};
+
 struct adreno_gpudev;
 
 struct adreno_device {
@@ -232,6 +241,10 @@ struct adreno_gpudev {
 	uint64_t (*perfcounter_read)(struct adreno_device *adreno_dev,
 		unsigned int group, unsigned int counter,
 		unsigned int offset);
+	int (*coresight_enable) (struct kgsl_device *device);
+	void (*coresight_disable) (struct kgsl_device *device);
+	void (*coresight_config_debug_reg) (struct kgsl_device *device,
+			int debug_reg, unsigned int val);
 };
 
 #define FT_DETECT_REGS_COUNT 12
@@ -279,6 +292,10 @@ extern const unsigned int a330_registers_count;
 
 extern unsigned int ft_detect_regs[];
 
+int adreno_coresight_enable(struct coresight_device *csdev);
+void adreno_coresight_disable(struct coresight_device *csdev);
+void adreno_coresight_remove(struct platform_device *pdev);
+int adreno_coresight_init(struct platform_device *pdev);
 
 int adreno_idle(struct kgsl_device *device);
 void adreno_regread(struct kgsl_device *device, unsigned int offsetwords,
