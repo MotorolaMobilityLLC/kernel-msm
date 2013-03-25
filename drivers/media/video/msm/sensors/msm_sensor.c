67e8a95e4aefe545b0fd5894ece7e05a04144ac3
Elena Satraitis <w04888@motorola.com>
2013-03-24 19:28:28 -0500

    media: ov10820: ov0660: updates for IQ
    Added in hw_rev to determine the difference between r1a
    and r1b sensor to pass back to user space to determine
    which IQ settings to use.
    
    Also updated registers in ov10820 and ov660 per OV
    to update CFA color pattern bits correctly since
    we are mirroring and flipping the image.
    
    Updated the vt_pixel_clk to match exact output of
    the sensor pixels needed to meet FPS requirements.
    
    Also fixed exposure issue where 3503 was being
    overwritten due to offset not being applied
    for the coarse integration time.
    
    Also updated LSC tuning for ov660 and updated
    clear channel coefficients per IQ team
    requirements.
    
    Change-Id: I28349e8a8a1f01e9505993297684ed0c5802a338
    Signed-off-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/526911
    SLT-Approved: Gerrit Code Review <gerrit-blurdev@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Tomasz Jamrozy <ctj010@motorola.com>
    Reviewed-by: Rebecca Emmert <mtrg87@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/video/msm/sensors/msm_sensor.c
+++ drivers/media/video/msm/sensors/msm_sensor.c
@@ -608,6 +608,8 @@ int32_t msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void __user *argp)
 			}
 
 			cdata.cfg.module_info.size = s_ctrl->sensor_otp.size;
+			cdata.cfg.module_info.hw_rev =
+				s_ctrl->sensor_otp.hw_rev;
 
 			if (copy_to_user((void *)argp,
 					&cdata,
