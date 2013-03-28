fa9e29866226f337dc9e4f22b2649fdfa26573c1
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-03-29 00:48:18 +0530

    wlan:Fix the errors observed in static PE source code analysis.
    Resolve the generic issues observed in static source code
    analysis.
    
    CRs-Fixed: 467286
    Change-Id: I306b672845dea020cbf86714f5e13747935a3095

--- CORE/MAC/src/pe/lim/limSmeReqUtils.c
+++ CORE/MAC/src/pe/lim/limSmeReqUtils.c
@@ -360,32 +360,39 @@ limSetRSNieWPAiefromSmeStartBSSReqMessage(tpAniSirGlobal pMac,
         }
 
         // Check validity of WPA IE
-        val = sirReadU32((tANI_U8 *) &pRSNie->rsnIEdata[wpaIndex + 2]);
-
-        if ((pRSNie->rsnIEdata[wpaIndex] == SIR_MAC_WPA_EID) &&
-#if 0 // Comparison always false
-            (pRSNie->rsnIEdata[wpaIndex + 1] > SIR_MAC_WPA_IE_MAX_LENGTH) ||
-#endif
-             ((pRSNie->rsnIEdata[wpaIndex + 1] < SIR_MAC_WPA_IE_MIN_LENGTH) ||
-             (SIR_MAC_WPA_OUI != val)))
+        if(wpaIndex +4 < SIR_MAC_MAX_IE_LENGTH )
         {
-            limLog(pMac, LOGE,
-               FL("WPA IE len %d not [%d,%d] OR data 0x%x not 0x%x\n"),
-               pRSNie->rsnIEdata[1], SIR_MAC_RSN_IE_MIN_LENGTH, 
-               SIR_MAC_RSN_IE_MAX_LENGTH, val, SIR_MAC_WPA_OUI);
+            val = sirReadU32((tANI_U8 *) &pRSNie->rsnIEdata[wpaIndex + 2]);
 
-            return false;
+            if ((pRSNie->rsnIEdata[wpaIndex] == SIR_MAC_WPA_EID) &&
+#if 0 // Comparison always false
+                (pRSNie->rsnIEdata[wpaIndex + 1] > SIR_MAC_WPA_IE_MAX_LENGTH) ||
+#endif
+                ((pRSNie->rsnIEdata[wpaIndex + 1] < SIR_MAC_WPA_IE_MIN_LENGTH) ||
+                (SIR_MAC_WPA_OUI != val)))
+            {
+                limLog(pMac, LOGE,
+                  FL("WPA IE len %d not [%d,%d] OR data 0x%x not 0x%x\n"),
+                  pRSNie->rsnIEdata[1], SIR_MAC_RSN_IE_MIN_LENGTH,
+                  SIR_MAC_RSN_IE_MAX_LENGTH, val, SIR_MAC_WPA_OUI);
+
+                return false;
+            }
+            else
+            {
+                /* Both RSN and WPA IEs are present */
+                dot11fUnpackIeRSN(pMac,&pRSNie->rsnIEdata[2],
+                      (tANI_U8)pRSNie->length,&pSessionEntry->gStartBssRSNIe);
+
+                dot11fUnpackIeWPA(pMac,&pRSNie->rsnIEdata[wpaIndex + 6],
+                                 pRSNie->rsnIEdata[wpaIndex + 1]-4,
+                                    &pSessionEntry->gStartBssWPAIe);
+
+            }
         }
         else
         {
-            /* Both RSN and WPA IEs are present */
-            dot11fUnpackIeRSN(pMac,&pRSNie->rsnIEdata[2],
-                  (tANI_U8)pRSNie->length,&pSessionEntry->gStartBssRSNIe);
-
-            dot11fUnpackIeWPA(pMac,&pRSNie->rsnIEdata[wpaIndex + 6],
-                              pRSNie->rsnIEdata[wpaIndex + 1]-4,
-                                &pSessionEntry->gStartBssWPAIe);
-
+            return false;
         }
     }
 
