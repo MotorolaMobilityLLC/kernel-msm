da5505c45841f972b83b27972cdfad3544694cd9
Jason Hrycay <jason.hrycay@motorola.com>
2013-04-01 14:24:26 -0500

    media: msm: update exp gain function parameters
    Update the ar0834, ov10820, ov8820, and ov8835 v4ls drivers to use the
    new _write_exp_gain function prototypes that add luma_avg and fgain
    parameters.
    
    Change-Id: I6f69320fb55d1e404b83f595e42d785bfbf36ce8
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>

--- drivers/media/video/msm/sensors/s5k5b3g_v4l2.c
+++ drivers/media/video/msm/sensors/s5k5b3g_v4l2.c
@@ -294,7 +294,7 @@ static int32_t s5k5b3g_get_module_info(struct msm_sensor_ctrl_t *s_ctrl)
 }
 
 static int32_t s5k5b3g_write_exp_gain(struct msm_sensor_ctrl_t *s_ctrl,
-		uint16_t gain, uint32_t line)
+		uint16_t gain, uint32_t line, int32_t luma_avg, uint16_t fgain)
 {
 	uint32_t fl_lines, offset;
 	int32_t rc = 0;
