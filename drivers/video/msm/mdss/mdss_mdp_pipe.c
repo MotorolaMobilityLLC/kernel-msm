f0a727c9361d851861087549f7bbf685164bee37
Han Jun-Yeong <junyeong.han@lge.com>
2013-04-02 09:49:23 +0900

    msm: mdss: Set XOR operation for opmode not OR operation.
    Set opmode to MDSS_MDP_OP_FLIP_UD default because LCD was rotated.
    When mdp pipe flag was set as MDSS_MDP_OP_FLIP_UD by Android orientation sensor,
    MDSS_MDP_OP_FLIP_UD of opmode should be clear.
    
    Change-Id: I69f12139f12670f233699380656a68f32df4585a

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -501,7 +501,7 @@ static int mdss_mdp_format_setup(struct mdss_mdp_pipe *pipe)
 #ifdef CONFIG_FB_MSM_MDSS_UD_FLIP
 	if (pipe->mfd && pipe->mfd->panel_info &&
 		pipe->mfd->panel_info->pdest == DISPLAY_1)
-		opmode |= MDSS_MDP_OP_FLIP_UD;
+		opmode ^= MDSS_MDP_OP_FLIP_UD;
 #endif
 	mdss_mdp_pipe_sspp_setup(pipe, &opmode);
 
