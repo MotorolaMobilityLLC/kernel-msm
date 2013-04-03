11523a9306cde8161b7ed339b5075f5823985f89
Jeff Johnson <jjohnson@qca.qualcomm.com>
2013-04-03 16:39:24 -0700

    wlan: Fix uninitialized variable reference in WLANTL_STARxAuth()
    Static source code analysis identified WAPI feature code in function
    WLANTL_STARxAuth() which is accessing an uninitialized variable.
    Modify the code to initialize the variable before it is used.
    
    Change-Id: I09d6e11cbb19e1b6dc07c9be6af3dadff7f306cf
    CRs-fixed: 470188

--- CORE/TL/src/wlan_qct_tl.c
+++ CORE/TL/src/wlan_qct_tl.c
@@ -7407,7 +7407,7 @@ WLANTL_STARxAuth
    v_U16_t                  usActualHLen = 0;   
    v_U8_t                   ucTid;
 #ifdef FEATURE_WLAN_WAPI
-   v_U16_t                  usEtherType;
+   v_U16_t                  usEtherType = 0;
 #endif
    v_U16_t                  usPktLen;
    vos_pkt_t*               vosDataBuff ;
