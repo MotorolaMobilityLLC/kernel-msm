ca186e3f08fde6e9660a917873e22cd8c7084286
Huaibin Yang <huaibiny@codeaurora.org>
2013-04-02 18:41:05 -0700

    msm_fb: display: fix the suspend/resume hang caused by wait4vsync stuck
    wait_for_completion in wait4vsync stucks after multiple suspend/resume
    tests. The cause is that when show_event timeout happens, or vsync isr
    is disabled for long time, wait_vsync_cnt resets to zero, and in vsync
    isr complete_all has no chance to be called which caused mdp hang. So
    complete_all needs to be called whenever show_event has timeouts.
    
    Change-Id: I124899ff95fd5550d2759d215f16fa9cb8c0c211
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -493,6 +493,7 @@ ssize_t mdp4_dsi_video_show_event(struct device *dev,
 	ret = wait_for_completion_interruptible_timeout(&vctrl->vsync_comp,
 		msecs_to_jiffies(VSYNC_PERIOD * 4));
 	if (ret <= 0) {
+		complete_all(&vctrl->vsync_comp);
 		vctrl->wait_vsync_cnt = 0;
 		vctrl->vsync_time = ktime_get();
 	}
