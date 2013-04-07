0102f0c9a232faa32390d510546fb74674309201
Russ W. Knize <Russ.Knize@motorola.com>
2013-04-07 16:21:43 -0500

    fs: rename wrapfs to esdfs
    Wrapfs is the base of the new Emulated SD Card file system for
    Android, which replaces the FUSE-backed "sdcard" service.
    
    Change-Id: I0a69ac606b8750f2a3a953987d795695d53da139
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- fs/esdfs/super.c
+++ fs/esdfs/super.c
@@ -9,44 +9,44 @@
  * published by the Free Software Foundation.
  */
 
-#include "wrapfs.h"
+#include "esdfs.h"
 
 /*
  * The inode cache is used with alloc_inode for both our inode info and the
  * vfs inode.
  */
-static struct kmem_cache *wrapfs_inode_cachep;
+static struct kmem_cache *esdfs_inode_cachep;
 
 /* final actions when unmounting a file system */
-static void wrapfs_put_super(struct super_block *sb)
+static void esdfs_put_super(struct super_block *sb)
 {
-	struct wrapfs_sb_info *spd;
+	struct esdfs_sb_info *spd;
 	struct super_block *s;
 
-	spd = WRAPFS_SB(sb);
+	spd = ESDFS_SB(sb);
 	if (!spd)
 		return;
 
 	/* decrement lower super references */
-	s = wrapfs_lower_super(sb);
-	wrapfs_set_lower_super(sb, NULL);
+	s = esdfs_lower_super(sb);
+	esdfs_set_lower_super(sb, NULL);
 	atomic_dec(&s->s_active);
 
 	kfree(spd);
 	sb->s_fs_info = NULL;
 }
 
-static int wrapfs_statfs(struct dentry *dentry, struct kstatfs *buf)
+static int esdfs_statfs(struct dentry *dentry, struct kstatfs *buf)
 {
 	int err;
 	struct path lower_path;
 
-	wrapfs_get_lower_path(dentry, &lower_path);
+	esdfs_get_lower_path(dentry, &lower_path);
 	err = vfs_statfs(&lower_path, buf);
-	wrapfs_put_lower_path(dentry, &lower_path);
+	esdfs_put_lower_path(dentry, &lower_path);
 
 	/* set return buf to our f/s to avoid confusing user-level utils */
-	buf->f_type = WRAPFS_SUPER_MAGIC;
+	buf->f_type = ESDFS_SUPER_MAGIC;
 
 	return err;
 }
@@ -55,7 +55,7 @@ static int wrapfs_statfs(struct dentry *dentry, struct kstatfs *buf)
  * @flags: numeric mount options
  * @options: mount options string
  */
-static int wrapfs_remount_fs(struct super_block *sb, int *flags, char *options)
+static int esdfs_remount_fs(struct super_block *sb, int *flags, char *options)
 {
 	int err = 0;
 
@@ -66,7 +66,7 @@ static int wrapfs_remount_fs(struct super_block *sb, int *flags, char *options)
 	 */
 	if ((*flags & ~(MS_RDONLY | MS_MANDLOCK | MS_SILENT)) != 0) {
 		printk(KERN_ERR
-		       "wrapfs: remount flags 0x%x unsupported\n", *flags);
+		       "esdfs: remount flags 0x%x unsupported\n", *flags);
 		err = -EINVAL;
 	}
 
@@ -79,7 +79,7 @@ static int wrapfs_remount_fs(struct super_block *sb, int *flags, char *options)
  * that needs to be, before the inode is completely destroyed and put
  * on the inode free list.
  */
-static void wrapfs_evict_inode(struct inode *inode)
+static void esdfs_evict_inode(struct inode *inode)
 {
 	struct inode *lower_inode;
 
@@ -89,80 +89,80 @@ static void wrapfs_evict_inode(struct inode *inode)
 	 * Decrement a reference to a lower_inode, which was incremented
 	 * by our read_inode when it was created initially.
 	 */
-	lower_inode = wrapfs_lower_inode(inode);
-	wrapfs_set_lower_inode(inode, NULL);
+	lower_inode = esdfs_lower_inode(inode);
+	esdfs_set_lower_inode(inode, NULL);
 	iput(lower_inode);
 }
 
-static struct inode *wrapfs_alloc_inode(struct super_block *sb)
+static struct inode *esdfs_alloc_inode(struct super_block *sb)
 {
-	struct wrapfs_inode_info *i;
+	struct esdfs_inode_info *i;
 
-	i = kmem_cache_alloc(wrapfs_inode_cachep, GFP_KERNEL);
+	i = kmem_cache_alloc(esdfs_inode_cachep, GFP_KERNEL);
 	if (!i)
 		return NULL;
 
 	/* memset everything up to the inode to 0 */
-	memset(i, 0, offsetof(struct wrapfs_inode_info, vfs_inode));
+	memset(i, 0, offsetof(struct esdfs_inode_info, vfs_inode));
 
 	i->vfs_inode.i_version = 1;
 	return &i->vfs_inode;
 }
 
-static void wrapfs_destroy_inode(struct inode *inode)
+static void esdfs_destroy_inode(struct inode *inode)
 {
-	kmem_cache_free(wrapfs_inode_cachep, WRAPFS_I(inode));
+	kmem_cache_free(esdfs_inode_cachep, ESDFS_I(inode));
 }
 
-/* wrapfs inode cache constructor */
+/* esdfs inode cache constructor */
 static void init_once(void *obj)
 {
-	struct wrapfs_inode_info *i = obj;
+	struct esdfs_inode_info *i = obj;
 
 	inode_init_once(&i->vfs_inode);
 }
 
-int wrapfs_init_inode_cache(void)
+int esdfs_init_inode_cache(void)
 {
 	int err = 0;
 
-	wrapfs_inode_cachep =
-		kmem_cache_create("wrapfs_inode_cache",
-				  sizeof(struct wrapfs_inode_info), 0,
+	esdfs_inode_cachep =
+		kmem_cache_create("esdfs_inode_cache",
+				  sizeof(struct esdfs_inode_info), 0,
 				  SLAB_RECLAIM_ACCOUNT, init_once);
-	if (!wrapfs_inode_cachep)
+	if (!esdfs_inode_cachep)
 		err = -ENOMEM;
 	return err;
 }
 
-/* wrapfs inode cache destructor */
-void wrapfs_destroy_inode_cache(void)
+/* esdfs inode cache destructor */
+void esdfs_destroy_inode_cache(void)
 {
-	if (wrapfs_inode_cachep)
-		kmem_cache_destroy(wrapfs_inode_cachep);
+	if (esdfs_inode_cachep)
+		kmem_cache_destroy(esdfs_inode_cachep);
 }
 
 /*
  * Used only in nfs, to kill any pending RPC tasks, so that subsequent
  * code can actually succeed and won't leave tasks that need handling.
  */
-static void wrapfs_umount_begin(struct super_block *sb)
+static void esdfs_umount_begin(struct super_block *sb)
 {
 	struct super_block *lower_sb;
 
-	lower_sb = wrapfs_lower_super(sb);
+	lower_sb = esdfs_lower_super(sb);
 	if (lower_sb && lower_sb->s_op && lower_sb->s_op->umount_begin)
 		lower_sb->s_op->umount_begin(lower_sb);
 }
 
-const struct super_operations wrapfs_sops = {
-	.put_super	= wrapfs_put_super,
-	.statfs		= wrapfs_statfs,
-	.remount_fs	= wrapfs_remount_fs,
-	.evict_inode	= wrapfs_evict_inode,
-	.umount_begin	= wrapfs_umount_begin,
+const struct super_operations esdfs_sops = {
+	.put_super	= esdfs_put_super,
+	.statfs		= esdfs_statfs,
+	.remount_fs	= esdfs_remount_fs,
+	.evict_inode	= esdfs_evict_inode,
+	.umount_begin	= esdfs_umount_begin,
 	.show_options	= generic_show_options,
-	.alloc_inode	= wrapfs_alloc_inode,
-	.destroy_inode	= wrapfs_destroy_inode,
+	.alloc_inode	= esdfs_alloc_inode,
+	.destroy_inode	= esdfs_destroy_inode,
 	.drop_inode	= generic_delete_inode,
 };
