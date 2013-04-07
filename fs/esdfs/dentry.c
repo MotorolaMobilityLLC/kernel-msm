b50ef1d47b97a8189112ba7ae359df86f92ce6f4
Russ W. Knize <Russ.Knize@motorola.com>
2013-04-07 16:21:43 -0500

    fs: rename wrapfs to esdfs
    Wrapfs is the base of the new Emulated SD Card file system for
    Android, which replaces the FUSE-backed "sdcard" service.
    
    Change-Id: I22a94ab9093f3427774b185d709dd55d5776d373
    CRs-fixed: (CR)
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- fs/esdfs/dentry.c
+++ fs/esdfs/dentry.c
@@ -9,14 +9,14 @@
  * published by the Free Software Foundation.
  */
 
-#include "wrapfs.h"
+#include "esdfs.h"
 
 /*
  * returns: -ERRNO if error (returned to user)
  *          0: tell VFS to invalidate dentry
  *          1: dentry is valid
  */
-static int wrapfs_d_revalidate(struct dentry *dentry, unsigned int flags)
+static int esdfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 {
 	struct path lower_path;
 	struct dentry *lower_dentry;
@@ -25,25 +25,25 @@ static int wrapfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	if (flags & LOOKUP_RCU)
 		return -ECHILD;
 
-	wrapfs_get_lower_path(dentry, &lower_path);
+	esdfs_get_lower_path(dentry, &lower_path);
 	lower_dentry = lower_path.dentry;
 	if (!(lower_dentry->d_flags & DCACHE_OP_REVALIDATE))
 		goto out;
 	err = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
 out:
-	wrapfs_put_lower_path(dentry, &lower_path);
+	esdfs_put_lower_path(dentry, &lower_path);
 	return err;
 }
 
-static void wrapfs_d_release(struct dentry *dentry)
+static void esdfs_d_release(struct dentry *dentry)
 {
 	/* release and reset the lower paths */
-	wrapfs_put_reset_lower_path(dentry);
+	esdfs_put_reset_lower_path(dentry);
 	free_dentry_private_data(dentry);
 	return;
 }
 
-const struct dentry_operations wrapfs_dops = {
-	.d_revalidate	= wrapfs_d_revalidate,
-	.d_release	= wrapfs_d_release,
+const struct dentry_operations esdfs_dops = {
+	.d_revalidate	= esdfs_d_revalidate,
+	.d_release	= esdfs_d_release,
 };
