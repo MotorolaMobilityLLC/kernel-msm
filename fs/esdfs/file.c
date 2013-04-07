7507ea4e71f64f457a64b54f3d7fa52844969616
Russ W. Knize <Russ.Knize@motorola.com>
2013-04-07 16:21:43 -0500

    fs: rename wrapfs to esdfs
    Wrapfs is the base of the new Emulated SD Card file system for
    Android, which replaces the FUSE-backed "sdcard" service.
    
    Change-Id: Ie3a64b9d01aba7fe054a3183bb1668bfecf157af
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/658209
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/esdfs/file.c
+++ fs/esdfs/file.c
@@ -9,16 +9,16 @@
  * published by the Free Software Foundation.
  */
 
-#include "wrapfs.h"
+#include "esdfs.h"
 
-static ssize_t wrapfs_read(struct file *file, char __user *buf,
+static ssize_t esdfs_read(struct file *file, char __user *buf,
 			   size_t count, loff_t *ppos)
 {
 	int err;
 	struct file *lower_file;
 	struct dentry *dentry = file->f_path.dentry;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	err = vfs_read(lower_file, buf, count, ppos);
 	/* update our inode atime upon a successful lower read */
 	if (err >= 0)
@@ -28,14 +28,14 @@ static ssize_t wrapfs_read(struct file *file, char __user *buf,
 	return err;
 }
 
-static ssize_t wrapfs_write(struct file *file, const char __user *buf,
+static ssize_t esdfs_write(struct file *file, const char __user *buf,
 			    size_t count, loff_t *ppos)
 {
 	int err = 0;
 	struct file *lower_file;
 	struct dentry *dentry = file->f_path.dentry;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	err = vfs_write(lower_file, buf, count, ppos);
 	/* update our inode times+sizes upon a successful lower write */
 	if (err >= 0) {
@@ -48,13 +48,13 @@ static ssize_t wrapfs_write(struct file *file, const char __user *buf,
 	return err;
 }
 
-static int wrapfs_readdir(struct file *file, void *dirent, filldir_t filldir)
+static int esdfs_readdir(struct file *file, void *dirent, filldir_t filldir)
 {
 	int err = 0;
 	struct file *lower_file = NULL;
 	struct dentry *dentry = file->f_path.dentry;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	err = vfs_readdir(lower_file, filldir, dirent);
 	file->f_pos = lower_file->f_pos;
 	if (err >= 0)		/* copy the atime */
@@ -63,13 +63,13 @@ static int wrapfs_readdir(struct file *file, void *dirent, filldir_t filldir)
 	return err;
 }
 
-static long wrapfs_unlocked_ioctl(struct file *file, unsigned int cmd,
+static long esdfs_unlocked_ioctl(struct file *file, unsigned int cmd,
 				  unsigned long arg)
 {
 	long err = -ENOTTY;
 	struct file *lower_file;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 
 	/* XXX: use vfs_ioctl if/when VFS exports it */
 	if (!lower_file || !lower_file->f_op)
@@ -86,13 +86,13 @@ out:
 }
 
 #ifdef CONFIG_COMPAT
-static long wrapfs_compat_ioctl(struct file *file, unsigned int cmd,
+static long esdfs_compat_ioctl(struct file *file, unsigned int cmd,
 				unsigned long arg)
 {
 	long err = -ENOTTY;
 	struct file *lower_file;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 
 	/* XXX: use vfs_ioctl if/when VFS exports it */
 	if (!lower_file || !lower_file->f_op)
@@ -105,7 +105,7 @@ out:
 }
 #endif
 
-static int wrapfs_mmap(struct file *file, struct vm_area_struct *vma)
+static int esdfs_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	int err = 0;
 	bool willwrite;
@@ -125,10 +125,10 @@ static int wrapfs_mmap(struct file *file, struct vm_area_struct *vma)
 	 * not, return EINVAL (the same error that
 	 * generic_file_readonly_mmap returns in that case).
 	 */
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	if (willwrite && !lower_file->f_mapping->a_ops->writepage) {
 		err = -EINVAL;
-		printk(KERN_ERR "wrapfs: lower file system does not "
+		printk(KERN_ERR "esdfs: lower file system does not "
 		       "support writeable mmap\n");
 		goto out;
 	}
@@ -138,10 +138,10 @@ static int wrapfs_mmap(struct file *file, struct vm_area_struct *vma)
 	 *
 	 * XXX: the VFS should have a cleaner way of finding the lower vm_ops
 	 */
-	if (!WRAPFS_F(file)->lower_vm_ops) {
+	if (!ESDFS_F(file)->lower_vm_ops) {
 		err = lower_file->f_op->mmap(lower_file, vma);
 		if (err) {
-			printk(KERN_ERR "wrapfs: lower mmap failed %d\n", err);
+			printk(KERN_ERR "esdfs: lower mmap failed %d\n", err);
 			goto out;
 		}
 		saved_vm_ops = vma->vm_ops; /* save: came from lower ->mmap */
@@ -152,17 +152,17 @@ static int wrapfs_mmap(struct file *file, struct vm_area_struct *vma)
 	 * don't want its test for ->readpage which returns -ENOEXEC.
 	 */
 	file_accessed(file);
-	vma->vm_ops = &wrapfs_vm_ops;
+	vma->vm_ops = &esdfs_vm_ops;
 
-	file->f_mapping->a_ops = &wrapfs_aops; /* set our aops */
-	if (!WRAPFS_F(file)->lower_vm_ops) /* save for our ->fault */
-		WRAPFS_F(file)->lower_vm_ops = saved_vm_ops;
+	file->f_mapping->a_ops = &esdfs_aops; /* set our aops */
+	if (!ESDFS_F(file)->lower_vm_ops) /* save for our ->fault */
+		ESDFS_F(file)->lower_vm_ops = saved_vm_ops;
 
 out:
 	return err;
 }
 
-static int wrapfs_open(struct inode *inode, struct file *file)
+static int esdfs_open(struct inode *inode, struct file *file)
 {
 	int err = 0;
 	struct file *lower_file = NULL;
@@ -175,41 +175,41 @@ static int wrapfs_open(struct inode *inode, struct file *file)
 	}
 
 	file->private_data =
-		kzalloc(sizeof(struct wrapfs_file_info), GFP_KERNEL);
-	if (!WRAPFS_F(file)) {
+		kzalloc(sizeof(struct esdfs_file_info), GFP_KERNEL);
+	if (!ESDFS_F(file)) {
 		err = -ENOMEM;
 		goto out_err;
 	}
 
 	/* open lower object and link wrapfs's file struct to lower's */
-	wrapfs_get_lower_path(file->f_path.dentry, &lower_path);
+	esdfs_get_lower_path(file->f_path.dentry, &lower_path);
 	lower_file = dentry_open(&lower_path, file->f_flags, current_cred());
 	path_put(&lower_path);
 	if (IS_ERR(lower_file)) {
 		err = PTR_ERR(lower_file);
-		lower_file = wrapfs_lower_file(file);
+		lower_file = esdfs_lower_file(file);
 		if (lower_file) {
-			wrapfs_set_lower_file(file, NULL);
+			esdfs_set_lower_file(file, NULL);
 			fput(lower_file); /* fput calls dput for lower_dentry */
 		}
 	} else {
-		wrapfs_set_lower_file(file, lower_file);
+		esdfs_set_lower_file(file, lower_file);
 	}
 
 	if (err)
-		kfree(WRAPFS_F(file));
+		kfree(ESDFS_F(file));
 	else
-		fsstack_copy_attr_all(inode, wrapfs_lower_inode(inode));
+		fsstack_copy_attr_all(inode, esdfs_lower_inode(inode));
 out_err:
 	return err;
 }
 
-static int wrapfs_flush(struct file *file, fl_owner_t id)
+static int esdfs_flush(struct file *file, fl_owner_t id)
 {
 	int err = 0;
 	struct file *lower_file = NULL;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	if (lower_file && lower_file->f_op && lower_file->f_op->flush)
 		err = lower_file->f_op->flush(lower_file, id);
 
@@ -217,21 +217,21 @@ static int wrapfs_flush(struct file *file, fl_owner_t id)
 }
 
 /* release all lower object references & free the file info structure */
-static int wrapfs_file_release(struct inode *inode, struct file *file)
+static int esdfs_file_release(struct inode *inode, struct file *file)
 {
 	struct file *lower_file;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	if (lower_file) {
-		wrapfs_set_lower_file(file, NULL);
+		esdfs_set_lower_file(file, NULL);
 		fput(lower_file);
 	}
 
-	kfree(WRAPFS_F(file));
+	kfree(ESDFS_F(file));
 	return 0;
 }
 
-static int wrapfs_fsync(struct file *file, loff_t start, loff_t end,
+static int esdfs_fsync(struct file *file, loff_t start, loff_t end,
 			int datasync)
 {
 	int err;
@@ -242,54 +242,54 @@ static int wrapfs_fsync(struct file *file, loff_t start, loff_t end,
 	err = generic_file_fsync(file, start, end, datasync);
 	if (err)
 		goto out;
-	lower_file = wrapfs_lower_file(file);
-	wrapfs_get_lower_path(dentry, &lower_path);
+	lower_file = esdfs_lower_file(file);
+	esdfs_get_lower_path(dentry, &lower_path);
 	err = vfs_fsync_range(lower_file, start, end, datasync);
-	wrapfs_put_lower_path(dentry, &lower_path);
+	esdfs_put_lower_path(dentry, &lower_path);
 out:
 	return err;
 }
 
-static int wrapfs_fasync(int fd, struct file *file, int flag)
+static int esdfs_fasync(int fd, struct file *file, int flag)
 {
 	int err = 0;
 	struct file *lower_file = NULL;
 
-	lower_file = wrapfs_lower_file(file);
+	lower_file = esdfs_lower_file(file);
 	if (lower_file->f_op && lower_file->f_op->fasync)
 		err = lower_file->f_op->fasync(fd, lower_file, flag);
 
 	return err;
 }
 
-const struct file_operations wrapfs_main_fops = {
+const struct file_operations esdfs_main_fops = {
 	.llseek		= generic_file_llseek,
-	.read		= wrapfs_read,
-	.write		= wrapfs_write,
-	.unlocked_ioctl	= wrapfs_unlocked_ioctl,
+	.read		= esdfs_read,
+	.write		= esdfs_write,
+	.unlocked_ioctl	= esdfs_unlocked_ioctl,
 #ifdef CONFIG_COMPAT
-	.compat_ioctl	= wrapfs_compat_ioctl,
+	.compat_ioctl	= esdfs_compat_ioctl,
 #endif
-	.mmap		= wrapfs_mmap,
-	.open		= wrapfs_open,
-	.flush		= wrapfs_flush,
-	.release	= wrapfs_file_release,
-	.fsync		= wrapfs_fsync,
-	.fasync		= wrapfs_fasync,
+	.mmap		= esdfs_mmap,
+	.open		= esdfs_open,
+	.flush		= esdfs_flush,
+	.release	= esdfs_file_release,
+	.fsync		= esdfs_fsync,
+	.fasync		= esdfs_fasync,
 };
 
 /* trimmed directory options */
-const struct file_operations wrapfs_dir_fops = {
+const struct file_operations esdfs_dir_fops = {
 	.llseek		= generic_file_llseek,
 	.read		= generic_read_dir,
-	.readdir	= wrapfs_readdir,
-	.unlocked_ioctl	= wrapfs_unlocked_ioctl,
+	.readdir	= esdfs_readdir,
+	.unlocked_ioctl	= esdfs_unlocked_ioctl,
 #ifdef CONFIG_COMPAT
-	.compat_ioctl	= wrapfs_compat_ioctl,
+	.compat_ioctl	= esdfs_compat_ioctl,
 #endif
-	.open		= wrapfs_open,
-	.release	= wrapfs_file_release,
-	.flush		= wrapfs_flush,
-	.fsync		= wrapfs_fsync,
-	.fasync		= wrapfs_fasync,
+	.open		= esdfs_open,
+	.release	= esdfs_file_release,
+	.flush		= esdfs_flush,
+	.fsync		= esdfs_fsync,
+	.fasync		= esdfs_fasync,
 };
