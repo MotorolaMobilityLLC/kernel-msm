95579f307f49e404a6d0f9f74053b9bea1b0f449
Varun Reddy Yeturu <c_vyetur@qca.qualcomm.com>
2013-04-09 09:57:16 -0700

    wlan: Feature Addition to disable RM
    Radio Measurement Support will be
    disabled during CCX using the feature
    FEATURE_DISABLE_RM.The CCX IE's will
    not be populated with RM support.
    
    Change-Id: I37d020ad4f242f33e2c9dbab9c7b6f5995c1afbb
    CRs-Fixed: 467735

--- CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -2160,7 +2160,9 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
     // For CCX Associations fill the CCX IEs
     if (psessionEntry->isCCXconnection)
     {
+#ifndef FEATURE_DISABLE_RM
         PopulateDot11fCCXRadMgmtCap(&pFrm->CCXRadMgmtCap);
+#endif
         PopulateDot11fCCXVersion(&pFrm->CCXVersion);
     }
 #endif
@@ -2488,7 +2490,9 @@ limSendReassocReqWithFTIEsMgmtFrame(tpAniSirGlobal     pMac,
     // For CCX Associations fill the CCX IEs
     if (psessionEntry->isCCXconnection)
     {
+#ifndef FEATURE_DISABLE_RM
         PopulateDot11fCCXRadMgmtCap(&frm.CCXRadMgmtCap);
+#endif
         PopulateDot11fCCXVersion(&frm.CCXVersion);
     }
 #endif //FEATURE_WLAN_CCX 
