7e6ca8404c108f629879243cd1c1b3f9034c6ba1
prabhu annabathula <prabhu.annabathula@motorola.com>
2013-04-12 16:35:52 -0500

    arm: msm: remove i2s board file
    Board file was also registering the i2s dai device along with
    device tree triggering the probe() twice. Broard file no longer
    needed.
    
    Change-Id: I2c04d0fab8637fdb1359222398de200653b1d884
    Signed-off-by: prabhu annabathula <prabhu.annabathula@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/531965
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Joseph Dwyer <wljd08@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-mmi.h
+++ arch/arm/mach-msm/board-mmi.h
@@ -54,9 +54,6 @@ extern void mmi_audio_dsp_init(void);
 /* from board-mmi-regulator.c */
 extern void mmi_regulator_init(struct msm8960_oem_init_ptrs *);
 
-/* from board-mmi-i2s.c */
-extern void mmi_i2s_dai_init(void);
-
 /* from board-mmi-vibrator.c */
 #ifdef CONFIG_VIB_TIMED
 void __init mmi_vibrator_init(void);
