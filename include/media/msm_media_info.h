0d2b011563a6d2c15a2e080a64a70261ddafe5cf
Praveen Chavan <pchavan@codeaurora.org>
2013-04-12 11:36:07 -0700

    msm: vidc: Change inline functions to function macros
    Stripped out kernel headers remove the function definitions
    so changing these functions to macros.
    
    Change-Id: I550157301b92855eaf2fbb92bcba050f84fe0b95
    Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>

--- include/media/msm_media_info.h
+++ include/media/msm_media_info.h
@@ -10,110 +10,90 @@ enum color_fmts {
 	COLOR_FMT_NV21,
 };
 
-static inline unsigned int VENUS_Y_STRIDE(int color_fmt, int width)
-{
-	unsigned int alignment, stride = 0;
-	if (!width)
-		goto invalid_input;
+#define VENUS_Y_STRIDE(__color_fmt, __width) ({\
+	unsigned int __alignment, __stride = 0; \
+	if (__width) { \
+		switch (__color_fmt) { \
+		case COLOR_FMT_NV12: \
+		__alignment = 128; \
+		__stride = MSM_MEDIA_ALIGN(__width, __alignment);\
+		break;\
+		default:\
+		break;\
+		}\
+	} \
+	__stride;\
+})
 
-	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
-		alignment = 128;
-		stride = MSM_MEDIA_ALIGN(width, alignment);
-		break;
-	default:
-		break;
-	}
-invalid_input:
-	return stride;
-}
+#define VENUS_UV_STRIDE(__color_fmt, __width) ({\
+	unsigned int __alignment, __stride = 0; \
+	if (__width) {\
+		switch (__color_fmt) { \
+		case COLOR_FMT_NV12: \
+		__alignment = 128; \
+		__stride = MSM_MEDIA_ALIGN(__width, __alignment); \
+		break; \
+		default: \
+		break; \
+		} \
+	} \
+	__stride; \
+})
 
-static inline unsigned int VENUS_UV_STRIDE(int color_fmt, int width)
-{
-	unsigned int alignment, stride = 0;
-	if (!width)
-		goto invalid_input;
+#define VENUS_Y_SCANLINES(__color_fmt, __height) ({ \
+	unsigned int __alignment, __sclines = 0; \
+	if (__height) {\
+		switch (__color_fmt) { \
+		case COLOR_FMT_NV12: \
+		__alignment = 32; \
+		__sclines = MSM_MEDIA_ALIGN(__height, __alignment); \
+		break; \
+		default: \
+		break; \
+		} \
+	} \
+	__sclines; \
+})
 
-	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
-		alignment = 128;
-		stride = MSM_MEDIA_ALIGN(width, alignment);
-		break;
-	default:
-		break;
-	}
-invalid_input:
-	return stride;
-}
+#define VENUS_UV_SCANLINES(__color_fmt, __height) ({\
+	unsigned int __alignment, __sclines = 0; \
+	if (__height) {\
+		switch (__color_fmt) { \
+		case COLOR_FMT_NV12: \
+			__alignment = 16; \
+			__sclines = MSM_MEDIA_ALIGN(((__height + 1) >> 1), __alignment); \
+			break; \
+		default: \
+			break; \
+		} \
+	} \
+	__sclines; \
+})
 
-static inline unsigned int VENUS_Y_SCANLINES(int color_fmt, int height)
-{
-	unsigned int alignment, sclines = 0;
-	if (!height)
-		goto invalid_input;
-
-	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
-		alignment = 32;
-		sclines = MSM_MEDIA_ALIGN(height, alignment);
-		break;
-	default:
-		break;
-	}
-invalid_input:
-	return sclines;
-}
-
-static inline unsigned int VENUS_UV_SCANLINES(int color_fmt, int height)
-{
-	unsigned int alignment, sclines = 0;
-	if (!height)
-		goto invalid_input;
-
-	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
-		alignment = 16;
-		sclines = MSM_MEDIA_ALIGN(((height + 1) >> 1), alignment);
-		break;
-	default:
-		break;
-	}
-invalid_input:
-	return sclines;
-}
-
-static inline unsigned int VENUS_BUFFER_SIZE(
-	int color_fmt, int width, int height)
-{
-	unsigned int uv_alignment;
-	unsigned int size = 0;
-	unsigned int y_plane, uv_plane, y_stride,
-		uv_stride, y_sclines, uv_sclines;
-	if (!width || !height)
-		goto invalid_input;
-
-	y_stride = VENUS_Y_STRIDE(color_fmt, width);
-	uv_stride = VENUS_UV_STRIDE(color_fmt, width);
-	y_sclines = VENUS_Y_SCANLINES(color_fmt, height);
-	uv_sclines = VENUS_UV_SCANLINES(color_fmt, height);
-	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
-		uv_alignment = 4096;
-		y_plane = y_stride * y_sclines;
-		uv_plane = uv_stride * uv_sclines + uv_alignment;
-		size = y_plane + uv_plane;
-		size = MSM_MEDIA_ALIGN(size, 4096);
-		break;
-	default:
-		break;
-	}
-invalid_input:
-	return size;
-}
+#define VENUS_BUFFER_SIZE( \
+	__color_fmt, __width, __height) ({ \
+	unsigned int __uv_alignment; \
+	unsigned int __size = 0; \
+	unsigned int __y_plane, __uv_plane, __y_stride, \
+		__uv_stride, __y_sclines, __uv_sclines; \
+	if (__width && __height) {\
+		__y_stride = VENUS_Y_STRIDE(__color_fmt, __width); \
+		__uv_stride = VENUS_UV_STRIDE(__color_fmt, __width); \
+		__y_sclines = VENUS_Y_SCANLINES(__color_fmt, __height); \
+		__uv_sclines = VENUS_UV_SCANLINES(__color_fmt, __height); \
+		switch (__color_fmt) { \
+		case COLOR_FMT_NV12: \
+			__uv_alignment = 4096; \
+			__y_plane = __y_stride * __y_sclines; \
+			__uv_plane = __uv_stride * __uv_sclines + __uv_alignment; \
+			__size = __y_plane + __uv_plane; \
+			__size = MSM_MEDIA_ALIGN(__size, 4096); \
+			break; \
+		default: \
+			break; \
+		} \
+	} \
+	__size; \
+})
 
 #endif
