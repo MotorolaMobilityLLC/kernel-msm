8d29efd890f51f9592a2f72776d2b6ed8349f031
Venkata Prathyusha Kuntupalli <c_vkuntu@qca.qualcomm.com>
2013-04-15 14:31:36 -0700

    wlan: Parsing the Multicast and Unicast encryption types correctly
    Multicast and unicast encryption types being swapped
    when being passed from CSR to LIM
    
    Change-Id: I6de8d6b6324381548f6a1d1d72f4ca17179a593b
    CRs-Fixed: 466231

--- CORE/MAC/src/pe/lim/limSerDesUtils.c
+++ CORE/MAC/src/pe/lim/limSerDesUtils.c
@@ -1073,13 +1073,13 @@ limJoinReqSerDes(tpAniSirGlobal pMac, tpSirSmeJoinReq pJoinReq, tANI_U8 *pBuf)
             return eSIR_FAILURE;
     }
 
-    pJoinReq->MCEncryptionType = limGetU32(pBuf);
+    pJoinReq->UCEncryptionType = limGetU32(pBuf);
     pBuf += sizeof(tANI_U32);
     len -= sizeof(tANI_U32);
     if (limCheckRemainingLength(pMac, len) == eSIR_FAILURE)
         return eSIR_FAILURE;    
     
-    pJoinReq->UCEncryptionType = limGetU32(pBuf);
+    pJoinReq->MCEncryptionType = limGetU32(pBuf);
     pBuf += sizeof(tANI_U32);
     len -= sizeof(tANI_U32);
     if (limCheckRemainingLength(pMac, len) == eSIR_FAILURE)
