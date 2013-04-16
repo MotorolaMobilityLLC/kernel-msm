2872ee10ead1c74f170bfb3af38d4da1ce5c79ab
Jared Suttles <jsuttles@motorola.com>
2013-04-16 12:08:23 -0500

    drivers: misc: add dropbox sysfs interface driver
    This driver is designed to allow people to queue events for a userspace
    daemon to read and copy into dropbox. The interface is intentionally
    generic to allow simple small changes to add dropbox data from
    kernel drivers.
    
    Change-Id: Icc62def663fdcc51dd6fbef1e864644655447031
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Patrick Auchter <wpa722@motorola.com>
    Reviewed-by: Joseph Swantek <w98568@motorola.com>
    Reviewed-by: Fred Fettinger <w04904@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/539592
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>

---
+++ include/linux/dropbox.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013, Motorola Mobility, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef DROPBOX_H
+#define DROPBOX_H
+
+#include <linux/types.h>
+#include <linux/time.h>
+
+#ifdef CONFIG_DROPBOX
+
+extern void dropbox_register_trigger_callback(const char *name,
+	void (*callback)(void *), void *data);
+extern void dropbox_queue_event_binary(char *name, void *data, size_t size);
+extern void dropbox_queue_event_text(char *name, void *data, size_t size);
+
+#else
+
+static void dropbox_register_trigger_callback(const char *name,
+	void (*callback)(void *), void *data) {}
+static void dropbox_queue_event_binary(char *name, void *data, size_t size) {}
+static void dropbox_queue_event_text(char *name, void *data, size_t size) {}
+
+#endif
+
+#endif /* DROPBOX_H */
