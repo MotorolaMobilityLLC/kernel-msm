533f467aabed752f1fbad4f7aa597a3f95525e17
Han Jun-Yeong <junyeong.han@lge.com>
2013-04-17 14:22:03 +0900

    platform: qpnp-power-on: fix for skipping disabled power-on node
    When qpnp-power-on node is handled, skip to config a node which has a
    property "qcom,disable=<1>". If a node has it, it is omitted from
    configuration counter value pon->num_pon_config.
    
    However, when qpnp_pon_config_init(pon) is called, it iterates by using
    child node, not num_pon_config to config each pon nodes.
    As pon->pon_cfg is alloced by kzalloc() based on pon->num_pon_config,
    it is possible that memory corruption may happen by access wrong position
    (e.g pon->pon_cfg[1] which has not been allocated may be accessed even
    pon->num_pon_config is 1).
    
    To fix this situation, skip it if node has "qcom,disable"
    in qpnp_pon_config_init().
    
    Change-Id: I7b75208777fd0452e5f8a8afe0d47fae45a6666f
    Signed-off-by: Han Jun-Yeong <junyeong.han@lge.com>

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -561,9 +561,13 @@ static int __devinit qpnp_pon_config_init(struct qpnp_pon *pon)
 	int rc = 0, i = 0;
 	struct device_node *pp = NULL;
 	struct qpnp_pon_config *cfg;
+	int disable = 0;
 
 	/* iterate through the list of pon configs */
 	while ((pp = of_get_next_child(pon->spmi->dev.of_node, pp))) {
+		rc = of_property_read_u32(pp, "qcom,disable", &disable);
+		if (!rc && disable)
+			continue;
 
 		cfg = &pon->pon_cfg[i++];
 
