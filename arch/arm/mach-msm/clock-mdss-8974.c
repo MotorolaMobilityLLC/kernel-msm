546efdc583c64bb286af2e342242feb9104090f9
Devin Kim <dojip.kim@lge.com>
2013-04-17 15:57:23 -0700

    msm: mdss: Fix mdss clock
    Change-Id: Ieff2fa50a5bb3b12ac52977e9a7e006453c6bc16

--- arch/arm/mach-msm/clock-mdss-8974.c
+++ arch/arm/mach-msm/clock-mdss-8974.c
@@ -230,7 +230,7 @@ static int __mdss_dsi_pll_byte_set_rate(struct clk *c, unsigned long rate)
 	REG_W(0x02, mdss_dsi_base + 0x0208); /* ChgPump */
 	REG_W(pll_divcfg1, mdss_dsi_base + 0x0204); /* postDiv1 */
 	REG_W(pll_divcfg2, mdss_dsi_base + 0x0224); /* postDiv2 */
-	REG_W(0x05, mdss_dsi_base + 0x0228); /* postDiv3 */
+	REG_W(0x02, mdss_dsi_base + 0x0228); /* postDiv3 */
 
 	REG_W(0x2b, mdss_dsi_base + 0x0278); /* Cal CFG3 */
 	REG_W(0x06, mdss_dsi_base + 0x027c); /* Cal CFG4 */
