4b1948f924f1fd6c20d07432800f9e19c01861fc
Joel King <joelking@codeaurora.org>
2013-04-17 18:04:35 -0700

    msm: 8064: remove flag to prevent soft reset of qsc on first power up
    This flag was an optimization because the SOFT_RESET
    gpio was initialized HIGH on first bootup of the APQ,
    so toggling the soft reset gpio was not necessary if
    the pmic on the qsc is still off. However, there is a
    corner case where the flag prevents phase 1 SSR from
    being collected because in that case, the pmic on the
    QSC is actually on. Removing the flag solves the SSR
    corner case issue at the expense of removing the
    optimization but the performance hit is very small.
    Cold boot functionality is not affected.
    
    Change-Id: Ib5399997e7de9f3f5133e238c600dd648f546893
    Signed-off-by: Joel King <joelking@codeaurora.org>

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -1866,7 +1866,6 @@ static struct mdm_platform_data sglte2_qsc_platform_data = {
 	.no_powerdown_after_ramdumps = 1,
 	.image_upgrade_supported = 1,
 	.no_a2m_errfatal_on_ssr = 1,
-	.no_reset_on_first_powerup = 1,
 	.kpd_not_inverted = 1,
 	.subsys_name = "external_modem",
 };
