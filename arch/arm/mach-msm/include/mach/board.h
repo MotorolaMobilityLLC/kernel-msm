7d0181bc3ae7b2b20898a1ca4714bac61351c3f7
Elena Satraitis <w04888@motorola.com>
2013-04-18 15:34:54 -0500

    IKJB42MAIN-10180: arm: msm: Add flag for PK supply
    Added flag to know when to turn on the PK supply for
    the ASIC.
    
    Change-Id: Icbc0c0d252233d3fbbcdb7885e597ee2b045a9ad
    Signed-off-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/533274
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -264,6 +264,7 @@ struct oem_camera_sensor_data {
 	int sensor_using_shared_mipi;
 	int sensor_allow_asic_bypass;
 	int sensor_asic_revision;
+	int sensor_using_new_pk_dvdd;
 };
 
 struct msm_camera_sensor_info {
