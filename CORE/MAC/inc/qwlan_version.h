842db37637ab7476dc9ef14b762a32eaf4280b4e
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-04-18 17:40:48 -0700

    Release 3.2.3.104
    Release 3.2.3.104

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -59,10 +59,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
-#define QWLAN_VERSION_EXTRA            "a"
-#define QWLAN_VERSION_BUILD            103
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            104
 
-#define QWLAN_VERSIONSTR               "3.2.3.103a"
+#define QWLAN_VERSIONSTR               "3.2.3.104"
 
 #endif /* QWLAN_VERSION_H */
 
