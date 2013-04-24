6fc99e2fe18153dc3013417a3afdfea5719efe1e
Yumin Tang (gvm346) <gvm346@motorola.com>
2013-04-24 08:22:54 -0500

    char: diag: add motorola optimized logging
    Qcom 23231 1st CPL drop caused a conflict with optimized logging
    implementaiton. To resolve this conflict, Qcom CPL drop is used as baseline
    and optimized logging changes is made on top of it.
    
    Change-Id: I9258f1a01d5f04484d5ebd5312453868dc39c264
    Signed-off-by: Yumin Tang (gvm346) <gvm346@motorola.com>
    (cherry picked from commit b7f3412075c0afba7e76d2b542d973aa5645ae8b)
    (cherry picked from commit c58ca9bcec77640f5968e1a8c09e2de16f663452)
    Reviewed-on: http://gerrit.pcs.mot.com/609637
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -78,8 +78,9 @@
 #define LPASS_DATA		1
 #define WCNSS_DATA		2
 #define APPS_DATA		3
-#define HSIC_DATA		4
-#define HSIC_2_DATA		5
+#define SDIO_DATA		4
+#define HSIC_DATA		5
+#define HSIC_2_DATA		6
 #define SMUX_DATA		10
 #define APPS_PROC		1
 /*
@@ -124,12 +125,10 @@
 #define DIAG_SS_PARAMS		0x32
 
 #define DIAG_DIAG_MAX_PKT_SZ	0x55
-#define DIAG_DIAG_STM		0x214
+#define DIAG_DIAG_STM		0x20E
 #define DIAG_DIAG_POLL		0x03
 #define DIAG_DEL_RSP_WRAP	0x04
 #define DIAG_DEL_RSP_WRAP_CNT	0x05
-
-#define BAD_PARAM_RESPONSE_MESSAGE 20
 
 #define MODE_CMD	41
 #define RESET_ID	2
@@ -322,7 +321,6 @@ struct diag_smd_info {
 	int notify_context;
 	struct work_struct diag_general_smd_work;
 	int general_context;
-	uint8_t inited;
 
 	/*
 	 * Function ptr for function to call to process the data that
@@ -404,11 +402,6 @@ struct diagchar_dev {
 	struct diag_ctrl_msg_mask *msg_mask;
 	struct diag_ctrl_feature_mask *feature_mask;
 	struct mutex log_mask_mutex;
-	/* Members for Sending response */
-	unsigned char *encoded_rsp_buf;
-	uint8_t rsp_buf_busy;
-	struct diag_request *rsp_write_ptr;
-	spinlock_t rsp_buf_busy_lock;
 	/* State for diag forwarding */
 	struct diag_smd_info smd_data[NUM_SMD_DATA_CHANNELS];
 	struct diag_smd_info smd_cntl[NUM_SMD_CONTROL_CHANNELS];
@@ -442,6 +435,7 @@ struct diagchar_dev {
 #ifdef CONFIG_DIAG_OVER_USB
 	int usb_connected;
 	struct usb_diag_ch *legacy_ch;
+	int usb_req_allocated;
 	struct work_struct diag_proc_hdlc_work;
 	struct work_struct diag_read_work;
 	struct work_struct diag_usb_connect_work;
@@ -476,6 +470,20 @@ struct diagchar_dev {
 	struct diag_ws_ref_t md_ws;
 	spinlock_t ws_lock;
 
+#ifdef CONFIG_DIAG_SDIO_PIPE
+	unsigned char *buf_in_sdio;
+	unsigned char *usb_buf_mdm_out;
+	struct sdio_channel *sdio_ch;
+	int read_len_mdm;
+	int in_busy_sdio;
+	struct usb_diag_ch *mdm_ch;
+	struct work_struct diag_read_mdm_work;
+	struct workqueue_struct *diag_sdio_wq;
+	struct work_struct diag_read_sdio_work;
+	struct work_struct diag_close_sdio_work;
+	struct diag_request *usb_read_mdm_ptr;
+	struct diag_request *write_ptr_mdm;
+#endif
 #ifdef CONFIG_DIAGFWD_BRIDGE_CODE
 	/* common for all bridges */
 	struct work_struct diag_connect_work;
@@ -487,6 +495,9 @@ struct diagchar_dev {
 	int diag_smux_enabled;
 	int smux_connected;
 	struct diag_request *write_ptr_mdm;
+#endif
+#ifdef CONFIG_DIAG_EXTENSION
+	struct list_head addon_list;
 #endif
 };
 
@@ -511,5 +522,59 @@ void diag_ws_on_copy_fail(int type);
 void diag_ws_on_copy_complete(int type);
 void diag_ws_reset(int type);
 void diag_ws_release(void);
+
+#ifdef CONFIG_DIAG_EXTENSION
+/* This structure is for addon. It is used by slate feature */
+struct diag_addon {
+	struct list_head list;
+
+	/* function list of addon
+	return-value of the functions decide
+	whether the callback-function of next-addon is called or not.
+	refer to DIAGADDON_BASE below.
+	*/
+	int (*ioctl)(struct file *filp, unsigned int iocmd,
+					unsigned long ioarg, int *retval);
+	int (*force_returntype)(int pkt_type, int *retval);
+	int (*addon_channel_diag_write)(struct diag_request *write_ptr,
+								int *retval);
+	int (*channel_diag_write)(struct usb_diag_ch *ch,
+						struct diag_request *d_req);
+	void *private;
+
+	/* function list of diag-driver to use addon */
+	int (*diag_process_apps_pkt)(unsigned char *buf, int len);
+};
+
+#define DIAGADDON_BASE(func, retval, ...)		\
+	do {						\
+		struct diag_addon *addon;		 \
+		int next_addon_call;			\
+		list_for_each_entry(addon, &driver->addon_list, list) {	\
+			if (addon->func) {		\
+				next_addon_call =	\
+					addon->func(__VA_ARGS__, retval);\
+				if (next_addon_call == false)	\
+					break;			\
+			}				\
+		}					\
+	} while (0)
+
+#define DIAGADDON_EXIST() (!list_empty(&driver->addon_list))
+#define DIAGADDON_ioctl(retval, ...)\
+		DIAGADDON_BASE(ioctl, retval, ##__VA_ARGS__)
+#define DIAGADDON_force_returntype(retval, ...)\
+		DIAGADDON_BASE(force_returntype, retval, ##__VA_ARGS__)
+#define DIAGADDON_channel_diag_write(retval, ...)\
+		DIAGADDON_BASE(addon_channel_diag_write, retval, ##__VA_ARGS__)
+
+int diag_addon_register(struct diag_addon *addon);
+int diag_addon_unregister(struct diag_addon *addon);
+#else
+#define DIAGADDON_EXIST() 0
+#define DIAGADDON_ioctl(retval, ...) do {} while (0)
+#define DIAGADDON_force_returntype(retval, ...) do {} while (0)
+#define DIAGADDON_channel_diag_write(retval, i...) do {} while (0)
+#endif /* endif of '#ifdef CONFIG_DIAG_EXTENSION' */
 
 #endif
--- drivers/char/diag/diagfwd_sdio.h
+++ drivers/char/diag/diagfwd_sdio.h
@@ -1,4 +1,4 @@
-/* Copyright (c)2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -10,10 +10,18 @@
  * GNU General Public License for more details.
  */
 
-#ifndef DIAG_DEBUGFS_H
-#define DIAG_DEBUGFS_H
+#ifndef DIAGFWD_SDIO_H
+#define DIAGFWD_SDIO_H
 
-void diag_debugfs_init(void);
-void diag_debugfs_cleanup(void);
+#include <mach/sdio_al.h>
+#define N_MDM_SDIO_WRITE	1 /* Upgrade to 2 with ping pong buffer */
+#define N_MDM_SDIO_READ	1
+
+void diagfwd_sdio_init(void);
+void diagfwd_sdio_exit(void);
+int diagfwd_connect_sdio(void);
+int diagfwd_disconnect_sdio(void);
+int diagfwd_read_complete_sdio(void);
+int diagfwd_write_complete_sdio(void);
 
 #endif
