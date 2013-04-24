8f6e2287e29a9dc0245c68e42a6cd120c0e88351
Dan Vacura <w36195@motorola.com>
2013-04-24 10:55:28 -0500

    msm: camera: vpe fix panic on close
    Static variable wasn't null'd out in current session but
    possibly used in subsequent sessions causing a crash
    
    Change-Id: I19f6f2190dfac39bdda113cff1a98ea7fccb8c01
    Signed-off-by: Dan Vacura <w36195@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/534380
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>

--- drivers/media/video/msm/msm_vpe.c
+++ drivers/media/video/msm/msm_vpe.c
@@ -1014,6 +1014,7 @@ static int msm_vpe_subdev_close(struct v4l2_subdev *sd,
 			frame_info->p_mctl->client, mctl->domain_num);
 		msm_mctl_unmap_user_frame(&frame_info->dest_frame,
 			frame_info->p_mctl->client, mctl->domain_num);
+		vpe_ctrl->pp_frame_info = NULL;
 	}
 	/* Drain the payload queue. */
 	msm_queue_drain(&vpe_ctrl->eventData_q, list_eventdata);
