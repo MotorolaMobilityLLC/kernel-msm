2421fc002b17181b0ddcf1905e43eeb478d6f4ce
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-04-25 11:57:56 -0700

    wlan: Release 3.2.3.107a
    Release 3.2.3.107a

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -59,10 +59,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
-#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_EXTRA            "a"
 #define QWLAN_VERSION_BUILD            107
 
-#define QWLAN_VERSIONSTR               "3.2.3.107"
+#define QWLAN_VERSIONSTR               "3.2.3.107a"
 
 #endif /* QWLAN_VERSION_H */
 
