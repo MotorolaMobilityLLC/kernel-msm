b3a64fae609dbfbf8a557a49b60b4e7a02c67bda
Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
2013-04-24 18:01:37 -0600

    msm: ipc: Do not load any subsystem by default
    Some users of IPC Router like IPC Router Security Configuration (IRSC)
    open up a socket, perform some configuration/lookup operation quickly and
    then close the socket. This causes the IPC Router to load a subsystem by
    default and then unload it immediately. This behavior causes the subsystem
    to lock up sometimes during the boot process.
    
    Remove loading and unloading a subsystem by default during the socket
    create and close operation.
    
    CRs-Fixed: 480156
    Change-Id: I9c1c01c18a538a462bcf65902e496bc773a03991
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- arch/arm/mach-msm/ipc_router_smd_xprt.c
+++ arch/arm/mach-msm/ipc_router_smd_xprt.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -473,31 +473,6 @@ static int msm_ipc_router_smd_remote_probe(struct platform_device *pdev)
 
 	return 0;
 }
-
-void *msm_ipc_load_default_node(void)
-{
-	void *pil = NULL;
-	const char *peripheral;
-
-	peripheral = smd_edge_to_subsystem(SMD_APPS_MODEM);
-	if (peripheral && !strncmp(peripheral, "modem", 6)) {
-		pil = pil_get(peripheral);
-		if (IS_ERR(pil)) {
-			pr_err("%s: Failed to load %s\n",
-				__func__, peripheral);
-			pil = NULL;
-		}
-	}
-	return pil;
-}
-EXPORT_SYMBOL(msm_ipc_load_default_node);
-
-void msm_ipc_unload_default_node(void *pil)
-{
-	if (pil)
-		pil_put(pil);
-}
-EXPORT_SYMBOL(msm_ipc_unload_default_node);
 
 static struct platform_driver msm_ipc_router_smd_remote_driver[] = {
 	{
