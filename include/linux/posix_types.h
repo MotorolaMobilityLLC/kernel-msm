c9ca2342d2d9feb5f40477333505c4d9a62c1fe1
Igor Kovalenko <cik009@motorola.com>
2013-04-30 15:22:31 -0500

    Revert "posix_types.h: Cleanup stale __NFDBITS and related definitions"
    This reverts commit 27cd8f51344dcf4799c7a092c1797402b833126a.

--- include/linux/posix_types.h
+++ include/linux/posix_types.h
@@ -15,14 +15,26 @@
  */
 
 /*
- * This macro may have been defined in <gnu/types.h>. But we always
- * use the one here.
+ * Those macros may have been defined in <gnu/types.h>. But we always
+ * use the ones here. 
  */
+#undef __NFDBITS
+#define __NFDBITS	(8 * sizeof(unsigned long))
+
 #undef __FD_SETSIZE
 #define __FD_SETSIZE	1024
 
+#undef __FDSET_LONGS
+#define __FDSET_LONGS	(__FD_SETSIZE/__NFDBITS)
+
+#undef __FDELT
+#define	__FDELT(d)	((d) / __NFDBITS)
+
+#undef __FDMASK
+#define	__FDMASK(d)	(1UL << ((d) % __NFDBITS))
+
 typedef struct {
-	unsigned long fds_bits[__FD_SETSIZE / (8 * sizeof(long))];
+	unsigned long fds_bits [__FDSET_LONGS];
 } __kernel_fd_set;
 
 /* Type of a signal handler.  */
