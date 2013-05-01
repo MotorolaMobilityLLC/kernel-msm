9c4bd9479d6b4483280b5d0282f514baf7b215d1
Konstantin Makariev <hcv867@motorola.com>
2013-04-30 19:02:25 -0500

    mipi_mot_panel: config option for display notifications
    Introduce a new option for display notifications. Enforce
    MIPI_MOT_PANEL_NOTIFICATIONS option when FB_MSM_MIPI_MOT_PT_PANEL
    is set.
    
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>

---
+++ drivers/video/msm/mipi_mot_notifications.c
@@ -0,0 +1,35 @@
+/* Copyright (c) 2013, Motorola Mobility, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include "mipi_mot.h"
+
+int mmi_panel_register_notifier(struct notifier_block *nb)
+{
+	struct mipi_mot_panel *mot_panel = mipi_mot_get_mot_panel();
+	return srcu_notifier_chain_register(
+				&mot_panel->panel_notifier_list, nb);
+}
+
+int mmi_panel_unregister_notifier(struct notifier_block *nb)
+{
+	struct mipi_mot_panel *mot_panel = mipi_mot_get_mot_panel();
+	return srcu_notifier_chain_unregister(
+				&mot_panel->panel_notifier_list, nb);
+}
+
+void mmi_panel_notify(unsigned int state, void *data)
+{
+	struct mipi_mot_panel *mot_panel = mipi_mot_get_mot_panel();
+	pr_debug("%s (%d) is called\n", __func__, state);
+	srcu_notifier_call_chain(&mot_panel->panel_notifier_list, state, data);
+}
