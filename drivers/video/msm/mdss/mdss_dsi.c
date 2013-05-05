3a85fd17cad5f0c2a24f9e78786b0903babcbd31
Se Hun Kim <sean2.kim@lge.com>
2013-05-05 15:33:46 -0700

    msm: mdss: Adding lcd notifier
    Supports notifier for start&end of LCD power on&off
    
    Change-Id: Idaa685a2c554286da5f764519305d830c4d1a1bd
    Signed-off-by: Se Hun Kim <sean2.kim@lge.com>
    Signed-off-by: Jongrak Kwon <jongrak.kwon@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -21,6 +21,7 @@
 #include <linux/gpio.h>
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
+#include <linux/lcd_notify.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -678,6 +679,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		rc = mdss_dsi_on(pdata);
 		if (ctrl_pdata->on_cmds->ctrl_state == DSI_LP_MODE) {
 			rc = mdss_dsi_unblank(pdata);
@@ -686,8 +688,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 	case MDSS_EVENT_PANEL_ON:
 		if (ctrl_pdata->on_cmds->ctrl_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		if (ctrl_pdata->off_cmds->ctrl_state == DSI_HS_MODE) {
 			rc = mdss_dsi_blank(pdata);
 		}
@@ -697,6 +701,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata);
 		}
 		rc = mdss_dsi_off(pdata);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->on_cmds->ctrl_state == DSI_LP_MODE) {
