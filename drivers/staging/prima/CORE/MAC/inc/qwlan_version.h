d754fb345c79a3044c128dcd955529a182d66bf6
Iliyan Malchev <malchev@google.com>
2013-05-07 16:32:06 -0700

    prima: release v3.2.2.8
    	git://codeaurora.org/external/wlan/prima.git
    
    	abf45e1 wlan: Release 3.2.2.8
    	a2e6c7c Fix the issue with zero bssid indication when connection is failed.
    	2cae715 wlan: assoc rejects and connection failures with ZERO bssid
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            7
+#define QWLAN_VERSION_BUILD            8
 
-#define QWLAN_VERSIONSTR               "3.2.2.7"
+#define QWLAN_VERSIONSTR               "3.2.2.8"
 
 #endif /* QWLAN_VERSION_H */
 
