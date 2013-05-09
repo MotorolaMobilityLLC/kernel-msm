53b43fb320db87c6a4634f05bc24c67d0f29f63f
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-05-09 17:23:45 +0530

    wlan: beacon filtering for quiet period removed from host.
    Beacon filtering for quiet period was enabled in the host,
    which leads to waking up of host every 5 seconds if quiet
    period enabled.
    All the intelligence for quiet period exists in RIVA.
    
    Change-Id: I32ac46d9daff02de8942bd43df26466678679ef5
    CRs-fixed: 484259

--- CORE/MAC/src/pe/lim/limSendMessages.c
+++ CORE/MAC/src/pe/lim/limSendMessages.c
@@ -74,7 +74,6 @@ static tBeaconFilterIe beaconFilterTable[] = {
    {SIR_MAC_EDCA_PARAM_SET_EID,  0, {0, 0, EDCA_FILTER_MASK,      0}},
    {SIR_MAC_QOS_CAPABILITY_EID,  0, {0, 0, QOS_FILTER_MASK,       0}},
    {SIR_MAC_CHNL_SWITCH_ANN_EID, 1, {0, 0, 0,                     0}},
-   {SIR_MAC_QUIET_EID,           1, {0, 0, 0,                     0}},
    {SIR_MAC_HT_INFO_EID,         0, {0, 0, HT_BYTE0_FILTER_MASK,  0}},  
    {SIR_MAC_HT_INFO_EID,         0, {2, 0, HT_BYTE2_FILTER_MASK,  0}}, 
    {SIR_MAC_HT_INFO_EID,         0, {5, 0, HT_BYTE5_FILTER_MASK,  0}}
