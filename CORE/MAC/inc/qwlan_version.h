58df97c1a5b911f6e186274082cbda69586c0343
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-05-13 10:23:57 -0700

    wlan: Release 3.2.3.116
    wlan: Release 3.2.3.116

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            115
+#define QWLAN_VERSION_BUILD            116
 
-#define QWLAN_VERSIONSTR               "3.2.3.115"
+#define QWLAN_VERSIONSTR               "3.2.3.116"
 
 #endif /* QWLAN_VERSION_H */
 
