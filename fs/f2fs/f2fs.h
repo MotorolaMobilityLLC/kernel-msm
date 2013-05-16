4d5df3ffcca1c6f77717143749878f50acd407c7
Russ W. Knize <Russ.Knize@motorola.com>
2013-05-16 15:13:48 -0500

    f2fs: add error handling mode mount option
    Add two mount options to control driver behavior when errors are
    encountered.  The "errors=panic" option will panic the kernel on an
    error, "error=recover" causes the driver to attempt to recover from
    the failure, and "error=continue" causes the driver to ignore the
    error and carry on.
    
    Change-Id: I50e02ca0e7bdf00a271b04983894e1c178893277
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/539331
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -40,6 +40,8 @@
 #define F2FS_MOUNT_DISABLE_EXT_IDENTIFY	0x00000040
 #define F2FS_MOUNT_INLINE_XATTR		0x00000080
 #define F2FS_MOUNT_INLINE_DATA		0x00000100
+#define F2FS_MOUNT_ERRORS_PANIC		0x00002000
+#define F2FS_MOUNT_ERRORS_RECOVER	0x00004000
 
 #define clear_opt(sbi, option)	(sbi->mount_opt.opt &= ~F2FS_MOUNT_##option)
 #define set_opt(sbi, option)	(sbi->mount_opt.opt |= F2FS_MOUNT_##option)
@@ -632,6 +634,15 @@ static inline int F2FS_HAS_BLOCKS(struct inode *inode)
 		return inode->i_blocks > F2FS_DEFAULT_ALLOCATED_BLOCKS;
 }
 
+static inline int f2fs_handle_error(struct f2fs_sb_info *sbi)
+{
+	if (test_opt(sbi, ERRORS_PANIC))
+		BUG();
+	if (test_opt(sbi, ERRORS_RECOVER))
+		return 1;
+	return 0;
+}
+
 static inline bool inc_valid_block_count(struct f2fs_sb_info *sbi,
 				 struct inode *inode, blkcnt_t count)
 {
@@ -787,9 +798,24 @@ static inline void dec_valid_node_count(struct f2fs_sb_info *sbi,
 {
 	spin_lock(&sbi->stat_lock);
 
-	f2fs_bug_on(!sbi->total_valid_block_count);
-	f2fs_bug_on(!sbi->total_valid_node_count);
-	f2fs_bug_on(!inode->i_blocks);
+	if (sbi->total_valid_block_count < 1) {
+		pr_crit("F2FS-fs (%s): block accounting error: %u < %u\n",
+			sbi->sb->s_id, sbi->total_valid_block_count, 1);
+		f2fs_handle_error(sbi);
+		sbi->total_valid_block_count = 1;
+	}
+	if (sbi->total_valid_node_count < 1) {
+		pr_crit("F2FS-fs (%s): node accounting error: %u < %u\n",
+			sbi->sb->s_id, sbi->total_valid_node_count, 1);
+		f2fs_handle_error(sbi);
+		sbi->total_valid_node_count = 1;
+	}
+	if (inode->i_blocks < 1) {
+		pr_crit("F2FS-fs (%s): inode accounting error: %llu < %u\n",
+			sbi->sb->s_id, inode->i_blocks, 1);
+		f2fs_handle_error(sbi);
+		inode->i_blocks = 1;
+	}
 
 	inode->i_blocks--;
 	sbi->total_valid_node_count--;
