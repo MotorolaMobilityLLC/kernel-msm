13e1fc414a08a0a0a8d4a46e02bc4dc0ef5a5804
Russ W. Knize <Russ.Knize@motorola.com>
2013-05-16 15:13:48 -0500

    f2fs: add error handling mode mount option
    Add two mount options to control driver behavior when errors are
    encountered.  The "errors=panic" option will panic the kernel on an
    error, "error=recover" causes the driver to attempt to recover from
    the failure, and "error=continue" causes the driver to ignore the
    error and carry on.
    
    Change-Id: I50e02ca0e7bdf00a271b04983894e1c178893277
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/539331
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -156,7 +156,7 @@ retry:
 	write_unlock(&nm_i->nat_tree_lock);
 }
 
-static void set_node_addr(struct f2fs_sb_info *sbi, struct node_info *ni,
+static int set_node_addr(struct f2fs_sb_info *sbi, struct node_info *ni,
 			block_t new_blkaddr)
 {
 	struct f2fs_nm_info *nm_i = NM_I(sbi);
@@ -179,7 +179,14 @@ retry:
 		 * So, reinitialize it with new information.
 		 */
 		e->ni = *ni;
-		f2fs_bug_on(ni->blk_addr != NULL_ADDR);
+		if (ni->blk_addr != NULL_ADDR) {
+			f2fs_msg(sbi->sb, KERN_ERR, "node block address is "
+				"already set: %u", ni->blk_addr);
+			f2fs_handle_error(sbi);
+			/* just give up on this node */
+			write_unlock(&nm_i->nat_tree_lock);
+			return -EIO;
+		}
 	}
 
 	/* sanity check */
@@ -202,6 +209,7 @@ retry:
 	nat_set_blkaddr(e, new_blkaddr);
 	__set_nat_cache_dirty(nm_i, e);
 	write_unlock(&nm_i->nat_tree_lock);
+	return 0;
 }
 
 int try_to_free_nats(struct f2fs_sb_info *sbi, int nr_shrink)
@@ -930,7 +938,13 @@ repeat:
 		goto repeat;
 	}
 got_it:
-	f2fs_bug_on(nid != nid_of_node(page));
+	if (nid != nid_of_node(page)) {
+		f2fs_msg(sbi->sb, KERN_ERR, "page node id does not match "
+			"request: %lu", nid);
+		f2fs_handle_error(sbi);
+		f2fs_put_page(page, 1);
+		return ERR_PTR(-EIO);
+	}
 	mark_page_accessed(page);
 	return page;
 }
@@ -1537,6 +1551,7 @@ int recover_inode_page(struct f2fs_sb_info *sbi, struct page *page)
 	nid_t ino = ino_of_node(page);
 	struct node_info old_ni, new_ni;
 	struct page *ipage;
+	int err;
 
 	ipage = grab_cache_page(NODE_MAPPING(sbi), ino);
 	if (!ipage)
@@ -1562,11 +1577,14 @@ int recover_inode_page(struct f2fs_sb_info *sbi, struct page *page)
 	new_ni.ino = ino;
 
 	if (unlikely(!inc_valid_node_count(sbi, NULL)))
-		WARN_ON(1);
-	set_node_addr(sbi, &new_ni, NEW_ADDR);
-	inc_valid_inode_count(sbi);
+
+		err = -ENOSPC;
+	else {
+		set_node_addr(sbi, &new_ni, NEW_ADDR);
+		inc_valid_inode_count(sbi);
+	}
 	f2fs_put_page(ipage, 1);
-	return 0;
+	return err;
 }
 
 /*
