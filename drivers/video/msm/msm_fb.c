79c042d0e0b2e43100443167347327bf73386929
Naseer Ahmed <naseer@codeaurora.org>
2013-05-17 18:44:40 -0400

    msm: mdp: Increase base fence timeout
    The earlier value of 1 second caused unnecessary warnings on
    heavy workloads by the GPU.
    
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -126,7 +126,7 @@ static int msm_fb_pan_idle(struct msm_fb_data_type *mfd);
 #define MSM_FB_MAX_DBGFS 1024
 #define MAX_BACKLIGHT_BRIGHTNESS 255
 
-#define WAIT_FENCE_FIRST_TIMEOUT MSEC_PER_SEC
+#define WAIT_FENCE_FIRST_TIMEOUT 3 * MSEC_PER_SEC
 #define WAIT_FENCE_FINAL_TIMEOUT 10 * MSEC_PER_SEC
 /* Display op timeout should be greater than total timeout */
 #define WAIT_DISP_OP_TIMEOUT (WAIT_FENCE_FIRST_TIMEOUT +\
