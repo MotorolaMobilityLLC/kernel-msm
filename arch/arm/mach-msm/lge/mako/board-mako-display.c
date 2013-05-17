587b9270c47bb7e89be9c314f03003965b3e24c4
Naseer Ahmed <naseer@codeaurora.org>
2013-05-17 17:20:36 -0400

    msm: mako: add customized mdp_max_bw
    Some HDMI use cases (notably 1080p video) need extra bandwidth.
    
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/lge/mako/board-mako-display.c
+++ arch/arm/mach-msm/lge/mako/board-mako-display.c
@@ -247,6 +247,9 @@ static struct msm_bus_scale_pdata mdp_bus_scale_pdata = {
 static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
 	.mdp_max_clk = 266667000,
+	.mdp_max_bw = 3000000000u,
+	.mdp_bw_ab_factor = 115,
+	.mdp_bw_ib_factor = 125,
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 	.mdp_rev = MDP_REV_44,
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
