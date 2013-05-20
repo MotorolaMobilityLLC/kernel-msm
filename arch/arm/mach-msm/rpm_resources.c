a4eeb93dc0cba8ca1c067525993a7ed16ddc5b94
Venkat Devarasetty <vdevaras@codeaurora.org>
2013-05-20 12:22:12 +0530

    msm: rpm: add debug msg for rpm outstanding req
    added a debug info for showing the power collapse
    abort due to rpm outstanding request.
    
    Change-Id: Ifbc8054033b3cc86ccebc625b501ddb8b2670707
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- arch/arm/mach-msm/rpm_resources.c
+++ arch/arm/mach-msm/rpm_resources.c
@@ -947,9 +947,11 @@ static void *msm_rpmrs_lowest_limits(bool from_idle,
 
 		if ((MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE == sleep_mode)
 			|| (MSM_PM_SLEEP_MODE_POWER_COLLAPSE == sleep_mode))
-			if (!cpu && msm_rpm_local_request_is_outstanding())
-					break;
-
+			if (!cpu && msm_rpm_local_request_is_outstanding()) {
+				if (MSM_RPMRS_DEBUG_OUTPUT & msm_rpmrs_debug_mask)
+					pr_info(" RPM Request is outstanding\n");
+				break;
+			}
 		if (next_wakeup_us <= 1) {
 			pwr = level->energy_overhead;
 		} else if (next_wakeup_us <= level->time_overhead_us) {
