1c7617962efdc6a7b402a488520bfbcc57348494
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2013-05-20 13:46:24 +0100

    mfd: arizona: Change fast_start pdata name to better reflect functionality
    The bit in the register enables MICBIAS fast startup when clear not when
    set. This patch changes the name of this pdata option to soft_start to
    better match the functionality. We rename rather than invert the
    handling to keep the same default functionality, which is fast start
    active.
    
    Change-Id: I50051441d77259cb48803fee615dda246eaad92e
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -817,7 +817,7 @@ int __devinit arizona_dev_init(struct arizona *arizona)
 		if (arizona->pdata.micbias[i].discharge)
 			val |= ARIZONA_MICB1_DISCH;
 
-		if (arizona->pdata.micbias[i].fast_start)
+		if (arizona->pdata.micbias[i].soft_start)
 			val |= ARIZONA_MICB1_RATE;
 
 		if (arizona->pdata.micbias[i].bypass)
