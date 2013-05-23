f1fa739d7728f27a188c6edecf2a3019ea6cf3ff
Carter Cooper <ccooper@codeaurora.org>
2013-05-23 08:42:23 -0600

    msm: kgsl: Support generic GPU trace events
    If available support two generic GPU trace events:
    
    	events/gpu/gpu_sched_switch
    	events/gpu/gpu_job_enqueue
    
    This will allow generic tools to get a better idea of when
    commands are queued and scheduled.
    
    Change-Id: Ic0dedbad5fa6c5fbb34aebdcf82fd10ee92da8d7
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -974,6 +974,9 @@ adreno_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 			return -EINVAL;
 	}
 
+	/* For now everybody has the same priority */
+	cmdbatch->priority = ADRENO_CONTEXT_DEFAULT_PRIORITY;
+
 	/* Queue the command in the ringbuffer */
 	ret = adreno_context_queue_cmd(adreno_dev, drawctxt, cmdbatch,
 		timestamp);
@@ -981,6 +984,22 @@ adreno_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 	if (ret)
 		KGSL_DRV_ERR(device, "adreno_context_queue_cmd returned %d\n",
 				ret);
+	else {
+		/*
+		 * only call trace_gpu_job_enqueue for actual commands - dummy
+		 * sync command batches won't get scheduled on the GPU
+		 */
+
+		if (!(cmdbatch->flags & KGSL_CONTEXT_SYNC)) {
+			const char *str = "3D";
+			if (drawctxt->type == KGSL_CONTEXT_TYPE_CL ||
+				drawctxt->type == KGSL_CONTEXT_TYPE_RS)
+				str = "compute";
+
+			kgsl_trace_gpu_job_enqueue(drawctxt->base.id,
+				cmdbatch->timestamp, str);
+		}
+	}
 
 	return ret;
 }
