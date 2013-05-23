3ee8c8f8a5997492921ddf6daa3e4c0a6fcf8623
Sridhar Vashist <a21300@motorola.com>
2013-05-23 13:24:13 -0500

    input: lis3dh_mot: Add LIS3DH_MOT accelerometer driver.
    Baseline lis3dh_mot accelerometer driver from jbrel2-intel
    
    Change-Id: I1f23cd04526c0341e204b9f0ee0a562eb54d4778
    
    Signed-off-by: Sridhar Vashist <a21300@motorola.com>
    Change-Id: Idccb15501466f664cabf4827b52ce710aa6e85b7
    Reviewed-on: http://gerrit.pcs.mot.com/542151
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/linux/lis3dh_mot.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2008-2010, Motorola, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __LIS3DH_H__
+#define __LIS3DH_H__
+
+#include <linux/ioctl.h>
+
+#define LIS3DH_IOCTL_BASE		71
+/* The following define the IOCTL command values via the ioctl macros */
+#define LIS3DH_IOCTL_SET_DELAY		_IOW(LIS3DH_IOCTL_BASE, 0, int)
+#define LIS3DH_IOCTL_GET_DELAY		_IOR(LIS3DH_IOCTL_BASE, 1, int)
+#define LIS3DH_IOCTL_SET_ENABLE		_IOW(LIS3DH_IOCTL_BASE, 2, int)
+#define LIS3DH_IOCTL_GET_ENABLE		_IOR(LIS3DH_IOCTL_BASE, 3, int)
+
+#ifdef __KERNEL__
+struct lis3dh_platform_data {
+	int poll_interval;
+	int min_interval;
+	int g_range;
+
+	int (*init)(void);
+	void (*exit)(void);
+	int (*power_on)(void);
+	int (*power_off)(void);
+
+};
+#endif /* __KERNEL__ */
+
+#endif  /* __LIS3DH_H__ */
+
