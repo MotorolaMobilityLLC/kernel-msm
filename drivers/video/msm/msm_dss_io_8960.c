8405cbcb55428f8cddc56010fb3a5c930622db40
Sangani Suryanarayana Raju <csuryan@codeaurora.org>
2013-05-27 20:26:55 +0530

    msm: display: Proper handling of prepare and unprepare clocks
    This change handles prepare and unprepare clocks properly to
    fix clock mismatches.
    
    Change-Id: I09986aa1f0e1a5be2e07655a1db0d23760dd0725
    Signed-off-by: Sangani Suryanarayana Raju <csuryan@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -649,20 +649,24 @@ void cont_splash_clk_ctrl(int enable)
 	}
 }
 
-void mipi_dsi_prepare_clocks(void)
+void mipi_dsi_prepare_ahb_clocks(void)
 {
 	clk_prepare(amp_pclk);
 	clk_prepare(dsi_m_pclk);
 	clk_prepare(dsi_s_pclk);
 }
 
+void mipi_dsi_unprepare_ahb_clocks(void)
+{
+	clk_unprepare(dsi_m_pclk);
+	clk_unprepare(dsi_s_pclk);
+	clk_unprepare(amp_pclk);
+}
+
 void mipi_dsi_unprepare_clocks(void)
 {
 	clk_unprepare(dsi_esc_clk);
 	clk_unprepare(dsi_byte_div_clk);
-	clk_unprepare(dsi_m_pclk);
-	clk_unprepare(dsi_s_pclk);
-	clk_unprepare(amp_pclk);
 }
 
 void mipi_dsi_ahb_ctrl(u32 enable)
