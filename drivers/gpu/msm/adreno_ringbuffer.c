02aa37e588687d3bcde9b82d636b467b4d920867
Jordan Crouse <jcrouse@codeaurora.org>
2013-05-28 17:08:17 -0600

    msm: kgsl: Export some kgsl-core functions to EXPORT_SYMBOLS
    Export some functions in the KGSL core driver so they can
    be seen by the leaf drivers.
    
    Change-Id: Ic0dedbad5dbe562c2e674f8e885a3525b6feac7b
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -18,7 +18,6 @@
 #include "kgsl.h"
 #include "kgsl_sharedmem.h"
 #include "kgsl_cffdump.h"
-#include "kgsl_trace.h"
 
 #include "adreno.h"
 #include "adreno_pm4types.h"
@@ -1097,7 +1096,7 @@ adreno_ringbuffer_issueibcmds(struct kgsl_device_private *dev_priv,
 	}
 
 done:
-	trace_kgsl_issueibcmds(device, context->id, ibdesc, numibs,
+	kgsl_trace_issueibcmds(device, context->id, ibdesc, numibs,
 		*timestamp, flags, ret, drawctxt->type);
 
 	kfree(link);
