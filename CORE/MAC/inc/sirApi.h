35dc7db48b5283d1312c9604ad333f777f5dba1e
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-05-29 21:37:47 +0530

    WLAN:Disable 11d when countryCode is received from UserSpace
    Country code from userspace will be prioritizedi(11d Disabled)
    only when actual
    country code received from UserSpace.
    
    Change-Id: Ia60926e7b92e5dba8090357e7dd91c000f84941a
    CRs-Fixed: 493319

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -2118,6 +2118,7 @@ typedef struct sAniChangeCountryCodeReq
     tANI_U16                msgType;    // message type is same as the request type
     tANI_U16                msgLen;     // length of the entire request
     tANI_U8                 countryCode[WNI_CFG_COUNTRY_CODE_LEN];   //3 char country code
+    tAniBool                countryFromUserSpace;
     void                    *changeCCCallback;
     void                    *pDevContext; //device context
     void                    *pVosContext; //voss context
