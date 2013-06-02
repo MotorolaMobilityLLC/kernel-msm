eded9ff08c5d461b1ebd74bab6e35a0f2d091bf5
Yuanyuan Zhong <a6510c@motorola.com>
2013-06-02 09:41:32 -0500

    arm: msm: watchdogv2: print TZ wdog dump
    This change is ported from:
    (CR) arm: msm: watchdog: print info saved by TZ after
    dogbark
    
    We have watchdog configured to generate secure FIQ. When TZ
    receives FIQ for watchdog bark, it saves cpu context to a
    buffer provided by Linux.
    This change lets Linux try to dump the info saved by TZ into
    last_kmsg.
    Signed-off-by: Yuanyuan Zhong <a6510c@motorola.com>
    
    Change-Id: Ibbd862a5e520bfc70698735df52d1f37d5287fd4
    Reviewed-on: http://gerrit.pcs.mot.com/543111
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/msm_watchdog_v2.c
+++ arch/arm/mach-msm/msm_watchdog_v2.c
@@ -27,6 +27,14 @@
 #include <linux/platform_device.h>
 #include <mach/scm.h>
 #include <mach/msm_memory_dump.h>
+#ifdef CONFIG_MSM_WATCHDOG_CTX_PRINT
+#include <linux/mm.h>
+#include <linux/persistent_ram.h>
+#include <asm/bootinfo.h>
+#include <asm/stacktrace.h>
+#include <asm/traps.h>
+#include <mach/socinfo.h>
+#endif
 
 #define MODULE_NAME "msm_watchdog"
 #define WDT0_ACCSCSSNBARK_INT 0
@@ -56,7 +64,8 @@ struct msm_watchdog_data {
 	unsigned long long last_pet;
 	unsigned min_slack_ticks;
 	unsigned long long min_slack_ns;
-	void *scm_regsave;
+	unsigned long scm_regsave;		/* phys */
+	unsigned long scm_regsave_size;
 	cpumask_t alive_mask;
 	struct mutex disable_lock;
 	struct work_struct init_dogwork_struct;
@@ -356,6 +365,561 @@ static irqreturn_t wdog_ppi_bark(int irq, void *dev_id)
 	return wdog_bark_handler(irq, wdog_dd);
 }
 
+#ifdef CONFIG_MSM_WATCHDOG_CTX_PRINT
+
+#define TZBSP_SC_STATUS_NS_BIT		0x01
+#define TZBSP_SC_STATUS_WDT		0x02
+#define TZBSP_SC_STATUS_SGI		0x04
+#define TZBSP_SC_STATUS_WARM_BOOT	0x08
+
+#define TZBSP_DUMP_CTX_MAGIC		0x44434151
+#define TZBSP_DUMP_CTX_VERSION		2
+
+#define MSM_WDT_CTX_SIG			0x77647473
+#define MSM_WDT_CTX_REV			0x00010003
+
+#define NR_CPUS_MAX			4
+#define NR_CPUS_2			2
+#define NR_CPUS_4			4
+
+#define RET_STAGE_SHIFT			(28)
+#define RET_SECTION_SHIFT		(24)
+#define RET_ERR_SHIFT			(16)
+#define RET_MISC_SHIFT			(12)
+
+#define RET_TZ				(0xfu << RET_STAGE_SHIFT)
+#define RET_SBL2			(0xeu << RET_STAGE_SHIFT)
+
+#define ERR_NONE			(0xffu << RET_ERR_SHIFT)
+
+struct tzbsp_mon_cpu_ctx_s {
+	u32 mon_lr;
+	u32 mon_spsr;
+	u32 usr_r0;
+	u32 usr_r1;
+	u32 usr_r2;
+	u32 usr_r3;
+	u32 usr_r4;
+	u32 usr_r5;
+	u32 usr_r6;
+	u32 usr_r7;
+	u32 usr_r8;
+	u32 usr_r9;
+	u32 usr_r10;
+	u32 usr_r11;
+	u32 usr_r12;
+	u32 usr_r13;
+	u32 usr_r14;
+	u32 irq_spsr;
+	u32 irq_r13;
+	u32 irq_r14;
+	u32 svc_spsr;
+	u32 svc_r13;
+	u32 svc_r14;
+	u32 abt_spsr;
+	u32 abt_r13;
+	u32 abt_r14;
+	u32 und_spsr;
+	u32 und_r13;
+	u32 und_r14;
+	u32 fiq_spsr;
+	u32 fiq_r8;
+	u32 fiq_r9;
+	u32 fiq_r10;
+	u32 fiq_r11;
+	u32 fiq_r12;
+	u32 fiq_r13;
+	u32 fiq_r14;
+};
+
+struct tzbsp_cpu_ctx_s {
+	struct tzbsp_mon_cpu_ctx_s saved_ctx;
+	u32 mon_sp;
+	u32 wdog_pc;
+};
+
+/* Structure of the entire non-secure context dump buffer. Because TZ is single
+ * entry only a single secure context is saved. */
+struct tzbsp_dump_buf_s0 {
+	u32 magic;
+	u32 version;
+	u32 cpu_count;
+};
+
+struct tzbsp_dump_buf_s2 {
+	u32 magic;
+	u32 version;
+	u32 cpu_count;
+	u32 sc_status[NR_CPUS_2];
+	struct tzbsp_cpu_ctx_s sc_ns[NR_CPUS_2];
+	struct tzbsp_mon_cpu_ctx_s sec;
+	u32 wdt_sts[NR_CPUS_2];
+};
+
+struct tzbsp_dump_buf_s4 {
+	u32 magic;
+	u32 version;
+	u32 cpu_count;
+	u32 sc_status[NR_CPUS_4];
+	struct tzbsp_cpu_ctx_s sc_ns[NR_CPUS_4];
+	struct tzbsp_mon_cpu_ctx_s sec;
+	u32 wdt_sts[NR_CPUS_4];
+};
+
+union tzbsp_dump_buf_s {
+	struct tzbsp_dump_buf_s0 s0;
+	struct tzbsp_dump_buf_s2 s2;
+	struct tzbsp_dump_buf_s4 s4;
+};
+
+struct msm_wdt_lnx_info {
+	u32 tsk_size;
+	u32 ti_tsk_offset;
+} __packed __aligned(4);
+
+struct msm_wdt_ctx_info {
+	struct msm_wdt_lnx_info lnx;
+	u32 size_tz;
+	u32 rev_tz;
+	u32 size;
+	u32 cpu_count;
+	u32 ret;
+	u32 rev;
+	u32 sig;
+} __packed __aligned(4);
+
+struct msm_wdt_page1 {
+	union tzbsp_dump_buf_s tzbsp;
+	u8 spare[PAGE_SIZE - sizeof(union tzbsp_dump_buf_s)
+			- sizeof(struct msm_wdt_ctx_info)];
+	struct msm_wdt_ctx_info cinfo;
+} __packed __aligned(PAGE_SIZE);
+
+struct msm_wdt_cpy {
+	u32 from;
+	u32 to;
+	u32 size;
+} __packed __aligned(4);
+
+#define LNX_STACK	0
+#define LNX_TASK	1
+#define LNX_CTX_AREAS	2
+
+struct msm_wdt_pcp {
+	u32 ret;
+	u32 stack_va;
+	struct msm_wdt_cpy jobs[LNX_CTX_AREAS];
+} __packed __aligned(4);
+
+struct msm_wdt_ctx {
+	struct msm_wdt_page1 p1;
+} __packed __aligned(PAGE_SIZE);
+
+/*
+ *	---------	---------------------
+ *	|	|	| tzbsp_dump_buf_s
+ *	| page1 |	| padding
+ *	|	|	| msm_wdt_ctx_info
+ *	---------	---------------------
+ *	| page2 |	| msm_wdt_pcp[0], ...
+ *	| ...	|	| task_struct[0], ...
+ *	| pagex |	| padding
+ *	---------	---------------------
+ *	| ...	|	| stack[0], ...
+ *	|	|	|
+ */
+
+#define MSM_WDT_CTX_PCP_SIZE	(sizeof(struct msm_wdt_pcp) * get_core_count())
+#define MSM_WDT_CTX_TSK_SIZE	(sizeof(struct task_struct) * get_core_count())
+
+#define MSM_WDT_CTX_MID_SIZE	(ALIGN((MSM_WDT_CTX_PCP_SIZE + \
+					MSM_WDT_CTX_TSK_SIZE), PAGE_SIZE))
+#define MSM_WDT_CTX_THREAD_SIZE	(THREAD_SIZE * get_core_count())
+#define MSM_WDT_CTX_SIZE	(sizeof(struct msm_wdt_ctx)	\
+				+ MSM_WDT_CTX_MID_SIZE \
+				+ MSM_WDT_CTX_THREAD_SIZE)
+
+#define MSMWDTD(fmt, args...) do {			\
+	persistent_ram_ext_oldbuf_print(fmt, ##args);	\
+} while (0)
+
+static void msm_wdt_show_status(u32 sc_status, const char *label)
+{
+	if (!sc_status) {
+		MSMWDTD("%s: probably didn't finish dump.\n", label);
+		return;
+	}
+	MSMWDTD("%s: was in %ssecure world.\n", label,
+		(sc_status & TZBSP_SC_STATUS_NS_BIT) ? "non-" : "");
+	if (sc_status & TZBSP_SC_STATUS_WDT)
+		MSMWDTD("%s: experienced a watchdog timeout.\n", label);
+	if (sc_status & TZBSP_SC_STATUS_SGI)
+		MSMWDTD("%s: some other core experienced a watchdog timeout.\n",
+			label);
+	if (sc_status & TZBSP_SC_STATUS_WARM_BOOT)
+		MSMWDTD("%s: WDT bark occured during TZ warm boot.\n", label);
+}
+
+static const char stat_nam[] = TASK_STATE_TO_CHAR_STR;
+static void msm_wdt_show_task(struct task_struct *p,
+			struct thread_info *ti)
+{
+	unsigned state;
+
+	state = p->state ? __ffs(p->state) + 1 : 0;
+	MSMWDTD("%-15.15s %c", p->comm,
+		state < sizeof(stat_nam) - 1 ? stat_nam[state] : '?');
+	if (state == TASK_RUNNING)
+		MSMWDTD(" running  ");
+	else
+		MSMWDTD(" %08x ", ti->cpu_context.pc);
+	MSMWDTD("pid %6d tgid %6d 0x%08lx\n", task_pid_nr(p), task_tgid_nr(p),
+			(unsigned long)ti->flags);
+}
+
+static void msm_wdt_show_regs(struct tzbsp_mon_cpu_ctx_s *regs,
+				const char *label)
+{
+	MSMWDTD("%s\n", label);
+	MSMWDTD("\tr12: %08x  r11: %08x  r10: %08x  r9 : %08x  r8 : %08x\n",
+			regs->usr_r12, regs->usr_r11, regs->usr_r10,
+			regs->usr_r9, regs->usr_r8);
+	MSMWDTD("\tr7 : %08x  r6 : %08x  r5 : %08x  r4 : %08x\n",
+			regs->usr_r7, regs->usr_r6, regs->usr_r5,
+			regs->usr_r4);
+	MSMWDTD("\tr3 : %08x  r2 : %08x  r1 : %08x  r0 : %08x\n",
+			regs->usr_r3, regs->usr_r2, regs->usr_r1,
+			regs->usr_r0);
+	MSMWDTD("MON:\tlr : %08x  spsr: %08x\n",
+			regs->mon_lr, regs->mon_spsr);
+	MSMWDTD("SVC:\tlr : %08x  sp : %08x  spsr : %08x\n",
+			regs->svc_r14, regs->svc_r13, regs->svc_spsr);
+	MSMWDTD("IRQ:\tlr : %08x  sp : %08x  spsr : %08x\n",
+			regs->irq_r14, regs->irq_r13, regs->irq_spsr);
+	MSMWDTD("ABT:\tlr : %08x  sp : %08x  spsr : %08x\n",
+			regs->abt_r14, regs->abt_r13, regs->abt_spsr);
+	MSMWDTD("UND:\tlr : %08x  sp : %08x  spsr : %08x\n",
+			regs->und_r14, regs->und_r13, regs->und_spsr);
+	MSMWDTD("FIQ:\tlr : %08x  sp : %08x  spsr : %08x\n",
+			regs->fiq_r14, regs->fiq_r13, regs->fiq_spsr);
+	MSMWDTD("\tr12: %08x  r11: %08x  r10: %08x  r9 : %08x  r8 : %08x\n",
+			regs->fiq_r12, regs->fiq_r11, regs->fiq_r10,
+			regs->fiq_r9, regs->fiq_r8);
+	MSMWDTD("USR:\tlr : %08x  sp : %08x\n",
+			regs->usr_r14, regs->usr_r13);
+}
+
+static void msm_wdt_show_raw_mem(unsigned long addr, int nbytes,
+			unsigned long old_addr, const char *label)
+{
+	int	i, j;
+	int	nlines;
+	u32	*p;
+
+	MSMWDTD("%s: %#lx: ", label, old_addr);
+	if (!virt_addr_valid(old_addr)) {
+		MSMWDTD("is not valid kernel address.\n");
+		return;
+	}
+	MSMWDTD("\n");
+
+	/*
+	 * round address down to a 32 bit boundary
+	 * and always dump a multiple of 32 bytes
+	 */
+	p = (u32 *)(addr & ~(sizeof(u32) - 1));
+	nbytes += (addr & (sizeof(u32) - 1));
+	nlines = (nbytes + 31) / 32;
+
+
+	for (i = 0; i < nlines; i++) {
+		/*
+		 * just display low 16 bits of address to keep
+		 * each line of the dump < 80 characters
+		 */
+		MSMWDTD("%04lx ", (unsigned long)old_addr & 0xffff);
+		for (j = 0; j < 8; j++) {
+			MSMWDTD(" %08x", *p);
+			++p;
+			old_addr += sizeof(*p);
+		}
+		MSMWDTD("\n");
+	}
+}
+
+static void msm_wdt_unwind_backtrace(struct tzbsp_mon_cpu_ctx_s *regs,
+			unsigned long addr, unsigned long stack)
+{
+	struct stackframe frame;
+	int offset;
+
+	if (!virt_addr_valid(addr)) {
+		MSMWDTD("%08lx is not valid kernel address.\n", addr);
+		return;
+	}
+	if ((regs->svc_r13 & ~(THREAD_SIZE - 1)) == addr) {
+		frame.fp = (regs->usr_r11 & (THREAD_SIZE - 1)) + stack;
+		frame.sp = (regs->svc_r13 & (THREAD_SIZE - 1)) + stack;
+		frame.lr = regs->svc_r14;
+		frame.pc = regs->mon_lr;
+	} else {
+		struct thread_info *ti = (struct thread_info *)stack;
+		frame.fp = ti->cpu_context.fp - addr + stack;
+		frame.sp = ti->cpu_context.sp - addr + stack;
+		frame.lr = 0;
+		frame.pc = ti->cpu_context.pc;
+	}
+	offset = (frame.sp - stack - 128) & ~(128 - 1);
+	msm_wdt_show_raw_mem(stack, 96, addr, "thread_info");
+	msm_wdt_show_raw_mem(stack + offset, THREAD_SIZE - offset,
+			addr + offset, "stack");
+	while (1) {
+		int urc;
+		unsigned long where = frame.pc;
+
+		urc = unwind_frame(&frame);
+		if (urc < 0)
+			break;
+		MSMWDTD("[<%08lx>] (%pS) from [<%08lx>] (%pS)\n", where,
+			(void *)where, frame.pc, (void *)frame.pc);
+		if (in_exception_text(where)) {
+			struct pt_regs *excep_regs =
+					(struct pt_regs *)(frame.sp);
+			if ((excep_regs->ARM_sp & ~(THREAD_SIZE - 1)) == addr) {
+				excep_regs->ARM_sp -= addr;
+				excep_regs->ARM_sp += stack;
+			}
+		}
+	}
+}
+
+static void msm_wdt_ctx_print(struct msm_wdt_ctx *ctx)
+{
+	int i;
+	unsigned long stack_tmp = 0, orr = 0;
+	u32 *sc_status, *wdt_sts;
+	struct tzbsp_cpu_ctx_s *sc_ns;
+	char label[64];
+	u8 (*stack)[THREAD_SIZE];
+	const int cpu_count = get_core_count();
+	struct msm_wdt_pcp *pcp;
+	struct task_struct *task;
+
+	switch (cpu_count) {
+	case NR_CPUS_2:
+		sc_status = ctx->p1.tzbsp.s2.sc_status;
+		sc_ns = ctx->p1.tzbsp.s2.sc_ns;
+		wdt_sts = ctx->p1.tzbsp.s2.wdt_sts;
+		break;
+	case NR_CPUS_4:
+		sc_status = ctx->p1.tzbsp.s4.sc_status;
+		sc_ns = ctx->p1.tzbsp.s4.sc_ns;
+		wdt_sts = ctx->p1.tzbsp.s4.wdt_sts;
+		break;
+	default:
+		MSMWDTD("msm_wdt_ctx: unsupported cpu_count %d\n", cpu_count);
+		return;
+	}
+	for (i = 0; i < cpu_count; i++) {
+		orr |= wdt_sts[i];
+		orr |= sc_ns[i].saved_ctx.mon_lr;
+	}
+	if (!orr) {
+		u32 *tzctx;
+		if (bi_powerup_reason() != PU_REASON_WDOG_AP_RESET)
+			return;
+		for (tzctx = sc_status; tzctx < (u32 *)ctx->p1.spare; tzctx++) {
+			orr |= *tzctx;
+			if (orr)
+				break;
+		}
+		if (!orr) {
+			MSMWDTD("\n*** Might be Watchdog Bite ***\n");
+			return;
+		} else {
+			MSMWDTD("*** Likely Bad Dump ***\n");
+		}
+	}
+	if ((ctx->p1.tzbsp.s0.magic != TZBSP_DUMP_CTX_MAGIC)
+		|| (ctx->p1.tzbsp.s0.version != TZBSP_DUMP_CTX_VERSION)
+		|| (ctx->p1.tzbsp.s0.cpu_count != cpu_count)) {
+		MSMWDTD("msm_wdt_ctx: tzbsp dump buffer mismatch.\n");
+		MSMWDTD("Expected: magic 0x%08x, version %d, cpu_count %d\n",
+			TZBSP_DUMP_CTX_MAGIC, TZBSP_DUMP_CTX_VERSION,
+			cpu_count);
+		MSMWDTD("Found:    magic 0x%08x, version %d, cpu_count %d\n",
+			ctx->p1.tzbsp.s0.magic, ctx->p1.tzbsp.s0.version,
+			ctx->p1.tzbsp.s0.cpu_count);
+		return;
+	}
+	MSMWDTD("sc_status: ");
+	for (i = 0; i < cpu_count; i++)
+		MSMWDTD("0x%x ", sc_status[i]);
+	MSMWDTD("\n");
+	for (i = 0; i < cpu_count; i++) {
+		snprintf(label, sizeof(label) - 1, "CPU%d", i);
+		msm_wdt_show_status(sc_status[i], label);
+	}
+	MSMWDTD("wdt_sts: ");
+	for (i = 0; i < cpu_count; i++)
+		MSMWDTD("0x%08x ", wdt_sts[i]);
+	MSMWDTD("\n");
+	for (i = 0; i < cpu_count; i++) {
+		snprintf(label, sizeof(label) - 1, "CPU%d nsec", i);
+		msm_wdt_show_regs(&sc_ns[i].saved_ctx, label);
+	}
+	msm_wdt_show_regs(&sc_ns[cpu_count].saved_ctx, "sec");
+	if ((ctx->p1.cinfo.sig != MSM_WDT_CTX_SIG)
+		|| (ctx->p1.cinfo.rev_tz != MSM_WDT_CTX_REV)
+		|| (ctx->p1.cinfo.size_tz != MSM_WDT_CTX_SIZE)) {
+		MSMWDTD("msm_wdt_ctx: linux dump buffer mismatch.\n");
+		MSMWDTD("Expected: sig 0x%08x, ver 0x%08x size 0x%08x\n",
+			MSM_WDT_CTX_SIG, MSM_WDT_CTX_REV, MSM_WDT_CTX_SIZE);
+		MSMWDTD("Found:    sig 0x%08x, ver 0x%08x size 0x%08x\n",
+			ctx->p1.cinfo.sig, ctx->p1.cinfo.rev_tz,
+			ctx->p1.cinfo.size_tz);
+		return;
+	}
+	/* now dump customized stuff */
+	MSMWDTD("\n");
+	MSMWDTD("ret 0x%08x\n", ctx->p1.cinfo.ret);
+	MSMWDTD("\n");
+	pcp = (struct msm_wdt_pcp *)(ctx + 1);
+	for (i = 0; i < cpu_count; i++) {
+		struct msm_wdt_cpy *job;
+		MSMWDTD("CPU%d: ", i);
+		MSMWDTD("ret 0x%x ", pcp[i].ret);
+		MSMWDTD("stack %08x ", pcp[i].stack_va);
+		job = &pcp[i].jobs[LNX_TASK];
+		MSMWDTD("%08x -> %08x (%x) ", job->from, job->to, job->size);
+		job = &pcp[i].jobs[LNX_STACK];
+		MSMWDTD("%08x -> %08x (%x)", job->from, job->to, job->size);
+		MSMWDTD("\n");
+	}
+	if ((ctx->p1.cinfo.ret != (RET_TZ | ERR_NONE)))
+		return;
+	MSMWDTD("\n");
+	task = (struct task_struct *)(pcp + cpu_count);
+	stack = (u8 (*)[THREAD_SIZE])(ALIGN((unsigned long)(task + cpu_count),
+					PAGE_SIZE));
+	if (!IS_ALIGNED((unsigned long)stack, THREAD_SIZE)) {
+		stack_tmp = __get_free_pages(GFP_KERNEL, THREAD_SIZE_ORDER);
+		if (!stack_tmp)
+			pr_err("Allocating temporary storage for stack failed.\n");
+	}
+	for (i = 0; i < cpu_count; i++) {
+		unsigned long addr, data;
+
+		if (pcp[i].ret != (RET_TZ | ERR_NONE))
+			continue;
+		MSMWDTD("CPU%d: ", i);
+		addr = pcp[i].stack_va;
+		if (stack_tmp) {
+			memcpy((void *)stack_tmp, stack[i], THREAD_SIZE);
+			data = stack_tmp;
+		} else {
+			data = (unsigned long)stack[i];
+		}
+		msm_wdt_show_task(&task[i], (struct thread_info *)data);
+		msm_wdt_unwind_backtrace(&sc_ns[i].saved_ctx, addr, data);
+		MSMWDTD("\n");
+	}
+	if (stack_tmp)
+		free_pages(stack_tmp, THREAD_SIZE_ORDER);
+}
+
+static void msm_watchdog_ctx_lnx(struct msm_wdt_lnx_info *lnx)
+{
+	lnx->tsk_size = sizeof(struct task_struct);
+	lnx->ti_tsk_offset = offsetof(struct thread_info, task);
+}
+
+static void msm_wdt_ctx_reset(struct msm_wdt_ctx *ctx)
+{
+	memset(ctx, 0, MSM_WDT_CTX_SIZE);
+	ctx->p1.cinfo.sig = MSM_WDT_CTX_SIG;
+	ctx->p1.cinfo.rev = MSM_WDT_CTX_REV;
+	ctx->p1.cinfo.cpu_count = get_core_count();
+	ctx->p1.cinfo.size = MSM_WDT_CTX_SIZE;
+	msm_watchdog_ctx_lnx(&ctx->p1.cinfo.lnx);
+}
+
+static void msm_watchdog_alloc_buf(struct msm_watchdog_data *wdog_dd)
+{
+	struct platform_device *pdev = to_platform_device(wdog_dd->dev);
+	struct device_node *node = pdev->dev.of_node;
+	int ret, size;
+	unsigned long phys;
+	struct msm_wdt_ctx *ctx;
+
+	BUILD_BUG_ON((sizeof(union tzbsp_dump_buf_s)
+		+ sizeof(struct msm_wdt_ctx_info)) > PAGE_SIZE);
+	BUILD_BUG_ON(CONFIG_NR_CPUS > NR_CPUS_MAX);
+
+	ret = of_property_read_u32(node, "qcom,memory-reservation-size", &size);
+
+	if (ret < 0) {
+		dev_err(wdog_dd->dev, "reservation not found.\n");
+		goto no_reservation;
+	}
+	if (size < MSM_WDT_CTX_SIZE) {
+		dev_err(wdog_dd->dev, "reserved buf too small: 0x%X < 0x%X\n",
+			size, MSM_WDT_CTX_SIZE);
+		goto no_reservation;
+	}
+
+	/* Memory with size specified in device tree has been
+	 * reserved together by mdesc->reserve().
+	 */
+	phys = allocate_contiguous_ebi_nomap(MSM_WDT_CTX_SIZE, PAGE_SIZE);
+	if (!phys) {
+		dev_err(wdog_dd->dev, "failed to alloc from mempool\n");
+		goto no_reservation;
+	}
+	wdog_dd->scm_regsave = phys;
+	wdog_dd->scm_regsave_size = MSM_WDT_CTX_SIZE;
+
+	dev_dbg(wdog_dd->dev, "buf 0x%08lX size 0x%X (reserved 0x%X)\n",
+			phys, MSM_WDT_CTX_SIZE, size);
+	ctx = ioremap(phys, MSM_WDT_CTX_SIZE);
+	if (!ctx) {
+		dev_err(wdog_dd->dev, "cannot remap buffer: %08lX\n", phys);
+		return;
+	}
+	msm_wdt_ctx_print(ctx);
+	msm_wdt_ctx_reset(ctx);
+	iounmap(ctx);
+	return;
+
+no_reservation:
+	ctx = (struct msm_wdt_ctx *)__get_free_page(GFP_KERNEL);
+	if (ctx) {
+		wdog_dd->scm_regsave = virt_to_phys(ctx);
+		wdog_dd->scm_regsave_size = PAGE_SIZE;
+	} else {
+		wdog_dd->scm_regsave = 0;
+		wdog_dd->scm_regsave_size = 0;
+	}
+	return;
+}
+
+#else /* CONFIG_MSM_WATCHDOG_CTX_PRINT undefined below */
+
+#define MSM_WDT_CTX_SIZE	PAGE_SIZE
+
+static void msm_watchdog_alloc_buf(struct msm_watchdog_data *wdog_dd)
+{
+	void *ctx_va = (void *)__get_free_page(GFP_KERNEL);
+	if (ctx_va) {
+		wdog_dd->scm_regsave = virt_to_phys(ctx_va);
+		wdog_dd->scm_regsave_size = PAGE_SIZE;
+	} else {
+		wdog_dd->scm_regsave = 0;
+		wdog_dd->scm_regsave_size = 0;
+	}
+}
+#endif /* CONFIG_MSM_WATCHDOG_CTX_PRINT */
+
 static void configure_bark_dump(struct msm_watchdog_data *wdog_dd)
 {
 	int ret;
@@ -365,10 +929,10 @@ static void configure_bark_dump(struct msm_watchdog_data *wdog_dd)
 		int len;
 	} cmd_buf;
 
-	wdog_dd->scm_regsave = (void *)__get_free_page(GFP_KERNEL);
+	msm_watchdog_alloc_buf(wdog_dd);
 	if (wdog_dd->scm_regsave) {
-		cmd_buf.addr = virt_to_phys(wdog_dd->scm_regsave);
-		cmd_buf.len  = PAGE_SIZE;
+		cmd_buf.addr = wdog_dd->scm_regsave;
+		cmd_buf.len  = wdog_dd->scm_regsave_size;
 		ret = scm_call(SCM_SVC_UTIL, SCM_SET_REGSAVE_CMD,
 					&cmd_buf, sizeof(cmd_buf), NULL, 0);
 		if (ret)
@@ -376,8 +940,9 @@ static void configure_bark_dump(struct msm_watchdog_data *wdog_dd)
 				       "Registers won't be dumped on a dog "
 				       "bite\n");
 		dump_entry.id = MSM_CPU_CTXT;
-		dump_entry.start_addr = virt_to_phys(wdog_dd->scm_regsave);
-		dump_entry.end_addr = dump_entry.start_addr + PAGE_SIZE;
+		dump_entry.start_addr = wdog_dd->scm_regsave;
+		dump_entry.end_addr = dump_entry.start_addr +
+					wdog_dd->scm_regsave_size;
 		ret = msm_dump_table_register(&dump_entry);
 		if (ret)
 			pr_err("Setting cpu dump region failed\n"
