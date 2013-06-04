f1c3901ab01d89d83b5e5cc30805e5bda5b82a95
yetta_wu <yetta_wu@asus.com>
2013-06-04 18:30:00 +0800

    display: flo: raise bandwidth ab/ib facter to avoid tearing
    Because of the not enough bandwidth, sometimes mipi clk and data
    would spend more than 16.7ms to transmit a frame. This would induce
    tearing.
    
    case 01209088
    Issue 9271363
    
    Change-Id: I6c15e981ead1d9199d3c455e4b4eb7d31826996a
    Signed-off-by: yetta_wu <yetta_wu@asus.com>

--- arch/arm/mach-msm/asustek/flo/board-flo-display.c
+++ arch/arm/mach-msm/asustek/flo/board-flo-display.c
@@ -264,8 +264,8 @@ static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
 	.mdp_max_clk = 266667000,
 	.mdp_max_bw = 3000000000u,
-	.mdp_bw_ab_factor = 115,
-	.mdp_bw_ib_factor = 125,
+	.mdp_bw_ab_factor = 200,
+	.mdp_bw_ib_factor = 250,
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 	.mdp_rev = MDP_REV_44,
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
