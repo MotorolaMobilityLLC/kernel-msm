f60c5f3b3745ceb7678cce0dcefe624670afa2f8
Devin Kim <dojip.kim@lge.com>
2013-06-07 23:40:16 -0700

    msm: mdss: modified the LCD poweroff sequence timing
    JDI LCD might get damage due to overcurrent on power off.
    So change power off sequence to avoid this issue.
    
    Change-Id: Ib3ed77e85d80121289d09b98bc5b42d614686980
    Signed-off-by: sooduek.kim <sooduek.kim@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -206,9 +206,19 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 		msleep(20);
 		wmb();
 	} else {
-		gpio_set_value((ctrl_pdata->rst_gpio), 0);
-		if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
-			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+		if (mdss_panel_id == PANEL_LGE_JDI_ORISE_VIDEO ||
+			mdss_panel_id == PANEL_LGE_JDI_ORISE_CMD ||
+			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_VIDEO ||
+			mdss_panel_id == PANEL_LGE_JDI_NOVATEK_CMD) {
+			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+				gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+			msleep(20);
+			gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		} else {
+			gpio_set_value((ctrl_pdata->rst_gpio), 0);
+			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
+				gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
+		}
 	}
 }
 
