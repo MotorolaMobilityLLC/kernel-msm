5878726f3d89721b2762c860997e2eecc7625058
Chris Fries <C.Fries@motorola.com>
2013-06-13 23:52:04 -0500

    Revert "msm: pm: Add support to query cpu status"
    This reverts commit 660eff3a737049821c26a90c47253657c25e8ec8.

--- arch/arm/mach-msm/devices-8930.c
+++ arch/arm/mach-msm/devices-8930.c
@@ -55,20 +55,6 @@ struct platform_device msm8930_pc_cntr = {
 	.resource	= msm8930_resources_pccntr,
 };
 
-static struct msm_pm_sleep_status_data msm_pm_slp_sts_data = {
-	.base_addr = MSM_ACC0_BASE + 0x08,
-	.cpu_offset = MSM_ACC1_BASE - MSM_ACC0_BASE,
-	.mask = 1UL << 13,
-};
-
-struct platform_device msm8930_cpu_slp_status = {
-	.name		= "cpu_slp_status",
-	.id		= -1,
-	.dev = {
-		.platform_data = &msm_pm_slp_sts_data,
-	},
-};
-
 struct msm_rpm_platform_data msm8930_rpm_data __initdata = {
 	.reg_base_addrs = {
 		[MSM_RPM_PAGE_STATUS] = MSM_RPM_BASE,
