b38c574efc8b7f0cb719ac0197b960fe7738f8fb
Gopichand Nakkala <gnakkala@qca.qualcomm.com>
2013-06-14 18:45:28 -0700

    wlan: Release 3.2.3.131
    Release 3.2.3.131

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            130
+#define QWLAN_VERSION_BUILD            131
 
-#define QWLAN_VERSIONSTR               "3.2.3.130"
+#define QWLAN_VERSIONSTR               "3.2.3.131"
 
 #endif /* QWLAN_VERSION_H */
 
