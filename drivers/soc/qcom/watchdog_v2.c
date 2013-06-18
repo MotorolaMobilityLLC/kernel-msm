8819c8e621b42ba62c3a15e56497fcd25e5036c4
Hong-Mei Li <a21834@motorola.com>
2013-06-19 01:48:09 +0800

    arm: msm: watchdogv2: export pet_watchdog interface
    Falcon is using msm_watchdog_v2 to replace the original msm_watchdog,
    but the msm_watchdog.h was not updated accordingly.
    
    So that the pet_watchdog and msm_watchdog_reset interfaces are defined as null,
    any callers to touch_hw_watchdog/pet_watchdog and msm_watchdog_reset would not
    take effect.
    
    The current callers include:
        apanic_mmc
        soft lockup detector
        modem panic/shutdown handler
        CPU power off handler
    
    That is the reason that we see a lot watchdog reset happens during softlockup
    and kernel panic dump.
    
    Change-Id: I813368786be4183fa56fe45181397877c956f4cb
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/546315
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 693c7b8a45c22c78de9dccd0a195d87b8173481c)

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -49,6 +49,7 @@
 
 static struct workqueue_struct *wdog_wq;
 static struct msm_watchdog_data *wdog_data;
+static struct msm_watchdog_data *g_wdog_dd;
 
 static int cpu_idle_pc_state[NR_CPUS];
 
@@ -138,6 +139,43 @@ static int msm_watchdog_resume(struct device *dev)
 	wdog_dd->enabled = true;
 	wdog_dd->last_pet = sched_clock();
 	return 0;
+}
+
+void msm_watchdog_reset(unsigned int timeout)
+{
+	unsigned long flags;
+	void __iomem *msm_wdt_base;
+
+	local_irq_save(flags);
+
+	if (timeout > 60)
+		timeout = 60;
+
+	msm_wdt_base = g_wdog_dd->base;
+	__raw_writel(WDT_HZ * timeout, msm_wdt_base + WDT0_BARK_TIME);
+	__raw_writel(WDT_HZ * (timeout + 2), msm_wdt_base + WDT0_BITE_TIME);
+	__raw_writel(1, msm_wdt_base + WDT0_EN);
+	__raw_writel(1, msm_wdt_base + WDT0_RST);
+
+	for (timeout += 2; timeout > 0; timeout--)
+		mdelay(1000);
+
+	for (timeout = 2; timeout > 0; timeout--) {
+		__raw_writel(0, msm_wdt_base + WDT0_BARK_TIME);
+		__raw_writel(WDT_HZ, msm_wdt_base + WDT0_BITE_TIME);
+		__raw_writel(1, msm_wdt_base + WDT0_RST);
+		mdelay(1000);
+	}
+
+	for (timeout = 2; timeout > 0; timeout--) {
+		__raw_writel(WDT_HZ, msm_wdt_base + WDT0_BARK_TIME);
+		__raw_writel(0, msm_wdt_base + WDT0_BITE_TIME);
+		__raw_writel(1, msm_wdt_base + WDT0_RST);
+		mdelay(1000);
+	}
+	pr_err("Watchdog reset has failed\n");
+
+	local_irq_restore(flags);
 }
 
 static int panic_wdog_handler(struct notifier_block *this,
@@ -283,6 +321,11 @@ static void pet_watchdog(struct msm_watchdog_data *wdog_dd)
 	if (slack_ns < wdog_dd->min_slack_ns)
 		wdog_dd->min_slack_ns = slack_ns;
 	wdog_dd->last_pet = time_ns;
+}
+
+void g_pet_watchdog(void)
+{
+	pet_watchdog(g_wdog_dd);
 }
 
 static void keep_alive_response(void *info)
@@ -695,6 +738,7 @@ static int msm_watchdog_probe(struct platform_device *pdev)
 	cpumask_clear(&wdog_dd->alive_mask);
 	INIT_WORK(&wdog_dd->init_dogwork_struct, init_watchdog_work);
 	INIT_DELAYED_WORK(&wdog_dd->dogwork_struct, pet_watchdog_work);
+	g_wdog_dd = wdog_dd;
 	queue_work(wdog_wq, &wdog_dd->init_dogwork_struct);
 	return 0;
 err:
