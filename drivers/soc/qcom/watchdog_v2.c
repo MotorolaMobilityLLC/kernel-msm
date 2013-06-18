5d6727f1f23d5a03d2ec78c8df69e95a9e4e9c66
Hong-Mei Li <a21834@motorola.com>
2013-06-19 01:48:09 +0800

    arm: msm: watchdogv2: export pet_watchdog interface
    Add pet_watchdog interface, so that we can feed watchdog timer
    before some cirtical scenarios:
        soft lockup detector
        modem panic/shutdown handler
        CPU power off handler
    
    Change-Id: I6546c233f86e3c54b3652545ce39aea72b3a7819
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -50,6 +50,7 @@
 #define MAX_CPU_CTX_SIZE	2048
 
 static struct msm_watchdog_data *wdog_data;
+static struct msm_watchdog_data *g_wdog_dd;
 
 static int cpu_idle_pc_state[NR_CPUS];
 
@@ -277,6 +278,11 @@ static void pet_watchdog(struct msm_watchdog_data *wdog_dd)
 	if (slack_ns < wdog_dd->min_slack_ns)
 		wdog_dd->min_slack_ns = slack_ns;
 	wdog_dd->last_pet = time_ns;
+}
+
+void g_pet_watchdog(void)
+{
+	pet_watchdog(g_wdog_dd);
 }
 
 static void keep_alive_response(void *info)
@@ -707,6 +713,7 @@ static int msm_watchdog_probe(struct platform_device *pdev)
 		goto err;
 	}
 	init_watchdog_data(wdog_dd);
+	g_wdog_dd = wdog_dd;
 	return 0;
 err:
 	kzfree(wdog_dd);
