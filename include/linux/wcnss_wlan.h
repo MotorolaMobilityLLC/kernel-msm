c75748ed4bf4b6a02af89b5ab74d0445f2731806
Sameer Thalappil <sameert@codeaurora.org>
2013-06-19 14:40:27 -0700

    wcnss: Add API to expose the IRIS XO mode set
    IRIS XO mode can be configured thru module parameter; and it can also
    be auto detected. Add an API to expose the IRIS XO mode set. WLAN driver
    can use this API for it's XO specific operation.
    
    Change-Id: If40ae715f5e400e13a6fc9588d2bd144958ee8c2
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -25,6 +25,12 @@ struct wcnss_wlan_config {
 	int		use_48mhz_xo;
 };
 
+enum {
+	WCNSS_XO_48MHZ = 1,
+	WCNSS_XO_19MHZ,
+	WCNSS_XO_INVALID,
+};
+
 #define WCNSS_WLAN_IRQ_INVALID -1
 #define HAVE_WCNSS_RESET_INTR 1
 #define HAVE_WCNSS_CAL_DOWNLOAD 1
@@ -58,6 +64,7 @@ void wcnss_riva_dump_pmic_regs(void);
 void *wcnss_prealloc_get(unsigned int size);
 int wcnss_prealloc_put(void *ptr);
 int wcnss_device_ready(void);
+int wcnss_wlan_iris_xo_mode(void);
 
 #define wcnss_wlan_get_drvdata(dev) dev_get_drvdata(dev)
 #define wcnss_wlan_set_drvdata(dev, data) dev_set_drvdata((dev), (data))
