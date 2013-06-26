2a71b1809fa8eaf5a6da65dc6d1b71e4d7401442
Jayachandran C <xnq784@motorola.com>
2013-06-26 19:28:05 +0530

    Delay UL powerdown if BAM is in polling mode
    Change-Id: I982c669863390d5ea828d8dd3158e76dab3163e9
    Reviewed-on: http://gerrit.pcs.mot.com/548359
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <cik009@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Alexander Filonenko <alexf@motorola.com>
    SLT-Approved: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -1694,7 +1694,11 @@ static void ul_timeout(struct work_struct *work)
 			ul_packet_written = 0;
 			schedule_delayed_work(&ul_timeout_work,
 					msecs_to_jiffies(UL_TIMEOUT_DELAY));
-		} else {
+                } else if(polling_mode) {
+                        DMUX_LOG_KERR("BAM is in polling mode, delay UL power down");
+                        schedule_delayed_work(&ul_timeout_work,
+                                       msecs_to_jiffies(UL_TIMEOUT_DELAY));
+                } else {
 			ul_powerdown();
 		}
 	}
