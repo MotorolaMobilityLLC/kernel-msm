01ccd84eb7984895cdfce61f4570683f5c1f7436
Vladimir Tsunaev <fvt005@motorola.com>
2013-07-02 07:35:12 -0400

    misc: capsense_prog: AP will only flash (no I2C)
    Cypress CapSense I2C bus will not be handled by apps cpu. Device
    driver in Linux on AP side should implement programming in-system
    flash memory only and it is not necessary to have it as I2C device
    driver. It is changed to platform device driver and usage details
    are described in kernel/Documentation/misc-devices/capsense_prog.txt
    
    Change-Id: Idc008554f96e0b1c90c2a0ab08627ab7a0648a87
    Signed-off-by: Vladimir Tsunaev <fvt005@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/549801
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/capsense_prog/cycapsense_issp.h
+++ drivers/misc/capsense_prog/cycapsense_issp.h
@@ -2,8 +2,6 @@
 /*
  * Header file for:
  * Cypress CapSense(TM) firmware downloader.
- * Supported parts include:
- * CY8C20237
  *
  * Copyright (c) 2012-2013 Motorola Mobility LLC
  *
@@ -20,13 +18,6 @@
 #ifndef __CYCAPSENSE_ISSP_H__
 #define __CYCAPSENSE_ISSP_H__
 
-struct capsense_id {
-	const char *name;
-	unsigned short id;
-	int block_size;
-	int num_of_blocks;
-	int secure_bytes;
-};
 struct hex_info {
 	const char *fw_name;
 	u8 *data;
@@ -35,17 +26,20 @@ struct hex_info {
 };
 struct issp_data {
 	struct hex_info inf;
-	const struct capsense_id *id;
+	const char *name;
+	unsigned int silicon_id;
+	int block_size;
+	int num_of_blocks;
+	int secure_bytes;
 	int c_gpio;
 	int d_gpio;
 	int rst_gpio;
 };
-int cycapsense_issp_parse_hex(struct hex_info *inf, const u8 *src, int len,
-		const struct capsense_id *id);
+int cycapsense_issp_parse_hex(struct issp_data *d, const u8 *src, int len);
 
 int cycapsense_issp_get_cs(const struct issp_data *d, unsigned int *cs);
 
-const struct capsense_id *cycapsense_issp_verify_id(const struct issp_data *d);
+int cycapsense_issp_verify_id(const struct issp_data *d);
 
 int cycapsense_issp_erase(const struct issp_data *d);
 
