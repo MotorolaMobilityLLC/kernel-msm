4b92bb0f44f1fcf3e5ff129e471f96780d9f93e2
Ravi Chebolu <arc095@motorola.com>
2013-07-05 14:16:03 -0500

    msm: Disable standalone power collapse
    Change-Id: I32cc3d408dcf66f4de6589394170306a1d55e1a5
    Signed-off-by: Ravi Chebolu <arc095@motorola.com>
    Change-Id: I8196aafc839d9ec75f775e511ef7b13b8e5dcef2
    Reviewed-on: http://gerrit.pcs.mot.com/550545
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Srinivas Sripathi <a14759@motorola.com>
    Reviewed-by: Connie Zhao <czhao1@motorola.com>
    SLT-Approved: Connie Zhao <czhao1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/pm-data.c
+++ arch/arm/mach-msm/pm-data.c
@@ -22,8 +22,8 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 	},
 
 	[MSM_PM_MODE(0, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
-		.idle_supported = 1,
-		.suspend_supported = 1,
+		.idle_supported = 0,
+		.suspend_supported = 0,
 		.idle_enabled = 0,
 		.suspend_enabled = 0,
 	},
@@ -50,8 +50,8 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 	},
 
 	[MSM_PM_MODE(1, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
-		.idle_supported = 1,
-		.suspend_supported = 1,
+		.idle_supported = 0,
+		.suspend_supported = 0,
 		.idle_enabled = 0,
 		.suspend_enabled = 0,
 	},
@@ -78,8 +78,8 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 	},
 
 	[MSM_PM_MODE(2, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
-		.idle_supported = 1,
-		.suspend_supported = 1,
+		.idle_supported = 0,
+		.suspend_supported = 0,
 		.idle_enabled = 0,
 		.suspend_enabled = 0,
 	},
@@ -106,8 +106,8 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 	},
 
 	[MSM_PM_MODE(3, MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE)] = {
-		.idle_supported = 1,
-		.suspend_supported = 1,
+		.idle_supported = 0,
+		.suspend_supported = 0,
 		.idle_enabled = 0,
 		.suspend_enabled = 0,
 	},
