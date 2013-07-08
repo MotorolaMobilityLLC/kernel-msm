86087239972bf3c295eb2e635b8da7848a0e1af3
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2013-07-08 15:51:57 +0530

    regulator: core: Mechanism to set initial voltage
    Bug 1321163
    
    Change-Id: I4cf279d27a6ad5b64f77f7f504f080e76cf8cf66
    Signed-off-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-on: http://git-master/r/246071
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: https://gerrit.mot.com/915358
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/regulator/machine.h
+++ include/linux/regulator/machine.h
@@ -70,6 +70,7 @@ struct regulator_state {
  *
  * @min_uV: Smallest voltage consumers may set.
  * @max_uV: Largest voltage consumers may set.
+ * @init_uV: Initial voltage consumers may set.
  * @uV_offset: Offset applied to voltages from consumer to compensate for
  *             voltage drops.
  *
@@ -109,6 +110,7 @@ struct regulation_constraints {
 	/* voltage output range (inclusive) - for voltage control */
 	int min_uV;
 	int max_uV;
+	int init_uV;
 
 	int uV_offset;
 
