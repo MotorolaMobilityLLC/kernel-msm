e364eebcd606c98c5214af8cced3f8280a0454c1
Ravi Aravamudhan <aravamud@codeaurora.org>
2013-07-08 19:02:49 -0700

    diag: Skip checking diag bridge for SMUX when it is not enabled
    Diag driver using SMUX over diag_bridge. When SMUX is not enabled,
    it checks the other channels during initialization. This patch
    skips checking other channels when SMUX is not enabled.
    
    Change-Id: Ia155bc91d0332eae0a1341ce450ffe49aa7b9e1a
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>

--- drivers/char/diag/diagfwd_bridge.c
+++ drivers/char/diag/diagfwd_bridge.c
@@ -61,9 +61,11 @@ void connect_bridge(int process_cable, int index)
 		diag_bridge[index].usb_connected = 1;
 	}
 
-	if (index == SMUX && driver->diag_smux_enabled) {
-		driver->in_busy_smux = 0;
-		diagfwd_connect_smux();
+	if (index == SMUX) {
+		if (driver->diag_smux_enabled) {
+			driver->in_busy_smux = 0;
+			diagfwd_connect_smux();
+		}
 	} else {
 		if (diag_hsic[index].hsic_device_enabled &&
 			(driver->logging_mode != MEMORY_DEVICE_MODE ||
