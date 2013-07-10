0d8af2b3b51ff3e1c3e56aa26663558482440a5a
Ravikumar Vembu <raviv@motorola.com>
2013-07-10 14:32:05 -0500

    usb: gadget: Allow rapid charging when usb is secure
    If /sys/class/android_usb/android0/secure is set (when the device is
    security locked), usb enumeration is disabled, hence limiting charging
    to 100mA which does not effectively charge the device. Allow the device
    to charge at bMaxPower as soon as SDP_CHARGER detection is complete, by
    notifying the controller/otg driver when the secure attribute is set.
    Remove unconditonal charging when the secure attribute is cleared.
    
    Signed-off-by: Balachandar Swami <w60013@motorola.com>
    (cherry picked from commit 680d462970505e1034eeb8959cfd4bf4ab315b92)
    
    Change-Id: I20abba1137e5d9a0e63f7891c6b2dc8c799d5f5e

--- drivers/usb/gadget/ci13xxx_udc.h
+++ drivers/usb/gadget/ci13xxx_udc.h
@@ -180,6 +180,7 @@ struct ci13xxx {
 	bool                      skip_flush; /* skip flushing remaining EP
 						upon flush timeout for the
 						first EP. */
+	int			  charge_enabled; /* unconditional charging */
 };
 
 /******************************************************************************
