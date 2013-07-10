4542a87dd16e131fbfd9450c61b3ec3acd28f8ea
Ravikumar Vembu <raviv@motorola.com>
2013-07-10 14:32:05 -0500

    usb: gadget: Allow rapid charging when usb is secure
    If /sys/class/android_usb/android0/secure is set (when the device is
    security locked), usb enumeration is disabled, hence limiting charging
    to 100mA which does not effectively charge the device. Allow the device
    to charge at bMaxPower as soon as SDP_CHARGER detection is complete, by
    notifying the controller/otg driver when the secure attribute is set.
    Remove unconditonal charging when the secure attribute is cleared.
    
    Change-Id: Iee1a92adf6e1a32d07efe9b1ea8ad9f8c87e62fc
    Signed-off-by: Balachandar Swami <w60013@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/551260
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Balachandar Swami <balaswami@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Reviewed-by: David Ding <dding@motorola.com>
    SLT-Approved: Sanjay Dhar <csd004@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -2347,6 +2347,11 @@ static void msm_otg_sm_work(struct work_struct *w)
 						OTG_STATE_B_PERIPHERAL;
 					break;
 				case USB_SDP_CHARGER:
+					if (otg->gadget &&
+						usb_gadget_get_charge_enabled(
+								otg->gadget))
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MIN);
 					if(!slimport_is_connected()) {
 						msm_otg_start_peripheral(otg, 1);
 						otg->phy->state =
