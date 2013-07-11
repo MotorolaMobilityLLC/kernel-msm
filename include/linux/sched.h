ef2c5d4ebd0962d29f7e60343f4629c208e47ca8
Hong-Mei Li <a21834@motorola.com>
2013-07-12 01:07:25 +0800

    staging: android: lmk: check free memory when tasks switch to background
    Add more free-memory check point.
    This patch trigger low memory killer to scan free memory when a process is moved
    to background task groups.
    
    Change-Id: If50eef2fb2c05f57ab808f95a9408204058f4f9b
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/557973
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    SLTApproved: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2766,6 +2766,9 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 #endif /* CONFIG_SMP */
 
 extern struct atomic_notifier_head migration_notifier_head;
+#ifdef CONFIG_ANDROID_BG_SCAN_MEM
+extern struct raw_notifier_head bgtsk_migration_notifier_head;
+#endif
 
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
