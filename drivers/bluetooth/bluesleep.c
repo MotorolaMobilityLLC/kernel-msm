7fd4b3a384b1bc973622773472b3343ced1d46ab
Devin Kim <dojip.kim@lge.com>
2013-07-12 19:03:12 -0700

    bluesleep: Support for device tree
    Bluesleep driver is able to get the board specific configrations from
    device tree.
    
    Change-Id: If9ede425c5a7134511e4fbbc82edf440015bdcc1
    Signed-off-by: Devin Kim <dojip.kim@lge.com>
    Signed-off-by: hyungjung0521.kim <hyungjung0521.kim@lge.com>

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -1,31 +1,31 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-   for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
 
 
-   Copyright (C) 2006-2007 - Motorola
-   Copyright (c) 2008-2010, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2006-2007 - Motorola
+ * Copyright (c) 2008-2010, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013, LGE Inc.
 
-   Date         Author           Comment
-   -----------  --------------   --------------------------------
-   2006-Apr-28	Motorola	 The kernel module for running the Bluetooth(R)
-				 Sleep-Mode Protocol from the Host side
-   2006-Sep-08  Motorola         Added workqueue for handling sleep work.
-   2007-Jan-24  Motorola         Added mbm_handle_ioi() call to ISR.
-   2009-Aug-10  Motorola         Changed "add_timer" to "mod_timer" to solve
-                                 race when flurry of queued work comes in.
-*/
+ * Date         Author           Comment
+ * -----------  --------------   --------------------------------
+ * 2006-Apr-28	Motorola	 The kernel module for running the Bluetooth(R)
+ *                               Sleep-Mode Protocol from the Host side
+ *  2006-Sep-08  Motorola        Added workqueue for handling sleep work.
+ *  2007-Jan-24  Motorola        Added mbm_handle_ioi() call to ISR.
+ *  2009-Aug-10  Motorola        Changed "add_timer" to "mod_timer" to solve
+ *                               race when flurry of queued work comes in.
+ */
 
 #include <linux/module.h>	/* kernel module definitions */
 #include <linux/errno.h>
@@ -48,6 +48,7 @@
 #include <linux/termios.h>
 #include <linux/wakelock.h>
 #include <linux/gpio.h>
+#include <linux/of_gpio.h>
 #include <linux/serial_core.h>
 #include <mach/msm_serial_hs.h>
 
@@ -516,7 +517,7 @@ static void bluesleep_stop(void)
 
 #if BT_ENABLE_IRQ_WAKE
 	if (disable_irq_wake(bsi->host_wake_irq))
-		BT_ERR("Couldn't disable hostwake IRQ wakeup mode\n");
+		BT_ERR("Couldn't disable hostwake IRQ wakeup mode");
 #endif
 	wake_lock_timeout(&bsi->wake_lock, HZ / 2);
 }
@@ -680,23 +681,80 @@ void bluesleep_setup_uart_port(struct platform_device *uart_dev)
 	bluesleep_uart_dev = uart_dev;
 }
 
+static int bluesleep_populate_dt_pinfo(struct platform_device *pdev)
+{
+	struct device_node *np = pdev->dev.of_node;
+	int tmp;
+
+	tmp = of_get_named_gpio(np, "bt_host_wake", 0);
+	if (tmp < 0) {
+		BT_ERR("couldn't find host_wake gpio");
+		return -ENODEV;
+	}
+	bsi->host_wake = tmp;
+
+	tmp = of_get_named_gpio(np, "bt_ext_wake", 0);
+	if (tmp < 0)
+		bsi->has_ext_wake = 0;
+	else
+		bsi->has_ext_wake = 1;
+
+	if (bsi->has_ext_wake)
+		bsi->ext_wake = tmp;
+
+	BT_INFO("bt_host_wake %d, bt_ext_wake %d",
+			bsi->host_wake,
+			bsi->ext_wake);
+	return 0;
+}
+
+static int bluesleep_populate_pinfo(struct platform_device *pdev)
+{
+	struct resource *res;
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_IO,
+				"gpio_host_wake");
+	if (!res) {
+		BT_ERR("couldn't find host_wake gpio");
+		return -ENODEV;
+	}
+	bsi->host_wake = res->start;
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_IO,
+				"gpio_ext_wake");
+	if (!res)
+		bsi->has_ext_wake = 0;
+	else
+		bsi->has_ext_wake = 1;
+
+	if (bsi->has_ext_wake)
+		bsi->ext_wake = res->start;
+
+	return 0;
+}
+
 static int bluesleep_probe(struct platform_device *pdev)
 {
-	int ret;
 	struct resource *res;
+	int ret;
 
 	bsi = kzalloc(sizeof(struct bluesleep_info), GFP_KERNEL);
 	if (!bsi)
 		return -ENOMEM;
 
-	res = platform_get_resource_byname(pdev, IORESOURCE_IO,
-				"gpio_host_wake");
-	if (!res) {
-		BT_ERR("couldn't find host_wake gpio\n");
-		ret = -ENODEV;
-		goto free_bsi;
+	if (pdev->dev.of_node) {
+		ret = bluesleep_populate_dt_pinfo(pdev);
+		if (ret < 0) {
+			BT_ERR("couldn't populate info from dt");
+			return ret;
+		}
+	} else {
+		ret = bluesleep_populate_pinfo(pdev);
+		if (ret < 0) {
+			BT_ERR("couldn't populate info");
+			return ret;
+		}
 	}
-	bsi->host_wake = res->start;
 
 	/* configure host_wake as input */
 	ret = gpio_request_one(bsi->host_wake, GPIOF_IN, "bt_host_wake");
@@ -706,14 +764,6 @@ static int bluesleep_probe(struct platform_device *pdev)
 				bsi->host_wake, ret);
 		goto free_bsi;
 	}
-
-	res = platform_get_resource_byname(pdev, IORESOURCE_IO,
-				"gpio_ext_wake");
-
-	if (!res)
-		bsi->has_ext_wake = 0;
-	else
-		bsi->has_ext_wake = 1;
 
 	if (bsi->has_ext_wake) {
 		/* configure ext_wake as output mode*/
@@ -725,19 +775,20 @@ static int bluesleep_probe(struct platform_device *pdev)
 				  bsi->ext_wake, ret);
 			goto free_bt_host_wake;
 		}
-	} else
+	} else {
 		set_bit(BT_EXT_WAKE, &flags);
+	}
 
 	res = platform_get_resource_byname(pdev, IORESOURCE_IRQ,
 						"host_wake");
 	if (!res) {
-		BT_ERR("couldn't find host_wake irq\n");
+		BT_ERR("couldn't find host_wake irq");
 		ret = -ENODEV;
 		goto free_bt_host_wake;
 	}
 	bsi->host_wake_irq = res->start;
 	if (bsi->host_wake_irq < 0) {
-		BT_ERR("couldn't find host_wake irq\n");
+		BT_ERR("couldn't find host_wake irq");
 		ret = -ENODEV;
 		goto free_bt_ext_wake;
 	}
@@ -748,6 +799,10 @@ static int bluesleep_probe(struct platform_device *pdev)
 
 	wake_lock_init(&bsi->wake_lock, WAKE_LOCK_SUSPEND, "bluesleep");
 	clear_bit(BT_SUSPEND, &flags);
+
+	BT_INFO("host_wake_irq %d, polarity %d",
+			bsi->host_wake_irq,
+			bsi->irq_polarity);
 
 	if (bsi->irq_polarity == POLARITY_LOW) {
 		ret = request_irq(bsi->host_wake_irq, bluesleep_hostwake_isr,
@@ -788,10 +843,10 @@ static int bluesleep_remove(struct platform_device *pdev)
 static int bluesleep_resume(struct platform_device *pdev)
 {
 	if (test_bit(BT_SUSPEND, &flags)) {
-		BT_DBG("bluesleep resuming...\n");
+		BT_DBG("bluesleep resuming...");
 	if ((bsi->uport != NULL) &&
 		(gpio_get_value(bsi->host_wake) == bsi->irq_polarity)) {
-			BT_DBG("bluesleep resume from BT event...\n");
+			BT_DBG("bluesleep resume from BT event...");
 			msm_hs_request_clock_on(bsi->uport);
 			msm_hs_set_mctrl(bsi->uport, TIOCM_RTS);
 		}
@@ -802,10 +857,15 @@ static int bluesleep_resume(struct platform_device *pdev)
 
 static int bluesleep_suspend(struct platform_device *pdev, pm_message_t state)
 {
-	BT_DBG("bluesleep suspending...\n");
+	BT_DBG("bluesleep suspending...");
 	set_bit(BT_SUSPEND, &flags);
 	return 0;
 }
+
+static struct of_device_id bluesleep_match_table[] = {
+	{ .compatible = "qcom,bluesleep" },
+	{}
+};
 
 static struct platform_driver bluesleep_driver = {
 	.probe = bluesleep_probe,
@@ -815,6 +875,7 @@ static struct platform_driver bluesleep_driver = {
 	.driver = {
 		.name = "bluesleep",
 		.owner = THIS_MODULE,
+		.of_match_table = bluesleep_match_table,
 	},
 };
 /**
@@ -961,7 +1022,7 @@ static void __exit bluesleep_exit(void)
 	set_bit(BT_EXT_WAKE, &flags);
 	if (test_bit(BT_PROTO, &flags)) {
 		if (disable_irq_wake(bsi->host_wake_irq))
-			BT_ERR("Couldn't disable hostwake IRQ wakeup mode\n");
+			BT_ERR("Couldn't disable hostwake IRQ wakeup mode");
 		free_irq(bsi->host_wake_irq, NULL);
 		del_timer(&tx_timer);
 		if (test_bit(BT_ASLEEP, &flags))
