f9385e91138f8ca8078f1c906cb9aceed2fc56c0
Devin Kim <dojip.kim@lge.com>
2013-07-15 14:17:00 -0700

    bluesleep: Set the file permission for /proc/bluetooth/sleep/*
    Set the writable permission only for user and group
    
    Change-Id: I47f3784cf80369816168c3d6846ecee6b99aef73
    Signed-off-by: Devin Kim@lge.com <dojip.kim@lge.com>
    Signed-off-by: hyungjung0521.kim <hyungjung0521.kim@lge.com>

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -899,7 +899,8 @@ static int __init bluesleep_init(void)
 	}
 
 	/* Creating read/write "btwake" entry */
-	ent = create_proc_entry("btwake", 0, sleep_dir);
+	ent = create_proc_entry("btwake", S_IRUGO | S_IWUSR | S_IWGRP,
+			sleep_dir);
 	if (ent == NULL) {
 		BT_ERR("Unable to create /proc/%s/btwake entry", PROC_DIR);
 		retval = -ENOMEM;
@@ -909,7 +910,7 @@ static int __init bluesleep_init(void)
 	ent->write_proc = bluepower_write_proc_btwake;
 
 	/* read only proc entries */
-	if (create_proc_read_entry("hostwake", 0, sleep_dir,
+	if (create_proc_read_entry("hostwake", S_IRUGO, sleep_dir,
 				bluepower_read_proc_hostwake, NULL) == NULL) {
 		BT_ERR("Unable to create /proc/%s/hostwake entry", PROC_DIR);
 		retval = -ENOMEM;
@@ -917,7 +918,8 @@ static int __init bluesleep_init(void)
 	}
 
 	/* read/write proc entries */
-	ent = create_proc_entry("proto", 0, sleep_dir);
+	ent = create_proc_entry("proto", S_IRUGO | S_IWUSR | S_IWGRP,
+			sleep_dir);
 	if (ent == NULL) {
 		BT_ERR("Unable to create /proc/%s/proto entry", PROC_DIR);
 		retval = -ENOMEM;
@@ -927,7 +929,7 @@ static int __init bluesleep_init(void)
 	ent->write_proc = bluesleep_write_proc_proto;
 
 	/* read only proc entries */
-	if (create_proc_read_entry("asleep", 0,
+	if (create_proc_read_entry("asleep", S_IRUGO,
 			sleep_dir, bluesleep_read_proc_asleep, NULL) == NULL) {
 		BT_ERR("Unable to create /proc/%s/asleep entry", PROC_DIR);
 		retval = -ENOMEM;
@@ -936,7 +938,8 @@ static int __init bluesleep_init(void)
 
 #if BT_BLUEDROID_SUPPORT
 	/* read/write proc entries */
-	ent = create_proc_entry("lpm", 0, sleep_dir);
+	ent = create_proc_entry("lpm", S_IRUGO | S_IWUSR | S_IWGRP,
+			sleep_dir);
 	if (ent == NULL) {
 		BT_ERR("Unable to create /proc/%s/lpm entry", PROC_DIR);
 		retval = -ENOMEM;
@@ -946,7 +949,8 @@ static int __init bluesleep_init(void)
 	ent->write_proc = bluesleep_write_proc_lpm;
 
 	/* read/write proc entries */
-	ent = create_proc_entry("btwrite", 0, sleep_dir);
+	ent = create_proc_entry("btwrite", S_IRUGO | S_IWUSR | S_IWGRP,
+			sleep_dir);
 	if (ent == NULL) {
 		BT_ERR("Unable to create /proc/%s/btwrite entry", PROC_DIR);
 		retval = -ENOMEM;
