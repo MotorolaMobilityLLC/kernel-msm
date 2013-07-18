837c95ffee3224f4a7494679803b17904377e2f4
Vladimir Karpovich <cvk011c@motorola.com>
2013-07-18 10:28:24 -0500

    msm: vibrator: Add voltage setting for haptic
    The haptic vibes ( <60ms) shall use higher voltage than
    the long vibes for stronger and faster kick.The long vibes need
    lower voltage to reduce the noise.
    
    Change-Id: I216e16be24a6dd4103aa10d3d7c1d4f3aa15bf0e
    Signed-off-by: Vladimir Karpovich <cvk011c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/553136
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -35,6 +35,8 @@
 #define QPNP_VIB_VTG_SET_MASK		0x1F
 #define QPNP_VIB_LOGIC_SHIFT		4
 
+#define QPNP_HAPTIC_THRESHOLD		60
+
 struct qpnp_vib {
 	struct spmi_device *spmi;
 	struct hrtimer vib_timer;
@@ -46,7 +48,10 @@ struct qpnp_vib {
 	u16 base;
 	int state;
 	int vtg_level;
+	int vtg_level_normal;
+	int vtg_level_haptic;
 	int timeout;
+	int haptic_threshold;
 	struct mutex lock;
 };
 
@@ -170,6 +175,8 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 		value = (value > vib->timeout ?
 				 vib->timeout : value);
 		vib->state = 1;
+		vib->vtg_level = (value < vib->haptic_threshold) ?
+				vib->vtg_level_haptic : vib->vtg_level_normal;
 		hrtimer_start(&vib->vib_timer,
 			      ktime_set(value / 1000, (value % 1000) * 1000000),
 			      HRTIMER_MODE_REL);
@@ -248,17 +255,31 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 		return rc;
 	}
 
-	vib->vtg_level = QPNP_VIB_DEFAULT_VTG_LVL;
+	vib->vtg_level_normal = QPNP_VIB_DEFAULT_VTG_LVL;
 	rc = of_property_read_u32(spmi->dev.of_node,
 			"qcom,vib-vtg-level-mV", &temp_val);
 	if (!rc) {
-		vib->vtg_level = temp_val;
+		vib->vtg_level_normal = temp_val;
 	} else if (rc != -EINVAL) {
 		dev_err(&spmi->dev, "Unable to read vtg level\n");
 		return rc;
 	}
 
-	vib->vtg_level /= 100;
+	vib->vtg_level_normal /= 100;
+	vib->vtg_level = vib->vtg_level_normal;
+	vib->vtg_level_haptic = vib->vtg_level_normal;
+
+	rc = of_property_read_u32(spmi->dev.of_node,
+			"qcom,vib-vtg-level-mV-haptic", &temp_val);
+	if (!rc)
+		vib->vtg_level_haptic = temp_val/100;
+
+	vib->haptic_threshold = QPNP_HAPTIC_THRESHOLD;
+
+	rc = of_property_read_u32(spmi->dev.of_node,
+			"qcom,vib-haptic-threshold-ms", &temp_val);
+	if (!rc)
+		vib->haptic_threshold = temp_val;
 
 	vib_resource = spmi_get_resource(spmi, 0, IORESOURCE_MEM, 0);
 	if (!vib_resource) {
