13c40aff89c4615a0a9ef9939401d2df04b27073
Madan Mohan Koyyalamudi <mkoyyala@qca.qualcomm.com>
2013-07-22 12:26:18 -0700

    Wlan: Revision 3.2.3.148
    Revision 3.2.3.148

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            147
+#define QWLAN_VERSION_BUILD            148
 
-#define QWLAN_VERSIONSTR               "3.2.3.147"
+#define QWLAN_VERSIONSTR               "3.2.3.148"
 
 #endif /* QWLAN_VERSION_H */
 
