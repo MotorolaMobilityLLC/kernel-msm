8cba304d40767e228de30b846b64e74b2af6f1cd
Russell King <rmk+kernel@arm.linux.org.uk>
2013-07-24 00:29:18 +0100

    ARM: move signal handlers into a vdso-like page
    Move the signal handlers into a VDSO page rather than keeping them in
    the vectors page.  This allows us to place them randomly within this
    page, and also map the page at a random location within userspace
    further protecting these code fragments from ROP attacks.  The new
    VDSO page is also poisoned in the same way as the vector page.
    
    Change-Id: Ic45bb26efb1f00de0a025d36fc15dbd6580dc7de
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Git-commit: 48be69a026b2c17350a5ef18a1959a919f60be7d
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [joonwoop@codeaurora.org: fixed merge conflict]
    CRs-fixed: 561044
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    
    (cherry picked from commit 10d98aaa7248045b0f103612d8e2f771be693393)

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -851,15 +851,6 @@ void __init early_trap_init(void *vectors_base)
 	 */
 	kuser_get_tls_init(vectors);
 
-	/*
-	 * Copy signal return handlers into the vector page, and
-	 * set sigreturn to be a pointer to these.
-	 */
-	memcpy((void *)(vectors + KERN_SIGRETURN_CODE - CONFIG_VECTORS_BASE),
-	       sigreturn_codes, sizeof(sigreturn_codes));
-	memcpy((void *)(vectors + KERN_RESTART_CODE - CONFIG_VECTORS_BASE),
-	       syscall_restart_code, sizeof(syscall_restart_code));
-
 	flush_icache_range(vectors, vectors + PAGE_SIZE * 2);
 	modify_domain(DOMAIN_USER, DOMAIN_CLIENT);
 }
