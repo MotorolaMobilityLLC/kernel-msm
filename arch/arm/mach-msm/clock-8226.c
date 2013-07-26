bb9cdeaeff4e067fc79bca4a9dad64265f8b12db
Jason Hrycay <jason.hrycay@motorola.com>
2013-07-26 08:13:58 -0500

    arm: mach-msm: call BUG() instead of panic()
    Change-Id: Ib205b21003870fff767f131193e6ecdd501e8454
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- arch/arm/mach-msm/clock-8226.c
+++ arch/arm/mach-msm/clock-8226.c
@@ -25,6 +25,7 @@
 #include <mach/socinfo.h>
 #include <mach/rpm-smd.h>
 #include <mach/clock-generic.h>
+#include <mach/subsystem_restart.h>
 
 #include "clock-local2.h"
 #include "clock-pll.h"
@@ -3699,39 +3700,39 @@ static void __init msm8226_clock_pre_init(void)
 {
 	virt_bases[GCC_BASE] = ioremap(GCC_CC_PHYS, GCC_CC_SIZE);
 	if (!virt_bases[GCC_BASE])
-		panic("clock-8226: Unable to ioremap GCC memory!");
+		PR_BUG("clock-8226: Unable to ioremap GCC memory!");
 
 	virt_bases[MMSS_BASE] = ioremap(MMSS_CC_PHYS, MMSS_CC_SIZE);
 	if (!virt_bases[MMSS_BASE])
-		panic("clock-8226: Unable to ioremap MMSS_CC memory!");
+		PR_BUG("clock-8226: Unable to ioremap MMSS_CC memory!");
 
 	virt_bases[LPASS_BASE] = ioremap(LPASS_CC_PHYS, LPASS_CC_SIZE);
 	if (!virt_bases[LPASS_BASE])
-		panic("clock-8226: Unable to ioremap LPASS_CC memory!");
+		PR_BUG("clock-8226: Unable to ioremap LPASS_CC memory!");
 
 	virt_bases[APCS_BASE] = ioremap(APCS_KPSS_GLB_PHYS,
 		APCS_KPSS_GLB_SIZE);
 	if (!virt_bases[APCS_BASE])
-		panic("clock-8226: Unable to ioremap APCS_GCC_CC memory!");
+		PR_BUG("clock-8226: Unable to ioremap APCS_GCC_CC memory!");
 
 	virt_bases[APCS_PLL_BASE] = ioremap(APCS_KPSS_SH_PLL_PHYS,
 		APCS_KPSS_SH_PLL_SIZE);
 	if (!virt_bases[APCS_PLL_BASE])
-		panic("clock-8226: Unable to ioremap APCS_GCC_CC memory!");
+		PR_BUG("clock-8226: Unable to ioremap APCS_GCC_CC memory!");
 
 	clk_ops_local_pll.enable = sr_hpm_lp_pll_clk_enable;
 
 	vdd_dig.regulator[0] = regulator_get(NULL, "vdd_dig");
 	if (IS_ERR(vdd_dig.regulator[0]))
-		panic("clock-8226: Unable to get the vdd_dig regulator!");
+		PR_BUG("clock-8226: Unable to get the vdd_dig regulator!");
 
 	vdd_sr2_pll.regulator[0] = regulator_get(NULL, "vdd_sr2_pll");
 	if (IS_ERR(vdd_sr2_pll.regulator[0]))
-		panic("clock-8226: Unable to get the sr2_pll regulator!");
+		PR_BUG("clock-8226: Unable to get the sr2_pll regulator!");
 
 	vdd_sr2_pll.regulator[1] = regulator_get(NULL, "vdd_sr2_dig");
 	if (IS_ERR(vdd_sr2_pll.regulator[1]))
-		panic("clock-8226: Unable to get the vdd_sr2_dig regulator!");
+		PR_BUG("clock-8226: Unable to get the vdd_sr2_dig regulator!");
 
 
 	enable_rpm_scaling();
