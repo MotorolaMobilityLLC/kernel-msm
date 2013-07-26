bf6300db6a4f93a089c3d3f39069c62413521026
Jason Hrycay <jason.hrycay@motorola.com>
2013-07-26 01:15:51 -0500

    usb: otg: Support for Controlled Factory Kill
    Add handler to safely halt the kernel before power down initiated by
    factory cable detach.
    
    Change-Id: Ie87de6a13460377c1b117e563c927bf0d1ee10be
    Signed-off-by: Ravikumar Vembu <a18394@motorola.com>

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -43,6 +43,7 @@
 #include <linux/mfd/pm8xxx/pm8921-charger.h>
 #include <linux/mfd/pm8xxx/misc.h>
 #include <linux/mhl_8334.h>
+#include <linux/reboot.h>
 
 #include <mach/scm.h>
 #include <mach/clk.h>
@@ -3329,17 +3330,48 @@ static void msm_otg_set_vbus_state(int online)
 		queue_work(system_nrt_wq, &motg->sm_work);
 }
 
+static int factory_cable;
+static int msm_pmic_is_factory_cable(struct msm_otg *motg)
+{
+	int id_gnd = 0;
+	int id_flt = 0;
+
+	if (!(motg->pdata->id_gnd_gpio && motg->pdata->id_flt_gpio))
+		return 0;
+	id_gnd = gpio_get_value(motg->pdata->id_gnd_gpio) ^
+		motg->pdata->id_gnd_active_high;
+	id_flt = gpio_get_value(motg->pdata->id_flt_gpio) ^
+		motg->pdata->id_flt_active_high;
+
+	if (!id_gnd && !id_flt)
+		return 1;
+	return 0;
+}
+
 static void msm_pmic_id_status_w(struct work_struct *w)
 {
 	struct msm_otg *motg = container_of(w, struct msm_otg,
 						pmic_id_status_work.work);
 	int work = 0;
+	int id_gnd = msm_otg_read_pmic_id_state(motg);
 	int id_flt = gpio_get_value(motg->pdata->id_flt_gpio) ^
 			motg->pdata->id_flt_active_high;
 
-	if (msm_otg_read_pmic_id_state(motg) || !id_flt) {
-		if (!id_flt)
-			pr_info_once("Factory Cable Attached\n");
+	pr_debug("PMIC: ID GND %d\n", id_gnd);
+	pr_debug("PMIC: ID FLT %d\n", id_flt);
+
+	if (!id_gnd && !id_flt) {
+		pr_info_once("Factory Cable Attached!\n");
+		factory_cable = 1;
+	} else
+		if (factory_cable) {
+			pr_info("Factory Cable Detached!,"
+					" 2 sec to power off.\n");
+			kernel_halt();
+			return;
+		}
+
+	if (id_gnd || !id_flt) {
 		if (!test_and_set_bit(ID, &motg->inputs)) {
 			pr_debug("PMIC: ID set\n");
 			work = 1;
@@ -4663,6 +4695,7 @@ static int __init msm_otg_probe(struct platform_device *pdev)
 	if (ret)
 		dev_dbg(&pdev->dev, "fail to setup cdev\n");
 
+	factory_cable = msm_pmic_is_factory_cable(motg);
 	return 0;
 
 remove_phy:
