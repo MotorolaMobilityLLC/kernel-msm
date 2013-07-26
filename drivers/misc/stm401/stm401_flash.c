e1d50b841b0e78b126c89af886c93a26fcc6a248
Robert Wiegand <qa2113@motorola.com>
2013-07-26 10:36:03 -0500

    drivers: misc: stm401: Modify names in the STM401 driver.
    Moved the stm401 file into a sub-directory, broke the driver into
    multiple files and changed all occurrences of msp430 to stm401.
    
    Change-Id: I7d2114bfe92a8d60983925e0b144094db3acc8ac
    Signed-off-by: Robert Wiegand <qa2113@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/554166
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jagatkumar Shah <jagat@motorola.com>
    Reviewed-by: Igor Kovalenko <cik009@motorola.com>
    Reviewed-by: George Standish <george.standish@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/misc/stm401/stm401_flash.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2010-2013 Motorola Mobility LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ */
+
+#include <linux/cdev.h>
+#include <linux/delay.h>
+#include <linux/earlysuspend.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/export.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/i2c.h>
+#include <linux/input.h>
+#include <linux/input-polldev.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/regulator/consumer.h>
+#include <linux/slab.h>
+#include <linux/switch.h>
+#include <linux/time.h>
+#include <linux/uaccess.h>
+#include <linux/wakelock.h>
+#include <linux/workqueue.h>
+
+#include <linux/stm401.h>
