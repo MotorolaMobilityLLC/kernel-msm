0f38a0b842a96c9a3b51c2eb76e59ce57dbcd3d0
Seemanta Dutta <seemanta@codeaurora.org>
2013-07-26 01:01:32 +0000

    msm: camera: Fix uninitialized memory returned to userspace
    Local structures have not been initialized to all zeroes, so fix
    this by setting them to all zeroes to prevent uninitialized memory
    being copied to userspace.
    
    Change-Id: I97c37ac23ef6e461d2644dfd315c502199266573
    CRs-fixed: 518478
    Signed-off-by: Masaki Sato <ams087@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/585862
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Reviewed-by: Sriram Divakar <sriramhd@motorola.com>
    SLTApproved: Sriram Divakar <sriramhd@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    (cherry picked from commit 062ff82fc27136c1f6d62334917fff274b442296)

--- drivers/media/video/msm/mercury/msm_mercury_sync.c
+++ drivers/media/video/msm/mercury/msm_mercury_sync.c
@@ -197,6 +197,7 @@ int msm_mercury_evt_get(struct msm_mercury_device *pmercury_dev,
 	int rc = 0;
 
 	MCR_DBG("(%d)%s() Enter\n", __LINE__, __func__);
+	memset(&ctrl_cmd, 0, sizeof(ctrl_cmd));
 	ctrl_cmd.type = (uint32_t)msm_mercury_q_wait(&pmercury_dev->evt_q);
 
 	rc = copy_to_user(arg, &ctrl_cmd, sizeof(ctrl_cmd));
