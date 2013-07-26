c4614e527567025812d7971e2df02e380af14d5f
Zac Crosby <azc095@motorola.com>
2013-07-26 01:22:39 -0500

    msm: mdss: Add support for multiple panel versions/types
    Allow a single product to support multiple types of displays and multiple
    versions of a single display type
    
    Change-Id: Ia47184cc6a41798d1c9b4d343374c949699c6731
    Signed-off-by: Zac Crosby <azc095@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -228,12 +228,12 @@ struct mdss_panel_esd_pdata {
 };
 
 struct mdss_panel_config {
-	bool is_panel_config_loaded;
 	bool esd_enable;
 	struct mutex panel_mutex;
 	bool esd_disable_bl;
 
 	bool bare_board;
+	char panel_name[32];
 	u64 panel_ver;
 };
 
@@ -403,4 +403,7 @@ int mdss_dsi_bta_status_check(struct mdss_dsi_ctrl_pdata *ctrl);
 int mdss_dsi_panel_init(struct device_node *node,
 		struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 		bool cmd_cfg_cont_splash);
+int mdss_panel_parse_panel_config_dt(struct mdss_dsi_ctrl_pdata *ctrl_pdata);
+bool mdss_dsi_match_chosen_panel(struct device_node *np,
+				struct mdss_panel_config *pconfig);
 #endif /* MDSS_DSI_H */
