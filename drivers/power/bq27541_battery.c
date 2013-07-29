77f043e2e3503f1dccb81267232808109bc811e9
Hank_Lee <Hank_Lee@asus.com>
2013-07-29 15:13:18 +0800

    power: bq27541: add error handle to report previous valid capacity
    Bug: 9984381
    Change-Id: Ie27d105c8e42b9e9f69dfa219759de23526b21f9
    Signed-off-by: Hank_Lee <Hank_Lee@asus.com>

--- drivers/power/bq27541_battery.c
+++ drivers/power/bq27541_battery.c
@@ -614,10 +614,14 @@ static int bq27541_get_capacity(union power_supply_propval *val)
 	bool check_cap = false;
 	int smb_retry_max = (SMBUS_RETRY + 2);
 
+	bq27541_device->bat_capacity = 0;
 	do {
 		bq27541_device->smbus_status = bq27541_smbus_read_data(REG_CAPACITY, 0 ,&bq27541_device->bat_capacity);
-		if ((bq27541_device->bat_capacity <= 0) || (bq27541_device->bat_capacity > 100))
+		if ((bq27541_device->bat_capacity <= 0) || (bq27541_device->bat_capacity > 100)) {
 			check_cap = true;
+			BAT_NOTICE("check capacity, cap = %d, smb_retry = %d\n", bq27541_device->bat_capacity, smb_retry);
+		} else
+			check_cap = false;
 	} while(((bq27541_device->smbus_status < 0) || check_cap) && ( ++smb_retry <= smb_retry_max));
 
 	if (bq27541_device->smbus_status < 0) {
@@ -634,9 +638,14 @@ static int bq27541_get_capacity(union power_supply_propval *val)
 		}
 	}
 
-	ret = bq27541_device->bat_capacity;
+	temp_capacity = ret = bq27541_device->bat_capacity;
 
-	temp_capacity = ((ret >= 100) ? 100 : ret);
+	if (!(bq27541_device->bat_capacity >= bq27541_data[REG_CAPACITY].min_value &&
+			bq27541_device->bat_capacity <= bq27541_data[REG_CAPACITY].max_value)) {
+		val->intval = bq27541_device->old_capacity;
+		BAT_NOTICE("use old capacity=%u\n", bq27541_device->old_capacity);
+		return 0;
+	}
 
 	/* start: for mapping %99 to 100%. Lose 84%*/
 	if(temp_capacity==99)
