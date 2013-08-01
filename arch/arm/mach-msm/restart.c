b6e8f36670082841ec8dab205fe6a97057824756
Devin Kim <dojip.kim@lge.com>
2013-07-31 19:05:22 -0700

    msm: restart: always do warm reset when reboot
    memory contents must be maintained for reboot reason.
    
    Change-Id: I21dd4d8702320391df68a26489c4e708607a540f
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -96,11 +96,6 @@ static void set_dload_mode(int on)
 	}
 }
 
-static bool get_dload_mode(void)
-{
-	return dload_mode_enabled;
-}
-
 static void enable_emergency_dload_mode(void)
 {
 	if (emergency_dload_mode_addr) {
@@ -221,11 +216,7 @@ static void msm_restart_prepare(const char *cmd)
 
 	pm8xxx_reset_pwr_off(1);
 
-	/* Hard reset the PMIC unless memory contents must be maintained. */
-	if (get_dload_mode() || (cmd != NULL && cmd[0] != '\0'))
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-	else
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
+	qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 
 	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
