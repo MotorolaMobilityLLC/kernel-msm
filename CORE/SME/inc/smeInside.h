1dae90858d07a106625e84da95b25a5f96a589f0
Madan Mohan Koyyalamudi <mkoyyala@qca.qualcomm.com>
2013-08-01 18:00:25 +0530

    SCAN-OFFLOAD: Remove the scan commands from smeScanCmdPendingList
    If Scan offload is enabled then remove the pending commands in
    smeScanCmdPendingList along with smeCmdPendingList while closing
    a session.
    
    Change-Id: Icc9c3148fa109f8c29ce760b858ad300e977c368
    CRs-Fixed: 500914

--- CORE/SME/inc/smeInside.h
+++ CORE/SME/inc/smeInside.h
@@ -241,7 +241,8 @@ tSmeCmd *smeGetCommandBuffer( tpAniSirGlobal pMac );
 void smePushCommand( tpAniSirGlobal pMac, tSmeCmd *pCmd, tANI_BOOLEAN fHighPriority );
 void smeProcessPendingQueue( tpAniSirGlobal pMac );
 void smeReleaseCommand(tpAniSirGlobal pMac, tSmeCmd *pCmd);
-void purgeSmeSessionCmdList(tpAniSirGlobal pMac, tANI_U32 sessionId);
+void purgeSmeSessionCmdList(tpAniSirGlobal pMac, tANI_U32 sessionId,
+        tDblLinkList *pList);
 tANI_BOOLEAN smeCommandPending(tpAniSirGlobal pMac);
 tANI_BOOLEAN pmcProcessCommand( tpAniSirGlobal pMac, tSmeCmd *pCommand );
 //this function is used to abort a command where the normal processing of the command
