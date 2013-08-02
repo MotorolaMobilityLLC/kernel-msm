MiCode/Xiaomi_Kernel_OpenSource

msm: bam_dmux: trigger modem subsystem restart in case of timeout
If bam_dmux times out waiting for a power collapse protocol response from
the modem, bam_dmux assumes the modem has crashed and will disable itself
until the subsystem restart callback is triggered.

Change the behavior in bam_dmux to request that the modem subsystem
restarts regardless of its current state so that the restart callback is
always triggered.

CRs-Fixed: 495690
Change-Id: I076ab16dd727694feb68b9e9052b1b6a60a2b0ca
Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

arch/arm/mach-msm/bam_dmux.c
@@ -1576,8 +1576,14 @@ static void ul_timeout(struct work_struct *work)
 
 static int ssrestart_check(void)
 {
-	DMUX_LOG_KERR("%s: modem timeout: BAM DMUX disabled\n", __func__);
+	int ret = 0;
+
+	DMUX_LOG_KERR("%s: modem timeout: BAM DMUX disabled for SSR\n",
+								__func__);
 	in_global_reset = 1;
+	ret = subsystem_restart("modem");
+	if (ret == -ENODEV)
+		panic("modem subsystem restart failed\n");
 	return 1;
 }
 

MotorolaMobilityLLC/kernel-msm

(cherry picked from commit 747eeec6978b88f8d0ddb954cc2af77d003b2f30)
Reviewed-on: http://gerrit.pcs.mot.com/554727
Submit-Approved: Jira Key <jirakey@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Jayachandran C <xnq784@motorola.com>
Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
Reviewed-by: Igor Kovalenko <cik009@motorola.com>
Reviewed-by: Sathish Karunakaran <w18372@motorola.com>
SLT-Approved: Christopher Fries <c.fries@motorola.com>

arch/arm/mach-msm/bam_dmux.c
@@ -1708,10 +1708,16 @@ static void ul_timeout(struct work_struct *work)
 
 static int ssrestart_check(void)
 {
-	DMUX_LOG_KERR("%s: modem timeout: BAM DMUX disabled\n", __func__);
+	int ret = 0;
+
+	DMUX_LOG_KERR("%s: modem timeout: BAM DMUX disabled for SSR\n",
+								__func__);
 	in_global_reset = 1;
-	if (get_restart_level() <= RESET_SOC)
-		DMUX_LOG_KERR("%s: ssrestart not enabled\n", __func__);
+	ret = subsystem_restart("modem");
+	if (ret == -ENODEV) {
+		DMUX_LOG_KERR("modem subsystem restart failed\n");
+		BUG();
+	}
 	return 1;
 }
 