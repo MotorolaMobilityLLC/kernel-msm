1fa3b0f3a928bd291d7b2965dd76eecc9511c0ca
Carter Cooper <ccooper@codeaurora.org>
2013-08-06 16:42:26 -0600

    msm: kgsl: Check the return value of _kgsl_context_get()
    Check the value returned by _kgsl_context_get() and fail if
    appropriate.  This prevents us from accidently increasing the
    ref count on a destroyed context.
    
    Change-Id: Ic0dedbad891842a73b1b87eb6671f9a39a275dd4
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -955,9 +955,10 @@ static int kgsl_release(struct inode *inodep, struct file *filep)
 			 * tries to put it while we are detaching
 			 */
 
-			_kgsl_context_get(context);
-			kgsl_context_detach(context);
-			kgsl_context_put(context);
+			if (_kgsl_context_get(context)) {
+				kgsl_context_detach(context);
+				kgsl_context_put(context);
+			}
 		}
 
 		next = next + 1;
@@ -1765,10 +1766,21 @@ static struct kgsl_cmdbatch *kgsl_cmdbatch_create(struct kgsl_device *device,
 	if (cmdbatch == NULL)
 		return ERR_PTR(-ENOMEM);
 
+	/*
+	 * Increase the reference count on the context so it doesn't disappear
+	 * during the lifetime of this command batch
+	 */
+
+	if (!_kgsl_context_get(context)) {
+		kfree(cmdbatch);
+		return ERR_PTR(-EINVAL);
+	}
+
 	if (!(flags & KGSL_CONTEXT_SYNC)) {
 		cmdbatch->ibdesc = kzalloc(sizeof(*cmdbatch->ibdesc) * numibs,
 			GFP_KERNEL);
 		if (cmdbatch->ibdesc == NULL) {
+			kgsl_context_put(context);
 			kfree(cmdbatch);
 			return ERR_PTR(-ENOMEM);
 		}
@@ -1782,12 +1794,6 @@ static struct kgsl_cmdbatch *kgsl_cmdbatch_create(struct kgsl_device *device,
 	cmdbatch->ibcount = (flags & KGSL_CONTEXT_SYNC) ? 0 : numibs;
 	cmdbatch->context = context;
 	cmdbatch->flags = flags;
-
-	/*
-	 * Increase the reference count on the context so it doesn't disappear
-	 * during the lifetime of this command batch
-	 */
-	_kgsl_context_get(context);
 
 	return cmdbatch;
 }
