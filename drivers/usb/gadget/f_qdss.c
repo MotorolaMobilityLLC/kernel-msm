6c83ebd814193ad4f663582c2ab1599509a60386
Kumar Gala <galak@codeaurora.org>
2013-08-08 15:33:30 -0500

    USB: gadget: qdss: Fix strncmp length usage
    Using a newer GCC we get the following warnings:
    
    drivers/usb/gadget/f_qdss.c: In function 'qdss_bind_config':
    drivers/usb/gadget/f_qdss.c:598: warning: argument to 'sizeof' in
    'strncmp' call is the same expression as the second source; did you mean
    to provide an explicit length? [-Wsizeof-pointer-memaccess]
    
    drivers/usb/gadget/f_qdss.c: In function 'usb_qdss_open':
    drivers/usb/gadget/f_qdss.c:760: warning: argument to 'sizeof' in
    'strncmp' call is the same expression as the second source; did you mean
    to provide an explicit length? [-Wsizeof-pointer-memaccess]
    
    These look to be due to using sizeof(ch->name) which is a pointer, use
    a simple strcmp instead.
    
    Change-Id: I09e09a18ffb3361fc351b177ed3a35432d9ed933
    Signed-off-by: Kumar Gala <galak@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/683701
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>

--- drivers/usb/gadget/f_qdss.c
+++ drivers/usb/gadget/f_qdss.c
@@ -583,7 +583,7 @@ static int qdss_bind_config(struct usb_configuration *c, const char *name)
 
 	spin_lock_irqsave(&d_lock, flags);
 	list_for_each_entry(ch, &usb_qdss_ch_list, list) {
-		if (!strncmp(name, ch->name, sizeof(ch->name))) {
+		if (!strcmp(name, ch->name)) {
 			found = 1;
 			break;
 		}
@@ -737,7 +737,7 @@ struct usb_qdss_ch *usb_qdss_open(const char *name, void *priv,
 	spin_lock_irqsave(&d_lock, flags);
 	/* Check if we already have a channel with this name */
 	list_for_each_entry(ch, &usb_qdss_ch_list, list) {
-		if (!strncmp(name, ch->name, sizeof(ch->name))) {
+		if (!strcmp(name, ch->name)) {
 			found = 1;
 			break;
 		}
