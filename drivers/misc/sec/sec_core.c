d2199fdcb410876c94b64af18900fdcbc7327b5e
Mark Reed <mreed1@motorola.com>
2013-08-16 11:51:03 -0500

    - mot: sec: fix buffer issues in mr2
    Use differnt method to allocate non-cachable physical memory
    for sharing data between kernel and trustzone.
    
    Change-Id: I697e09d6faee112946c3d33023eb0a99f27dc898
    Signed-off-by: Mark Reed <mreed1@motorola.com>
    Change-Id: Ib77bc947727723e6af361d3877c494dcea488856
    Reviewed-on: http://gerrit.pcs.mot.com/558137
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Mark Reed <mreed@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/sec/sec_core.c
+++ drivers/misc/sec/sec_core.c
@@ -31,6 +31,7 @@
 #include <linux/miscdevice.h>
 #include <linux/io.h>
 #include <mach/scm.h>
+#include <linux/memory_alloc.h>
 #include "sec_core.h"
 
 static ssize_t sec_read(struct file *filp, char *buf,
@@ -41,8 +42,16 @@ static long sec_ioctl(struct file *file, unsigned int ioctl_num,
 		unsigned long ioctl_param);
 static int sec_init(void);
 static void sec_exit(void);
+static bool sec_buf_prepare(void);
+static bool sec_buf_updated(void);
 
 #define TZBSP_SVC_OEM 254
+#define SEC_BUF_SIZE 32
+#define SEC_NO_BUFFER 0x00dead00
+
+static u8 *sec_shared_mem;
+static u32 sec_phy_mem = SEC_NO_BUFFER;
+static int sec_failures;
 
 /* Structure that declares the usual file */
 /* access functions */
@@ -51,6 +60,13 @@ const struct file_operations sec_fops = {
 	.open = sec_open,
 	.release = sec_release,
 	.unlocked_ioctl = sec_ioctl
+};
+
+struct sec_cmd {
+	int mot_cmd;
+	int parm1;
+	int parm2;
+	int parm3;
 };
 
 /* Mapping of the module init and exit functions */
@@ -80,6 +96,9 @@ int sec_init(void)
 	}
 
 	printk(KERN_INFO "Inserting sec module\n");
+
+	sec_phy_mem = SEC_NO_BUFFER;
+	sec_failures = 0;
 	return 0;
 }
 
@@ -109,69 +128,81 @@ ssize_t sec_read(struct file *filp, char *buf,
 	return 0;
 }
 
-static int data_was_updated(u8 *buffer, int size)
+static bool sec_buf_prepare()
+{
+	/* Prepare shared memory buffer */
+	if (sec_phy_mem == SEC_NO_BUFFER) {
+
+		sec_phy_mem = allocate_contiguous_ebi_nomap
+			(SEC_BUF_SIZE, SEC_BUF_SIZE);
+		sec_shared_mem = ioremap_nocache
+			(sec_phy_mem, SEC_BUF_SIZE);
+
+		if (!sec_shared_mem || !sec_phy_mem) {
+			free_contiguous_memory_by_paddr(sec_phy_mem);
+			sec_phy_mem = SEC_NO_BUFFER;
+			return false;
+		}
+	}
+
+	memset(sec_shared_mem, 0xff, SEC_BUF_SIZE);
+
+	return true;
+}
+
+static bool sec_buf_updated()
 {
 	int i;
-	for (i = 0; i < size; i++)
-		if (buffer[i] != 0xff)
-			return 1;
+	for (i = 0; i < SEC_BUF_SIZE; i++)
+		if (sec_shared_mem[i] != 0xff)
+			return true;
 
-	return 0;
+	return false;
 }
 
 long sec_ioctl(struct file *file, unsigned int ioctl_num,
 		unsigned long ioctl_param)
 {
-	struct {
-		int mot_cmd;
-		int parm1;
-		int parm2;
-		int parm3;
-	} my_cmd;
-
+	struct sec_cmd my_cmd;
 	struct SEC_EFUSE_PARM_T efuse_data;
-
-	u8 *shared_mem;
 	long ret_val = SEC_KM_FAIL;
 	u32 ctr;
 
 	mutex_lock(&sec_core_lock);
 
+	if (sec_buf_prepare() == false)
+		ioctl_num = 0;
+
 	switch (ioctl_num) {
 
 	case SEC_IOCTL_READ_PROC_ID:
 
-		for (ctr = 0; ctr < 10 && ret_val != SEC_KM_SUCCESS; ctr++) {
+		for (ctr = 0; ctr < 5 && ret_val != SEC_KM_SUCCESS; ctr++) {
 
-			shared_mem = kmalloc(SEC_PROC_ID_SIZE,
-					GFP_KERNEL | GFP_DMA);
-
-			if (!shared_mem)
-				continue;
-
-			memset(shared_mem, 0xff, SEC_PROC_ID_SIZE);
 			my_cmd.mot_cmd = 10;
-			my_cmd.parm1 = virt_to_phys(shared_mem);
+			my_cmd.parm1 = sec_phy_mem;
 			my_cmd.parm2 = SEC_PROC_ID_SIZE;
 
 			if (scm_call(TZBSP_SVC_OEM, 1, &my_cmd,
 				sizeof(my_cmd), NULL, 0) == 0) {
 
 				if (copy_to_user((void __user *)ioctl_param,
-					(const void *) shared_mem, SEC_PROC_ID_SIZE) == 0) {
+					(const void *) sec_shared_mem,
+					SEC_PROC_ID_SIZE) == 0) {
 
 					/* check data was actually updated */
-					if (data_was_updated(shared_mem,
-								SEC_PROC_ID_SIZE))
+					if (sec_buf_updated())
 						ret_val = SEC_KM_SUCCESS;
 					else
-						printk(KERN_ERR "sec: not updated.\n");
+						printk(KERN_ERR "sec: not updated\n");
+
+					ret_val = SEC_KM_SUCCESS;
 				}
 			} else
 				printk(KERN_ERR "sec: scm call failed!\n");
-
-			kfree(shared_mem);
 		}
+
+
 		break;
 
 	case SEC_IOCTL_WRITE_FUSE:
@@ -203,66 +234,91 @@ long sec_ioctl(struct file *file, unsigned int ioctl_num,
 			break;
 		}
 
-		for (ctr = 0; ctr < 10 && ret_val != SEC_KM_SUCCESS; ctr++) {
+		for (ctr = 0; ctr < 5 && ret_val != SEC_KM_SUCCESS; ctr++) {
 
-			shared_mem = kmalloc(4, GFP_KERNEL | GFP_DMA);
-
-			if (!shared_mem)
-				continue;
-
-			memset(shared_mem, 0xff, 4);
 			my_cmd.mot_cmd = 1;
 			my_cmd.parm1 = efuse_data.which_bank;
-			my_cmd.parm2 = virt_to_phys(shared_mem);
+			my_cmd.parm2 = sec_phy_mem;
 
 			if (scm_call(TZBSP_SVC_OEM, 1, &my_cmd,
 						sizeof(my_cmd), NULL, 0) == 0) {
 
-				efuse_data.efuse_value = *(u32 *)shared_mem;
+				efuse_data.efuse_value = *(u32 *)sec_shared_mem;
 
 				if (copy_to_user((void *)ioctl_param,
 					&efuse_data, sizeof(efuse_data)) == 0) {
 
 					/* check data was actually updated */
-					if (data_was_updated(shared_mem, 4))
+					if (sec_buf_updated())
 						ret_val = SEC_KM_SUCCESS;
 					else
 						printk(KERN_ERR "sec: not updated!\n");
 				}
 			} else
 				printk(KERN_ERR "sec: scm call failed!\n");
-
-			kfree(shared_mem);
 		}
 
 		break;
 
 	case SEC_IOCTL_GET_TZ_VERSION:
 
-		shared_mem = kmalloc(4, GFP_KERNEL);
 		my_cmd.mot_cmd = 11;
-		my_cmd.parm1 = virt_to_phys(shared_mem);
+		my_cmd.parm1 = sec_phy_mem;
 
 		if (scm_call(TZBSP_SVC_OEM, 1, &my_cmd,
 			sizeof(my_cmd), NULL, 0) == 0) {
 
 			if (copy_to_user((void *)ioctl_param,
-				shared_mem, 4) == 0) {
+				sec_shared_mem, 4) == 0) {
 
 				ret_val = SEC_KM_SUCCESS;
 			}
 		}
 
-		kfree(shared_mem);
+		break;
+
+
+	case SEC_IOCTL_GET_TZ_CODES:
+
+		pr_err("sec: fail counter = %d\n", sec_failures);
+		print_hab_fail_codes();
+		break;
 
 	default:
+		printk(KERN_ERR "sec: error\n");
 		break;
 	}
+
+	if (ret_val != SEC_KM_SUCCESS)
+		sec_failures++;
 
 	mutex_unlock(&sec_core_lock);
 
 	return ret_val;
 }
+
+void print_hab_fail_codes(void)
+{
+	struct sec_cmd my_cmd;
+
+	mutex_lock(&sec_core_lock);
+
+	if (sec_buf_prepare())
+	{
+		my_cmd.mot_cmd = 9;
+		my_cmd.parm1 = 16;
+		my_cmd.parm2 = sec_phy_mem;
+
+		scm_call(254, 1, &my_cmd, sizeof(my_cmd), NULL, 0);
+
+		pr_err("HAB fail codes: 0x%x 0x%x 0x%x 0x%x\n",
+		sec_shared_mem[0], sec_shared_mem[1],
+		sec_shared_mem[2], sec_shared_mem[3]);
+	}
+
+	mutex_unlock(&sec_core_lock);
+}
+EXPORT_SYMBOL(print_hab_fail_codes);
 
 /****************************************************************************/
 /*Kernel Module License Information                                         */
