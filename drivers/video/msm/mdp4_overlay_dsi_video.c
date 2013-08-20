c103842b0e01cebaec5b323e7e495a1d08a52c37
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2013-08-20 15:19:20 -0500

    Revert "(CR): msm_fb: display: checking video_wait4vsync's ret"
    This reverts commit aa644de09beaa02f9bdc8e62df968baa5afad020.
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    
    Change-Id: I687f422a76540020eff165cfa65bc38ecfd7b3a7

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -433,11 +433,8 @@ void mdp4_dsi_video_wait4vsync(int cndx)
 	/* double the timeout in vsync time stamp generation */
 	ret = wait_for_completion_interruptible_timeout(&vctrl->vsync_comp,
 		msecs_to_jiffies(VSYNC_PERIOD * 8));
-	if (ret == -ERESTARTSYS)
-		pr_warning("%s is interrupted\n", __func__);
-	else if (ret <= 0)
-		pr_err("%s timeout/error. ret = %d", __func__, ret);
-
+	if (ret <= 0)
+		pr_err("%s timeout ret=%d", __func__, ret);
 	mdp4_video_vsync_irq_ctrl(cndx, 0);
 	mdp4_stat.wait4vsync0++;
 }
