f586e5603048f2930a9ad31f5485ca114e844156
Mark Reed <mreed1@motorola.com>
2013-08-21 13:21:40 -0500

    misc: sec: relocate header for export
    Export API to dump info on trustzone image validation failure.
    
    Change-Id: I677e85acfbc4527904573e63e9b0fffe57e4e1f6
    Signed-off-by: Mark Reed <mreed1@motorola.com>

--- include/linux/sec_export.h
+++ include/linux/sec_export.h
@@ -16,20 +16,4 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define SEC_PROC_ID_SIZE   16
-
-#define SEC_IOCTL_WRITE_FUSE	_IOWR(0x99, 120, int*)
-#define SEC_IOCTL_READ_FUSE	_IOWR(0x99, 121, int*)
-#define SEC_IOCTL_READ_PROC_ID  _IOWR(0x99, 123, int*)
-#define SEC_IOCTL_GET_TZ_VERSION _IOWR(0x99, 124, int*)
-#define SEC_IOCTL_GET_TZ_CODES _IOWR(0x99, 125, int*)
-
-struct SEC_EFUSE_PARM_T {
-	unsigned int which_bank;
-	unsigned int efuse_value;
-};
-
-enum SEC_KM_RESULT_T {
-	SEC_KM_SUCCESS = 98,
-	SEC_KM_FAIL = 99,
-};
+void print_hab_fail_codes(void);
