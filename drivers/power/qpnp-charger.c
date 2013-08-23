96622da0b683dcb4424a6b757f867fb579c7d1ab
Kenny Kessler <kenny.kessler@motorola.com>
2013-08-23 10:59:46 -0500

    power: qpnp-charger: Port Chrg OCV
    This change ports the following CRs to Falcon
    (CR) and (CR)
    
    This will add the Charge OCV calculation
    which will prevent charging for 4 minutes
    on charger Insertion so that an OCV can be
    calculated.  This iwll only happen if SOC
    is between 20% and 60% and it has been 1
    week since the last calculation.  There can
    also only be 1 attempt every hour.
    
    Change-Id: Idd430fd004e8a17555f8b34d6d030777d4145d87
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -396,6 +396,20 @@ struct qpnp_chg_chip {
 	unsigned int			step_charge_mv;
 	unsigned int			step_charge_ma;
 	unsigned int			cutoff_mv;
+	struct delayed_work		chrg_ocv_work;
+	unsigned int			fcc_mah;
+	struct pc_temp_ocv_lut		*pc_temp_ocv_lut;
+	int				chrg_ocv_state;
+	int				chrg_ocv_ocv_time;
+	int				chrg_ocv_low_soc_thr;
+	int				chrg_ocv_high_soc_thr;
+	int				chrg_ocv_dchrg_thr_ma;
+	int				chrg_ocv_cc_thr_uah;
+	int				chrg_ocv_time;
+	int				chrg_ocv_cc_bf_uah;
+	int				chrg_ocv_cc_af_uah;
+	int				chrg_ocv_cc_ef_uah;
+	int				chrg_ocv_bv_mv;
 };
 
 
@@ -408,6 +422,12 @@ enum bpd_type {
 	BPD_TYPE_BAT_ID,
 	BPD_TYPE_BAT_THM,
 	BPD_TYPE_BAT_THM_BAT_ID,
+};
+
+enum chrg_ocv_states {
+	CHRG_OCV_NO_CHRG,
+	CHRG_OCV_OCV_WAIT,
+	CHRG_OCV_FULL_WAIT,
 };
 
 static const char * const bpd_label[] = {
@@ -435,6 +455,15 @@ enum usbin_health {
 	USBIN_OK,
 	USBIN_OVP,
 };
+
+static int bms_chrg_capacity;
+module_param(bms_chrg_capacity, int, 0644);
+static int bms_chrg_capacity_temp;
+module_param(bms_chrg_capacity_temp, int, 0644);
+static int chrg_timestamp;
+module_param(chrg_timestamp, int, 0644);
+MODULE_PARM_DESC(chrg_timestamp,
+		 "Epoch format timestamp chg_capacity time");
 
 static void
 qpnp_chg_set_appropriate_vddmax(struct qpnp_chg_chip *chip);
@@ -1156,7 +1185,9 @@ qpnp_arb_stop_work(struct work_struct *work)
 				struct qpnp_chg_chip, arb_stop_work);
 
 	if (!chip->chg_done || !chip->out_of_temp || !chip->ext_hi_temp)
-		qpnp_chg_charge_en(chip, !chip->charging_disabled);
+		qpnp_chg_charge_en(chip, !chip->charging_disabled &&
+				   (chip->chrg_ocv_state !=
+				    CHRG_OCV_OCV_WAIT));
 	qpnp_chg_force_run_on_batt(chip, chip->charging_disabled);
 }
 
@@ -1512,6 +1543,8 @@ qpnp_chg_regulator_batfet_set(struct qpnp_chg_chip *chip, bool enable)
 	return rc;
 }
 
+#define SECS_PER_WEEK 604800
+#define SECS_PER_HR 3600
 #define ENUM_T_STOP_BIT		BIT(0)
 static irqreturn_t
 qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
@@ -1519,6 +1552,12 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 	struct qpnp_chg_chip *chip = _chip;
 	int usb_present, host_mode, usbin_health;
 	u8 psy_health_sts;
+	struct timeval tv;
+	int chrg_ocv_time = chrg_timestamp;
+	static int chrg_ocv_try_time;
+
+	do_gettimeofday(&tv);
+	chrg_ocv_time += SECS_PER_WEEK;
 
 	usb_present = qpnp_chg_is_usb_chg_plugged_in(chip);
 	host_mode = qpnp_chg_is_otg_en_set(chip);
@@ -1561,6 +1600,7 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 			qpnp_chg_iusbmax_set(chip, QPNP_CHG_I_MAX_MIN_100);
 			chip->prev_usb_max_ma = -EINVAL;
 			chip->aicl_settled = false;
+			chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
 		} else {
 			/* when OVP clamped usbin, and then decrease
 			 * the charger voltage to lower than the OVP
@@ -1586,6 +1626,17 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 			if (!qpnp_chg_is_dc_chg_plugged_in(chip)) {
 				chip->delta_vddmax_mv = 0;
 				qpnp_chg_set_appropriate_vddmax(chip);
+			}
+			schedule_delayed_work(&chip->chrg_ocv_work,
+				msecs_to_jiffies(0));
+			if ((tv.tv_sec >= chrg_ocv_time) &&
+			    (tv.tv_sec >= (chrg_ocv_try_time + SECS_PER_HR))) {
+				chip->chrg_ocv_cc_bf_uah = 0;
+				chip->chrg_ocv_time = 0;
+				chrg_ocv_try_time = tv.tv_sec;
+				qpnp_chg_charge_en(chip, 0);
+				chip->chrg_ocv_state = CHRG_OCV_OCV_WAIT;
+				pr_info("Start Chrg OCV calculation\n");
 			}
 			schedule_delayed_work(&chip->eoc_work,
 				msecs_to_jiffies(EOC_CHECK_PERIOD_MS));
@@ -2200,6 +2251,9 @@ get_prop_batt_status(struct qpnp_chg_chip *chip)
 			&& get_batt_capacity(chip) == 100) {
 		return POWER_SUPPLY_STATUS_FULL;
 	}
+
+	if (chip->chrg_ocv_state == CHRG_OCV_OCV_WAIT)
+		return POWER_SUPPLY_STATUS_CHARGING;
 
 	return POWER_SUPPLY_STATUS_DISCHARGING;
 }
@@ -3260,6 +3314,144 @@ qpnp_chg_adjust_vddmax(struct qpnp_chg_chip *chip, int vbat_mv)
 	qpnp_chg_set_appropriate_vddmax(chip);
 }
 
+static int calculate_pc(struct qpnp_chg_chip *chip, int ocv_uv,
+							int batt_temp)
+{
+	int pc;
+
+	pc = interpolate_pc(chip->pc_temp_ocv_lut,
+			batt_temp / 10, ocv_uv / 1000);
+	pr_debug("pc = %u %% for ocv = %d uv batt_temp = %d\n",
+					pc, ocv_uv, batt_temp);
+	return pc;
+}
+
+void qpnp_chg_calculate_chrg_fcc(struct qpnp_chg_chip *chip,
+				 int ocv_mv,
+				 int ocv_cc_uah,
+				 int full_cc_uah,
+				 int full_temp_c)
+{
+	struct timeval tv;
+	int ocv_soc = calculate_pc(chip, (ocv_mv * 1000), full_temp_c);
+	unsigned int chrg_ocv_fcc = 100 * abs(full_cc_uah - ocv_cc_uah);
+	pr_info("ocv_mv = %d, ocv_soc = %d, ocv_cc = %d, full_cc = %d\n",
+		ocv_mv, ocv_soc, ocv_cc_uah, full_cc_uah);
+
+	/* Calculate Percentage of Initial FCC */
+	chrg_ocv_fcc /= (100 - ocv_soc);
+	chrg_ocv_fcc /= 10;
+	chrg_ocv_fcc /= chip->fcc_mah;
+	pr_info("Actual chrg_ocv_fcc = %d\n", chrg_ocv_fcc);
+
+	/* Round up */
+	if (chrg_ocv_fcc % 10) {
+		chrg_ocv_fcc /= 10;
+		chrg_ocv_fcc *= 10;
+		chrg_ocv_fcc += 10;
+	}
+
+	/* Max of 100 */
+	if (chrg_ocv_fcc > 100)
+		chrg_ocv_fcc = 100;
+
+	/* Mark Time Stamp */
+	do_gettimeofday(&tv);
+	chrg_timestamp = tv.tv_sec;
+
+	bms_chrg_capacity = (int)chrg_ocv_fcc;
+	bms_chrg_capacity_temp = full_temp_c;
+
+	pr_info("bms_chrg_capacity = %d, bms_chrg_capacity_temp = %d\n",
+		bms_chrg_capacity, bms_chrg_capacity_temp);
+}
+
+#define CHRG_OCV_WORK_MS 1000
+static void
+qpnp_chrg_ocv_work(struct work_struct *work)
+{
+	struct delayed_work *dwork = to_delayed_work(work);
+	struct qpnp_chg_chip *chip = container_of(dwork,
+				struct qpnp_chg_chip, chrg_ocv_work);
+	int ibat_ma, vbat_mv = 0;
+	int cc;
+	int percent_soc;
+	int batt_temp;
+	int curr_time =
+		ktime_to_timespec(alarm_get_elapsed_realtime()).tv_sec;
+
+	if (!chip->bms_psy) {
+		chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
+		return;
+	}
+
+	ibat_ma = get_prop_current_now(chip) / 1000;
+	vbat_mv = get_prop_battery_voltage_now(chip) / 1000;
+	percent_soc = get_prop_capacity(chip);
+	batt_temp = get_prop_batt_temp(chip);
+	cc = get_prop_charge_counter(chip);
+
+	if (chip->chrg_ocv_state == CHRG_OCV_OCV_WAIT) {
+		if (chip->chrg_ocv_time == 0) {
+			chip->chrg_ocv_cc_bf_uah = cc;
+			chip->chrg_ocv_time = curr_time;
+			if ((percent_soc > chip->chrg_ocv_high_soc_thr) ||
+			    (percent_soc < chip->chrg_ocv_low_soc_thr)) {
+				chip->chrg_ocv_time = 0;
+				chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
+				qpnp_chg_charge_en(chip,
+						   (!chip->charging_disabled &&
+						    !chip->out_of_temp &&
+						    !chip->ext_hi_temp));
+				pr_info("High SOC or Low SOC - NO CHRG\n");
+				goto reschedule;
+			}
+		}
+		if (curr_time < (chip->chrg_ocv_ocv_time +
+				 chip->chrg_ocv_time)) {
+			pr_debug("OCV = %d, ibat_ma = %d\n", vbat_mv, ibat_ma);
+			goto reschedule;
+		} else {
+			chip->chrg_ocv_time = 0;
+			chip->chrg_ocv_cc_af_uah = cc;
+			chip->chrg_ocv_bv_mv = vbat_mv;
+			pr_info("chrg_ocv_bv_mv = %d\n", chip->chrg_ocv_bv_mv);
+			pr_debug("ibat_ma = %d\n", ibat_ma);
+			pr_debug("af_uah = %d\n", chip->chrg_ocv_cc_af_uah);
+			pr_debug("bf_uah = %d\n", chip->chrg_ocv_cc_bf_uah);
+			qpnp_chg_charge_en(chip, (!chip->charging_disabled &&
+						  !chip->out_of_temp &&
+						  !chip->ext_hi_temp));
+			if ((abs(ibat_ma) >= chip->chrg_ocv_dchrg_thr_ma) ||
+			    (abs(chip->chrg_ocv_cc_af_uah -
+				 chip->chrg_ocv_cc_bf_uah) >=
+			     chip->chrg_ocv_cc_thr_uah)){
+				chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
+				pr_info("CHRG_OCV_OCV_WAIT - NO CHRG\n");
+				return;
+			} else {
+				pr_debug("CHRG_OCV_OCV_WAIT - Done\n");
+				chip->chrg_ocv_state = CHRG_OCV_FULL_WAIT;
+			}
+		}
+	}
+
+	if ((chip->chrg_ocv_state == CHRG_OCV_FULL_WAIT) && chip->chg_done) {
+		qpnp_chg_calculate_chrg_fcc(chip,
+					    chip->chrg_ocv_bv_mv,
+					    chip->chrg_ocv_cc_af_uah,
+					    chip->chrg_ocv_cc_ef_uah,
+					    batt_temp);
+		chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
+		power_supply_changed(&chip->batt_psy);
+		return;
+	}
+
+reschedule:
+	schedule_delayed_work(&chip->chrg_ocv_work,
+		msecs_to_jiffies(CHRG_OCV_WORK_MS));
+	return;
+}
 #define CONSECUTIVE_COUNT	3
 #define VBATDET_MAX_ERR_MV	50
 static void
@@ -3294,7 +3486,11 @@ qpnp_eoc_work(struct work_struct *work)
 
 	qpnp_chg_charge_en(chip, (!chip->charging_disabled &&
 				  !chip->out_of_temp &&
-				  !chip->ext_hi_temp));
+				  !chip->ext_hi_temp &&
+				  (chip->chrg_ocv_state != CHRG_OCV_OCV_WAIT)));
+
+	if (chip->chrg_ocv_state == CHRG_OCV_OCV_WAIT)
+		goto check_again_later;
 
 	rc = qpnp_chg_read(chip, &batt_sts, INT_RT_STS(chip->bat_if_base), 1);
 	if (rc) {
@@ -3390,6 +3586,8 @@ qpnp_eoc_work(struct work_struct *work)
 				}
 				chip->delta_vddmax_mv = 0;
 				qpnp_chg_set_appropriate_vddmax(chip);
+				chip->chrg_ocv_cc_ef_uah =
+					get_prop_charge_counter(chip);
 				qpnp_chg_charge_en(chip, 0);
 				/* sleep for a second before enabling */
 				msleep(2000);
@@ -3592,7 +3790,9 @@ qpnp_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 		qpnp_chg_set_appropriate_vbatdet(chip);
 		qpnp_chg_charge_en(chip, (!chip->charging_disabled &&
 					  !chip->out_of_temp &&
-					  !chip->ext_hi_temp));
+					  !chip->ext_hi_temp &&
+					  (chip->chrg_ocv_state !=
+					   CHRG_OCV_OCV_WAIT)));
 	}
 
 	pr_debug("warm %d, cool %d, low = %d deciDegC, high = %d deciDegC\n",
@@ -4280,7 +4480,9 @@ qpnp_chg_load_battery_data(struct qpnp_chg_chip *chip)
 						LR_MUX2_BAT_ID, rc);
 			return rc;
 		}
-
+		chip->pc_temp_ocv_lut =
+			kzalloc(sizeof(struct pc_temp_ocv_lut), GFP_KERNEL);
+		batt_data.pc_temp_ocv_lut = chip->pc_temp_ocv_lut;
 		batt_data.max_voltage_uv = -1;
 		batt_data.iterm_ua = -1;
 		rc = of_batterydata_read_data(node,
@@ -4295,6 +4497,7 @@ qpnp_chg_load_battery_data(struct qpnp_chg_chip *chip)
 			(int)batt_data.max_voltage_uv,
 			(int)batt_data.iterm_ua);
 
+		chip->fcc_mah = batt_data.fcc;
 		if (batt_data.max_voltage_uv >= 0) {
 			chip->max_voltage_mv = batt_data.max_voltage_uv / 1000;
 			chip->safe_voltage_mv = chip->max_voltage_mv
@@ -4305,6 +4508,7 @@ qpnp_chg_load_battery_data(struct qpnp_chg_chip *chip)
 
 		if (batt_data.cutoff_uv >= 0)
 			chip->cutoff_mv = batt_data.cutoff_uv / 1000;
+		batt_data.pc_temp_ocv_lut = NULL;
 	}
 
 	return 0;
@@ -4798,6 +5002,18 @@ qpnp_charger_probe(struct spmi_device *spmi)
 	chip->spmi = spmi;
 	chip->charger_enable_set = 0;
 
+	chip->chrg_ocv_state = CHRG_OCV_NO_CHRG;
+	chip->chrg_ocv_ocv_time = 240;
+	chip->chrg_ocv_low_soc_thr = 20;
+	chip->chrg_ocv_high_soc_thr = 60;
+	chip->chrg_ocv_dchrg_thr_ma = 20;
+	chip->chrg_ocv_cc_thr_uah = 1320;
+	chip->chrg_ocv_time = 0;
+	chip->chrg_ocv_cc_bf_uah = 0;
+	chip->chrg_ocv_cc_af_uah = 0;
+	chip->chrg_ocv_cc_ef_uah = 0;
+	chip->chrg_ocv_bv_mv = 0;
+
 	chip->usb_psy = power_supply_get_by_name("usb");
 	if (!chip->usb_psy) {
 		pr_err("usb supply not found deferring probe\n");
@@ -5039,6 +5255,7 @@ qpnp_charger_probe(struct spmi_device *spmi)
 	}
 
 	INIT_DELAYED_WORK(&chip->eoc_work, qpnp_eoc_work);
+	INIT_DELAYED_WORK(&chip->chrg_ocv_work, qpnp_chrg_ocv_work);
 	INIT_DELAYED_WORK(&chip->arb_stop_work, qpnp_arb_stop_work);
 	INIT_DELAYED_WORK(&chip->usbin_health_check,
 			qpnp_usbin_health_check_work);
@@ -5176,9 +5393,11 @@ qpnp_charger_remove(struct spmi_device *spmi)
 	mutex_destroy(&chip->batfet_vreg_lock);
 	mutex_destroy(&chip->jeita_configure_lock);
 
+	cancel_delayed_work_sync(&chip->chrg_ocv_work);
 	regulator_unregister(chip->otg_vreg.rdev);
 	regulator_unregister(chip->boost_vreg.rdev);
 
+	kfree(chip->pc_temp_ocv_lut);
 	return 0;
 }
 
