09e4cc62d1caf6ca6d410dba23a0ec6896028f3c
Konstantin Makariev <hcv867@motorola.com>
2013-08-26 13:30:34 -0500

    input: atmel_mxt_ts: switch code baseline
    Resetting atmel_mxt_ts.c to commit id 28ac293 to adopt code line
    from git://github.com/ndyer/linux. Atmel_mxt_ts code has advanced
    significantly and is a better match for Atmel mXT 540S chipset.
    
    Change-Id: I7c21211228d26d25d8d5131130ccefb2a75c61ce
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>

--- include/linux/i2c/atmel_mxt_ts.h
+++ include/linux/i2c/atmel_mxt_ts.h
@@ -3,7 +3,6 @@
  *
  * Copyright (C) 2010 Samsung Electronics Co.Ltd
  * Author: Joonyoung Shim <jy0922.shim@samsung.com>
- * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
@@ -26,63 +25,20 @@
 #define MXT_ROTATED_180		0x6
 #define MXT_DIAGONAL_COUNTER	0x7
 
-/* MXT_TOUCH_KEYARRAY_T15 */
-#define MXT_KEYARRAY_MAX_KEYS	32
-
-/* Bootoader IDs */
-#define MXT_BOOTLOADER_ID_224		0x0A
-#define MXT_BOOTLOADER_ID_224E		0x06
-#define MXT_BOOTLOADER_ID_336S		0x1A
-#define MXT_BOOTLOADER_ID_1386		0x01
-#define MXT_BOOTLOADER_ID_1386E		0x10
-#define MXT_BOOTLOADER_ID_1664S		0x14
-
-/* Config data for a given maXTouch controller with a specific firmware */
-struct mxt_config_info {
-	const u8 *config;
-	size_t config_length;
-	u8 family_id;
-	u8 variant_id;
-	u8 version;
-	u8 build;
-	u8 bootldr_id;
-	/* Points to the firmware name to be upgraded to */
-	const char *fw_name;
-};
-
 /* The platform data for the Atmel maXTouch touchscreen driver */
 struct mxt_platform_data {
-	const struct mxt_config_info *config_array;
-	size_t config_array_size;
+	const u8 *config;
+	size_t config_length;
 
-	/* touch panel's minimum and maximum coordinates */
-	u32 panel_minx;
-	u32 panel_maxx;
-	u32 panel_miny;
-	u32 panel_maxy;
-
-	/* display's minimum and maximum coordinates */
-	u32 disp_minx;
-	u32 disp_maxx;
-	u32 disp_miny;
-	u32 disp_maxy;
-
+	unsigned int x_line;
+	unsigned int y_line;
+	unsigned int x_size;
+	unsigned int y_size;
+	unsigned int blen;
+	unsigned int threshold;
+	unsigned int voltage;
+	unsigned char orient;
 	unsigned long irqflags;
-	bool	i2c_pull_up;
-	bool	digital_pwr_regulator;
-	int reset_gpio;
-	u32 reset_gpio_flags;
-	int irq_gpio;
-	u32 irq_gpio_flags;
-	int *key_codes;
-	bool need_calibration;
-	bool no_force_update;
-	bool no_lpm_support;
-	u8 bl_addr;
-
-	u8(*read_chg) (void);
-	int (*init_hw) (bool);
-	int (*power_on) (bool);
 };
 
 #endif /* __LINUX_ATMEL_MXT_TS_H */
