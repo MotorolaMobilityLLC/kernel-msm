8bda11a4d018207c9a87dc242b392103b54fdcf2
Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
2013-08-28 14:58:09 -0600

    msm: kgsl: Use a read/lock for the context idr
    Everybody loves a rcu but in this case we are dangerously mixing rcu's
    and atomic operations. Add a read/write lock to explicitly protect the
    idr. Also fix a few spots where the idr was used without protection.
    
    Change-Id: Ic2f1e5c45692683b52a9f662ccdd2fb59bc7ae0e
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    (cherry picked from commit 53a6f6062dc537cd72cc39785f3dda2601d35e4b)
    Reviewed-on: http://gerrit.pcs.mot.com/585985
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -295,7 +295,6 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	unsigned int no_page_fault_log = 0;
 	unsigned int curr_context_id = 0;
 	unsigned int curr_global_ts = 0;
-	static struct adreno_context *curr_context;
 	static struct kgsl_context *context;
 	unsigned int pid;
 	unsigned int fsynr0, fsynr1;
@@ -368,11 +367,10 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 
 	kgsl_sharedmem_readl(&device->memstore, &curr_context_id,
 		KGSL_MEMSTORE_OFFSET(KGSL_MEMSTORE_GLOBAL, current_context));
-	context = idr_find(&device->context_idr, curr_context_id);
+	context = kgsl_context_get(device, curr_context_id);
 
-	if ((context != NULL) && (context->devctxt != NULL)) {
-
-		curr_context = context->devctxt;
+	if (context != NULL && (context->devctxt != NULL)) {
+		struct adreno_context *drawctxt = context->devctxt;
 
 		ret = kgsl_sharedmem_readl(&device->memstore, &curr_global_ts,
 				KGSL_MEMSTORE_OFFSET(KGSL_MEMSTORE_GLOBAL, eoptimestamp));
@@ -383,13 +381,18 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 		}
 
 		/*
-		 * Store pagefault's timestamp and ib1 addr in context,
-		 * this information is used in GFT
+		 * Store pagefault's timestamp in adreno context,
+		 * this information will be used in GFT
 		 */
-		curr_context->pagefault = 1;
-		curr_context->pagefault_ts = curr_global_ts;
 
+		if (drawctxt != NULL) {
+			drawctxt->pagefault = 1;
+			drawctxt->pagefault_ts = curr_global_ts;
+		}
+
+		kgsl_context_put(context);
 	}
+
 
 	trace_kgsl_mmu_pagefault(iommu_dev->kgsldev, addr,
 			kgsl_mmu_get_ptname_from_ptbase(mmu, ptbase),
