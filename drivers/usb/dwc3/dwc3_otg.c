a67e81db36a316fec4302303c927d3c100ef219c
mohamed.khadri <mohamed.khadri@lge.com>
2013-08-28 12:16:12 -0700

    usb: dwc3: suspend current restriction not required for CDP
    Reference BC1.2 - CDP is allowed to draw IDEV_CHG without
    having to be configured or follow the rules of suspend
    
    Change-Id: Ibfb702ba665e537ae164f50b18e8d51f1ee29385
    Signed-off-by: mohamed.khadri <mohamed.khadri@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -503,7 +503,7 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 
 	power_supply_set_supply_type(dotg->psy, power_supply_type);
 
-	if ((dotg->charger->chg_type == DWC3_CDP_CHARGER) && mA > 2)
+	if ((dotg->charger->chg_type == DWC3_CDP_CHARGER) && mA > 0)
 		mA = DWC3_IDEV_CHG_MAX;
 
 	if (slimport_is_connected() && mA)
