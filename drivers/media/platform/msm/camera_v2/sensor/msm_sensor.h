1ddfe7bd5d74ade82147a65599d4e9b60f9e24a4
Brian Smith <ebs011@motorola.com>
2013-08-30 10:20:14 -0400

    msm: camera: ar0543: add rev3 otp support
    Add support for rev3 OTP which includes LSC and WP
    calibration data.
    
    Change-Id: Iafcc119510fd853428242f4db7118d11a34c5102
    Signed-off-by: Brian Smith <ebs011@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/562983
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Bryan Knottek <knottek@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -49,6 +49,8 @@ struct msm_sensor_fn_t {
 	int (*sensor_power_up) (struct msm_sensor_ctrl_t *);
 	int32_t (*sensor_match_id)(struct msm_sensor_ctrl_t *s_ctrl);
 	int32_t (*sensor_get_module_info)(struct msm_sensor_ctrl_t *);
+	int32_t (*sensor_read_otp_info)(struct msm_sensor_ctrl_t *);
+	int32_t (*sensor_set_lsc)(struct msm_sensor_ctrl_t *);
 };
 
 struct msm_sensor_ctrl_t {
