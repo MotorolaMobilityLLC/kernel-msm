dd4dc7fe29ea7db5a8978a03493b1acab9c58ca6
Rom Lemarchand <romlem@google.com>
2013-09-06 12:37:51 -0700

    msm: kgsl: inherit owner from kgsl/proc debug directory
    Make all entries under kgsl/proc inherit its owner's uid/gid.
    
    Bug: 10550827
    Change-Id: Ia530f3332e1b7fe789f229bd124ccc822f2d43a6
    Signed-off-by: Rom Lemarchand <romlem@google.com>

--- drivers/gpu/msm/kgsl_debugfs.c
+++ drivers/gpu/msm/kgsl_debugfs.c
@@ -323,12 +323,21 @@ void
 kgsl_process_init_debugfs(struct kgsl_process_private *private)
 {
 	unsigned char name[16];
+	struct dentry *mem_dentry;
 
 	snprintf(name, sizeof(name), "%d", private->pid);
 
 	private->debug_root = debugfs_create_dir(name, proc_d_debugfs);
-	debugfs_create_file("mem", 0400, private->debug_root, private,
+	if (private->debug_root) {
+		private->debug_root->d_inode->i_uid = proc_d_debugfs->d_inode->i_uid;
+		private->debug_root->d_inode->i_gid = proc_d_debugfs->d_inode->i_gid;
+	}
+	mem_dentry = debugfs_create_file("mem", 0400, private->debug_root, private,
 			    &process_mem_fops);
+	if (mem_dentry) {
+		mem_dentry->d_inode->i_uid = proc_d_debugfs->d_inode->i_uid;
+		mem_dentry->d_inode->i_gid = proc_d_debugfs->d_inode->i_gid;
+	}
 }
 
 void kgsl_core_debugfs_init(void)
