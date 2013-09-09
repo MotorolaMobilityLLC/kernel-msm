cf7d1147fb613a3d8179c7664838f70883e8ce4b
santosh mahapatra <jfvq87@motorola.com>
2013-09-09 12:57:09 +0530

    IKJBMR2X-268: arm: msm: mmi: Fix max current for Charging LED
    The commit "5b5868efc38f1a98878cc8c12da00c84dd5bb9ea" enables the
    PWM LPG banks based on max current. previously without this commit,
    the led driver was enabling "PWM LPG high bank" even though the max
    current specified by the board file was only 4mA and this was functional.
    Hence we need to fix the max current in the board file that enables
    the "PWM LPG high bank", so that the Charging/SOL LED can work.
    
    Change-Id: Ic1488348c9d0de736958c8c20a6a76f8384ed4f8
    Signed-off-by: santosh mahapatra <jfvq87@motorola.com>
    Signed-off-by: Ravikumar Vembu <a18394@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/564786
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    SLTApproved: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-mmi-pmic.c
+++ arch/arm/mach-msm/board-mmi-pmic.c
@@ -369,7 +369,7 @@ void w1_gpio_enable_regulators(int enable)
 	}
 }
 
-#define PM8921_LC_LED_MAX_CURRENT	4	/* I = 4mA */
+#define PM8921_LC_LED_MAX_CURRENT	8	/* I = 8mA */
 #define PM8XXX_LED_PWM_PERIOD		1000
 #define PM8XXX_LED_PWM_DUTY_MS		20
 /**
@@ -442,14 +442,14 @@ static struct pm8xxx_led_config pm8921_led_configs[] = {
 	[1] = {
 		.id = PM8XXX_ID_LED_1,
 		.mode = PM8XXX_LED_MODE_PWM1,
-		.max_current = 8,
+		.max_current = PM8921_LC_LED_MAX_CURRENT,
 		.pwm_channel = 4,
 		.pwm_period_us = PM8XXX_LED_PWM_PERIOD,
 	},
 	[2] = {
 		.id = PM8XXX_ID_LED_2,
 		.mode = PM8XXX_LED_MODE_PWM1,
-		.max_current = 8,
+		.max_current = PM8921_LC_LED_MAX_CURRENT,
 		.pwm_channel = PM8XXX_PWM_CHANNEL_NONE,
 		.pwm_period_us = PM8XXX_LED_PWM_PERIOD,
 	},
