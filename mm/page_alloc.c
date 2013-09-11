830c279c85db7897f5d8ac51a03b761e41ed23a5
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2013-09-11 14:20:36 -0700

    mm/page_alloc.c: fix the value of fallback_migratetype in alloc_extfrag tracepoint()
    In the current code, the value of fallback_migratetype that is printed
    using the mm_page_alloc_extfrag tracepoint, is the value of the
    migratetype *after* it has been set to the preferred migratetype (if the
    ownership was changed).  Obviously that wouldn't have been the original
    intent.  (We already have a separate 'change_ownership' field to tell
    whether the ownership of the pageblock was changed from the
    fallback_migratetype to the preferred type.)
    
    The intent of the fallback_migratetype field is to show the migratetype
    from which we borrowed pages in order to satisfy the allocation request.
    So fix the code to print that value correctly.
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Cody P Schafer <cody@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1134,8 +1134,9 @@ __rmqueue_fallback(struct zone *zone, int order, int start_migratetype)
 			 */
 			set_freepage_migratetype(page, new_type);
 
-			trace_mm_page_alloc_extfrag(page, order, current_order,
-				start_migratetype, new_type);
+			trace_mm_page_alloc_extfrag(page, order,
+				current_order, start_migratetype, migratetype,
+				new_type == start_migratetype);
 
 			return page;
 		}
