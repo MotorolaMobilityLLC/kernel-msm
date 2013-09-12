a424070eb37b8994df24685df31b68a3108658a1
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-09-12 15:41:31 -0700

    Revert "staging: zram: Add auto loading of module if user opens /dev/zram."
    This reverts commit c70bda992c12e593e411c02a52e4bd6985407539.
    
    It's incorrect, Kay writes:
    	Please just remove it. "devname" is meant to be used for
    	single-instance devices with a static dev_t, never for things
    	like zramX.
    
    	It will not do anything useful here, it does nothing really
    	without a statically assigned dev_t, and it should not be used
    	for devices of this kind anyway.
    
    Reported-by: Tom Gundersen <teg@jklm.no>
    Reported-by: Kay Sievers <kay@vrfy.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1335,4 +1335,3 @@ MODULE_PARM_DESC(num_devices, "Number of zram devices");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Nitin Gupta <ngupta@vflare.org>");
 MODULE_DESCRIPTION("Compressed RAM Block Device");
-MODULE_ALIAS("devname:zram");
