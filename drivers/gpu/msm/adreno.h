aecdd8feadd1206adecfc1a7b50abb2664c2467c
Richard Ruigrok <rruigrok@codeaurora.org>
2013-09-13 18:09:49 -0600

    Execute shader on wakeup from power collapse
    After wakeup from power-collapse, a shader execution is needed
    to reset the GPU to a known and stable state on certain targets.
    This change implements the required shader execution only
    on initialization and on recovery from power-collapse on A3xx
    GPU targets.
    
    CRs-fixed: 526544
    
    Change-Id: Ibd33578ecbb94df5ff276d1e460d94235a82dc43
    Signed-off-by: Richard Ruigrok <rruigrok@codeaurora.org>
    Signed-off-by: Ananta Kishore K <akollipa@codeaurora.org>
    (cherry picked from commit 28eabf6987637c6c438c461aa31ce25e0f7edcc5)
    Reviewed-on: http://gerrit.pcs.mot.com/585992
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -35,6 +35,7 @@
 #define KGSL_CMD_FLAGS_PMODE		0x00000001
 #define KGSL_CMD_FLAGS_INTERNAL_ISSUE	0x00000002
 #define KGSL_CMD_FLAGS_GET_INT		0x00000004
+#define KGSL_CMD_FLAGS_PWRON_FIXUP      0x00000008
 #define KGSL_CMD_FLAGS_EOF	        0x00000100
 
 /* Command identifiers */
@@ -45,6 +46,7 @@
 #define KGSL_END_OF_IB_IDENTIFIER	0x2ABEDEAD
 #define KGSL_END_OF_FRAME_IDENTIFIER	0x2E0F2E0F
 #define KGSL_NOP_IB_IDENTIFIER	        0x20F20F20
+#define KGSL_PWRON_FIXUP_IDENTIFIER	0x2AFAFAFA
 
 #ifdef CONFIG_MSM_SCM
 #define ADRENO_DEFAULT_PWRSCALE_POLICY  (&kgsl_pwrscale_policy_tz)
@@ -80,6 +82,7 @@ struct adreno_gpudev;
 
 struct adreno_device {
 	struct kgsl_device dev;    /* Must be first field in this struct */
+	unsigned long priv;
 	unsigned int chip_id;
 	enum adreno_gpurev gpurev;
 	unsigned long gmem_base;
@@ -115,6 +118,8 @@ struct adreno_device {
 	struct kgsl_memdesc on_resume_cmd;
 	unsigned int on_resume_ib[3];
 	bool on_resume_issueib;
+	struct kgsl_memdesc pwron_fixup;
+	unsigned int pwron_fixup_dwords;
 };
 
 #define PERFCOUNTER_FLAG_NONE 0x0
@@ -153,6 +158,17 @@ struct adreno_perfcount_group {
 struct adreno_perfcounters {
 	struct adreno_perfcount_group *groups;
 	unsigned int group_count;
+};
+
+/**
+ * enum adreno_device_flags - Private flags for the adreno_device
+ * @ADRENO_DEVICE_PWRON - Set during init after a power collapse
+ * @ADRENO_DEVICE_PWRON_FIXUP - Set if the target requires the shader fixup
+ * after power collapse
+ */
+enum adreno_device_flags {
+	ADRENO_DEVICE_PWRON = 0,
+	ADRENO_DEVICE_PWRON_FIXUP = 1,
 };
 
 struct adreno_gpudev {
@@ -312,6 +328,8 @@ int adreno_perfcounter_put(struct adreno_device *adreno_dev,
 
 int adreno_ft_init_sysfs(struct kgsl_device *device);
 void adreno_ft_uninit_sysfs(struct kgsl_device *device);
+
+int adreno_a3xx_pwron_fixup_init(struct adreno_device *adreno_dev);
 
 static inline int adreno_is_a200(struct adreno_device *adreno_dev)
 {
