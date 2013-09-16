81893a1fc49f5339d685e7bde730242242fe9c42
Jordan Crouse <jcrouse@codeaurora.org>
2013-09-16 12:06:45 -0600

    msm: kgsl: Fix the KGSL driver to compile when CONFIG_DEBUG_FS=n
    Fix a compilation error when CONFIG_DEBUG_FS is disabled. Add an
    initialization flag to the process struct rather than rely on a
    pointer to the debugfs node which will always be NULL if debugfs
    is not available.
    
    CRs-fixed: 542231
    Change-Id: Ic0dedbadf0241fd476e56d6e116a811697b38190
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    (cherry picked from commit 595b04e4d515f2a5a6d770cbd320f6e534232f5f)
    Reviewed-on: http://gerrit.pcs.mot.com/585989
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -54,8 +54,6 @@
 #define ADRENO_DEFAULT_PWRSCALE_POLICY  NULL
 #endif
 
-void adreno_debugfs_init(struct kgsl_device *device);
-
 #define ADRENO_ISTORE_START 0x5000 /* Istore offset */
 
 #define ADRENO_NUM_CTX_SWITCH_ALLOWED_BEFORE_DRAW	50
@@ -511,5 +509,11 @@ static inline int adreno_add_idle_cmds(struct adreno_device *adreno_dev,
 
 	return cmds - start;
 }
+
+#ifdef CONFIG_DEBUG_FS
+void adreno_debugfs_init(struct kgsl_device *device);
+#else
+static inline void adreno_debugfs_init(struct kgsl_device *device) { }
+#endif
 
 #endif /*__ADRENO_H */
