959140bd3b4ac574f25be078460884751b1a704c
Madan Mohan Koyyalamudi <mkoyya@codeaurora.org>
2013-09-16 16:04:23 -0700

    wlan: Revision 3.2.3.173
    Revision 3.2.3.173

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            172
+#define QWLAN_VERSION_BUILD            173
 
-#define QWLAN_VERSIONSTR               "3.2.3.172"
+#define QWLAN_VERSIONSTR               "3.2.3.173"
 
 #endif /* QWLAN_VERSION_H */
 
