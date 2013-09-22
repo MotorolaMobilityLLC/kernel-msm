375ad998c46a56e79f2165df1c5dd73dc25c6545
Guo-Jian Chen <a21757@motorola.com>
2013-09-22 15:39:48 -0400

    printk: Kernel panic in console_unlock
    Panic signature: printk: PC=suspend_timeout+0x20/0x28
    
    (_raw_spin_unlock_irqrestore+0x10/0x38) from [] (down_trylock+0x24/0x2c)
    (down_trylock+0x24/0x2c) from [] (console_trylock+0xc/0x50)
    (console_trylock+0xc/0x50) from [] (console_unlock+0x1ec/0x214)
    (console_unlock+0x1ec/0x214) from [] (vt_move_to_console+0x24/0xbc)
    (vt_move_to_console+0x24/0xbc) from [] (pm_restore_console+0x1c/0x2c)
    (pm_restore_console+0x1c/0x2c) from [] (pm_suspend+0xd4/0x250)
    (pm_suspend+0xd4/0x250) from [] (try_to_suspend+0x64/0xa8)
    (try_to_suspend+0x64/0xa8) from [] (process_one_work+0x200/0x400)
    (process_one_work+0x200/0x400) from [] (worker_thread+0x184/0x2a4)
    (worker_thread+0x184/0x2a4) from [] (kthread+0x80/0x90)
    (kthread+0x80/0x90) from [] (kernel_thread_exit+0x0/0x8)
    
    During the system console resume, pm_restore_console() is taking more
    than 60s, from the backtrace, it happened after the down_trylock(),
    there is a possility on SMP, printk is running on anyone of the active
    cpu, once the variable retry is set to 1, it will never get a chance to
    clear it to 0, this may cause the pm_restore_console() take more than
    60s.
    
    The 60s limitation is specific to motorola, it is an indication of
    the suspend/resume performance.
    
    (cherry picked from commit 90057e144dd2fdfabb5cbbd631a487463c381ae8)
    
    Change-Id: Ic4028bf07e29d8d2c954a727115e1661212f83a4
    Signed-off-by: Guo-Jian Chen <a21757@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/568468
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Connie Zhao <czhao1@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>
    Reviewed-by: Lian-Wei Wang <lian-wei.wang@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/571082
    SLTApproved: Slta Waiver <sltawvr@motorola.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -1418,6 +1418,8 @@ again:
 	raw_spin_lock(&logbuf_lock);
 	if (con_start != log_end)
 		retry = 1;
+	else
+		retry = 0;
 	raw_spin_unlock_irqrestore(&logbuf_lock, flags);
 
 	if (retry && console_trylock())
