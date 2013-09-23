11a480539f63b98fb25a88a3cd043b9984b9e40c
Yuan Jiangli <jlyuan@motorola.com>
2013-09-23 10:56:49 +0800

    power:qpnp-charger: Adjust schedule_work(chrg_ocv_work) position.
    chrg_ocv_work is supposed to run after chrg_ocv_state is updated during boot.
    
    Change-Id: I6f440f28ec7f3aaebe19fe2cadaa841683ec98a1
    Signed-off-by: Yuan Jiangli <jlyuan@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/568467
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    SLTApproved: Yi-Wei Zhao <gbjc64@motorola.com>
    Reviewed-by: Ke Lv <a2435c@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -1628,8 +1628,6 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 				chip->delta_vddmax_mv = 0;
 				qpnp_chg_set_appropriate_vddmax(chip);
 			}
-			schedule_delayed_work(&chip->chrg_ocv_work,
-				msecs_to_jiffies(0));
 			if ((tv.tv_sec >= chrg_ocv_time) &&
 			    (tv.tv_sec >= (chrg_ocv_try_time + SECS_PER_HR))) {
 				chip->chrg_ocv_cc_bf_uah = 0;
@@ -1639,6 +1637,8 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 				chip->chrg_ocv_state = CHRG_OCV_OCV_WAIT;
 				pr_info("Start Chrg OCV calculation\n");
 			}
+			schedule_delayed_work(&chip->chrg_ocv_work,
+				msecs_to_jiffies(0));
 			schedule_delayed_work(&chip->eoc_work,
 				msecs_to_jiffies(EOC_CHECK_PERIOD_MS));
 			schedule_work(&chip->soc_check_work);
