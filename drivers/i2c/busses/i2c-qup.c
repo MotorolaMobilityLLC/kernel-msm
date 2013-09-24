ed24cc3aa49a01fb0341d2efe84f93918bb2212c
Jared Suttles <jsuttles@motorola.com>
2013-09-24 10:59:24 -0500

    i2c: qup: change suspend/resume to be suspend_late/resume_early
    This allows us to have i2c up and running for drivers to use in
    quickwakeup scenarios.  Specifically, the msp430 driver requires i2c to
    determine whether or not to fully resume or to suspend again.
    
    Change-Id: I442e48f29a02a35b9886080c1692022126f51dd2
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Signed-off-by: Joe Swantek <w98568@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/584480
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -1501,10 +1501,13 @@ static int qup_i2c_resume(struct device *device)
 #endif /* CONFIG_PM */
 
 static const struct dev_pm_ops i2c_qup_dev_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(
-		qup_i2c_suspend,
-		qup_i2c_resume
-	)
+	.resume_early = qup_i2c_resume,
+	.suspend_late = qup_i2c_suspend,
+	.freeze = qup_i2c_suspend,
+	.thaw = qup_i2c_resume,
+	.poweroff = qup_i2c_suspend,
+	.restore = qup_i2c_resume,
+
 	SET_RUNTIME_PM_OPS(
 		i2c_qup_pm_suspend_runtime,
 		i2c_qup_pm_resume_runtime,
