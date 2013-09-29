da0fdaf8a1ff7e3411c8d0f378fe7ee2bad9d5b8
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2013-09-29 16:35:19 +0100

    extcon: arizona: Add support for headphone detection on wm5110 rev D
    Change-Id: I0a715b952a10e4dd720100c9d7509ffa21f9b4e5
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    (cherry picked from commit 0480d648e7d1703c2b79462cad147aecfa414c4d)

--- drivers/extcon/extcon-arizona.c
+++ drivers/extcon/extcon-arizona.c
@@ -437,6 +437,7 @@ static int arizona_hpdet_read(struct arizona_extcon_info *info)
 		}
 
 		val &= ARIZONA_HP_LVL_B_MASK;
+		val /= 2;
 
 		regmap_read(arizona->regmap, ARIZONA_HEADPHONE_DETECT_1,
 			    &range);
@@ -1126,6 +1127,16 @@ static int arizona_extcon_probe(struct platform_device *pdev)
 		default:
 			info->micd_clamp = true;
 			info->hpdet_ip = 1;
+			break;
+		}
+		break;
+	case WM5110:
+		switch (arizona->rev) {
+		case 0 ... 2:
+			break;
+		default:
+			info->micd_clamp = true;
+			info->hpdet_ip = 2;
 			break;
 		}
 		break;
