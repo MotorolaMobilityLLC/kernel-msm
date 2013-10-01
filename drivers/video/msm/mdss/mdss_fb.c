381ce030a324bf2fe34f6602e7c0b12c0f3a867d
Naseer Ahmed <naseer@codeaurora.org>
2013-10-01 16:19:29 -0400

    mdss: Validate sync timeline object before inc
    An earlier commit - 4b7db574f061e13857e3780ceddd24ab0681628b
    removed this by mistake. Restore this check.
    
    Bug: 10953656
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1297,7 +1297,9 @@ void mdss_fb_signal_timeline(struct msm_sync_pt_data *sync_pt_data)
 {
 	mutex_lock(&sync_pt_data->sync_mutex);
 	if (atomic_add_unless(&sync_pt_data->commit_cnt, -1, 0) &&
-			sync_pt_data->timeline) {
+			sync_pt_data->timeline &&
+			!list_empty((const struct list_head *)
+			(&(sync_pt_data->timeline->obj.active_list_head)))) {
 		sw_sync_timeline_inc(sync_pt_data->timeline, 1);
 		sync_pt_data->timeline_value++;
 
