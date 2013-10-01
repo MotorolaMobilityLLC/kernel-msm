6cf9f8be4d9f0cbcc3d32c3c0640720120f1908c
Naseer Ahmed <naseer@codeaurora.org>
2013-10-01 18:00:55 -0400

    msm: mdss: add wait4pingpong in stop func
    stop function may shut down mdp clocks while display thread kicked off
    a frame and is waiting for pingpong done. Adding this wait to avoid
    the race condition.
    
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -485,6 +485,8 @@ int mdss_mdp_cmd_stop(struct mdss_mdp_ctl *ctl)
 	if (cancel_work_sync(&ctx->clk_work))
 		pr_debug("no pending clk work\n");
 
+	mdss_mdp_cmd_wait4pingpong(ctl, NULL);
+
 	mdss_mdp_cmd_clk_off(ctx);
 
 	ctx->panel_on = 0;
