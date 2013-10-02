dfb91bd66664182790029e8d5355e9f2b8e9787c
Ravikumar Vembu <a18394@motorola.com>
2013-10-02 17:14:31 -0500

    power: Vote to stay awake in the USB_IN interrupt
    The vote to stay awake when a charger is plugged in is done in the
    fast charge irq. But since fast charge irq is not wakeable, there are
    cases where the system can go to sleep after charger detection is
    complete and the eoc work never runs until a further wakeup happens
    subsequently. To avoid this we vote to stay awake in the usb_in_valid
    interrupt when a usb or charger is plugged in.
    
    Change-Id: I764cb911c5bcc4cb97f151d9838832fb9937f1e5
    Signed-off-by: Ravikumar Vembu <a18394@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/571503
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -1641,6 +1641,7 @@ qpnp_chg_usb_usbin_valid_irq_handler(int irq, void *_chip)
 				chip->chrg_ocv_state = CHRG_OCV_OCV_WAIT;
 				pr_info("Start Chrg OCV calculation\n");
 			}
+			pm_stay_awake(chip->dev);
 			schedule_delayed_work(&chip->chrg_ocv_work,
 				msecs_to_jiffies(0));
 			schedule_delayed_work(&chip->eoc_work,
@@ -1751,6 +1752,7 @@ qpnp_chg_dc_dcin_valid_irq_handler(int irq, void *_chip)
 				chip->delta_vddmax_mv = 0;
 				qpnp_chg_set_appropriate_vddmax(chip);
 			}
+			pm_stay_awake(chip->dev);
 			schedule_delayed_work(&chip->eoc_work,
 				msecs_to_jiffies(EOC_CHECK_PERIOD_MS));
 			schedule_work(&chip->soc_check_work);
