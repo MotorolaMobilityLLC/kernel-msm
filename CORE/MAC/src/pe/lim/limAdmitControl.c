f15dea1a2ccdef8d1e3b5e2b8ab21ff6d73dd7c4
Madan Mohan Koyyalamudi <mkoyyala@qca.qualcomm.com>
2013-10-07 04:20:49 +0530

    wlan : wmm : Switch from UAPSD to legacy power save with DelTS
    With the existing support, STA does not switch back from UAPSD to
    legacy power when AP sends DelTSpec for a tid. And Exit UAPSD
    doesnot happen when processing the DelTspec received from AP.
    Changes made in address these issues.
    
    Change-Id: I7975dbf72e4097aef9d3648af82959ab70f3f278
    CRs-fixed: 550442

--- CORE/MAC/src/pe/lim/limAdmitControl.c
+++ CORE/MAC/src/pe/lim/limAdmitControl.c
@@ -1149,7 +1149,8 @@ limSendHalMsgDelTs(
   tANI_U16       staIdx,
   tANI_U8         tspecIdx,
   tSirDeltsReqInfo delts,
-  tANI_U8        sessionId)
+  tANI_U8        sessionId,
+  tANI_U8        *bssId)
 {
   tSirMsgQ msg;
   tpDelTsParams pDelTsParam;
@@ -1169,6 +1170,7 @@ limSendHalMsgDelTs(
   //filling message parameters.
   pDelTsParam->staIdx = staIdx;
   pDelTsParam->tspecIdx = tspecIdx;
+  vos_mem_copy(&pDelTsParam->bssId, bssId, sizeof(tSirMacAddr));
 
   PELOGW(limLog(pMac, LOGW, FL("calling wdaPostCtrlMsg()"));)
   MTRACE(macTraceMsgTx(pMac, sessionId, msg.type));
