ad7dbc836e3c65ff13e22af9ae0835ffe00440b5
Kiran Kumar Lokere <klokere@qca.qualcomm.com>
2013-10-07 20:12:50 -0700

    Add the fix that was deleted in other change
    Added the deleted changes for probe req data rate issue on 5G fix
    
    Change-Id: I4312091bf8f4935b09a04d0398f887da05b19fdc
    CRs-Fixed: 497478

--- CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -4543,7 +4543,7 @@ __limProcessSmeAddStaSelfReq(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
    }
 
    vos_mem_copy( pAddStaSelfParams->selfMacAddr, pSmeReq->selfMacAddr, sizeof(tSirMacAddr) );
-
+   pAddStaSelfParams->currDeviceMode = pSmeReq->currDeviceMode;
    msg.type = SIR_HAL_ADD_STA_SELF_REQ;
    msg.reserved = 0;
    msg.bodyptr =  pAddStaSelfParams;
