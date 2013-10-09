840b2eda1901bc3e8d59b6d45499bca7015addd6
Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
2013-10-09 12:40:05 +0530

    msm: msm_fb: Remove Warnings when required BW exceeds Max Bandwidth.
    When required BW exceeds max bandwidth, warnings are printed on the
    console which are causing performance degradation. This Patch fixes
    the issue.
    
    Change-Id: I769f55dd76a3f28639b848bd5461e2ff3abae3f5
    Signed-off-by: Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
    (cherry picked from commit 31d3e58f23d6a2732e071e70a30d4ea63f5d5434)
    Reviewed-on: http://gerrit.pcs.mot.com/600169
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3226,10 +3226,10 @@ int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd)
 		 ib_quota_port1, perf_req->mdp_ib_port1_bw);
 
 	if (ab_quota_total > mdp_max_bw)
-		pr_warn("%s: req ab bw=%llu is larger than max bw=%llu",
+		pr_debug("%s: req ab bw=%llu is larger than max bw=%llu",
 			__func__, ab_quota_total, mdp_max_bw);
 	if (ib_quota_total > mdp_max_bw)
-		pr_warn("%s: req ib bw=%llu is larger than max bw=%llu",
+		pr_debug("%s: req ib bw=%llu is larger than max bw=%llu",
 			__func__, ib_quota_total, mdp_max_bw);
 
 	pr_debug("%s %d: pid %d cnt %d clk %d ov0_blt %d, ov1_blt %d\n",
