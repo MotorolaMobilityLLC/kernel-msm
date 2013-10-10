0eafe608e38a6a87183f5e40d93ae3195e4951a6
Ajay Dudani <adudani@codeaurora.org>
2013-10-09 23:08:30 -0700

    Revert "msm: pm-8x60: Don't save or restore GIC,VFP state in power collapse"
    Save and restore of GIC and VFP state is required to avoid subtle
    userspace crashes due to corrupted state across power-collapse
    
    This reverts commit ddc642d6dcc71ebf65cda22287a0aa468218e994
    
    CRs-Fixed: 550405
    Change-Id: I1ebe5666dc858116ddc5b830d706e3dacbb16cbe
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -480,6 +480,10 @@ static bool __ref msm_pm_spm_power_collapse(
 	void *entry;
 	bool collapsed = 0;
 	int ret;
+	unsigned int saved_gic_cpu_ctrl;
+
+	saved_gic_cpu_ctrl = readl_relaxed(MSM_QGIC_CPU_BASE + GIC_CPU_CTRL);
+	mb();
 
 	if (MSM_PM_DEBUG_POWER_COLLAPSE & msm_pm_debug_mask)
 		pr_info("CPU%u: %s: notify_rpm %d\n",
@@ -502,6 +506,9 @@ static bool __ref msm_pm_spm_power_collapse(
 	if (from_idle && msm_pm_pc_reset_timer)
 		clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER, &cpu);
 
+#ifdef CONFIG_VFP
+	vfp_pm_suspend();
+#endif
 	collapsed = msm_pm_collapse();
 
 	if (from_idle && msm_pm_pc_reset_timer)
@@ -510,7 +517,14 @@ static bool __ref msm_pm_spm_power_collapse(
 	msm_pm_boot_config_after_pc(cpu);
 
 	if (collapsed) {
+#ifdef CONFIG_VFP
+		vfp_pm_resume();
+#endif
 		cpu_init();
+		writel(0xF0, MSM_QGIC_CPU_BASE + GIC_CPU_PRIMASK);
+		writel_relaxed(saved_gic_cpu_ctrl,
+				MSM_QGIC_CPU_BASE + GIC_CPU_CTRL);
+		mb();
 		local_fiq_enable();
 	}
 
