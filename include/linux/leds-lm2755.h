0f998fc880fbfd36b9ac89b92200c2b0a8e459ad
Dmitry Ornatsky <cdo021c@motorola.com>
2013-10-14 11:33:33 -0500

    leds: leds-lm2755: white led support
    Change driver to use only leds configured in device
    tree.
    
    Change-Id: I7fc16a6ccc4e4cf3671cb8e035b25d2651e46a40
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/573965
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/leds-lm2755.h
+++ include/linux/leds-lm2755.h
@@ -23,30 +23,23 @@
 #ifndef __LINUX_LM2755_H__
 #define __LINUX_LM2755_H__
 
-#define LM2755_CLOCK_INT	         0
-#define LM2755_CLOCK_EXT	      0x40
+#define LM2755_CLOCK_INT	     0
+#define LM2755_CLOCK_EXT	     0x40
 #define LM2755_CHARGE_PUMP_BEFORE    0
-#define LM2755_CHARGE_PUMP_AFTER  0x80
-#define LM2755_MAX_LEVEL 0x1F
+#define LM2755_CHARGE_PUMP_AFTER     0x80
+#define LM2755_MAX_LEVEL             0x1F
 
 enum {
-	LM2755_RED,
-	LM2755_GREEN,
-	LM2755_BLUE,
+	LM2755_D1,
+	LM2755_D2,
+	LM2755_D3,
 	LM2755_NUM_LEDS
 };
-
-#define LM2755_GROUP_1 0
-#define LM2755_GROUP_2 1
-#define LM2755_GROUP_3 2
 
 struct lm2755_platform_data {
 	/* Group A, group B and group C LED names in that order */
 	const char *led_names[LM2755_NUM_LEDS];
 	const char *rgb_name;    /* RGB LED name */
-	uint8_t red_group;       /* Red group address */
-	uint8_t green_group;     /* Green group address */
-	uint8_t blue_group;      /* Blue group address */
 	uint8_t	clock_mode;      /* Internal or external */
 	uint8_t charge_pump_mode;
 	/* Minimum tstep interval in nanoseconds(!) for external clock */
