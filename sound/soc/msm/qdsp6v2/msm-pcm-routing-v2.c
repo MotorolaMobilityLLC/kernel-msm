e95b9b603abccc63f4a0db2a4a5661405056fe13
prabhu annabathula <prabhu.annabathula@motorola.com>
2013-10-18 14:49:36 -0500

    ASoC: msm: add for stereo smartboost
    added tfa9890 left and codecs dai link's and routing mixer controls
    to support stereo smartboost.
    
    Change-Id: I9732ef7bb336a4a81dd630be103bb7275a7b67e8
    Signed-off-by: prabhu annabathula <prabhu.annabathula@motorola.com>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -3949,6 +3949,86 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"SLIMBUS_0_RX", NULL, "SLIM0_RX_VI_FB_LCH_MUX"},
 };
 
+#if defined(CONFIG_SND_SOC_WM5110) && defined(CONFIG_SND_SOC_TFA9890)
+static int tfa9890_wm5110_active;
+
+static int msm_tfa9890_routing_get(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_value *ucontrol)
+{
+	ucontrol->value.integer.value[0] = tfa9890_wm5110_active;
+
+	return 0;
+}
+
+static int msm_tfa9890_routing_put(struct snd_kcontrol *kcontrol,
+			struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_dapm_widget_list *wlist = snd_kcontrol_chip(kcontrol);
+	struct snd_soc_dapm_widget *widget = wlist->widgets[0];
+
+	if (ucontrol->value.integer.value[0])
+		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+	else
+		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+
+	tfa9890_wm5110_active = ucontrol->value.integer.value[0];
+
+	return 1;
+}
+
+static const struct snd_kcontrol_new tfa9890_rx_mixer_controls[] = {
+	SOC_SINGLE_EXT("TFA9890_LEFT", 0,
+		0, 1, 0, msm_tfa9890_routing_get, msm_tfa9890_routing_put),
+	SOC_SINGLE_EXT("TFA9890_RIGHT", 0,
+		0, 1, 0, msm_tfa9890_routing_get, msm_tfa9890_routing_put),
+};
+
+/* platform widgets to help with tfa9890<->wm5110 (codec-codec) linking
+ * and to trigger tfa9890 BE DAI, when playback streams are started.
+*/
+static const struct snd_soc_dapm_widget msm8974_wm5110_tfa9890_widgets[] = {
+	SND_SOC_DAPM_AIF_OUT("TFA9890_WM5110_RXL",
+			"codec-codec link left Playback",
+			0, 0, 0, 0),
+	SND_SOC_DAPM_AIF_OUT("TFA9890_WM5110_RXR",
+			"codec-codec link right Playback",
+			0, 0, 0, 0),
+	SND_SOC_DAPM_MIXER("TFA9890_WM5110_RX Mixer", SND_SOC_NOPM, 0, 0,
+			tfa9890_rx_mixer_controls,
+			ARRAY_SIZE(tfa9890_rx_mixer_controls)),
+};
+
+static const struct snd_soc_dapm_route wm5110_tfa9890_routes[] = {
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL1"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL2"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL3"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL4"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL5"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL6"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL7"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "MM_DL8"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "VOIP_DL"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "CS-VOICE_DL1"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "VOICE2_DL"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_LEFT", "VoLTE_DL"},
+	{ "TFA9890_WM5110_RXL", NULL, "TFA9890_WM5110_RX Mixer"},
+
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL1"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL2"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL3"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL4"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL5"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL6"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL7"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "MM_DL8"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "VOIP_DL"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "CS-VOICE_DL1"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "VOICE2_DL"},
+	{ "TFA9890_WM5110_RX Mixer", "TFA9890_RIGHT", "VoLTE_DL"},
+	{ "TFA9890_WM5110_RXR", NULL, "TFA9890_WM5110_RX Mixer"},
+};
+#endif
+
 static int msm_pcm_routing_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params)
 {
@@ -4141,6 +4221,16 @@ static int msm_routing_probe(struct snd_soc_platform *platform)
 			   ARRAY_SIZE(msm_qdsp6_widgets));
 	snd_soc_dapm_add_routes(&platform->dapm, intercon,
 		ARRAY_SIZE(intercon));
+
+#if defined(CONFIG_SND_SOC_WM5110) && defined(CONFIG_SND_SOC_TFA9890)
+	snd_soc_dapm_new_controls(&platform->dapm,
+				msm8974_wm5110_tfa9890_widgets,
+				ARRAY_SIZE(msm8974_wm5110_tfa9890_widgets));
+
+	snd_soc_dapm_add_routes(&platform->dapm,
+				wm5110_tfa9890_routes,
+				ARRAY_SIZE(wm5110_tfa9890_routes));
+#endif
 
 	snd_soc_dapm_new_widgets(&platform->dapm);
 
