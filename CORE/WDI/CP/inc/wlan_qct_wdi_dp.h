7f774eb3f3f775a2a6f155ffc2b0bcdab4e4abc1
Kiet Lam <c_kietl@qca.qualcomm.com>
2013-10-24 11:43:30 +0530

    Transmit EAPOL packets through WQ5
    EAPOL packets are passed through the WQ5 that enables the firmware
    to not suspend WLAN access of the channel till the response is
    received. This is required during BTC, enabling the WiFi to acquire
    the radio till the response corresponding to the transmitted
    EAPOL frame is received.
    
    CRs-fixed: 570570
    Change-Id: Ib89593df32400a1adb3e2cce287dc0fffa32235a

--- CORE/WDI/CP/inc/wlan_qct_wdi_dp.h
+++ CORE/WDI/CP/inc/wlan_qct_wdi_dp.h
@@ -426,6 +426,7 @@ WDI_FillTxBd
     wpt_uint8              ucTxFlag, 
     wpt_uint8              ucProtMgmtFrame,
     wpt_uint32             uTimeStamp,
+    wpt_uint8              isEapol,
     wpt_uint8*             staIndex
 );
 
