78e8006443c8545a116e21b39f318377ec1627da
Russ W. Knize <Russ.Knize@motorola.com>
2013-11-01 16:41:21 -0500

    f2fs: improve handling of failed truncations
    When botched recovery attempts leave inodes pointing to non-existant
    data nodes, truncations are handled poorly.  This commit makes the
    handling of these more robust by not bugging out so often and
    catching and cleaning up SIT inconsistencies.
    
    Change-Id: I48a07523843bfc880749e9eea789c112ba213d9a
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/579491
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    SLTApproved: Christopher Fries <c.fries@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	fs/f2fs/node.c
    	fs/f2fs/segment.c

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -800,7 +800,11 @@ void remove_inode_page(struct inode *inode)
 		return;
 	}
 	/* 0 is possible, after f2fs_new_inode() is failed */
-	f2fs_bug_on(inode->i_blocks != 0 && inode->i_blocks != 1);
+	if (inode->i_blocks != 0 && inode->i_blocks != 1) {
+		f2fs_msg(sbi->sb, KERN_ERR, "inode %u still has %llu blocks",
+				ino, inode->i_blocks);
+		f2fs_handle_error(sbi);
+	}
 	set_new_dnode(&dn, inode, page, page, ino);
 	truncate_node(&dn);
 }
