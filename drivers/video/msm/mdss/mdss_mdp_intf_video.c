84eee96b8516023a7d86ef53df3f93a0c7b228dd
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-11-05 18:13:42 -0800

    msm: mdss: send frame events on polling mode
    Due to potential heavy load in the system, in some cases the system may
    fall into polling mode for vsyncs if interrupts are not happening fast
    enough. In such cases frame events should still be sent so that sync
    fences are properly released.
    
    Change-Id: I41903540921e9b4e9c79de3ad0b6c11c20b30520
    CRs-Fixed: 569334
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    (cherry picked from commit 6b618bba6be3646a74e69a979de137595c000b7a)
    Reviewed-on: http://gerrit.pcs.mot.com/586482
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>
    (cherry picked from commit 694cc5c486364a2af93f6bd9a74fec21535d4809)

--- drivers/video/msm/mdss/mdss_mdp_intf_video.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -444,10 +444,9 @@ static int mdss_mdp_video_wait4comp(struct mdss_mdp_ctl *ctl, void *arg)
 
 			rc = 0;
 		}
-
-		mdss_mdp_ctl_notify(ctl,
-			rc ? MDP_NOTIFY_FRAME_TIMEOUT : MDP_NOTIFY_FRAME_DONE);
 	}
+	mdss_mdp_ctl_notify(ctl,
+			rc ? MDP_NOTIFY_FRAME_TIMEOUT : MDP_NOTIFY_FRAME_DONE);
 
 	if (ctx->wait_pending) {
 		ctx->wait_pending = 0;
