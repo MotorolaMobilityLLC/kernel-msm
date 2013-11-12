654938ddafa57be8e4d6ac6c16d45ec822a0a9ae
Iliyan Malchev <malchev@google.com>
2013-11-12 11:21:18 -0800

    prima: release v3.2.3.11
    	git://codeaurora.org/external/wlan/prima.git
    
    	97b38c1 Revert "wlan: Change regulatory followed customer request"
    	8b40611 wlan: Change wlan driver version 3.2.3.10 to 3.2.3.11
    	905ce09 wlan: Do not check for mcbc flag to resume.
    	41687f0 wlan: Fix race conditions while configuring mc/bc filter
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/prima/CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            10
+#define QWLAN_VERSION_BUILD            11
 
-#define QWLAN_VERSIONSTR              "3.2.3.10"
+#define QWLAN_VERSIONSTR              "3.2.3.11"
 
 #endif /* QWLAN_VERSION_H */
 
