9b99269a832a9076b27a92360293f9eab5a2cb67
Kiet Lam <c_kietl@qca.qualcomm.com>
2013-11-14 21:44:28 +0530

    wlan: rrm: Fix Empty Beacon reports in table mode
    With the existing implementation, empty beacon report is sent
    by STA when AP requests for beacon report in table mode and when
    STA doesnot have info of any BSSs other than the one AP that it is
    connected to. Changes are made for STA to send details of the
    AP it is currently associated to.
    
    Change-Id: Ic4e3878818744cc3f550413f025328df6939d887
    CRs-fixed: 572335

--- CORE/MAC/src/pe/rrm/rrmApi.c
+++ CORE/MAC/src/pe/rrm/rrmApi.c
@@ -817,6 +817,9 @@ rrmProcessBeaconReportXmit( tpAniSirGlobal pMac,
       return eSIR_FAILURE;
    }
 
+   pBcnReport->numBssDesc = (pBcnReport->numBssDesc == RRM_BCN_RPT_NO_BSS_INFO)?
+                            RRM_BCN_RPT_MIN_RPT : pBcnReport->numBssDesc;
+
    if (NULL == pCurrentReq)
    {
       PELOGE(limLog( pMac, LOGE,
@@ -939,8 +942,12 @@ rrmProcessBeaconReportXmit( tpAniSirGlobal pMac,
 #if defined WLAN_VOWIFI_DEBUG
    PELOGE(limLog( pMac, LOGE, "Sending Action frame ");)
 #endif
-   limSendRadioMeasureReportActionFrame( pMac, pCurrentReq->dialog_token, bssDescCnt,
-            pReport, pBcnReport->bssId, pSessionEntry );
+   limSendRadioMeasureReportActionFrame( pMac,
+                                         pCurrentReq->dialog_token,
+                                         bssDescCnt,
+                                         pReport,
+                                         pBcnReport->bssId,
+                                         pSessionEntry );
 
 
    if( pBcnReport->fMeasureDone )
