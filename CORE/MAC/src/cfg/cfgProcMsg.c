ffce934d5a0474ba223dadf32223da5a350cbe7e
Kiet Lam <c_kietl@qca.qualcomm.com>
2013-11-16 22:40:57 +0530

    wlan : Replace PAL mem APIs with VOS APIs in cfg and PE utils
    The existing PE code uses PAL APIs which in turn uses VOS mem
    APIs. Replacing the PAL mem APIs with the VOS mem APIs so as to
    optimize the code size by removing the extra PAL wrappers.
    
    Change-Id: I2a578cd234feeb31d641a13e173b408926ac36b1
    CRs-Fixed: 576672

--- CORE/MAC/src/cfg/cfgProcMsg.c
+++ CORE/MAC/src/cfg/cfgProcMsg.c
@@ -111,7 +111,7 @@ cfgProcessMbMsg(tpAniSirGlobal pMac, tSirMbMsg *pMsg)
 
     if (index >= (sizeof(gCfgFunc) / sizeof(gCfgFunc[0])))
     {
-        palFreeMemory( pMac->hHdd, (void*)pMsg);
+        vos_mem_free(pMsg);
         return;
     }
     len    = pMsg->msgLen - WNI_CFG_MB_HDR_LEN;
