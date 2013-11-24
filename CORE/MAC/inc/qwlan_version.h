777725070e0f1f09277e9c53640f96a8e573a090
Kiet Lam <kietl@codeaurora.org>
2013-11-23 18:11:50 -0800

    Wlan: Revision 3.2.3.198
    Revision 3.2.3.198

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            197
+#define QWLAN_VERSION_BUILD            198
 
-#define QWLAN_VERSIONSTR               "3.2.3.197"
+#define QWLAN_VERSIONSTR               "3.2.3.198"
 
 #endif /* QWLAN_VERSION_H */
 
