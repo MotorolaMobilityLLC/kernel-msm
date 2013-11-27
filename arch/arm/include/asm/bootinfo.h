1d51f591778f68a00e7a558e8157f2b884138b04
Alexander Filonenko <vtp684@motorola.com>
2013-11-26 21:28:33 -0600

    Revert "(CR) bootinfo: add standardized bootreason"
    Revert bootinfo commit from (CR) to rework it.
    This reverts commit 94a01cefb781b266555211699bdcda8006776646.
    
    Change-Id: I165a7820d43fc9ab2a1055c6da6aa9ae69acb76c
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/585228
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>

--- arch/arm/include/asm/bootinfo.h
+++ arch/arm/include/asm/bootinfo.h
@@ -82,7 +82,6 @@
 extern struct proc_dir_entry proc_root;
 
 u32  bi_powerup_reason(void);
-const char *bi_bootreason(void);
 u32  bi_mbm_version(void);
 
 void __init bi_add_bl_build_sig(char *bld_sig);
