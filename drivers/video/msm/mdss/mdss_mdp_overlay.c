70ebfda8983d3c8eecc822c44ff061bcb0d58fb5
Fred Fettinger <fred.fettinger@motorola.com>
2013-12-02 19:04:39 -0600

    msm: mdss: use open2 and release2 to fix refcount
    Look up callers to open and release using a file* to ensure that we
    correctly determine when all files opened by a process are released.
    Once this happens, call mdp.release_fnc to free all resources associated
    with that pid. Note that the pid passed to mdp.release_fnc may not be
    the same as current->tgid.
    
    Change-Id: If6a8851a37c1a4fe6493e42e0b8f44728b445eea
    Signed-off-by: Fred Fettinger <fred.fettinger@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/585124
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1196,14 +1196,13 @@ done:
  * Release any resources allocated by calling process, this can be called
  * on fb_release to release any overlays/rotator sessions left open.
  */
-static int __mdss_mdp_overlay_release_all(struct msm_fb_data_type *mfd)
+static int __mdss_mdp_overlay_release_all(struct msm_fb_data_type *mfd, int pid)
 {
 	struct mdss_mdp_pipe *pipe;
 	struct mdss_mdp_rotator_session *rot, *tmp;
 	struct mdss_overlay_private *mdp5_data = mfd_to_mdp5_data(mfd);
 	u32 unset_ndx = 0;
 	int cnt = 0;
-	int pid = current->tgid;
 
 	pr_debug("releasing all resources for fb%d pid=%d\n", mfd->index, pid);
 
