0166e3d8f8fb8f7411e993497ef5ad147029e2a8
Zac Crosby <azc095@motorola.com>
2013-12-04 15:38:16 -0600

    msm: mdss: Enable DSI clock during TX power get/set
    Enable DSI clocks when getting & setting the DSI TX power mode. This is
    needed to prevent accessing these DSI registers while the clocks are disable
    on command mode panels.
    
    Change-Id: I969415b7d7a3a72e8faa843c6dca7bbacdbf4451
    Signed-off-by: Zac Crosby <azc095@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/586460
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -403,6 +403,7 @@ void mdss_set_tx_power_mode(int mode, struct mdss_panel_data *pdata)
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
+	mdss_dsi_clk_ctrl(ctrl_pdata, 1);
 	data = MIPI_INP((ctrl_pdata->ctrl_base) + 0x3c);
 
 	if (mode == 0)
@@ -411,6 +412,7 @@ void mdss_set_tx_power_mode(int mode, struct mdss_panel_data *pdata)
 		data |= BIT(26);
 
 	MIPI_OUTP((ctrl_pdata->ctrl_base) + 0x3c, data);
+	mdss_dsi_clk_ctrl(ctrl_pdata, 0);
 }
 
 int mdss_get_tx_power_mode(struct mdss_panel_data *pdata)
@@ -421,7 +423,9 @@ int mdss_get_tx_power_mode(struct mdss_panel_data *pdata)
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
+	mdss_dsi_clk_ctrl(ctrl_pdata, 1);
 	data = MIPI_INP((ctrl_pdata->ctrl_base) + 0x3c);
+	mdss_dsi_clk_ctrl(ctrl_pdata, 0);
 	return !!(data & BIT(26));
 }
 
