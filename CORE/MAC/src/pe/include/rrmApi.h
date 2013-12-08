588ae878e66759759ad7aaa2a3c1c1b9d33e8f01
DARAM SUDHA <dsudha@qti.qualcomm.com>
2013-12-08 07:54:56 +0530

    Logging enhancements on 11k rrmApi.c file
    As part of PE logging enhancements, more feature logging would be needed.
    Since there are many instances which have logging with PELOG1,
    defining PELOG1 might lead to phone going into download mode
    if the logs are also dynamically enabled.
    The plan is to define PELOG4 statically and have meaningful & sufficient
    feature logging in PE till the instances with PELOG1 gets thin.
    
    Change-Id: I204614ab6bd7cfcbabfbedd310498b54cfc54b7d
    CRs-Fixed: 587373

--- CORE/MAC/src/pe/include/rrmApi.h
+++ CORE/MAC/src/pe/include/rrmApi.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -47,10 +47,6 @@
   
   \brief RRM APIs
   
-   Copyright 2008 (c) Qualcomm, Incorporated.  All Rights Reserved.
-   
-   Qualcomm Confidential and Proprietary.
-  
   ========================================================================*/
 
 /* $Header$ */
@@ -64,7 +60,7 @@
 #define RRM_BCN_RPT_NO_BSS_INFO    0
 #define RRM_BCN_RPT_MIN_RPT        1
 
-tANI_U8 rrmGetMinOfMaxTxPower(tPowerdBm regMax, tPowerdBm apTxPower);
+tANI_U8 rrmGetMinOfMaxTxPower(tpAniSirGlobal pMac, tPowerdBm regMax, tPowerdBm apTxPower);
 
 extern tSirRetStatus rrmInitialize(tpAniSirGlobal pMac);
 
