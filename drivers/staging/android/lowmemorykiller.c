67ce75cace5df62f3c023c166de884aac9aaa749
Yuanyuan Zhong <zyy@motorola.com>
2013-12-08 21:23:33 -0600

    staging: android: lmk: check TIF directly
    When trying to fix that lmk can't kill an exiting task
    (commit 895304e926352e0272964a421bd6271e47616ab3
    (CR) staging: android: lmk: skip if killed by lmk),
    additional check was added for the TIF_MEMDIE flag.
    However the task lock has already been taken when examining
    the task. The same helper function which tries to grab the
    task lock again will be deadlocked.
    Change to check the thread flag of thread group leader directly.
    
    Change-Id: If1f880aaca2f053a54ab38c62f275f0a714eb5d2
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/587751
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -372,7 +372,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		}
 		if (fatal_signal_pending(p) ||
 				((p->flags & PF_EXITING) &&
-					test_task_flag(p, TIF_MEMDIE))) {
+					test_tsk_thread_flag(p, TIF_MEMDIE))) {
 			lowmem_print(2, "skip slow dying process %d\n", p->pid);
 			task_unlock(p);
 			continue;
