976c5a00c4e78b5e5bdf3a33add957317641b620
AnjaneeDevi Kapparapu <akappa@codeaurora.org>
2013-12-27 19:42:24 +0530

    Wlan: Revision 3.2.3.208a
    Revision 3.2.3.208a

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            207
+#define QWLAN_VERSION_BUILD            208a
 
-#define QWLAN_VERSIONSTR               "3.2.3.207"
+#define QWLAN_VERSIONSTR               "3.2.3.208a"
 
 #endif /* QWLAN_VERSION_H */
 
