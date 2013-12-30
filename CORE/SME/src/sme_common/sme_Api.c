d0a498406706816da78fadbffd35ff464a7ca8c4
Leo Chang <schang@qca.qualcomm.com>
2013-12-30 11:41:04 -0800

    wlan: CH144 VHT80 fix
    If center CH set as CH144, VHT80 is not working.
    CH144 was missing during CH bonding mode configuration.
    Add CH144 also for channel bonding configuration.
    
    Change-Id: I2f2ae4fc4e74bdd3a8c8460c9cb1ccc83849ec3e
    CRs-fixed: 584100

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -9699,7 +9699,7 @@ VOS_STATUS sme_SelectCBMode(tHalHandle hHal, eCsrPhyMode eCsrPhyMode, tANI_U8 ch
             PHY_QUADRUPLE_CHANNEL_20MHZ_LOW_40MHZ_HIGH -1;
       }
       else if ( channel == 48 || channel == 64 || channel == 112 ||
-            channel == 128 || channel == 161 )
+            channel == 128 || channel == 144 || channel == 161 )
       {
          smeConfig.csrConfig.channelBondingMode5GHz =
             PHY_QUADRUPLE_CHANNEL_20MHZ_HIGH_40MHZ_HIGH - 1;
