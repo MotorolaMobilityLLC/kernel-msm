3ef4e561c6678051ac2113a4ccd8629c85b82a9e
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2014-01-06 10:59:41 -0800

    qcacld: Add debug tools to system by default
    Athdiag/fwdebuglog/pktlog come by default
    in the build of apq8084.
    
    Change-Id: I129286eba19d7321baf4d37ca47a576b8e70fa27
    CRs-Fixed: 596303

--- Android.mk
+++ Android.mk
@@ -39,6 +39,44 @@ endif
 # DLKM_DIR was moved for JELLY_BEAN (PLATFORM_SDK 16)
 ifeq ($(call is-platform-sdk-version-at-least,16),true)
        DLKM_DIR := $(TOP)/device/qcom/common/dlkm
+WLAN_BLD_DIR := $(call my-dir)
+
+ifeq ($(BOARD_HAS_QCOM_WLAN), true)
+#Build/Package Libra Mono only in case of 7627 target
+#ifeq ($(call is-chipset-in-board-platform,msm7627),true)
+#        include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+#endif
+
+#Build/Package Libra and Volans Module in case of msm7x30_surf target
+ifeq ($(call is-board-platform,msm7630_surf),true)
+        include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
+#       include $(WLAN_BLD_DIR)/libra/CORE/HDD/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+endif
+
+#Build/Package Volans Module only in case of 8660 target variants
+ifeq ($(call is-board-platform,msm8660),true)
+        include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+endif
+
+#Build/Package Volans Module only in case of 7627a target
+ifeq ($(call is-board-platform,msm7627a),true)
+        include $(WLAN_BLD_DIR)/volans/CORE/HDD/src/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/ptt/Android.mk
+#        include $(WLAN_BLD_DIR)/utils/asf/src/Android.mk
+endif
+
+#Build/Package CLD Module only in case of apq8084 target
+ifeq ($(call is-board-platform,apq8084),true)
+        include $(WLAN_BLD_DIR)/qcacld-new/Android.mk
+        include $(WLAN_BLD_DIR)/qcacld-new/tools/athdiag/Android.mk
+        include $(WLAN_BLD_DIR)/qcacld-new/tools/fwdebuglog/Android.mk
+        include $(WLAN_BLD_DIR)/qcacld-new/tools/pktlog/Android.mk
 else
        DLKM_DIR := build/dlkm
 endif
