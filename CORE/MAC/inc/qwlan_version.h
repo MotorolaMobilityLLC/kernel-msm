f9beabeeb2318a18cbfed6cad31380d71b9cf7da
AnjaneeDevi Kapparapu <akappa@codeaurora.org>
2014-01-06 15:26:50 +0530

    Wlan: Revision 3.2.3.212
    Revision 3.2.3.212

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            211
+#define QWLAN_VERSION_BUILD            212
 
-#define QWLAN_VERSIONSTR               "3.2.3.211"
+#define QWLAN_VERSIONSTR               "3.2.3.212"
 
 #endif /* QWLAN_VERSION_H */
 
