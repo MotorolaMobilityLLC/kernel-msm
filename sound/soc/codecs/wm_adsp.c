88f0d64f1d8cd69dbf4295adc064fbf0dfed5b99
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-01-10 14:16:46 +0000

    ASoC: wm_adsp: Move core_ena to be co-located with start bit
    Some firmwares do not wait for the start bit before they begin
    processing, move these core enable to happen at same time as the start
    bit to ensure these firmwares behave.
    
    Change-Id: I61aaf25d19f7386cf26890e8c62eacbcc0d66af8
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    (cherry picked from commit ca56c2a5b46aee9364e91bca031914dcacf2dd50)

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -1987,13 +1987,6 @@ static void wm_adsp2_boot_work(struct work_struct *work)
 	if (ret != 0)
 		goto err;
 
-	ret = regmap_update_bits(dsp->regmap,
-				 dsp->base + ADSP2_CONTROL,
-				 ADSP2_CORE_ENA,
-				 ADSP2_CORE_ENA);
-	if (ret != 0)
-		goto err;
-
 	dsp->running = true;
 
 	return;
@@ -2085,8 +2078,8 @@ int wm_adsp2_event(struct snd_soc_dapm_widget *w,
 
 		ret = regmap_update_bits(dsp->regmap,
 					 dsp->base + ADSP2_CONTROL,
-					 ADSP2_START,
-					 ADSP2_START);
+					 ADSP2_CORE_ENA | ADSP2_START,
+					 ADSP2_CORE_ENA | ADSP2_START);
 		if (ret != 0)
 			goto err;
 		break;
