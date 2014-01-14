61ea0290ad2a2eee09bc25b61ec9ac95563a4400
Joe Swantek <w98568@motorola.com>
2014-01-14 17:43:12 -0600

    misc: msp430: fix mem leak on invalid draw/erase message
    Signed-off-by: Joe Swantek <w98568@motorola.com>
    Change-Id: Ib67e2e095ff14a69b88bf0bf7a6a1b35c0fd40f6
    Reviewed-on: http://gerrit.pcs.mot.com/600590
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/msp430.c
+++ drivers/misc/msp430.c
@@ -1858,6 +1858,7 @@ static void msp430_irq_wake_work_func(struct work_struct *work)
 					"Reading peek draw data from msp failed\n");
 				msp430_quickpeek_status_ack(ps_msp430,
 					qp_message, AOD_QP_ACK_INVALID);
+				kfree(qp_message);
 				goto EXIT;
 			}
 			qp_message->buffer_id = read_cmdbuff[0] & 0x3f;
@@ -1883,6 +1884,7 @@ static void msp430_irq_wake_work_func(struct work_struct *work)
 					"Reading peek erase data from msp failed\n");
 				msp430_quickpeek_status_ack(ps_msp430,
 					qp_message, AOD_QP_ACK_INVALID);
+				kfree(qp_message);
 				goto EXIT;
 			}
 			qp_message->x1 = read_cmdbuff[1] | read_cmdbuff[2] << 8;
