081d62bf72453d5e9605a42ca7066f6436b97419
Kiet Lam <kietl@codeaurora.org>
2014-01-14 18:29:06 -0800

    wlan: Disable NSOffload on all slots during resume.
    NSOffload is enabled on per slot basis for each IPv6 addresses
    during suspend. During resume slot index 0 was the only slot
    being disabled, which leaves other slot index still active in the
    firmware. This use to filter out MC packets being received to the
    host even when host is active/out of suspend.
    
    To overcome this, NSOffload is being disabled on all slots during
    resume.
    
    Change-Id: I821bf2f93cfacef5b0bddef01b4014178e3abe4e
    CRs-Fixed: 579662

--- CORE/HDD/src/wlan_hdd_early_suspend.c
+++ CORE/HDD/src/wlan_hdd_early_suspend.c
@@ -766,12 +766,17 @@ void hdd_conf_ns_offload(hdd_adapter_t *pAdapter, int fenable)
         offLoadRequest.enableOrDisable = SIR_OFFLOAD_DISABLE;
         offLoadRequest.offloadType =  SIR_IPV6_NS_OFFLOAD;
 
-        if (eHAL_STATUS_SUCCESS !=
+        //Disable NSOffload on all slots
+        for (i = 0; i<SIR_MAC_NUM_TARGET_IPV6_NS_OFFLOAD_NA; i++)
+        {
+            offLoadRequest.nsOffloadInfo.slotIdx = i;
+            if (eHAL_STATUS_SUCCESS !=
                  sme_SetHostOffload(WLAN_HDD_GET_HAL_CTX(pAdapter),
                  pAdapter->sessionId, &offLoadRequest))
-        {
-            hddLog(VOS_TRACE_LEVEL_ERROR, FL("Failure to disable"
-                             "NSOffload feature"));
+            {
+                hddLog(VOS_TRACE_LEVEL_ERROR, FL("Failed to disable NSOflload"
+                             " on slot %d"), i);
+            }
         }
     }
     return;
