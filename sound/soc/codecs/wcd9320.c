17101f1a458b353d2c31a20dda05282a91aac192
prabhu annabathula <prabhu.annabathula@motorola.com>
2014-01-15 19:23:51 -0600

    ASoC: wcd9320: remove calls to MBHC if using TPA6165A2
    Prop from mkk-b: 4b19759c3faf531e5e5ef285bcc78e97909475cd
    
    calling into the MBHC driver will cause panics as we are configured to use
    TPA6165A2 and the MBHC driver initialization is skipped.
    
    Signed-off-by: Rajasekhar Goparaju <chb764@motorola.com>
    Reviewed-by: Vladimir Karpovich <vkarpovich@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -1601,8 +1601,13 @@ static int taiko_hph_impedance_get(struct snd_kcontrol *kcontrol,
 	mc = (struct soc_multi_mixer_control *)(kcontrol->private_value);
 
 	hphr = mc->shift;
+	pr_debug("%s: taiko_priv: %p\n", __func__, priv);
+#ifndef CONFIG_SND_SOC_TPA6165A2
 	wcd9xxx_mbhc_get_impedance(&priv->mbhc, &zl, &zr);
 	pr_debug("%s: zl %u, zr %u\n", __func__, zl, zr);
+#else
+	zr = zl = 0;
+#endif
 	ucontrol->value.integer.value[0] = hphr ? zr : zl;
 
 	return 0;
@@ -3313,6 +3318,7 @@ static int taiko_hphl_dac_event(struct snd_soc_dapm_widget *w,
 						WCD9XXX_CLSAB_REQ_ENABLE);
 		}
 
+#ifndef CONFIG_SND_SOC_TPA6165A2
 		ret = wcd9xxx_mbhc_get_impedance(&taiko_p->mbhc,
 					&impedl, &impedr);
 		if (!ret)
@@ -3320,6 +3326,10 @@ static int taiko_hphl_dac_event(struct snd_soc_dapm_widget *w,
 		else
 			dev_err(codec->dev, "Failed to get mbhc impedance %d\n",
 						ret);
+#else
+		impedl = impedr = 0;
+		ret = 0;
+#endif
 		break;
 	case SND_SOC_DAPM_POST_PMD:
 		snd_soc_update_bits(codec, TAIKO_A_CDC_CLK_RDAC_CLK_EN_CTL,
