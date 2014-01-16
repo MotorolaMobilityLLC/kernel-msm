ef657d3ae2f4413ec0350dafb1e937595f95bdc7
Wilson Yang <c_yangw@qca.qualcomm.com>
2014-01-15 19:19:23 -0800

    wlan: broken coding rule
    In CR587500 sme_RoamDelPMKIDfromCache() is defined to return
    eHalStatus,but sme_RoamDelPMKIDfromCache() is modified
    to return -1 which is not a valid eHalStatus enumeration.
    The correct fix would be to modify wlan_hdd_cfg80211_flush_pmksa()
    such that when sme_RoamDelPMKIDfromCache()  returns an error,
    it in turn returns a negative errno value to userspace.
    
    Change-Id: I89d625fcd428bba153283089dd726066a87f6fe6
    CRs-fixed: 600824

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3303,11 +3303,6 @@ eHalStatus sme_RoamDelPMKIDfromCache( tHalHandle hHal, tANI_U8 sessionId, tANI_U
       }
       sme_ReleaseGlobalLock( &pMac->sme );
    }
-   if ( status >0 )
-   {
-      status = -1;
-   }
-
    return (status);
 }
 
