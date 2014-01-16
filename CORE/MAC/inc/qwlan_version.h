0d56b0e4bfe0827341f80a1030353f5144dae688
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-01-16 14:17:16 -0800

    Wlan: Revision 3.2.3.216f
    Revision 3.2.3.216f
    
    Change-Id: Id319268ea7cd71c5d65250f8850fc0cb721cb302

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -59,10 +59,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
-#define QWLAN_VERSION_EXTRA            "e"
+#define QWLAN_VERSION_EXTRA            "f"
 #define QWLAN_VERSION_BUILD            216
 
-#define QWLAN_VERSIONSTR               "3.2.3.216e"
+#define QWLAN_VERSIONSTR               "3.2.3.216f"
 
 #endif /* QWLAN_VERSION_H */
 
