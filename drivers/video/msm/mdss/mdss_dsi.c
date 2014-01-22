e03a3c6bab88f83f88e9116a04d915910318a3ef
Bang Nguyen <bnguyen1@motorola.com>
2014-01-22 13:39:51 -0600

    msm:mdss: Cleanup DSI code from previous ES5 merge
    - Remove double entries of mdss_panel_set_reg_boot_on() in dsi_host_v2.c
      and mdss_dsi_get_dt_vreg_data() in mdss_dsi.c
    
    Change-Id: Ib7fb01ed9d6c0a2765ead26b2a0125e7500b2e64
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1281,15 +1281,6 @@ static int __devinit mdss_dsi_ctrl_probe(struct platform_device *pdev)
 		goto error_pan_node;
 	}
 
-	/* Parse the regulator information */
-	rc = mdss_dsi_get_dt_vreg_data(&pdev->dev,
-					&ctrl_pdata->power_data, NULL);
-	if (rc) {
-		pr_err("%s: failed to get vreg data from dt. rc=%d\n",
-								__func__, rc);
-		goto error_pan_node;
-	}
-
 	cmd_cfg_cont_splash = mdss_panel_get_boot_cfg() ? true : false;
 
 	ctrl_pdata->pdev = pdev;
