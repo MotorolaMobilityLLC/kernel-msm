7a4f38748ad73b4deb8fdc765d5b5c24736680fb
choongryeol.lee <choongryeol.lee@lge.com>
2014-01-26 21:52:52 -0800

    power: qpnp-charger: fix the battery temp notification storm
    There is a corner case where the adc_tm notification happen
    repeatedly. If the battery temperature which is read in adc_tm
    callback is slightly different with adc_tm threshold, the next
    adc_tm threshold is not updated. So the adc_tm notification happen
    repeatedly. To prevent this, remove checking the battery temperature
    in the adc_tm callback. It is enough to check current adc_tm threshold
    for getting the next adc_tm threshold.
    
    Change-Id: Iecf9261124aa996c25a719f9514add74db26c505
    Signed-off-by: choongryeol.lee <choongryeol.lee@lge.com>

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -3286,7 +3286,7 @@ qpnp_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 			state == ADC_TM_WARM_STATE ? "warm" : "cool");
 
 	if (state == ADC_TM_WARM_STATE) {
-		if (temp > chip->warm_bat_decidegc) {
+		if (chip->adc_param.high_temp == chip->warm_bat_decidegc) {
 			/* Normal to warm */
 			bat_warm = true;
 			bat_cool = false;
@@ -3294,7 +3294,7 @@ qpnp_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 				chip->warm_bat_decidegc - HYSTERISIS_DECIDEGC;
 			chip->adc_param.state_request =
 				ADC_TM_COOL_THR_ENABLE;
-		} else if (temp >
+		} else if (chip->adc_param.high_temp ==
 				chip->cool_bat_decidegc + HYSTERISIS_DECIDEGC){
 			/* Cool to normal */
 			bat_warm = false;
@@ -3306,7 +3306,7 @@ qpnp_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 					ADC_TM_HIGH_LOW_THR_ENABLE;
 		}
 	} else {
-		if (temp < chip->cool_bat_decidegc) {
+		if (chip->adc_param.low_temp == chip->cool_bat_decidegc) {
 			/* Normal to cool */
 			bat_warm = false;
 			bat_cool = true;
@@ -3314,7 +3314,7 @@ qpnp_chg_adc_notification(enum qpnp_tm_state state, void *ctx)
 				chip->cool_bat_decidegc + HYSTERISIS_DECIDEGC;
 			chip->adc_param.state_request =
 				ADC_TM_WARM_THR_ENABLE;
-		} else if (temp <
+		} else if (chip->adc_param.low_temp ==
 				chip->warm_bat_decidegc - HYSTERISIS_DECIDEGC){
 			/* Warm to normal */
 			bat_warm = false;
