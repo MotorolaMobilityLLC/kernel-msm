fae26eb5030612f5ba2a7b272d826b0f5f1c714d
Leela Venkata Kiran Kumar Reddy Chirala <kchirala@qca.qualcomm.com>
2014-02-03 11:20:14 -0800

    Fix the errors identified by Static source code analysis in MAC
    This commit fixes the errors identified by the static source code
    analysis in MAC module.
    
    Change-Id: Ie77862c77d3a0627f1e8cec3cbdecded9c3d503e
    CRs-fixed: 609573

--- CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
+++ CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
@@ -421,6 +421,8 @@ limProcessMlmStartCnf(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
         /// Start BSS is a failure
         peDeleteSession(pMac,psessionEntry);
         psessionEntry = NULL;
+        PELOGE(limLog(pMac, LOGE,FL("Start BSS Failed "));)
+        return;
     }
     /// Send response to Host
     limSendSmeStartBssRsp(pMac, eWNI_SME_START_BSS_RSP,
