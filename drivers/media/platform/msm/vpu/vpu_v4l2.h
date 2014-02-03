99af859864fa58cb50d6789c8544f77bd55b5531
Stephane Viau <sviau@codeaurora.org>
2014-02-03 08:36:41 -0500

    msm: VPU: Add bus bandwidth scaling support
    MSM multimedia IP are supposed to scale the bus according to
    their instant bandwidth needs. This change adds this support by
    calculating the current VPU load and selecting the right
    bandwidth before voting for bus scaling.
    
    Change-Id: I9e1b9698b079b1924881e0c1678fe1467ee83dfa
    Signed-off-by: Stephane Viau <sviau@codeaurora.org>

--- drivers/media/platform/msm/vpu/vpu_v4l2.h
+++ drivers/media/platform/msm/vpu/vpu_v4l2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -160,7 +160,7 @@ struct vpu_dev_session {
 	/* controls handler */
 	struct vpu_controller *controller;
 
-	/* load in bits per second (bps)*/
+	/* load in kilo-bits per second (kbps)*/
 	u32 load;
 
 	/* session has dual outputs */
