99b0fb7a50f4ea164219413214b83785db6bae0e
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-02-10 10:04:39 +0000

    ASoC: arizona: Add mutex lock for enable/disable pin
    The core does not lock for snd_soc_dapm_enable_pin and the other pin
    state functions. This patch adds a mutex lock around these calls.
    
    Change-Id: I3922d8705caedc8605aecc7c0fdd62ecf1e803ee
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    (cherry picked from commit 16a304993a797578bf155980b62a1303359a3a35)

--- sound/soc/codecs/florida.c
+++ sound/soc/codecs/florida.c
@@ -1743,7 +1743,9 @@ static int florida_codec_probe(struct snd_soc_codec *codec)
 	if (ret != 0)
 		return ret;
 
+	mutex_lock(&codec->card->dapm_mutex);
 	snd_soc_dapm_disable_pin(&codec->dapm, "HAPTICS");
+	mutex_unlock(&codec->card->dapm_mutex);
 
 	priv->core.arizona->dapm = &codec->dapm;
 
@@ -1763,7 +1765,10 @@ static int florida_codec_probe(struct snd_soc_codec *codec)
 		return ret;
 	}
 
+	mutex_lock(&codec->card->dapm_mutex);
 	snd_soc_dapm_enable_pin(&codec->dapm, "DRC2 Signal Activity");
+	mutex_unlock(&codec->card->dapm_mutex);
+
 	ret = regmap_update_bits(arizona->regmap, ARIZONA_IRQ2_STATUS_3_MASK,
 				 ARIZONA_IM_DRC2_SIG_DET_EINT2,
 				 ARIZONA_IM_DRC2_SIG_DET_EINT2);
