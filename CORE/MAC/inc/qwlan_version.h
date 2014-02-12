e8d6071ecc3d9d9a32ba69ea3da9114c36cac2fd
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-02-11 22:30:38 -0800

    Wlan: Revision 3.2.3.240
    Revision 3.2.3.240

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            239
+#define QWLAN_VERSION_BUILD            240
 
-#define QWLAN_VERSIONSTR               "3.2.3.239"
+#define QWLAN_VERSIONSTR               "3.2.3.240"
 
 #endif /* QWLAN_VERSION_H */
 
