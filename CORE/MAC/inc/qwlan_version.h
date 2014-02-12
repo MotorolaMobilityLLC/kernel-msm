af6a7b1c1953d0442d9f2cdc8ff81e5f742425f3
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-02-12 12:00:10 -0800

    wlan: Release 3.2.3.241
    Revision 3.2.3.241
    
    Change-Id: I081089aed58bde8e5e8f18e8c69ea725f4426b85

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -23,10 +23,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
-#define QWLAN_VERSION_EXTRA            "b"
-#define QWLAN_VERSION_BUILD            240
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            241
 
-#define QWLAN_VERSIONSTR               "3.2.3.240b"
+#define QWLAN_VERSIONSTR               "3.2.3.241"
 
 #endif /* QWLAN_VERSION_H */
 
