32be2cd7c8fca68e416874a1fba272131688d493
Russ Knize <rknize@motorola.com>
2014-02-13 15:54:09 -0600

    IKVPREL1KK-3426: mmc: improve recovery behavior when SDCC is jammed
    Ported from IKJBREL1-6845 on K3.0.
    
    Some cards can make the SDC controller hang for no apparent reason.
    There is a catch-all timer in the host driver to recover from this.
    The elaborate retry logic in the MMC driver will cause long delays
    as it repeatedly polls the card status through a dead controller.
    The real timeout for status commands is deterministic and very
    short, so there is no need to wait 8 seconds for the SDCC on CMD13.
    
    Signed-off-by: Russ Knize <rknize@motorola.com>
    Change-Id: Iad7731168049a9416441da11213fc7123479381b
    Reviewed-on: http://gerrit.pcs.mot.com/611291
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -2251,12 +2251,20 @@ msmsdcc_request(struct mmc_host *mmc, struct mmc_request *mrq)
 	spin_lock_irqsave(&host->lock, flags);
 
 	/*
-	 * Set timeout value to 10 secs (or more in case of buggy cards)
+	 * Set the controller catch-all timer to:
+	 *  - 20s for quirky cards
+	 * otherwise:
+	 *  - 500ms for CMD13
+	 *  - 8s for everything else
 	 */
 	if ((mmc->card) && (mmc->card->quirks & MMC_QUIRK_INAND_DATA_TIMEOUT))
 		host->curr.req_tout_ms = 20000;
-	else
-		host->curr.req_tout_ms = MSM_MMC_REQ_TIMEOUT;
+	else {
+		if (mrq->cmd->opcode == MMC_SEND_STATUS)
+			host->curr.req_tout_ms = 500;
+		else
+			host->curr.req_tout_ms = MSM_MMC_REQ_TIMEOUT;
+	}
 	/*
 	 * Kick the software request timeout timer here with the timeout
 	 * value identified above
