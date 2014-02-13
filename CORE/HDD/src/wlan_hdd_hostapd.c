1953afeaa1959aab4a4f759ed938b66518da224f
Mahesh A Saptasagar <c_msapta@qti.qualcomm.com>
2014-02-13 15:05:02 +0530

    wlan: Fix for netd crash due to improper length updation.
    Netd crash due to improper length updation to the user space. i.e
    actual length of data copied to the user space buffer being
    calculated after indication of the same to the user and wrong value
    being updated to the user space.
    This commit ensures that the length of the data copied to the user
    space buffer and the length indicated to user is same.
    
    Change-Id: I0a402798f5d923eaad1a681724a63cf0cb2c2663
    CRs-Fixed: 612326

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -1777,15 +1777,15 @@ static iw_softap_getassoc_stamacaddr(struct net_device *dev,
     } 
     spin_unlock_bh( &pHostapdAdapter->staInfo_lock );
 
+    wrqu->data.length -= len;
     if (copy_to_user((void *)wrqu->data.pointer + maclist_index,
                      (void *)&maclist_null, sizeof(maclist_null)) ||
         copy_to_user((void *)wrqu->data.pointer,
-                     (void *)&wrqu->data.length, sizeof(wrqu->data.length)))
+                     (void *)&wrqu->data.length, sizeof(unsigned long int)))
     {
         hddLog(LOG1, "%s: failed to copy data to user buffer", __func__);
         return -EFAULT;
     }
-    wrqu->data.length -= len;
 
     return 0;
 }
