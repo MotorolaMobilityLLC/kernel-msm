6f714fd3d63e2b08d63b5b32b5e4583d369fee4a
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-02-14 13:58:33 +0000

    switch: arizona: Update headphone tuning to use arizona->hp_impedance
    Change-Id: I6844b3e8c862ee4e523f3be1d61586772f29ac39
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    (cherry picked from commit 0c060ce245010b9a9ab0e089d2fb32fa658e575c)

--- include/linux/mfd/arizona/core.h
+++ include/linux/mfd/arizona/core.h
@@ -81,6 +81,7 @@ enum arizona_type {
 
 #define ARIZONA_NUM_IRQ                   52
 
+#define ARIZONA_HP_SHORT_IMPEDANCE        4
 struct snd_soc_dapm_context;
 
 struct arizona {
@@ -105,7 +106,6 @@ struct arizona {
 
 	bool hpdet_magic;
 	unsigned int hp_ena;
-	unsigned int hp_short;
 
 	unsigned int hp_impedance;
 
