3ec88a884f677fc636b7ae3f99f46bf4c52ae650
Jason Hrycay <jason.hrycay@motorola.com>
2014-02-17 09:29:52 -0600

    bootinfo: Remove unnecessary conditional
    Remove the conditional compilation logic around the #defines
    in this header file so components can still compile when the
    feature is disabled.
    
    Change-Id: I46a177595552e21ca20d6d3df6199bee034379b9
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/611048
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jayachandran Chinnakkannu <jayc@motorola.com>
    Reviewed-by: Grace Chen <w30234@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 967d084980037daf65293a369af40b4e289abfe0)

--- include/soc/qcom/bootinfo.h
+++ include/soc/qcom/bootinfo.h
@@ -26,8 +26,6 @@
 #define __ASMARM_BOOTINFO_H
 
 
-#if !defined(__KERNEL__) || defined(CONFIG_BOOTINFO)
-
 /*
  * These #defines are used for the bits in powerup_reason.
  */
@@ -72,8 +70,6 @@
 #define BOOTINFO_MAX_VAL_LEN    128
 #define BOOTINFO_BUF_SIZE       (BOOTINFO_MAX_NAME_LEN + \
 					3 + BOOTINFO_MAX_VAL_LEN)
-
-#endif
 
 
 #if defined(__KERNEL__)
