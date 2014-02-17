d82bcc85b8e6a6be5b5e8752d0b5f62f62cd1495
Arun Kumar Khandavalli <c_arunku@qti.qualcomm.com>
2014-02-17 18:52:29 +0530

    wlan: make cfg.dat file read 64 bit compatible
    sizeof long on a 32 bit machine is 4 and 8 on a 64 bit machine.
    cfg.dat is generated with the assumption that long fields are 32 bit.
    Change the data types of all the long fields here to a 32 bit type.
    
    Change-Id: I159f337c08fcc01c59e8a32b9eca3b2a78f0c579
    CRs-Fixed: 612760

--- CORE/MAC/src/cfg/polFile.h
+++ CORE/MAC/src/cfg/polFile.h
@@ -124,8 +124,8 @@ typedef struct sPolFileHeader {
 
   tPolFileVersion FileVersion;
   tPolFileVersion HWCapabilities;
-  unsigned long   FileLength;
-  unsigned long   NumDirectoryEntries;
+  unsigned int   FileLength;
+  unsigned int   NumDirectoryEntries;
 
 } tPolFileHeader;
 
@@ -144,9 +144,9 @@ typedef enum ePolFileDirTypes {
 
 typedef struct sPolFileDirEntry {
 
-  unsigned long DirEntryType;
-  unsigned long DirEntryFileOffset;
-  unsigned long DirEntryLength;
+  unsigned int DirEntryType;
+  unsigned int DirEntryFileOffset;
+  unsigned int DirEntryLength;
 
 } tPolFileDirEntry;
 
