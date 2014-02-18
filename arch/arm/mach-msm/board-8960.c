1e6fe394562ce9216cf3b5ec65103905f251bf38
ndeepa <ndeepa@motorola.com>
2014-02-18 19:38:55 +0530

    IK8960KK-569: arm: msm: Change vidc reserved address to use vidc ver 1.41
    Change reserved memory settings to reserve memory
    area 0xAFE00000 + 0x20000 for vidc firmware loading
    
    Change-Id: Icf20f46dff44f2afc76171950d23020bbdb4b06f
    Signed-off-by: ndeepa <ndeepa@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/611129
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -151,7 +151,7 @@ struct sx150x_platform_data msm8960_sx150x_data[] = {
 #define MSM_HDMI_PRIM_PMEM_SIZE 0x4000000 /* 64 Mbytes */
 
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
-#define HOLE_SIZE	0x20000
+#define HOLE_SIZE	0x100000 /* 1 MB */
 #define MSM_CONTIG_MEM_SIZE  0x65000
 #ifdef CONFIG_MSM_IOMMU
 #define MSM_ION_MM_SIZE            0x3800000 /* Need to be multiple of 64K */
@@ -164,7 +164,8 @@ struct sx150x_platform_data msm8960_sx150x_data[] = {
 #define MSM_ION_QSECOM_SIZE        0x600000 /* (6MB) */
 #define MSM_ION_HEAP_NUM	8
 #endif
-#define MSM_ION_MM_FW_SIZE	(0x200000 - HOLE_SIZE) /* 128kb */
+
+#define MSM_ION_MM_FW_SIZE	0x200000 /* 2 MB */
 #define MSM_ION_MFC_SIZE	SZ_8K
 #define MSM_ION_AUDIO_SIZE	MSM_PMEM_AUDIO_SIZE
 
@@ -172,9 +173,8 @@ struct sx150x_platform_data msm8960_sx150x_data[] = {
 #define MSM_LIQUID_ION_SF_SIZE MSM_LIQUID_PMEM_SIZE
 #define MSM_HDMI_PRIM_ION_SF_SIZE MSM_HDMI_PRIM_PMEM_SIZE
 
-#define MSM_MM_FW_SIZE		(0x200000 - HOLE_SIZE) /* 2mb -128kb*/
-#define MSM8960_FIXED_AREA_START (0xa0000000 - (MSM_ION_MM_FW_SIZE + \
-							HOLE_SIZE))
+#define MSM_MM_FW_SIZE		(0x200000) /* 2mb */
+#define MSM8960_FIXED_AREA_START (0xb0000000 - MSM_ION_MM_FW_SIZE)
 #define MAX_FIXED_AREA_SIZE	0x10000000
 #define MSM8960_FW_START	MSM8960_FIXED_AREA_START
 #define MSM_ION_ADSP_SIZE	SZ_8M
