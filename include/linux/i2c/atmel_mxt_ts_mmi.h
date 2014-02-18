2f27eb1b77dd8f3707283a95c2ba2cf9fd0945fd
Alexander Filonenko <vtp684@motorola.com>
2014-02-18 09:39:18 -0600

    input: touch: create atmel_mxt_ts_mmi
    Fork atmel_mxt_ts_mmi from atmel_mxt_ts variant introduced
    and proven on 8974. Integrated the driver with Kernel 3.10.
    
    Change-Id: I6d619172787512613956063035872b2d4a7486be
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/611155
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/linux/i2c/atmel_mxt_ts_mmi.h
@@ -0,0 +1,42 @@
+/*
+ * Atmel maXTouch Touchscreen driver
+ *
+ * Copyright (C) 2010 Samsung Electronics Co.Ltd
+ * Author: Joonyoung Shim <jy0922.shim@samsung.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#ifndef __LINUX_ATMEL_MXT_TS_H
+#define __LINUX_ATMEL_MXT_TS_H
+
+#include <linux/types.h>
+
+struct mxt_info {
+	u8 family_id;
+	u8 variant_id;
+	u8 version;
+	u8 build;
+	u8 matrix_xsize;
+	u8 matrix_ysize;
+	u8 object_num;
+};
+
+/* The platform data for the Atmel maXTouch touchscreen driver */
+struct mxt_platform_data {
+	struct mxt_info dt_info;
+	unsigned long irqflags;
+	u8 t19_num_keys;
+	const unsigned int *t19_keymap;
+	int t15_num_keys;
+	const unsigned int *t15_keymap;
+	unsigned gpio_reset;
+	unsigned gpio_irq;
+	bool common_vdd_supply;
+	const char *cfg_name;
+};
+
+#endif /* __LINUX_ATMEL_MXT_TS_H */
