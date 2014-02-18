b0a36322d2f1749a903a262257b748740ffd6575
Russ W. Knize <Russ.Knize@motorola.com>
2014-02-18 10:44:22 -0600

    mmc: improve handling of bad removable cards
    Includes:
    
    (CR): mmc: refactor card recovery improvements
    (CR): mmc: improve handling of bad removable cards
    (CR): don't use req->disk_name after calling __blk_end_request()
    (CR): msm: ignore spurious IRQs in the POWER_UP state
    (CR): completely ignore spurious interrupts
    (CR): mmc: msm_sdcc: don't enable IRQ for MMC_POWER_UP
    (CR): mmc: card recovery improvements
    
    MMC/SD cards that are bad or go bad during use can cause the MMC
    framework to spend long periods of time trying to recover the card.
    This can trigger various issues and temporary deadlocks that show up
    as DPM timeouts, task migration failures, etc.  It can also cause poor
    overall performance of the phone due to blocked I/O.  This change
    improves the situation by addressing these vulnerable areas of the
    MMC driver design:
    
    - power-cycle/reset cards that experience several back-to-back failures
    transparently to the block I/O layer
    
    - give up on cards that consistently fail so that the user will
    do something about it
    
    - forgive previously failing cards that start working again
    
    Change-Id: Ie78daac8a78a7040b8d8459bf0cc9ed6e88db1c1
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -369,6 +369,7 @@ struct mmc_host {
 #ifdef CONFIG_MMC_DEBUG
 	unsigned int		removed:1;	/* host is being removed */
 #endif
+	unsigned int		card_bad:1;	/* the card is bad; ignore it */
 
 	int			rescan_disable;	/* disable card detection */
 	int			rescan_entered;	/* used with nonremovable devices */
@@ -385,6 +386,8 @@ struct mmc_host {
 	const char		*wlock_name;
 	int			detect_change;	/* card detect flag */
 	struct mmc_slot		slot;
+
+	struct wake_lock	recovery_wake_lock;
 
 	const struct mmc_bus_ops *bus_ops;	/* current bus driver */
 	unsigned int		bus_refs;	/* reference counter */
