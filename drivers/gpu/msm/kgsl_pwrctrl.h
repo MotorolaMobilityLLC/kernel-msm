10771a58c65edcf826d2d1c0a847d0b4e2e01945
Lucille Sylvester <lsylvest@codeaurora.org>
2014-02-19 11:59:55 -0700

    msm: kgsl: Remove the busy waiting loop from idle_check
    Previously there were issues with extra long GPU sequences
    running during the idle_check NAP attempt thwarting the
    transition to NAP.  With the dispatcher architecture this
    is much less likely to occur.  In addition the dispatcher
    increases chances of a very short pause between a command
    coming in and being submitted increasing the occurence of
    "micro-NAPs" being caught by this loop.  These extremely
    short NAP periods are actually a power loss on the CX rail.
    Given that hw clock gating will take care of any long GPU
    command tails remove the busy wait loop for power savings.
    
    Conflicts:
            drivers/gpu/msm/kgsl_pwrctrl.h
    
    Change-Id: I4efc6215689c42ab03d4e478ad97839208d04004
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -118,7 +118,6 @@ struct kgsl_pwrctrl {
 	struct pm_qos_request pm_qos_req_dma;
 	unsigned int pm_qos_active_latency;
 	unsigned int pm_qos_wakeup_latency;
-	unsigned int irq_last;
 	bool bus_control;
 	int bus_mod;
 	unsigned int bus_index[KGSL_MAX_PWRLEVELS];
