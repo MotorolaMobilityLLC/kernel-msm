e2e44730f17bed141439ca2b71d11648236cae5e
Mihir Shete <smihir@qti.qualcomm.com>
2014-02-19 19:39:01 +0530

    wlan: do not wait for WDA_HALDumpCmdReq() to complete
    WDA_HALDumpCmdReq() does not return till it receives
    a response from the firmware, so do not wait for it
    to complete as it is a synchronous call.
    
    Change-Id: I89743c58d2f82b7cae561c024349ca5be394649e
    CRs-Fixed: 609232

--- CORE/VOSS/src/vos_api.c
+++ CORE/VOSS/src/vos_api.c
@@ -2188,31 +2188,5 @@ VOS_STATUS vos_wlanRestart(void)
 v_VOID_t vos_fwDumpReq(tANI_U32 cmd, tANI_U32 arg1, tANI_U32 arg2,
                         tANI_U32 arg3, tANI_U32 arg4)
 {
-   VOS_STATUS vStatus          = VOS_STATUS_SUCCESS;
-
-   /* Reset wda wait event */
-   vos_event_reset(&gpVosContext->wdaCompleteEvent);
-
    WDA_HALDumpCmdReq(NULL, cmd, arg1, arg2, arg3, arg4, NULL);
-
-   /* Need to update time out of complete */
-   vStatus = vos_wait_single_event(&gpVosContext->wdaCompleteEvent,
-                                   VOS_WDA_RESP_TIMEOUT );
-
-   if (vStatus != VOS_STATUS_SUCCESS)
-   {
-      if (vStatus == VOS_STATUS_E_TIMEOUT)
-      {
-         VOS_TRACE( VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_ERROR,
-          "%s: Timeout occurred before WDA HAL DUMP complete", __func__);
-      }
-      else
-      {
-         VOS_TRACE( VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_ERROR,
-           "%s: reporting other error", __func__);
-      }
-   }
-
-   return;
-
 }
