2f9bf401c4ccec603d402d1d87ace21e6ce9acbd
Jared Suttles <jsuttles@motorola.com>
2014-02-20 15:01:23 -0600

    msm: mdss: init timeout framework after ctl is setup
    For mdp5 the ctl structure is not initialized during probe, but instead
    is setup during the first unblank call. We must delay initializing the
    timeout diag framework until after this.
    
    Change-Id: I467fa6bfe9384fec85a044bf76f2f10f452e8117
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/611928
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Stephen Rossbach <rossbach@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -59,6 +59,7 @@ static int mdss_mdp_overlay_free_fb_pipe(struct msm_fb_data_type *mfd);
 static int mdss_mdp_overlay_fb_parse_dt(struct msm_fb_data_type *mfd);
 static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd);
 static int mdss_mdp_overlay_splash_parse_dt(struct msm_fb_data_type *mfd);
+static void mdss_mdp5_dump_ctl(void *data);
 
 static int mdss_mdp_overlay_sd_ctrl(struct msm_fb_data_type *mfd,
 					unsigned int enable)
@@ -2610,6 +2611,8 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		if (IS_ERR_OR_NULL(ctl))
 			return PTR_ERR(ctl);
 		mdp5_data->ctl = ctl;
+
+		mdss_timeout_init(mdss_mdp5_dump_ctl, mdp5_data->ctl);
 	}
 
 	if (!mfd->panel_info->cont_splash_enabled &&
@@ -2705,7 +2708,7 @@ int mdss_panel_register_done(struct mdss_panel_data *pdata)
 	return 0;
 }
 
-void mdss_mdp5_dump_ctl(void *data)
+static void mdss_mdp5_dump_ctl(void *data)
 {
 	struct mdss_mdp_ctl *ctl = (struct mdss_mdp_ctl *)data;
 
@@ -2718,16 +2721,18 @@ void mdss_mdp5_dump_ctl(void *data)
 	MDSS_TIMEOUT_LOG("global irqs disabled: %d\n", irqs_disabled());
 	MDSS_TIMEOUT_LOG("------ MDP5 INTERRUPT DATA DONE ------\n");
 
-	MDSS_TIMEOUT_LOG("-------- MDP5 CTL DATA ---------\n");
-	MDSS_TIMEOUT_LOG("play_cnt=%u\n", ctl->play_cnt);
-	MDSS_TIMEOUT_LOG("vsync_cnt=%u\n", ctl->vsync_cnt);
-	MDSS_TIMEOUT_LOG("underrun_cnt=%u\n", ctl->underrun_cnt);
-	MDSS_TIMEOUT_LOG("------ MDP5 CTL DATA DONE ------\n");
+	if (ctl) {
+		MDSS_TIMEOUT_LOG("-------- MDP5 CTL DATA ---------\n");
+		MDSS_TIMEOUT_LOG("play_cnt=%u\n", ctl->play_cnt);
+		MDSS_TIMEOUT_LOG("vsync_cnt=%u\n", ctl->vsync_cnt);
+		MDSS_TIMEOUT_LOG("underrun_cnt=%u\n", ctl->underrun_cnt);
+		MDSS_TIMEOUT_LOG("------ MDP5 CTL DATA DONE ------\n");
 
-	if (ctl->ctx_dump_fnc) {
-		MDSS_TIMEOUT_LOG("-------- MDP5 CTX DATA ---------\n");
-		ctl->ctx_dump_fnc(ctl);
-		MDSS_TIMEOUT_LOG("------ MDP5 CTX DATA DONE ------\n");
+		if (ctl->ctx_dump_fnc) {
+			MDSS_TIMEOUT_LOG("-------- MDP5 CTX DATA ---------\n");
+			ctl->ctx_dump_fnc(ctl);
+			MDSS_TIMEOUT_LOG("------ MDP5 CTX DATA DONE ------\n");
+		}
 	}
 }
 
@@ -3088,8 +3093,6 @@ int mdss_mdp_overlay_init(struct msm_fb_data_type *mfd)
 			rc = 0;
 		}
 	}
-
-	mdss_timeout_init(mdss_mdp5_dump_ctl, mdp5_data->ctl);
 
 	return rc;
 init_fail:
