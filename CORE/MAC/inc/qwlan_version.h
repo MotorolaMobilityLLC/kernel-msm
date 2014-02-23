030910c01780fd2133cd6d6d1fc9adb6ee33da60
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-02-23 11:57:50 -0800

    wlan: Release 3.2.3.249
    Release 3.2.3.249 includes:
    03b1f1f wlan: Default INI file across SPs.
    650b440 wlan: Fix for missing WMM IE in beacon when system is in IBSS role
    da57770 wlan:Fix CCX UPLOAD logging issues in HDD
    7776633 wlan: add channel number 144 to array index-channel mapping
    
    Change-Id: I54c5da835e943e2928ed85e319c3f6a0241f9be3

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -46,9 +46,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            248
+#define QWLAN_VERSION_BUILD            249
 
-#define QWLAN_VERSIONSTR               "3.2.3.248"
+#define QWLAN_VERSIONSTR               "3.2.3.249"
 
 #endif /* QWLAN_VERSION_H */
 
