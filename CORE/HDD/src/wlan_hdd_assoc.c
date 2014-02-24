45350fc5e799f7dfd76bc50307d780b66024e854
Rashmi Ramanna <c_ramanr@qti.qualcomm.com>
2014-02-24 18:58:29 +0530

    wlan: Dump MTRACE logs when connection failure occurs.
    As MTRACE logs are stored in the form of ring buffer, if issuing
    commands to enable dumping MTRACE logs and to dump the MTRACE
    logs are delayed, the messages may get overwritten.
    MTRACE logs helps in debugging issues like connection failure,
    so enable MTRACE logs for all modules and dump MTRACE messages
    whenever there is connection failure.
    
    CRs-Fixed: 622049
    Change-Id: Ifa387da6f09e957b4fa88610c53cd5f0771d8720
    (cherry picked from commit 2605976645f1b00d18089049ffcb9897a649b94c)

--- CORE/HDD/src/wlan_hdd_assoc.c
+++ CORE/HDD/src/wlan_hdd_assoc.c
@@ -63,6 +63,7 @@
 #include "wlan_hdd_tdls.h"
 #endif
 #include "sme_Api.h"
+#include "vos_trace.h"
 
 v_BOOL_t mibIsDot11DesiredBssTypeInfrastructure( hdd_adapter_t *pAdapter );
 
@@ -1163,6 +1164,7 @@ static eHalStatus hdd_AssociationCompletionHandler( hdd_adapter_t *pAdapter, tCs
     int ft_carrier_on = FALSE;
 #endif
     int status;
+    tHalHandle hHal = WLAN_HDD_GET_HAL_CTX(pAdapter);
 
     if ( eCSR_ROAM_RESULT_ASSOCIATED == roamResult )
     {
@@ -1453,6 +1455,13 @@ static eHalStatus hdd_AssociationCompletionHandler( hdd_adapter_t *pAdapter, tCs
 
         /*Handle all failure conditions*/
         hdd_connSetConnectionState( pHddStaCtx, eConnectionState_NotConnected);
+        /*Connection failure trigger MTRACE */
+        vosTraceEnable(ENABLE_ALL_MODULE_MTRACE, ENABLE_CONNECTION_FAIL_DUMP_LOG);
+        vosTraceDumpAll(hHal, CODE_ARG1_FOR_DUMP_LOG,
+                        SESSION_ARG2_FOR_DUMP_LOG,
+                        NUM_CONNECTION_FAIL_DUMP_LOG_MSG,
+                        ENABLE_ALL_MODULE_MTRACE);
+
         if((pHddCtx->concurrency_mode <= 1) && (pHddCtx->no_of_sessions[WLAN_HDD_INFRA_STATION] <=1))
         {
             pHddCtx->isAmpAllowed = VOS_TRUE;
