7519f65e294922c5da7c0314462a7fa800ef3512
Russ W. Knize <Russ.Knize@motorola.com>
2014-02-25 11:09:22 -0600

    mmc: refactor card recovery improvements
    This commit refactors the "reset and retry or give up" algorithm
    from K3.0 to better align it with the existing "reset and retry"
    functionality from upstream.  This approach makes use of the
    existing framework instead of attempting to replace it.
    
    The logic used to determine when to give up on a card
    has been improved.  Instead of giving up after a set number of
    failures, we now track the failure rate when trouble starts and
    forgive cards that behave themselves for a period of time.  This
    improves the user experience for extremely low-cost cards that
    tend to misbehave in small spurts and then work perfectly fine
    for hours or days.
    
    Change-Id: I3541a970ad2179ba35f0e953776594d61f23eb65
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/614071
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -188,7 +188,6 @@ struct sdio_func;
 struct sdio_func_tuple;
 
 #define SDIO_MAX_FUNCS		7
-#define MMC_MAX_CARD_INFIRMITY 8
 
 enum mmc_packed_stop_reasons {
 	EXCEEDS_SEGMENTS = 0,
@@ -349,7 +348,10 @@ struct mmc_card {
 
 	struct notifier_block reboot_notify;
 
-	unsigned int		infirmity;	/* measure of a card's health */
+#define MMC_ERROR_FAILURE_RATIO	10		/* give up on cards with too many failures/successes */
+#define MMC_ERROR_FORGIVE_RATIO	10		/* forgive cards with enough successes/failures */
+	unsigned int		failures;	/* number of recent request failures */
+	unsigned int		successes;	/* successful requests since 1st recorded failure  */
 };
 
 /*
@@ -488,8 +490,7 @@ static inline void __maybe_unused remove_quirk(struct mmc_card *card, int data)
 #define mmc_card_clr_doing_bkops(c)	((c)->state &= ~MMC_STATE_DOING_BKOPS)
 #define mmc_card_set_need_bkops(c)	((c)->state |= MMC_STATE_NEED_BKOPS)
 #define mmc_card_clr_need_bkops(c)	((c)->state &= ~MMC_STATE_NEED_BKOPS)
-#define mmc_card_recoverable(c) ((c) && (c)->host && \
-				!((c)->host->caps & MMC_CAP_NONREMOVABLE))
+
 /*
  * Quirk add/remove for MMC products.
  */
