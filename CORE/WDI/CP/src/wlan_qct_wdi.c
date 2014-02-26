921bf2070616819588bbb476be7060a5e567ff34
Katya Nigam <knigam@qti.qualcomm.com>
2014-02-26 14:52:24 +0530

    wlan: Adding FIQ in WDI before panic.
    This fix will issue FIQ first and then panic to WCNSS
    code after WDI timeout during SSR recovery,
    while currently panic is issued before FIQ.
    This is done so that proper ramdumps can be collected.
    
    Change-Id: I45dd5be6547695dd412eeb81154b0d8838979d0a
    CRs-Fixed:  623285
    (cherry picked from commit 08337825bc990f6e294c501247b7328031aa909e)

--- CORE/WDI/CP/src/wlan_qct_wdi.c
+++ CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -21583,12 +21583,11 @@ WDI_ResponseTimerCB
        return;
     }
 #ifndef WDI_RE_ENABLE_WIFI_ON_WDI_TIMEOUT
-   if(wpalIsWDresetInProgress())
-   {
-       wpalDevicePanic();
-   }
-
     wpalWcnssResetIntr();
+    if(wpalIsWDresetInProgress())
+    {
+        wpalDevicePanic();
+    }
     /* if this timer fires, it means Riva did not receive the FIQ */
     wpalTimerStart(&pWDICtx->ssrTimer, WDI_SSR_TIMEOUT);
 #else
