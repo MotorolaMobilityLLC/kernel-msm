a30b6be5e8829af47d08cfcff228d5d87b7eb71e
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2014-02-27 11:45:12 +0530

    wlan : Revision 3.2.3.252
    Revision  3.2.3.252
    
    Change-Id: I58189952b0b93ef3276050532789afc13ea93d07

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -46,9 +46,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            251
+#define QWLAN_VERSION_BUILD            252
 
-#define QWLAN_VERSIONSTR               "3.2.3.251"
+#define QWLAN_VERSIONSTR               "3.2.3.252"
 
 #endif /* QWLAN_VERSION_H */
 
