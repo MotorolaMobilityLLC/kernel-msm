b62e9eb98198425d6fd0387c4ff3259d263e6798
suhasb <suhasb@motorola.com>
2014-02-28 22:44:56 -0600

    arm:msm: Avoid underrun by increasing mdp_bw_ab_factor
    Tuning mdp_bw_ab_factor for bigger resolution panel.
    According to QCOMthe current setting for mdp_bw_ab_factor
    is for lower resolution panel. On vanquish panel resolution
    is bigger so requires mdp_bw_ab_factor to be tuned.
    Signed-off-by: suhasb <suhasb@motorola.com>
    
    Change-Id: I10eb2a70e73aea15c09d89f435dfad2d24d4e3b9
    Reviewed-on: http://gerrit.pcs.mot.com/614582
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-8960-display.c
+++ arch/arm/mach-msm/board-8960-display.c
@@ -579,7 +579,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
 	.mdp_max_clk = 200000000,
 	.mdp_max_bw = 2000000000,
-	.mdp_bw_ab_factor = 115,
+	.mdp_bw_ab_factor = 150,
 	.mdp_bw_ib_factor = 150,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
