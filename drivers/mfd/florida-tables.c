1fb9ac4b47db5f47b97ca67143e9539e9b8ba37e
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-03-03 09:26:21 +0000

    mfd: florida: Mark DSP clocking registers as volatile
    The DSPs will often control there own clock speeds whilst running as
    such we should mark the registers controlling this as volatile.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    (cherry picked from commit 5978b8015022841e458aeff6279d5ea567eab8f9)
    
    Change-Id: I22f657d69f56715280d5ad90050e64cc792f3b94

--- drivers/mfd/florida-tables.c
+++ drivers/mfd/florida-tables.c
@@ -2716,6 +2716,7 @@ static bool florida_volatile_register(struct device *dev, unsigned int reg)
 	case ARIZONA_DSP2_SCRATCH_1:
 	case ARIZONA_DSP2_SCRATCH_2:
 	case ARIZONA_DSP2_SCRATCH_3:
+	case ARIZONA_DSP2_CLOCKING_1:
 	case ARIZONA_DSP3_STATUS_1:
 	case ARIZONA_DSP3_STATUS_2:
 	case ARIZONA_DSP3_STATUS_3:
@@ -2744,6 +2745,7 @@ static bool florida_volatile_register(struct device *dev, unsigned int reg)
 	case ARIZONA_DSP3_SCRATCH_1:
 	case ARIZONA_DSP3_SCRATCH_2:
 	case ARIZONA_DSP3_SCRATCH_3:
+	case ARIZONA_DSP3_CLOCKING_1:
 	case ARIZONA_DSP4_STATUS_1:
 	case ARIZONA_DSP4_STATUS_2:
 	case ARIZONA_DSP4_STATUS_3:
@@ -2772,6 +2774,7 @@ static bool florida_volatile_register(struct device *dev, unsigned int reg)
 	case ARIZONA_DSP4_SCRATCH_1:
 	case ARIZONA_DSP4_SCRATCH_2:
 	case ARIZONA_DSP4_SCRATCH_3:
+	case ARIZONA_DSP4_CLOCKING_1:
 		return true;
 	default:
 		return florida_is_adsp_memory(dev, reg);
