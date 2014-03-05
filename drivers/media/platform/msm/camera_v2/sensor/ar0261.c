de12098980649e237876d027a3f1c92eaf0f7316
Alice Zhang <w18656@motorola.com>
2014-03-05 15:15:26 -0600

    driver: AR0261: clean up for power up/down
    This change convert the power up/down array to manual power up/down,
    which gives Motorola more flexibility to control power up/down
    sequence. In addition, it clean up logging in ar0261 driver.
    
    Change-Id: If80938728d4dcdb95ba6477109609082bffd3001
    Signed-off-by: Alice Zhang <w18656@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/610353
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Elena Satraitis <esatrait@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/ar0261.c
+++ drivers/media/platform/msm/camera_v2/sensor/ar0261.c
@@ -11,7 +11,13 @@
  *
  */
 #include "msm_sensor.h"
-#define AR0261_SENSOR_NAME "ar0261"
+#include <linux/of_gpio.h>
+#include "msm_camera_io_util.h"
+#include <linux/regulator/consumer.h>
+#include "msm_cci.h"
+#include <mach/gpiomux.h>
+#include "msm_camera_i2c_mux.h"
+#include <linux/device.h>
 
 /*
  * Highjack the sensor_power_down routine
@@ -21,16 +27,6 @@
 /*
 #define SET_ALT_I2C_ADDRESS
  */
-#define CONFIG_MSMB_CAMERA_DEBUG 1
-#undef CDBG
-#ifdef CONFIG_MSMB_CAMERA_DEBUG
-#define CDBG(fmt, args...) pr_debug(fmt, ##args)
-#else
-#define CDBG(fmt, args...) do { } while (0)
-#endif
-
-#define ALT_I2C_ADDRESS 0x6E
-#define PRI_I2C_ADDRESS 0x6C
 
 #ifdef SET_ALT_I2C_ADDRESS
 #include <mach/gpiomux.h>
@@ -45,132 +41,26 @@
 #include <linux/regulator/consumer.h>
 #endif
 
+#define AR0261_SENSOR_NAME "ar0261"
+#define ALT_I2C_ADDRESS 0x6E
+#define PRI_I2C_ADDRESS 0x6C
 DEFINE_MSM_MUTEX(ar0261_mut);
 
 static struct msm_sensor_ctrl_t ar0261_s_ctrl;
 
-/* Power up sequencing if Aptina devboard is used */
-static struct msm_sensor_power_setting ar0261_power_setting_with_devboard[] = {
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_RESET,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VDIG,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VANA,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_VREG,
-		.seq_val	= CAM_VIO,
-		.config_val	= 0,
-		.delay		= 5,
-	},
-	/* ar0261 dev boards needs regulator l10 */
-	{
-		.seq_type	= SENSOR_VREG,
-		.seq_val	= CAM_VANA,
-		.config_val	= 0,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VANA,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_VREG,
-		.seq_val	= CAM_VDIG,
-		.config_val	= 0,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VDIG,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_CLK,
-		.seq_val	= SENSOR_CAM_MCLK,
-		.config_val	= 24000000,
-		.delay		= 15,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_RESET,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 20,
-	},
-};
+static struct regulator *ar0261_cam_vddio;
+static struct regulator *ar0261_cam_vdd;
+static struct regulator *ar0261_cam_vana;
+struct clk *ar0261_cam_mclk[2];
 
-/* Power up sequencing if Aptina devboard is not used */
-static struct msm_sensor_power_setting ar0261_power_setting[] = {
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_RESET,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VDIG,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VANA,
-		.config_val	= GPIO_OUT_LOW,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_VREG,
-		.seq_val	= CAM_VIO,
-		.config_val	= 0,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VANA,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_VREG,
-		.seq_val	= CAM_VDIG,
-		.config_val	= 0,
-		.delay		= 1,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_VDIG,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 5,
-	},
-	{
-		.seq_type	= SENSOR_CLK,
-		.seq_val	= SENSOR_CAM_MCLK,
-		.config_val	= 24000000,
-		.delay		= 15,
-	},
-	{
-		.seq_type	= SENSOR_GPIO,
-		.seq_val	= SENSOR_GPIO_RESET,
-		.config_val	= GPIO_OUT_HIGH,
-		.delay		= 20,
-	},
-};
+static bool ar0261_devboard_config;
+
+#define VREG_ON              1
+#define VREG_OFF             0
+#define GPIO_REQUEST_USE     1
+#define GPIO_REQUEST_NO_USE  0
+#define CLK_ON               1
+#define CLK_OFF              0
 
 static struct v4l2_subdev_info ar0261_subdev_info[] = {
 	{
@@ -263,7 +153,7 @@ int32_t ar0261_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 			0x301a,
 			0x0110, MSM_CAMERA_I2C_WORD_DATA);
 	if (rc < 0) {
-		pr_info("%s: %d I2C address configure failed!\n",
+		pr_err("%s: %d I2C address configure failed!\n",
 			__func__, __LINE__);
 	}
 	/* Change sensor to use alternate I2C address */
@@ -273,7 +163,7 @@ int32_t ar0261_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 			0x3026,
 			0x0FF83, MSM_CAMERA_I2C_WORD_DATA);
 	if (rc < 0) {
-		pr_info("%s: %d I2C address configure failed!\n",
+		pr_err("%s: %d I2C address configure failed!\n",
 			__func__, __LINE__);
 	}
 
@@ -288,31 +178,223 @@ int32_t ar0261_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 	rc = msm_sensor_match_id(s_ctrl);
 
 	if (rc < 0)
-		pr_err("%s:%d match id failed rc %d\n", __func__, __LINE__, rc);
+		pr_err("%s:%d match id failed rc %d\n",
+				__func__, __LINE__, rc);
 	else
-		pr_info("%s success\n", __func__);
+		pr_info("%s success with slave addr 0x%x\n", __func__,
+				s_ctrl->sensor_i2c_client->cci_client->sid);
 
-	CDBG("%s exit\n", __func__);
 	return rc;
 }
 
+static int32_t ar0261_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int32_t rc = 0;
+	struct msm_camera_sensor_board_info *info = s_ctrl->sensordata;
+	struct device *dev = s_ctrl->dev;
+
+	pr_debug("%s\n", __func__);
+
+	if (info->gpio_conf->cam_gpiomux_conf_tbl != NULL) {
+		msm_gpiomux_install(
+				(struct msm_gpiomux_config *)
+				info->gpio_conf->cam_gpiomux_conf_tbl,
+				info->gpio_conf->cam_gpiomux_conf_tbl_size);
+	}
+
+	rc = msm_camera_request_gpio_table(
+		info->gpio_conf->cam_gpio_req_tbl,
+		info->gpio_conf->cam_gpio_req_tbl_size, GPIO_REQUEST_USE);
+	if (rc < 0) {
+		pr_err("%s: request gpio failed\n", __func__);
+		return rc;
+	}
+
+	/* Set initial values for gpios */
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[3].gpio,
+			GPIO_OUT_LOW);
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[2].gpio,
+			GPIO_OUT_LOW);
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[1].gpio,
+			GPIO_OUT_LOW);
+
+	/* Enable VDDIO supply */
+	rc = msm_camera_config_single_vreg(dev, &info->cam_vreg[1],
+			&ar0261_cam_vddio, VREG_ON);
+	if (rc < 0) {
+		pr_err("%s: Unable to turn on VDDIO supply!\n", __func__);
+		goto abort1;
+	}
+	usleep_range(5000, 6000);
+
+	/* If using aptina dev board, enable regulator l10 */
+	if (ar0261_devboard_config == true) {
+		rc = msm_camera_config_single_vreg(dev, &info->cam_vreg[2],
+			&ar0261_cam_vana, VREG_ON);
+		if (rc < 0) {
+			pr_err("%s: Unable to turn on VDDIO supply!\n",
+					__func__);
+			goto abort2;
+		}
+		usleep_range(5000, 6000);
+	}
+
+	/* Enable AVDD */
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[2].gpio,
+			GPIO_OUT_HIGH);
+	usleep_range(5000, 6000);
+
+	/* enable Core Supply (VDD) from PMIC */
+	rc = msm_camera_config_single_vreg(dev, &info->cam_vreg[0],
+			&ar0261_cam_vdd, VREG_ON);
+	if (rc < 0) {
+		pr_err("%s: Unable to turn on VDD supply!\n", __func__);
+		goto abort3;
+	}
+	usleep_range(5000, 6000);
+
+	/* Enable load switch */
+	gpio_set_value_cansleep(
+			info->gpio_conf->cam_gpio_req_tbl[3].gpio,
+			GPIO_OUT_HIGH);
+	usleep_range(1000, 2000);
+
+	/* Enable MCLK */
+	rc = msm_cam_clk_enable(dev, &s_ctrl->clk_info[0],
+			(struct clk **)&ar0261_cam_mclk[0],
+			s_ctrl->clk_info_size, CLK_ON);
+	if (rc < 0) {
+		pr_err("%s: Unable to enable MCLK!\n", __func__);
+		goto abort4;
+	}
+	usleep_range(5000, 6000);
+
+	/* Set reset to normal mode */
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[1].gpio,
+			GPIO_OUT_HIGH);
+	usleep_range(1000, 2000);
+
+	if (s_ctrl->sensor_device_type == MSM_CAMERA_PLATFORM_DEVICE) {
+		rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_util(
+				s_ctrl->sensor_i2c_client, MSM_CCI_INIT);
+		if (rc < 0) {
+			pr_err("%s cci_init failed\n", __func__);
+			goto abort5;
+		}
+	}
+
+	rc = ar0261_match_id(s_ctrl);
+	if (rc < 0) {
+		pr_err("%s:%d match id failed rc %d\n", __func__, __LINE__, rc);
+		goto abort6;
+	}
+
+	goto power_up_done;
+abort6:
+	if (s_ctrl->sensor_device_type == MSM_CAMERA_PLATFORM_DEVICE) {
+		s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_util(
+			s_ctrl->sensor_i2c_client, MSM_CCI_RELEASE);
+	}
+
+abort5:
+	msm_cam_clk_enable(dev, &s_ctrl->clk_info[0],
+			(struct clk **)&ar0261_cam_mclk[0],
+			s_ctrl->clk_info_size, CLK_OFF);
+	gpio_set_value_cansleep(
+			info->gpio_conf->cam_gpio_req_tbl[1].gpio,
+			GPIO_OUT_LOW);
+
+abort4:
+	gpio_set_value_cansleep(
+			info->gpio_conf->cam_gpio_req_tbl[3].gpio,
+			GPIO_OUT_LOW);
+	msm_camera_config_single_vreg(dev, &info->cam_vreg[0],
+			&ar0261_cam_vdd, VREG_OFF);
+
+abort3:
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[2].gpio,
+			GPIO_OUT_LOW);
+	if (ar0261_devboard_config == true)
+		msm_camera_config_single_vreg(dev, &info->cam_vreg[2],
+			&ar0261_cam_vana, VREG_OFF);
+
+abort2:
+	msm_camera_config_single_vreg(dev, &info->cam_vreg[1],
+			&ar0261_cam_vddio, VREG_OFF);
+
+abort1:
+	msm_camera_request_gpio_table(
+			info->gpio_conf->cam_gpio_req_tbl,
+			info->gpio_conf->cam_gpio_req_tbl_size,
+			GPIO_REQUEST_NO_USE);
+
+power_up_done:
+	return rc;
+}
+
+static int32_t ar0261_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int32_t rc = 0;
+	struct msm_camera_sensor_board_info *info = s_ctrl->sensordata;
+	struct device *dev = s_ctrl->dev;
+
+	pr_debug("%s: Enter\n", __func__);
+
+	if (s_ctrl->sensor_device_type == MSM_CAMERA_PLATFORM_DEVICE) {
+		s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_util(
+			s_ctrl->sensor_i2c_client, MSM_CCI_RELEASE);
+	}
+
+	/* Disable MCLK */
+	msm_cam_clk_enable(dev, &s_ctrl->clk_info[0],
+			(struct clk **)&ar0261_cam_mclk[0],
+			s_ctrl->clk_info_size, CLK_OFF);
+	usleep_range(5000, 6000);
+
+	/* reset low */
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[1].gpio,
+			GPIO_OUT_LOW);
+	usleep_range(5000, 6000);
+
+	/* Disable VDD */
+	gpio_set_value_cansleep(
+			info->gpio_conf->cam_gpio_req_tbl[3].gpio,
+			GPIO_OUT_LOW);
+
+	msm_camera_config_single_vreg(dev, &info->cam_vreg[0],
+			&ar0261_cam_vdd, VREG_OFF);
+	usleep_range(5000, 6000);
+
+	/* Disable AVDD */
+	gpio_set_value_cansleep(info->gpio_conf->cam_gpio_req_tbl[2].gpio,
+			GPIO_OUT_LOW);
+	usleep_range(5000, 6000);
+
+	/* If using devboard, turn off l10 */
+	if (ar0261_devboard_config == true) {
+		msm_camera_config_single_vreg(dev, &info->cam_vreg[2],
+			&ar0261_cam_vana, VREG_OFF);
+		usleep_range(1000, 2000);
+	}
+
+	msm_camera_request_gpio_table(
+		info->gpio_conf->cam_gpio_req_tbl,
+		info->gpio_conf->cam_gpio_req_tbl_size, GPIO_REQUEST_NO_USE);
+
+	return rc;
+}
 
 static int32_t ar0261_platform_probe(struct platform_device *pdev)
 {
 	int32_t rc = 0;
 	const struct of_device_id *match;
-	struct msm_sensor_ctrl_t *s_ctrl;
 
 	match = of_match_device(ar0261_dt_match, &pdev->dev);
 	if (match) {
-		if (of_property_read_bool(pdev->dev.of_node, "qcom,cam-nondirect") == true) {
-			/* Devboard is used.  Replace the power up sequence */
-			s_ctrl = (struct msm_sensor_ctrl_t *)match->data;
-			s_ctrl->power_setting_array.power_setting =
-				ar0261_power_setting_with_devboard;
-			s_ctrl->power_setting_array.size =
-				ARRAY_SIZE(ar0261_power_setting_with_devboard);
-		}
+		if (of_property_read_bool(pdev->dev.of_node,
+					"qcom,cam-nondirect") == true)
+			/* Devboard is used */
+			ar0261_devboard_config = true;
 
 		rc = msm_sensor_platform_probe(pdev, match->data);
 	} else {
@@ -346,15 +428,13 @@ static void __exit ar0261_exit_module(void)
 
 static struct msm_sensor_fn_t ar0261_func_tbl = {
 	.sensor_config = msm_sensor_config,
-	.sensor_power_up = msm_sensor_power_up,
-	.sensor_power_down = msm_sensor_power_down,
+	.sensor_power_up = ar0261_sensor_power_up,
+	.sensor_power_down = ar0261_sensor_power_down,
 	.sensor_match_id = ar0261_match_id,
 };
 
 static struct msm_sensor_ctrl_t ar0261_s_ctrl = {
 	.sensor_i2c_client = &ar0261_sensor_i2c_client,
-	.power_setting_array.power_setting = ar0261_power_setting,
-	.power_setting_array.size = ARRAY_SIZE(ar0261_power_setting),
 	.msm_sensor_mutex = &ar0261_mut,
 	.sensor_v4l2_subdev_info = ar0261_subdev_info,
 	.sensor_v4l2_subdev_info_size = ARRAY_SIZE(ar0261_subdev_info),
