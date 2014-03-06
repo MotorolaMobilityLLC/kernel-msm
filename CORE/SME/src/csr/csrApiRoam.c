cdccfa9634fa787a75ddab1e3a3a675a708d600b
Praveen Kumar Sirisilla <c_pksiri@qca.qualcomm.com>
2014-03-06 15:32:06 -0800

    STA is disconnected with specific AP IPTIME N904NS-second
    STA gets disconnected with AP IPTIME N904NS when AP is configured
    for WPA1 + TKIP on CH 161.
    
    NOTE: Dependent on CR 599931
    
    Change-Id: I28255e9939f47c77bce74877c57c7ab4773acf22
    CRs-Fixed: 627650
    (cherry picked from commit 405002b18eb8616bf3bb745b586c6cfb40dda0be)

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -11143,13 +11143,14 @@ tANI_BOOLEAN csrRoamIsValid40MhzChannel(tpAniSirGlobal pMac, tANI_U8 channel)
         {
             // Check set as TKIP or not.
             if ((NULL != &(pIes->RSN.pwise_cipher_suites[0][0]) &&
-               !memcmp( &(pIes->RSN.pwise_cipher_suites[0][0]),
+                (pIes->RSN.pwise_cipher_suite_count == 1) &&
+                !memcmp( &(pIes->RSN.pwise_cipher_suites[0][0]),
                                         "\x00\x0f\xac\x02" ,4))
                || (((NULL != &(pIes->WPA)) &&
                     (pIes->WPA.unicast_cipher_count == 1))
                     && ((NULL != &(pIes->WPA.unicast_ciphers[0]))
                         && memcmp(&(pIes->WPA.unicast_ciphers[0]),
-                                  "\x00\x0f\xac\x02", 4))))
+                                  "\x00\x50\xf2\x02", 4))))
             {
                 smsLog(pMac, LOGW, " No channel bonding in TKIP mode ");
                 eRet = PHY_SINGLE_CHANNEL_CENTERED;
