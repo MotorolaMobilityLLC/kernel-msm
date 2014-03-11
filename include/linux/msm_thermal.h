96813df642e067d769141eb3584cacce156e9741
Ram Chandrasekar <rkumbako@codeaurora.org>
2014-03-10 18:36:05 -0600

    msm: thermal: Add thermal reset feature to KTM
    Add support for issuing a thermal reset from msm thermal driver.
    The thermal driver is capable of monitoring all the tsens
    temperatures and issuing a reset right from the boot.
    The reset temperature can be given as input using devicetree.
    Failure to do so will disable this feature for the target.
    
    Change-Id: I7f7d3b978bb2374a7ea272f9e73a6678283ff41a
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -45,6 +45,7 @@ struct msm_thermal_data {
 	int32_t cx_phase_hot_temp_degC;
 	int32_t cx_phase_hot_temp_hyst_degC;
 	int32_t cx_phase_request_key;
+	int32_t therm_reset_temp_degC;
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
