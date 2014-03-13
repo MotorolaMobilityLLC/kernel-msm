46cde17b9d798d6cee690a5ed7a9f0f9cd00088f
Yuanyuan Zhong <zyy@motorola.com>
2014-03-13 10:43:30 -0500

    msm: mmi: export pvs bin info
    Export PVS Bin to /proc/cpu/msm_acpu_pvs.
    This is ported from:
        (CR): arm: msm: mmi: add socinfo to panic log
        (CR) msm: mmi: export pvs bin info
    Function to export socinfo to panic log is removed. Will add it back
    when panic logging is ready.
    
    Change-Id: I91d26212d9912ab636caf5c76041fb320bc3a605
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/617758
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/clk/qcom/clock-a7.c
+++ drivers/clk/qcom/clock-a7.c
@@ -29,6 +29,7 @@
 #include <linux/pm_opp.h>
 #include <soc/qcom/clock-local2.h>
 #include <dt-bindings/clock/msm-clocks-a7.h>
+#include <soc/qcom/mmi_soc_info.h>
 
 #include "clock.h"
 
@@ -392,6 +393,8 @@ static int clock_a7_probe(struct platform_device *pdev)
 		get_speed_bin_b(pdev, &speed_bin, &version);
 	else
 		get_speed_bin(pdev, &speed_bin, &version);
+
+	mmi_acpu_bin_set(&speed_bin, NULL, &version);
 
 	snprintf(prop_name, ARRAY_SIZE(prop_name),
 			"qcom,speed%d-bin-v%d", speed_bin, version);
