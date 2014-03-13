8b759b0c29578cb96e70ec0f38d185264bea10dd
Yuanyuan Zhong <zyy@motorola.com>
2014-03-13 11:37:32 -0500

    msm: qpnp-power-on: reorder warm reset checking in probe
    Global variable sys_reset_dev is used when checking warm reset status.
    Do the checking after sys_reset_dev is populated.
    
    Change-Id: Iee42a11f27956d255bdf5bf1156b97c048a0e3b5
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/617784
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -1128,11 +1128,6 @@ static int __devinit qpnp_pon_probe(struct spmi_device *spmi)
 	if ((index > PON_REASON_MAX) || (index < 0))
 		index = 0;
 
-	cold_boot = !qpnp_pon_is_warm_reset();
-	pr_info("PMIC@SID%d Power-on reason: %s and '%s' boot\n",
-		pon->spmi->sid, index ? qpnp_pon_reason[index - 1] :
-		"Unknown", cold_boot ? "cold" : "warm");
-
 	rc = of_property_read_u32(pon->spmi->dev.of_node,
 				"qcom,pon-dbc-delay", &delay);
 	if (rc) {
@@ -1193,6 +1188,11 @@ static int __devinit qpnp_pon_probe(struct spmi_device *spmi)
 	/* Only populate sys_reset_dev on success */
 	if (sys_reset)
 		sys_reset_dev = pon;
+
+	cold_boot = !qpnp_pon_is_warm_reset();
+	pr_info("PMIC@SID%d Power-on reason: %s and '%s' boot\n",
+		pon->spmi->sid, index ? qpnp_pon_reason[index - 1] :
+		"Unknown", cold_boot ? "cold" : "warm");
 
 	return rc;
 }
