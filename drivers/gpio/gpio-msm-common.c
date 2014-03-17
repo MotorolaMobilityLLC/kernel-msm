cc034fc5406fc93ea2a6440aaf2b3d39047aff68
Patrick Tjin <pattjin@google.com>
2014-03-17 12:49:12 -0700

    gpio: msm-common: Add wakeup reason logging
    Add call to log_wakeup_reason from msm_gpio_show_resume_irq
    and remove redundant print.
    
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -25,6 +25,7 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/mach/irq.h>
 
@@ -443,8 +444,7 @@ void msm_gpio_show_resume_irq(void)
 		intstat = __msm_gpio_get_intr_status(i);
 		if (intstat) {
 			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
-			pr_warning("%s: %d triggered\n",
-				__func__, irq);
+			log_wakeup_reason(irq);
 		}
 	}
 	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
