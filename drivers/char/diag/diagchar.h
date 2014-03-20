05643e54bf0049cacdcb9409189801e342b5697f
Ravi Aravamudhan <aravamud@codeaurora.org>
2014-03-19 18:52:19 -0700

    diag: Prevent suspend in Memory Device mode
    Apps processor can goto suspend when receiving data from peripherals.
    This can result in data being drop from Diag driver to the user
    space clients in Memory Device mode. Make changes to acquire
    wakeup source object till the data is copied to user space.
    
    Change-Id: I7479c89707199557420bac18e1744a829477164c
    CRs-Fixed: 624445
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>
    CR-fixed: (CR)
    Change-Id: I3f5d7ee2c26a3fda3ea0537243f1e7a28a9523a4
    Reviewed-on: http://gerrit.mot.com/695007
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -237,14 +237,6 @@ struct diag_client_map {
 	int pid;
 };
 
-struct diag_nrt_wake_lock {
-	int enabled;
-	int ref_count;
-	int copy_count;
-	struct wake_lock read_lock;
-	spinlock_t read_spinlock;
-};
-
 struct real_time_vote_t {
 	uint16_t proc;
 	uint8_t real_time_vote;
@@ -289,8 +281,6 @@ struct diag_smd_info {
 
 	struct diag_request *write_ptr_1;
 	struct diag_request *write_ptr_2;
-
-	struct diag_nrt_wake_lock nrt_lock;
 
 	struct workqueue_struct *wq;
 
@@ -471,6 +461,10 @@ struct diagchar_dev {
 #ifdef CONFIG_DIAG_EXTENSION
 	struct list_head addon_list;
 #endif
+	/* Wakeup source related variables */
+	spinlock_t ws_lock;
+	int ws_ref_count;
+	int copy_count;
 };
 
 extern struct diag_bridge_dev *diag_bridge;
