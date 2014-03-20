309d22bb1db30401a69ac5c906d1e039d72297d5
Vladimir Razgulin <vrazguli@codeaurora.org>
2014-03-20 10:56:39 -0600

    msm: kgsl: Fixing min_pwrlevel kgsl sysfs element
    The change 30a2b23 modified kgsl_sysfs_store function calls
    everywhere except in the kgsl_pwrctrl_min_pwrlevel_store()
    function. As a result min_pwrlevel sysfs item behaved incorrectly.
    
    Correct the return code check for kgsl_sysfs_store call in
    kgsl_pwrctrl_min_pwrlevel_store().
    
    Change-Id: If2b0cc9b26625c1a326b6644ab183a36868666f9
    Signed-off-by: Vladimir Razgulin <vrazguli@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -305,7 +305,7 @@ static ssize_t kgsl_pwrctrl_min_pwrlevel_store(struct device *dev,
 	pwr = &device->pwrctrl;
 
 	ret = kgsl_sysfs_store(buf, &level);
-	if (ret != count)
+	if (ret)
 		return ret;
 
 	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
