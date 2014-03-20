03703226b36abbe45e26a18f2aba0f9c5015f30d
Dave Schmidt <d.schmidt@motorola.com>
2014-03-19 20:43:39 -0500

    IKMMINTG-6453: msm: camera: add mot cci tool.
    - Port motorola cci tool to 8952.
      2a9b998 IKMMINTG-2142: msm: camera: add mot cci tool
    
    - Fix bug in cci tool for 32bit kernel
      - v4l2 subdev fops msm_cci_intf_v4l2_subdev_fops
        not declare in 32bit environment.
    - Set cci subdev to 50
    
    Change-Id: I075f70120c74370ec82044d05193537fc4a0bf01
    Signed-off-by: Sumit Jamadar <xkbr38@motorola.com>
    Reviewed-on: http://gerrit.mot.com/794977
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Reviewed-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Satya Prasad Kottisa <fvgm76@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/media/msmb_camera.h
+++ include/media/msmb_camera.h
@@ -66,6 +66,8 @@
 #define MSM_CAMERA_SUBDEV_TOF          17
 #define MSM_CAMERA_SUBDEV_EXT          18
 
+#define MSM_CAMERA_SUBDEV_CCI_INTF     50
+
 #define MSM_MAX_CAMERA_SENSORS  5
 
 /* The below macro is defined to put an upper limit on maximum
