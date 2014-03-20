ca3f71d8a74249505574c20f390beceeb382b4cc
Dave Schmidt <d.schmidt@motorola.com>
2014-03-19 20:43:39 -0500

    drivers: msm: camera: mot cci updt
    Porting the following commits to 8084.
    
    ---
    (CR): msm: camera: add cci interface
    
    Adds a new v4l2 subdev that implements a generic CCI interface
    for userland tools.
    ---
    (CR): msm: camera: add cci vddio support
    
    Allows the CCI module to enable its own IO regulator.
    ---
    (CR): msm: camera: fix math error in cci intf
    ---
    (CR): msm: camera: fix cci tool typo
    
    When writing values in the cci tool, there was a
    typo in the condition of determining the address type
    of one byte or two byte. Fixed this.
    ---
    
    Change-Id: I2d2499bd8495e414d6cd114d6de4266cfa6d9e6c
    Signed-off-by: Bryan Knottek <ebk012@motorola.com>
    Reviewed-on: http://gerrit.mot.com/636529
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Kubicki <w17311@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/media/msmb_camera.h
+++ include/media/msmb_camera.h
@@ -38,6 +38,8 @@
 #define MSM_CAMERA_SUBDEV_BUF_MNGR     13
 #define MSM_CAMERA_SUBDEV_SENSOR_INIT  14
 
+#define MSM_CAMERA_SUBDEV_CCI_INTF     20
+
 #define MSM_MAX_CAMERA_SENSORS  5
 
 /* The below macro is defined to put an upper limit on maximum
