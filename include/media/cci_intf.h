8dbe65d53c764ad06252206af5356b7e4446d6a9
Dave Schmidt <d.schmidt@motorola.com>
2014-03-19 20:43:39 -0500

    drivers: msm: camera: mot cci updt
    Porting the following commits to 8084.
    
    ---
    (CR): msm: camera: add cci interface
    
    Adds a new v4l2 subdev that implements a generic CCI interface
    for userland tools.
    ---
    (CR): msm: camera: add cci vddio support
    
    Allows the CCI module to enable its own IO regulator.
    ---
    (CR): msm: camera: fix math error in cci intf
    ---
    (CR): msm: camera: fix cci tool typo
    
    When writing values in the cci tool, there was a
    typo in the condition of determining the address type
    of one byte or two byte. Fixed this.
    ---
    
    Change-Id: I2d2499bd8495e414d6cd114d6de4266cfa6d9e6c
    Signed-off-by: Bryan Knottek <ebk012@motorola.com>
    Reviewed-on: http://gerrit.mot.com/636529
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Kubicki <w17311@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/media/cci_intf.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 Motorola Mobility LLC.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _CCI_INTF_H_
+#define _CCI_INTF_H_
+
+#include <linux/ioctl.h>
+#include <linux/videodev2.h>
+
+#define MSM_CCI_INTF_MAX_XFER 256
+
+struct msm_cci_intf_register {
+	unsigned short width; /* 1 or 2 */
+	unsigned short addr;
+};
+
+struct msm_cci_intf_data {
+	unsigned short count; /* between 1 and MSM_CCI_INTF_MAX_XFER */
+	unsigned char  buf[MSM_CCI_INTF_MAX_XFER];
+};
+
+struct msm_cci_intf_xfer {
+	unsigned short cci_bus;     /* 0 = MASTER_0, 1 = MASTER_1 */
+	unsigned short slave_addr;  /* 7-bit addr of intended device */
+	struct msm_cci_intf_register  reg;
+	struct msm_cci_intf_data      data;
+};
+
+#define MSM_CCI_INTF_READ \
+	_IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_WRITE \
+	_IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+
+#endif
