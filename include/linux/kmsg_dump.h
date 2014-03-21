9284f9801250bf3edaa9462bb4c550581c9c38de
Hong-Mei Li <a21834@motorola.com>
2014-03-21 13:20:21 -0700

    printk: add kmsg_dump_get_buffer_panic interface
    In the new log buffer maintainence mechanism, we can only read the
    content from the newest to the older lines.
    When kernel panic, we need to dump out the whole log buffer content
    from older to newer, so we add this interface.
    
    Change-Id: I292b1977bb89ded3d55811d07244db5c46b770a7
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/620216
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/kmsg_dump.h
+++ include/linux/kmsg_dump.h
@@ -64,6 +64,9 @@ bool kmsg_dump_get_line(struct kmsg_dumper *dumper, bool syslog,
 bool kmsg_dump_get_buffer(struct kmsg_dumper *dumper, bool syslog,
 			  char *buf, size_t size, size_t *len);
 
+bool kmsg_dump_get_buffer_panic(struct kmsg_dumper *dumper, bool syslog,
+			  char *buf, size_t size, size_t *len);
+
 void kmsg_dump_rewind_nolock(struct kmsg_dumper *dumper);
 
 void kmsg_dump_rewind(struct kmsg_dumper *dumper);
@@ -90,6 +93,12 @@ static inline bool kmsg_dump_get_line(struct kmsg_dumper *dumper, bool syslog,
 }
 
 static inline bool kmsg_dump_get_buffer(struct kmsg_dumper *dumper, bool syslog,
+					char *buf, size_t size, size_t *len)
+{
+	return false;
+}
+
+static inline bool kmsg_dump_get_buffer_panic(struct kmsg_dumper *dumper, bool syslog,
 					char *buf, size_t size, size_t *len)
 {
 	return false;
