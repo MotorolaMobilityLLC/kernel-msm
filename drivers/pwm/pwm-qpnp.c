6e72830924e02b4e4304b8f95836733c48fc7045
Dmitry Ornatsky <cdo021c@motorola.com>
2014-03-25 17:30:23 -0500

    drivers: pwm: fix pwm-qpnp clk configuration
    In case if global pwm control structure already has
    period set to the same value as requested by pwm_config
    driver does not calculate clock settings. This is the
    case when using core pwm driver framework. As a result
    pwm configuration results in pwm_size 0 all the time.
    
    Change-Id: I441759c55720777807e191eb2ccc87dfd59a918a
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/621227
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dmitry Ornatsky <ornatsky@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/pwm/pwm-qpnp.c
+++ drivers/pwm/pwm-qpnp.c
@@ -328,6 +328,7 @@ struct qpnp_pwm_chip {
 	u32			dtest_line;
 	u32			dtest_output;
 	bool			in_test_mode;
+	bool			shadow_ready;	/* first clk init call */
 };
 
 /* Internal functions */
@@ -1327,12 +1328,13 @@ static int qpnp_pwm_config(struct pwm_chip *pwm_chip,
 
 	spin_lock_irqsave(&chip->lpg_lock, flags);
 
-	if (prev_period_us > INT_MAX / NSEC_PER_USEC ||
+	if (!chip->shadow_ready || prev_period_us > INT_MAX / NSEC_PER_USEC ||
 			prev_period_us * NSEC_PER_USEC != period_ns) {
 		qpnp_lpg_calc_period(LVL_NSEC, period_ns, chip);
 		qpnp_lpg_save_period(chip);
 		pwm->period = period_ns;
 		chip->pwm_config.pwm_period = period_ns / NSEC_PER_USEC;
+		chip->shadow_ready = true;
 	}
 
 	rc = _pwm_config(chip, LVL_NSEC, duty_ns, period_ns);
@@ -2035,6 +2037,7 @@ static int qpnp_pwm_probe(struct spmi_device *spmi)
 	pwm_chip->chip.ops = &qpnp_pwm_ops;
 	pwm_chip->chip.base = -1;
 	pwm_chip->chip.npwm = 1;
+	pwm_chip->shadow_ready = false;
 
 	rc = pwmchip_add(&pwm_chip->chip);
 	if (rc < 0) {
