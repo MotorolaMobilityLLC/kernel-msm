417397e13055c0d1580dc3bb06bfd182fa3ab5b5
Alexander Filonenko <vtp684@motorola.com>
2014-03-25 11:40:47 -0500

    misc: capsense_prog: Add CapSense flash driver
    This is proparation from (CR) AP will only flash (no I2C)
    
    Cypress CapSense I2C bus will not be handled by apps cpu. Device
    driver in Linux on AP side should implement programming in-system
    flash memory only and it is not necessary to have it as I2C device
    driver. It is changed to platform device driver and usage details
    are described in kernel/Documentation/misc-devices/capsense_prog.txt
    
    Signed-off-by: Vladimir Tsunaev <fvt005@motorola.com>
    Signed-off-by: Vladimir Karpovich <cvk011c@motorola.com>
    Signed-off-by: Alexander Filonenko <vtp684@motorola.com>
    
    Change-Id: Ib74f8dd1222bbb82518262ef9e16981efdcaa02f
    Reviewed-on: http://gerrit.pcs.mot.com/621193
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/misc/capsense_prog/cycapsense_issp.h
@@ -0,0 +1,48 @@
+
+/*
+ * Header file for:
+ * Cypress CapSense(TM) firmware downloader.
+ *
+ * Copyright (c) 2012-2013 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __CYCAPSENSE_ISSP_H__
+#define __CYCAPSENSE_ISSP_H__
+
+struct hex_info {
+	const char *fw_name;
+	u8 *data;
+	u8 *s_data;
+	u16 cs;
+};
+struct issp_data {
+	struct hex_info inf;
+	const char *name;
+	unsigned int silicon_id;
+	int block_size;
+	int num_of_blocks;
+	int secure_bytes;
+	int c_gpio;
+	int d_gpio;
+	int rst_gpio;
+};
+int cycapsense_issp_parse_hex(struct issp_data *d, const u8 *src, int len);
+
+int cycapsense_issp_get_cs(const struct issp_data *d, unsigned int *cs);
+
+int cycapsense_issp_verify_id(const struct issp_data *d);
+
+int cycapsense_issp_erase(const struct issp_data *d);
+
+int cycapsense_issp_dnld(const struct issp_data *d);
+
+#endif /* __CYCAPSENSE_ISSP_H__ */
