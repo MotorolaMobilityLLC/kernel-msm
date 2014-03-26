3f5cd8c8ec68c472096e67b7a4c23a07a5c5a9f1
Patrick Tjin <pattjin@google.com>
2014-03-26 16:08:17 -0700

    msm8226: correct compile error for include file
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- sound/soc/msm/msm8226.c
+++ sound/soc/msm/msm8226.c
@@ -27,7 +27,7 @@
 #include <sound/jack.h>
 #include <sound/q6afe-v2.h>
 
-#include <qdsp6v2/msm-pcm-routing-v2.h>
+#include "qdsp6v2/msm-pcm-routing-v2.h"
 #include "qdsp6v2/q6core.h"
 #include "../codecs/wcd9xxx-common.h"
 #include "../codecs/wcd9306.h"
