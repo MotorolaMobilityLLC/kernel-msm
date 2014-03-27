8c95db0bb9e0849c85e99e7d8bb01c387a8f7375
Jeevan Shriram <jshriram@codeaurora.org>
2014-03-26 19:47:49 -0700

    mdss: msm_fb: Allocate fb memory during mmap
    Allocate frame buffer memory from system heap
    during mmap and remove once all the references
    to framebuffer are 0.
    
    Change-Id: I1b6040187bda8095007faa4d9df7dc046aa30c6e
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    (cherry picked from commit 7381756d8fe4e757de108fa5623f6290c1e054e9)
    Signed-off-by: Fred Fettinger <fred.fettinger@motorola.com>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c
    	drivers/video/msm/mdss/mdss_fb.h
    Change-Id: I59292cc2e04a2a76d5e8b53bb90c1945013e4e42
    Reviewed-on: http://gerrit.mot.com/659694
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -227,6 +227,8 @@ struct msm_fb_data_type {
 
 	u32 dcm_state;
 	struct list_head proc_list;
+	struct ion_client *fb_ion_client;
+	struct ion_handle *fb_ion_handle;
 
 	int (*quickdraw_fb_cleanup)(struct msm_fb_data_type *mfd);
 	int (*quickdraw_fb_prepare)(struct msm_fb_data_type *mfd);
@@ -269,6 +271,7 @@ void mdss_fb_signal_timeline(struct msm_sync_pt_data *sync_pt_data);
 struct sync_fence *mdss_fb_sync_get_fence(struct sw_sync_timeline *timeline,
 				const char *fence_name, int val);
 int mdss_fb_register_mdp_instance(struct msm_mdp_interface *mdp);
+int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd);
 int mdss_fb_dcm(struct msm_fb_data_type *mfd, int req_state);
 int mdss_fb_pan_display_ex(struct fb_info *info,
 			   struct mdp_display_commit *disp_commit);
