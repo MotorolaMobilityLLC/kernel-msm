d462436f143a1fab7331518957eb53a4c13bd727
Zac Crosby <zac@motorola.com>
2014-03-27 18:05:21 -0500

    msm: mdss: Prevent solid fill on smooth transition
    When using the smooth tranisition feature on a video mode display,
    prevent a solid fill from occuring at the handoff.  This solid fill is
    causing a black frame to be seen at the start of the boot animation.
    
    Fix from QCOM SR #01434987.
    
    Change-Id: If29e93677b1b51c14af73eacd54b78db4b96e638
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/623669
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1733,6 +1733,12 @@ static int mdss_panel_parse_dt(struct device_node *np,
 		pr_info("%s:%d Quickdraw disabled.\n", __func__, __LINE__);
 	}
 
+	pinfo->no_solid_fill =
+		of_property_read_bool(np, "qcom,splash-no-solid-fill");
+	if (pinfo->mipi.mode == DSI_VIDEO_MODE && !pinfo->no_solid_fill)
+		pr_warn("%s: 'No Solid Fill' not set for video mode panel",
+			__func__);
+
 	return 0;
 
 error:
