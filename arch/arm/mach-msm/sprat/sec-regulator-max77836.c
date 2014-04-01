f5b1ba67ea2a9fd90f08fcf7fd0be1f7a3d3bd7f
Kyunghan,Baek <wildtaz.baek@samsung.com>
2014-04-01 09:41:47 +0900

    sprat: regulator: max77836: add regulator init data.
    To use max77836 regulator, add regualtor init data for sprat.
    
    Change-Id: Icde13f042f2ac0021a7ac213629f5fe9fdcbaed5
    Signed-off-by: Kyunghan,Baek <wildtaz.baek@samsung.com>

---
+++ arch/arm/mach-msm/sprat/sec-regulator-max77836.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2013 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/regulator/max77836-regulator.h>
+
+static struct regulator_consumer_supply ldo1_consumer =
+	REGULATOR_SUPPLY("max77836_ldo1", NULL);
+
+static struct regulator_consumer_supply ldo2_consumer =
+	REGULATOR_SUPPLY("max77836_ldo2", NULL);
+
+static struct regulator_init_data max77836_ldo1_data = {
+	.constraints	= {
+		.name		= "max77836_ldo1",
+		.min_uV		= 1500000,
+		.max_uV		= 2700000,
+		.apply_uV	= 1,
+		.always_on	= 0,
+		.state_mem	= {
+			.enabled	= 0,
+		},
+		.valid_ops_mask =
+			REGULATOR_CHANGE_VOLTAGE | REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies	= 1,
+	.consumer_supplies	= &ldo1_consumer,
+};
+
+static struct regulator_init_data max77836_ldo2_data = {
+	.constraints	= {
+		.name		= "max77836_ldo2",
+		.min_uV		= 800000,
+		.max_uV		= 3950000,
+		.apply_uV	= 1,
+		.always_on	= 0,
+		.state_mem	= {
+			.enabled	= 0,
+		},
+		.valid_ops_mask =
+			REGULATOR_CHANGE_VOLTAGE | REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies	= 1,
+	.consumer_supplies	= &ldo2_consumer,
+};
+
+struct max77836_reg_platform_data max77836_reglator_pdata[] = {
+	{
+		.reg_id = MAX77836_LDO1,
+		.init_data = &max77836_ldo1_data,
+		.overvoltage_clamp_enable = 0,
+		.auto_low_power_mode_enable = 0,
+		.compensation_ldo = 0,
+		.active_discharge_enable = 0,
+		.soft_start_slew_rate = 0,
+		.enable_mode = REG_NORMAL_MODE,
+
+	},
+	{
+		.reg_id = MAX77836_LDO2,
+		.init_data = &max77836_ldo2_data,
+		.overvoltage_clamp_enable = 0,
+		.auto_low_power_mode_enable = 0,
+		.compensation_ldo = 0,
+		.active_discharge_enable = 0,
+		.soft_start_slew_rate = 0,
+		.enable_mode = REG_NORMAL_MODE,
+
+	},
+};
