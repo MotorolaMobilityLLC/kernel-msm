50810731e4839dfd6318dae5cfcd1a7c345a9502
Simon Wilson <simonwilson@google.com>
2014-04-03 16:41:56 -0700

    power: max17050: interrupt every 5% change in soc
    Userspace needs to be aware of changes in capacity
    to track charge statistics. Interrupt every 5% change
    in capacity to inform userspace.
    
    Change-Id: I589c3513aa448f671c014c5e0cedc8517931c5ae
    Signed-off-by: Simon Wilson <simonwilson@google.com>

--- drivers/power/max17050_battery.c
+++ drivers/power/max17050_battery.c
@@ -614,6 +614,26 @@ static int max17050_get_property(struct power_supply *psy,
 	return 0;
 }
 
+static void max17050_set_soc_thresholds(struct max17050_chip *chip,
+								s16 threshold)
+{
+	s16 soc_now;
+	s16 soc_max;
+	s16 soc_min;
+
+	soc_now = max17050_read_reg(chip->client, MAX17050_REPSOC) >> 8;
+
+	soc_max = soc_now + threshold;
+	if (soc_max > 100)
+		soc_max = 100;
+	soc_min = soc_now - threshold;
+	if (soc_min < 0)
+		soc_min = 0;
+
+	max17050_write_reg(chip->client, MAX17050_SALRT_TH,
+		(u16)soc_min | ((u16)soc_max << 8));
+}
+
 static irqreturn_t max17050_interrupt(int id, void *dev)
 {
 	struct max17050_chip *chip = dev;
@@ -624,11 +644,14 @@ static irqreturn_t max17050_interrupt(int id, void *dev)
 
 	val = max17050_read_reg(client, MAX17050_STATUS);
 
-	/* Signal userspace when the capacity reaches 0 or 100% */
+	/* Signal userspace when the capacity exceeds the limits */
 	if ((val & STATUS_INTR_SOCMIN_BIT) || (val & STATUS_INTR_SOCMAX_BIT)) {
 		/* Clear interrupt status bits */
 		max17050_write_reg(client, MAX17050_STATUS, val &
 			~(STATUS_INTR_SOCMIN_BIT | STATUS_INTR_SOCMAX_BIT));
+
+		/* Reset capacity thresholds */
+		max17050_set_soc_thresholds(chip, 5);
 
 		power_supply_changed(&chip->battery);
 	}
@@ -656,11 +679,8 @@ static void max17050_complete_init(struct max17050_chip *chip)
 		chip->power_supply_registered = true;
 
 	if (client->irq) {
-		/*
-		 * Set capacity thresholds to 1 and 99%, which will cause
-		 * interrupts to be generated at <1% and >99%.
-		 */
-		max17050_write_reg(client, MAX17050_SALRT_TH, 0x6301);
+		/* Set capacity thresholds to +/- 5% of current capacity */
+		max17050_set_soc_thresholds(chip, 5);
 
 		/* Enable capacity interrupts */
 		val = max17050_read_reg(client, MAX17050_CONFIG);
