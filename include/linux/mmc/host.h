837236c3b4934e1742b30ec62dee295b76769a3f
Russ W. Knize <Russ.Knize@motorola.com>
2014-04-04 08:24:00 -0500

    IKVPREL1L-1355: mmc: support eMMC driver strength configuration
    Propagated from IKXREL3KK-1683
    
    eMMC v4.5 specifies a method to change the card driver strengths to
    better match board impedance requirements.  The strength types roughly
    match those in the SD v3.00 spec, but are configured differently
    and do not take advantage of the SDHCI implementation.  In addition,
    eMMC v5.0 introduced a new driver strength type that has no equivalent
    in the current SD spec.  The two functionally map as follows:
    
      SD   eMMC   Impedance   Relative Drive
      B    0      50 ohms     x1  (default)
      A    1      33 ohms     x1.5
      C    2      66 ohms     x0.75
      D    3      100 ohms    x0.5
      x    4      40 ohms     x1.2
    
    This commit implements card drive strengths for eMMC.  Because the
    four SD driver strength types are closely coupled to the SDHCI
    HOST_CONTROL2 register and it has no extra, adjacent bits available
    for the new drive strengths defined by eMMC v5.0, a new group of
    driver types are defined.  This commit does take advantage of the
    existing CAP bits for SD driver types A-D.
    
    Change-Id: Ia70bb4bd271944d377fff65cfd6302e45b22b2e2
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -82,6 +82,7 @@ struct mmc_ios {
 #define MMC_SET_DRIVER_TYPE_A	1
 #define MMC_SET_DRIVER_TYPE_C	2
 #define MMC_SET_DRIVER_TYPE_D	3
+#define MMC_SET_DRIVER_TYPE_4	4		/* eMMC only */
 };
 
 /* states to represent load on the host */
@@ -147,7 +148,8 @@ struct mmc_host_ops {
 
 	/* The tuning command opcode value is different for SD and eMMC cards */
 	int	(*execute_tuning)(struct mmc_host *host, u32 opcode);
-	int	(*select_drive_strength)(unsigned int max_dtr, int host_drv, int card_drv);
+	int	(*select_drive_strength)(struct mmc_host *host,
+					 int host_drv, int card_drv);
 	void	(*hw_reset)(struct mmc_host *host);
 	void	(*card_event)(struct mmc_host *host);
 	unsigned long (*get_max_frequency)(struct mmc_host *host);
@@ -288,9 +290,9 @@ struct mmc_host {
 #define MMC_CAP_UHS_SDR50	(1 << 17)	/* Host supports UHS SDR50 mode */
 #define MMC_CAP_UHS_SDR104	(1 << 18)	/* Host supports UHS SDR104 mode */
 #define MMC_CAP_UHS_DDR50	(1 << 19)	/* Host supports UHS DDR50 mode */
-#define MMC_CAP_DRIVER_TYPE_A	(1 << 23)	/* Host supports Driver Type A */
-#define MMC_CAP_DRIVER_TYPE_C	(1 << 24)	/* Host supports Driver Type C */
-#define MMC_CAP_DRIVER_TYPE_D	(1 << 25)	/* Host supports Driver Type D */
+#define MMC_CAP_DRIVER_TYPE_A	(1 << 23)	/* Host supports SD Driver Type A (eMMC Type 1) */
+#define MMC_CAP_DRIVER_TYPE_C	(1 << 24)	/* Host supports SD Driver Type C (eMMC Type 2) */
+#define MMC_CAP_DRIVER_TYPE_D	(1 << 25)	/* Host supports SD Driver Type D (eMMC Type 3) */
 #define MMC_CAP_CMD23		(1 << 30)	/* CMD23 supported. */
 #define MMC_CAP_HW_RESET	(1 << 31)	/* Hardware reset */
 
@@ -331,6 +333,8 @@ struct mmc_host {
 #define MMC_CAP2_HS400		(MMC_CAP2_HS400_1_8V | \
 				 MMC_CAP2_HS400_1_2V)
 #define MMC_CAP2_NONHOTPLUG	(1 << 25)	/*Don't support hotplug*/
+#define MMC_CAP2_DRIVER_TYPE_4	(1 << 31)	/* Host supports eMMC Driver Type 4 */
+
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
 	int			clk_requests;	/* internal reference counter */
