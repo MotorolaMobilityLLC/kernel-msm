f7dd90dc6b5bd192f2df033478d324e28543b0ca
Chris Fries <cfries@motorola.com>
2014-04-07 09:36:42 -0500

    f2fs: catchup to 3.15
    48b230a f2fs: fix wrong statistics of inline data
    3a8861e f2fs: check the acl's validity before setting
    6b4afdd f2fs: introduce f2fs_issue_flush to avoid redundant flush issue
    ce23447 f2fs: fix to cover io->bio with io_rwsem
    d54c795 f2fs: fix error path when fail to read inline data
    2d7b822 f2fs: use list_for_each_entry{_safe} for simplyfying code
    cf0ee0f f2fs: avoid free slab cache under spinlock
    6e452d6 f2fs: avoid unneeded lookup when xattr name length is too long
    df0f8dc f2fs: avoid unnecessary bio submit when wait page writeback
    3bb5e2c f2fs: return -EIO when node id is not matched
    808a1d7 f2fs: avoid RECLAIM_FS-ON-W warning
    479f40c f2fs: skip unnecessary node writes during fsync
    d928bfb f2fs: introduce fi->i_sem to protect fi's info
    58c4103 f2fs: change reclaim rate in percentage
    9179682 f2fs: add missing documentation for dir_level
    a5f4201 f2fs: remove unnecessary threshold
    cdfc41c f2fs: throttle the memory footprint with a sysfs entry
    40bb005 f2fs: avoid to drop nat entries due to the negative nr_shrink
    3cb5ad1 f2fs: call f2fs_wait_on_page_writeback instead of native function
    50c8cdb f2fs: introduce nr_pages_to_write for segment alignment
    d3baf95 f2fs: increase pages_skipped when skipping writepages
    87d6f89 f2fs: avoid small data writes by skipping writepages
    f8b2c1f f2fs: introduce get_dirty_dents for readability
    04c0938 f2fs: fix incorrect parsing with option string
    e4fc5fb f2fs: avoid to return incorrect errno of read_normal_summaries
    4bc8e9b f2fs: introduce f2fs_has_xattr_block for better readability
    90aa6dc f2fs: print type for each segment in segment_info's show
    910bb12 f2fs: check upper bound of ino value in f2fs_nfs_get_inode
    987c7c3 f2fs: introduce f2fs_has_inline_xattr for better readability
    28cdce0 f2fs: recover inline xattr data in roll-forward process
    d653788 f2fs: optimize restore_node_summary slightly
    46c0436 f2fs: format segment_info's show for better legibility
    e8512d2 f2fs: remove the unused ctor argument of f2fs_kmem_cache_create()
    b6ce391 f2fs: update start nid only once each circle
    20f7075 f2fs: fix wrong kernel coding style
    c81bf1c f2fs: fix to write node pages with WRITE_SYNC
    9cf3c38 f2fs: fix dirty page accounting when redirty
    695fd1e f2fs: use existing macro to clean up some codes
    81c1a0f f2fs: readahead contiguous SSA blocks for f2fs_gc
    ab9fa66 f2fs: add an sysfs entry to control the directory level
    3843154 f2fs: introduce large directory support
    5d0c6671 f2fs: remove costly bit operations for f2fs_find_entry
    
    Signed-off-by: Chris Fries <cfries@motorola.com>
    
    Conflicts:
    	Documentation/ABI/testing/sysfs-fs-f2fs
    	fs/f2fs/acl.c
    	fs/f2fs/checkpoint.c
    	fs/f2fs/f2fs.h
    	fs/f2fs/node.c
    	fs/f2fs/super.c
    
    Change-Id: I738dc2159f9acb987f58f2fe8db8a06e80cd9771
    Reviewed-on: http://gerrit.pcs.mot.com/624718
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -51,6 +51,7 @@ enum {
 	Opt_disable_ext_identify,
 	Opt_inline_xattr,
 	Opt_inline_data,
+	Opt_flush_merge,
 	Opt_err_continue,
 	Opt_err_panic,
 	Opt_err_recover,
@@ -70,6 +71,7 @@ static match_table_t f2fs_tokens = {
 	{Opt_disable_ext_identify, "disable_ext_identify"},
 	{Opt_inline_xattr, "inline_xattr"},
 	{Opt_inline_data, "inline_data"},
+	{Opt_flush_merge, "flush_merge"},
 	{Opt_err_continue, "errors=continue"},
 	{Opt_err_panic, "errors=panic"},
 	{Opt_err_recover, "errors=recover"},
@@ -80,6 +82,7 @@ static match_table_t f2fs_tokens = {
 enum {
 	GC_THREAD,	/* struct f2fs_gc_thread */
 	SM_INFO,	/* struct f2fs_sm_info */
+	NM_INFO,	/* struct f2fs_nm_info */
 	F2FS_SBI,	/* struct f2fs_sb_info */
 };
 
@@ -98,6 +101,8 @@ static unsigned char *__struct_ptr(struct f2fs_sb_info *sbi, int struct_type)
 		return (unsigned char *)sbi->gc_thread;
 	else if (struct_type == SM_INFO)
 		return (unsigned char *)SM_I(sbi);
+	else if (struct_type == NM_INFO)
+		return (unsigned char *)NM_I(sbi);
 	else if (struct_type == F2FS_SBI)
 		return (unsigned char *)sbi;
 	return NULL;
@@ -189,7 +194,9 @@ F2FS_RW_ATTR(SM_INFO, f2fs_sm_info, reclaim_segments, rec_prefree_segments);
 F2FS_RW_ATTR(SM_INFO, f2fs_sm_info, max_small_discards, max_discards);
 F2FS_RW_ATTR(SM_INFO, f2fs_sm_info, ipu_policy, ipu_policy);
 F2FS_RW_ATTR(SM_INFO, f2fs_sm_info, min_ipu_util, min_ipu_util);
+F2FS_RW_ATTR(NM_INFO, f2fs_nm_info, ram_thresh, ram_thresh);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, max_victim_search, max_victim_search);
+F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, dir_level, dir_level);
 
 #define ATTR_LIST(name) (&f2fs_attr_##name.attr)
 static struct attribute *f2fs_attrs[] = {
@@ -202,6 +209,8 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(ipu_policy),
 	ATTR_LIST(min_ipu_util),
 	ATTR_LIST(max_victim_search),
+	ATTR_LIST(dir_level),
+	ATTR_LIST(ram_thresh),
 	NULL,
 };
 
@@ -262,9 +271,9 @@ static int parse_options(struct super_block *sb, char *options)
 
 			if (!name)
 				return -ENOMEM;
-			if (!strncmp(name, "on", 2))
+			if (strlen(name) == 2 && !strncmp(name, "on", 2))
 				set_opt(sbi, BG_GC);
-			else if (!strncmp(name, "off", 3))
+			else if (strlen(name) == 3 && !strncmp(name, "off", 3))
 				clear_opt(sbi, BG_GC);
 			else {
 				kfree(name);
@@ -333,6 +342,9 @@ static int parse_options(struct super_block *sb, char *options)
 		case Opt_inline_data:
 			set_opt(sbi, INLINE_DATA);
 			break;
+		case Opt_flush_merge:
+			set_opt(sbi, FLUSH_MERGE);
+			break;
 		case Opt_err_continue:
 			clear_opt(sbi, ERRORS_RECOVER);
 			clear_opt(sbi, ERRORS_PANIC);
@@ -371,11 +383,15 @@ static struct inode *f2fs_alloc_inode(struct super_block *sb)
 	fi->i_current_depth = 1;
 	fi->i_advise = 0;
 	rwlock_init(&fi->ext.ext_lock);
+	init_rwsem(&fi->i_sem);
 
 	set_inode_flag(fi, FI_NEW_INODE);
 
 	if (test_opt(F2FS_SB(sb), INLINE_XATTR))
 		set_inode_flag(fi, FI_INLINE_XATTR);
+
+	/* Will be used by directory only */
+	fi->i_dir_level = F2FS_SB(sb)->dir_level;
 
 	return &fi->vfs_inode;
 }
@@ -550,6 +566,8 @@ static int f2fs_show_options(struct seq_file *seq, struct dentry *root)
 		seq_puts(seq, ",disable_ext_identify");
 	if (test_opt(sbi, INLINE_DATA))
 		seq_puts(seq, ",inline_data");
+	if (test_opt(sbi, FLUSH_MERGE))
+		seq_puts(seq, ",flush_merge");
 
 	return 0;
 }
@@ -562,13 +580,22 @@ static int segment_info_seq_show(struct seq_file *seq, void *offset)
 			le32_to_cpu(sbi->raw_super->segment_count_main);
 	int i;
 
+	seq_puts(seq, "format: segment_type|valid_blocks\n"
+		"segment_type(0:HD, 1:WD, 2:CD, 3:HN, 4:WN, 5:CN)\n");
+
 	for (i = 0; i < total_segs; i++) {
-		seq_printf(seq, "%u", get_valid_blocks(sbi, i, 1));
-		if (i != 0 && (i % 10) == 0)
-			seq_puts(seq, "\n");
+		struct seg_entry *se = get_seg_entry(sbi, i);
+
+		if ((i % 10) == 0)
+			seq_printf(seq, "%-5d", i);
+		seq_printf(seq, "%d|%-3u", se->type,
+					get_valid_blocks(sbi, i, 1));
+		if ((i % 10) == 9 || i == (total_segs - 1))
+			seq_putc(seq, '\n');
 		else
-			seq_puts(seq, " ");
+			seq_putc(seq, ' ');
 	}
+
 	return 0;
 }
 
@@ -660,6 +687,8 @@ static struct inode *f2fs_nfs_get_inode(struct super_block *sb,
 	struct inode *inode;
 
 	if (unlikely(ino < F2FS_ROOT_INO(sbi)))
+		return ERR_PTR(-ESTALE);
+	if (unlikely(ino >= NM_I(sbi)->max_nid))
 		return ERR_PTR(-ESTALE);
 
 	/*
@@ -808,6 +837,8 @@ static void init_sb_info(struct f2fs_sb_info *sbi)
 
 	for (i = 0; i < NR_COUNT_TYPE; i++)
 		atomic_set(&sbi->nr_pages[i], 0);
+
+	sbi->dir_level = DEF_DIR_LEVEL;
 }
 
 /*
@@ -920,11 +951,11 @@ static int f2fs_fill_super(struct super_block *sb, void *data, int silent)
 	sbi->por_doing = false;
 	spin_lock_init(&sbi->stat_lock);
 
-	mutex_init(&sbi->read_io.io_mutex);
+	init_rwsem(&sbi->read_io.io_rwsem);
 	sbi->read_io.sbi = sbi;
 	sbi->read_io.bio = NULL;
 	for (i = 0; i < NR_PAGE_TYPE; i++) {
-		mutex_init(&sbi->write_io[i].io_mutex);
+		init_rwsem(&sbi->write_io[i].io_rwsem);
 		sbi->write_io[i].sbi = sbi;
 		sbi->write_io[i].bio = NULL;
 	}
@@ -1119,7 +1150,7 @@ MODULE_ALIAS_FS("f2fs");
 static int __init init_inodecache(void)
 {
 	f2fs_inode_cachep = f2fs_kmem_cache_create("f2fs_inode_cache",
-			sizeof(struct f2fs_inode_info), NULL);
+			sizeof(struct f2fs_inode_info));
 	if (!f2fs_inode_cachep)
 		return -ENOMEM;
 	return 0;
