6b74b662ee31008c3bca27bf0792602981adfc43
Chris Fries <cfries@motorola.com>
2014-04-07 09:36:42 -0500

    f2fs: catchup to 3.15
    48b230a f2fs: fix wrong statistics of inline data
    3a8861e f2fs: check the acl's validity before setting
    6b4afdd f2fs: introduce f2fs_issue_flush to avoid redundant flush issue
    ce23447 f2fs: fix to cover io->bio with io_rwsem
    d54c795 f2fs: fix error path when fail to read inline data
    2d7b822 f2fs: use list_for_each_entry{_safe} for simplyfying code
    cf0ee0f f2fs: avoid free slab cache under spinlock
    6e452d6 f2fs: avoid unneeded lookup when xattr name length is too long
    df0f8dc f2fs: avoid unnecessary bio submit when wait page writeback
    3bb5e2c f2fs: return -EIO when node id is not matched
    808a1d7 f2fs: avoid RECLAIM_FS-ON-W warning
    479f40c f2fs: skip unnecessary node writes during fsync
    d928bfb f2fs: introduce fi->i_sem to protect fi's info
    58c4103 f2fs: change reclaim rate in percentage
    9179682 f2fs: add missing documentation for dir_level
    a5f4201 f2fs: remove unnecessary threshold
    cdfc41c f2fs: throttle the memory footprint with a sysfs entry
    40bb005 f2fs: avoid to drop nat entries due to the negative nr_shrink
    3cb5ad1 f2fs: call f2fs_wait_on_page_writeback instead of native function
    50c8cdb f2fs: introduce nr_pages_to_write for segment alignment
    d3baf95 f2fs: increase pages_skipped when skipping writepages
    87d6f89 f2fs: avoid small data writes by skipping writepages
    f8b2c1f f2fs: introduce get_dirty_dents for readability
    04c0938 f2fs: fix incorrect parsing with option string
    e4fc5fb f2fs: avoid to return incorrect errno of read_normal_summaries
    4bc8e9b f2fs: introduce f2fs_has_xattr_block for better readability
    90aa6dc f2fs: print type for each segment in segment_info's show
    910bb12 f2fs: check upper bound of ino value in f2fs_nfs_get_inode
    987c7c3 f2fs: introduce f2fs_has_inline_xattr for better readability
    28cdce0 f2fs: recover inline xattr data in roll-forward process
    d653788 f2fs: optimize restore_node_summary slightly
    46c0436 f2fs: format segment_info's show for better legibility
    e8512d2 f2fs: remove the unused ctor argument of f2fs_kmem_cache_create()
    b6ce391 f2fs: update start nid only once each circle
    20f7075 f2fs: fix wrong kernel coding style
    c81bf1c f2fs: fix to write node pages with WRITE_SYNC
    9cf3c38 f2fs: fix dirty page accounting when redirty
    695fd1e f2fs: use existing macro to clean up some codes
    81c1a0f f2fs: readahead contiguous SSA blocks for f2fs_gc
    ab9fa66 f2fs: add an sysfs entry to control the directory level
    3843154 f2fs: introduce large directory support
    5d0c6671 f2fs: remove costly bit operations for f2fs_find_entry
    
    Signed-off-by: Chris Fries <cfries@motorola.com>
    
    Conflicts:
    	Documentation/ABI/testing/sysfs-fs-f2fs
    	fs/f2fs/acl.c
    	fs/f2fs/checkpoint.c
    	fs/f2fs/f2fs.h
    	fs/f2fs/node.c
    	fs/f2fs/super.c
    
    Change-Id: I738dc2159f9acb987f58f2fe8db8a06e80cd9771
    Reviewed-on: http://gerrit.pcs.mot.com/624718
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -708,6 +708,11 @@ gc_more:
 		goto stop;
 	ret = 0;
 
+	/* readahead multi ssa blocks those have contiguous address */
+	if (sbi->segs_per_sec > 1)
+		ra_meta_pages(sbi, GET_SUM_BLOCK(sbi, segno), sbi->segs_per_sec,
+								META_SSA);
+
 	for (i = 0; i < sbi->segs_per_sec; i++)
 		do_garbage_collect(sbi, segno + i, &ilist, gc_type);
 
@@ -737,7 +742,7 @@ void build_gc_manager(struct f2fs_sb_info *sbi)
 int __init create_gc_caches(void)
 {
 	winode_slab = f2fs_kmem_cache_create("f2fs_gc_inodes",
-			sizeof(struct inode_entry), NULL);
+			sizeof(struct inode_entry));
 	if (!winode_slab)
 		return -ENOMEM;
 	return 0;
--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -107,6 +107,7 @@ static int do_read_inode(struct inode *inode)
 	fi->flags = 0;
 	fi->i_advise = ri->i_advise;
 	fi->i_pino = le32_to_cpu(ri->i_pino);
+	fi->i_dir_level = ri->i_dir_level;
 
 	get_extent_info(&fi->ext, ri->i_ext);
 	get_inline_info(fi, ri);
@@ -204,6 +205,7 @@ void update_inode(struct inode *inode, struct page *node_page)
 	ri->i_flags = cpu_to_le32(F2FS_I(inode)->i_flags);
 	ri->i_pino = cpu_to_le32(F2FS_I(inode)->i_pino);
 	ri->i_generation = cpu_to_le32(inode->i_generation);
+	ri->i_dir_level = F2FS_I(inode)->i_dir_level;
 
 	__set_inode_rdev(inode, ri);
 	set_cold_node(inode, node_page);
@@ -271,7 +273,7 @@ void f2fs_evict_inode(struct inode *inode)
 			inode->i_ino == F2FS_META_INO(sbi))
 		goto no_delete;
 
-	f2fs_bug_on(atomic_read(&F2FS_I(inode)->dirty_dents));
+	f2fs_bug_on(get_dirty_dents(inode));
 	remove_dirty_dir_inode(inode);
 
 	if (inode->i_nlink || is_bad_inode(inode))
