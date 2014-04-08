9b6fa30083bea9615a6f675de452e84b1ab593ed
Dmitry Ornatsky <cdo021c@motorola.com>
2014-04-08 08:11:06 -0500

    fs: pstore: add annotation support
    Add annotation type prz support to pstore. Publish
    annotate function for kernel modules. On warm power
    up pstore will make annotation information found in
    new ramprz available in annotation-ramoops file.
    
    Change-Id: I715222683466209d4d96913730d31f5f4c45bab5
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/625449
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/782685
    SME-Granted: SME Approvals Granted
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -367,6 +367,9 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 	case PSTORE_TYPE_PPC_OPAL:
 		sprintf(name, "powerpc-opal-%s-%lld", psname, id);
 		break;
+	case PSTORE_TYPE_ANNOTATE:
+		scnprintf(name, sizeof(name), "annotate-%s-%lld", psname, id);
+		break;
 	case PSTORE_TYPE_UNKNOWN:
 		scnprintf(name, sizeof(name), "unknown-%s-%lld", psname, id);
 		break;
