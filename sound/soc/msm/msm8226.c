1aca02bbfa168c9d43a6315c5681b5709510a8c8
Devin Kim <dojip.kim@lge.com>
2014-04-07 19:39:05 -0700

    ASoC: msm8226: Fix merge error
    Change-Id: I0bfcd98f7795c8dd4f189739ffef683794d4d7a0
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- sound/soc/msm/msm8226.c
+++ sound/soc/msm/msm8226.c
@@ -26,9 +26,8 @@
 #include <sound/pcm.h>
 #include <sound/jack.h>
 #include <sound/q6afe-v2.h>
-#include <mach/socinfo.h>
-#include <mach/subsystem_notif.h>
-#include <qdsp6v2/msm-pcm-routing-v2.h>
+
+#include "qdsp6v2/msm-pcm-routing-v2.h"
 #include "qdsp6v2/q6core.h"
 #include "../codecs/wcd9xxx-common.h"
 #include "../codecs/wcd9306.h"
