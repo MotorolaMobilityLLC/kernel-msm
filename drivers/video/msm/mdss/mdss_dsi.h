b301e6fbc1e34cf2dea1d8dac3d2cdbf4ff7a900
Zac Crosby <zac@motorola.com>
2014-04-09 17:49:16 -0500

    IKKRNBSP-3074: msm: mdss: Allow user space control of HBM
    Some displays support a special 'high brightness mode', which is a special
    max brightness mode which is separate from the display's usual brightness
    controls.  Add a method via sysfs to allow user space to enable/disable this
    mode.
    
    Change-Id: I4856d87622507e51317aaec4b75b847954823e2e
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/722880
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -316,6 +316,7 @@ struct mdss_dsi_ctrl_pdata {
 	int (*check_read_status) (struct mdss_dsi_ctrl_pdata *pdata);
 	int (*cmdlist_commit)(struct mdss_dsi_ctrl_pdata *ctrl, int from_mdp);
 	void (*switch_mode) (struct mdss_panel_data *pdata, int mode);
+	int (*set_hbm)(struct mdss_dsi_ctrl_pdata *ctrl, int state);
 	struct mdss_panel_data panel_data;
 	struct mdss_panel_config panel_config;
 	unsigned char *ctrl_base;
@@ -430,6 +431,9 @@ struct mdss_dsi_ctrl_pdata {
 	struct mdss_util_intf *mdss_util;
 
 	bool dfps_status;	/* dynamic refresh status */
+
+	struct dsi_panel_cmds hbm_on_cmds;
+	struct dsi_panel_cmds hbm_off_cmds;
 };
 
 struct dsi_status_data {
