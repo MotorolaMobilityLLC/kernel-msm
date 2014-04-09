1983c50f65a7fac1c9b24f04b4d61b29b2093e6b
Elena Satraitis <w04888@motorola.com>
2014-04-09 09:55:15 -0500

    drivers: ar0261: Add OTP support
    Adding in support to read the OTP data of the
    ar0261 to obtain the calibrated awb points
    and module information.
    
    Change-Id: Ia840d0000d985cc73c0389bdf490f5aa659ef27c
    Signed-off-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-on: http://gerrit.mot.com/625677
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Sumit Jamadar <xkbr38@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/ar0261.c
+++ drivers/media/platform/msm/camera_v2/sensor/ar0261.c
@@ -55,6 +55,15 @@ struct clk *ar0261_cam_mclk[2];
 
 static bool ar0261_devboard_config;
 
+#define AR0261_OTP_BASE_PAGE 0x32
+#define AR0261_OTP_NUM_PAGES 3
+#define AR0261_OTP_PAGE_SIZE 50
+#define AR0261_RESET_REG 0x301A
+#define AR0261_OTPM_CONTROL_REG 0x304A
+#define AR0261_OTPM_RECORD_REG 0x304C
+#define AR0261_OTPM_DATA_BASE_REG 0x3800
+#define AR0261_GPIO_CONTROL_REG 0x30F8
+
 #define VREG_ON              1
 #define VREG_OFF             0
 #define GPIO_REQUEST_USE     1
@@ -134,6 +143,151 @@ static int32_t msm_sensor_disable_i2c_mux(struct msm_camera_i2c_conf *i2c_conf)
 }
 #endif
 
+static int32_t ar0261_check_crc(uint8_t *otp_data)
+{
+	int32_t crc_match = 0;
+	uint16_t crc = 0x0000;
+	int i, j;
+	uint32_t tmp;
+	uint16_t otp_crc = otp_data[AR0261_OTP_PAGE_SIZE - 2] << 8 |
+		otp_data[AR0261_OTP_PAGE_SIZE - 1];
+
+	for (i = 0; i < (AR0261_OTP_PAGE_SIZE - 2); i++) {
+		tmp = otp_data[i] & 0xff;
+		for (j = 0; j < 8; j++) {
+			if (((crc & 0x8000) >> 8) ^ (tmp & 0x80))
+				crc = (crc << 1) ^ 0x8005;
+			else
+				crc = crc << 1;
+
+			tmp <<= 1;
+		}
+	}
+
+	if (crc != otp_crc)
+		crc_match = -1;
+
+	pr_debug("%s: OTP_CRC %x CRC %x matches? %d\n", __func__,
+			otp_crc, crc, crc_match);
+	return crc_match;
+}
+
+static void ar0261_read_otp(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int32_t rc = 0;
+	uint16_t otp_page = AR0261_OTP_BASE_PAGE;
+	int i;
+	int poll_times;
+	uint16_t otpm_control;
+	uint8_t *otp_data_ptr;
+
+	if (s_ctrl->sensor_otp.otp_read)
+		return;
+
+	otp_data_ptr = s_ctrl->sensor_otp.otp_info;
+
+	/* Enable Streaming */
+	rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_write(
+			s_ctrl->sensor_i2c_client,
+			AR0261_RESET_REG, 0x021C,
+			MSM_CAMERA_I2C_WORD_DATA);
+	if (rc < 0) {
+		pr_err("%s: Unable to streamon the sensor!\n", __func__);
+		goto exit0;
+	}
+
+	/* Work in reverse order of the pages to find the correct data */
+	for (i = AR0261_OTP_NUM_PAGES; i > 0; i--) {
+		/* Set Page Index */
+		rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_write(
+				s_ctrl->sensor_i2c_client,
+				AR0261_OTPM_RECORD_REG, (otp_page << 8),
+				MSM_CAMERA_I2C_WORD_DATA);
+		otp_page -= 0x1;
+		if (rc < 0) {
+			pr_err("%s: Unable to write OTP record reg!\n",
+					__func__);
+			goto exit1;
+		}
+
+		/* Issue Read of OTP memory to put memory into the OTP data
+		 * registers of AR0261 */
+		rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_write(
+				s_ctrl->sensor_i2c_client,
+				AR0261_OTPM_CONTROL_REG, 0x0010,
+				MSM_CAMERA_I2C_WORD_DATA);
+		if (rc < 0) {
+			pr_err("%s: Unable to write OTP control reg read!\n",
+					__func__);
+			goto exit1;
+		}
+
+		/* Poll to see if read is complete in the internal memory */
+		poll_times = 0;
+		while (poll_times < 3) {
+			rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_read(
+					s_ctrl->sensor_i2c_client,
+					AR0261_OTPM_CONTROL_REG, &otpm_control,
+					MSM_CAMERA_I2C_WORD_DATA);
+			if (rc < 0) {
+				pr_err("%s: Unable to read OTPM_CONTROL reg!\n",
+						__func__);
+				goto exit1;
+			}
+
+			if (0x20 == (otpm_control & 0x20))
+				break;
+
+			usleep_range(2000, 3000);
+			poll_times++;
+		}
+
+		/* Check to see if read was successful so that we can
+		 * officially read the OTP data over the i2c bus */
+		if (0x40 == (otpm_control & 0x40)) {
+			rc = s_ctrl->sensor_i2c_client->i2c_func_tbl->
+				i2c_read_seq(s_ctrl->sensor_i2c_client,
+						AR0261_OTPM_DATA_BASE_REG,
+						otp_data_ptr,
+						AR0261_OTP_PAGE_SIZE);
+			if (rc < 0) {
+				pr_err("%s: Unable to read otp data over i2c!\n"
+						, __func__);
+				goto exit1;
+			}
+
+			/* check crc */
+			rc = ar0261_check_crc(otp_data_ptr);
+			if (rc < 0) {
+				pr_warn("%s: CRC failed on page %x!\n",
+						__func__, (otp_page - 0x1));
+				s_ctrl->sensor_otp.otp_crc_pass = 0;
+			} else {
+				s_ctrl->sensor_otp.otp_crc_pass = 1;
+				break;
+			}
+		} else
+			pr_warn("%s: OTP Read not successful, on page %x.\n",
+					__func__, (otp_page + 0x1));
+
+	}
+
+	s_ctrl->sensor_otp.otp_read = 1;
+exit1:
+	/* Issue Stream off */
+	s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_write(
+			s_ctrl->sensor_i2c_client,
+			AR0261_RESET_REG, 0x0218,
+			MSM_CAMERA_I2C_WORD_DATA);
+
+	s_ctrl->sensor_i2c_client->i2c_func_tbl->i2c_write(
+			s_ctrl->sensor_i2c_client,
+			AR0261_GPIO_CONTROL_REG, 0x0330,
+			MSM_CAMERA_I2C_WORD_DATA);
+exit0:
+	return;
+}
+
 /* ar0261 match_id function */
 int32_t ar0261_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 {
@@ -190,9 +344,11 @@ int32_t ar0261_match_id(struct msm_sensor_ctrl_t *s_ctrl)
 		}
 	}
 
-	if (rc >= 0)
+	if (rc >= 0) {
 		pr_info("%s success with slave addr 0x%x\n", __func__,
 				s_ctrl->sensor_i2c_client->cci_client->sid);
+		ar0261_read_otp(s_ctrl);
+	}
 
 	return rc;
 }
@@ -410,6 +566,15 @@ static int32_t ar0261_platform_probe(struct platform_device *pdev)
 			/* Devboard is used */
 			ar0261_devboard_config = true;
 
+		ar0261_s_ctrl.sensor_otp.otp_info = devm_kzalloc(&pdev->dev,
+				AR0261_OTP_PAGE_SIZE, GFP_KERNEL);
+		if (ar0261_s_ctrl.sensor_otp.otp_info == NULL) {
+			pr_err("%s: Unable to allocate memory for OTP!\n",
+					__func__);
+			return -ENOMEM;
+		}
+
+		ar0261_s_ctrl.sensor_otp.size = AR0261_OTP_PAGE_SIZE;
 		rc = msm_sensor_platform_probe(pdev, match->data);
 	} else {
 		pr_err("%s:%d failed match device\n", __func__, __LINE__);
@@ -417,6 +582,12 @@ static int32_t ar0261_platform_probe(struct platform_device *pdev)
 	}
 
 	return rc;
+}
+
+static int32_t ar0261_get_module_info(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	/* Place holder so that data is transferred to user space */
+	return 0;
 }
 
 static int __init ar0261_init_module(void)
@@ -445,6 +616,7 @@ static struct msm_sensor_fn_t ar0261_func_tbl = {
 	.sensor_power_up = ar0261_sensor_power_up,
 	.sensor_power_down = ar0261_sensor_power_down,
 	.sensor_match_id = ar0261_match_id,
+	.sensor_get_module_info = ar0261_get_module_info,
 };
 
 static struct msm_sensor_ctrl_t ar0261_s_ctrl = {
