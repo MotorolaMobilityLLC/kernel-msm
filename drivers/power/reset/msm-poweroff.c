1bddbe66ab8efc9900fd29766e13a35100b5f32b
Stanley Lei <a23075@motorola.com>
2014-04-08 17:14:52 -0700

    msm: restart: Force cold reboot for reboot bootloader
    This patch forces the cold reboot for the command "adb reboot bootloader"
    to avoid the 2nd reboot by modem when the user tries to continue boot
    from boot menu.
    
    Change-Id: Iaa49a7eaecaefa4ef43d3ed1f9ad5ebf5c7e6a59
    Signed-off-by: Stanley Lei <a23075@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/625574
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Qing Chang <qing@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/783404
    SME-Granted: SME Approvals Granted
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -303,6 +303,14 @@ static void msm_restart_prepare(const char *cmd)
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
 			__raw_writel(0x77665500, restart_reason);
+			/* set reboot_bl flag in PMIC for cold reset */
+			qpnp_pon_store_extra_reset_info(RESET_EXTRA_REBOOT_BL_REASON,
+				RESET_EXTRA_REBOOT_BL_REASON);
+			/*
+			 * force cold reboot here to avoid impaction from
+			 * modem double reboot workaround solution.
+			 */
+			qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_RECOVERY);
