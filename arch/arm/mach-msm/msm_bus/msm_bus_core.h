090eb32aaa08a794fa6df5ad5125c40da1e954df
Girish Mahadevan <girishm@codeaurora.org>
2014-04-10 16:19:44 -0600

    msm: msm_bus: Introduce new dynamic throttling scheme
    Introduce a new dynamic throttling scheme for certain "non-real time"
    masters based on the available bus bandwidth and the currently voted
    bandwidth requirements of some "real time" masters.
    
    Change-Id: Icf019f89bbe2425c706f2fc49884f41110a78299
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>
    (cherry picked from commit 1f8da21605b0f7f53e89377cf97530d3b40612c0)

--- arch/arm/mach-msm/msm_bus/msm_bus_core.h
+++ arch/arm/mach-msm/msm_bus/msm_bus_core.h
@@ -53,6 +53,12 @@ enum msm_bus_hw_sel {
 	MSM_BUS_BIMC,
 };
 
+enum {
+	SLAVE_NODE,
+	MASTER_NODE,
+	CLK_NODE,
+	NR_LIM_NODE,
+};
 extern struct bus_type msm_bus_type;
 
 struct msm_bus_node_info {
@@ -87,9 +93,13 @@ struct msm_bus_node_info {
 	u64 cur_lim_bw;
 	unsigned int mode_thresh;
 	bool dual_conf;
+	bool nr_lim;
+	u32 ff;
+	bool rt_mas;
 	u64 *bimc_bw;
 	u32 bimc_gp;
 	u32 bimc_thmp;
+	u64 floor_bw;
 	const char *name;
 };
 
@@ -122,6 +132,8 @@ struct msm_bus_inode_info {
 	struct msm_bus_node_info *node_info;
 	uint64_t max_bw;
 	uint64_t max_clk;
+	uint64_t cur_lim_bw;
+	uint64_t cur_prg_bw;
 	struct msm_bus_link_info link_info;
 	int num_pnodes;
 	struct path_node *pnode;
@@ -159,6 +171,8 @@ struct msm_bus_hw_algorithm {
 	void (*config_master)(struct msm_bus_fabric_registration *fab_pdata,
 		struct msm_bus_inode_info *info,
 		uint64_t req_clk, uint64_t req_bw);
+	void (*config_limiter)(struct msm_bus_fabric_registration *fab_pdata,
+		struct msm_bus_inode_info *info);
 };
 
 struct msm_bus_fabric_device {
@@ -169,9 +183,28 @@ struct msm_bus_fabric_device {
 	const struct msm_bus_board_algorithm *board_algo;
 	struct msm_bus_hw_algorithm hw_algo;
 	int visited;
+	int num_nr_lim;
+	u64 nr_lim_thresh;
+	u32 eff_fact;
 };
 #define to_msm_bus_fabric_device(d) container_of(d, \
 		struct msm_bus_fabric_device, d)
+
+struct msm_bus_fabric {
+	struct msm_bus_fabric_device fabdev;
+	int ahb;
+	void *cdata[NUM_CTX];
+	bool arb_dirty;
+	bool clk_dirty;
+	struct radix_tree_root fab_tree;
+	int num_nodes;
+	struct list_head gateways;
+	struct msm_bus_inode_info info;
+	struct msm_bus_fabric_registration *pdata;
+	void *hw_data;
+};
+#define to_msm_bus_fabric(d) container_of(d, \
+	struct msm_bus_fabric, d)
 
 
 struct msm_bus_fab_algorithm {
@@ -194,6 +227,8 @@ struct msm_bus_fab_algorithm {
 	void (*config_master)(struct msm_bus_fabric_device *fabdev,
 		struct msm_bus_inode_info *info, uint64_t req_clk,
 		uint64_t req_bw);
+	void (*config_limiter)(struct msm_bus_fabric_device *fabdev,
+		struct msm_bus_inode_info *info);
 };
 
 struct msm_bus_board_algorithm {
