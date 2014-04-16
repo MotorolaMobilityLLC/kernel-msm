fbc58b51bdb967c5d96db4face62b1c74e4b4fe0
Fred Fettinger <fred.fettinger@motorola.com>
2014-04-16 18:13:18 -0500

    msm: kgsl: process_private: remove use of kref_get_unless_zero
    kref_get_unless_zero() is unsafe since if the refcount is zero, and
    kgsl_driver.process_mutex is no longer locked by
    kgsl_destroy_process_private(), the kgsl_process_private has been freed
    and should not be used. Even checking if the refcount=0 is unsafe after
    kfree() is called on the process_private. This can be demonstrated
    easily with CONFIG_SLUG_DEBUG=y.
    
    Instead, change kgsl_process_private_get() to find the process_private
    in the process_list and increment the refcount while the process_mutex
    is held. Since kgsl_process_private_put also holds this mutex while
    destroying process_private, this ensures that the process_private is
    not used after it is freed.
    
    Change-Id: I73b1949d1162b4569accdc754cb5d6fac348a2d9
    Signed-off-by: Fred Fettinger <fred.fettinger@motorola.com>
    Reviewed-on: http://gerrit.mot.com/627848
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -833,8 +833,7 @@ kgsl_find_process_private(struct kgsl_device_private *cur_dev_priv)
 	mutex_lock(&kgsl_driver.process_mutex);
 	list_for_each_entry(private, &kgsl_driver.process_list, list) {
 		if (private->pid == task_tgid_nr(current)) {
-			if (!kgsl_process_private_get(private))
-				private = NULL;
+			kref_get(&private->refcount);
 			goto done;
 		}
 	}
