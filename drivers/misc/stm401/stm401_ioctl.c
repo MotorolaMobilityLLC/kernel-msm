9d82d11f3f83f790fc4adbf255d98eadd3a19a70
Jonathan Kliegman <kliegs@motorola.com>
2014-04-16 18:25:25 -0400

    drivers: misc: stm401: Add rate registers for IR
    Allow the IR raw and gesture sensors to specify the requested data rate.
    
    This also adds the definition for the IR status register used by the
    rate adjustment code to report on the state.
    
    Change-Id: I3f3d5c89f0ad5952c974f306404fe32981c2cc62
    Signed-off-by: Jonathan Kliegman <kliegs@motorola.com>
    Reviewed-on: http://gerrit.mot.com/628589
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/stm401/stm401_ioctl.c
+++ drivers/misc/stm401/stm401_ioctl.c
@@ -647,6 +647,36 @@ long stm401_misc_ioctl(struct file *file, unsigned int cmd,
 			dev_err(&stm401_misc_data->client->dev,
 				"Unable to write IR config reg %d\n", err);
 		break;
+	case STM401_IOCTL_SET_IR_GESTURE_DELAY:
+		dev_dbg(&ps_stm401->client->dev,
+			"STM401_IOCTL_SET_IR_GESTURE_DELAY");
+		delay = 0;
+		if (copy_from_user(&delay, argp, sizeof(delay))) {
+			dev_dbg(&ps_stm401->client->dev,
+				"Copy IR gesture delay returned error\n");
+			err = -EFAULT;
+			break;
+		}
+		stm401_cmdbuff[0] = IR_GESTURE_RATE;
+		stm401_cmdbuff[1] = delay;
+		stm401_g_ir_gesture_delay = delay;
+		err = stm401_i2c_write(ps_stm401, stm401_cmdbuff, 2);
+		break;
+	case STM401_IOCTL_SET_IR_RAW_DELAY:
+		dev_dbg(&ps_stm401->client->dev,
+			"STM401_IOCTL_SET_IR_RAW_DELAY");
+		delay = 0;
+		if (copy_from_user(&delay, argp, sizeof(delay))) {
+			dev_dbg(&ps_stm401->client->dev,
+				"Copy IR raw delay returned error\n");
+			err = -EFAULT;
+			break;
+		}
+		stm401_cmdbuff[0] = IR_RAW_RATE;
+		stm401_cmdbuff[1] = delay;
+		stm401_g_ir_raw_delay = delay;
+		err = stm401_i2c_write(ps_stm401, stm401_cmdbuff, 2);
+		break;
 
 		case STM401_IOCTL_ENABLE_BREATHING:
 			if (copy_from_user(&byte, argp, sizeof(byte))) {
