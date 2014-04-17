2985fab6bedd6d8fe645082c731ff95922bfa539
Russ W. Knize <Russ.Knize@motorola.com>
2014-04-17 10:19:55 -0500

    mmc: health and performance reporting
    Propagated from (CR)
    
    Expose device health and performance attributes that are available
    in the eMMC v5.0 and SD v3.0 specs.
    
    Change-Id: I18b277303a8f6a5a73d2fd0e63989a01ef24c7d1
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -619,6 +619,37 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 		card->ext_csd.data_sector_size = 512;
 	}
 
+	/* eMMC v4.5 or later */
+	if (card->ext_csd.rev >= 7) {
+		card->ext_csd.firmware_version[0] =
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 0] << 0 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 1] << 8 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 2] << 16 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 3] << 24;
+		card->ext_csd.firmware_version[1] =
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 4] << 0 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 5] << 8 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 6] << 16 |
+			ext_csd[EXT_CSD_FIRMWARE_VERSION + 7] << 24;
+		card->ext_csd.device_version =
+			ext_csd[EXT_CSD_DEVICE_VERSION + 0] << 0 |
+			ext_csd[EXT_CSD_DEVICE_VERSION + 1] << 8;
+		card->ext_csd.raw_optimal_trim_size =
+			ext_csd[EXT_CSD_OPTIMAL_TRIM_UNIT_SIZE];
+		card->ext_csd.raw_optimal_write_size =
+			ext_csd[EXT_CSD_OPTIMAL_WRITE_SIZE];
+		card->ext_csd.raw_optimal_read_size =
+			ext_csd[EXT_CSD_OPTIMAL_READ_SIZE];
+		card->ext_csd.pre_eol_info =
+			ext_csd[EXT_CSD_PRE_EOL_INFO];
+		card->ext_csd.dev_life_time_est_a =
+			ext_csd[EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A];
+		card->ext_csd.dev_life_time_est_b =
+			ext_csd[EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_B];
+		memcpy(card->ext_csd.vendor_health_report,
+		       &ext_csd[EXT_CSD_VENDOR_PROPRIETARY_HEALTH_REPORT],
+		       sizeof(card->ext_csd.vendor_health_report));
+	}
 out:
 	return err;
 }
@@ -706,6 +737,28 @@ MMC_DEV_ATTR(enhanced_area_offset, "%llu\n",
 MMC_DEV_ATTR(enhanced_area_size, "%u\n", card->ext_csd.enhanced_area_size);
 MMC_DEV_ATTR(raw_rpmb_size_mult, "%#x\n", card->ext_csd.raw_rpmb_size_mult);
 MMC_DEV_ATTR(rel_sectors, "%#x\n", card->ext_csd.rel_sectors);
+MMC_DEV_ATTR(firmware_version, "0x%08x%08x\n",
+	card->ext_csd.firmware_version[1], card->ext_csd.firmware_version[0]);
+MMC_DEV_ATTR(device_version, "0x%04x\n", card->ext_csd.device_version);
+MMC_DEV_ATTR(optimal_trim_unit_size, "%d\n",
+		card->ext_csd.raw_optimal_trim_size);
+MMC_DEV_ATTR(optimal_write_size, "%d\n", card->ext_csd.raw_optimal_write_size);
+MMC_DEV_ATTR(optimal_read_size, "%d\n", card->ext_csd.raw_optimal_read_size);
+MMC_DEV_ATTR(pre_eol_info, "%d\n", card->ext_csd.pre_eol_info);
+MMC_DEV_ATTR(device_life_time_est_typ_a, "%d\n",
+		card->ext_csd.dev_life_time_est_a);
+MMC_DEV_ATTR(device_life_time_est_typ_b, "%d\n",
+		card->ext_csd.dev_life_time_est_b);
+MMC_DEV_ATTR(vendor_proprietary_health_report,
+		"%08x%08x%08x%08x%08x%08x%08x%08x\n",
+		card->ext_csd.vendor_health_report[0],
+		card->ext_csd.vendor_health_report[1],
+		card->ext_csd.vendor_health_report[2],
+		card->ext_csd.vendor_health_report[3],
+		card->ext_csd.vendor_health_report[4],
+		card->ext_csd.vendor_health_report[5],
+		card->ext_csd.vendor_health_report[6],
+		card->ext_csd.vendor_health_report[7]);
 
 static struct attribute *mmc_std_attrs[] = {
 	&dev_attr_cid.attr,
@@ -724,6 +777,15 @@ static struct attribute *mmc_std_attrs[] = {
 	&dev_attr_enhanced_area_size.attr,
 	&dev_attr_raw_rpmb_size_mult.attr,
 	&dev_attr_rel_sectors.attr,
+	&dev_attr_firmware_version.attr,
+	&dev_attr_device_version.attr,
+	&dev_attr_optimal_trim_unit_size.attr,
+	&dev_attr_optimal_write_size.attr,
+	&dev_attr_optimal_read_size.attr,
+	&dev_attr_pre_eol_info.attr,
+	&dev_attr_device_life_time_est_typ_a.attr,
+	&dev_attr_device_life_time_est_typ_b.attr,
+	&dev_attr_vendor_proprietary_health_report.attr,
 	NULL,
 };
 
@@ -1079,6 +1141,7 @@ static int mmc_select_driver_type(struct mmc_card *card)
 		host_drv_type, card_drv_type);
 	mmc_host_clk_release(card->host);
 
+	pr_info("%s: %s: %d\n", mmc_hostname(card->host), __func__, drv_type);
 	/* We send the driver type as part of the HS_TIMING command. */
 	card->ext_csd.drv_type = drv_type;
 
