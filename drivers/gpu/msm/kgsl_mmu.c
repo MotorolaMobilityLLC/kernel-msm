d9f8302ed44d3f37a83be8e98d53af5e708d7755
Jordan Crouse <jcrouse@codeaurora.org>
2014-04-17 10:05:21 -0600

    msm: kgsl: Mark the IOMMU setstate memory as read only
    Mark the IOMMU setstate memory as read only in the pagetable.
    
    Change-Id: Ic0dedbadb19e499c749cd744c3e89be3bcb4c2a2
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    (cherry picked from commit 970b5dd8ea04905a471ea6960bf0eb43c16dc49f)
    Reviewed-on: http://gerrit.mot.com/641207
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>

--- drivers/gpu/msm/kgsl_mmu.c
+++ drivers/gpu/msm/kgsl_mmu.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -371,6 +371,10 @@ int kgsl_mmu_init(struct kgsl_device *device)
 	status = kgsl_allocate_contiguous(&mmu->setstate_memory, PAGE_SIZE);
 	if (status)
 		return status;
+
+	/* Mark the setstate memory as read only */
+	mmu->setstate_memory.flags |= KGSL_MEMFLAGS_GPUREADONLY;
+
 	kgsl_sharedmem_set(&mmu->setstate_memory, 0, 0,
 				mmu->setstate_memory.size);
 
