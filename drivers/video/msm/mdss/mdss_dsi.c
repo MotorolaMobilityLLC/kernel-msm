e300c83c2802aee6958c90c28af958c03b34bef6
Fred Fettinger <fred.fettinger@motorola.com>
2014-04-22 16:51:19 -0500

    msm: mdss: init mode_gpio when mode_gpio_state=NOT_VALID
    If mode_gpio_state == MODE_GPIO_NOT_VALID, mode_gpio is left untouched
    in dsi_panel_device_register(). Since mode_gpio was originally
    initialized by a memset to 0, mdss_dsi_request_gpios() incorrectly
    requests gpio 0 for mode_gpio. This causes problems for the unlucky
    driver which needs to use gpio 0.
    
    The defect was originally introduced in:
    
    eec66f0 msm: mdss: Support panel mode selection through GPIO
    
    But did not cause a problem until mode_gpio was used when
    mode_gpio_state == MODE_GPIO_NOT_VALID on this change:
    
    246dedb mdss: dsi: Handle gpio configuration properly
    
    Fix this problem by always initializing mode_gpio with
    of_get_named_gpio(), similar to the other gpios in
    dsi_panel_device_register().
    
    Change-Id: I47daf5e878710d97fe032d38ba1293e5d62bccb3
    Signed-off-by: Fred Fettinger <fred.fettinger@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1606,11 +1606,10 @@ int dsi_panel_device_register(struct device_node *pan_node,
 		pr_err("%s:%d, reset gpio not specified\n",
 						__func__, __LINE__);
 
+	ctrl_pdata->mode_gpio = of_get_named_gpio(
+				ctrl_pdev->dev.of_node,
+				"qcom,platform-mode-gpio", 0);
 	if (pinfo->mode_gpio_state != MODE_GPIO_NOT_VALID) {
-
-		ctrl_pdata->mode_gpio = of_get_named_gpio(
-					ctrl_pdev->dev.of_node,
-					"qcom,platform-mode-gpio", 0);
 		if (!gpio_is_valid(ctrl_pdata->mode_gpio))
 			pr_info("%s:%d, mode gpio not specified\n",
 							__func__, __LINE__);
