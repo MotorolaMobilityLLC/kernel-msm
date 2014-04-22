69b6bfd46ce6af52ea2a19eae14b0493f36b2126
Tarun Karra <tkarra@codeaurora.org>
2014-04-22 16:16:11 -0700

    msm: kgsl: Restructure IOMMU clock management
    Restructure IOMMU clock management code to enable/disable clocks
    based on iommu unit instead of context. In hardware IOMMU clocks
    are associated with each IOMMU unit. There is no granularity of
    clock control for each iommu context. Turning on the clocks for
    one context in a IOMMU unit automatically turns on the clocks for
    all the contexts in IOMMU unit. Reduce the number of clock
    enable/disable calls and structure the software design as per
    hardware design by doing clock management per IOMMU unit.
    
    Change-Id: Ib54dbb7a31d6b30b65c07ef130bee149e3e587d8
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    (cherry picked from commit 56de958f1ffe6afad3e0dffc36c861172975d3ca)
    Reviewed-on: http://gerrit.mot.com/702937
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- drivers/gpu/msm/kgsl_iommu.h
+++ drivers/gpu/msm/kgsl_iommu.h
@@ -98,10 +98,16 @@ struct kgsl_iommu_register_list {
  * Max number of iommu units that the gpu core can have
  * On APQ8064, KGSL can control a maximum of 2 IOMMU units.
  */
-#define KGSL_IOMMU_MAX_UNITS 2
+enum kgsl_iommu_units {
+	KGSL_IOMMU_UNIT_0 = 0,
+	KGSL_IOMMU_UNIT_1 = 1,
+	KGSL_IOMMU_MAX_UNITS = 2,
+};
 
 /* Max number of iommu contexts per IOMMU unit */
 #define KGSL_IOMMU_MAX_DEVS_PER_UNIT 2
+/* Max number of iommu clks per IOMMU unit */
+#define KGSL_IOMMU_MAX_CLKS 4
 
 /* Macros to read/write IOMMU registers */
 #define KGSL_IOMMU_SET_CTX_REG_Q(iommu, iommu_unit, ctx, REG, val)	\
@@ -172,7 +178,6 @@ struct kgsl_iommu_register_list {
  * are on, else the clocks are off
  * fault: Flag when set indicates that this iommu device has caused a page
  * fault
- * @clk_enable_count: The ref count of clock enable calls
  */
 struct kgsl_iommu_device {
 	struct device *dev;
@@ -182,7 +187,6 @@ struct kgsl_iommu_device {
 	bool clk_enabled;
 	struct kgsl_device *kgsldev;
 	int fault;
-	atomic_t clk_enable_count;
 };
 
 /*
@@ -198,6 +202,8 @@ struct kgsl_iommu_device {
  * @iommu_halt_enable: Valid only on IOMMU-v1, when set indicates that the iommu
  * unit supports halting of the IOMMU, which can be enabled while programming
  * the IOMMU registers for synchronization
+ * @clk_enable_count: The ref count of clock enable calls
+ * @clks: iommu unit clks
  */
 struct kgsl_iommu_unit {
 	struct kgsl_iommu_device dev[KGSL_IOMMU_MAX_DEVS_PER_UNIT];
@@ -205,6 +211,8 @@ struct kgsl_iommu_unit {
 	struct kgsl_memdesc reg_map;
 	unsigned int ahb_base;
 	int iommu_halt_enable;
+	atomic_t clk_enable_count;
+	struct clk *clks[KGSL_IOMMU_MAX_CLKS];
 };
 
 /*
@@ -254,13 +262,13 @@ struct kgsl_iommu_pt {
  * struct kgsl_iommu_disable_clk_param - Parameter struct for disble clk event
  * @mmu: The mmu pointer
  * @rb_level: the rb level in which the timestamp of the event belongs to
- * @ctx_id: The IOMMU context whose clock is to be turned off
+ * @unit: The IOMMU unit whose clock is to be turned off
  * @ts: Timestamp on which clock is to be disabled
  */
 struct kgsl_iommu_disable_clk_param {
 	struct kgsl_mmu *mmu;
 	int rb_level;
-	int ctx_id;
+	int unit;
 	unsigned int ts;
 };
 
