9ecded9fc945e918f49dd58ae15c2c1641e3eb9b
John Zafiris <qemx13@motorola.com>
2014-04-23 16:41:38 -0500

    atmel_mxt_ts_mmi: add support for predictive touch
    Change-Id: I5c10239e64d0b687c1b10bb865a90d75edbd3f51
    Signed-off-by: John Zafiris <qemx13@motorola.com>
    Reviewed-on: http://gerrit.mot.com/627721
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -27,6 +27,9 @@
 #include <linux/slab.h>
 #include <linux/regulator/consumer.h>
 #include <linux/gpio.h>
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+#include "touchx.h"
+#endif
 
 #define ATMEL_MXT_STATES { \
 	MXT_DEF(UNKNOWN), \
@@ -346,6 +349,11 @@ struct mxt_data {
 	struct mxt_tdat_section fw;
 	struct mxt_tdat_section tsett;
 };
+
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+struct touchxs touchxp;
+EXPORT_SYMBOL(touchxp);
+#endif
 
 static int mxt_resume(struct device *dev);
 static int mxt_get_sensor_state(struct mxt_data *data);
@@ -1023,6 +1031,10 @@ static void mxt_proc_t100_message(struct mxt_data *data, u8 *message)
 	int x;
 	int y;
 	int tool;
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+	unsigned char number_of_fingers_actually_touching;
+	static char finger_register[32];
+#endif
 
 	/* do not report events if input device not yet registered */
 	if (!data->enable_reporting)
@@ -1047,6 +1059,13 @@ static void mxt_proc_t100_message(struct mxt_data *data, u8 *message)
 		(data->t100_aux_ampl) ? message[data->t100_aux_ampl] : 0,
 		(data->t100_aux_vect) ? message[data->t100_aux_vect] : 0);
 
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+	if (touchxp.touchx)
+		mutex_lock(&touchxp.virtual_touch_mutex);
+
+	finger_register[id < 32 ? id : 31] += 1;
+#endif
+
 	input_mt_slot(input_dev, id);
 
 	if (status & MXT_T100_DETECT) {
@@ -1056,6 +1075,20 @@ static void mxt_proc_t100_message(struct mxt_data *data, u8 *message)
 			tool = MT_TOOL_PEN;
 		else
 			tool = MT_TOOL_FINGER;
+
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+		if (touchxp.touchx) {
+			int i;
+			number_of_fingers_actually_touching = 0;
+			for (i = 0; i < 32; i++) {
+				if (finger_register[i] != 0)
+					number_of_fingers_actually_touching++;
+			}
+			touchxp.touch_magic_dev = input_dev;
+			touchxp.touchx(&x, &y, id,
+				       number_of_fingers_actually_touching);
+		}
+#endif
 
 		/* Touch active */
 		input_mt_report_slot_state(input_dev, tool, 1);
@@ -1080,8 +1113,17 @@ static void mxt_proc_t100_message(struct mxt_data *data, u8 *message)
 					 message[data->t100_aux_vect]);
 	} else {
 		/* Touch no longer active, close out slot */
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+		touchxp.finger_down = 0;
+		finger_register[id < 32 ? id : 31] = 0;
+#endif
 		input_mt_report_slot_state(input_dev, MT_TOOL_FINGER, 0);
 	}
+
+#ifdef CONFIG_TOUCHSCREEN_TOUCHX_BASE
+	if (touchxp.touchx)
+		mutex_unlock(&touchxp.virtual_touch_mutex);
+#endif
 
 	data->update_input = true;
 }
