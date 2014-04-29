0bd9604da6fbe43a9eaaaa15bd23e0673cd0272b
Patrick Auchter <auchter@motorola.com>
2014-04-29 14:05:51 -0500

    msm: mdss: remove wait for pingpong during status check
    The wait for pingpong is only necessary if using the method of triggering
    BTA to check the panel status.  Since we don't use this method, the wait
    for pingpong can be removed.
    
    Change-Id: I524ad1d974d6d62ca65bde89d8f54555ab532fd5
    Signed-off-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-on: http://gerrit.mot.com/631642
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    SLTApproved: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_status.c
+++ drivers/video/msm/mdss/mdss_dsi_status.c
@@ -100,30 +100,11 @@ static void check_dsi_ctrl_status(struct work_struct *work)
 	mutex_lock(&ctl->offlock);
 	if (ctl->shared_lock)
 		mutex_lock(ctl->shared_lock);
-	if (ctl->wait_pingpong)
-		mutex_lock(&mdp5_data->ov_lock);
-
-	/*
-	 * For the command mode panels, we return pan display
-	 * IOCTL on vsync interrupt. So, after vsync interrupt comes
-	 * and when DMA_P is in progress, if the panel stops responding
-	 * and if we trigger BTA before DMA_P finishes, then the DSI
-	 * FIFO will not be cleared since the DSI data bus control
-	 * doesn't come back to the host after BTA. This may cause the
-	 * display reset not to be proper. Hence, wait for DMA_P done
-	 * for command mode panels before triggering BTA.
-	 */
-	if (ctl->wait_pingpong)
-		ctl->wait_pingpong(ctl, NULL);
-
-	pr_debug("%s: DSI ctrl wait for ping pong done\n", __func__);
 
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 	ret = ctrl_pdata->check_status(ctrl_pdata);
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 
-	if (ctl->wait_pingpong)
-		mutex_unlock(&mdp5_data->ov_lock);
 	if (ctl->shared_lock)
 		mutex_unlock(ctl->shared_lock);
 	mutex_unlock(&ctl->offlock);
