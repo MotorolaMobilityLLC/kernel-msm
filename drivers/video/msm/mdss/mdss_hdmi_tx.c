dced82848791e30fbdf6bd0683ccf1aadcf42e96
Devin Kim <dojip.kim@lge.com>
2014-04-29 12:54:07 -0700

    msm: mdss: hdmi: Ensure the audio has completed its processing
    Before hdmi tx core is off, the audio should be completed its processing.
    
    Change-Id: Ida370027fdcbf88fcbe6bc4ae9c2a48a5476e096
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/video/msm/mdss/mdss_hdmi_tx.c
+++ drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -2381,8 +2381,13 @@ static void hdmi_tx_power_off_work(struct work_struct *work)
 		hdmi_hdcp_off(hdmi_ctrl->feature_data[HDMI_TX_FEAT_HDCP]);
 	}
 
-	if (!hdmi_tx_is_dvi_mode(hdmi_ctrl))
+	if (!hdmi_tx_is_dvi_mode(hdmi_ctrl)) {
+		if (hdmi_ctrl->panel_power_on && hdmi_ctrl->hpd_state) {
+			hdmi_tx_set_audio_switch_node(hdmi_ctrl, 0, false);
+			hdmi_tx_wait_for_audio_engine(hdmi_ctrl);
+		}
 		hdmi_tx_audio_off(hdmi_ctrl);
+	}
 
 	hdmi_tx_powerdown_phy(hdmi_ctrl);
 
