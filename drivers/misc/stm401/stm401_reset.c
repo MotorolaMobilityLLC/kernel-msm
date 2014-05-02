07c16d3bde4c7b7fcaa3ab2448983855bcee3eba
w12004 <w12004@motorola.com>
2014-05-02 14:38:49 -0500

    drivers: misc: stm401: add wake lock during reset_and_init
    Acquire wake lock to ensure reset_and_init of stm401 completes.
    
    If i2c operations continue to fail such that the stm401_hub_fail is set,
    give up on re-initializing.
    
    Signed-off-by: James Rumpler <w12004@motorola.com>
    Change-Id: I0c0c76f7b0ac4c2af889f427d50c5f8379eef359
    Reviewed-on: http://gerrit.mot.com/632776
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>
    Reviewed-by: Daniel Moore <dtmoore@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Iyengar Sriram Kaushik <sriramkaushik@motorola.com>

--- drivers/misc/stm401/stm401_reset.c
+++ drivers/misc/stm401/stm401_reset.c
@@ -91,6 +91,8 @@ int stm401_reset_and_init(void)
 	int mutex_locked = 0;
 	dev_dbg(&stm401_misc_data->client->dev, "stm401_reset_and_init\n");
 
+	wake_lock(&stm401_misc_data->reset_wakelock);
+
 	pdata = stm401_misc_data->pdata;
 
 	stm401_reset(pdata);
@@ -101,81 +103,117 @@ int stm401_reset_and_init(void)
 	stm401_cmdbuff[0] = ACCEL_UPDATE_RATE;
 	stm401_cmdbuff[1] = stm401_g_acc_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_i2c_retry_delay = 10;
 
 	stm401_cmdbuff[0] = MAG_UPDATE_RATE;
 	stm401_cmdbuff[1] = stm401_g_mag_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = GYRO_UPDATE_RATE;
 	stm401_cmdbuff[1] = stm401_g_gyro_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = PRESSURE_UPDATE_RATE;
 	stm401_cmdbuff[1] = stm401_g_baro_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = STEP_COUNTER_UPDATE_RATE;
 	stm401_cmdbuff[1] = (stm401_g_step_counter_delay>>8);
 	stm401_cmdbuff[2] = stm401_g_step_counter_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 3);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = IR_GESTURE_RATE;
 	stm401_cmdbuff[1] = stm401_g_ir_gesture_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = IR_RAW_RATE;
 	stm401_cmdbuff[1] = stm401_g_ir_raw_delay;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = NONWAKESENSOR_CONFIG;
 	stm401_cmdbuff[1] = stm401_g_nonwake_sensor_state & 0xFF;
 	stm401_cmdbuff[2] = (stm401_g_nonwake_sensor_state >> 8) & 0xFF;
 	stm401_cmdbuff[3] = stm401_g_nonwake_sensor_state >> 16;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 4);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = WAKESENSOR_CONFIG;
 	stm401_cmdbuff[1] = stm401_g_wake_sensor_state & 0xFF;
 	stm401_cmdbuff[2] = stm401_g_wake_sensor_state >> 8;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 3);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = ALGO_CONFIG;
 	stm401_cmdbuff[1] = stm401_g_algo_state & 0xFF;
 	stm401_cmdbuff[2] = stm401_g_algo_state >> 8;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 3);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = MOTION_DUR;
 	stm401_cmdbuff[1] = stm401_g_motion_dur;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = ZRMOTION_DUR;
 	stm401_cmdbuff[1] = stm401_g_zmotion_dur;
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff, 2);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	for (i = 0; i < STM401_NUM_ALGOS; i++) {
 		if (stm401_g_algo_requst[i].size > 0) {
@@ -186,6 +224,11 @@ int stm401_reset_and_init(void)
 			err = stm401_i2c_write_no_reset(stm401_misc_data,
 				stm401_cmdbuff,
 				stm401_g_algo_requst[i].size + 1);
+			if (err < 0) {
+				ret_err = err;
+				if (stm401_misc_data->stm401_hub_fail)
+					goto EXIT;
+			}
 		}
 	}
 	stm401_cmdbuff[0] = INTERRUPT_STATUS;
@@ -209,11 +252,16 @@ int stm401_reset_and_init(void)
 		dev_err(&stm401_misc_data->client->dev,
 			"unable to write proximity settings %d\n", err);
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
 	}
 
 	err = stm401_load_brightness_table(stm401_misc_data);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	getnstimeofday(&current_time);
 	current_time.tv_sec += stm401_time_delta;
@@ -228,15 +276,21 @@ int stm401_reset_and_init(void)
 		& 0xff);
 	err = stm401_i2c_write_no_reset(stm401_misc_data,
 					stm401_cmdbuff, 5);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	stm401_cmdbuff[0] = MAG_CAL;
 	memcpy(&stm401_cmdbuff[1], stm401_g_mag_cal, STM401_MAG_CAL_SIZE);
 	err = stm401_i2c_write_no_reset(stm401_misc_data, stm401_cmdbuff,
 		STM401_MAG_CAL_SIZE);
-	if (err < 0)
+	if (err < 0) {
 		ret_err = err;
+		if (stm401_misc_data->stm401_hub_fail)
+			goto EXIT;
+	}
 
 	if (stm401_g_ir_config_reg_restore) {
 		stm401_cmdbuff[0] = IR_CONFIG;
@@ -258,6 +312,8 @@ int stm401_reset_and_init(void)
 	if (mutex_locked)
 		mutex_unlock(&stm401_misc_data->lock);
 
+EXIT:
 	stm401_sleep(stm401_misc_data);
+	wake_unlock(&stm401_misc_data->reset_wakelock);
 	return ret_err;
 }
