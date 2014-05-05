051ecb8a9c7ddc49ae0e67f8529c6f43a35e1495
Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
2014-05-05 16:23:17 -0600

    msm: kgsl: Add a flag to mark user mapped GPU buffers
    Add a flag in /sys/kernel/debug/kgsl/kgsl-3do/proc/<pid>/mem
    file to indicate for each GPU buffer if it is mapped to userspace
    in <pid> or not.
    
    CRs-fixed: 634962
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_debugfs.c
    
    Change-Id: I8abda74ef5656aca6b1c0315af8deb77460fa5a9
    Signed-off-by: Harsh Vardhan Dwivedi <hdwivedi@codeaurora.org>
    Signed-off-by: Prabhat Awasthi <pawasthi@codeaurora.org>
    (cherry picked from commit 315f5e0c64f72bd6705e8e162737f6473823bf97)
    Reviewed-on: http://gerrit.mot.com/653189
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- drivers/gpu/msm/kgsl_debugfs.c
+++ drivers/gpu/msm/kgsl_debugfs.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2008-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2008-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -247,7 +247,7 @@ static char get_cacheflag(const struct kgsl_memdesc *m)
 
 static void print_mem_entry(struct seq_file *s, struct kgsl_mem_entry *entry)
 {
-	char flags[6];
+	char flags[7];
 	char usage[16];
 	struct kgsl_memdesc *m = &entry->memdesc;
 
@@ -256,11 +256,12 @@ static void print_mem_entry(struct seq_file *s, struct kgsl_mem_entry *entry)
 	flags[2] = get_alignflag(m);
 	flags[3] = get_cacheflag(m);
 	flags[4] = kgsl_memdesc_use_cpu_map(m) ? 'p' : '-';
-	flags[5] = '\0';
+	flags[5] = (m->useraddr) ? 'Y' : 'N';
+	flags[6] = '\0';
 
 	kgsl_get_memory_usage(usage, sizeof(usage), m->flags);
 
-	seq_printf(s, "%08x %08lx %8d %5d %5s %10s %16s %5d\n",
+	seq_printf(s, "%08x %08lx %8d %5d %6s %10s %16s %5d\n",
 			m->gpuaddr, m->useraddr, m->size, entry->id, flags,
 			memtype_str(entry->memtype), usage, m->sglen);
 }
@@ -272,7 +273,7 @@ static int process_mem_print(struct seq_file *s, void *unused)
 	struct kgsl_process_private *private = s->private;
 	int next = 0;
 
-	seq_printf(s, "%8s %8s %8s %5s %5s %10s %16s %5s\n",
+	seq_printf(s, "%8s %8s %8s %5s %6s %10s %16s %5s\n",
 		   "gpuaddr", "useraddr", "size", "id", "flags", "type",
 		   "usage", "sglen");
 
