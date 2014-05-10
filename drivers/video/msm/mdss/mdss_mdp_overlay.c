1a2cca6135950c5825d937984cfeea0a3bb93517
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-05-09 20:25:37 -0700

    msm: mdss: fix handling of runtime pm ref counts for fb device
    In the current implementation, pm runtime status for the framebuffer
    device changes to active when the overlay session is started. With idle
    power collapse feature enabled, it is possible that the MDSS core could
    be turned off prior to starting the overlay session depending on the
    duration of the idle timeout. This may result in an invalid hardware
    configuration leading to various target instabilities. Fix this by
    holding a reference to the pm runtime when mdp overlay is turned on.
    Similarly, when the userspace process is killed, wake up MDSS from idle
    power collapse before shutting down the fb device.
    
    Change-Id: Ie7748732fe76f9b2766da32b972e1e9fe7ff8751
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Tirtha Kanti Ghosh <tirtha@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1077,12 +1077,6 @@ static int mdss_mdp_overlay_start(struct msm_fb_data_type *mfd)
 
 	pr_debug("starting fb%d overlay\n", mfd->index);
 
-	rc = pm_runtime_get_sync(&mfd->pdev->dev);
-	if (IS_ERR_VALUE(rc)) {
-		pr_err("unable to resume with pm_runtime_get_sync rc=%d\n", rc);
-		return rc;
-	}
-
 	/*
 	 * We need to do hw init before any hw programming.
 	 * Also, hw init involves programming the VBIF registers which
@@ -3052,22 +3046,35 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		mdp5_data->ctl = ctl;
 	}
 
+	rc = pm_runtime_get_sync(&mfd->pdev->dev);
+	if (IS_ERR_VALUE(rc)) {
+		pr_err("unable to resume with pm_runtime_get_sync rc=%d\n", rc);
+		goto end;
+	}
+
 	if (!mfd->panel_info->cont_splash_enabled &&
 		(mfd->panel_info->type != DTV_PANEL)) {
 		rc = mdss_mdp_overlay_start(mfd);
-		if (!IS_ERR_VALUE(rc) &&
-			(mfd->panel_info->type != WRITEBACK_PANEL))
+		if (rc)
+			goto error_pm;
+		if (mfd->panel_info->type != WRITEBACK_PANEL)
 			rc = mdss_mdp_overlay_kickoff(mfd, NULL);
 	} else {
 		rc = mdss_mdp_ctl_setup(mdp5_data->ctl);
 		if (rc)
-			return rc;
+			goto error_pm;
 	}
 
 	if (IS_ERR_VALUE(rc)) {
 		pr_err("Failed to turn on fb%d\n", mfd->index);
 		mdss_mdp_overlay_off(mfd);
+		goto end;
 	}
+
+error_pm:
+	if (rc)
+		pm_runtime_put_sync(&mfd->pdev->dev);
+end:
 	return rc;
 }
 
@@ -3093,6 +3100,11 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 
 	if (!mdp5_data->ctl->power_on)
 		return 0;
+
+	if (mdp5_data->mdata->ulps) {
+		mdss_mdp_footswitch_ctrl_ulps(1, &mfd->pdev->dev);
+		mdss_mdp_ctl_restore(mdp5_data->ctl);
+	}
 
 	mdss_mdp_overlay_free_fb_pipe(mfd);
 
