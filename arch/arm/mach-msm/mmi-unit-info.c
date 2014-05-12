43ce145c7f95119edfe2f2c1961345c49954cede
Mohamad Abdi <pmdf37@motorola.com>
2014-05-12 14:40:38 -0500

    arm: msm: mss: rework modem restart flag usage in smem.
    Reworked (CR) as Titan BL is using SMEM_ID_VENDOR2 for Foxconn
    Hardware information purposes. This change moves the modem restart flag
    from SMEM_ID_VENDOR2 to SMEM_ID_VENDOR0 as part of the mmi-unit-info
    structure data passed to the modem.
    
    Signed-off-by: Mohamad Abdi <pmdf37@motorola.com>
    Change-Id: I0e58117649653c829b2e98acb7636e0a70becae8
    Reviewed-on: http://gerrit.mot.com/634739
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Divya <cvf384@motorola.com>
    Reviewed-by: Matthew Deheck <w15756@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- arch/arm/mach-msm/mmi-unit-info.c
+++ arch/arm/mach-msm/mmi-unit-info.c
@@ -74,6 +74,16 @@ int __init setup_androidboot_radio_init(char *s)
 __setup("androidboot.radio=", setup_androidboot_radio_init);
 
 static char msm_hw[MSMHW_MAX_LEN+1];
+static struct mmi_unit_info *mui;
+
+void mmi_set_pureason(uint32_t val)
+{
+	if (mui) {
+		mui->pureason = val;
+		pr_debug("%s: Set modem PU reason value in SMEM to %d\n",
+				__func__, mui->pureason);
+	}
+}
 
 void mach_cpuinfo_show(struct seq_file *m, void *v)
 {
@@ -120,8 +130,6 @@ static void __init mmi_of_populate_setup(void)
 
 static int __init mmi_unit_info_init(void)
 {
-	struct mmi_unit_info *mui;
-
 	mmi_of_populate_setup();
 
 	#define SMEM_KERNEL_RESERVE_SIZE 1024
@@ -144,6 +152,9 @@ static int __init mmi_unit_info_init(void)
 	strlcpy(mui->carrier, carrier, CARRIER_MAX_LEN);
 	strlcpy(mui->device, androidboot_device, DEVICE_MAX_LEN);
 	mui->radio = androidboot_radio;
+	mui->pureason = bi_powerup_reason();
+	pr_debug("%s: Set modem PU reason value in SMEM to %d\n",
+			__func__, mui->pureason);
 
 	if (mui->version != MMI_UNIT_INFO_VER) {
 		pr_err("%s: unexpected unit_info version %d in SMEM\n",
