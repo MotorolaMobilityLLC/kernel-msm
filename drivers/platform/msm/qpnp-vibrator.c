c4e954636381c0bab618ae6db9d420d1439c0894
allench_lin <allench_lin@asus.com>
2014-05-14 17:18:41 +0800

    [WI500Q][Vibrator][NA][ATD] Vibrator vibrates ceaselessly
    Change-Id: I7871558afc5b4131993e4994f75e538afe59fe55
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/126003
    Reviewed-by: allench_lin <allench_lin@asus.com>
    Tested-by: allench_lin <allench_lin@asus.com>

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -185,14 +185,17 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 
 	if (value == 0)
 		vib->state = 0;
-	else {
-		value = (value > vib->timeout ?
-				 vib->timeout : value);
+	else if (value == -1)
 		vib->state = 1;
-		hrtimer_start(&vib->vib_timer,
-			      ktime_set(value / 1000, (value % 1000) * 1000000),
-			      HRTIMER_MODE_REL);
-	}
+	
+//	else {
+//		value = (value > vib->timeout ?
+//				 vib->timeout : value);
+//		vib->state = 1;
+//		hrtimer_start(&vib->vib_timer,
+//			      ktime_set(value / 1000, (value % 1000) * 1000000),
+//			      HRTIMER_MODE_REL);
+//	}
 	mutex_unlock(&vib->lock);
 	schedule_work(&vib->work);
 }
