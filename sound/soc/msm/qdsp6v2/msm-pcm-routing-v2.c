f440d5353b81d1448ed81b95d3d4badc5254182f
Ravi Kumar Alamanda <ralama@codeaurora.org>
2014-05-15 00:04:00 -0700

    ASoC: msm: qdsp6v2: extra low latency mode change for audio capture
    For low latency audio capture sessions adm should be opened
    in low latency mode. This change checks for low latency
    mode and passes the info to ADM.
    
    Signed-off-by: Ravi Kumar Alamanda <ralama@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -401,7 +401,8 @@ void msm_pcm_routing_reg_phy_stream(int fedai_id, bool perf_mode,
 				path_type,
 				msm_bedais[i].sample_rate,
 				msm_bedais[i].channel,
-				topology, false,
+				topology,
+				test_bit(fedai_id, &msm_bedais[i].perf_mode),
 				bits_per_sample);
 
 			payload.copp_ids[payload.num_copps++] =
@@ -523,10 +524,9 @@ static void msm_pcm_routing_process_audio(u16 reg, u16 val, int set)
 			    msm_bedais[reg].port_id == VOICE_RECORD_TX)
 				topology = DEFAULT_COPP_TOPOLOGY;
 
+			perf_mode = test_bit(val, &msm_bedais[reg].perf_mode);
 			if ((session_type == SESSION_TYPE_RX) &&
 				(channels > 0)) {
-				perf_mode = test_bit(val,
-						&msm_bedais[reg].perf_mode);
 				adm_multi_ch_copp_open(msm_bedais[reg].port_id,
 				path_type,
 				msm_bedais[reg].sample_rate,
@@ -538,7 +538,7 @@ static void msm_pcm_routing_process_audio(u16 reg, u16 val, int set)
 				adm_open(msm_bedais[reg].port_id,
 				path_type,
 				msm_bedais[reg].sample_rate, channels,
-				topology, false, bits_per_sample);
+				topology, perf_mode, bits_per_sample);
 
 			msm_pcm_routing_build_matrix(val,
 				fe_dai_map[val][session_type], path_type,
@@ -3639,8 +3639,8 @@ static int msm_pcm_routing_prepare(struct snd_pcm_substream *substream)
 			    bedai->port_id == VOICE_RECORD_TX)
 				topology = DEFAULT_COPP_TOPOLOGY;
 
+			perf_mode = test_bit(i, &(bedai->perf_mode));
 			if ((playback) && (channels > 0)) {
-				perf_mode = test_bit(i, &(bedai->perf_mode));
 				adm_multi_ch_copp_open(bedai->port_id,
 					path_type,
 					bedai->sample_rate,
@@ -3653,7 +3653,8 @@ static int msm_pcm_routing_prepare(struct snd_pcm_substream *substream)
 				path_type,
 				bedai->sample_rate,
 				channels,
-				topology, false,
+				topology,
+				perf_mode,
 				bits_per_sample);
 			}
 
