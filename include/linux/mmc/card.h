052a0cd0a11a24d45989fb5f971bbec0902ca764
Russ W. Knize <Russ.Knize@motorola.com>
2014-05-21 16:57:25 -0500

    mmc: don't blindly retry on timed-out flushes
    The commit "mmc: block: flush request requeue after timeout"
    introduced logic that blindly requeues flush requests that have
    timed-out without checking the card state or taking advantage of
    any of the error recovery logic in the driver.  This leads to an
    infinite loop if the card continues to time out because of how
    flush requests are prioritized over other requests.
    
    This commit disables the retry except in the case of Hynix cards.
    
    Change-Id: Ie545de673cc6588fa193dc31f43f150db7490c90
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/638228
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>
    Reviewed-by: Sergio Silva Vilela <sergiov@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 3f3abb77859f8e817a3faa716b816286ea0018ae)

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -356,6 +356,7 @@ struct mmc_card {
 #define MMC_QUIRK_BROKEN_DATA_TIMEOUT	(1<<12)
 
 #define MMC_QUIRK_CACHE_DISABLE (1 << 14)       /* prevent cache enable */
+#define MMC_QUIRK_RETRY_FLUSH_TIMEOUT (1 << 31) /* requeue flush command timeouts */
 
 	unsigned int		erase_size;	/* erase size in sectors */
  	unsigned int		erase_shift;	/* if erase unit is power 2 */
