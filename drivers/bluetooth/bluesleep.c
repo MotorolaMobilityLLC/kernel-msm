68ef7187c67a650389b796429513063fedbf2453
sc47.yun <sc47.yun@samsung.com>
2014-05-20 17:24:31 -0700

    bluetooth: sprat: Changed TX_TIMER_INTERVAL_time(uart timer)
    cause: There are much battery power consumptions.
    
    solution : In order to reduce power consumptions, uart timer is changed to 1 sec.
    
    Bug: 15092257
    
    Signed-off-by: sc47.yun <sc47.yun@samsung.com>
    
    Change-Id : I5fc1e851da66da04d16ea6a8f0d5457ccdb0621b

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -101,7 +101,7 @@ DECLARE_DELAYED_WORK(uart_awake_wq, bluesleep_uart_awake_work);
 #define bluesleep_uart_work() schedule_delayed_work(&uart_awake_wq, 0)
 
 /* 0.5 second timeout */
-#define TX_TIMER_INTERVAL	3
+#define TX_TIMER_INTERVAL	1
 
 /* state variable names and bit positions */
 #define BT_PROTO	 0x01
