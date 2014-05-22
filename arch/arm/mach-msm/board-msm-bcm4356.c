200052d015c686331b769041afa06c3ead00fd98
Chris Fries <cfries@motorola.com>
2014-05-22 10:07:50 -0500

    arm: msm: broadcom wlan board file
    Initial revision of broadcom WLAN board file for BCM4356.
    
    Signed-off-by: Chris Fries <cfries@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

---
+++ arch/arm/mach-msm/board-msm-bcm4356.c
@@ -0,0 +1,370 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <linux/skbuff.h>
+#include <linux/wlan_plat.h>
+#include <linux/mmc/host.h>
+#include <linux/if.h>
+
+#include <mach/gpio.h>
+#include <mach/board.h>
+#include <mach/msm_pcie.h>
+#include <linux/of_gpio.h>
+
+#include <linux/fcntl.h>
+#include <linux/fs.h>
+#include "pcie.h"
+
+#define BCM_DBG pr_debug
+
+#define WLAN_STATIC_SCAN_BUF0		5
+#define WLAN_STATIC_SCAN_BUF1		6
+#define WLAN_STATIC_DHD_INFO_BUF	7
+#define WLAN_SCAN_BUF_SIZE		(64 * 1024)
+#define PREALLOC_WLAN_SEC_NUM		4
+#define PREALLOC_WLAN_BUF_NUM		160
+#define PREALLOC_WLAN_SECTION_HEADER	24
+
+#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_BUF_NUM * 512)
+#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_BUF_NUM * 1024)
+
+#define DHD_SKB_HDRSIZE			336
+#define DHD_SKB_1PAGE_BUFSIZE	((PAGE_SIZE*1)-DHD_SKB_HDRSIZE)
+#define DHD_SKB_2PAGE_BUFSIZE	((PAGE_SIZE*2)-DHD_SKB_HDRSIZE)
+#define DHD_SKB_4PAGE_BUFSIZE	((PAGE_SIZE*4)-DHD_SKB_HDRSIZE)
+
+#define WLAN_SKB_BUF_NUM	17
+
+static int gpio_wl_reg_on = 82;
+
+static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
+
+struct wlan_mem_prealloc {
+	void *mem_ptr;
+	unsigned long size;
+};
+
+static struct wlan_mem_prealloc wlan_mem_array[PREALLOC_WLAN_SEC_NUM] = {
+	{NULL, (WLAN_SECTION_SIZE_0 + PREALLOC_WLAN_SECTION_HEADER)},
+	{NULL, (WLAN_SECTION_SIZE_1 + PREALLOC_WLAN_SECTION_HEADER)},
+	{NULL, (WLAN_SECTION_SIZE_2 + PREALLOC_WLAN_SECTION_HEADER)},
+	{NULL, (WLAN_SECTION_SIZE_3 + PREALLOC_WLAN_SECTION_HEADER)}
+};
+
+void *wlan_static_scan_buf0;
+void *wlan_static_scan_buf1;
+void *wlan_static_dhd_info_buf;
+
+static void *brcm_wlan_mem_prealloc(int section, unsigned long size)
+{
+	if (section == PREALLOC_WLAN_SEC_NUM)
+		return wlan_static_skb;
+
+	if (section == WLAN_STATIC_SCAN_BUF0)
+		return wlan_static_scan_buf0;
+
+	if (section == WLAN_STATIC_SCAN_BUF1)
+		return wlan_static_scan_buf1;
+
+	if ((section < 0) || (section > PREALLOC_WLAN_SEC_NUM))
+		return NULL;
+
+	if (wlan_mem_array[section].size < size)
+		return NULL;
+
+	return wlan_mem_array[section].mem_ptr;
+}
+
+static int brcm_init_wlan_mem(void)
+{
+	int i;
+	int j;
+	for (i = 0; i < WLAN_SKB_BUF_NUM; i++)
+		wlan_static_skb[i] = NULL;
+
+	for (i = 0; i < 8; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_1PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
+	}
+
+	for (; i < 16; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_2PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
+	}
+
+	wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_4PAGE_BUFSIZE);
+	if (!wlan_static_skb[i])
+		goto err_skb_alloc;
+
+	for (i = 0; i < PREALLOC_WLAN_SEC_NUM; i++) {
+		wlan_mem_array[i].mem_ptr =
+				kmalloc(wlan_mem_array[i].size, GFP_KERNEL);
+
+		if (!wlan_mem_array[i].mem_ptr)
+			goto err_mem_alloc;
+	}
+
+	wlan_static_scan_buf0 = kmalloc(WLAN_SCAN_BUF_SIZE, GFP_KERNEL);
+	if (!wlan_static_scan_buf0)
+		goto err_mem_alloc;
+
+	wlan_static_scan_buf1 = kmalloc(WLAN_SCAN_BUF_SIZE, GFP_KERNEL);
+	if (!wlan_static_scan_buf1)
+		goto err_mem_alloc;
+
+
+	BCM_DBG("%s: WIFI MEM Allocated\n", __func__);
+	return 0;
+
+ err_mem_alloc:
+	pr_err("Failed to mem_alloc for WLAN\n");
+	for (j = 0; j < i; j++)
+		kfree(wlan_mem_array[j].mem_ptr);
+
+	i = WLAN_SKB_BUF_NUM;
+
+ err_skb_alloc:
+	pr_err("Failed to skb_alloc for WLAN\n");
+	for (j = 0; j < i; j++)
+		dev_kfree_skb(wlan_static_skb[j]);
+
+	return -ENOMEM;
+}
+
+
+int __init brcm_wifi_init_gpio(struct device_node *np)
+{
+	if (np) {
+		int wl_reg_on;
+		wl_reg_on = of_get_named_gpio(np, "wl_reg_on", 0);
+		if (wl_reg_on >= 0) {
+			gpio_wl_reg_on = wl_reg_on;
+			BCM_DBG("%s: wl_reg_on GPIO %d\n", __func__, wl_reg_on);
+		}
+	}
+
+	if (gpio_request(gpio_wl_reg_on, "WL_REG_ON"))
+		pr_err("%s: Faiiled to request gpio %d for WL_REG_ON\n",
+			__func__, gpio_wl_reg_on);
+	else
+		pr_err("%s: gpio_request WL_REG_ON done\n", __func__);
+
+	if (gpio_direction_output(gpio_wl_reg_on, 1))
+		pr_err("%s: WL_REG_ON failed to pull up\n", __func__);
+	else
+		BCM_DBG("%s: WL_REG_ON is pulled up\n", __func__);
+
+	if (gpio_get_value(gpio_wl_reg_on))
+		BCM_DBG("%s: Initial WL_REG_ON: [%d]\n",
+			__func__, gpio_get_value(gpio_wl_reg_on));
+
+	return 0;
+}
+
+int brcm_wlan_power(int on)
+{
+	BCM_DBG("------------------------------------------------");
+	BCM_DBG("------------------------------------------------\n");
+	pr_info("%s Enter: power %s\n", __func__, on ? "on" : "off");
+
+	if (on) {
+		if (gpio_direction_output(gpio_wl_reg_on, 1)) {
+			pr_err("%s: WL_REG_ON didn't output high\n", __func__);
+			return -EIO;
+		}
+		if (!gpio_get_value(gpio_wl_reg_on))
+			pr_err("[%s] gpio didn't set high.\n", __func__);
+	} else {
+		if (gpio_direction_output(gpio_wl_reg_on, 0)) {
+			pr_err("%s: WL_REG_ON didn't output low\n", __func__);
+			return -EIO;
+		}
+	}
+	return 0;
+}
+EXPORT_SYMBOL(brcm_wlan_power);
+
+static int brcm_wlan_reset(int onoff)
+{
+	return 0;
+}
+
+static int brcm_wlan_set_carddetect(int val)
+{
+	return msm_pcie_status_notify(val);
+}
+
+/* Customized Locale table : OPTIONAL feature */
+#define WLC_CNTRY_BUF_SZ        4
+struct cntry_locales_custom {
+	char iso_abbrev[WLC_CNTRY_BUF_SZ];
+	char custom_locale[WLC_CNTRY_BUF_SZ];
+	int  custom_locale_rev;
+};
+
+static struct cntry_locales_custom brcm_wlan_translate_custom_table[] = {
+	/* Table should be filled out based on custom platform regulatory
+	 * requirements.
+	 */
+	{"",   "XY", 4},  /* universal */
+	{"US", "US", 69}, /* input ISO "US" to : US regrev 69 */
+	{"CA", "US", 69}, /* input ISO "CA" to : US regrev 69 */
+	{"EU", "EU", 5},  /* European union countries */
+	{"AT", "EU", 5},
+	{"BE", "EU", 5},
+	{"BG", "EU", 5},
+	{"CY", "EU", 5},
+	{"CZ", "EU", 5},
+	{"DK", "EU", 5},
+	{"EE", "EU", 5},
+	{"FI", "EU", 5},
+	{"FR", "EU", 5},
+	{"DE", "EU", 5},
+	{"GR", "EU", 5},
+	{"HU", "EU", 5},
+	{"IE", "EU", 5},
+	{"IT", "EU", 5},
+	{"LV", "EU", 5},
+	{"LI", "EU", 5},
+	{"LT", "EU", 5},
+	{"LU", "EU", 5},
+	{"MT", "EU", 5},
+	{"NL", "EU", 5},
+	{"PL", "EU", 5},
+	{"PT", "EU", 5},
+	{"RO", "EU", 5},
+	{"SK", "EU", 5},
+	{"SI", "EU", 5},
+	{"ES", "EU", 5},
+	{"SE", "EU", 5},
+	{"GB", "EU", 5},  /* input ISO "GB" to : EU regrev 05 */
+	{"IL", "IL", 0},
+	{"CH", "CH", 0},
+	{"TR", "TR", 0},
+	{"NO", "NO", 0},
+	{"KR", "XY", 3},
+	{"AU", "XY", 3},
+	{"CN", "XY", 3},  /* input ISO "CN" to : XY regrev 03 */
+	{"TW", "XY", 3},
+	{"AR", "XY", 3},
+	{"MX", "XY", 3}
+};
+
+static void *brcm_wlan_get_country_code(char *ccode)
+{
+	int size = ARRAY_SIZE(brcm_wlan_translate_custom_table);
+	int i;
+
+	if (!ccode)
+		return NULL;
+
+	for (i = 0; i < size; i++)
+		if (strcmp(ccode,
+		brcm_wlan_translate_custom_table[i].iso_abbrev) == 0)
+			return &brcm_wlan_translate_custom_table[i];
+	return &brcm_wlan_translate_custom_table[0];
+}
+static unsigned char brcm_mac_addr[IFHWADDRLEN] = { 0, 0x90, 0x4c, 0, 0, 0 };
+
+static int __init brcm_mac_addr_setup(char *str)
+{
+	char macstr[IFHWADDRLEN*3];
+	char *macptr = macstr;
+	char *token;
+	int i = 0;
+
+	if (!str)
+		return 0;
+	BCM_DBG("wlan MAC = %s\n", str);
+	if (strlen(str) >= sizeof(macstr))
+		return 0;
+	strlcpy(macstr, str, sizeof(macstr));
+
+	while ((token = strsep(&macptr, ":")) != NULL) {
+		unsigned long val;
+		int res;
+
+		if (i >= IFHWADDRLEN)
+			break;
+		res = kstrtoul(token, 0x10, &val);
+		if (res < 0)
+			return 0;
+		brcm_mac_addr[i++] = (u8)val;
+	}
+	return 1;
+}
+
+__setup("androidboot.wifimacaddr=", brcm_mac_addr_setup);
+
+static int brcm_wifi_get_mac_addr(unsigned char *buf)
+{
+	uint rand_mac;
+
+	if (!buf)
+		return -EFAULT;
+
+	if ((brcm_mac_addr[4] == 0) && (brcm_mac_addr[5] == 0)) {
+		prandom_seed((uint)jiffies);
+		rand_mac = prandom_u32();
+		brcm_mac_addr[3] = (unsigned char)rand_mac;
+		brcm_mac_addr[4] = (unsigned char)(rand_mac >> 8);
+		brcm_mac_addr[5] = (unsigned char)(rand_mac >> 16);
+	}
+	memcpy(buf, brcm_mac_addr, IFHWADDRLEN);
+	return 0;
+}
+
+
+static struct resource brcm_wlan_resources[] = {
+	[0] = {
+		.name	= "bcmdhd_wlan_irq",
+		.start	= 0, /* Dummy */
+		.end	= 0, /* Dummy */
+		.flags	= IORESOURCE_IRQ | IORESOURCE_IRQ_SHAREABLE
+			| IORESOURCE_IRQ_HIGHLEVEL, /* Dummy */
+	},
+};
+
+static struct wifi_platform_data brcm_wlan_control = {
+	.set_power	= brcm_wlan_power,
+	.set_reset	= brcm_wlan_reset,
+	.set_carddetect	= brcm_wlan_set_carddetect,
+	.get_mac_addr = brcm_wifi_get_mac_addr,
+	.mem_prealloc	= brcm_wlan_mem_prealloc,
+	.get_country_code = brcm_wlan_get_country_code,
+};
+
+static struct platform_device brcm_device_wlan = {
+	.name		= "bcmdhd_wlan",
+	.id		= 1,
+	.num_resources	= ARRAY_SIZE(brcm_wlan_resources),
+	.resource	= brcm_wlan_resources,
+	.dev		= {
+		.platform_data = &brcm_wlan_control,
+	},
+};
+
+int __init brcm_wlan_init(void)
+{
+	int rc;
+	struct device_node *np;
+
+	BCM_DBG("%s: START\n", __func__);
+
+	brcm_init_wlan_mem();
+	rc = platform_device_register(&brcm_device_wlan);
+
+	np = of_find_compatible_node(NULL, NULL, "bcm,bcm4356");
+	brcm_device_wlan.dev.of_node = np;
+
+	brcm_wifi_init_gpio(np);
+	return rc;
+}
+subsys_initcall(brcm_wlan_init);
