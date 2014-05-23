3669b24062d19c78d45161fedc46ef013330fe4c
Kuogee Hsieh <khsieh@codeaurora.org>
2014-05-23 12:40:39 -0700

    mdss: display: extend mutex coverage inside cmdlist_put
    At Current implementation mutex is released after dcs
    commands had been added to queue and before cmdlist_commit
    called to tx commands. This is created a possibility to allow
    other thread do take over command queue and call cmdlist_commit
    to tx commands which not added by it. Extend mutex coverage to
    including both cmdlsit_put and cmdlist_commit to fix the problem.
    
    CRs-Fixed: 669136
    Change-Id: I317adae2b39505d96afe1edd27f1a1ab813b343d
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    (cherry picked from commit 3ae393a2ff16b1af62630f1f9fcea0a7c4ccc6cb)

--- drivers/video/msm/mdss/mdss_dsi_cmd.c
+++ drivers/video/msm/mdss/mdss_dsi_cmd.c
@@ -672,7 +672,6 @@ int mdss_dsi_cmdlist_put(struct mdss_dsi_ctrl_pdata *ctrl,
 		clist->get %= CMD_REQ_MAX;
 		clist->tot--;
 	}
-	mutex_unlock(&ctrl->cmd_mutex);
 
 	pr_debug("%s: tot=%d put=%d get=%d\n", __func__,
 		clist->tot, clist->put, clist->get);
@@ -683,6 +682,8 @@ int mdss_dsi_cmdlist_put(struct mdss_dsi_ctrl_pdata *ctrl,
 		else
 			ret = ctrl->cmdlist_commit(ctrl, 0);
 	}
+	mutex_unlock(&ctrl->cmd_mutex);
+
 	return ret;
 }
 
