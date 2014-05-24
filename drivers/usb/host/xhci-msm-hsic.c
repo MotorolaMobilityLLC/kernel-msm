7a969d8a7bbf4f209bdd72421eba3034ab6539ff
Hemant Kumar <hemantk@codeaurora.org>
2014-05-24 01:03:37 -0700

    xhci: hsic: Enable xhci irq after xhci memory initialization
    Spurious port change interrupt is causing access to xhci
    data structures from xhci_irq() which causes memory corruption.
    Avoid this by enabling xhci irq right before enabling HSIC phy
    drives idle state on to the bus.
    
    CRs-Fixed: 669583
    Change-Id: I35bf904859fda7ad41df8e40b86338395adf2099
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/host/xhci-msm-hsic.c
+++ drivers/usb/host/xhci-msm-hsic.c
@@ -1285,6 +1285,7 @@ static int mxhci_hsic_probe(struct platform_device *pdev)
 		}
 	}
 
+	irq_set_status_flags(irq, IRQ_NOAUTOEN);
 	ret = usb_add_hcd(hcd, irq, IRQF_SHARED);
 	if (ret)
 		goto deinit_vddcx;
@@ -1369,6 +1370,7 @@ static int mxhci_hsic_probe(struct platform_device *pdev)
 		dev_dbg(&pdev->dev, "%s: unable to create imod sysfs entry\n",
 					__func__);
 
+	enable_irq(irq);
 	/* Enable HSIC PHY */
 	mxhci_hsic_ulpi_write(mxhci, 0x01, MSM_HSIC_CFG_SET);
 
