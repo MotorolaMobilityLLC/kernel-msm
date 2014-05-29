704fd2d0b9a77d55ebd0490dc5428d563f8d64e6
jh32.park <jh32.park@samsung.com>
2014-05-29 11:58:39 +0900

    firmware: sprat: tsp firmware update(V24)
    - Rev03: TMA545(QFN): firmware update V24
      : Ghost touch tuning
      : Palm detection tuning
    
    Bug: 14570795
    Bug: 15092875
    Bug: 15089271
    
    Change-Id: I078ade9b35ccc4e3eb07e67839ebc16e6adf487a
    Signed-off-by: jh32.park <jh32.park@samsung.com>

--- drivers/input/touchscreen/cyttsp5/cyttsp5_loader.c
+++ drivers/input/touchscreen/cyttsp5/cyttsp5_loader.c
@@ -49,7 +49,7 @@ static const u8 cyttsp5_security_key[] = {
 #define CY_HW_VERSION 0x01
 #define CY_CSP_FW_VERSION 0x0600
 #define CY_QFN_FW_VERSION 0x1400
-#define CY_FW_VERSION 0x2100
+#define CY_FW_VERSION 0x2400
 
 #ifdef CYTTSP5_PLATFORM_FW_UPGRADE
 #include "cyttsp5_firmware.h"
