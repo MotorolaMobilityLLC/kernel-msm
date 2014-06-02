7e8d71bb21468fa10ac1aeaf67bfd6a084937405
Russ W. Knize <Russ.Knize@motorola.com>
2014-06-02 17:24:58 -0500

    mmc: support driver strength
    Add driver strength support and Allow tuning
    of the SD card driver strength via a debugfs.
    
    Change-Id: Ifa056f6e8a35535936eac19d1998bd3adce48728
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Reviewed-on: http://gerrit.mot.com/818934
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -151,6 +151,7 @@ struct sdhci_msm_pltfm_data {
 	unsigned char sup_ice_clk_cnt;
 	u32 ice_clk_max;
 	u32 ice_clk_min;
+	u32 drv_types;
 	struct sdhci_msm_pm_qos_data pm_qos_data;
 	bool sdr104_wa;
 };
@@ -216,6 +217,10 @@ struct sdhci_msm_host {
 	bool tuning_in_progress;
 	bool core_3_0v_support;
 	bool pltfm_init_done;
+#if defined(CONFIG_MMC_SDHCI_MSM_DEBUG)
+	struct dentry *debugfs_host_dir;
+	struct dentry *debugfs_drv_types;
+#endif
 };
 
 extern char *saved_command_line;
