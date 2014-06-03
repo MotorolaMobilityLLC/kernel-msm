0d0383722da89bce76004e1ffbe18b8c95de1a7c
Liam Mark <lmark@codeaurora.org>
2014-06-03 13:33:18 -0700

    mm, oom: make dump_tasks public
    Allow other functions to dump the list of tasks.
    Useful for when debugging memory leaks.
    
    Bug: 17871993
    Change-Id: I76c33a118a9765b4c2276e8c76de36399c78dbf6
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -389,7 +389,7 @@ static struct task_struct *select_bad_process(unsigned int *ppoints,
  *
  * Call with tasklist_lock read-locked.
  */
-static void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
+void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
 {
 	struct task_struct *p;
 	struct task_struct *task;
