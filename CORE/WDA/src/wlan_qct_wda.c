2ab1d49229f254b22947ede3678add61b586f233
Mihir Shete <smihir@qti.qualcomm.com>
2014-06-04 14:21:05 +0530

    wlan: flush cached frames on deleting the station context
    TL caches the frames for all STAs which are not yet registered.
    It has been seen that the firmware allows frames for a STA to
    reach the Host even when 802.11 authentication is not completed
    in some cases. If the authentication times out, all the frames
    which are cached remain in the TL cache consuming lot of resources.
    Whenever authentication fails the host driver will delete the STA
    context from the firmware, and after getting a successful response
    from the firmware we will try to flush all cached frames and free
    up the resources.
    
    Change-Id: I1b4678c33cc99a6138a77d0574d01c4c7e51b4b3
    CRs-Fixed: 673062

--- CORE/WDA/src/wlan_qct_wda.c
+++ CORE/WDA/src/wlan_qct_wda.c
@@ -3770,6 +3770,9 @@ void WDA_DelBSSReqCallback(WDI_DelBSSRspParamsType *wdiDelBssRsp,
                  "%s: Clear STA index form table Fail", __func__);
      VOS_ASSERT(0) ;
    }
+
+   WLANTL_StartForwarding(staIdx,0,0);
+
    vos_mem_free(pWdaParams->wdaWdiApiMsgParam);
    vos_mem_free(pWdaParams) ;
    /* reset the the system role*/
@@ -3875,6 +3878,7 @@ void WDA_DelSTAReqCallback(WDI_DelSTARspParamsType *wdiDelStaRsp,
          VOS_ASSERT(0) ;
       }
       delStaReqParam->staIdx = wdiDelStaRsp->ucSTAIdx ;
+      WLANTL_StartForwarding(delStaReqParam->staIdx,0,0);
    }
    vos_mem_free(pWdaParams->wdaWdiApiMsgParam);
    vos_mem_free(pWdaParams) ;
