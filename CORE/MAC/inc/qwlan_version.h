fcc7c50a67f64f230895561bfe25edda3507b1f7
Satyanarayana Dash <c_sadash@qti.qualcomm.com>
2014-06-06 14:40:39 +0530

    Wlan: Revision 3.2.3.345
    Revision 3.2.3.345
    
    Change-Id: Ic5279a7ae69c21b98e7d654e52871cd14152b353

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            344
+#define QWLAN_VERSION_BUILD            345
 
-#define QWLAN_VERSIONSTR               "3.2.3.344"
+#define QWLAN_VERSIONSTR               "3.2.3.345"
 
 #endif /* QWLAN_VERSION_H */
