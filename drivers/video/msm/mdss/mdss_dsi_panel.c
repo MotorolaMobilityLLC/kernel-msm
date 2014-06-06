af5ce018b309767ef590179fbded9c94df7aa7d7
Shengzhe Zhao <a18689@motorola.com>
2014-06-06 14:29:31 -0700

    IKKRNBSP-2622: msm: mdss: provide display panel info in sysfs
    Provide display panel information including panel name, panel version
    manufacture id, controller version, controller drv version under
    /sys/class/graphics/fb0
    
    Change-Id: Ibe134058d95ab067552a65ba8fa2925b7f1b0887
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>
    Reviewed-on: http://gerrit.mot.com/672438
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -23,6 +23,7 @@
 #include <linux/err.h>
 
 #include "mdss_dsi.h"
+#include "mdss_fb.h"
 
 #define MDSS_PANEL_DEFAULT_VER 0xffffffffffffffff
 #define MDSS_PANEL_UNKNOWN_NAME "unknown"
@@ -456,6 +457,9 @@ int mdss_panel_parse_panel_config_dt(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
 		panel_ver & 0xff, (panel_ver & 0xff00) >> 8,
 		(panel_ver & 0xff0000) >> 16,
 		ctrl_pdata->panel_config.panel_ver);
+
+	panelinfo.panel_name = (char *) &ctrl_pdata->panel_config.panel_name;
+	panelinfo.panel_ver = &ctrl_pdata->panel_config.panel_ver;
 
 	of_node_put(np);
 
