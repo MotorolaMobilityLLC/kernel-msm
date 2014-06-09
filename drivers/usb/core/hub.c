d7a1605dabfd1120c3e36fcd7e6cd92984ee719f
Jane Zhou <a17711@motorola.com>
2014-06-09 12:15:31 -0700

    usb/hub: checking validation of hub pointer before usage
    Checking validation of hub pointer before usage to avoid null pointer issue
    during the process of disconnect usb connection.
    
    Change-Id: I866969971014f6cf7a7dc9e394794dff1fa576ee
    Signed-off-by: Jane Zhou <a17711@motorola.com>
    Reviewed-on: http://gerrit.mot.com/643112
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1884,13 +1884,18 @@ static void recursively_mark_NOTATTACHED(struct usb_device *udev)
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev);
 	int i;
 
-	for (i = 0; i < udev->maxchild; ++i) {
-		if (hub->ports[i]->child)
-			recursively_mark_NOTATTACHED(hub->ports[i]->child);
+	if (hub != NULL) {
+		for (i = 0; i < udev->maxchild; ++i) {
+			if (hub->ports[i]->child)
+				recursively_mark_NOTATTACHED(
+					hub->ports[i]->child);
+		}
 	}
+
 	if (udev->state == USB_STATE_SUSPENDED)
 		udev->active_duration -= jiffies;
 	udev->state = USB_STATE_NOTATTACHED;
+
 }
 
 /**
