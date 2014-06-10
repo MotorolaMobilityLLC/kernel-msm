4f94ba1904083f3db9c31db84fa14f6acb92fb90
Alok Kediya <kediya@codeaurora.org>
2014-06-10 13:25:12 +0530

    msm-camera: move target checks to userspace
    move all the target specific checks to userspace.
    
    Change-Id: I67f3aec92a0cc0bba1a239958c200c54834a349a
    Signed-off-by: Alok Kediya <kediya@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/717485
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sumit Jamadar <xkbr38@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    SLTApproved: Robert Neely <wrn002@motorola.com>

--- include/media/msmb_isp.h
+++ include/media/msmb_isp.h
@@ -143,6 +143,7 @@ struct msm_vfe_axi_stream_request_cmd {
 	uint8_t buf_divert; /* if TRUE no vb2 buf done. */
 	/*Return values*/
 	uint32_t axi_stream_handle;
+	uint32_t burst_len;
 };
 
 struct msm_vfe_axi_stream_release_cmd {
@@ -214,6 +215,7 @@ struct msm_vfe_stats_stream_cfg_cmd {
 	uint8_t num_streams;
 	uint32_t stream_handle[MSM_ISP_STATS_MAX];
 	uint8_t enable;
+	uint32_t stats_burst_len;
 };
 
 enum msm_vfe_reg_cfg_type {
@@ -229,6 +231,7 @@ enum msm_vfe_reg_cfg_type {
 	VFE_READ_DMI_64BIT,
 	GET_SOC_HW_VER,
 	GET_MAX_CLK_RATE,
+	SET_WM_UB_SIZE,
 };
 
 struct msm_vfe_cfg_cmd2 {
