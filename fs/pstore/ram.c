aacdda2d85525e2bd5e089e92649a519c18fc48c
Yuanyuan Zhong <zyy@motorola.com>
2014-06-10 14:23:07 -0500

    pstore/ram: allow annotation appending
    Existing pstore ram annotation stores messages and exports them
    after reset. Add function which can be used to append messages
    to the annotation after reset and before they are exported to
    annotation-ramoops file.
    
    Change-Id: I0823c7c7345f5350c5774c0f9c1743c5ae37126b
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.mot.com/644635
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit be4d6cd34e3890e7e5c2db2fd46c5bc9c0efac88)
    Reviewed-on: http://gerrit.mot.com/664832
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/782687
    SME-Granted: SME Approvals Granted
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -224,18 +224,23 @@ static ssize_t ramoops_pstore_read(u64 *id, enum pstore_type_id *type,
 		}
 	}
 
-	if (!prz_ok(prz))
+	if (!prz_ok(prz)) {
 		prz = ramoops_get_next_prz(&cxt->cprz, &cxt->console_read_cnt,
 					   1, id, type, PSTORE_TYPE_CONSOLE, 0);
+		if (!prz_ok(prz))
+			persistent_ram_annotation_merge(NULL);
+	}
 	if (!prz_ok(prz))
 		prz = ramoops_get_next_prz(&cxt->fprz, &cxt->ftrace_read_cnt,
 					   1, id, type, PSTORE_TYPE_FTRACE, 0);
 	if (!prz_ok(prz))
 		prz = ramoops_get_next_prz(&cxt->mprz, &cxt->pmsg_read_cnt,
 					   1, id, type, PSTORE_TYPE_PMSG, 0);
-	if (!prz_ok(prz))
+	if (!prz_ok(prz)) {
 		prz = ramoops_get_next_prz(&cxt->aprz, &cxt->annotate_read_cnt,
 					1, id, type, PSTORE_TYPE_ANNOTATE, 0);
+		persistent_ram_annotation_merge(prz);
+	}
 	if (!prz_ok(prz))
 		return 0;
 
