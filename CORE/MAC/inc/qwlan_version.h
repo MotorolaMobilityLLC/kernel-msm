4cf230ee4a91ba258a58be261daa08de82f5ec7e
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-06-11 14:14:49 -0700

    Wlan: Revision 3.2.3.350
    Revision 3.2.3.350
    
    Change-Id: I9dd7dfce9d40527ade04fa0d7acadc14310a1e31

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            349
+#define QWLAN_VERSION_BUILD            350
 
-#define QWLAN_VERSIONSTR               "3.2.3.349"
+#define QWLAN_VERSIONSTR               "3.2.3.350"
 
 #endif /* QWLAN_VERSION_H */
