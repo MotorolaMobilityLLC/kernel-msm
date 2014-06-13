2719f0b413bdfeaf97e94787a10ae9f306824da7
Bang Nguyen <bnguyen1@motorola.com>
2014-06-13 15:51:53 -0500

    msm: mdss: Support disp_util tool
    Support the disp_util tool to set and get the register information from
    MIPI DSI display from user space.
    
    Port of 90ce56f.
    
    Change-Id: I2d53aca00a1590d4ddf9b4ff3253e8e88c8e7137
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/653177
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -923,6 +923,7 @@ void mdss_mdp_crop_rect(struct mdss_rect *src_rect,
 
 int mdss_mdp_wb_kickoff(struct msm_fb_data_type *mfd);
 int mdss_mdp_wb_ioctl_handler(struct msm_fb_data_type *mfd, u32 cmd, void *arg);
+int mdss_dsi_ioctl_handler(struct mdss_panel_data *pdata, u32 cmd, void *arg);
 
 int mdss_mdp_get_ctl_mixers(u32 fb_num, u32 *mixer_id);
 u32 mdss_mdp_get_mixercfg(struct mdss_mdp_mixer *mixer);
