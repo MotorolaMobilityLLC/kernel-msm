b0f3edbfa1f95f8c43d522ecb2ae4b3b1357414c
Russ W. Knize <Russ.Knize@motorola.com>
2014-06-17 15:51:32 -0500

    esdfs: unified derived permissions support
    This commit implements the missing pieces of "unified" derived
    permissions in order to support external SD cards.
    
    Change-Id: I5a4229814b663937f1a2cbcabb495acab2af3898
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- fs/esdfs/lookup.c
+++ fs/esdfs/lookup.c
@@ -232,7 +232,7 @@ static struct dentry *__esdfs_lookup(struct dentry *dentry,
 		esdfs_set_lower_path(dentry, &lower_path);
 		err = esdfs_interpose(dentry, dentry->d_sb, &lower_path);
 		if (err) /* path_put underlying path on error */
-			esdfs_put_reset_lower_path(dentry);
+			esdfs_put_reset_lower_paths(dentry);
 		goto out;
 	}
 
@@ -279,14 +279,21 @@ struct dentry *esdfs_lookup(struct inode *dir, struct dentry *dentry,
 			    unsigned int flags)
 {
 	int err;
-	struct dentry *ret, *old_parent, *parent;
+	struct dentry *ret, *real_parent, *parent;
 	struct path lower_parent_path, old_lower_parent_path;
 	const struct cred *creds =
 			esdfs_override_creds(ESDFS_SB(dir->i_sb), NULL);
 	if (!creds)
 		return NULL;
 
-	parent = old_parent = dget_parent(dentry);
+	parent = real_parent = dget_parent(dentry);
+
+	/* allocate dentry private data.  We free it in ->d_release */
+	err = new_dentry_private_data(dentry);
+	if (err) {
+		ret = ERR_PTR(err);
+		goto out;
+	}
 
 	if (ESDFS_DERIVE_PERMS(ESDFS_SB(dir->i_sb))) {
 		err = esdfs_derived_lookup(dentry, &parent);
@@ -298,12 +305,6 @@ struct dentry *esdfs_lookup(struct inode *dir, struct dentry *dentry,
 
 	esdfs_get_lower_path(parent, &lower_parent_path);
 
-	/* allocate dentry private data.  We free it in ->d_release */
-	err = new_dentry_private_data(dentry);
-	if (err) {
-		ret = ERR_PTR(err);
-		goto out_put;
-	}
 	ret = __esdfs_lookup(dentry, flags, &lower_parent_path);
 	if (IS_ERR(ret))
 		goto out_put;
@@ -316,19 +317,22 @@ struct dentry *esdfs_lookup(struct inode *dir, struct dentry *dentry,
 	fsstack_copy_attr_atime(parent->d_inode,
 				esdfs_lower_inode(parent->d_inode));
 
-	/* More pseudo-hard-link artifacts */
-	if (parent != old_parent) {
-		esdfs_get_lower_path(old_parent, &old_lower_parent_path);
+	/*
+	 * If this is a pseudo hard link, store the real parent and ensure
+	 * that the link target directory contains any derived contents.
+	 */
+	if (parent != real_parent) {
+		esdfs_get_lower_path(real_parent, &old_lower_parent_path);
 		esdfs_set_lower_parent(dentry, old_lower_parent_path.dentry);
-		esdfs_put_lower_path(old_parent, &old_lower_parent_path);
+		esdfs_put_lower_path(real_parent, &old_lower_parent_path);
 		esdfs_derive_mkdir_contents(dentry);
 	}
 out_put:
 	esdfs_put_lower_path(parent, &lower_parent_path);
 out:
 	dput(parent);
-	if (parent != old_parent)
-		dput(old_parent);
+	if (parent != real_parent)
+		dput(real_parent);
 
 	esdfs_revert_creds(creds, NULL);
 	return ret;
