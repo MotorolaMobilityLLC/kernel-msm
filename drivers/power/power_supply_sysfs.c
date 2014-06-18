4a67290f25df1a75d471000febdbc8830b41d041
Jason Knopsnyder <jasonk@motorola.com>
2014-06-18 17:24:47 -0500

    power_supply: Add NUM_SYSTEM_TEMP_LEVELS property
    The NUM_SYSTEM_TEMP_LEVELS property holds the number of possible
    settings supported by the SYSTEM_TEMP_LEVEL property.
    
    Change-Id: Iaf33c43cd4f3cb00a1c253ffb126b6d165f685e2
    Signed-off-by: Jason Knopsnyder <jasonk@motorola.com>
    Reviewed-on: http://gerrit.mot.com/647270
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -185,6 +185,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(type),
 	POWER_SUPPLY_ATTR(scope),
 	POWER_SUPPLY_ATTR(system_temp_level),
+	POWER_SUPPLY_ATTR(num_system_temp_levels),
 	POWER_SUPPLY_ATTR(resistance),
 	POWER_SUPPLY_ATTR(low_power),
 	/* Properties of type `const char *' */
