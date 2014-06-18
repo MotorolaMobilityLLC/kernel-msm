ce61406b7d5d6dc68d08049acfd6089d4d44eaaa
Lucille Sylvester <lsylvest@codeaurora.org>
2014-06-18 16:17:05 -0600

    msm: kgsl: fix MIN power constraints in non-trivial cases
    Switching from a MAX to a MIN power constraint in the same
    context is broken.  So is switching from a MAX context constraint
    to a MIN constraint on a different context.  Clarify constraint
    ownership, update the timestamps accordingly, and reset constraints
    when changed by the context.
    
    Change-Id: Id27dbc20e2aac994101817af3857451c1703219e
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2283,6 +2283,11 @@ static int adreno_set_constraint(struct kgsl_device *device,
 		break;
 	}
 
+	/* If a new constraint has been set for a context, cancel the old one */
+	if ((status == 0) &&
+		(context->id == device->pwrctrl.constraint.owner_id))
+		device->pwrctrl.constraint.type = KGSL_CONSTRAINT_NONE;
+
 	return status;
 }
 
