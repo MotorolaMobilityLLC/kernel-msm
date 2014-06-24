fd5f777b6af578f4c30641a5f4b7fa54b40201c8
Rajesh Bondugula <rajeshb@codeaurora.org>
2014-06-24 11:39:06 -0700

    msm: camera: sensor: Set the default actuator_state to POWER_DOWN
    Set the default state of actuator to POWR_DOWN
    during actuator probe.
    
    Change-Id: I86fe4fb8692e4d080863c52a5939ab494b47d428
    Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1106,6 +1106,8 @@ static int32_t msm_actuator_platform_probe(struct platform_device *pdev)
 	msm_actuator_t->msm_sd.sd.entity.group_id = MSM_CAMERA_SUBDEV_ACTUATOR;
 	msm_actuator_t->msm_sd.close_seq = MSM_SD_CLOSE_2ND_CATEGORY | 0x2;
 	msm_sd_register(&msm_actuator_t->msm_sd);
+	msm_actuator_t->actuator_state = ACTUATOR_POWER_DOWN;
+
 	CDBG("Exit\n");
 	return rc;
 }
