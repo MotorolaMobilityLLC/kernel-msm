cc0518902a16fcbc5bd07caa6c978c36faf5a5dc
Gu Zheng <guz.fnst@cn.fujitsu.com>
2014-06-24 18:21:23 +0800

    f2fs: arguments cleanup of finding file flow functions
    Signed-off-by: Gu Zheng <guz.fnst@cn.fujitsu.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Conflicts:
    	fs/f2fs/dir.c
    
    Change-Id: Ia52c5e40bc509014361d551ca56e2f6b55334fc8

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -78,8 +78,8 @@ static unsigned long dir_block_index(unsigned int level,
 	return bidx;
 }
 
-static bool early_match_name(const char *name, size_t namelen,
-			f2fs_hash_t namehash, struct f2fs_dir_entry *de)
+static bool early_match_name(size_t namelen, f2fs_hash_t namehash,
+				struct f2fs_dir_entry *de)
 {
 	if (le16_to_cpu(de->name_len) != namelen)
 		return false;
@@ -91,7 +91,7 @@ static bool early_match_name(const char *name, size_t namelen,
 }
 
 static struct f2fs_dir_entry *find_in_block(struct page *dentry_page,
-			const char *name, size_t namelen, int *max_slots,
+			struct qstr *name, int *max_slots,
 			f2fs_hash_t namehash, struct page **res_page,
 			unsigned int flags)
 {
@@ -113,15 +113,16 @@ static struct f2fs_dir_entry *find_in_block(struct page *dentry_page,
 
 		de = &dentry_blk->dentry[bit_pos];
 		if (flags & LOOKUP_NOCASE) {
-			if ((le16_to_cpu(de->name_len) == namelen) &&
+			if ((le16_to_cpu(de->name_len) == name->len) &&
 			    !strncasecmp(dentry_blk->filename[bit_pos],
-				name, namelen)) {
+					    name->name, name->len)) {
 				*res_page = dentry_page;
 				goto found;
 			}
-		} else if (early_match_name(name, namelen, namehash, de)) {
+		} else if (early_match_name(name->len, namehash, de)) {
 			if (!memcmp(dentry_blk->filename[bit_pos],
-							name, namelen)) {
+							name->name,
+							name->len)) {
 				*res_page = dentry_page;
 				goto found;
 			}
@@ -142,11 +143,11 @@ found:
 }
 
 static struct f2fs_dir_entry *find_in_level(struct inode *dir,
-		unsigned int level, const char *name, size_t namelen,
+			unsigned int level, struct qstr *name,
 			f2fs_hash_t namehash, struct page **res_page,
-				unsigned int flags)
+			unsigned int flags)
 {
-	int s = GET_DENTRY_SLOTS(namelen);
+	int s = GET_DENTRY_SLOTS(name->len);
 	unsigned int nbucket, nblock;
 	unsigned int bidx, end_block;
 	struct page *dentry_page;
@@ -177,8 +178,8 @@ static struct f2fs_dir_entry *find_in_level(struct inode *dir,
 		    F2FS_I(dir)->i_advise & FADVISE_ANDROID_EMU)
 			flags |= LOOKUP_NOCASE;
 
-		de = find_in_block(dentry_page, name, namelen,
-					&max_slots, namehash, res_page, flags);
+		de = find_in_block(dentry_page, name, &max_slots,
+					namehash, res_page, flags);
 		if (de)
 			break;
 
@@ -204,8 +205,6 @@ static struct f2fs_dir_entry *find_in_level(struct inode *dir,
 struct f2fs_dir_entry *f2fs_find_entry(struct inode *dir, struct qstr *child,
 		struct page **res_page, unsigned int flags)
 {
-	const char *name = child->name;
-	size_t namelen = child->len;
 	unsigned long npages = dir_blocks(dir);
 	struct f2fs_dir_entry *de = NULL;
 	f2fs_hash_t name_hash;
@@ -217,12 +216,12 @@ struct f2fs_dir_entry *f2fs_find_entry(struct inode *dir, struct qstr *child,
 
 	*res_page = NULL;
 
-	name_hash = f2fs_dentry_hash(name, namelen);
+	name_hash = f2fs_dentry_hash(child);
 	max_depth = F2FS_I(dir)->i_current_depth;
 
 	for (level = 0; level < max_depth; level++) {
-		de = find_in_level(dir, level, name,
-				namelen, name_hash, res_page, flags);
+		de = find_in_level(dir, level, child, name_hash,
+						res_page, flags);
 		if (de)
 			break;
 	}
@@ -477,7 +476,7 @@ int __f2fs_add_link(struct inode *dir, const struct qstr *name,
 	int err = 0;
 	int i;
 
-	dentry_hash = f2fs_dentry_hash(name->name, name->len);
+	dentry_hash = f2fs_dentry_hash(name);
 	level = 0;
 	current_depth = F2FS_I(dir)->i_current_depth;
 	if (F2FS_I(dir)->chash == dentry_hash) {
