e60ea02662d82df96758e7bcb6a9bc418ee144a1
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-06-26 12:59:09 -0700

    Wlan: Revision 3.2.3.363
    Revision 3.2.3.363
    
    Change-Id: I362d6631b68cc72f78fe6aff00488c3444c32af2
    CRs-Fixed: 686314

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            362
+#define QWLAN_VERSION_BUILD            363
 
-#define QWLAN_VERSIONSTR               "3.2.3.362"
+#define QWLAN_VERSIONSTR               "3.2.3.363"
 
 #endif /* QWLAN_VERSION_H */
