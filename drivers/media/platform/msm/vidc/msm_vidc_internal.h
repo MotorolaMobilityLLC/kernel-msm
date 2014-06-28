9bf8c2c74b78bf34af163d564ae294f02cc6809f
Sowmya Pandiri <spandiri@codeaurora.org>
2014-06-27 19:19:23 -0700

    msm: vidc: Add DCVS support
    This change enables DCVS for 4K/H264 decoder use case thus reducing
    power utilization. This is achieved by dynamically adapting
    venus core clock vote to varying load conditions by monitoring
    the buffer count released to and recycled from display.
    
    Change-Id: Id0a9b4b6f5c79ba97fecd3d1d27c34832022edb3
    Signed-off-by: Sowmya Pandiri <spandiri@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_internal.h
+++ drivers/media/platform/msm/vidc/msm_vidc_internal.h
@@ -58,6 +58,40 @@
 #define MAX_NAME_LENGTH 64
 
 #define EXTRADATA_IDX(__num_planes) (__num_planes - 1)
+
+#define NUM_MBS_PER_SEC(__height, __width, __fps) ({\
+	(__height / 16) * (__width  / 16) * __fps; \
+})
+
+#define NUM_MBS_PER_FRAME(__height, __width) ({\
+	((__height + 15) >> 4) * ((__width + 15) >> 4); \
+})
+
+/* Default threshold to reduce the core frequency */
+#define DCVS_NOMINAL_THRESHOLD 8
+/* Default threshold to increase the core frequency */
+#define DCVS_TURBO_THRESHOLD 4
+/* Instance max load above which DCVS kicks in */
+#define DCVS_NOMINAL_LOAD NUM_MBS_PER_SEC(1088, 1920, 60)
+/* Considering one output buffer with core */
+#define DCVS_BUFFER_WITH_DEC 1
+/* Considering one safeguard buffer */
+#define DCVS_BUFFER_SAFEGUARD 1
+/* Considering one output buffer in transition after decode */
+#define DCVS_BUFFER_RELEASED_DEC 1
+/* Considering atleast one FTB between each FBD */
+#define DCVS_MIN_DRAIN_RATE 1
+/* Ensures difference of 4 between min and max threshold always*/
+#define DCVS_MIN_THRESHOLD_DIFF 4
+/* Maintains the number of FTB's between each FBD over a window */
+#define DCVS_FTB_WINDOW 16
+/* Empirical number arrived at to calculate the high threshold*/
+#define DCVS_EMP_THRESHOLD_HIGH 8
+/* Supported DCVS MBs per frame */
+#define DCVS_MIN_SUPPORTED_MBPERFRAME NUM_MBS_PER_FRAME(2160, 3840)
+/* Window size used to calculate the low threshold */
+#define DCVS_FTB_STAT_SAMPLES 4
+
 enum vidc_ports {
 	OUTPUT_PORT,
 	CAPTURE_PORT,
@@ -162,6 +196,24 @@ struct buf_count {
 	int ebd;
 };
 
+struct dcvs_stats {
+	int num_ftb[DCVS_FTB_WINDOW];
+	int ftb_index;
+	int ftb_counter;
+	int prev_ftb_count;
+	bool prev_freq_lowered;
+	bool prev_freq_increased;
+	bool change_initial_freq;
+	int threshold_disp_buf_high;
+	int threshold_disp_buf_low;
+	int load;
+	int load_low;
+	int load_high;
+	int min_threshold;
+	int max_threshold;
+	bool is_clock_scaled;
+};
+
 struct profile_data {
 	int start;
 	int stop;
@@ -245,6 +297,7 @@ struct msm_vidc_inst {
 	void *priv;
 	struct msm_vidc_debug debug;
 	struct buf_count count;
+	struct dcvs_stats dcvs;
 	enum msm_vidc_modes flags;
 	struct msm_vidc_core_capability capability;
 	enum buffer_mode_type buffer_mode_set[MAX_PORT_NUM];
@@ -252,6 +305,7 @@ struct msm_vidc_inst {
 	bool map_output_buffer;
 	atomic_t get_seq_hdr_cnt;
 	struct v4l2_ctrl **ctrls;
+	bool dcvs_mode;
 };
 
 extern struct msm_vidc_drv *vidc_driver;
