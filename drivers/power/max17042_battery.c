f0fcd26828f5266e2e358e2fa20779cb2c6164c9
Kenny Kessler <kenny.kessler@motorola.com>
2014-07-07 14:46:09 -0500

    IKDREL3KK-3853: power: max17042: Set More Registers
    Add the COFF, CGAIN, and MiscCFG Registers.
    With this change these register will be able to
    be set through the Device Tree.
    
    Change-Id: If483cbcee5a344ffc290253c3fec700b69368921
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: http://gerrit.mot.com/651285
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/max17042_battery.c
+++ drivers/power/max17042_battery.c
@@ -77,6 +77,8 @@
 #define MAX17050_CFG_REV_REG	MAX17042_ManName
 #define MAX17050_CFG_REV_MASK	0x0007
 
+#define MAX17050_FORCE_POR	0x000F
+
 #define INIT_DATA_PROPERTY		"maxim,regs-init-data"
 #define CONFIG_NODE			"maxim,configuration"
 #define CONFIG_PROPERTY			"config"
@@ -86,6 +88,7 @@
 #define LEARN_CFG_PROPERTY		"learn_cfg"
 #define FILTER_CFG_PROPERTY		"filter_cfg"
 #define RELAX_CFG_PROPERTY		"relax_cfg"
+#define MISC_CFG_PROPERTY		"misc_cfg"
 #define FULLCAP_PROPERTY		"fullcap"
 #define FULLCAPNOM_PROPERTY		"fullcapnom"
 #define QRTBL00_PROPERTY		"qrtbl00"
@@ -97,6 +100,8 @@
 #define CELL_CHAR_TBL_PROPERTY		"maxim,cell-char-tbl"
 #define TGAIN_PROPERTY			"tgain"
 #define TOFF_PROPERTY			"toff"
+#define CGAIN_PROPERTY			"cgain"
+#define COFF_PROPERTY			"coff"
 #define TEMP_CONV_NODE			"maxim,temp-conv"
 #define RESULT_PROPERTY			"result"
 #define START_PROPERTY			"start"
@@ -1279,6 +1284,9 @@ static void max17042_cfg_optnl_prop(struct device_node *np,
 {
 	of_property_read_u16(np, TGAIN_PROPERTY, &config_data->tgain);
 	of_property_read_u16(np, TOFF_PROPERTY, &config_data->toff);
+	of_property_read_u16(np, CGAIN_PROPERTY, &config_data->cgain);
+	of_property_read_u16(np, COFF_PROPERTY, &config_data->coff);
+	of_property_read_u16(np, MISC_CFG_PROPERTY, &config_data->misc_cfg);
 	of_property_read_u16(np, REVISION, &config_data->revision);
 	config_data->revision &= MAX17050_CFG_REV_MASK;
 }
@@ -1310,8 +1318,6 @@ max17042_get_config_data(struct device *dev)
 	dev_warn(dev, "Config Revision = %d", config_data->revision);
 
 	config_data->cur_sense_val = 10;
-	config_data->cgain = 0x4000;
-	config_data->coff = 0x0000;
 	config_data->valrt_thresh = 0xFF97;
 	config_data->talrt_thresh = 0x7F80;
 	config_data->soc_alrt_thresh = 0xFF00;
@@ -1651,15 +1657,18 @@ static int max17042_probe(struct i2c_client *client,
 		}
 	}
 
-	regmap_read(chip->regmap, MAX17042_STATUS, &val);
 	regmap_read(chip->regmap, MAX17050_CFG_REV_REG, &val2);
 	val2 &= MAX17050_CFG_REV_MASK;
+	if (val2 != chip->pdata->config_data->revision) {
+		dev_warn(&client->dev, "Revision Change, Forcing POR!\n");
+		max17042_write_reg(client, MAX17042_VFSOC0Enable,
+				   MAX17050_FORCE_POR);
+		msleep(20);
+	}
+
+	regmap_read(chip->regmap, MAX17042_STATUS, &val);
 	if (val & STATUS_POR_BIT) {
 		dev_warn(&client->dev, "POR Detected, Loading Config\n");
-		INIT_WORK(&chip->work, max17042_init_worker);
-		schedule_work(&chip->work);
-	} else if (val2 != chip->pdata->config_data->revision) {
-		dev_warn(&client->dev, "Revision Change, Loading Config\n");
 		INIT_WORK(&chip->work, max17042_init_worker);
 		schedule_work(&chip->work);
 	} else {
