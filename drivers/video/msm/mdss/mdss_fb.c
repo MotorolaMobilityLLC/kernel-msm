2d677192dc791624db6d8d7540890f4821d6282b
Andrew Wheeler <awheeler@motorola.com>
2014-07-07 15:07:32 -0500

    Revert "mdss: fb: ensure that shared memory is available during probe"
    This change requires dependencies not in our mainline.
    
    This reverts commit 45b3dea76c6dbd928513a3d7e9cd5026428d6be1.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -90,8 +90,6 @@ static int mdss_fb_ioctl(struct fb_info *info, unsigned int cmd,
 			 unsigned long arg);
 static int mdss_fb_fbmem_ion_mmap(struct fb_info *info,
 		struct vm_area_struct *vma);
-static int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd,
-		size_t fb_size);
 static void mdss_fb_release_fences(struct msm_fb_data_type *mfd);
 static int __mdss_fb_sync_buf_done_callback(struct notifier_block *p,
 		unsigned long val, void *data);
@@ -1079,6 +1077,7 @@ void mdss_fb_free_fb_ion_memory(struct msm_fb_data_type *mfd)
 
 	mfd->fbi->screen_base = NULL;
 	mfd->fbi->fix.smem_start = 0;
+	mfd->fbi->fix.smem_len = 0;
 
 	ion_unmap_kernel(mfd->fb_ion_client, mfd->fb_ion_handle);
 
@@ -1090,9 +1089,9 @@ void mdss_fb_free_fb_ion_memory(struct msm_fb_data_type *mfd)
 	ion_free(mfd->fb_ion_client, mfd->fb_ion_handle);
 }
 
-static int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd,
-						size_t fb_size)
+int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd)
 {
+	size_t size;
 	unsigned long buf_size;
 	int rc;
 	void *vaddr;
@@ -1110,9 +1109,10 @@ static int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd,
 		}
 	}
 
-	pr_debug("size for mmap = %d", (int) fb_size);
+	size = mfd->fbi->fix.line_length * mfd->fbi->var.yres * MDSS_FB_NUM;
+	pr_debug("size for mmap = %d", (int) size);
 
-	mfd->fb_ion_handle = ion_alloc(mfd->fb_ion_client, fb_size, SZ_4K,
+	mfd->fb_ion_handle = ion_alloc(mfd->fb_ion_client, size, SZ_4K,
 			ION_HEAP(ION_SYSTEM_HEAP_ID), 0);
 	if (IS_ERR_OR_NULL(mfd->fb_ion_handle)) {
 		pr_err("unable to alloc fbmem from ion - %ld\n",
@@ -1145,12 +1145,12 @@ static int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd,
 		goto fb_mmap_failed;
 	}
 
-	pr_info("alloc 0x%xB vaddr = %p (%pa iova) for fb%d\n", fb_size, vaddr,
+	pr_info("alloc 0x%xB vaddr = %p (%pa iova) for fb%d\n", size, vaddr,
 			&mfd->iova, mfd->index);
 
 	mfd->fbi->screen_base = (char *) vaddr;
 	mfd->fbi->fix.smem_start = (unsigned int) mfd->iova;
-	mfd->fbi->fix.smem_len = fb_size;
+	mfd->fbi->fix.smem_len = size;
 
 	return rc;
 
@@ -1190,14 +1190,14 @@ static int mdss_fb_fbmem_ion_mmap(struct fb_info *info,
 	}
 
 	req_size = vma->vm_end - vma->vm_start;
-	fb_size = mfd->fbi->fix.smem_len;
+	fb_size = mfd->fbi->fix.line_length * mfd->fbi->var.yres * MDSS_FB_NUM;
 	if (req_size > fb_size) {
 		pr_warn("requested map is greater than framebuffer");
 		return -EOVERFLOW;
 	}
 
 	if (!mfd->fbi->screen_base) {
-		rc = mdss_fb_alloc_fb_ion_memory(mfd, fb_size);
+		rc = mdss_fb_alloc_fb_ion_memory(mfd);
 		if (rc < 0) {
 			pr_err("fb mmap failed!!!!");
 			return rc;
@@ -1449,14 +1449,8 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 	var->hsync_len = panel_info->lcdc.h_pulse_width;
 	var->pixclock = panel_info->clk_rate / 1000;
 
-	/*
-	 * Populate smem length with fb size. smem_len gets
-	 * over written if physically contiguous  memory is
-	 * allocated at boot time
-	 */
-	fix->smem_len = fix->line_length * var->yres_virtual;
-
 	/* id field for fb app  */
+
 	id = (int *)&mfd->panel;
 
 	snprintf(fix->id, sizeof(fix->id), "mdssfb_%x", (u32) *id);
