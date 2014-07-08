0eab2a554648f891b8140fa91e66917e870d926d
Russ W. Knize <Russ.Knize@motorola.com>
2014-07-08 12:06:13 -0500

    mmc: raw command logging
    Propagated from (CR)
    
    This commit implements raw command logging for the MMC driver framework.
    Unlike existing solutions, this facility is built with speed and
    efficiency in mind.  It has a negligible performance impact during driver
    operation and the amount of detail collected is run-time configurable.
    This allows for data collection that can span long periods of time. It
    is intended to log commands as they are programmed into the controller
    hardware.  This effectively unrolls some of the compound commands that
    are obfuscated from the higher-layer MMC_DEBUG logging.
    
    Raw MMC commands and arguments are logged to a ring buffer.  Optionally,
    command R1 responses and time stamps can also be stored.  These options,
    the total log size, and log retrieval are all accessed via the MMC
    driver's debugfs directory (/sys/kernel/debug/mmcN):
    
      cmd_log_mode - enable logging, control verbosity, dump to printk
      cmd_log_size - number of records to allocate ring buffer memory for
      cmd_log - read the log contents
    
    The mode or size can be changed at any time.
    
    Change-Id: I1f07de92390d1616b2ea6552a3bf36c0dab7879f
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1249,6 +1249,9 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	if (cmd->data)
 		host->data_start_time = ktime_get();
 	trace_mmc_cmd_rw_start(cmd->opcode, cmd->arg, cmd->flags);
+
+	mmc_cmd_log(host->mmc, cmd->opcode, cmd->arg);
+
 	sdhci_writew(host, SDHCI_MAKE_CMD(cmd->opcode, flags), SDHCI_COMMAND);
 }
 
@@ -1272,6 +1275,7 @@ static void sdhci_finish_command(struct sdhci_host *host)
 		} else {
 			host->cmd->resp[0] = sdhci_readl(host, SDHCI_RESPONSE);
 		}
+		mmc_cmd_log_resp(host->mmc, host->cmd->resp[0]);
 	}
 
 	/* Finished CMD23, now send actual command. */
