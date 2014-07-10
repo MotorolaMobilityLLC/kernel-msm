52ae570a9ff28180304c4767586837dc8ba9b94b
Jane Zhou <a17711@motorola.com>
2014-07-10 16:47:22 -0700

    usb:xhci add condition for xhci_bus_suspend
    It is necessary to add result check for xhci_ stop_device to decide whether
    xhci_bus_suspend is successful or not. i.e. when xhci_stop_device is
    failed, xhci_bus_suspend will be considered failed too, which will make
    mxhci_hsic_bus_suspend exit properly.
    
    Change-Id: I304eb32de42aad8267c742987ceb648485698b80
    Signed-off-by: Jane Zhou <a17711@motorola.com>
    Reviewed-on: http://gerrit.mot.com/652510
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -1226,6 +1226,7 @@ int xhci_bus_suspend(struct usb_hcd *hcd)
 	__le32 __iomem **port_array;
 	struct xhci_bus_state *bus_state;
 	unsigned long flags;
+	int ret = 0;
 
 	max_ports = xhci_get_ports(hcd, &port_array);
 	bus_state = &xhci->bus_state[hcd_index(hcd)];
@@ -1260,7 +1261,7 @@ int xhci_bus_suspend(struct usb_hcd *hcd)
 					port_index + 1);
 			if (slot_id) {
 				spin_unlock_irqrestore(&xhci->lock, flags);
-				xhci_stop_device(xhci, slot_id, 1);
+				ret = xhci_stop_device(xhci, slot_id, 1) | ret;
 				spin_lock_irqsave(&xhci->lock, flags);
 			}
 			t2 &= ~PORT_PLS_MASK;
@@ -1308,7 +1309,7 @@ int xhci_bus_suspend(struct usb_hcd *hcd)
 	hcd->state = HC_STATE_SUSPENDED;
 	bus_state->next_statechange = jiffies + msecs_to_jiffies(10);
 	spin_unlock_irqrestore(&xhci->lock, flags);
-	return 0;
+	return ret;
 }
 
 int xhci_bus_resume(struct usb_hcd *hcd)
