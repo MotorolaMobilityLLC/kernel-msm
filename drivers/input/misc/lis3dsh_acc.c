c16191bc1057291559025abeec71d2554e2360a8
maggielee <maggie_lee@asus.com>
2014-07-11 14:02:22 +0800

    [WI500Q][Sensors][NA][fix] increase knock threshold and implement stability limitation
    Change-Id: Iafbb4ce359faad90522c4eee79a11201fa437d57
    Change-Id: Ic2f6a32efef16b3528eb7dca189f2e8caffa6d76
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/135253
    Reviewed-by: Maggie Lee <maggie_lee@asus.com>
    Tested-by: Maggie Lee <maggie_lee@asus.com>

--- drivers/input/misc/lis3dsh_acc.c
+++ drivers/input/misc/lis3dsh_acc.c
@@ -466,18 +466,19 @@ static void lis3dsh_acc_set_init_statepr2_inst(struct lis3dsh_acc_data *acc)
 {
 #if (LOAD_SM2_PROGRAM == 1)
 	/* Place here state machine 2 program */
-	acc->resume_stmach_program2[0] = 0x15;
-	acc->resume_stmach_program2[1] = 0x47;
-	acc->resume_stmach_program2[2] = 0x03;
-	acc->resume_stmach_program2[3] = 0x62;
-	acc->resume_stmach_program2[4] = 0x15;
-	acc->resume_stmach_program2[5] = 0x47;
-	acc->resume_stmach_program2[6] = 0x03;
-	acc->resume_stmach_program2[7] = 0x62;
-	acc->resume_stmach_program2[8] = 0x11;
-	acc->resume_stmach_program2[9] = 0x00;
-	acc->resume_stmach_program2[10] = 0x00;
-	acc->resume_stmach_program2[11] = 0x00;
+	/* Knock Knock Function */
+	acc->resume_stmach_program2[0] = 0x08;
+	acc->resume_stmach_program2[1] = 0x61;
+	acc->resume_stmach_program2[2] = 0x61;
+	acc->resume_stmach_program2[3] = 0x15;
+	acc->resume_stmach_program2[4] = 0x47;
+	acc->resume_stmach_program2[5] = 0x03;
+	acc->resume_stmach_program2[6] = 0x52;
+	acc->resume_stmach_program2[7] = 0x15;
+	acc->resume_stmach_program2[8] = 0x47;
+	acc->resume_stmach_program2[9] = 0x03;
+	acc->resume_stmach_program2[10] = 0x52;
+	acc->resume_stmach_program2[11] = 0x11;
 	acc->resume_stmach_program2[12] = 0x00;
 	acc->resume_stmach_program2[13] = 0x00;
 	acc->resume_stmach_program2[14] = 0x00;
@@ -564,8 +565,8 @@ static void lis3dsh_acc_set_init_statepr2_param(struct lis3dsh_acc_data *acc)
 	acc->resume_state[RES_LIS3DSH_TIM2_2_H] = 0x00;
 	acc->resume_state[RES_LIS3DSH_TIM1_2_L] = 0x86;
 	acc->resume_state[RES_LIS3DSH_TIM1_2_H] = 0x00;
-	acc->resume_state[RES_LIS3DSH_THRS2_2] = 0x03;
-	acc->resume_state[RES_LIS3DSH_THRS1_2] = 0x03;
+	acc->resume_state[RES_LIS3DSH_THRS2_2] = 0x02;
+	acc->resume_state[RES_LIS3DSH_THRS1_2] = 0x05;
 	acc->resume_state[RES_LIS3DSH_DES_2] = 0x00;
 	acc->resume_state[RES_LIS3DSH_SA_2] = 0x00;
 	acc->resume_state[RES_LIS3DSH_MA_2] = 0x03;
