13ec5a3c748fae1fd29228bbfd26b4e014b0d515
Jordan Crouse <jcrouse@codeaurora.org>
2014-07-15 09:00:31 -0600

    msm: kgsl: Maintain a list of global pagetable entries
    Store "global" pagetable entries in a master list and use
    that list to automatically set and add the entries to
    pagetables as they are created.
    
    Conflicts:
    	drivers/gpu/msm/adreno_ringbuffer.c
    	drivers/gpu/msm/kgsl.c
    
    Change-Id: Ic0dedbad1b216539c8e3b7290f415e410be51792
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    (cherry picked from commit a997bcda421aa1417f8e7e3756aac58327d3722f)

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4267,11 +4267,11 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_dest_work_q;
 	}
 
-	status = kgsl_allocate_contiguous(device, &device->memstore,
-		KGSL_MEMSTORE_SIZE);
+	status = kgsl_allocate_global(device, &device->memstore,
+		KGSL_MEMSTORE_SIZE, 0);
 
 	if (status != 0) {
-		KGSL_DRV_ERR(device, "kgsl_allocate_contiguous failed %d\n",
+		KGSL_DRV_ERR(device, "kgsl_allocate_global failed %d\n",
 				status);
 		goto error_close_mmu;
 	}
@@ -4325,7 +4325,7 @@ void kgsl_device_platform_remove(struct kgsl_device *device)
 
 	idr_destroy(&device->context_idr);
 
-	kgsl_sharedmem_free(&device->memstore);
+	kgsl_free_global(&device->memstore);
 
 	kgsl_mmu_close(device);
 
