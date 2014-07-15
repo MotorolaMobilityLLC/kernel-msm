182bf11eaee93ac2d44c6fe15d6ebc292620bdc2
Zac Crosby <zac@motorola.com>
2014-07-15 11:15:29 -0500

    msm: mdss: Add checking for bare_board flag.
    Skip to communicate with panel if this is a bare-board configuration.
    
    Port of 204f480.
    
    Change-Id: I13f809120b5655a9e9788d4e9b3c239be9938bce
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/678662
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -360,6 +360,11 @@ static int mdss_dsi_get_pwr_mode(struct mdss_panel_data *pdata, u8 *pwr_mode,
 
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata, panel_data);
 
+	if (ctrl->panel_config.bare_board == true) {
+		*pwr_mode = 0;
+		goto end;
+	}
+
 	old_rd_mode = mdss_dsi_get_tx_power_mode(pdata);
 	if (read_mode != old_rd_mode)
 		mdss_dsi_set_tx_power_mode(read_mode, pdata);
@@ -369,6 +374,7 @@ static int mdss_dsi_get_pwr_mode(struct mdss_panel_data *pdata, u8 *pwr_mode,
 	if (read_mode != old_rd_mode)
 		mdss_dsi_set_tx_power_mode(old_rd_mode, pdata);
 
+end:
 	pr_debug("%s: panel power mode = 0x%x\n", __func__, *pwr_mode);
 
 	return 0;
@@ -711,6 +717,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 			goto end;
 	}
 
+	if (ctrl->panel_config.bare_board == true) {
+		pr_warn("%s: This is bare_board configuration\n", __func__);
+		goto end;
+	}
+
 	if (ctrl->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->on_cmds);
 
@@ -781,6 +792,9 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		if (ctrl->ndx != DSI_CTRL_LEFT)
 			goto end;
 	}
+
+	if (ctrl->panel_config.bare_board == true)
+		goto end;
 
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
