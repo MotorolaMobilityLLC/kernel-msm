c89e7307deca5bd21904b663e82dc095a55e40fb
Carter Cooper <ccooper@codeaurora.org>
2014-07-17 14:49:12 -0600

    msm: kgsl: Respect power constraints for a full interval
    No matter what type of constraint leave it in effect until
    the constraint has expired.
    
    Change-Id: I75118823fd484f87dda8e0f26fa3fe1ae12ca07d
    Signed-off-by: Lucille Sylester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -192,8 +192,6 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		val *= pwr->step_mul;
 
 	if ((pwr->constraint.type == KGSL_CONSTRAINT_NONE) ||
-			((pwr->active_pwrlevel + val) <
-			pwr->constraint.hint.pwrlevel.level) ||
 			(time_after(jiffies, pwr->constraint.expires))) {
 
 		kgsl_pwrctrl_pwrlevel_change(device,
