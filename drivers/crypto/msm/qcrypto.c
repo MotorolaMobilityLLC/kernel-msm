cd6e925fc38d107411c59791cc2b00be3a7abd95
William Clark <wclark@codeaurora.org>
2014-07-24 11:10:52 -0700

    crypto: msm: change timer operation to fix timer list corruption
    remove the dulplicate init_timer operation in _qcrypto_resume and
    replace add_timer with mod_timer to resolve timer list corruption.
    
    Change-Id: I206016686be568ce2eefc41ea74d34639a8b04e5
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: William Clark <wclark@codeaurora.org>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -515,7 +515,8 @@ static void qcrypto_bw_set_timeout(struct crypto_engine *pengine)
 			(unsigned long)(pengine);
 	pengine->bw_reaper_timer.expires = jiffies +
 			msecs_to_jiffies(QCRYPTO_HIGH_BANDWIDTH_TIMEOUT);
-	add_timer(&(pengine->bw_reaper_timer));
+	mod_timer(&(pengine->bw_reaper_timer),
+		pengine->bw_reaper_timer.expires);
 }
 
 static void qcrypto_ce_bw_allocate_req(struct crypto_engine *pengine)
@@ -4723,7 +4724,6 @@ static int  _qcrypto_resume(struct platform_device *pdev)
 		if (qce_pm_table.resume)
 			qce_pm_table.resume(pengine->qce);
 
-		init_timer(&(pengine->bw_reaper_timer));
 		qcrypto_bw_set_timeout(pengine);
 
 		spin_lock_irqsave(&cp->lock, flags);
