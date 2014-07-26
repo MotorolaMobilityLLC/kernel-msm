a28f96017193ad4c1a418562ce55b70c7589f5ff
Adrian Salido-Moreno <adrianm@codeaurora.org>
2014-07-25 19:17:05 -0700

    msm: mdss: delay iommu mapping of buffers until commit
    Currently iommu map call is done in client's context during overlay
    play, this requires iommu attachment to correctly map buffers.
    This mapping can be delayed until the actual commit and avoid
    unnecessary delays in client's context. Also, only unmap buffers if they
    have been mapped, instead of checking if iommu is attached.
    
    Change-Id: I6b26acb3ed843f03b42df1386ee7bdd412365aff
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    (cherry picked from commit f01f7cd13954e86172a5745f5838487b0d2400e5)
    Reviewed-on: http://gerrit.mot.com/678514
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_rotator.c
+++ drivers/video/msm/mdss/mdss_mdp_rotator.c
@@ -638,29 +638,45 @@ int mdss_mdp_rotator_play(struct msm_fb_data_type *mfd,
 	if (!rot) {
 		pr_err("invalid session id=%x\n", req->id);
 		ret = -ENOENT;
-		goto dst_buf_fail;
+		goto session_fail;
 	}
 
 	flgs = rot->flags & MDP_SECURE_OVERLAY_SESSION;
 
+	mdss_iommu_ctrl(1);
 	ret = mdss_mdp_rotator_busy_wait_ex(rot);
 	if (ret) {
 		pr_err("rotator busy wait error\n");
 		goto dst_buf_fail;
 	}
 
-	mdss_mdp_overlay_free_buf(&rot->src_buf);
-	ret = mdss_mdp_overlay_get_buf(mfd, &rot->src_buf, &req->data, 1, flgs);
+	mdss_mdp_data_free(&rot->src_buf);
+	ret = mdss_mdp_data_get(&rot->src_buf, &req->data, 1, flgs);
 	if (ret) {
 		pr_err("src_data pmem error\n");
 		goto dst_buf_fail;
 	}
 
-	mdss_mdp_overlay_free_buf(&rot->dst_buf);
-	ret = mdss_mdp_overlay_get_buf(mfd, &rot->dst_buf,
-			&req->dst_data, 1, flgs);
+	ret = mdss_mdp_data_map(&rot->src_buf);
+	if (ret) {
+		pr_err("unable to map source buffer\n");
+		mdss_mdp_data_free(&rot->src_buf);
+		goto dst_buf_fail;
+	}
+
+	mdss_mdp_data_free(&rot->dst_buf);
+	ret = mdss_mdp_data_get(&rot->dst_buf, &req->dst_data, 1, flgs);
 	if (ret) {
 		pr_err("dst_data pmem error\n");
+		mdss_mdp_data_free(&rot->src_buf);
+		goto dst_buf_fail;
+	}
+
+	ret = mdss_mdp_data_map(&rot->dst_buf);
+	if (ret) {
+		pr_err("unable to map destination buffer\n");
+		mdss_mdp_data_free(&rot->dst_buf);
+		mdss_mdp_data_free(&rot->src_buf);
 		goto dst_buf_fail;
 	}
 
@@ -670,6 +686,8 @@ int mdss_mdp_rotator_play(struct msm_fb_data_type *mfd,
 		pr_err("rotator queue error session id=%x\n", req->id);
 
 dst_buf_fail:
+	mdss_iommu_ctrl(0);
+session_fail:
 	mutex_unlock(&rotator_lock);
 	return ret;
 }
@@ -681,8 +699,8 @@ int mdss_mdp_rotator_unset(int ndx)
 	mutex_lock(&rotator_lock);
 	rot = mdss_mdp_rotator_session_get(ndx);
 	if (rot) {
-		mdss_mdp_overlay_free_buf(&rot->src_buf);
-		mdss_mdp_overlay_free_buf(&rot->dst_buf);
+		mdss_mdp_data_free(&rot->src_buf);
+		mdss_mdp_data_free(&rot->dst_buf);
 
 		rot->pid = 0;
 		ret = mdss_mdp_rotator_finish(rot);
