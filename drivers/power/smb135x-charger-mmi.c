0c242afd89f9c5b1c8c691dbb2386333df466741
Ravi Vembu <raviv@motorola.com>
2014-07-29 23:31:25 -0500

    power: smb135x: Turn off OTG regulator on power down.
    Leaving the OTG regulator ON during a shutdown causes the
    phone to go into infinite boot-loops (since the reverse boost
    causes a turn on event) that is eventually ignored by the bootloader
    as an invalid powerup reason. It should be safe to unconditionally
    turn off the OTG regulator in the SMB upon a shutdown/reboot.
    
    Change-Id: I7e84db194989448f9f0e045070ca5f3a79d6e4b0
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/657928
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>

--- drivers/power/smb135x-charger-mmi.c
+++ drivers/power/smb135x-charger-mmi.c
@@ -27,6 +27,7 @@
 #include <linux/regulator/driver.h>
 #include <linux/regulator/of_regulator.h>
 #include <linux/regulator/machine.h>
+#include <linux/reboot.h>
 
 #define SMB135X_BITS_PER_REG	8
 
@@ -401,6 +402,7 @@ struct smb135x_chg {
 	int				charger_rate;
 	struct delayed_work		rate_check_work;
 	int				rate_check_count;
+	struct notifier_block		smb_reboot;
 };
 
 static struct smb135x_chg *the_chip;
@@ -4581,6 +4583,22 @@ static bool smb135x_charger_mmi_factory(void)
 	return factory;
 }
 
+static int smb135x_charger_reboot(struct notifier_block *nb,
+				unsigned long event, void *unused)
+{
+	struct smb135x_chg *chip =
+			container_of(nb, struct smb135x_chg, smb_reboot);
+	int rc = 0;
+
+	dev_dbg(chip->dev, "SMB Reboot\n");
+
+	rc = smb135x_masked_write(chip, CMD_CHG_REG, OTG_EN, 0);
+	if (rc < 0)
+		dev_err(chip->dev, "Couldn't disable OTG mode rc=%d\n", rc);
+
+	return NOTIFY_DONE;
+}
+
 static int smb135x_charger_probe(struct i2c_client *client,
 				const struct i2c_device_id *id)
 {
@@ -4732,6 +4750,13 @@ static int smb135x_charger_probe(struct i2c_client *client,
 		}
 		enable_irq_wake(client->irq);
 	}
+
+	chip->smb_reboot.notifier_call = smb135x_charger_reboot;
+	chip->smb_reboot.next = NULL;
+	chip->smb_reboot.priority = 1;
+	rc = register_reboot_notifier(&chip->smb_reboot);
+	if (rc)
+		dev_err(chip->dev, "register for reboot failed\n");
 
 	chip->debug_root = debugfs_create_dir("smb135x", NULL);
 	if (!chip->debug_root)
@@ -4929,6 +4954,7 @@ static int smb135x_charger_remove(struct i2c_client *client)
 			pr_err("Couldn't disable therm-bias rc = %d\n", rc);
 	}
 
+	unregister_reboot_notifier(&chip->smb_reboot);
 	debugfs_remove_recursive(chip->debug_root);
 
 	if (chip->dc_psy_type != -EINVAL)
