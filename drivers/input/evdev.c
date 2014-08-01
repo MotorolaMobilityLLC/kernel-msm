10bf1ef587f57896f89969de3b829cc4db3e642a
Yinjun Chen <a7301c@motorola.com>
2014-08-01 13:10:50 -0500

    Revert "Input: evdev - fall back to vmalloc for client event buffer"
    This reverts commit cb8e5e98aeae9e0d845576f6f59227c9ad8f76ff.
    
    THe commit has conflict with our commit:
    0747c69 (CR) input: evdev: daisy-chain header and client buffers
    
    Conflicts:
    	drivers/input/evdev.c
    
    Change-Id: I20d388f3c48476532a4878e1c03e018564e7c6dc

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -18,8 +18,6 @@
 #include <linux/poll.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/vmalloc.h>
-#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/input/mt.h>
@@ -302,10 +300,7 @@ static int evdev_release(struct inode *inode, struct file *file)
 	if (client->use_wake_lock)
 		wake_lock_destroy(&client->wake_lock);
 
-	if (is_vmalloc_addr(client))
-		vfree(client);
-	else
-		kfree(client);
+	kfree(client);
 
 	evdev_close_device(evdev);
 
@@ -325,14 +320,12 @@ static int evdev_open(struct inode *inode, struct file *file)
 {
 	struct evdev *evdev = container_of(inode->i_cdev, struct evdev, cdev);
 	unsigned int bufsize = evdev_compute_buffer_size(evdev->handle.dev);
-	unsigned int size = sizeof(struct evdev_client) +
-					bufsize * sizeof(struct input_event);
 	struct evdev_client *client;
 	int error;
 
-	client = kzalloc(size, GFP_KERNEL | __GFP_NOWARN);
-	if (!client)
-		client = vzalloc(size);
+	client = kzalloc(sizeof(struct evdev_client) +
+				bufsize * sizeof(struct input_event),
+			 GFP_KERNEL);
 	if (!client)
 		return -ENOMEM;
 
