c6e7a58dc062134c60973ffe1a3826ef805c8234
Russ W. Knize <Russ.Knize@motorola.com>
2014-08-01 14:49:20 -0500

    esdfs: carefully consider permissions at root
    Instead of dancing around the problem, just call
    esdfs_check_derived_permission() again in unified mode when creating
    files.  This will cause a redunant hash table lookup, but it's the only
    safe way to ensure that we make the right choice when dealing with the
    root directory.
    
    Change-Id: I4cebd2cb8d1096a091197cdeef9240f4c67c5ac8
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/659123
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>

--- fs/esdfs/inode.c
+++ fs/esdfs/inode.c
@@ -20,8 +20,17 @@ static int esdfs_create(struct inode *dir, struct dentry *dentry,
 	struct dentry *lower_parent_dentry = NULL;
 	struct path lower_path, saved_path;
 	int mask;
-	const struct cred *creds =
-			esdfs_override_creds(ESDFS_SB(dir->i_sb), &mask);
+	const struct cred *creds;
+
+	/*
+	 * Need to recheck derived permissions unified mode to prevent certain
+	 * applications from creating files at the root.
+	 */
+	if (test_opt(ESDFS_SB(dir->i_sb), DERIVE_UNIFIED) &&
+	    esdfs_check_derived_permission(dir, ESDFS_MAY_CREATE) != 0)
+		return -EACCES;
+
+	creds = esdfs_override_creds(ESDFS_SB(dir->i_sb), &mask);
 	if (!creds)
 		return -ENOMEM;
 
@@ -32,18 +41,6 @@ static int esdfs_create(struct inode *dir, struct dentry *dentry,
 	err = mnt_want_write(lower_path.mnt);
 	if (err)
 		goto out_unlock;
-
-	/*
-	 * Don't allow file creation at the root in unified mode.  Other
-	 * restrictions, including hash lookups, were done previously via
-	 * esdfs_permission().
-	 */
-	if (test_opt(ESDFS_SB(dir->i_sb), DERIVE_UNIFIED) &&
-	    ESDFS_I(dir)->tree == ESDFS_TREE_ROOT &&
-	    creds->fsuid != 0) {
-		err = -EACCES;
-		goto out_unlock;
-	}
 
 	esdfs_set_lower_mode(ESDFS_SB(dir->i_sb), &mode);
 
