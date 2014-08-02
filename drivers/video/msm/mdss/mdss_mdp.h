5a328bc94c9b504c9908dbcbced8a165871585dd
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-08-01 17:20:51 -0700

    msm: mdss: use helper functions to check for panel power state
    Whenever the display always-on feature is enabled, it is possible
    that the display pipeline can be in an intermediate state between being
    fully interactive to fully turned off. In the current implementation, a
    number of state transition decisions are made based on whether the
    display pipeline is on or not. Instead of explicitly checking for the
    panel power state, define generic helper functions which classify the
    panel power state into three different stages - interactive, low-power
    and off. Replace any existing check for the panel power state with these
    newly defined helper functions.
    
    Change-Id: I4d6dd883ea6aca8abd57fa048a79e0c88da09a95
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Tirtha Kanti Ghosh <tirtha@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -645,9 +645,25 @@ static inline int mdss_mdp_get_pixel_ram_size(struct mdss_data_type *mdata)
 						MDSS_MDP_PIXEL_RAM_SIZE : 0;
 }
 
+static inline bool mdss_mdp_ctl_is_power_off(struct mdss_mdp_ctl *ctl)
+{
+	return mdss_panel_is_power_off(ctl->power_state);
+}
+
+static inline bool mdss_mdp_ctl_is_power_on_interactive(
+	struct mdss_mdp_ctl *ctl)
+{
+	return mdss_panel_is_power_on_interactive(ctl->power_state);
+}
+
 static inline bool mdss_mdp_ctl_is_power_on(struct mdss_mdp_ctl *ctl)
 {
-	return (ctl->power_state != MDSS_PANEL_POWER_OFF);
+	return mdss_panel_is_power_on(ctl->power_state);
+}
+
+static inline bool mdss_mdp_ctl_is_power_on_lp(struct mdss_mdp_ctl *ctl)
+{
+	return mdss_panel_is_power_on_lp(ctl->power_state);
 }
 
 irqreturn_t mdss_mdp_isr(int irq, void *ptr);
