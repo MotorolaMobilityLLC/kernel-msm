f3b1920b2d964ec8c28ad8ccf8fd7ca7d98c73f0
Chao Yu <chao2.yu@samsung.com>
2014-08-04 09:54:58 +0800

    f2fs: invalidate xattr node page when evict inode
    When inode is evicted, all the page cache belong to this inode should be
    released including the xattr node page. But previously we didn't do this, this
    patch fixed this issue.
    
    v2:
     o reposition invalidate_mapping_pages() to the right place suggested by
    Jaegeuk Kim.
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -266,6 +266,7 @@ int f2fs_write_inode(struct inode *inode, struct writeback_control *wbc)
 void f2fs_evict_inode(struct inode *inode)
 {
 	struct f2fs_sb_info *sbi = F2FS_SB(inode->i_sb);
+	nid_t xnid = F2FS_I(inode)->i_xattr_nid;
 
 	trace_f2fs_evict_inode(inode);
 	truncate_inode_pages(&inode->i_data, 0);
@@ -293,6 +294,8 @@ void f2fs_evict_inode(struct inode *inode)
 
 no_delete:
 	invalidate_mapping_pages(NODE_MAPPING(sbi), inode->i_ino, inode->i_ino);
+	if (xnid)
+		invalidate_mapping_pages(NODE_MAPPING(sbi), xnid, xnid);
 	if (is_inode_flag_set(F2FS_I(inode), FI_APPEND_WRITE))
 		add_dirty_inode(sbi, inode->i_ino, APPEND_INO);
 	if (is_inode_flag_set(F2FS_I(inode), FI_UPDATE_WRITE))
