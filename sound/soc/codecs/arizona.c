fd1ff811371addadbaaa31a8e0bb7623b4be9d5d
Anil Kumar <Anil.Kumar@Wolfsonmicro.com>
2014-08-05 18:44:57 +0900

    ASoC: arizona: Add pdata for HPDET short level impedance
    Change-Id: I32eda56afe50344a1300463dedb27d8730b65b2e
    Signed-off-by: Anil Kumar <Anil.Kumar@Wolfsonmicro.com>
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    (cherry picked from commit 93315625bc112cd5be5e3a9f306097727c5fd31b)

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -1237,7 +1237,7 @@ int arizona_hp_ev(struct snd_soc_dapm_widget *w,
 
 	/* Force off if HPDET magic is active */
 	if (priv->arizona->hpdet_magic ||
-	    priv->arizona->hp_impedance <= ARIZONA_HP_SHORT_IMPEDANCE)
+	    priv->arizona->hp_impedance <= priv->arizona->pdata.hpdet_short_circuit_imp)
 		val = 0;
 
 	snd_soc_update_bits(w->codec, ARIZONA_OUTPUT_ENABLES_1, mask, val);
