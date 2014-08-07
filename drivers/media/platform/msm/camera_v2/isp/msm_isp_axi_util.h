7cb82c39c1e4095ebf76c8fc55ee672c6e5502b9
Hideki Yoshino <pgq674@motorola.com>
2014-08-07 16:20:11 -0500

    msm: camera: fix exposure flicker issue
    Configure epoch irq for half of active frame and report to
    mm-qcamera-daemon which is used to schedule trigger update in ISP.
    
    Change-Id: I3978c7818472b5084b6a1be7bf55b9d699e039b7
    Signed-off-by: Hideki Yoshino <pgq674@motorola.com>
    Reviewed-on: http://gerrit.mot.com/660932
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Kubicki <w17311@motorola.com>
    Reviewed-by: Sergey Kudakov <sergey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.h
@@ -63,4 +63,8 @@ void msm_isp_sof_notify(struct vfe_device *vfe_dev,
 void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
 	uint32_t irq_status0, uint32_t irq_status1,
 	struct msm_isp_timestamp *ts);
+
+void msm_isp_epoch_notify(struct vfe_device *vfe_dev,
+	enum msm_isp_epoch_idx epoch_idx);
+
 #endif /* __MSM_ISP_AXI_UTIL_H__ */
