26640013f7f47ffb49a8ea174e1bb44fa3d8be1e
Dmitry Ornatsky <cdo021c@motorola.com>
2014-08-07 14:45:06 -0500

    leds: bd65b60: add new driver for the ROHM IC
    This adds binding, eables building and has code of new
    driver for ROHM BD65B60GWL IC used by 8916 platform.
    Deleted PWM control available in 3.10 kernel version.
    
    Change-Id: I56c55162677a320e6d0f9bf73a1ef15562c3cfc1
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/661393
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    SLTApproved: Robert Neely <wrn002@motorola.com>

---
+++ include/linux/platform_data/leds-bd65b60.h
@@ -0,0 +1,54 @@
+/*
+* Simple driver for ROHM Semiconductor BD65B60GWL Backlight driver chip
+* Copyright (C) 2014 ROHM Semiconductor.com
+* Copyright (C) 2014 MMI
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
+enum bd65b60_ovp {
+	BD65B60_25V_OVP = 0x00,
+	BD65B60_30V_OVP = 0x08,
+	BD65B60_35V_OVP = 0x10,
+};
+
+enum bd65b60_ledsel {
+	BD65B60_DISABLE = 0x00,
+	BD65B60_LED1SEL = 0x01,
+	BD65B60_LED2SEL = 0x04,
+	BD65B60_LED12SEL = 0x05,
+};
+
+enum bd65b60_pwm_ctrl {
+	BD65B60_PWM_DISABLE = 0x06,
+	BD65B60_PWM_ENABLE = 0x38,
+};
+
+/*
+ *@init_level   : led a init brightness. 4~255
+ *@led_sel	: led rail enable/disable
+ *@ovp_val	: LED OVP Settings
+ *@pwm_period   : pwm period
+ *@pwm_ctrl     : pwm enable/disable
+ */
+struct bd65b60_platform_data {
+
+	/* led config.  */
+	int init_level;
+	enum bd65b60_ledsel led_sel;
+	enum bd65b60_ovp ovp_val;
+	const char *name;
+	const char *trigger;
+	bool default_on;
+	bool pwm_on;
+	/* pwm config. */
+	unsigned int pwm_period;
+	enum bd65b60_pwm_ctrl pwm_ctrl;
+};
