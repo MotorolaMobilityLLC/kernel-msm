68d1a4ef950c958634210007ec7e6e52a051144f
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-08-12 10:59:00 -0700

    Wlan: Release 3.2.3.398
    Release 3.2.3.398
    
    Change-Id: Ia571da81a91187438d581d9d90bbdadbe4928132
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            397
+#define QWLAN_VERSION_BUILD            398
 
-#define QWLAN_VERSIONSTR               "3.2.3.397"
+#define QWLAN_VERSIONSTR               "3.2.3.398"
 
 #endif /* QWLAN_VERSION_H */
