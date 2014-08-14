33554b8ac0a6af91185750cfb35a17524fa4d881
Piyush Gupta <guptapi@codeaurora.org>
2014-08-14 12:04:50 -0700

    Revert "msm: mdss: use helper functions to check for panel power state"
    This reverts commit 4fa6493f38b11d6a56dd9a9cf01ed486ebf37b1a.
    
    Change-Id: If74b131b3c0f57f53533f22130a1200087d3ec37
    Signed-off-by: Piyush Gupta <guptapi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_status.c
+++ drivers/video/msm/mdss/mdss_dsi_status.c
@@ -123,7 +123,7 @@ static void check_dsi_ctrl_status(struct work_struct *work)
 	if (ctl->shared_lock)
 		mutex_unlock(ctl->shared_lock);
 
-	if (mdss_fb_is_power_on_interactive(mfd)) {
+	if (mfd->panel_power_state == MDSS_PANEL_POWER_ON) {
 		if (ret > 0) {
 			schedule_delayed_work(&pdsi_status->check_status,
 				msecs_to_jiffies(pdsi_status->check_interval));
