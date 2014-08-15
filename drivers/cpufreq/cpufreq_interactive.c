2f1cb798c5e1e6ff8b547ca3b017601b2a5fbcb4
Omprakash Dhyade <odhyade@codeaurora.org>
2014-08-14 17:34:29 -0700

    cpufreq: interactive: Limit timer rearm to just once on limits change
    Currently when a CPU changes its limits, its timer is canceled and
    then rearmed from cpufreq_governor_interactive(). However this can
    lead to timer not getting to run for a long duration, for example
    due to rapid changes in scaling_min.
    This patch ensures that the cancelation and re-arming of timer is
    not done more than once before the timer function gets a chance to
    run.
    
    Change-Id: Ia34dcc05781db12d36a71f6a5dba4c2f3054269a
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    Signed-off-by: Omprakash Dhyade <odhyade@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -56,6 +56,7 @@ struct cpufreq_interactive_cpuinfo {
 	int governor_enabled;
 	int prev_load;
 	bool limits_changed;
+	unsigned int nr_timer_resched;
 };
 
 #define MIN_TIMER_JIFFIES 1UL
@@ -378,6 +379,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	if (!pcpu->governor_enabled)
 		goto exit;
 
+	pcpu->nr_timer_resched = 0;
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	now = update_load(data);
 	delta_time = (unsigned int)(now - pcpu->cputime_speedadj_timestamp);
@@ -1321,7 +1323,24 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			 */
 			del_timer_sync(&pcpu->cpu_timer);
 			del_timer_sync(&pcpu->cpu_slack_timer);
-			if (policy->min >= pcpu->target_freq) {
+
+			if (pcpu->nr_timer_resched) {
+				if (pcpu->policy->min >= pcpu->target_freq)
+					pcpu->target_freq = pcpu->policy->min;
+				/*
+				 * To avoid deferring load evaluation for a
+				 * long time rearm the timer for the same jiffy
+				 * as it was supposed to fire at, if it has
+				 * already been rescheduled once. The timer
+				 * start and rescheduling functions aren't used
+				 * here so that the timestamps used for load
+				 * calculations do not get reset.
+				 */
+				add_timer_on(&pcpu->cpu_timer, j);
+				if (timer_slack_val >= 0 && pcpu->target_freq >
+							pcpu->policy->min)
+					add_timer_on(&pcpu->cpu_slack_timer, j);
+			} else if (policy->min >= pcpu->target_freq) {
 				pcpu->target_freq = policy->min;
 				/*
 				 * Reschedule timer.
@@ -1329,6 +1348,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 				 * the load after changing policy parameters.
 				 */
 				cpufreq_interactive_timer_start(j, 0);
+				pcpu->nr_timer_resched++;
 			} else {
 				/*
 				 * Reschedule timer with variable duration.
@@ -1344,6 +1364,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 						  expire_time);
 
 				cpufreq_interactive_timer_start(j, expire_time);
+				pcpu->nr_timer_resched++;
 			}
 			pcpu->limits_changed = true;
 			up_write(&pcpu->enable_sem);
