3180e5bf73fd90842a64d3dd9ad293633212e959
Jack Pham <jackp@codeaurora.org>
2014-08-15 19:04:12 -0700

    usb: dwc3-msm: Disable clock gating always
    There is a known HW bug on the DWC3 MSM implementation that
    causes the internal RAM clock to get stuck when entering low
    power modes. The workaround for this is to disable internal
    clock gating always. As the DWC3_GCTL register will get reset
    when coming out of power collapse, make sure to save and
    restore the value during suspend and resume.
    
    Change-Id: Id5c7496fbc65f328c84e63bb0588c91fb226c1d7
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Signed-off-by: Ravikumar Vembu <a18394@motorola.com>
    Reviewed-on: http://gerrit.mot.com/690752
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -197,6 +197,7 @@ struct dwc3_msm {
 #define MDWC3_PHY_REF_AND_CORECLK_OFF	BIT(0)
 #define MDWC3_TCXO_SHUTDOWN		BIT(1)
 
+	u32 gctl_val;
 	u32 qscratch_ctl_val;
 	dev_t ext_chg_dev;
 	struct cdev ext_chg_cdev;
@@ -1008,6 +1009,13 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned event)
 		dwc->tx_fifo_size = mdwc->tx_fifo_size;
 		break;
 	case DWC3_CONTROLLER_POST_INITIALIZATION_EVENT:
+		/*
+		 * Workaround: Disable internal clock gating always, as there
+		 * is a known HW bug that causes the internal RAM clock to get
+		 * stuck when entering low power modes.
+		 */
+		dwc3_msm_write_reg_field(mdwc->base, DWC3_GCTL,
+					DWC3_GCTL_DSBLCLKGTNG, 1);
 		usb_phy_post_init(mdwc->ss_phy);
 	default:
 		dev_dbg(mdwc->dev, "unknown dwc3 event\n");
@@ -1388,6 +1396,9 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	/* make sure above writes are completed before turning off clocks */
 	wmb();
 
+	/* Save value of GCTL to rewrite upon resume */
+	mdwc->gctl_val = dwc3_msm_read_reg(mdwc->base, DWC3_GCTL);
+
 	/* remove vote for controller power collapse */
 	if (!host_bus_suspend)
 		dwc3_msm_config_gdsc(mdwc, 0);
@@ -1508,6 +1519,9 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 
 	if (resume_from_core_clk_off)
 		usb_phy_set_suspend(mdwc->ss_phy, 0);
+
+	dwc3_msm_write_reg(mdwc->base, DWC3_GCTL, mdwc->gctl_val);
+
 	atomic_set(&mdwc->in_lpm, 0);
 
 	/* match disable_irq call from isr */
