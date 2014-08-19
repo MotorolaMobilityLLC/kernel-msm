68570f3f04e80aab2d4f54b7d939f3235056674d
VijayKumar Gn <gxnf38@motorola.com>
2014-08-19 05:40:54 -0500

    arm: msm: 8226: devicetree clock overriding for 8x26
    Front camera in Thea P1B uses MCLK1 where as Thea P1A & Titan
    Uses MCLK0. Providing a facility to introduce Overriding logic
    
    Change-Id: Iac48eda90e7b74229d81d88991e7cce606c42f20
    Signed-off-by: VijayKumar Gn <gxnf38@motorola.com>
    Reviewed-on: http://gerrit.mot.com/663248
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Reviewed-by: Jian-Jun Fan <stevenfan@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/clock.c
+++ arch/arm/mach-msm/clock.c
@@ -27,6 +27,7 @@
 #include <trace/events/power.h>
 #include <mach/clk-provider.h>
 #include "clock.h"
+#include "mmi-clock.h"
 
 struct handoff_clk {
 	struct list_head list;
@@ -798,6 +799,8 @@ int __init msm_clock_init(struct clock_init_data *data)
 
 	if (data->pre_init)
 		data->pre_init();
+
+	mmi_msm_clock_init(data);
 
 	mutex_lock(&msm_clock_init_lock);
 	if (data->late_init)
