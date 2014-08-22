0fb3bec391b4c044f90b737ced7ac6375d600343
alan_lu <alan_lu@asus.com>
2014-08-22 15:26:32 +0800

    fugu: asustek: create sysfs node to control LED
    The LED sysfs as follows:
    
    switch on/off the LED:
            /sys/class/leds/white/device/led_enable
            0 = led disable, 1 = led enable
    
    modify frequency of the LED:
            /sys/class/leds/white/device/led_freq
            0 = 4sec, 1 = 2sec, 2 = 1sec, 3 = 0.5sec
    
    modify the LED lighting effect:
            /sys/class/leds/white/device/led_lighting_effect
            0 = led off, 1 = led on, 2 = blinking, 3 = breathing
    
    set the LED brightness:
            /sys/class/leds/white/brightness
            0~49 = 1mA, 50~149 = 2.5mA, 150~199 = 5mA, else = 10mA
    
    Bug: 16846098
    Signed-off-by: alan_lu <alan_lu@asus.com>

--- arch/x86/platform/intel-mid/fugu/fugu_led.c
+++ arch/x86/platform/intel-mid/fugu/fugu_led.c
@@ -18,6 +18,7 @@
 #include <asm/intel_scu_pmic.h>
 #include <asm/intel_mid_rpmsg.h>
 
+#include <linux/leds.h>
 
 /* Charger LED Control Registr Definition */
 #define CHRLEDCTRL_REG 0x53
@@ -28,6 +29,208 @@
 
 static struct rpmsg_instance *pmic_instance;
 
+static int fugu_led_level(enum led_brightness value)
+{
+	if ((value >= 0) && (value < 50))
+		return 1;
+	else if ((value >= 50) && (value < 150))
+		return 2;
+	else if ((value >= 150) && (value < 200))
+		return 3;
+	else
+		return 0;
+}
+
+static u8 is_enabled(void)
+{
+	u8 data;
+
+	intel_scu_ipc_ioread8(CHRLEDCTRL_REG, &data);
+
+	data &= BIT(1);
+
+	return !!data;
+}
+
+static void fugu_led_enable(bool enable)
+{
+	u8 data;
+
+	intel_scu_ipc_ioread8(CHRLEDCTRL_REG, &data);
+
+	if (enable)
+		data |= BIT(1);
+	else
+		data &= ~BIT(1);
+
+	intel_scu_ipc_iowrite8(CHRLEDCTRL_REG, data);
+}
+
+static void fugu_led_brightness_set(struct led_classdev *led_cdev,
+			     enum led_brightness value)
+{
+	s32 level;
+	u8 data;
+
+	level = fugu_led_level(value);
+
+	intel_scu_ipc_ioread8(CHRLEDCTRL_REG, &data);
+	data &= ~(BIT(3) | BIT(2));
+	data |= (level << 2);
+	intel_scu_ipc_iowrite8(CHRLEDCTRL_REG, data);
+	led_cdev->brightness = value;
+}
+
+static enum led_brightness
+fugu_led_brightness_get(struct led_classdev *led_cdev)
+{
+	return led_cdev->brightness;
+}
+
+static int fugu_led_freq_set(u8 freq)
+{
+	u8 data;
+
+	if (freq > 3)
+		return -EINVAL;
+
+	intel_scu_ipc_ioread8(CHRLEDCTRL_REG, &data);
+	data &= ~(BIT(5) | BIT(4));
+	data |= (freq << 4);
+	intel_scu_ipc_iowrite8(CHRLEDCTRL_REG, data);
+
+	return 0;
+}
+
+static u8 fugu_led_freq_get(void)
+{
+	u8 data;
+
+	intel_scu_ipc_ioread8(CHRLEDCTRL_REG, &data);
+	data = data | (BIT(5) | BIT(4));
+	data >>= 4;
+
+	return data;
+}
+
+static int fugu_led_set_lighting_effect(u8 lighting_effect)
+{
+	u8 data;
+	if (lighting_effect > 3)
+		return -EINVAL;
+
+	intel_scu_ipc_ioread8(CHRLEDFSM_REG, &data);
+	data &= ~(BIT(2) | BIT(1));
+	data |= (lighting_effect << 1);
+	intel_scu_ipc_iowrite8(CHRLEDFSM_REG, data);
+
+	return 0;
+}
+
+static u8 fugu_led_get_lighting_effect(void)
+{
+	u8 data;
+
+	intel_scu_ipc_ioread8(CHRLEDFSM_REG, &data);
+	data = data | (BIT(2) | BIT(1));
+	data >>= 1;
+
+	return data;
+}
+
+static ssize_t fugu_led_freq_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	u8 freq;
+
+	freq = fugu_led_freq_get();
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", freq);
+}
+
+static ssize_t fugu_led_freq_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	unsigned long val;
+	int ret;
+
+	ret = kstrtoul(buf, 10, &val);
+	if (ret)
+		return ret;
+
+	ret = fugu_led_freq_set(val);
+	if (ret)
+		dev_err(dev, "fail to set freq\n");
+	 else
+		ret = size;
+
+	return ret;
+}
+
+static ssize_t fugu_led_enable_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%s\n", is_enabled() ? "enabled" : "disabled");
+}
+
+static ssize_t fugu_led_enable_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	unsigned long val;
+	int ret;
+
+	ret = kstrtoul(buf, 10, &val);
+	if (ret)
+		return ret;
+
+	fugu_led_enable(!!val);
+
+	return size;
+}
+
+static ssize_t fugu_led_lighting_effect_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	u8 lighting_effect;
+
+	lighting_effect = fugu_led_get_lighting_effect();
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", lighting_effect);
+}
+
+static ssize_t fugu_led_lighting_effect_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	unsigned long val;
+	int ret;
+
+	ret = kstrtoul(buf, 10, &val);
+	if (ret)
+		return ret;
+
+	ret = fugu_led_set_lighting_effect(val);
+	if (ret)
+		dev_err(dev, "fail to set lighting effect\n");
+	else
+		ret = size;
+
+	return ret;
+}
+
+static DEVICE_ATTR(led_freq, 0644, fugu_led_freq_show, fugu_led_freq_store);
+static DEVICE_ATTR(led_enable, 0644,
+	fugu_led_enable_show, fugu_led_enable_store);
+static DEVICE_ATTR(led_lighting_effect, 0644,
+	fugu_led_lighting_effect_show, fugu_led_lighting_effect_store);
+
+static struct led_classdev fugu_white_led = {
+	.name			= "white",
+	.brightness		= LED_OFF,
+	.max_brightness	= LED_FULL,
+	.brightness_set		= fugu_led_brightness_set,
+	.brightness_get		= fugu_led_brightness_get,
+};
+
 void led_init(void)
 {
 	/* LED is software controll and enabled */
@@ -36,8 +239,10 @@ void led_init(void)
 	/* LED lighting effect: Breathing varies Duty Cycle */
 	intel_scu_ipc_iowrite8(CHRLEDFSM_REG, 6);
 
-	/* LED PWM: 40/256. There are a total of 256 duty cycle steps */
-	intel_scu_ipc_iowrite8(CHRLEDPWM_REG, 40);
+	/* LED PWM: 256/256. There are a total of 256 duty cycle steps */
+	intel_scu_ipc_iowrite8(CHRLEDPWM_REG, 0xFF);
+
+	fugu_led_brightness_set(&fugu_white_led, 0xFF);
 }
 
 static int fugu_led_probe(struct rpmsg_channel *rpdev)
@@ -60,6 +265,24 @@ static int fugu_led_probe(struct rpmsg_channel *rpdev)
 	}
 	/* Initialize rpmsg instance */
 	init_rpmsg_instance(pmic_instance);
+
+	ret = led_classdev_register(&rpdev->dev, &fugu_white_led);
+	if (ret < 0) {
+		dev_err(&rpdev->dev, "fail to register led class.");
+		goto out;
+	}
+
+	ret = device_create_file(&rpdev->dev, &dev_attr_led_freq);
+	if (ret)
+		dev_err(&rpdev->dev, "failed device_create_file(led_freq)\n");
+
+	ret = device_create_file(&rpdev->dev, &dev_attr_led_enable);
+	if (ret)
+		dev_err(&rpdev->dev, "failed device_create_fiaild(led_enable)\n");
+
+	ret = device_create_file(&rpdev->dev, &dev_attr_led_lighting_effect);
+	if (ret)
+		dev_err(&rpdev->dev, "failed device_create_file(led_lighting_effect)\n");
 
 	/* Initialize led */
 	led_init();
