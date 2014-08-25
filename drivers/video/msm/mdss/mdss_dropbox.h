81fd7c35e4d6f86b8647b078e0bbe973b9b0d90d
Zac Crosby <zac@motorola.com>
2014-08-25 15:36:31 -0500

    msm: mdss: Add display_issue dropbox utility function
    Add a common utility function for reporting display_issue bug2gos.
    
    (cherry picked from commit 610cb18403f241029d5af57bc5d9d4d28dc78dea)
    Signed-off-by: Zac Crosby <zac@motorola.com>
    
    Change-Id: I762516cc1425baa6760f9db03800d2c89805fbe0
    Reviewed-on: http://gerrit.mot.com/784189
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>

---
+++ drivers/video/msm/mdss/mdss_dropbox.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2014 - 2015, Motorola Mobility LLC. All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 and
+* only version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+#ifndef MDSS_DROPBOX_H
+#define MDSS_DROPBOX_H
+
+#define MDSS_DROPBOX_MSG_ESD "ESD event detected"
+#define MDSS_DROPBOX_MSG_PWR_MODE_BLACK "PWR_MODE black screen detected"
+
+void mdss_dropbox_report_event(char *msg, int count);
+#endif /* MDSS_DROPBOX_H */
