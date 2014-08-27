7a8af21f31cbad51cb8c1f6cf0564201b0f19376
Anton Mazur <cam094c@motorola.com>
2014-08-27 15:52:40 -0500

    8916:msm: restart: Update AP panic bit
    Motorola definition of AP panic bit set in extra info pmic reg
    conflicts with QC definition of Warm reboot bit set in extra info reg
    move Mot bit to available position to avoid conflict
    
    Change-Id: I23baf67ba9eda1da7c3f2f2f67e08aa7b81ff373
    Signed-off-by: Anton Mazur <cam094c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/666663
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dong-Hua Yan <a18317@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Change-Id: Iea1862bf415dad258f2fda833366eaebe8c56f30
    Reviewed-on: http://gerrit.mot.com/783401
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -344,7 +344,7 @@ static void msm_restart_prepare(const char *cmd)
 			__raw_writel(0x77665501, restart_reason);
 		}
 	} else if (in_panic == 1) {
-		qpnp_pon_store_extra_reset_info(RESET_EXTRA_PANIC_REASON, 1);
+		qpnp_pon_store_extra_reset_info(RESET_EXTRA_PANIC_REASON, 0xFF);
 	}
 
 	flush_cache_all();
