42e24607fa2d5dbf8de8e98dbf8b62e130ff6aea
Wesley Ribeiro <wwr004@motorola.com>
2014-08-27 15:32:12 -0700

    module: add self integrity check
    Add support in kernel to allow kernel modules to perform self-integrity
    checks thru configuration option CONFIG_MODULE_EXTRA_COPY.
    
    Port of (CR)
    Reference gerrit: http://gerrit.mot.com/834976
    
    Change-Id: I3978da2d94d3d3983d2a2311137fafba437b37ce
    
    Change-Id: I59fbe018a88d0d4eac6a567f89a5b52404cfee04
    Signed-off-by: Wesley Ribeiro <wwr004@motorola.com>
    Reviewed-on: http://gerrit.mot.com/834976
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1033693
    SLTApproved: Slta Waiver
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1047742
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- kernel/module.c
+++ kernel/module.c
@@ -3398,6 +3398,58 @@ static void do_free_init(struct rcu_head *head)
 	kfree(m);
 }
 
+
+#ifdef CONFIG_MODULE_EXTRA_COPY
+/* Make an extra copy of the module. */
+static int make_extra_copy(Elf_Ehdr *elf_hdr, unsigned long elf_len,
+			   void **extra_copy)
+{
+	void *dest = *extra_copy = vmalloc(elf_len);
+
+	if (dest == NULL)
+		return -ENOMEM;
+	memcpy(dest, elf_hdr, elf_len);
+	return 0;
+}
+
+/*
+ * Keep the linked copy as well as the raw copy, in case the
+ * module wants to inspect both.
+ */
+static int keep_extra_copy_info(struct module *mod, void *extra_copy,
+				Elf_Ehdr *elf_hdr, unsigned long elf_len)
+{
+	mod->raw_binary_ptr = extra_copy;
+	mod->raw_binary_size = elf_len;
+	mod->linked_binary_ptr = elf_hdr;
+	mod->linked_binary_size = elf_len;
+	return 1;
+}
+
+/* Release module extra copy information. */
+static void cleanup_extra_copy_info(struct module *mod)
+{
+	vfree(mod->raw_binary_ptr);
+	vfree(mod->linked_binary_ptr);
+	mod->raw_binary_ptr = mod->linked_binary_ptr = NULL;
+	mod->raw_binary_size = mod->linked_binary_size = 0;
+}
+#else/* !CONFIG_MODULE_EXTRA_COPY */
+static inline int make_extra_copy(Elf_Ehdr *elf_hdr, unsigned long elf_len,
+				  void **extra_copy)
+{
+	*extra_copy = NULL;
+	return 0;
+}
+static inline int keep_extra_copy_info(struct module *mod, void *extra_copy,
+				       Elf_Ehdr *elf_hdr,
+				       unsigned long elf_len)
+{
+	return 0;
+}
+static inline void cleanup_extra_copy_info(struct module *mod) { }
+#endif/* CONFIG_MODULE_EXTRA_COPY */
+
 /*
  * This is where the real work happens.
  *
@@ -3426,6 +3478,7 @@ static noinline int do_init_module(struct module *mod)
 	/* Start the module */
 	if (mod->init != NULL)
 		ret = do_one_initcall(mod->init);
+	cleanup_extra_copy_info(mod);
 	if (ret < 0) {
 		goto fail_free_freeinit;
 	}
@@ -3632,6 +3685,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err;
 	char *after_dashes;
+	void *extra_copy = NULL;
 
 	err = module_sig_check(info, flags);
 	if (err)
@@ -3641,11 +3695,16 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	if (err)
 		goto free_copy;
 
+	/* Make extra copy of the module, if needed. */
+	err = make_extra_copy(info->hdr, info->len, &extra_copy);
+	if (err)
+		goto free_copy;
+
 	/* Figure out module layout, and allocate all the memory. */
 	mod = layout_and_allocate(info, flags);
 	if (IS_ERR(mod)) {
 		err = PTR_ERR(mod);
-		goto free_copy;
+		goto free_extra_copy;
 	}
 
 	/* Reserve our place in the list. */
@@ -3747,8 +3806,11 @@ static int load_module(struct load_info *info, const char __user *uargs,
 			goto sysfs_cleanup;
 	}
 
-	/* Get rid of temporary copy. */
-	free_copy(info);
+	/* Keep extra copy information, if needed. */
+	if (!keep_extra_copy_info(mod, extra_copy, info->hdr, info->len)) {
+		/* Get rid of temporary copy. */
+		free_copy(info);
+	}
 
 	/* Done! */
 	trace_module_load(mod);
@@ -3801,6 +3863,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	lockdep_free_key_range(mod->core_layout.base, mod->core_layout.size);
 
 	module_deallocate(mod, info);
+ free_extra_copy:
+	vfree(extra_copy);
  free_copy:
 	free_copy(info);
 	return err;
