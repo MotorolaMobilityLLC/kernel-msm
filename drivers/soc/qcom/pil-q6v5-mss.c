395b0c5a7355034a78704ddfc08ec65fd72c26e5
Bharat Srinivasan <bharatsrinivasan@motorola.com>
2014-08-27 07:18:10 -0500

    ASoC: pil-q6v5-mss: Export modem ssr reason.
    port from (CR)
    
    Export modem ssr reason to userspace. The Modem panic reason is
    needed by the user space daemon that collects the ramdumps and
    posts it to the native dropbox for Bug2Go.
    
    Change-Id: If10a49e9938db44ba4586d1ae14941ad8e0e1b4f
    Signed-off-by: Bharat Srinivasan <bharatsrinivasan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/666479
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/787269
    SME-Granted: SME Approvals Granted
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/788570
    Reviewed-on: https://gerrit.mot.com/947383
    
    
    Change-Id: I4404a822ed597a78339d9046cb87b0ca6c6deb5d
    Reviewed-on: https://gerrit.mot.com/1291242
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -44,10 +44,14 @@
 
 #define subsys_to_drv(d) container_of(d, struct modem_data, subsys_desc)
 
+static char pil_ssr_reason[MAX_SSR_REASON_LEN];
+static char *ssr_reason = pil_ssr_reason;
+module_param(ssr_reason, charp, S_IRUGO);
+
 static void log_modem_sfr(void)
 {
 	u32 size;
-	char *smem_reason, reason[MAX_SSR_REASON_LEN];
+	char *smem_reason;
 
         struct mmi_unit_info * mui = (struct mmi_unit_info *) smem_alloc(SMEM_KERNEL_RESERVE,
 		SMEM_KERNEL_RESERVE_SIZE, 0, SMEM_ANY_HOST_FLAG);
@@ -67,8 +71,9 @@ static void log_modem_sfr(void)
 		return;
 	}
 
-	strlcpy(reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
-	pr_err("modem subsystem failure reason: %s.\n", reason);
+	strlcpy(pil_ssr_reason, smem_reason, min((size_t)size, sizeof(pil_ssr_reason)));
+	pr_err("modem subsystem failure reason: %s.\n", pil_ssr_reason);
+
 }
 
 static void restart_modem(struct modem_data *drv)
