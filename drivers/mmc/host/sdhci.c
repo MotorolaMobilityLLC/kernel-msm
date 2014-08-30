b1f6e2778b4097771a7ccd8180c027571b471e82
Chanho Min <chanho.min@lge.com>
2014-08-30 12:40:40 +0900

    mmc: sdhci: handle busy-end interrupt during command
    It is fully legal for a controller to start handling busy-end interrupt
    before it has signaled that the command has completed. So make sure
    we do things in the proper order, Or it results that command interrupt
    is ignored so it can cause unexpected operations. This is founded at some
    toshiba emmc with the bellow warning.
    
    "mmc0: Got command interrupt 0x00000001 even though
    no command operation was in progress."
    
    This issue has been also reported by Youssef TRIKI:
    It is not specific to Toshiba devices, and happens with eMMC devices
    as well as SD card which support Auto-CMD12 rather than CMD23.
    
    Also, similar patch is submitted by:
    Gwendal Grignou <gwendal@chromium.org>
    
    Changes since v1:
     Fixed conflict with the next of git.linaro.org/people/ulf.hansson/mmc.git
     and Tested if issue is fixed again.
    
    Change-Id: Iccdabc180ef0fa2c2cbf965ac4fd869970699729
    Signed-off-by: Hankyung Yu <hankyung.yu@lge.com>
    Signed-off-by: Chanho Min <chanho.min@lge.com>
    Tested-by: Youssef TRIKI <youssef.triki@st.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/827534
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1219,6 +1219,7 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 				(msecs_to_jiffies(cmd->cmd_timeout_ms * 2)));
 
 	host->cmd = cmd;
+	host->busy_handle = 0;
 
 	sdhci_prepare_data(host, cmd);
 
@@ -3106,8 +3107,12 @@ static void sdhci_cmd_irq(struct sdhci_host *host, u32 intmask)
 		if (host->cmd->data)
 			DBG("Cannot wait for busy signal when also "
 				"doing a data transfer");
-		else if (!(host->quirks & SDHCI_QUIRK_NO_BUSY_IRQ))
+		else if (!(host->quirks & SDHCI_QUIRK_NO_BUSY_IRQ)
+				&& !host->busy_handle) {
+			/* Mark that command complete before busy is ended */
+			host->busy_handle = 1;
 			return;
+		}
 
 		/* The controller does not support the end-of-busy IRQ,
 		 * fall through and take the SDHCI_INT_RESPONSE */
@@ -3177,7 +3182,15 @@ static void sdhci_data_irq(struct sdhci_host *host, u32 intmask)
 		 */
 		if (host->cmd && (host->cmd->flags & MMC_RSP_BUSY)) {
 			if (intmask & SDHCI_INT_DATA_END) {
-				sdhci_finish_command(host);
+				/*
+				 * Some cards handle busy-end interrupt
+				 * before the command completed, so make
+				 * sure we do things in the proper order.
+				 */
+				if (host->busy_handle)
+					sdhci_finish_command(host);
+				else
+					host->busy_handle = 1;
 				return;
 			}
 			if (host->quirks2 &
