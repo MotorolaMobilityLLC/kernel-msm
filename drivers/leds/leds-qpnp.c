3cf0c8b3533111843e4c5fac15692d7c2bf1e488
Dmitry Ornatsky <cdo021c@motorola.com>
2014-09-02 17:37:57 -0500

    leds: leds-qpnp: add charging led control
    Add charging led control to the leds-qpnp driver. Need
    to have it to test PMIC control path for the charging LED.
    
    Change-Id: Ie91010692754c6824f8e9a7eb04a6334a1ccfa94
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/667685
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dmitry Ornatsky <ornatsky@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/leds/leds-qpnp.c
+++ drivers/leds/leds-qpnp.c
@@ -250,6 +250,8 @@
 #define NUM_KPDBL_LEDS			4
 #define KPDBL_MASTER_BIT_INDEX		0
 
+#define ATC_LED_CTRL(base)		(base + 0x4D)
+
 /**
  * enum qpnp_leds - QPNP supported led ids
  * @QPNP_ID_WLED - White led backlight
@@ -264,6 +266,7 @@ enum qpnp_leds {
 	QPNP_ID_LED_MPP,
 	QPNP_ID_KPDBL,
 	QPNP_ID_LED_GPIO,
+	QPNP_ID_ATC,
 	QPNP_ID_MAX,
 };
 
@@ -858,6 +861,19 @@ static int qpnp_wled_set(struct qpnp_led_data *led)
 		return rc;
 	}
 	return 0;
+}
+
+static int qpnp_atc_set(struct qpnp_led_data *led)
+{
+	int rc;
+	u8 val;
+	val = (led->cdev.brightness) ? 1 : 0;
+	rc = spmi_ext_register_writel(led->spmi_dev->ctrl,
+		led->spmi_dev->sid, ATC_LED_CTRL(led->base), &val, 1);
+	if (rc)
+		dev_err(&led->spmi_dev->dev,
+			"ATC LED set failed(%d)\n", rc);
+	return rc;
 }
 
 static int qpnp_mpp_set(struct qpnp_led_data *led)
@@ -1828,6 +1844,9 @@ static void __qpnp_led_work(struct qpnp_led_data *led,
 		mutex_lock(&led->lock);
 
 	switch (led->id) {
+	case QPNP_ID_ATC:
+		qpnp_atc_set(led);
+		break;
 	case QPNP_ID_WLED:
 		rc = qpnp_wled_set(led);
 		if (rc < 0)
@@ -1892,6 +1911,9 @@ static void qpnp_led_work(struct work_struct *work)
 static int qpnp_led_set_max_brightness(struct qpnp_led_data *led)
 {
 	switch (led->id) {
+	case QPNP_ID_ATC:
+		/* Do nothing. Needed to have valid id check */
+		break;
 	case QPNP_ID_WLED:
 		led->cdev.max_brightness = WLED_MAX_LEVEL;
 		break;
@@ -3025,6 +3047,9 @@ static int qpnp_led_initialize(struct qpnp_led_data *led)
 	int rc = 0;
 
 	switch (led->id) {
+	case QPNP_ID_ATC:
+		/* Do nothing. Needed to have valid id check */
+		break;
 	case QPNP_ID_WLED:
 		rc = qpnp_wled_init(led);
 		if (rc)
@@ -3972,6 +3997,8 @@ static int qpnp_leds_probe(struct spmi_device *spmi)
 						"Unable to read gpio config data\n");
 				goto fail_id_check;
 			}
+		} else if (strncmp(led_label, "atc", sizeof("atc")) == 0) {
+			/* Do nothing. Needed to have valid id check */
 		} else if (strncmp(led_label, "kpdbl", sizeof("kpdbl")) == 0) {
 			bitmap_zero(kpdbl_leds_in_use, NUM_KPDBL_LEDS);
 			is_kpdbl_master_turn_on = false;
