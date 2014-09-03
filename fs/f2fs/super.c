0b00c8d55bd5d6e4693612f22f73da4dbfd5f10e
Jaegeuk Kim <jaegeuk@kernel.org>
2014-09-02 17:19:04 -0700

    f2fs: need fsck.f2fs if the recovery was failed
    If the roll-forward recovery was failed, we'd better conduct fsck.f2fs.
    
    Change-Id: Ifc55055a0d1bd4cd7a216848556984618a285b76
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Reviewed-on: http://gerrit.mot.com/668425
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Reviewed-by: Jaegeuk Kim <jaegeuk@motorola.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1148,6 +1148,9 @@ try_onemore:
 	if (err)
 		goto free_proc;
 
+	if (!retry)
+		sbi->need_fsck = true;
+
 	/* recover fsynced data */
 	if (!test_opt(sbi, DISABLE_ROLL_FORWARD)) {
 		err = recover_fsync_data(sbi);
