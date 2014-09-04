c957268ffc859815d16e76c15d35b7a6d45877c1
Jaegeuk Kim <jaegeuk@motorola.com>
2014-09-04 16:09:34 -0700

    f2fs: remove ANDROID_EMU
    This patch removes the ANDROID_EMU infra.
    And, fix some minor coding styles.
    
    Change-Id: I652f5a7804dd1c54a42d4533774e0dbf711e8249
    Signed-off-by: Jaegeuk Kim <jaegeuk@motorola.com>
    Reviewed-on: http://gerrit.mot.com/668429
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Christopher Fries <cfries@motorola.com>
    Tested-by: Christopher Fries <cfries@motorola.com>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -42,20 +42,6 @@ static struct inode *f2fs_new_inode(struct inode *dir, umode_t mode)
 	}
 	f2fs_unlock_op(sbi);
 
-	if (IS_ANDROID_EMU(sbi, F2FS_I(dir), F2FS_I(dir)))
-		f2fs_android_emu(sbi, inode, &inode->i_uid,
-				 &inode->i_gid, &mode);
-	else {
-		inode->i_uid = current_fsuid();
-
-		if (dir->i_mode & S_ISGID) {
-			inode->i_gid = dir->i_gid;
-			if (S_ISDIR(mode))
-				mode |= S_ISGID;
-		} else {
-			inode->i_gid = current_fsgid();
-		}
-	}
 	inode_init_owner(inode, dir, mode);
 
 	inode->i_ino = ino;
