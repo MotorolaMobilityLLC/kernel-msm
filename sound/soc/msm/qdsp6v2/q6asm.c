f14caf2d9a9ee12898724b86644494a8ca9058bc
Linux Build Service Account <lnxbuild@localhost>
2014-09-05 22:37:08 -0700

    Merge "ASoC: msm: Add Buffer overflow check"
    Conflicts:
    	sound/soc/msm/qdsp6v2/q6asm.c
    (cherry picked from commit ad09dc54de4c958cafcf386d1f58421e41b5f841)
    
    Change-Id: Ide07012f40463a08f8c90d3ce1601b4510924ff1

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -53,6 +53,7 @@ enum {
 	ASM_MAX_CAL_TYPES
 };
 
+#define FRAME_NUM   (8)
 /* TODO, combine them together */
 static DEFINE_MUTEX(session_lock);
 struct asm_mmap {
@@ -1051,6 +1052,8 @@ int q6asm_audio_client_buf_alloc(unsigned int dir,
 			pr_debug("%s: buffer already allocated\n", __func__);
 			return 0;
 		}
+		if (bufcnt != FRAME_NUM)
+			goto fail;
 		mutex_lock(&ac->cmd_lock);
 		buf = kzalloc(((sizeof(struct audio_buffer))*bufcnt),
 				GFP_KERNEL);
