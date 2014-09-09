b77e0d1eb3e8609316f05abba2cf82c673260850
Hong-Mei Li <a21834@motorola.com>
2014-09-09 11:13:18 -0700

    trace: memkill: trace LMK and OOM kills
    How to enable tracer:
    
      # mount -t debugfs nodev /sys/kernel/debug
      # echo 1 > /sys/kernel/debug/tracing/tracing_on
      # echo 1 > /sys/kernel/debug/tracing/events/memkill/enable
      # cat /sys/kernel/debug/tracing/trace
    
    Event format:
    
      oom_kill: <pid> <comm> <total_vm> <anon_rss> <file_rss> <oom_score_adj> <order> <points> <cached> <freeswap>
      oom_kill_shared: <pid> <comm>
      lmk_kill: <pid> <comm> <selected_oom_adj> <selected_tasksize> <min_adj> <cached> <freeswap>
    
    For LMK, we  considers memory status per zone. And appends gfp mask and
    per-zone free/file page state to LMK kill tracing. A field
    <Node_id>:<Zone_id>:<Nr_free>:<Nr_page> is printed for each zone.
    
    Also, we guarantees lmk trace log to always have fixed number of columns.
    
    (cherry picked from commit 8bc7dc4d892f140163f50824c9ae3479f01033b1)
    (cherry picked from commit 86111e237a69874f8d55608f82087c2bc7b9ed3d)
    (cherry picked from commit 34acdf16bbe3544c91ab13f04665ae579b59c80d)
    
    Change-Id: I46a2b4301c2aaed621ac95b66bcc029c3e5c695a
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-on: http://gerrit.mot.com/650046
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/669139
    Reviewed-on: http://gerrit.mot.com/727654
    SME-Granted: SME Approvals Granted

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -38,6 +38,7 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/oom.h>
+#include <trace/events/memkill.h>
 
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
@@ -501,6 +502,11 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 		task_pid_nr(victim), victim->comm, K(victim->mm->total_vm),
 		K(get_mm_counter(victim->mm, MM_ANONPAGES)),
 		K(get_mm_counter(victim->mm, MM_FILEPAGES)));
+	trace_oom_kill(
+		task_pid_nr(victim), victim->comm, K(victim->mm->total_vm),
+		K(get_mm_counter(victim->mm, MM_ANONPAGES)),
+		K(get_mm_counter(victim->mm, MM_FILEPAGES)),
+		victim->signal->oom_score_adj, order, victim_points);
 	task_unlock(victim);
 
 	/*
@@ -521,6 +527,8 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 
 			task_lock(p);	/* Protect ->comm from prctl() */
 			pr_err("Kill process %d (%s) sharing same memory\n",
+				task_pid_nr(p), p->comm);
+			trace_oom_kill_shared(
 				task_pid_nr(p), p->comm);
 			task_unlock(p);
 			do_send_sig_info(SIGKILL, SEND_SIG_FORCED, p, true);
