b625e1bd695855111fbddfe2445fbda0f20eac1b
Paul Moore <pmoore@redhat.com>
2014-09-10 17:09:57 -0400

    selinux: make the netif cache namespace aware
    While SELinux largely ignores namespaces, for good reason, there are
    some places where it needs to at least be aware of namespaces in order
    to function correctly.  Network namespaces are one example.  Basic
    awareness of network namespaces are necessary in order to match a
    network interface's index number to an actual network device.
    
    This patch corrects a problem with network interfaces added to a
    non-init namespace, and can be reproduced with the following commands:
    
     [NOTE: the NetLabel configuration is here only to active the dynamic
            networking controls ]
    
     # netlabelctl unlbl add default address:0.0.0.0/0 \
       label:system_u:object_r:unlabeled_t:s0
     # netlabelctl unlbl add default address:::/0 \
       label:system_u:object_r:unlabeled_t:s0
     # netlabelctl cipsov4 add pass doi:100 tags:1
     # netlabelctl map add domain:lspp_test_netlabel_t \
       protocol:cipsov4,100
    
     # ip link add type veth
     # ip netns add myns
     # ip link set veth1 netns myns
     # ip a add dev veth0 10.250.13.100/24
     # ip netns exec myns ip a add dev veth1 10.250.13.101/24
     # ip l set veth0 up
     # ip netns exec myns ip l set veth1 up
    
     # ping -c 1 10.250.13.101
     # ip netns exec myns ping -c 1 10.250.13.100
    
    Reported-by: Jiri Jaburek <jjaburek@redhat.com>
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    (cherry picked from commit 872ecde0211c30e7ef8cdf13ee0600bbb43fed75)
    
    Change-Id: I3d50b9eee8b6f70fe7b5c587084cba2d335f5075

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -26,6 +26,7 @@
 #include <linux/spinlock.h>
 #include "flask.h"
 #include "avc.h"
+#include <net/net_namespace.h>
 
 struct task_security_struct {
 	u32 osid;		/* SID prior to last execve */
@@ -78,6 +79,7 @@ struct ipc_security_struct {
 };
 
 struct netif_security_struct {
+	struct net *ns;			/* network namespace */
 	int ifindex;			/* device index */
 	u32 sid;			/* SID for this interface */
 };
