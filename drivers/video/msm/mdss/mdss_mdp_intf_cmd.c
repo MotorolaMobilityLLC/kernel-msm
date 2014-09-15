b82c3c0d33bcbda1c5cbc306f148f9d57571f764
Patrick Auchter <auchter@motorola.com>
2014-09-15 14:49:28 -0500

    Revert "Revert "msm: mdss: Configure TE GPIO using gpiomux framework""
    This change can be added back now that we are properly configuring the
    TE GPIO using pinctrl.
    
    This reverts commit 9a40eef910188c7a2bf2b5876d9fe30dedad404a.
    
    Change-Id: I1e76474f631d797647b698fa0e9de1847596e059
    Signed-off-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-on: http://gerrit.mot.com/670802
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -594,7 +594,6 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 			if (!ctx->pp_timeout_report_cnt) {
 				WARN(1, "cmd kickoff timed out (%d) ctl=%d\n",
 						rc, ctl->num);
-				mdss_dsi_debug_check_te(pdata);
 				MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0", "dsi1",
 						"edp", "hdmi", "panic");
 			}
