6a48d63620516460e479ac99db3ce73001e75742
Shuzhen Wang <shuzhenw@codeaurora.org>
2014-09-21 21:41:09 -0700

    msm: camera: isp: Avoid CGC override in ISP
    In ISP, avoid CGC override to allow hardware to take care of
    clock gating. This helps to save power.
    
    Bug: 17203093
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
    Signed-off-by: Shuzhen Wang <shuzhenw@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -147,6 +147,8 @@ struct msm_vfe_axi_ops {
 	uint32_t (*get_comp_mask) (uint32_t irq_status0, uint32_t irq_status1);
 	uint32_t (*get_pingpong_status) (struct vfe_device *vfe_dev);
 	long (*halt) (struct vfe_device *vfe_dev);
+	void (*update_cgc_override) (struct vfe_device *vfe_dev,
+		uint8_t wm_idx, uint8_t cgc_override);
 };
 
 struct msm_vfe_core_ops {
@@ -199,6 +201,9 @@ struct msm_vfe_stats_ops {
 	uint32_t (*get_wm_mask) (uint32_t irq_status0, uint32_t irq_status1);
 	uint32_t (*get_comp_mask) (uint32_t irq_status0, uint32_t irq_status1);
 	uint32_t (*get_pingpong_status) (struct vfe_device *vfe_dev);
+
+	void (*update_cgc_override) (struct vfe_device *vfe_dev,
+		uint32_t stats_mask, uint8_t enable);
 };
 
 struct msm_vfe_ops {
