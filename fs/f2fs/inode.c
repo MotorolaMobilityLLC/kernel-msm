39f1e82fd24049cb66eeb24f8c1539372779e20e
Jaegeuk Kim <jaegeuk@kernel.org>
2014-09-25 21:54:45 -0700

    f2fs: support atomic_write feature for database
    This patch introduces a very limited functionality for atomic write support.
    In order to support atomic write, this patch adds two ioctls:
     o F2FS_IOC_ATOMIC_OPEN
     o F2FS_IOC_ATOMIC_COMMIT
    
    The database engine should be aware of the following sequence.
    1. open
     : ioctl(F2FS_IOC_ATOMIC_OPEN);
    2. writes
      -> all the written data will be treated as atomic pages.
    3. commit
     : ioctl(F2FS_IOC_ATOMIC_COMMIT);
      -> this flushes all the data blocks to the disk, which will be shown all or
      nothing by f2fs recovery procedure.
    
    The IO pattens should be:
    
     CP | D D D D D D | FSYNC | D D D D | FSYNC ...
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Change-Id: I99ec9968b76fd2b967965f50f089b94ac55fe61e
    Reviewed-on: http://gerrit.mot.com/675120
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jaegeuk Kim <jaegeuk@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -268,6 +268,10 @@ void f2fs_evict_inode(struct inode *inode)
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	nid_t xnid = F2FS_I(inode)->i_xattr_nid;
 
+	/* some remained atomic pages should discarded */
+	if (is_inode_flag_set(F2FS_I(inode), FI_ATOMIC_FILE))
+		commit_atomic_pages(inode, true);
+
 	trace_f2fs_evict_inode(inode);
 	truncate_inode_pages(&inode->i_data, 0);
 
