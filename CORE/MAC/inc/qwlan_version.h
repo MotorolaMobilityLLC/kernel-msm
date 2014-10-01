a66823592bcf46b4f191087fa3a41b107b0c6752
Kiet Lam <c_kietl@qca.qualcomm.com>
2014-10-01 11:06:07 -0700

    Wlan: Release 3.2.3.426
    Release 3.2.3.426
    
    Change-Id: Iff94305ee7d5942f607aa634a8665b00dec859e6
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            425
+#define QWLAN_VERSION_BUILD            426
 
-#define QWLAN_VERSIONSTR               "3.2.3.425"
+#define QWLAN_VERSIONSTR               "3.2.3.426"
 
 #endif /* QWLAN_VERSION_H */
