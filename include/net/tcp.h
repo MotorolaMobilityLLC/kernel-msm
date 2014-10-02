11c09c473178a74e364223b3a7bcb226a1d10204
JP Abgrall <jpa@google.com>
2014-10-02 14:50:37 -0700

    tcp: add a sysctl to config the tcp_default_init_rwnd
    The default initial rwnd is hardcoded to 10.
    
    Now we allow it to be controlled via
    /proc/sys/net/ipv4/tcp_default_init_rwnd
    which limits the values from 3 to 100
    
    This is somewhat needed because ipv6 routes are
    autoconfigured by the kernel.
    
    See "An Argument for Increasing TCP's Initial Congestion Window"
    in https://developers.google.com/speed/articles/tcp_initcwnd_paper.pdf
    
    Change-Id: I386b2a9d62de0ebe05c1ebe1b4bd91b314af5c54
    Signed-off-by: JP Abgrall <jpa@google.com>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/679680
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -255,6 +255,7 @@ extern int sysctl_tcp_cookie_size;
 extern int sysctl_tcp_thin_linear_timeouts;
 extern int sysctl_tcp_thin_dupack;
 extern int sysctl_tcp_challenge_ack_limit;
+extern int sysctl_tcp_default_init_rwnd;
 
 /* sysctl variables for controlling various tcp parameters */
 extern int sysctl_tcp_delack_seg;
