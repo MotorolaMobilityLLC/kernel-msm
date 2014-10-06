811ea5e2dcaee632fa3888e9d2b57f87caa8ef49
Jaegeuk Kim <jaegeuk@motorola.com>
2014-10-06 16:19:58 -0700

    - Revert "f2fs: support atomic_write feature for database"
    This revert patch is intended to support another atomic writes with
    a different approach.
    
    This reverts commit beae017c7bd8ccda19fbc4d7a88fc93031fdc040.
    
    Change-Id: I073b61aed81386c9f4735c35a3abd504cea2760c
    Signed-off-by: Jaegeuk Kim <jaegeuk@motorola.com>
    Reviewed-on: http://gerrit.mot.com/676677
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -26,7 +26,6 @@
 
 static struct kmem_cache *discard_entry_slab;
 static struct kmem_cache *sit_entry_set_slab;
-static struct kmem_cache *aw_entry_slab;
 
 /**
  * Copied from latest lib/llist.c
@@ -219,78 +218,6 @@ found_first:
 		return result + size;   /* Nope. */
 found_middle:
 	return result + __reverse_ffz(tmp);
-}
-
-/* For atomic write support */
-void register_atomic_page(struct inode *inode, struct page *page)
-{
-	struct f2fs_inode_info *fi = F2FS_I(inode);
-	struct atomic_pages *new;
-
-	new = f2fs_kmem_cache_alloc(aw_entry_slab, GFP_NOFS);
-
-	/* add atomic page indices to the list */
-	new->aid = (u64)current->pid;
-	new->page = page;
-	INIT_LIST_HEAD(&new->list);
-
-	/* increase reference count with clean state */
-	mutex_lock(&fi->atomic_lock);
-	get_page(page);
-	list_add_tail(&new->list, &fi->atomic_pages);
-	mutex_unlock(&fi->atomic_lock);
-}
-
-void invalidate_atomic_page(struct inode *inode, struct page *page)
-{
-	struct f2fs_inode_info *fi = F2FS_I(inode);
-	struct atomic_pages *cur, *tmp;
-
-	mutex_lock(&fi->atomic_lock);
-	list_for_each_entry_safe(cur, tmp, &fi->atomic_pages, list) {
-		if (cur->page == page) {
-			put_page(page);
-			list_del(&cur->list);
-			kmem_cache_free(aw_entry_slab, cur);
-		}
-	}
-	mutex_unlock(&fi->atomic_lock);
-}
-
-void commit_atomic_pages(struct inode *inode, bool abort)
-{
-	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
-	struct f2fs_inode_info *fi = F2FS_I(inode);
-	struct atomic_pages *cur, *tmp;
-	bool submit_bio = false;
-	struct f2fs_io_info fio = {
-		.type = DATA,
-		.rw = WRITE_SYNC,
-	};
-
-	f2fs_balance_fs(sbi);
-	f2fs_lock_op(sbi);
-
-	mutex_lock(&fi->atomic_lock);
-	list_for_each_entry_safe(cur, tmp, &fi->atomic_pages, list) {
-		lock_page(cur->page);
-		if (!abort && cur->page->mapping == inode->i_mapping) {
-			f2fs_wait_on_page_writeback(cur->page, DATA);
-			if (clear_page_dirty_for_io(cur->page))
-				inode_dec_dirty_pages(inode);
-			do_write_data_page(cur->page, &fio);
-			submit_bio = true;
-		}
-		f2fs_put_page(cur->page, 1);
-		list_del(&cur->list);
-		kmem_cache_free(aw_entry_slab, cur);
-	}
-	if (submit_bio)
-		f2fs_submit_merged_bio(sbi, DATA, WRITE);
-	mutex_unlock(&fi->atomic_lock);
-
-	filemap_fdatawait_range(inode->i_mapping, 0, LLONG_MAX);
-	f2fs_unlock_op(sbi);
 }
 
 /*
@@ -2268,14 +2195,8 @@ int __init create_segment_manager_caches(void)
 			sizeof(struct nat_entry_set));
 	if (!sit_entry_set_slab)
 		goto destory_discard_entry;
-	aw_entry_slab = f2fs_kmem_cache_create("atomic_entry",
-			sizeof(struct atomic_pages));
-	if (!aw_entry_slab)
-		goto destroy_sit_entry_set;
 	return 0;
 
-destroy_sit_entry_set:
-	kmem_cache_destroy(sit_entry_set_slab);
 destory_discard_entry:
 	kmem_cache_destroy(discard_entry_slab);
 fail:
@@ -2286,5 +2207,4 @@ void destroy_segment_manager_caches(void)
 {
 	kmem_cache_destroy(sit_entry_set_slab);
 	kmem_cache_destroy(discard_entry_slab);
-	kmem_cache_destroy(aw_entry_slab);
 }
