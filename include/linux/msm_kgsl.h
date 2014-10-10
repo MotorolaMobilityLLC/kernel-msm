afb99b536e49cd37a733c50a0413b373fbb745a4
Yizheng Zhou <yizhengz@codeaurora.org>
2014-10-10 15:17:10 -0700

    msm: kgsl: Vote against deep low power modes when GPU wakes up from slumber
    System should not enter global low power modes during GPU wakeup.
    To avoid this problem, explicitly vote against these modes at the
    beginning of wakeup, and restore the vote to the default value
    after that.
    
    Change-Id: I1e88e2f3619fb3cb8d4d8f59213c1720af8981ef
    Signed-off-by: Yizheng Zhou <yizhengz@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/684062
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>
    Reviewed-on: http://gerrit.mot.com/686544
    SLTApproved: Slta Waiver <sltawvr@motorola.com>

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -70,20 +70,21 @@ struct kgsl_pwrlevel {
 /**
  * struct kgsl_device_platform_data - Struct holding all the device info
  * obtained from the dtsi file
- * @pwrlevel:		Array of struct holding pwrlevel information
- * @init_level:		Pwrlevel device is initialized with
- * @num_levels:		Number of pwrlevels for the specific device
- * @idle_timeout:	Timeout for GPU to turn its resources off
- * @strtstp_sleepwake:  Flag to decide b/w SLEEP and SLUMBER
- * @bus_control:	Flag if independent bus voting is supported
- * @clk_map:		Clocks map per platform
- * @bus_scale_table:	Bus table with different b/w votes
- * @iommu_data:		Struct holding iommu context data
- * @iommu_count:	Number of IOMMU units for the GPU
- * @csdev:		Pointer to the coresight device for this device
- * @coresight_pdata:	Coresight configuration for specific device
- * @chipid:		Chip ID for the device's GPU
- * @pm_qos_latency:		latency value for cpu
+ * @pwrlevel:			Array of struct holding pwrlevel information
+ * @init_level:			Pwrlevel device is initialized with
+ * @num_levels:			Number of pwrlevels for the specific device
+ * @idle_timeout:		Timeout for GPU to turn its resources off
+ * @strtstp_sleepwake:		Flag to decide b/w SLEEP and SLUMBER
+ * @bus_control:		Flag if independent bus voting is supported
+ * @clk_map:			Clocks map per platform
+ * @bus_scale_table:		Bus table with different b/w votes
+ * @iommu_data:			Struct holding iommu context data
+ * @iommu_count:		Number of IOMMU units for the GPU
+ * @csdev:			Pointer to the coresight device for this device
+ * @coresight_pdata:		Coresight configuration for specific device
+ * @chipid:			Chip ID for the device's GPU
+ * @pm_qos_active_latency:	GPU PM QoS latency request for active state
+ * @pm_qos_wakeup_latency:	GPU PM QoS latency request during wakeup
  */
 struct kgsl_device_platform_data {
 	struct kgsl_pwrlevel pwrlevel[KGSL_MAX_PWRLEVELS];
@@ -100,7 +101,8 @@ struct kgsl_device_platform_data {
 	struct coresight_device *csdev;
 	struct coresight_platform_data *coresight_pdata;
 	unsigned int chipid;
-	unsigned int pm_qos_latency;
+	unsigned int pm_qos_active_latency;
+	unsigned int pm_qos_wakeup_latency;
 };
 
 #ifdef CONFIG_MSM_KGSL_DRM
