7df1636fd6fc895309d6ac0866510d297f5962e0
Yi-wei Zhao <gbjc64@motorola.com>
2014-10-13 11:40:12 -0700

    pstore : ram_core : not map as device memory
    mapping as device memory will trigger "unalignment" exception.
    
    Change-Id: I015520c2dd7ad2c5c44a9c61ba276f0c9513fe5f
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/678625
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -483,7 +483,7 @@ static void *persistent_ram_vmap(phys_addr_t start, size_t size)
 	page_start = start - offset_in_page(start);
 	page_count = DIV_ROUND_UP(size + offset_in_page(start), PAGE_SIZE);
 
-	prot = pgprot_noncached(PAGE_KERNEL);
+	prot = pgprot_writecombine(PAGE_KERNEL);
 
 	pages = kmalloc(sizeof(struct page *) * page_count, GFP_KERNEL);
 	if (!pages) {
