7bd58cb7acdaaf4560922a994a484c8614fb5a83
Yi-wei Zhao <gbjc64@motorola.com>
2014-10-13 10:46:13 -0700

    pstore : don't input 0 to rounddown_pow_of_two()
    Undefined value will be returned when input 0 to rounddown_pow_of_two().
    
    Change-Id: Id6aef19ea3d9c1988235d3552263962c4ca06915
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/678623
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -506,15 +506,15 @@ static int ramoops_probe(struct platform_device *pdev)
 		goto fail_out;
 	}
 
-	if (!is_power_of_2(pdata->mem_size))
+	if (pdata->mem_size && !is_power_of_2(pdata->mem_size))
 		pdata->mem_size = rounddown_pow_of_two(pdata->mem_size);
-	if (!is_power_of_2(pdata->record_size))
+	if (pdata->record_size && !is_power_of_2(pdata->record_size))
 		pdata->record_size = rounddown_pow_of_two(pdata->record_size);
-	if (!is_power_of_2(pdata->console_size))
+	if (pdata->console_size && !is_power_of_2(pdata->console_size))
 		pdata->console_size = rounddown_pow_of_two(pdata->console_size);
-	if (!is_power_of_2(pdata->ftrace_size))
+	if (pdata->ftrace_size && !is_power_of_2(pdata->ftrace_size))
 		pdata->ftrace_size = rounddown_pow_of_two(pdata->ftrace_size);
-	if (!is_power_of_2(pdata->annotate_size))
+	if (pdata->annotate_size && !is_power_of_2(pdata->annotate_size))
 		pdata->annotate_size =
 			rounddown_pow_of_two(pdata->annotate_size);
 
