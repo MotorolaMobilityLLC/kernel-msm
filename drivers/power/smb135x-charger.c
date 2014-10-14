4acf6db52ef7e679ff21236cc1ab15b21bfa21c2
Kenny Kessler <kenny.kessler@motorola.com>
2014-10-14 10:56:27 -0500

    power: smb135x: Stay Awake in Heartbeat
    Suspend is not prevented while running the Heartbeat.
    That can lead to execution that uses the I2C interface
    in a Suspended state.  To prevent this take the Wakelock
    at the begining of the Heartbeat.
    
    Change-Id: I1d340cf5d0499cf05396e6025ca0b9db7d592da3
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: http://gerrit.mot.com/678936
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    SLTApproved: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -432,6 +432,7 @@ struct smb135x_chg {
 	bool				hvdcp_powerup;
 	int				prev_batt_health;
 	bool				demo_mode;
+	bool				hb_running;
 };
 
 static struct smb135x_chg *the_chip;
@@ -2676,12 +2677,18 @@ static void heartbeat_work(struct work_struct *work)
 	if (chip->demo_mode)
 		dev_warn(chip->dev, "Battery in Demo Mode charging Limited\n");
 
-	if (smb135x_get_prop_batt_capacity(chip, &batt_soc) ||
+	if (!chip->resume_completed ||
+	    smb135x_get_prop_batt_capacity(chip, &batt_soc) ||
 	    smb135x_get_prop_batt_health(chip, &batt_health)) {
+		dev_warn(chip->dev, "HB Failed to run resume = %d!\n",
+			 (int)chip->resume_completed);
 		schedule_delayed_work(&chip->heartbeat_work,
 				      msecs_to_jiffies(1000));
 		return;
 	}
+
+	smb_stay_awake(&chip->smb_wake_source);
+	chip->hb_running = true;
 
 	dev_dbg(chip->dev, "HB Pound!\n");
 	cancel_delayed_work(&chip->src_removal_work);
@@ -2775,7 +2782,7 @@ static void heartbeat_work(struct work_struct *work)
 
 	schedule_delayed_work(&chip->heartbeat_work,
 			      msecs_to_jiffies(60000));
-
+	chip->hb_running = false;
 	if (!usb_present && !dc_present)
 		smb_relax(&chip->smb_wake_source);
 }
@@ -5082,6 +5089,7 @@ static int smb135x_charger_probe(struct i2c_client *client,
 	chip->dc_therm_lvl_sel = -EINVAL;
 	chip->hvdcp_powerup = false;
 	chip->demo_mode = false;
+	chip->hb_running = false;
 
 	wakeup_source_init(&chip->smb_wake_source.source, "smb135x_wake");
 	INIT_DELAYED_WORK(&chip->wireless_insertion_work,
@@ -5485,6 +5493,9 @@ static int smb135x_suspend(struct device *dev)
 	struct i2c_client *client = to_i2c_client(dev);
 	struct smb135x_chg *chip = i2c_get_clientdata(client);
 	int i, rc;
+
+	if (chip->hb_running)
+		return -EAGAIN;
 
 	/* Save the current IRQ config */
 	for (i = 0; i < 3; i++) {
