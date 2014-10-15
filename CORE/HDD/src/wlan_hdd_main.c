7697221d8393b094960cbdaf312d472681639f65
Siddharth Bhal <sbhal@qti.qualcomm.com>
2014-10-15 16:22:51 +0530

    wlan: hdd: Add host scan mac-spoofing support
    Extend the functionality of spoofing of mac addr
    in host initiated scans. Both FW initiated
    scans and host initiated scans will be spoofed.
    
    - Check for MAC_SPOOFED_SCAN feature capability flag
    - provided spoof mac addr is stored in pHddCtx
    - Spoofing request will be deferred till scan is completed
    - new spoofed mac addr is updated with FW after scan completion
    - 00:00:00 mac addr OUI provided to disable spoofing
    
    Change-Id: I85fad66779f2e4eed0303b938386bd4b90caeda8
    CRs-Fixed: 737889

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -8642,6 +8642,7 @@ int hdd_wlan_startup(struct device *dev )
     */
    mutex_init(&pHddCtx->tdls_lock);
 #endif
+   mutex_init(&pHddCtx->spoofMacAddr.macSpoofingLock);
    /* By default Strict Regulatory For FCC should be false */
 
    pHddCtx->nEnableStrictRegulatoryForFCC = FALSE;
