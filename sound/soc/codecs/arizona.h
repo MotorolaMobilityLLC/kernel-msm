3d4e5970fb75d6dfe41dd34ffd87305f509cc585
Jon Eklund <jeklund@motorola.com>
2014-10-15 13:03:52 -0500

    ASoC: enable SLIMbus for XMCS1000
    Required for MSM8994 integration
    
    Change-Id: Ide233b052c5ca8b37c1d8df300dd307cd33300cf
    Signed-off-by: Jon Eklund <jeklund@motorola.com>
    (cherry picked from commit c943ad01a75471b09b40b1139a790106b4038a82)
    Reviewed-on: http://gerrit.mot.com/679260
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Vladimir Karpovich <vkarpovich@motorola.com>

--- sound/soc/codecs/arizona.h
+++ sound/soc/codecs/arizona.h
@@ -63,6 +63,10 @@
 #define ARIZONA_MAX_DAI  10
 #define ARIZONA_MAX_ADSP 4
 
+#define ARIZONA_SLIM1 4
+#define ARIZONA_SLIM2 5
+#define ARIZONA_SLIM3 6
+
 struct arizona;
 struct wm_adsp;
 struct arizona_jd_state;
@@ -249,11 +253,19 @@ extern int arizona_anc_ev(struct snd_soc_dapm_widget *w,
 extern int arizona_put_sample_rate_enum(struct snd_kcontrol *kcontrol,
 					struct snd_ctl_elem_value *ucontrol);
 
+extern int arizona_slim_tx_ev(struct snd_soc_dapm_widget *w,
+			     struct snd_kcontrol *kcontrol,
+			     int event);
+extern int arizona_slim_rx_ev(struct snd_soc_dapm_widget *w,
+			     struct snd_kcontrol *kcontrol,
+			     int event);
+
 extern int arizona_set_sysclk(struct snd_soc_codec *codec, int clk_id,
 			      int source, unsigned int freq, int dir);
 
 extern const struct snd_soc_dai_ops arizona_dai_ops;
 extern const struct snd_soc_dai_ops arizona_simple_dai_ops;
+extern const struct snd_soc_dai_ops arizona_slim_dai_ops;
 
 #define ARIZONA_FLL_NAME_LEN 20
 
