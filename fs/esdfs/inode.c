77d7c60335077b0067ce4d52e0561f3b3ae829e9
Russ W. Knize <Russ.Knize@motorola.com>
2014-10-16 16:44:53 -0500

    esdfs: improve support for SELinux
    Propagated from (CR) and (CR).
    
    With an ever tightening sepolicy, esdfs needs to use the correct
    SID for any VFS calls that it makes.  The Linux security API does
    not provide an elegant way to do this, so its done with brute force.
    The security context is hard-coded in the lower half, for security
    reasons.
    
    Change-Id: I156ab9e6d75fae988c7da731fc6c0ff3d2830323
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/689178
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/esdfs/inode.c
+++ fs/esdfs/inode.c
@@ -19,6 +19,7 @@ static int esdfs_create(struct inode *dir, struct dentry *dentry,
 	struct dentry *lower_dentry;
 	struct dentry *lower_parent_dentry = NULL;
 	struct path lower_path;
+	struct inode *lower_inode;
 	int mask;
 	const struct cred *creds;
 
@@ -40,8 +41,8 @@ static int esdfs_create(struct inode *dir, struct dentry *dentry,
 
 	esdfs_set_lower_mode(ESDFS_SB(dir->i_sb), &mode);
 
-	err = vfs_create(lower_parent_dentry->d_inode, lower_dentry, mode,
-			 want_excl);
+	lower_inode = esdfs_lower_inode(dir);
+	err = vfs_create(lower_inode, lower_dentry, mode, want_excl);
 	if (err)
 		goto out;
 
@@ -278,11 +279,15 @@ static int esdfs_setattr(struct dentry *dentry, struct iattr *ia)
 	struct inode *lower_inode;
 	struct path lower_path;
 	struct iattr lower_ia;
+	const struct cred *creds =
+		esdfs_override_creds(ESDFS_SB(dentry->d_inode->i_sb), NULL);
+	if (!creds)
+		return -ENOMEM;
 
 	/* We don't allow chmod or chown, so skip those */
 	ia->ia_valid &= ~(ATTR_UID | ATTR_GID | ATTR_MODE);
 	if (!ia->ia_valid)
-		return 0;
+		goto out_err;
 
 	inode = dentry->d_inode;
 
@@ -349,6 +354,7 @@ static int esdfs_setattr(struct dentry *dentry, struct iattr *ia)
 out:
 	esdfs_put_lower_path(dentry, &lower_path);
 out_err:
+	esdfs_revert_creds(creds, NULL);
 	return err;
 }
 
@@ -360,6 +366,10 @@ static int esdfs_getattr(struct vfsmount *mnt, struct dentry *dentry,
 	struct kstat lower_stat;
 	struct inode *lower_inode;
 	struct inode *inode = dentry->d_inode;
+	const struct cred *creds =
+			esdfs_override_creds(ESDFS_SB(inode->i_sb), NULL);
+	if (!creds)
+		return -ENOMEM;
 
 	esdfs_get_lower_path(dentry, &lower_path);
 
@@ -377,6 +387,7 @@ static int esdfs_getattr(struct vfsmount *mnt, struct dentry *dentry,
 
 out:
 	esdfs_put_lower_path(dentry, &lower_path);
+	esdfs_revert_creds(creds, NULL);
 	return err;
 }
 
