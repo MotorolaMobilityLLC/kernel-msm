6bfbe4cb865ee986612dca7e8388aff94a69f63f
Zac Crosby <zac@motorola.com>
2014-10-17 15:56:51 -0500

    msm: mdss: fix race condition of DSI off and status check.
    fb_event_callback() is what is responsible for cancelling the delayed_work
    when it gets the FB_BLANK_POWERDOWN event. However, this event occurs after
    the panel & DSI have been turned off. So if the check_status function was
    being called right when the panel is being turned off, we have a race
    conditon. Hence Add offlock mutex to ensure that bta status check is done
    when dsi is being turned off.
    
    Port of 6a6338704
    
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Change-Id: Ic0bc5a2734472a143e35c0701d93ff1f34b1ed9a
    Reviewed-on: http://gerrit.mot.com/685417
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -138,6 +138,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		return;
 	}
 
+	mutex_lock(&ctl->offlock);
 	mutex_lock(&ctrl_pdata->mutex);
 
 	/*
@@ -156,6 +157,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 			ctrl_pdata->status_mode == ESD_BTA)
 			mutex_unlock(&mdp5_data->ov_lock);
 		mutex_unlock(&ctrl_pdata->mutex);
+		mutex_unlock(&ctl->offlock);
 		pr_err("%s: DSI turning off, avoiding panel status check\n",
 							__func__);
 		return;
@@ -186,6 +188,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		ctrl_pdata->status_mode == ESD_BTA)
 		mutex_unlock(&mdp5_data->ov_lock);
 	mutex_unlock(&ctrl_pdata->mutex);
+	mutex_unlock(&ctl->offlock);
 
 	if ((pstatus_data->mfd->panel_power_state == MDSS_PANEL_POWER_ON)) {
 		if (ret > 0)
