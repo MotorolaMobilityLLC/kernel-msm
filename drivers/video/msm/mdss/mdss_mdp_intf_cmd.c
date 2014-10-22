44d2b56ba28fcbd4b31f987c583730d5acc393b6
Naseer Ahmed <naseer@codeaurora.org>
2014-10-22 15:52:12 -0400

    msm: mdss: keep clocks on during kickoff operation
    There is deadlock scenario where work thread to turn clocks off is
    trying to acquire DSI's mdp_busy lock while holding clk_mtx to turn
    mdp clocks on.  Meanwhile kickoff thread holds mdp_busy lock and
    trying to acquire clk_mtx to turn on clock.
    This patch ensure that clocks are kept on and work thread is not
    scheduled while kickoff is in progress so that deadlock will not happen.
    
    Change-Id: Iceb5db431a7f9f0e1f56a0e3c1ced2d08d762b86
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -261,6 +261,10 @@ static void mdss_mdp_cmd_readptr_done(void *arg)
 	if (!ctx->vsync_enabled) {
 		if (ctx->rdptr_enabled)
 			ctx->rdptr_enabled--;
+
+		/* keep clk on during kickoff */
+		if (ctx->rdptr_enabled == 0 && ctx->koff_cnt)
+			ctx->rdptr_enabled++;
 	}
 
 	if (ctx->rdptr_enabled == 0) {
@@ -475,19 +479,19 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 		WARN(rc, "intf %d panel on error (%d)\n", ctl->intf_num, rc);
 	}
 
+	spin_lock_irqsave(&ctx->clk_lock, flags);
+	ctx->koff_cnt++;
+	spin_unlock_irqrestore(&ctx->clk_lock, flags);
+
+	mdss_mdp_cmd_clk_on(ctx);
+
 	/*
 	 * tx dcs command if had any
 	 */
 	mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_DSI_CMDLIST_KOFF, NULL);
-
-	mdss_mdp_cmd_clk_on(ctx);
-
 	INIT_COMPLETION(ctx->pp_comp);
 	mdss_mdp_irq_enable(MDSS_MDP_IRQ_PING_PONG_COMP, ctx->pp_num);
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_START, 1);
-	spin_lock_irqsave(&ctx->clk_lock, flags);
-	ctx->koff_cnt++;
-	spin_unlock_irqrestore(&ctx->clk_lock, flags);
 	mb();
 
 	return 0;
