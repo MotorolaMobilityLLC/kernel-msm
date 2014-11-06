9e5b7c6cd9c68b32bf7fc6fbf0c0ed08ef80c75c
Kuogee Hsieh <khsieh@codeaurora.org>
2014-11-06 15:09:50 -0800

    msm: mdss: un map dsi transmit buffer properly
    DSi tx buffer is mapped to iommu memory if iommu
    attached. Therefore it needs to be iommu un mapped
    at end of transmission.
    
    CRs-Fixed: 752164
    Change-Id: I2d3edad78c0cd859b5c31b5e2c6d66e5148ccf5f
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>
    Signed-off-by: Remil Ram <remil@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -310,6 +310,7 @@ struct mdss_dsi_ctrl_pdata {
 	bool cmd_sync_wait_trigger;
 
 	struct mdss_rect roi;
+	bool dmap_iommu_map;
 	struct pwm_device *pwm_bl;
 	struct dsi_drv_cm_data shared_pdata;
 	u32 pclk_rate;
