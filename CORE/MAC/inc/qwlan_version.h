f0b63e53e1673e9f58e762bc1d17e3a141df15f3
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2014-11-06 19:31:41 +0530

    Wlan: Revision 3.2.3.445
    Revision 3.2.3.445
    
    Change-Id: I0ad0aab7a3db2f5cf185ce582729d2fe581104ae
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            444
+#define QWLAN_VERSION_BUILD            445
 
-#define QWLAN_VERSIONSTR               "3.2.3.444"
+#define QWLAN_VERSIONSTR               "3.2.3.445"
 
 #endif /* QWLAN_VERSION_H */
