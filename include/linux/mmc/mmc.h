51071ba02a6fa09d39c6a501990344cb9242823f
Avi Shchislowski <avi.shchislowski@sandisk.com>
2014-11-18 10:26:17 -0600

    Add eMMC5.0 ffu ext_csd fields.
    Mot-CRs-fixed: (CR)
    
    Change-Id: I417c54c390f91585008bb3913e84285e967ccd7f
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842388
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -224,6 +224,9 @@ struct _mmc_csd {
  */
 
 #define EXT_CSD_CMDQ			15	/* R/W */
+#define EXT_CSD_FFU_STATUS             26      /* R */
+#define EXT_CSD_MODE_OPERATION_CODES   29      /* W */
+#define EXT_CSD_MODE_CONFIG            30      /* R/W */
 #define EXT_CSD_BARRIER_CTRL		31      /* R/W */
 #define EXT_CSD_FLUSH_CACHE		32      /* W */
 #define EXT_CSD_CACHE_CTRL		33      /* R/W */
@@ -244,6 +247,7 @@ struct _mmc_csd {
 #define EXT_CSD_SANITIZE_START		165     /* W */
 #define EXT_CSD_WR_REL_PARAM		166	/* RO */
 #define EXT_CSD_RPMB_MULT		168	/* RO */
+#define EXT_CSD_FW_CONFIG		169	/* R/W */
 #define EXT_CSD_BOOT_WP			173	/* R/W */
 #define EXT_CSD_ERASE_GROUP_DEF		175	/* R/W */
 #define EXT_CSD_PART_CONFIG		179	/* R/W */
@@ -284,12 +288,17 @@ struct _mmc_csd {
 #define EXT_CSD_CACHE_SIZE		249	/* RO, 4 bytes */
 #define EXT_CSD_PWR_CL_DDR_200_360	253	/* RO */
 #define EXT_CSD_FW_VERSION		254	/* RO */
+#define EXT_CSD_NUM_OF_FW_SEC_PROG     302     /* RO, 4 bytes */
 #define EXT_CSD_CMDQ_DEPTH		307	/* RO */
 #define EXT_CSD_CMDQ_SUPPORT		308	/* RO */
 #define EXT_CSD_BARRIER_SUPPORT		486	/* RO */
 #define EXT_CSD_PRE_EOL_INFO		267	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A	268	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_B	269	/* RO */
+#define EXT_CSD_FFU_ARG			487     /* RO, 4 bytes */
+#define EXT_CSD_OPERATION_CODE_TIMEOUT 491     /* RO */
+#define EXT_CSD_FFU_FEATURES		492     /* RO */
+#define EXT_CSD_SUPPORTED_MODE		493     /* RO */
 #define EXT_CSD_TAG_UNIT_SIZE		498	/* RO */
 #define EXT_CSD_DATA_TAG_SUPPORT	499	/* RO */
 #define EXT_CSD_MAX_PACKED_WRITES	500	/* RO */
