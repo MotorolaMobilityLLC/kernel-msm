aac90527fa5688f78c456bba4fcfebf8f1c3c59d
Li RongQing <roy.qing.li@gmail.com>
2014-11-18 18:05:26 -0800

    ipv6: always set the new created dst's from in ip6_rt_copy
    [ Upstream commit 24f5b855e17df7e355eacd6c4a12cc4d6a6c9ff0 ]
    ip6_rt_copy only sets dst.from if ort has flag RTF_ADDRCONF
    and RTF_DEFAULT. but the prefix routes which did get installed
    by hand locally can have an expiration, and no any flag
    combination which can ensure a potential from does never expire,
    so we should always set the new created dst's from.
    This also fixes the new created dst is always expired since
    the ort, which is created by RA, maybe has RTF_EXPIRES and
    RTF_ADDRCONF, but no RTF_DEFAULT.
    
    Suggested-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    CC: Gao feng <gaofeng@cn.fujitsu.com>
    Signed-off-by: Li RongQing <roy.qing.li@gmail.com>
    Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Change-Id: I5d7b2f98b2a1832492ac62b43575b3f92d3ad8d2
    Signed-off-by: Jane Zhou <a17711@motorola.com>
    Reviewed-on: http://gerrit.mot.com/690804
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -1834,9 +1834,7 @@ static struct rt6_info *ip6_rt_copy(struct rt6_info *ort,
 
 		rt->rt6i_gateway = ort->rt6i_gateway;
 		rt->rt6i_flags = ort->rt6i_flags;
-		if ((ort->rt6i_flags & (RTF_DEFAULT | RTF_ADDRCONF)) ==
-		    (RTF_DEFAULT | RTF_ADDRCONF))
-			rt6_set_from(rt, ort);
+		rt6_set_from(rt, ort);
 		rt->rt6i_metric = 0;
 
 #ifdef CONFIG_IPV6_SUBTREES
