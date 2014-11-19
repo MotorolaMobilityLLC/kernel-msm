b1e75db1be8c0d1086c564cc4f43647369d3aa9c
Shiju Mathew <shijum@codeaurora.org>
2014-11-18 20:14:42 -0500

    msm: thermal: Add RT priority to kernel thermal threads
    Add RT priority to KTM frequency mitigation thread. This
    is required since we have seen cases where this thread
    is not scheduled fast enough during high temperature rampup
    that lead to thermal runaway. So this change would make sure
    the thermal frequency mitigation thread gets high priority
    and mitigate core as soon as a temperature threshold is triggered.
    
    Change-Id: Ie8a6fd80fb8e9bdb894a26a8bf7abd159a3f3bce
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>
    Signed-off-by: Vijayakumar Gn <vijaygn@motorola.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c
    Change-Id: I009d9037c578026ee1695e81d5a2abc57bc49f93
    Reviewed-on: http://gerrit.mot.com/777303
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -36,6 +36,7 @@
 #include <mach/rpm-regulator-smd.h>
 #include <linux/regulator/consumer.h>
 #include <linux/msm_thermal_ioctl.h>
+#include <linux/sched.h>
 
 #define MAX_CURRENT_UA 1000000
 #define MAX_RAILS 5
@@ -1344,7 +1345,9 @@ static __ref int do_freq_mitigation(void *data)
 {
 	int ret = 0;
 	uint32_t cpu = 0, max_freq_req = 0, min_freq_req = 0;
+	struct sched_param param = {.sched_priority = MAX_RT_PRIO-1};
 
+	sched_setscheduler(current, SCHED_FIFO, &param);
 	while (!kthread_should_stop()) {
 		wait_for_completion(&freq_mitigation_complete);
 		INIT_COMPLETION(freq_mitigation_complete);
