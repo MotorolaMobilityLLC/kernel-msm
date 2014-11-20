4c97c8e535e5dc2440764d53032874db384d9a26
Xiaofei Ma <xiaofei@motorola.com>
2014-11-20 14:28:52 -0600

    clk : clock-gcc-8916: Support 200MHz for emmc
    Set max eMMC clock frequency to 200MHz
    from current value of 177.77MHz.
    
    Change-Id: I073e74d867217edbad301f0ca291c06184dfdf76
    Signed-off-by: Xiaofei Ma <xiaofei@motorola.com>
    Reviewed-on: http://gerrit.mot.com/691313
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -1190,6 +1190,7 @@ static struct clk_freq_tbl ftbl_gcc_sdcc1_apps_clk[] = {
 	F(  50000000,	   gpll0,  16,	  0,	0),
 	F( 100000000,	   gpll0,   8,	  0,	0),
 	F( 177770000,	   gpll0, 4.5,	  0,	0),
+	F( 200000000,	   gpll0,   4,	  0,	0),
 	F_END
 };
 
