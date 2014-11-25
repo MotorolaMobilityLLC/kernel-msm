bb4e9c8cf9c236b66ac3ebe939922a949b86d3f9
Ravikumar Vembu <a18394@motorola.com>
2014-11-25 09:51:18 -0600

    usb: diag_bridge: Reduce Autosuspend delay to 200 ms
    Autosuspend time is set to 1000ms since diag polls every 500 msec.
    Since diag is not enabled on consumer builds, reduce it to 200 msec
    based on a module param that can be set by userspace.
    
    Change-Id: Iab597e328b730e3056453744f4b3cd8f3e41e33b
    Signed-off-by: Ravikumar Vembu <a18394@motorola.com>
    Reviewed-on: http://gerrit.mot.com/692540
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/misc/diag_bridge.c
+++ drivers/usb/misc/diag_bridge.c
@@ -32,6 +32,8 @@
 
 #define MAX_DIAG_BRIDGE_DEVS	2
 #define AUTOSUSP_DELAY_WITH_USB 1000
+static unsigned int  default_auto_susp_enabled;
+module_param(default_auto_susp_enabled, uint, S_IRUGO | S_IWUSR);
 
 struct diag_bridge {
 	struct usb_device	*udev;
@@ -83,11 +85,14 @@ int diag_bridge_open(int id, struct diag_bridge_ops *ops)
 	dev->ops = ops;
 	dev->err = 0;
 
+	if (!default_auto_susp_enabled) {
 #ifdef CONFIG_PM_RUNTIME
-	dev->default_autosusp_delay = dev->udev->dev.power.autosuspend_delay;
+		dev->default_autosusp_delay =
+			dev->udev->dev.power.autosuspend_delay;
 #endif
-	pm_runtime_set_autosuspend_delay(&dev->udev->dev,
-			AUTOSUSP_DELAY_WITH_USB);
+		pm_runtime_set_autosuspend_delay(&dev->udev->dev,
+				AUTOSUSP_DELAY_WITH_USB);
+	}
 
 	kref_get(&dev->kref);
 
@@ -130,8 +135,9 @@ void diag_bridge_close(int id)
 	usb_kill_anchored_urbs(&dev->submitted);
 	dev->ops = 0;
 
-	pm_runtime_set_autosuspend_delay(&dev->udev->dev,
-			dev->default_autosusp_delay);
+	if (dev->default_autosusp_delay)
+		pm_runtime_set_autosuspend_delay(&dev->udev->dev,
+				dev->default_autosusp_delay);
 
 	kref_put(&dev->kref, diag_bridge_delete);
 }
