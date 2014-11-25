1fbcdf251d2acd973408ad44a1cef91d05bd0a12
Lianwei Wang <a22439@motorola.com>
2014-11-25 15:13:27 -0800

    power: make sync on suspend optional
    propagate from (CR).
    
    On embedded devices with built-in batteries, it is not so
    important to sync the file systems before suspend.  The chance
    of losing power during suspend are no greater than they are
    when the system is awake.  The sync operations can greatly
    increase suspend latency when the system has accrued many dirty
    pages and/or the target storage devices are not particularly
    fast.
    
    This commit adds a kernel config option to allow file system
    sync in the suspend path to be disabled.
    
    It is enabled by default.
    
    Change-Id: Ib0f5fdbb55cd3e503a3768f8424b2863fbc7ff22
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/726915
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -348,9 +348,11 @@ static int enter_state(suspend_state_t state)
 	if (state == PM_SUSPEND_FREEZE)
 		freeze_begin();
 
+#ifdef CONFIG_PM_SYNC_BEFORE_SUSPEND
 	printk(KERN_INFO "PM: Syncing filesystems ... ");
 	sys_sync();
 	printk("done.\n");
+#endif
 
 	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state].label);
 	error = suspend_prepare(state);
