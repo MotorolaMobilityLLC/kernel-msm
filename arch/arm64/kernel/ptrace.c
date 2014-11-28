f5a32d5a5b155f1c9efcec0621555c09df8e5179
AKASHI Takahiro <takahiro.akashi@linaro.org>
2014-11-28 05:26:34 +0000

    arm64: ptrace: add NT_ARM_SYSTEM_CALL regset
    This regeset is intended to be used to get and set a system call number
    while tracing.
    There was some discussion about possible approaches to do so:
    
    (1) modify x8 register with ptrace(PTRACE_SETREGSET) indirectly,
        and update regs->syscallno later on in syscall_trace_enter(), or
    (2) define a dedicated regset for this purpose as on s390, or
    (3) support ptrace(PTRACE_SET_SYSCALL) as on arch/arm
    
    Thinking of the fact that user_pt_regs doesn't expose 'syscallno' to
    tracer as well as that secure_computing() expects a changed syscall number,
    especially case of -1, to be visible before this function returns in
    syscall_trace_enter(), (1) doesn't work well.
    We will take (2) since it looks much cleaner.
    
    cfries: Add #include <linux/audit.h>
    
    Change-Id: I345fd36d74e12c51820bb8197f4d97a8fbc80b93
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/audit.h>
 #include <linux/compat.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
