2d9b78ab37c597062cf1cd828df30a3398190452
AKASHI Takahiro <takahiro.akashi@linaro.org>
2014-11-28 05:26:37 +0000

    arm64: add seccomp syscall for compat task
    This patch allows compat task to issue seccomp() system call.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -785,4 +785,9 @@ __SYSCALL(__NR_finit_module, sys_finit_module)
 __SYSCALL(__NR_sched_setattr, sys_sched_setattr)
 #define __NR_sched_getattr 381
 __SYSCALL(__NR_sched_getattr, sys_sched_getattr)
+/* Backporting seccomp, stub out missing renameat2 call */
+#define __NR_renameat2 382
+__SYSCALL(__NR_renameat2, sys_ni_syscall)
+#define __NR_seccomp 383
+__SYSCALL(__NR_seccomp, sys_seccomp)
 
