9a1ba950e9db17530b182762c95d85c5216b38aa
Ravi Shankar Upadrasta <raviupadrasta@codeaurora.org>
2014-12-03 17:02:52 +0530

    Wlan: Release 3.8.22.10
    Release 3.8.22.10
    
    Change-Id: Iff746722b13acd41d89855464006a483845e6994
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            22
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            9
+#define QWLAN_VERSION_BUILD            10
 
-#define QWLAN_VERSIONSTR               "3.8.22.9"
+#define QWLAN_VERSIONSTR               "3.8.22.10"
 
 #endif /* QWLAN_VERSION_H */
