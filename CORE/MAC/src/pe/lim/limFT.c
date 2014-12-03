2ccfcbe00c273e7a77d848f0e6313ba6a2fd9165
mukul sharma <mukul@qti.qualcomm.com>
2014-12-03 22:41:07 +0530

    wlan: Handle error case in FT preauth rsp
    Currently if pe session is not created in FT preauth rsp then
    driver is not posting the preauth rsp to SME and TL will remain
    in suspended mode.Due to this tx timeOut happens and eventually
    crash.
    
    Fix is done to send the preauth rsp to SME in case of preauth rsp
    failure and resume the link.
    
    Change-Id: I0421b88795ef13e898e365e4557d8ad8587b8e85
    CRs-Fixed: 765939

--- CORE/MAC/src/pe/lim/limFT.c
+++ CORE/MAC/src/pe/lim/limFT.c
@@ -1151,7 +1151,9 @@ void limHandleFTPreAuthRsp(tpAniSirGlobal pMac, tSirRetStatus status,
                                               &sessionId, pMac->lim.maxStation)) == NULL)
         {
             limLog(pMac, LOGE, FL("Session Can not be created for pre-auth 11R AP"));
-            return;
+            status = eSIR_FAILURE;
+            pMac->ft.ftPEContext.ftPreAuthStatus = status;
+            goto out;
         }
         pftSessionEntry->peSessionId = sessionId;
         sirCopyMacAddr(pftSessionEntry->selfMacAddr, psessionEntry->selfMacAddr);
@@ -1180,7 +1182,7 @@ void limHandleFTPreAuthRsp(tpAniSirGlobal pMac, tSirRetStatus status,
         sirCopyMacAddr(psessionEntry->limReAssocbssId, pbssDescription->bssId);
         limPrintMacAddr(pMac, psessionEntry->limReAssocbssId, LOG1);
     }
-
+out:
     if (psessionEntry->currentOperChannel != 
         pMac->ft.ftPEContext.pFTPreAuthReq->preAuthchannelNum) 
     {
