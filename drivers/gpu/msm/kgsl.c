c399ff96bf3a52519866bf9b8c39f1848388c087
Anupam Sakargayan <asakarga@codeaurora.org>
2014-12-04 15:47:43 +0530

    msm: kgsl: Device mutex lock for kgsl_cmdbatch_destroy
    kgsl_cancel_event must be called with device->mutex
    otherwise it will cause a BUG_ON in kgsl_cancel_event
    
    kgsl_cmdbatch_destroy will call kgsl_cancel_event, hence added
    device->mutex lock/unlock wherever kgsl_cmdbatch_destroy is called
    
    Change-Id: I9b9959236d6be0d80987279a64a0715363229f7b
    Signed-off-by: Anupam Sakargayan <asakarga@codeaurora.org>
    (cherry picked from commit ade2acf82103fa68b97e89c01b1c8aa3bc425796)
    Reviewed-on: http://gerrit.mot.com/726715
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2255,8 +2255,11 @@ free_cmdbatch:
 	 * -EPROTO is a "success" error - it just tells the user that the
 	 * context had previously faulted
 	 */
-	if (result && result != -EPROTO)
+	if (result && result != -EPROTO) {
+		kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 		kgsl_cmdbatch_destroy(cmdbatch);
+		kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
+	}
 
 done:
 	kgsl_context_put(context);
@@ -2307,8 +2310,11 @@ free_cmdbatch:
 	 * -EPROTO is a "success" error - it just tells the user that the
 	 * context had previously faulted
 	 */
-	if (result && result != -EPROTO)
+	if (result && result != -EPROTO) {
+		kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 		kgsl_cmdbatch_destroy(cmdbatch);
+		kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
+	}
 
 done:
 	kgsl_context_put(context);
