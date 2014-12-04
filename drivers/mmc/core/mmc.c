b7de55758e9c42d8e50b5a8445c60907d6b37411
Russ W. Knize <Russ.Knize@motorola.com>
2014-12-04 16:42:23 -0600

    mmc: refresh card identification for MMC cards
    One or more CID entries may change on a card whose firmware has been
    reflashed.  If the raw CID buffer has been zeroed-out, just assume
    that the CID, CSD, and EXT_CSD need to be re-read and re-parsed.
    
     Propagated from (CR)
    
    Change-Id: I377ae49e69444cbdc95bb558ce24071fd0402853
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/764643
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/799510
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842391
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1729,6 +1729,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	u32 cid[4];
 	u32 rocr;
 	u8 *ext_csd = NULL;
+	bool scan = (oldcard == NULL);
 
 	BUG_ON(!host);
 	WARN_ON(!host->claimed);
@@ -1781,6 +1782,15 @@ reinit:
 	}
 
 	if (oldcard) {
+		if (oldcard->raw_cid[0] == 0 && oldcard->raw_cid[1] == 0 &&
+		    oldcard->raw_cid[2] == 0 && oldcard->raw_cid[3] == 0) {
+			scan = true;
+			pr_info("%s: updating card identification\n", mmc_hostname(host));
+			memcpy(oldcard->raw_cid, cid, sizeof(oldcard->raw_cid));
+			err = mmc_decode_cid(oldcard);
+			if (err)
+				goto err;
+		}
 		if (memcmp(cid, oldcard->raw_cid, sizeof(cid)) != 0) {
 			err = -ENOENT;
 			pr_err("%s: %s: CID memcmp failed %d\n",
@@ -1823,7 +1833,7 @@ reinit:
 		mmc_set_bus_mode(host, MMC_BUSMODE_PUSHPULL);
 	}
 
-	if (!oldcard) {
+	if (scan) {
 		/*
 		 * Fetch CSD from card.
 		 */
@@ -1867,7 +1877,7 @@ reinit:
 		}
 	}
 
-	if (!oldcard) {
+	if (scan) {
 		/*
 		 * Fetch and process extended CSD.
 		 */
