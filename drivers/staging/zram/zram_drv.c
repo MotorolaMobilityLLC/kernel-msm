2a80e9d845040f50807f235404ffe445f3dc37dc
Chris Fries <cfries@motorola.com>
2014-12-04 10:55:40 -0600

    zram: Add LZ4 support
    Add LZ4 compressor support.  Enable it with
    CONFIG_ZRAM_LZ4_COMPRESS=y
    
    Change-Id: I4bb62c1b51be9171f03386a2b714c6f7542cf321
    Signed-off-by: Chris Fries <cfries@motorola.com>
    Reviewed-on: http://gerrit.mot.com/694989
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -30,11 +30,41 @@
 #include <linux/highmem.h>
 #include <linux/slab.h>
 #include <linux/lzo.h>
+#include <linux/lz4.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/ratelimit.h>
 
 #include "zram_drv.h"
+
+static inline int z_decompress_safe(const unsigned char *src, size_t src_len,
+			unsigned char *dest, size_t *dest_len)
+{
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+	return lz4_decompress_unknownoutputsize(src, src_len, dest, dest_len);
+#else
+	return lzo1x_decompress_safe(src, src_len, dest, dest_len);
+#endif
+}
+
+static inline int z_compress(const unsigned char *src, size_t src_len,
+			unsigned char *dst, size_t *dst_len, void *wrkmem)
+{
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+	return lz4_compress(src, src_len, dst, dst_len, wrkmem);
+#else
+	return lzo1x_1_compress(src, src_len, dst, dst_len, wrkmem);
+#endif
+}
+
+static inline size_t z_scratch_size(void)
+{
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+	return LZ4_MEM_COMPRESS;
+#else
+	return LZO1X_MEM_COMPRESS;
+#endif
+}
 
 /* Globals */
 static int zram_major;
@@ -210,7 +240,7 @@ static struct zram_meta *zram_meta_alloc(u64 disksize)
 	if (!meta)
 		goto out;
 
-	meta->compress_workmem = kzalloc(LZO1X_MEM_COMPRESS, GFP_KERNEL);
+	meta->compress_workmem = kzalloc(z_scratch_size(), GFP_KERNEL);
 	if (!meta->compress_workmem)
 		goto free_meta;
 
@@ -337,7 +367,7 @@ static int zram_decompress_page(struct zram *zram, char *mem, u32 index)
 	if (meta->table[index].size == PAGE_SIZE)
 		copy_page(mem, cmem);
 	else
-		ret = lzo1x_decompress_safe(cmem, meta->table[index].size,
+		ret = z_decompress_safe(cmem, meta->table[index].size,
 						mem, &clen);
 	zs_unmap_object(meta->mem_pool, handle);
 
@@ -457,7 +487,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 			zram_test_flag(meta, index, ZRAM_ZERO)))
 		zram_free_page(zram, index);
 
-	ret = lzo1x_1_compress(uncmem, PAGE_SIZE, src, &clen,
+	ret = z_compress(uncmem, PAGE_SIZE, src, &clen,
 			       meta->compress_workmem);
 
 	if (!is_partial_io(bvec)) {
