aa3e8bdb59243430cce8ee380b76795a661b833f
Jared Suttles <jsuttles@motorola.com>
2014-12-09 14:26:50 -0600

    mdss: display: add mdp timeout dropbox dump
    This change adds the utility functions to dump an MDP timeout in the
    display driver.
    
    Change-Id: I2fa66d2cdc8252b8f9f4b7c5a217773108fd2aff
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-on: http://gerrit.mot.com/696413
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/video/msm/mdss/mdss_timeout.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2013, Motorola Mobility LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef MDSS_TIMEOUT_H
+#define MDSS_TIMEOUT_H
+
+#include "mdss.h"
+#include "mdss_mdp.h"
+
+#ifdef CONFIG_FB_MSM_MDSS_TIMEOUT_DUMP
+
+extern char *mdss_timeout_data;
+extern u32 mdss_timeout_data_pos;
+extern void mdss_timeout_init(void (*mdss_mdp_dump_func)(void *data),
+			      void *data);
+extern void mdss_timeout_dump(const char *timeout_type);
+
+#define MDSS_DUMP_SIZE (2*PAGE_SIZE)
+#define MDSS_TIMEOUT_DUMP(fmt, args...) \
+	do { \
+		if (mdss_timeout_data != NULL) { \
+			mdss_timeout_data_pos += scnprintf( \
+				&mdss_timeout_data[mdss_timeout_data_pos], \
+				MDSS_DUMP_SIZE-mdss_timeout_data_pos-1, \
+				fmt, ##args); \
+			if (mdss_timeout_data_pos > 0 && \
+				mdss_timeout_data[mdss_timeout_data_pos-1] != '\n') \
+				mdss_timeout_data_pos += scnprintf( \
+					&mdss_timeout_data[mdss_timeout_data_pos], \
+					MDSS_DUMP_SIZE-mdss_timeout_data_pos-1, \
+					"\n"); \
+		} \
+	} while (0)
+
+#define MDSS_TIMEOUT_LOG(fmt, args...) \
+	do { \
+		pr_err(fmt, ##args); \
+		MDSS_TIMEOUT_DUMP(fmt, ##args); \
+	} while (0)
+
+#else
+
+#define MDSS_TIMEOUT_DUMP(fmt, args...)
+#define MDSS_TIMEOUT_LOG(fmt, args...) pr_err(fmt, ##args)
+
+#define mdss_timeout_init(x)
+#define mdss_timeout_dump(x)
+
+#endif
+
+#endif /* MDSS_TIMEOUT_H */
