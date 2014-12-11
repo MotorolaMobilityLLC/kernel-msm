9041060590530a19b163ab6db17a6aef828be873
Chris Fries <cfries@motorola.com>
2014-12-11 17:27:15 -0600

    cgroup: fix cherry-pick/merge issue
    when picking this commit:
    
    916b6c67cd471fafa3e5e53ce8389c36749ed6f1
    cgroup: fix exit() vs rmdir() race
    
    .. a check for notify_on_release(cgrp) was added before notifying
    rmdir waiters.
    
    Change-Id: I8538d999672be20522375f41f889a387cfeb5bb5
    Signed-off-by: Chris Fries <cfries@motorola.com>
    Reviewed-on: http://gerrit.mot.com/697285
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/708958
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Sudharsan Yettapu <sudharsan.yettapu@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -392,8 +392,7 @@ static void free_css_set_work(struct work_struct *work)
 		 * rcu_read_lock is used to keep it alive.
 		 */
 		rcu_read_lock();
-		if (atomic_dec_and_test(&cgrp->count) &&
-		    notify_on_release(cgrp)) {
+		if (atomic_dec_and_test(&cgrp->count)) {
 			check_for_release(cgrp);
 			cgroup_wakeup_rmdir_waiter(cgrp);
 		}
