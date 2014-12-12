35730eba2bc6773e79cf4c2ec1da1f2e40758ff3
Ravikumar Vembu <raviv@motorola.com>
2014-12-11 19:09:14 -0600

    Revert "(CR): usb: phy: Disable udc irqs before vbus notifcation"
    This reverts commit c306a9c6a3d32d287eb7538922a366722e3fb926.
    
    http://gerrit.mot.com/#/c/697239/ takes care of this issue. Hence revert this.
    
    Change-Id: Ia98ccf8e671ac61eb681f159b95df7bfc9a069a5
    Reviewed-on: http://gerrit.mot.com/697308
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -2048,7 +2048,6 @@ static void msm_otg_start_peripheral(struct usb_otg *otg, int on)
 	if (!otg->gadget)
 		return;
 
-	disable_irq(motg->irq);
 	if (on) {
 		dev_dbg(otg->phy->dev, "gadget on\n");
 
@@ -2103,7 +2102,6 @@ static void msm_otg_start_peripheral(struct usb_otg *otg, int on)
 			}
 		}
 	}
-	enable_irq(motg->irq);
 }
 
 static int msm_otg_set_peripheral(struct usb_otg *otg,
