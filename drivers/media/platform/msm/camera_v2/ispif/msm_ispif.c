8e75fa23b3562ec70db71ddb3a2e8aac4b4f0b4b
Suman Mukherjee <sumam@codeaurora.org>
2014-12-17 10:00:49 +0530

    msm: camera: ispif: Validate vfe_intf parameter
    Validate vfe_intf parameter to avoid invalid register access.
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
    
    Bug: 19141503
    Change-Id: Ie0b57071cc5fca1c48d3a5e2e7819f9af9ff544c
    Signed-off-by: Suman Mukherjee <sumam@codeaurora.org>
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -52,8 +52,8 @@ static void msm_ispif_io_dump_reg(struct ispif_device *ispif)
 static inline int msm_ispif_is_intf_valid(uint32_t csid_version,
 	uint8_t intf_type)
 {
-	return (csid_version <= CSID_VERSION_V2 && intf_type != VFE0) ?
-		false : true;
+        return ((csid_version <= CSID_VERSION_V2 && intf_type != VFE0) ||
+                (intf_type >= VFE_MAX)) ? false : true;
 }
 
 static struct msm_cam_clk_info ispif_8960_clk_info[] = {
