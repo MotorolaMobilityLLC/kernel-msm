70bc6a9b040fe2616a26ec1c5d49977b0616e59e
maggie_lee <maggie_lee@asus.com>
2014-12-23 10:34:01 +0800

    anthias:sensors: add wellness flag to ecg driver using IOCTL
    Change-Id: Ib52e74e41922035a5187f568ff9c0de69f496457
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/155856
    Reviewed-by: Maggie Lee <maggie_lee@asus.com>
    Tested-by: Maggie Lee <maggie_lee@asus.com>

--- drivers/sensors/bmd101.c
+++ drivers/sensors/bmd101.c
@@ -18,6 +18,8 @@
 #include <linux/device.h>
 #include <asm/gpio.h>
 #include <asm/mach-types.h>
+#include <linux/proc_fs.h>
+#include <linux/ioctl.h>
 #include <linux/mutex.h>
 #include <linux/tty.h>
 #include <linux/sysfs.h>
@@ -32,6 +34,10 @@
 #include <linux/notifier.h>
 #include <linux/asus_utility.h>
 #endif
+
+#define ECG_MAJOR_NUM 0x71
+#define IOCTL_SET_WELLNESS_ENABLE _IOR(ECG_MAJOR_NUM, 0, int *)
+#define IOCTL_GET_WELLNESS_ENABLE _IOR(ECG_MAJOR_NUM, 1, int *)
 
 #define bmd101_uart			"/dev/ttyHSL1"
 #define bmd101_regulator		"8226_l28"
@@ -58,6 +64,7 @@ MODULE_PARM_DESC(debug, "Activate debugging output");
 
 #define BMD101_filter_array          10
 #define BMD101_setup_time		5
+#define BMD101_timeout_delay_default            300
 #define SENSOR_HEART_RATE_CONFIDENCE_NO_CONTACT		-1
 #define SENSOR_HEART_RATE_CONFIDENCE_UNRELIABLE		0
 #define SENSOR_HEART_RATE_CONFIDENCE_LOW			1
@@ -76,7 +83,7 @@ struct bpm_array{
 static struct sensors_classdev bmd101_cdev = {
 	.name = "bmd101",
 	.vendor = "NeuroSky",
-	.version = 1,
+	.version = 2,				//v1: sensor always on, v2: sensor controlled by HAL, default off, /proc/wellness has ioctl
 	.enabled = 0,
 	.sensors_enable = NULL,
 };
@@ -91,6 +98,7 @@ struct bmd101_data {
 	int accuracy;
 	int timeout_delay;
 	int low_power_mode;
+       unsigned int wellness_on;
 	struct mutex lock;
 	struct delayed_work timeout_work;
 	struct delayed_work suspend_resume_work;
@@ -146,20 +154,6 @@ static void bmd101_hw_enable(int enable) {
     		}
 		msleep(100);
 
-              rc = gpio_request(BMD101_RST_GPIO, "BMD101_RST");
-              if (rc) {
-	            pr_err("[bmd101] %s: Failed to request gpio %d\n", __func__, BMD101_RST_GPIO);
-	            goto gpio_rst_fail;
-	       }
-              rc = gpio_request(BMD101_CS_GPIO, "BMD101_CS");
-              if (rc) {
-	            pr_err("[bmd101] %s: Failed to request gpio %d\n", __func__, BMD101_CS_GPIO);
-	            goto gpio_cs_fail;
-              }
-
-              msm_gpiomux_write(BMD101_CS_GPIO, GPIOMUX_ACTIVE, &gpio_act_cfg, &gpio_sus_cfg);
-              msm_gpiomux_write(BMD101_RST_GPIO, GPIOMUX_ACTIVE, &gpio_act_cfg, &gpio_sus_cfg);
-
 		gpio_direction_output(BMD101_CS_GPIO, 0);
 		usleep(300);
 		gpio_direction_output(BMD101_CS_GPIO, 1);
@@ -175,17 +169,15 @@ static void bmd101_hw_enable(int enable) {
 		sensor_debug(DEBUG_VERBOSE, "[bmd101] %s: gpio %d and %d pulled hig, sensor hw(%d)\n", __func__, BMD101_CS_GPIO, BMD101_RST_GPIO, sensor_data->hw_enabled);
 	}
 	else if (!enable && sensor_data->hw_enabled) {
-              msm_gpiomux_write(BMD101_CS_GPIO, GPIOMUX_ACTIVE, &gpio_sus_cfg, &gpio_act_cfg);
-              msm_gpiomux_write(BMD101_RST_GPIO, GPIOMUX_ACTIVE, &gpio_sus_cfg, &gpio_act_cfg);
-
+		gpio_direction_output(BMD101_RST_GPIO, 0);
+		msleep(100);
+		gpio_direction_output(BMD101_CS_GPIO, 0);
+		msleep(100);
 		rc = regulator_disable(pm8921_l28);
     		if (rc) {
 			pr_err("%s: regulator_enable of 8921_l28 failed(%d)\n", __func__, rc);
 			goto reg_put_LDO28;
     		}
-
-		gpio_free(BMD101_RST_GPIO);
-		gpio_free(BMD101_CS_GPIO);
 
 		sensor_data->hw_enabled = 0;
 		sensor_debug(DEBUG_VERBOSE, "[bmd101] %s: gpio %d and %d pulled low, sensor hw(%d)\n", __func__, BMD101_CS_GPIO, BMD101_RST_GPIO, sensor_data->hw_enabled);
@@ -197,10 +189,6 @@ static void bmd101_hw_enable(int enable) {
 
 	return;
 
-	gpio_rst_fail:
-	gpio_free(BMD101_RST_GPIO);
-	gpio_cs_fail:
-	gpio_free(BMD101_CS_GPIO);
 	reg_put_LDO28:
 	regulator_put(pm8921_l28);
 
@@ -331,10 +319,8 @@ static int bmd101_enable_set(struct sensors_classdev *sensors_cdev, unsigned int
 {
 	sensor_debug(DEBUG_INFO, "[bmd101] %s: sensor currently %s, turning sensor %s\n", __func__, sensor_data->cdev.enabled? "on":"off", enable ? "on":"off");
 	if (enable && !sensor_data->cdev.enabled) {
-		if (!sensor_data->hw_enabled)
-			bmd101_hw_enable(1);
-
-		sensor_data->timeout_delay = 300;
+		bmd101_hw_enable(enable);
+		sensor_data->timeout_delay = BMD101_timeout_delay_default;
 		sensor_data->cdev.enabled = 1;
 	}
 	else if (!enable && sensor_data->cdev.enabled) {
@@ -342,11 +328,11 @@ static int bmd101_enable_set(struct sensors_classdev *sensors_cdev, unsigned int
 			printk("[bmd101] timeout cancelled.\n");
 			cancel_delayed_work(&sensor_data->timeout_work);
 		}
-			
+		bmd101_hw_enable(enable);
 		sensor_data->cdev.enabled = 0;
 		sensor_data->status = 0;
 		sensor_data->count = 0;
-		sensor_data->timeout_delay = 300;
+		sensor_data->timeout_delay = BMD101_timeout_delay_default;
 
 		sensor_debug(DEBUG_VERBOSE, "[bmd101] %s: bmd101_enable(%d)\n", __func__, sensor_data->cdev.enabled);
 	}
@@ -362,10 +348,7 @@ static void bmd101_suspend_resume_work(struct work_struct *work) {
 	if(sensor_data->low_power_mode) {
 		sensor_data->count = 0;
 		mutex_unlock(&sensor_data->lock);
-		bmd101_hw_enable(0);
 	}
-	else
-		bmd101_hw_enable(1);
 
 	sensor_debug(DEBUG_INFO, "[bmd101] %s low power mode\n", sensor_data->low_power_mode?"enter":"exit");
 }
@@ -397,6 +380,7 @@ static DEVICE_ATTR(status, S_IWUSR | S_IRUGO, sensors_status_show, sensors_statu
 //ASUS_BSP --- Maggie_Lee "Add ATD interface"
 
 //ASUS_BSP +++ Maggie_Lee "Add ESD interface"
+#ifndef ASUS_USER_BUILD
 static ssize_t bmd101_show_esd(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	sensor_debug(DEBUG_VERBOSE, "[bmd101] ESD = %d\n",sensor_data->esd);
@@ -416,6 +400,7 @@ static ssize_t bmd101_store_esd(struct device *dev, struct device_attribute *att
 	return size;
 }
 static DEVICE_ATTR(esd, S_IWUSR | S_IRUGO, bmd101_show_esd, bmd101_store_esd);
+#endif
 //ASUS_BSP --- Maggie_Lee "Add ESD interface"
 
 static int bmd101_show_bpm (struct device *dev, struct device_attribute *attr, char *buf)
@@ -439,6 +424,7 @@ static ssize_t bmd101_store_bpm(struct device *dev, struct device_attribute *att
 }
 static DEVICE_ATTR(bpm, S_IWUSR | S_IRUGO, bmd101_show_bpm, bmd101_store_bpm);
 
+#ifndef ASUS_USER_BUILD
 static int bmd101_show_hw_enable (struct device *dev, struct device_attribute *attr, char *buf)
 {
 	sensor_debug(DEBUG_VERBOSE, "[bmd101] sensor hw_enabled = %d\n",sensor_data->hw_enabled);
@@ -459,6 +445,7 @@ static ssize_t bmd101_store_hw_enable(struct device *dev, struct device_attribut
 	return size;
 }
 static DEVICE_ATTR(hw_enable, S_IWUSR | S_IRUGO, bmd101_show_hw_enable, bmd101_store_hw_enable);
+#endif
 
 static struct attribute *bmd101_attributes[] = {
 	&dev_attr_status.attr,			//atd interface
@@ -467,12 +454,59 @@ static struct attribute *bmd101_attributes[] = {
 	&dev_attr_esd.attr,				//esd interface
 	&dev_attr_hw_enable.attr,		//sensor hw enable/disable interface
 	#endif
-
 	NULL
 };
 
 static const struct attribute_group bmd101_attr_group = {
-	.attrs = bmd101_attributes,
+        .attrs = bmd101_attributes,
+};
+
+static int bmd101_proc_show_wellness(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%d\n", sensor_data->cdev.version);
+	return 0;
+}
+
+static int bmd101_proc_open_wellness(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, bmd101_proc_show_wellness, NULL);
+}
+
+static long bmd101_proc_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	void __user *data;
+	int value = 0;
+	int ret = 0;
+
+	data = (void __user *) arg;
+	ret = copy_from_user(&value, data, sizeof(int));
+	if(data==NULL) {
+		pr_err("[bmd101][ioctl] %s: null data!!\n", __func__);
+		return -EFAULT;
+	}
+	switch (cmd) {
+		case IOCTL_SET_WELLNESS_ENABLE:
+			sensor_debug(DEBUG_INFO, "[bmd101] %s setting wellness (%d)\n", __func__, value);
+			sensor_data->wellness_on = value;
+			break;
+		case IOCTL_GET_WELLNESS_ENABLE:
+			sensor_debug(DEBUG_INFO, "[bmd101] %s getting wellness (%d)\n", __func__, value);
+			ret = __put_user(sensor_data->wellness_on, (int __user *)arg);
+			break;
+		default:
+			sensor_debug(DEBUG_INFO, "[bmd101] %s, unknown cmd(0x%x)\n", __func__, cmd);
+			break;
+	}
+
+	return ret;
+}
+
+static const struct file_operations proc_bmd101_operations = {
+	.owner		= THIS_MODULE,
+	.unlocked_ioctl = bmd101_proc_ioctl,
+	.read		= seq_read,
+	.open		= bmd101_proc_open_wellness,
+	.release	= single_release,
 };
 
 #ifdef CONFIG_ASUS_UTILITY
@@ -544,14 +578,15 @@ static int bmd101_probe(struct platform_device *pdev)
 	sensor_data = kzalloc(sizeof(struct bmd101_data),GFP_KERNEL);
 	if(!sensor_data)
 	{
-		printk(KERN_ERR "%s: failed to allocate bmd101_data\n", __func__);
-		return -ENOMEM;
+		pr_err("[BMD101] %s: failed to allocate bmd101_data\n", __func__);
+		goto allocate_data_fail;
 	}
 	sensor_data->esd = 0;
 	sensor_data->bpm = 0;
 	sensor_data->accuracy = 0;
 	sensor_data->hw_enabled = 0;
-	sensor_data->timeout_delay = 300;
+	sensor_data->wellness_on = 0;
+	sensor_data->timeout_delay = BMD101_timeout_delay_default;
 	sensor_data->cdev = bmd101_cdev;
 	sensor_data->cdev.enabled = 0;
 	sensor_data->cdev.sensors_enable = bmd101_enable_set;
@@ -563,7 +598,7 @@ static int bmd101_probe(struct platform_device *pdev)
 	
 	ret = sensors_classdev_register(&pdev->dev, &sensor_data->cdev);
 	if (ret) {
-		pr_err("[BMD101] class device create failed: %d\n", ret);
+		pr_err("[BMD101] %s: class device create failed: %d\n", __func__, ret);
 		goto classdev_register_fail;
 	}
 
@@ -571,9 +606,11 @@ static int bmd101_probe(struct platform_device *pdev)
 	if (ret)
 		goto classdev_register_fail;
 
+	proc_create("wellness", 0, NULL, &proc_bmd101_operations);
+
 	ret = bmd101_input_init();
 	if (ret < 0) {
-		pr_err("[BMD101] init input device failed: %d\n", ret);
+		pr_err("[BMD101] %s: init input device failed: %d\n", __func__, ret);
 		goto input_init_fail;
 	}
 	
@@ -583,21 +620,39 @@ static int bmd101_probe(struct platform_device *pdev)
 
 	pm8921_l28 = regulator_get(&pdev->dev, bmd101_regulator);
 	if (IS_ERR(pm8921_l28)) {
-        	pr_err("%s: regulator get of 8921_l28 failed (%ld)\n", __func__, PTR_ERR(pm8921_l28));
+        	pr_err("[bmd101] %s: regulator get of 8921_l28 failed (%ld)\n", __func__, PTR_ERR(pm8921_l28));
 		ret = PTR_ERR(pm8921_l28);
 		goto regulator_get_fail;
 	}
 	
     	ret = regulator_set_voltage(pm8921_l28, 2850000, 2850000);
     	if (ret) {
-		pr_err("%s: regulator_set_voltage of 8921_l28 failed(%d)\n", __func__, ret);
+		pr_err("[bmd101] %s: regulator_set_voltage of 8921_l28 failed(%d)\n", __func__, ret);
 		goto reg_put_LDO28;
     	}
+
+    	ret = gpio_request(BMD101_RST_GPIO, "BMD101_RST");
+    	if (ret) {
+		pr_err("[bmd101] %s: Failed to request gpio %d\n", __func__, BMD101_RST_GPIO);
+		goto gpio_rst_fail;
+    	}
+    	ret = gpio_request(BMD101_CS_GPIO, "BMD101_CS");
+    	if (ret) {
+		pr_err("[bmd101] %s: Failed to request gpio %d\n", __func__, BMD101_CS_GPIO);
+		goto gpio_cs_fail;
+    	}
+
+    	msm_gpiomux_write(BMD101_CS_GPIO, GPIOMUX_ACTIVE, &gpio_act_cfg, &gpio_sus_cfg);
+    	msm_gpiomux_write(BMD101_RST_GPIO, GPIOMUX_ACTIVE, &gpio_act_cfg, &gpio_sus_cfg);
 
 	sensor_debug(DEBUG_INFO, "[bmd101] %s: ---\n", __func__);
 
 	return 0;
 
+	gpio_cs_fail:
+	gpio_free(BMD101_CS_GPIO);
+	gpio_rst_fail:
+	gpio_free(BMD101_RST_GPIO);
 	reg_put_LDO28:
 	regulator_put(pm8921_l28);
 	regulator_get_fail:
@@ -605,6 +660,7 @@ static int bmd101_probe(struct platform_device *pdev)
 	input_init_fail:
 	classdev_register_fail:
 	kfree(sensor_data);
+	allocate_data_fail:
 
 	return ret;
 
@@ -616,6 +672,8 @@ static int bmd101_remove(struct platform_device *pdev)
 	#ifdef CONFIG_ASUS_UTILITY
 	unregister_mode_notifier(&display_mode_notifier);
 	#endif
+	gpio_free(BMD101_RST_GPIO);
+	gpio_free(BMD101_CS_GPIO);
 
 	return 0;
 }
