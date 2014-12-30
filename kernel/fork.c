228d63fd9a2a03353c5910f53d2f10357a034006
Yi-wei Zhao <gbjc64@motorola.com>
2014-12-30 17:41:53 -0600

    kernel: support task's adj rbtree
    Add (or del) a task to (or from) task's adj rbtree
    when a task is created or exit.
    
    Change-Id: Ic63e03355a1fed8c500097bad223c59c742a2346
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/701207
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1540,6 +1540,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 			attach_pid(p, PIDTYPE_SID, task_session(current));
 			list_add_tail(&p->sibling, &p->real_parent->children);
 			list_add_tail_rcu(&p->tasks, &init_task.tasks);
+			add_2_adj_tree(p);
 			__this_cpu_inc(process_counts);
 		} else {
 			list_add_tail_rcu(&p->thread_node,
