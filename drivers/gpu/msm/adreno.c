f3e3419c19324eab53a3b7864c171c2c8bcca213
Hareesh Gundu <hareeshg@codeaurora.org>
2015-01-05 14:32:18 +0530

    msm: kgsl: Skip soft reset for A304
    A304 gpu is not able to update the TTBR0 register via the
    GPU self-programming path after soft reset. This will result
    into NOC error in GPU fault recovery path. To avoid this
    use hard reset path for A304.
    
    CRs-Fixed: 775197
    Change-Id: I9663780ea601b7b5e8780111f286dec1950e5c25
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    (cherry picked from commit fa00d5639115a8b714ef5e29cffe36e34edd62c2)

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1339,8 +1339,12 @@ int adreno_reset(struct kgsl_device *device)
 	/* clear pending vbif transactions before reset */
 	adreno_vbif_clear_pending_transactions(device);
 
-	/* Try soft reset first, for non mmu fault case only */
-	if (!atomic_read(&mmu->fault)) {
+	/*
+	 * Try soft reset first, for non mmu fault case only.
+	 * Skip soft reset and use hard reset for A304 GPU, As
+	 * A304 is not able to do SMMU programming after soft reset.
+	 */
+	if (!atomic_read(&mmu->fault) && !adreno_is_a304(adreno_dev)) {
 		ret = adreno_soft_reset(device);
 		if (ret)
 			KGSL_DEV_ERR_ONCE(device, "Device soft reset failed\n");
