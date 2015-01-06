f09dd7285636070c155292347155f90c655489fa
Patrick Tjin <pattjin@google.com>
2015-01-06 09:19:39 -0800

    Fix sizeof-pointer-memaccess warnings
    Changed to use strcmp since sizeof buffer is not known.
    Changed sizeof to PAGE_SIZE in snprintf.
    
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/video/msm/mipi_novatek.c
+++ drivers/video/msm/mipi_novatek.c
@@ -680,7 +680,7 @@ static ssize_t mipi_dsi_3d_barrier_read(struct device *dev,
 				struct device_attribute *attr,
 				char *buf)
 {
-	return snprintf((char *)buf, sizeof(buf), "%u\n", barrier_mode);
+	return snprintf((char *)buf, PAGE_SIZE, "%u\n", barrier_mode);
 }
 
 static ssize_t mipi_dsi_3d_barrier_write(struct device *dev,
