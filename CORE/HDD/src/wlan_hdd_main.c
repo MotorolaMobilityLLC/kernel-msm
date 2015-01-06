23eb15a2a231a828837339d7657feab229f47d8b
c_hpothu <c_hpothu@qti.qualcomm.com>
2015-01-06 18:19:47 +0530

    wlan: Allow suspend when WoWL is enabled.
    When WoWL is enabled, APPs is not entering to suspend state.
    As WoWL works on both BMPS and FULL power mode, allow APPs to power
    collapse.
    
    Change-Id: Iab6e402e07bb8658efe259abb06af8cf99336405
    CRs-Fixed: 777836

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -10288,7 +10288,8 @@ v_BOOL_t hdd_is_apps_power_collapse_allowed(hdd_context_t* pHddCtx)
         {
             if (((pConfig->fIsImpsEnabled || pConfig->fIsBmpsEnabled)
                  && (pmcState != IMPS && pmcState != BMPS && pmcState != UAPSD
-                  &&  pmcState != STOPPED && pmcState != STANDBY)) ||
+                  &&  pmcState != STOPPED && pmcState != STANDBY &&
+                      pmcState != WOWL)) ||
                  (eANI_BOOLEAN_TRUE == scanRspPending) ||
                  (eANI_BOOLEAN_TRUE == inMiddleOfRoaming))
             {
