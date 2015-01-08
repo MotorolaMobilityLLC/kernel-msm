bdfa0df9847e9a7c53843f76e2796e03969ec97a
SeungYoon Lee <w22905@motorola.com>
2015-01-08 16:23:31 -0600

    ASoC: msm8x16-wcd: set BUS_DOWN on device_up
    QC's new code introduce BUS_DOWN bit set and clear on SSR,
    to prevent codec register writing when device is offline.
    However it wasn't integrated correctly and couldn't be cleared.
    it prevents all codec writing once device is down in any reason.
    
    Change-Id: Iabf00d8d2936ee76b2836508d7b689977f84ceee
    Signed-off-by: SeungYoon Lee <w22905@motorola.com>
    Reviewed-on: http://gerrit.mot.com/702387
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Vladimir Karpovich <vkarpovich@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -5191,17 +5191,13 @@ static int msm8x16_wcd_device_down(struct snd_soc_codec *codec)
 
 static int msm8x16_wcd_device_up(struct snd_soc_codec *codec)
 {
-#ifndef CONFIG_SND_SOC_FSA8500
 	struct msm8x16_wcd_priv *msm8x16_wcd_priv =
 		snd_soc_codec_get_drvdata(codec);
-#endif
 	dev_dbg(codec->dev, "%s: device up!\n", __func__);
 
 	mutex_lock(&codec->mutex);
 
-#ifndef CONFIG_SND_SOC_FSA8500
 	clear_bit(BUS_DOWN, &msm8x16_wcd_priv->status_mask);
-#endif
 	snd_soc_card_change_online_state(codec->card, 1);
 	/* delay is required to make sure sound card state updated */
 	usleep_range(5000, 5100);
