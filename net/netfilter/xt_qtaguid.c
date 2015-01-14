a77b40ccd06c90b59e98ae8c47799d5117040e9a
Mohamad Ayyash <mkayyash@google.com>
2015-01-13 19:20:44 -0800

    xt_qtaguid: Use sk_callback_lock read locks before reading sk->sk_socket
    It prevents a kernel panic when accessing sk->sk_socket fields due to NULLing sk->sk_socket when sock_orphan is called through
    sk_common_release.
    
    Change-Id: I4aa46b4e2d8600e4d4ef8dcdd363aa4e6e5f8433
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>
    (cherry picked from commit cdea0ebcb8bcfe57688f6cb692b49e550ebd9796)
    
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Change-Id: Ibfbf6d5a3bf58399358ad4d5c2f5e3b2ce0c486e
    Reviewed-on: http://gerrit.mot.com/730573
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -1652,6 +1652,7 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	struct sock *sk;
 	uid_t sock_uid;
 	bool res;
+	bool set_sk_callback_lock = false;
 	/*
 	 * TODO: unhack how to force just accounting.
 	 * For now we only do tag stats when the uid-owner is not requested
@@ -1714,6 +1715,8 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	MT_DEBUG("qtaguid[%d]: sk=%p got_sock=%d fam=%d proto=%d\n",
 		 par->hooknum, sk, got_sock, par->family, ipx_proto(skb, par));
 	if (sk != NULL) {
+		set_sk_callback_lock = true;
+		read_lock_bh(&sk->sk_callback_lock);
 		MT_DEBUG("qtaguid[%d]: sk=%p->sk_socket=%p->file=%p\n",
 			par->hooknum, sk, sk->sk_socket,
 			sk->sk_socket ? sk->sk_socket->file : (void *)-1LL);
@@ -1785,6 +1788,8 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 put_sock_ret_res:
 	if (got_sock)
 		xt_socket_put_sk(sk);
+	if (set_sk_callback_lock)
+		read_unlock_bh(&sk->sk_callback_lock);
 ret_res:
 	MT_DEBUG("qtaguid[%d]: left %d\n", par->hooknum, res);
 	return res;
