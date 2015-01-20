02557326cb86d08ec0c9ac3851b04849df4f187c
Bryan Knottek <ebk012@motorola.com>
2015-01-20 13:58:16 -0500

    msm: camera: support factory intf
    This is a merge of the following gerrit, as well
    as an extension of the same work to the new
    msm flash driver architecture.
    
    (CR): msm: camera: extend flash intf for factory
    
    Factory production requires direct i2c access to camera flash HW.
    
    Change-Id: I8eb6363d3dec50cda9b20c4789f9e9671e247761
    Signed-off-by: Bryan Knottek <ebk012@motorola.com>
    Reviewed-on: http://gerrit.mot.com/705976
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Dave Schmidt <d.schmidt@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/730365
    SME-Granted: SME Approvals Granted
    Reviewed-by: Bryan Knottek <knottek@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -54,6 +54,98 @@ static struct msm_camera_i2c_fn_t msm_sensor_cci_func_tbl = {
 	.i2c_poll =  msm_camera_cci_i2c_poll,
 };
 
+static int32_t flash_cci_config(struct msm_flash_ctrl_t  *fctrl,
+	void __user *argp)
+{
+	struct sensorb_cfg_data *cdata = (struct sensorb_cfg_data *)argp;
+	int32_t rc = 0;
+
+	CDBG("%s:%d  cfgtype = %d\n", __func__, __LINE__, cdata->cfgtype);
+
+	switch (cdata->cfgtype) {
+	case CFG_WRITE_I2C_ARRAY: {
+		struct msm_camera_i2c_reg_setting conf_array;
+		struct msm_camera_i2c_reg_array *reg_setting = NULL;
+
+		if (copy_from_user(&conf_array,
+				(void *)cdata->cfg.setting,
+				sizeof(struct msm_camera_i2c_reg_setting))) {
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
+
+		if (!conf_array.size) {
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
+
+		reg_setting = kzalloc(conf_array.size *
+				(sizeof(struct msm_camera_i2c_reg_array)),
+				GFP_KERNEL);
+		if (!reg_setting) {
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -ENOMEM;
+			break;
+		}
+		if (copy_from_user(reg_setting, (void *)conf_array.reg_setting,
+				conf_array.size *
+				sizeof(struct msm_camera_i2c_reg_array))) {
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			kfree(reg_setting);
+			rc = -EFAULT;
+			break;
+		}
+
+		conf_array.reg_setting = reg_setting;
+		rc = fctrl->flash_i2c_client.i2c_func_tbl->i2c_write_table(
+				&fctrl->flash_i2c_client, &conf_array);
+		if (rc < 0)
+			pr_err("%s:%d: i2c_write failed\n", __func__, __LINE__);
+		kfree(reg_setting);
+		break;
+	}
+
+	case CFG_SLAVE_READ_I2C: {
+		struct msm_camera_i2c_read_config read_config;
+		uint16_t local_data = 0;
+
+		if (copy_from_user(&read_config,
+			(void *)cdata->cfg.setting,
+			sizeof(struct msm_camera_i2c_read_config))) {
+			pr_err("%s:%d failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
+
+		rc = fctrl->flash_i2c_client.i2c_func_tbl->i2c_read(
+			&fctrl->flash_i2c_client,
+			read_config.reg_addr,
+			&local_data, read_config.data_type);
+		if (rc < 0) {
+			pr_err("%s:%d: i2c_read failed\n", __func__, __LINE__);
+			break;
+		}
+		read_config.data = local_data;
+		if (copy_to_user((void __user *)cdata->cfg.setting,
+			(void *)&read_config,
+			sizeof(struct msm_camera_i2c_read_config))) {
+			pr_err("%s:%d copy failed\n", __func__, __LINE__);
+			rc = -EFAULT;
+			break;
+		}
+		break;
+	}
+
+	default:
+		rc = -EFAULT;
+		break;
+	}
+
+	return rc;
+}
+
 void msm_torch_brightness_set(struct led_classdev *led_cdev,
 				enum led_brightness value)
 {
@@ -655,6 +747,8 @@ static long msm_flash_subdev_ioctl(struct v4l2_subdev *sd,
 			*(int *)argp = CFG_FLASH_RELEASE;
 			return msm_flash_config(fctrl, argp);
 		}
+	case VIDIOC_MSM_SENSOR_CFG:
+		return flash_cci_config(fctrl, argp);
 	default:
 		pr_err_ratelimited("invalid cmd %d\n", cmd);
 		return -ENOIOCTLCMD;
