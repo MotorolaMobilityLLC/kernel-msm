c23313199be5ccc5d0040b4848fbf8e545eaef7f
Gabriel Burca <Gabriel.Burca@motorola.com>
2015-01-27 11:05:21 -0600

    drivers/misc/stml0xx: Handle swapped accel parts
    - On dual-accel phones, the camera gesture algorithm expects accel 1 to
      be on the left and accel 2 on the right. We allow the AP to inform the
      sensor hub if the accel parts are swapped.
    
    Change-Id: Id02c61f17896b82455836a0c988c9424d0498788
    Signed-off-by: Gabriel Burca <Gabriel.Burca@motorola.com>
    Reviewed-on: http://gerrit.mot.com/708404
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Daniel Moore <dtmoore@motorola.com>
    Reviewed-by: Jason Jae Hyun Koh <jasonkoh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/stml0xx/stml0xx_core.c
+++ drivers/misc/stml0xx/stml0xx_core.c
@@ -456,6 +456,10 @@ static struct stml0xx_platform_data *stml0xx_of_init(struct spi_device *spi)
 	of_property_read_u32(np, "accel_orientation_2",
 			     &pdata->accel_orientation_2);
 
+	pdata->accel_swap = 0;
+	of_property_read_u32(np, "accel_swap",
+			     &pdata->accel_swap);
+
 	return pdata;
 }
 #else
