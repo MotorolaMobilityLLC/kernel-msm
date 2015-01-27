e4953deaa5a0e6e315d9431fa365aed5293705f8
Greg Meiste <w30289@motorola.com>
2015-01-27 12:02:51 -0600

    tty: serial: msm_serial_hs: Fix flow control management
    When disabling the clock, the driver was incorrectly turning flow
    control back on. This resulted in lost UART messages since the slave
    device would send them before the host was ready.
    
    Flow control management was removed completely from the clock control
    functions since it is already handled by the mctrl function.
    
    Change-Id: Id7a8bb5801ea48df087190b45c21442bbb88dda8
    Signed-off-by: Greg Meiste <w30289@motorola.com>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2026,14 +2026,8 @@ static int msm_hs_check_clock_off(struct uart_port *uport)
 	if (msm_uport->clk_state != MSM_HS_CLK_REQUEST_OFF ||
 	    !uart_circ_empty(tx_buf) || msm_uport->tx.dma_in_flight ||
 	    msm_uport->imr_reg & UARTDM_ISR_TXLEV_BMSK) {
-		if (msm_uport->clk_state == MSM_HS_CLK_REQUEST_OFF) {
+		if (msm_uport->clk_state == MSM_HS_CLK_REQUEST_OFF)
 			msm_uport->clk_state = MSM_HS_CLK_ON;
-			/*
-			 * Enable flow control that
-			 * we disabled in request clock off
-			 */
-			msm_hs_enable_flow_control(uport);
-		}
 		spin_unlock_irqrestore(&uport->lock, flags);
 		mutex_unlock(&msm_uport->clk_mutex);
 		MSM_HS_DBG("%s(): clkstate %d", __func__, msm_uport->clk_state);
@@ -2063,7 +2057,6 @@ static int msm_hs_check_clock_off(struct uart_port *uport)
 	}
 
 	spin_unlock_irqrestore(&uport->lock, flags);
-	msm_hs_enable_flow_control(uport);
 
 	/* we really want to clock off */
 	mutex_unlock(&msm_uport->clk_mutex);
@@ -2242,7 +2235,6 @@ void msm_hs_request_clock_off(struct uart_port *uport) {
 	if (msm_uport->clk_state == MSM_HS_CLK_ON) {
 		msm_uport->clk_state = MSM_HS_CLK_REQUEST_OFF;
 
-		msm_hs_disable_flow_control(uport);
 		data = msm_hs_read(uport, UART_DM_SR);
 		MSM_HS_DBG("%s(): TXEMT, queuing clock off work\n",
 			__func__);
@@ -2273,10 +2265,8 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 	mutex_lock(&msm_uport->clk_mutex);
 	spin_lock_irqsave(&uport->lock, flags);
 
-	if (cur_clk_state == MSM_HS_CLK_REQUEST_OFF) {
+	if (cur_clk_state == MSM_HS_CLK_REQUEST_OFF)
 		msm_uport->clk_state = MSM_HS_CLK_ON;
-		msm_hs_enable_flow_control(uport);
-	}
 
 	switch (cur_clk_state) {
 	case MSM_HS_CLK_OFF:
