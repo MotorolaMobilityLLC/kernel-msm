8cced1b5b53acc99496061fbde3fd76f452ea9c9
Venkatesh S G <sgvenkat@motorola.com>
2015-02-13 17:49:50 +0530

    board: 8960: Increase MM ION heap to 60MB
    GTS 2.0 WV DASH test fails due to memory shortage.
    
    QCOM SR 01867497 - Need to increase the memory for L upgrade in
    A family for GTS as per Qcom recommendation. The increments need
    to be in 4MB. With 4MB increase to 0x3C00000 from 0x3800000 fixes 
    the GTS issue.
    
    Change-Id: Ie916893aaace2204fc60e0f2e1b17bf19cba7088
    Signed-off-by: Venkatesh S G <sgvenkat@motorola.com>
    Reviewed-on: http://gerrit.mot.com/715227
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -154,7 +154,7 @@ struct sx150x_platform_data msm8960_sx150x_data[] = {
 #define HOLE_SIZE	0x20000
 #define MSM_CONTIG_MEM_SIZE  0x65000
 #ifdef CONFIG_MSM_IOMMU
-#define MSM_ION_MM_SIZE            0x3800000 /* Need to be multiple of 64K */
+#define MSM_ION_MM_SIZE            0x3C00000 /* Need to be multiple of 64K */
 #define MSM_ION_SF_SIZE            0x0
 #define MSM_ION_QSECOM_SIZE        0x780000 /* (7.5MB) */
 #define MSM_ION_HEAP_NUM	8
