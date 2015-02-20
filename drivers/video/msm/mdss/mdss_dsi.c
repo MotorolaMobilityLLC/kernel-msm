e28851b56c77bfeb5d88c6555b434df6b59d3506
Shiyong Li <a22381@motorola.com>
2015-02-20 11:38:26 -0800

    msm/mdss: fix power mode setting race for disp_util
    Set power mode with cmd_mutex protection of mdss_dsi_cmdlist_put() to
    avoid race condition.
    
    Change-Id: I360aaf656ebce1a7bb32406aea8a5350ee859d87
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: http://gerrit.mot.com/717531
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1326,8 +1326,6 @@ int mdss_dsi_ioctl_handler(struct mdss_panel_data *pdata, u32 cmd, void *arg)
 {
 	int rc = -ENOSYS;
 	struct msmfb_reg_access reg_access;
-	int old_tx_mode;
-	int mode = DSI_MODE_BIT_LP;
 
 	if (mdss_panel_is_power_off(pdata->panel_info.panel_power_state)) {
 		pr_err("%s: Panel is off\n", __func__);
@@ -1340,17 +1338,7 @@ int mdss_dsi_ioctl_handler(struct mdss_panel_data *pdata, u32 cmd, void *arg)
 		if (copy_from_user(&reg_access, arg, sizeof(reg_access)))
 			return -EFAULT;
 
-		if (reg_access.use_hs_mode)
-			mode = DSI_MODE_BIT_HS;
-
-		old_tx_mode = mdss_dsi_get_tx_power_mode(pdata);
-		if (mode != old_tx_mode)
-			mdss_dsi_set_tx_power_mode(mode, pdata);
-
 		rc = mdss_dsi_panel_ioctl_handler(pdata, cmd, arg);
-
-		if (mode != old_tx_mode)
-			mdss_dsi_set_tx_power_mode(old_tx_mode, pdata);
 		break;
 	default:
 		pr_err("%s: unsupport ioctl =0x%x\n", __func__, cmd);
