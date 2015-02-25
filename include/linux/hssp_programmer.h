096d126f77bf315c9b8e43d73b124f8f0af781fd
Vladimir Karpovich <vkarpovich@motorola.com>
2015-02-25 11:08:49 -0600

    misc: capsense: Add programming notification
    Add programming notification interface to help
    sensor hub driver avoid I2c communication with CapSense during
    firmware checking and programming.
    Also HW reset was added after FW verification.
    
    Change-Id: I895d68f9965acb2aadfb788e191f493b6f6aaa5e
    Signed-off-by: Vladimir Karpovich <vkarpovich@motorola.com>
    Reviewed-on: http://gerrit.mot.com/719013
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/linux/hssp_programmer.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/notifier.h>
+
+#if IS_ENABLED(CONFIG_CYPRESS_CAPSENSE_HSSP)
+void register_hssp_update_notify(struct notifier_block *nb);
+void unregister_hssp_update_notify(struct notifier_block *nb);
+#else
+static inline void register_hssp_update_notify(struct notifier_block *nb) { }
+static inline void unregister_hssp_update_notify(struct notifier_block *nb) { }
+#endif
+
+#define HSSP_START 0
+#define HSSP_STOP 1
