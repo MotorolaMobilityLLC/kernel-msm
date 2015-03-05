e5d1bf2d170417ef47821f892f31fb967c7ed466
choongryeol.lee <choongryeol.lee@lge.com>
2015-03-05 10:22:04 -0800

    ARM64: bullhead: add lge specific drivers
    Add lge specific drivers to drivers/soc/qcom/lge
    
    Change-Id: I49dbd64833685dadbb64dabba3a3fc36a0570b8d
    Signed-off-by: choongryeol.lee <choongryeol.lee@lge.com>

---
+++ drivers/soc/qcom/lge/devices_lge.c
@@ -0,0 +1,113 @@
+/* Copyright (c) 2015, LGE Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <soc/qcom/lge/board_lge.h>
+#include <linux/of.h>
+#include <linux/of_fdt.h>
+#include <linux/platform_device.h>
+
+static enum hw_rev_type lge_bd_rev = HW_REV_MAX;
+
+/* CAUTION: These strings are come from LK. */
+char *rev_str[] = {"evb1", "evb2", "evb3", "rev_0", "rev_a", "rev_b", "rev_c",
+	"rev_d", "rev_e", "rev_f", "rev_g", "rev_10", "rev_11", "rev_12",
+	"reserved"};
+
+static int __init board_revno_setup(char *rev_info)
+{
+	int i;
+
+	for (i = 0; i < HW_REV_MAX; i++) {
+		if (!strncmp(rev_info, rev_str[i], 6)) {
+			lge_bd_rev = i;
+			break;
+		}
+	}
+
+	pr_info("BOARD : LGE %s\n", rev_str[lge_bd_rev]);
+	return 1;
+}
+__setup("lge.rev=", board_revno_setup);
+
+enum hw_rev_type lge_get_board_revno(void)
+{
+	return lge_bd_rev;
+}
+
+/*
+   for download complete using LAF image
+   return value : 1 --> right after laf complete & reset
+ */
+
+int android_dlcomplete = 0;
+
+int __init lge_android_dlcomplete(char *s)
+{
+	if (strncmp(s, "1", 1) == 0)
+		android_dlcomplete = 1;
+	else
+		android_dlcomplete = 0;
+	pr_info("androidboot.dlcomplete = %d\n", android_dlcomplete);
+
+	return 1;
+}
+__setup("androidboot.dlcomplete=", lge_android_dlcomplete);
+
+int lge_get_android_dlcomplete(void)
+{
+	return android_dlcomplete;
+}
+
+static enum lge_laf_mode_type lge_laf_mode = LGE_LAF_MODE_NORMAL;
+
+int __init lge_laf_mode_init(char *s)
+{
+	if (strcmp(s, ""))
+		lge_laf_mode = LGE_LAF_MODE_LAF;
+
+	return 1;
+}
+__setup("androidboot.laf=", lge_laf_mode_init);
+
+enum lge_laf_mode_type lge_get_laf_mode(void)
+{
+	return lge_laf_mode;
+}
+
+static int lge_boot_reason = -1; /* undefined for error checking */
+static int __init lge_check_bootreason(char *reason)
+{
+	int ret = 0;
+
+	/* handle corner case of kstrtoint */
+	if (!strcmp(reason, "0xffffffff")) {
+		lge_boot_reason = 0xffffffff;
+		return 1;
+	}
+
+	ret = kstrtoint(reason, 16, &lge_boot_reason);
+	if (!ret)
+		printk(KERN_INFO "LGE REBOOT REASON: %x\n", lge_boot_reason);
+	else
+		printk(KERN_INFO "LGE REBOOT REASON: Couldn't get bootreason - %d\n",
+				ret);
+
+	return 1;
+}
+__setup("lge.bootreason=", lge_check_bootreason);
+
+int lge_get_bootreason(void)
+{
+	return lge_boot_reason;
+}
---
+++ include/soc/qcom/lge/board_lge.h
@@ -0,0 +1,50 @@
+/* Copyright (c) 2015, LGE Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __ASM_ARCH_MSM_BOARD_LGE_H
+#define __ASM_ARCH_MSM_BOARD_LGE_H
+
+enum hw_rev_type {
+	HW_REV_EVB1 = 0,
+	HW_REV_EVB2,
+	HW_REV_EVB3,
+	HW_REV_0,
+	HW_REV_A,
+	HW_REV_B,
+	HW_REV_C,
+	HW_REV_D,
+	HW_REV_E,
+	HW_REV_F,
+	HW_REV_G,
+	HW_REV_1_0,
+	HW_REV_1_1,
+	HW_REV_1_2,
+	HW_REV_MAX
+};
+
+extern char *rev_str[];
+
+enum hw_rev_type lge_get_board_revno(void);
+
+enum lge_laf_mode_type {
+	LGE_LAF_MODE_NORMAL = 0,
+	LGE_LAF_MODE_LAF,
+};
+
+enum lge_laf_mode_type lge_get_laf_mode(void);
+
+enum lge_boot_mode_type lge_get_boot_mode(void);
+int lge_get_android_dlcomplete(void);
+int get_lge_frst_status(void);
+
+extern int lge_get_bootreason(void);
+#endif
