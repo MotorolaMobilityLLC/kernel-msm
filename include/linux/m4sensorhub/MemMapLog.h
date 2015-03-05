9793786b59b0435b6d09f7b032b30cadcc1434ad
Viditha Hanumanthareddy <ngjq36@motorola.com>
2015-03-05 15:27:57 -0600

    IKXCLOCK-5462: Fix log_level sysfs to support all logtags
    Change-Id: I319e6a61d0cce0c8d8a669a923266fe8f4d2767e

--- include/linux/m4sensorhub/MemMapLog.h
+++ include/linux/m4sensorhub/MemMapLog.h
@@ -9,13 +9,20 @@ Purpose :
 #ifndef __MEMMAP_LOG_H__
 #define __MEMMAP_LOG_H__
 /****************************** Defines *******************************/
+#define LOG_EN_SIZE 2
+/* No of log tags per element of the log enable array ( 32 bits in 1 log enable element, 2 bits per tag, hence 32/2 = 16 ) */
+#define LOG_TAGS_PER_ENABLE 16
+#define LOG_MAX 24
+#define LOG_NO_OF_BITS_PER_TAG 2
+/* this is set to 0x03 since each logtag uses 2 bits in logenable for is depth */
+#define LOG_TAG_MASK 0x03
+
 struct memMapLog
 {
-  u64 logEnable;
+  u32 logEnable[LOG_EN_SIZE];
   u8 isLogImmediate;
 };
 
-#define LOG_MAX 24
 #define LOG_LEVELS_MAX 4
 
 static char acLogTags[LOG_MAX][40] = {
@@ -39,9 +46,10 @@ static char acLogTags[LOG_MAX][40] = {
   "LOG_DISP",
   "LOG_WRIST",
   "LOG_PASSIVE",
-  "LOG_EMG"
+  "LOG_EMG",
   "LOG_HR",
-  "LOG_ALS"
+  "LOG_ALS",
+  "LOG_ADS"
 };
 
 static char acLogLevels[LOG_LEVELS_MAX][15] = {
