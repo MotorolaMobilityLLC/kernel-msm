9062f9b2f37661b659941433cea4f2b904bf252b
Hong-Mei Li <a21834@motorola.com>
2015-03-06 16:46:06 -0600

    fs: support task's adj rbtree
    Add (or del) a task to (or from) task's adj rbtree
    when its oom_score_adj is modified.
    
    Change-Id: I998fdee33dd83c1bf67d6797de3a41de19ccd511
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -919,7 +919,9 @@ static ssize_t oom_adj_write(struct file *file, const char __user *buf,
 		  current->comm, task_pid_nr(current), task_pid_nr(task),
 		  task_pid_nr(task));
 
+	delete_from_adj_tree(task);
 	task->signal->oom_score_adj = oom_adj;
+	add_2_adj_tree(task);
 	trace_oom_score_adj_update(task);
 err_sighand:
 	unlock_task_sighand(task, &flags);
@@ -1005,7 +1007,10 @@ static ssize_t oom_score_adj_write(struct file *file, const char __user *buf,
 		goto err_sighand;
 	}
 
+	delete_from_adj_tree(task);
 	task->signal->oom_score_adj = (short)oom_score_adj;
+	add_2_adj_tree(task);
+
 	if (has_capability_noaudit(current, CAP_SYS_RESOURCE))
 		task->signal->oom_score_adj_min = (short)oom_score_adj;
 	trace_oom_score_adj_update(task);
