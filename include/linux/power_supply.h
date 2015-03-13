2da6ca21b5cd3f8013d3112aa692f1892df19e6f
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2015-03-13 12:58:47 -0700

    power: power-supply: add property POWER_SUPPLY_PROP_ALLOW_DETECTION
    There are situations when one needs to notify involved power supply
    nodes that type detection is about to commence and they should
    modify their state and hardware to allow it.
    
    For example, the D+/D- lines are shared by USB Phy and the Charger
    driver where the phy pullsdown the D+/D- lines. When USB is inserted
    the charger power supply node will need to notify the USB power supply
    node to release the pulldowns and allow for type detection.
    
    This property serves as that indication.
    
    Also, create a helper function to set this property.
    
    Change-Id: I67f14de81214f0bed1bcccf71e1c28b8e5212359
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/764197
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -197,6 +197,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_INPUT_CURRENT_LIMITED,
 	POWER_SUPPLY_PROP_IGNORE_FALSE_NEGATIVE_ISENSE,
 	POWER_SUPPLY_PROP_ENABLE_JEITA_DETECTION,
+	POWER_SUPPLY_PROP_ALLOW_DETECTION,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
@@ -321,6 +322,9 @@ extern int power_supply_set_low_power_state(struct power_supply *psy,
 							int value);
 extern int power_supply_set_dp_dm(struct power_supply *psy,
 							int value);
+extern int power_supply_set_allow_detection(struct power_supply *psy,
+							int value);
+
 extern int power_supply_is_system_supplied(void);
 extern int power_supply_register(struct device *parent,
 				 struct power_supply *psy);
@@ -369,6 +373,10 @@ static inline int power_supply_set_low_power_state(struct power_supply *psy,
 static inline int power_supply_set_dp_dm(struct power_supply *psy,
 							int value)
 							{ return -ENOSYS; }
+static inline int power_supply_set_allow_detection(struct power_supply *psy,
+							int value)
+							{ return -ENOSYS; }
+
 static inline int power_supply_is_system_supplied(void) { return -ENOSYS; }
 static inline int power_supply_register(struct device *parent,
 					struct power_supply *psy)
