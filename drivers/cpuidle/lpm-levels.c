c4b60bc36ceea1d894ca2b41431655fce78d2103
Anil Kumar Mamidala <amami@codeaurora.org>
2015-03-17 17:06:05 +0530

    cpuidle: lpm-workarounds: Skip L2 spm enabling in HLOS
    Due to a hardware bug on 8939 and 8909, secure world needs
    to disable and enable L2 SPM to get the proper context in
    secure watchdog bite cases. With this workaround there is
    a race in programming L2 SPM between HLOS and secure world.
    This leads to stability issues. To avoid this program L2 SPM
    enable in secure world.
    
    Change-Id: I426289e1fc66664ed9c4ecf81c4106ce2c7c1145
    Signed-off-by: Anil Kumar Mamidala <amami@codeaurora.org>
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>
    Reviewed-on: http://gerrit.mot.com/760105
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Guo-Jian Chen <a21757@motorola.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -214,7 +214,13 @@ int set_l2_mode(struct low_power_ops *ops, int mode, bool notify_rpm)
 		lpm = MSM_SPM_MODE_DISABLED;
 		break;
 	}
-	rc = msm_spm_config_low_power_mode(ops->spm, lpm, true);
+
+	/* Do not program L2 SPM enable bit. This will be set by TZ */
+	if (lpm_wa_get_skip_l2_spm())
+		rc = msm_spm_config_low_power_mode_addr(ops->spm, lpm,
+							true);
+	else
+		rc = msm_spm_config_low_power_mode(ops->spm, lpm, true);
 
 	if (rc)
 		pr_err("%s: Failed to set L2 low power mode %d, ERR %d",
