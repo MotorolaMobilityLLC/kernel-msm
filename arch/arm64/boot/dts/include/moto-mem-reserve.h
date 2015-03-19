786546c2fcc86d5d457bcc1594920e5baaf0c631
Hong-Mei Li <a21834@motorola.com>
2015-03-18 16:20:16 -0800

    arm/dt: msm8952: reserve buf for watchdog dump
    This change adds buffer reservation for qcom,tz-log and
    qcom,msm-watchdog. After a watchdog reset, kernel will be
    able to check the buffers for cpu context dump and tz log.
    
    Change-Id: Icc96b90260d9702732dbb026135af9d0a3c4991f
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/722584
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/787909
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>

---
+++ arch/arm64/boot/dts/include/moto-mem-reserve.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2015 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __MOTO_MEM_RESERVE_H
+#define __MOTO_MEM_RESERVE_H
+
+#define WDOG_CPUCTX_SIZE_PERCPU	0x5800 /* sizeof(struct msm_wdog_cpuctx) */
+#define WDOG_CPUCTX_SIZE_8CPUS	(WDOG_CPUCTX_SIZE_PERCPU * 8)
+
+#define WDOG_CPUCTX_SIZE_8952	WDOG_CPUCTX_SIZE_8CPUS
+#define WDOG_CPUCTX_BASE_8952	(0xaec00000-0x400000)
+
+#define TZLOG_BCK_SIZE		0x2000
+#define TZLOG_BCK_BASE_8952	(WDOG_CPUCTX_BASE_8952 - 0x400000)
+
+#endif /* __MOTO_MEM_RESERVE_H */
