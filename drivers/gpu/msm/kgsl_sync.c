c27d2c5ace4a1fb8ea5fb550e09b2e24acb6f88a
Rama Vaddula <rvaddula@codeaurora.org>
2015-03-23 17:10:44 -0700

    msm: kgsl: Fix double put for context in an error case
    This change fixes a duplicate put for context in error case.
    
    Change-Id: I3b76b742b164191933b92aa210b9a4664a379443
    Signed-off-by: Rama Vaddula <rvaddula@codeaurora.org>
    (cherry picked from commit a6aab1df86d37c90146e6ab3d3596c91a03eee48)

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -233,6 +233,7 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	return 0;
 
 unlock:
+	kgsl_context_put(context);
 	mutex_unlock(&device->mutex);
 
 out:
@@ -242,7 +243,6 @@ out:
 	if (fence)
 		sync_fence_put(fence);
 
-	kgsl_context_put(context);
 	return ret;
 }
 
