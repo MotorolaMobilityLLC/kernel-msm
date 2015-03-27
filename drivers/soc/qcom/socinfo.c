aca66afc4e9b5f5921245da180bec1ba57e71f2f
r00902181 <r00902181@notesmail.huawei.com>
2015-03-27 15:17:24 +0800

    DTS:DTS2014050602054 Description:Fixing of boot issue on new baseline - overflow boardid PIC:renlipeng,rooepsh
    APPx:DRV
    Bug-Type:Vendor-Bugfix
    General or Risk:General
    
    Change-Id: I5db3336280e963b6ea099e58e4f36b57b0c16977
    
    Signed-off-by: m00146432 <m00146432@notesmail.huawei.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -44,6 +44,11 @@
 #define SMEM_IMAGE_VERSION_OEM_OFFSET 96
 #define SMEM_IMAGE_VERSION_PARTITION_APPS 10
 
+/* < DTS2014050602054 renlipeng 20140506 begin */
+#ifdef CONFIG_HUAWEI_KERNEL
+#define HW_BOARDID_BEGIN_NUM 8000
+#endif
+/*  DTS2014050602054 renlipeng 20140506 end >*/
 enum {
 	HW_PLATFORM_UNKNOWN = 0,
 	HW_PLATFORM_SURF    = 1,
@@ -694,6 +699,17 @@ msm_get_hw_platform(struct device *dev,
 	uint32_t hw_type;
 	hw_type = socinfo_get_platform_type();
 
+    /* < DTS2014050602054 renlipeng 20140506 begin */
+#ifdef CONFIG_HUAWEI_KERNEL
+    if(hw_type >= HW_BOARDID_BEGIN_NUM)
+	{
+         hw_type = HW_PLATFORM_MTP;
+	}else if(hw_type >= HW_PLATFORM_INVALID)
+    {
+         hw_type = HW_PLATFORM_UNKNOWN;
+	}
+#endif
+    /*  DTS2014050602054 renlipeng 20140506 end >*/
 	return snprintf(buf, PAGE_SIZE, "%-.32s\n",
 			hw_platform[hw_type]);
 }
