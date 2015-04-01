ed458b8c39e528688e5326b134c6679336dd1086
Shengzhe Zhao <a18689@motorola.com>
2015-03-31 17:05:04 -0700

    dm: dm-req-crypt: fix null pointer for _req_crypt_io_pool
    When dm_get_device(&dev) fails or KMEM_CACHE return NULL in req_crypt_ctr,
    the "_req_crypt_io_pool" variable could be NULL, so calling
    kmem_cache_destroy(_req_crypt_io_pool) crash the device.
    
    [<c01e7dc4>] (kmem_cache_destroy+0x1c/0xc8)
    [<c076e0e0>] (req_crypt_dtr+0xc4/0xe8)
    [<c076e464>] (req_crypt_ctr+0x360/0x4f0)
    [<c0765f50>] (dm_table_add_target+0x148/0x3bc)
    [<c076898c>] (table_load+0xf0/0x2b8)
    [<c0769578>] (ctl_ioctl+0x21c/0x51c)
    [<c021cb4c>] (do_vfs_ioctl+0x40c/0x5e4)
    [<c021cd94>] (SyS_ioctl+0x70/0x80)
    
    Change-Id: I7d67e3e4c7b08c747e90f361af067212356be16c
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Reviewed-on: http://gerrit.mot.com/730605
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joel Voss <jvoss@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/md/dm-req-crypt.c
+++ drivers/md/dm-req-crypt.c
@@ -878,7 +878,10 @@ static void req_crypt_dtr(struct dm_target *ti)
 		req_crypt_queue = NULL;
 	}
 	kmem_cache_destroy(_req_dm_scatterlist_pool);
-	kmem_cache_destroy(_req_crypt_io_pool);
+
+	if (_req_crypt_io_pool)
+		kmem_cache_destroy(_req_crypt_io_pool);
+
 	if (dev) {
 		dm_put_device(ti, dev);
 		dev = NULL;
