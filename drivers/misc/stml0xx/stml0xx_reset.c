3a726fbe0c42b6f00f360e7db8c39b87b46a65fa
Yu Liu <e7664c@motorola.com>
2015-04-02 18:18:01 -0500

    Revert "(CR):drivers:misc:stml0xx: Extend WAKE_SENSORS array to 24 bits -- L0" Revert Type: Compile Reverted By: ECSCM(e7664c) Root Cause:  break 8916 jenkins build
    This reverts commit 84a2606d5ec343169d4742b810e16e2084897e2e.
    
    Change-Id: I46b95b5c331416490584ecaacc21b1c614877702
    Reviewed-on: http://gerrit.mot.com/731502
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Yu Liu <e7664c@motorola.com>

--- drivers/misc/stml0xx/stml0xx_reset.c
+++ drivers/misc/stml0xx/stml0xx_reset.c
@@ -123,10 +123,9 @@ void stml0xx_initialize_work_func(struct work_struct *work)
 		ret_err = err;
 
 	buf[0] = stml0xx_g_wake_sensor_state & 0xFF;
-	buf[1] = (stml0xx_g_wake_sensor_state >> 8) & 0xFF;
-	buf[2] = (stml0xx_g_wake_sensor_state >> 16) & 0xFF;
+	buf[1] = stml0xx_g_wake_sensor_state >> 8;
 	err = stml0xx_spi_send_write_reg_reset(WAKESENSOR_CONFIG, buf,
-			3, RESET_NOT_ALLOWED);
+			2, RESET_NOT_ALLOWED);
 	if (err < 0)
 		ret_err = err;
 
