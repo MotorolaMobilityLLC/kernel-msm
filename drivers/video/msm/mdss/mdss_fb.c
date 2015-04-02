c61d9e9e9da4d6c0bff2f59544e81f8d7f61331d
Arun Kumar K.R <akumarkr@codeaurora.org>
2015-04-02 16:14:16 -0700

    mdss: display: terminate the panel info with \n
    The string added to the msm_fb_panel_info must be terminated
    by \n for it to be correctly updated in the sysfs file.
    
    Signed-off-by: Arun Kumar K.R <akumarkr@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -434,7 +434,7 @@ static ssize_t mdss_fb_get_panel_info(struct device *dev,
 
 	ret = scnprintf(buf, PAGE_SIZE,
 			"pu_en=%d\nxstart=%d\nwalign=%d\nystart=%d\nhalign=%d\n"
-			"min_w=%d\nmin_h=%d\nroi_merge=%d",
+			"min_w=%d\nmin_h=%d\nroi_merge=%d\n",
 			pinfo->partial_update_enabled, pinfo->xstart_pix_align,
 			pinfo->width_pix_align, pinfo->ystart_pix_align,
 			pinfo->height_pix_align, pinfo->min_width,
