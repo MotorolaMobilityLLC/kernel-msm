68f3742bc6317ab3c9ca3bee8674e4164304a51f
Kazuhiro Ondo <ondo@motorola.com>
2015-04-06 19:37:06 -0500

    ASoC: aov_trigger: Add panic and event notification
    Adding panic and event logging feature on XMCS based AoV.
    
    Change-Id: Iec47491973f815c818c97e5c86260006d0cf51a7
    Signed-off-by: Kazuhiro Ondo <ondo@motorola.com>
    Reviewed-on: http://gerrit.mot.com/733615
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/aov_trigger.c
+++ sound/soc/codecs/aov_trigger.c
@@ -14,14 +14,27 @@
 
 #include <linux/export.h>
 #include <linux/kobject.h>
+#include <linux/mutex.h>
 #include <linux/printk.h>
 #include <linux/string.h>
 #include <linux/sysfs.h>
 #include <sound/soc.h>
 #include "arizona.h"
 
+#define MAX_DSP_TO_CHECK 4
+#define MAX_NUM_PANIC_CODE 4
+
+struct dsp_event_info {
+	int panic;
+	int event;
+	u16 panic_code[MAX_NUM_PANIC_CODE];
+};
+
 static struct kobject aov_trigger_kobj;
 static struct snd_soc_codec *aov_codec;
+
+static struct dsp_event_info dsp_info[MAX_DSP_TO_CHECK];
+static DEFINE_MUTEX(dsp_info_mutex);
 
 static struct attribute aov_sysfs_attr_trigger = {
 	.name = "trigger",
@@ -31,6 +44,11 @@ static struct attribute aov_sysfs_attr_trigger = {
 static struct attribute aov_sysfs_attr_register = {
 	.name = "register",
 	.mode = S_IWUSR | S_IWGRP
+};
+
+static struct attribute aov_sysfs_attr_event = {
+	.name = "event",
+	.mode = S_IRUSR | S_IRGRP
 };
 
 static const char *reg_cmd = "register";
@@ -49,6 +67,42 @@ static void aov_trigger_notify(void)
 		sysfs_notify(&aov_trigger_kobj, NULL,
 			     aov_sysfs_attr_trigger.name);
 	}
+}
+
+static void aov_panic_notify(int dsp, u16 *msg)
+{
+	if (dsp < 0 || dsp >= MAX_DSP_TO_CHECK)
+		return;
+
+	if (aov_codec == NULL)
+		return;
+
+	mutex_lock(&dsp_info_mutex);
+	dsp_info[dsp].panic = 1;
+	if (msg != NULL) {
+		memcpy(dsp_info[dsp].panic_code, msg,
+		       sizeof(u16) * MAX_NUM_PANIC_CODE);
+	}
+	mutex_unlock(&dsp_info_mutex);
+	dev_info(aov_codec->dev, "aov notify panic.");
+	sysfs_notify(&aov_trigger_kobj, NULL,
+		     aov_sysfs_attr_event.name);
+}
+
+static void aov_event_notify(int dsp)
+{
+	if (dsp < 0 || dsp >= MAX_DSP_TO_CHECK)
+		return;
+
+	if (aov_codec == NULL)
+		return;
+
+	mutex_lock(&dsp_info_mutex);
+	dsp_info[dsp].event = 1;
+	mutex_unlock(&dsp_info_mutex);
+	dev_info(aov_codec->dev, "aov notify event.");
+	sysfs_notify(&aov_trigger_kobj, NULL,
+		     aov_sysfs_attr_event.name);
 }
 
 static void aov_register_trigger(void)
@@ -75,7 +129,31 @@ static void aov_sysfs_release(struct kobject *kobj)
 static ssize_t aov_sysfs_show(struct kobject *kobj,
 				  struct attribute *attr, char *buf)
 {
-	return 0;
+	int i, j;
+	int count;
+	char *ptr = buf;
+
+	if (attr != &aov_sysfs_attr_event)
+		return 0;
+
+	mutex_lock(&dsp_info_mutex);
+	for (i = 0; i < MAX_DSP_TO_CHECK; i++) {
+		count = scnprintf(ptr, PAGE_SIZE - (ptr - buf),
+				  "dsp %d: panic:%d event:%d code:",
+				  i, dsp_info[i].panic, dsp_info[i].event);
+		ptr += count;
+		for (j = 0; j < MAX_NUM_PANIC_CODE; j++) {
+			count = scnprintf(ptr, PAGE_SIZE - (ptr - buf),
+					  "%04x", dsp_info[i].panic_code[j]);
+			ptr += count;
+		}
+		count = scnprintf(ptr, PAGE_SIZE - (ptr - buf), "\n");
+		ptr += count;
+	}
+	memset(dsp_info, 0, sizeof(struct dsp_event_info) * MAX_DSP_TO_CHECK);
+	mutex_unlock(&dsp_info_mutex);
+
+	return ptr - buf;
 }
 
 static ssize_t aov_sysfs_store(struct kobject *kobj,
@@ -83,6 +161,10 @@ static ssize_t aov_sysfs_store(struct kobject *kobj,
 				   const char *buf, size_t count)
 {
 	ssize_t ret = -EIO;
+
+	if (attr != &aov_sysfs_attr_register) {
+		return ret;
+	}
 
 	if (!strncmp(reg_cmd, buf, strlen(reg_cmd))) {
 		aov_register_trigger();
@@ -133,6 +215,17 @@ int aov_trigger_init(struct snd_soc_codec *codec)
 			       __func__, ret);
 			goto done;
 		}
+
+		sysfs_attr_init(&aov_sysfs_attr_event);
+		ret = sysfs_create_file(&aov_trigger_kobj,
+					&aov_sysfs_attr_event);
+		if (ret) {
+			pr_err("%s: event node creation failed, ret=%d\n",
+			       __func__, ret);
+			goto done;
+		}
+		arizona_set_ez2panic_cb(codec, &aov_panic_notify);
+		arizona_set_ez2text_cb(codec, &aov_event_notify);
 		aov_codec = codec;
 	}
 done:
