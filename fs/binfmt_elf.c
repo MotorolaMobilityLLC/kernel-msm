28a66f1cb700d85d2c34bbb956c341a1a0d3ebc5
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2015-04-08 16:37:01 -0500

    binfmt: convert all core_dump implementers to new API
    Need to call dump_init and dump_finish and pass the coredump params back
    up via existing callbacks.
    
    Change-Id: If6be6c5fffce36e4743f1064a7e4097a1fa26fcc
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-on: https://gerrit.mot.com/944711
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -2231,6 +2231,9 @@ static int elf_core_dump(struct coredump_params *cprm)
 	fs = get_fs();
 	set_fs(KERNEL_DS);
 
+	if (!dump_init(cprm))
+		goto end_coredump;
+
 	offset += sizeof(*elf);				/* Elf header */
 	offset += segs * sizeof(struct elf_phdr);	/* Program headers */
 
@@ -2353,6 +2356,7 @@ static int elf_core_dump(struct coredump_params *cprm)
 	}
 
 end_coredump:
+	dump_finish(cprm);
 	set_fs(fs);
 
 cleanup:
