315aaf4c1062a1044c4e1a28c977373c7de6e9e1
Kuogee Hsieh <khsieh@codeaurora.org>
2015-04-09 12:47:29 -0700

    msm: mdss: add panel on command support after te enable
    Few DSI panels has requirement of receiving display on command
    after receiving sleep out command and pixel stream data. This
    can be achieved by sending panel post on command followed by
    timing engine enable. This change adds the support for such
    kind of DSI panels.
    
    Change-Id: Ifa225aec4ad9bfd692e19c02bcb726c8d60ce4fb
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>
    Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -302,6 +302,7 @@ enum {
 struct mdss_dsi_ctrl_pdata {
 	int ndx;	/* panel_num */
 	int (*on) (struct mdss_panel_data *pdata);
+	int (*post_panel_on)(struct mdss_panel_data *pdata);
 	int (*off) (struct mdss_panel_data *pdata);
 	int (*low_power_config) (struct mdss_panel_data *pdata, int enable);
 	int (*set_col_page_addr) (struct mdss_panel_data *pdata);
@@ -370,6 +371,8 @@ struct mdss_dsi_ctrl_pdata {
 	struct mdss_intf_recovery *recovery;
 
 	struct dsi_panel_cmds on_cmds;
+	struct dsi_panel_cmds post_dms_on_cmds;
+	struct dsi_panel_cmds post_panel_on_cmds;
 	struct dsi_panel_cmds off_cmds;
 	struct dsi_panel_cmds status_cmds;
 	u32 status_cmds_rlen;
