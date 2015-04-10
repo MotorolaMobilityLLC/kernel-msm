441755e49de54a57f9e6b25335bba65692e29ad2
Yuanyuan Zhong <zyy@motorola.com>
2015-04-10 16:46:22 -0500

    vmstat: add NR_ION_PAGES and NR_ION_CMA_PAGES
    Add NR_ION_PAGES and NR_ION_CMA_PAGES to account for memory
    allocated by ION.
    
    Change-Id: I1404f5943306824a0f64f35994b9b11d00b2cd6a
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.mot.com/754832
    Reviewed-by: Jeffrey Vanhoof <jvanhoof@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    (cherry picked from commit ddae4d16eb7418e594cb0a78986ea8ce939f73aa)
    
    Conflicts:
    
    	include/linux/mmzone.h
    	mm/vmstat.c
    
    Change-Id: I85cf9e18cc53948204a00341f5c5db57a3d14f8c
    Reviewed-on: http://gerrit.mot.com/826388
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3182,7 +3182,7 @@ void show_free_areas(unsigned int filter)
 		" dirty:%lu writeback:%lu unstable:%lu\n"
 		" free:%lu slab_reclaimable:%lu slab_unreclaimable:%lu\n"
 		" mapped:%lu shmem:%lu pagetables:%lu bounce:%lu\n"
-		" free_cma:%lu\n",
+		" free_cma:%lu ion:%lu ion_cma:%lu\n",
 		global_page_state(NR_ACTIVE_ANON),
 		global_page_state(NR_INACTIVE_ANON),
 		global_page_state(NR_ISOLATED_ANON),
@@ -3200,7 +3200,9 @@ void show_free_areas(unsigned int filter)
 		global_page_state(NR_SHMEM),
 		global_page_state(NR_PAGETABLE),
 		global_page_state(NR_BOUNCE),
-		global_page_state(NR_FREE_CMA_PAGES));
+		global_page_state(NR_FREE_CMA_PAGES),
+		global_page_state(NR_ION_PAGES),
+		global_page_state(NR_ION_CMA_PAGES));
 
 	for_each_populated_zone(zone) {
 		int i;
@@ -3234,6 +3236,8 @@ void show_free_areas(unsigned int filter)
 			" unstable:%lukB"
 			" bounce:%lukB"
 			" free_cma:%lukB"
+			" ion:%lukB"
+			" ion_cma:%lukB"
 			" writeback_tmp:%lukB"
 			" pages_scanned:%lu"
 			" all_unreclaimable? %s"
@@ -3265,6 +3269,8 @@ void show_free_areas(unsigned int filter)
 			K(zone_page_state(zone, NR_UNSTABLE_NFS)),
 			K(zone_page_state(zone, NR_BOUNCE)),
 			K(zone_page_state(zone, NR_FREE_CMA_PAGES)),
+			K(zone_page_state(zone, NR_ION_PAGES)),
+			K(zone_page_state(zone, NR_ION_CMA_PAGES)),
 			K(zone_page_state(zone, NR_WRITEBACK_TEMP)),
 			zone->pages_scanned,
 			(!zone_reclaimable(zone) ? "yes" : "no")
