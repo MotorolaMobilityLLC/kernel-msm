ba1fe41d5925604f4ba8ec4e3b14575e6ea4e832
Ravikumar Vembu <raviv@motorola.com>
2015-04-23 14:33:14 -0500

    Revert "ARM: mm: don't limit default CMA region only to low memory"
    This reverts commit e0e75bbdce65e594071c4e5eb6dbf0b5aaf557d1.
    
    Reverting temporarily since XPU violations are seen at bootup.
    
    Change-Id: Ibbeaf081a5814daf3ec4953f35faab0ae11a4074

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -403,7 +403,7 @@ void __init arm_memblock_init(const struct machine_desc *mdesc)
 	 * reserve memory for DMA contigouos allocations,
 	 * must come from DMA area inside low memory
 	 */
-	dma_contiguous_reserve(arm_dma_limit);
+	dma_contiguous_reserve(min(arm_dma_limit, arm_lowmem_limit));
 
 	arm_memblock_steal_permitted = false;
 	memblock_allow_resize();
