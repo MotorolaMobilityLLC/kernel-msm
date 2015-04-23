a7dd8b699bba4c363d2400168161d41308fb0726
Zhihui Wu <zhihui.wu@huawei.com>
2015-04-22 19:25:45 -0700

    power: angler: enable warm reset for panic.
    Signed-off-by: Zhihui Wu <zhihui.wu@huawei.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -242,6 +242,11 @@ static void msm_restart_prepare(const char *cmd)
 			need_warm_reset = true;
 	}
 
+	if(in_panic) {
+		pr_notice("Warm reset for panic\n");
+		need_warm_reset = true;
+	}
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
