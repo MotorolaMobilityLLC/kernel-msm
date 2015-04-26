71de4dbab389dbff3f5ec2b7c3b9851f96eae1c9
Russ W. Knize <Russ.Knize@motorola.com>
2015-04-26 17:29:37 -0500

    mmc: disable cache on known-bad Micron eMMCs
    A specific version of firmware on Micron eMMCs has a bug that can cause
    data corruption when cache is enabled.  Disable the cache on those devices.
    
    Change-Id: I139dd4caf246fbf95de017692f5602f1594f2472
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/742112
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 09c7b71b9b7d35c70a4e98cd2a471531e1f15097)

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -66,6 +66,9 @@ static const struct mmc_fixup mmc_fixups[] = {
 	MMC_FIXUP_EXT_CSD_REV(CID_NAME_ANY, CID_MANFID_HYNIX,
 			      0x014a, add_quirk, MMC_QUIRK_BROKEN_HPI, 5),
 
+	MMC_FIXUP_FW_VER(CID_NAME_ANY, CID_MANFID_MICRON2, CID_OEMID_ANY,
+			 add_quirk_mmc, MMC_QUIRK_CACHE_DISABLE, 6, 0xaf),
+
 	END_FIXUP
 };
 
@@ -1718,10 +1721,8 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		} else {
 			card->ext_csd.cache_ctrl = 1;
 		}
-	}
-	if (card->quirks & MMC_QUIRK_CACHE_DISABLE) {
-		pr_warn("%s: This is Hynix card, cache disabled!\n",
-				mmc_hostname(card->host));
+	} else if (card->quirks & MMC_QUIRK_CACHE_DISABLE) {
+		pr_debug("%s: cache disabled\n", mmc_hostname(card->host));
 		card->ext_csd.cache_ctrl = 0;
 	}
 
