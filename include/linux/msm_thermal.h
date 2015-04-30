6019d6054fc24d6a6ea8daece6142c2ed257045e
Anji Jonnala <anjir@codeaurora.org>
2015-04-30 16:55:44 +0530

    msm: thermal: Add support for device manager in KTM
    Add support for device manager framework to KTM. Device manager allows
    for aggregation of muliple kernel clients request on mitigation
    devices. Clients can set and register for device change notifications.
    Currently it supports hotplug device and cpuX (X= 0... NR_CPUS - 1)
    devices.
    
    Device manager framework exports below 3 APIs for a device for
    clients to use.
    
    1. struct device_clnt_data *devmgr_register_mitigation_client(
                            struct device *dev,
                            const char *device_name,
                            void (*callback)(struct device_clnt_data *,
                                    union device_request *, void *))
    
    API to register mitigation device. This API returns handle, which
    should be used by clients when placing mitigation request.
    It supports to register for device change notification with callback
    as an optional parameter to client.
    
    @dev: Client device structure.
    @device_name: Mitgation device name which the client is interested
                  to mitigate.
    @callback: Optional callback pointer for device change notification,
               otherwise pass NULL.
    
    Returns client handle structure for that device on success, or NULL
    with IS_ERR() condition containing error number.
    
    2. int devmgr_client_request_mitigation(struct device_clnt_data *clnt,
                                    enum device_req_type type,
                                    union device_request *req);
    
    API for clients to set valid mitigation for a registered device.
    
    @clnt: Client handle for device.
    @type: Type of device request populated above.
    @req:  Valid mitigation request.
    
    Returns zero on successful mitigation update or
    negative error number.
    
    3. void devmgr_unregister_mitigation_client(
                                    struct device *dev,
                                    struct device_clnt_data *clnt)
    
    API for clients to unregister for a device whenever it is done with
    device.
    
    @dev: Client device structure.
    @clnt: Client handle for device.
    
    Change-Id: Ieda99577a035a32b745df6b004b5487a55ec70ac
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -14,6 +14,20 @@
 #ifndef __MSM_THERMAL_H
 #define __MSM_THERMAL_H
 
+#define TSENS_NAME_MAX 20
+#define HOTPLUG_DEVICE "hotplug"
+#define CPU0_DEVICE     "cpu0"
+#define CPU1_DEVICE     "cpu1"
+#define CPU2_DEVICE     "cpu2"
+#define CPU3_DEVICE     "cpu3"
+#define CPU4_DEVICE     "cpu4"
+#define CPU5_DEVICE     "cpu5"
+#define CPU6_DEVICE     "cpu6"
+#define CPU7_DEVICE     "cpu7"
+#define CPUFREQ_MAX_NO_MITIGATION     UINT_MAX
+#define CPUFREQ_MIN_NO_MITIGATION     0
+#define HOTPLUG_NO_MITIGATION(_mask)  cpumask_clear(_mask)
+
 struct msm_thermal_data {
 	struct platform_device *pdev;
 	uint32_t sensor_id;
@@ -55,6 +69,56 @@ struct msm_thermal_data {
 	int32_t therm_reset_temp_degC;
 };
 
+enum device_req_type {
+	DEVICE_REQ_NONE = -1,
+	HOTPLUG_MITIGATION_REQ,
+	CPUFREQ_MITIGATION_REQ,
+	DEVICE_REQ_MAX,
+};
+
+/**
+ * For frequency mitigation request, if client is interested
+ * only in one, either max_freq or min_freq, update default
+ * value for other one also for mitigation request.
+ * Default value for request structure variables:
+ *   max_freq = UINT_MAX;
+ *   min_freq = 0;
+ *   offline_mask =  CPU_MASK_NONE;
+ */
+struct cpufreq_request {
+	uint32_t                     max_freq;
+	uint32_t                     min_freq;
+};
+
+union device_request {
+	struct cpufreq_request       freq;
+	cpumask_t                    offline_mask;
+};
+
+struct device_clnt_data;
+struct device_manager_data {
+	char                         device_name[TSENS_NAME_MAX];
+	union device_request         active_req;
+	struct list_head             client_list;
+	struct list_head             dev_ptr;
+	struct mutex                 clnt_lock;
+	int (*request_validate)(struct device_clnt_data *,
+			union device_request *,
+			enum device_req_type);
+	int (*update)(struct device_manager_data *);
+	void                         *data;
+};
+
+struct device_clnt_data {
+	struct device_manager_data   *dev_mgr;
+	bool                         req_active;
+	union device_request         request;
+	struct list_head             clnt_ptr;
+	void (*callback)(struct device_clnt_data *,
+			union device_request *req, void *);
+	void                         *usr_data;
+};
+
 #ifdef CONFIG_THERMAL_MONITOR
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
@@ -66,6 +130,43 @@ extern int msm_thermal_get_freq_plan_size(uint32_t cluster,
 	unsigned int *table_len);
 extern int msm_thermal_get_cluster_freq_plan(uint32_t cluster,
 	unsigned int *table_ptr);
+/**
+ * devmgr_register_mitigation_client - Register for a device and
+ *                                     gets a handle for mitigation.
+ * @dev: Client device structure.
+ * @device_name: Mitgation device name which the client is interested
+ *               to mitigate.
+ * @callback: Optional callback pointer for device change notification,
+ *            otherwise pass NULL.
+ *
+ * Returns client handle structure for that device on success, or NULL
+ * with IS_ERR() condition containing error number.
+ */
+extern struct device_clnt_data *devmgr_register_mitigation_client(
+				struct device *dev,
+				const char *device_name,
+				void (*callback)(struct device_clnt_data *,
+				union device_request *, void *));
+/**
+ * devmgr_client_request_mitigation -  Set a valid mitigation for
+ *                                     registered device.
+ * @clnt: Client handle for device.
+ * @type: Type of device request populated above.
+ * @req:  Valid mitigation request.
+ *
+ * Returns zero on successful mitigation update, or negative error number.
+ */
+extern int devmgr_client_request_mitigation(struct device_clnt_data *clnt,
+						enum device_req_type type,
+						union device_request *req);
+/**
+ * devmgr_unregister_mitigation_client - Unregister mitigation device
+ * @dev: Client device structure.
+ * @clnt: Client handle for device.
+ */
+extern void devmgr_unregister_mitigation_client(
+					struct device *dev,
+					struct device_clnt_data *clnt);
 #else
 static inline int msm_thermal_init(struct msm_thermal_data *pdata)
 {
@@ -94,6 +195,26 @@ static inline int msm_thermal_get_cluster_freq_plan(uint32_t cluster,
 	unsigned int *table_ptr);
 {
 	return -ENOSYS;
+}
+static inline struct device_clnt_data *devmgr_register_mitigation_client(
+				struct device *dev,
+				const char *device_name,
+				void (*callback)(struct device_clnt_data *,
+				union device_request *, void *))
+{
+	return NULL;
+}
+static inline int devmgr_client_request_mitigation(
+					struct device_clnt_data *clnt,
+					enum device_req_type type,
+					union device_request *req)
+{
+	return -ENOSYS;
+}
+static inline void devmgr_unregister_mitigation_client(
+					struct device *dev,
+					struct device_clnt_data *clnt)
+{
 }
 #endif
 
