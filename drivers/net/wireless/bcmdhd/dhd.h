f66c8c3ad7d1bff37ad3cde45521ab7832117bb0
Dmitry Shmidt <dimitrysh@google.com>
2015-05-08 10:47:32 -0700

    net: wireless: bcmdhd: Increase allowed DTIM in power-save mode
    Change-Id: I48ffca5770cc09ff06817a9eabacdf75978340f9
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -787,7 +787,7 @@ extern uint dhd_force_tx_queueing;
 #define CUSTOM_LISTEN_INTERVAL 		LISTEN_INTERVAL
 #endif /* CUSTOM_LISTEN_INTERVAL */
 
-#define DEFAULT_SUSPEND_BCN_LI_DTIM		3
+#define DEFAULT_SUSPEND_BCN_LI_DTIM		5
 #ifndef CUSTOM_SUSPEND_BCN_LI_DTIM
 #define CUSTOM_SUSPEND_BCN_LI_DTIM		DEFAULT_SUSPEND_BCN_LI_DTIM
 #endif
@@ -815,7 +815,9 @@ extern uint dhd_force_tx_queueing;
 
 
 #define MAX_DTIM_SKIP_BEACON_INTERVAL	100 /* max allowed associated AP beacon for DTIM skip */
-#define MAX_DTIM_ALLOWED_INTERVAL 600 /* max allowed total beacon interval for DTIM skip */
+#ifndef MAX_DTIM_ALLOWED_INTERVAL
+#define MAX_DTIM_ALLOWED_INTERVAL 900 /* max allowed total beacon interval for DTIM skip */
+#endif
 #define NO_DTIM_SKIP 1
 #ifdef SDTEST
 /* Echo packet generator (SDIO), pkts/s */
