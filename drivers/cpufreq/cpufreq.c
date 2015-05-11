7fb0af087c788d2f0efe541ffaae9e9d8ab7881f
Lianwei Wang <a22439@motorola.com>
2015-05-11 12:00:06 -0700

    IKSWL-14278 cpufreq: set policy with user_policy data
    When we update/set cpufreq policy, we should set the new
    policy with user_policy data, and the cpufreq notifier
    will limit the min/max freq accordingly.
    
    This is the right process to update the cpufreq policy.
    
    But we miss it in store_scaling_governor func, which cause
    the governor set failed if the policy.max is less than
    user_policy.min.
    
    Change-Id: Ie21593e146b8d06fa017c87434e85ff33456c20f
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/746985
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ke Lu <a2435c@motorola.com>
    Reviewed-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -512,6 +512,9 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 						&new_policy.governor))
 		return -EINVAL;
 
+	new_policy.min = new_policy.user_policy.min;
+	new_policy.max = new_policy.user_policy.max;
+
 	ret = cpufreq_set_policy(policy, &new_policy);
 
 	policy->user_policy.policy = policy->policy;
