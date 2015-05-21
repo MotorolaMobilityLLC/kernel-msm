80ca00252e3aa5c8239dd04b8b5e223a670fe53b
Iliyan Malchev <malchev@google.com>
2015-05-20 21:25:43 -0700

    Revert "gpio-msm: remove logging of wakeup reason"
    Also rename:
    -                       log_wakeup_reason(irq);
    +                       log_base_wakeup_reason(irq);
    
    This reverts commit b72e1a8b95efa8982790239f7156131a8cddeb93.
    
    Change-Id: I4d085cc226a3c2a38741a8d5430583349f8fdecb

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -21,6 +21,7 @@
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/spinlock.h>
+#include <linux/syscore_ops.h>
 #include <linux/irqdomain.h>
 #include <linux/of.h>
 #include <linux/err.h>
@@ -350,6 +351,70 @@ static struct irq_chip msm_gpio_irq_chip = {
 	.irq_disable	= msm_gpio_irq_disable,
 };
 
+#ifdef CONFIG_PM
+static int msm_gpio_suspend(void)
+{
+	unsigned long irq_flags;
+	unsigned long i;
+	int ngpio = msm_gpio.gpio_chip.ngpio;
+
+	spin_lock_irqsave(&tlmm_lock, irq_flags);
+	for_each_set_bit(i, msm_gpio.enabled_irqs, ngpio)
+		__msm_gpio_set_intr_cfg_enable(i, 0);
+
+	for_each_set_bit(i, msm_gpio.wake_irqs, ngpio)
+		__msm_gpio_set_intr_cfg_enable(i, 1);
+	mb();
+	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
+	return 0;
+}
+
+void msm_gpio_show_resume_irq(void)
+{
+	unsigned long irq_flags;
+	int i, irq, intstat;
+	int ngpio = msm_gpio.gpio_chip.ngpio;
+
+	if (!msm_show_resume_irq_mask)
+		return;
+
+	spin_lock_irqsave(&tlmm_lock, irq_flags);
+	for_each_set_bit(i, msm_gpio.wake_irqs, ngpio) {
+		intstat = __msm_gpio_get_intr_status(i);
+		if (intstat) {
+			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
+			log_base_wakeup_reason(irq);
+		}
+	}
+	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
+}
+
+static void msm_gpio_resume(void)
+{
+	unsigned long irq_flags;
+	unsigned long i;
+	int ngpio = msm_gpio.gpio_chip.ngpio;
+
+	msm_gpio_show_resume_irq();
+
+	spin_lock_irqsave(&tlmm_lock, irq_flags);
+	for_each_set_bit(i, msm_gpio.wake_irqs, ngpio)
+		__msm_gpio_set_intr_cfg_enable(i, 0);
+
+	for_each_set_bit(i, msm_gpio.enabled_irqs, ngpio)
+		__msm_gpio_set_intr_cfg_enable(i, 1);
+	mb();
+	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
+}
+#else
+#define msm_gpio_suspend NULL
+#define msm_gpio_resume NULL
+#endif
+
+static struct syscore_ops msm_gpio_syscore_ops = {
+	.suspend = msm_gpio_suspend,
+	.resume = msm_gpio_resume,
+};
 #endif /* CONFIG_USE_PINCTRL_IRQ */
 
 static void msm_tlmm_set_field(const struct tlmm_field_cfg *configs,
@@ -474,6 +539,7 @@ static int msm_gpio_setup_irqchip(struct platform_device *pdev)
 				ret);
 		return ret;
 	}
+	register_syscore_ops(&msm_gpio_syscore_ops);
 	return 0;
 }
 #else
@@ -534,6 +600,9 @@ static struct of_device_id msm_gpio_of_match[] = {
 static int msm_gpio_remove(struct platform_device *pdev)
 {
 	int ret;
+#ifndef CONFIG_USE_PINCTRL_IRQ
+	unregister_syscore_ops(&msm_gpio_syscore_ops);
+#endif
 	ret = gpiochip_remove(&msm_gpio.gpio_chip);
 	if (ret < 0)
 		return ret;
