6dc50156306acefca5123a93ff4f41a12f803fb8
Yuanyuan Zhong <zyy@motorola.com>
2015-05-22 15:23:36 -0500

    Revert "(CR) ARM: DMA-mapping: clear cached kmap mappings"
    This reverts commit 5190ea2d4a8fd38cfaaad9cc75a57457b0211e13.
    
    Even if the pages allocated for DMA are allowed to be remapped by kernel,
    some use cases could still add access protection on the pages and thus
    require the relatively slow operation to flush the stray kernel mappings.
    Removing the lowmem address limitation on CMA area does not make much
    lowmem room. We can revert the commit b1b3386 (ARM: mm: don't limit
    default CMA region only to low memory). Then commit 5190ea2 (ARM:
    DMA-mapping: clear cached kmap mapping) is no longer needed.
    
    Change-Id: I99cbe3b4f463aa24133f79869ae27bd54795d7a0
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.mot.com/750947
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -684,8 +684,6 @@ static void *__alloc_from_contiguous(struct device *dev, size_t size,
 			 * clients trying to use the address incorrectly
 			 */
 			ptr = (void *)NO_KERNEL_MAPPING_DUMMY;
-			kmap_flush_unused();
-			kmap_atomic_flush_unused();
 		} else {
 			ptr = __dma_alloc_remap(page, size, GFP_KERNEL, prot,
 						caller);
