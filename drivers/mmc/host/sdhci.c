80c6adb3365682fa999c4ff6f6d0e1ac2b9459ea
Russ W. Knize <Russ.Knize@motorola.com>
2015-05-26 16:19:00 -0500

    mmc: sdhci: handle long transfers more intelligently
    If TACC and R2W_FACTOR are multiplied over the entire length of a very
    large transfer (on the order of megabytes), it can lead to extremely long
    worst-case transfer times (on the order of hours).  In practice, the
    transfer time is many orders of magnitude faster than this, so setting the
    controller timer for the worst case is not practical nor desirable if we
    want to catch real lock-ups.  There are also other timers elsewhere in the
    driver that would expire anyway.
    
    If the SDHCI host catch-all timer fires during a data transfer, don't
    just assume that the controller is stuck.  Instead, extend the timer if we
    see that the transfer is progressing.
    
    Change-Id: Iacb5a0fcdb65fec6a92de2928b793245a7b67a87
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/751766
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1066,6 +1066,7 @@ static void sdhci_prepare_data(struct sdhci_host *host, struct mmc_command *cmd)
 	/* Set the DMA boundary value and block size */
 	sdhci_set_blk_size_reg(host, data->blksz, SDHCI_DEFAULT_BOUNDARY_ARG);
 	sdhci_writew(host, data->blocks, SDHCI_BLOCK_COUNT);
+	host->last_blocks = 0;
 }
 
 static void sdhci_set_transfer_mode(struct sdhci_host *host,
@@ -1223,7 +1224,8 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	if (timeout < cmd->cmd_timeout_ms * 2)
 		timeout = cmd->cmd_timeout_ms * 2;
 
-	mod_timer(&host->timer, jiffies + msecs_to_jiffies(timeout));
+	host->timeout_jiffies = msecs_to_jiffies(timeout);
+	mod_timer(&host->timer, jiffies + host->timeout_jiffies);
 
 	host->cmd = cmd;
 
@@ -2850,11 +2852,29 @@ static void sdhci_timeout_timer(unsigned long data)
 {
 	struct sdhci_host *host;
 	unsigned long flags;
+	unsigned int blocks = 0;
 
 	host = (struct sdhci_host*)data;
 
 	spin_lock_irqsave(&host->lock, flags);
 
+	if (host->data) {
+		blocks = (sdhci_readw(host, SDHCI_BLOCK_SIZE) & 0xFFF) *
+			 sdhci_readw(host, SDHCI_BLOCK_COUNT);
+		/* If the transfer is not actually stuck, keep waiting. */
+		if (blocks != host->last_blocks) {
+			pr_info("%s: %swaiting on long transfer (%d of %d blocks)\n",
+				mmc_hostname(host->mmc),
+				host->last_blocks ? "still " : "",
+				(host->data->blksz * host->data->blocks) -
+				blocks,
+				(host->data->blksz * host->data->blocks));
+			host->last_blocks = blocks;
+			mod_timer(&host->timer,
+				  jiffies + host->timeout_jiffies);
+			goto out;
+		}
+	}
 	if (host->mrq) {
 		if (!host->mrq->cmd->ignore_timeout) {
 			pr_err("%s: Timeout waiting for hardware interrupt.\n",
@@ -2869,8 +2889,8 @@ static void sdhci_timeout_timer(unsigned long data)
 			pr_info("%s: bytes to transfer: %d transferred: %d\n",
 				mmc_hostname(host->mmc),
 				(host->data->blksz * host->data->blocks),
-				(sdhci_readw(host, SDHCI_BLOCK_SIZE) & 0xFFF) *
-				sdhci_readw(host, SDHCI_BLOCK_COUNT));
+				(host->data->blksz * host->data->blocks) -
+				blocks);
 			host->data->error = -ETIMEDOUT;
 			sdhci_finish_data(host);
 		} else {
@@ -2883,6 +2903,7 @@ static void sdhci_timeout_timer(unsigned long data)
 		}
 	}
 
+out:
 	mmiowb();
 	spin_unlock_irqrestore(&host->lock, flags);
 }
