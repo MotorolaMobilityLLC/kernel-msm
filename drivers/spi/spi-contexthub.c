655f7c6325c5da07944869296f500d881ac5fdd2
Andreas Huber <andih@google.com>
2015-05-26 11:46:33 -0700

    spi-contexthub: initial contexthub device driver
    Minimal driver to interface with the context hub device.
    
    Change-Id: I713f312257bd1522af0022c79edb75e4c60f6af8

---
+++ drivers/spi/spi-contexthub.c
@@ -0,0 +1,882 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <linux/cdev.h>
+#include <linux/compat.h>
+#include <linux/completion.h>
+#include <linux/delay.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/ioctl.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
+#include <linux/poll.h>
+#include <linux/slab.h>
+#include <linux/spi/spi-contexthub.h>
+#include <linux/spi/spi.h>
+
+#include <asm/poll.h>
+#include <asm/uaccess.h>
+
+#define DRV_CLASS_NAME "contexthub"
+#define DRV_NAME "spich"
+#define SPICH_BUFFER_SIZE       4096
+
+enum {
+	SPICH_FLAG_TIMESTAMPS_ENABLED = 1,
+};
+
+struct spich_data {
+	dev_t devno;
+	spinlock_t spi_lock;
+	struct spi_device *spi;
+	struct class *class;
+	struct device *device;
+	struct cdev cdev;
+	unsigned users;
+	struct mutex buf_lock;
+	u8 *buffer;
+	u8 *bufferrx;
+	u32 flags;
+	int int1_irq;
+	struct completion int1_completion;
+	struct gpio gpio_array[4];
+};
+
+#define GPIO_IDX_INT1  0
+#define GPIO_IDX_INT2  1
+#define GPIO_IDX_BOOT0 2
+#define GPIO_IDX_NRST  3
+
+static int spich_suspend(struct spi_device *spi, pm_message_t state)
+{
+	dev_dbg(&spi->dev, "spich_suspend\n");
+	return 0;
+}
+
+static int spich_resume(struct spi_device *spi)
+{
+	dev_dbg(&spi->dev, "spich_resume\n");
+	return 0;
+}
+
+static irqreturn_t int1_isr(int irq, void *data)
+{
+	struct spich_data *spich = data;
+
+	complete(&spich->int1_completion);
+
+	return IRQ_HANDLED;
+}
+
+static int spich_init_instance(struct spich_data *spich)
+{
+	int status = 0;
+
+	spich->buffer = kmalloc(SPICH_BUFFER_SIZE, GFP_KERNEL);
+	if (!spich->buffer) {
+		dev_err(&spich->spi->dev, "open/ENOMEM\n");
+		status = -ENOMEM;
+		goto bail;
+	}
+
+	spich->bufferrx = kmalloc(SPICH_BUFFER_SIZE, GFP_KERNEL);
+	if (!spich->bufferrx) {
+		dev_err(&spich->spi->dev, "open/ENOMEM\n");
+		status = -ENOMEM;
+		goto bail2;
+	}
+
+	status = gpio_request_array(spich->gpio_array,
+				    ARRAY_SIZE(spich->gpio_array));
+
+	if (status != 0) {
+		dev_err(&spich->spi->dev, "open/gpio_request\n");
+		goto bail3;
+	}
+
+	spich->int1_irq = gpio_to_irq(spich->gpio_array[GPIO_IDX_INT1].gpio);
+
+	init_completion(&spich->int1_completion);
+
+	status = devm_request_irq(&spich->spi->dev,
+				  spich->int1_irq,
+				  int1_isr,
+				  IRQF_TRIGGER_FALLING,
+				  dev_name(&spich->spi->dev), spich);
+
+	if (status != 0) {
+		dev_err(&spich->spi->dev, "open/devm_request_irq\n");
+		goto bail4;
+	}
+
+	return 0;
+
+bail4:
+	gpio_free_array(spich->gpio_array, ARRAY_SIZE(spich->gpio_array));
+
+bail3:
+	kfree(spich->bufferrx);
+	spich->bufferrx = NULL;
+
+bail2:
+	kfree(spich->buffer);
+	spich->buffer = NULL;
+
+bail:
+	return status;
+}
+
+static void spich_destroy_instance(struct spich_data *spich)
+{
+	devm_free_irq(&spich->spi->dev, spich->int1_irq, spich);
+
+	gpio_free_array(spich->gpio_array, ARRAY_SIZE(spich->gpio_array));
+
+	kfree(spich->buffer);
+	spich->buffer = NULL;
+
+	kfree(spich->bufferrx);
+	spich->bufferrx = NULL;
+}
+
+static int spich_open(struct inode *inode, struct file *filp)
+{
+	struct spich_data *spich;
+	unsigned users;
+
+	spich = container_of(inode->i_cdev, struct spich_data, cdev);
+
+	spin_lock_irq(&spich->spi_lock);
+	users = spich->users++;
+
+	if (users != 0) {
+		--spich->users;
+		spin_unlock_irq(&spich->spi_lock);
+		return -EBUSY;
+	}
+	spin_unlock_irq(&spich->spi_lock);
+
+	filp->private_data = spich;
+	nonseekable_open(inode, filp);
+
+	return 0;
+}
+
+static int spich_release(struct inode *inode, struct file *filp)
+{
+	struct spich_data *spich;
+	unsigned users;
+
+	spich = filp->private_data;
+	filp->private_data = NULL;
+
+	spin_lock_irq(&spich->spi_lock);
+	users = --spich->users;
+	spin_unlock_irq(&spich->spi_lock);
+
+	if (users == 0) {
+		if (spich->spi == NULL) {
+			spich_destroy_instance(spich);
+			mutex_destroy(&spich->buf_lock);
+			kfree(spich);
+		}
+	}
+
+	return 0;
+}
+
+static void spich_complete(void *arg)
+{
+	complete(arg);
+}
+
+static ssize_t spich_sync(struct spich_data *spich, struct spi_message *message)
+{
+	DECLARE_COMPLETION_ONSTACK(done);
+	int status;
+
+	message->complete = spich_complete;
+	message->context = &done;
+
+	gpio_set_value(spich->gpio_array[GPIO_IDX_INT2].gpio, 1);
+
+	spin_lock_irq(&spich->spi_lock);
+	if (spich->spi == NULL) {
+		status = -ESHUTDOWN;
+	} else {
+		status = spi_async(spich->spi, message);
+	}
+	spin_unlock_irq(&spich->spi_lock);
+
+	if (status == 0) {
+		wait_for_completion(&done);
+		status = message->status;
+		if (status == 0) {
+			status = message->actual_length;
+		}
+	}
+
+	gpio_set_value(spich->gpio_array[GPIO_IDX_INT2].gpio, 0);
+
+	return status;
+}
+
+static void SET_U64(void *_data, uint64_t x)
+{
+	uint8_t *data = (uint8_t *) _data;
+	data[0] = (x >> 56) & 0xff;
+	data[1] = (x >> 48) & 0xff;
+	data[2] = (x >> 40) & 0xff;
+	data[3] = (x >> 32) & 0xff;
+	data[4] = (x >> 24) & 0xff;
+	data[5] = (x >> 16) & 0xff;
+	data[6] = (x >> 8) & 0xff;
+	data[7] = x & 0xff;
+}
+
+static int spich_message(struct spich_data *spich,
+			 struct spi_ioc_transfer *u_xfers, unsigned n_xfers)
+{
+	struct spi_message msg;
+	struct spi_transfer *k_xfers;
+	struct spi_transfer *k_tmp;
+	struct spi_ioc_transfer *u_tmp;
+	u8 *buf;
+	u8 *bufrx;
+	int status = -EFAULT;
+	unsigned n;
+	unsigned total;
+	struct timespec t;
+	uint64_t now_us;
+
+	spi_message_init(&msg);
+
+	k_xfers = kcalloc(n_xfers, sizeof(struct spi_transfer), GFP_KERNEL);
+	if (k_xfers == NULL) {
+		return -ENOMEM;
+	}
+
+	if (spich->flags & SPICH_FLAG_TIMESTAMPS_ENABLED) {
+		do_posix_clock_monotonic_gettime(&t);
+		now_us = t.tv_sec * 1000000ull + (t.tv_nsec + 500ull) / 1000ull;
+	}
+
+	buf = spich->buffer;
+	bufrx = spich->bufferrx;
+	total = 0;
+	for (n = n_xfers, k_tmp = k_xfers, u_tmp = u_xfers; n;
+	     --n, ++k_tmp, ++u_tmp) {
+		k_tmp->len = u_tmp->len;
+
+		total += k_tmp->len;
+		if (total > SPICH_BUFFER_SIZE) {
+			status = -EMSGSIZE;
+			goto done;
+		}
+
+		if (u_tmp->rx_buf) {
+			k_tmp->rx_buf = bufrx;
+			if (!access_ok(VERIFY_WRITE,
+				       (u8 __user *) (uintptr_t) u_tmp->rx_buf,
+				       u_tmp->len)) {
+				goto done;
+			}
+		}
+
+		if (u_tmp->tx_buf) {
+			k_tmp->tx_buf = buf;
+			if (copy_from_user(buf, (const u8 __user *)(uintptr_t)
+					   u_tmp->tx_buf, u_tmp->len)) {
+				goto done;
+			}
+
+			if (u_tmp->len >= 9
+			    && (spich->flags & SPICH_FLAG_TIMESTAMPS_ENABLED)) {
+				SET_U64(&buf[1], now_us);
+			}
+		}
+
+		buf += k_tmp->len;
+		bufrx += k_tmp->len;
+
+		k_tmp->cs_change = ! !u_tmp->cs_change;
+		k_tmp->bits_per_word = u_tmp->bits_per_word;
+		k_tmp->delay_usecs = u_tmp->delay_usecs;
+		k_tmp->speed_hz = u_tmp->speed_hz;
+
+		spi_message_add_tail(k_tmp, &msg);
+	}
+
+	status = spich_sync(spich, &msg);
+
+	if (status < 0) {
+		goto done;
+	}
+
+	buf = spich->bufferrx;
+	for (n = n_xfers, u_tmp = u_xfers; n; --n, ++u_tmp) {
+		if (u_tmp->rx_buf) {
+			if (__copy_to_user
+			    ((u8 __user *) (uintptr_t) u_tmp->rx_buf, buf,
+			     u_tmp->len)) {
+				status = -EFAULT;
+				goto done;
+			}
+		}
+
+		buf += u_tmp->len;
+	}
+
+	status = total;
+
+done:
+	kfree(k_xfers);
+	return status;
+}
+
+#define SPI_MODE_MASK                           \
+    (SPI_CPHA | SPI_CPOL | SPI_CS_HIGH          \
+    | SPI_LSB_FIRST | SPI_3WIRE | SPI_LOOP      \
+    | SPI_NO_CS | SPI_READY)
+
+static long spich_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	int err = 0;
+	struct spich_data *spich;
+	struct spi_device *spi;
+
+	if (_IOC_TYPE(cmd) != SPI_IOC_MAGIC) {
+		return -ENOTTY;
+	}
+
+	if (_IOC_DIR(cmd) & _IOC_READ) {
+		err =
+		    !access_ok(VERIFY_WRITE, (void __user *)arg,
+			       _IOC_SIZE(cmd));
+	}
+
+	if (err == 0 && (_IOC_DIR(cmd) & _IOC_WRITE)) {
+		err =
+		    !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
+	}
+
+	if (err) {
+		return -EFAULT;
+	}
+
+	spich = filp->private_data;
+	spin_lock_irq(&spich->spi_lock);
+	spi = spi_dev_get(spich->spi);
+	spin_unlock_irq(&spich->spi_lock);
+
+	if (spi == NULL) {
+		return -ESHUTDOWN;
+	}
+
+	mutex_lock(&spich->buf_lock);
+
+	switch (cmd) {
+	case SPI_IOC_RESET_HUB:
+		{
+			u32 tmp;
+			err = __get_user(tmp, (u8 __user *) arg);
+
+			if (err != 0) {
+				break;
+			}
+
+			gpio_set_value(spich->gpio_array[GPIO_IDX_BOOT0].gpio,
+				       tmp ? 1 : 0);
+
+			gpio_set_value(spich->gpio_array[GPIO_IDX_NRST].gpio,
+				       0);
+			mdelay(50);
+			gpio_set_value(spich->gpio_array[GPIO_IDX_NRST].gpio,
+				       1);
+			break;
+		}
+
+	case SPI_IOC_ENABLE_TIMESTAMPS:
+		{
+			u32 tmp;
+			err = __get_user(tmp, (u8 __user *) arg);
+			if (err != 0) {
+				break;
+			}
+
+			if (tmp) {
+				spich->flags |= SPICH_FLAG_TIMESTAMPS_ENABLED;
+			} else {
+				spich->flags &= ~SPICH_FLAG_TIMESTAMPS_ENABLED;
+			}
+			break;
+		}
+
+	case SPI_IOC_WR_MODE:
+		{
+			u32 tmp;
+			u8 save;
+
+			err = __get_user(tmp, (u8 __user *) arg);
+			if (err != 0) {
+				break;
+			}
+
+			save = spi->mode;
+			if (tmp & ~SPI_MODE_MASK) {
+				err = -EINVAL;
+				break;
+			}
+
+			tmp |= spi->mode & ~SPI_MODE_MASK;
+			spi->mode = (u8) tmp;
+
+			err = spi_setup(spi);
+			if (err < 0) {
+				spi->mode = save;
+			} else {
+				dev_dbg(&spi->dev, "spi mode %02x\n", tmp);
+			}
+			break;
+		}
+
+	case SPI_IOC_WR_LSB_FIRST:
+		{
+			u32 tmp;
+			u8 save;
+
+			err = __get_user(tmp, (u8 __user *) arg);
+			if (err != 0) {
+				break;
+			}
+
+			save = spi->mode;
+
+			if (tmp) {
+				spi->mode |= SPI_LSB_FIRST;
+			} else {
+				spi->mode &= ~SPI_LSB_FIRST;
+			}
+
+			err = spi_setup(spi);
+			if (err < 0) {
+				spi->mode = save;
+			} else {
+				dev_dbg(&spi->dev, "%csb first\n",
+					tmp ? 'l' : 'm');
+			}
+			break;
+		}
+
+	case SPI_IOC_WR_BITS_PER_WORD:
+		{
+			u32 tmp;
+			u8 save;
+
+			err = __get_user(tmp, (u8 __user *) arg);
+			if (err != 0) {
+				break;
+			}
+
+			save = spi->bits_per_word;
+
+			spi->bits_per_word = tmp;
+
+			err = spi_setup(spi);
+
+			if (err < 0) {
+				spi->bits_per_word = save;
+			} else {
+				dev_dbg(&spi->dev, "%d bits per word\n", tmp);
+			}
+			break;
+		}
+
+	case SPI_IOC_WR_MAX_SPEED_HZ:
+		{
+			u32 tmp;
+			u32 save;
+
+			err = __get_user(tmp, (__u32 __user *) arg);
+			if (err != 0) {
+				break;
+			}
+
+			save = spi->max_speed_hz;
+
+			spi->max_speed_hz = tmp;
+
+			err = spi_setup(spi);
+
+			if (err < 0) {
+				spi->max_speed_hz = save;
+			} else {
+				dev_dbg(&spi->dev, "%d Hz (max)\n", tmp);
+			}
+			break;
+		}
+
+	case SPI_IOC_TXRX:
+		{
+			struct spi_ioc_transfer t;
+
+			if (__copy_from_user(&t, (void __user *)arg, sizeof(t))) {
+				err = -EFAULT;
+			}
+
+			if (gpio_get_value
+			    (spich->gpio_array[GPIO_IDX_INT1].gpio) == 1) {
+				wait_for_completion_interruptible
+				    (&spich->int1_completion);
+			}
+
+			err = spich_message(spich, &t, 1);
+			break;
+		}
+
+	default:
+		{
+			u32 tmp;
+			unsigned n_ioc;
+			struct spi_ioc_transfer *ioc;
+
+			if (_IOC_NR(cmd) != _IOC_NR(SPI_IOC_MESSAGE(0))
+			    || _IOC_DIR(cmd) != _IOC_WRITE) {
+				err = -ENOTTY;
+				break;
+			}
+
+			tmp = _IOC_SIZE(cmd);
+			if ((tmp % sizeof(struct spi_ioc_transfer)) != 0) {
+				err = -EINVAL;
+				break;
+			}
+
+			n_ioc = tmp / sizeof(struct spi_ioc_transfer);
+			if (n_ioc == 0) {
+				break;
+			}
+
+			ioc = kmalloc(tmp, GFP_KERNEL);
+			if (!ioc) {
+				err = -ENOMEM;
+				break;
+			}
+
+			if (__copy_from_user(ioc, (void __user *)arg, tmp)) {
+				kfree(ioc);
+				err = -EFAULT;
+			} else {
+				err = spich_message(spich, ioc, n_ioc);
+			}
+
+			kfree(ioc);
+			break;
+		}
+	}
+
+	mutex_unlock(&spich->buf_lock);
+	spi_dev_put(spi);
+
+	return err;
+}
+
+#ifdef CONFIG_COMPAT
+static long spich_compat_ioctl(struct file *filp, unsigned int cmd,
+			       unsigned long arg)
+{
+	return spich_ioctl(filp, cmd, (unsigned long)compat_ptr(arg));
+}
+#else
+#define spich_compat_ioctl      NULL
+#endif
+
+static ssize_t spich_sync_read(struct spich_data *spich, size_t len)
+{
+	struct spi_transfer t = {
+		.tx_buf = spich->buffer,
+		.rx_buf = spich->bufferrx,
+		.len = len,
+	};
+	struct spi_message m;
+	struct timespec ts;
+	uint64_t now_us;
+	ssize_t status;
+
+	memset(spich->buffer, 0, len);
+	spich->buffer[0] = 0x55;
+
+	do_posix_clock_monotonic_gettime(&ts);
+	now_us = ts.tv_sec * 1000000ull + (ts.tv_nsec + 500ull) / 1000ull;
+	SET_U64(&spich->buffer[1], now_us);
+
+	spi_message_init(&m);
+	spi_message_add_tail(&t, &m);
+
+	status = spich_sync(spich, &m);
+
+	if (status != 0) {
+		return status;
+	}
+
+	status = len;
+	return status;
+}
+
+static ssize_t spich_read(struct file *filp, char __user * buf, size_t count,
+			  loff_t * f_pos)
+{
+	struct spich_data *spich;
+	ssize_t status;
+
+	if (count > SPICH_BUFFER_SIZE) {
+		return -EMSGSIZE;
+	}
+
+	spich = filp->private_data;
+	if (gpio_get_value(spich->gpio_array[GPIO_IDX_INT1].gpio) == 1) {
+		wait_for_completion_interruptible(&spich->int1_completion);
+	}
+
+	mutex_lock(&spich->buf_lock);
+
+	status = spich_sync_read(spich, count);
+
+	if (status > 0) {
+		unsigned long missing =
+		    copy_to_user(buf, spich->bufferrx, status);
+
+		if (missing == status) {
+			status = -EFAULT;
+		} else {
+			status -= missing;
+		}
+	}
+
+	mutex_unlock(&spich->buf_lock);
+
+	return status;
+}
+
+static unsigned int spich_poll(struct file *filp,
+			       struct poll_table_struct *wait)
+{
+	struct spich_data *spich;
+	unsigned int mask = 0;
+
+	spich = filp->private_data;
+
+	if (gpio_get_value(spich->gpio_array[GPIO_IDX_INT1].gpio) == 1) {
+		poll_wait(filp, &spich->int1_completion.wait, wait);
+	}
+
+	if (gpio_get_value(spich->gpio_array[GPIO_IDX_INT1].gpio) == 0) {
+		mask |= POLLIN | POLLRDNORM;
+	}
+
+	return mask;
+}
+
+static const struct file_operations spich_fops = {
+	.owner = THIS_MODULE,
+
+	.unlocked_ioctl = spich_ioctl,
+	.compat_ioctl = spich_compat_ioctl,
+	.open = spich_open,
+	.release = spich_release,
+	.llseek = no_llseek,
+	.read = spich_read,
+	.poll = spich_poll,
+};
+
+static int spich_probe(struct spi_device *spi)
+{
+	struct spich_data *spich;
+	int error = 0;
+	int i;
+
+	spich = kzalloc(sizeof(struct spich_data), GFP_KERNEL);
+
+	if (!spich) {
+		dev_err(&spi->dev, "memory allocation error!.\n");
+		return -ENOMEM;
+	}
+
+	spich->spi = spi;
+	spin_lock_init(&spich->spi_lock);
+	mutex_init(&spich->buf_lock);
+
+	spich->gpio_array[GPIO_IDX_INT1].flags = GPIOF_DIR_IN;
+	spich->gpio_array[GPIO_IDX_INT1].label = "contexthub,int1";
+	spich->gpio_array[GPIO_IDX_INT2].flags = GPIOF_OUT_INIT_LOW;
+	spich->gpio_array[GPIO_IDX_INT2].label = "contexthub,int2";
+	spich->gpio_array[GPIO_IDX_BOOT0].flags = GPIOF_OUT_INIT_LOW;
+	spich->gpio_array[GPIO_IDX_BOOT0].label = "contexthub,boot0";
+	spich->gpio_array[GPIO_IDX_NRST].flags = GPIOF_OUT_INIT_HIGH;
+	spich->gpio_array[GPIO_IDX_NRST].label = "contexthub,nrst";
+
+	spich->class = class_create(THIS_MODULE, DRV_CLASS_NAME);
+	if (IS_ERR(spich->class)) {
+		dev_err(&spich->spi->dev, "failed to create class.\n");
+		error = PTR_ERR(spich->class);
+		goto err_class_create;
+	}
+
+	error = alloc_chrdev_region(&spich->devno, 0, 1, DRV_NAME);
+	if (error < 0) {
+		dev_err(&spich->spi->dev, "alloc_chrdev_region failed.\n");
+		goto err_alloc_chrdev;
+	}
+
+	spich->device = device_create(spich->class, NULL, spich->devno,
+				      NULL, "%s", DRV_NAME);
+
+	if (IS_ERR(spich->device)) {
+		dev_err(&spich->spi->dev, "device_create failed.\n");
+		error = PTR_ERR(spich->device);
+		goto err_device_create;
+	}
+
+	cdev_init(&spich->cdev, &spich_fops);
+	spich->cdev.owner = THIS_MODULE;
+
+	error = cdev_add(&spich->cdev, spich->devno, 1);
+	if (error) {
+		dev_err(&spich->spi->dev, "cdev_add failed.\n");
+		goto err_device_create;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(spich->gpio_array); i++) {
+		int gpio;
+
+		gpio = of_get_named_gpio(spich->spi->dev.of_node,
+					 spich->gpio_array[i].label, 0);
+		if (gpio < 0) {
+			dev_err(&spich->spi->dev,
+				"failed to find %s property in DT\n",
+				spich->gpio_array[i].label);
+			goto err_device_create;
+		}
+		spich->gpio_array[i].gpio = gpio;
+		dev_info(&spich->spi->dev, "%s: %s=%u\n",
+			 __func__, spich->gpio_array[i].label,
+			 spich->gpio_array[i].gpio);
+	}
+
+	error = spich_init_instance(spich);
+	if (error) {
+		dev_err(&spich->spi->dev, "spich_init_instance failed.\n");
+		goto err_spich_init_instance;
+	}
+
+	spi_set_drvdata(spi, spich);
+
+	return 0;
+
+err_spich_init_instance:
+	cdev_del(&spich->cdev);
+
+err_device_create:
+	device_destroy(spich->class, spich->devno);
+
+err_alloc_chrdev:
+	class_destroy(spich->class);
+
+err_class_create:
+	kfree(spich);
+
+	return error;
+}
+
+static int spich_remove(struct spi_device *spi)
+{
+	struct spich_data *spich = spi_get_drvdata(spi);
+	unsigned users;
+
+	cdev_del(&spich->cdev);
+	device_destroy(spich->class, spich->devno);
+	class_destroy(spich->class);
+
+	spin_lock_irq(&spich->spi_lock);
+	spich->spi = NULL;
+	users = spich->users;
+	spi_set_drvdata(spi, NULL);
+	spin_unlock_irq(&spich->spi_lock);
+
+	if (users == 0) {
+		spich_destroy_instance(spich);
+		mutex_destroy(&spich->buf_lock);
+		kfree(spich);
+	}
+
+	return 0;
+}
+
+static const struct of_device_id spich_dt_ids[] = {
+	{.compatible = "contexthub," DRV_NAME},
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, spich_dt_ids);
+
+static struct spi_driver spich_spi_driver = {
+	.driver = {
+		   .name = DRV_NAME,
+		   .owner = THIS_MODULE,
+		   .of_match_table = of_match_ptr(spich_dt_ids),
+		   },
+	.probe = spich_probe,
+	.remove = spich_remove,
+	.suspend = spich_suspend,
+	.resume = spich_resume,
+};
+
+static int __init spich_init(void)
+{
+	pr_info(DRV_NAME ": %s\n", __func__);
+
+	if (spi_register_driver(&spich_spi_driver)) {
+		pr_err(DRV_NAME ": failed to spi_register_driver\n");
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static void __exit spich_exit(void)
+{
+	pr_info(DRV_NAME ": %s\n", __func__);
+
+	spi_unregister_driver(&spich_spi_driver);
+}
+
+module_init(spich_init);
+module_exit(spich_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Google");
+MODULE_DESCRIPTION("User mode SPI context hub interface");
---
+++ include/uapi/linux/spi/spi-contexthub.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef SPI_CONTEXHUB_H
+
+#define SPI_CONTEXHUB_H
+
+#define SPI_CPHA                0x01
+#define SPI_CPOL                0x02
+
+#define SPI_MODE_0              (0|0)
+#define SPI_MODE_1              (0|SPI_CPHA)
+#define SPI_MODE_2              (SPI_CPOL|0)
+#define SPI_MODE_3              (SPI_CPOL|SPI_CPHA)
+
+#define SPI_CS_HIGH             0x04
+#define SPI_LSB_FIRST           0x08
+#define SPI_3WIRE               0x10
+#define SPI_LOOP                0x20
+#define SPI_NO_CS               0x40
+#define SPI_READY               0x80
+
+#define SPI_IOC_MAGIC           'k'
+
+struct spi_ioc_transfer {
+	__u64 tx_buf;
+	__u64 rx_buf;
+
+	__u32 len;
+	__u32 speed_hz;
+
+	__u16 delay_usecs;
+	__u8 bits_per_word;
+	__u8 cs_change;
+	__u32 pad;
+
+	/* If the contents of 'struct spi_ioc_transfer' ever change
+	 * incompatibly, then the ioctl number (currently 0) must change;
+	 * ioctls with constant size fields get a bit more in the way of
+	 * error checking than ones (like this) where that field varies.
+	 *
+	 * NOTE: struct layout is the same in 64bit and 32bit userspace.
+	 */
+};
+
+/* not all platforms use <asm-generic/ioctl.h> or _IOC_TYPECHECK() ... */
+#define SPI_MSGSIZE(N) \
+	((((N)*(sizeof (struct spi_ioc_transfer))) < (1 << _IOC_SIZEBITS)) \
+	? ((N)*(sizeof (struct spi_ioc_transfer))) : 0)
+
+#define SPI_IOC_MESSAGE(N) _IOW(SPI_IOC_MAGIC, 0, char[SPI_MSGSIZE(N)])
+
+/* Read / Write of SPI mode (SPI_MODE_0..SPI_MODE_3) */
+#define SPI_IOC_RD_MODE                 _IOR(SPI_IOC_MAGIC, 1, __u8)
+#define SPI_IOC_WR_MODE                 _IOW(SPI_IOC_MAGIC, 1, __u8)
+
+/* Read / Write SPI bit justification */
+#define SPI_IOC_RD_LSB_FIRST            _IOR(SPI_IOC_MAGIC, 2, __u8)
+#define SPI_IOC_WR_LSB_FIRST            _IOW(SPI_IOC_MAGIC, 2, __u8)
+
+/* Read / Write SPI device word length (1..N) */
+#define SPI_IOC_RD_BITS_PER_WORD        _IOR(SPI_IOC_MAGIC, 3, __u8)
+#define SPI_IOC_WR_BITS_PER_WORD        _IOW(SPI_IOC_MAGIC, 3, __u8)
+
+/* Read / Write SPI device default max speed hz */
+#define SPI_IOC_RD_MAX_SPEED_HZ         _IOR(SPI_IOC_MAGIC, 4, __u32)
+#define SPI_IOC_WR_MAX_SPEED_HZ         _IOW(SPI_IOC_MAGIC, 4, __u32)
+
+#define SPI_IOC_ENABLE_TIMESTAMPS       _IOW(SPI_IOC_MAGIC, 5, __u8)
+#define SPI_IOC_RESET_HUB               _IOW(SPI_IOC_MAGIC, 6, __u8)
+#define SPI_IOC_TXRX                    _IOW(SPI_IOC_MAGIC, 7, char[SPI_MSGSIZE(1)])
+
+#endif // SPI_CONTEXHUB_H
