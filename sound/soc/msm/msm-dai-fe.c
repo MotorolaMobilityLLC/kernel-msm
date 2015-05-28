5d54a2e1e59bddc1d2c8eafea8c91f84d7e4e786
Sridhar Vashist <svashist@motorola.com>
2015-05-28 17:06:54 -0500

    IKXCLOCK-6750: ASoC: msm8226: configure mI2S-3 RX output for a2dp
    Change-Id: Icd52d164024c7bd4026009706a87b41e6e0d4dc6
    Signed-off-by: Sridhar Vashist <svashist@motorola.com>

--- sound/soc/msm/msm-dai-fe.c
+++ sound/soc/msm/msm-dai-fe.c
@@ -685,6 +685,16 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 		.probe = fe_dai_probe,
 	},
 	{
+		.playback = {
+			.stream_name = "Tertiary MI2S_RX Hostless Playback",
+			.aif_name = "TERT_MI2S_DL_HL",
+			.rates = SNDRV_PCM_RATE_8000_48000,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.channels_min = 1,
+			.channels_max = 2,
+			.rate_min = 8000,
+			.rate_max = 48000,
+		},
 		.capture = {
 			.stream_name = "Tertiary MI2S_TX Hostless Capture",
 			.aif_name = "TERT_MI2S_UL_HL",
