acfd7ae5a143def21a4c4313abdd0bb06e594944
Devin Kim <dojip.kim@lge.com>
2015-05-31 11:04:32 -0700

    nfc: bcm2079x: Remove unused MTK codes
    Change-Id: I81866f9aeaa37112171f0f1dcf9068df8a7d5a63
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/nfc/bcm2079x.c
+++ drivers/nfc/bcm2079x.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 Broadcom Corporation.
+ * Copyright (C) 2015 LGE Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,34 +42,12 @@
 #include <linux/nfc/bcm2079x.h>
 #include <linux/wakelock.h>
 
-//#define BCM2079X_MTK_PLATFORM 1
-#if defined (BCM2079X_MTK_PLATFORM)
-#include <cust_gpio_usage.h>
-#include <cust_eint.h>
-
-#include <mach/mt_gpio.h>
-#include <mach/eint.h>
-
-#include <linux/dma-mapping.h>
-#else
 #include <linux/clk.h>
-#endif
 
 /* do not change below */
 #define MAX_BUFFER_SIZE     288
 #define I2C_ID_NAME "bcm2079x"
 #define I2C_DEVICE_ADDR 0x76
-
-#if defined (BCM2079X_MTK_PLATFORM)
-#define GPIO_NFC_VEN  GPIO_NFC_VENB_PIN	//(105| 0x80000000)
-#define GPIO_NFC_IRQ  GPIO_IRQ_NFC_PIN	//(5 | 0x80000000)
-#define GPIO_NFC_WAKE GPIO_NFC_MODE	//(117 | 0x80000000)
-
-static struct i2c_board_info __initdata nfc_board_info = {
-	I2C_BOARD_INFO(I2C_ID_NAME, I2C_DEVICE_ADDR)
-};
-struct bcm2079x_dev *g_bcm2079x_dev;
-#endif
 
 struct bcm2079x_platform_data platform_data;
 
@@ -95,53 +74,7 @@ struct bcm2079x_dev {
 
 struct wake_lock nfc_wake_lock;
 
-#if defined(BCM2079X_MTK_PLATFORM)
-
-#else
 struct clk *s_clk;
-#endif
-
-#if defined(BCM2079X_MTK_PLATFORM)
-static u8 *I2CDMABuf_va = NULL;
-static u32 I2CDMABuf_pa = NULL;
-
-int nfc_i2c_dma_write(struct i2c_client *client, const uint8_t * buf, int len)
-{
-	int i = 0;
-
-	for (i = 0; i < len; i++)
-		I2CDMABuf_va[i] = buf[i];
-
-	if (len < 8) {
-		client->addr = client->addr & I2C_MASK_FLAG | I2C_ENEXT_FLAG;
-		return i2c_master_send(client, buf, len);
-	} else {
-		client->addr = client->addr &
-			I2C_MASK_FLAG | I2C_DMA_FLAG | I2C_ENEXT_FLAG;
-		return i2c_master_send(client, I2CDMABuf_pa, len);
-	}
-}
-
-int nfc_i2c_dma_read(struct i2c_client *client, uint8_t * buf, int len)
-{
-	int i = 0, ret = 0;
-
-	if (len < 8) {
-		client->addr = client->addr & I2C_MASK_FLAG | I2C_ENEXT_FLAG;
-		return i2c_master_recv(client, buf, len);
-	} else {
-		client->addr = client->addr &
-			I2C_MASK_FLAG | I2C_DMA_FLAG | I2C_ENEXT_FLAG;
-		ret = i2c_master_recv(client, I2CDMABuf_pa, len);
-		if (ret < 0)
-			return ret;
-
-		for (i = 0; i < len; i++)
-			buf[i] = I2CDMABuf_va[i];
-	}
-	return ret;
-}
-#endif /*End of BCM2079X_MTK_PLATFORM */
 
 static void bcm2079x_init_stat(struct bcm2079x_dev *bcm2079x_dev)
 {
@@ -154,17 +87,9 @@ static void bcm2079x_disable_irq(struct bcm2079x_dev *bcm2079x_dev)
 
 	spin_lock_irqsave(&bcm2079x_dev->irq_enabled_lock, flags);
 	if (bcm2079x_dev->irq_enabled) {
-#if defined (BCM2079X_MTK_PLATFORM)
-		mt_eint_mask(bcm2079x_dev->client->irq);
-#else
 		disable_irq_nosync(bcm2079x_dev->client->irq);
-#endif
 		if (bcm2079x_dev->irq_wake_enabled) {
-#if defined (BCM2079X_MTK_PLATFORM)
-
-#else
 			disable_irq_wake(bcm2079x_dev->client->irq);
-#endif
 			bcm2079x_dev->irq_wake_enabled = false;
 		}
 		bcm2079x_dev->irq_enabled = false;
@@ -179,16 +104,9 @@ static void bcm2079x_enable_irq(struct bcm2079x_dev *bcm2079x_dev)
 	spin_lock_irqsave(&bcm2079x_dev->irq_enabled_lock, flags);
 	if (!bcm2079x_dev->irq_enabled) {
 		bcm2079x_dev->irq_enabled = true;
-#if defined (BCM2079X_MTK_PLATFORM)
-		mt_eint_unmask(bcm2079x_dev->client->irq);
-#else
 		enable_irq(bcm2079x_dev->client->irq);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 		if (!bcm2079x_dev->irq_wake_enabled) {
-#if defined (BCM2079X_MTK_PLATFORM)
-#else
 			enable_irq_wake(bcm2079x_dev->client->irq);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 			bcm2079x_dev->irq_wake_enabled = true;
 		}
 	}
@@ -229,11 +147,7 @@ static int change_client_addr(struct bcm2079x_dev *bcm2079x_dev, int addr)
 		"0x%04x, addr_data[%d] = %02x\n",
 		client->addr, client->flags, sizeof(addr_data) - 1,
 		addr_data[sizeof(addr_data) - 1]);
-#if defined (BCM2079X_MTK_PLATFORM)
-	ret = nfc_i2c_dma_write(client, addr_data, sizeof(addr_data));
-#else
 	ret = i2c_master_send(client, addr_data, sizeof(addr_data));
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	if (ret != sizeof(addr_data)) {
 		client->addr = ALIAS_ADDRESS;
 		client->flags &= ~I2C_CLIENT_TEN;
@@ -241,11 +155,7 @@ static int change_client_addr(struct bcm2079x_dev *bcm2079x_dev, int addr)
 			"0x%04x, addr_data[%d] = %02x\n",
 			client->addr, client->flags, sizeof(addr_data) - 1,
 			addr_data[sizeof(addr_data) - 1]);
-#if defined (BCM2079X_MTK_PLATFORM)
-		ret = nfc_i2c_dma_write(client, addr_data, sizeof(addr_data));
-#else
 		ret = i2c_master_send(client, addr_data, sizeof(addr_data));
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	}
 	client->addr = addr_data[5];
 
@@ -256,19 +166,9 @@ static int change_client_addr(struct bcm2079x_dev *bcm2079x_dev, int addr)
 	return (ret == sizeof(addr_data) ? 0 : -EIO);
 }
 
-#if defined (BCM2079X_MTK_PLATFORM)
-static void bcm2079x_dev_irq_handler(void)
-#else
 static irqreturn_t bcm2079x_dev_irq_handler(int irq, void *dev_id)
-#endif				/*End of BCM2079X_MTK_PLATFORM */
 {
-#if defined (BCM2079X_MTK_PLATFORM)
-	struct bcm2079x_dev *bcm2079x_dev = g_bcm2079x_dev;
-	pr_info("%s irq_gpio %d\n", __func__,
-		mt_get_gpio_in(platform_data.irq_gpio));
-#else
 	struct bcm2079x_dev *bcm2079x_dev = dev_id;
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	wake_up(&bcm2079x_dev->read_wq);
 	wake_lock(&nfc_wake_lock);
 
@@ -282,26 +182,12 @@ static unsigned int bcm2079x_dev_poll(struct file *filp, poll_table * wait)
 	unsigned long flags;
 
 	/* patch for GKI exception from Broadcom CSP 796884 */
-#if defined (BCM2079X_MTK_PLATFORM)
-	dev_info(&bcm2079x_dev->client->dev, "%s poll_wait before %d\n",
-		 __func__, mt_get_gpio_in(platform_data.irq_gpio));
 	poll_wait(filp, &bcm2079x_dev->read_wq, wait);
-	dev_info(&bcm2079x_dev->client->dev, "%s poll_wait after %d\n",
-		 __func__, mt_get_gpio_in(platform_data.irq_gpio));
-#else
-	poll_wait(filp, &bcm2079x_dev->read_wq, wait);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 
 	spin_lock_irqsave(&bcm2079x_dev->irq_enabled_lock, flags);
 
-#if defined (BCM2079X_MTK_PLATFORM)
-	if (mt_get_gpio_in(platform_data.irq_gpio) == 1) /* irq gpio get */
-#else
 	if (gpio_get_value(platform_data.irq_gpio) == 1) /* irq gpio get */
-#endif /*End of BCM2079X_MTK_PLATFORM */
-	{
 		mask |= POLLIN | POLLRDNORM;
-	}
 
 	spin_unlock_irqrestore(&bcm2079x_dev->irq_enabled_lock, flags);
 
@@ -326,11 +212,7 @@ static ssize_t bcm2079x_dev_read(struct file *filp, char __user * buf,
 	/* Read the first 4 bytes to include the length of the NCI or
 	 * HCI packet.
 	 */
-#if defined (BCM2079X_MTK_PLATFORM)
-	ret = nfc_i2c_dma_read(bcm2079x_dev->client, tmp, 4);
-#else
 	ret = i2c_master_recv(bcm2079x_dev->client, tmp, 4);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	if (ret == 4) {
 		total = ret;
 		/* First byte is the packet type */
@@ -355,14 +237,8 @@ static ssize_t bcm2079x_dev_read(struct file *filp, char __user * buf,
 		/* make sure full packet fits in the buffer */
 		if (len > 0 && (len + total) <= count) {
 			/* read the remainder of the packet. */
-#if defined (BCM2079X_MTK_PLATFORM)
-			ret = nfc_i2c_dma_read(
-					bcm2079x_dev->client, tmp + total, len);
-#else
 			ret = i2c_master_recv(
 					bcm2079x_dev->client, tmp + total, len);
-#endif /*End of BCM2079X_MTK_PLATFORM */
-
 			if (ret == len)
 				total += len;
 		}
@@ -397,11 +273,7 @@ static ssize_t bcm2079x_dev_write(struct file *filp, const char __user * buf,
 
 	mutex_lock(&bcm2079x_dev->rw_mutex);
 	/* Write data */
-#if defined (BCM2079X_MTK_PLATFORM)
-	ret = nfc_i2c_dma_write(bcm2079x_dev->client, tmp, count);
-#else
 	ret = i2c_master_send(bcm2079x_dev->client, tmp, count);
-#endif /*BCM2079X_MTK_PLATFORM */
 	if (ret != count) {
 		pr_err("failed to write %d\n", ret);
 		ret = -EIO;
@@ -441,21 +313,12 @@ static long bcm2079x_dev_unlocked_ioctl(struct file *filp,
 	case BCMNFC_CHANGE_ADDR:
 		return change_client_addr(bcm2079x_dev, arg);
 	case BCMNFC_POWER_CTL:
-		if (arg == 0) {
+		if (arg == 0)
 			wake_unlock(&nfc_wake_lock);
-		}
-#if defined(BCM2079X_MTK_PLATFORM)
-		mt_set_gpio_out(bcm2079x_dev->en_gpio, (int)arg);
-#else
 		gpio_set_value(bcm2079x_dev->en_gpio, arg);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 		break;
 	case BCMNFC_WAKE_CTL:
-#if defined (BCM2079X_MTK_PLATFORM)
-		mt_set_gpio_out(bcm2079x_dev->wake_gpio, (int)arg);
-#else
 		gpio_set_value(bcm2079x_dev->wake_gpio, arg);
-#endif /*BCM2079X_MTK_PLATFORM */
 		break;
 	default:
 		pr_err("%s, unknown cmd (%x, %lx)\n", __func__, cmd, arg);
@@ -486,35 +349,6 @@ static int bcm2079x_probe(struct i2c_client *client,
 		ret = -ENOMEM;
 		goto err_exit;
 	}
-#if defined (BCM2079X_MTK_PLATFORM)
-	g_bcm2079x_dev = bcm2079x_dev;
-
-	platform_data.irq_gpio = GPIO_NFC_IRQ;
-	platform_data.en_gpio = GPIO_NFC_VEN;
-	platform_data.wake_gpio = GPIO_NFC_WAKE;
-
-	/* irq_gpio setup */
-	pr_info("%s irq_gpio setup\n", __func__);
-	mt_set_gpio_mode(platform_data.irq_gpio, GPIO_IRQ_NFC_PIN_M_EINT);
-	mt_set_gpio_dir(platform_data.irq_gpio, GPIO_DIR_IN);
-	mt_set_gpio_pull_enable(platform_data.irq_gpio, GPIO_PULL_ENABLE);
-	mt_set_gpio_pull_select(platform_data.irq_gpio, GPIO_PULL_DOWN);
-
-	/* ven_gpio setup */
-	pr_info("%s ven_gpio setup\n", __func__);
-	mt_set_gpio_mode(platform_data.en_gpio, GPIO_NFC_VENB_PIN_M_GPIO);
-	mt_set_gpio_dir(platform_data.en_gpio, GPIO_DIR_OUT);
-
-	/* wake_gpio setup */
-	pr_info("%s mode_gpio setup\n", __func__);
-	mt_set_gpio_mode(platform_data.wake_gpio, GPIO_NFC_MODE_M_GPIO);
-	mt_set_gpio_dir(platform_data.wake_gpio, GPIO_DIR_OUT);
-
-	mt_set_gpio_out(platform_data.en_gpio, 1);
-	msleep(50);
-	mt_set_gpio_out(platform_data.en_gpio, 0);
-	mt_set_gpio_out(platform_data.wake_gpio, 0);
-#else
 	if (client->dev.of_node) {
 		platform_data.irq_gpio =
 		    of_get_named_gpio_flags(client->dev.of_node, "bcm,gpio_irq",
@@ -556,7 +390,6 @@ static int bcm2079x_probe(struct i2c_client *client,
 	msleep(50);
 	gpio_set_value(platform_data.en_gpio, 0);
 	gpio_set_value(platform_data.wake_gpio, 0);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 
 	bcm2079x_dev->wake_gpio = platform_data.wake_gpio;
 	bcm2079x_dev->irq_gpio = platform_data.irq_gpio;
@@ -586,21 +419,12 @@ static int bcm2079x_probe(struct i2c_client *client,
 	pr_info("requesting IRQ %d\n", client->irq);
 	bcm2079x_dev->irq_enabled = true;
 
-#if defined (BCM2079X_MTK_PLATFORM)
-	client->irq = CUST_EINT_IRQ_NFC_NUM;
-	mt_eint_set_sens(CUST_EINT_IRQ_NFC_NUM, CUST_EINT_IRQ_NFC_TYPE);
-	mt_eint_set_hw_debounce(CUST_EINT_IRQ_NFC_NUM,
-				CUST_EINT_IRQ_NFC_DEBOUNCE_CN);
-	mt_eint_registration(CUST_EINT_IRQ_NFC_NUM, CUST_EINT_IRQ_NFC_TYPE,
-			     bcm2079x_dev_irq_handler, 1);
-#else
 	ret = request_irq(client->irq, bcm2079x_dev_irq_handler,
 			  IRQF_TRIGGER_RISING, client->name, bcm2079x_dev);
 	if (ret) {
 		pr_err("request_irq failed\n");
 		goto err_request_irq_failed;
 	}
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	bcm2079x_dev->irq_wake_enabled = false;
 	bcm2079x_disable_irq(bcm2079x_dev);
 	i2c_set_clientdata(client, bcm2079x_dev);
@@ -650,7 +474,7 @@ static struct i2c_driver bcm2079x_driver = {
 	.driver = {
 		   .owner = THIS_MODULE,
 		   .name = I2C_ID_NAME,
-		   },
+	},
 };
 
 /*
@@ -659,15 +483,6 @@ static struct i2c_driver bcm2079x_driver = {
 
 static int __init bcm2079x_dev_init(void)
 {
-#if defined (BCM2079X_MTK_PLATFORM)
-	I2CDMABuf_va =
-	    (u8 *) dma_alloc_coherent(NULL, 4096, &I2CDMABuf_pa, GFP_KERNEL);
-	if (!I2CDMABuf_va) {
-		pr_err("%s: Allocate NFC DMA I2C Buffer failed!\n", __func__);
-		return ENOMEM;
-	}
-	i2c_register_board_info(2, &nfc_board_info, 1);
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	return i2c_add_driver(&bcm2079x_driver);
 }
 
@@ -675,13 +490,6 @@ module_init(bcm2079x_dev_init);
 
 static void __exit bcm2079x_dev_exit(void)
 {
-#if defined (BCM2079X_MTK_PLATFORM)
-	if (I2CDMABuf_va) {
-		dma_free_coherent(NULL, 4096, I2CDMABuf_va, I2CDMABuf_pa);
-		I2CDMABuf_va = NULL;
-		I2CDMABuf_pa = 0;
-	}
-#endif /*End of BCM2079X_MTK_PLATFORM */
 	i2c_del_driver(&bcm2079x_driver);
 }
 
