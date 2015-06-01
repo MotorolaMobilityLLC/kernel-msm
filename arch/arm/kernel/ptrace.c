992fe3c4c9abd8dcc35110b041d7e8889685b6ab
M1cha <sigmaepsilon92@gmail.com>
2015-06-01 11:48:39 +0200

    ptrace: fix ptrace defect cause by a merge fail
    this bug was introduced with I9493f28c30356a10eccb320e0a2d1a141388af9a
    
    Signed-off-by: M1cha <sigmaepsilon92@gmail.com>

--- arch/arm/kernel/ptrace.c
+++ arch/arm/kernel/ptrace.c
@@ -932,13 +932,6 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 	ip = regs->ARM_ip;
 	regs->ARM_ip = why;
 
-	/*
-	 * IP is used to denote syscall entry/exit:
-	 * IP = 0 -> entry, =1 -> exit
-	 */
-	ip = regs->ARM_ip;
-	regs->ARM_ip = why;
-
 	/* the 0x80 provides a way for the tracing parent to distinguish
 	   between a syscall stop and SIGTRAP delivery */
 	ptrace_notify(SIGTRAP | ((current->ptrace & PT_TRACESYSGOOD)
