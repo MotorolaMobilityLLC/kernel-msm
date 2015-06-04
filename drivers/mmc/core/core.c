624c7062184ff8cec1d6d168831cdc415de94af4
anarkia1976 <stefano.villa1976@gmail.com>
2015-06-04 22:18:05 +0200

    mmc: disable software crc - 30% boost

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -67,7 +67,7 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 1;
+bool use_spi_crc = 0;
 module_param(use_spi_crc, bool, 0);
 
 /*
