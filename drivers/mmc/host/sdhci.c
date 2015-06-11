9bd79ae397aded97402fbd0c653f947deb1291e3
Russ W. Knize <Russ.Knize@motorola.com>
2015-06-11 09:05:40 -0500

    IKSWL-20160: mmc: sdhci: don't ignore card events because of quirks
    Don't completely ignore card events from the core, even if the
    SDHCI_QUIRK_BROKEN_CARD_DETECTION is set.  If slot-gpio detection is
    implemented, fall back on that in lieu of the SDHCI state register.
    
    Change-Id: Iff1288248bab4f400b0c18edae7f406f9b0e8f1b
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/756417
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2665,17 +2665,14 @@ static void sdhci_card_event(struct mmc_host *mmc)
 	struct sdhci_host *host = mmc_priv(mmc);
 	unsigned long flags;
 
-	if (host->quirks & SDHCI_QUIRK_BROKEN_CARD_DETECTION)
-		return;
-
 	spin_lock_irqsave(&host->lock, flags);
 
 	/* Check host->mrq first in case we are runtime suspended */
 	if (host->mrq &&
-	    !(sdhci_readl(host, SDHCI_PRESENT_STATE) & SDHCI_CARD_PRESENT)) {
-		pr_err("%s: Card removed during transfer!\n",
-			mmc_hostname(host->mmc));
-		pr_err("%s: Resetting controller.\n",
+	    ((!(host->quirks & SDHCI_QUIRK_BROKEN_CARD_DETECTION) &&
+	      !(sdhci_readl(host, SDHCI_PRESENT_STATE) & SDHCI_CARD_PRESENT)) ||
+	     mmc_gpio_get_cd(host->mmc) == 0)) {
+		pr_err("%s: card removed during transfer\n",
 			mmc_hostname(host->mmc));
 
 		sdhci_reset(host, SDHCI_RESET_CMD);
