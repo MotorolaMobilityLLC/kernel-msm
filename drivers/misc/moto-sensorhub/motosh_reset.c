bb5a4f118ba8a5f054822e7b83dbba7bb2950c49
Daniel Moore <dtmoore@motorola.com>
2015-06-17 11:07:34 -0500

    drivers/misc: motosh:make aod touch mode configurable
    Make AOD touch mode configurable by the device
    tree.
    
    The default will be interrupt based and work with
    new HW.
    
    Legacy HW will be configured for both interrupt and
    polling modes.
    
    Change-Id: Ieb4c975a122815e7066d0ab0d035dd2f47a078ad
    Signed-off-by: Daniel Moore <dtmoore@motorola.com>
    Reviewed-on: http://gerrit.mot.com/757994
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>
    Reviewed-by: Philip Lee <pglee@motorola.com>
    Reviewed-by: James Rumpler <w12004@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Scott Debates <scottdebates@motorola.com>

--- drivers/misc/moto-sensorhub/motosh_reset.c
+++ drivers/misc/moto-sensorhub/motosh_reset.c
@@ -143,8 +143,9 @@ int motosh_reset_and_init(enum reset_mode mode)
 	rst_cmdbuff[3] = pdata->mag_orient & 0xff;
 	rst_cmdbuff[4] = pdata->panel_type & 0xff;
 	rst_cmdbuff[5] = pdata->IR_config & 0xff;
+	rst_cmdbuff[6] = pdata->aod_touch_mode & 0xff;
 	err = motosh_i2c_write_no_reset(motosh_misc_data,
-					rst_cmdbuff, 6);
+					rst_cmdbuff, 7);
 	if (err < 0) {
 		dev_err(&motosh_misc_data->client->dev,
 			"Unable to write sensor orientation value");
