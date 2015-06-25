62b4777fb0f7c5a3d952c340b45699b662200e49
Dorimanx <yuri@bynet.co.il>
2015-06-25 04:24:27 -0400

    block: zen: set fifo batch to 16 to reduce overload on eMMC and CPU.
    Conflicts:
    	block/sio-iosched.c
    	block/zen-iosched.c
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- block/zen-iosched.c
+++ block/zen-iosched.c
@@ -18,7 +18,7 @@ enum zen_data_dir { ASYNC, SYNC };
 
 static const int sync_expire  = HZ / 2;    /* max time before a sync is submitted. */
 static const int async_expire = 5 * HZ;    /* ditto for async, these limits are SOFT! */
-static const int fifo_batch = 1;
+static const int fifo_batch = 16;
 
 struct zen_data {
 	/* Runtime Data */
