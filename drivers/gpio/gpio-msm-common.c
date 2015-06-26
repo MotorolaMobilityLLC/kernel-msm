88a5c3c45d4dab84acbfa159b3ce3b574f9cb8b7
Sean Wan <swan@google.com>
2015-06-26 16:57:58 -0700

    Log wakeup reason.

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -27,6 +27,7 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+#include <linux/wakeup_reason.h>
 
 #include <mach/msm_iomap.h>
 #include <mach/gpiomux.h>
@@ -390,6 +391,8 @@ void msm_gpio_show_resume_irq(void)
 				name = "stray irq";
 			else if (desc->action && desc->action->name)
 				name = desc->action->name;
+
+                        log_wakeup_reason(irq);
 
 			pr_warning("%s: %d triggered %s\n",
 					__func__, irq, name);
