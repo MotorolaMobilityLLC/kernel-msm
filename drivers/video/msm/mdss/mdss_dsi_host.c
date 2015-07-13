5b10b64657d9267fd070a8110969dc7c4df10a26
Bang Nguyen <bangnguyen@motorola.com>
2015-07-13 10:30:35 -0500

    msm: mdss: Checking for HW IRQ when xxx_dma_tx() timeout
    If there is a thread that might blocks IRQ for long time that can cause
    ESD detection to fail because it doesn't have the DSI IRQ call back to
    indicate the power_mode successfully transfers and responds from panel.
    If there is timeout during waiting for the DSI IRQ, then read the DSI
    IRQ register to make sure DSI HW still works as expected.
    
    Change-Id: I8812efbec6fb96cb1b8bafc560e877d481fb1661
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/766252
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1717,6 +1717,28 @@ static int mdss_dsi_cmd_dma_tx(struct mdss_dsi_ctrl_pdata *ctrl,
 
 	ret = wait_for_completion_timeout(&ctrl->dma_comp,
 				msecs_to_jiffies(DMA_TX_TIMEOUT));
+
+	if (ret == 0) {
+		u32 reg_val, status, mask;
+		unsigned long flag;
+
+		spin_lock_irqsave(&ctrl->mdp_lock, flag);
+		reg_val = MIPI_INP(ctrl->ctrl_base + 0x0110);/* DSI_INTR_CTRL */
+		mask = reg_val & DSI_INTR_CMD_DMA_DONE_MASK;
+		status = mask & reg_val;
+		if (status) {
+			pr_warn("dma tx done but irq not triggered\n");
+			reg_val &= DSI_INTR_MASK_ALL;
+			/* clear CMD DMA isr only */
+			reg_val |= DSI_INTR_CMD_DMA_DONE;
+			MIPI_OUTP(ctrl->ctrl_base + 0x0110, reg_val);
+			mdss_dsi_disable_irq_nosync(ctrl, DSI_MDP_TERM);
+			complete(&ctrl->dma_comp);
+			ret = 1;
+		}
+		spin_unlock_irqrestore(&ctrl->mdp_lock, flag);
+	}
+
 	if (ret == 0) {
 		pr_err("%s(%d): wait for dma_comp timed out. ret = 0x%x\n",
 					__func__, ctrl->ndx, ret);
