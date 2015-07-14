6de822a91e349a93beef432f4f18a67d65551ead
soyoung.min <soyoung.min@lge.com>
2015-07-14 15:02:29 -0700

    msm: mdss: use usleep to have precise timing
    msleep often sleeps longer for (1m~10ms) when en/disable
    regulator. To improve display on time, msleep was replaced
    by usleep. As a result, it saves about 35ms during display on.
    
    Change-Id: I70c1b4c367d8092a03b78502f1260611fce41a90
    Signed-off-by: soyoung.min <soyoung.min@lge.com>

--- drivers/video/msm/mdss/mdss_io_util.c
+++ drivers/video/msm/mdss/mdss_io_util.c
@@ -223,7 +223,7 @@ int msm_dss_enable_vreg(struct dss_vreg *in_vreg, int num_vreg, int enable)
 			}
 			need_sleep = !regulator_is_enabled(in_vreg[i].vreg);
 			if (in_vreg[i].pre_on_sleep && need_sleep)
-				msleep(in_vreg[i].pre_on_sleep);
+				usleep(in_vreg[i].pre_on_sleep * 1000);
 			rc = regulator_set_optimum_mode(in_vreg[i].vreg,
 				in_vreg[i].enable_load);
 			if (rc < 0) {
@@ -234,7 +234,7 @@ int msm_dss_enable_vreg(struct dss_vreg *in_vreg, int num_vreg, int enable)
 			}
 			rc = regulator_enable(in_vreg[i].vreg);
 			if (in_vreg[i].post_on_sleep && need_sleep)
-				msleep(in_vreg[i].post_on_sleep);
+				usleep(in_vreg[i].post_on_sleep * 1000);
 			if (rc < 0) {
 				DEV_ERR("%pS->%s: %s enable failed\n",
 					__builtin_return_address(0), __func__,
@@ -246,12 +246,12 @@ int msm_dss_enable_vreg(struct dss_vreg *in_vreg, int num_vreg, int enable)
 		for (i = num_vreg-1; i >= 0; i--)
 			if (regulator_is_enabled(in_vreg[i].vreg)) {
 				if (in_vreg[i].pre_off_sleep)
-					msleep(in_vreg[i].pre_off_sleep);
+					usleep(in_vreg[i].pre_off_sleep * 1000);
 				regulator_set_optimum_mode(in_vreg[i].vreg,
 					in_vreg[i].disable_load);
 				regulator_disable(in_vreg[i].vreg);
 				if (in_vreg[i].post_off_sleep)
-					msleep(in_vreg[i].post_off_sleep);
+					usleep(in_vreg[i].post_off_sleep * 1000);
 			}
 	}
 	return rc;
@@ -262,12 +262,12 @@ disable_vreg:
 vreg_set_opt_mode_fail:
 	for (i--; i >= 0; i--) {
 		if (in_vreg[i].pre_off_sleep)
-			msleep(in_vreg[i].pre_off_sleep);
+			usleep(in_vreg[i].pre_off_sleep * 1000);
 		regulator_set_optimum_mode(in_vreg[i].vreg,
 			in_vreg[i].disable_load);
 		regulator_disable(in_vreg[i].vreg);
 		if (in_vreg[i].post_off_sleep)
-			msleep(in_vreg[i].post_off_sleep);
+			usleep(in_vreg[i].post_off_sleep * 1000);
 	}
 
 	return rc;
