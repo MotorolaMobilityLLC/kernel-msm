e7af5436e7bd4f7f3031a973178b73b5cfd886dd
Ravi Vembu <raviv@motorola.com>
2015-07-16 14:48:13 -0500

    factory: Fix warning due to incorrect OF API usage
    commit 4ecd31a0c6ab ("of: Generate warnings for malformed boolean
    properties") added a warning for malformed boolean properties. This
    results in the warning being thrown in several places where the
    mmi,factory-cable device tree node is being read. The API usage for
    reading factory cable status does not conform to the guidelines
    implemented by this commit.
    
    Change-Id: I02052e7acb4eeb96e3bf147f2eb74130904cdfa3
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/767608
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/max17042_battery.c
+++ drivers/power/max17042_battery.c
@@ -2054,14 +2054,13 @@ iterm_fail:
 static bool max17042_mmi_factory(void)
 {
 	struct device_node *np = of_find_node_by_path("/chosen");
-	bool factory = false;
+	u32 fact_cable = 0;
 
 	if (np)
-		factory = of_property_read_bool(np, "mmi,factory-cable");
+		of_property_read_u32(np, "mmi,factory-cable", &fact_cable);
 
 	of_node_put(np);
-
-	return factory;
+	return !!fact_cable ? true : false;
 }
 
 
