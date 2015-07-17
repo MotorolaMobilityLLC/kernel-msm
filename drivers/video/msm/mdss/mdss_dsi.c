a4854ba6814bdf3d937c30860233539bd440df78
Sandeep Panda <spanda@codeaurora.org>
2015-07-17 23:21:06 +0530

    msm: mdss: enable ref count based PM_QOS request for DSI
    DSI controller requests for PM_QOS while sending DCS commands,
    so that CPU does not go to idle state until DCS command transfer
    is finished. But this request can come independently from multiple
    clients such as different DSI controllers. So this change adds
    ref count based PM_QOS request to avoid warning for multiple QOS add
    request and unwanted results, if one client removes the request while
    the other clients still need it.
    
    Change-Id: If3ca0161923fcd73fdde77984aa5d80bfaec79a1
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>
    
    Mot-CRs-fixed: (CR)
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_dsi.h
    Change-Id: I5b85ff1fda5e23c93f096996a6bac9ebf390a79a
    Reviewed-on: http://gerrit.mot.com/865415
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Lian-Wei Wang <lian-wei.wang@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -45,17 +45,37 @@ static struct mdss_dsi_data *mdss_dsi_res;
 
 static struct pm_qos_request mdss_dsi_pm_qos_request;
 
-static void mdss_dsi_pm_qos_add_request(void)
+static void mdss_dsi_pm_qos_add_request(struct dsi_shared_data *sdata)
 {
-	pr_debug("%s: add request", __func__);
-	pm_qos_add_request(&mdss_dsi_pm_qos_request, PM_QOS_CPU_DMA_LATENCY,
-			PM_QOS_DEFAULT_VALUE);
+	if (!sdata)
+		return;
+
+	mutex_lock(&sdata->pm_qos_lock);
+	if (!sdata->pm_qos_req_cnt) {
+		pr_debug("%s: add request", __func__);
+		pm_qos_add_request(&mdss_dsi_pm_qos_request,
+			PM_QOS_CPU_DMA_LATENCY,	PM_QOS_DEFAULT_VALUE);
+	}
+	sdata->pm_qos_req_cnt++;
+	mutex_unlock(&sdata->pm_qos_lock);
 }
 
-static void mdss_dsi_pm_qos_remove_request(void)
+static void mdss_dsi_pm_qos_remove_request(struct dsi_shared_data *sdata)
 {
-	pr_debug("%s: remove request", __func__);
-	pm_qos_remove_request(&mdss_dsi_pm_qos_request);
+	if (!sdata)
+		return;
+
+	mutex_lock(&sdata->pm_qos_lock);
+	if (sdata->pm_qos_req_cnt) {
+		sdata->pm_qos_req_cnt--;
+		if (!sdata->pm_qos_req_cnt) {
+			pr_debug("%s: remove request", __func__);
+			pm_qos_remove_request(&mdss_dsi_pm_qos_request);
+		}
+	} else {
+		pr_warn("%s: unbalanced pm_qos ref count\n", __func__);
+	}
+	mutex_unlock(&sdata->pm_qos_lock);
 }
 
 static void mdss_dsi_pm_qos_update_request(int val)
@@ -3192,7 +3212,7 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 	else
 		ctrl_pdata->shared_data->dsi1_active = true;
 
-	mdss_dsi_pm_qos_add_request();
+	mdss_dsi_pm_qos_add_request(ctrl_pdata->shared_data);
 
 	return 0;
 
@@ -3390,6 +3410,7 @@ static int mdss_dsi_res_init(struct platform_device *pdev)
 		}
 
 		mutex_init(&sdata->phy_reg_lock);
+		mutex_init(&sdata->pm_qos_lock);
 
 		for (i = 0; i < DSI_CTRL_MAX; i++) {
 			mdss_dsi_res->ctrl_pdata[i] = devm_kzalloc(&pdev->dev,
@@ -3660,7 +3681,7 @@ static int mdss_dsi_ctrl_remove(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	mdss_dsi_pm_qos_remove_request();
+	mdss_dsi_pm_qos_remove_request(ctrl_pdata->shared_data);
 
 	if (msm_dss_config_vreg(&pdev->dev,
 			ctrl_pdata->panel_power_data.vreg_config,
