4c4a3722df4be673ee8fcb91c279753b38e5afba
Ecco Park <eccopark@broadcom.com>
2015-07-22 11:44:31 -0700

    net: wireless: bcmdhd: Hold the sdlock while doing fw_memdump
    1) Bug : 22450727
    2) We need to hold the sdlock to prevent watchdog thread
       from being executed.
    
    Change-Id: Ibf562933a8b698f0f11c37890cf83c1761d633fe
    Signed-off-by: Ecco Park <eccopark@broadcom.com>

--- drivers/net/wireless/bcmdhd/dhd_sdio.c
+++ drivers/net/wireless/bcmdhd/dhd_sdio.c
@@ -3160,6 +3160,7 @@ dhdsdio_mem_dump(dhd_bus_t *bus)
 		DHD_ERROR(("%s : dhd->soc_ram is NULL\n", __FUNCTION__));
 		return -1;
 	}
+	dhd_os_sdlock(bus->dhd);
 	BUS_WAKE(bus);
 	dhdsdio_clkctl(bus, CLK_AVAIL, FALSE);
 
@@ -3173,7 +3174,7 @@ dhdsdio_mem_dump(dhd_bus_t *bus)
 		if ((ret = dhdsdio_membytes(bus, FALSE, start, databuf, read_size)))
 		{
 			DHD_ERROR(("%s: Error membytes %d\n", __FUNCTION__, ret));
-			return -1;
+			break;
 		}
 		/* Decrement size and increment start address */
 		size -= read_size;
@@ -3186,9 +3187,10 @@ dhdsdio_mem_dump(dhd_bus_t *bus)
 		bus->activity = FALSE;
 		dhdsdio_clkctl(bus, CLK_NONE, TRUE);
 	}
-
-	dhd_save_fwdump(bus->dhd, dhd->soc_ram, dhd->soc_ram_length);
-	return 0;
+	dhd_os_sdunlock(bus->dhd);
+	if (!ret)
+		dhd_save_fwdump(bus->dhd, dhd->soc_ram, dhd->soc_ram_length);
+	return ret;
 }
 
 int
