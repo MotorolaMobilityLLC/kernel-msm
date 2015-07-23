fd0793bdb552884d340aa1b0cb7de4e40622abc2
Xiaoming Zhou <zhoux@codeaurora.org>
2015-07-23 15:52:45 -0400

    msm: mdss: re-order the ovlock to prevent dead-lock situation
    Ovlock should be acquired before acquiring mutex in the ESD thread
    such that ESD thread and off sequence are synchronized.
    
    Change-Id: Ibfc879162bf6875a2f431444eee1a912648b504c
    Signed-off-by: Xiaoming Zhou <zhoux@codeaurora.org>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -74,7 +74,6 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		return;
 	}
 
-	mutex_lock(&ctrl_pdata->mutex);
 
 	/*
 	 * TODO: Because mdss_dsi_cmd_mdp_busy has made sure DMA to
@@ -85,11 +84,12 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	 */
 	if (mipi->mode == DSI_CMD_MODE)
 		mutex_lock(&mdp5_data->ov_lock);
+	mutex_lock(&ctrl_pdata->mutex);
 
 	if (mdss_panel_is_power_off(pstatus_data->mfd->panel_power_state)) {
+		mutex_unlock(&ctrl_pdata->mutex);
 		if (mipi->mode == DSI_CMD_MODE)
 			mutex_unlock(&mdp5_data->ov_lock);
-		mutex_unlock(&ctrl_pdata->mutex);
 		pr_err("%s: DSI turning off, avoiding panel status check\n",
 							__func__);
 		return;
@@ -114,9 +114,9 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	ret = ctrl_pdata->check_status(ctrl_pdata);
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF);
 
+	mutex_unlock(&ctrl_pdata->mutex);
 	if (mipi->mode == DSI_CMD_MODE)
 		mutex_unlock(&mdp5_data->ov_lock);
-	mutex_unlock(&ctrl_pdata->mutex);
 
 	if ((pstatus_data->mfd->panel_power_state == MDSS_PANEL_POWER_ON)) {
 		if (ret > 0) {
