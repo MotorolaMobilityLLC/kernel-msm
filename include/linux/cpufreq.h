d83f044853cda665c0d0f1884e25a9e9f3b13085
Snuzzo <jrracinfan@gmail.com>
2015-07-22 19:03:41 -0700

    cpufreq: Add dancedance governor
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -507,6 +507,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_DANCEDANCE)
 #endif
 
 /*********************************************************************
