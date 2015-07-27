d4fe023c4ae47b23d556af760b271d8918a4d74d
Yuanyuan Zhong <zyy@motorola.com>
2015-07-27 12:15:53 -0500

    fs/seq_file: use vmalloc for large private field allocation
    When allocating buffer for private field with size larger than
    PAGE_SIZE, use vmalloc by default.
    
    Change-Id: I86bb9a72fdfdb2405d29f319c85f9e2f1080f374
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.mot.com/771147
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    (cherry picked from commit 7aab7ae4d5a7858eaa8b81279dd932a2007910b8)
    Reviewed-on: http://gerrit.mot.com/830933
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -649,7 +649,7 @@ int seq_release_private(struct inode *inode, struct file *file)
 {
 	struct seq_file *seq = file->private_data;
 
-	kfree(seq->private);
+	kvfree(seq->private);
 	seq->private = NULL;
 	return seq_release(inode, file);
 }
@@ -662,7 +662,10 @@ void *__seq_open_private(struct file *f, const struct seq_operations *ops,
 	void *private;
 	struct seq_file *seq;
 
-	private = kzalloc(psize, GFP_KERNEL);
+	if (psize > PAGE_SIZE)
+		private = vzalloc(psize);
+	else
+		private = kzalloc(psize, GFP_KERNEL);
 	if (private == NULL)
 		goto out;
 
@@ -675,7 +678,7 @@ void *__seq_open_private(struct file *f, const struct seq_operations *ops,
 	return private;
 
 out_free:
-	kfree(private);
+	kvfree(private);
 out:
 	return NULL;
 }
