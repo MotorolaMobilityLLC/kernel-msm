950a378a1b1856ff028cc88784822a1558a1fd39
kyungkeun.kim <kyungkeun.kim@lge.com>
2015-07-30 10:53:45 +0900

    power: qpnp-fg: fine tune the reported SOC
    Simplify conversion equation of reported SOC and fine tune
    the duration of 100% SOC.
    
    Change-Id: I8cf1a3dfd0a7369c1be802964bac1789933e393f
    Signed-off-by: kyungkeun.kim <kyungkeun.kim@lge.com>
    Signed-off-by: choongryeol.lee <choongryeol.lee@lge.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -1225,8 +1225,7 @@ static int get_prop_capacity(struct fg_chip *chip)
 		return EMPTY_CAPACITY;
 	else if (msoc == FULL_SOC_RAW)
 		return FULL_CAPACITY;
-	return DIV_ROUND_CLOSEST((msoc - 1) * (FULL_CAPACITY - 2),
-			FULL_SOC_RAW - 2) + 1;
+	return DIV_ROUND_UP(msoc * FULL_CAPACITY, FULL_SOC_RAW);
 }
 
 #define HIGH_BIAS	3
