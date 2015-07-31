b4f27eaae8def831754b64c42f3c5be5c544f058
Siva Yarravarapu <sivay@codeaurora.org>
2015-07-31 13:56:18 -0700

    msm: socinfo: add new hw_platform_subtype for msm8926
    Add new hw_platform_subtype to support wearables platform
    with jdi panel.
    
    Change-Id: I9e63d918970777cff3145b8a214a58de10c696f2
    Signed-off-by: Siva Yarravarapu <sivay@codeaurora.org>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -111,6 +111,7 @@ enum {
 	PLATFORM_SUBTYPE_STRANGE_2A = 0x3,
 	PLATFORM_SUBTYPE_QVGA = 0x4,
 	PLATFORM_SUBTYPE_G_QVGA = 0x5,
+	PLATFORM_SUBTYPE_JDI_QVGA = 0x6,
 	PLATFORM_SUBTYPE_INVALID,
 };
 
@@ -121,6 +122,7 @@ const char *hw_platform_subtype[] = {
 	[PLATFORM_SUBTYPE_STRANGE_2A] = "strange_2a,",
 	[PLATFORM_SUBTYPE_QVGA] = "qvga",
 	[PLATFORM_SUBTYPE_G_QVGA] = "qvga_g",
+	[PLATFORM_SUBTYPE_JDI_QVGA] = "jdi_qvga",
 };
 
 /* Used to parse shared memory.  Must match the modem. */
