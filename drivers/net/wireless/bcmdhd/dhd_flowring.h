8bc4f7d39efaeea04bf937cfbd0905f644611f4e
Ecco Park <eccopark@broadcom.com>
2015-08-03 18:19:02 -0700

    net: wireless: bcmdhd: Prioritize 802.1x pkt
    1) Bug: 22339691
    2) Prioritize EAPOL packets to avoid delay
       In the processing EAPOL packets in case of high data traffic
    3) This code is required to work with new FW(7.35.79.96)
    
    Change-Id: I65e03b57ba2f23c60d8fa663df5843abc0ee34fe
    Signed-off-by: Ecco Park <eccopark@broadcom.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/dhd_flowring.h
+++ drivers/net/wireless/bcmdhd/dhd_flowring.h
@@ -6,13 +6,13 @@
  * flow rings at high level
  *
  * Copyright (C) 1999-2014, Broadcom Corporation
- * 
+ *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
  * following added to such license:
- * 
+ *
  *      As a special exception, the copyright holders of this software give you
  * permission to link this software with independent modules, and to copy and
  * distribute the resulting executable under terms of your choice, provided that
@@ -20,7 +20,7 @@
  * the license of that module.  An independent module is a module which is not
  * derived from this software.  The special exception does not apply to any
  * modifications of the software.
- * 
+ *
  *      Notwithstanding the above, under no circumstances may you combine this
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
@@ -54,6 +54,7 @@
 
 #define DHD_FLOW_PRIO_AC_MAP		0
 #define DHD_FLOW_PRIO_TID_MAP		1
+#define DHD_FLOW_PRIO_LLR_MAP		2
 
 
 /* Pkttag not compatible with PROP_TXSTATUS or WLFC */
