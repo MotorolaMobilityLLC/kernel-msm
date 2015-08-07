9749cf6cc37edaeb02b25ce8e7e23b9256398871
Greg Meiste <w30289@motorola.com>
2015-08-07 10:29:40 -0500

    tty: Minor changes to allow kernel modules to use tty ports
    Change-Id: Ib8b306082d8733b051add1bd1f9b3ec5b091c53f
    Signed-off-by: Greg Meiste <w30289@motorola.com>
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    [jasonh: Minor merge fixes for 4.4 kernel]

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -133,6 +133,7 @@ EXPORT_SYMBOL(tty_std_termios);
    into this file */
 
 LIST_HEAD(tty_drivers);			/* linked list of tty drivers */
+EXPORT_SYMBOL(tty_drivers);
 
 /* Mutex to protect creating and releasing a tty. This is shared with
    vt.c for deeply disgusting hack reasons */
@@ -157,7 +158,6 @@ static long tty_compat_ioctl(struct file *file, unsigned int cmd,
 #endif
 static int __tty_fasync(int fd, struct file *filp, int on);
 static int tty_fasync(int fd, struct file *filp, int on);
-static void release_tty(struct tty_struct *tty, int idx);
 
 /**
  *	free_tty_struct		-	free a disused tty
@@ -1575,6 +1575,7 @@ err_release_tty:
 	release_tty(tty, idx);
 	return ERR_PTR(retval);
 }
+EXPORT_SYMBOL(tty_init_dev);
 
 void tty_free_termios(struct tty_struct *tty)
 {
@@ -1690,7 +1691,7 @@ EXPORT_SYMBOL(tty_kref_put);
  *	of ttys that the driver keeps.
  *
  */
-static void release_tty(struct tty_struct *tty, int idx)
+void release_tty(struct tty_struct *tty, int idx)
 {
 	/* This should always be true but check for the moment */
 	WARN_ON(tty->index != idx);
@@ -1709,6 +1710,7 @@ static void release_tty(struct tty_struct *tty, int idx)
 	tty_kref_put(tty->link);
 	tty_kref_put(tty);
 }
+EXPORT_SYMBOL(release_tty);
 
 /**
  *	tty_release_checks - check a tty before real release
