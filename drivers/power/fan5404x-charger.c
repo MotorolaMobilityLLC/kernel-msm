5ae5834099754498df471ffd1c0d6bf948e7573d
Ravi Vembu <raviv@motorola.com>
2015-08-20 16:18:13 -0500

    power: fan5404x-charger: Avoid SW kill during kernel power off
    Check if a kernel reboot/shutdown is in progress before allowing
    SW Factory kill. Otherwise the reboot process (notifiers) is
    triggered again with unpredictable consequences.
    
    Change-Id: I07e4d4cbed7e8f08ea7c540c75d4f559d0e89f76
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/778193
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/fan5404x-charger.c
+++ drivers/power/fan5404x-charger.c
@@ -846,7 +846,8 @@ static void fan5404x_external_power_changed(struct power_supply *psy)
 		chip->factory_present = true;
 
 	if (chip->factory_mode && chip->usb_psy && chip->factory_present
-						&& !factory_kill_disable) {
+						&& !factory_kill_disable
+						&& !reboot_in_progress()) {
 		rc = chip->usb_psy->get_property(chip->usb_psy,
 			POWER_SUPPLY_PROP_ONLINE, &prop);
 		if (!rc && (prop.intval == 0) && !chip->usb_present) {
