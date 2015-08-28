68f808ca1739813a1a9d24dfd8fffab01e12d1f4
Kumar Anand <kanand@codeaurora.org>
2015-08-28 13:04:44 -0700

    Release 4.4.22.061 msm8992 LA.BF64.1.2.9
    Signed-off-by: Kumar Anand <kanand@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -42,8 +42,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            22
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            60
-#define QWLAN_VERSIONSTR               "4.4.22.060"
+#define QWLAN_VERSION_BUILD            61
+#define QWLAN_VERSIONSTR               "4.4.22.061"
 
 
 #define AR6320_REV1_VERSION             0x5000000
