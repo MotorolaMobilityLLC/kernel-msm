f4475963f37a9ece2672d7b29dea67782bf87e73
Lorenzo Colitti <lorenzo@google.com>
2015-08-31 15:38:44 +0900

    Revert "Handle 'sk' being NULL in UID-based routing."
    This reverts commit c841195caad53534838e30de3806f2eb2bff7ebb.

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -531,8 +531,7 @@ static void __build_flow_key(struct flowi4 *fl4, struct sock *sk,
 	flowi4_init_output(fl4, oif, mark, tos,
 			   RT_SCOPE_UNIVERSE, prot,
 			   flow_flags,
-			   iph->daddr, iph->saddr, 0, 0,
-			   sk ? sock_i_uid(sk) : 0);
+			   iph->daddr, iph->saddr, 0, 0, sock_i_uid(sk));
 }
 
 static void build_skb_flow_key(struct flowi4 *fl4, const struct sk_buff *skb,
