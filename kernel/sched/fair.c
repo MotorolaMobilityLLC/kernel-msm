8882445f98cf09256657d57126e9f907251dd9a3
Riley Andrews <riandrews@google.com>
2015-09-01 12:51:16 -0700

    sched: fair: Change the synchronous wakeup logic in hmp.
    If doing a synchronous wakeup keep the woken task on the same
    core as the waker, ignoring task history (i.e. power) on task placement.
    
    Change-Id: I911702ef2ed9e3705d19591da1f0e33d2198c1df

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2026,7 +2026,7 @@ static int select_best_cpu(struct task_struct *p, int target, int reason,
 		sync = 0;
 	}
 
-	if (small_task && !boost) {
+	if (small_task && !boost && !sync) {
 		best_cpu = best_small_task_cpu(p, sync);
 		prefer_idle = 0;	/* For sched_task_load tracepoint */
 		goto done;
@@ -2034,6 +2034,14 @@ static int select_best_cpu(struct task_struct *p, int target, int reason,
 
 	trq = task_rq(p);
 	cpumask_and(&search_cpus, tsk_cpus_allowed(p), cpu_online_mask);
+	if (sync) {
+		unsigned int cpuid = smp_processor_id();
+		if (cpumask_test_cpu(cpuid, &search_cpus)) {
+			best_cpu = cpuid;
+			goto done;
+		}
+	}
+
 	for_each_cpu(i, &search_cpus) {
 		struct rq *rq = cpu_rq(i);
 
