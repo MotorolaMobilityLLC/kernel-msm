65258b61321fa733faa608bbf18334cbdcaa6040
VijayaKumar T M <vtmuni@codeaurora.org>
2015-09-01 14:01:10 +0530

    msm:cpp: Ignore hardware interrupt when timer expires
    Check for validity of timer before processing hardware
    Interrupt. Also in case of timeout reset timer variable
    so that if interrupt in case is received after timeout
    can be ignored to avoid timing issue.
    
    Change-Id: I87f5cc64f6d6aa6dcbf5dcabd4c5e973fce68033
    Signed-off-by: dpxv34 <dpxv34@motorola.com>
    Reviewed-on: http://gerrit.mot.com/780468
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Satya Prasad Kottisa <fvgm76@motorola.com>

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -608,7 +608,8 @@ void msm_cpp_do_tasklet(unsigned long data)
 			if (tx_fifo[i] == MSM_CPP_MSG_ID_CMD) {
 				cmd_len = tx_fifo[i+1];
 				msg_id = tx_fifo[i+2];
-				if (msg_id == MSM_CPP_MSG_ID_FRAME_ACK) {
+				if (msg_id == MSM_CPP_MSG_ID_FRAME_ACK
+					&& (atomic_read(&cpp_timer.used))) {
 					CPP_DBG("Frame done!!\n");
 					/* delete CPP timer */
 					CPP_DBG("delete timer.\n");
@@ -617,8 +618,9 @@ void msm_cpp_do_tasklet(unsigned long data)
 					del_timer(&timer->cpp_timer);
 					timer->data.processed_frame = NULL;
 					msm_cpp_notify_frame_done(cpp_dev);
-				} else if (msg_id ==
-					MSM_CPP_MSG_ID_FRAME_NACK) {
+				} else if ((msg_id ==
+					MSM_CPP_MSG_ID_FRAME_NACK)
+					&& (atomic_read(&cpp_timer.used))) {
 					pr_err("NACK error from hw!!\n");
 					CPP_DBG("delete timer.\n");
 					timer = &cpp_timer;
@@ -1143,7 +1145,13 @@ static void msm_cpp_do_timeout_work(struct work_struct *work)
 
 	disable_irq(cpp_timer.data.cpp_dev->irq->start);
 	pr_err("Reloading firmware\n");
-	cpp_load_fw(cpp_timer.data.cpp_dev, NULL);
+	if (!atomic_read(&cpp_timer.used)) {
+		pr_err("Delayed trigger, IRQ serviced\n");
+		return;
+	}
+	atomic_set(&cpp_timer.used, 0);
+	cpp_load_fw(cpp_timer.data.cpp_dev,
+		cpp_timer.data.cpp_dev->fw_name_bin);
 	pr_err("Firmware loading done\n");
 	enable_irq(cpp_timer.data.cpp_dev->irq->start);
 	msm_camera_io_w_mb(0x8, cpp_timer.data.cpp_dev->base +
@@ -1151,11 +1159,6 @@ static void msm_cpp_do_timeout_work(struct work_struct *work)
 	msm_camera_io_w_mb(0xFFFF,
 		cpp_timer.data.cpp_dev->base +
 		MSM_CPP_MICRO_IRQGEN_CLR);
-
-	if (!atomic_read(&cpp_timer.used)) {
-		pr_err("Delayed trigger, IRQ serviced\n");
-		return;
-	}
 
 	this_frame = cpp_timer.data.processed_frame;
 	pr_err("ReInstalling cpp_timer\n");
@@ -1635,8 +1638,11 @@ long msm_cpp_subdev_ioctl(struct v4l2_subdev *sd,
 			}
 
 			kfree(process_frame->cpp_cmd_msg);
+			process_frame->cpp_cmd_msg = NULL;
 			kfree(process_frame);
+			process_frame = NULL;
 			kfree(event_qcmd);
+			event_qcmd = NULL;
 		} else {
 			pr_err("Empty command list\n");
 			mutex_unlock(&cpp_dev->mutex);
