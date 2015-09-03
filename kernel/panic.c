9b1420afd163a421160648e477cd5844040c4d5f
Jongrak Kwon <jongrak.kwon@lge.com>
2015-09-02 23:00:38 -0700

    panic: resume console on panic
    Device rebooted by kernel panic but no panic message if
    console was suspended. This patch enables console if it's
    suspended to get the panic messages.
    
    Bug: 23629167
    
    Change-Id: I61220b7de74f51677f3c0c8117bd8468ca9abcd3
    Signed-off-by: Jongrak Kwon <jongrak.kwon@lge.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -22,6 +22,7 @@
 #include <linux/sysrq.h>
 #include <linux/init.h>
 #include <linux/nmi.h>
+#include <linux/console.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/exception.h>
@@ -142,6 +143,9 @@ void panic(const char *fmt, ...)
 	smp_send_stop();
 
 	kmsg_dump(KMSG_DUMP_PANIC);
+
+        /* print last_kmsg even after console suspend */
+	resume_console();
 
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
