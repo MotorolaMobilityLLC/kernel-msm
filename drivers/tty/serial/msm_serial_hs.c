3ad643d6aa97772d83c5832a1f3d744074d78a4b
Naveen Ramaraj <nramaraj@codeaurora.org>
2015-09-03 11:56:54 -0700

    Revert "msm_serial_hs: handle resource voting with pm callbacks disabled"
    This reverts commit 456eb6916d8717d529c17cef8ceadd4ab2133398.
    
    The original change is not required when userspace holds appropriate
    wakelocks.
    
    Bug: 23754146
    Change-Id: I4c69efa3b5c534e738e0f1a006fb37870246e5a0
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -402,22 +402,13 @@ static void msm_hs_resource_unvote(struct msm_hs_port *msm_uport)
 	atomic_dec(&msm_uport->clk_count);
 	pm_runtime_mark_last_busy(uport->dev);
 	pm_runtime_put_autosuspend(uport->dev);
-	__pm_relax(&msm_uport->ws);
 }
 
  /* Vote for resources before accessing them */
 static void msm_hs_resource_vote(struct msm_hs_port *msm_uport)
 {
-	int ret;
 	struct uart_port *uport = &(msm_uport->uport);
-	__pm_stay_awake(&msm_uport->ws);
-	ret = pm_runtime_get_sync(uport->dev);
-	if (ret < 0 || msm_uport->pm_state != MSM_HS_PM_ACTIVE) {
-		MSM_HS_WARN("%s(): %p runtime PM callback not invoked",
-			__func__, uport->dev);
-		msm_hs_pm_resume(uport->dev);
-	}
-
+	pm_runtime_get_sync(uport->dev);
 	atomic_inc(&msm_uport->clk_count);
 }
 
@@ -690,7 +681,6 @@ static int msm_hs_remove(struct platform_device *pdev)
 	msm_uport->rx.buffer = NULL;
 	msm_uport->rx.rbuffer = 0;
 
-	wakeup_source_trash(&msm_uport->ws);
 	destroy_workqueue(msm_uport->hsuart_wq);
 	mutex_destroy(&msm_uport->mtx);
 
@@ -2249,6 +2239,12 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 	struct msm_hs_port *msm_uport = UARTDM_TO_MSM(uport);
 	msm_hs_resource_vote(UARTDM_TO_MSM(uport));
 
+	if (msm_uport->pm_state != MSM_HS_PM_ACTIVE) {
+		MSM_HS_WARN("%s(): %p runtime PM callback not invoked",
+			__func__, uport->dev);
+		msm_hs_pm_resume(uport->dev);
+	}
+
 	/* Clear the flag */
 	if (msm_uport->obs)
 		atomic_set(&msm_uport->client_req_state, 0);
@@ -2509,6 +2505,7 @@ static int msm_hs_startup(struct uart_port *uport)
 			goto free_uart_irq;
 		}
 	}
+
 
 	ret = msm_hs_config_uart_gpios(uport);
 	if (ret) {
@@ -3160,7 +3157,6 @@ static int msm_hs_probe(struct platform_device *pdev)
 	int core_irqres, bam_irqres, wakeup_irqres;
 	struct msm_serial_hs_platform_data *pdata = pdev->dev.platform_data;
 	unsigned long data;
-	struct tty_struct *tty;
 
 	if (pdev->dev.of_node) {
 		dev_dbg(&pdev->dev, "device tree enabled\n");
@@ -3366,8 +3362,6 @@ static int msm_hs_probe(struct platform_device *pdev)
 		uport->line = pdata->userid;
 	ret = uart_add_one_port(&msm_hs_driver, uport);
 	if (!ret) {
-		tty = msm_uport->uport.state->port.tty;
-		wakeup_source_init(&msm_uport->ws, tty->name);
 		msm_hs_clk_bus_unvote(msm_uport);
 		msm_serial_hs_rt_init(uport);
 		return ret;
@@ -3447,7 +3441,6 @@ static void msm_hs_shutdown(struct uart_port *uport)
 		disable_irq(uport->irq);
 
 	msm_uport->wakeup.enabled = false;
-
 	/* make sure tx lh finishes */
 	flush_kthread_worker(&msm_uport->tx.kworker);
 	ret = wait_event_timeout(msm_uport->tx.wait,
