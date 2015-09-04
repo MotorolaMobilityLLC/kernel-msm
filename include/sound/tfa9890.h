23f5af654357a47a97c5ef007ef4e5c65837a566
Xiaofei Ma <xiaofei@motorola.com>
2015-09-04 11:22:51 -0500

    ASOC TFA 9890: port tfa9890 driver
    Integrated smartboost driver.
    
    Change-Id: Ieae9296f7e9e443aa985418d9e386546e26edb7a
    Signed-off-by: Xiaofei Ma <xiaofei@motorola.com>
    Reviewed-on: http://gerrit.mot.com/781560
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Jonathan Eklund <jeklund@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/sound/tfa9890.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2012 Motorola Mobility, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ */
+
+#ifndef _TFA9890_H
+#define _TFA9890_H
+
+struct tfa9890_pdata {
+	int reset_gpio;
+	int max_vol_steps;
+	const char *tfa_dev;
+	const char *fw_path;
+	const char *fw_name;
+	int pcm_start_delay;
+};
+
+#endif
