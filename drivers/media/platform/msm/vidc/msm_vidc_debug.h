a7abd906e03a37bd002ba41fef920663f70730b5
Riley Andrews <riandrews@google.com>
2015-09-04 15:13:56 -0700

    Turn off trace_printk in vidc's dprink.
    This saves us 12MB of memory + ctx switch overhead.
    
    Change-Id: I21d6b4c97e5068f87b00bbcab964548bbd207728
    Bug 23594723

--- drivers/media/platform/msm/vidc/msm_vidc_debug.h
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.h
@@ -42,7 +42,6 @@ enum vidc_msg_prio {
 
 enum vidc_msg_out {
 	VIDC_OUT_PRINTK = 0,
-	VIDC_OUT_FTRACE,
 };
 
 enum msm_vidc_debugfs_event {
@@ -104,10 +103,6 @@ extern int msm_vidc_thermal_mitigation_disabled;
 		if (msm_vidc_debug & __level) { \
 			if (msm_vidc_debug_out == VIDC_OUT_PRINTK) { \
 				pr_info(VIDC_DBG_TAG __fmt, \
-						VIDC_MSG_PRIO2STRING(__level), \
-						## arg); \
-			} else if (msm_vidc_debug_out == VIDC_OUT_FTRACE) { \
-				trace_printk(KERN_DEBUG VIDC_DBG_TAG __fmt, \
 						VIDC_MSG_PRIO2STRING(__level), \
 						## arg); \
 			} \
