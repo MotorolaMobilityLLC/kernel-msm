dbef23845b80b6988f67106a8b97e41d12a6a67c
Dmitry Shmidt <dimitrysh@google.com>
2015-09-03 17:40:50 -0700

    gpio: msm: Add wake source logging
    Bug: 23792736
    
    Change-Id: Ic00b39cf9769fc0da48c6c44cfc7c0232bdd7a90
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -27,6 +27,7 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+#inlcude <linux/wakeup_reason.h>
 
 #include <mach/msm_iomap.h>
 #include <mach/gpiomux.h>
@@ -385,6 +386,9 @@ void msm_gpio_show_resume_irq(void)
 			const char *name = "null";
 
 			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
+
+			log_base_wakeup_reason(irq);
+
 			desc = irq_to_desc(irq);
 			if (desc == NULL)
 				name = "stray irq";
