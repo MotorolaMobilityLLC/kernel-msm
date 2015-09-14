d56885d774b9e1e732326b7c8217cbec483a48a5
Jevin Tang <a21266@motorola.com>
2015-09-14 11:54:56 -0500

    tty: diag: fix qcomd code problem for msm8996
    1. null pointer in usb_diag_request_size
    2. dealock caused by write_lock
    
    Change-Id: If8973bd261829c1ac26c6e436d2e6aead6dce381
    Signed-off-by: Jevin Tang <a21266@motorola.com>
    Reviewed-on: http://gerrit.mot.com/782602
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/char/diag/diag_usb.c
+++ drivers/char/diag/diag_usb.c
@@ -303,7 +303,9 @@ static void diag_usb_write_done(struct diag_usb_info *ch,
 	int len = 0;
 	struct diag_usb_buf_tbl_t *entry = NULL;
 	unsigned char *buf = NULL;
+	/* MOT: comment out
 	unsigned long flags;
+	*/
 
 	if (!ch || !req)
 		return;
@@ -324,7 +326,12 @@ static void diag_usb_write_done(struct diag_usb_info *ch,
 	}
 	DIAG_LOG(DIAG_DEBUG_MUX, "full write_done, ctxt: %d\n",
 		 ctxt);
+	/* MOT: diag_usb_write uses write_lock as well, tty_diag_channel_write
+	directly send USB_DIAG_WRITE_DONE, this can cause a deadlock here.
+	Actually, diag_ws_on_copy_complete has its own lock, we don't need lock
+	here
 	spin_lock_irqsave(&ch->write_lock, flags);
+	*/
 	list_del(&entry->track);
 	ctxt = entry->ctxt;
 	buf = entry->buf;
