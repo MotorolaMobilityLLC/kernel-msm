ed9b0c4536e887bcd231abb35e3093f82ef40261
Paul Handrigan <Paul.Handrigan@cirrus.com>
2015-09-14 12:16:52 -0500

    florida: mfd: Merged in code from Cirrus v3.18
    code fro Cirrus v3.18 internal branch, including support for marley.
    patch for mfd changes
    
    Change-Id: I4ed1ff14f0efc2ff510ac7d41c87e967a9a563e6
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Reviewed-on: http://gerrit.mot.com/784206
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

--- drivers/mfd/arizona-spi.c
+++ drivers/mfd/arizona-spi.c
@@ -1,6 +1,7 @@
 /*
  * arizona-spi.c  --  Arizona SPI bus interface
  *
+ * Copyright 2014 Cirrus Logic
  * Copyright 2012 Wolfson Microelectronics plc
  *
  * Author: Mark Brown <broonie@opensource.wolfsonmicro.com>
@@ -28,6 +29,7 @@ static int arizona_spi_probe(struct spi_device *spi)
 	const struct spi_device_id *id = spi_get_device_id(spi);
 	struct arizona *arizona;
 	const struct regmap_config *regmap_config;
+	const struct regmap_config *regmap_32bit_config = NULL;
 	unsigned long type;
 	int ret;
 
@@ -42,9 +44,36 @@ static int arizona_spi_probe(struct spi_device *spi)
 		regmap_config = &wm5102_spi_regmap;
 		break;
 #endif
-#ifdef CONFIG_MFD_WM5110
+#ifdef CONFIG_MFD_FLORIDA
+	case WM8280:
 	case WM5110:
-		regmap_config = &wm5110_spi_regmap;
+		regmap_config = &florida_spi_regmap;
+		break;
+#endif
+#ifdef CONFIG_MFD_CLEARWATER
+	case WM8285:
+	case WM1840:
+		regmap_config = &clearwater_16bit_spi_regmap;
+		regmap_32bit_config = &clearwater_32bit_spi_regmap;
+		break;
+#endif
+#ifdef CONFIG_MFD_LARGO
+	case WM1831:
+	case CS47L24:
+		regmap_config = &largo_spi_regmap;
+		break;
+#endif
+#ifdef CONFIG_MFD_MARLEY
+	case CS47L35:
+		regmap_config = &marley_16bit_spi_regmap;
+		regmap_32bit_config = &marley_32bit_spi_regmap;
+		break;
+#endif
+#ifdef CONFIG_MFD_MOON
+	case CS47L90:
+	case CS47L91:
+		regmap_config = &moon_16bit_spi_regmap;
+		regmap_32bit_config = &moon_32bit_spi_regmap;
 		break;
 #endif
 	default:
@@ -65,6 +94,18 @@ static int arizona_spi_probe(struct spi_device *spi)
 		return ret;
 	}
 
+	if (regmap_32bit_config) {
+		arizona->regmap_32bit = devm_regmap_init_spi(spi,
+							   regmap_32bit_config);
+		if (IS_ERR(arizona->regmap_32bit)) {
+			ret = PTR_ERR(arizona->regmap_32bit);
+			dev_err(&spi->dev,
+				"Failed to allocate dsp register map: %d\n",
+				ret);
+			return ret;
+		}
+	}
+
 	arizona->type = id->driver_data;
 	arizona->dev = &spi->dev;
 	arizona->irq = spi->irq;
@@ -81,7 +122,17 @@ static int arizona_spi_remove(struct spi_device *spi)
 
 static const struct spi_device_id arizona_spi_ids[] = {
 	{ "wm5102", WM5102 },
+	{ "wm8280", WM8280 },
+	{ "wm8281", WM8280 },
 	{ "wm5110", WM5110 },
+	{ "wm8285", WM8285 },
+	{ "wm1840", WM1840 },
+	{ "wm1831", WM1831 },
+	{ "cs47l24", CS47L24 },
+	{ "cs47l35", CS47L35 },
+	{ "cs47l85", WM8285 },
+	{ "cs47l90", CS47L90 },
+	{ "cs47l91", CS47L91 },
 	{ },
 };
 MODULE_DEVICE_TABLE(spi, arizona_spi_ids);
