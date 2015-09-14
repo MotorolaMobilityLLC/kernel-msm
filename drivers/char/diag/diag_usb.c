d40fc056484791c1d50ed014a78d3452aaff37ef
Jevin Tang <a21266@motorola.com>
2015-09-14 11:54:56 -0500

    tty: diag: fix qcomd code problem for msm8998
    port from (CR)
    
    1. null pointer in usb_diag_request_size
    2. dealock caused by write_lock
    3. change the ttydiag module depends on ARCH_QCOM instead
       of ARCH_TTY
    4. Enable DIAG_OVER_TTY in Defenderconfig
    
    Change-Id: If8973bd261829c1ac26c6e436d2e6aead6dce381
    Signed-off-by: Jevin Tang <a21266@motorola.com>
    Reviewed-on: http://gerrit.mot.com/782602
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	drivers/char/diag/diag_usb.c
    	drivers/usb/gadget/function/f_diag.c
    Change-Id: I3e6b4fdba0d167e13d4a1d728549eb24ea1db059
    Reviewed-on: https://gerrit.mot.com/901862
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	arch/arm64/configs/ext_config/moto-sdm450-ali.config
    Change-Id: I7d15068fdb08f0ec869e5dbf9d97fd439bc37fae
    Reviewed-on: https://gerrit.mot.com/1168841
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Xiaoliang Duan <duanxl1@motorola.com>

--- drivers/char/diag/diag_usb.c
+++ drivers/char/diag/diag_usb.c
@@ -307,18 +307,27 @@ static void diag_usb_write_done(struct diag_usb_info *ch,
 	int len = 0;
 	struct diag_usb_buf_tbl_t *entry = NULL;
 	unsigned char *buf = NULL;
+	/* MOT: comment out
 	unsigned long flags;
+	*/
 
 	if (!ch || !req)
 		return;
 
+	/* MOT: diag_usb_write uses write_lock as well, tty_diag_channel_write
+	directly send USB_DIAG_WRITE_DONE, this can cause a deadlock here.
+	Actually, diag_ws_on_copy_complete has its own lock, we don't need lock
+	here
 	spin_lock_irqsave(&ch->write_lock, flags);
+	*/
 	ch->write_cnt++;
 	entry = diag_usb_buf_tbl_get(ch, req->context);
 	if (!entry) {
 		pr_err_ratelimited("diag: In %s, unable to find entry %pK in the table\n",
 				   __func__, req->context);
+		/* MOT: comment out
 		spin_unlock_irqrestore(&ch->write_lock, flags);
+		*/
 		return;
 	}
 	if (atomic_read(&entry->ref_count) != 0) {
