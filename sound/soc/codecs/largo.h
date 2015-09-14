a589463cfebea3a5cb80ee5b62909fad3c0bca2a
Paul Handrigan <Paul.Handrigan@cirrus.com>
2015-09-14 15:37:46 -0500

    florida: sound: Merged in code from Cirrus v3.18
    code fro Cirrus v3.18 internal branch, including support for marley.
    
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Change-Id: I0f8bab21ca3b61f0fa3e087577735d969de852c3
    Reviewed-on: http://gerrit.mot.com/784207
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

---
+++ sound/soc/codecs/largo.h
@@ -0,0 +1,23 @@
+/*
+ * largo.h  --  ALSA SoC Audio driver for Largo
+ *
+ * Copyright 2014 Cirrus Logic
+ *
+ * Author: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _LARGO_H
+#define _LARGO_H
+
+#include "arizona.h"
+
+#define LARGO_FLL1        1
+#define LARGO_FLL2        2
+#define LARGO_FLL1_REFCLK 3
+#define LARGO_FLL2_REFCLK 4
+
+#endif
---
+++ sound/soc/codecs/moon.h
@@ -0,0 +1,24 @@
+/*
+ * moon.h  --  ALSA SoC Audio driver for Moon-class codecs
+ *
+ * Copyright 2015 Cirrus Logic
+ *
+ * Author: Nikesh Oswal <nikesh@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _MOON_H
+#define _MOON_H
+
+#include "arizona.h"
+
+#define MOON_FLL1        1
+#define MOON_FLL2        2
+#define MOON_FLL1_REFCLK 3
+#define MOON_FLL2_REFCLK 4
+#define MOON_FLLAO       5
+
+#endif
