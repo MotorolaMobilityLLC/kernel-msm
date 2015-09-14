306f6994ad056991be798ac03f28de546d16645b
Paul Handrigan <Paul.Handrigan@cirrus.com>
2015-09-14 15:37:46 -0500

    florida: sound: Merged in code from Cirrus v3.18
    code fro Cirrus v3.18 internal branch, including support for marley.
    
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Change-Id: I0f8bab21ca3b61f0fa3e087577735d969de852c3
    Reviewed-on: http://gerrit.mot.com/784207
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

---
+++ sound/soc/codecs/marley.h
@@ -0,0 +1,21 @@
+/*
+ * marley.h  --  ALSA SoC Audio driver for Marley class codecs
+ *
+ * Copyright 2015 Cirrus Logic
+ *
+ * Author: Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _MARLEY_H
+#define _MARLEY_H
+
+#include "arizona.h"
+
+#define MARLEY_FLL1        1
+#define MARLEY_FLL1_REFCLK 3
+
+#endif
