7002497aaf9b2af3d2c39a64e884ad379e1efe5c
Lorenzo Colitti <lorenzo@google.com>
2015-09-15 00:14:23 +0900

    net: Fix NULL pointer dereference in tcp_nuke_addr.
    tcp_nuke addr only grabs the bottom half socket lock, but not the
    userspace socket lock. This allows a userspace program to call
    close() while the socket is running, which causes a NULL pointer
    dereference in inet_put_port.
    
    Bug: 23663111
    Bug: 24072792
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: Iecb63af68c2db4764c74785153d1c9054f76b94f
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: Prashant Kulkarni <prashank@motorola.com>
    (cherry picked from 74d66ee756afcc3269e4c1341f793c52be629af9)
    Reviewed-on: https://gerrit.mot.com/944275
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Srihari Sathyanarayana <sriharis@motorola.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3631,7 +3631,6 @@ restart:
 
 			lock_sock(sk);
 			local_bh_disable();
-			bh_lock_sock(sk);
 
 			if (!sock_flag(sk, SOCK_DEAD)) {
 				smp_wmb();  /* be consistent with tcp_reset */
@@ -3640,7 +3639,6 @@ restart:
 				tcp_done(sk);
 			}
 
-			bh_unlock_sock(sk);
 			local_bh_enable();
 			release_sock(sk);
 			sock_put(sk);
