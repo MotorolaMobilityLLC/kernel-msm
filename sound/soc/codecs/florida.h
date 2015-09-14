c778f0c8e87eacf5098c99291f1e3f515049a55a
Paul Handrigan <Paul.Handrigan@cirrus.com>
2015-09-14 15:37:46 -0500

    florida: sound: Merged in code from Cirrus v3.18
    code fro Cirrus v3.18 internal branch, including support for marley.
    
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Change-Id: I0f8bab21ca3b61f0fa3e087577735d969de852c3
    Reviewed-on: http://gerrit.mot.com/784207
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

---
+++ sound/soc/codecs/florida.h
@@ -0,0 +1,23 @@
+/*
+ * florida.h  --  ALSA SoC Audio driver for Florida-class codecs
+ *
+ * Copyright 2012 Wolfson Microelectronics plc
+ *
+ * Author: Mark Brown <broonie@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _FLORIDA_H
+#define _FLORIDA_H
+
+#include "arizona.h"
+
+#define FLORIDA_FLL1        1
+#define FLORIDA_FLL2        2
+#define FLORIDA_FLL1_REFCLK 3
+#define FLORIDA_FLL2_REFCLK 4
+
+#endif
---
+++ sound/soc/codecs/vegas.h
@@ -0,0 +1,23 @@
+/*
+ * vegas.h -- ALSA SoC Audio driver for Vegas codecs
+ *
+ * Copyright 2014-2015 Cirrus Logic
+ *
+ * Author: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _VEGAS_H
+#define _VEGAS_H
+
+#include "arizona.h"
+
+#define VEGAS_FLL1        1
+#define VEGAS_FLL2        2
+#define VEGAS_FLL1_REFCLK 3
+#define VEGAS_FLL2_REFCLK 4
+
+#endif
