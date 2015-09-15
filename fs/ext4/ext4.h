72f01570d42db79e4353e8dccda29d7dea627c3b
Russ W. Knize <Russ.Knize@motorola.com>
2015-09-15 09:49:31 -0500

    ext4: add LOOKUP_NOCASE support
    Add case-insensitive name lookup support.
    
    Change-Id: I1b414a8bf77144759502ea7ffeffd7c1532ec359
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -2368,7 +2368,8 @@ extern int ext4_search_dir(struct buffer_head *bh,
 			   struct ext4_filename *fname,
 			   const struct qstr *d_name,
 			   unsigned int offset,
-			   struct ext4_dir_entry_2 **res_dir);
+			   struct ext4_dir_entry_2 **res_dir,
+			   int flags);
 extern int ext4_generic_delete_entry(handle_t *handle,
 				     struct inode *dir,
 				     struct ext4_dir_entry_2 *de_del,
@@ -2833,7 +2834,8 @@ extern struct buffer_head *ext4_find_inline_entry(struct inode *dir,
 					struct ext4_filename *fname,
 					const struct qstr *d_name,
 					struct ext4_dir_entry_2 **res_dir,
-					int *has_inline_data);
+					int *has_inline_data,
+					int flags);
 extern int ext4_delete_inline_entry(handle_t *handle,
 				    struct inode *dir,
 				    struct ext4_dir_entry_2 *de_del,
