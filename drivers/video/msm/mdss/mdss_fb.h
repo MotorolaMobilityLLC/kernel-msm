af8fc067d60de798927b8dc21b26e8b609c70853
Shiyong Li <a22381@motorola.com>
2015-09-17 17:22:38 -0700

    msm: mdss: store panel info based on DSI bus number
    This change ensures every DSI channel has channel specific
    information to avoid possible incorrect sysfs output for two DSI
    channel cases.
    
    Change-Id: I1b5bc8566b1d02dd08fccfe5a88030368ddd17ab
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/785796
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Sandersary Crosby <zac@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -370,14 +370,6 @@ struct msm_fb_data_type {
 	struct input_handler *input_handler;
 };
 
-struct sys_panelinfo {
-	char *panel_name;
-	char *panel_supplier;
-	u64 *panel_ver;
-};
-
-extern struct sys_panelinfo panelinfo;
-
 static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)
 {
 	int needs_complete = 0;
