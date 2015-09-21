9d0fe7751f86d9182d64f229d0bada18a5116358
Ravikumar Vembu <raviv@motorola.com>
2015-09-21 11:44:32 -0500

    Revert "uid_cputime: Extends the cputime functionality to report power per uid"
    This reverts commit fc3cb81141fb504700bab718808dc9fa99815aaf.
    
    Change-Id: I1ed8c7a7313db9523cfcdf33ef025a4c572d0640
    Reviewed-on: http://gerrit.mot.com/784966
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/uid_cputime.c
+++ drivers/misc/uid_cputime.c
@@ -38,8 +38,6 @@ struct uid_entry {
 	cputime_t stime;
 	cputime_t active_utime;
 	cputime_t active_stime;
-	unsigned long long active_power;
-	unsigned long long power;
 	struct hlist_node hash;
 };
 
@@ -85,7 +83,6 @@ static int uid_stat_show(struct seq_file *m, void *v)
 	hash_for_each(hash_table, bkt, uid_entry, hash) {
 		uid_entry->active_stime = 0;
 		uid_entry->active_utime = 0;
-		uid_entry->active_power = 0;
 	}
 
 	read_lock(&tasklist_lock);
@@ -103,7 +100,6 @@ static int uid_stat_show(struct seq_file *m, void *v)
 		task_cputime_adjusted(task, &utime, &stime);
 		uid_entry->active_utime += utime;
 		uid_entry->active_stime += stime;
-		uid_entry->active_power += task->cpu_power;
 	}
 	read_unlock(&tasklist_lock);
 
@@ -112,12 +108,9 @@ static int uid_stat_show(struct seq_file *m, void *v)
 							uid_entry->active_utime;
 		cputime_t total_stime = uid_entry->stime +
 							uid_entry->active_stime;
-		unsigned long long total_power = uid_entry->power +
-							uid_entry->active_power;
-		seq_printf(m, "%d: %u %u %llu\n", uid_entry->uid,
+		seq_printf(m, "%d: %u %u\n", uid_entry->uid,
 						cputime_to_usecs(total_utime),
-						cputime_to_usecs(total_stime),
-						total_power);
+						cputime_to_usecs(total_stime));
 	}
 
 	mutex_unlock(&uid_lock);
@@ -210,7 +203,6 @@ static int process_notifier(struct notifier_block *self,
 	task_cputime_adjusted(task, &utime, &stime);
 	uid_entry->utime += utime;
 	uid_entry->stime += stime;
-	uid_entry->power += task->cpu_power;
 
 exit:
 	mutex_unlock(&uid_lock);
