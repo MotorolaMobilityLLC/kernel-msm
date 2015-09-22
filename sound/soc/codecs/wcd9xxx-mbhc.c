137a8435818202396f2e63069a64e905efc08af3
Ravi Kumar Alamanda <ralama@codeaurora.org>
2015-09-22 13:43:55 -0700

    ASoC: wcd9xxx-mbhc: Disable EU detection
    Disable HPH-R to GND switch to avoid noise during
    5-sec polling for headphone detection.
    
    Change-Id: I7063355110ae42b86df998aaf7404ad7c3bc3826
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>
    Signed-off-by: Ravi Kumar Alamanda <ralama@codeaurora.org>

--- sound/soc/codecs/wcd9xxx-mbhc.c
+++ sound/soc/codecs/wcd9xxx-mbhc.c
@@ -1925,7 +1925,7 @@ wcd9xxx_codec_cs_get_plug_type(struct wcd9xxx_mbhc *mbhc, bool highhph)
 	rt[0].mic_bias = false;
 
 	for (i = 1; i < NUM_DCE_PLUG_INS_DETECT - 1; i++) {
-		rt[i].swap_gnd = (i == NUM_DCE_PLUG_INS_DETECT - 3);
+		rt[i].swap_gnd = false;
 		rt[i].mic_bias = ((i == NUM_DCE_PLUG_INS_DETECT - 4) &&
 				   highhph);
 		rt[i].hphl_status = wcd9xxx_hphl_status(mbhc);
@@ -2003,7 +2003,7 @@ wcd9xxx_codec_get_plug_type(struct wcd9xxx_mbhc *mbhc, bool highhph)
 	rt[0].vddio = false;
 	rt[0].hwvalue = true;
 	for (i = 1; i < NUM_DCE_PLUG_INS_DETECT; i++) {
-		rt[i].swap_gnd = (i == NUM_DCE_PLUG_INS_DETECT - 2);
+		rt[i].swap_gnd = false;
 		if (detect_use_vddio_switch)
 			rt[i].vddio = (i == 1);
 		else
