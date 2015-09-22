d9131dd7c5b560ed010515ecb54c70de37f0cec1
Deva Ramasubramanian <dramasub@codeaurora.org>
2015-09-22 16:27:09 -0700

    msm: vidc: Apply a QOS vote when Venus is active
    During video playback, L2 power collapse is occurring far too often to
    actually save power.  As such, apply a vote to prevent L2 PC from
    occurring.
    
    Change-Id: I1d86b47a1ed9dffb02d099d3158892bf99ed955e
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_res_parse.c
+++ drivers/media/platform/msm/vidc/msm_vidc_res_parse.c
@@ -745,6 +745,10 @@ int read_platform_resources_from_dt(
 		dprintk(VIDC_DBG,
 				"Using fw-bias : %pa", &res->firmware_base);
 	}
+
+	of_property_read_u32(pdev->dev.of_node,
+			"qcom,pm-qos-latency-us", &res->pm_qos_latency_us);
+
 	return rc;
 err_load_max_hw_load:
 	msm_vidc_free_clock_table(res);
--- drivers/media/platform/msm/vidc/venus_hfi.h
+++ drivers/media/platform/msm/vidc/venus_hfi.h
@@ -17,6 +17,7 @@
 #include <linux/clk.h>
 #include <linux/mutex.h>
 #include <linux/platform_device.h>
+#include <linux/pm_qos.h>
 #include <linux/spinlock.h>
 #include <linux/msm_iommu_domains.h>
 #include <soc/qcom/ocmem.h>
@@ -205,6 +206,7 @@ struct venus_hfi_device {
 	enum venus_hfi_state state;
 	struct hfi_packetization_ops *pkt_ops;
 	enum hfi_packetization_type packetization_type;
+	struct pm_qos_request qos;
 };
 
 void venus_hfi_delete_device(void *device);
