45dc1cf486d9251946230eaf694a144a80b9b411
Hong-Mei Li <a21834@motorola.com>
2015-09-24 23:29:44 -0500

    printk: add cpu info into kernel log
    Append cpu# to timestamp of per line kernel message.
    
    Change-Id: I1925ea99a79d5b58450701fe2ad018cd99e0458a
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -230,6 +230,7 @@ struct printk_log {
 #if defined(CONFIG_LOG_BUF_MAGIC)
 	u32 magic;		/* handle for ramdump analysis tools */
 #endif
+	u8 cpu;			/* which cpu that print the message*/
 }
 #ifdef CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS
 __packed __aligned(4)
@@ -424,7 +425,7 @@ static u32 truncate_msg(u16 *text_len, u16 *trunc_msg_len,
 static int log_store(int facility, int level,
 		     enum log_flags flags, u64 ts_nsec,
 		     const char *dict, u16 dict_len,
-		     const char *text, u16 text_len)
+		     const char *text, u16 text_len, u32 cpu)
 {
 	struct printk_log *msg;
 	u32 size, pad_len;
@@ -473,6 +474,7 @@ static int log_store(int facility, int level,
 		msg->ts_nsec = local_clock();
 	memset(log_dict(msg) + dict_len, 0, pad_len);
 	msg->len = size;
+	msg->cpu = (u8)cpu;
 
 	/* insert message */
 	log_next_idx += msg->len;
@@ -1015,7 +1017,7 @@ static inline void boot_delay_msec(int level)
 static bool printk_time = IS_ENABLED(CONFIG_PRINTK_TIME);
 module_param_named(time, printk_time, bool, S_IRUGO | S_IWUSR);
 
-static size_t print_time(u64 ts, char *buf)
+static size_t print_time(u64 ts, char *buf, u8 cpu)
 {
 	unsigned long rem_nsec;
 
@@ -1025,10 +1027,11 @@ static size_t print_time(u64 ts, char *buf)
 	rem_nsec = do_div(ts, 1000000000);
 
 	if (!buf)
-		return snprintf(NULL, 0, "[%5lu.000000] ", (unsigned long)ts);
+		return snprintf(NULL, 0, "[%5lu.000000,%u] ",
+			(unsigned long)ts, cpu);
 
-	return sprintf(buf, "[%5lu.%06lu] ",
-		       (unsigned long)ts, rem_nsec / 1000);
+	return sprintf(buf, "[%5lu.%06lu,%u] ",
+			(unsigned long)ts, rem_nsec / 1000, cpu);
 }
 
 static size_t print_prefix(const struct printk_log *msg, bool syslog, char *buf)
@@ -1050,7 +1053,8 @@ static size_t print_prefix(const struct printk_log *msg, bool syslog, char *buf)
 		}
 	}
 
-	len += print_time(msg->ts_nsec, buf ? buf + len : NULL);
+	len += print_time(msg->ts_nsec, buf ? buf + len : NULL,
+				msg->cpu);
 	return len;
 }
 
@@ -1537,14 +1541,20 @@ static struct cont {
 	u8 facility;			/* log facility of first message */
 	enum log_flags flags;		/* prefix, newline flags */
 	bool flushed:1;			/* buffer sealed and committed */
+	u8 cpu;				/* which cpu is using the cont*/
 } cont;
 
 static void cont_flush(enum log_flags flags)
 {
+	u32 this_cpu;
+
 	if (cont.flushed)
 		return;
 	if (cont.len == 0)
 		return;
+
+	this_cpu = smp_processor_id();
+	cont.cpu = (u8)this_cpu;
 
 	if (cont.cons) {
 		/*
@@ -1553,7 +1563,8 @@ static void cont_flush(enum log_flags flags)
 		 * line. LOG_NOCONS suppresses a duplicated output.
 		 */
 		log_store(cont.facility, cont.level, flags | LOG_NOCONS,
-			  cont.ts_nsec, NULL, 0, cont.buf, cont.len);
+			  cont.ts_nsec, NULL, 0, cont.buf, cont.len,
+			  this_cpu);
 		cont.flags = flags;
 		cont.flushed = true;
 	} else {
@@ -1562,7 +1573,7 @@ static void cont_flush(enum log_flags flags)
 		 * just submit it to the store and free the buffer.
 		 */
 		log_store(cont.facility, cont.level, flags, 0,
-			  NULL, 0, cont.buf, cont.len);
+			  NULL, 0, cont.buf, cont.len, this_cpu);
 		cont.len = 0;
 	}
 }
@@ -1603,7 +1614,7 @@ static size_t cont_print_text(char *text, size_t size)
 	size_t len;
 
 	if (cont.cons == 0 && (console_prev & LOG_NEWLINE)) {
-		textlen += print_time(cont.ts_nsec, text);
+		textlen += print_time(cont.ts_nsec, text, cont.cpu);
 		size -= textlen;
 	}
 
@@ -1684,7 +1695,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 		/* emit KERN_CRIT message */
 		printed_len += log_store(0, 2, LOG_PREFIX|LOG_NEWLINE, 0,
 					 NULL, 0, recursion_msg,
-					 strlen(recursion_msg));
+					 strlen(recursion_msg), this_cpu);
 	}
 
 	/*
@@ -1746,7 +1757,8 @@ asmlinkage int vprintk_emit(int facility, int level,
 		else
 			printed_len += log_store(facility, level,
 						 lflags | LOG_CONT, 0,
-						 dict, dictlen, text, text_len);
+						 dict, dictlen, text, text_len,
+						 this_cpu);
 	} else {
 		bool stored = false;
 
@@ -1769,7 +1781,8 @@ asmlinkage int vprintk_emit(int facility, int level,
 			printed_len += text_len;
 		else
 			printed_len += log_store(facility, level, lflags, 0,
-						 dict, dictlen, text, text_len);
+						 dict, dictlen, text, text_len,
+						 this_cpu);
 	}
 
 	logbuf_cpu = UINT_MAX;
