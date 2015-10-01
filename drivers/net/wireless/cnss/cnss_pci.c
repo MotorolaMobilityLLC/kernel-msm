04494f17cd7136a10ecd67c2aeb5aa3728d6a66c
Santhosh Gillella <gvx468@motorola.com>
2015-10-01 14:46:44 -0500

    Revert "cnss: Clear runtime_error flag when exit runtime PM feature"
    This reverts commit 732b4f57e8b5f7428133fd6d620dbf4360338b02.
    
    Reverting back to L-based wifi driver.
    
    Signed-off-by: Santhosh Gillella <gvx468@motorola.com>
    Change-Id: I3bba39acf8efff72902485c774fb01074f61e8eb
    Reviewed-on: http://gerrit.mot.com/787803
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Christopher Fries <cfries@motorola.com>
    Tested-by: Christopher Fries <cfries@motorola.com>

--- drivers/net/wireless/cnss/cnss_pci.c
+++ drivers/net/wireless/cnss/cnss_pci.c
@@ -3084,7 +3084,6 @@ EXPORT_SYMBOL(cnss_runtime_init);
 void cnss_runtime_exit(struct device *dev)
 {
 	pm_runtime_get_noresume(dev);
-	pm_runtime_set_active(dev);
 }
 EXPORT_SYMBOL(cnss_runtime_exit);
 module_init(cnss_initialize);
