ef5d3331ce1302f61613519e8dafd50b60613caf
Shengzhe Zhao <a18689@motorola.com>
2015-10-02 14:35:08 -0700

    mmc: temporarily reset mmc code to MPH24.47
    RT throttling issue may be related to new changes for mmc
    introduced in Qcom's drop AU15, temporarily reset mmc code to
    before AU15.
    
    Change-Id: Ic307958cb907a910bac143ae77493099b9ca7466
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Reviewed-on: http://gerrit.mot.com/788185
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1396,7 +1396,6 @@ static const struct mmc_bus_ops mmc_sd_ops = {
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
 	.change_bus_speed = mmc_sd_change_bus_speed,
-	.shutdown = mmc_sd_suspend,
 	.throttle_back = mmc_sd_throttle_back,
 };
 
@@ -1408,7 +1407,6 @@ static const struct mmc_bus_ops mmc_sd_ops_unsafe = {
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
 	.change_bus_speed = mmc_sd_change_bus_speed,
-	.shutdown = mmc_sd_suspend,
 	.throttle_back = mmc_sd_throttle_back,
 };
 
