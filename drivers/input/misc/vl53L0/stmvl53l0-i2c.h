ce302592b827c9c829d98d299c2494afaad702ca
Jingnan Zhu <e3334c@motorola.com>
2015-10-11 11:33:00 -0500

    IKQCOMSD1-3030 input: misc: vl5310: mot changes
    1. device config reading and power up/down sequence
    2. change for cci and i2c co-exist in code instead of compile option
    3. sar basic functions
    4. state managerment for sar, calibaration, camera
    5. added some camera required interface
    6. added cleanup logic for laser device close
    7. minor bug fix in st code
    8. removed some unneed code in st drivers
    
    Change-Id: If2a933c993e187cbcdf21f324528be73761e2fcc
    Signed-off-by: Jingnan Zhu <e3334c@motorola.com>
    Reviewed-on: http://gerrit.mot.com/790453
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Oleg Klementiev <oleg@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Change-Id: Iecb72733026f58b79cbeef44ad5364ea8bacb77b
    Reviewed-on: http://gerrit.mot.com/799754

--- drivers/input/misc/vl53L0/stmvl53l0-i2c.h
+++ drivers/input/misc/vl53L0/stmvl53l0-i2c.h
@@ -23,17 +23,24 @@
 #ifndef STMVL53L0_I2C_H
 #define STMVL53L0_I2C_H
 #include <linux/types.h>
+#include <soc/qcom/camera2.h>
+#include "msm_camera_i2c.h"
+#include "msm_camera_dt_util.h"
+#include "msm_camera_io_util.h"
+#include "msm_cci.h"
 
-#ifndef CAMERA_CCI
 struct i2c_data {
 	struct i2c_client *client;
 	struct regulator *vana;
 	uint8_t power_up;
+	uint32_t lowv;
+	uint32_t highv;
+	struct msm_camera_gpio_conf gconf;
+	struct msm_pinctrl_info pinctrl_info;
 };
 int stmvl53l0_init_i2c(void);
 void stmvl53l0_exit_i2c(void *);
 int stmvl53l0_power_up_i2c(void *, unsigned int *);
 int stmvl53l0_power_down_i2c(void *);
 
-#endif /* NOT CAMERA_CCI */
 #endif /* STMVL53L0_I2C_H */
