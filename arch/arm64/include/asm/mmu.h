e08e7f5018a93ad18324cb933088623000f2675a
Jiangli Yuan <jlyuan@motorola.com>
2015-10-23 10:23:12 -0700

    arm64: Add earlyprintk support
      This is a combined patches of following commits from 8992 kernel:
    
        cb31f3a arm64: fixmap: fix missing sub-page offset for earlyprintk
        cbb4516 arm64: add early_ioremap support
        da5784d arm64: add support for MSM HSL earlyprintk
        182a6f7 arm64: Fix duplicate definition of early_console
        e53f2f4 arm64: add support for 8250/16550 earlyprintk
        0492f72 arm64: early_printk: add support for FastModel console output
        2475ff9 arm64: Add simple earlyprintk support
    
       To enable the earlyprintk, pls add "earlyprintk=msm_hsl_uart,0x075B0000"
       to the kernel cmdline.
    
    Change-Id: I3147c8488799820704b0feefaf671181ef2699a5
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Reviewed-on: /791415
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm64/include/asm/mmu.h
+++ arch/arm64/include/asm/mmu.h
@@ -36,4 +36,5 @@ extern void create_pgd_mapping(struct mm_struct *mm, phys_addr_t phys,
 			       unsigned long virt, phys_addr_t size,
 			       pgprot_t prot);
 
+extern void __iomem *early_io_map(phys_addr_t phys, unsigned long virt);
 #endif
