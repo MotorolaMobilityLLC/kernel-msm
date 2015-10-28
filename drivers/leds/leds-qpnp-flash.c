465c2fc1aebd1c64f6a55915d6c59641c0f2e155
Yinjun Chen <a7301c@motorola.com>
2015-10-27 20:47:50 -0500

    Revert "leds: leds-qpnp-flash: remove mutex lock when queue work thread"
    This reverts commit 7d60e6da3c3ca67d1b30ba8da59d7ddf5936fdca.

--- drivers/leds/leds-qpnp-flash.c
+++ drivers/leds/leds-qpnp-flash.c
@@ -1264,7 +1264,9 @@ static void qpnp_flash_led_brightness_set(struct led_classdev *led_cdev,
 		flash_node->prgm_current = value;
 	}
 
+	mutex_lock(&led->flash_led_lock);
 	queue_work(led->ordered_workq, &flash_node->work);
+	mutex_unlock(&led->flash_led_lock);
 
 	return;
 }
