880e687f78e1376c34f7fb7384769495f4b78f37
Amarnath Hullur Subramanyam <amarnath@codeaurora.org>
2015-10-28 21:28:52 -0700

    Release 4.4.22.080.1 msm8992 LA.BF64.1.2.9
    Release 4.4.22.080.1 msm8992 LA.BF64.1.2.9
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -42,8 +42,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            22
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            79
-#define QWLAN_VERSIONSTR               "4.4.22.079.1"
+#define QWLAN_VERSION_BUILD            80
+#define QWLAN_VERSIONSTR               "4.4.22.080.1"
 
 
 #define AR6320_REV1_VERSION             0x5000000
