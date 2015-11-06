aa35135fc7867e6d40a1b52ed1484936083906f7
Jiangli Yuan <jlyuan@motorola.com>
2015-11-06 14:19:56 -0800

    mmc/debugfs: re-write the mmc_bkops_stats_read/write
     Directly accessing userspace buf is not allowed from kernel space.
     copy_to_user/copy_from_user should be used.
    
     1. mmc_bkops_stats_read is replaced with seq_file's way to print BKOPS
        statistics.
     2. mmc_bkops_stats_write is implementated with copy_from_users.
    
    Change-Id: Ia28fb7ab9362b64eacd3e51b1aef3948eea9497c
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/798160
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Reviewed-by: Sheng-Zhe Zhao <a18689@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/core/debugfs.c
+++ drivers/mmc/core/debugfs.c
@@ -910,102 +910,71 @@ static const struct file_operations mmc_dbg_wr_pack_stats_fops = {
 	.write		= mmc_wr_pack_stats_write,
 };
 
-static int mmc_bkops_stats_open(struct inode *inode, struct file *filp)
+
+static int mmc_bkops_stats_show(struct seq_file *s, void *v)
 {
-	struct mmc_card *card = inode->i_private;
-
-	filp->private_data = card;
-
-	card->bkops_info.bkops_stats.print_stats = 1;
-	return 0;
-}
-
-static ssize_t mmc_bkops_stats_read(struct file *filp, char __user *ubuf,
-				     size_t cnt, loff_t *ppos)
-{
-	struct mmc_card *card = filp->private_data;
+	struct mmc_card *card = (struct mmc_card *)s->private;
 	struct mmc_bkops_stats *bkops_stats;
 	int i;
-	char *temp_buf;
 
 	if (!card)
-		return cnt;
-
-	if (!access_ok(VERIFY_WRITE, ubuf, cnt))
-		return cnt;
-
-	bkops_stats = &card->bkops_info.bkops_stats;
-
-	if (!bkops_stats->print_stats)
 		return 0;
 
+	bkops_stats = &card->bkops_info.bkops_stats;
 	if (!bkops_stats->enabled) {
 		pr_info("%s: bkops statistics are disabled\n",
 			 mmc_hostname(card->host));
 		goto exit;
 	}
-
-	temp_buf = kmalloc(TEMP_BUF_SIZE, GFP_KERNEL);
-	if (!temp_buf)
-		goto exit;
+	if (!bkops_stats->print_stats)
+		return 0;
 
 	spin_lock(&bkops_stats->lock);
-
-	memset(ubuf, 0, cnt);
-
-	snprintf(temp_buf, TEMP_BUF_SIZE, "%s: bkops statistics:\n",
+	seq_printf(s, "%s: bkops statistics:\n",
 		mmc_hostname(card->host));
-	strlcat(ubuf, temp_buf, cnt);
 
 	for (i = 0 ; i < BKOPS_NUM_OF_SEVERITY_LEVELS ; ++i) {
-		snprintf(temp_buf, TEMP_BUF_SIZE,
-			 "%s: BKOPS: due to level %d: %u\n",
+		seq_printf(s, "%s: BKOPS: due to level %d: %u\n",
 		 mmc_hostname(card->host), i, bkops_stats->bkops_level[i]);
-		strlcat(ubuf, temp_buf, cnt);
 	}
-
-	snprintf(temp_buf, TEMP_BUF_SIZE,
-		 "%s: BKOPS: stopped due to HPI: %u\n",
+	seq_printf(s, "%s: BKOPS: stopped due to HPI: %u\n",
 		 mmc_hostname(card->host), bkops_stats->hpi);
-	strlcat(ubuf, temp_buf, cnt);
-
-	snprintf(temp_buf, TEMP_BUF_SIZE,
-		 "%s: BKOPS: how many time host was suspended: %u\n",
+	seq_printf(s, "%s: BKOPS: how many time host was suspended: %u\n",
 		 mmc_hostname(card->host), bkops_stats->suspend);
-	strlcat(ubuf, temp_buf, cnt);
-
 	spin_unlock(&bkops_stats->lock);
-
-	kfree(temp_buf);
-
-	pr_info("%s", ubuf);
-
 exit:
-	if (bkops_stats->print_stats == 1) {
+	if (bkops_stats->print_stats == 1)
 		bkops_stats->print_stats = 0;
-		return strnlen(ubuf, cnt);
-	}
 
 	return 0;
+}
+
+static int mmc_bkops_stats_open(struct inode *inode, struct file *filp)
+{
+	struct mmc_card *card = inode->i_private;
+
+	card->bkops_info.bkops_stats.print_stats = 1;
+
+	return single_open(filp, mmc_bkops_stats_show, inode->i_private);
 }
 
 static ssize_t mmc_bkops_stats_write(struct file *filp,
 				      const char __user *ubuf, size_t cnt,
 				      loff_t *ppos)
 {
-	struct mmc_card *card = filp->private_data;
+	struct inode *in = filp->f_inode;
+	struct mmc_card *card = (struct mmc_card *)(in->i_private);
 	int value;
 	struct mmc_bkops_stats *bkops_stats;
 
 	if (!card)
 		return cnt;
 
-	if (!access_ok(VERIFY_READ, ubuf, cnt))
-		return cnt;
-
 	bkops_stats = &card->bkops_info.bkops_stats;
 
-	sscanf(ubuf, "%d", &value);
+	if (copy_from_user(&value, ubuf, sizeof(int)))
+		return -EFAULT;
+
 	if (value) {
 		mmc_blk_init_bkops_statistics(card);
 	} else {
@@ -1019,8 +988,10 @@ static ssize_t mmc_bkops_stats_write(struct file *filp,
 
 static const struct file_operations mmc_dbg_bkops_stats_fops = {
 	.open		= mmc_bkops_stats_open,
-	.read		= mmc_bkops_stats_read,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
 	.write		= mmc_bkops_stats_write,
+	.release	= single_release,
 };
 
 void mmc_add_card_debugfs(struct mmc_card *card)
@@ -1063,7 +1034,7 @@ void mmc_add_card_debugfs(struct mmc_card *card)
 
 	if (mmc_card_mmc(card) && (card->ext_csd.rev >= 5) &&
 	    (mmc_card_get_bkops_en_manual(card)))
-		if (!debugfs_create_file("bkops_stats", S_IRUSR, root, card,
+		if (!debugfs_create_file("bkops_stats", S_IRUSR | S_IWUSR, root, card,
 					 &mmc_dbg_bkops_stats_fops))
 			goto err;
 
