59f07d0362c39ff8c5b5503134f80c7306c5f927
flar2 <asegaert@gmail.com>
2015-11-08 17:39:13 -0500

    platform/msm: haptic: expose vibrate function

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -1770,6 +1770,11 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 	schedule_work(&hap->work);
 }
 
+void set_vibrate(int value)
+{
+	qpnp_hap_td_enable(&ghap->timed_dev, value);
+}
+
 /* play pwm bytes */
 int qpnp_hap_play_byte(u8 data, bool on)
 {
