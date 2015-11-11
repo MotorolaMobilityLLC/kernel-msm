06507cca39b6b78497db850133b1b7c57eec38d0
Russ W. Knize <Russ.Knize@motorola.com>
2015-11-11 14:25:33 -0600

    zram: use LZ4 by Defenderault
    Use the LZ4 compression algorithm by Defenderault for ZRAM.
    
    Change-Id: I8ae360ea4ca957ce46f6adfc279d3f39fe389e52
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -40,7 +40,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = "lzo";
+static const char *default_compressor = "lz4";
 
 /*
  * We don't need to see memory allocation errors more than once every 1
