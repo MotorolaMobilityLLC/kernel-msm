596439d0d0615044990beba3b3c115408fbcd756
SeungYoon Lee <w22905@motorola.com>
2015-11-18 21:32:38 -0600

    ASoC: msm8952: add OPSL mixer controls
    Opalum speaker protection algothithm runs at qDSP AFE.
    AudioHAL and factory commands need controls of it.
    
    new OSPL mixer commands will be the interface in between
    qDSP and AP over AFE APR messages.
    
    Change-Id: I8c8db7a2593a51ef9ffde19e7740fb5ddfdaf8c4
    Signed-off-by: SeungYoon Lee <w22905@motorola.com>
    Reviewed-on: http://gerrit.mot.com/801309
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/sound/ospl2xx.h
@@ -0,0 +1,93 @@
+/* Copyright (c) 2015-2016, Motorola Mobility, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __OSPL2XX_H__
+#define __OSPL2XX_H__
+
+#include <sound/apr_audio-v2.h>
+#include <sound/soc.h>
+
+/* module IDs */
+#define AFE_CUSTOM_OPALUM_RX_MODULE                    0x00A1AF00
+#define AFE_CUSTOM_OPALUM_TX_MODULE                    0x00A1BF00
+
+/* RX module parameter IDs */
+/* Switch between internal config strings */
+#define PARAM_ID_OPALUM_RX_SET_USE_CASE                0x00A1AF02
+/* Run production line test */
+#define PARAM_ID_OPALUM_RX_RUN_CALIBRATION             0x00A1AF03
+/* Initialize parameters from external config string */
+#define PARAM_ID_OPALUM_RX_SET_EXTERNAL_CONFIG         0x00A1AF05
+/* Get the currently set excursion model */
+#define PARAM_ID_OPALUM_RX_EXC_MODEL                   0x00A1AF06
+/* Get current temperature */
+#define PARAM_ID_OPLAUM_RX_TEMPERATURE                 0x00A1AF07
+/* Set temperature calibration values */
+#define PARAM_ID_OPALUM_RX_TEMP_CAL_DATA               0x00A1AF08
+
+/* TX module parameter IDs */
+/* Run production line test */
+#define PARAM_ID_OPALUM_TX_RUN_CALIBRATION             0x00A1BF03
+/* Get f0 calibration values */
+#define PARAM_ID_OPALUM_TX_F0_CALIBRATION_VALUE        0x00A1BF05
+/* Get impedance calibration values */
+#define PARAM_ID_OPALUM_TX_TEMP_MEASUREMENT_VALUE      0x00A1BF06
+/* Initialize parameters from external config string */
+#define PARAM_ID_OPALUM_TX_SET_EXTERNAL_CONFIG         0x00A1BF08
+
+struct afe_custom_opalum_set_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v2 param;
+	struct afe_port_param_data_v2 data;
+} __packed;
+
+struct afe_custom_opalum_get_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v2 param;
+	struct afe_port_param_data_v2 data;
+} __packed;
+
+struct opalum_single_data_ctrl_t {
+	int32_t value;
+};
+
+struct opalum_dual_data_ctrl_t {
+	int32_t data1;
+	int32_t data2;
+};
+
+struct opalum_f0_calib_data_t {
+	int f0;
+	int ref_diff;
+};
+
+struct opalum_temp_calib_data_t {
+	int acc;
+	int count;
+};
+
+struct opalum_tri_data_ctrl_t {
+	int32_t data1;
+	int32_t data2;
+	int32_t data3;
+};
+
+struct opalum_external_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	uint32_t done;
+	const char *config;
+};
+
+int ospl2xx_init(struct snd_soc_pcm_runtime *rtd);
+
+#endif /* __OSPL2XX_H__ */
