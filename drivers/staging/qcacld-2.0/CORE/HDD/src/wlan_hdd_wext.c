f4e4417cb8a37fd667cc699e24ccf17468ceeef7
Amarnath Hullur Subramanyam <amarnath@codeaurora.org>
2015-11-18 19:19:58 -0800

    qcacld-2.0: SSR-protect iw_set_mode()
    Currently iw_set_mode() does not use the SSR protection wrapper.
    This can lead to unexpected behavior if the function is executing
    concurrently with SSR, so add the SSR protection wrapper.
    
    Change-Id: Ied574a68b5f9dfbf75ab6157b3e80b25b69bd54b
    CRs-Fixed: 891029
    Git-commit: 342c9a73550ac9c5d2070faa1453314c067d69de
    Bug: 25740368
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_wext.c
@@ -1799,9 +1799,17 @@ static int iw_get_name(struct net_device *dev,
     return 0;
 }
 
-static int iw_set_mode(struct net_device *dev,
-                             struct iw_request_info *info,
-                             union iwreq_data *wrqu, char *extra)
+/**
+ * __iw_set_mode() - SIOCSIWMODE ioctl handler
+ * @dev: device upon which the ioctl was received
+ * @info: ioctl request information
+ * @wrqu: ioctl request data
+ * @extra: ioctl extra data
+ *
+ * Return: 0 on success, non-zero on error
+ */
+static int __iw_set_mode(struct net_device *dev, struct iw_request_info *info,
+			 union iwreq_data *wrqu, char *extra)
 {
     hdd_wext_state_t         *pWextState;
     hdd_adapter_t *pAdapter = WLAN_HDD_GET_PRIV_PTR(dev);
@@ -1874,13 +1882,33 @@ static int iw_set_mode(struct net_device *dev,
                      hddLog(VOS_TRACE_LEVEL_ERROR,
                             FL("failed wait on disconnect_comp_var"));
             }
-}
+        }
     }
 
     EXIT();
     return 0;
 }
 
+/**
+ * iw_set_mode() - SSR wrapper for __iw_set_mode()
+ * @dev: pointer to net_device
+ * @info: pointer to iw_request_info
+ * @wrqu: pointer to iwreq_data
+ * @extra: pointer to extra ioctl payload
+ *
+ * Return: 0 on success, error number otherwise
+ */
+static int iw_set_mode(struct net_device *dev, struct iw_request_info *info,
+		       union iwreq_data *wrqu, char *extra)
+{
+	int ret;
+
+	vos_ssr_protect(__func__);
+	ret = __iw_set_mode(dev, info, wrqu, extra);
+	vos_ssr_unprotect(__func__);
+
+	return ret;
+}
 
 static int
 iw_get_mode(struct net_device *dev, struct iw_request_info *info,
