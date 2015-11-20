3be08e2caea0c0a04f63b3188f36bb0a87cc0b5d
Paul Handrigan <Paul.Handrigan@cirrus.com>
2015-11-20 10:47:54 -0600

    ASoC: arizona: Add AoV trigger funtionality
    Add aov trigger functionality to the arizona codec platform.
    
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Change-Id: Ieb88a63b165e0bdf08d5fc8edea9929d781834e5
    Reviewed-on: http://gerrit.mot.com/810844
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

---
+++ sound/soc/codecs/aov_trigger.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014, Motorola Mobility, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __AOV_TRIGGER_H
+#define __AOV_TRIGGER_H
+
+struct snd_soc_codec;
+
+int aov_trigger_init(struct snd_soc_codec *codec);
+#endif /* __AOV_TRIGGER_H */
