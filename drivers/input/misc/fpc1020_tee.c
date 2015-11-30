4222fa8daa11dae6e216ddbe8b9269a15c45a496
Thomas Merrell-ATM098 <thomasmerrell@motorola.com>
2015-11-30 09:45:40 -0600

    input: fpc: Initial FPC sensor driver
    Adding FPS driver for Vector
    
    Change-Id: I553d5d38c7f05eb74db111496e97888ea3ad278b
    Signed-off-by: Thomas Merrell-ATM098 <thomasmerrell@motorola.com>
    Reviewed-on: http://gerrit.mot.com/803286
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Xiaofei Ma <xiaofei@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>

---
+++ drivers/input/misc/fpc1020_tee.c
@@ -0,0 +1,488 @@
+/*
+ * FPC1020 Fingerprint sensor device driver
+ *
+ * Copyright (c) 2015 Fingerprint Cards AB <tech@fingerprints.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License Version 2
+ * as published by the Free Software Foundation.
+ */
+
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/input.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/regulator/consumer.h>
+#include <linux/spi/spi.h>
+#include <linux/wakelock.h>
+#include <linux/notifier.h>
+
+struct vreg_config {
+	char *name;
+	unsigned long vmin;
+	unsigned long vmax;
+	int ua_load;
+};
+
+static const struct vreg_config const vreg_conf[] = {
+	{ "vdd_ana", 1800000UL, 1800000UL, 6000, },
+	{ "vcc_spi", 1800000UL, 1800000UL, 10, },
+	{ "vdd_io", 1800000UL, 1800000UL, 6000, },
+};
+
+struct FPS_data {
+	unsigned int enabled;
+	unsigned int state;
+	struct blocking_notifier_head nhead;
+} *fpsData;
+
+struct FPS_data *FPS_init(void)
+{
+	struct FPS_data *mdata = kzalloc(
+			sizeof(struct FPS_data), GFP_KERNEL);
+	if (mdata) {
+		BLOCKING_INIT_NOTIFIER_HEAD(&mdata->nhead);
+		pr_debug("%s: FPS notifier data structure init-ed\n", __func__);
+	}
+	return mdata;
+}
+
+int FPS_register_notifier(struct notifier_block *nb,
+	unsigned long stype, bool report)
+{
+	int error;
+	struct FPS_data *mdata = fpsData;
+
+	if (!mdata)
+		return -ENODEV;
+
+	mdata->enabled = (unsigned int)stype;
+	pr_debug("%s: FPS sensor %lu notifier enabled\n", __func__, stype);
+
+	error = blocking_notifier_chain_register(&mdata->nhead, nb);
+	if (!error && report) {
+		int state = mdata->state;
+		/* send current FPS state on register request */
+		blocking_notifier_call_chain(&mdata->nhead,
+				stype, (void *)&state);
+		pr_debug("%s: FPS reported state %d\n", __func__, state);
+	}
+	return error;
+}
+EXPORT_SYMBOL_GPL(FPS_register_notifier);
+
+int FPS_unregister_notifier(struct notifier_block *nb,
+		unsigned long stype)
+{
+	int error;
+	struct FPS_data *mdata = fpsData;
+
+	if (!mdata)
+		return -ENODEV;
+
+	error = blocking_notifier_chain_unregister(&mdata->nhead, nb);
+	pr_debug("%s: FPS sensor %lu notifier unregister\n", __func__, stype);
+
+	if (!mdata->nhead.head) {
+		mdata->enabled = 0;
+		pr_debug("%s: FPS sensor %lu no clients\n", __func__, stype);
+	}
+
+	return error;
+}
+
+void FPS_notify(unsigned long stype, int state)
+{
+	struct FPS_data *mdata = fpsData;
+
+	pr_err("%s: Enter", __func__);
+
+	if (!mdata) {
+		pr_err("%s: FPS notifier not initialized yet\n", __func__);
+		return;
+	}
+
+	pr_err("%s: FPS current state %d -> (0x%x)\n", __func__,
+	       mdata->state, state);
+
+	if (mdata->enabled && mdata->state != state) {
+		mdata->state = state;
+		blocking_notifier_call_chain(&mdata->nhead,
+					     stype, (void *)&state);
+		pr_err("%s: FPS notification sent\n", __func__);
+	} else if (!mdata->enabled) {
+		pr_err("%s: !mdata->enabled", __func__);
+	} else {
+		pr_err("%s: mdata->state==state", __func__);
+	}
+}
+
+struct fpc1020_data {
+	struct device *dev;
+	struct spi_device *spi;
+	struct clk *iface_clk;
+	struct clk *core_clk;
+	struct regulator *vreg[ARRAY_SIZE(vreg_conf)];
+	struct wake_lock wlock;
+
+	struct notifier_block nb;
+
+	int irq_gpio;
+	int rst_gpio;
+	int irq_num;
+	bool clocks_enabled;
+	bool clocks_suspended;
+};
+
+static int vreg_setup(struct fpc1020_data *fpc1020, const char *name,
+	bool enable)
+{
+	size_t i;
+	int rc;
+	struct regulator *vreg;
+	struct device *dev = fpc1020->dev;
+
+	for (i = 0; i < ARRAY_SIZE(fpc1020->vreg); i++) {
+		const char *n = vreg_conf[i].name;
+
+		if (!strncmp(n, name, strlen(n)))
+			goto found;
+	}
+	dev_err(dev, "Regulator %s not found\n", name);
+	return -EINVAL;
+found:
+	vreg = fpc1020->vreg[i];
+	if (enable) {
+		if (!vreg) {
+			vreg = regulator_get(dev, name);
+			if (!vreg) {
+				dev_err(dev, "Unable to get  %s\n", name);
+				return -ENODEV;
+			}
+		}
+		if (regulator_count_voltages(vreg) > 0) {
+			rc = regulator_set_voltage(vreg, vreg_conf[i].vmin,
+					vreg_conf[i].vmax);
+			if (rc)
+				dev_err(dev,
+					"Unable to set voltage on %s, %d\n",
+					name, rc);
+		}
+		rc = regulator_set_optimum_mode(vreg, vreg_conf[i].ua_load);
+		if (rc < 0)
+			dev_err(dev, "Unable to set current on %s, %d\n",
+					name, rc);
+		rc = regulator_enable(vreg);
+		if (rc) {
+			dev_err(dev, "error enabling %s: %d\n", name, rc);
+			regulator_put(vreg);
+			vreg = NULL;
+		}
+		fpc1020->vreg[i] = vreg;
+	} else {
+		if (vreg) {
+			if (regulator_is_enabled(vreg)) {
+				regulator_disable(vreg);
+				dev_dbg(dev, "disabled %s\n", name);
+			}
+			regulator_put(vreg);
+			fpc1020->vreg[i] = NULL;
+		}
+		rc = 0;
+	}
+	return rc;
+}
+
+static int set_clks(struct fpc1020_data *fpc1020, bool enable)
+{
+	int rc = 0;
+
+	if (enable == fpc1020->clocks_enabled) {
+		dev_info(fpc1020->dev, "enable == clocks_enabled\n");
+		goto out;
+	}
+
+	if (enable) {
+		dev_info(fpc1020->dev, "setting clk rates\n");
+		wake_lock(&fpc1020->wlock);
+		rc = clk_set_rate(fpc1020->core_clk,
+				fpc1020->spi->max_speed_hz);
+		if (rc) {
+			dev_err(fpc1020->dev,
+					"%s: Error setting clk_rate: %u, %d\n",
+					__func__, fpc1020->spi->max_speed_hz,
+					rc);
+			return rc;
+		}
+		dev_info(fpc1020->dev, "enabling core_clk\n");
+		rc = clk_prepare_enable(fpc1020->core_clk);
+		if (rc) {
+			dev_err(fpc1020->dev,
+					"%s: Error enabling core clk: %d\n",
+					__func__, rc);
+			goto out;
+		}
+
+		dev_info(fpc1020->dev, "enabling iface_clk\n");
+		rc = clk_prepare_enable(fpc1020->iface_clk);
+		if (rc) {
+			dev_err(fpc1020->dev,
+					"%s: Error enabling iface clk: %d\n",
+					__func__, rc);
+			clk_disable_unprepare(fpc1020->core_clk);
+			goto out;
+		}
+		dev_dbg(fpc1020->dev, "%s ok. clk rate %u hz\n", __func__,
+				fpc1020->spi->max_speed_hz);
+
+		fpc1020->clocks_enabled = true;
+	} else {
+		dev_info(fpc1020->dev, "disabling clks\n");
+		clk_disable_unprepare(fpc1020->iface_clk);
+		clk_disable_unprepare(fpc1020->core_clk);
+		fpc1020->clocks_enabled = false;
+		wake_unlock(&fpc1020->wlock);
+	}
+
+out:
+	return rc;
+}
+
+static ssize_t dev_enable_set(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct  fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	int state = (*buf == '1') ? 1 : 0;
+
+	dev_err(fpc1020->dev, "%s state=%d...\n", __func__, state);
+	FPS_notify(0xbeef, state);
+	return 1;
+}
+
+static DEVICE_ATTR(dev_enable, S_IWUSR | S_IWGRP, NULL, dev_enable_set);
+
+static ssize_t clk_enable_set(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct  fpc1020_data *fpc1020 = dev_get_drvdata(dev);
+
+	return set_clks(fpc1020, (*buf == '1')) ? : count;
+}
+
+static DEVICE_ATTR(clk_enable, S_IWUSR | S_IWGRP, NULL, clk_enable_set);
+
+static ssize_t irq_get(struct device *device,
+		       struct device_attribute *attribute,
+		       char *buffer)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(device);
+	int irq = gpio_get_value(fpc1020->irq_gpio);
+
+	return scnprintf(buffer, PAGE_SIZE, "%i\n", irq);
+}
+static DEVICE_ATTR(irq, S_IRUSR | S_IRGRP, irq_get, NULL);
+
+static struct attribute *attributes[] = {
+	&dev_attr_dev_enable.attr,
+	&dev_attr_clk_enable.attr,
+	&dev_attr_irq.attr,
+	NULL
+};
+
+static const struct attribute_group attribute_group = {
+	.attrs = attributes,
+};
+
+static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
+{
+	struct fpc1020_data *fpc1020 = handle;
+
+	dev_err(fpc1020->dev, "%s\n", __func__);
+	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
+	return IRQ_HANDLED;
+}
+
+static int fpc1020_request_named_gpio(struct fpc1020_data *fpc1020,
+		const char *label, int *gpio)
+{
+	struct device *dev = fpc1020->dev;
+	struct device_node *np = dev->of_node;
+	int rc;
+
+	*gpio = of_get_named_gpio(np, label, 0);
+	rc = devm_gpio_request(dev, *gpio, label);
+	if (rc) {
+		dev_err(dev, "failed to request gpio %d\n", *gpio);
+		return rc;
+	}
+	dev_info(dev, "%s %d\n", label, *gpio);
+	return 0;
+}
+
+static int fpc1020_probe(struct spi_device *spi)
+{
+	struct device *dev = &spi->dev;
+	int rc = 0;
+	int irqf;
+	struct device_node *np = dev->of_node;
+	struct fpc1020_data *fpc1020 = devm_kzalloc(dev, sizeof(*fpc1020),
+			GFP_KERNEL);
+	if (!fpc1020) {
+		rc = -ENOMEM;
+		goto exit;
+	}
+
+	fpsData = FPS_init();
+
+	fpc1020->dev = dev;
+	dev_set_drvdata(dev, fpc1020);
+	fpc1020->spi = spi;
+
+	if (!np) {
+		dev_err(dev, "no of node found\n");
+		rc = -EINVAL;
+		goto exit;
+	}
+
+	rc = fpc1020_request_named_gpio(fpc1020, "fpc,gpio_irq",
+			&fpc1020->irq_gpio);
+	gpio_direction_input(fpc1020->irq_gpio);
+	rc = fpc1020_request_named_gpio(fpc1020, "fpc,gpio_rst",
+			&fpc1020->rst_gpio);
+	if (rc)
+		goto exit;
+	gpio_direction_output(fpc1020->rst_gpio, 1);
+	gpio_export(fpc1020->rst_gpio, 1);
+	fpc1020->iface_clk = clk_get(dev, "iface_clk");
+	if (IS_ERR(fpc1020->iface_clk)) {
+		dev_err(dev, "%s: Failed to get iface_clk\n", __func__);
+		rc = -EINVAL;
+		goto exit;
+	}
+
+	fpc1020->core_clk = clk_get(dev, "core_clk");
+	if (IS_ERR(fpc1020->core_clk)) {
+		dev_err(dev, "%s: Failed to get core_clk\n", __func__);
+		rc = -EINVAL;
+		goto exit;
+	}
+
+
+	fpc1020->clocks_enabled = false;
+	fpc1020->clocks_suspended = false;
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+
+	rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
+			NULL, fpc1020_irq_handler, irqf,
+			dev_name(dev), fpc1020);
+	if (rc) {
+		dev_err(dev, "could not request irq %d\n",
+				gpio_to_irq(fpc1020->irq_gpio));
+		goto exit;
+	}
+	dev_dbg(dev, "requested irq %d\n", gpio_to_irq(fpc1020->irq_gpio));
+
+	/* Request that the interrupt should be wakeable */
+	enable_irq_wake(gpio_to_irq(fpc1020->irq_gpio));
+
+	wake_lock_init(&fpc1020->wlock, WAKE_LOCK_SUSPEND, "fpc1020");
+
+	rc = sysfs_create_group(&dev->kobj, &attribute_group);
+	if (rc) {
+		dev_err(dev, "could not create sysfs\n");
+		goto exit;
+	}
+
+	if (of_property_read_bool(dev->of_node, "fpc,enable-on-boot")) {
+		dev_info(dev, "Enabling hardware\n");
+		set_clks(fpc1020, true);
+	}
+
+	dev_info(dev, "%s: ok\n", __func__);
+exit:
+	return rc;
+}
+
+static int fpc1020_remove(struct spi_device *spi)
+{
+	struct  fpc1020_data *fpc1020 = dev_get_drvdata(&spi->dev);
+
+	sysfs_remove_group(&spi->dev.kobj, &attribute_group);
+	(void)vreg_setup(fpc1020, "vdd_io", false);
+	(void)vreg_setup(fpc1020, "vcc_spi", false);
+	(void)vreg_setup(fpc1020, "vdd_ana", false);
+	wake_lock_destroy(&fpc1020->wlock);
+	dev_info(&spi->dev, "%s\n", __func__);
+	return 0;
+}
+
+static int fpc1020_suspend(struct spi_device *spi, pm_message_t mesg)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(&spi->dev);
+
+	fpc1020->clocks_suspended = fpc1020->clocks_enabled;
+	dev_info(fpc1020->dev, "fpc1020_suspend\n");
+	set_clks(fpc1020, false);
+	return 0;
+}
+
+static int fpc1020_resume(struct spi_device *spi)
+{
+	struct fpc1020_data *fpc1020 = dev_get_drvdata(&spi->dev);
+
+	if (fpc1020->clocks_suspended) {
+		dev_info(fpc1020->dev, "fpc1020_resume\n");
+		set_clks(fpc1020, true);
+	}
+	return 0;
+}
+
+static struct of_device_id fpc1020_of_match[] = {
+	{ .compatible = "fpc,fpc1020", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, fpc1020_of_match);
+
+static struct spi_driver fpc1020_driver = {
+	.driver = {
+		.name	= "fpc1020",
+		.owner	= THIS_MODULE,
+		.of_match_table = fpc1020_of_match,
+	},
+	.probe		= fpc1020_probe,
+	.remove		= fpc1020_remove,
+	.suspend	= fpc1020_suspend,
+	.resume		= fpc1020_resume,
+};
+
+static int __init fpc1020_init(void)
+{
+	int rc = spi_register_driver(&fpc1020_driver);
+
+	if (!rc)
+		pr_info("%s OK\n", __func__);
+	else
+		pr_err("%s %d\n", __func__, rc);
+	return rc;
+}
+
+static void __exit fpc1020_exit(void)
+{
+	pr_info("%s\n", __func__);
+	spi_unregister_driver(&fpc1020_driver);
+}
+
+module_init(fpc1020_init);
+module_exit(fpc1020_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Aleksej Makarov");
+MODULE_AUTHOR("Henrik Tillman <henrik.tillman@fingerprints.com>");
+MODULE_DESCRIPTION("FPC1020 Fingerprint sensor device driver.");
