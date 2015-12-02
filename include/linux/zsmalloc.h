767f94fafebe3f981154dbf49ea33c48555eb114
Russ W. Knize <Russ.Knize@motorola.com>
2015-12-02 15:18:38 -0600

    IKSWM-13740: zsmalloc: page migration using "mobile" pages
    Allow kernel page migration to move zpage pages by marking them as
    "mobile".  When a page within a zpage is to be isolated, zsmalloc must:
    
    1) Pin all parts of all objects that may reside in the page
    2) Vacate page->lru, which means:
      a) first pages must vacate their fullness list
      b) sub-pages must be snipped out of their internal list
      c) last pages must mark their previous pages as last.
    
    Step 2b is accomplished by recording the relative list position into
    page->_mapcount and re-inserting it back into the sub-page list later.
    
    When the page is finally to be migrated via migratepage(), zsmalloc must:
    
    1) Copy all of the inter-page glue to the new page, which means:
      a) first pages must update all sub-page references to themselves
      b) second pages must update their first page's reference to themselves
      c) sub-pages must walk to their previous location via _mapcount
      b) last pages must mark themselves last and unmark the previous last
    2) Copy the page contents to the new page.
    3) Update all handles that have PFNs pointing to the moved page.
    
    Because we lose page->lru, we cannot move the first page and a sub-page
    from a common zpage on the same pass with this approach.
    
    Change-Id: I959cd78bb74f932229a2fd5e056cc69a9763efd6
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.mot.com/822466
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -35,8 +35,10 @@ enum zs_mapmode {
 };
 
 struct zs_pool_stats {
-	/* How many pages were migrated (freed) */
+	/* How many pages were compacted (freed) */
 	unsigned long pages_compacted;
+	/* How many pages were migrated */
+	unsigned long pages_moved;
 };
 
 struct zs_pool;
