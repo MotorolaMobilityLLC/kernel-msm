61b5c8d13d3b1c2bc88f291612604a970decd2c8
Bang Nguyen <bangnguyen@motorola.com>
2015-12-15 12:51:27 -0600

    include: linux: platform_data: Add MOT slimport support
    Add MOT's ANX reset pinctrl, power supplies, and clock support
    
    Change-Id: Ibe9875f26cf76f1f6b0ddcb5734a30c5a927d862
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/807527
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Sandersary Crosby <zac@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/platform_data/slimport_device.h
+++ include/linux/platform_data/slimport_device.h
@@ -16,8 +16,18 @@
 #define SLIMPORT_DEVICE
 #include <linux/pinctrl/consumer.h>
 
-struct anx7816_platform_data
-{
+#define ANX_PINCTRL_STATE_DEFAULT "anx_default"
+#define ANX_PINCTRL_STATE_SLEEP  "anx_sleep"
+
+
+struct anx7816_pinctrl_res {
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *gpio_state_active;
+	struct pinctrl_state *gpio_state_suspend;
+};
+
+
+struct anx7816_platform_data {
 	bool check_slimport_connection;
 	int gpio_p_dwn;
 	int gpio_reset;
@@ -25,12 +35,14 @@ struct anx7816_platform_data
 	int gpio_cbl_det;
 	int gpio_v10_ctrl;
 	int gpio_v33_ctrl;
+	struct anx7816_pinctrl_res pin_res;
 	spinlock_t lock;
 	int external_ldo_control;
-	int (* avdd_power) (unsigned int onoff);
-	int (* dvdd_power) (unsigned int onoff);
+	int (*avdd_power)(unsigned int onoff);
 	struct regulator *avdd_33;
 	struct regulator *dvdd_10;
+	struct regulator *vdd_18;
+	struct clk *mclk;
 	struct pinctrl *pinctrl;
 	struct pinctrl_state *hdmi_pinctrl_active;
 	struct pinctrl_state *hdmi_pinctrl_suspend;
@@ -40,24 +52,7 @@ struct anx7816_platform_data
 
 };
 
-struct anx7808_platform_data
-{
-	int gpio_p_dwn;
-	int gpio_reset;
-	int gpio_int;
-	int gpio_cbl_det;
-	int gpio_v10_ctrl;
-	int gpio_v33_ctrl;
-	spinlock_t lock;
-	int external_ldo_control;
-	int (* avdd_power) (unsigned int onoff);
-	int (* dvdd_power) (unsigned int onoff);
-	struct regulator *avdd_10;
-	struct regulator *dvdd_10;
-#ifdef CONFIG_SLIMPORT_DYNAMIC_HPD
-	struct platform_device *hdmi_pdev;
-#endif
-
-};
+unchar sp_get_rx_bw(void);
+int slimport_read_edid_block(int block, uint8_t *edid_buf);
 
 #endif /* SLIMPORT_DEVICE */
