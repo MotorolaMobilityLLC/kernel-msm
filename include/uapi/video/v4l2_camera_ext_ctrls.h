8a20744be3e86376d75088cc0424516f26b4faa1
Long Ling <a21040@motorola.com>
2015-12-18 10:10:38 -0600

    camera_ext: predefined control id
    Initial base.
    
    Change-Id: I1b075487ecb2f58163eaba36ebb19c54a8e35557
    Signed-off-by: Long Ling <a21040@motorola.com>
    Reviewed-on: http://gerrit.mot.com/808511
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jason Hrycay <jason.hrycay@motorola.com>

---
+++ include/uapi/video/v4l2_camera_ext_ctrls.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Motorola Mobility, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CAMERA_EXT_CTRLS_H
+#define __CAMERA_EXT_CTRLS_H
+
+#define CID_CAM_EXT_CLASS_BASE	(0x00f00000 | 0xf000)
+#define MOD_CID_MOD_CLASS	(0x00f00000 | 1)
+
+/* TODO: predefined camera_ext cid start from CID_CAM_EXT_CLASS_BASE */
+
+#endif /* __CAMERA_EXT_CTRLS_H */
