e24429f34d550c158b5cb0662871abe92892bf34
Nikhilesh Reddy <reddyn@codeaurora.org>
2015-12-17 18:40:28 -0800

    fuse: Add support for fuse stacked I/O
    Add support for filesystem stacked read/write of files
    when enabled through a userspace init option of FUSE_STACKED_IO.
    
    When FUSE_STACKED_IO is enabled all the reads and writes
    to the fuse mount point go directly to the native filesystem
    rather than through the fuse daemon. All requests that aren't
    read/write still go thought the userspace code.
    
    Mmaped I/O is still not supported through stacking and can be
    added in.
    
    This allows for significantly better performance on read and writes.
    The difference in performance  between fuse and the native lower
    filesystem is negligible.
    
    There is also a significant cpu/power savings that is achieved which
    is really important on embedded systems that use fuse for I/O
    
    Change-Id: Ic2e6b69df5acd92844999a0b7dd9c1c1db185d50
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -903,6 +903,11 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_req *req)
 				fc->async_dio = 1;
 			if (arg->flags & FUSE_WRITEBACK_CACHE)
 				fc->writeback_cache = 1;
+			if (arg->flags & FUSE_STACKED_IO) {
+				fc->stacked_io = 1;
+				pr_info("FUSE: Stacked io is enabled [%s : %d]!\n",
+					current->comm, current->pid);
+			}
 		} else {
 			ra_pages = fc->max_read / PAGE_CACHE_SIZE;
 			fc->no_lock = 1;
