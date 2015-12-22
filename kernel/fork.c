e25927b969ed65717955d4eeb42d322bd135b1e7
Thierry Strudel <tstrudel@google.com>
2015-12-22 10:13:14 -0800

    Revert "android/lowmemorykiller: Ignore tasks with freed mm"
    This reverts commit 05d5ad4d0af90e05d63da7945282c7f5dc18f682.
    
    Change-Id: Ib5770b5b38f123116322507646ae9bce6c3c186a

--- kernel/fork.c
+++ kernel/fork.c
@@ -629,9 +629,8 @@ EXPORT_SYMBOL_GPL(__mmdrop);
 /*
  * Decrement the use count and release all resources for an mm.
  */
-int mmput(struct mm_struct *mm)
+void mmput(struct mm_struct *mm)
 {
-	int mm_freed = 0;
 	might_sleep();
 
 	if (atomic_dec_and_test(&mm->mm_users)) {
@@ -649,9 +648,7 @@ int mmput(struct mm_struct *mm)
 		if (mm->binfmt)
 			module_put(mm->binfmt->module);
 		mmdrop(mm);
-		mm_freed = 1;
 	}
-	return mm_freed;
 }
 EXPORT_SYMBOL_GPL(mmput);
 
