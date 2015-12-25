b9cf69e11468d20d14af490047a0056b19d830b9
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-25 04:08:06 +0000

    msm: performance: prevent userspace hints to decrease max cpu freq
    For some reason the max freq on the big cluster changes from 1,9GHz to 1,8GHz way too frequently for no apparent reason, specially during input touch events. The device isn't even under high temperatures. Confirmed that the device can thermal throttle during high temperature cases even with this disable
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -402,6 +402,7 @@ module_param_cb(cpu_min_freq, &param_ops_cpu_min_freq, NULL, 0644);
  */
 static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -449,6 +450,7 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }
