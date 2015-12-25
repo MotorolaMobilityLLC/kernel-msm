a41baf0e75b487174723aef8c564e5771f83b3c9
zonghua liu <a17671@motorola.com>
2015-12-25 00:34:44 -0600

    misc:fusb302:Release 3.3 from Fairchild
    Formatted the style with dos2unix and Lindent script
    
    Change-Id: I1c4e7535a8823e9e6b8c2bfe85c9abb22b1915e0
    Signed-off-by: zonghua liu <a17671@motorola.com>
    Reviewed-on: http://gerrit.mot.com/825178
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/misc/fusb302/core/vdm/DisplayPort/dp.h
@@ -0,0 +1,43 @@
+#ifdef FSC_HAVE_DP
+
+#ifndef __DISPLAYPORT_DP_H__
+#define __DISPLAYPORT_DP_H__
+
+#include "../../platform.h"
+#include "../../PD_Types.h"
+#include "../vdm_types.h"
+#include "dp_types.h"
+
+extern FSC_BOOL DpEnabled;
+extern FSC_BOOL DpAutoModeEntryEnabled;
+extern DisplayPortCaps_t DpModeEntryMask;
+extern DisplayPortCaps_t DpModeEntryValue;
+extern DisplayPortCaps_t DpCaps;
+extern DisplayPortStatus_t DpStatus;
+extern DisplayPortConfig_t DpConfig;
+extern DisplayPortStatus_t DpPpStatus;
+extern DisplayPortConfig_t DpPpRequestedConfig;
+extern DisplayPortConfig_t DpPpConfig;
+extern FSC_U32 DpModeEntered;
+
+/* Initialize stuff! */
+void initializeDp(void);
+
+/* reset stuff - for detaches */
+void resetDp(void);
+
+/* Process incoming DisplayPort messages! */
+FSC_BOOL processDpCommand(FSC_U32 * arr_in);
+
+/* Internal function to send Status data to port partner */
+void sendStatusData(doDataObject_t svdmh_in);
+
+/* Internal function to reply to a Config request (to port partner) */
+void replyToConfig(doDataObject_t svdmh_in, FSC_BOOL success);
+
+/* Evaluate a mode VDO for mode entry */
+FSC_BOOL dpEvaluateModeEntry(FSC_U32 mode_in);
+
+#endif // __DISPLAYPORT_DP_H__
+
+#endif // FSC_HAVE_DP
