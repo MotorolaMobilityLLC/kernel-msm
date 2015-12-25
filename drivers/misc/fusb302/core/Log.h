3fc10b2c3ba304406e292ab6012e45b979510ba2
zonghua liu <a17671@motorola.com>
2015-12-25 00:34:44 -0600

    misc:fusb302:Release 3.3 from Fairchild
    Formatted the style with dos2unix and Lindent script
    
    Change-Id: I1c4e7535a8823e9e6b8c2bfe85c9abb22b1915e0
    Signed-off-by: zonghua liu <a17671@motorola.com>
    Reviewed-on: http://gerrit.mot.com/825178
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Ravikumar Vembu <Raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/misc/fusb302/core/Log.h
@@ -0,0 +1,49 @@
+/* 
+ * File:   Log.h
+ * Author: W0017688
+ *
+ * Created on July 13, 2015, 10:46 AM
+ * 
+ * Creates a circular queue to track state transitions and times
+ * Log does not overwrite if full - entries are dropped
+ */
+#ifdef FSC_DEBUG
+
+#ifndef FSC_LOG_H
+#define	FSC_LOG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "platform.h"
+
+#define LOG_SIZE 64
+
+	typedef struct {
+		FSC_U16 state;
+		FSC_U16 time_ms;
+		FSC_U16 time_s;
+	} StateLogEntry;
+
+	typedef struct {
+		StateLogEntry logQueue[LOG_SIZE];
+		FSC_U8 Start;
+		FSC_U8 End;
+		FSC_U8 Count;
+	} StateLog;
+
+	void InitializeStateLog(StateLog * log);
+	FSC_BOOL WriteStateLog(StateLog * log, FSC_U16 state, FSC_U16 time_ms,
+			       FSC_U16 time_s);
+	FSC_BOOL ReadStateLog(StateLog * log, FSC_U16 * state,
+			      FSC_U16 * time_ms, FSC_U16 * time_s);
+	FSC_BOOL IsStateLogFull(StateLog * log);
+	FSC_BOOL IsStateLogEmpty(StateLog * log);
+	void DeleteStateLog(StateLog * log);
+
+#ifdef	__cplusplus
+}
+#endif				// __cplusplus
+#endif				/* FSC_LOG_H */
+#endif				// FSC_DEBUG
---
+++ drivers/misc/fusb302/core/vdm/vdm.h
@@ -0,0 +1,107 @@
+/****************************************************************************
+ * Company:         Fairchild Semiconductor
+ *
+ * Author           Date          Comment
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * G. Noblesmith
+ *
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * Software License Agreement:
+ *
+ * The software supplied herewith by Fairchild Semiconductor (the “Company”)
+ * is supplied to you, the Company's customer, for exclusive use with its
+ * USB Type C / USB PD products.  The software is owned by the Company and/or
+ * its supplier, and is protected under applicable copyright laws.
+ * All rights are reserved. Any use in violation of the foregoing restrictions
+ * may subject the user to criminal sanctions under applicable laws, as well
+ * as to civil liability for the breach of the terms and conditions of this
+ * license.
+ *
+ * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ *****************************************************************************/
+#ifdef FSC_HAVE_VDM
+
+#ifndef __VDM_MANAGER_H__
+#define __VDM_MANAGER_H__
+
+#include "../platform.h"
+#include "fsc_vdm_defs.h"
+#include "vdm_callbacks_defs.h"
+#include "../PD_Types.h"
+
+#define NUM_VDM_MODES 6
+#define MAX_NUM_SVIDS_PER_SOP 30
+#define MAX_SVIDS_PER_MESSAGE 12
+#define MIN_DISC_ID_RESP_SIZE 3
+
+// Millisecond values ticked by 0.1ms timer.
+#define tVDMSenderResponse 27 * 10
+#define tVDMWaitModeEntry  50 * 10
+#define tVDMWaitModeExit   50 * 10
+
+/*
+ * VDM Manager object, so I can have multiple instances intercommunicating using the same functions!
+ */
+typedef struct {
+	// callbacks!
+	RequestIdentityInfo req_id_info;
+	RequestSvidInfo req_svid_info;
+	RequestModesInfo req_modes_info;
+	ModeEntryRequest req_mode_entry;
+	ModeExitRequest req_mode_exit;
+	EnterModeResult enter_mode_result;
+	ExitModeResult exit_mode_result;
+	InformIdentity inform_id;
+	InformSvids inform_svids;
+	InformModes inform_modes;
+	InformAttention inform_attention;
+} VdmManager;
+
+/*
+ * Initialization functions.
+ */
+FSC_S32 initializeVdm(void);
+
+/*
+ * Functions to go through PD VDM flow. 
+ */
+	// Initiations from DPM
+FSC_S32 requestDiscoverIdentity(SopType sop);	// Discovers identities in the system!
+FSC_S32 requestDiscoverSvids(SopType sop);	// Discovers SVIDs in the system!
+FSC_S32 requestDiscoverModes(SopType sop, FSC_U16 svid);	// Discovers Modes available for each SVID
+FSC_S32 requestSendAttention(SopType sop, FSC_U16 svid, FSC_U8 mode);	// send attention command to specified sop
+FSC_S32 requestEnterMode(SopType sop, FSC_U16 svid, FSC_U32 mode_index);	// enter mode specified by SVID and mode index
+FSC_S32 requestExitMode(SopType sop, FSC_U16 svid, FSC_U32 mode_index);	// exit mode specified by SVID and mode index
+FSC_S32 requestExitAllModes(void);	// exits all modes
+
+	// receiving end
+FSC_S32 processVdmMessage(SopType sop, FSC_U32 * arr, FSC_U32 length);	// function to call when we receive VDM messages
+FSC_S32 processDiscoverIdentity(SopType sop, FSC_U32 * arr_in,
+				FSC_U32 length_in);
+FSC_S32 processDiscoverSvids(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+FSC_S32 processDiscoverModes(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+FSC_S32 processEnterMode(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+FSC_S32 processExitMode(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+FSC_S32 processAttention(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+FSC_S32 processSvidSpecific(SopType sop, FSC_U32 * arr_in, FSC_U32 length_in);
+
+void sendVdmMessageWithTimeout(SopType sop, FSC_U32 * arr, FSC_U32 length, FSC_S32 n_pe);	// for internal use
+void vdmMessageTimeout(void);
+FSC_BOOL expectingVdmResponse(void);
+void startVdmTimer(FSC_S32 n_pe);
+void sendVdmMessageFailed(void);
+void resetPolicyState(void);
+
+void sendVdmMessage(SopType sop, FSC_U32 * arr, FSC_U32 length,
+		    PolicyState_t next_ps);
+
+#endif // header guard
+#endif // FSC_HAVE_VDM
