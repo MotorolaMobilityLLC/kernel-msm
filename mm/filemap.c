719ea1bbc3e535ccf6d3518d70b457a62faf66b8
jeboo <jeb00o@yahoo.com>
2016-01-03 15:00:17 -0500

    kernel 3.10.93 patch (xt1575_mm_6.0.0 specific)

--- mm/filemap.c
+++ mm/filemap.c
@@ -2348,6 +2348,11 @@ again:
 			break;
 		}
 
+		if (fatal_signal_pending(current)) {
+			status = -EINTR;
+			break;
+		}
+
 		status = a_ops->write_begin(file, mapping, pos, bytes, flags,
 						&page, &fsdata);
 		if (unlikely(status))
@@ -2388,10 +2393,6 @@ again:
 		written += copied;
 
 		balance_dirty_pages_ratelimited(mapping);
-		if (fatal_signal_pending(current)) {
-			status = -EINTR;
-			break;
-		}
 	} while (iov_iter_count(i));
 
 	return written ? written : status;
