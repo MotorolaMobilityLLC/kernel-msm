290f6a1e595bc2a5c53d50af453d98472e53f3a8
SeungYoon Lee <w22905@motorola.com>
2016-01-05 11:50:12 -0600

    ASoC: msm: qdspv6: add Motorola Tx module sample rate control
    Add Motorola Custom Tx module (0x1000E004) for sample rate configuration.
    SiENCS supports only 8K and 16K, sample rate should be adjusted.
    
    Change-Id: I0d2d6c8bc273b647b3be18235d915808375575f4
    Signed-off-by: SeungYoon Lee <w22905@motorola.com>
    Reviewed-on: http://gerrit.mot.com/810723
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Dwyer <josephdwyer@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -2431,7 +2431,8 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 
 	if ((topology == VPM_TX_SM_ECNS_COPP_TOPOLOGY) ||
 	    (topology == VPM_TX_DM_FLUENCE_COPP_TOPOLOGY) ||
-	    (topology == VPM_TX_DM_RFECNS_COPP_TOPOLOGY))
+	    (topology == VPM_TX_DM_RFECNS_COPP_TOPOLOGY) ||
+	    (topology == VPM_TX_MOT_COPP_TOPOLOGY))
 		rate = 16000;
 
 	copp_idx = adm_get_idx_if_copp_exists(port_idx, topology, perf_mode,
