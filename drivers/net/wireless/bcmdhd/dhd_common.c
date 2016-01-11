df69a65653128db16a307dd51fe26370258fad89
joe_tsai <joe_tsai@asus.com>
2016-01-11 21:31:15 +0800

    Common: Wifi: Fix Ethertype Security Vulnerability in BRCM WLAN Software
    Broadcom fix patch for security issue:
    A specific Ethernet type is used internal to Broadcom devices for events from WLAN firmware to host or host driver.
    These events are used to communicate firmware state as well as delivering any received 802.1X frames to a supplicant running on the host.
    Because of a defect in the handling of these specific management messages,
    frames using this type can be injected into firmware, host driver and also into the host for processing.
    The injected frames can cause a denial of service through firmware or host driver crash, or hypothetically code injection.
    
    Change-Id: I43b8365b21e2a21b7ea67e28065cd5e71c6a9a5d
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/209002
    Reviewed-by: joe_tsai <joe_tsai@asus.com>
    Tested-by: joe_tsai <joe_tsai@asus.com>

--- drivers/net/wireless/bcmdhd/dhd_common.c
+++ drivers/net/wireless/bcmdhd/dhd_common.c
@@ -1644,25 +1644,27 @@ wl_show_host_event(dhd_pub_t *dhd_pub, wl_event_msg_t *event, void *event_data,
 #endif /* SHOW_EVENTS */
 
 int
-wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata,
+wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata, uint16 pktlen,
 	wl_event_msg_t *event, void **data_ptr, void *raw_event)
 {
 	/* check whether packet is a BRCM event pkt */
-	bcm_event_t *pvt_data = (bcm_event_t *)pktdata;
+	bcm_event_t *pvt_data;
 	uint8 *event_data;
 	uint32 type, status, datalen;
 	uint16 flags;
 	int evlen;
 	int hostidx;
 
-	if (bcmp(BRCM_OUI, &pvt_data->bcm_hdr.oui[0], DOT11_OUI_LEN)) {
-		DHD_ERROR(("%s: mismatched OUI, bailing\n", __FUNCTION__));
-		return (BCME_ERROR);
-	}
+	if (pktlen < sizeof(bcm_event_t))
+	return (BCME_ERROR);
 
-	/* BRCM event pkt may be unaligned - use xxx_ua to load user_subtype. */
-	if (ntoh16_ua((void *)&pvt_data->bcm_hdr.usr_subtype) != BCMILCP_BCM_SUBTYPE_EVENT) {
-		DHD_ERROR(("%s: mismatched subtype, bailing\n", __FUNCTION__));
+	pvt_data = (bcm_event_t *)pktdata;
+
+	if (ntoh16_ua((void *)&pvt_data->bcm_hdr.subtype) != BCMILCP_SUBTYPE_VENDOR_LONG ||
+		(bcmp(BRCM_OUI, &pvt_data->bcm_hdr.oui[0], DOT11_OUI_LEN)) ||
+		ntoh16_ua((void *)&pvt_data->bcm_hdr.usr_subtype) != BCMILCP_BCM_SUBTYPE_EVENT)
+	{
+		DHD_ERROR(("%s: mismatched bcm_event_t info, bailing out\n", __FUNCTION__));
 		return (BCME_ERROR);
 	}
 
@@ -1677,7 +1679,14 @@ wl_host_event(dhd_pub_t *dhd_pub, int *ifidx, void *pktdata,
 	flags = ntoh16_ua((void *)&event->flags);
 	status = ntoh32_ua((void *)&event->status);
 	datalen = ntoh32_ua((void *)&event->datalen);
+
+	if (datalen > pktlen)
+		return (BCME_ERROR);
+
 	evlen = datalen + sizeof(bcm_event_t);
+
+	if (evlen > pktlen)
+		return (BCME_ERROR);
 
 	/* find equivalent host index for event ifidx */
 	hostidx = dhd_ifidx2hostidx(dhd_pub->info, event->ifidx);
