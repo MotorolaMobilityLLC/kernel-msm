85d8a6ec97c08242c805a4e698bb4838bf72a86d
Jared Suttles <jsuttles@motorola.com>
2016-01-12 10:12:52 -0600

    slimport: anx7816: myDP mod display support skeleton
    Cherry-pick commit 22ac74d to Analogix SW baseline
    
    Change-Id: Ia1b70e5b24d6865b072629513495d9f2e1be23c7
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/827449
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/slimport/anx7816/slimport.c
+++ drivers/video/slimport/anx7816/slimport.c
@@ -21,6 +21,9 @@
 #include "quick_charge.h"
 #endif
 
+#include <linux/mod_display.h>
+#include <linux/mod_display_ops.h>
+
 /* Enable or Disable HDCP by default */
 /* hdcp_enable = 1: Enable,  0: Disable */
 static int hdcp_enable = 1;
@@ -1378,6 +1381,71 @@ static int anx7816_parse_dt(struct device *dev,
 }
 #endif
 
+static int slimport_mod_display_handle_available(void *data)
+{
+	struct anx7816_data *anx7816;
+
+	pr_debug("%s+\n", __func__);
+
+	anx7816 = (struct anx7816_data *)data;
+
+	pr_debug("%s-\n", __func__);
+
+	return 0;
+}
+
+static int slimport_mod_display_handle_unavailable(void *data)
+{
+	struct anx7816_data *anx7816;
+
+	pr_debug("%s+\n", __func__);
+
+	anx7816 = (struct anx7816_data *)data;
+
+	pr_debug("%s-\n", __func__);
+
+	return 0;
+}
+
+static int slimport_mod_display_handle_connect(void *data)
+{
+	struct anx7816_data *anx7816;
+
+	pr_debug("%s+\n", __func__);
+
+	anx7816 = (struct anx7816_data *)data;
+
+	pr_debug("%s-\n", __func__);
+
+	return 0;
+}
+
+static int slimport_mod_display_handle_disconnect(void *data)
+{
+	struct anx7816_data *anx7816;
+
+	pr_debug("%s+\n", __func__);
+
+	anx7816 = (struct anx7816_data *)data;
+
+	pr_debug("%s-\n", __func__);
+
+	return 0;
+}
+
+static struct mod_display_ops slimport_mod_display_ops = {
+	.handle_available = slimport_mod_display_handle_available,
+	.handle_unavailable = slimport_mod_display_handle_unavailable,
+	.handle_connect = slimport_mod_display_handle_connect,
+	.handle_disconnect = slimport_mod_display_handle_disconnect,
+	.data = NULL,
+};
+
+static struct mod_display_impl_data slimport_mod_display_impl = {
+	.mod_display_type = MOD_DISPLAY_TYPE_DP,
+	.ops = &slimport_mod_display_ops,
+};
+
 static int anx7816_i2c_probe(struct i2c_client *client,
 			     const struct i2c_device_id *id)
 {
@@ -1525,6 +1593,10 @@ static int anx7816_i2c_probe(struct i2c_client *client,
 #ifdef QUICK_CHARGE_SUPPORT
 	BLOCKING_INIT_NOTIFIER_HEAD(get_notifier_list_head());
 #endif
+
+	slimport_mod_display_ops.data = (void *)anx7816;
+	mod_display_register_impl(&slimport_mod_display_impl);
+
 	pr_debug("%s %s end\n", LOG_TAG, __func__);
 	goto exit;
 
