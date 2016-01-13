9c3a9cd7851367a60a2f57b1bb9c7373931fa0da
Shiyong Li <a22381@motorola.com>
2016-01-13 15:18:59 -0800

    msm: mdss: support panel DSI parameter sysfs interface
    Common implementation to support changing panel DSI parameters on the
    fly by sysfs interface.
    
    Change-Id: Ib542185e2aea7ccee10aa47c4d8ac4c71c103f8d
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: http://gerrit.mot.com/807245
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -32,6 +32,23 @@ struct panel_id {
 /* worst case prefill lines for all chipsets including all vertical blank */
 #define MDSS_MDP_MAX_PREFILL_FETCH 25
 
+enum panel_param_id {
+	PARAM_ID_NUM = 0,
+};
+
+struct panel_param_val_map {
+	char *name;
+	char *prop;
+};
+
+struct panel_param {
+	char *param_name;
+	struct panel_param_val_map *val_map;
+	u16 val_max;
+	u16 value;
+	bool is_supported;
+};
+
 /* panel type list */
 #define NO_PANEL		0xffff	/* No Panel */
 #define MDDI_PANEL		1	/* MDDI */
@@ -552,11 +569,14 @@ struct mdss_panel_info {
 
 	u32 disp_on_check_val;
 	bool blank_progress_notify_enabled;
+	struct panel_param *param[PARAM_ID_NUM];
 };
 
 struct mdss_panel_data {
 	struct mdss_panel_info panel_info;
 	void (*set_backlight) (struct mdss_panel_data *pdata, u32 bl_level);
+	int (*set_param)(struct mdss_panel_data *pdata,
+		u16 id, u16 value, bool sent_cmd);
 	unsigned char *mmss_cc_base;
 
 	/**
@@ -806,4 +826,15 @@ static inline void mdss_panel_debugfs_cleanup(
 static inline void mdss_panel_debugfsinfo_to_panelinfo(
 			struct mdss_panel_info *panel_info) { };
 #endif
+
+static inline bool mdss_panel_param_is_supported(struct mdss_panel_info *p,
+	u16 id)
+{
+	if (id < PARAM_ID_NUM && p && p->param[id] &&
+		p->param[id]->is_supported)
+		return true;
+
+	return false;
+};
+
 #endif /* MDSS_PANEL_H */
