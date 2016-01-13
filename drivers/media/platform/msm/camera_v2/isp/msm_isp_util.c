2b581bd065dae0293c23b7dd090de9fba7a7ac4b
Shuzhen Wang <shuzhenwang@google.com>
2016-01-13 13:50:15 -0800

    camera_v2: isp: Replace monotonic timestamp with boot_time
    BOOT_TIME timestamp takes into consideration of sleep
    time.
    
    Bug: 22214409
    Change-Id: I3a9f06e9db234f3f5b583bce36009b5438c9c434
    Signed-off-by: Shuzhen Wang <shuzhenwang@google.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -333,7 +333,7 @@ int msm_isp_get_clk_info(struct vfe_device *vfe_dev,
 static inline void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp)
 {
 	struct timespec ts;
-	ktime_get_ts(&ts);
+	get_monotonic_boottime(&ts);
 	time_stamp->buf_time.tv_sec = ts.tv_sec;
 	time_stamp->buf_time.tv_usec = ts.tv_nsec/1000;
 	do_gettimeofday(&(time_stamp->event_time));
