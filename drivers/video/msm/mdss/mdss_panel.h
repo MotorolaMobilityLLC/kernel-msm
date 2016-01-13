35bc2e1cc1269a33908ce1d10b751de8b8a42b2a
Shiyong Li <a22381@motorola.com>
2016-01-13 15:18:59 -0800

    msm: mdss: support panel DSI parameter sysfs interface
    Common implementation to support changing panel DSI parameters on the
    fly by sysfs interface.
    
    Change-Id: Ib542185e2aea7ccee10aa47c4d8ac4c71c103f8d
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    (cherry picked from commit 172821f13a9807a1e470a887601f033cea43d3d3)
    Reviewed-on: http://gerrit.mot.com/840577
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Sandersary Crosby <zac@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -30,6 +30,23 @@ struct panel_id {
 #define MDSS_DSI_RST_SEQ_LEN	10
 #define MDSS_MDP_MAX_FETCH 12
 
+enum panel_param_id {
+	PARAM_ID_NUM = 0,
+};
+
+struct panel_param_val_map {
+	char *name;
+	char *prop;
+};
+
+struct panel_param {
+	char *param_name;
+	struct panel_param_val_map *val_map;
+	u16 val_max;
+	u16 value;
+	bool is_supported;
+};
+
 /* panel type list */
 #define NO_PANEL		0xffff	/* No Panel */
 #define MDDI_PANEL		1	/* MDDI */
@@ -450,11 +467,14 @@ struct mdss_panel_info {
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
 	bool blank_progress_notify_enabled;
+	struct panel_param *param[PARAM_ID_NUM];
 };
 
 struct mdss_panel_data {
 	struct mdss_panel_info panel_info;
 	void (*set_backlight) (struct mdss_panel_data *pdata, u32 bl_level);
+	int (*set_param)(struct mdss_panel_data *pdata,
+		u16 id, u16 value, bool sent_cmd);
 	unsigned char *mmss_cc_base;
 
 	/**
@@ -688,4 +708,15 @@ static inline void mdss_panel_debugfs_cleanup(
 static inline void mdss_panel_debugfsinfo_to_panelinfo(
 			struct mdss_panel_info *panel_info) { };
 #endif
+
+static inline bool mdss_panel_param_is_supported(struct mdss_panel_info *p,
+	u16 id)
+{
+	if (id < PARAM_ID_NUM && p && p->param[id] &&
+		p->param[id]->is_supported)
+		return true;
+
+	return false;
+};
+
 #endif /* MDSS_PANEL_H */
