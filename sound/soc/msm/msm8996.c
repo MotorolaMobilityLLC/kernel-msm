378eb6802e7532f71c7a05f27dce4b809889b69b
Paul Handrigan <Paul.Handrigan@cirrus.com>
2016-01-13 14:15:31 -0600

    ASoC: msm8996: Change SLIMCLK reference to MCLK2
    Change the FLL reference from SLIMCLK to MCLK2 32kHz.
    
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    
    audio playback messes up AoV mic audio
     * patch 2 of 2 for workaround
     * see gerrit 813037 for patch 1 of 2 for workaround
    
    Change-Id: I647bfa626b9706e932536a1510f7ab211068e842
    Reviewed-on: http://gerrit.mot.com/813038
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/msm8996.c
+++ sound/soc/msm/msm8996.c
@@ -501,7 +501,7 @@ static int msm8996_mclk_event(struct snd_soc_dapm_widget *w,
 	case SND_SOC_DAPM_PRE_PMU:
 #ifdef CONFIG_SND_SOC_FLORIDA
 		ret = snd_soc_codec_set_pll(w->codec, FLORIDA_FLL1,
-			ARIZONA_FLL_SRC_SLIMCLK, 1536000, FLORIDA_SYSCLK_RATE);
+			ARIZONA_FLL_SRC_MCLK2, 32768, FLORIDA_SYSCLK_RATE);
 		if (ret != 0)
 			dev_err(w->codec->dev, "set PLL clk failed\n");
 
@@ -515,7 +515,6 @@ static int msm8996_mclk_event(struct snd_soc_dapm_widget *w,
 			ARIZONA_FLL_SRC_MCLK2, 32768, FLORIDA_SYSCLK_RATE);
 		if (ret != 0)
 			dev_err(w->codec->dev, "set PLL clk failed\n");
-
 
 		break;
 #else
