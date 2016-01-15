11340ea09bd0ffac6c7e32d1910380027d559d01
Jevin Tang <a21266@motorola.com>
2016-01-15 13:02:45 -0600

    diag: provide peripherals query
    port from (CR)
    
    User space can query the supported peripherals status
    
    Change-Id: Iddc6410e887d6f9363b50bf8bf3060d47fcc7677
    Signed-off-by: Jevin Tang <a21266@motorola.com>
    Change-Id: Ie20c1a69522e7c94cee9a484670ee2b39cae3eb9
    Reviewed-on: http://gerrit.mot.com/816213
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	drivers/char/diag/diagchar_core.c
    	include/linux/diagchar.h
    Change-Id: Ic9630708b0816a3082e123aead30a4a4210789fe
    Reviewed-on: https://gerrit.mot.com/1173694
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -172,6 +172,7 @@ void *diag_ipc_log;
 #endif
 
 static void diag_md_session_close(int pid);
+extern uint16_t md_support;
 
 /*
  * Returns the next delayed rsp id. If wrapping is enabled,
@@ -1455,6 +1456,15 @@ static void diag_md_session_close(int pid)
 	kfree(session_info);
 	session_info = NULL;
 	DIAG_LOG(DIAG_DEBUG_USERSPACE, "cleared up session\n");
+}
+
+static int diag_ioctl_md_support_list(unsigned long ioarg)
+{
+	if (copy_to_user((void __user *)ioarg, &md_support,
+			sizeof(md_support)))
+		return -EFAULT;
+	else
+		return 0;
 }
 
 struct diag_md_session_t *diag_md_session_get_pid(int pid)
@@ -2779,6 +2789,9 @@ long diagchar_compat_ioctl(struct file *filp,
 		else
 			result = 0;
 		break;
+	case DIAG_IOCTL_MD_SUPPORT_LIST:
+		result = diag_ioctl_md_support_list(ioarg);
+		break;
 	}
 	return result;
 }
@@ -2928,6 +2941,9 @@ long diagchar_ioctl(struct file *filp,
 			result = -EFAULT;
 		else
 			result = 0;
+		break;
+	case DIAG_IOCTL_MD_SUPPORT_LIST:
+		result = diag_ioctl_md_support_list(ioarg);
 		break;
 	}
 	return result;
