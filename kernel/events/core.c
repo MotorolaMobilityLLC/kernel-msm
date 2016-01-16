094b859def52db81d6bd075bb3ded0c479df8885
Wish Wu <wishwu007@gmail.com>
2016-01-15 20:03:14 -0500

    msm: null pointer dereferencing
    Prevent unintended kernel NULL pointer dereferencing.
    
    Orignal code:
      hlist_del_rcu(&event->hlist_entry);
    
    Fix: Adding pointer check:
      if(!hlist_unhashed(&p_event->hlist_entry))
        hlist_del_rcu(&p_event->hlist_entry);
    
    Bug: 25364034
    Change-Id: Ieda6d8f4bb567827fa6c7709e9e729905c6c3882
    Signed-off-by: Yuan Lin <yualin@google.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -4993,7 +4993,8 @@ static int perf_swevent_add(struct perf_event *event, int flags)
 
 static void perf_swevent_del(struct perf_event *event, int flags)
 {
-	hlist_del_rcu(&event->hlist_entry);
+	if(!hlist_unhashed(&event->hlist_entry))
+		hlist_del_rcu(&event->hlist_entry);
 }
 
 static void perf_swevent_start(struct perf_event *event, int flags)
@@ -6205,6 +6206,9 @@ SYSCALL_DEFINE5(perf_event_open,
 	err = perf_copy_attr(attr_uptr, &attr);
 	if (err)
 		return err;
+
+	if (attr.__reserved_1)
+		return -EINVAL;
 
 	if (!attr.exclude_kernel) {
 		if (perf_paranoid_kernel() && !capable(CAP_SYS_ADMIN))
