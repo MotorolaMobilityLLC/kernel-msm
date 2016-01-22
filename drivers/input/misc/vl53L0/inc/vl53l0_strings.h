ba4cea67b8d7530c02bf16760c4cd4019f3b142e
Teresa TAO <teresa.tao@st.com>
2016-01-22 13:20:53 -0600

    input: misc: vl53l0: st driver update to 1.11.14
        - Driver 0.0.9
    		. Use the default settings.
    		. Linux coding styple compliant - in progress
    
        - Driver 0.0.8
    		. Add PerformRefCalibration call in the driver.
    
    Change-Id: I52ab30dbacaf04b3c8256d3666ff73578e89c4dd
    Signed-off-by:  Teresa TAO <teresa.tao@st.com>
    Reviewed-on: http://gerrit.mot.com/815547
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hideki Yoshino <hideki.yoshino@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Yun Ma <yunma@motorola.com>
    Reviewed-on: http://gerrit.mot.com/816159
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/misc/vl53L0/inc/vl53l0_strings.h
+++ drivers/input/misc/vl53L0/inc/vl53l0_strings.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Copyright © 2015, STMicroelectronics International N.V.
+Copyright © 2016, STMicroelectronics International N.V.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -38,6 +38,88 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 extern "C" {
 #endif
 
+#ifdef USE_EMPTY_STRING
+    #define  VL53L0_STRING_DEVICE_INFO_NAME                                 ""
+    #define  VL53L0_STRING_DEVICE_INFO_NAME_TS0                             ""
+    #define  VL53L0_STRING_DEVICE_INFO_NAME_TS1                             ""
+    #define  VL53L0_STRING_DEVICE_INFO_NAME_TS2                             ""
+    #define  VL53L0_STRING_DEVICE_INFO_NAME_ES1                             ""
+    #define  VL53L0_STRING_DEVICE_INFO_TYPE                                 ""
+
+    /* PAL ERROR strings */
+    #define  VL53L0_STRING_ERROR_NONE                                       ""
+    #define  VL53L0_STRING_ERROR_CALIBRATION_WARNING                        ""
+    #define  VL53L0_STRING_ERROR_MIN_CLIPPED                                ""
+    #define  VL53L0_STRING_ERROR_UNDEFINED                                  ""
+    #define  VL53L0_STRING_ERROR_INVALID_PARAMS                             ""
+    #define  VL53L0_STRING_ERROR_NOT_SUPPORTED                              ""
+    #define  VL53L0_STRING_ERROR_RANGE_ERROR                                ""
+    #define  VL53L0_STRING_ERROR_TIME_OUT                                   ""
+    #define  VL53L0_STRING_ERROR_MODE_NOT_SUPPORTED                         ""
+    #define  VL53L0_STRING_ERROR_BUFFER_TOO_SMALL                           ""
+    #define  VL53L0_STRING_ERROR_GPIO_NOT_EXISTING                          ""
+    #define  VL53L0_STRING_ERROR_GPIO_FUNCTIONALITY_NOT_SUPPORTED           ""
+    #define  VL53L0_STRING_ERROR_CONTROL_INTERFACE                          ""
+    #define  VL53L0_STRING_ERROR_INVALID_COMMAND                            ""
+    #define  VL53L0_STRING_ERROR_DIVISION_BY_ZERO                           ""
+    #define  VL53L0_STRING_ERROR_REF_SPAD_INIT                              ""
+	#define  VL53L0_STRING_ERROR_NOT_IMPLEMENTED                            ""
+
+    #define  VL53L0_STRING_UNKNOW_ERROR_CODE                                ""
+
+
+
+	/* Range Status */
+	#define  VL53L0_STRING_RANGESTATUS_NONE                                 ""
+	#define  VL53L0_STRING_RANGESTATUS_RANGEVALID                           ""
+	#define  VL53L0_STRING_RANGESTATUS_SIGMA                                ""
+	#define  VL53L0_STRING_RANGESTATUS_SIGNAL                               ""
+	#define  VL53L0_STRING_RANGESTATUS_MINRANGE                             ""
+	#define  VL53L0_STRING_RANGESTATUS_PHASE                                ""
+	#define  VL53L0_STRING_RANGESTATUS_HW                                   ""
+
+
+	/* Range Status */
+	#define  VL53L0_STRING_STATE_POWERDOWN                                  ""
+	#define  VL53L0_STRING_STATE_WAIT_STATICINIT                            ""
+	#define  VL53L0_STRING_STATE_STANDBY                                    ""
+	#define  VL53L0_STRING_STATE_IDLE                                       ""
+	#define  VL53L0_STRING_STATE_RUNNING                                    ""
+	#define  VL53L0_STRING_STATE_UNKNOWN                                    ""
+	#define  VL53L0_STRING_STATE_ERROR                                      ""
+
+
+	/* Device Specific */
+	#define  VL53L0_STRING_DEVICEERROR_NONE                                 ""
+	#define  VL53L0_STRING_DEVICEERROR_VCSELCONTINUITYTESTFAILURE           ""
+	#define  VL53L0_STRING_DEVICEERROR_VCSELWATCHDOGTESTFAILURE             ""
+	#define  VL53L0_STRING_DEVICEERROR_NOVHVVALUEFOUND                      ""
+	#define  VL53L0_STRING_DEVICEERROR_MSRCNOTARGET                         ""
+	#define  VL53L0_STRING_DEVICEERROR_SNRCHECK                             ""
+	#define  VL53L0_STRING_DEVICEERROR_RANGEPHASECHECK                      ""
+	#define  VL53L0_STRING_DEVICEERROR_SIGMATHRESHOLDCHECK                  ""
+	#define  VL53L0_STRING_DEVICEERROR_TCC                                  ""
+	#define  VL53L0_STRING_DEVICEERROR_PHASECONSISTENCY                     ""
+	#define  VL53L0_STRING_DEVICEERROR_MINCLIP                              ""
+	#define  VL53L0_STRING_DEVICEERROR_RANGECOMPLETE                        ""
+	#define  VL53L0_STRING_DEVICEERROR_ALGOUNDERFLOW                        ""
+	#define  VL53L0_STRING_DEVICEERROR_ALGOOVERFLOW                         ""
+	#define  VL53L0_STRING_DEVICEERROR_RANGEIGNORETHRESHOLD                 ""
+	#define  VL53L0_STRING_DEVICEERROR_UNKNOWN                              ""
+
+    /* Check Enable */
+    #define  VL53L0_STRING_CHECKENABLE_SIGMA_FINAL_RANGE                    ""
+	#define  VL53L0_STRING_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE              ""
+	#define  VL53L0_STRING_CHECKENABLE_SIGNAL_REF_CLIP                      ""
+	#define  VL53L0_STRING_CHECKENABLE_RANGE_IGNORE_THRESHOLD               ""
+
+	/* Sequence Step */
+    #define  VL53L0_STRING_SEQUENCESTEP_TCC                                 ""
+	#define  VL53L0_STRING_SEQUENCESTEP_DSS                                 ""
+	#define  VL53L0_STRING_SEQUENCESTEP_MSRC                                ""
+	#define  VL53L0_STRING_SEQUENCESTEP_PRE_RANGE                           ""
+	#define  VL53L0_STRING_SEQUENCESTEP_FINAL_RANGE                         ""
+#else
     #define  VL53L0_STRING_DEVICE_INFO_NAME                                 "VL53L0 cut1.0"
     #define  VL53L0_STRING_DEVICE_INFO_NAME_TS0                             "VL53L0 TS0"
     #define  VL53L0_STRING_DEVICE_INFO_NAME_TS1                             "VL53L0 TS1"
@@ -55,15 +137,17 @@ extern "C" {
     #define  VL53L0_STRING_ERROR_RANGE_ERROR                                "Range error"
     #define  VL53L0_STRING_ERROR_TIME_OUT                                   "Time out error"
     #define  VL53L0_STRING_ERROR_MODE_NOT_SUPPORTED                         "Mode not supported error"
-    #define  VL53L0_STRING_ERROR_NOT_IMPLEMENTED                            "Not implemented error"
-
-    #define  VL53L0_STRING_UNKNOW_ERROR_CODE                                "Unknow Error Code"
     #define  VL53L0_STRING_ERROR_BUFFER_TOO_SMALL                           "Buffer too small"
-
     #define  VL53L0_STRING_ERROR_GPIO_NOT_EXISTING                          "GPIO not existing"
-    #define  VL53L0_STRING_ERROR_GPIO_FUNCTIONALITY_NOT_SUPPORTED           "GPIO functionality not supported"
+    #define  VL53L0_STRING_ERROR_GPIO_FUNCTIONALITY_NOT_SUPPORTED           "GPIO funct not supported"
     #define  VL53L0_STRING_ERROR_CONTROL_INTERFACE                          "Control Interface Error"
+    #define  VL53L0_STRING_ERROR_INVALID_COMMAND                            "Invalid Command Error"
     #define  VL53L0_STRING_ERROR_DIVISION_BY_ZERO                           "Division by zero Error"
+    #define  VL53L0_STRING_ERROR_REF_SPAD_INIT                              "Reference Spad Init Error"
+	#define  VL53L0_STRING_ERROR_NOT_IMPLEMENTED                            "Not implemented error"
+
+    #define  VL53L0_STRING_UNKNOW_ERROR_CODE                                "Unknown Error Code"
+
 
 
 	/* Range Status */
@@ -74,6 +158,16 @@ extern "C" {
 	#define  VL53L0_STRING_RANGESTATUS_MINRANGE                             "Min Range Fail"
 	#define  VL53L0_STRING_RANGESTATUS_PHASE                                "Phase Fail"
 	#define  VL53L0_STRING_RANGESTATUS_HW                                   "Hardware Fail"
+
+
+	/* Range Status */
+	#define  VL53L0_STRING_STATE_POWERDOWN                                  "POWERDOWN State"
+	#define  VL53L0_STRING_STATE_WAIT_STATICINIT                            "Wait for staticinit State"
+	#define  VL53L0_STRING_STATE_STANDBY                                    "STANDBY State"
+	#define  VL53L0_STRING_STATE_IDLE                                       "IDLE State"
+	#define  VL53L0_STRING_STATE_RUNNING                                    "RUNNING State"
+	#define  VL53L0_STRING_STATE_UNKNOWN                                    "UNKNOWN State"
+	#define  VL53L0_STRING_STATE_ERROR                                      "ERROR State"
 
 
 	/* Device Specific */
@@ -100,11 +194,18 @@ extern "C" {
 	#define  VL53L0_STRING_CHECKENABLE_SIGNAL_REF_CLIP                      "SIGNAL REF CLIP"
 	#define  VL53L0_STRING_CHECKENABLE_RANGE_IGNORE_THRESHOLD               "RANGE IGNORE THRESHOLD"
 
+	/* Sequence Step */
+    #define  VL53L0_STRING_SEQUENCESTEP_TCC                                 "TCC"
+	#define  VL53L0_STRING_SEQUENCESTEP_DSS                                 "DSS"
+	#define  VL53L0_STRING_SEQUENCESTEP_MSRC                                "MSRC"
+	#define  VL53L0_STRING_SEQUENCESTEP_PRE_RANGE                           "PRE RANGE"
+	#define  VL53L0_STRING_SEQUENCESTEP_FINAL_RANGE                         "FINAL RANGE"
+#endif //USE_EMPTY_STRING
 
 
 #ifdef __cplusplus
 }
 #endif
-
+						
 #endif
 
