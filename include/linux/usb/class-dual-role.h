a4154417288fbd48ad946977d87f14f31d735b91
Chris Fries <cfries@motorola.com>
2016-02-12 16:46:30 -0600

    usb: phy: compile with Dual Role disabled
    In file included from ../../../../../../kernel/drivers/misc/fusb302/fusb302.c:15:0:
    ../../../../../../kernel/include/linux/usb/class-dual-role.h:112:13: warning: 'dual_role_instance_changed' defined but not used [-Wunused-function]
    error, forbidden warning: class-dual-role.h:112
    
    Change-Id: I9286231fb883016d7eff0121866208827fa79a0c
    Signed-off-by: Chris Fries <cfries@motorola.com>

--- include/linux/usb/class-dual-role.h
+++ include/linux/usb/class-dual-role.h
@@ -109,8 +109,6 @@ extern int dual_role_property_is_writeable(struct dual_role_phy_instance
 					   enum dual_role_property prop);
 extern void *dual_role_get_drvdata(struct dual_role_phy_instance *dual_role);
 #else /* CONFIG_DUAL_ROLE_USB_INTF */
-static inline void dual_role_instance_changed(struct dual_role_phy_instance
-				       *dual_role){}
 static inline struct dual_role_phy_instance *__must_check
 devm_dual_role_instance_register(struct device *parent,
 				 const struct dual_role_phy_desc *desc)
