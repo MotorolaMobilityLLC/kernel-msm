24e310cfbc0f4139ec98e2dbc7391b0b49d5b020
Xuehui Ye <yexh1@motorola.com>
2016-02-16 16:23:37 +0800

    usb: otg: report power supply type as usb by default
    If power supply type is "Unknown" when framework is coming up, it
    completely ignores all reports to power supply type and does not indicate
    charging status correctly in the UI.
    
    Change-Id: Ife3bb7e82237a877f733e0569441ea9c456d4e39
    Signed-off-by: Xuehui Ye <yexh1@motorola.com>
    Reviewed-on: http://gerrit.mot.com/822354
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -4981,6 +4981,7 @@ static int otg_power_set_property_usb(struct power_supply *psy,
 			break;
 		default:
 			motg->chg_type = USB_INVALID_CHARGER;
+			psy->type = POWER_SUPPLY_TYPE_USB;
 			break;
 		}
 
