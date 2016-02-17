d4c5dea142f5b1002b272f01bb967cbd61529344
Zac Crosby <zac@motorola.com>
2016-02-17 14:06:23 -0600

    msm: mdss: Add xlog dump & dropbox event for ping pong timeouts
    Dump xlog & create a dropbox entry on a ping pong timeout.
    
    Change-Id: I2532b08760b84ef7e3929b6fe85f610ce2e83d1e
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/822968
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -21,6 +21,7 @@
 #include "mdss_debug.h"
 #include "mdss_mdp_trace.h"
 #include "mdss_dsi_clk.h"
+#include "mdss_dropbox.h"
 
 #define MAX_RECOVERY_TRIALS 10
 #define MAX_SESSIONS 2
@@ -1933,6 +1934,12 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0_ctrl", "dsi0_phy",
 				"dsi1_ctrl", "dsi1_phy", "vbif", "vbif_nrt",
 				"dbg_bus", "vbif_dbg_bus", "panic");
+			MDSS_XLOG_TOUT_HANDLER_MMI("mdp",
+						"dsi0_ctrl", "dsi0_phy",
+						"dsi1_ctrl", "dsi1_phy");
+			mdss_dropbox_report_event_ratelimit(
+				MDSS_DROPBOX_MSG_PP_TO, 1,
+				&mdss_dropbox_global_rl);
 		} else if (ctx->pp_timeout_report_cnt == MAX_RECOVERY_TRIALS) {
 			MDSS_XLOG(0xbad2);
 			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0_ctrl", "dsi0_phy",
