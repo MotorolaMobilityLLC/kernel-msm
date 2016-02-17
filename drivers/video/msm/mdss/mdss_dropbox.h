ae1d708b1d2070451a23579c28dfc34f6f3b78f1
Zac Crosby <zac@motorola.com>
2016-02-17 13:43:30 -0600

    msm: mdss: Add a rate limited API for sending dropbox events
    Currently we've avoided adding dropbox events to MDSS errors which could
    happen very frequently due to us wanting to avoid spamming.  To allow us
    to add dropbox events in such situations, implement a new dropbox API
    which is rate limited.
    
    Change-Id: I70a266c9dcc57f1a40035b1837aabe198412e1a5
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/822965
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dropbox.h
+++ drivers/video/msm/mdss/mdss_dropbox.h
@@ -12,8 +12,14 @@
 #ifndef MDSS_DROPBOX_H
 #define MDSS_DROPBOX_H
 
+#include <linux/ratelimit.h>
+
 #define MDSS_DROPBOX_MSG_ESD "ESD event detected"
 #define MDSS_DROPBOX_MSG_PWR_MODE_BLACK "PWR_MODE black screen detected"
 
+extern struct ratelimit_state mdss_dropbox_global_rl;
 void mdss_dropbox_report_event(char *msg, int count);
+void mdss_dropbox_report_event_ratelimit(char *msg, int count,
+					struct ratelimit_state *rs);
+
 #endif /* MDSS_DROPBOX_H */
