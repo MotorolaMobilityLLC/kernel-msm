fadddf023ce7d8d762f506c57e02f8f7d91d8914
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2016-02-17 14:36:32 -0700

    msm: thermal: Add range checking for cluster_id
    The cluster id flag is passed in from the userspace through ioctl
    interface. Ensure correctness of cluster id to avoid out of bounds array
    accesses.
    
    CRS-fixed: 977508
    Change-Id: I778b962d347b90488b983a15087b13e90ad06688
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/thermal/msm_thermal-dev.c
+++ drivers/thermal/msm_thermal-dev.c
@@ -112,6 +112,9 @@ static long msm_thermal_process_freq_table_req(struct msm_thermal_ioctl *query,
 	uint32_t table_idx, idx = 0, cluster_id = query->clock_freq.cluster_num;
 	struct clock_plan_arg *clock_freq = &(query->clock_freq);
 
+	if (cluster_id >= num_possible_cpus())
+		return -EINVAL;
+
 	if (!freq_table_len[cluster_id]) {
 		ret = msm_thermal_get_freq_plan_size(cluster_id,
 			&freq_table_len[cluster_id]);
