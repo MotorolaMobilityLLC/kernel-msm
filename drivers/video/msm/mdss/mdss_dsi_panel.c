9e635697e8157fcd70813309507f2cb9b89c85dd
Zac Crosby <zac@motorola.com>
2016-02-17 13:50:30 -0600

    msm: mdss: Execute xlog dump on black screen detected
    Do an xlog dump when black screen is detected.
    
    Change-Id: I1b15bd0d7e76eee8c872122626945e57ccd9774e
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/822967
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,6 +30,7 @@
 #include "mdss_dba_utils.h"
 #include "mdss_fb.h"
 #include "mdss_dropbox.h"
+#include "mdss_debug.h"
 
 #define MDSS_PANEL_DEFAULT_VER 0xffffffffffffffff
 #define MDSS_PANEL_UNKNOWN_NAME "unknown"
@@ -952,6 +953,8 @@ end:
 
 	if (dropbox_issue != NULL) {
 		dropbox_count++;
+		MDSS_XLOG_TOUT_HANDLER_MMI("mdp", "dsi0_ctrl", "dsi0_phy",
+					"dsi1_ctrl", "dsi1_phy");
 		mdss_dropbox_report_event(dropbox_issue, dropbox_count);
 	} else
 		dropbox_count = 0;
