a512950539999b18c54cdaad944d7d205d9e5c64
Bang Nguyen <bangnguyen@motorola.com>
2016-02-17 15:20:27 -0600

    include: linux: platform: Add slimport_device.h
    Add MOT's HW support for ANX7816
    
    Change-Id: I178cac2c52e9edfebb4d3fd92e10023936db6d79
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/827438
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Sandersary Crosby <zac@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/linux/platform_data/slimport_device.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright(c) 2016, Motorola Mobility LLC. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef SLIMPORT_DEVICE
+#define SLIMPORT_DEVICE
+unchar sp_get_link_bw(void);
+int slimport_read_edid_block(int block, uint8_t *edid_buf);
+void anx7816_force_mydp_det(bool connected);
+#endif /* SLIMPORT_DEVICE */
