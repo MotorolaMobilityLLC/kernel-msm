663f97883c2a7e8980477a139b65de24a1020ad8
Bang Nguyen <bangnguyen@motorola.com>
2016-02-29 09:33:46 -0600

    slimport: anx7816: Add MOT's cbl_det debouning
    Add MOT's cable debouncing logic that will wait for 50msec before
    checking if cable is still connected. Analogix has its cbl_det logic,
    that will sampling 10 times with 5msec delay between each time, and if
    it can read logic "High" of cbl_det GPIO more than 5 times then it will
    consider cable is connected. This logic is kind of odd, because it
    doen't check for the final state of the cable connection.
    
    Change-Id: I9be533709bd64bbeda5608c59d2c8c8885db1435
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: /827442
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Sandersary Crosby <zac@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/slimport/anx7816/slimport.c
+++ drivers/video/slimport/anx7816/slimport.c
@@ -946,7 +946,8 @@ void cable_disconnect(void *data)
 
 /*JIRA: CLD-110,
 Software patch for cable det pin has glitch before stable "High"*/
-#define CABLE_DET_PIN_HAS_GLITCH
+/*#define CABLE_DET_PIN_HAS_GLITCH */
+#define DEBOUNCING_DELAY_US 50000
 static unsigned char confirmed_cable_det(void *data)
 {
 	struct anx7816_data *anx7816 = data;
@@ -963,6 +964,7 @@ static unsigned char confirmed_cable_det(void *data)
 
 	return (cable_det_count > 5) ? 0 : 1;
 	#else
+	usleep_range(DEBOUNCING_DELAY_US, DEBOUNCING_DELAY_US);
 	return gpio_get_value(anx7816->pdata->gpio_cbl_det);
 	#endif
 }
