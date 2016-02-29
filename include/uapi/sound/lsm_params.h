583d3a4eea36a8e6f8361028cf06a8cdb1ada6ad
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2016-02-29 14:13:02 +0530

    uapi: lsm_params: Add interface to disable polling
    Polling needs to be disabled to support Linear Echo Cancelation(LEC)
    for SVA. Add interface to enable or disable polling through
    set_params.
    
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
    Change-Id: If027418a6d8a1ea48dcb6a0c146f68e7dd7a2664

--- include/uapi/sound/lsm_params.h
+++ include/uapi/sound/lsm_params.h
@@ -43,6 +43,7 @@ enum LSM_PARAM_TYPE {
 	LSM_REG_SND_MODEL,
 	LSM_DEREG_SND_MODEL,
 	LSM_CUSTOM_PARAMS,
+	LSM_POLLING_ENABLE,
 	/* driver ioctl will parse only so many params */
 	LSM_PARAMS_MAX,
 };
@@ -75,6 +76,14 @@ struct snd_lsm_gain {
 	__u16 gain;
 };
 
+/*
+ * Data for LSM_POLLING_ENABLE param_type
+ * @poll_en: Polling enable or disable
+ */
+struct snd_lsm_poll_enable {
+	bool poll_en;
+};
+
 
 struct snd_lsm_sound_model_v2 {
 	__u8 __user *data;
@@ -100,6 +109,7 @@ struct snd_lsm_detection_params {
 	enum lsm_detection_mode detect_mode;
 	__u8 num_confidence_levels;
 	bool detect_failure;
+	bool poll_enable;
 };
 
 /*
