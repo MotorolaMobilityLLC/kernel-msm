b649bfb932af0d5bde327e2b25d497660f54812f
Ravi Vembu <raviv@motorola.com>
2016-02-29 19:48:55 -0600

    usb: dwc3: Log controller number in the debug buffer.
    Since both USB controllers use the same debug buffer, log
    the controller number in the debug buffer messages.
    This will help debug issues when both controllers are active.
    
    Change-Id: Ifa736a18cd566f031595375b9f960e7e3c2d6fea
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/827568
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -283,7 +283,7 @@ int __dwc3_gadget_ep0_set_halt(struct usb_ep *ep, int value)
 	struct dwc3_ep			*dep = to_dwc3_ep(ep);
 	struct dwc3			*dwc = dep->dwc;
 
-	dbg_event(dep->number, "EP0STAL", value);
+	dbg_event(dwc->ctrl_num, dep->number, "EP0STAL", value);
 	dwc3_ep0_stall_and_restart(dwc);
 
 	return 0;
@@ -311,7 +311,7 @@ void dwc3_ep0_out_start(struct dwc3 *dwc)
 			DWC3_TRBCTL_CONTROL_SETUP);
 
 	if (WARN_ON_ONCE(ret < 0))
-		dbg_event(dwc->eps[0]->number, "EOUTSTART", ret);
+		dbg_event(dwc->ctrl_num, dwc->eps[0]->number, "EOUTSTART", ret);
 }
 
 static struct dwc3_ep *dwc3_wIndex_to_dep(struct dwc3 *dwc, __le16 wIndex_le)
@@ -668,7 +668,7 @@ static void dwc3_ep0_set_sel_cmpl(struct usb_ep *ep, struct usb_request *req)
 	ret = dwc3_send_gadget_generic_command(dwc,
 			DWC3_DGCMD_SET_PERIODIC_PAR, param);
 	if (WARN_ON_ONCE(ret < 0))
-		dbg_event(dep->number, "ESET_SELCMPL", ret);
+		dbg_event(dwc->ctrl_num, dep->number, "ESET_SELCMPL", ret);
 }
 
 static int dwc3_ep0_set_sel(struct dwc3 *dwc, struct usb_ctrlrequest *ctrl)
@@ -794,7 +794,7 @@ static void dwc3_ep0_inspect_setup(struct dwc3 *dwc,
 		dwc->ep0_next_event = DWC3_EP0_NRDY_DATA;
 	}
 
-	dbg_setup(0x00, ctrl);
+	dbg_setup(dwc->ctrl_num, 0x00, ctrl);
 	if ((ctrl->bRequestType & USB_TYPE_MASK) == USB_TYPE_STANDARD)
 		ret = dwc3_ep0_std_request(dwc, ctrl);
 	else
@@ -805,7 +805,7 @@ static void dwc3_ep0_inspect_setup(struct dwc3 *dwc,
 
 out:
 	if (ret < 0) {
-		dbg_event(0x0, "ERRSTAL", ret);
+		dbg_event(dwc->ctrl_num, 0x0, "ERRSTAL", ret);
 		dwc3_ep0_stall_and_restart(dwc);
 	}
 }
@@ -873,7 +873,7 @@ static void dwc3_ep0_complete_data(struct dwc3 *dwc,
 
 	if ((epnum & 1) && ur->actual < ur->length) {
 		/* for some reason we did not get everything out */
-		dbg_event(epnum, "INDATSTAL", 0);
+		dbg_event(dwc->ctrl_num, epnum, "INDATSTAL", 0);
 		dwc3_ep0_stall_and_restart(dwc);
 	} else {
 		dwc3_gadget_giveback(ep0, r, 0);
@@ -888,7 +888,8 @@ static void dwc3_ep0_complete_data(struct dwc3 *dwc,
 					dwc->ctrl_req_addr, 0,
 					DWC3_TRBCTL_CONTROL_DATA);
 			if (WARN_ON_ONCE(ret < 0))
-				dbg_event(epnum, "ECTRL_DATA", ret);
+				dbg_event(dwc->ctrl_num, epnum, "ECTRL_DATA",
+						ret);
 		}
 	}
 }
@@ -917,7 +918,7 @@ static void dwc3_ep0_complete_status(struct dwc3 *dwc,
 		if (ret < 0) {
 			dwc3_trace(trace_dwc3_ep0, "Invalid Test #%d",
 					dwc->test_mode_nr);
-			dbg_event(0x00, "INVALTEST", ret);
+			dbg_event(dwc->ctrl_num, 0x00, "INVALTEST", ret);
 			dwc3_ep0_stall_and_restart(dwc);
 			return;
 		}
@@ -927,7 +928,7 @@ static void dwc3_ep0_complete_status(struct dwc3 *dwc,
 	if (status == DWC3_TRBSTS_SETUP_PENDING)
 		dwc3_trace(trace_dwc3_ep0, "Setup Pending received\n");
 
-	dbg_print(dep->number, "DONE", status, "STATUS");
+	dbg_print(dwc->ctrl_num, dep->number, "DONE", status, "STATUS");
 	dwc->ep0state = EP0_SETUP_PHASE;
 	dwc3_ep0_out_start(dwc);
 }
@@ -1015,7 +1016,7 @@ static void __dwc3_ep0_do_control_data(struct dwc3 *dwc,
 				req->request.length, DWC3_TRBCTL_CONTROL_DATA);
 	}
 
-	dbg_queue(dep->number, &req->request, ret);
+	dbg_queue(dwc->ctrl_num, dep->number, &req->request, ret);
 }
 
 static int dwc3_ep0_start_control_status(struct dwc3_ep *dep)
@@ -1041,7 +1042,7 @@ static void __dwc3_ep0_do_control_status(struct dwc3 *dwc, struct dwc3_ep *dep)
 
 	ret = dwc3_ep0_start_control_status(dep);
 	if (WARN_ON_ONCE(ret))
-		dbg_event(dep->number, "ECTRLSTATUS", ret);
+		dbg_event(dwc->ctrl_num, dep->number, "ECTRLSTATUS", ret);
 }
 
 static void dwc3_ep0_do_control_status(struct dwc3 *dwc,
@@ -1069,7 +1070,7 @@ static void dwc3_ep0_end_control_data(struct dwc3 *dwc, struct dwc3_ep *dep)
 	if (ret) {
 		dev_dbg(dwc->dev, "%s: send ep cmd ENDTRANSFER failed",
 			dep->name);
-		dbg_event(dep->number, "EENDXFER", ret);
+		dbg_event(dwc->ctrl_num, dep->number, "EENDXFER", ret);
 	}
 	dep->resource_index = 0;
 }
@@ -1105,7 +1106,7 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 			dwc3_trace(trace_dwc3_ep0,
 					"Wrong direction for Data phase");
 			dwc3_ep0_end_control_data(dwc, dep);
-			dbg_event(epnum, "WRONGDR", 0);
+			dbg_event(dwc->ctrl_num, epnum, "WRONGDR", 0);
 			dwc3_ep0_stall_and_restart(dwc);
 			return;
 		}
@@ -1115,7 +1116,7 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 			ret = dwc3_ep0_start_trans(dwc, epnum,
 					dwc->ctrl_req_addr, 0,
 					DWC3_TRBCTL_CONTROL_DATA);
-			dbg_event(epnum, "ZLP", ret);
+			dbg_event(dwc->ctrl_num, epnum, "ZLP", ret);
 			if (ret)
 				dev_dbg(dwc->dev, "%s: start xfer cmd failed",
 					dep->name);
@@ -1136,7 +1137,8 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 		if (dwc->delayed_status &&
 				list_empty(&dwc->eps[0]->request_list)) {
 			if (event->endpoint_number != 1)
-				dbg_event(epnum, "EEPNUM", event->status);
+				dbg_event(dwc->ctrl_num, epnum, "EEPNUM",
+					event->status);
 			dwc3_trace(trace_dwc3_ep0, "Delayed Status");
 			return;
 		}
