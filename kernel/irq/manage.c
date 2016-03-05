280f48f109133c72430d096f7af90290386c80e9
jeboo <jeb00o@yahoo.com>
2016-03-05 11:28:17 -0500

    kernel 3.10.99 patch (xt1575_mm_6.0.0 specific)

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1280,6 +1280,7 @@ static struct irqaction *__free_irq(unsigned int irq, void *dev_id)
 	if (!desc)
 		return NULL;
 
+	chip_bus_lock(desc);
 	raw_spin_lock_irqsave(&desc->lock, flags);
 
 	/*
@@ -1293,7 +1294,7 @@ static struct irqaction *__free_irq(unsigned int irq, void *dev_id)
 		if (!action) {
 			WARN(1, "Trying to free already-free IRQ %d\n", irq);
 			raw_spin_unlock_irqrestore(&desc->lock, flags);
-
+			chip_bus_sync_unlock(desc);
 			return NULL;
 		}
 
@@ -1323,6 +1324,7 @@ static struct irqaction *__free_irq(unsigned int irq, void *dev_id)
 #endif
 
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
+	chip_bus_sync_unlock(desc);
 
 	unregister_handler_proc(irq, action);
 
@@ -1400,9 +1402,7 @@ void free_irq(unsigned int irq, void *dev_id)
 		kref_put(&notify->kref, notify->release);
 #endif
 
-	chip_bus_lock(desc);
 	kfree(__free_irq(irq, dev_id));
-	chip_bus_sync_unlock(desc);
 }
 EXPORT_SYMBOL(free_irq);
 
