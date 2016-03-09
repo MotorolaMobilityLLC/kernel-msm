1c187f43ee4c4de704c6c213069230eb02344527
pangkun <pangkun@longcheer.net>
2016-03-09 15:00:26 +0800

    Modified the current limited for DCP charger
    Modified the value from 1800 t0 3000mA.
    
    Change-Id: Ifda5705a9212b948e5b55e95614a989811d158c6
    Signed-off-by: pangkun <pangkun@longcheer.net>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -407,7 +407,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 1800;
+static int smbchg_default_dcp_icl_ma = 3000;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
