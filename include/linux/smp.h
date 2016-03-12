aad37a54a45a9dd4de3fc080f9fa6157830ede80
Lianwei Wang <a22439@motorola.com>
2016-03-11 18:21:44 -0600

    smp: make wake up idle cpus more generic
    wake_up_all_idle_cpus always wake up all the online cpus, but sometime
    we only want to wake up a set of cpus.
    
    Make the API more generic and wake up a set of idle cpus specified by
    the cpumask parameter.
    
    Change-Id: Idd1890a941106ff8b9a57a3167331f25f090070f
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/832905
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -101,7 +101,7 @@ int smp_call_function_any(const struct cpumask *mask,
 			  smp_call_func_t func, void *info, int wait);
 
 void kick_all_cpus_sync(void);
-void wake_up_all_idle_cpus(void);
+void wake_up_idle_cpus(const struct cpumask *mask);
 
 /*
  * Generic and arch helpers
@@ -150,7 +150,7 @@ smp_call_function_any(const struct cpumask *mask, smp_call_func_t func,
 }
 
 static inline void kick_all_cpus_sync(void) {  }
-static inline void wake_up_all_idle_cpus(void) {  }
+static inline void wake_up_idle_cpus(const struct cpumask *mask) {  }
 
 #endif /* !SMP */
 
