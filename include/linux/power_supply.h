1010a83abb6a476536e913a60e7ad0c8e12f2260
Ravi Vembu <raviv@motorola.com>
2016-03-15 15:50:04 -0500

    fusb302: Add a property to kick FUSB302
    Add a psy property to wakeup the FUSB302 state machine. Useful for
    scenarios when the statemachine stalls because of a missing interrupt.
    
    Change-Id: I9b9e495c12a08eb2a7c8a3d0a721aa678bd461f1
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/833469
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -329,6 +329,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_EXTERN_STATE,
 	POWER_SUPPLY_PROP_DISABLE_USB,
 	POWER_SUPPLY_PROP_SWITCH_STATE,
+	POWER_SUPPLY_PROP_WAKEUP,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
