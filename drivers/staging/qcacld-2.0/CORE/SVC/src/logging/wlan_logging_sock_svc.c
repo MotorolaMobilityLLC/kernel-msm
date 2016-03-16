3a2bb7cf1b062be69b92e745ef7e920bc466fea4
mohamed.khadri <mohamed.khadri@lge.com>
2016-03-15 17:34:38 -0700

    wlan: qcacld-2.0: reset sk_buff correctly using skb_trim
    in wlan_get_pkt_stats_free_node, skbuff is also reused from
    filled list, inconsistent tail and len leads to skb_panic
    
    Bug: 27587228
    
    Change-Id: Idca4f913fbbbffd70636c9471e5df1deba0e339c
    Signed-off-by: Mohamed Khadri <mohamed.khadri@lge.com>

--- drivers/staging/qcacld-2.0/CORE/SVC/src/logging/wlan_logging_sock_svc.c
+++ drivers/staging/qcacld-2.0/CORE/SVC/src/logging/wlan_logging_sock_svc.c
@@ -43,6 +43,7 @@
 #include <adf_os_time.h>
 #include "pktlog_ac.h"
 #include <linux/rtc.h>
+#include <linux/skbuff.h>
 #include <vos_diag_core_log.h>
 #include "limApi.h"
 #include "ol_txrx_api.h"
@@ -1120,8 +1121,8 @@ static int wlan_get_pkt_stats_free_node(void)
 		ret = 1;
 	}
 
-	/* Reset the current node values */
-	gwlan_logging.pkt_stats_pcur_node->skb->len = 0;
+	/* Reset the skb values, essential if dequeued from filled list */
+	skb_trim(gwlan_logging.pkt_stats_pcur_node->skb, 0);
 	return ret;
 }
 
