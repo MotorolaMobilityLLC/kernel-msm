3908613fec2f62ea6afb228f00ee71b5b9103444
SeungYoon Lee <w22905@motorola.com>
2016-03-17 21:17:51 -0500

    ASoC: msm: qdspv6: Add callback interface
    Add APR packet sending and callback interface to allow
    external AFE module to send APR messages and handle its
    callback from outside of q6afe.c
    
    Change-Id: I433c28fb7ee0ddd3f2440f676e6d76ac8b4507d6
    Signed-off-by: SeungYoon Lee <w22905@motorola.com>
    Reviewed-on: http://gerrit.mot.com/834485
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Sierawski <jeffsierawski@motorola.com>

--- include/sound/q6afe-v2.h
+++ include/sound/q6afe-v2.h
@@ -337,4 +337,10 @@ int afe_send_custom_tdm_header_cfg(
 	u16 port_id);
 int afe_tdm_port_start(u16 port_id, struct afe_tdm_port_config *tdm_port,
 		u32 rate, u16 num_groups);
+
+#ifdef CONFIG_SND_SOC_OPALUM
+int ospl2xx_afe_set_callback(int32_t (*ospl2xx_callback_func)
+				(struct apr_client_data *data));
+int ospl2xx_afe_apr_send_pkt(void *data, int index);
+#endif
 #endif /* __Q6AFE_V2_H__ */
