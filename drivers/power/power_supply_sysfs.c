61b0df615429ffc76ff7ffc4a0505f8e2ccc3a05
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2016-03-21 15:54:35 -0700

    power_supply: add support to control hvdcp3 detection
    Currently, HVDCP3 detection is enabled by default. Add support
    to control it dynamically through a power supply property
    "allow_hvdcp3".
    
    CRs-Fixed: 991060
    Change-Id: Id283b572cdb51094ad53d590e230104dc3e1beb7
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -234,6 +234,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cycle_count_id),
 	POWER_SUPPLY_ATTR(safety_timer_expired),
 	POWER_SUPPLY_ATTR(restricted_charging),
+	POWER_SUPPLY_ATTR(allow_hvdcp3),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
