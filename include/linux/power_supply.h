613e17158425b45ea14cb8565d53f0262ded4793
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2016-03-21 15:54:35 -0700

    power_supply: add support to control hvdcp3 detection
    Currently, HVDCP3 detection is enabled by default. Add support
    to control it dynamically through a power supply property
    "allow_hvdcp3".
    
    CRs-Fixed: 991060
    Change-Id: Id283b572cdb51094ad53d590e230104dc3e1beb7
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -201,6 +201,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CYCLE_COUNT_ID,
 	POWER_SUPPLY_PROP_SAFETY_TIMER_EXPIRED,
 	POWER_SUPPLY_PROP_RESTRICTED_CHARGING,
+	POWER_SUPPLY_PROP_ALLOW_HVDCP3,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
