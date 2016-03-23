e7344d845da9772ee7dea4741e95b1a30649740f
Yongbin Chen <chenyb1@motorola.com>
2016-03-23 00:05:52 -0600

    Revert "Mot-CRs-fixed:(CR) cpufreq: fix sleeping in atomic... Cause CTS test failed!! (CR)
    kernel Panic occurred and die in below function
    (show_all_time_in_state+0x168/0x208)
    
    
    This reverts commit 41e85255d26066f7a626c7a669cc91fa78900b47.
    Change-Id: Icd853a1215958c5a9682e1c4bd68e54136fa3424
    Signed-off-by: chenyb1 <chenyb1@motorola.com>
    Reviewed-on: http://gerrit.mot.com/836375
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Xuehui Ye <yexh1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -423,7 +423,7 @@ static void add_all_freq_table(unsigned int freq)
 	unsigned int size;
 	size = sizeof(unsigned int) * (all_freq_table->table_size + 1);
 	all_freq_table->freq_table = krealloc(all_freq_table->freq_table,
-			size, GFP_ATOMIC);
+			size, GFP_KERNEL);
 	if (IS_ERR(all_freq_table->freq_table)) {
 		pr_warn("Could not reallocate memory for freq_table\n");
 		all_freq_table->freq_table = NULL;
