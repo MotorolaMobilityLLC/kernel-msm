MiCode/Xiaomi_Kernel_OpenSource

msm: mdss: flush ongoing hpd work before turning hpd off
Since hpd off and hpd interrupt work are always in different contexts,
in a very strict timing window it is possible that hpd off is scheduled the
middle of hpd interrupt work. If this happens, when hpd interrupt work
resumes after hpd off is serviced, un-clocked register access may lead
to unknown outcome. Counter this situation by flushing the hpd interrupt
work, if any, before calling hpd off.

Change-Id: I669db5d4b8cbdd20fa24e03f0a8a0f3f2a029ee0
CRs-Fixed: 482357
Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -2331,6 +2331,9 @@ static void hdmi_tx_hpd_off(struct hdmi_tx_ctrl *hdmi_ctrl)
 		return;
 	}
 
+	/* finish the ongoing hpd work if any */
+	flush_work_sync(&hdmi_ctrl->hpd_int_work);
+
 	/* Turn off HPD interrupts */
 	DSS_REG_W(io, HDMI_HPD_INT_CTRL, 0);
 

MotorolaMobilityLLC/kernel-msm

drivers/video/fbdev/msm/mdss_hdmi_tx.c
@@ -2334,6 +2334,9 @@ static void hdmi_tx_hpd_off(struct hdmi_tx_ctrl *hdmi_ctrl)
 		return;
 	}
 
+	/* finish the ongoing hpd work if any */
+	flush_work(&hdmi_ctrl->hpd_int_work);
+
 	/* Turn off HPD interrupts */
 	DSS_REG_W(io, HDMI_HPD_INT_CTRL, 0);
 