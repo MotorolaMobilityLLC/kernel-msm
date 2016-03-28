9a8299064ad40dc83becdb5cb31ce4c1513595be
Sumit Jamadar <xkbr38@motorola.com>
2016-03-28 17:25:09 -0500

    msm: camera: Enable logsync in debug builds only.
    Propagated from: (CR)
    
    Enable logsync in debug builds only to avoid writable
    sysfs file security issue.
    
    Change-Id: I0801d26c7250e53f5508c1df98a702c82de90926
    Signed-off-by: Sumit Jamadar <xkbr38@motorola.com>
    Reviewed-on: http://gerrit.mot.com/843448
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -1162,6 +1162,7 @@ static void msm_sd_notify(struct v4l2_subdev *sd,
 	}
 }
 
+#ifdef CONFIG_DEBUG_CAMERA
 static ssize_t write_logsync(struct file *file, const char __user *buf,
 		size_t count, loff_t *ppos)
 {
@@ -1185,11 +1186,14 @@ static ssize_t write_logsync(struct file *file, const char __user *buf,
 static const struct file_operations logsync_fops = {
 		.write = write_logsync,
 };
+#endif
 
 static int msm_probe(struct platform_device *pdev)
 {
 	struct msm_video_device *pvdev = NULL;
+#ifdef CONFIG_DEBUG_CAMERA
 	static struct dentry *cam_debugfs_root;
+#endif
 	int rc = 0;
 
 	msm_v4l2_dev = kzalloc(sizeof(*msm_v4l2_dev),
@@ -1271,6 +1275,7 @@ static int msm_probe(struct platform_device *pdev)
 	spin_lock_init(&msm_pid_lock);
 	INIT_LIST_HEAD(&ordered_sd_list);
 
+#ifdef CONFIG_DEBUG_CAMERA
 	cam_debugfs_root = debugfs_create_dir(MSM_CAM_LOGSYNC_FILE_BASEDIR,
 						NULL);
 	if (!cam_debugfs_root) {
@@ -1283,6 +1288,7 @@ static int msm_probe(struct platform_device *pdev)
 					 &logsync_fops))
 			pr_warn("NON-FATAL: failed to create logsync debugfs file\n");
 	}
+#endif
 
 	rc = cam_ahb_clk_init(pdev);
 	if (rc < 0) {
