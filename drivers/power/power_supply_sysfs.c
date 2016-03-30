2c63e1fd921f08dea79e70db575e67893ce9b798
Dmitriy Gruzman <dgruzman@motorola.com>
2016-03-30 15:40:54 -0500

    power_supply: Add/remove properties from POWER_SUPPLY_TYPE_PTP
    Add properties to report what type of power source is available to
    provide current and how much current can be pulled from the source.
    
    Remove unused property INTERNAL_MAX_RECEIVE_VOLTAGE.
    
    Change-Id: I1a8a28377609ab191d2ab56891d2e1f30e621e9e
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -305,12 +305,14 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(sdp_current_max),
 	POWER_SUPPLY_ATTR(internal_send),
 	POWER_SUPPLY_ATTR(internal_receive),
-	POWER_SUPPLY_ATTR(internal_max_receive_v),
 	POWER_SUPPLY_ATTR(external),
 	POWER_SUPPLY_ATTR(current_flow),
 	POWER_SUPPLY_ATTR(max_input_current),
+	POWER_SUPPLY_ATTR(max_output_current),
 	POWER_SUPPLY_ATTR(external_present),
 	POWER_SUPPLY_ATTR(power_required),
+	POWER_SUPPLY_ATTR(power_available),
+	POWER_SUPPLY_ATTR(power_source),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
