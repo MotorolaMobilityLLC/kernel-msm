d21f3e3dd9515851ed61004ccbc03c12ecdf0bda
Gabriel Burca <Gabriel.Burca@motorola.com>
2016-04-04 13:50:33 -0500

    iio: core: Add IIO event for buffer empty
    This new event type is needed to notify the HAL when the sensor
    data FIFO has been flushed (and is empty).
    
    Change-Id: Id215fed41246fffa3817017a1636d646b5129449
    Signed-off-by: Gabriel Burca <Gabriel.Burca@motorola.com>
    Reviewed-on: http://gerrit.mot.com/841441
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Jason Jae Hyun Koh <jasonkoh@motorola.com>
    Reviewed-by: Eric Hefner <ehefner@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -70,6 +70,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_CCT] = "cct",
 	[IIO_PRESSURE] = "pressure",
 	[IIO_HUMIDITYRELATIVE] = "humidityrelative",
+	[IIO_PROPRIETARY] = "proprietary",
 };
 
 static const char * const iio_modifier_names[] = {
