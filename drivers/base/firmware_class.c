d5b6c9e96732a4785254bfb17f7e9b10624ec92b
Jason Hrycay <jason.hrycay@motorola.com>
2016-04-06 08:24:35 -0500

    firmware_class: Ensure fw set to null on failure
    Ensure that when calling request_firmware_nowait, all failure paths
    properly send @fw == NULL to the provided cont() callback. The case
    where a null-string "" is passed as the firmware name will cause a path
    where the pointer passed is uninitialized stack data. Ensure we properly
    set the pointer to NULL in this case.
    
    Change-Id: I2905b590ee57a8663dc3d9c482621329d86bb462
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.mot.com/841706
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: John Ratke <jratke@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/898072

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -1303,15 +1303,17 @@ static int assign_firmware_buf(struct firmware *fw, struct device *device,
 /* called from request_firmware() and request_firmware_work_func() */
 static int _request_firmware(struct fw_desc *desc)
 {
-	struct firmware *fw;
+	struct firmware *fw = NULL;
 	long timeout;
 	int ret;
 
 	if (!desc->firmware_p)
 		return -EINVAL;
 
-	if (!desc->name || desc->name[0] == '\0')
-		return -EINVAL;
+	if (!desc->name || desc->name[0] == '\0') {
+		ret = -EINVAL;
+		goto out;
+	}
 
 	ret = _request_firmware_prepare(&fw, desc);
 	if (ret <= 0) /* error or already assigned */
