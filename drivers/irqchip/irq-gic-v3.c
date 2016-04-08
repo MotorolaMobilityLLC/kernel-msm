b4eea85565a0e99f5269b6a23cfd3b8ce6b9db84
Ravi Chebolu <arc095@motorola.com>
2016-04-08 16:36:49 -0500

    Use log_wakeup_reason() to log wakeup reason
    Instead of using printk to log the wakeup reason, use
    log_wakeup_reason() API. This function not only logs the
    wakeup reason but also exposes the last wakeup reason to
    userspace via "/sys/kernel/wakeup_reasons/last_wakeup_reason"
    
    Android batterystats reads the last wakeup reason from this
    sysfs and logs in the bug report.
    
    Change-Id: Ic5c667b3d0838e4d514d2dea44eb88f8a635a1e4
    Signed-off-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842863
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <Raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -28,6 +28,7 @@
 
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/syscore_ops.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/cputype.h>
 #include <asm/exception.h>
@@ -385,7 +386,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		log_base_wakeup_reason(irq);
 	}
 }
 
