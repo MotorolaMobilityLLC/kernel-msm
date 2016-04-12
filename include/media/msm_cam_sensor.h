87a41ac607d4b6dbe9d8911ca4fbadc490cdd4dc
Nagappan Rathinam <dgxp63@motorola.com>
2016-04-12 11:14:18 -0500

    msm: camera: Add Mot lc898212xd Actuator.
    Port lc898212xd Actuator driver from 8952 to 8953/8996 baseline
    for Addison product.
    
    Change-Id: I88ffbe53a8cfc2f9fa3bbadc7cfd44a00d161ee3
    Signed-off-by: Nagappan Rathinam <dgxp63@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842950
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -133,6 +133,11 @@ struct msm_camera_i2c_array_write_config32 {
 	uint16_t slave_addr;
 };
 
+struct msm_mot_actuator_tuning_params_t32 {
+	int16_t infinity_dac;
+	int16_t macro_dac;
+};
+
 struct msm_actuator_tuning_params_t32 {
 	int16_t initial_code;
 	uint16_t pwd_step;
@@ -158,6 +163,7 @@ struct msm_actuator_params_t32 {
 struct msm_actuator_set_info_t32 {
 	struct msm_actuator_params_t32 actuator_params;
 	struct msm_actuator_tuning_params_t32 af_tuning_params;
+	struct msm_mot_actuator_tuning_params_t32 mot_af_tuning_params;
 };
 
 struct sensor_init_cfg_data32 {
