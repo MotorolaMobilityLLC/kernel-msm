29cca91db553dde04ed171ae4e94244de70d35b3
Long Ling <a21040@motorola.com>
2016-04-12 16:45:25 -0500

    camera_ext: share some camera_ext defines with user space
    Move some definitions to a new file shared with user space.
    * input capabilities
    
    Change-Id: Id12b479ef68d380cdb7d9e7a21862bcb685f44dd
    Signed-off-by: Long Ling <a21040@motorola.com>
    Reviewed-on: http://gerrit.mot.com/843741
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/uapi/video/v4l2_camera_ext_defs.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 Motorola Mobility, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CAMERA_EXT_DEFS_H__
+#define __CAMERA_EXT_DEFS_H__
+
+#define CAMERA_EXT_STREAM_CAP_PREVIEW   (1 << 0)
+#define CAMERA_EXT_STREAM_CAP_VIDEO     (2 << 1)
+#define CAMERA_EXT_STREAM_CAP_SNAPSHOT  (3 << 2)
+
+#endif
