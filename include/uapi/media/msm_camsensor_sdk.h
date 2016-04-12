961280b7f2104c5f70cf8ba7caa47e6565b1af4f
Nagappan Rathinam <dgxp63@motorola.com>
2016-04-12 11:14:18 -0500

    msm: camera: Add Mot lc898212xd Actuator.
    Port lc898212xd Actuator driver from 8952 to 8953/8996 baseline
    for Addison product.
    
    Change-Id: I88ffbe53a8cfc2f9fa3bbadc7cfd44a00d161ee3
    Signed-off-by: Nagappan Rathinam <dgxp63@motorola.com>
    Reviewed-on: http://gerrit.mot.com/842950
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -147,9 +147,19 @@ enum camerab_mode_t {
 	CAMERA_MODE_INVALID = (1<<2),
 };
 
+/* Need to keep this table aligned with
+ * enum msm_camera_i2c_data_type
+ */
 enum msm_actuator_data_type {
 	MSM_ACTUATOR_BYTE_DATA = 1,
 	MSM_ACTUATOR_WORD_DATA,
+	MSM_ACTUATOR_DWORD_DATA,
+	MSM_ACTUATOR_SET_BYTE_MASK,
+	MSM_ACTUATOR_UNSET_BYTE_MASK,
+	MSM_ACTUATOR_SET_WORD_MASK,
+	MSM_ACTUATOR_UNSET_WORD_MASK,
+	MSM_ACTUATOR_SET_BYTE_WRITE_MASK_DATA,
+	MSM_ACTUATOR_DATA_TYPE_MAX,
 };
 
 enum msm_actuator_addr_type {
@@ -164,11 +174,13 @@ enum msm_actuator_write_type {
 	MSM_ACTUATOR_WRITE_DIR_REG,
 	MSM_ACTUATOR_POLL,
 	MSM_ACTUATOR_READ_WRITE,
+	MSM_ACTUATOR_WRITE_REG,
 };
 
 enum msm_actuator_i2c_operation {
 	MSM_ACT_WRITE = 0,
 	MSM_ACT_POLL,
+	MSM_ACT_READ_SET,
 };
 
 enum actuator_type {
@@ -176,6 +188,7 @@ enum actuator_type {
 	ACTUATOR_PIEZO,
 	ACTUATOR_HVCM,
 	ACTUATOR_BIVCM,
+	ACTUATOR_MOT_HVCM,
 };
 
 enum msm_flash_driver_type {
@@ -411,6 +424,7 @@ struct reg_settings_t {
 	enum msm_camera_i2c_data_type data_type;
 	enum msm_actuator_i2c_operation i2c_operation;
 	unsigned int delay;
+	unsigned short eeprom_offset;
 };
 
 struct msm_camera_i2c_reg_setting_array {
