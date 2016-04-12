f36ba6212949f0ed27d1358b385b88bd4129dbbf
hyeonsu9.lee <hyeonsu9.lee@samsung.com>
2016-04-12 11:02:05 +0900

    fs : change access permission for oom_adj related node
    BUG=27816975
    
    Change access permission of proc/self/oom_adj, proc/self/oom_score_adj.
    
    To fix CTS fail.
    android.permission.cts.FileSystemPermissionTest#testProcSelfOomAdjSane
    android.permission.cts.FileSystemPermissionTest#testProcSelfOomScoreAdjSane
    
    Change-Id: I432a88f4163499c7cdfe2406ac36ce0eb2cddd00
    Signed-off-by: hyeonsu9.lee <hyeonsu9.lee@samsung.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2738,8 +2738,8 @@ static const struct pid_entry tgid_base_stuff[] = {
 	REG("cgroup",  S_IRUGO, proc_cgroup_operations),
 #endif
 	INF("oom_score",  S_IRUGO, proc_oom_score),
-	ANDROID("oom_adj", S_IRUGO|S_IWUSR, oom_adj),
-	REG("oom_score_adj", S_IRUGO|S_IWUSR, proc_oom_score_adj_operations),
+	ANDROID("oom_adj", S_IRUGO, oom_adj),
+	REG("oom_score_adj", S_IRUGO, proc_oom_score_adj_operations),
 #ifdef CONFIG_AUDITSYSCALL
 	REG("loginuid",   S_IWUSR|S_IRUGO, proc_loginuid_operations),
 	REG("sessionid",  S_IRUGO, proc_sessionid_operations),
@@ -3094,8 +3094,8 @@ static const struct pid_entry tid_base_stuff[] = {
 	REG("cgroup",  S_IRUGO, proc_cgroup_operations),
 #endif
 	INF("oom_score", S_IRUGO, proc_oom_score),
-	REG("oom_adj",   S_IRUGO|S_IWUSR, proc_oom_adj_operations),
-	REG("oom_score_adj", S_IRUGO|S_IWUSR, proc_oom_score_adj_operations),
+	REG("oom_adj",   S_IRUGO, proc_oom_adj_operations),
+	REG("oom_score_adj", S_IRUGO, proc_oom_score_adj_operations),
 #ifdef CONFIG_AUDITSYSCALL
 	REG("loginuid",  S_IWUSR|S_IRUGO, proc_loginuid_operations),
 	REG("sessionid",  S_IRUGO, proc_sessionid_operations),
