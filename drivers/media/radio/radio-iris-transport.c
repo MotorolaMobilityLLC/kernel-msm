60768b30573d222f4414b16126dc1d02045deb7f
Kamal Negi <kamaln@codeaurora.org>
2016-04-14 18:36:27 +0530

    radio: iris: Check SMD channel state on init
    Add condition to check whether channel is already opened or closed.
    This will prevent smd channel to reopen if it is already opened.
    
    Change-Id: I82cbe05062d3ee58969586e70dc46dc829e529f3
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>

--- drivers/media/radio/radio-iris-transport.c
+++ drivers/media/radio/radio-iris-transport.c
@@ -49,7 +49,7 @@ static void radio_hci_smd_exit(void);
 
 static void radio_hci_smd_destruct(struct radio_hci_dev *hdev)
 {
-	radio_hci_unregister_dev(hs.hdev);
+	radio_hci_unregister_dev();
 }
 
 
@@ -170,7 +170,6 @@ static int radio_hci_smd_register_dev(struct radio_data *hsmd)
 	if (hdev == NULL)
 		return -ENODEV;
 
-	hsmd->hdev = hdev;
 	tasklet_init(&hsmd->rx_task, radio_hci_smd_recv_event,
 		(unsigned long) hsmd);
 	hdev->send  = radio_hci_smd_send_frame;
@@ -198,12 +197,13 @@ static int radio_hci_smd_register_dev(struct radio_data *hsmd)
 		return -ENODEV;
 	}
 
+	hsmd->hdev = hdev;
 	return 0;
 }
 
 static void radio_hci_smd_deregister(void)
 {
-	radio_hci_unregister_dev(hs.hdev);
+	radio_hci_unregister_dev();
 	kfree(hs.hdev);
 	hs.hdev = NULL;
 
@@ -215,6 +215,11 @@ static void radio_hci_smd_deregister(void)
 static int radio_hci_smd_init(void)
 {
 	int ret;
+
+	if (chan_opened) {
+		FMDBG("Channel is already opened");
+		return 0;
+	}
 
 	/* this should be called with fm_smd_enable lock held */
 	ret = radio_hci_smd_register_dev(&hs);
@@ -229,6 +234,11 @@ static int radio_hci_smd_init(void)
 
 static void radio_hci_smd_exit(void)
 {
+	if (!chan_opened) {
+		FMDBG("Channel already closed");
+		return;
+	}
+
 	/* this should be called with fm_smd_enable lock held */
 	radio_hci_smd_deregister();
 	chan_opened = false;
