1d1e66872a07f53b10326bd66f8ee74c8fd6b6b4
Tim Murray <timmurray@google.com>
2016-04-14 10:52:48 -0700

    mdss: lock the offlock mutex correctly
    mdss_mdp_cmd_add_vsync_handler was unlocking a mutex instead of locking
    it.
    
    bug 28160740
    
    Change-Id: Ifac563b4e36f07032e687cb8a1d52db2d5db8e1e

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -474,7 +474,7 @@ static int mdss_mdp_cmd_add_vsync_handler(struct mdss_mdp_ctl *ctl,
 	bool enable_rdptr = false;
 	int ret = 0;
 
-	mutex_unlock(&ctl->offlock);
+	mutex_lock(&ctl->offlock);
 	ctx = (struct mdss_mdp_cmd_ctx *) ctl->priv_data;
 	if (!ctx) {
 		pr_err("%s: invalid ctx\n", __func__);
