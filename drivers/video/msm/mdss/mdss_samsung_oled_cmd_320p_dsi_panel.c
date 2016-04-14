44aedd2e7abf38de0e710a35a4616beb81e41248
Kim Donghyeon <dh_mr.kim@samsung.com>
2016-04-14 22:31:38 +0900

    sprat: mdss: send brightness command in HS mode
    Is transferred in LP mode is suspected to be a problem.
    So I cahnged the mode to hs, and the mode change applied
    to brightness command only.
    
    Bug: 27899789
    
    Change-Id: I4cdca3f59c7f1f28c554a23dff8b5342fa5a35cf
    Signed-off-by: Kim Donghyeon <dh_mr.kim@samsung.com>

--- drivers/video/msm/mdss/mdss_samsung_oled_cmd_320p_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_samsung_oled_cmd_320p_dsi_panel.c
@@ -253,7 +253,7 @@ static void mdss_dsi_panel_bklt_dcs(struct mdss_dsi_ctrl_pdata *ctrl,
 	memset(&cmdreq, 0, sizeof(cmdreq));
 	cmdreq.cmds = &backlight_cmd;
 	cmdreq.cmds_cnt = 1;
-	cmdreq.flags = CMD_REQ_COMMIT | CMD_CLK_CTRL | CMD_REQ_TRIGGER_TE;
+	cmdreq.flags = CMD_REQ_COMMIT | CMD_CLK_CTRL | CMD_REQ_TRIGGER_TE | CMD_REQ_HS_MODE;
 	cmdreq.rlen = 0;
 	cmdreq.cb = NULL;
 
