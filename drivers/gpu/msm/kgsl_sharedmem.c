09a22c66ac761c2b33b46080a731bea47fd477fa
Rajesh Kemisetti <rajeshk@codeaurora.org>
2016-04-19 16:40:31 -0700

    msm: kgsl: Add missing checks for alloc size and sglen
    In _kgsl_sharedmem_page_alloc():
    
    - Make len of type size_t to be in line with size.
    - Check for boundary limits of requested alloc size before honoring.
    - Make sure sglen is greater than zero before marking it as end
    of sg list.
    
    Bug: 27475454
    Change-Id: I5389bcef3478f2ccf2be5f719049867496235f28
    Signed-off-by: Yuan Lin <yualin@google.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -580,9 +580,14 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 			size_t size)
 {
 	int order, ret = 0;
-	int len, page_size, sglen_alloc, sglen = 0;
+	int page_size, sglen_alloc, sglen = 0;
 	void *ptr;
+	size_t len;
 	unsigned int align;
+
+	size = PAGE_ALIGN(size);
+	if (size == 0 || size > UINT_MAX)
+		return -EINVAL;
 
 	align = (memdesc->flags & KGSL_MEMALIGN_MASK) >> KGSL_MEMALIGN_SHIFT;
 
