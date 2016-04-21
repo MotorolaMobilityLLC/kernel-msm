58cc2e044a5b24e48e0ab2594b9acbb9e628afed
Robb Glasser <rglasser@google.com>
2016-04-20 17:15:16 -0700

    wlan: validate essid length before processing scan req
    Presently we are not validating the length of the essid received
    and directly copying the buffer without size checking.
    Perform bound checking before processing the scan req.
    
    Change-Id: I786e4feb67bf039df3d217138a412da54f51787d
    CRs-fixed: 890228

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_scan.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_scan.c
@@ -687,7 +687,8 @@ int __iw_set_scan(struct net_device *dev, struct iw_request_info *info,
 
       if (wrqu->data.flags & IW_SCAN_THIS_ESSID)  {
 
-          if(scanReq->essid_len) {
+          if(scanReq->essid_len  &&
+               (scanReq->essid_len <= SIR_MAC_MAX_SSID_LENGTH)) {
               scanRequest.SSIDs.numOfSSIDs = 1;
               scanRequest.SSIDs.SSIDList =( tCsrSSIDInfo *)vos_mem_malloc(sizeof(tCsrSSIDInfo));
               if(scanRequest.SSIDs.SSIDList) {
@@ -699,7 +700,11 @@ int __iw_set_scan(struct net_device *dev, struct iw_request_info *info,
                 scanRequest.SSIDs.numOfSSIDs = 0;
                 VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR, "%s: Unable to allocate memory",__func__);
                 VOS_ASSERT(0);
-              }
+               }
+           }
+          else
+          {
+            hddLog(LOGE, FL("Invalid essid length : %d"), scanReq->essid_len);
           }
       }
 
