7cc81883de7ea114b605cb41efe1bcc27b0dec1a
Donghua Yan <a18317@motorola.com>
2016-04-25 16:02:17 -0500

    bootinfo: allow more BL signatures
    BL signatures exceed 10 now on 8996.
    Enlarge it to 16 to allow more items.
    
    Change-Id: I768ff2ddbe5cd175d0f3058c3775510413cf14af
    Signed-off-by: Donghua Yan <a18317@motorola.com>
    Reviewed-on: http://gerrit.mot.com/848543
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/soc/qcom/bootinfo.c
+++ drivers/soc/qcom/bootinfo.c
@@ -185,7 +185,7 @@ EXPORT_SYMBOL(bi_boot_seq);
  * as passed along from bootloader via ATAG_BL_BUILD_SIG(s)
  */
 
-#define MAX_BL_BUILD_SIG  10
+#define MAX_BL_BUILD_SIG  16
 #define MAX_BLD_SIG_ITEM  20
 #define MAX_BLD_SIG_VALUE 80
 
