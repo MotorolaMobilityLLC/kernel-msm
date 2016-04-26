e3025405734bb63b518a8372f6e9334ecc111fb2
Jiangli Yuan <jlyuan@motorola.com>
2016-04-26 16:27:10 -0700

    mmc/cmdq: Clear the host halted flag before
     There is a small chance that cmdq is enabled but host halt flag
     is not cleared on time. This will cause sdhci_irq skip the
     cmdq_irq handler leading unexpected interrupt(0x4000).
    
    Solution:
    
      Clear the host halt flag before enabling the CMDQ.
    
    Change-Id: Ic3f92f13db0067294b2462d239f560fb9854bf39
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Reviewed-on: http://gerrit.mot.com/845655
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Signed-off-by: Jiangli Yuan <jlyuan@motorola.com>
    Change-Id: If0adbb23f8e12d019aa1ea92fc111a76c8df8fb7
    Reviewed-on: http://gerrit.mot.com/849180
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -1028,6 +1028,7 @@ static int cmdq_halt(struct mmc_host *mmc, bool halt)
 	struct cmdq_host *cq_host = (struct cmdq_host *)mmc_cmdq_private(mmc);
 	u32 ret = 0;
 	int retries = 3;
+	unsigned long flags;
 
 	cmdq_runtime_pm_get(cq_host);
 	if (halt) {
@@ -1051,6 +1052,8 @@ static int cmdq_halt(struct mmc_host *mmc, bool halt)
 		}
 		ret = retries ? 0 : -ETIMEDOUT;
 	} else {
+
+		local_irq_save(flags);
 		if (cq_host->ops->set_transfer_params)
 			cq_host->ops->set_transfer_params(mmc);
 		if (cq_host->ops->set_block_size)
@@ -1060,8 +1063,11 @@ static int cmdq_halt(struct mmc_host *mmc, bool halt)
 		if (cq_host->ops->clear_set_irqs)
 			cq_host->ops->clear_set_irqs(mmc, true);
 		MMC_TRACE(mmc, "%s: unhalt done\n", __func__);
+		/* clear the halt flag before we enable cmdq */
+		mmc_host_clr_halt(mmc);
 		cmdq_writel(cq_host, cmdq_readl(cq_host, CQCTL) & ~HALT,
 			    CQCTL);
+		local_irq_restore(flags);
 	}
 	cmdq_runtime_pm_put(cq_host);
 	return ret;
