8aa6297670051cc2a0f4482ad53764663168044d
Subhani Shaik <subhanis@codeaurora.org>
2016-04-26 22:13:45 -0700

    qcacld-2.0: Configure correct channel width to FW during TDLS session
    prima to qcacld-2.0 propagation
    
    Due to incorrect configuration of channel width to FW, TDLS
    session has sluggish video.
    Fix is to configure minimum of AP and TDLS peer channel width
    to FW.
    
    CRs-Fixed: 996556
    
    Change-Id: I476bb927d9ca951a59dc5f514f6a4b89667eb152
    Signed-off-by: Subhani Shaik <subhanis@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessTdls.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limProcessTdls.c
@@ -2340,8 +2340,20 @@ static void limTdlsUpdateHashNodeInfo(tpAniSirGlobal pMac, tDphHashNode *pStaDs,
          * base channel should be less than or equal to channel width of
          * STA-AP link. So take this setting from the psessionEntry.
          */
+        limLog(pMac, LOG1,
+               FL("supportedChannelWidthSet %x htSupportedChannelWidthSet %x"),
+               htCaps->supportedChannelWidthSet,
+               psessionEntry->htSupportedChannelWidthSet);
+
         pStaDs->htSupportedChannelWidthSet =
-            psessionEntry->htSupportedChannelWidthSet;
+            (htCaps->supportedChannelWidthSet <
+              psessionEntry->htSupportedChannelWidthSet) ?
+                 htCaps->supportedChannelWidthSet :
+                 psessionEntry->htSupportedChannelWidthSet;
+
+        limLog(pMac, LOG1, FL("pStaDs->htSupportedChannelWidthSet %x"),
+               pStaDs->htSupportedChannelWidthSet);
+
         pStaDs->htMIMOPSState = htCaps->mimoPowerSave ;
         pStaDs->htMaxAmsduLength =  htCaps->maximalAMSDUsize;
         pStaDs->htAMpduDensity =    htCaps->mpduDensity;
