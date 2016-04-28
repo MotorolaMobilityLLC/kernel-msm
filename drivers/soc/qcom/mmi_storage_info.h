98aa5aed1381ea183ef9634fb5790d1a6b042b9a
Hong-Mei Li <a21834@motorola.com>
2016-04-27 18:24:04 -0700

    soc:qcom: add storage info support
    1. Read and parse storage info from device tree;
    2. Append the storage info to apnic annotation as below:
       UFS: 32GB SAMSUNG KLUBG4G1CE-B0B1 FV=0700
    3. Export the infomation to /sys/storage:
        fw
        model
        size
        type
        vendor
    
    Change-Id: If80356359cf2e1e31ca831397db9a0e7b6548466
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/849685
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/soc/qcom/mmi_storage_info.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __ARCH_ARM_MACH_MSM_MMI_EMMC_INFO_H
+#define __ARCH_ARM_MACH_MSM_MMI_EMMC_INFO_H
+
+struct mmi_storage_info {
+	char type[16];	/* UFS or eMMC */
+	char size[16];	/* size in GB */
+	char card_manufacturer[32];
+	char product_name[32];	/* model ID */
+	char firmware_version[16];
+};
+
+#endif
