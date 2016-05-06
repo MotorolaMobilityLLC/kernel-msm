d8d150f4be8fff5dcde73d15beb3f43a86cd084d
Jared Suttles <jsuttles@motorola.com>
2016-05-06 13:15:26 -0500

    msm: mdss: Enable DSI panel notifications
    This patch adds panel notifier functionality to the MDSS dsi
    panel driver. This can be used to perform blocking operations
    at various times during panel on/off.
    
    Change-Id: Idf4a3c2817b1c518fee291ba76d9bb1d127f8cb0
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-on: http://gerrit.mot.com/853490
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,7 @@
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <linux/msm_mdp.h>
+#include <linux/panel_notifier.h>
 
 
 #include "mdss_dsi.h"
@@ -1091,6 +1092,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		goto end;
 	}
 
+	panel_notify(PANEL_EVENT_PRE_DISPLAY_ON, pinfo);
+
 	on_cmds = &ctrl->on_cmds;
 
 	if ((pinfo->mipi.dms_mode == DYNAMIC_MODE_SWITCH_IMMEDIATE) &&
@@ -1130,6 +1133,9 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		} else
 			panel_recovery_retry = 0;
 	}
+
+	panel_notify(PANEL_EVENT_DISPLAY_ON, pinfo);
+
 end:
 	if (pinfo->forced_tx_mode_ftr_enabled)
 		mdss_dsi_panel_forced_tx_mode_set(pinfo, true);
@@ -1243,6 +1249,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 			goto end;
 	}
 
+	panel_notify(PANEL_EVENT_PRE_DISPLAY_OFF, pinfo);
+
 	if (pinfo->forced_tx_mode_ftr_enabled)
 		mdss_dsi_panel_forced_tx_mode_set(pinfo, false);
 
@@ -1250,6 +1258,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
+
+	panel_notify(PANEL_EVENT_DISPLAY_OFF, pinfo);
 
 end:
 	/* clear idle state */
