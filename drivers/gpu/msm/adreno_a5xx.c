680108433dafefcbd6d0f7e8903ed1d6f76d26db
Hareesh Gundu <hareeshg@codeaurora.org>
2016-05-10 17:29:51 +0530

    msm: kgsl: Enable context done bug fix for A510
    In A5x, CCU can send context_done event of a particular
    context to UCHE which ultimately reaches CP even when
    there is valid transaction of that context inside CCU.
    This can let CP to program config registers, which will
    make the "valid transaction" inside CCU to be interpreted
    differently. This can cause gpu fault. This bug is fixed
    for A510 later revision, enable the bug fix.
    
    Change-Id: I2212f0a1fb528e3e1fc70db8120f44b61da5e419
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/adreno_a5xx.c
+++ drivers/gpu/msm/adreno_a5xx.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1439,6 +1439,22 @@ static void a5xx_start(struct adreno_device *adreno_dev)
 
 	/* Set the USE_RETENTION_FLOPS chicken bit */
 	kgsl_regwrite(device, A5XX_CP_CHICKEN_DBG, 0x02000000);
+
+	/*
+	 *  In A5x, CCU can send context_done event of a particular context to
+	 *  UCHE which ultimately reaches CP even when there is valid
+	 *  transaction of that context inside CCU. This can let CP to program
+	 *  config registers, which will make the "valid transaction" inside
+	 *  CCU to be interpreted differently. This can cause gpu fault. This
+	 *  bug is fixed in latest A510 revision. To enable this bug fix -
+	 *  bit[11] of RB_DBG_ECO_CNTL need to be set to 0, default is 1
+	 *  (disable). For older A510 version this bit is unused.
+	 */
+	if (adreno_is_a510(adreno_dev)) {
+		kgsl_regread(device, A5XX_RB_DBG_ECO_CNTL, &val);
+		val = (val & (~(1 << 11)));
+		kgsl_regwrite(device, A5XX_RB_DBG_ECO_CNTL, val);
+	}
 
 	/* Enable ISDB mode if requested */
 	if (test_bit(ADRENO_DEVICE_ISDB_ENABLED, &adreno_dev->priv)) {
