79105378c7860f956839d41a38c6696871ce4674
Chris Fries <cfries@motorola.com>
2016-05-10 11:33:50 -0500

    Revert "(CR) android:binder: enable preempt before alloc_fd"
    Fix tombstone issue
    
    Mot-CRs-fixed: (CR)
    
    This reverts commit 54837a968a7d3d12ccb23f4e1b51c01728a225fa.
    
    Change-Id: Ie1001e9af8f35266b5eb6eb30979e3f5c693b0df
    Reviewed-on: http://gerrit.mot.com/854649
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -375,7 +375,6 @@ static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 	struct files_struct *files = proc->files;
 	unsigned long rlim_cur;
 	unsigned long irqs;
-	int ret;
 
 	if (files == NULL)
 		return -ESRCH;
@@ -386,11 +385,7 @@ static int task_get_unused_fd_flags(struct binder_proc *proc, int flags)
 	rlim_cur = task_rlimit(proc->tsk, RLIMIT_NOFILE);
 	unlock_task_sighand(proc->tsk, &irqs);
 
-	preempt_enable_no_resched();
-	ret = __alloc_fd(files, 0, rlim_cur, flags);
-	preempt_disable();
-
-	return ret;
+	return __alloc_fd(files, 0, rlim_cur, flags);
 }
 
 /*
