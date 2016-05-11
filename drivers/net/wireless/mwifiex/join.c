e39a6c95f9c34e77bff55805c835b941e04edf0a
Mahesh A Saptasagar <c_msapta@qti.qualcomm.com>
2016-05-11 18:14:06 +0530

    cfg80211: properly send NL80211_ATTR_DISCONNECTED_BY_AP in disconnect
    When we disconnect from the AP, drivers call cfg80211_disconnect().
    This doesn't know whether the disconnection was initiated locally
    or by the AP though, which can cause problems with the supplicant,
    for example with WPS. This issue obviously doesn't show up with any
    mac80211 based driver since mac80211 doesn't call this function.
    
    Fix this by requiring drivers to indicate whether the disconnect is
    locally generated or not. I've tried to update the drivers, but may
    not have gotten the values correct, and some drivers may currently
    not be able to report correct values. In case of doubt I left it at
    false, which is the current behaviour.
    
    For libertas, make adjustments as indicated by Dan Williams.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: Ib6623397f31b30e4a884f922bc8e6922167c40aa
    Reported-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Tested-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: http://gerrit.mot.com/857921
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Ranjeet Gupta <ranjeetgupta@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/net/wireless/mwifiex/join.c
+++ drivers/net/wireless/mwifiex/join.c
@@ -1422,7 +1422,7 @@ int mwifiex_deauthenticate(struct mwifiex_private *priv, u8 *mac)
 	case NL80211_IFTYPE_P2P_CLIENT:
 		ret = mwifiex_deauthenticate_infra(priv, mac);
 		if (ret)
-			cfg80211_disconnected(priv->netdev, 0, NULL, 0,
+			cfg80211_disconnected(priv->netdev, 0, NULL, 0, true,
 					      GFP_KERNEL);
 		break;
 	case NL80211_IFTYPE_ADHOC:
