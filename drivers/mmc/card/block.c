84be9d556181ffe3e3816dd7f34541f4b26cb546
Shigeru Murai <sharp_android_SA@list.sharp.co.jp>
2016-05-12 21:24:00 +0900

    mmc: Add cc error detection for eve
    eMMC device which is used by eve sometimes return CC ERROR at status check command after any comannd is succeded.
    The memory vendor recommends retry at that time.

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -46,6 +46,7 @@
 #include <linux/mmc/host.h>
 #include <linux/mmc/mmc.h>
 #include <linux/mmc/sd.h>
+#include <soc/qcom/socinfo.h>
 
 #include <asm/uaccess.h>
 
@@ -1842,6 +1843,15 @@ static int mmc_blk_err_check(struct mmc_card *card,
 				       req->rq_disk->disk_name, __func__,
 				       status);
 				gen_err = 1;
+			}
+
+			if ((of_board_is_sharp_eve()) && (mmc_card_mmc(card))) {
+				if (status & R1_CC_ERROR) {
+					pr_err("%s: %s: general error sending status command(for CC), card status %#x\n",
+					       req->rq_disk->disk_name, __func__,
+					       status);
+					gen_err = 1;
+				}
 			}
 
 			/* Timeout if the device never becomes ready for data
