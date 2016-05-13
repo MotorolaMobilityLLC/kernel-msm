59c48ed3c43f89e32b5ccde8e93f05cad681cb0e
Yuanyuan Zhong <zyy@motorola.com>
2016-05-13 14:49:49 -0500

    coresight: etmv4: double etm dump buffer size
    ETM driver uses the reg size from device tree to allocate buffer and
    register the buffer to dump table. The reg size is 4096.
    ETM dumping is supposed to write 1024*4 bytes to the buffer. But
    actually 1024*8 bytes are written.
    Temporarily double the buffer size.
    
    Change-Id: I38e24874e9e70244f983f517c548791108bb620a
    Signed-off-by: Yuanyuan Zhong <zyy@motorola.com>
    Reviewed-on: http://gerrit.mot.com/856430
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/coresight/coresight-etmv4.c
+++ drivers/coresight/coresight-etmv4.c
@@ -3356,10 +3356,10 @@ static int etm_late_init(struct etm_drvdata *drvdata)
 
 	etm_init_default_data(drvdata);
 
-	baddr = devm_kzalloc(dev, drvdata->reg_size, GFP_KERNEL);
+	baddr = devm_kzalloc(dev, drvdata->reg_size * 2, GFP_KERNEL);
 	if (baddr) {
 		drvdata->reg_data.addr = virt_to_phys(baddr);
-		drvdata->reg_data.len = drvdata->reg_size;
+		drvdata->reg_data.len = drvdata->reg_size * 2;
 		dump_entry.id = MSM_DUMP_DATA_ETM_REG + drvdata->cpu;
 		dump_entry.addr = virt_to_phys(&drvdata->reg_data);
 		ret = msm_dump_data_register(MSM_DUMP_TABLE_APPS,
