b91a423537da2d94fbe903e2a90adcda2fa8d6c3
Patrick Auchter <auchter@motorola.com>
2016-05-17 16:50:45 -0700

    msm: mdss: fix possible overflow errors in panel_debug_base_reg_read
    The panel_reg_buf is a dynamically allocated buffer of size reg_buf_len
    so checking sizeof(panel_reg_buf) is incorrect.  Using scnprintf will
    ensure that len is at most reg_buf_len - 1 after all the prints.  This
    allows sanity checks to be removed which were incorrectly skipping
    clock disable, resulting in an extra clock reference count.
    
    Change-Id: Ic3bb685c7b83eefef7bc207ad93d6a2a9e36fd33
    Signed-off-by: Patrick Auchter <auchter@motorola.com>
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/msm/mdss/mdss_debug.c
+++ drivers/video/msm/mdss/mdss_debug.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -226,21 +226,20 @@ static ssize_t panel_debug_base_reg_read(struct file *file,
 	mdss_dsi_panel_cmd_read(ctrl_pdata, panel_reg[0], panel_reg[1],
 				NULL, rx_buf, dbg->cnt);
 
-	len = snprintf(panel_reg_buf, reg_buf_len, "0x%02zx: ", dbg->off);
-	if (len < 0 || len >= sizeof(panel_reg_buf))
-		goto read_reg_fail;
+	len = scnprintf(panel_reg_buf, reg_buf_len, "0x%02zx: ", dbg->off);
 
 	for (i = 0; (len < reg_buf_len) && (i < ctrl_pdata->rx_len); i++)
 		len += scnprintf(panel_reg_buf + len, reg_buf_len - len,
 				"0x%02x ", rx_buf[i]);
 
-	panel_reg_buf[len - 1] = '\n';
+	if (len)
+		panel_reg_buf[len - 1] = '\n';
 
 	if (mdata->debug_inf.debug_enable_clock)
 		mdata->debug_inf.debug_enable_clock(0);
 
-	if ((count < sizeof(panel_reg_buf))
-			|| copy_to_user(user_buf, panel_reg_buf, len))
+	if ((count < reg_buf_len)
+			|| (copy_to_user(user_buf, panel_reg_buf, len)))
 		goto read_reg_fail;
 
 	kfree(rx_buf);
