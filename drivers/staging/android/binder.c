fbe28df491ee2bc80bc0eeb5b4c24bc688448446
Yi-wei Zhao <gbjc64@motorola.com>
2016-05-20 17:35:17 +0800

    binder: add barrier before enable preemption
    "get_user" & "put_user" have a short piece of instructions, and without
    the barrier compiler will optimize (i.e. delete) the "enable preemption".
    
    Change-Id: Ice55a28ca0e38ea88127eb6dc35bf99a877bbbc7
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/857889
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -476,6 +476,7 @@ static inline long copy_from_user_preempt_disabled(void *to, const void __user *
 ({						\
 	int __ret;				\
 	preempt_enable_no_resched();		\
+	barrier();				\
 	__ret = get_user(x, ptr);		\
 	preempt_disable();			\
 	__ret;					\
@@ -485,6 +486,7 @@ static inline long copy_from_user_preempt_disabled(void *to, const void __user *
 ({						\
 	int __ret;				\
 	preempt_enable_no_resched();		\
+	barrier();				\
 	__ret = put_user(x, ptr);		\
 	preempt_disable();			\
 	__ret;					\
