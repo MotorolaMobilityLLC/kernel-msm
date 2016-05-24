bdc23dfcc54aff66e69c1ac38f9eb8bf144d6cec
Konstantin Makariev <kmakariev@motorola.com>
2016-05-24 16:24:18 -0500

    IKSWM-40868 input: synaptics_dsx_i2c: pm qos latency
    Register touch IRQ to PM QOS service to enforce scheduling
    latency.
    
    Change-Id: I3b0abff98d449a4851cb45bd6f402dbff7629508
    Signed-off-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-on: http://gerrit.mot.com/860462
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -50,6 +50,10 @@
 #define INPUT_PHYS_NAME "synaptics_dsx_i2c/input0"
 #define TYPE_B_PROTOCOL
 
+#define IRQ_DISPATCH_LATENCY	5000
+
+static int latency_in_effect = IRQ_DISPATCH_LATENCY;
+
 #define RMI4_WAIT_READY 0
 #define RMI4_HW_RESET 1
 #define RMI4_SW_RESET 2
@@ -2090,6 +2094,12 @@ static ssize_t synaptics_rmi4_drv_irq_show(struct device *dev,
 static ssize_t synaptics_rmi4_drv_irq_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count);
 
+static ssize_t synaptics_rmi4_pm_opt_show(struct device *dev,
+		struct device_attribute *attr, char *buf);
+
+static ssize_t synaptics_rmi4_pm_opt_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count);
+
 static ssize_t synaptics_rmi4_hw_irqstat_show(struct device *dev,
 		struct device_attribute *attr, char *buf);
 
@@ -2290,6 +2300,9 @@ static struct device_attribute attrs[] = {
 	__ATTR(tsi, S_IRUSR | S_IRGRP,
 			synaptics_rmi4_ud_show,
 			synaptics_rmi4_store_error),
+	__ATTR(pm_opt, (S_IRUSR | S_IRGRP | S_IWUSR | S_IWGRP),
+			synaptics_rmi4_pm_opt_show,
+			synaptics_rmi4_pm_opt_store),
 };
 
 struct synaptics_exp_fn_ctrl {
@@ -3395,6 +3408,38 @@ static ssize_t synaptics_rmi4_drv_irq_store(struct device *dev,
 	case 1:
 		/* Enable irq */
 		synaptics_rmi4_irq_enable(rmi4_data, true);
+		break;
+	default:
+		pr_err("Invalid value\n");
+		return -EINVAL;
+	}
+	return count;
+}
+
+static ssize_t synaptics_rmi4_pm_opt_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%d\n", latency_in_effect);
+}
+
+static ssize_t synaptics_rmi4_pm_opt_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	unsigned long value = 0;
+	int err = 0;
+
+	err = kstrtoul(buf, 10, &value);
+	if (err < 0) {
+		pr_err("Failed to convert value\n");
+		return -EINVAL;
+	}
+
+	switch (value) {
+	case 0:
+		latency_in_effect = PM_QOS_DEFAULT_VALUE;
+		break;
+	case 1:
+		latency_in_effect = IRQ_DISPATCH_LATENCY;
 		break;
 	default:
 		pr_err("Invalid value\n");
@@ -7109,6 +7154,10 @@ static int synaptics_rmi4_probe(struct i2c_client *client,
 			rmi4_data->is_fps_registered = true;
 	}
 
+	rmi4_data->pm_qos_irq.type = PM_QOS_REQ_AFFINE_IRQ;
+	rmi4_data->pm_qos_irq.irq = rmi4_data->irq;
+	pm_qos_add_request(&rmi4_data->pm_qos_irq, PM_QOS_CPU_DMA_LATENCY,
+			latency_in_effect);
 	return retval;
 
 err_sysfs:
@@ -7211,6 +7260,8 @@ static int synaptics_rmi4_remove(struct i2c_client *client)
 
 	synaptics_dsx_sysfs_touchscreen(rmi4_data, false);
 	synaptics_rmi4_cleanup(rmi4_data);
+
+	pm_qos_remove_request(&rmi4_data->pm_qos_irq);
 
 #ifdef CONFIG_MMI_HALL_NOTIFICATIONS
 	if (rmi4_data->folio_detection_enabled)
@@ -7487,6 +7538,8 @@ static int synaptics_rmi4_suspend(struct device *dev)
 		rmi4_data->ic_on = false;
 	}
 
+	pm_qos_update_request(&rmi4_data->pm_qos_irq, PM_QOS_DEFAULT_VALUE);
+
 	return 0;
 }
 
@@ -7512,6 +7565,9 @@ static int synaptics_rmi4_resume(struct device *dev)
 
 	if (atomic_cmpxchg(&rmi4_data->touch_stopped, 1, 0) == 0)
 		return 0;
+
+	pm_qos_update_request(&rmi4_data->pm_qos_irq, latency_in_effect);
+	pr_debug("set pm_qos latency %d\n", latency_in_effect);
 
 	synaptics_dsx_resumeinfo_start(rmi4_data);
 
