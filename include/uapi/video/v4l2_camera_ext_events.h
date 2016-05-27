7e27072c1d458cf0533b09e2a1c0a3cfb4523980
Long Ling <a21040@motorola.com>
2016-05-27 17:17:25 -0500

    camera_ext: add v4l2 events definition
    Add definition for error message from Mod.
    Add event struct shared with user space.
    
    Change-Id: I23ef126b01f5e0746c1ef732995fa1ffa56c6d86
    CRs-fixed: (CR)
    Signed-off-by: Long Ling <a21040@motorola.com>
    Reviewed-on: http://gerrit.mot.com/861958
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>
    Reviewed-by: Sridhar Vashist <svashist@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/uapi/video/v4l2_camera_ext_events.h
+++ include/uapi/video/v4l2_camera_ext_events.h
@@ -15,21 +15,19 @@
 #define __V4L2_CAMERA_EXT_EVENTS_H
 
 #include <linux/videodev2.h>
+#include "v4l2_camera_ext_defs.h"
 
 #define V4L2_CAMERA_EXT_EVENT_TYPE (V4L2_EVENT_PRIVATE_START + 0x00001000)
-
-/* camera_ext v4l2 event type to subscribe */
-#define V4L2_CAMERA_EXT_EVENT_ERROR 1
 
 /* Event object is passed to user space in v4l2_event data field which is
  * char[64]. So the object's size should not exceed 64 bytes.
  */
 
-#define V4L2_CAMERA_EXT_EVENT_ERROR_DESC_LEN 60
-struct v4l2_camera_ext_event_error {
-	int code;
-	/* Description mainly for debug purpose */
-	char desc[V4L2_CAMERA_EXT_EVENT_ERROR_DESC_LEN];
+struct v4l2_camera_ext_event {
+	int type; /* CAMERA_EXT_REPORT_XXX */
+	union {
+		uint32_t error_code;
+	};
 };
 
 #endif /* __V4L2_CAMERA_EXT_EVENTS_H */
