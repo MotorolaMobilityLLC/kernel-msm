cf491449288a7ef7dcd0bac07d3834463f7792f4
Lianwei Wang <a22439@motorola.com>
2016-05-31 19:44:54 -0500

    mmc: don't wait in pm notifier during card detection
    If the kernel is busy in sending mmc request, then the mmc card
    detection work will wait for the host lock for a long time. In
    such situation the pm notifier will wait a long time to flush the
    card detection work, which may trigger kernel panic.
    
    Give another 2s for others to release the host, and return
    -EAGAIN to abort the suspend process immediately to avoid kernel
    panic.
    
    Change-Id: I89806ba2e33b9cfe239911a0d9967678b7f2cc2a
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1025787
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -365,6 +365,7 @@ struct mmc_host {
 	u32			ocr_avail_sd;	/* SD-specific OCR */
 	u32			ocr_avail_mmc;	/* MMC-specific OCR */
 	struct notifier_block	pm_notify;
+	struct wakeup_source	pm_ws;
 	u32			max_current_330;
 	u32			max_current_300;
 	u32			max_current_180;
