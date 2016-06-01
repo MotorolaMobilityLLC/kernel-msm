f8627f0dd3d6a4c6b439b360bd9ce7050c400872
Lianwei Wang <a22439@motorola.com>
2016-05-31 19:44:54 -0500

    mmc: don't wait in pm notifier during card detection
    Rework on and refer to below patches:
      8265d212abcb "mmc: hold a wakeup source during card detection"
      ae99ac9b4f26 "mmc: Don't wait in pm PREPARE notifier during card detection"
    
    If the kernel is busy in sending mmc request, then the mmc card
    detection work will wait for the host lock for a long time. In
    such situation the pm notifier will wait a long time to flush the
    card detection work, which may trigger kernel panic.
    
    Give another 2s for others to release the host, and return
    -EAGAIN to abort the suspend process immediately to avoid kernel
    panic.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I89806ba2e33b9cfe239911a0d9967678b7f2cc2a
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/862831
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -523,6 +523,7 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	init_waitqueue_head(&host->wq);
 	INIT_DELAYED_WORK(&host->detect, mmc_rescan);
 #ifdef CONFIG_PM
+	wakeup_source_init(&host->pm_ws, dev_name(&host->class_dev));
 	host->pm_notify.notifier_call = mmc_pm_notify;
 #endif
 
@@ -848,6 +849,8 @@ void mmc_free_host(struct mmc_host *host)
 	spin_lock(&mmc_host_lock);
 	idr_remove(&mmc_host_idr, host->index);
 	spin_unlock(&mmc_host_lock);
+
+	wakeup_source_trash(&host->pm_ws);
 
 	put_device(&host->class_dev);
 }
