402beeeca50d7ec91588d2f3268137ce4d498b73
zonghua liu <a17671@motorola.com>
2016-06-06 15:12:02 -0500

    misc:fusb302:moto changes after 3.3.2
    Add moto features back
    9V PD charger support
    Nitro support
    Pass lecroy compliance test suite
    Fix audio accessory break of r3.3.2
    Fix VCON and PDWN conflict of r3.3.2
    
    Change-Id: Ieecae166deca12454d3ef92e363f272d9791fac0
    Signed-off-by: zonghua liu <a17671@motorola.com>
    
    Conflicts:
    	drivers/misc/fusb302/core/TypeC.c
    Change-Id: Ie260ec8af5d847b92b04d1fc718a8cc9dff490c6
    Reviewed-on: http://gerrit.mot.com/865005
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/misc/fusb302/core/core.h
+++ drivers/misc/fusb302/core/core.h
@@ -14,7 +14,7 @@ void core_initialize(void);
 void core_state_machine(void);
 void core_tick(void);
 void core_enable_typec(FSC_BOOL enable);
-
+void core_state_machine_imp(void);
 void core_set_vbus_transition_time(FSC_U32 time_ms);
 
 #ifdef FSC_DEBUG
@@ -30,7 +30,8 @@ void core_get_sink_caps(FSC_U8 * buf);
 void core_set_sink_req(FSC_U8 * buf);
 void core_get_sink_req(FSC_U8 * buf);
 void core_send_hard_reset(void);
-
+void core_send_sink_request(void);
+void core_send_sink_request_voltage(void);
 void core_process_pd_buffer_read(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_process_typec_pd_status(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_process_typec_pd_control(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
@@ -38,7 +39,8 @@ void core_process_local_register_request(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_process_set_typec_state(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_process_read_typec_state_log(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_process_read_pd_state_log(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
-
+void core_process_send_dr_swap(void);
+void core_process_send_vdm(void);
 void core_set_alternate_modes(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 void core_set_manual_retries(FSC_U8 * InBuffer, FSC_U8 * OutBuffer);
 FSC_U8 core_get_alternate_modes(void);
