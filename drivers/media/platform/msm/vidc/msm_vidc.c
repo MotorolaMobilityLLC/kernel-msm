8451c8e5da81409d6aad453dfb313734a130e512
Praveen Chavan <pchavan@codeaurora.org>
2016-06-07 10:08:44 -0700

    msm: vidc: update the check to ignore extradata plane in dqbuf
    userptr may or may not be a null-value. Checking for this field
    to qualify a plane as extradata has no merit. Ignore that check
    
    Bug: 28747768
    Change-Id: I08d85ef462f09003aaa17f6ebc5d27de7083796f
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -983,8 +983,7 @@ int msm_vidc_dqbuf(void *instance, struct v4l2_buffer *b)
 		if (!inst->map_output_buffer)
 			continue;
 		if (EXTRADATA_IDX(b->length) &&
-			(i == EXTRADATA_IDX(b->length)) &&
-			!b->m.planes[i].m.userptr) {
+			i == EXTRADATA_IDX(b->length)) {
 			continue;
 		}
 		buffer_info = device_to_uvaddr(inst,
