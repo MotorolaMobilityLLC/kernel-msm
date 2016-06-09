92799c820a972b04722a87c95c12b3ac304dd417
Lianwei Wang <a22439@motorola.com>
2016-06-09 16:37:05 -0500

    mmc: support gpio cd wakeup
    Support wakeup from mmc card detect gpio irq and don't start
    mmc detect work if the gpio slot is configured wakeup-able.
    
    The mmc detect work will be scheduled by the card detect irq.
    
    Change-Id: Ib90f91b897f03c15e62966d6df7461f85361a023
    Reviewed-on: https://gerrit.mot.com/1175356
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/core/slot-gpio.c
+++ drivers/mmc/core/slot-gpio.c
@@ -40,6 +40,9 @@ static irqreturn_t mmc_gpio_cd_irqt(int irq, void *dev_id)
 	pr_debug("%s: cd gpio irq, gpio state %d (CARD_%s)\n",
 		mmc_hostname(host), present, present?"INSERT":"REMOVAL");
 
+	host->slot.cd_status = mmc_gpio_get_cd(host);
+	pr_info("%s: mmc cd gpio status: %d\n", __func__, host->slot.cd_status);
+
 	host->trigger_card_event = true;
 	mmc_detect_change(host, msecs_to_jiffies(200));
 
