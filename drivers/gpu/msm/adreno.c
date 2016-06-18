774b159b2e0c790dba186f3786b6459166c063e8
Ravi Chebolu <arc095@motorola.com>
2016-06-18 12:02:21 -0500

    clk/msm: Override GPU speed bin through kernel config option
    Add kernel config GPU_SPEED_BIN_OVERRIDE to override GPU speed bin.
    
    Change-Id: Iab5f2a502a2ac7c4f7f1983f142a5ce1e3b9ce71
    Signed-off-by: Ravi Chebolu <arc095@motorola.com>
    Reviewed-on: http://gerrit.mot.com/868372
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -806,10 +806,20 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 		struct device_node *parent)
 {
 	struct device_node *node, *child;
+	unsigned int speed_bin;
+	int gpu_speed_bin_override = CONFIG_GPU_SPEED_BIN_OVERRIDE;
 
 	node = of_find_node_by_name(parent, "qcom,gpu-pwrlevel-bins");
 	if (node == NULL)
 		return adreno_of_get_legacy_pwrlevels(adreno_dev, parent);
+
+	if (gpu_speed_bin_override) {
+		speed_bin = gpu_speed_bin_override;
+		pr_info("%s: 8996-lite GPU speed bin %d. Override to %d\n",
+			__func__, adreno_dev->speed_bin,
+			gpu_speed_bin_override);
+	} else
+		speed_bin = adreno_dev->speed_bin;
 
 	for_each_child_of_node(node, child) {
 		unsigned int bin;
@@ -817,7 +827,7 @@ static int adreno_of_get_pwrlevels(struct adreno_device *adreno_dev,
 		if (of_property_read_u32(child, "qcom,speed-bin", &bin))
 			continue;
 
-		if (bin == adreno_dev->speed_bin) {
+		if (bin == speed_bin) {
 			int ret;
 
 			ret = adreno_of_parse_pwrlevels(adreno_dev, child);
