tuxology/OpenCSD

perf symbols: Check before overwriting build_id
Added check to see if has_build_id is set before overwriting build_id.

Signed-off-by: Tor Jeremiassen <tor@ti.com>

tools/perf/util/symbol.c
@@ -1466,7 +1466,8 @@ int dso__load(struct dso *dso, struct map *map, symbol_filter_t filter)
 	 * Read the build id if possible. This is required for
 	 * DSO_BINARY_TYPE__BUILDID_DEBUGINFO to work
 	 */
-	if (filename__read_build_id(dso->long_name, build_id, BUILD_ID_SIZE) > 0)
+	if ((!dso->has_build_id) && 
+            (filename__read_build_id(dso->long_name, build_id, BUILD_ID_SIZE) > 0))
 		dso__set_build_id(dso, build_id);
 
 	/*

MotorolaMobilityLLC/kernel-msm

tools/perf/util/symbol.c
@@ -1465,7 +1465,8 @@ int dso__load(struct dso *dso, struct map *map, symbol_filter_t filter)
 	 * Read the build id if possible. This is required for
 	 * DSO_BINARY_TYPE__BUILDID_DEBUGINFO to work
 	 */
-	if (filename__read_build_id(dso->name, build_id, BUILD_ID_SIZE) > 0)
+	if ((!dso->has_build_id) &&
+	    (filename__read_build_id(dso->name, build_id, BUILD_ID_SIZE) > 0))
 		dso__set_build_id(dso, build_id);
 
 	/*