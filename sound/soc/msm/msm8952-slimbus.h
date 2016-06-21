2150ec6c6a1c7990a401686c8be3889133dc9dd1
Rajasekhar Goparaju <chb764@motorola.com>
2016-06-21 14:24:04 -0500

    ASoC: msm8953: add high-res mod support
    On Addison, mod uses QUAT_MI2S. Add high-res support for
    QUAT_MI2S channel
    
    PROPAGATED_from:
    2cc3819 (CR): add missing 32khz and 192khz rates
    75e60ec (CR): add missing channels selection mixer control
    499290f (CR): expand supported rates and pcm fmts
    5fb3495 (CR):finish support fortert_mi2s backend
    
    Change-Id: I6106d81c46f9490c23a851f5c7011906352b3b86
    Signed-off-by: Rajasekhar Goparaju <chb764@motorola.com>
    Reviewed-on: http://gerrit.mot.com/869733
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Prabhu Annabathula <hnbv46@motorola.com>
    Reviewed-by: Jonathan Eklund <jeklund@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/msm8952-slimbus.h
+++ sound/soc/msm/msm8952-slimbus.h
@@ -52,6 +52,8 @@ int msm8952_slimbus_2_hw_params(struct snd_pcm_substream *substream,
 					  struct snd_pcm_hw_params *params);
 int msm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params);
+int msm_quat_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+				struct snd_pcm_hw_params *params);
 int msm_quin_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params);
 int msm_btsco_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
@@ -68,6 +70,8 @@ int marley_dai_init(struct snd_soc_pcm_runtime *rtd);
 int marley_cs35l34_dai_init(struct snd_soc_pcm_runtime *rtd);
 #endif
 int msm_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
+			     struct snd_pcm_hw_params *params);
+int msm_quat_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params);
 int msm_snd_cpe_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params);
