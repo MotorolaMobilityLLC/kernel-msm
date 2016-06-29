d2d9903f3fd5b0bdf8ae7071281a29f9c1440b70
Patrick Auchter <auchter@motorola.com>
2016-06-29 12:18:52 -0500

    video: slimport: Add function to reset to standby
    This can be used to reset the chip to standby if the cable detect gpio
    was tripped incorrectly putting the chip into a bad state.
    
    Change-Id: Ia264b51c7d1647d0379e243ec36123ef8b86ee0f
    Signed-off-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-on: http://gerrit.mot.com/871866
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/slimport/anx7816/slimport.c
+++ drivers/video/slimport/anx7816/slimport.c
@@ -1444,6 +1444,41 @@ static int anx7816_parse_dt(struct device *dev,
 }
 #endif
 
+int slimport_reset_standby(void)
+{
+	int ret = 0;
+	struct anx7816_data *anx7816;
+
+	if (!g_data) {
+		pr_err("%s: g_data is not set\n", __func__);
+		return -ENODEV;
+	}
+
+	anx7816 = g_data;
+
+	if (atomic_read(&anx7816->slimport_connected)) {
+		pr_err("%s: connected, not resetting!\n", __func__);
+		ret = -EBUSY;
+	} else if (gpio_get_value(anx7816->pdata->gpio_cbl_det) ==
+			DONGLE_CABLE_INSERT) {
+		pr_warn("%s: in cable detected state, resetting!\n", __func__);
+		sp_tx_hardware_poweron();
+		sp_tx_hardware_powerdown();
+
+		usleep_range(250000, 250001);
+
+		if (gpio_get_value(anx7816->pdata->gpio_cbl_det) ==
+			DONGLE_CABLE_INSERT) {
+			pr_err("%s: cable detected after reset!\n", __func__);
+			ret = -EAGAIN;
+		}
+	} else {
+		pr_debug("%s: already in standby, not resetting!\n", __func__);
+	}
+
+	return ret;
+}
+
 #ifdef MML_DYNAMIC_IRQ_SUPPORT
 static int anx7816_enable_irq(int enable)
 {
