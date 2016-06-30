86683e838b667beff83433ad96d1bb5047e3335f
Lianwei Wang <a22439@motorola.com>
2016-06-30 16:23:07 -0500

    fs: esdfs: fix race in esdfs unlink
    There is a ABBA deadlock between esdfs_unlink and esdfs_lookup.
    
         esdfs_unlink                   esdfs_lookup
    
      -lock_parent(lower_dentry);     -vfs_lookup
        |-esdfs_drop_shared_icache      |-lock_parent(dentry);
          ....                            ....
    
          |-lock_parent(dentry);        |-vfs_lookup
                                           |-lock_parent(lower_dentry);
    
    The esdfs_drop_shared_icache function only remove the shared
    dcache object before unlink the lower inode. It does not touch
    the lower inode so we don't need to hold the lower inode to do
    it.
    
    Move the lock_parent(lower_dentry) after esdfs_drop_shared_icache
    function to solve the ABBA deadlock issue.
    
    Mot-CRs-fixed: (CR)
    Change-Id: I7e9e76874282b8c37828d1c0419f686c965f38eb
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/872637
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Reviewed-by: Sheng-Zhe Zhao <a18689@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/esdfs/inode.c
+++ fs/esdfs/inode.c
@@ -84,10 +84,10 @@ static int esdfs_unlink(struct inode *dir, struct dentry *dentry)
 	esdfs_get_lower_path(dentry, &lower_path);
 	lower_dentry = lower_path.dentry;
 	dget(lower_dentry);
-	lower_dir_dentry = lock_parent(lower_dentry);
 
 	esdfs_drop_shared_icache(dir->i_sb, lower_dentry->d_inode);
 
+	lower_dir_dentry = lock_parent(lower_dentry);
 	err = vfs_unlink(lower_dir_inode, lower_dentry, NULL);
 
 	/*
