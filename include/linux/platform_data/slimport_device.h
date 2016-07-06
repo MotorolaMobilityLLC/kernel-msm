1da24c09f9de53af1b32af703137833100f44a2a
Fred Fettinger <fettinge@motorola.com>
2016-07-06 11:59:31 -0500

    drivers: video: msm: hdmi: limit resolution by RX bandwidth
    The resolution should be limited by the maximum clock rate supported by
    the receiver. But it doesn't make sense to replace the unsupported
    resolution with a resolution requiring a lower clock rate, as the EDID
    already lists all supported timings, and the replacement resolutions
    are frequently strange aspect ratios. Instead, check the required clock
    rate of each resolution listed in the EDID and drop any where the rate
    is higher than the receiver's max clock.
    
    Change-Id: I5d7cb1fc2761902f1e5367396f29e8fa2ae8960d
    Signed-off-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-on: http://gerrit.mot.com/871097
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/platform_data/slimport_device.h
+++ include/linux/platform_data/slimport_device.h
@@ -14,7 +14,7 @@
 
 #ifndef SLIMPORT_DEVICE
 #define SLIMPORT_DEVICE
-unchar sp_get_rx_bw(void);
+u32 sp_get_rx_bw_khz(void);
 int slimport_read_edid_block(int block, uint8_t *edid_buf);
 int slimport_reset_standby(void);
 #endif /* SLIMPORT_DEVICE */
