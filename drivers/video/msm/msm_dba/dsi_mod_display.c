93b2571cc91071cfe334656427d0c55e4d9e2051
Bang Nguyen <bangnguyen@motorola.com>
2016-07-07 08:11:35 -0500

    Revert "msm: msm_dba: Add QOS workaround to mod_display"
    Since the real fix was pushed under http://gerrit.mot.com/872634,
    therefore this change is nolonger needed.
    
    This reverts commit 88765ae8f1ddca6af0893a037ca84ea9b01492d5.
    
    Change-Id: Ic51499d1566b64ce0877ba9c03e63bbf0844d8f0
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: http://gerrit.mot.com/874350
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Michael Corrigan <michael.corrigan@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/msm_dba/dsi_mod_display.c
+++ drivers/video/msm/msm_dba/dsi_mod_display.c
@@ -23,7 +23,6 @@
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/wakelock.h>
-#include <linux/pm_qos.h>
 #include "video/msm_hdmi_modes.h"
 #include "msm_dba_internal.h"
 #include "../mdss/mdss_dsi.h"
@@ -47,8 +46,6 @@ struct dsi_mod_display {
 	struct notifier_block panel_nb;
 	struct mutex ops_mutex;
 	struct wake_lock mod_disp_lock;
-	int qos_latency;
-	struct pm_qos_request mod_disp_pm_qos_request;
 
 	int dsi_connect;
 };
@@ -368,11 +365,6 @@ static int dsi_mod_display_parse_dt(struct platform_device *pdev,
 	} else
 		pdata->dsi_connect = temp_val;
 
-	if (of_property_read_u32(pdev->dev.of_node, "mml,qos_latency",
-							&pdata->qos_latency))
-		pdata->qos_latency = -1;
-	pr_debug("%s: qos_latency=%d\n", __func__, pdata->qos_latency);
-
 	return ret;
 }
 
@@ -443,10 +435,6 @@ static int dsi_mod_display_handle_connect(void *data)
 	reinit_completion(&pdata->apba_on_wait);
 	wake_lock(&pdata->mod_disp_lock);
 
-	if (pdata->qos_latency >= 0)
-		pm_qos_add_request(&pdata->mod_disp_pm_qos_request,
-			PM_QOS_CPU_DMA_LATENCY, pdata->qos_latency);
-
 	pdata->connecting = true;
 	dsi_mod_display_do_hotplug(pdata, 1);
 	if (wait_for_completion_timeout(&pdata->dsi_on_wait,
@@ -471,8 +459,6 @@ static int dsi_mod_display_handle_connect(void *data)
 		kfree(pdata->display_config);
 		pdata->display_config = NULL;
 		wake_unlock(&pdata->mod_disp_lock);
-		if (pdata->qos_latency >= 0)
-			pm_qos_remove_request(&pdata->mod_disp_pm_qos_request);
 	}
 
 exit:
@@ -500,9 +486,6 @@ static int dsi_mod_display_handle_disconnect(void *data)
 	pdata->edid_buf = NULL;
 	kfree(pdata->display_config);
 	pdata->display_config = NULL;
-
-	if (pdata->qos_latency >= 0)
-		pm_qos_remove_request(&pdata->mod_disp_pm_qos_request);
 
 	pr_debug("%s-\n", __func__);
 
