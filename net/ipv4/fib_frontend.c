0bf708a5a50349b2aa1598d8bd1ceb40bc958a70
Hong-Mei Li <a21834@motorola.com>
2016-07-11 10:04:18 -0700

    ipv4: accept local address for icmp
    On some condition, the ICMP error message may contain same src
    and dst address. Since kernel would drop the packets with local
    address(ACCEPT_LOCAL is not set on the device), so that the ICMP
    packets were finally dropped in IP layer without really handling
    the error. Example, in case the ICMP is for MTU handshake, the
    talk would fianlly be ignored and failed.
    
    Fix the issue to have this kind of local packet got deliver to up
    layer.
    
    Change-Id: Icc9bc5ca05ece7435dfd60eda9794688a2941971
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Reviewed-on: http://gerrit.mot.com/875403
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- net/ipv4/fib_frontend.c
+++ net/ipv4/fib_frontend.c
@@ -322,7 +322,6 @@ int fib_validate_source(struct sk_buff *skb, __be32 src, __be32 dst,
 	int r = secpath_exists(skb) ? 0 : IN_DEV_RPFILTER(idev);
 
 	if (!r && !fib_num_tclassid_users(dev_net(dev)) &&
-	    IN_DEV_ACCEPT_LOCAL(idev) &&
 	    (dev->ifindex != oif || !IN_DEV_TX_REDIRECTS(idev))) {
 		*itag = 0;
 		return 0;
