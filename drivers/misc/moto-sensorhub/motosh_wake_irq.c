61c1b24c409bec6267810075fcace20059296eb6
Robert Wiegand <qa2113@motorola.com>
2016-07-12 12:47:46 -0500

    drivers/misc: Add the Motion/Stationary sensors
    Add the Motion/Stationary sensor reporting to the sensor hub
    kernel driver. These sensors are new in the Android N release.
    
    Change-Id: I3cf54fa8b2aa355fdbd0a0c0f9eb48a08cbf9ee7
    Signed-off-by: Robert Wiegand <qa2113@motorola.com>
    Reviewed-on: http://gerrit.mot.com/875936
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Daniel Moore <dtmoore@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Russell Knize <rknize@motorola.com>

--- drivers/misc/moto-sensorhub/motosh_wake_irq.c
+++ drivers/misc/moto-sensorhub/motosh_wake_irq.c
@@ -226,6 +226,16 @@ void motosh_irq_wake_thread_func(struct kthread_work *work)
 			goto EXIT;
 	}
 #endif /* CONFIG_SENSORS_MOTOSH_MOTODISP */
+	if (irq_status & M_MOTION_DETECT) {
+		motosh_as_data_buffer_write(ps_motosh, DT_MOTION_DETECT,
+				NULL, 0, 0, false);
+		dev_dbg(&ps_motosh->client->dev, "Motion Detect");
+	}
+	if (irq_status & M_STATIONARY_DETECT) {
+		motosh_as_data_buffer_write(ps_motosh, DT_STATIONARY_DETECT,
+				NULL, 0, 0, false);
+		dev_dbg(&ps_motosh->client->dev, "Stationary Detect");
+	}
 
 	/* read wake queue length */
 	cmdbuff[0] = WAKE_MSG_QUEUE_LEN;
