cef4f176af2032f12254bc388ddcbfc46eaf47a5
engstk <eng.stk@sapo.pt>
2016-07-13 12:03:29 +0100

    Soc: msm: qdsp6v2: Fix improper param checks
    The commit "5720ed5 Soc: msm: qdsp6v2: Fix invalid params handling"
    introduced improper checks.
    
    Proper codeaurora patch
    https://source.codeaurora.org/quic/la/kernel/msm/commit/?h=LA.BF.1.1.3_rb1.12&id=6eb6d1817c8be8eecda0dbb096c58100a36a44af
    instead of
    https://source.codeaurora.org/quic/la/kernel/msm/patch/?id=5720ed5c3a786e3ba0a2428ac45da5d7ec996b4e
    
    Bug: 30101076
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Change-Id: Ie515af356d97dea118713887a57bca352fee2a75
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- arch/arm/mach-msm/qdsp6v2/msm_audio_ion.c
+++ arch/arm/mach-msm/qdsp6v2/msm_audio_ion.c
@@ -116,8 +116,7 @@ int msm_audio_ion_import(const char *name, struct ion_client **client,
 			ion_phys_addr_t *paddr, size_t *pa_len, void **vaddr)
 {
 	int rc = 0;
-	if (!name || !client || !handle || !ionflag || !bufsz || !paddr
-		|| !pa_len || !vaddr) {
+	if (!name || !client || !handle || !paddr || !vaddr || !pa_len) {
 		pr_err("%s: Invalid params\n", __func__);
 		rc = -EINVAL;
 		goto err;
