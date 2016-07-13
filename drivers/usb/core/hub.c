b26dd6cd63ba7d917f2b2d03846d697bdcf1495d
Choon Lee Seah <andyseah@motorola.com>
2016-07-13 17:33:01 -0500

    Revert "USB: fix invalid memory access in hub_activate()"
    To fix build breakage
    
    This reverts commit ee0e402c56a3fd6de99668322197436aa1f225ab.
    
    Change-Id: I2bb52ed888822e7a44072548b82f89b8054faa4d
    Reviewed-on: http://gerrit.mot.com/876656
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Choon Lee Seah <andyseah@motorola.com>
    Tested-by: Choon Lee Seah <andyseah@motorola.com>
    Submit-Approved: Choon Lee Seah <andyseah@motorola.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1001,20 +1001,10 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 	unsigned delay;
 
 	/* Continue a partial initialization */
-	if (type == HUB_INIT2 || type == HUB_INIT3) {
-		device_lock(hub->intfdev);
-
-		/* Was the hub disconnected while we were waiting? */
-		if (hub->disconnected) {
-			device_unlock(hub->intfdev);
-			kref_put(&hub->kref, hub_release);
-			return;
-		}
-		if (type == HUB_INIT2)
-			goto init2;
+	if (type == HUB_INIT2)
+		goto init2;
+	if (type == HUB_INIT3)
 		goto init3;
-	}
-	kref_get(&hub->kref);
 
 	/* The superspeed hub except for root hub has to use Hub Depth
 	 * value as an offset into the route string to locate the bits
@@ -1219,7 +1209,6 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func3);
 			schedule_delayed_work(&hub->init_work,
 					msecs_to_jiffies(delay));
-			device_unlock(hub->intfdev);
 			return;		/* Continues at init3: below */
 		} else {
 			msleep(delay);
@@ -1240,11 +1229,6 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 	/* Allow autosuspend if it was suppressed */
 	if (type <= HUB_INIT3)
 		usb_autopm_put_interface_async(to_usb_interface(hub->intfdev));
-
-	if (type == HUB_INIT2 || type == HUB_INIT3)
-		device_unlock(hub->intfdev);
-
-	kref_put(&hub->kref, hub_release);
 }
 
 /* Implement the continuations for the delays above */
