117fd3a134f72e133acda7c70cace225eb690c3d
Ben Romberger <bromberg@codeaurora.org>
2016-07-14 18:36:56 -0700

    ASoC: msm: qdsp6v2: Add size check in audio cal ioctl
    For the audio get calibration ioctl compare the allocated
    buffer size to the size of the header and cal type header
    to ensure the buffer is big enough.
    
    Android CVE ID(s): CVE-2016-3860
    Mot-CRs-fixed: (CR)
    
    Change-Id: I851b4454e8420706ad3263d67e892720d46e5718
    Signed-off-by: Ben Romberger <bromberg@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1318565
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Seungyoon Lee <yoon@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/msm/qdsp6v2/audio_calibration.c
+++ sound/soc/msm/qdsp6v2/audio_calibration.c
@@ -496,7 +496,13 @@ static long audio_cal_shared_ioctl(struct file *file, unsigned int cmd,
 			goto unlock;
 		if (data == NULL)
 			goto unlock;
-		if (copy_to_user(arg, data,
+		if ((sizeof(data->hdr) + data->hdr.cal_type_size) > size) {
+			pr_err("%s: header size %zd plus cal type size %d are greater than data buffer size %d\n",
+				__func__, sizeof(data->hdr),
+				data->hdr.cal_type_size, size);
+			ret = -EFAULT;
+			goto unlock;
+		} else if (copy_to_user((void *)arg, data,
 			sizeof(data->hdr) + data->hdr.cal_type_size)) {
 			pr_err("%s: Could not copy cal type to user\n",
 				__func__);
