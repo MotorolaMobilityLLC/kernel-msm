3659f73e63a2e4d2b6879f5c678e5119ee50f8bf
Shiyong Li <a22381@motorola.com>
2016-07-20 15:09:26 -0700

    msm: mdss: use param_lock for race of panel access and off
    Use param_lock to avoid race condition between panel DSI access and panel
    off sequence instead of offlock. Holding offlock in DSI access thread
    would potentially block kick-off thread, ending up with frame drop.
    
    Change-Id: I497295446757bc04543f5c838c804042bae3047c
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: http://gerrit.mot.com/878298
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -128,11 +128,11 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 
 	if ((mipi->mode == DSI_CMD_MODE) && !ctrl_pdata->burst_mode_enabled)
 		mutex_lock(&mdp5_data->ov_lock);
-	mutex_lock(&ctl->offlock);
+	mutex_lock(&ctl->mfd->param_lock);
 
 	if (mdss_panel_is_power_off(pstatus_data->mfd->panel_power_state) ||
 			pstatus_data->mfd->shutdown_pending) {
-		mutex_unlock(&ctl->offlock);
+		mutex_unlock(&ctl->mfd->param_lock);
 		if ((mipi->mode == DSI_CMD_MODE) &&
 		    !ctrl_pdata->burst_mode_enabled)
 			mutex_unlock(&mdp5_data->ov_lock);
@@ -160,7 +160,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	ret = ctrl_pdata->check_status(ctrl_pdata);
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF);
 
-	mutex_unlock(&ctl->offlock);
+	mutex_unlock(&ctl->mfd->param_lock);
 	if ((mipi->mode == DSI_CMD_MODE) && !ctrl_pdata->burst_mode_enabled)
 		mutex_unlock(&mdp5_data->ov_lock);
 
