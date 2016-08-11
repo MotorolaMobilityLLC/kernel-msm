353e6f3c0ec5d22c5eae3549aeedb7694e2765be
Konstantin Makariev <kmakariev@motorola.com>
2016-08-11 08:57:24 -0500

    input: synaptics_mmi: bringup
    This commit adds MMI revision of Synaptics DSX touchscreen driver.
    Major differences with M base line are:
     * added dropbox reports
     * renamed shared include file to synaptics_dsx_mmi.h
     * use of SYN_TIME_SEC & SYN_TIME_NSEC is macro Defenderined feature
     * add weak Defenderinitions for external FPS functions
     * removed obsolete interfaces and debug features
     * move some global variables into rmi4_data
    
    Change-Id: Ic145997baac7fd3ab84c0b5276315c8b56d949a1
    Signed-off-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-on: http://gerrit.mot.com/885987
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/input/touchscreen/synaptics_mmi/synaptics_dsx_dropbox.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2014 - 2015, Motorola Mobility LLC. All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 and
+* only version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+#ifndef SYNAPTICS_DROPBOX_H
+#define SYNAPTICS_DROPBOX_H
+
+#include <linux/ratelimit.h>
+
+#define SYNAPTICS_DROPBOX_MSG_I2C "Touch I2C communication error detected"
+#define SYNAPTICS_DROPBOX_MSG_RESET_TIMEOUT "Touch reset time out detected"
+
+static DEFINE_RATELIMIT_STATE(synaptics_dropbox_global_rl, 3600 * HZ, 1);
+
+#if defined(CONFIG_DROPBOX)
+void synaptics_dropbox_report_event(char *msg, int count);
+void synaptics_dropbox_report_event_ratelimit(char *msg, int count,
+					struct ratelimit_state *rs);
+#else
+static inline void synaptics_dropbox_report_event(char *msg, int count) {}
+static inline void synaptics_dropbox_report_event_ratelimit(char *msg, int count,
+					struct ratelimit_state *rs) {}
+#endif
+#endif /* SYNAPTICS_DROPBOX_H */
