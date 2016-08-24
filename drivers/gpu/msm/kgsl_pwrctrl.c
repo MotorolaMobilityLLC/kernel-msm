65431dc5a24c51eddf8ea6a75f8d483d557be48d
Divya Ponnusamy <pdivya@codeaurora.org>
2016-08-24 17:06:54 +0530

    msm: kgsl: Change %p to %pK in debug messages
    The format specifier %p can leak kernel addresses
    while not valuing the kptr_restrict system settings.
    Use %pK instead of %p, which evaluates whether
    kptr_restrict is set.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2016-6749
    
    Change-Id: I0778e43e0a03852ca2944377256a7b401586a747
    Signed-off-by: Divya Ponnusamy <pdivya@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/909141
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 33bbbb8a07c82de7184e3f830b8b7cb286be3fde)
    Reviewed-on: https://gerrit.mot.com/911219
    Reviewed-by: Patrick Auchter <auchter@motorola.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1358,7 +1358,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	if (!pwr->pcl) {
 		KGSL_PWR_ERR(device,
 				"msm_bus_scale_register_client failed: "
-				"id %d table %pK %pK", device->id,
+				"id %d table %pK %pk", device->id,
 				pdata->bus_scale_table,
 				ocmem_scale_table);
 		result = -EINVAL;
