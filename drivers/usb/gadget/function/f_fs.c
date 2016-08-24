184597469e1dac294f83a70a25ece265556ba2a3
chailu1 <chailu1@lenovo.com>
2016-08-24 10:19:19 +0800

    USB: f_fs: change wait as freezable
    Change task to wait on wait_event_freezable so that
    it does not need to return a error on every suspend time.
    
    Change-Id: Id68adbcb4212709956223e2e20bb16f71d651aca
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: http://gerrit.mot.com/891667
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -23,6 +23,7 @@
 #include <linux/export.h>
 #include <linux/hid.h>
 #include <linux/module.h>
+#include <linux/freezer.h>
 #include <asm/unaligned.h>
 
 #include <linux/usb/composite.h>
@@ -747,7 +748,7 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 		 * and wait for next epfile open to happen
 		 */
 		if (!atomic_read(&epfile->error)) {
-			ret = wait_event_interruptible(epfile->wait,
+			ret = wait_event_freezable(epfile->wait,
 					(ep = epfile->ep));
 			if (ret < 0)
 				goto error;
