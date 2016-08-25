dd293295ae6b85b4cfad536e441dbf40828a4491
Lianwei Wang <a22439@motorola.com>
2016-08-25 15:57:23 -0700

    fs: esdfs: fix deadlock between rmdir and lookup
    The commit 453011e5a9de fix the deadlock between unlink
    and lookup, and looks like the fix is applicable to rmdir
    as well.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I09bd9097bd1570874b130da211854c61108bee1a
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/892385
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Reviewed-by: Sheng-Zhe Zhao <a18689@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/esdfs/inode.c
+++ fs/esdfs/inode.c
@@ -214,10 +214,10 @@ static int esdfs_rmdir(struct inode *dir, struct dentry *dentry)
 	else
 		esdfs_get_lower_path(dentry, &lower_path);
 	lower_dentry = lower_path.dentry;
-	lower_dir_dentry = lock_parent(lower_dentry);
 
 	esdfs_drop_shared_icache(dir->i_sb, d_inode(lower_dir_dentry));
 
+	lower_dir_dentry = lock_parent(lower_dentry);
 	err = vfs_rmdir(d_inode(lower_dir_dentry), lower_dentry);
 	if (err)
 		goto out;
