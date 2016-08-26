4d39d44ffa3d40ad56b5eec63407d278b067ce1b
Alexander Filonenko <alexf@motorola.com>
2016-08-26 14:12:27 -0500

    input: synaptics_dsx_i2c: remove pm qos request
    Remove default low latency PM QoS request and refactor the feature
    to be configurable via device tree.
    
    Propagation of (CR).
    
    Change-Id: I8881d535812064ca8e6813ae7c25985e22cf8109
    Signed-off-by: Alexander Filonenko <alexf@motorola.com>
    Reviewed-on: https://gerrit.mot.com/893072
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: https://gerrit.mot.com/895910

--- drivers/input/touchscreen/synaptics_dsx_i2c.h
+++ drivers/input/touchscreen/synaptics_dsx_i2c.h
@@ -578,7 +578,7 @@ struct synaptics_rmi4_data {
 	struct notifier_block fps_notif;
 
 	struct mutex rmi4_exp_init_mutex;
-	int pm_qos_latency;
+	uint32_t pm_qos_latency;
 	struct pm_qos_request pm_qos_irq;
 
 	bool touch_data_contiguous;
