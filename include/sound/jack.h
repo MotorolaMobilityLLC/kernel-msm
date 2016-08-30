058290f519c965cce7f1eb25766954d9a57fd6aa
Dmitry Ornatsky <cdo021c@motorola.com>
2016-08-30 12:35:41 -0500

    sound/soc: fsa8500: port to msmcobalt HS detect.
    Fix compilatioin errors of fsa8500 driver on msmcobalt 4.4
    kernel. Add Kconfig option to select fsa8500 for msmcobalt
    target.
    
    Change-Id: Id633420f495ce025bf02327ffd45324641833d33
    Signed-off-by: Dmitry Ornatsky <cdo021c@motorola.com>
    Reviewed-on: https://gerrit.mot.com/893529
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/sound/jack.h
+++ include/sound/jack.h
@@ -43,6 +43,8 @@ struct input_dev;
  * @SND_JACK_BTN_3: Button 3
  * @SND_JACK_BTN_4: Button 4
  * @SND_JACK_BTN_5: Button 5
+ * @SND_JACK_BTN_6: Button 6
+ * @SND_JACK_BTN_7: Button 7
  *
  * These values are used as a bitmask.
  *
@@ -66,12 +68,14 @@ enum snd_jack_types {
 				 SND_JACK_MICROPHONE2,
 
 	/* Kept separate from switches to facilitate implementation */
-	SND_JACK_BTN_0		= 0x8000,
-	SND_JACK_BTN_1		= 0x4000,
-	SND_JACK_BTN_2		= 0x2000,
-	SND_JACK_BTN_3		= 0x1000,
-	SND_JACK_BTN_4		= 0x0800,
-	SND_JACK_BTN_5		= 0x0400,
+	SND_JACK_BTN_0		= 0x8000000,
+	SND_JACK_BTN_1		= 0x4000000,
+	SND_JACK_BTN_2		= 0x2000000,
+	SND_JACK_BTN_3		= 0x1000000,
+	SND_JACK_BTN_4		= 0x0800000,
+	SND_JACK_BTN_5		= 0x0400000,
+	SND_JACK_BTN_6		= 0x0200000,
+	SND_JACK_BTN_7		= 0x0100000,
 };
 
 /* Keep in sync with definitions above */
