aebf8c3b4a3f122104e827f13a4fd272095d0f02
Paul Handrigan <Paul.Handrigan@cirrus.com>
2016-09-02 12:35:11 -0500

    ASoC: madera: Add support for madera
    Add support for madera class codecs.
    
    Change-Id: Ie5658c9dabf7f8d3732dbbd539ad6484dd2b48c0
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Mot-CRs-fixed: (CR)

---
+++ include/dt-bindings/sound/madera.h
@@ -0,0 +1,18 @@
+/*
+ * Device Tree defines for Madera codecs
+ *
+ * Copyright 2016 Cirrus Logic
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_SOUND_MADERA_H
+#define _DT_BINDINGS_SOUND_MADERA_H
+
+#define MADERA_INMODE_DIFF	0
+#define MADERA_INMODE_SE	1
+#define MADERA_INMODE_DMIC	2
+
+#endif
