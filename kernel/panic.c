5da974d13ea8c6ae5d47f3f3973368d23c16ef0d
jeboo <jeb00o@yahoo.com>
2016-09-04 22:03:20 -0400

    kernel 3.10.103 patch (xt1575_mm_6.0.0 specific)

--- kernel/panic.c
+++ kernel/panic.c
@@ -22,6 +22,7 @@
 #include <linux/sysrq.h>
 #include <linux/init.h>
 #include <linux/nmi.h>
+#include <linux/console.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/exception.h>
@@ -143,6 +144,8 @@ void panic(const char *fmt, ...)
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
 	bust_spinlocks(0);
+
+	console_flush_on_panic();
 
 	if (!panic_blink)
 		panic_blink = no_blink;
