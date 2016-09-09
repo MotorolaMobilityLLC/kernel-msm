aaff8fbf417fcb6fef1e852fbfae77b67f8db9cc
Alexander Filonenko <alexf@motorola.com>
2016-09-09 16:19:30 -0500

    of: add checking for conditional status
    Change validation of status property to support
    conditinal content.
    
    Propagation of (CR).
    
    Change-Id: If6815eaa9fd08dd7bfd6afa9f0f7059ecbc2c71d
    Reviewed-on: https://gerrit.mot.com/1268177
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- scripts/dtc/checks.c
+++ scripts/dtc/checks.c
@@ -179,6 +179,25 @@ static void check_is_string(struct check *c, struct dt_info *dti,
 #define ERROR_IF_NOT_STRING(nm, propname) \
 	ERROR(nm, check_is_string, (propname))
 
+static void check_is_null_terminated(struct check *c, struct node *root,
+			    struct node *node)
+{
+	struct property *prop;
+	char *propname = c->data;
+
+	prop = get_property(node, propname);
+	if (!prop)
+		return; /* Not present, assumed ok */
+
+	if (prop->val.val[prop->val.len-1] != '\0')
+		FAIL(c, "\"%s\" property in %s is not null terminated",
+		     propname, node->fullpath);
+}
+#define WARNING_IF_NOT_NULL_TERMINATED(nm, propname) \
+	WARNING(nm, NULL, check_is_null_terminated, NULL, (propname))
+#define ERROR_IF_NOT_NULL_TERMINATED(nm, propname) \
+	ERROR(nm, NULL, check_is_null_terminated, NULL, (propname))
+
 static void check_is_cell(struct check *c, struct dt_info *dti,
 			  struct node *node)
 {
@@ -585,7 +604,7 @@ WARNING_IF_NOT_CELL(interrupt_cells_is_cell, "#interrupt-cells");
 
 WARNING_IF_NOT_STRING(device_type_is_string, "device_type");
 WARNING_IF_NOT_STRING(model_is_string, "model");
-WARNING_IF_NOT_STRING(status_is_string, "status");
+WARNING_IF_NOT_NULL_TERMINATED(status_is_null_terminated, "status");
 
 static void fixup_addr_size_cells(struct check *c, struct dt_info *dti,
 				  struct node *node)
@@ -967,7 +986,7 @@ static struct check *check_table[] = {
 	&phandle_references, &path_references,
 
 	&address_cells_is_cell, &size_cells_is_cell, &interrupt_cells_is_cell,
-	&device_type_is_string, &model_is_string, &status_is_string,
+	&device_type_is_string, &model_is_string, &status_is_null_terminated,
 
 	&property_name_chars_strict,
 	&node_name_chars_strict,
