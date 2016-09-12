f23647528264c1a2ca1e0bdaca1005b40d4647b8
Bang Nguyen <bangnguyen@motorola.com>
2016-09-12 16:12:39 -0500

    fbdev: mdss: Add display_issue dropbox utility function
    Add a common utility function for reporting display_issue bug2gos.
    
    Port from 0bc6475
    
    Change-Id: I1bb47fe7e444c0a7b407fd04a76b1dbda4fb3f97
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1165969
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Shanshan Dai <daiss1@mt.com>

---
+++ drivers/video/fbdev/msm/mdss_dropbox.c
@@ -0,0 +1,32 @@
+/* Copyright (c) 2014 - 2015, Motorola Mobility LLC. All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 and
+* only version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+#include <linux/dropbox.h>
+#include <linux/string.h>
+
+#include "mdss_dropbox.h"
+
+#define DROPBOX_DISPLAY_ISSUE "display_issue"
+
+void mdss_dropbox_report_event(char *msg, int count)
+{
+	char dropbox_entry[256];
+
+	snprintf(dropbox_entry, sizeof(dropbox_entry), "%s\ncount: %d",
+		msg, count);
+
+	pr_err("%s: display issue detected[count: %d][%s], reporting dropbox\n",
+		__func__, count, msg);
+	dropbox_queue_event_text(DROPBOX_DISPLAY_ISSUE,
+				dropbox_entry, strlen(dropbox_entry));
+}
+
+
