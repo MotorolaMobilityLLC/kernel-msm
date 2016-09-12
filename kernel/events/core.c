f6b53ad24f1e04729a0f6cbe62722c201855d79e
Mark Salyzyn <salyzyn@google.com>
2016-09-12 22:13:54 +0000

    Revert "BACKPORT: perf: Fix race in swevent hash"
    This reverts commit 31df97aff998324122d36e7076a714ec93ab2ccf.
    
    Change-Id: I3bdbc32d8bc1783cc4f44ffdca36a18f0a152437
    Git-commit: 2233f5cacfd502b66efe109cb30edaa622aaa183
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5767,6 +5767,7 @@ static int swevent_hlist_get_cpu(struct perf_event *event, int cpu)
 	int err = 0;
 
 	mutex_lock(&swhash->hlist_mutex);
+
 	if (!swevent_hlist_deref(swhash) && cpu_online(cpu)) {
 		struct swevent_hlist *hlist;
 
@@ -7959,6 +7960,12 @@ static void perf_event_start_swclock(int cpu)
 
 static void perf_event_exit_cpu(int cpu)
 {
+	struct swevent_htable *swhash = &per_cpu(swevent_htable, cpu);
+
+	mutex_lock(&swhash->hlist_mutex);
+	swevent_hlist_release(swhash);
+	mutex_unlock(&swhash->hlist_mutex);
+
 	perf_event_exit_cpu_context(cpu);
 }
 #else
