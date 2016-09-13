eb353dedcc72e32df41b32685fbee1e718ffc7ca
Ravi Vembu <raviv@motorola.com>
2016-09-13 13:41:38 -0500

    IKSWM-47904: usb: dwc3: Avoid DWC3_CONTROLLER_ERROR flood
    DWC3_CONTROLLER_ERROR events can flood the system repeatedly
    causing the controller to be reset in an endless loopi and hog
    the system. Avoid this by ignoring any Error events after 5 retries.
    
    Change-Id: I5f896cf7a347645e7296ba4e9c901d37dc72ee4f
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: https://gerrit.mot.com/898933
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zong-Hua Liu <a17671@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1901,6 +1901,7 @@ static void dwc3_msm_qscratch_reg_init(struct dwc3_msm *mdwc)
 
 }
 
+#define MAX_ERR_CNT 5
 static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned event,
 							unsigned value)
 {
@@ -1912,6 +1913,10 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned event,
 
 	switch (event) {
 	case DWC3_CONTROLLER_ERROR_EVENT:
+		/* Avoid a flood of Error events */
+		if (dwc->err_cnt++ >= MAX_ERR_CNT)
+			break;
+
 		dev_info(mdwc->dev,
 			"DWC3_CONTROLLER_ERROR_EVENT received, irq cnt %lu\n",
 			dwc->irq_cnt);
@@ -4279,6 +4284,9 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 		}
 	}
 
+	/* Reset Controller Error Count */
+	dwc->err_cnt = 0;
+
 	if (on) {
 		dev_dbg(mdwc->dev, "%s: turn on host\n", __func__);
 
@@ -4462,6 +4470,9 @@ static void dwc3_override_vbus_status(struct dwc3_msm *mdwc, bool vbus_present)
 static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 {
 	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
+
+	/* Reset Controller Error Count */
+	dwc->err_cnt = 0;
 
 	pm_runtime_get_sync(mdwc->dev);
 	dbg_event(dwc->ctrl_num, 0xFF, "StrtGdgt gsync",
