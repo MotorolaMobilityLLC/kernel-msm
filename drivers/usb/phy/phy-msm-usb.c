0d1dac65fe0e471595ca95765e209a17737373cc
chailu1 <chailu1@lenovo.com>
2016-09-14 15:01:15 +0800

    usb: power: add POWER_SUPPLY_PROP_CHG_PRESENT property
    Add POWER_SUPPLY_PROP_CHG_PRESENT property to phy-msm-usb.c.
    resolve cedric project can not charging issue.
    
    Change-Id: Ib0e099f5a1f13fbbe84cbd4ebe3e98417aa0c58b
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/899208
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -3578,6 +3578,7 @@ static int otg_power_get_property_usb(struct power_supply *psy,
 		val->intval = motg->typec_current_max;
 		break;
 	case POWER_SUPPLY_PROP_PRESENT:
+	case POWER_SUPPLY_PROP_CHG_PRESENT:
 		val->intval = !!test_bit(B_SESS_VLD, &motg->inputs);
 		break;
 	case POWER_SUPPLY_PROP_DP_DM:
@@ -3624,6 +3625,7 @@ static int otg_power_set_property_usb(struct power_supply *psy,
 		break;
 	/* Process PMIC notification in PRESENT prop */
 	case POWER_SUPPLY_PROP_PRESENT:
+	case POWER_SUPPLY_PROP_CHG_PRESENT:
 		msm_otg_set_vbus_state(val->intval);
 		break;
 	/* The ONLINE property reflects if usb has enumerated */
@@ -3727,6 +3729,7 @@ static int otg_power_property_is_writeable_usb(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_HEALTH:
 	case POWER_SUPPLY_PROP_PRESENT:
+	case POWER_SUPPLY_PROP_CHG_PRESENT:
 	case POWER_SUPPLY_PROP_ONLINE:
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
@@ -3757,6 +3760,7 @@ static enum power_supply_property otg_pm_power_props_usb[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
 	POWER_SUPPLY_PROP_DP_DM,
 	POWER_SUPPLY_PROP_USB_OTG,
+	POWER_SUPPLY_PROP_CHG_PRESENT,
 };
 
 const struct file_operations msm_otg_bus_fops = {
