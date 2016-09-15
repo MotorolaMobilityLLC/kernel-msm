54a5a8a29d2172b4f4bedb12bfb589c50bcb63ce
Josh Galicia <cjg040@motorola.com>
2016-09-15 13:43:49 -0500

    lowmemorykiller: redesign memory instrumentation
    Add tracking node for LMK counts for memory instrumentation.
    
    
    Signed-off-by: Josh Galicia <cjg040@motorola.com>
    Reviewed-on: https://gerrit.mot.com/899855
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c
    Change-Id: If0f0fb29127e275641f4d7d06962a4a7f20f183d
    Reviewed-on: https://gerrit.mot.com/1257161
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhangqing Huang <huangzq2@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Kuldeep Prabhakar Ghan <dhp764@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1529701
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    SLTApproved: Ashok Oliver Prabhu <ashok@motorola.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -90,6 +90,11 @@ static int lowmem_minfree[6] = {
 static int lowmem_minfree_size = 4;
 static int lmk_fast_run = 1;
 
+/*
+ * This parameter tracks the kill count per minfree since boot.
+ */
+static int lowmem_per_minfree_count[6];
+
 static unsigned long lowmem_deathpending_timeout;
 
 #define lowmem_print(level, x...)			\
@@ -610,6 +615,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int other_free;
 	int other_file;
 	int other_indirect;
+	int minfree_count_offset = 0;
 
 	if (!mutex_trylock(&scan_mutex))
 		return 0;
@@ -639,6 +645,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		minfree = lowmem_minfree[i];
 		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
+			minfree_count_offset = i;
 			break;
 		}
 	}
@@ -754,6 +761,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		}
 		task_unlock(selected);
 		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
+		lowmem_per_minfree_count[minfree_count_offset]++;
 		lowmem_print(1, "Killing '%s' (%d) (tgid %d), adj %hd,\n"
 			"to free %ldkB on behalf of '%s' (%d) because\n"
 			"cache %ldkB is below limit %ldkB for oom score %hd\n"
@@ -922,6 +930,8 @@ module_param_array_named(adj, lowmem_adj, short, &lowmem_adj_size, 0644);
 #endif
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
+module_param_array_named(lmk_count, lowmem_per_minfree_count, uint, NULL,
+			 S_IRUGO);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
 module_param_named(lmk_fast_run, lmk_fast_run, int, S_IRUGO | S_IWUSR);
 
