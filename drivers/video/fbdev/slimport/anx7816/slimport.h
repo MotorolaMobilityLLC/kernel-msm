51e4236828cc987b2efeb1122884a178487c989e
Bang Nguyen <bangnguyen@motorola.com>
2016-09-19 13:14:28 -0500

    fbdev: slimport: add slimport ANX7816 driver
    Add Slimport ANX7816 driver which converts from HDMI protocol to
    myDP (MobilitY DisplayPort) protocol
    
    Port from f9363e9
    
    Change-Id: I304994d00a28477d54acdb9d97d70dca637d0fd9
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/906088
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Jared Suttles <jsuttles@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/video/fbdev/slimport/anx7816/slimport.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright(c) 2014, Analogix Semiconductor. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _SLIMPORT_H
+#define _SLIMPORT_H
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/i2c.h>
+#include <linux/interrupt.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/workqueue.h>
+#include <linux/wakelock.h>
+#include <linux/regulator/consumer.h>
+#include <linux/err.h>
+#include <linux/async.h>
+#include <linux/of_gpio.h>
+#include <linux/of_platform.h>
+#include <slimport.h>
+
+#define SSC_EN
+
+#define LOG_TAG "SlimPort Colorado3"
+
+#define AUX_ERR 1
+#define AUX_OK 0
+bool slimport_dongle_is_connected(void);
+void slimport_set_hdmi_hpd(int on);
+int sp_read_reg(uint8_t slave_addr, uint8_t offset, uint8_t *buf);
+int sp_write_reg(uint8_t slave_addr, uint8_t offset, uint8_t value);
+void sp_tx_hardware_poweron(void);
+void sp_tx_hardware_powerdown(void);
+
+#endif
