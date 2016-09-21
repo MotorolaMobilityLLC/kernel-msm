9e121f64bcc8d2e7bea31bc12d65fb342b3f36af
Nagappan Rathinam <dgxp63@motorola.com>
2016-09-21 18:06:06 -0500

    Change namebase for RC camera device.
    To overcome the SE linux security issue on accessing the video device
    from platform app.
    Add new VFL_TYPE_MOT_GRABBER to differentiate the camera device and
    name the video device as mot_camera_device for RC camera.
    
    Change-Id: I310b8905c0fe06167882a8f5aba1a29369f7997e
    Signed-off-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-on: https://gerrit.mot.com/903610
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/v4l2-core/v4l2-dev.c
+++ drivers/media/v4l2-core/v4l2-dev.c
@@ -520,7 +520,8 @@ static void determine_valid_ioctls(struct video_device *vdev)
 {
 	DECLARE_BITMAP(valid_ioctls, BASE_VIDIOC_PRIVATE);
 	const struct v4l2_ioctl_ops *ops = vdev->ioctl_ops;
-	bool is_vid = vdev->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vdev->vfl_type == VFL_TYPE_GRABBER ||
+		       vdev->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_vbi = vdev->vfl_type == VFL_TYPE_VBI;
 	bool is_radio = vdev->vfl_type == VFL_TYPE_RADIO;
 	bool is_sdr = vdev->vfl_type == VFL_TYPE_SDR;
@@ -697,7 +698,8 @@ static void determine_valid_ioctls(struct video_device *vdev)
 			SET_VALID_IOCTL(ops, VIDIOC_G_AUDOUT, vidioc_g_audout);
 			SET_VALID_IOCTL(ops, VIDIOC_S_AUDOUT, vidioc_s_audout);
 		}
-		if (ops->vidioc_g_parm || (vdev->vfl_type == VFL_TYPE_GRABBER &&
+		if (ops->vidioc_g_parm || ((vdev->vfl_type == VFL_TYPE_GRABBER
+			|| vdev->vfl_type == VFL_TYPE_MOT_GRABBER) &&
 					ops->vidioc_g_std))
 			set_bit(_IOC_NR(VIDIOC_G_PARM), valid_ioctls);
 		SET_VALID_IOCTL(ops, VIDIOC_S_PARM, vidioc_s_parm);
@@ -799,6 +801,9 @@ int __video_register_device(struct video_device *vdev, int type, int nr,
 		/* Use device name 'swradio' because 'sdr' was already taken. */
 		name_base = "swradio";
 		break;
+	case VFL_TYPE_MOT_GRABBER:
+		name_base = "mot_camera_ext";
+		break;
 	default:
 		printk(KERN_ERR "%s called with unknown type: %d\n",
 		       __func__, type);
@@ -825,6 +830,7 @@ int __video_register_device(struct video_device *vdev, int type, int nr,
 	 * (new style). */
 	switch (type) {
 	case VFL_TYPE_GRABBER:
+	case VFL_TYPE_MOT_GRABBER:
 		minor_offset = 0;
 		minor_cnt = 64;
 		break;
