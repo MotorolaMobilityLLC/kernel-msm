5ef2cfa3511acaf8ce2cdfeff9cce08cc09f257b
Nagappan Rathinam <dgxp63@motorola.com>
2016-09-21 18:06:06 -0500

    Change namebase for RC camera device.
    To overcome the SE linux security issue on accessing the video device
    from platform app.
    Add new VFL_TYPE_MOT_GRABBER to differentiate the camera device and
    name the video device as mot_camera_device for RC camera.
    
    Change-Id: I310b8905c0fe06167882a8f5aba1a29369f7997e
    Signed-off-by: Elena Satraitis <w04888@motorola.com>
    Reviewed-on: https://gerrit.mot.com/903610
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -920,7 +920,8 @@ static int check_fmt(struct file *file, enum v4l2_buf_type type)
 {
 	struct video_device *vfd = video_devdata(file);
 	const struct v4l2_ioctl_ops *ops = vfd->ioctl_ops;
-	bool is_vid = vfd->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vfd->vfl_type == VFL_TYPE_GRABBER ||
+			vfd->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_vbi = vfd->vfl_type == VFL_TYPE_VBI;
 	bool is_sdr = vfd->vfl_type == VFL_TYPE_SDR;
 	bool is_rx = vfd->vfl_dir != VFL_DIR_TX;
@@ -1381,7 +1382,8 @@ static int v4l_enum_fmt(const struct v4l2_ioctl_ops *ops,
 {
 	struct v4l2_fmtdesc *p = arg;
 	struct video_device *vfd = video_devdata(file);
-	bool is_vid = vfd->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vfd->vfl_type == VFL_TYPE_GRABBER ||
+			vfd->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_sdr = vfd->vfl_type == VFL_TYPE_SDR;
 	bool is_rx = vfd->vfl_dir != VFL_DIR_TX;
 	bool is_tx = vfd->vfl_dir != VFL_DIR_RX;
@@ -1434,7 +1436,8 @@ static int v4l_g_fmt(const struct v4l2_ioctl_ops *ops,
 {
 	struct v4l2_format *p = arg;
 	struct video_device *vfd = video_devdata(file);
-	bool is_vid = vfd->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vfd->vfl_type == VFL_TYPE_GRABBER ||
+			vfd->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_sdr = vfd->vfl_type == VFL_TYPE_SDR;
 	bool is_rx = vfd->vfl_dir != VFL_DIR_TX;
 	bool is_tx = vfd->vfl_dir != VFL_DIR_RX;
@@ -1530,7 +1533,8 @@ static int v4l_s_fmt(const struct v4l2_ioctl_ops *ops,
 {
 	struct v4l2_format *p = arg;
 	struct video_device *vfd = video_devdata(file);
-	bool is_vid = vfd->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vfd->vfl_type == VFL_TYPE_GRABBER ||
+			vfd->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_sdr = vfd->vfl_type == VFL_TYPE_SDR;
 	bool is_rx = vfd->vfl_dir != VFL_DIR_TX;
 	bool is_tx = vfd->vfl_dir != VFL_DIR_RX;
@@ -1614,7 +1618,8 @@ static int v4l_try_fmt(const struct v4l2_ioctl_ops *ops,
 {
 	struct v4l2_format *p = arg;
 	struct video_device *vfd = video_devdata(file);
-	bool is_vid = vfd->vfl_type == VFL_TYPE_GRABBER;
+	bool is_vid = (vfd->vfl_type == VFL_TYPE_GRABBER ||
+			vfd->vfl_type == VFL_TYPE_MOT_GRABBER);
 	bool is_sdr = vfd->vfl_type == VFL_TYPE_SDR;
 	bool is_rx = vfd->vfl_dir != VFL_DIR_TX;
 	bool is_tx = vfd->vfl_dir != VFL_DIR_RX;
