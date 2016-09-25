561bb4f41914c13f389652b32e01a46feee61eab
Yi-wei Zhao <gbjc64@motorola.com>
2016-09-25 18:35:14 -0500

    arch/arm64: skip randomization within stack
    It's a WA to make 32-bit dhrystone generate consistent
    scores during "sustained peformance mode" cts test.
    
    Change-Id: Ib146224ff0701899640cbaba649227aba85534a0
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: https://gerrit.mot.com/902751
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -493,8 +493,6 @@ unsigned long get_wchan(struct task_struct *p)
 
 unsigned long arch_align_stack(unsigned long sp)
 {
-	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
-		sp -= get_random_int() & ~PAGE_MASK;
 	return sp & ~0xf;
 }
 
