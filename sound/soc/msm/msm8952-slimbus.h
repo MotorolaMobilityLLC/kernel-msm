ac4b24b025f6130199b69f40b84104d90e29998f
Paul Handrigan <Paul.Handrigan@cirrus.com>
2016-10-05 14:44:54 -0500

    ASoC: msm8953: Add changes for albus audio
    Add in the tommy driver and change the mod dai for albus
    audio.
    Mot-CRs-fixed: (CR)
    Change-Id: I02c3c4db99433e7f71af410a12d789fc03bc797d
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Signed-off-by: Xinyu Fang <a21849@motorola.com>
    Reviewed-on: https://gerrit.mot.com/906943
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: John Ratke <jratke@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/msm8952-slimbus.h
+++ sound/soc/msm/msm8952-slimbus.h
@@ -68,6 +68,7 @@ int msm_audrx_init(struct snd_soc_pcm_runtime *rtd);
 #ifdef CONFIG_SND_SOC_MARLEY
 int marley_dai_init(struct snd_soc_pcm_runtime *rtd);
 int marley_cs35l34_dai_init(struct snd_soc_pcm_runtime *rtd);
+int marley_cs35l35_dai_init(struct snd_soc_pcm_runtime *rtd);
 #endif
 int msm_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params);
