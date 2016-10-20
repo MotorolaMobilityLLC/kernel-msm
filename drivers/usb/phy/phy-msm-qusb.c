b85160ab3001c5c16a4e09b829fed601bdb5f534
xuwt2 <xuwt2@lenovo.com>
2016-10-20 10:45:11 +0800

    phy-msm-qusb: Add mutex lock in qusb_phy_enable_power
    Prevent reentrancy with async threads in qusb_phy_enable_power,
    need to add mutex lock to prevent critical sections , or may be
    impossible to repeat power down regulator,and if other devices
    also using this regulator together
    
    Change-Id: I5c34d1d3f7d4c9d8bfa1d71d32b843bca8f334ff
    Signed-off-by: xuwt2 <xuwt2@mt.com>
    Reviewed-on: https://gerrit.mot.com/911062
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -165,6 +165,8 @@ struct qusb_phy {
 	int			*emu_dcm_reset_seq;
 	int			emu_dcm_reset_seq_len;
 	spinlock_t		pulse_lock;
+	struct mutex regulator_lock;
+
 };
 
 static void qusb_phy_enable_clocks(struct qusb_phy *qphy, bool on)
@@ -211,11 +213,14 @@ static int qusb_phy_enable_power(struct qusb_phy *qphy, bool on)
 {
 	int ret = 0;
 
+	mutex_lock(&qphy->regulator_lock);
+
 	dev_dbg(qphy->phy.dev, "%s turn %s regulators. power_enabled:%d\n",
 			__func__, on ? "on" : "off", qphy->power_enabled);
 
 	if (qphy->power_enabled == on) {
 		dev_dbg(qphy->phy.dev, "PHYs' regulators are already ON.\n");
+		mutex_unlock(&qphy->regulator_lock);
 		return 0;
 	}
 
@@ -278,6 +283,7 @@ static int qusb_phy_enable_power(struct qusb_phy *qphy, bool on)
 	qphy->power_enabled = true;
 
 	pr_debug("%s(): QUSB PHY's regulators are turned ON.\n", __func__);
+	mutex_unlock(&qphy->regulator_lock);
 	return ret;
 
 disable_vdda33:
@@ -326,6 +332,7 @@ unconfig_vdd:
 err_vdd:
 	qphy->power_enabled = false;
 	dev_dbg(qphy->phy.dev, "QUSB PHY's regulators are turned OFF.\n");
+	mutex_unlock(&qphy->regulator_lock);
 	return ret;
 }
 
@@ -1044,6 +1051,7 @@ static int qusb_phy_probe(struct platform_device *pdev)
 
 	qphy->phy.dev = dev;
 	spin_lock_init(&qphy->pulse_lock);
+	mutex_init(&qphy->regulator_lock);
 
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
 							"qusb_phy_base");
