d866a455a85cd87b2e87450d896514e789ea3eb5
Martijn Coenen <maco@android.com>
2016-10-25 13:48:44 +0200

    android: binder: use copy_from_user_preempt_disabled
    To keep the driver consistent, and until we have
    fine-grained locking in place.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: Idda7ae8df889b5fae5e96bf343ab17782b4c46b1
    Signed-off-by: Martijn Coenen <maco@android.com>
    (cherry picked from commit 288f5164a4e434939ae92b86dae2768e57ca41ac)
    Reviewed-on: https://gerrit.mot.com/1168852
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>
    Submit-Approved: Vijayakumar Gn <vijaygn@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2255,9 +2255,10 @@ static void binder_transaction(struct binder_proc *proc,
 				return_error = BR_FAILED_REPLY;
 				goto err_bad_offset;
 			}
-			if (copy_from_user(sg_bufp,
-					   (const void __user *)(uintptr_t)
-					   bp->buffer, bp->length)) {
+			if (copy_from_user_preempt_disabled(
+					sg_bufp,
+					(const void __user *)(uintptr_t)
+					bp->buffer, bp->length)) {
 				binder_user_error("%d:%d got transaction with invalid offsets ptr\n",
 						  proc->pid, thread->pid);
 				return_error = BR_FAILED_REPLY;
@@ -2552,7 +2553,8 @@ int binder_thread_write(struct binder_proc *proc,
 		case BC_REPLY_SG: {
 			struct binder_transaction_data_sg tr;
 
-			if (copy_from_user(&tr, ptr, sizeof(tr)))
+			if (copy_from_user_preempt_disabled(&tr, ptr,
+							    sizeof(tr)))
 				return -EFAULT;
 			ptr += sizeof(tr);
 			binder_transaction(proc, thread, &tr.transaction_data,
