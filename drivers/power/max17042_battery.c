sultanqasim/android_kernel_motorola_msm8916

IKDREL3KK-12724: power: max17042: Remove Forced POR
Doing a Forced POR causes the MAX17050 to re-run all
its OCV calculations and thus can lead to an OCV
measurement taken at a time when the Battery is not
actually at OCV. This can force the SOC after the
load to be off by as much as 30%.

Change-Id: I2f116c7b8ab486c87cf2591a39f2faa0c69e912d
Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
Reviewed-on: http://gerrit.mot.com/670053
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
SLTApproved: Christopher Fries <cfries@motorola.com>
Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/power/max17042_battery.c
@@ -1773,6 +1773,7 @@ static int max17042_probe(struct i2c_client *client,
 	struct max17042_chip *chip;
 	int ret;
 	int reg;
+	int reg2;
 	int i;
 
 	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_WORD_DATA))
@@ -1886,18 +1887,16 @@ static int max17042_probe(struct i2c_client *client,
 		}
 	}
 
-	reg = max17042_read_reg(chip->client, MAX17050_CFG_REV_REG);
-	reg &= MAX17050_CFG_REV_MASK;
-	if (reg != chip->pdata->config_data->revision) {
-		dev_warn(&client->dev, "Revision Change, Forcing POR!\n");
-		max17042_write_reg(client, MAX17042_VFSOC0Enable,
-				   MAX17050_FORCE_POR);
-		msleep(20);
-	}
-
 	reg = max17042_read_reg(chip->client, MAX17042_STATUS);
+	reg2 = max17042_read_reg(chip->client, MAX17050_CFG_REV_REG);
+	reg2 &= MAX17050_CFG_REV_MASK;
+
 	if (reg & STATUS_POR_BIT) {
 		dev_warn(&client->dev, "POR Detected, Loading Config\n");
+		INIT_WORK(&chip->work, max17042_init_worker);
+		schedule_work(&chip->work);
+	} else if (reg2 != chip->pdata->config_data->revision) {
+		dev_warn(&client->dev, "Revision Change, Loading Config\n");
 		INIT_WORK(&chip->work, max17042_init_worker);
 		schedule_work(&chip->work);
 	} else {

MotorolaMobilityLLC/kernel-msm

drivers/power/max17042_battery.c
@@ -2007,25 +2007,25 @@ static int max17042_probe(struct i2c_client *client,
 		}
 	}
 
+	regmap_read(chip->regmap, MAX17042_STATUS, &val);
 	regmap_read(chip->regmap, MAX17050_CFG_REV_REG, &val2);
 	val2 &= MAX17050_CFG_REV_MASK;
-	if (val2 != chip->pdata->config_data->revision) {
-		dev_warn(&client->dev, "Revision Change, Forcing POR!\n");
-		max17042_write_reg(client, MAX17042_VFSOC0Enable,
-				   MAX17050_FORCE_POR);
-		msleep(20);
-	}
 
-	regmap_read(chip->regmap, MAX17042_STATUS, &val);
+	INIT_WORK(&chip->work, max17042_init_worker);
+
 	if (val & STATUS_POR_BIT) {
 		dev_warn(&client->dev, "POR Detected, Loading Config\n");
+		INIT_WORK(&chip->work, max17042_init_worker);
+		schedule_work(&chip->work);
+	} else if (val2 != chip->pdata->config_data->revision) {
+		dev_warn(&client->dev, "Revision Change, Loading Config\n");
 		INIT_WORK(&chip->work, max17042_init_worker);
 		schedule_work(&chip->work);
 	} else {
 		config = chip->pdata->config_data;
 		if (chip->chip_type == MAX17047)
-			max17042_write_reg(chip->client, MAX17047_FullSOCThr,
-					   config->full_soc_thresh);
+			regmap_write(chip->regmap, MAX17047_FullSOCThr,
+				     config->full_soc_thresh);
 		chip->init_complete = 1;
 	}
 