98be296315ad9a1b796471dd1997ee5d078e8d5d
Shiyong Li <a22381@motorola.com>
2016-11-09 16:47:32 -0800

    fbdev: msm: export ACL sysfs interface
    Panel's ACL state is able to be turned on or off by sysfs.
    
    port of  0057226.
    
    Change-Id: I5092542d4d311c737210f735dbd74f58d0659679
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/918595
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1453,8 +1453,14 @@ static struct panel_param_val_map hbm_map[HBM_STATE_NUM] = {
 	{"1", "qcom,mdss-dsi-hbm-on-command"},
 };
 
+static struct panel_param_val_map acl_map[ACL_STATE_NUM] = {
+	{"0", "qcom,mdss-dsi-acl-off-command"},
+	{"1", "qcom,mdss-dsi-acl-on-command"},
+};
+
 static struct panel_param mdss_dsi_panel_param[PARAM_ID_NUM] = {
 	{"HBM", hbm_map, HBM_STATE_NUM, HBM_OFF_STATE, HBM_OFF_STATE, false},
+	{"ACL", acl_map, ACL_STATE_NUM, ACL_OFF_STATE, ACL_OFF_STATE, false},
 };
 
 static int mdss_panel_parse_param_prop(struct device_node *np,
