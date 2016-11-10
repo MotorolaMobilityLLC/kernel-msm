5dba54f46cff55fac56fcff9d76b5c1a268e271b
Shiyong Li <a22381@motorola.com>
2016-11-09 16:15:52 -0800

    fbdev: msm: export HBM sysfs interface
    Panel's HBM state is able to be turned on or off by sysfs.
    
    port of bc6b25a.
    
    Change-Id: I95769a6ba315f58208d25279b8d9b3824f8e8f17
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/918593
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1448,8 +1448,13 @@ void mdss_dsi_panel_parse_forced_tx_mode(struct device_node *np,
 	}
 }
 
+static struct panel_param_val_map hbm_map[HBM_STATE_NUM] = {
+	{"0", "qcom,mdss-dsi-hbm-off-command"},
+	{"1", "qcom,mdss-dsi-hbm-on-command"},
+};
+
 static struct panel_param mdss_dsi_panel_param[PARAM_ID_NUM] = {
-	/* parameters to be added */
+	{"HBM", hbm_map, HBM_STATE_NUM, HBM_OFF_STATE, HBM_OFF_STATE, false},
 };
 
 static int mdss_panel_parse_param_prop(struct device_node *np,
