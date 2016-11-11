e7318ac38344563a6983f3ac009448758bb972e8
Manu Gautam <mgautam@codeaurora.org>
2016-11-11 09:58:16 -0800

    USB: f_fs: Fix disconnect check during ongoing IO
    F_FS function driver allocated ffs_eps and updates ffs_ep->ep
    to corresponding usb_ep during func->bind and never clears it.
    On bind it also saves ffs_ep context in epfile->ep.
    During func->disable, it clears only ffs_ep context in epfile->ep
    and on func->unbind it frees ffs_eps memory.
    ffs_epfile_io routine currently relies on ffs_ep->ep (which is
    never cleared and ffs_ep could be freed on unbind) to detect any
    disconnect during active IO. This can result in various issues e.g.
    use after free use of ffs_ep if unbind finished before epfile_io
    could resume or "stop adbd" trying to dequeue a freed USB request
    when epfile_io could execute only after F_FS got disabled as
    'if (ep->ep)' check would be TRUE.
    Fix this by checking stored ffs_ep context against latest epfile->ep
    to figure out if endpoint got disabled or changed before acquiring
    spin_lock.
    
    Mot-CRs-fixed:(CR)
    
    Change-Id: I6bdcdf0dff0813ed7b2af8c24f544a22796b0369
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1349639
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1356595
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1449148

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1074,7 +1074,12 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 			 */
 			spin_lock_irq(&epfile->ffs->eps_lock);
 			interrupted = true;
-			if (ep->ep) {
+			/*
+			 * While we were acquiring lock endpoint got
+			 * disabled (disconnect) or changed
+			 (composition switch) ?
+			 */
+			if (epfile->ep == ep) {
 				usb_ep_dequeue(ep->ep, req);
 				spin_unlock_irq(&epfile->ffs->eps_lock);
 				wait_for_completion(&done);
@@ -1094,7 +1099,12 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 
 		ret = -ENODEV;
 		spin_lock_irq(&epfile->ffs->eps_lock);
-		if (ep->ep)
+		/*
+		 * While we were acquiring lock endpoint got
+		 * disabled (disconnect) or changed
+		 * (composition switch) ?
+		 */
+		if (epfile->ep == ep)
 			ret = ep->status;
 		spin_unlock_irq(&epfile->ffs->eps_lock);
 		if (io_data->read && ret > 0)
@@ -3750,6 +3760,7 @@ static void ffs_func_unbind(struct usb_configuration *c,
 		if (ep->ep && ep->req)
 			usb_ep_free_request(ep->ep, ep->req);
 		ep->req = NULL;
+		ep->ep = NULL;
 		++ep;
 	}
 	spin_unlock_irqrestore(&func->ffs->eps_lock, flags);
