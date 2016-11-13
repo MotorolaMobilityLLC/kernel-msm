b64712c4e0c3a6050d3518cb2411e4d6ae92e4bb
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2016-11-12 21:57:02 -0600

    input misc vl53L1:add ST driver code
    Initial vl53L1 driver code from ST
      release_1.0.0_fw398
    
    Change-Id: I3b204858b5e7f4db422be6fd72b8f1bb2a193cc8
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/919394
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/input/misc/vl53L1/stmvl53l1_if.h
@@ -0,0 +1,379 @@
+/*
+* Copyright (c) 2016, STMicroelectronics - All Rights Reserved
+*
+* License terms: BSD 3-clause "New" or "Revised" License.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+/**
+ *  @file stmvl53l1_if.h  vl53l1 kernel driver user interface
+ *
+  * @note to use this header in a user space application it requires
+ *  all st bare/ll driver platform wrapper files (for data struct def)
+ *  this files (types etc ..) shall be same or compliant with bar driver version
+ *  used in the kernel module
+ */
+
+#ifndef STMVL53L1_IF_H
+#define STMVL53L1_IF_H
+
+
+#include "vl53l1_def.h"
+/**
+ * misc device  name
+ *
+ * for mutli instance all device 2nd and next instance are basic name +"1"+"2"
+ * @li stmvl53l1_ranging
+ * @li stmvl53l1_ranging1
+ * @li stmvl53l1_ranging2
+ */
+
+#define VL53L1_MISC_DEV_NAME		"stmvl53l1_ranging"
+
+/**
+ * @addtogroup vl53l1_ioctl
+ * @{
+ */
+
+/**
+ * register data use for simple/single ranging data @ref VL53L1_IOCTL_GETDATAS
+ *
+ * @warning this definition is subject to change !
+ */
+typedef VL53L1_RangingMeasurementData_t  stmvl531_range_data_t;
+
+/**
+ * IOCTL register data structure use in @ref VL53L1_IOCTL_REGISTER
+ *
+ * for convenience an ease of use full data size capability is exposed here
+ * but the @ref stmvl53l1_register_flexi can be used instead
+ */
+struct stmvl53l1_register {
+	uint32_t is_read; 	/*!< type of the access 1: read 0: write*/
+	uint32_t index;		/*!< register index */
+	uint32_t cnt;		/*!< register size shall be 1 to n */
+	int32_t status;		/*!< operation status 0 ok else error */
+
+	union reg_data_t {
+		uint8_t b;	/*!< single data byte*/
+		uint16_t w;	/*!< single data word (16 bits)*/
+		uint32_t dw;	/*!< single data dword (32 bits)*/
+		uint8_t bytes[256]; /*!< any size byte array
+		@note only effectively used array size is needed and will be
+		set/used another possible register definition is
+		@ref stmvl53l1_register_flexi  */
+	} data; /*!< data only *@warning device is big endian and
+	no endianess adaptation is performed by @ref VL53L1_IOCTL_REGISTER */
+};
+
+/**
+ * flexible size data length register access structure
+ * for use in @ref VL53L1_IOCTL_REGISTER
+ */
+struct stmvl53l1_register_flexi {
+	uint32_t is_read;	/*!< [in] type of the access 1: read 0: write*/
+	uint32_t index;		/*!< [in] register index */
+	uint32_t cnt;		/*!< [în] register size shall be 1 to n */
+	int32_t status;		/*!< [out] operation status 0 ok else error */
+	uint8_t data[];		/*!< [in/out] flexible array size data */
+	/*!< data only *@warning device is big endian and
+	no endianess adaptation is performed by @ref VL53L1_IOCTL_REGISTER */
+};
+
+
+/**
+ * parameter name in @ref stmvl53l1_parameter when using  @ref VL53L1_IOCTL_PARAMETER
+ */
+typedef enum {
+	OFFSET_PAR = 0,
+	/*!< OFFSET_PAR  @warning NOT supported yet */
+	XTALKRATE_PAR = 1,
+	/*!< XTALKRATE_PAR @warning NOT supported yet */
+	XTALKENABLE_PAR = 2,
+	/*!< XTALKENABLE_PAR @warning NOT supported yet*/
+	GPIOFUNC_PAR = 3,
+	/*!< GPIOFUNC_PAR @warning NOT supported yet */
+	LOWTHRESH_PAR = 4,
+	/*!< LOWTHRESH_PAR @warning NOT supported yet */
+	HIGHTHRESH_PAR = 5,
+	/*!< HIGHTHRESH_PAR @warning NOT supported yet */
+	VL53L1_DEVICEMODE_PAR = 6,
+	/*!< DEVICEMODE_PAR set ranging mode  \n
+	 * valid mode value :
+	 * @li 1 @a VL53L1_PRESETMODE_STANDARD_RANGING standard ranging
+	 * @li 2 @a  VL53L1_PRESETMODE_MULTI_OBJECT multiple object one roi
+	 * @li 3 @a  VL53L1_PRESETMODE_MULTI_ZONES  multiple zone and object
+	 *
+	 * @warning mode can only be set while not ranging
+	 */
+
+	VL53L1_INTERMEASUREMENT_PAR = 7,
+	/*!< INTERMEASUREMENT_PAR  NOT supported yet */
+
+	REFERENCESPADS_PAR = 8,
+	/*!< REFERENCESPADS_PAR @warning NOT supported yet */
+	REFCALIBRATION_PAR = 9,
+	/*!< REFCALIBRATION_PAR @warning NOT supported yet */
+
+	VL53L1_POLLDELAY_PAR = 10,
+	/*!< set the polling delay (msec)\n
+	 *
+	 * @note apply only when operates in polling mode  as no effect otherwise
+	 */
+	VL53L1_TIMINGBUDGET_PAR = 11,
+	/*!< VL53L1_TIMINGBUDGET_PAR
+	@ref stmvl53l1_parameter.value field is timing budget in micro second
+
+	@note the value cannot be set while ranging will set ebusy errno,
+	value set is absorbed at next range start @ref VL53L1_IOCTL_INIT
+	*/
+
+
+} stmv53l1_parameter_name_e;
+
+
+/**
+ * parameter structure use in @ref VL53L1_IOCTL_PARAMETER
+ */
+struct stmvl53l1_parameter {
+	uint32_t is_read;	/*!< [in] 1: Get 0: Set*/
+	stmv53l1_parameter_name_e name;	/*!< [in] parameter to set/get
+	see @ref stmv53l1_parameter_name_e */
+
+	int32_t value;		/*!< [in/out] value to set /get */
+	int32_t value2;		/*!< [in/out] optional 2nd value */
+	int32_t status;		/*!< [out] status of the operation */
+};
+
+
+/**
+ * roi structure use as  @ref VL53L1_IOCTL_ROI arg
+ *
+ * see @ref stmvl53l1_roi_full_t for easy to use type variable declaration
+ * required
+ */
+struct stmvl53l1_roi_t {
+	int32_t		is_read;
+	/*!<  specify roi transfer direction \n
+	 * @li 0 to get roi
+	 * @li !0 to set roi */
+	struct roi_cfg_t {
+		uint8_t NumberOfRoi;
+		/*!< Number of Rois to set/get defined\n
+		 *  0 is set can be used to return to device default roi usage
+		 *  @warning 0 in get will not return any data in UserRois!
+		*/
+		uint8_t FirstRoiToScan; /*!< not used as of now */
+		VL53L1_UserRoi_t    UserRois[1];
+		/*!< roi data array length  definition is 1 but
+		 * NumberOfRoi+ FirstRoiToScan in array are required
+		 * and will be effectively copy to/from user space
+		 */
+	} roi_cfg;
+};
+
+/**
+ * full roi struct use in @ref VL53L1_IOCTL_ROI arg
+ *
+ * @sa stmvl53l1_roi_t for detail this one
+ * make it easier to declare a fully static variable with the max roi storage
+ * capabilities.
+ */
+struct stmvl53l1_roi_full_t {
+	int32_t		is_read;
+	VL53L1_RoiConfig_t roi_cfg;
+} ;
+
+
+/*
+ * IOCTL definitions
+ */
+
+
+/**
+ * Start ranging (no argument)
+ *
+ * @note  sysfs and ioctl control are assumed mutual exclusive use
+ * control from ioctl execute with no consideration of sysfs path.
+ *
+ * @return :
+ *	@li 0 on success
+ *	@li -EBUSY if already started
+ *	@li other any possible over "st bare driver error code" (shall be positive)
+ *
+ * example user land  :
+ @code
+ int smtvl53l1_start(int fd){error
+	int rc;
+	rc= ioctl(fd, VL53L1_IOCTL_INIT,NULL);
+	if( rc ){
+		if( errno == EBUSY){
+			//the device is already started
+			ioctl_warn("already started");
+			return EBUSY;
+		}
+	}
+	if( rc ){
+		ioctl_error("%d %s", rc,strerror(errno));
+	}
+	return rc;
+}
+ @endcode
+*/
+
+#define VL53L1_IOCTL_INIT			_IO('p', 0x01)
+
+/**
+ * stop ranging (no argument)
+
+ * @note  sysfs and ioctl control are assumed mutual exclusive use
+ * control from ioctl execute action with no consideration of sysfs path.
+ *
+ * @return
+ * @li 0 on success
+ * @li -EBUSY if it was already
+ * @li other specific error code shall be >0
+ *
+ * c example userland :
+ @code
+int smtvl53l1_stop(int fd){
+	int rc;
+	rc= ioctl(fd, VL53L1_IOCTL_STOP,NULL);
+	if( rc ){
+		if( errno == EBUSY ){
+			ioctl_warn("already stopped");
+			return errno;
+		}
+		ioctl_error("%d %s", rc,strerror(errno));
+	}
+	return rc;
+}
+@endcode
+ */
+ #define VL53L1_IOCTL_STOP			_IO('p', 0x05)
+
+/**
+ * set the offset calibration
+ *  ioctl arg [IN] one byte offset
+ *  @param offset type int8_t is the offset in ??  to be set
+ */
+#define VL53L1_IOCTL_SETOFFSET		_IOW('p', 0x07, int8_t)
+
+/**
+ * get single ranging data @sa for multi zone/objet
+ *
+ * retrieve the last range data available form the device
+ *
+ * @param in/out data struct ptr of type @ref stmvl531_range_data_t
+ * it may come in but is out as of now
+ *
+ * this function succeed whatever device state (off, stopped )
+ *
+ * @warning this ioctl will not wait for a new range sample acquisition
+ * it will return what available at time it get called . Hence same data maybe
+ * returned many time when doing fast polling.\n
+ * End user must inspect the data structure (time stamp etc )to find about it\n
+ * Despite it's non "waiting" nature this ioctl may still block/sleep shortly
+ * to ensure race free usage acquiring mutex and/or locks.
+ */
+#define VL53L1_IOCTL_GETDATAS \
+			_IOWR('p', 0x0b, stmvl531_range_data_t)
+
+/**
+ * low level register access IOCTL
+ * ioctl arg [IN]/[OUT] pointer to register struct @ref stmvl53l1_register
+ * or @ref stmvl53l1_register_flexi
+ *
+ * ioctl arg [out] set back with status and data when applicable
+ */
+#define VL53L1_IOCTL_REGISTER	_IOWR('p', 0x0c, struct stmvl53l1_register)
+
+/**
+ * set or get parameter
+ *
+ * @param parameter in/out  @ref stmvl53l1_parameter
+ * @sa stmv53l1_parameter_name_e
+ *
+ * for get if ioctl fail do not check for out params it is not valid
+ * for set theirs not copy back only see ioctl status, errno to get error case
+ *
+ * @note a set parameter may not be absorbed straight aways !
+ */
+#define VL53L1_IOCTL_PARAMETER \
+			_IOWR('p', 0x0d, struct stmvl53l1_parameter)
+
+
+/**
+ * set/get roi
+ *
+ * shall only be use while device is stopped (EBUSY error otherwise)
+ * setting 0 rois stand for "disable  user define roi usage, use device default"
+ *
+ * @param roi_cfg [in/out] type @ref stmvl53l1_roi_t and
+ * @ref stmvl53l1_roi_full_t
+ * @note when getting roi the returned roi cnt is set to available number
+ * of roi in driver but  at most requested number or available one
+ * will be set in returned structure
+ * @warning the coordinate system is not usual image x,y (y down)but traditional
+ * ecludian x,y (y up)
+ *
+ * @warning once defined the user roi is kept alive until unset by user .
+ * User shall update roi when required (mode change etc ..)\n
+ * To return to default unset roi by setting none, device will return to default
+ * at next start
+ *
+ * @note roi validity is only checked at start ranging , as such invalid roi set
+ * can make start to fail
+ *
+ * @return 0 on success , see errno for error detail
+ *  @li EBUSY when trying to set roi while ranging
+ *  @li ENODEV never device get started and trying to get more rois than set
+ *  @li other errno code could be ll driver specific
+ */
+#define VL53L1_IOCTL_ROI\
+			_IOWR('p', 0x0e, struct stmvl53l1_roi_t)
+
+/**
+ * Get multi object/zone ranging data
+ *
+ * this call is non blocking and will return what available internally
+ * in all case (veen error)
+ *
+ * @param [out] multi zone range @ref VL53L1_MultiRangingData_t always update
+ * but -EFAULT error case
+ *
+ * @return 0 on success else o, error check errno
+ * @li -EFAULT fault in cpy to f/m user out range data not copyed
+ * @li -ENOEXEC active mode is not mutli-zone
+ * @li -ENODEV device is not ranging
+ * as in that case MZ data may not be fully valid
+ */
+#define VL53L1_IOCTL_MZ_DATA\
+			_IOR('p', 0x0f, VL53L1_MultiRangingData_t)
+/** @} */ /* ioctl group */
+#endif /* STMVL53L1_IF_H */
---
+++ drivers/input/misc/vl53L1/vl53l1_platform.h
@@ -0,0 +1,291 @@
+/*******************************************************************************
+* Copyright (c) 2016, STMicroelectronics - All Rights Reserved
+*
+* License terms: BSD 3-clause "New" or "Revised" License.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**
+ * @file   vl53l1_platform.h
+ *
+ * @brief  linux kernel driver OS/platform/application porting
+ */
+#ifndef _VL53L1_PLATFORM_H_
+#define _VL53L1_PLATFORM_H_
+
+/** API fct export type
+ *
+ *  N/A undefined
+ **/
+#define VL53L1_API
+
+/**
+ * time out for polling/Wait method  use in ST bare drivers
+ * even not used require for code to compile*/
+#define VL53L1_BOOT_COMPLETION_POLLING_TIMEOUT_MS     500
+#define VL53L1_RANGE_COMPLETION_POLLING_TIMEOUT_MS   1000
+#define VL53L1_TEST_COMPLETION_POLLING_TIMEOUT_MS   10000
+#define VL53L1_POLLING_DELAY_MS                         1
+
+#include "vl53l1_def.h"
+
+
+#define VL53L1_MAX_I2C_XFER_SIZE	256
+/* linux porting of bare driver */
+#define _LOG_FUNCTION_START(...) (void)0
+#define _LOG_FUNCTION_END(...) (void)0
+
+#include "linux/string.h"
+#define VL53L1_COPYSTRING(str, ...) strcpy(str, ##__VA_ARGS__)
+
+/**
+ * ST bare driver data type access macro
+ */
+#define PALDevDataGet(Dev, field) (Dev->field)
+
+#define PALDevDataSet(Dev, field, data) (Dev->field) = (data)
+
+/**
+ * ST bare LL driver data type access macro
+ */
+#define VL53L1DevStructGetLLDriverHandle(Dev) (&PALDevDataGet(Dev, LLData))
+
+
+
+/**
+ * @brief Typedef defining 8 bit unsigned char type.\n
+ * The developer should modify this to suit the platform being deployed.
+ *
+ */
+
+#ifndef bool_t
+typedef unsigned char bool_t;
+#endif
+
+
+
+
+/**
+ * VL53L1_DEV ST ll/bare driver absract type
+ *
+ * we include st base stuct within our driver data and will pass ptr to it
+ * on any call back from st layer we'll find out or struct ptr using
+ *  container_of(pdev,struct stmvl53l1_data, stdev);
+ */
+typedef VL53L1_DevData_t *VL53L1_DEV;
+
+
+/**
+ * @brief Writes the supplied byte buffer to the device
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[in]   pdata     : pointer to uint8_t (byte) buffer containing the
+ *  data to be written
+ * @param[in]   count     : number of bytes in the supplied byte buffer
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_WriteMulti(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint8_t    *pdata,
+		uint32_t   count);
+
+
+/**
+ * @brief  Reads the requested number of bytes from the device
+ *
+ * @param[in]   pdev  : pointer to device structure (device handle)
+ * @param[in]   index : uint16_t register index value
+ * @param[out]  pdata : pointer to the uint8_t (byte) buffer to store read data
+ * @param[in]   count : number of bytes to read
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_ReadMulti(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint8_t    *pdata,
+		uint32_t    count);
+
+
+/**
+ * @brief  Writes a single byte to the device
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[in]   data      : uint8_t data value to write
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_WrByte(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint8_t    data);
+
+
+/**
+ * @brief  Writes a single w(ord (16-bit unsigned) to the device
+ *
+ * Manages the big-endian nature of the device register map
+ * (first byte written is the MS byte).
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[in]   data      : uin16_t data value write
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_WrWord(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint16_t   data);
+
+
+/**
+ * @brief  Writes a single dword (32-bit unsigned) to the device
+ *
+ * Manages the big-endian nature of the device register map
+ * (first byte written is the MS byte).
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[in]   data      : uint32_t data value to write
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_WrDWord(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint32_t   data);
+
+
+
+/**
+ * @brief  Reads a single byte from the device
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index
+ * @param[out]  pdata     : pointer to uint8_t data value
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ *
+ */
+
+VL53L1_API VL53L1_Error VL53L1_RdByte(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint8_t    *pdata);
+
+
+/**
+ * @brief  Reads a single word (16-bit unsigned) from the device
+ *
+ * Manages the big-endian nature of the device (first byte read is the MS byte).
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[out]  pdata     : pointer to uint16_t data value
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_RdWord(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint16_t   *pdata);
+
+
+/**
+ * @brief  Reads a single dword (32-bit unsigned) from the device
+ *
+ * Manages the big-endian nature of the device (first byte read is the MS byte).
+ *
+ * @param[in]   pdev      : pointer to device structure (device handle)
+ * @param[in]   index     : uint16_t register index value
+ * @param[out]  pdata     : pointer to uint32_t data value
+ *
+ * @return   VL53L1_ERROR_NONE    Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_RdDWord(
+		VL53L1_DEV pdev,
+		uint16_t   index,
+		uint32_t   *pdata);
+
+/**
+ * @brief Register "wait for value" polling routine
+ * Poll for
+ *
+ * @param[in]   pdev          : pointer to device structure (device handle)
+ * @param[in]   timeout_ms    : timeout in [ms]
+ * @param[in]   index         : uint16_t register index value
+ * @param[in]   value         : value to wait for
+ * @param[in]   mask          : mask to be applied before comparison with value
+ * @param[in]   poll_delay_ms : polling delay been each read transaction in [ms]
+ *
+ * @return  VL53L1_ERROR_NONE     Success
+ * @return  "Other error code"    See ::VL53L1_Error
+ */
+
+VL53L1_API VL53L1_Error VL53L1_WaitValueMaskEx(
+		VL53L1_DEV pdev,
+		uint32_t   timeout_ms,
+		uint16_t   index,
+		uint8_t    value,
+		uint8_t    mask,
+		uint32_t   poll_delay_ms);
+
+/* we do not support waiting us make an error */
+#define VL53L1_WaitUs(...) -1
+/* we do not support waiting ms make an error */
+#define VL53L1_WaitMs(...) -1
+
+/**
+ * we do not support Get tick count
+ * the implementation will hang kernel with BUG_ON() if called
+ */
+void VL53L1_GetTickCount(void *);
+
+
+#include "vl53l1_platform_log.h"
+
+#endif
+
