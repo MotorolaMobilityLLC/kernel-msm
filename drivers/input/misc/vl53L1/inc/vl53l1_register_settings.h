a02ec61fda275d526f100ba315fd0c0a323ee7c4
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2016-11-12 21:57:02 -0600

    input misc vl53L1:add ST driver code
    Initial vl53L1 driver code from ST
      release_1.0.0_fw398
    
    Change-Id: I3b204858b5e7f4db422be6fd72b8f1bb2a193cc8
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/919394
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/input/misc/vl53L1/inc/vl53l1_register_settings.h
@@ -0,0 +1,250 @@
+
+/* 
+* Copyright (c) 2016, STMicroelectronics - All Rights Reserved 
+* 
+* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license' 
+* or 'BSD 3-clause "New" or "Revised" License' , at your option. 
+* 
+******************************************************************************** 
+* 
+* 'STMicroelectronics Proprietary license' 
+* 
+******************************************************************************** 
+* 
+* License terms: STMicroelectronics Proprietary in accordance with licensing terms at www.st.com/sla0044
+*
+* STMicroelectronics confidential 
+* Reproduction and Communication of this document is strictly prohibited unless 
+* specifically authorized in writing by STMicroelectronics.
+*  
+* 
+******************************************************************************** 
+* 
+* Alternatively, VL53L1 Core may be distributed under the terms of 
+* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones 
+* mentioned above : 
+* 
+******************************************************************************** 
+* 
+* License terms: BSD 3-clause "New" or "Revised" License. 
+* 
+* Redistribution and use in source and binary forms, with or without 
+* modification, are permitted provided that the following conditions are met: 
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this 
+* list of conditions and the following disclaimer. 
+* 
+* 2. Redistributions in binary form must reproduce the above copyright notice, 
+* this list of conditions and the following disclaimer in the documentation 
+* and/or other materials provided with the distribution. 
+* 
+* 3. Neither the name of the copyright holder nor the names of its contributors 
+* may be used to endorse or promote products derived from this software 
+* without specific prior written permission. 
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+* 
+* 
+******************************************************************************** 
+* 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef _VL53L1_REGISTER_SETTINGS_H_
+#define _VL53L1_REGISTER_SETTINGS_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define VL53L1_DEF_00119  0x00
+#define VL53L1_DEF_00115    0x01
+#define VL53L1_DEF_00094    0x02
+
+
+
+
+
+
+
+
+
+
+
+#define VL53L1_DEF_00116        (0x00 << 2)
+#define VL53L1_DEF_00122          (0x01 << 2)
+#define VL53L1_DEF_01381    (0x02 << 2)
+#define VL53L1_DEF_00123     (0x03 << 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define VL53L1_DEF_00130          0xF0
+#define VL53L1_DEF_00067          0x0F
+
+#define VL53L1_DEF_00131             0x02
+
+#define VL53L1_DEF_00132          0x1F
+#define VL53L1_DEF_01382     0x20
+#define VL53L1_DEF_01383     0x40
+#define VL53L1_DEF_01384   0x80
+
+
+
+
+#define VL53L1_DEF_01385                0x29
+	
+
+#define VL53L1_DEF_01386                            0x00
+#define VL53L1_DEF_01387                         0x00
+#define VL53L1_DEF_01388                     0x00
+
+#define VL53L1_DEF_01389                      0x08
+#define VL53L1_DEF_01390                      0x10
+#define VL53L1_DEF_01391                     0x18
+#define VL53L1_DEF_01392                     0x20
+#define VL53L1_DEF_01393                           0x40
+
+
+
+#define VL53L1_DEF_01394                      0x1F
+
+
+
+#define VL53L1_DEF_01395              0x00
+#define VL53L1_DEF_00118            0x01
+#define VL53L1_DEF_00107    0x02
+
+
+
+#define VL53L1_DEF_01396               0x00
+#define VL53L1_DEF_01397              0x01
+#define VL53L1_DEF_01398           0x02
+#define VL53L1_DEF_01399               0x03
+#define VL53L1_DEF_00106        0x20
+
+
+
+#define VL53L1_DEF_00114                          0x01
+#define VL53L1_DEF_01400                          0x02
+
+
+
+#define VL53L1_DEF_00108						    0x01
+#define VL53L1_DEF_00109                     0x02
+#define VL53L1_DEF_01401              0x04
+#define VL53L1_DEF_00110                         0x08
+#define VL53L1_DEF_00111                         0x10
+#define VL53L1_DEF_00117                          0x20
+#define VL53L1_DEF_00112                          0x40
+#define VL53L1_DEF_00113                        0x80
+
+
+
+#define VL53L1_DEF_01402                0x20
+#define VL53L1_DEF_01403               0x10
+
+#define VL53L1_DEF_01404               0x00
+#define VL53L1_DEF_01405            0x01
+#define VL53L1_DEF_01406               0x02
+#define VL53L1_DEF_01407            0x03
+
+
+
+
+
+
+
+
+
+
+#define VL53L1_DEF_00154              0x45
+#define VL53L1_DEF_01408     0x05
+#define VL53L1_DEF_01409  0x55
+#define VL53L1_DEF_01410    0x25
+#define VL53L1_DEF_00153          0xF5
+
+
+#define VL53L1_DEF_01411                  0x6C
+
+
+
+
+
+#endif
+
+
+
+
+
