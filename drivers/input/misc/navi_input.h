5e4a018fc18b4427af2b92fc4ac279ac6f629c33
Yan Zhang <zhangy26@motorola.com>
2016-11-14 15:41:20 +0800

    input: Initial egis sensor driver
    Adding Egis driver for vendor Egis fingerprint sensor et320.
    
    Change-Id: If909a280e0aa99d5904f57ec222eb84fa67527db
    Signed-off-by: Yan Zhang <zhangy26@motorola.com>
    Reviewed-on: https://gerrit.mot.com/919465
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Reviewed-by: xiangpo zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ drivers/input/misc/navi_input.h
@@ -0,0 +1,6 @@
+
+
+void uinput_egis_init(struct etspi_data *etspi);
+void uinput_egis_destroy(struct etspi_data *etspi);
+void sysfs_egis_init(struct etspi_data *etspi);
+void sysfs_egis_destroy(struct etspi_data *etspi);
