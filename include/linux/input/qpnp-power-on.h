a00ed99af51674aacc0529afceca0995fbb16fb8
Kenny Kessler <kenny.kessler@motorola.com>
2016-11-17 16:35:37 -0600

    input: qpnp: power-on: Track Resin
    Keep track of the Resin Key so that it can be used
    with the HW Reset to Force a Watchdog.
    
    Change-Id: Ib694b55cfd35ddfa3de742a10a6dff6c1cba1489
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/921443
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -65,8 +65,12 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_OEM_MAX		= 0x3f,
 };
 
+#define RESET_EXTRA_RESET_KUNPOW_REASON        BIT(9)
 #define RESET_EXTRA_PANIC_REASON       BIT(3)
 #define RESET_EXTRA_REBOOT_BL_REASON   BIT(2)
+
+#define QPNP_PON_KEY_RESIN_BIT         BIT(1)
+extern int qpnp_pon_key_status;
 
 #ifdef CONFIG_INPUT_QPNP_POWER_ON
 int qpnp_pon_system_pwr_off(enum pon_power_off_type type);
