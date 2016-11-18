4cf9f31831831969e304ab1d153b07ef00b06ed5
Yoon (Seungyoon) Lee <w22905@motorola.com>
2016-11-18 17:55:19 -0600

    ASoC: msm8952: control micbias over dt
    8953 gets two different products and they have different
    micbias configurations even in single product per hw rev.
    move micbias configuration to kernel dt to clean out the code
    and have more flexibility for later products. (if any)
    
    Change-Id: I6418854eefd73e733183933b15f91b2df3e95121
    Signed-off-by: Yoon (Seungyoon) Lee <w22905@motorola.com>
    Reviewed-on: https://gerrit.mot.com/921861
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/msm8952-slimbus.c
+++ sound/soc/msm/msm8952-slimbus.c
@@ -3885,29 +3885,6 @@ static struct snd_soc_dapm_route marley_audio_routes[] = {
 	{"Slim2 Capture", NULL, "MCLK"},
 
 };
-
-static struct snd_soc_dapm_route marley_mic_routes[] = {
-	{"IN1AL", NULL, "MICBIAS1A"}, /* Mic 1 */
-	{"IN1AR", NULL, "MICBIAS1B"}, /* Mic 3 */
-	{"IN2L", NULL, "MICBIAS2B"},  /* Mic 2 */
-	{"IN1BR", NULL, "MICBIAS2A"}, /* Headset mic */
-};
-
-static struct snd_soc_dapm_route marley_albus_p1a_mic_routes[] = {
-	/* Rev 05 */
-	{"IN1AL", NULL, "MICBIAS1A"}, /* Mic 1 */
-	{"IN2R", NULL, "MICBIAS1B"},  /* Mic 3 */
-	{"IN2L", NULL, "MICBIAS2B"},  /* Mic 2 */
-	{"IN1BR", NULL, "MICBIAS2A"}, /* Headset mic */
-};
-
-static struct snd_soc_dapm_route marley_albus_mic_routes[] = {
-	/* Rev 06 */
-	{"IN1AL", NULL, "MICBIAS2A"}, /* Mic 1 */
-	{"IN2R", NULL, "MICBIAS2B"},  /* Mic 3 */
-	{"IN2L", NULL, "MICBIAS2A"},  /* Mic 2 */
-	{"IN1BR", NULL, "MICBIAS1B"}, /* Headset mic */
-};
 #endif
 
 static int msm8952_codec_event_cb(struct snd_soc_codec *codec,
@@ -4201,25 +4178,6 @@ int marley_dai_init(struct snd_soc_pcm_runtime *rtd)
 
 	albus_audio = of_property_read_bool(card->dev->of_node,
 					    "qcom,albus-audio");
-	if (albus_audio) {
-		if (!strncmp(card->name, "msm8952-marley-cardp1a", 22)) {
-			ret = snd_soc_dapm_add_routes(dapm,
-				marley_albus_p1a_mic_routes,
-				ARRAY_SIZE(marley_albus_p1a_mic_routes));
-		} else {
-			ret = snd_soc_dapm_add_routes(dapm,
-				marley_albus_mic_routes,
-				ARRAY_SIZE(marley_albus_mic_routes));
-		}
-	} else {
-		ret = snd_soc_dapm_add_routes(dapm,
-			marley_mic_routes,
-			ARRAY_SIZE(marley_mic_routes));
-	}
-	if (ret != 0) {
-		dev_err(codec->dev, "Failed to add mic routes %d\n", ret);
-		return ret;
-	}
 
 	/* Ensures that GPIO3 is set to an output clock. */
 	snd_soc_write(codec, 0x1704, 0);
@@ -4733,6 +4691,15 @@ static int msm8952_asoc_machine_probe(struct platform_device *pdev)
 			"qcom,audio-routing");
 	if (ret)
 		goto err;
+#endif
+
+#ifdef CONFIG_SND_SOC_MARLEY
+	ret = snd_soc_of_parse_audio_routing(card, "cs47l35,dapm-routing");
+	if (ret) {
+		dev_err(&pdev->dev, "parse dapm-routing failed, err:%d\n",
+			ret);
+		goto err;
+	}
 #endif
 	ret = msm8952_populate_dai_link_component_of_node(card);
 	if (ret) {
