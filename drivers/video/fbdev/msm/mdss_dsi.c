ab463a372a1079aa65dd368c64aa84a13e39b785
Abhinav Kumar <abhinavk@codeaurora.org>
2016-11-17 19:03:59 -0800

    Add support for T_CLK_PRE/POST calculations.
    Add support for T_CLK_PRE and T_CLK_POST calculations for
    DSI_PHY_REV_20 and DSI_PHY_REV_30
    
    Moto-CRs-fixed: (CR)
    QCOM SR# 02681405
    
    Change-Id: I8623e23431d08a6757b8685907671858419b0890
    Reviewed-on: https://gerrit.mot.com/928654
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -2728,8 +2728,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 	case MDSS_EVENT_LINK_READY:
 		if (ctrl_pdata->refresh_clk_rate)
 			rc = mdss_dsi_clk_refresh(pdata,
-				ctrl_pdata->update_phy_timing);
-
+					ctrl_pdata->update_phy_timing);
 		rc = mdss_dsi_on(pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
