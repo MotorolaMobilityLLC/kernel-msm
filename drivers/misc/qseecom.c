d7f0deb3a4760c193afb35c0da3a269831874cda
Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>
2016-11-22 17:24:46 +0530

    qseecom: remove entry from qseecom_registered_app_list
    In an error handling case, the QSEECOM_IOCTL_LOAD_APP_REQ ioctl
    freed the entry for new TA, but didn't removed it from
    qseecom_registered_app_list. Make change to remove it.
    
    Change-Id: Id681fbf3c923027d3db875d506cbe3f971919a8d
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1323,6 +1323,7 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 	struct qseecom_command_scm_resp resp;
 	struct qseecom_check_app_ireq req;
 	struct qseecom_load_app_ireq load_req;
+	bool first_time = false;
 
 	/* Copy the relevant information needed for loading the image */
 	if (copy_from_user(&load_img_req,
@@ -1369,6 +1370,7 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 		&qseecom.registered_app_list_lock, flags);
 		ret = 0;
 	} else {
+		first_time = true;
 		pr_warn("App (%s) does'nt exist, loading apps for first time\n",
 			(char *)(load_img_req.img_name));
 		/* Get the handle of the shared fd */
@@ -1469,8 +1471,15 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 	load_img_req.app_id = app_id;
 	if (copy_to_user(argp, &load_img_req, sizeof(load_img_req))) {
 		pr_err("copy_to_user failed\n");
-		kzfree(entry);
 		ret = -EFAULT;
+		if (first_time == true) {
+			spin_lock_irqsave(
+				&qseecom.registered_app_list_lock, flags);
+			list_del(&entry->list);
+			spin_unlock_irqrestore(
+				&qseecom.registered_app_list_lock, flags);
+			kzfree(entry);
+		}
 	}
 
 loadapp_err:
