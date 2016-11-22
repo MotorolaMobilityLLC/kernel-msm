8cf992ffe622429bf2f301afc237aa9a0c6b0811
Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>
2016-11-22 17:24:46 +0530

    qseecom: remove entry from qseecom_registered_app_list
    In an error handling case, the QSEECOM_IOCTL_LOAD_APP_REQ ioctl
    freed the entry for new TA, but didn't removed it from
    qseecom_registered_app_list. Make change to remove it.
    
    Mot-CRs-fixed:(CR)
    CVE-Fixed:CVE-2016-8480
    
    Change-Id: Iafc81ccadf205ecd60f75d87a866c2e1e0b89e68
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>
    Signed-off-by: amarenr <amarenr@motorola.com>
    Reviewed-on: https://gerrit.mot.com/939294
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit c3a4fb8dd7ab70acbb46b9f96f894112bb599ed8)

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1840,6 +1840,7 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 	struct qseecom_command_scm_resp resp;
 	struct qseecom_check_app_ireq req;
 	struct qseecom_load_app_ireq load_req;
+	bool first_time = false;
 	struct qseecom_load_app_64bit_ireq load_req_64bit;
 	void *cmd_buf = NULL;
 	size_t cmd_len;
@@ -1914,6 +1915,7 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 		&qseecom.registered_app_list_lock, flags);
 		ret = 0;
 	} else {
+		first_time = true;
 		pr_warn("App (%s) does'nt exist, loading apps for first time\n",
 			(char *)(load_img_req.img_name));
 		/* Get the handle of the shared fd */
@@ -2034,8 +2036,15 @@ static int qseecom_load_app(struct qseecom_dev_handle *data, void __user *argp)
 	load_img_req.app_id = app_id;
 	if (copy_to_user(argp, &load_img_req, sizeof(load_img_req))) {
 		pr_err("copy_to_user failed\n");
-		kzfree(entry);
 		ret = -EFAULT;
+		if (first_time == true) {
+			spin_lock_irqsave(
+				&qseecom.registered_app_list_lock, flags);
+			list_del(&entry->list);
+			spin_unlock_irqrestore(
+				&qseecom.registered_app_list_lock, flags);
+			kzfree(entry);
+		}
 	}
 
 loadapp_err:
