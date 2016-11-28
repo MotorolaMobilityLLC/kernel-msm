57d9245e30fc9390111cd806cd01e5bc17ce284b
Sanjay Singh <sisanj@codeaurora.org>
2016-11-28 16:57:13 +0530

    msm: vidc: WARN_ON() reveals fuction addresses
    There is a security vulnerability where function addresses are
    printed in kernel message if WARN_ON() is invoked implicitly.
    WARN_ON() call is made explicit to avoid this issue.
    
    Change-Id: If75581803adf62cb9bda3784ad1d4f4088e0d797
    Signed-off-by: Sanjay Singh <sisanj@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -267,7 +267,7 @@ static int venus_hfi_acquire_regulator(struct regulator_info *rinfo)
 					rinfo->name);
 		}
 	}
-	WARN_ON(!regulator_is_enabled(rinfo->regulator));
+	WARN_ON(!regulator_is_enabled(rinfo->regulator) && (msm_vidc_debug & VIDC_INFO));
 	return rc;
 }
 
@@ -4386,7 +4386,7 @@ static int venus_hfi_disable_regulator(struct regulator_info *rinfo)
 disable_regulator_failed:
 
 	/* Bring attention to this issue */
-	WARN_ON(1);
+	WARN_ON(msm_vidc_debug & VIDC_INFO);
 	return rc;
 }
 
