ad76da4cdd9758c98ba6e8524f7b35c127277113
Doug Zobel <dzobel1@motorola.com>
2016-11-28 15:26:43 -0600

    msm flash: Handle different LED current limit for front flash
    Some products have both front and rear camera flashes driven by the same
    PMIC driver.  The appropraite LEDs are MUXed on depending on which camera
    is active.  The 'alt' dts parameters are added to configure different current
    limits for the front LEDs.  These are optional parameters.  If they are not
    specified then the default limits are used for both front and rear LEDs.
    
    Change-Id: I63f95c22f9ea18b30ad26fd0ec7b4fdeda902a11
    Signed-off-by: Doug Zobel <dzobel1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/926341
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/uapi/media/msm_cam_sensor.h
+++ include/uapi/media/msm_cam_sensor.h
@@ -173,6 +173,12 @@ enum cci_i2c_master_t {
 	MASTER_MAX,
 };
 
+enum flash_position {
+	REAR_FLASH,
+	FRONT_FLASH,
+	INVALID_FLASH,
+};
+
 struct msm_camera_i2c_array_write_config {
 	struct msm_camera_i2c_reg_setting conf_array;
 	uint16_t slave_addr;
@@ -545,6 +551,8 @@ struct msm_flash_cfg_data_t {
 	enum msm_flash_cfg_type_t cfg_type;
 	int32_t flash_current[MAX_LED_TRIGGERS];
 	int32_t flash_duration[MAX_LED_TRIGGERS];
+	enum flash_position position;
+
 	union {
 		struct msm_flash_init_info_t *flash_init_info;
 		struct msm_camera_i2c_reg_setting_array *settings;
