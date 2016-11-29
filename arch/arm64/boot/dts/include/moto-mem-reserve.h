84d49aa81536c390021e4b2524ba2ae78adf0cd9
Chris Fries <cfries@motorola.com>
2016-11-29 10:25:40 -0600

    arm/dt: 8998: Add ramoops region
    Add ramoops region for storing kernel log.  tzlog and wdog
    context are not yet available to log.
    
    Signed-off-by: Chris Fries <cfries@motorola.com>
    Change-Id: I90b14a66897f01369b3110067a3dbf4481bcf298
    Reviewed-on: https://gerrit.mot.com/924199
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted

---
+++ arch/arm64/boot/dts/include/moto-mem-reserve.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __MOTO_MEM_RESERVE_H
+#define __MOTO_MEM_RESERVE_H
+
+/*
+ * todo: implement for msm8998. WDOG and TZLOG aren't yet available.
+ */
+#define WDOG_CPUCTX_SIZE_PERCPU	0x5800 /* sizeof(struct msm_wdog_cpuctx) */
+#define WDOG_CPUCTX_SIZE_8CPUS	(WDOG_CPUCTX_SIZE_PERCPU * 8)
+
+#define WDOG_CPUCTX_END_ADDR	0xaf000000
+
+#define WDOG_CPUCTX_SIZE_8998	WDOG_CPUCTX_SIZE_8CPUS
+#define WDOG_CPUCTX_BASE_8998	(WDOG_CPUCTX_END_ADDR - WDOG_CPUCTX_SIZE_8CPUS)
+
+#define TZLOG_BCK_SIZE		0x2000
+#define TZLOG_BCK_BASE_8998	(WDOG_CPUCTX_BASE_8998 - TZLOG_BCK_SIZE)
+
+#endif /* __MOTO_MEM_RESERVE_H */
