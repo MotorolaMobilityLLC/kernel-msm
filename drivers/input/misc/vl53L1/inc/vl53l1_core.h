d1167a93e81838da08730a7fa84e969db7e3b669
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2016-12-02 10:39:54 -0600

    input:misc:vl53L1: update ST vl53l1 driver
    Update VL53L1_LinuxDriver_Prelim_cut11(11/28/2016)
    Implement following modes:
    - RANGING: This uses the Histograms and is the recommended
      mode. Typical timing budget is 16msec
    - MULTIZONES_SCANNING: This allows configuration of individual
      ROIs. Typical timing budget is 16msec
    - LITE_RANGING: This is legacy mode.Typical timing budget is
      16msec
    
    Change-Id: I98ff56c640dfdd71bc1c158af32f877d8f2cce97
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/926912
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/misc/vl53L1/inc/vl53l1_core.h
+++ drivers/input/misc/vl53L1/inc/vl53l1_core.h
@@ -1,61 +1,61 @@
 
-/* 
-* Copyright (c) 2016, STMicroelectronics - All Rights Reserved 
-* 
-* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license' 
-* or 'BSD 3-clause "New" or "Revised" License' , at your option. 
-* 
-******************************************************************************** 
-* 
-* 'STMicroelectronics Proprietary license' 
-* 
-******************************************************************************** 
-* 
+/*
+* Copyright (c) 2016, STMicroelectronics - All Rights Reserved
+*
+* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license'
+* or 'BSD 3-clause "New" or "Revised" License' , at your option.
+*
+********************************************************************************
+*
+* 'STMicroelectronics Proprietary license'
+*
+********************************************************************************
+*
 * License terms: STMicroelectronics Proprietary in accordance with licensing terms at www.st.com/sla0044
 *
-* STMicroelectronics confidential 
-* Reproduction and Communication of this document is strictly prohibited unless 
+* STMicroelectronics confidential
+* Reproduction and Communication of this document is strictly prohibited unless
 * specifically authorized in writing by STMicroelectronics.
-*  
-* 
-******************************************************************************** 
-* 
-* Alternatively, VL53L1 Core may be distributed under the terms of 
-* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones 
-* mentioned above : 
-* 
-******************************************************************************** 
-* 
-* License terms: BSD 3-clause "New" or "Revised" License. 
-* 
-* Redistribution and use in source and binary forms, with or without 
-* modification, are permitted provided that the following conditions are met: 
-* 
-* 1. Redistributions of source code must retain the above copyright notice, this 
-* list of conditions and the following disclaimer. 
-* 
-* 2. Redistributions in binary form must reproduce the above copyright notice, 
-* this list of conditions and the following disclaimer in the documentation 
-* and/or other materials provided with the distribution. 
-* 
-* 3. Neither the name of the copyright holder nor the names of its contributors 
-* may be used to endorse or promote products derived from this software 
-* without specific prior written permission. 
-* 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-* 
-* 
-******************************************************************************** 
-* 
+*
+*
+********************************************************************************
+*
+* Alternatively, VL53L1 Core may be distributed under the terms of
+* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones
+* mentioned above :
+*
+********************************************************************************
+*
+* License terms: BSD 3-clause "New" or "Revised" License.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*
+********************************************************************************
+*
 */
 
 
@@ -96,10 +96,7 @@
 #ifndef _VL53L1_CORE_H_
 #define _VL53L1_CORE_H_
 
-#include "vl53l1_ll_def.h"
 #include "vl53l1_platform.h"
-#include "vl53l1_register_structs.h"
-#include "vl53l1_hist_structs.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -113,7 +110,7 @@ extern "C" {
 
 
 
-void VL53L1_FCTN_00026(
+void VL53L1_FCTN_00033(
 	VL53L1_DEV         Dev);
 
 
@@ -125,9 +122,9 @@ void VL53L1_FCTN_00026(
 
 
 
-void VL53L1_FCTN_00030(
+void VL53L1_FCTN_00038(
 	VL53L1_DEV         Dev,
-	VL53L1_DeviceState VL53L1_PRM_00065);
+	VL53L1_DeviceState VL53L1_PRM_00069);
 
 
 
@@ -140,7 +137,7 @@ void VL53L1_FCTN_00030(
 
 
 
-VL53L1_Error VL53L1_FCTN_00071(
+VL53L1_Error VL53L1_FCTN_00091(
 	VL53L1_DEV         Dev);
 
 
@@ -155,7 +152,7 @@ VL53L1_Error VL53L1_FCTN_00071(
 
 
 
-VL53L1_Error VL53L1_FCTN_00081(
+VL53L1_Error VL53L1_FCTN_00104(
 	VL53L1_DEV         Dev);
 
 
@@ -169,7 +166,7 @@ VL53L1_Error VL53L1_FCTN_00081(
 
 
 
-VL53L1_Error VL53L1_FCTN_00072(
+VL53L1_Error VL53L1_FCTN_00092(
 	VL53L1_DEV         Dev);
 
 
@@ -183,7 +180,7 @@ VL53L1_Error VL53L1_FCTN_00072(
 
 
 
-void VL53L1_FCTN_00091(
+void VL53L1_FCTN_00115(
 	VL53L1_system_results_t      *pdata);
 
 
@@ -195,10 +192,84 @@ void VL53L1_FCTN_00091(
 
 
 
+void VL53L1_FCTN_00079(
+	uint8_t                 VL53L1_PRM_00020,
+	VL53L1_zone_results_t  *pdata);
 
-void VL53L1_FCTN_00034(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00113(
+	uint8_t   even_bin0,
+	uint8_t   even_bin1,
+	uint8_t   even_bin2,
+	uint8_t   even_bin3,
+	uint8_t   even_bin4,
+	uint8_t   even_bin5,
+	uint8_t   odd_bin0,
+	uint8_t   odd_bin1,
+	uint8_t   odd_bin2,
+	uint8_t   odd_bin3,
+	uint8_t   odd_bin4,
+	uint8_t   odd_bin5,
+	VL53L1_histogram_config_t  *pdata);
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00022(
 	int32_t                      bin_value,
-	uint16_t                     VL53L1_PRM_00110,
+	uint16_t                     VL53L1_PRM_00139,
 	VL53L1_histogram_bin_data_t *pdata);
 
 
@@ -210,9 +281,10 @@ void VL53L1_FCTN_00034(
 
 
 
-void VL53L1_FCTN_00035(
+
+void VL53L1_FCTN_00042(
 	uint32_t                       bin_value,
-	uint16_t                       VL53L1_PRM_00110,
+	uint16_t                       VL53L1_PRM_00139,
 	VL53L1_xtalk_histogram_data_t *pdata);
 
 
@@ -225,9 +297,23 @@ void VL53L1_FCTN_00035(
 
 
 
+void VL53L1_FCTN_00116(
+		VL53L1_xtalk_histogram_data_t *pxtalk,
+		VL53L1_histogram_bin_data_t   *phist);
 
 
-void VL53L1_FCTN_00092(
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00117(
 	uint16_t    ip_value,
 	uint16_t    count,
 	uint8_t    *pbuffer);
@@ -245,7 +331,7 @@ void VL53L1_FCTN_00092(
 
 
 
-uint16_t VL53L1_FCTN_00082(
+uint16_t VL53L1_FCTN_00105(
 	uint16_t    count,
 	uint8_t    *pbuffer);
 
@@ -261,7 +347,7 @@ uint16_t VL53L1_FCTN_00082(
 
 
 
-void VL53L1_FCTN_00093(
+void VL53L1_FCTN_00118(
 	int16_t     ip_value,
 	uint16_t    count,
 	uint8_t    *pbuffer);
@@ -279,7 +365,7 @@ void VL53L1_FCTN_00093(
 
 
 
-int16_t VL53L1_FCTN_00094(
+int16_t VL53L1_FCTN_00119(
 	uint16_t    count,
 	uint8_t    *pbuffer);
 
@@ -295,7 +381,7 @@ int16_t VL53L1_FCTN_00094(
 
 
 
-void VL53L1_FCTN_00095(
+void VL53L1_FCTN_00120(
 	uint32_t    ip_value,
 	uint16_t    count,
 	uint8_t    *pbuffer);
@@ -313,7 +399,7 @@ void VL53L1_FCTN_00095(
 
 
 
-uint32_t VL53L1_FCTN_00083(
+uint32_t VL53L1_FCTN_00106(
 	uint16_t    count,
 	uint8_t    *pbuffer);
 
@@ -333,7 +419,7 @@ uint32_t VL53L1_FCTN_00083(
 
 
 
-uint32_t VL53L1_FCTN_00096(
+uint32_t VL53L1_FCTN_00121(
 	uint16_t    count,
 	uint8_t    *pbuffer,
 	uint32_t    bit_mask,
@@ -352,7 +438,7 @@ uint32_t VL53L1_FCTN_00096(
 
 
 
-void VL53L1_FCTN_00097(
+void VL53L1_FCTN_00122(
 	int32_t     ip_value,
 	uint16_t    count,
 	uint8_t    *pbuffer);
@@ -370,7 +456,7 @@ void VL53L1_FCTN_00097(
 
 
 
-int32_t VL53L1_FCTN_00098(
+int32_t VL53L1_FCTN_00123(
 	uint16_t    count,
 	uint8_t    *pbuffer);
 
@@ -386,41 +472,41 @@ int32_t VL53L1_FCTN_00098(
 
 
 
-VL53L1_Error VL53L1_FCTN_00022(
-	VL53L1_DEV     Dev,
-	uint8_t        VL53L1_PRM_00165);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-VL53L1_Error VL53L1_FCTN_00099(
-	VL53L1_DEV         Dev,
-	uint8_t            value);
-
-
-
-
-
-
-
-
-
-
-
-
 VL53L1_Error VL53L1_FCTN_00029(
+	VL53L1_DEV     Dev,
+	uint8_t        VL53L1_PRM_00206);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00124(
+	VL53L1_DEV         Dev,
+	uint8_t            value);
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00037(
 	VL53L1_DEV         Dev);
 
 
@@ -436,7 +522,7 @@ VL53L1_Error VL53L1_FCTN_00029(
 
 
 
-VL53L1_Error VL53L1_FCTN_00028(
+VL53L1_Error VL53L1_FCTN_00036(
 	VL53L1_DEV         Dev);
 
 
@@ -454,7 +540,7 @@ VL53L1_Error VL53L1_FCTN_00028(
 
 
 
-VL53L1_Error VL53L1_FCTN_00100(
+VL53L1_Error VL53L1_FCTN_00125(
 	VL53L1_DEV         Dev,
 	uint8_t            value);
 
@@ -475,7 +561,7 @@ VL53L1_Error VL53L1_FCTN_00100(
 
 
 
-VL53L1_Error VL53L1_FCTN_00015(
+VL53L1_Error VL53L1_FCTN_00019(
 	VL53L1_DEV         Dev);
 
 
@@ -492,7 +578,7 @@ VL53L1_Error VL53L1_FCTN_00015(
 
 
 
-VL53L1_Error VL53L1_FCTN_00101(
+VL53L1_Error VL53L1_FCTN_00126(
 	VL53L1_DEV         Dev);
 
 
@@ -507,7 +593,7 @@ VL53L1_Error VL53L1_FCTN_00101(
 
 
 
-VL53L1_Error VL53L1_FCTN_00024(
+VL53L1_Error VL53L1_FCTN_00031(
 	VL53L1_DEV         Dev);
 
 
@@ -522,7 +608,7 @@ VL53L1_Error VL53L1_FCTN_00024(
 
 
 
-VL53L1_Error VL53L1_FCTN_00102(
+VL53L1_Error VL53L1_FCTN_00127(
 	VL53L1_DEV         Dev);
 
 
@@ -543,9 +629,9 @@ VL53L1_Error VL53L1_FCTN_00102(
 
 
 
-uint32_t VL53L1_FCTN_00021(
-	uint16_t VL53L1_PRM_00073,
-	uint8_t  VL53L1_PRM_00040);
+uint32_t VL53L1_FCTN_00028(
+	uint16_t VL53L1_PRM_00098,
+	uint8_t  VL53L1_PRM_00041);
 
 
 
@@ -557,24 +643,8 @@ uint32_t VL53L1_FCTN_00021(
 
 
 
-uint32_t VL53L1_FCTN_00085(
-	uint16_t VL53L1_PRM_00073);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-uint32_t VL53L1_FCTN_00104(
-	uint16_t VL53L1_PRM_00073);
+uint32_t VL53L1_FCTN_00109(
+	uint16_t VL53L1_PRM_00098);
 
 
 
@@ -588,8 +658,24 @@ uint32_t VL53L1_FCTN_00104(
 
 
 
-uint16_t VL53L1_FCTN_00105(
-	uint32_t  VL53L1_PRM_00041,
+
+uint32_t VL53L1_FCTN_00129(
+	uint16_t VL53L1_PRM_00098);
+
+
+
+
+
+
+
+
+
+
+
+
+
+uint16_t VL53L1_FCTN_00130(
+	uint32_t  VL53L1_PRM_00042,
     uint32_t  macro_period_ns);
 
 
@@ -602,7 +688,7 @@ uint16_t VL53L1_FCTN_00105(
 
 
 
-uint16_t VL53L1_FCTN_00106(
+uint16_t VL53L1_FCTN_00131(
 	uint32_t timeout_mclks);
 
 
@@ -616,7 +702,7 @@ uint16_t VL53L1_FCTN_00106(
 
 
 
-uint32_t VL53L1_FCTN_00084(
+uint32_t VL53L1_FCTN_00108(
 	uint16_t encoded_timeout);
 
 
@@ -637,9 +723,9 @@ uint32_t VL53L1_FCTN_00084(
 
 
 VL53L1_Error  VL53L1_FCTN_00005(
+	uint32_t                VL53L1_PRM_00006,
 	uint32_t                VL53L1_PRM_00007,
-	uint32_t                VL53L1_PRM_00008,
-	uint16_t                VL53L1_PRM_00073,
+	uint16_t                VL53L1_PRM_00098,
 	VL53L1_timing_config_t *ptiming);
 
 
@@ -653,8 +739,8 @@ VL53L1_Error  VL53L1_FCTN_00005(
 
 
 
-uint8_t VL53L1_FCTN_00107(
-	uint8_t vcsel_period_pclks);
+uint8_t VL53L1_FCTN_00132(
+	uint8_t VL53L1_PRM_00311);
 
 
 
@@ -667,7 +753,7 @@ uint8_t VL53L1_FCTN_00107(
 
 
 
-uint8_t VL53L1_FCTN_00103(
+uint8_t VL53L1_FCTN_00128(
 	uint8_t vcsel_period_reg);
 
 
@@ -683,7 +769,7 @@ uint8_t VL53L1_FCTN_00103(
 
 
 
-uint32_t VL53L1_FCTN_00108(
+uint32_t VL53L1_FCTN_00133(
 	uint8_t  *pbuffer,
 	uint8_t   no_of_bytes);
 
@@ -698,7 +784,7 @@ uint32_t VL53L1_FCTN_00108(
 
 
 
-void   VL53L1_FCTN_00109(
+void   VL53L1_FCTN_00134(
 	uint32_t  ip_value,
 	uint8_t   no_of_bytes,
 	uint8_t  *pbuffer);
@@ -718,8 +804,8 @@ void   VL53L1_FCTN_00109(
 
 
 
-uint32_t VL53L1_FCTN_00086(
-	uint32_t  VL53L1_PRM_00102,
+uint32_t VL53L1_FCTN_00110(
+	uint32_t  VL53L1_PRM_00131,
 	uint32_t  vcsel_parm_pclks,
 	uint32_t  window_vclks,
 	uint32_t  periods_elapsed_mclks);
@@ -738,8 +824,8 @@ uint32_t VL53L1_FCTN_00086(
 
 
 
-uint16_t VL53L1_FCTN_00110(
-	int32_t   VL53L1_PRM_00158,
+uint16_t VL53L1_FCTN_00135(
+	int32_t   VL53L1_PRM_00199,
 	uint32_t  time_us);
 
 
@@ -757,10 +843,11 @@ uint16_t VL53L1_FCTN_00110(
 
 
 
-uint16_t VL53L1_FCTN_00111(
-	uint32_t frac_bits,
-	int32_t  peak_count_rate,
-	int32_t  num_spads);
+int32_t VL53L1_FCTN_00136(
+	uint16_t  VL53L1_PRM_00098,
+	uint16_t  VL53L1_PRM_00094,
+	uint16_t  VL53L1_PRM_00306,
+	int32_t   VL53L1_PRM_00202);
 
 
 
@@ -777,9 +864,13 @@ uint16_t VL53L1_FCTN_00111(
 
 
 
-void VL53L1_FCTN_00088(
-	VL53L1_histogram_bin_data_t    *pdata,
-	uint8_t                         remove_ambient_bins);
+
+
+uint16_t VL53L1_FCTN_00137(
+	uint32_t  frac_bits,
+	uint32_t  peak_count_rate,
+	uint16_t  num_spads,
+	uint32_t  max_output_value);
 
 
 
@@ -790,7 +881,47 @@ void VL53L1_FCTN_00088(
 
 
 
-void VL53L1_FCTN_00087(
+
+
+
+
+
+
+uint32_t VL53L1_FCTN_00138(
+	int32_t   VL53L1_PRM_00072,
+	uint16_t  num_spads,
+	uint32_t  duration);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00139(
+	VL53L1_histogram_bin_data_t   *pdata);
+
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00140(
 	VL53L1_histogram_bin_data_t    *pdata);
 
 
@@ -804,7 +935,77 @@ void VL53L1_FCTN_00087(
 
 
 
-void  VL53L1_FCTN_00089(
+
+
+
+void VL53L1_FCTN_00112(
+	VL53L1_histogram_bin_data_t    *pdata);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00141(
+	int32_t                      ambient_threshold_sigma,
+	VL53L1_histogram_bin_data_t *pdata);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00100(
+	VL53L1_histogram_bin_data_t    *pidata,
+	VL53L1_histogram_bin_data_t    *podata);
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00111(
+	VL53L1_histogram_bin_data_t    *pdata);
+
+
+
+
+
+
+
+
+
+
+
+
+void  VL53L1_FCTN_00107(
 	VL53L1_DEV                     Dev,
 	VL53L1_histogram_bin_data_t   *pdata);
 
@@ -818,7 +1019,33 @@ void  VL53L1_FCTN_00089(
 
 
 
-void VL53L1_FCTN_00047(
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error  VL53L1_FCTN_00101(
+	VL53L1_DEV                Dev,
+	VL53L1_range_results_t   *previous,
+	VL53L1_range_results_t   *pcurrent);
+
+
+
+
+
+
+
+
+
+
+
+
+void VL53L1_FCTN_00052(
 	uint8_t   spad_number,
 	uint8_t  *prow,
 	uint8_t  *pcol);
@@ -835,7 +1062,7 @@ void VL53L1_FCTN_00047(
 
 
 
-void VL53L1_FCTN_00045(
+void VL53L1_FCTN_00050(
 	uint8_t  row,
 	uint8_t  col,
 	uint8_t *pspad_number);
@@ -853,16 +1080,98 @@ void VL53L1_FCTN_00045(
 
 
 
-void VL53L1_FCTN_00080(
+void VL53L1_FCTN_00102(
 	VL53L1_histogram_bin_data_t      *pbins,
 	VL53L1_range_results_t           *phist,
 	VL53L1_system_results_t          *psys,
 	VL53L1_core_results_t            *pcore);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00024 (
+		VL53L1_histogram_bin_data_t *phist_input,
+		VL53L1_histogram_bin_data_t *phist_output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00026 (
+		uint8_t VL53L1_PRM_00064,
+		VL53L1_histogram_bin_data_t *phist_sum,
+		VL53L1_histogram_bin_data_t *phist_avg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+uint32_t VL53L1_FCTN_00142(
+	uint32_t  num);
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00083(
+	VL53L1_DEV  Dev);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+VL53L1_Error VL53L1_FCTN_00103(
+	VL53L1_DEV  Dev,
+	VL53L1_range_results_t *presults);
+
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif 
+#endif
 
