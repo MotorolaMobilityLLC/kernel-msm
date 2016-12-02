431793ef710682af8f6c3ba397174c549860013f
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2016-12-02 10:39:54 -0600

    input:misc:vl53L1: update ST vl53l1 driver
    Update VL53L1_LinuxDriver_Prelim_cut11(11/28/2016)
    Implement following modes:
    - RANGING: This uses the Histograms and is the recommended
      mode. Typical timing budget is 16msec
    - MULTIZONES_SCANNING: This allows configuration of individual
      ROIs. Typical timing budget is 16msec
    - LITE_RANGING: This is legacy mode.Typical timing budget is
      16msec
    
    Change-Id: I98ff56c640dfdd71bc1c158af32f877d8f2cce97
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/926912
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Conflicts:
    	drivers/input/misc/vl53L1/stmvl53l1_ipp_nl.c
    Change-Id: I3285a50e3530b8133ae0d262747e5acf8c44cfd4
    Reviewed-on: https://gerrit.mot.com/929999
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hideki Yoshino <hideki.yoshino@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/misc/vl53L1/src/vl53l1_error_strings.c
+++ drivers/input/misc/vl53L1/src/vl53l1_error_strings.c
@@ -1,61 +1,61 @@
 
-/* 
-* Copyright (c) 2016, STMicroelectronics - All Rights Reserved 
-* 
-* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license' 
-* or 'BSD 3-clause "New" or "Revised" License' , at your option. 
-* 
-******************************************************************************** 
-* 
-* 'STMicroelectronics Proprietary license' 
-* 
-******************************************************************************** 
-* 
+/*
+* Copyright (c) 2016, STMicroelectronics - All Rights Reserved
+*
+* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license'
+* or 'BSD 3-clause "New" or "Revised" License' , at your option.
+*
+********************************************************************************
+*
+* 'STMicroelectronics Proprietary license'
+*
+********************************************************************************
+*
 * License terms: STMicroelectronics Proprietary in accordance with licensing terms at www.st.com/sla0044
 *
-* STMicroelectronics confidential 
-* Reproduction and Communication of this document is strictly prohibited unless 
+* STMicroelectronics confidential
+* Reproduction and Communication of this document is strictly prohibited unless
 * specifically authorized in writing by STMicroelectronics.
-*  
-* 
-******************************************************************************** 
-* 
-* Alternatively, VL53L1 Core may be distributed under the terms of 
-* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones 
-* mentioned above : 
-* 
-******************************************************************************** 
-* 
-* License terms: BSD 3-clause "New" or "Revised" License. 
-* 
-* Redistribution and use in source and binary forms, with or without 
-* modification, are permitted provided that the following conditions are met: 
-* 
-* 1. Redistributions of source code must retain the above copyright notice, this 
-* list of conditions and the following disclaimer. 
-* 
-* 2. Redistributions in binary form must reproduce the above copyright notice, 
-* this list of conditions and the following disclaimer in the documentation 
-* and/or other materials provided with the distribution. 
-* 
-* 3. Neither the name of the copyright holder nor the names of its contributors 
-* may be used to endorse or promote products derived from this software 
-* without specific prior written permission. 
-* 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-* 
-* 
-******************************************************************************** 
-* 
+*
+*
+********************************************************************************
+*
+* Alternatively, VL53L1 Core may be distributed under the terms of
+* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones
+* mentioned above :
+*
+********************************************************************************
+*
+* License terms: BSD 3-clause "New" or "Revised" License.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*
+********************************************************************************
+*
 */
 
 
@@ -94,7 +94,8 @@
 
 #include "vl53l1_error_codes.h"
 #include "vl53l1_error_strings.h"
-
+#include "vl53l1_platform_log.h"
+#include "vl53l1_ll_def.h"
 
 #define LOG_FUNCTION_START(fmt, ...) \
 	_LOG_FUNCTION_START(VL53L1_TRACE_MODULE_API, fmt, ##__VA_ARGS__)
@@ -113,7 +114,7 @@ VL53L1_Error VL53L1_get_pal_error_string(
 
 	LOG_FUNCTION_START("");
 
-#ifdef USE_EMPTY_STRING
+#ifdef VL53L1_USE_EMPTY_STRING
 	VL53L1_COPYSTRING(pPalErrorString, "");
 #else
 
@@ -195,24 +196,36 @@ VL53L1_Error VL53L1_get_pal_error_string(
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_STREAM_COUNT_CHECK_FAIL);
 	break;
+	case VL53L1_ERROR_GPH_ID_CHECK_FAIL:
+		VL53L1_COPYSTRING(pPalErrorString,
+			VL53L1_STRING_ERROR_GPH_ID_CHECK_FAIL);
+	break;
+	case VL53L1_ERROR_ZONE_STREAM_COUNT_CHECK_FAIL:
+		VL53L1_COPYSTRING(pPalErrorString,
+			VL53L1_STRING_ERROR_ZONE_STREAM_COUNT_CHECK_FAIL);
+	break;
+	case VL53L1_ERROR_ZONE_GPH_ID_CHECK_FAIL:
+		VL53L1_COPYSTRING(pPalErrorString,
+			VL53L1_STRING_ERROR_ZONE_GPH_ID_CHECK_FAIL);
+	break;
 
-	case VL53L1_DEF_00046:
+	case VL53L1_DEF_00164:
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_DEVICE_FIRMWARE_TOO_OLD);
 	break;
-	case VL53L1_DEF_00048:
+	case VL53L1_DEF_00165:
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_DEVICE_FIRMWARE_TOO_NEW);
 	break;
-	case VL53L1_DEF_00145:
+	case VL53L1_DEF_00166:
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_UNIT_TEST_FAIL);
 	break;
-	case VL53L1_DEF_00146:
+	case VL53L1_DEF_00167:
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_FILE_READ_FAIL);
 	break;
-	case VL53L1_DEF_00147:
+	case VL53L1_DEF_00168:
 		VL53L1_COPYSTRING(pPalErrorString,
 			VL53L1_STRING_ERROR_FILE_WRITE_FAIL);
 	break;
