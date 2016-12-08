dd1d5a223b003d34750237d945669a975c81b510
Yoon (Seungyoon) Lee <w22905@motorola.com>
2016-12-08 14:44:12 -0600

    ASoC: madera: set slimbus rate and width
    read slimbus rx sample rate and bitwidth from MSM to start slimbus
    with given info. this will allow us to have different madera slimbus
    configuration per usecases, not fixed at 16bit/48kHz.
    
    Change-Id: I5ca8af3a364d5b25e53b5bd2f9ee171892b8dc55
    Signed-off-by: Yoon (Seungyoon) Lee <w22905@motorola.com>
    Reviewed-on: https://gerrit.mot.com/929608
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/madera.c
+++ sound/soc/codecs/madera.c
@@ -3315,11 +3315,18 @@ static int madera_hw_params_rate(struct snd_pcm_substream *substream,
 	const unsigned int *sources = NULL;
 	unsigned int cur, tar;
 	bool change_rate = false;
+	u32 rx_sampleszbits, rx_samplerate;
+
+	rx_sampleszbits = snd_pcm_format_width(params_format(params));
+	if (rx_sampleszbits < 16)
+		rx_sampleszbits = 16;
 
 	/* currently we use a single sample rate for SYSCLK */
 	for (i = 0; i < ARRAY_SIZE(madera_sr_vals); i++)
-		if (madera_sr_vals[i] == params_rate(params))
+		if (madera_sr_vals[i] == params_rate(params)) {
+			rx_samplerate = params_rate(params);
 			break;
+		}
 
 	if (i == ARRAY_SIZE(madera_sr_vals)) {
 		madera_aif_err(dai, "Unsupported sample rate %dHz\n",
@@ -3327,6 +3334,20 @@ static int madera_hw_params_rate(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 	sr_val = i;
+
+	switch (dai->id) {
+	case 4: /* cs47l35-slim1 */
+		priv->rx1_sampleszbits = rx_sampleszbits;
+		priv->rx1_samplerate = rx_samplerate;
+		break;
+	case 5: /* cs47l35-slim2 */
+		priv->rx2_sampleszbits = rx_sampleszbits;
+		priv->rx2_samplerate = rx_samplerate;
+		break;
+	case 6:
+	default:
+		break;
+	}
 
 	if (base) {
 		switch (dai_priv->clk) {
