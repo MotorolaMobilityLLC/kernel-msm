d10c3ff4725ee15217cf783ce204d2c4de965a63
guoyc1 <guoyc1@lenovo.com>
2016-12-09 16:11:32 +0800

    input:misc:add mxg3330 driver
    Add driver for mxg3330, it's a magnitic sensor, this is
    revision 1.0 from vendor.
    
    Change-Id: I399ae4a55515a37d65615eb3ac97e76f1ebec342
    Signed-off-by: guoyc1 <guoyc1@mt.com>
    Reviewed-on: https://gerrit.mot.com/929801
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ include/linux/mxg3300.h
@@ -0,0 +1,103 @@
+#ifndef __MXG2320_H__
+#define __MXG2320_H__
+
+#include <linux/ioctl.h>
+#include <linux/mutex.h>
+
+#define MXG_DEV_NAME            "mxg3300"
+#define MXG_INPUT_DEV_NAME      "mxg3300_compass"
+#define MXG_MISC_DEV_NAME       "mxg3300_dev"
+
+#define MXG_CMP1_ID             0x13
+#define MXG_CMP2_ID             0x30
+
+/* MXG3300 Operation Mode */
+#define MXG_MODE_SNG_MEASURE    0xC1
+#define MXG_MODE_CONT_10HZ      0xC2
+#define MXG_MODE_CONT_20HZ      0xC4
+#define MXG_MODE_CONT_50HZ      0xC6
+#define MXG_MODE_CONT_100HZ     0xC8
+#define MXG_MODE_CONT_200HZ     0xCC
+#define MXG_MODE_SELF_TEST      0x10
+#define MXG_MODE_FUSE_ACCESS    0x1F
+#define MXG_MODE_POWER_DOWN     0xC0
+#define MXG_SOFT_RESET          0x01
+
+/* Register map */
+#define MXG_REG_DID             0x50
+#define MXG_REG_INFO            0x52
+#define MXG_REG_ST1             0x00
+#define MXG_REG_XDH             0x01
+#define MXG_REG_XDL             0x02
+#define MXG_REG_YDH             0x03
+#define MXG_REG_YDL             0x04
+#define MXG_REG_ZDH             0x05
+#define MXG_REG_ZDL             0x06
+#define MXG_REG_ST2             0x08
+#define MXG_REG_OPMODE          0x10
+#define MXG_REG_SRST            0x11
+#define MXG_REG_CHOP            0x12
+#define MXG_REG_XASA            0x53
+#define MXG_REG_YASA            0x54
+#define MXG_REG_ZASA            0x55
+
+/* Event Type */
+#define EVENT_RAW_X             ABS_X
+#define EVENT_RAW_Y             ABS_Y
+#define EVENT_RAW_Z             ABS_Z
+
+/* IOCTL Magic code */
+#define MXG_TYPE                'M'
+
+/* IOCTL Command List */
+#define MXG_IOCTL_SET_ENABLE    _IOW(MXG_TYPE, 0x10, int)
+#define MXG_IOCTL_GET_ENABLE    _IOR(MXG_TYPE, 0x20, int)
+#define MXG_IOCTL_SET_DELAY     _IOW(MXG_TYPE, 0x30, unsigned int)
+#define MXG_IOCTL_GET_DELAY     _IOR(MXG_TYPE, 0x40, unsigned int)
+#define MXG_IOCTL_GET_INFO      _IOR(MXG_TYPE, 0x50, unsigned char[2])
+#define MXG_IOCTL_GET_MCOEF     _IOR(MXG_TYPE, 0x60, unsigned char[3])
+
+struct mxg_platform_data {
+	/*
+	 * position_array[0-2] value is 3-axis coodination.
+	 * "0" value is [x-axis]
+	 * "1" value is [y-axis]
+	 * "2" value is [z-axis]
+	 * position_array[3-5] value is axis sign.
+	 * "0" value is Positive
+	 * "1" value is Negative
+	 */
+	u32 position_array[6];
+};
+
+struct mxg_sensor_data {
+	struct mxg_platform_data *plat_data;
+	struct i2c_client *client;
+	struct input_dev *input_dev;
+	struct miscdevice *misc_dev;
+	struct device *dev;
+	struct sensors_classdev sensor_cdev;
+
+	struct workqueue_struct *work_queue;
+	struct delayed_work work;
+	struct delayed_work selftest_work;
+	wait_queue_head_t selftest_wq;
+	atomic_t selftest_done;
+
+	struct regulator *vdd_ana;
+	struct regulator *vdd_io;
+	bool power_on;
+
+	struct mutex lock;
+	atomic_t en;
+	u8 mode; /* Mode 0x1 : Single, 0x10 : Self-test */
+	u32 odr;
+	u8 mcoef[3];    /* axis sensitivity adjustment */
+	s8 self[3];
+	u8 info[3];
+	s32 raw_data[3];
+
+	u32 flush_count;
+};
+
+#endif /* __MXG2320_H__ */
