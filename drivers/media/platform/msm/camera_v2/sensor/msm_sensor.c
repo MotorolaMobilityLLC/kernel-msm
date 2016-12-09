c10fdf65a703cff835bb19fdc068e2c5077c851e
Sergey Kudakov <sergey@motorola.com>
2016-12-09 15:29:40 -0600

    msm: camera: notify sar to disable rear prox
    For the camera sensors impacted by rear prox led(s) (which
    is to be indicated via devtree) send uevent upon camera
    power up/down. SAR (which by Defenderault enables rear prox(s))
    can listen to this event and not enable rear prox(s)
    
    Change-Id: I189ebf2caaceb9e4ff05671ea6b279a2a28e25c0
    Signed-off-by: Sergey Kudakov <sergey@motorola.com>
    Reviewed-on: https://gerrit.mot.com/922687
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -17,6 +17,7 @@
 #include "msm_camera_i2c_mux.h"
 #include <linux/regulator/rpm-smd-regulator.h>
 #include <linux/regulator/consumer.h>
+#include "media/v4l2-device.h"
 
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
@@ -141,6 +142,15 @@ int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 	if (s_ctrl->is_secure)
 		msm_camera_tz_i2c_power_down(sensor_i2c_client);
 
+	if (s_ctrl && s_ctrl->msm_sd.sd.devnode &&
+		s_ctrl->sensordata->sensor_info &&
+		s_ctrl->sensordata->sensor_info->is_rear_prox_interfering) {
+		struct kobject *kobj = &s_ctrl->msm_sd.sd.devnode->dev.kobj;
+		char *envp[2] = {"RearImagerActive=0", 0};
+
+		if (kobject_uevent_env(kobj, KOBJ_CHANGE, envp) == 0)
+			sysfs_notify(kobj, NULL, NULL);
+	}
 	return msm_camera_power_down(power_info, sensor_device_type,
 		sensor_i2c_client);
 }
@@ -239,6 +249,15 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 		}
 	}
 
+	if (rc == 0 && s_ctrl && s_ctrl->msm_sd.sd.devnode &&
+		s_ctrl->sensordata->sensor_info &&
+		s_ctrl->sensordata->sensor_info->is_rear_prox_interfering) {
+		struct kobject *kobj = &s_ctrl->msm_sd.sd.devnode->dev.kobj;
+		char *envp[2] = {"RearImagerActive=1", 0};
+
+		if (kobject_uevent_env(kobj, KOBJ_CHANGE, envp) == 0)
+			sysfs_notify(kobj, NULL, NULL);
+	}
 	return rc;
 }
 
