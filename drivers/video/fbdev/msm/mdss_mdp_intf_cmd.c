341f2d898ee4500ec0e8df0cb9708c27ff06bc14
Shiyong Li <a22381@motorola.com>
2016-12-19 12:30:02 -0800

    fbdev: msm: support xlog dump for display failure
    Support xlog dump for ESD error, black screen and pingpong timeout error.
    
    Port of fd1a210, 2d8a923 and 2554976 on 8996 with minor change.
    
    Change-Id: I78d7a6cb13ae72b07b13d1ec2530ba52a6acbc93
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/933290
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/fbdev/msm/mdss_mdp_intf_cmd.c
+++ drivers/video/fbdev/msm/mdss_mdp_intf_cmd.c
@@ -21,6 +21,7 @@
 #include "mdss_debug.h"
 #include "mdss_mdp_trace.h"
 #include "mdss_dsi_clk.h"
+#include "mdss_dropbox.h"
 
 #define MAX_RECOVERY_TRIALS 10
 #define MAX_SESSIONS 2
@@ -2150,6 +2151,9 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 				"dsi1_ctrl", "dsi1_phy", "vbif", "vbif_nrt",
 				"dbg_bus", "vbif_dbg_bus",
 				"dsi_dbg_bus", "panic");
+			MDSS_XLOG_TOUT_HANDLER_MMI("mdp",
+				"dsi0_ctrl", "dsi0_phy",
+				"dsi1_ctrl", "dsi1_phy");
 		} else if (ctx->pp_timeout_report_cnt == MAX_RECOVERY_TRIALS) {
 			MDSS_XLOG(0xbad2);
 			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0_ctrl", "dsi0_phy",
