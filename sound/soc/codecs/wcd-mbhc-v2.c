6de4b1297562f93e8a329bc98cb20065d5e2447f
zhanggb <zhanggb@motorola.com>
2016-12-28 13:48:05 +0800

    mbhc: check validation before call micb callback
    The callback pointer mbhc_micb_ctrl_thr_mic is not set when
    mbhc is initialized, so must check validataion before use it.
    
    Change-Id: Ie9eb2a6a34418b335f6b0dbe17ff330f4f5f3b4a
    Signed-off-by: zhanggb <zhanggb@motorola.com>
    Reviewed-on: https://gerrit.mot.com/935692
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -1253,8 +1253,9 @@ correct_plug_type:
 			wcd_enable_curr_micbias(mbhc,
 						WCD_MBHC_EN_NONE);
 			if (mbhc->micbias_enable) {
-				mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic(
-					mbhc->codec, MIC_BIAS_2, false);
+				if (mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic)
+					mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic(
+						mbhc->codec, MIC_BIAS_2, false);
 				if (mbhc->mbhc_cb->set_micbias_value)
 					mbhc->mbhc_cb->set_micbias_value(
 							mbhc->codec);
@@ -1278,8 +1279,9 @@ correct_plug_type:
 			wcd_enable_curr_micbias(mbhc,
 						WCD_MBHC_EN_NONE);
 			if (mbhc->micbias_enable) {
-				mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic(
-					mbhc->codec, MIC_BIAS_2, false);
+				if (mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic)
+					mbhc->mbhc_cb->mbhc_micb_ctrl_thr_mic(
+						mbhc->codec, MIC_BIAS_2, false);
 				if (mbhc->mbhc_cb->set_micbias_value)
 					mbhc->mbhc_cb->set_micbias_value(
 							mbhc->codec);
