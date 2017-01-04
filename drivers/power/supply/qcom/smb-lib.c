b77ae74fed7878fe5c13e26871d17520c842aa12
Ravi Vembu <raviv@motorola.com>
2017-01-04 15:16:27 -0600

    power: smb2: Implement Factory Kill
    Implement Factory kill to power off the device when USBIN is
    removed in factory mode.
    
    Change-Id: I18984257a9c99bd5a4d04460c5ec07801aa058c1
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: https://gerrit.mot.com/937858
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3313,6 +3313,8 @@ void smblib_usb_plugin_hard_reset_locked(struct smb_charger *chg)
 }
 
 #define PL_DELAY_MS			30000
+static int factory_kill_disable;
+module_param(factory_kill_disable, int, 0644);
 void smblib_usb_plugin_locked(struct smb_charger *chg)
 {
 	int rc;
@@ -3341,6 +3343,9 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 		vote(chg->awake_votable, PL_DELAY_VOTER, true, 0);
 		schedule_delayed_work(&chg->pl_enable_work,
 					msecs_to_jiffies(PL_DELAY_MS));
+
+		if (chg->mmi.factory_mode)
+			chg->mmi.factory_kill_armed = true;
 	} else {
 		if (chg->wa_flags & BOOST_BACK_WA) {
 			data = chg->irq_info[SWITCH_POWER_OK_IRQ].irq_data;
@@ -3363,6 +3368,12 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 		rc = smblib_request_dpdm(chg, false);
 		if (rc < 0)
 			smblib_err(chg, "Couldn't disable DPDM rc=%d\n", rc);
+
+		if (chg->mmi.factory_kill_armed && !factory_kill_disable) {
+			smblib_err(chg, "Factory kill power off\n");
+			kernel_power_off();
+		} else
+			chg->mmi.factory_kill_armed = false;
 	}
 
 	if (chg->micro_usb_mode)
@@ -5092,8 +5103,6 @@ int smblib_deinit(struct smb_charger *chg)
 
 struct smb_charger *the_chip;
 
-static int factory_kill_disable;
-module_param(factory_kill_disable, int, 0644);
 static int smbchg_reboot(struct notifier_block *nb,
 			 unsigned long event, void *unused)
 {
