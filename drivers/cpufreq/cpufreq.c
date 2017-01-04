9368652c3604c9454a136d75678a616672fc8e4c
Igor Kovalenko <cik009@motorola.com>
2017-01-04 17:43:56 -0600

    Revert "drivers: cpufreq: Send a uevent when governor changes"
    MOT-CRs-fixed: (CR)
    Signed-off-by: Igor Kovalenko <cik009@motorola.com>
    
    This reverts commit 44765ba9c6a71115c54c4faaaccfe7efe9e63ae1.
    
    Change-Id: I6a43c5ab84e76e8bf93a9b30a51b040fdb726bfb
    Reviewed-on: https://gerrit.mot.com/937902
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -673,9 +673,6 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	int ret;
 	char	str_governor[16];
 	struct cpufreq_policy new_policy;
-	char *envp[3];
-	char buf1[64];
-	char buf2[64];
 
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);
 	if (ret)
@@ -693,13 +690,6 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 
 	policy->user_policy.policy = policy->policy;
 	policy->user_policy.governor = policy->governor;
-
-	snprintf(buf1, sizeof(buf1), "GOV=%s", policy->governor->name);
-	snprintf(buf2, sizeof(buf2), "CPU=%u", policy->cpu);
-	envp[0] = buf1;
-	envp[1] = buf2;
-	envp[2] = NULL;
-	kobject_uevent_env(cpufreq_global_kobject, KOBJ_ADD, envp);
 
 	if (ret)
 		return ret;
