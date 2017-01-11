c43d32512850bf84d7bfcbe8bd52410a4b5823f3
Naseer Ahmed <naseer@codeaurora.org>
2017-01-11 12:00:14 -0500

    mdss: Validate cursor image size
    Check size of cursor image provided by userspace to avoid buffer
    overflow.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2017-0579
    
    Bug: 34125463
    Change-Id: I31aee3c9219921cf5c4306b36f8708582b838c38
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Signed-off-by: Steve Pfetsch <spfetsch@google.com>
    Reviewed-on: https://gerrit.mot.com/964069
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -4705,6 +4705,12 @@ static int mdss_mdp_hw_cursor_pipe_update(struct msm_fb_data_type *mfd,
 	req->transp_mask = img->bg_color & ~(0xff << var->transp.offset);
 
 	if (mfd->cursor_buf && (cursor->set & FB_CUR_SETIMAGE)) {
+		if (img->width * img->height * 4 > cursor_frame_size) {
+			pr_err("cursor image size is too large\n");
+			ret = -EINVAL;
+			goto done;
+		}
+
 		ret = copy_from_user(mfd->cursor_buf, img->data,
 				     img->width * img->height * 4);
 		if (ret) {
