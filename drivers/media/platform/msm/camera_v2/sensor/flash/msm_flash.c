2ee8950f0cc411fdc57fca00e0a80af1cf54f2ad
Fernando Hortellani <fjhortel@motorola.com>
2017-01-19 15:31:53 -0200

    msm_flash: Add support for muxed pmic flash
    Some products that use PMIC to drive the LEDs have muxed
    front/rear flashes selected by a GPIO. This commit adds
    support to muxed LEDs in the msm_flash driver.
    
    Change-Id: I92fcccd0b645cf12b2a96fad7aeb458e570fb778
    Signed-off-by: Fernando Hortellani <fjhortel@motorola.com>
    Reviewed-on: https://gerrit.mot.com/942711
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -400,6 +400,30 @@ static int32_t msm_flash_i2c_release(
 	return 0;
 }
 
+static void msm_flash_set_mux_gpio_value(
+	struct msm_flash_ctrl_t *flash_ctrl,
+	struct msm_flash_cfg_data_t *flash_data,
+	bool enable)
+{
+	/* Set or reset GPIO that selects which flash to use */
+	if (flash_data && flash_data->mux_sel == USE_MUX_SEL) {
+		struct msm_camera_power_ctrl_t *power_info =
+			&flash_ctrl->power_info;
+		if (power_info->gpio_conf &&
+		    power_info->gpio_conf->gpio_num_info &&
+		    power_info->gpio_conf->gpio_num_info->
+		    valid[SENSOR_GPIO_CUSTOM1]) {
+			gpio_set_value_cansleep(
+				power_info->gpio_conf->gpio_num_info->
+				    gpio_num[SENSOR_GPIO_CUSTOM1],
+				enable ? GPIO_OUT_HIGH : GPIO_OUT_LOW);
+		} else {
+			pr_err("%s: Invalid GPIO\n", __func__);
+		}
+
+	}
+}
+
 static int32_t msm_flash_off(struct msm_flash_ctrl_t *flash_ctrl,
 	struct msm_flash_cfg_data_t *flash_data)
 {
@@ -416,6 +440,8 @@ static int32_t msm_flash_off(struct msm_flash_ctrl_t *flash_ctrl,
 			led_trigger_event(flash_ctrl->torch_trigger[i], 0);
 	if (flash_ctrl->switch_trigger)
 		led_trigger_event(flash_ctrl->switch_trigger, 0);
+
+	msm_flash_set_mux_gpio_value(flash_ctrl, flash_data, FALSE);
 
 	CDBG("Exit\n");
 	return 0;
@@ -568,6 +594,8 @@ static int32_t msm_flash_low(
 		if (flash_ctrl->flash_trigger[i])
 			led_trigger_event(flash_ctrl->flash_trigger[i], 0);
 
+	msm_flash_set_mux_gpio_value(flash_ctrl, flash_data, TRUE);
+
 	/* Turn on flash triggers */
 	for (i = 0; i < flash_ctrl->torch_num_sources; i++) {
 		if (flash_ctrl->torch_trigger[i]) {
@@ -615,6 +643,8 @@ static int32_t msm_flash_high(
 	for (i = 0; i < flash_ctrl->torch_num_sources; i++)
 		if (flash_ctrl->torch_trigger[i])
 			led_trigger_event(flash_ctrl->torch_trigger[i], 0);
+
+	msm_flash_set_mux_gpio_value(flash_ctrl, flash_data, TRUE);
 
 	/* Turn on flash triggers */
 	for (i = 0; i < flash_ctrl->flash_num_sources; i++) {
