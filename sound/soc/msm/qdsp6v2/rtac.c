b7da97f8e79c5458d77421832ae9f6aed6bb22d8
a7301c <a7301c@motorola.com>
2017-01-20 12:02:57 -0600

    Revert "msm: qdsp6v2: extend media format provided to voice driver"
    This reverts commit dd764997fa84cf5e42beb3cb9f3e5cceee5058ea.
    Which causes no-audio during active call
    
    Change-Id: I1595caa8f2087b6c707533bb21b5c303ab737413

--- sound/soc/msm/qdsp6v2/rtac.c
+++ sound/soc/msm/qdsp6v2/rtac.c
@@ -1526,7 +1526,7 @@ int send_voice_apr(u32 mode, void *buf, u32 opcode)
 		goto err;
 	}
 
-	if (opcode == VSS_ICOMMON_CMD_SET_PARAM_V2) {
+	if (opcode == VOICE_CMD_SET_PARAM) {
 		/* set payload size to in-band payload */
 		/* set data size to actual out of band payload size */
 		data_size = payload_size - 4 * sizeof(u32);
@@ -1579,9 +1579,7 @@ int send_voice_apr(u32 mode, void *buf, u32 opcode)
 	voice_params.dest_svc = 0;
 	voice_params.dest_domain = APR_DOMAIN_MODEM;
 	voice_params.dest_port = (u16)dest_port;
-	voice_params.token = (opcode == VSS_ICOMMON_CMD_SET_PARAM_V2) ?
-				     VOC_RTAC_SET_PARAM_TOKEN :
-				     0;
+	voice_params.token = 0;
 	voice_params.opcode = opcode;
 
 	/* fill for out-of-band */
@@ -1626,7 +1624,7 @@ int send_voice_apr(u32 mode, void *buf, u32 opcode)
 		goto err;
 	}
 
-	if (opcode == VSS_ICOMMON_CMD_GET_PARAM_V2) {
+	if (opcode == VOICE_CMD_GET_PARAM) {
 		bytes_returned = ((u32 *)rtac_cal[VOICE_RTAC_CAL].cal_data.
 			kvaddr)[2] + 3 * sizeof(u32);
 
@@ -1727,20 +1725,20 @@ static long rtac_ioctl_shared(struct file *f,
 			ASM_STREAM_CMD_SET_PP_PARAMS_V2);
 		break;
 	case AUDIO_GET_RTAC_CVS_CAL:
-		result = send_voice_apr(RTAC_CVS, (void *) arg,
-					VSS_ICOMMON_CMD_GET_PARAM_V2);
+		result = send_voice_apr(RTAC_CVS, (void *)arg,
+			VOICE_CMD_GET_PARAM);
 		break;
 	case AUDIO_SET_RTAC_CVS_CAL:
-		result = send_voice_apr(RTAC_CVS, (void *) arg,
-					VSS_ICOMMON_CMD_SET_PARAM_V2);
+		result = send_voice_apr(RTAC_CVS, (void *)arg,
+			VOICE_CMD_SET_PARAM);
 		break;
 	case AUDIO_GET_RTAC_CVP_CAL:
-		result = send_voice_apr(RTAC_CVP, (void *) arg,
-					VSS_ICOMMON_CMD_GET_PARAM_V2);
+		result = send_voice_apr(RTAC_CVP, (void *)arg,
+			VOICE_CMD_GET_PARAM);
 		break;
 	case AUDIO_SET_RTAC_CVP_CAL:
-		result = send_voice_apr(RTAC_CVP, (void *) arg,
-					VSS_ICOMMON_CMD_SET_PARAM_V2);
+		result = send_voice_apr(RTAC_CVP, (void *)arg,
+			VOICE_CMD_SET_PARAM);
 		break;
 	case AUDIO_GET_RTAC_AFE_CAL:
 		result = send_rtac_afe_apr((void *)arg,
