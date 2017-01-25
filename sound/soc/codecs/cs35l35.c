a0a9e166f0a46752fbb59932ef3bd1c73a1cd665
Paul Handrigan <Paul.Handrigan@cirrus.com>
2017-01-25 10:55:02 -0600

    ASoC: cs35l35: Add code to power down amp state machine
    Disable digital soft ramp and add a delay before the device is
    powered down to make sure that the device is completely powered
    down.
    
    Change-Id: Ie6429567997886625acc792867f5f29c51ba1653
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Reviewed-on: https://gerrit.mot.com/959134
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Yun Ma <yunma@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/cs35l35.c
+++ sound/soc/codecs/cs35l35.c
@@ -330,6 +330,8 @@ static int cs35l35_mclk_event(struct snd_soc_dapm_widget *w,
 		break;
 	case SND_SOC_DAPM_PRE_PMU:
 		cs35l35->i2s_enabled = true;
+		regmap_update_bits(cs35l35->regmap, CS35L35_AMP_DIG_VOL_CTL,
+					2, 2);
 		if (cs35l35->pdm_mclk_switch) {
 			cs35l35->pdm_mclk_switch = false;
 			return cs35l35_reset_and_sync(cs35l35, false);
@@ -341,11 +343,14 @@ static int cs35l35_mclk_event(struct snd_soc_dapm_widget *w,
 			cs35l35->pdm_mclk_switch = true;
 			return cs35l35_reset_and_sync(cs35l35, true);
 		}
+		regmap_update_bits(cs35l35->regmap, CS35L35_AMP_DIG_VOL_CTL,
+					2, 0);
 		regmap_update_bits(cs35l35->regmap, CS35L35_PWRCTL1,
 					  CS35L35_PDN_ALL_MASK, 1);
 		regmap_update_bits(cs35l35->regmap, CS35L35_PWRCTL1,
 					CS35L35_DISCHG_FILT_MASK,
 					1 << CS35L35_DISCHG_FILT_SHIFT);
+		usleep_range(10000, 10010);
 		ret = wait_for_completion_timeout(&cs35l35->pdn_done,
 							msecs_to_jiffies(100));
 		if (ret == 0) {
@@ -372,6 +377,8 @@ static int cs35l35_pdm_event(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
+		regmap_update_bits(cs35l35->regmap, CS35L35_AMP_DIG_VOL_CTL,
+					2, 2);
 		if (!cs35l35->pdm_mclk_switch) {
 			cs35l35->pdm_mclk_switch = true;
 			return cs35l35_reset_and_sync(cs35l35, true);
@@ -386,11 +393,14 @@ static int cs35l35_pdm_event(struct snd_soc_dapm_widget *w,
 					CS35L35_PDN_ALL_MASK, 0);
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
+		regmap_update_bits(cs35l35->regmap, CS35L35_AMP_DIG_VOL_CTL,
+					2, 0);
 		regmap_update_bits(cs35l35->regmap, CS35L35_PWRCTL1,
 					  CS35L35_PDN_ALL_MASK, 1);
 		regmap_update_bits(cs35l35->regmap, CS35L35_PWRCTL1,
 					CS35L35_DISCHG_FILT_MASK,
 					1 << CS35L35_DISCHG_FILT_SHIFT);
+		usleep_range(10000, 10010);
 		ret = wait_for_completion_timeout(&cs35l35->pdn_done,
 							msecs_to_jiffies(100));
 		if (ret == 0) {
