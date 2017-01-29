77f6c97e4b861c05d4da6785f8ea7612233001e4
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2017-01-28 20:31:07 -0600

    input:misc:vl53L1: update ST driver 7.2.1
    7.2.1 : Wed Jan 25 09:48:09 CET 2017
    - fix bug in ipp xtalk calibration process
    
    7.2.0 : Tue Jan 24 15:00:49 CET 2017
    - use bare driver v3.7.0.1118
    - replace VL53L1_IOCTL_INIT with VL53L1_IOCTL_START
    
    Change-Id: I3ad2b565e8718f1bb142fcd9680117a4816ee85e
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/946369
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/misc/vl53L1/src/vl53l1_core.c
+++ drivers/input/misc/vl53L1/src/vl53l1_core.c
@@ -2,7 +2,8 @@
 /*
 * Copyright (c) 2016, STMicroelectronics - All Rights Reserved
 *
-* This file is part of VL53L1 Core and is dual licensed, either 'STMicroelectronics Proprietary license'
+* This file is part of VL53L1 Core and is dual licensed, either
+* Proprietary license'
 * or 'BSD 3-clause "New" or "Revised" License' , at your option.
 *
 ********************************************************************************
@@ -11,7 +12,8 @@
 *
 ********************************************************************************
 *
-* License terms: STMicroelectronics Proprietary in accordance with licensing terms at www.st.com/sla0044
+*License terms : STMicroelectronics Proprietary in accordance with licensing
+* terms at www.st.com/sla0044
 *
 * STMicroelectronics confidential
 * Reproduction and Communication of this document is strictly prohibited unless
@@ -21,12 +23,13 @@
 ********************************************************************************
 *
 * Alternatively, VL53L1 Core may be distributed under the terms of
-* 'BSD 3-clause "New" or "Revised" License', in which case the following provisions apply instead of the ones
+* 'BSD 3-clause "New" or "Revised" License', in which case the following
+* provisions apply instead of the ones
 * mentioned above :
 *
 ********************************************************************************
 *
-* License terms: BSD 3-clause "New" or "Revised" License.
+*License terms : BSD 3-clause "New" or "Revised" License.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -2316,8 +2319,16 @@ uint32_t VL53L1_events_per_spad_maths(
 
 
 
-	total_hist_counts = ((uint64_t)VL53L1_PRM_00009
-			* 1000 * 256) / (uint64_t)num_spads;
+
+
+
+
+
+
+	uint64_t dividend = ((uint64_t)VL53L1_PRM_00009
+			* 1000 * 256);
+
+	total_hist_counts = do_division_u(dividend, (uint64_t)num_spads);
 
 
 
