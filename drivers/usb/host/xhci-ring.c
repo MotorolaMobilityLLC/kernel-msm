8cea4021bdc998012e8b1294b1ba28852791f63e
Ravi Vembu <raviv@motorola.com>
2017-01-30 10:04:33 -0600

    Revert "xhci: fix null pointer dereference in stop command timeout function"
    This reverts commit 1b78406265d2d8f8bb4ef70b464af4033538f63e.
    Reverting since we are seeing lockups with RC removal.
    
    Mot-CRs-fixed: (CR)
    Change-Id: I0af4ffed7f2ec01275ad4ee4f1d66f56ac7be393
    Reviewed-on: https://gerrit.mot.com/945636
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -839,10 +839,6 @@ void xhci_stop_endpoint_command_watchdog(unsigned long arg)
 	spin_lock_irqsave(&xhci->lock, flags);
 
 	ep->stop_cmds_pending--;
-	if (xhci->xhc_state & XHCI_STATE_REMOVING) {
-		spin_unlock_irqrestore(&xhci->lock, flags);
-		return;
-	}
 	if (xhci->xhc_state & XHCI_STATE_DYING) {
 		xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 				"Stop EP timer ran, but another timer marked "
@@ -896,7 +892,7 @@ void xhci_stop_endpoint_command_watchdog(unsigned long arg)
 	spin_unlock_irqrestore(&xhci->lock, flags);
 	xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 			"Calling usb_hc_died()");
-	usb_hc_died(xhci_to_hcd(xhci));
+	usb_hc_died(xhci_to_hcd(xhci)->primary_hcd);
 	xhci_dbg_trace(xhci, trace_xhci_dbg_cancel_urb,
 			"xHCI host controller is dead.");
 	if (xhci->quirks & XHCI_PANIC_ON_WDOG)
