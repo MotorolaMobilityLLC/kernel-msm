bec49d2688b51148a2588d4135680b77dbbba8d6
Kenny Kessler <kenny.kessler@motorola.com>
2017-02-03 09:30:41 -0600

    power: smb-lib: Charger Rate Detection
    Add the Charger Rate detection and notification so
    that Turbo and Weak Chargers can be told to the
    interface.
    
    Change-Id: I89cac6f970b01cbf5840a34cb426cb3112b8eaf4
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/947316
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -973,6 +973,7 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_CHARGE_FULL,
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
 	POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED,
+	POWER_SUPPLY_PROP_CHARGE_RATE,
 };
 
 static int smb2_batt_get_prop(struct power_supply *psy,
@@ -1083,6 +1084,10 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED:
 		val->intval = !get_effective_result(chg->chg_disable_votable);
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_RATE:
+		mmi_chrg_rate_check(chg);
+		val->intval = chg->mmi.charger_rate;
 		break;
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
