2da764e10ca3e722064b3ee0e1af0c4d38ad0bc1
Kenny Kessler <kenny.kessler@motorola.com>
2017-02-15 19:49:57 -0600

    power: smb-lib: Ignore USB votes
    The USB PSY has the ability to send a Vote that
    is essentially a Suspend command.  That can
    cause a Charger to get pre-empted and be in a
    suspended State thus not charging will occur.
    
    Fix this by ignoring ICL calls from USB PSY. The
    Heartbeat has fiull control and will make the
    correct decision.
    
    Change-Id: I5012c719e4c712b02b0e210ddf9eb1f95dae9ef9
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/951453
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2590,14 +2590,26 @@ int smblib_set_prop_pd_current_max(struct smb_charger *chg,
 {
 	int rc;
 
+#ifdef QCOM_BASE
 	if (chg->pd_active)
 		rc = vote(chg->usb_icl_votable, PD_VOTER, true, val->intval);
 	else
 		rc = -EPERM;
+#else
+	bool enable = true;
 
+	if (0 == val->intval)
+		enable = false;
+
+	if (chg->pd_active)
+		rc = vote(chg->usb_icl_votable, PD_VOTER, enable, val->intval);
+	else
+		rc = -EPERM;
+#endif
 	return rc;
 }
 
+#ifdef QCOM_BASE
 static int smblib_handle_usb_current(struct smb_charger *chg,
 					int usb_current)
 {
@@ -2638,12 +2650,14 @@ static int smblib_handle_usb_current(struct smb_charger *chg,
 
 	return rc;
 }
+#endif
 
 int smblib_set_prop_sdp_current_max(struct smb_charger *chg,
 				    const union power_supply_propval *val)
 {
 	int rc = 0;
 
+#ifdef QCOM_BASE
 	if (!chg->pd_active) {
 		rc = smblib_handle_usb_current(chg, val->intval);
 	} else if (chg->system_suspend_supported) {
@@ -2654,6 +2668,24 @@ int smblib_set_prop_sdp_current_max(struct smb_charger *chg,
 			rc = vote(chg->usb_icl_votable,
 				PD_SUSPEND_SUPPORTED_VOTER, false, 0);
 	}
+#else
+	bool enable = true;
+
+	if (0 == val->intval)
+		enable = false;
+
+	if (!chg->pd_active) {
+		rc = vote(chg->usb_icl_votable, USB_PSY_VOTER,
+				enable, val->intval);
+	} else if (chg->system_suspend_supported) {
+		if (val->intval <= USBIN_25MA)
+			rc = vote(chg->usb_icl_votable, USB_PSY_VOTER,
+					enable, val->intval);
+		else
+			rc = vote(chg->usb_icl_votable, USB_PSY_VOTER,
+					false, 0);
+	}
+#endif
 	return rc;
 }
 
@@ -6578,6 +6610,15 @@ static void mmi_heartbeat_work(struct work_struct *work)
 	smblib_dbg(chip, PR_MOTO, "Step State = %s, EB State %s\n",
 		   stepchg_str[(int)mmi->pres_chrg_step],
 		   ebchg_str[(int)mmi->ebchg_state]);
+	smblib_dbg(chip, PR_MOTO,
+		   "EFFECTIVE: FV = %d, CDIS = %d, FCC = %d, USUS = %d, "
+		   "USBICL = %d, DCICL = %d\n",
+		   get_effective_result(chip->fv_votable),
+		   get_effective_result(chip->chg_disable_votable),
+		   get_effective_result(chip->fcc_votable),
+		   get_effective_result(chip->usb_suspend_votable),
+		   get_effective_result(chip->usb_icl_votable),
+		   get_effective_result(chip->dc_icl_votable));
 end_hb:
 	if (chip->batt_psy)
 		power_supply_changed(chip->batt_psy);
