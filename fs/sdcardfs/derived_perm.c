117e7ee580633e9f17cf15a4d53bd14d7d9b0fc1
Daniel Rosenberg <drosen@google.com>
2017-02-16 17:55:22 -0800

    ANDROID: sdcardfs: Add missing path_put
    "ANDROID: sdcardfs: Add GID Derivation to sdcardfs" introduced
    an unbalanced pat_get, leading to storage space not being freed
    after deleting a file until rebooting. This adds the missing path_put.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 34691169
    Change-Id: Ia7ef97ec2eca2c555cc06b235715635afc87940e
    Git-commit: c87888365d0fccbb6c748d5d592f082e588e0f40
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- fs/sdcardfs/derived_perm.c
+++ fs/sdcardfs/derived_perm.c
@@ -236,6 +236,7 @@ void fixup_lower_ownership(struct dentry* dentry, const char *name) {
 		if (error)
 			pr_err("sdcardfs: Failed to touch up lower fs gid/uid.\n");
 	}
+	sdcardfs_put_lower_path(dentry, &path);
 }
 
 static int descendant_may_need_fixup(struct sdcardfs_inode_info *info, struct limit_search *limit) {
