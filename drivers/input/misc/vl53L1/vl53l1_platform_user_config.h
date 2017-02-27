d749180546a4f4341d07e20727205753860f8d1d
Kalyan <kalyan-kumar.vadlamudireddy@st.com>
2017-02-27 10:39:09 -0600

    input:misc:vl53L1:patch 8.0.0 b4.1.0_1268
     - Use bare driver v4.1.0.1268
     - Add gain value configuration in calibration data
    
    Change-Id: I713be9946759caec3410052a1c94be3ad995609c
    Signed-off-by: Kalyan <kalyan-kumar.vadlamudireddy@st.com>
    Reviewed-on: https://gerrit.mot.com/956098
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/misc/vl53L1/vl53l1_platform_user_config.h
+++ drivers/input/misc/vl53L1/vl53l1_platform_user_config.h
@@ -1,33 +1,30 @@
-/*
-* Copyright (c) 2016, STMicroelectronics - All Rights Reserved
-*
-* License terms: BSD 3-clause "New" or "Revised" License.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-* this list of conditions and the following disclaimer in the documentation
-* and/or other materials provided with the distribution.
-*
-* 3. Neither the name of the copyright holder nor the names of its contributors
-* may be used to endorse or promote products derived from this software
-* without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+/*******************************************************************************
+Copyright (C) 2015, STMicroelectronics International N.V.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of STMicroelectronics nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+NON-INFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS ARE DISCLAIMED.
+IN NO EVENT SHALL STMICROELECTRONICS INTERNATIONAL N.V. BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************************/
 
 /**
  * @file  vl53l1_platform_user_config.h
@@ -58,14 +55,34 @@
 	     1.11 format. 1.0 = 0x0800, 0.975 = 0x07CC */
 
 
-#define VL53L1_OFFSET_CAL_MIN_MM1_EFFECTIVE_SPADS  0x0500
+#define VL53L1_OFFSET_CAL_MIN_EFFECTIVE_SPADS  0x0500
 	/*!< Lower Limit for the  MM1 effective SPAD count during offset
 	     calibration Format 8.8 0x0500 -> 5.0 effective SPADs */
 
-#define VL53L1_OFFSET_CAL_MAX_PRE_PEAK_RATE_MCPS   0x1400
-	/*!< Max Limit for the pre range preak rate during offset
-	     calibration Format 9.7 0x1400 -> 40.0 Mcps.
+#define VL53L1_OFFSET_CAL_MAX_PRE_PEAK_RATE_MCPS   0x1900
+	/*!< Max Limit for the pre range peak rate during offset
+	     calibration Format 9.7 0x1900 -> 50.0 Mcps.
 	     If larger then in pile up */
+
+#define VL53L1_OFFSET_CAL_MAX_SIGMA_MM             0x0040
+	/*!< Max sigma estimate limit during offset calibration
+	     Check applies to pre-range, mm1 and mm2 ranges
+	     Format 14.2 0x0040 -> 16.0mm. */
+
+
+#define VL53L1_ZONE_CAL_MAX_PRE_PEAK_RATE_MCPS     0x1900
+	/*!< Max Peak Rate Limit for the during zone calibration
+	     Format 9.7 0x1900 -> 50.0 Mcps.
+	     If larger then in pile up */
+
+#define VL53L1_ZONE_CAL_MAX_SIGMA_MM               0x0040
+	/*!< Max sigma estimate limit during zone calibration
+	     Format 14.2 0x0040 -> 16.0mm. */
+
+
+#define VL53L1_XTALK_EXTRACT_MAX_SIGMA_MM          0x008C
+	/*!< Max Sigma value allowed for a successful xtalk extraction
+	     Format 14.2 0x008C -> 35.0 mm.*/
 
 
 #define VL53L1_MAX_USER_ZONES                169
