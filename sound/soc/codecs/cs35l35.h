3f5fe165c6f06b542febd0c80c91645a0329605a
Paul Handrigan <Paul.Handrigan@cirrus.com>
2017-02-28 10:41:44 -0600

    ASoC: cs35l35: Change device tree entries and data types
    Change device tree entries and change the input date types to
    not have 32 bit data.
    
    Change-Id: Ia243a8ab6c3bfcee2911fbb2c9d35eb64e17b32b
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Reviewed-on: https://gerrit.mot.com/959137
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Yun Ma <yunma@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/codecs/cs35l35.h
+++ sound/soc/codecs/cs35l35.h
@@ -257,7 +257,7 @@
 #define CS35L35_IMON_OVFL		0x04
 
 #define CS35L35_FORMATS (SNDRV_PCM_FMTBIT_U8 | SNDRV_PCM_FMTBIT_S16_LE | \
-			SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S32_LE)
+			SNDRV_PCM_FMTBIT_S24_LE)
 
 struct  cs35l35_private {
 	struct i2c_client *i2c_client;
