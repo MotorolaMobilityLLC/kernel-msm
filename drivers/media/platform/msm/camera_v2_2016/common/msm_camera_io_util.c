b09791983b4dac34483a2e833f8470cce139625d
Shreesha Innaje <dpxv34@motorola.com>
2017-03-20 18:42:17 +0530

    [addison] Fix camera breakage
    This is a fix for regression caused due to
    security bulletin change CVE-2016-10233.
    
    Change-Id: I8b871d90f9c396a47f941f85fdf47f0fa143c99c
    Signed-off-by: Shreesha Innaje <dpxv34@motorola.com>
    Reviewed-on: https://gerrit.mot.com/965224
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/media/platform/msm/camera_v2_2016/common/msm_camera_io_util.c
+++ drivers/media/platform/msm/camera_v2_2016/common/msm_camera_io_util.c
@@ -395,7 +395,7 @@ int msm_camera_config_vreg(struct device *dev, struct camera_vreg_t *cam_vreg,
 		return -EINVAL;
 	}
 
-	if (cam_vreg == NULL) {
+	if (cam_vreg == NULL && num_vreg_seq > 0) {
 		pr_err("%s:%d cam_vreg sequence invalid\n", __func__, __LINE__);
 		return -EINVAL;
 	}
