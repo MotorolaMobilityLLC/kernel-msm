3ebe21346c9d5da9f1fb6063662930050e5194bf
Marina Gabriela Silva <mgsilva@motorola.com>
2017-03-21 15:27:02 -0300

    Revert "netlink: Fix dump skb leak/double free"
    This reverts commit c19c868453d7151fe27bd7e8faa344211f77d163.

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -2409,7 +2409,6 @@ static int netlink_dump(struct sock *sk)
 	struct netlink_callback *cb;
 	struct sk_buff *skb = NULL;
 	struct nlmsghdr *nlh;
-	struct module *module;
 	int len, err = -ENOBUFS;
 	int alloc_size;
 
@@ -2459,11 +2458,9 @@ static int netlink_dump(struct sock *sk)
 	if (cb->done)
 		cb->done(cb);
 	nlk->cb = NULL;
-	module = cb->module;
-	skb = cb->skb;
 	mutex_unlock(nlk->cb_mutex);
-	module_put(module);
-	consume_skb(skb);
+
+	module_put(cb->module);
 	netlink_consume_callback(cb);
 	return 0;
 
