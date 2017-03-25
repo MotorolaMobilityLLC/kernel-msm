2f3bd8a1c0889fde418ac61bfb6e221c97c7efea
luodw1 <luodw1@motorola.com>
2017-03-25 10:31:18 +0800

    touchscreen: fix shutdown panic
    The shutdown function will happened panic when
    not connected touchscreen.
    Change-Id: I0cb5dcb99b3403f0337510ecdafa6780b5183088
    Reviewed-on: https://gerrit.mot.com/968699
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Wen Xie <xiewen3@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -367,6 +367,7 @@ struct ft5x06_ts_data {
 	struct notifier_block fps_notif;
 	bool clipping_on;
 	struct ft5x06_clip_area *clipa;
+	bool is_tp_registered;
 };
 
 static int ft5x06_ts_start(struct device *dev);
@@ -3247,6 +3248,8 @@ void ft5x06_ts_shutdown(struct i2c_client *client)
 	struct ft5x06_ts_data *data = i2c_get_clientdata(client);
 	int retval;
 
+	if (!data->is_tp_registered)
+		return;
 	free_irq(client->irq, data);
 
 	if (gpio_is_valid(data->pdata->reset_gpio)) {
@@ -3340,6 +3343,7 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 	data->force_reflash = (data->pdata->no_force_update) ? false : true;
 	data->flash_enabled = true;
 	data->irq_enabled = false;
+	data->is_tp_registered = false;
 
 	data->patching_enabled = true;
 	err = ft5x06_dt_parse_modifiers(data);
@@ -3767,6 +3771,7 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 		} else
 			data->is_fps_registered = true;
 	}
+	data->is_tp_registered = true;
 	return 0;
 
 free_fb_notifier:
