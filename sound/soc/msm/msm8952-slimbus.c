d560b997b5376b89531043afb3f92afb80a9fe83
Pratik Kamdar <djc637@motorola.com>
2017-03-27 13:26:51 -0500

    ASoC: aov_trigger: create sysfs nodes earlier
    - Move creation of aov sysfs nodes earlier in the boot up
    process.
    - This is done to avoid getting delayed by missing DAI links
    later in the codec initialization sequence
    - Better error handling during node creation
    
    Change-Id: I4747e967b2887a5900e70cc5d60b9eccb72902d5
    Signed-off-by: Pratik Kamdar <djc637@motorola.com>
    Reviewed-on: https://gerrit.mot.com/972412
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- sound/soc/msm/msm8952-slimbus.c
+++ sound/soc/msm/msm8952-slimbus.c
@@ -2903,7 +2903,7 @@ int marley_dai_init(struct snd_soc_pcm_runtime *rtd)
 	struct snd_soc_dapm_context *dapm = &codec->dapm;
 	struct snd_soc_card *card = codec->component.card;
 
-	aov_trigger_init(codec);
+	aov_trigger_register_notifier(codec);
 	ret = snd_soc_codec_set_pll(codec, MARLEY_FLL1_REFCLK,
 			ARIZONA_FLL_SRC_NONE,
 			0, 0);
@@ -3435,6 +3435,7 @@ static int msm8952_asoc_machine_probe(struct platform_device *pdev)
 			ret);
 		goto err;
 	}
+	aov_trigger_init();
 #endif
 	ret = msm8952_populate_dai_link_component_of_node(card);
 	if (ret) {
