5a6835ef51a662f0b347968ae436b64476cfca21
Walter Yang <yandongy@codeaurora.org>
2017-03-29 13:15:26 -0300

    ASoC: msm: set pointers to NULL after kfree
    In lsm-related driver files, some pointers are not set as NULL
    after the memory is freed, which will leave many dangling pointers.
    Set them to NULL explicitly to avoid potential risk.
    
    Change-Id: I4e904522253ee5734fee0c06f174cf8c276aa9fe

--- sound/soc/msm/msm-cpe-lsm.c
+++ sound/soc/msm/msm-cpe-lsm.c
@@ -639,6 +639,7 @@ static int msm_cpe_lsm_ioctl(struct snd_pcm_substream *substream,
 			dev_err(rtd->dev, "%s: No memory for sound model\n",
 				__func__);
 			kfree(session->conf_levels);
+			session->conf_levels = NULL;
 			return -ENOMEM;
 		}
 		session->snd_model_size = snd_model.data_size;
@@ -650,6 +651,8 @@ static int msm_cpe_lsm_ioctl(struct snd_pcm_substream *substream,
 				__func__);
 			kfree(session->conf_levels);
 			kfree(session->snd_model_data);
+			session->conf_levels = NULL;
+			session->snd_model_data = NULL;
 			return -EFAULT;
 		}
 
@@ -661,6 +664,8 @@ static int msm_cpe_lsm_ioctl(struct snd_pcm_substream *substream,
 			       __func__, rc);
 			kfree(session->snd_model_data);
 			kfree(session->conf_levels);
+			session->snd_model_data = NULL;
+			session->conf_levels = NULL;
 			return rc;
 		}
 
@@ -674,6 +679,8 @@ static int msm_cpe_lsm_ioctl(struct snd_pcm_substream *substream,
 			lsm_ops->lsm_shmem_dealloc(cpe->core_handle, session);
 			kfree(session->snd_model_data);
 			kfree(session->conf_levels);
+			session->snd_model_data = NULL;
+			session->conf_levels = NULL;
 			return rc;
 		}
 
