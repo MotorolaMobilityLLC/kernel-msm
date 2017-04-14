769205a5b8055808eb7e860899d680ab7cab5747
Ravi Vembu <raviv@motorola.com>
2017-04-14 12:48:46 -0500

    usb: dwc3: Avoid DWC3_CONTROLLER_ERROR flood
    DWC3_CONTROLLER_ERROR events can flood the system repeatedly
    causing the controller to be reset in an endless loop and hog
    the system. Avoid this by ignoring any Error events after 5 retries.
    
    Change-Id: I5a9e98df560090f329869510af53eaccc4e93041
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Moto-CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/980536
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1167327
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key
    
    Conflicts:
    	drivers/usb/dwc3/core.h
    Change-Id: I5a9e98df560090f329869510af53eaccc4e93041
    Reviewed-on: https://gerrit.mot.com/1257196
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1338860

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1975,6 +1975,7 @@ static void dwc3_msm_vbus_draw_work(struct work_struct *w)
 	dwc3_msm_gadget_vbus_draw(mdwc, dwc->vbus_draw);
 }
 
+#define MAX_ERR_CNT 5
 static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 							unsigned int value)
 {
@@ -1986,6 +1987,10 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 
 	switch (event) {
 	case DWC3_CONTROLLER_ERROR_EVENT:
+		/* Avoid a flood of Error events */
+		if (dwc->err_cnt++ >= MAX_ERR_CNT)
+			break;
+
 		dev_info(mdwc->dev,
 			"DWC3_CONTROLLER_ERROR_EVENT received, irq cnt %lu\n",
 			dwc->irq_cnt);
@@ -4572,6 +4577,9 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 		}
 	}
 
+	/* Reset Controller Error Count */
+	dwc->err_cnt = 0;
+
 	if (on) {
 		dev_dbg(mdwc->dev, "%s: turn on host\n", __func__);
 
@@ -4745,6 +4753,9 @@ static void dwc3_override_vbus_status(struct dwc3_msm *mdwc, bool vbus_present)
 static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 {
 	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
+
+	/* Reset Controller Error Count */
+	dwc->err_cnt = 0;
 
 	pm_runtime_get_sync(mdwc->dev);
 	dbg_event(0xFF, "StrtGdgt gsync",
