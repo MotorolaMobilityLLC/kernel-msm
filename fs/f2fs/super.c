d08311c8d33af0ae6208cc55b075bcd178629e6e
Jin Qian <jinqian@google.com>
2017-04-25 16:28:48 -0700

    UPSTREAM: f2fs: sanity check segment count
    commit b9dd46188edc2f0d1f37328637860bb65a771124 upstream.
    
    F2FS uses 4 bytes to represent block address. As a result, supported
    size of disk is 16 TB and it equals to 16 * 1024 * 1024 / 2 segments.
    
    Signed-off-by: Jin Qian <jinqian@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Bug: 36815012
    Change-Id: I30ea36df066bc07e32e767336b7cae12063fe415
    Git-commit: 2d62b25936243b6994b55674ec817ddf20590073
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -442,6 +442,14 @@ static int sanity_check_raw_super(struct super_block *sb,
 		f2fs_msg(sb, KERN_INFO, "Invalid log sectors per block");
 		return 1;
 	}
+
+	if (le32_to_cpu(raw_super->segment_count) > F2FS_MAX_SEGMENT) {
+		f2fs_msg(sb, KERN_INFO,
+			"Invalid segment count (%u)",
+			le32_to_cpu(raw_super->segment_count));
+		return 1;
+	}
+
 	return 0;
 }
 
