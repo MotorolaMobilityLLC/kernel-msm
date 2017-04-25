2f5e4bbfb8dd7b94126f62bac504c353cd830e0b
Utkarsh Saxena <usaxena@codeaurora.org>
2017-04-25 17:39:41 +0530

    msm: ipa: Fix for missing int overflow check in the refcount library
    Overflow of reference counter can lead to memory leak.
    
    Before incrementing the reference count, check with
    U32_MAX and return for error check.
    
    Mot-CRs-fixed:(CR)
    CVE-fixed: CVE-2017-0746
    
    Bug: 35467471
    CRs-Fixed: 2029392
    
    Change-Id: Ib96d36574ee086ec73c9836110cb2c98e8ae3d66
    Acked-by: Mohammed Javid <mjavid@qti.qualcomm.com>
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1025042
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 9d22c7a559ea8d83fd3b55130782bae7d2f458af)

--- drivers/platform/msm/ipa/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_rt.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, 2016-2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1325,6 +1325,10 @@ int ipa_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 	mutex_lock(&ipa_ctx->lock);
 	entry = __ipa_find_rt_tbl(lookup->ip, lookup->name);
 	if (entry && entry->cookie == IPA_COOKIE) {
+		if (entry->ref_cnt == U32_MAX) {
+			IPAERR("fail: ref count crossed limit\n");
+			goto ret;
+		}
 		entry->ref_cnt++;
 		lookup->hdl = entry->id;
 
@@ -1334,6 +1338,8 @@ int ipa_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 
 		result = 0;
 	}
+
+ret:
 	mutex_unlock(&ipa_ctx->lock);
 
 	return result;
