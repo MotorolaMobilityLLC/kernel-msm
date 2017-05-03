a348342164cb012e36ec5b15fa2aeae48e6d6e0a
huangys <huangys@motorola.com>
2017-05-03 08:49:22 +0800

    Revert "ASoC: sdm660: Force channels num for cs47l35 slim dais"
    This reverts commit 364b3cae2ca950f8cfaa80c6f4a9bb092dfc3597.
    CRs-fixed: (CR)
    
    Change-Id: I77a5b6fa660cfbc96740d8d7e8a317f21f87b71e
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/987936
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Vitor Queiroz Albuquerque <vitoralb@motorola.com>

--- sound/soc/msm/sdm660-external.c
+++ sound/soc/msm/sdm660-external.c
@@ -122,29 +122,29 @@ struct dev_config {
 
 /* Default configuration of slimbus channels */
 static struct dev_config slim_rx_cfg[] = {
-	[SLIM_RX_0] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_1] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_2] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_3] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_4] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_5] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
-	[SLIM_RX_6] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
+	[SLIM_RX_0] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_1] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_2] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_3] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_4] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_5] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_RX_6] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_RX_7] = {SAMPLING_RATE_8KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 };
 
 static struct dev_config slim_tx_cfg[] = {
-	[SLIM_TX_0] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 3},
-	[SLIM_TX_1] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
+	[SLIM_TX_0] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_TX_1] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_TX_2] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
-	[SLIM_TX_3] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 3},
-	[SLIM_TX_4] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 3},
+	[SLIM_TX_3] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+	[SLIM_TX_4] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_TX_5] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_TX_6] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_TX_7] = {SAMPLING_RATE_8KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SLIM_TX_8] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
 };
 
-static int msm_vi_feed_tx_ch = 3;
+static int msm_vi_feed_tx_ch = 2;
 static const char *const slim_rx_ch_text[] = {"One", "Two"};
 static const char *const slim_tx_ch_text[] = {"One", "Two", "Three", "Four",
 						"Five", "Six", "Seven",
@@ -661,14 +661,9 @@ static int msm_slim_rx_ch_put(struct snd_kcontrol *kcontrol,
 	if (ch_num < 0)
 		return ch_num;
 
-#ifndef CONFIG_SND_SOC_MADERA
 	slim_rx_cfg[ch_num].channels = ucontrol->value.enumerated.item[0] + 1;
 	pr_debug("%s: msm_slim_[%d]_rx_ch  = %d\n", __func__,
 		 ch_num, slim_rx_cfg[ch_num].channels);
-#else
-	pr_debug("%s: cs47l35 dai, using default: msm_slim_[%d]_rx_ch  = %d\n",
-		__func__, ch_num, slim_rx_cfg[ch_num].channels);
-#endif
 
 	return 1;
 }
@@ -695,14 +690,10 @@ static int msm_slim_tx_ch_put(struct snd_kcontrol *kcontrol,
 
 	if (ch_num < 0)
 		return ch_num;
-#ifndef CONFIG_SND_SOC_MADERA
+
 	slim_tx_cfg[ch_num].channels = ucontrol->value.enumerated.item[0] + 1;
 	pr_debug("%s: msm_slim_[%d]_tx_ch = %d\n", __func__,
 		 ch_num, slim_tx_cfg[ch_num].channels);
-#else
-	pr_debug("%s: cs47l35 dai, using default: msm_slim_[%d]_tx_ch  = %d\n",
-		 __func__, ch_num, slim_tx_cfg[ch_num].channels);
-#endif
 
 	return 1;
 }
@@ -719,14 +710,9 @@ static int msm_vi_feed_tx_ch_get(struct snd_kcontrol *kcontrol,
 static int msm_vi_feed_tx_ch_put(struct snd_kcontrol *kcontrol,
 				 struct snd_ctl_elem_value *ucontrol)
 {
-#ifndef CONFIG_SND_SOC_MADERA
 	msm_vi_feed_tx_ch = ucontrol->value.integer.value[0] + 1;
 
 	pr_debug("%s: msm_vi_feed_tx_ch = %d\n", __func__, msm_vi_feed_tx_ch);
-#else
-	pr_debug("%s: cs47l35 dai, using default msm_vi_feed_tx_ch = %d\n",
-		 __func__, msm_vi_feed_tx_ch);
-#endif
 	return 1;
 }
 
