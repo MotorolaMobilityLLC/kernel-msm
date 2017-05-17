c004b58a9bb4d9b52045deccaf57dd5cbf54ad31
Ravi Vembu <raviv@motorola.com>
2017-05-17 10:21:02 -0500

    power: battery: Fix Type-C audio adapter detection at boot.
    Commit "update parallel algorithm for ICL change" breaks type-C
    Analog Audio adapter detection at boot time. Avoid re-running
    AICL at boot (when icl_ua is unitialized) and also minimize
    the wait time for AICL to complete.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: Ib841bc0fe84edbcd34374554909e5dde0b65e775
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: https://gerrit.mot.com/994075
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -549,27 +549,29 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	}
 
 	/* rerun AICL if new ICL is above settled ICL */
-	if (icl_ua > pval.intval)
+	if (icl_ua != INT_MAX && icl_ua > pval.intval)
 		rerun_aicl = true;
 
 	if (rerun_aicl) {
 		/* set a lower ICL */
 		pval.intval = max(pval.intval - ICL_STEP_UA, ICL_STEP_UA);
+		pr_debug("****initial step icl setting %d\n", pval.intval);
 		power_supply_set_property(chip->main_psy,
 				POWER_SUPPLY_PROP_CURRENT_MAX,
 				&pval);
 		/* wait for ICL change */
-		msleep(100);
+		udelay(500);
 	}
 
 	/* set the effective ICL */
 	pval.intval = icl_ua;
+	pr_debug("****final icl setting %d\n", pval.intval);
 	power_supply_set_property(chip->main_psy,
 			POWER_SUPPLY_PROP_CURRENT_MAX,
 			&pval);
 	if (rerun_aicl)
 		/* wait for ICL change */
-		msleep(100);
+		udelay(500);
 
 	vote(chip->pl_disable_votable, ICL_CHANGE_VOTER, false, 0);
 
