60bc59b8f4ca27abccbafe27e726001b31b23570
a7301c <a7301c@motorola.com>
2017-05-21 19:28:32 -0500

    IKQCOMSD1-3550 Revert "ASoC: msm: Add changes to enable TDM feature in APQ8017"
    This reverts commit b5369025927a41ccb4600931c704cb0278abebca.
    Which breaks addison audio function.
    ++++
    01-02 18:40:50.021     0     0 E         : [    5.589100,5]
    msm8952-slimbus-wcd c051000.sound: MUX addr invalid for QUIN PCM
    01-02 18:40:50.021     0     0 E         : [    5.589103,5]
    msm8952-slimbus-wcd c051000.sound: MUX addr invalid for EXT CLK CTL
    01-02 18:40:50.021     0     0 E         : [    5.589106,5]
    msm8952-slimbus-wcd c051000.sound: MUX addr invalid for SEC TLMM CTL
    01-02 18:40:50.021     0     0 E         : [    5.589483,5]
    msm8952-slimbus-wcd c051000.sound: control 3:0:0:IEC958 Playback PCM
    Stream:0 is already present
    01-02 18:40:50.021     0     0 E         : [    5.589487,5]
    msm-hdmi-dba-codec-rx soc:qcom,msm-hdmi-dba-codec-rx: ASoC: Failed to
    add IEC958 Playback PCM Stream: -16
    ...
    01-02 18:40:50.022     0     0 W         : [    7.184341,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: no dapm match for
    QUIN_MI2S_TX --> QUIN_MI2S_TX --> MultiMedia5 Mixer
    01-02 18:40:50.022     0     0 E         : [    7.184344,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: Failed to add route
    QUIN_MI2S_TX -> QUIN_MI2S_TX -> MultiMedia5 Mixer
    01-02 18:40:50.022     0     0 W         : [    7.187115,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: no dapm match for
    VOICEMMODE1_DL --> VoiceMMode1 --> SEC_MI2S_RX_Voice Mixer
    01-02 18:40:50.022     0     0 E         : [    7.187118,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: Failed to add route
    VOICEMMODE1_DL -> VoiceMMode1 -> SEC_MI2S_RX_Voice Mixer
    01-02 18:40:50.022     0     0 W         : [    7.187130,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: no dapm match for
    VOICEMMODE2_DL --> VoiceMMode2 --> SEC_MI2S_RX_Voice Mixer
    01-02 18:40:50.022     0     0 E         : [    7.187133,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: Failed to add route
    VOICEMMODE2_DL -> VoiceMMode2 -> SEC_MI2S_RX_Voice Mixer
    01-02 18:40:50.022     0     0 E         : [    7.188974,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: no source widget found
    for AUDIO_REF_EC_UL3 MUX
    01-02 18:40:50.022     0     0 E         : [    7.188978,5]
    msm-pcm-routing soc:qcom,msm-pcm-routing: ASoC: Failed to add route
    AUDIO_REF_EC_UL3 MUX -> direct -> MM_UL3
    
    ++++
    
    Change-Id: I3bf6e9642b2e26b1dc1ae2a6304919a27690d3c4
    Signed-off-by: a7301c <a7301c@motorola.com>
    Reviewed-on: https://gerrit.mot.com/998755
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    
    # Conflicts:
    #	sound/soc/msm/msm8952-slimbus.c

--- include/sound/q6afe-v2.h
+++ include/sound/q6afe-v2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -336,7 +336,7 @@ int afe_send_custom_tdm_header_cfg(
 	struct afe_param_id_custom_tdm_header_cfg *custom_tdm_header_cfg,
 	u16 port_id);
 int afe_tdm_port_start(u16 port_id, struct afe_tdm_port_config *tdm_port,
-		u32 rate, u16 num_groups);
+		u32 rate);
 
 #ifdef CONFIG_SND_SOC_OPALUM
 int ospl2xx_afe_set_callback(int32_t (*ospl2xx_callback_func)
