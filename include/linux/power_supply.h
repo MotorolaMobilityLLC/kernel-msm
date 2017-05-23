b7268ab3bf34f6810c4951c822ceb5d0a30ecb38
Kenny Kessler <kenny.kessler@motorola.com>
2017-05-23 14:25:05 -0500

    power: core: Free drv_data
    The driver that calls for a PSY to
    be created will also be able to pass
    a pointer to its memory in the
    struct power_supply_config at registration.
    
    Extend the struct power_supply_config so
    the creation driver can also give control
    of this memory to the PSY.  That way if the
    kref is still inuse when the driver runs
    power_supply_unregister the driver memory
    can be freed upon release of the kref.
    
    Change-Id: I435ec4a7adcffd10dcabc65d39185994f9237020
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1000223
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -439,6 +439,7 @@ struct power_supply_config {
 
 	char **supplied_to;
 	size_t num_supplicants;
+	bool free_drv_data;
 };
 
 /* Description of power supply */
@@ -492,6 +493,7 @@ struct power_supply {
 
 	/* Driver private data */
 	void *drv_data;
+	bool free_pdd_on_release;
 
 	/* private */
 	struct device dev;
