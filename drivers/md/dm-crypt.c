d667ccf1ff23697235faee5aed02d16c97357806
Erfan Abdi <erfangplus@gmail.com>
2017-05-28 18:57:05 +0430

    dm-crypt: increase threads
    # Conflicts:
    #	drivers/md/dm-crypt.c

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1851,7 +1851,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		cc->crypt_queue = alloc_workqueue("kcryptd", WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM, 1);
 	else
 		cc->crypt_queue = alloc_workqueue("kcryptd", WQ_CPU_INTENSIVE | WQ_MEM_RECLAIM | WQ_UNBOUND,
-						  num_online_cpus());
+						  num_online_cpus() * 2);
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
