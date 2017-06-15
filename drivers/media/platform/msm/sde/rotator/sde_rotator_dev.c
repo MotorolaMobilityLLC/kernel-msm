4ce5838c566b55bf1c9f491c86674059198aa890
Benjamin Chan <bkchan@codeaurora.org>
2017-06-15 18:18:54 -0400

    msm: sde: Remove output fence object after user request completed
    The fence object is allocated in the driver during the buffer queuing
    time. When user requested the output buffer fence fd, driver should not
    hold on to the fence object anymore because there is only a single
    reference to the fence. When user destroy the fence by closing the fd,
    the reference is cleared. Driver only needs to destroy the fence object
    if the user does not request it.
    
    Mot-CRs-fixed: (CR)
    CVE-fixed: CVE-2017-11031
    
    CRs-Fixed: 2059181
    Change-Id: Ic83d93fd3c7f404774007065df02b402adbf80af
    Signed-off-by: Benjamin Chan <bkchan@codeaurora.org>
    Signed-off-by: Ashwin Pathmudi <jfxr63@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1096455
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/sde/rotator/sde_rotator_dev.c
+++ drivers/media/platform/msm/sde/rotator/sde_rotator_dev.c
@@ -625,7 +625,7 @@ static void sde_rotator_stop_streaming(struct vb2_queue *q)
 			struct sde_rotator_vbinfo *vbinfo =
 					&ctx->vbinfo_cap[i];
 
-			if (vbinfo->fence && vbinfo->fd < 0) {
+			if (vbinfo->fence) {
 				/* fence is not used */
 				SDEDEV_DBG(rot_dev->dev,
 						"put fence s:%d t:%d i:%d\n",
@@ -1555,7 +1555,7 @@ static int sde_rotator_dqbuf(struct file *file,
 			&& (buf->index < ctx->nbuf_cap)) {
 		int idx = buf->index;
 
-		if (ctx->vbinfo_cap[idx].fence && ctx->vbinfo_cap[idx].fd < 0) {
+		if (ctx->vbinfo_cap[idx].fence) {
 			/* fence is not used */
 			SDEDEV_DBG(ctx->rot_dev->dev, "put fence s:%d i:%d\n",
 					ctx->session_id, idx);
@@ -1961,6 +1961,7 @@ static long sde_rotator_private_ioctl(struct file *file, void *fh,
 					ctx->session_id);
 				return vbinfo->fd;
 			}
+			vbinfo->fence = NULL;
 		}
 		fence->fd = vbinfo->fd;
 
