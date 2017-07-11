0f0d6e724302346dd657375bd782f81aa7a52881
Kenny Kessler <kenny.kessler@motorola.com>
2017-07-11 12:56:15 -0500

    power: smb2: Clear HDC AICL
    High Duty Cycle can kick in at Full on
    some chargers.  This will cause AICL to drop
    the ICL to a low value such that it will not
    maintain Full.  When charging resumes at 99%
    it will not have enough Input current to
    bring the battery back to Full.
    
    Turn OFF AICL trip via HDC.
    
    Change-Id: I9ad5843bba5a2859459f8d09f06438b4acac34d0
    Moto-CRs-fixed: (CR)
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1025130
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: yanyh2 <yanyh2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1074283
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -2031,8 +2031,11 @@ static int smb2_init_hw(struct smb2 *chip)
 	 * start from min, AICL rerun enable and AICL ADC disable
 	 */
 	rc = smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,
-			USBIN_AICL_START_AT_MAX_BIT | USBIN_AICL_RERUN_EN_BIT
-			| USBIN_AICL_ADC_EN_BIT, USBIN_AICL_RERUN_EN_BIT);
+			USBIN_AICL_START_AT_MAX_BIT
+			| USBIN_AICL_ADC_EN_BIT
+			| USBIN_AICL_RERUN_EN_BIT
+			| USBIN_AICL_HDC_EN_BIT,
+			USBIN_AICL_RERUN_EN_BIT);
 	if (rc < 0) {
 		dev_err(chg->dev, "Couldn't configure AICL rc=%d\n", rc);
 		return rc;
