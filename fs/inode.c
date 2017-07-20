aa08646641a3b4dd8a5cbacaa57dfe87acaf0438
Shiyong Li <a22381@motorola.com>
2017-07-19 18:10:07 -0700

    fs: support partial relatime update
    Allow updating relative access time of specific path if seeing inode
    flag S_RELATIME whatever flag S_NOATIME is set.
    
    port (CR) from P.
    
    Change-Id: I5ea5b34264740619fd305f1ff703949800749c34
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1435306
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- fs/inode.c
+++ fs/inode.c
@@ -1676,6 +1676,34 @@ static int update_time(struct inode *inode, struct timespec64 *time, int flags)
 	return update_time(inode, time, flags);
 }
 
+static bool partial_relatime_needs_update(const struct path *path,
+	struct inode *inode)
+{
+	struct timespec64 now;
+
+	if (!(inode->i_flags & S_RELATIME))
+		return false;
+
+	now = current_time(inode);
+
+	if (timespec64_compare(&inode->i_mtime, &inode->i_atime) >= 0) {
+		pr_debug("%s: %lu - younger mtime\n", __func__, inode->i_ino);
+		return true;
+	}
+
+	if (timespec64_compare(&inode->i_ctime, &inode->i_atime) >= 0) {
+		pr_debug("%s: %lu - younger ctime\n", __func__, inode->i_ino);
+		return true;
+	}
+
+	if ((long)(now.tv_sec - inode->i_atime.tv_sec) >= 24*60*60) {
+		pr_debug("%s: %lu - atime older than a day\n", __func__,
+			inode->i_ino);
+		return true;
+	}
+
+	return false;
+}
 /**
  *	touch_atime	-	update the access time
  *	@path: the &struct path to update
@@ -1689,6 +1717,9 @@ bool atime_needs_update(const struct path *path, struct inode *inode)
 {
 	struct vfsmount *mnt = path->mnt;
 	struct timespec64 now;
+
+	if (partial_relatime_needs_update(path, inode))
+		return true;
 
 	if (inode->i_flags & S_NOATIME)
 		return false;
