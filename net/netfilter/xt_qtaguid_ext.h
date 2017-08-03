faa3dc152c20ebf937b23b758e6c312e3e31ee81
Yu Miao <miaoyu4@lenovo.com>
2017-08-03 20:44:53 +0800

    print uid,ip,port of a sk_buff.
    This feature is only used for debugging and issue analyzing.
    Many applications consumed much mobile data, however according
    to the tcpdump log, it's difficult to identify who consumed much
    data. This feature could meet this target.identify which process
    consumed more internet data and how much data consumed.
    
    Change-Id: Id309c991e67b8e1f55b65cd3c018a587a447eb9a
    Signed-off-by: miaoyu4 <miaoyu4@mt.com>
    Reviewed-on: https://gerrit.mot.com/1017076
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xu Gao <njh348@motorola.com>
    Reviewed-by: Hongjie Liu <liuhj7@mt.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1035962
    Reviewed-by: Igor Kovalenko <igork@motorola.com>

--- net/netfilter/xt_qtaguid_ext.h
+++ net/netfilter/xt_qtaguid_ext.h
@@ -9,6 +9,10 @@
 
 #include <linux/netfilter/x_tables.h>
 
+#define DEFAULT_MASK	0
+#define DEBUG_OS_MASK	BIT(0)
+#define DEBUG_ALL_MASK	BIT(1)
+
 int recent_owner_init(void);
 
 uid_t recent_owner_lookup(const struct sk_buff *skb,
@@ -16,6 +20,7 @@ uid_t recent_owner_lookup(const struct sk_buff *skb,
 
 void recent_owner_update(const struct sk_buff *skb,
 			 struct xt_action_param *par,
-			 uid_t uid);
+			 uid_t uid,
+			 int debug_os_data);
 
 #endif  /* ifndef __XT_QTAGUID_EXT_H__ */
