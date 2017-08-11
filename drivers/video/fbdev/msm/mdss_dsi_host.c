be7d978f99d474d1f2f7a28ede214e34b83fb921
wangyq13 <wangyq13@lenovo.com>
2017-08-11 16:49:19 +0800

    fbdev: msm: add support for long response of short read
    Some ICs such as HIMAX 8399C, when DCS short read(1 or 2 byte), IC reply
    using long packet. Add these codes to support such behavior.
    
    Change-Id: I3dae0562120906ec9e8450465d41089c3e7939fa
    Signed-off-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1037912
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/video/fbdev/msm/mdss_dsi_host.c
+++ drivers/video/fbdev/msm/mdss_dsi_host.c
@@ -2033,7 +2033,8 @@ int mdss_dsi_cmds_rx(struct mdss_dsi_ctrl_pdata *ctrl,
 do_send:
 	ctrl->cmd_cfg_restore = __mdss_dsi_cmd_mode_config(ctrl, 1);
 
-	if (rlen <= 2) {
+	if ((rlen <= 2) &&
+		!ctrl->panel_data.panel_info.panel_reply_long_response) {
 		short_response = 1;
 		pkt_size = rlen;
 		rx_byte = 4;
