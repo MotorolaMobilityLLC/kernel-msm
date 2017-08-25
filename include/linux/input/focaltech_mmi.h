60c253508447320d68f161612e5099b6cd852708
wengjun1 <wengjun1@motorola.com>
2017-08-25 16:11:17 +0800

    input: focaltech_mmi: bringup
    Port focaltech_mmi basecode to bo-8996.
    - Use %zd instead of %lu to print variables which type is size_t
    - Use psy-> instead of psy->desc
    
    Change-Id: I9981da564ec495206e0cdd6df9989311717697c8
    Signed-off-by: wengjun1 <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1047073
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

---
+++ include/linux/input/focaltech_mmi.h
@@ -0,0 +1,73 @@
+/*
+ *
+ * FocalTech mmi TouchScreen driver header file.
+ *
+ * Copyright (c) 2010  Focal tech Ltd.
+ * Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __LINUX_FOCALTECH_MMI_H__
+#define __LINUX_FOCALTECH_MMI_H__
+
+#define FT5X06_ID		0x55
+#define FT5X16_ID		0x0A
+#define FT5X36_ID		0x14
+#define FT6X06_ID		0x06
+#define FT6X36_ID		0x36
+#define FT8716_ID		0x87
+#define FT5X46_ID		0x54
+
+struct fw_upgrade_info {
+	bool auto_cal;
+	u16 delay_aa;
+	u16 delay_55;
+	u8 upgrade_id_1;
+	u8 upgrade_id_2;
+	u16 delay_readid;
+	u16 delay_erase_flash;
+};
+
+struct ft_ts_platform_data {
+	struct fw_upgrade_info info;
+	const char *name;
+	const char *fw_name;
+	u32 irqflags;
+	u32 irq_gpio;
+	u32 irq_gpio_flags;
+	u32 reset_gpio;
+	u32 reset_gpio_flags;
+	u32 family_id;
+	u32 x_max;
+	u32 y_max;
+	u32 x_min;
+	u32 y_min;
+	u32 panel_minx;
+	u32 panel_miny;
+	u32 panel_maxx;
+	u32 panel_maxy;
+	u32 group_id;
+	u32 hard_rst_dly;
+	u32 soft_rst_dly;
+	u32 num_max_touches;
+	bool fw_vkey_support;
+	bool no_force_update;
+	bool i2c_pull_up;
+	bool ignore_id_check;
+	bool gesture_support;
+	bool resume_in_workqueue;
+	bool x_flip;
+	bool y_flip;
+	int (*power_init)(bool);
+	int (*power_on)(bool);
+};
+
+#endif
