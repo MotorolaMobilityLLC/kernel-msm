113807638cf19d946e99275fc588163f904ef1e6
Bjorn Andersson <bjorn.andersson@linaro.org>
2017-09-06 23:03:24 -0700

    net: qrtr: Move constants to header file
    The constants are used by both the name server and clients, so clarify
    their value and move them to the uapi header.
    
    Change-Id: I670694bb1ab71eace1efa186bcdf097b7a920653
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Git-Commit: 28978713c51b0a70acf748f76f9d6d2d20dcf980
    Git-Repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [bharad@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Naitik Bharadiya <bharad@codeaurora.org>

--- include/uapi/linux/qrtr.h
+++ include/uapi/linux/qrtr.h
@@ -3,6 +3,9 @@
 
 #include <linux/socket.h>
 
+#define QRTR_NODE_BCAST        0xffffffffu
+#define QRTR_PORT_CTRL 0xfffffffeu
+
 struct sockaddr_qrtr {
 	__kernel_sa_family_t sq_family;
 	__u32 sq_node;
