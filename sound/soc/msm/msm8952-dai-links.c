fd9e3a9a4b01aaccb62831c86aa0d5d27abaec29
chengx2 <chengx2@motorola.com>
2017-09-12 19:32:57 +0800

    soc/msm: Fix up the issue that FM has no sound
    1),The ali project has no mods, so wrap the mods dai with
    CONFIG_SND_SOC_MODS_CODEC_SHIM
    2),The hw parameters have been set as 16k by mods control, it should be 48k for FM
    
    Change-Id: Iadb5b3e6909e47e13d535622e7ee2057a534f05f
    Signed-off-by: chengx2 <chengx2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1056415
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/msm/msm8952-dai-links.c
+++ sound/soc/msm/msm8952-dai-links.c
@@ -531,6 +531,7 @@ static struct snd_soc_dai_link msm8952_marley_mods_be_dai[] = {
 	}
 };
 
+#ifdef CONFIG_SND_SOC_MODS_CODEC_SHIM
 static struct snd_soc_dai_link msm8952_marley_albus_mods_be_dai[] = {
 	{
 		/* mods I2S in and out */
@@ -563,6 +564,7 @@ static struct snd_soc_dai_link msm8952_marley_albus_mods_be_dai[] = {
 		.ignore_suspend = 1,
 	}
 };
+#endif
 
 static struct snd_soc_dai_link msm8952_marley_be_dai[] = {
 	/* Backend DAI Links */
@@ -2095,18 +2097,26 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 			memcpy(msm8952_marley_dai_links + len_2a,
 				msm8952_marley_l35_dai_link,
 				sizeof(msm8952_marley_l35_dai_link));
-			memcpy(msm8952_marley_dai_links + len3,
-				msm8952_marley_albus_mods_be_dai,
-				sizeof(msm8952_marley_albus_mods_be_dai));
-			len4 = len3 + ARRAY_SIZE(msm8952_marley_mods_be_dai);
+			#ifdef CONFIG_SND_SOC_MODS_CODEC_SHIM
+				memcpy(msm8952_marley_dai_links + len3,
+					msm8952_marley_albus_mods_be_dai,
+					sizeof(msm8952_marley_albus_mods_be_dai));
+				len4 = len3 + ARRAY_SIZE(msm8952_marley_mods_be_dai);
+			#else
+				len4 = len3;
+			#endif
 		} else {
 			memcpy(msm8952_marley_dai_links + len_2a,
 				msm8952_marley_l34_dai_link,
 				sizeof(msm8952_marley_l34_dai_link));
-			memcpy(msm8952_marley_dai_links + len3,
-				msm8952_marley_mods_be_dai,
-				sizeof(msm8952_marley_mods_be_dai));
-			len4 = len3 + ARRAY_SIZE(msm8952_marley_mods_be_dai);
+			#ifdef CONFIG_SND_SOC_MODS_CODEC_SHIM
+				memcpy(msm8952_marley_dai_links + len3,
+					msm8952_marley_mods_be_dai,
+					sizeof(msm8952_marley_mods_be_dai));
+				len4 = len3 + ARRAY_SIZE(msm8952_marley_mods_be_dai);
+			#else
+				len4 = len3;
+			#endif
 		}
 	}
 #endif
