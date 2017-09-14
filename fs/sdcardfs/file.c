d55bdfcaea020fe92f5c6512bfe66d124e0b22c2
Shiyong Li <a22381@motorola.com>
2017-09-14 13:49:51 -0700

    sdcardfs: support multiple writers of xattr "user.dwriter"
    - Check if directory writer name has been listed in xattr at every
      writing. If not, append it to writer name list in xattr. Put "overrun"
      there if seeing too many writers.
    - Rename xattr from "user.firstwriter" to "user.dwriter" accordingly.
    
    Change-Id: I7f02dd76201ab66b18ca43fa8370c0e8be6b3411
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1059231
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Coverity Analysis <coverity@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -225,7 +225,7 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 	struct dentry *parent = dget_parent(dentry);
 	struct sdcardfs_sb_info *sbi = SDCARDFS_SB(dentry->d_sb);
 	const struct cred *saved_cred = NULL;
-#if defined(CONFIG_SDCARD_FS_DIR_FIRSTWRITER) || defined(CONFIG_SDCARD_FS_PARTIAL_RELATIME)
+#if defined(CONFIG_SDCARD_FS_DIR_WRITER) || defined(CONFIG_SDCARD_FS_PARTIAL_RELATIME)
 	uid_t writer_uid = current_fsuid().val;
 	struct sdcardfs_inode_data *pd = SDCARDFS_I(d_inode(parent))->data;
 #endif
@@ -276,11 +276,11 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 		sdcardfs_update_relatime_flag(lower_file,
 			sdcardfs_lower_inode(inode), writer_uid);
 #endif
-#ifdef CONFIG_SDCARD_FS_DIR_FIRSTWRITER
+#ifdef CONFIG_SDCARD_FS_DIR_WRITER
 	/* update xattr for writing operation under non "Android" folders */
 	if (!err && (lower_file->f_flags & O_ACCMODE) &&
 		pd->perm < PERM_ANDROID)
-		sdcardfs_update_xattr_firstwriter(lower_file->f_path.dentry,
+		sdcardfs_update_xattr_dirwriter(lower_file->f_path.dentry,
 			writer_uid);
 #endif
 
