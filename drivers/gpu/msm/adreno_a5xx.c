0eb3ba6cbc7ceb22f58848c2b0b781e76c635768
Archana Sriram <apsrir@codeaurora.org>
2017-09-18 17:33:31 +0530

    msm: kgsl: Fix calculation of size in _load_regfile
    During firmware load, there could be data over reads
    due to calculation of lm_size and lm_sequence from
    block and block_size. Added bounds checking to prevent
    this and improved the size calculation.
    
    CRs-Fixed: 2107981
    Change-Id: Ib4283951b0d6c8fb699af1f85e657981ad4c0318
    Signed-off-by: Archana Sriram <apsrir@codeaurora.org>

--- drivers/gpu/msm/adreno_a5xx.c
+++ drivers/gpu/msm/adreno_a5xx.c
@@ -1053,8 +1053,8 @@ static void _load_regfile(struct adreno_device *adreno_dev)
 {
 	struct kgsl_device *device = &adreno_dev->dev;
 	const struct firmware *fw;
-	uint32_t block_size = 0, block_total = 0, fw_size;
-	uint32_t *block;
+	uint64_t block_size = 0, block_total = 0;
+	uint32_t fw_size, *block;
 	int ret = -EINVAL;
 
 	ret = request_firmware(&fw, adreno_dev->gpucore->regfw_name,
@@ -1073,7 +1073,8 @@ static void _load_regfile(struct adreno_device *adreno_dev)
 	/* All offset numbers calculated from file description */
 	while (block_total < fw_size) {
 		block_size = block[0];
-		if (block_size >= fw_size || block_size < 2)
+		if (((block_total + block_size) >= fw_size)
+				|| block_size < 5)
 			goto err;
 		if (block[1] != GPMU_SEQUENCE_ID)
 			goto err;
@@ -1088,6 +1089,9 @@ static void _load_regfile(struct adreno_device *adreno_dev)
 				goto err;
 
 			adreno_dev->lm_fw = fw;
+
+			if (block[2] > (block_size - 2))
+				goto err;
 			adreno_dev->lm_sequence = block + block[2] + 3;
 			adreno_dev->lm_size = block_size - block[2] - 2;
 		}
@@ -1100,7 +1104,7 @@ static void _load_regfile(struct adreno_device *adreno_dev)
 err:
 	release_firmware(fw);
 	KGSL_PWR_ERR(device,
-		"Register file failed to load sz=%d bsz=%d header=%d\n",
+		"Register file failed to load sz=%d bsz=%llu header=%d\n",
 		fw_size, block_size, ret);
 	return;
 }
