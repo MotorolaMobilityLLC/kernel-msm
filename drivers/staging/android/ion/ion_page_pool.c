327108d7f52a70954802c82d2213773aa18e9d2b
Jeff Vanhoof <qjv001@motorola.com>
2017-09-19 13:19:40 -0500

    ION: Improve cache accounting
    ION cache contents are not really accounted for anywhere.
    The Android LMK doesn't know how much it contains and may kill
    processes too soon if it thinks memory is unavailable.
    The MemAvailable and Cache fields in /proc/meminfo also do not
    reflect what is there either.
    
    Add/Decrement NR_FILE_PAGES/NR_INACTIVE_PAGES so that we can
    get somewhat better accounting of where memory is used in the system
    and what is actually available. Reusing NR_FILE_PAGES in order to
    avoid creating a new ENUM just for ION.
    
    Although QC has added them to NR_INDIRECTLY_RECLAIMABLE_BYTES, it only
    is included into /proc/meminfo/available, lowmemorykiller and ams will
    not count them. So we need to ignore QC changes and add the moto changes
    
    Change-Id: Ifbed1841925aafbe12312ab1889ae691c5a5ec26
    Signed-off-by: Jeff Vanhoof <qjv001@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1061999
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: Bingqian Yang <yangbq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1390886
    Reviewed-by: Yang Xiao <xiaoy3@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1414653
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -48,6 +48,8 @@ static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 
 static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
+	int page_count = 1 << pool->order;
+
 	mutex_lock(&pool->mutex);
 	if (PageHighMem(page)) {
 		list_add_tail(&page->lru, &pool->high_items);
@@ -58,8 +60,11 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 	}
 
 	nr_total_pages += 1 << pool->order;
-	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
-							1 << pool->order);
+	/*mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
+							1 << pool->order);*/
+	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, page_count);
+	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, page_count);
+
 	mutex_unlock(&pool->mutex);
 	return 0;
 }
@@ -67,6 +72,7 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 {
 	struct page *page;
+	int page_count = 1 << pool->order;
 
 	if (high) {
 		BUG_ON(!pool->high_count);
@@ -80,8 +86,12 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 
 	list_del(&page->lru);
 	nr_total_pages -= 1 << pool->order;
-	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
-							-(1 << pool->order));
+	/*mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
+							-(1 << pool->order));*/
+
+	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, -page_count);
+	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, -page_count);
+
 	return page;
 }
 
