b530f5a97d275dca6b95c0f60b603f5bd15b3add
Jeff Vanhoof <qjv001@motorola.com>
2017-09-19 13:19:40 -0500

    ION: Improve cache accounting
    ION cache contents are not really accounted for anywhere.
    The Android LMK doesn't know how much it contains and may kill
    processes too soon if it thinks memory is unavailable.
    The MemAvailable and Cache fields in /proc/meminfo also do not
    reflect what is there either.
    
    Add/Decrement NR_FILE_PAGES/NR_INACTIVE_PAGES so that we can
    get somewhat better accounting of where memory is used in the system
    and what is actually available. Reusing NR_FILE_PAGES in order to
    avoid creating a new ENUM just for ION.
    
    Change-Id: Ifbed1841925aafbe12312ab1889ae691c5a5ec26
    Signed-off-by: Jeff Vanhoof <qjv001@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1061999
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 77a9e94b95641d64d17a6dfc320890e24daf0e16)
    Reviewed-on: https://gerrit.mot.com/1146627
    Tested-by: Jira Key
    Reviewed-by: Jeffrey Vanhoof <jvanhoof@motorola.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -23,6 +23,8 @@
 #include <linux/slab.h>
 #include <linux/swap.h>
 #include <linux/vmalloc.h>
+#include <linux/vmstat.h>
+#include <linux/mmzone.h>
 #include "ion_priv.h"
 
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
@@ -56,7 +58,10 @@ static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 
 static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
+	int page_count = 1 << pool->order;
+
 	mutex_lock(&pool->mutex);
+
 	if (PageHighMem(page)) {
 		list_add_tail(&page->lru, &pool->high_items);
 		pool->high_count++;
@@ -64,6 +69,10 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 		list_add_tail(&page->lru, &pool->low_items);
 		pool->low_count++;
 	}
+
+	mod_zone_page_state(page_zone(page), NR_FILE_PAGES, page_count);
+	mod_zone_page_state(page_zone(page), NR_INACTIVE_FILE, page_count);
+
 	mutex_unlock(&pool->mutex);
 	return 0;
 }
@@ -71,6 +80,7 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 {
 	struct page *page;
+	int page_count = 1 << pool->order;
 
 	if (high) {
 		BUG_ON(!pool->high_count);
@@ -83,6 +93,10 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	}
 
 	list_del(&page->lru);
+
+	mod_zone_page_state(page_zone(page), NR_INACTIVE_FILE, -page_count);
+	mod_zone_page_state(page_zone(page), NR_FILE_PAGES, -page_count);
+
 	return page;
 }
 
