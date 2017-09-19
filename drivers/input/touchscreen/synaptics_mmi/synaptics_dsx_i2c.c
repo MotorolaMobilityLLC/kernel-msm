53120dc68b0b097a128b6c21434ed1890a25fb29
Konstantin Makariev <hcv867@motorola.com>
2017-09-19 09:39:01 -0500

    input: synaptics_mmi: read flashprog status
    Add status reading pointer to expansion function structure.
    Part of new function handling will determine status for
    RMI_FW_UPDATER and update in_bootloader flag within rmi4_data
    structure. This will ensure reading flashprog sysfs entry
    returns valid data.
    
    Change-Id: I8b32c80a8f27b7f69b60457340b5ad3438829953
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1061907
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/synaptics_mmi/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_mmi/synaptics_dsx_i2c.c
@@ -2270,6 +2270,7 @@ struct synaptics_rmi4_exp_fn {
 	void (*func_remove)(struct synaptics_rmi4_data *rmi4_data);
 	void (*func_attn)(struct synaptics_rmi4_data *rmi4_data,
 			unsigned char intr_mask);
+	int (*func_status)(struct synaptics_rmi4_data *rmi4_data);
 	struct list_head link;
 };
 
@@ -2782,24 +2783,33 @@ static int synaptics_dsx_wait_for_idle(struct synaptics_rmi4_data *rmi4_data)
 	return 0;
 }
 
+static int synaptics_rmi4_f01_flashprog_status(
+		struct synaptics_rmi4_data *rmi4_data)
+{
+	int retval;
+	struct synaptics_rmi4_f01_device_status status;
+
+	retval = synaptics_rmi4_i2c_read(rmi4_data,
+					rmi4_data->f01_data_base_addr,
+					status.data,
+					sizeof(status.data));
+	return retval < 0 ? retval : status.flash_prog;
+}
+
 static int synaptics_dsx_sensor_ready_state(
 		struct synaptics_rmi4_data *rmi4_data, bool standby)
 {
 	bool ui_mode;
 	int retval, state, i;
-	struct synaptics_rmi4_f01_device_status status;
 
 	for (i = 0; i < 10; ++i) {
-		retval = synaptics_rmi4_i2c_read(rmi4_data,
-					rmi4_data->f01_data_base_addr,
-					status.data,
-					sizeof(status.data));
+		retval = rmi4_data->get_status(rmi4_data);
 		if (retval < 0) {
 			pr_err("(%d) Failed to query touch ic status\n", i);
 			return retval;
 		}
 
-		ui_mode = status.flash_prog == 0;
+		ui_mode = retval == 0;
 		pr_debug("(%d) UI mode: %s\n", i, ui_mode ? "true" : "false");
 
 		if (ui_mode)
@@ -2858,6 +2868,7 @@ static void synaptics_dsx_sensor_state(struct synaptics_rmi4_data *rmi4_data,
 
 	switch (state) {
 	case STATE_UNKNOWN:
+			rmi4_data->in_bootloader = false;
 	case STATE_FLASH:
 		/* no special handling for these states */
 			break;
@@ -6054,8 +6065,11 @@ static int synaptics_rmi4_query_device(struct synaptics_rmi4_data *rmi4_data)
 				if (retval < 0)
 					return retval;
 
-				rmi4_data->in_bootloader =
-						status.flash_prog == 1;
+				/* FlashProg bit might be inaccurate, so */
+				/* do not change the flag if it is set */
+				if (!rmi4_data->in_bootloader)
+					rmi4_data->in_bootloader =
+							status.flash_prog == 1;
 				break;
 
 			case SYNAPTICS_RMI4_F11:
@@ -6640,6 +6654,28 @@ static void synaptics_rmi4_detection_work(struct work_struct *work)
 			} else
 				control_access_block_update_dynamic(rmi4_data);
 		}
+
+		if (exp_fhandler->fn_type == RMI_FW_UPDATER) {
+			int status;
+
+			if (!exp_fhandler->func_status)
+				continue;
+			status = exp_fhandler->func_status(rmi4_data);
+			/* consider changing to */
+			/* if (status == 1) */
+			/* if Synaptics confirm only 1 is valid */
+			if (status) {
+				struct synaptics_rmi4_device_info *rmi;
+
+				rmi = &(rmi4_data->rmi4_mod_info);
+				rmi4_data->in_bootloader = true;
+				pr_info("Product: %s is in bootloader mode\n",
+					rmi->product_id_string);
+			}
+			/* replace default status retrieval function */
+			rmi4_data->get_status = exp_fhandler->func_status;
+			pr_info("using F34 status retrieval function\n");
+		}
 	}
 
 release_mutex:
@@ -6664,7 +6700,9 @@ void synaptics_rmi4_new_function(enum exp_fn fn_type, bool insert,
 		int (*func_init)(struct synaptics_rmi4_data *rmi4_data),
 		void (*func_remove)(struct synaptics_rmi4_data *rmi4_data),
 		void (*func_attn)(struct synaptics_rmi4_data *rmi4_data,
-		unsigned char intr_mask), enum ic_modes mode)
+		unsigned char intr_mask),
+		int (*func_status)(struct synaptics_rmi4_data *rmi4_data),
+		enum ic_modes mode)
 {
 	struct synaptics_rmi4_exp_fn *exp_fhandler;
 
@@ -6692,6 +6730,7 @@ void synaptics_rmi4_new_function(enum exp_fn fn_type, bool insert,
 		exp_fhandler->fn_type = fn_type;
 		exp_fhandler->func_init = func_init;
 		exp_fhandler->func_attn = func_attn;
+		exp_fhandler->func_status = func_status;
 		exp_fhandler->func_remove = func_remove;
 		exp_fhandler->inserted = false;
 		exp_fhandler->mode = mode;
@@ -6702,6 +6741,7 @@ void synaptics_rmi4_new_function(enum exp_fn fn_type, bool insert,
 				exp_fhandler->inserted = false;
 				exp_fhandler->func_init = NULL;
 				exp_fhandler->func_attn = NULL;
+				exp_fhandler->func_status = NULL;
 				goto exit;
 			}
 		}
@@ -7091,6 +7131,7 @@ static int synaptics_rmi4_probe(struct i2c_client *client,
 	rmi4_data->ready_state = synaptics_dsx_sensor_ready_state;
 	rmi4_data->irq_enable = synaptics_rmi4_irq_enable;
 	rmi4_data->reset_device = synaptics_rmi4_reset_device;
+	rmi4_data->get_status = synaptics_rmi4_f01_flashprog_status;
 
 	mutex_init(&(rmi4_data->rmi4_io_ctrl_mutex));
 	mutex_init(&(rmi4_data->state_mutex));
