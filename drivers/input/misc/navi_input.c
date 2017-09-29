4cc94ca573ea524cbe7091d0aa7f7f45d643bcde
Yan Zhang <zhangy26@motorola.com>
2017-09-29 11:46:46 +0800

    Enable Y direction navigation for ETS fps
    1. Enable Y direction navigation for ETS fps.
    2. Reduce the fps log for performance
    
    Change-Id: I78caf668efc6c343ecfb4be35b2740a414e96348
    Signed-off-by: Yan Zhang <zhangy26@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1067801
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/navi_input.c
+++ drivers/input/misc/navi_input.c
@@ -53,7 +53,8 @@ struct navi_cmd_struct cmd_list;
  *     DISABLE : Ignore swipe-left & swipe-right navigation events.
  *               Don't care properties.
  */
-#define ENABLE_SWIPE_UP_DOWN	DISABLE
+
+#define ENABLE_SWIPE_UP_DOWN	ENABLE
 #define ENABLE_SWIPE_LEFT_RIGHT	ENABLE
 #define ENABLE_FINGER_DOWN_UP	ENABLE
 #define KEY_FPS_DOWN   614
