08b702b1f5eb5d7b337caddc19ce88e5f100c6e5
yanyh2 <yanyh2@lenovo.com>
2017-10-11 20:14:51 +0800

    (CR): spmi-pmic-arb: fix irq missing issue
    Cherry pick from Qcom patch:
    https://source.codeaurora.org/quic/la/kernel/msm-3.18/commit/?id=
    eb3d61392ee65747301e94719414d1ddb7cc583c
    
    spmi-pmic-arb: add support to dispatch interrupt based on IRQ status
    
    Current implementation of SPMI arbiter dispatches interrupt based on the
    Arbiter's accumulator status, in some cases the accumulator status may
    remain zero and the interrupt remains un-handled. Add logic to dispatch
    interrupts based Arbiter's IRQ status if the accumulator status is zero.
    
    Author: Ashay Jaiswal <ashayj@codeaurora.org>
    commit 1e7190d6b7b13d5fc076877147f52eb4ed251502
    CRs-Fixed: 2934741
    Change-Id: I068f5c7d33758063878721d7cce1308fa803e3bd
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    
    Signed-off-by: yanyh2 <yanyh2@mt.com>
    Change-Id: I382ed9a24a62accbecc9d84b02b1ce6621696008
    Reviewed-on: https://gerrit.mot.com/1072109
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1124289
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>

--- drivers/spmi/spmi-pmic-arb.c
+++ drivers/spmi/spmi-pmic-arb.c
@@ -460,16 +460,45 @@ static void pmic_arb_chained_irq(unsigned int irq, struct irq_desc *desc)
 	int last = pa->max_apid >> 5;
 	u32 status;
 	int i, id;
+	/* status based dispatch */
+	bool acc_valid = false;
+	u32 irq_status = 0;
 
 	chained_irq_enter(chip, desc);
 
 	for (i = first; i <= last; ++i) {
 		status = readl_relaxed(intr +
 				      pa->ver_ops->owner_acc_status(pa->ee, i));
+		if (status)
+			acc_valid = true;
+
 		while (status) {
 			id = ffs(status) - 1;
 			status &= ~(1 << id);
 			periph_interrupt(pa, id + i * 32);
+		}
+	}
+
+	/* ACC_STATUS is empty but IRQ fired check IRQ_STATUS */
+	if (!acc_valid) {
+		for (i = pa->min_apid; i <= pa->max_apid; i++) {
+			/* skip if APPS is not irq owner */
+			if (pa->apid_data[i].irq_owner != pa->ee)
+				continue;
+
+			irq_status = readl_relaxed(pa->intr +
+						pa->ver_ops->irq_status(i));
+
+			if (irq_status) {
+				enable = readl_relaxed(pa->intr +
+						pa->ver_ops->acc_enable(i));
+				if (enable & SPMI_PIC_ACC_ENABLE_BIT) {
+					dev_dbg(&pa->spmic->dev,
+						"Dispatching IRQ for apid=%d status=%x\n",
+						i, irq_status);
+					periph_interrupt(pa, i);
+				}
+			}
 		}
 	}
 
