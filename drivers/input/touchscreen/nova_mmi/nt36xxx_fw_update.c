345a7642821d9c8593406a1a6f507bf3ea3f6f77
dengwei1 <dengwei1@motorola.com>
2017-10-26 15:53:56 +0800

    input: add touch firmware upgrade support
    add firmware upgrade support
    
    Change-Id: Iaaf6d96e804a1a67058005cef6c8407a5353c15c
    Signed-off-by: dengwei1 <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1080918
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/nova_mmi/nt36xxx_fw_update.c
+++ drivers/input/touchscreen/nova_mmi/nt36xxx_fw_update.c
@@ -21,7 +21,7 @@
 
 #include "nt36xxx.h"
 
-#if BOOT_UPDATE_FIRMWARE
+#if (BOOT_UPDATE_FIRMWARE || NVT_TOUCH_FW)
 
 #define FW_BIN_SIZE_116KB 118784
 #define FW_BIN_SIZE FW_BIN_SIZE_116KB
@@ -867,7 +867,9 @@ int32_t Update_Firmware(void)
 
 	return ret;
 }
+#endif
 
+#if BOOT_UPDATE_FIRMWARE
 /*******************************************************
 Description:
 	Novatek touchscreen update firmware when booting
@@ -891,7 +893,12 @@ void Boot_Update_Firmware(struct work_struct *work)
 	}
 
 	mutex_lock(&ts->lock);
-
+#if NVT_TOUCH_ESD_PROTECT
+	nvt_esd_check_enable(false);
+#endif
+#if NVT_TOUCH_FW
+	ts->loading_fw = 1;
+#endif
 	nvt_sw_reset_idle();
 
 	ret = Check_CheckSum();
@@ -908,8 +915,365 @@ void Boot_Update_Firmware(struct work_struct *work)
 		nvt_check_fw_reset_state(RESET_STATE_INIT);
 	}
 
+#if NVT_TOUCH_FW
+	ts->loading_fw = 0;
+#endif
 	mutex_unlock(&ts->lock);
 
 	update_firmware_release();
 }
 #endif /* BOOT_UPDATE_FIRMWARE */
+
+#if NVT_TOUCH_FW
+static ssize_t nvt_poweron_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	bool val;
+
+	mutex_lock(&ts->lock);
+	val = ts->suspended;
+	mutex_unlock(&ts->lock);
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n",
+		val == 0);
+}
+
+static DEVICE_ATTR(poweron, 0444, nvt_poweron_show, NULL);
+
+static ssize_t nvt_ic_ver_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "[Vendor]Tianma,[FW]%02x,[IC]NT36525\n",
+		ts->fw_ver);
+}
+
+static DEVICE_ATTR(ic_ver, 0444, nvt_ic_ver_show, NULL);
+
+static ssize_t nvt_productinfo_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+/* Update script use as $touch_product_id in fw name ==> (chip id from Lenovo&script) OR (vendor name from Focaltech) OR (project ID from Longcheer&Himax)
+   Project ID ?? ( - Jeter - Hannah)
+   Chip ID ?? (NVT-ts or NVTCapacitiveTouchScreen or NT36xxx) */
+	return scnprintf(buf, PAGE_SIZE, "%s\n",
+		ts->product_id);
+}
+
+static DEVICE_ATTR(productinfo, 0444, nvt_productinfo_show, NULL);
+
+static ssize_t nvt_force_reflash_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	unsigned int input;
+
+	if (sscanf(buf, "%u", &input) != 1)
+		return -EINVAL;
+
+	ts->force_reflash = (input == 0) ? false : true;
+
+	return count;
+}
+
+static DEVICE_ATTR(forcereflash, 0220, NULL, nvt_force_reflash_store);
+
+static ssize_t nvt_flashprog_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%d\n",
+		(ts->loading_fw) ? 1 : 0);
+}
+
+static DEVICE_ATTR(flashprog, 0444, nvt_flashprog_show, NULL);
+
+static ssize_t nvt_do_reflash_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	int retval;
+	char prefix[FW_NAME_MAX_LEN] = VENDOR_NAME;
+	char template[FW_NAME_MAX_LEN];
+	char firmware_name[256] = "";
+
+	if (count > FW_NAME_MAX_LEN) {
+		NVT_ERR("FW filename is too long\n");
+		retval = -EINVAL;
+		goto exit;
+	}
+
+	if (ts->suspended) {
+		NVT_ERR("In suspend state, try again later\n");
+		retval = -EINVAL;
+		goto exit;
+	}
+
+	if (ts->loading_fw) {
+		NVT_ERR("In FW flashing state, try again later\n");
+		retval = -EINVAL;
+		goto exit;
+	}
+
+	if (!ts->force_reflash) {
+		if (strncmp(buf, prefix,
+			strnlen(prefix, sizeof(prefix)))) {
+			NVT_ERR("FW does not belong to Novatek\n");
+			retval = -EINVAL;
+			goto exit;
+		}
+
+		snprintf(template, sizeof(template), "-%s-",
+						ts->product_id);
+		if (!strnstr(buf + strnlen(prefix, sizeof(prefix)), template,
+			count)) {
+			NVT_ERR("FW does not belong to %s\n", ts->product_id);
+			retval = -EINVAL;
+			goto exit;
+		}
+	}
+
+	strlcpy(firmware_name, buf, count);
+	NVT_LOG("FW filename: %s\n", firmware_name);
+
+	retval = update_firmware_request(firmware_name);
+	if (retval) {
+		NVT_ERR("update_firmware_request failed. (%d)\n", retval);
+		goto exit;
+	}
+
+	mutex_lock(&ts->lock);
+#if NVT_TOUCH_ESD_PROTECT
+	nvt_esd_check_enable(false);
+#endif
+	ts->loading_fw = 1;
+	nvt_sw_reset_idle();
+
+	retval = Check_CheckSum();
+
+	if (retval < 0) {
+		NVT_ERR("read firmware checksum failed\n");
+		Update_Firmware();
+	} else if ((retval == 0) && (Check_FW_Ver() == 0)) {
+		NVT_LOG("firmware version not match\n");
+		Update_Firmware();
+	} else {
+		/* Bootloader Reset */
+		nvt_bootloader_reset();
+		nvt_check_fw_reset_state(RESET_STATE_INIT);
+	}
+
+	ts->loading_fw = 0;
+	mutex_unlock(&ts->lock);
+
+	update_firmware_release();
+
+	retval = count;
+exit:
+	return retval;
+}
+
+static DEVICE_ATTR(doreflash, 0220, NULL, nvt_do_reflash_store);
+
+static ssize_t nvt_build_id_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+/* Update script use for comparing $str_cfg_id_latest in fw name */
+	uint8_t tmp_buf[4] = {0};
+	uint8_t fw_version = 0;
+	uint8_t date_Y = 0;
+	uint8_t date_M = 0;
+	uint8_t date_D = 0;
+
+	tmp_buf[0] = 0xFF;
+	tmp_buf[1] = (ts->mmap->EVENT_BUF_ADDR >> 16) & 0xFF;
+	tmp_buf[2] = (ts->mmap->EVENT_BUF_ADDR >> 8) & 0xFF;
+	CTP_I2C_WRITE(ts->client, I2C_FW_Address, tmp_buf, 3);
+
+	tmp_buf[0] = EVENT_MAP_FWINFO;
+	CTP_I2C_READ(ts->client, I2C_FW_Address, tmp_buf, 2);
+	fw_version = tmp_buf[1];
+
+	tmp_buf[0] = EVENT_MAP_FWDATE;
+	CTP_I2C_READ(ts->client, I2C_FW_Address, tmp_buf, 4);
+	date_Y = tmp_buf[1];
+	date_M = tmp_buf[2];
+	date_D = tmp_buf[3];
+
+	return scnprintf(buf, PAGE_SIZE, "%02x-%02d%02d%02d\n",
+		fw_version, date_Y, date_M, date_D);
+}
+
+static DEVICE_ATTR(buildid, 0444, nvt_build_id_show, NULL);
+
+
+#include <linux/slab.h>
+#include <linux/major.h>
+#include <linux/kdev_t.h>
+
+/* Attribute: path (RO) */
+static ssize_t path_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	ssize_t blen;
+	const char *path;
+
+	path = kobject_get_path(&ts->client->dev.kobj, GFP_KERNEL);
+	blen = scnprintf(buf, PAGE_SIZE, "%s", path ? path : "na");
+	kfree(path);
+	return blen;
+}
+
+/* Attribute: vendor (RO) */
+static ssize_t vendor_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, VENDOR_NAME);
+}
+
+static struct device_attribute touchscreen_attributes[] = {
+	__ATTR_RO(path),
+	__ATTR_RO(vendor),
+	__ATTR_NULL
+};
+
+#define TSDEV_MINOR_BASE 128
+#define TSDEV_MINOR_MAX 32
+
+/*******************************************************
+Description:
+	Novatek touchscreen FW function class. file node
+	initial function.
+
+return:
+	Executive outcomes. 0---succeed. -1---failed.
+*******************************************************/
+int32_t nvt_fw_class_init(bool create)
+{
+	struct device_attribute *attrs = touchscreen_attributes;
+	int i, error = 0;
+	static struct class *touchscreen_class;
+	static struct device *ts_class_dev;
+	static int minor;
+
+	if (create) {
+		minor = input_get_new_minor(ts->client->addr,
+						1, false);
+		if (minor < 0)
+			minor = input_get_new_minor(TSDEV_MINOR_BASE,
+					TSDEV_MINOR_MAX, true);
+		NVT_LOG("assigned minor %d\n", minor);
+
+		touchscreen_class = class_create(THIS_MODULE, "touchscreen");
+		if (IS_ERR(touchscreen_class)) {
+			error = PTR_ERR(touchscreen_class);
+			touchscreen_class = NULL;
+			return error;
+		}
+
+		ts_class_dev = device_create(touchscreen_class, NULL,
+				MKDEV(INPUT_MAJOR, minor),
+				ts, NVT_I2C_NAME);
+		if (IS_ERR(ts_class_dev)) {
+			error = PTR_ERR(ts_class_dev);
+			ts_class_dev = NULL;
+			return error;
+		}
+
+		for (i = 0; attrs[i].attr.name != NULL; ++i) {
+			error = device_create_file(ts_class_dev, &attrs[i]);
+			if (error)
+				break;
+		}
+
+		if (error)
+			goto device_destroy;
+		else
+			NVT_LOG("create /sys/class/touchscreen/%s Succeeded!\n", NVT_I2C_NAME);
+	} else {
+		if (!touchscreen_class || !ts_class_dev)
+			return -ENODEV;
+
+		for (i = 0; attrs[i].attr.name != NULL; ++i)
+			device_remove_file(ts_class_dev, &attrs[i]);
+
+		device_unregister(ts_class_dev);
+		class_unregister(touchscreen_class);
+	}
+
+	return 0;
+
+device_destroy:
+	for (--i; i >= 0; --i)
+		device_remove_file(ts_class_dev, &attrs[i]);
+	device_destroy(touchscreen_class, MKDEV(INPUT_MAJOR, minor));
+	ts_class_dev = NULL;
+	class_unregister(touchscreen_class);
+	NVT_ERR("error creating touchscreen class\n");
+
+	return -ENODEV;
+}
+
+int nvt_fw_sysfs_init(void)
+{
+	int ret;
+	struct i2c_client *client = ts->client;
+
+	ret = nvt_fw_class_init(true);
+	if (ret != 0) {
+		NVT_ERR("fw class init failed. ret=%d\n", ret);
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_forcereflash);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_forcereflash failed\n");
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_flashprog);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_flashprog failed\n");
+		return ret;
+	}
+	ret = device_create_file(&client->dev, &dev_attr_doreflash);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_doreflash failed\n");
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_buildid);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_buildid failed\n");
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_productinfo);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_productinfo failed\n");
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_poweron);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_poweron failed\n");
+		return ret;
+	}
+
+	ret = device_create_file(&client->dev, &dev_attr_ic_ver);
+	if (ret) {
+		NVT_ERR("create_file dev_attr_ic_ver failed\n");
+		return ret;
+	}
+
+	return 0;
+}
+void nvt_fw_sysfs_deinit(void)
+{
+	struct i2c_client *client = ts->client;
+	device_remove_file(&client->dev, &dev_attr_forcereflash);
+	device_remove_file(&client->dev, &dev_attr_flashprog);
+	device_remove_file(&client->dev, &dev_attr_doreflash);
+	device_remove_file(&client->dev, &dev_attr_buildid);
+	device_remove_file(&client->dev, &dev_attr_productinfo);
+	device_remove_file(&client->dev, &dev_attr_poweron);
+	device_remove_file(&client->dev, &dev_attr_ic_ver);
+	nvt_fw_class_init(false);
+}
+#endif
