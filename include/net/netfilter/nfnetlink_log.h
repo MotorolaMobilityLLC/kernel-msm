b6d8db534e577634f797ace370bb0f973c00fa61
Yu Miao <miaoyu4@lenovo.com>
2017-10-31 16:40:17 +0800

    kernel: support layered NFLOG
    Add parameter --nflog-layer to get specified
    layer data. All by Defenderault, so no effect on
    existed code.
    
    Change-Id: I14081e402be21b4ef5859c015f09d7ed9431f646
    Signed-off-by: Yu Miao <miaoyu4@mt.com>
    Reviewed-on: https://gerrit.mot.com/1082466
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- include/net/netfilter/nfnetlink_log.h
+++ include/net/netfilter/nfnetlink_log.h
@@ -10,6 +10,16 @@ nfulnl_log_packet(struct net *net,
 		  const struct net_device *out,
 		  const struct nf_loginfo *li_user,
 		  const char *prefix);
+void
+__nfulnl_log_packet(struct net *net,
+		  u_int8_t pf,
+		  unsigned int hooknum,
+		  const struct sk_buff *skb,
+		  const struct net_device *in,
+		  const struct net_device *out,
+		  const struct nf_loginfo *li_user,
+		  const char *prefix,
+		  unsigned int layer);
 
 #define NFULNL_COPY_DISABLED    0xff
 
