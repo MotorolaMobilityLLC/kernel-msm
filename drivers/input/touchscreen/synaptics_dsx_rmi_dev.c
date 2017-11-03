a01b76402750632c4413a0c14d6aa486f175df19
Steve Pfetsch <spfetsch@google.com>
2017-11-03 16:26:00 -0700

    input: synaptics: restrict sysfs node write permissions
    Mot-CRs-fixed: (CR)
    CVE-fixed: CVE-2017-13219
    Bug: 62800865
    Change-Id: I83428f32afc77050580d1f1b3f69938cf90f9256
    Signed-off-by: Steve Pfetsch <spfetsch@google.com>
    Signed-off-by: Ashwin Pathmudi <jfxr63@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1107053
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/synaptics_dsx_rmi_dev.c
+++ drivers/input/touchscreen/synaptics_dsx_rmi_dev.c
@@ -83,7 +83,7 @@ struct rmidev_data {
 static struct bin_attribute attr_data = {
 	.attr = {
 		.name = "data",
-		.mode = (S_IRUGO | S_IWUSR | S_IWGRP),
+		.mode = (S_IRUGO | S_IWUSR),
 	},
 	.size = 0,
 	.read = rmidev_sysfs_data_show,
@@ -91,16 +91,16 @@ static struct bin_attribute attr_data = {
 };
 
 static struct device_attribute attrs[] = {
-	__ATTR(open, S_IWUSR | S_IWGRP,
+	__ATTR(open, S_IWUSR,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_open_store),
-	__ATTR(release, S_IWUSR | S_IWGRP,
+	__ATTR(release, S_IWUSR,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_release_store),
-	__ATTR(address, S_IWUSR | S_IWGRP,
+	__ATTR(address, S_IWUSR,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_address_store),
-	__ATTR(length, S_IWUSR | S_IWGRP,
+	__ATTR(length, S_IWUSR,
 			synaptics_rmi4_show_error,
 			rmidev_sysfs_length_store),
 	__ATTR(attn_state, S_IRUSR | S_IRGRP,
