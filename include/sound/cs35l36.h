f2ceddff4fb28425c4eb1c194805c76722ed171b
huangys <huangys@motorola.com>
2017-11-17 17:40:11 +0800

    ASoC: cs35l36: enable tx port config
    This path has been verifed for music playback case.
    tx port data in AFE point can be captured
    
    Change-Id: Ib1a8173c6c7aff06f757acb3a302c08e265e6e5c
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1091217
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Submit-Approved: Jira Key

--- include/sound/cs35l36.h
+++ include/sound/cs35l36.h
@@ -15,6 +15,24 @@ struct classh_cfg {
 
 };
 
+/* ASP TX Platform Data */
+struct asp_tx_cfg {
+	bool is_present;
+	bool tx1, tx2, tx3, tx4, tx5, tx6;
+	u8 tx1_slot;
+	u8 tx1_src_select;
+	u8 tx2_slot;
+	u8 tx2_src_select;
+	u8 tx3_slot;
+	u8 tx3_src_select;
+	u8 tx4_slot;
+	u8 tx4_src_select;
+	u8 tx5_slot;
+	u8 tx5_src_select;
+	u8 tx6_slot;
+	u8 tx6_src_select;
+};
+
 /* INT/GPIO pin configuration */
 struct irq_cfg {
 	bool is_present;
@@ -40,6 +58,7 @@ struct cs35l36_platform_data {
 	int bst_ipk;
 	int temp_warn_thld;
 	struct irq_cfg irq_config;
+	struct asp_tx_cfg asp_tx_config;
 };
 
 #endif /* __CS35L36_H */
