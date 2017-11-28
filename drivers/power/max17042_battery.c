sultanqasim/android_kernel_motorola_msm8916

IKVPREL1L-3462: power: max17042: Support Alert INTR testing in factory.
The VALRT Threshold register sets upper and lower limits that generate
an ALRT pin interrupt. When factory forces both the thresholds to 0V,
the code to support HVDCP at lower voltages overwrites this setting.

Change-Id: Ib310cfab79c64b9776d9b3046d4bdf33564f3490
Signed-off-by: Vidyadhar Gurram <rnh786@motorola.com>
Reviewed-on: http://gerrit.mot.com/694763
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
Reviewed-by: Connie Zhao <czhao1@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/power/max17042_battery.c
@@ -135,6 +135,7 @@ struct max17042_chip {
 	struct max17042_wakeup_source max17042_wake_source;
 	int charge_full_des;
 	int taper_reached;
+	bool factory_mode;
 };
 
 static void max17042_stay_awake(struct max17042_wakeup_source *source)
@@ -755,7 +756,9 @@ static inline void max17042_override_por_values(struct max17042_chip *chip)
 	max17042_override_por(client, MAX17042_CGAIN, config->cgain);
 	max17042_override_por(client, MAX17042_COFF, config->coff);
 
-	max17042_override_por(client, MAX17042_VALRT_Th, POR_VALRT_THRESHOLD);
+	if (!chip->factory_mode)
+		max17042_override_por(client, MAX17042_VALRT_Th,
+				      POR_VALRT_THRESHOLD);
 	max17042_override_por(client, MAX17042_TALRT_Th, config->talrt_thresh);
 	max17042_override_por(client, MAX17042_SALRT_Th,
 			config->soc_alrt_thresh);
@@ -1054,7 +1057,8 @@ static irqreturn_t max17042_thread_handler(int id, void *dev)
 		chip->batt_undervoltage = true;
 	} else if (val & STATUS_INTR_VMAX_BIT) {
 		dev_info(&chip->client->dev, "Battery overvoltage INTR\n");
-		max17042_write_reg(chip->client, MAX17042_VALRT_Th,
+		if (!chip->factory_mode)
+			max17042_write_reg(chip->client, MAX17042_VALRT_Th,
 				   chip->pdata->config_data->valrt_thresh);
 	}
 
@@ -1910,6 +1914,20 @@ iterm_fail:
 	return;
 }
 
+static bool max17042_mmi_factory(void)
+{
+	struct device_node *np = of_find_node_by_path("/chosen");
+	bool factory = false;
+
+	if (np)
+		factory = of_property_read_bool(np, "mmi,factory-cable");
+
+	of_node_put(np);
+
+	return factory;
+}
+
+
 static int max17042_probe(struct i2c_client *client,
 			const struct i2c_device_id *id)
 {
@@ -1935,6 +1953,7 @@ static int max17042_probe(struct i2c_client *client,
 		return -EINVAL;
 	}
 
+	chip->factory_mode = false;
 	chip->charge_full_des = (chip->pdata->config_data->design_cap / 2) * 1000;
 
 	i2c_set_clientdata(client, chip);
@@ -1982,6 +2001,10 @@ static int max17042_probe(struct i2c_client *client,
 		dev_err(&client->dev, "failed: power supply register\n");
 		return ret;
 	}
+
+	chip->factory_mode = max17042_mmi_factory();
+	if (chip->factory_mode)
+		dev_info(&client->dev, "max17042: Factory Mode\n");
 
 	chip->temp_state = POWER_SUPPLY_HEALTH_UNKNOWN;
 	chip->taper_reached = 0;

MotorolaMobilityLLC/kernel-msm

drivers/power/max17042_battery.c
@@ -138,6 +138,7 @@ struct max17042_chip {
 	struct max17042_wakeup_source max17042_wake_source;
 	int charge_full_des;
 	int taper_reached;
+	bool factory_mode;
 };
 
 static void max17042_stay_awake(struct max17042_wakeup_source *source)
@@ -734,7 +735,9 @@ static inline void max17042_override_por_values(struct max17042_chip *chip)
 	max17042_override_por(map, MAX17042_CGAIN, config->cgain);
 	max17042_override_por(map, MAX17042_COFF, config->coff);
 
-	max17042_override_por(map, MAX17042_VALRT_Th, POR_VALRT_THRESHOLD);
+	if (!chip->factory_mode)
+		max17042_override_por(map, MAX17042_VALRT_Th,
+				      POR_VALRT_THRESHOLD);
 	max17042_override_por(map, MAX17042_TALRT_Th, config->talrt_thresh);
 	max17042_override_por(map, MAX17042_SALRT_Th,
 						config->soc_alrt_thresh);
@@ -753,6 +756,7 @@ static inline void max17042_override_por_values(struct max17042_chip *chip)
 
 	max17042_override_por(map, MAX17042_FullCAP, config->fullcap);
 	max17042_override_por(map, MAX17042_FullCAPNom, config->fullcapnom);
+
 	if (chip->chip_type == MAX17042)
 		max17042_override_por(map, MAX17042_SOC_empty,
 						config->socempty);
@@ -1033,7 +1037,8 @@ static irqreturn_t max17042_thread_handler(int id, void *dev)
 		chip->batt_undervoltage = true;
 	} else if (val & STATUS_INTR_VMAX_BIT) {
 		dev_info(&chip->client->dev, "Battery overvoltage INTR\n");
-		max17042_write_reg(chip->client, MAX17042_VALRT_Th,
+		if (!chip->factory_mode)
+			max17042_write_reg(chip->client, MAX17042_VALRT_Th,
 				   chip->pdata->config_data->valrt_thresh);
 	}
 
@@ -1890,6 +1895,19 @@ iterm_fail:
 	return;
 }
 
+static bool max17042_mmi_factory(void)
+{
+	struct device_node *np = of_find_node_by_path("/chosen");
+	bool factory = false;
+
+	if (np)
+		factory = of_property_read_bool(np, "mmi,factory-cable");
+
+	of_node_put(np);
+
+	return factory;
+}
+
 static struct regmap_config max17042_regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 16,
@@ -1977,6 +1995,10 @@ static int max17042_probe(struct i2c_client *client,
 		dev_err(&client->dev, "failed: power supply register\n");
 		return ret;
 	}
+
+	chip->factory_mode = max17042_mmi_factory();
+	if (chip->factory_mode)
+		dev_info(&client->dev, "max17042: Factory Mode\n");
 
 	chip->temp_state = POWER_SUPPLY_HEALTH_UNKNOWN;
 	chip->taper_reached = 0;