22504fadef8e63e24d995d6ee948fe71fcbe845d
Haibin Liu <haibinl@codeaurora.org>
2017-12-02 02:11:35 +0800

    msm: sensor: actuator: check NULL pointer for actuator ctrl
    Add NULL pointer checking for actuator ctrl.
    
    Bug-id: A-65122765
    CRs-Fixed: 2152399
    CVE-fixed: CVE-2017-15857
    Mot-CRs-fixed: (CR)
    
    Change-Id: I2e206516e5ea1383c7396625455e1d7403df51cf
    Signed-off-by: Haibin Liu <haibinl@codeaurora.org>
    Signed-off-by: Srikanth A R <arsrikan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1161536
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -100,13 +100,10 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 	struct msm_camera_i2c_reg_array *i2c_tbl = NULL;
 	CDBG("Enter\n");
 
-	if (a_ctrl == NULL) {
-		pr_err("failed. actuator ctrl is NULL");
-		return;
-	}
-
-	if (a_ctrl->i2c_reg_tbl == NULL) {
-		pr_err("failed. i2c reg tabl is NULL");
+	if ((!a_ctrl) ||
+		(!a_ctrl->reg_tbl) ||
+		(!a_ctrl->i2c_reg_tbl)) {
+		pr_err("failed. NULL actuator pointers");
 		return;
 	}
 
