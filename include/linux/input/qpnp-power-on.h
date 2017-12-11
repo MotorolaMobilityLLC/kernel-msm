9b25470d6a90b9ba726a25b0119c024e3c7fcc5c
Ryan Lattrel <ryanl@motorola.com>
2017-12-11 17:40:38 -0600

    msm-poweroff: Store restart reason
    After cold reset is introduced, the phone is cold reset instead of
    warm reset, such as panic. In this case, the power up reason saved
    in shared memory will be lost. Consequently, the power up reason on
    next boot up time will be normal boot instead of correct ones. To
    resolve this issue, we can use the two spared pmic registers which
    are power lost safe to save the restart reason for these cases.
    
    Motorola definition of AP panic bit set in extra info pmic reg
    conflicts with QC definition of Warm reboot bit set in extra info reg
    move Mot bit to available position to avoid conflict
    
    Force a cold reboot for the command "adb reboot bootloader"
    to avoid the warm boot when user tries to continue boot from boot menu.
    
    For early kernel panic before PMIC is initialized, the function
    qpnp_pon_store_extra_reset_info will fail. Need to set IMEM flag
    for fail safe in this case.
    
    For post-panic, post-wdt, post-pmicwdt reboot, we do cold reset.
    And set the PMIC register accordingly, to nofity BL the special
    reboot behavior.
    
    Configure panic as warm restart, so that we have chance to get panic logs.
    
    According to SR 01277290, 0x88D and 0x88E are spare registers for PMIC
    where we could store oem specific reset information
    
    Ported from: https://gerrit.mot.com/#/c/1211826/
    
    Change-Id: Id3a010b8a52e72180685b1f92491d9616b2cb714
    Signed-off-by: Andrew Abbott <drewa@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1446282
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Cesar Augusto Marcelino dos Santos <cmsantos@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1778582
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -57,6 +57,13 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
 };
 
+#define RESET_EXTRA_POST_PANIC_REASON	(BIT(4) | BIT(5))
+#define RESET_EXTRA_POST_PMICWDT_REASON	BIT(5)
+#define RESET_EXTRA_POST_WDT_REASON	BIT(4)
+#define RESET_EXTRA_POST_REBOOT_MASK	(BIT(4) | BIT(5) | BIT(6))
+#define RESET_EXTRA_PANIC_REASON       BIT(3)
+#define RESET_EXTRA_REBOOT_BL_REASON   BIT(2)
+
 #if IS_ENABLED(CONFIG_INPUT_QPNP_POWER_ON)
 int qpnp_pon_system_pwr_off(enum pon_power_off_type type);
 int qpnp_pon_is_warm_reset(void);
@@ -65,6 +72,7 @@ int qpnp_pon_wd_config(bool enable);
 int qpnp_pon_set_restart_reason(enum pon_restart_reason reason);
 bool qpnp_pon_check_hard_reset_stored(void);
 int qpnp_pon_modem_pwr_off(enum pon_power_off_type type);
+int qpnp_pon_store_extra_reset_info(u16 mask, u16 val);
 
 #else
 
@@ -104,6 +112,10 @@ static inline int qpnp_pon_modem_pwr_off(enum pon_power_off_type type)
 	return -ENODEV;
 }
 
+static inline int qpnp_pon_store_extra_reset_info(u16 mask, u16 val)
+{
+	return -ENODEV;
+}
 #endif
 
 #endif
