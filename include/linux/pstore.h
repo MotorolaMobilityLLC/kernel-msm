c0da5dbe7294a783fd580133304b6220eaac5bab
Ryan Lattrel <ryanl@motorola.com>
2017-12-11 17:40:33 -0600

    fs:pstore:ramoops: Add annotation support to pstore
    Add annotation support to the pstore. This is used by
    bootinfo, tzlog, and the watchdog to add extra
    information in the /data/dontpanic logs.
    
    The memory regions used by pstore are copied to
    /data/dontpanic by the kpreadwrite apps when a panic
    occurs.
    
    Ported from: http://gerrit.mot.com/782684,
                 http://gerrit.mot.com/782685,
                 http://gerrit.mot.com/782686,
                 http://gerrit.mot.com/782690,
                 http://gerrit.mot.com/782687,
                 http://gerrit.mot.com/782688,
                 http://gerrit.mot.com/782689,
                 http://gerrit.mot.com/782691,
                 http://gerrit.mot.com/783406,
                 http://gerrit.mot.com/956664
    
    Change-Id: Ib5e1eeb42cd6cbc086bf2945437e8daf88a29dc2
    Signed-off-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1102744
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Reviewed-by: Dong-Hua Yan <a18317@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/pstore.h
+++ include/linux/pstore.h
@@ -42,6 +42,7 @@ enum pstore_type_id {
 	PSTORE_TYPE_PPC_COMMON	= 6,
 	PSTORE_TYPE_PMSG	= 7,
 	PSTORE_TYPE_PPC_OPAL	= 8,
+	PSTORE_TYPE_ANNOTATE    = 9,
 	PSTORE_TYPE_UNKNOWN	= 255
 };
 
@@ -88,5 +89,6 @@ struct pstore_info {
 extern int pstore_register(struct pstore_info *);
 extern void pstore_unregister(struct pstore_info *);
 extern bool pstore_cannot_block_path(enum kmsg_dump_reason reason);
+extern int pstore_annotate(const char *buf);
 
 #endif /*_LINUX_PSTORE_H*/
--- include/linux/pstore_ram.h
+++ include/linux/pstore_ram.h
@@ -79,6 +79,15 @@ void *persistent_ram_old(struct persistent_ram_zone *prz);
 void persistent_ram_free_old(struct persistent_ram_zone *prz);
 ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 	char *str, size_t len);
+#ifdef CONFIG_PSTORE_RAM_ANNOTATION_APPEND
+__printf(1, 2)  int persistent_ram_annotation_append(const char *fmt, ...);
+void persistent_ram_annotation_merge(struct persistent_ram_zone *prz);
+#else
+static inline __printf(1, 2) void persistent_ram_annotation_append(
+			const char *fmt, ...) { };
+static inline void persistent_ram_annotation_merge(
+			struct persistent_ram_zone *prz) { };
+#endif
 
 void ramoops_console_write_buf(const char *buf, size_t size);
 
@@ -96,6 +105,7 @@ struct ramoops_platform_data {
 	unsigned long	console_size;
 	unsigned long	ftrace_size;
 	unsigned long	pmsg_size;
+	unsigned long   annotate_size;
 	int		dump_oops;
 	struct persistent_ram_ecc_info ecc_info;
 };
