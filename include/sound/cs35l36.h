c1aace7b004a68d845fba3cbba3518b04f921c53
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2017-12-14 15:29:46 -0600

    ASoC: cs33l36: Update driver with B0 support
    The driver is updated to public version posted on
    https://github.com/CirrusLogic/linux-drivers
    Branch v4.4-cs35l36.
    
    Change-Id: Ibdad95bd1f227e3a2b5aedd2e0e14abb77457a00
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Mot-CRs-fixed: (CR) Womack driver, dev tree, & AoV kernel updates
    Reviewed-on: https://gerrit.mot.com/1116717
    Tested-by: Jira Key
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- include/sound/cs35l36.h
+++ include/sound/cs35l36.h
@@ -11,28 +11,6 @@
 #ifndef __CS35L36_H
 #define __CS35L36_H
 
-struct classh_cfg {
-
-};
-
-/* ASP TX Platform Data */
-struct asp_tx_cfg {
-	bool is_present;
-	bool tx1, tx2, tx3, tx4, tx5, tx6;
-	u8 tx1_slot;
-	u8 tx1_src_select;
-	u8 tx2_slot;
-	u8 tx2_src_select;
-	u8 tx3_slot;
-	u8 tx3_src_select;
-	u8 tx4_slot;
-	u8 tx4_src_select;
-	u8 tx5_slot;
-	u8 tx5_src_select;
-	u8 tx6_slot;
-	u8 tx6_src_select;
-};
-
 /* INT/GPIO pin configuration */
 struct irq_cfg {
 	bool is_present;
@@ -46,7 +24,7 @@ struct irq_cfg {
 struct cs35l36_platform_data {
 	bool sclk_frc;
 	bool lrclk_frc;
-	bool right_channel;
+	bool multi_amp_mode;
 	bool dcm_mode;
 	int ldm_mode_sel;
 	bool amp_gain_zc;
@@ -56,9 +34,9 @@ struct cs35l36_platform_data {
 	int bst_vctl;
 	int bst_vctl_sel;
 	int bst_ipk;
+	bool extern_boost;
 	int temp_warn_thld;
 	struct irq_cfg irq_config;
-	struct asp_tx_cfg asp_tx_config;
 };
 
 #endif /* __CS35L36_H */
