c9dd5a5750c91d1c83c8b206985a095e9e4761ac
zoujc <zoujc@motorola.com>
2018-01-02 17:20:06 +0800

    msm_hs irq: fix irq warning
    [...] irq 267, desc: ffffffc0f87d5a00, depth: 0, count: 0, unhandled: 0
    [...] ->handle_irq():  ffffffc0003a99f0, msm_gpio_irq_handler+0x0/0x17c
    [   67.945295] ->irq_data.chip(): ffffffc001f0b5b0, gic_chip+0x0/0xe8
    [   67.945314] ->action():           (null)
    [   67.945322]    IRQ_NOPROBE set
    [   67.945329]  IRQ_NOREQUEST set
    [   67.945335]   IRQ_NOTHREAD set
    
    Change-Id: I1aa7d0f75424335de40f4743a79faf1892549479
    Signed-off-by: zoujc <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1110696
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jialei Hao <haojl2@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2614,10 +2614,12 @@ static int msm_hs_startup(struct uart_port *uport)
 	msm_hs_resource_vote(msm_uport);
 
 	if (is_use_low_power_wakeup(msm_uport)) {
+		irq_set_status_flags(msm_uport->wakeup.irq, IRQ_DISABLE_UNLAZY);
 		ret = request_irq(msm_uport->wakeup.irq, msm_hs_wakeup_isr,
 					IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
 					"msm_hs_wakeup", msm_uport);
 		if (unlikely(ret)) {
+			irq_clear_status_flags(msm_uport->wakeup.irq, IRQ_DISABLE_UNLAZY);
 			MSM_HS_ERR("%s():Err getting uart wakeup_irq %d\n",
 				  __func__, ret);
 			goto unvote_exit;
@@ -3665,6 +3667,7 @@ static void msm_hs_shutdown(struct uart_port *uport)
 	/* Free the interrupt */
 	free_irq(uport->irq, msm_uport);
 	if (is_use_low_power_wakeup(msm_uport)) {
+		irq_clear_status_flags(msm_uport->wakeup.irq, IRQ_DISABLE_UNLAZY);
 		free_irq(msm_uport->wakeup.irq, msm_uport);
 		MSM_HS_DBG("%s(): wakeup irq freed", __func__);
 	}
