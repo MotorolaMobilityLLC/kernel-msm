bd53843d31d34a50cd63d4177159e97a94e2913f
Daniel Rosenberg <drosen@google.com>
2018-01-02 14:44:49 -0800

    ANDROID: sdcardfs: Add default_normal option
    The default_normal option causes mounts with the gid set to
    AID_SDCARD_RW to have user specific gids, as in the normal case.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I9619b8ac55f41415df943484dc8db1ea986cef6f
    Bug: 64672411
    
    Mot-CRs-fixed: (CR)
    
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Change-Id: I1173fb51631cdc1e8cfedd6908ad679245005611
    Reviewed-on: https://gerrit.mot.com/1146735
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -234,6 +234,7 @@ struct sdcardfs_mount_options {
 struct sdcardfs_vfsmount_options {
 	gid_t gid;
 	mode_t mask;
+	bool default_normal;
 };
 
 extern int parse_options_remount(struct super_block *sb, char *options, int silent,
@@ -425,7 +426,7 @@ static inline int get_gid(struct vfsmount *mnt,
 {
 	struct sdcardfs_vfsmount_options *opts = mnt->data;
 
-	if (opts->gid == AID_SDCARD_RW)
+	if (opts->gid == AID_SDCARD_RW && !opts->default_normal)
 		/* As an optimization, certain trusted system components only run
 		 * as owner but operate across all users. Since we're now handing
 		 * out the sdcard_rw GID only to trusted apps, we're okay relaxing
