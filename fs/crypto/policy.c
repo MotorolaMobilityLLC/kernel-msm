f4acc4d7789bbff79a1c9aac5d6b15d60bf4aea3
Shiyong Li <a22381@motorola.com>
2018-01-08 18:04:20 -0800

    fs/crypto: print encryption info while seeing mismatch
    Print parent and child node encryption information while seeing
    mismatch to better debug issue.
    
    This is a port of the following commits on 8998:
    5198858 (CR) fs/ext4: add more details in crypt-mismatching log
    2ccee72 (CR) fs/ext4: add log to debug encryption mismatch
    
    Change-Id: Ie806cd23c2fb452d80a16f14b13eef4afcc0344a
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1116758
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/crypto/policy.c
+++ fs/crypto/policy.c
@@ -11,6 +11,7 @@
 #include <linux/random.h>
 #include <linux/string.h>
 #include <linux/mount.h>
+#include <trace/events/android_fs.h>
 #include "fscrypt_private.h"
 
 /*
@@ -133,6 +134,24 @@ int fscrypt_ioctl_get_policy(struct file *filp, void __user *arg)
 }
 EXPORT_SYMBOL(fscrypt_ioctl_get_policy);
 
+static void fs_encryption_info_print(struct inode *inode, const char *key,
+	char d_mode, char f_mode, char flags)
+{
+	int i;
+	char key_hex[FS_KEY_DESCRIPTOR_SIZE * 2 + 1];
+	char *p = key_hex;
+	char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
+
+	for (i = FS_KEY_DESCRIPTOR_SIZE - 1; i >= 0; i--)
+		p += snprintf(p, sizeof(key_hex) - (p - key_hex),
+			"%02x",	key[i]);
+	key_hex[sizeof(key_hex) - 1] = 0;
+	path = android_fstrace_get_pathname(pathbuf,
+		MAX_TRACE_PATHBUF_LEN, inode);
+	pr_info("%s %02x %02x %02x %lu-%s\n",
+		key_hex, d_mode, f_mode, flags, inode->i_ino, path);
+}
+
 /**
  * fscrypt_has_permitted_context() - is a file's encryption policy permitted
  *				     within its directory?
@@ -170,8 +189,12 @@ int fscrypt_has_permitted_context(struct inode *parent, struct inode *child)
 		return 1;
 
 	/* Encrypted directories must not contain unencrypted files */
-	if (!IS_ENCRYPTED(child))
+	if (!IS_ENCRYPTED(child)){
+		pr_warn("%s:(%s) unencrypted file %lu in encrypted dir %lu\n",
+			__func__, child->i_sb->s_id,
+			child->i_ino, parent->i_ino);
 		return 0;
+	}
 
 	/*
 	 * Both parent and child are encrypted, so verify they use the same
@@ -198,12 +221,25 @@ int fscrypt_has_permitted_context(struct inode *parent, struct inode *child)
 	child_ci = child->i_crypt_info;
 
 	if (parent_ci && child_ci) {
-		return memcmp(parent_ci->ci_master_key, child_ci->ci_master_key,
+		res = memcmp(parent_ci->ci_master_key, child_ci->ci_master_key,
 			      FS_KEY_DESCRIPTOR_SIZE) == 0 &&
 			(parent_ci->ci_data_mode == child_ci->ci_data_mode) &&
 			(parent_ci->ci_filename_mode ==
 			 child_ci->ci_filename_mode) &&
 			(parent_ci->ci_flags == child_ci->ci_flags);
+		if (!res) {
+			pr_warn("%s:(%s) crypt info mismatch %d-%s\n", __func__,
+				child->i_sb->s_id, current->pid, current->comm);
+			fs_encryption_info_print(parent,
+				parent_ci->ci_master_key,
+				parent_ci->ci_data_mode,
+				parent_ci->ci_filename_mode,
+				parent_ci->ci_flags);
+			fs_encryption_info_print(child,
+				child_ci->ci_master_key, child_ci->ci_data_mode,
+				child_ci->ci_filename_mode, child_ci->ci_flags);
+		}
+		return res;
 	}
 
 	res = cops->get_context(parent, &parent_ctx, sizeof(parent_ctx));
@@ -214,7 +250,7 @@ int fscrypt_has_permitted_context(struct inode *parent, struct inode *child)
 	if (res != sizeof(child_ctx))
 		return 0;
 
-	return memcmp(parent_ctx.master_key_descriptor,
+	res = memcmp(parent_ctx.master_key_descriptor,
 		      child_ctx.master_key_descriptor,
 		      FS_KEY_DESCRIPTOR_SIZE) == 0 &&
 		(parent_ctx.contents_encryption_mode ==
@@ -222,6 +258,21 @@ int fscrypt_has_permitted_context(struct inode *parent, struct inode *child)
 		(parent_ctx.filenames_encryption_mode ==
 		 child_ctx.filenames_encryption_mode) &&
 		(parent_ctx.flags == child_ctx.flags);
+	if (!res) {
+		pr_warn("%s:(%s) crypt context mismatch %d-%s\n", __func__,
+			child->i_sb->s_id, current->pid, current->comm);
+		fs_encryption_info_print(parent,
+			parent_ctx.master_key_descriptor,
+			parent_ctx.contents_encryption_mode,
+			parent_ctx.filenames_encryption_mode,
+			parent_ctx.flags);
+		fs_encryption_info_print(child,
+			child_ctx.master_key_descriptor,
+			child_ctx.contents_encryption_mode,
+			child_ctx.filenames_encryption_mode,
+			child_ctx.flags);
+	}
+	return res;
 }
 EXPORT_SYMBOL(fscrypt_has_permitted_context);
 
