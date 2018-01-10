9ff7160e72f7d5706a5f27681d96eb847eec45ba
Shiyong Li <a22381@motorola.com>
2018-01-09 16:50:52 -0800

    sdcardfs: support extended attributes of directory
    Add setxattr() and getxattr() interfaces to access associated extended
    attributes of lower file system via sdcardfs path.
    
    Port of 6d87b2e on 8998.
    
    Change-Id: I8a5cf8e7cd8f86087534451d17a730ecb2904de0
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1116748
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -21,6 +21,7 @@
 #include "sdcardfs.h"
 #include <linux/fs_struct.h>
 #include <linux/ratelimit.h>
+#include <linux/xattr.h>
 
 const struct cred *override_fsids(struct sdcardfs_sb_info *sbi,
 		struct sdcardfs_inode_data *data)
@@ -552,6 +553,56 @@ void copy_attrs(struct inode *dest, const struct inode *src)
 #endif
 }
 
+static int sdcardfs_permission_default(struct inode *inode, int mask)
+{
+	int err;
+	int owner_mode, visible_mode = 0775;
+	struct inode tmp;
+	struct sdcardfs_inode_data *top = top_data_get(SDCARDFS_I(inode));
+
+	if (!top)
+		return -EINVAL;
+
+	/*
+	 * Permission check on sdcardfs inode.
+	 * Since generic_permission only needs i_mode, i_uid,
+	 * i_gid, and i_sb, we can create a fake inode to pass
+	 * this information down in.
+	 *
+	 * The underlying code may attempt to take locks in some
+	 * cases for features we're not using, but if that changes,
+	 * locks must be dealt with to avoid undefined behavior.
+	 */
+	copy_attrs(&tmp, inode);
+	tmp.i_uid = make_kuid(&init_user_ns, top->d_uid);
+	tmp.i_gid = make_kgid(&init_user_ns,
+		multiuser_get_uid(top->userid, AID_SDCARD_RW));
+
+	if (top->perm == PERM_PRE_ROOT) {
+		/*
+		 * Top of multi-user view should always be visible to ensure
+		 * secondary users can traverse inside.
+		 */
+		visible_mode = 0711;
+	} else if (top->under_android) {
+		/*
+		 * Block "other" access to Android directories, since only apps
+		 * belonging to a specific user should be in there.
+		 */
+		visible_mode = visible_mode & ~0007;
+	}
+	owner_mode = SDCARDFS_I(inode)->lower_inode->i_mode & 0700;
+	tmp.i_mode = (inode->i_mode & S_IFMT) |
+		(visible_mode &
+		 (owner_mode | (owner_mode >> 3) | (owner_mode >> 6)));
+	data_put(top);
+	tmp.i_sb = inode->i_sb;
+	if (IS_POSIXACL(inode))
+		pr_warn("%s: This may be undefined behavior...\n", __func__);
+	err = generic_permission(&tmp, mask);
+	return err;
+}
+
 static int sdcardfs_permission(struct vfsmount *mnt, struct inode *inode, int mask)
 {
 	int err;
@@ -798,7 +849,7 @@ const struct inode_operations sdcardfs_symlink_iops = {
 const struct inode_operations sdcardfs_dir_iops = {
 	.create		= sdcardfs_create,
 	.lookup		= sdcardfs_lookup,
-	.permission	= sdcardfs_permission_wrn,
+	.permission	= sdcardfs_permission_default,
 	.permission2	= sdcardfs_permission,
 	.unlink		= sdcardfs_unlink,
 	.mkdir		= sdcardfs_mkdir,
