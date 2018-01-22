67e44f07e49537b253b0dd475f60413e42bce3e8
Brian Austin <brian.austin@cirrus.com>
2018-01-22 13:16:29 -0600

    ASoC: cs35l36: adjust 10V/12V part distinction
    Decided it would be best to check in bus probe
    and print out what the real part number is.
    Also, eliminate function and just hit the
    test space in codec_probe
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I2d02c8b57c1651cc2656d7f2561aee338d602026
    Signed-off-by: Brian Austin <brian.austin@cirrus.com>
    Reviewed-on: https://gerrit.mot.com/1162068
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Ravikumar Vembu <raviv@motorola.com>

--- sound/soc/codecs/cs35l36.c
+++ sound/soc/codecs/cs35l36.c
@@ -64,10 +64,11 @@ struct  cs35l36_private {
 	int extclk_cfg;
 	int fll_igain;
 	int sclk;
+	int chip_version;
+	int rev_id;
 	struct gpio_desc *reset_gpio;
 	struct completion global_pup_done;
 	struct completion global_pdn_done;
-	int rev_id;
 };
 
 struct cs35l36_pll_sysclk_config {
@@ -843,51 +844,6 @@ static int cs35l36_boost_inductor(struct cs35l36_private *cs35l36, int inductor)
 	return 0;
 }
 
-/*
- * Rev B0 has 2 versions
- * L36 is 10V
- * L37 is 12V
- * If L36 we need to clamp some values for safety
- * after probe has setup dt values. We want to make
- * sure we dont miss any values set in probe
- */
-static int cs35l36_revb_bstlimit(struct cs35l36_private *cs35l36)
-{
-	int ret = 0;
-	u32 l37_id_reg;
-
-	ret = regmap_read(cs35l36->regmap, CS35L36_OTP_MEM30, &l37_id_reg);
-	if (ret < 0) {
-		dev_err(cs35l36->dev, "Failed to read otp_id Register %d\n", ret);
-		return ret;
-	}
-
-	if ((l37_id_reg & CS35L36_OTP_REV_MASK) == CS35L36_OTP_REV_L37) {
-		regmap_update_bits(cs35l36->regmap, CS35L36_BSTCVRT_OVERVOLT_CTRL,
-					CS35L36_BST_OVP_THLD_MASK,
-					CS35L36_BST_OVP_THLD_11V);
-		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
-					CS35L36_TEST_UNLOCK1);
-		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
-					CS35L36_TEST_UNLOCK2);
-		regmap_update_bits(cs35l36->regmap, CS35L36_BST_ANA2_TEST,
-					CS35L36_BST_OVP_TRIM_MASK,
-					CS35L36_BST_OVP_TRIM_11V <<
-					CS35L36_BST_OVP_TRIM_SHIFT);
-		regmap_update_bits(cs35l36->regmap, CS35L36_BSTCVRT_VCTRL2,
-					CS35L36_BST_CTRL_LIM_MASK,
-					1 << CS35L36_BST_CTRL_LIM_SHIFT);
-		regmap_update_bits(cs35l36->regmap, CS35L36_BSTCVRT_VCTRL1,
-					CS35L35_BSTCVRT_CTL_MASK,
-					CS35L36_BST_CTRL_10V_CLAMP);
-		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
-					CS35L36_TEST_LOCK1);
-		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
-					CS35L36_TEST_LOCK2);
-	}
-	return 0;
-}
-
 static int cs35l36_codec_probe(struct snd_soc_codec *codec)
 {
 	struct cs35l36_private *cs35l36 = snd_soc_codec_get_drvdata(codec);
@@ -993,11 +949,37 @@ static int cs35l36_codec_probe(struct snd_soc_codec *codec)
 		regmap_update_bits(cs35l36->regmap, CS35L36_DTEMP_WARN_THLD,
 					CS35L36_TEMP_THLD_MASK,
 					cs35l36->pdata.temp_warn_thld);
-
-	ret = cs35l36_revb_bstlimit(cs35l36);
-	if (!ret) {
-		dev_err(cs35l36->dev, "Rev B0 BST Limit Error\n");
-		return -EINVAL;
+/*
+ * Rev B0 has 2 versions
+ * L36 is 10V
+ * L37 is 12V
+ * If L36 we need to clamp some values for safety
+ * after probe has setup dt values. We want to make
+ * sure we dont miss any values set in probe
+ */
+	if (cs35l36->chip_version == CS35L36_10V_L36) {
+		regmap_update_bits(cs35l36->regmap,
+				CS35L36_BSTCVRT_OVERVOLT_CTRL,
+				CS35L36_BST_OVP_THLD_MASK,
+				CS35L36_BST_OVP_THLD_11V);
+		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
+				CS35L36_TEST_UNLOCK1);
+		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
+					CS35L36_TEST_UNLOCK2);
+		regmap_update_bits(cs35l36->regmap, CS35L36_BST_ANA2_TEST,
+					CS35L36_BST_OVP_TRIM_MASK,
+					CS35L36_BST_OVP_TRIM_11V <<
+					CS35L36_BST_OVP_TRIM_SHIFT);
+		regmap_update_bits(cs35l36->regmap, CS35L36_BSTCVRT_VCTRL2,
+					CS35L36_BST_CTRL_LIM_MASK,
+					1 << CS35L36_BST_CTRL_LIM_SHIFT);
+		regmap_update_bits(cs35l36->regmap, CS35L36_BSTCVRT_VCTRL1,
+					CS35L35_BSTCVRT_CTL_MASK,
+					CS35L36_BST_CTRL_10V_CLAMP);
+		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
+					CS35L36_TEST_LOCK1);
+		regmap_write(cs35l36->regmap, CS35L36_TESTKEY_CTRL,
+					CS35L36_TEST_LOCK2);
 	}
 	snd_soc_dapm_ignore_suspend(dapm, "SDIN");
 	snd_soc_dapm_ignore_suspend(dapm, "SDOUT");
@@ -1460,7 +1442,7 @@ static int cs35l36_i2c_probe(struct i2c_client *i2c_client,
 
 	int i;
 	int ret;
-	u32 reg_id, reg_revid;
+	u32 reg_id, reg_revid, l37_id_reg;
 	int irq_pol = IRQF_TRIGGER_HIGH;
 
 	cs35l36 = devm_kzalloc(dev, sizeof(struct cs35l36_private), GFP_KERNEL);
@@ -1538,7 +1520,7 @@ static int cs35l36_i2c_probe(struct i2c_client *i2c_client,
 	}
 
 	if (reg_id != CS35L36_CHIP_ID) {
-		dev_err(dev, "CS35L36 Device ID (%X). Expected ID %X\n",
+		dev_err(dev, "Device ID (%X). Expected ID %X\n",
 			reg_id, CS35L36_CHIP_ID);
 		ret = -ENODEV;
 		goto err;
@@ -1551,6 +1533,18 @@ static int cs35l36_i2c_probe(struct i2c_client *i2c_client,
 	}
 
 	cs35l36->rev_id = reg_revid >> 8;
+
+	ret = regmap_read(cs35l36->regmap, CS35L36_OTP_MEM30, &l37_id_reg);
+	if (ret < 0) {
+		dev_err(&i2c_client->dev, "Failed to read otp_id Register %d\n",
+			ret);
+		return ret;
+	}
+
+	if ((l37_id_reg & CS35L36_OTP_REV_MASK) == CS35L36_OTP_REV_L37)
+		cs35l36->chip_version = CS35L36_12V_L37;
+	else
+		cs35l36->chip_version = CS35L36_10V_L36;
 
 	ret = cs35l36_pac(cs35l36);
 	if (ret < 0) {
@@ -1617,8 +1611,8 @@ static int cs35l36_i2c_probe(struct i2c_client *i2c_client,
 			CS35L36_INT3_MASK_DEFAULT);
 
 	dev_info(&i2c_client->dev,
-			"Cirrus Logic CS35L36 (%x), Revision: %02X\n", reg_id,
-			reg_revid >> 8);
+			"Cirrus Logic CS35L%d, Revision: %02X\n",
+			cs35l36->chip_version, reg_revid >> 8);
 
 	ret =  snd_soc_register_codec(dev, &soc_codec_dev_cs35l36, cs35l36_dai,
 					ARRAY_SIZE(cs35l36_dai));
