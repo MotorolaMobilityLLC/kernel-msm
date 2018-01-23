76cb50d997151cec62f5a5ce3543d69da835f313
Daniel Rosenberg <drosen@google.com>
2018-01-23 14:34:38 -0800

    ANDROID: xattr: Pass EOPNOTSUPP to permission2
    The permission call for xattr operations happens regardless of
    whether or not the xattr functions are implemented.
    
    The xattr functions currently don't have support for permission2.
    Passing EOPNOTSUPP as the mount point in xattr_permission allows
    us to return EOPNOTSUPP early in permission2, if the filesystem
    supports it.
    
    Change-Id: I9d07e4cd633cf40af60450ffbff7ac5c1b4e8c2c
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 35848445
    
    Mot-CRs-fixed: (CR)
    
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Change-Id: I909e67d0f78fd390d5e3131951b34b15ea82052f
    Reviewed-on: https://gerrit.mot.com/1146738
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -620,6 +620,8 @@ static int sdcardfs_permission(struct vfsmount *mnt, struct inode *inode, int ma
 	uid_t inode_userid;
 #endif
 
+	if (IS_ERR(mnt))
+		return PTR_ERR(mnt);
 	if (!top)
 		return -EINVAL;
 
