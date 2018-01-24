5d6998328c75accd6b1fd1e5dfbc197031ba7088
Donghua Yan <a18317@motorola.com>
2018-01-24 12:02:10 -0600

    msm: pon: Add interface to check Shipmode
    Added an new API qpnp_pon_check_shipmode_info() to check
    whether shipmode bit is set.
    
    Change-Id: I759c6e5367cd62e160eaf0a7d519d232103eacd3
    Signed-off-by: Donghua Yan <a18317@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1123513
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 193fb4fbd08d4f185cd0827b4aec5e3fa3af7596)
    Reviewed-on: https://gerrit.mot.com/1124329
    Reviewed-by: Qing Chang <qing@motorola.com>

--- include/linux/qpnp/power-on.h
+++ include/linux/qpnp/power-on.h
@@ -85,6 +85,7 @@ bool qpnp_pon_check_hard_reset_stored(void);
 
 int qpnp_pon_store_extra_reset_info(u16 mask, u16 val);
 int qpnp_pon_store_shipmode_info(u16 mask, u16 val);
+bool qpnp_pon_check_shipmode_info(void);
 #else
 static int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
 {
@@ -115,6 +116,10 @@ static inline int qpnp_pon_store_extra_reset_info(u16 mask, u16 val)
 static inline int qpnp_pon_store_shipmode_info(u16 mask, u16 val)
 {
 	return -ENODEV;
+}
+static inline bool qpnp_pon_check_shipmode_info(void)
+{
+	return false;
 }
 #endif
 
