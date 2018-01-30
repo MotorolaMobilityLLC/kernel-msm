dff9ca9ad59ee12dd6135215ae1ee93b71ddb9b1
Lianwei Wang <a22439@motorola.com>
2018-01-30 14:54:49 +0530

    PM / Sleep: fix watchdog timer deletion
    Add the mising deletion of watchdog timer. The watchdog
    will be fault triggered without deleting.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: If024b42facf92aeb92019f0e8851763a700dfa4f
    Fixes: 4f0d28602ff7 ("drivers: power: Add watchdog timer to catch drivers which lockup during suspend.")
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Signed-off-by: amarenr <amarenr@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1126416
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1385,7 +1385,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 		if (pm_runtime_status_suspended(dev)) {
 			pm_runtime_disable(dev);
 			if (pm_runtime_suspended_if_enabled(dev))
-				goto Complete;
+				goto Deltimer;
 
 			pm_runtime_enable(dev);
 		}
@@ -1458,6 +1458,8 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	}
 
 	device_unlock(dev);
+
+Deltimer:
 	dpm_watchdog_clear(&wd);
 
  Complete:
