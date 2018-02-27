e44b9cc3e16709dd96ea1cf8d7c393049951f857
luodw1 <luodw1@motorola.com>
2018-02-27 13:46:57 +0800

    driver:debug optimize wakeup reason statistic
    Fix wakeup reason match error and record gpio interrupt
    wakeup reason.
    Current wakeup reason as follows:
    gic_show_resume_irq: 57 triggered qcom,smd-modem
    gic_show_resume_irq: 200 triggered qcom,smd-rpm
    But matched wakeup reason as follows:
    Resume caused by IRQ 200
    Resume caused by IRQ 57, mmc0
    
    After repair wakeup reason as follows:
    gic_show_resume_irq: 57(IRQ 290) triggered qcom,smd-modem
    gic_show_resume_irq: 200(IRQ 296) triggered qcom,smd-rpm
    ...
    Resume caused by IRQ 296, qcom,smd-rpm
    Resume caused by IRQ 290, qcom,smd-modem
    
    Change-Id: I6fc5f1c9a66481e368e7588a61924c6c40c55bba
    Signed-off-by: luodw1 <luodw1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1140493
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 02e6a24f503cf4adfb87f424d742ab98c6901aa8)
    Reviewed-on: https://gerrit.mot.com/1142248
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

--- drivers/platform/msm/spmi/qpnp-int.c
+++ drivers/platform/msm/spmi/qpnp-int.c
@@ -28,6 +28,7 @@
 #include <linux/printk.h>
 #include <linux/ratelimit.h>
 #include <linux/irqchip/qpnp-int.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/irq.h>
 
@@ -641,6 +642,7 @@ static int __qpnpint_handle_irq(struct spmi_controller *spmi_ctrl,
 
 		pr_warn("%d triggered [0x%01x, 0x%02x,0x%01x] %s\n",
 				irq, spec->slave, spec->per, spec->irq, name);
+		log_base_wakeup_reason(irq);
 	} else {
 		generic_handle_irq(irq);
 	}
