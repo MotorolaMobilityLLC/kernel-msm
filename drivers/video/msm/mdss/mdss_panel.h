c74fdce546652c26694dada73da28d8555d43517
hujing3 <hujing3@lenovo.com>
2018-03-21 10:14:29 +0800

    msm: mdss: add rst_disable dsi config
    root cause: add rst_disable for mdss-dsi-off-reset-disable
     to optimize power when suspend
    
    Change-Id: I4d88cb68f3c9fdba565ac8ce99036e995e0cb7ff
    Signed-off-by: hujing3 <hujing3@mt.com>
    Reviewed-on: https://gerrit.mot.com/1150818
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -698,6 +698,7 @@ struct mdss_panel_info {
 	u32 out_format;
 	u32 rst_seq[MDSS_DSI_RST_SEQ_LEN];
 	u32 rst_seq_len;
+	bool rst_disable;        /*add xinjunjie at 20180312*/
 	u32 vic; /* video identification code */
 	struct mdss_rect roi;
 	int pwm_pmic_gpio;
