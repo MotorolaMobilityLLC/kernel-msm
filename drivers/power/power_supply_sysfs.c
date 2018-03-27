c466715061b79cc02b86118ea5770aca47037553
Srihari Sathyanarayana <sriharis@motorola.com>
2018-03-27 11:47:13 +0530

    Revert "power_supply: Add REAL_TYPE power_supply_property"
    This reverts commit 07d424c142ae720347fcbaf02fa1432f6066142f
    
    The QC change require battery HAL change and it is new feature.
    Revert it to  resolve USB connection issues for now.
    
    Mot-CRs-fixed:(CR)
    
    Change-Id: Iaff9bce66198ca7b9f2cf179ae17b81137cf748b
    Signed-off-by: Srihari Sathyanarayana <sriharis@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1154551
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Vijayakumar Gn <vijaygn@motorola.com>
    Reviewed-by: Nagarathna Kodandaram <cnfk37@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -127,8 +127,7 @@ static ssize_t power_supply_show_property(struct device *dev,
 		return sprintf(buf, "%s\n", technology_text[value.intval]);
 	else if (off == POWER_SUPPLY_PROP_CAPACITY_LEVEL)
 		return sprintf(buf, "%s\n", capacity_level_text[value.intval]);
-	else if (off == POWER_SUPPLY_PROP_TYPE ||
-			off == POWER_SUPPLY_PROP_REAL_TYPE)
+	else if (off == POWER_SUPPLY_PROP_TYPE)
 		return sprintf(buf, "%s\n", type_text[value.intval]);
 	else if (off == POWER_SUPPLY_PROP_SCOPE)
 		return sprintf(buf, "%s\n", scope_text[value.intval]);
@@ -330,7 +329,6 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(die_health),
 	POWER_SUPPLY_ATTR(connector_health),
 	POWER_SUPPLY_ATTR(hw_current_max),
-	POWER_SUPPLY_ATTR(real_type),
 	POWER_SUPPLY_ATTR(internal_send),
 	POWER_SUPPLY_ATTR(internal_receive),
 	POWER_SUPPLY_ATTR(external),
