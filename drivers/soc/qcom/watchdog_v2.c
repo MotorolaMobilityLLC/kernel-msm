cfd579b5d851193310543cda96530e8b04ae5432
xiaoy3 <xiaoy3@motorola.com>
2018-04-03 12:13:50 +0800

    DLKM: Export func msm_trigger_wdog_bit for watchdogtest
    Change-Id: I40a61edef6db8bf161a2fea7c5e2d6afe51287fd
    Signed-off-by: xiaoy3 <xiaoy3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1157188
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -511,6 +511,7 @@ void msm_trigger_wdog_bite(void)
 		__raw_readl(wdog_data->base + WDT0_BARK_TIME),
 		__raw_readl(wdog_data->base + WDT0_BITE_TIME));
 }
+EXPORT_SYMBOL_GPL(msm_trigger_wdog_bite);
 
 static irqreturn_t wdog_bark_handler(int irq, void *dev_id)
 {
