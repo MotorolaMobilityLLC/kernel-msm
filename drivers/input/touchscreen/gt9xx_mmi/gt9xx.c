d0eb778e7530e6f41e187c91bdecadc33502c6e5
hujing3 <hujing3@lenovo.com>
2018-04-03 11:32:11 +0800

    input: goodix touch driver update
    input: goodix tp update, to check hw error case
    
    Change-Id: Ia9f152d7c22b60e7f1d0a2b63f3e945855a2d17d
    Signed-off-by: hujing3 <hujing3@mt.com>
    Reviewed-on: https://gerrit.mot.com/1160277
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/gt9xx_mmi/gt9xx.c
+++ drivers/input/touchscreen/gt9xx_mmi/gt9xx.c
@@ -1594,14 +1594,18 @@ static ssize_t gtp_productinfo_show(struct device *dev,
 {
 	struct goodix_ts_data *data = dev_get_drvdata(dev);
 
-	if (data->product_id[3] == 0x00) {
-		return scnprintf(buf, PAGE_SIZE, "GT%c%c%c\n",
-				data->product_id[0], data->product_id[1],
-				data->product_id[2]);
+	if (data->fw_error == 1) {
+		return scnprintf(buf, PAGE_SIZE, "GT917D");
 	} else {
-		return scnprintf(buf, PAGE_SIZE, "GT%c%c%c%c\n",
-			data->product_id[0], data->product_id[1],
-			data->product_id[2], data->product_id[3]);
+		if (data->product_id[3] == 0x00) {
+			return scnprintf(buf, PAGE_SIZE, "GT%c%c%c\n",
+					data->product_id[0], data->product_id[1],
+					data->product_id[2]);
+		} else {
+			return scnprintf(buf, PAGE_SIZE, "GT%c%c%c%c\n",
+				data->product_id[0], data->product_id[1],
+				data->product_id[2], data->product_id[3]);
+		}
 	}
 }
 
@@ -1613,9 +1617,14 @@ static ssize_t gtp_buildid_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
 	struct goodix_ts_data *data = dev_get_drvdata(dev);
+	u16 default_version = 0;
 
-	return scnprintf(buf, PAGE_SIZE, "%04x\n",
-			data->version_info);
+	if (data->fw_error == 1) {
+		return scnprintf(buf, PAGE_SIZE, "%04x\n", default_version);
+	} else {
+		return scnprintf(buf, PAGE_SIZE, "%04x\n",
+				data->version_info);
+	}
 }
 
 static DEVICE_ATTR(buildid, S_IRUGO,
