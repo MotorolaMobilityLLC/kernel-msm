5b45f402706366f0790757b4d4684b6fdd3a96ef
lixp5 <lixp5@lenovo.com>
2018-04-04 12:01:16 +0800

    add sysfs for tcmd mydp audio test
    For tcmd to read mydp audio status
    
    Change-Id: I347571ec61365ab1e6336f2d264efd06495561ed
    Signed-off-by: lixp5 <lixp5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1157727
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Wei <weiweij@motorola.com>
    Submit-Approved: Wei Wei <weiweij@motorola.com>
    Tested-by: Wei Wei <weiweij@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/video/fbdev/slimport/anx7805/slimport_tx_drv.c
+++ drivers/video/fbdev/slimport/anx7805/slimport_tx_drv.c
@@ -157,6 +157,43 @@ int get_tx_system_state(void)
 	return sp_tx_system_state;
 }
 
+int get_tx_audio_state(void)
+{
+	BYTE c;
+	BYTE audio_status;
+	long int audio_M_val, audio_N_val;
+
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_M_VID_2, &c);
+	audio_M_val = c * 0x10000;
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_M_VID_1, &c);
+	audio_M_val = audio_M_val + c * 0x100;
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_M_VID_0, &c);
+	audio_M_val = audio_M_val + c;
+
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_N_VID_2, &c);
+	audio_N_val = c * 0x10000;
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_N_VID_1, &c);
+	audio_N_val = audio_N_val + c * 0x100;
+	sp_read_reg(SP_TX_PORT0_ADDR, AUDIO_N_VID_0, &c);
+	audio_N_val = audio_N_val + c;
+
+	pr_info("%s: Audio M = %lu, N = %lu\n", __func__, audio_M_val, audio_N_val);
+
+	sp_read_reg(SP_TX_PORT2_ADDR, SP_AUDIO_TX_STATUS, &c);
+	audio_status = c;
+	pr_info("%s: Audio Status = 0x%02x\n", __func__, (unsigned int)audio_status);
+
+	if ((audio_M_val == 0x05D6 || audio_M_val == 0x05D7)
+	  && (audio_status == 0x45 || audio_status == 0x46)
+	  && audio_N_val == 0x8000) {
+		pr_info("%s: audio status return 1\n", __func__);
+		return 1;
+	} else {
+		pr_info("%s: audio status return 0\n", __func__);
+		return 0;
+	}
+}
+
 void SP_TX_Initialization(struct VideoFormat* pInputFormat)
 {
 	BYTE c;
