20bd8c82a8825f1fbaab635427a20208dea71064
longch7 <niuxiaoyan@longcheer.com>
2018-04-10 18:54:59 +0800

    msm: mdss: add panel_reg_read_lp_enable flag
    Remark:
    add panel_reg_read_lp_enable flag. Some IC such as FOCAL FT8006U,
    read reg lp mode only
    Signed-off-by: longch7 <xinjunjie@longcheer.com>
    
    Change-Id: Iec3c9b80288ab01b14fcbb8ab96fe6feebb0e21f
    Reviewed-on: https://gerrit.mot.com/1160843
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Chu Sheng <niuxiaoyan@longcheer.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -714,6 +714,7 @@ struct mdss_panel_info {
 	u32 rst_seq[MDSS_DSI_RST_SEQ_LEN];
 	u32 rst_seq_len;
 	bool panel_off_rst_disable;
+	bool panel_reg_read_lp_enable;
 	u32 vic; /* video identification code */
 	struct mdss_rect roi;
 	int pwm_pmic_gpio;
