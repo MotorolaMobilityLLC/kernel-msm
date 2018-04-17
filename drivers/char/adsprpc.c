d187e4ef80aa68e17d7d38d26259d337af1b3326
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-04-17 17:53:40 +0530

    msm: adsprpc: destroy mutex before file free
    Destroy mutex before file free, to avoid use after free of mutex.
    
    Bug-id: A-119052960
    CRs-fixed: 2277735
    CVE-fixed: CVE-2018-11967
    Mot-CRs-fixed: (CR), (CR)
    
    Change-Id: I4ff73dc17b15043eacbb299219a379bfd1a8efa6
    Acked-by: Himateja Reddy <hmreddy@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>
    Signed-off-by: Srikanth A R <arsrikan@motorola.com>
    (cherry picked from commit 1e74efda1df55e33ed966155668caed20fbc4b39)
    Reviewed-on: https://gerrit.mot.com/1329677
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 79c9914f7ef240c75ffaf0cc5e857a5b7fe59dd6)

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1853,6 +1853,7 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	if (fl->ssrcount == fl->apps->channel[cid].ssrcount)
 		kref_put_mutex(&fl->apps->channel[cid].kref,
 				fastrpc_channel_close, &fl->apps->smd_mutex);
+	mutex_destroy(&fl->map_mutex);
 	kfree(fl);
 	return 0;
 }
@@ -2006,7 +2007,6 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 		}
 		me->pending_free++;
 		mutex_unlock(&me->flfree_mutex);
-		mutex_destroy(&fl->map_mutex);
 		file->private_data = NULL;
 	}
 bail:
