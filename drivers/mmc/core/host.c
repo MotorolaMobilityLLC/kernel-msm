c54b6ae5221342bf3609beecf302f17eb773f6a3
Lianwei Wang <a22439@motorola.com>
2018-05-15 09:22:47 +0800

    mmc: don't wait in pm notifier during card detection
    If the kernel is busy in sending mmc request, then the mmc card
    detection work will wait for the host lock for a long time. In
    such situation the pm notifier will wait a long time to flush the
    card detection work, which may trigger kernel panic.
    
    Give another 2s for others to release the host, and return
    -EAGAIN to abort the suspend process immediately to avoid kernel
    panic.
    
    Change-Id: I89806ba2e33b9cfe239911a0d9967678b7f2cc2a
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1025787
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1175239
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -717,6 +717,9 @@ again:
 	spin_lock_init(&host->lock);
 	init_waitqueue_head(&host->wq);
 	INIT_DELAYED_WORK(&host->detect, mmc_rescan);
+  #ifdef CONFIG_PM
+	wakeup_source_init(&host->pm_ws, dev_name(&host->class_dev));
+  #endif
 	setup_timer(&host->retune_timer, mmc_retune_timer, (unsigned long)host);
 
 	mutex_init(&host->rpmb_req_mutex);
@@ -1046,6 +1049,7 @@ EXPORT_SYMBOL(mmc_remove_host);
 void mmc_free_host(struct mmc_host *host)
 {
 	mmc_pwrseq_free(host);
+	wakeup_source_trash(&host->pm_ws);
 	put_device(&host->class_dev);
 }
 
