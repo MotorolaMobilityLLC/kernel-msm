2a35821e2c8f270dae998be971e3728d0a5252ea
yaozm1 <yaozm1@lenovo.com>
2018-05-25 14:56:01 +0800

    asoc:Fix AHB_TIMOUT for lpi gpio access failure
    Qcom KBA-170622023423：
    
    Tertiary and Quaternary MI2S interfaces use LPI GPIOs in SDM660.
    Accessing these LPI GPIOs when LPASS is in ISLAND state might result in
    AHB Timeout/ LPI GPIO read/write failures.
    
    Present approach of configuring tertiary/quaternary LPI GPIOs has pinctrl
    entries added in respective mi2s node. ASoC would call active/suspend of
    pinctrl at session start/end of session and hence there is possibility
    that these GPIOs might get accessed when LPI is in ISLAND mode resulting
    in playback issues(audio mute/playback noisy/AHB timeout etc).
    
    To Solve this, MI2S related LPI GPIOs need to be configured in machine
    driver startup(/shutdown) which would ensure that LPASS bitclock would
    be enabled and MCLK AFE call is successful before accessing LPI GPIOs.
    
    Propagated_From: (CR)
    
    Change-Id: I3b34c3f38fe2f9af1e678b8b7cb5f04dc5e3582f
    Signed-off-by: yaozm1 <yaozm1@lenovo.com>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1202885
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/msm/sdm660-common.h
+++ sound/soc/msm/sdm660-common.h
@@ -101,6 +101,9 @@ struct msm_asoc_mach_data {
 	struct device_node *comp_gpio_p; /* used by pinctrl API */
 	struct device_node *dmic_gpio_p; /* used by pinctrl API */
 	struct device_node *ext_spk_gpio_p; /* used by pinctrl API */
+#ifdef CONFIG_SND_SOC_TAS2560
+	struct device_node *tert_mi2s_gpio_p; /* used by pinctrl API*/
+#endif
 	struct snd_soc_codec *codec;
 	struct sdm660_codec sdm660_codec_fn;
 	struct snd_info_entry *codec_root;
