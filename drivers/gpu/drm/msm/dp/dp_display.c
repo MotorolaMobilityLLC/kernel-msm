unknownbaka/utopia_kernel_polaris

drm/msm/dp: limit the maximum lane count for multi-functional dongles
Some dongles which support multi-function return the lane count support
in DPCD to 4. But the link training fails for such dongles since all the
lanes do not support DisplayPort functionality. Limit the maximum lane
count for such dongles to 2.

Change-Id: I19e0fc8d914b4a1f1bedb680772b8f10a38240b1
Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

drivers/gpu/drm/msm/dp/dp_display.c
@@ -494,7 +494,8 @@ static int dp_display_process_hpd_high(struct dp_display_private *dp)
 	if (!dp->dp_display.connector)
 		return 0;
 
-	rc = dp->panel->read_sink_caps(dp->panel, dp->dp_display.connector);
+	rc = dp->panel->read_sink_caps(dp->panel,
+		dp->dp_display.connector, dp->usbpd->multi_func);
 	if (rc) {
 		if (rc == -ETIMEDOUT) {
 			pr_err("Sink cap read failed, skip notification\n");

MotorolaMobilityLLC/kernel-msm

Signed-off-by: Tatenda Chipeperekwa <tatendac@codeaurora.org>

drivers/gpu/drm/msm/dp/dp_display.c
@@ -544,8 +544,7 @@ static int dp_display_process_hpd_high(struct dp_display_private *dp)
 		return 0;
 
 	rc = dp->panel->read_sink_caps(dp->panel,
-			dp->dp_display.base_connector);
-
+			dp->dp_display.base_connector, dp->usbpd->multi_func);
 	if (rc) {
 		/*
 		 * ETIMEDOUT --> cable may have been removed