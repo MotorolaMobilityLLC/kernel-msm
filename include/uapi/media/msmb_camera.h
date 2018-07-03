a6c8f656983a8204cb3010eed7401c469cd57ae0
huangsh4 <huangsh4@motorola.com>
2018-07-03 14:13:07 +0800

    ocean: Port CCI Tool to sdm632 branch.
    Change-Id: I7ca56427e35af6a21ec818ac7167281223859d45
    Reviewed-on: https://gerrit.mot.com/1203265
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Zilong Zhang <zhangzl7@lenovo.com>
    Reviewed-by: Chuanxin Guo <guocx4@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- include/uapi/media/msmb_camera.h
+++ include/uapi/media/msmb_camera.h
@@ -57,6 +57,7 @@
 #define MSM_CAMERA_SUBDEV_EXT          (MSM_CAMERA_SUBDEV_BASE + 19)
 #define MSM_CAMERA_SUBDEV_TOF          (MSM_CAMERA_SUBDEV_BASE + 20)
 #define MSM_CAMERA_SUBDEV_LASER_LED    (MSM_CAMERA_SUBDEV_BASE + 21)
+#define MSM_CAMERA_SUBDEV_CCI_INTF     (MSM_CAMERA_SUBDEV_BASE + 22)
 #define MSM_MAX_CAMERA_SENSORS  5
 
 /* The below macro is defined to put an upper limit on maximum
