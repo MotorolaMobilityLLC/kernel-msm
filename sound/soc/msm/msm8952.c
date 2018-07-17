63adbd272cdb06efb418bea8184c62c824764437
Vitor Albuquerque <vitoralb@motorola.com>
2018-07-17 17:38:29 -0300

    ASoC: fix VOL_UP and media buttons triggering assistant
    Google defines impedance range 110-180 Ohms for
    VOICECOMMAND key. In qcom implementation the actual
    range that will trigger assitant is 90-195 Ohms.
    
    Some headsets and selfie sticks users are reporting
    assistant being triggered incorrectly.
    
    Since a dedicated assistant button is a much more rare
    usecase, we fix this issue by reducing the range associated with
    VOICECOMMAND from 90-195 to 110-180 to match CDD without
    any extra tolerance.
    
    With this change range 0-90 will trigger MEDIA key and
    180-325 will trigger VOLUME_UP key
    
    Change-Id: I9333fa9af22c87ad8a9a18f305553ab686b2911e
    Signed-off-by: Vitor Albuquerque <vitoralb@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1210819
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- sound/soc/msm/msm8952.c
+++ sound/soc/msm/msm8952.c
@@ -1728,10 +1728,10 @@ static void *def_msm8952_wcd_mbhc_cal(void)
 	 * 210-290 == Button 2
 	 * 360-680 == Button 3
 	 */
-	btn_low[0] = 75;
-	btn_high[0] = 75;
-	btn_low[1] = 150;
-	btn_high[1] = 150;
+	btn_low[0] = 88;
+	btn_high[0] = 88;
+	btn_low[1] = 138;
+	btn_high[1] = 138;
 	btn_low[2] = 225;
 	btn_high[2] = 225;
 	btn_low[3] = 450;
