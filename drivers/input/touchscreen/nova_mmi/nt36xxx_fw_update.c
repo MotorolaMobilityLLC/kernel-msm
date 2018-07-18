c11d0ac8251b1b4c149620c9364fc1558f3fdb65
dengwei1 <dengwei1@motorola.com>
2018-07-18 12:50:13 +0800

    jeter input: update nova touch firmware update rule
    Change-Id: Ic116a16ea8d49df8f0990f55c0221b97d8ec5d98
    Signed-off-by: dengwei1 <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1210951
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/nova_mmi/nt36xxx_fw_update.c
+++ drivers/input/touchscreen/nova_mmi/nt36xxx_fw_update.c
@@ -131,7 +131,7 @@ int32_t Check_FW_Ver(void)
 	}
 
 	// compare IC and binary FW version
-	if (buf[1] > fw_entry->data[FW_BIN_VER_OFFSET])
+	if (buf[1] == fw_entry->data[FW_BIN_VER_OFFSET])
 		return 1;
 	else
 		return 0;
