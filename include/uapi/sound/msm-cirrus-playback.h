8b99b4bfabe71d4e3f4d2a19899e5efaa13add44
Yoon (Seungyoon) Lee <w22905@motorola.com>
2018-07-19 20:28:33 -0500

    ASoC: qdsp6v2: update Cirrus playback driver
    Motorola doesn't use full Cirrus playback which has protection
    and enhancement on QDSP, but use Prince DSP for protection and
    QDSP for enhancement. Modified generic driver provided by Cirrus
    accordingly. Also Motorola kernel has QDSP AFE multi-instance
    enabled, therefore AFE APR messages structure is different.
    This portion is re-factored and afe ff-port-id is added to dt.
    
    Change-Id: I3990359b18e7ff7abc48cc9c4c129bd9b6953491
    Signed-off-by: Yoon (Seungyoon) Lee <w22905@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1212961
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- include/uapi/sound/msm-cirrus-playback.h
+++ include/uapi/sound/msm-cirrus-playback.h
@@ -9,61 +9,44 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#ifndef _UAPI_MSM_CIRRUS_SPK_PR_H
-#define _UAPI_MSM_CIRRUS_SPK_PR_H
+#ifndef _UAPI_MSM_CIRRUS_SPK_EN_H
+#define _UAPI_MSM_CIRRUS_SPK_EN_H
 
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
 
-#define CIRRUS_SP			0x10027053
-#define CIRRUS_SP_ENABLE		0x10002001
+#define CIRRUS_SE			0x1000A1AF
+#define CIRRUS_SE_ENABLE		0x10002001
+#define CIRRUS_SE_PARAM_ID_CTRL		0x10002002
 
-#define CRUS_MODULE_ID_TX		0x00000002
 #define CRUS_MODULE_ID_RX		0x00000001
 
 /*
- * CRUS_PARAM_RX/TX_SET_USECASE
+ * CRUS_PARAM_RX_SET_USECASE
  * 0 = Music Playback in firmware
  * 1 = VOICE Playback in firmware
  * 2 = Tuning file loaded using external
  * config load command
  *
  * uses crus_rx_run_case_ctrl for RX apr pckt
- * uses crus_single_data_ctrl for TX apr pckt
  *
  */
 #define CRUS_PARAM_RX_SET_USECASE	0x00A1AF02
-#define CRUS_PARAM_TX_SET_USECASE	0x00A1BF0A
 /*
- * CRUS_PARAM_RX/TX_SET_CALIB
- * Non-zero value to run speaker
- * calibration sequence
+ * CRUS_PARAM_RX_GET_USECASE
  *
- * uses crus_single_data_t apr pckt
+ *
  */
-#define CRUS_PARAM_RX_SET_CALIB		0x00A1AF03
-#define CRUS_PARAM_TX_SET_CALIB		0x00A1BF03
+#define CRUS_PARAM_RX_GET_USECASE	0x00A1AF0B
 /*
- * CRUS_PARAM_RX/TX_SET_EXT_CONFIG
+ * CRUS_PARAM_RX_SET_EXT_CONFIG
  * config string loaded from libfirmware
  * max of 7K parameters
  *
  * uses crus_external_config_t apr pckt
  */
 #define CRUS_PARAM_RX_SET_EXT_CONFIG	0x00A1AF05
-#define CRUS_PARAM_TX_SET_EXT_CONFIG	0x00A1BF08
-/*
- * CRUS_PARAM_RX_GET_TEMP
- * get current Temp and calibration data
- *
- * CRUS_PARAM_TX_GET_TEMP_CAL
- * get results of calibration sequence
- *
- * uses cirrus_cal_result_t apr pckt
- */
-#define CRUS_PARAM_RX_GET_TEMP		0x00A1AF07
-#define CRUS_PARAM_TX_GET_TEMP_CAL	0x00A1BF06
 /*
  * CRUS_PARAM_RX_SET_DELTA_CONFIG
  * load seamless transition config string
@@ -81,23 +64,17 @@
 
 #define CRUS_AFE_PARAM_ID_ENABLE	0x00010203
 
-#define SPK_PROT_IOCTL_MAGIC		'a'
+#define SPK_ENHA_IOCTL_MAGIC		'a'
 
-#define CRUS_SP_IOCTL_GET	_IOWR(SPK_PROT_IOCTL_MAGIC, 219, void *)
-#define CRUS_SP_IOCTL_SET	_IOWR(SPK_PROT_IOCTL_MAGIC, 220, void *)
-#define CRUS_SP_IOCTL_GET_CALIB	_IOWR(SPK_PROT_IOCTL_MAGIC, 221, void *)
-#define CRUS_SP_IOCTL_SET_CALIB	_IOWR(SPK_PROT_IOCTL_MAGIC, 222, void *)
+#define CRUS_SE_IOCTL_GET	_IOWR(SPK_ENHA_IOCTL_MAGIC, 219, void *)
+#define CRUS_SE_IOCTL_SET	_IOWR(SPK_ENHA_IOCTL_MAGIC, 220, void *)
 
-#define CRUS_SP_IOCTL_GET32		_IOWR(SPK_PROT_IOCTL_MAGIC, 219, \
+#define CRUS_SE_IOCTL_GET32		_IOWR(SPK_ENHA_IOCTL_MAGIC, 219, \
 	compat_uptr_t)
-#define CRUS_SP_IOCTL_SET32		_IOWR(SPK_PROT_IOCTL_MAGIC, 220, \
-	compat_uptr_t)
-#define CRUS_SP_IOCTL_GET_CALIB32	_IOWR(SPK_PROT_IOCTL_MAGIC, 221, \
-	compat_uptr_t)
-#define CRUS_SP_IOCTL_SET_CALIB32	_IOWR(SPK_PROT_IOCTL_MAGIC, 222, \
+#define CRUS_SE_IOCTL_SET32		_IOWR(SPK_ENHA_IOCTL_MAGIC, 220, \
 	compat_uptr_t)
 
-struct crus_sp_ioctl_header {
+struct crus_se_ioctl_header {
 	uint32_t size;
 	uint32_t module_id;
 	uint32_t param_id;
@@ -105,4 +82,4 @@ struct crus_sp_ioctl_header {
 	void *data;
 };
 
-#endif /* _UAPI_MSM_CIRRUS_SPK_PR_H */
+#endif /* _UAPI_MSM_CIRRUS_SPK_EN_H */
