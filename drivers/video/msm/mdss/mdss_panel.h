854a139e144853f417c6a90ecca2bb923ee8b4a3
longch5 <xinjunjie@longcheer.com>
2018-07-24 11:56:44 +0800

    msm/mdss:add tp rst sequence config
    Remark:
    add panel_tp_rst sequence config.
    Signed-off-by: longch5 <xinjunjie@longcheer.com>
    
    Change-Id: Iec0cdac7a37d26f4ee3bdeba59fe9c1babf6128c
    Reviewed-on: https://gerrit.mot.com/1214481
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit ce6c04f5df36a5c7e84cbc49eb449e4c9239923d)
    Reviewed-on: https://gerrit.mot.com/1218217
    Reviewed-by: Lu Lu <lulu2@lenovo.com>
    SLTApproved: Lu Lu <lulu2@lenovo.com>
    Submit-Approved: Lu Lu <lulu2@lenovo.com>
    Tested-by: Lu Lu <lulu2@lenovo.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -698,6 +698,7 @@ struct mdss_panel_info {
 	u32 out_format;
 	u32 rst_seq[MDSS_DSI_RST_SEQ_LEN];
 	u32 rst_seq_len;
+	u32 external_rst_gpio;
 	bool rst_disable;        /*add xinjunjie at 20180312*/
 	bool panel_reg_read_lp_enable;
 	u32 vic; /* video identification code */
