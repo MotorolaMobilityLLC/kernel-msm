70856ca337529c364664858af91bb737b7382c57
Wen Xie <xiewen3@motorola.com>
2018-07-27 10:40:57 +0800

    input: update sx9310 calibrate method
    When opening the reference channel,disable sensor
    and enable sensor to calibrate sensor.
    
    Change-Id: I58565cdb916d64f3335Defender48141b831af9f3695d
    Signed-off-by: Wen Xie <xiewen3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1216723
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Reviewed-by: Lijun Chen <chenlj2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/sx9310_triple.c
+++ drivers/input/misc/sx9310_triple.c
@@ -1024,11 +1024,18 @@ static void ps_notify_callback_work(struct work_struct *work)
 	input_top = pDevice->pbuttonInformation->input_top;
 	input_bottom = pDevice->pbuttonInformation->input_bottom;
 
-	LOG_INFO("Usb insert,going to force calibrate\n");
-	ret = write_register(this, SX9310_IRQSTAT_REG, 0xff);
-	if (ret < 0)
-		LOG_ERR(" Usb insert,calibrate cap sensor failed\n");
-
+	mutex_lock(&this->mutex);
+	if (mEnabled) {
+		LOG_INFO("Usb insert,going to force calibrate\n");
+		ret = write_register(this, SX9310_CPS_CTRL0_REG, 0x20);
+		if (ret < 0)
+			LOG_ERR(" Usb insert,disable cap sensor failed\n");
+		msleep(60);
+		ret = write_register(this, SX9310_CPS_CTRL0_REG, 0x2f);
+		if (ret < 0)
+			LOG_ERR(" Usb insert,enabel cap sensor failed\n");
+	}
+	mutex_unlock(&this->mutex);
 	input_report_abs(input_top, ABS_DISTANCE, 0);
 	input_sync(input_top);
 	input_report_abs(input_bottom, ABS_DISTANCE, 0);
