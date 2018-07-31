213ef6ad08940c1e6906c2b134131e2757abba90
Haijian Ma <mahj8@motorola.com>
2018-07-31 14:19:07 +0800

    power: smblib: Add Age
    Make the Battery Age available.
    
    Porting from
    https://gerrit.mot.com/993904
    https://gerrit.mot.com/#/c/993903/
    
    Change-Id: If726235a24b3d287fa34c4c30605cf0ce81c11a6
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1218037
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -315,6 +315,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CC_SOC,
 	POWER_SUPPLY_PROP_QG_VBMS_MODE,
 	POWER_SUPPLY_PROP_REAL_CAPACITY,
+	POWER_SUPPLY_PROP_AGE,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
