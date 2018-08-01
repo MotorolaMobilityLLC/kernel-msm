8d6746c8f3cff0834e64f86acc91e8a8b10a5f87
Haijian Ma <mahj8@motorola.com>
2018-08-01 19:16:47 +0800

    qpnp-smb5: fix FV and FCC reporting
    Currently the read of battery's voltage_max property (FV) and battery's
    current_max property (FCC) returns the value of DefenderAULT_VOTER voter.
    
    But such a voter doesn't exist on battery's voltage_max property. Fix it
    by returning the value voted by BATT_PROFILE_VOTER.
    
    Porting from I2dac98f2ce9a423d5045788dbe120591f10c5a9e
    
    Change-Id: Ib5ca17e1b22d8331a4b0f897655c02f329ad272f
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1219037
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1378,14 +1378,16 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 				POWER_SUPPLY_PROP_VOLTAGE_NOW, val);
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
-		val->intval = get_effective_result(chg->fv_votable);
+		val->intval = get_client_vote(chg->fv_votable,
+				BATT_PROFILE_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 		rc = smblib_get_prop_from_bms(chg,
 				POWER_SUPPLY_PROP_CURRENT_NOW, val);
 		break;
 	case POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX:
-		val->intval = get_effective_result(chg->fcc_votable);
+		val->intval = get_client_vote(chg->fcc_votable,
+					      BATT_PROFILE_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_TERM_CURRENT:
 		rc = smb5_get_prop_batt_iterm(chg, val);
