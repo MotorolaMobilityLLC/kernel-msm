7baf2357432c36411b007baa858a467a781971f6
xizx <xizx@motorola.com>
2018-08-02 17:17:10 +0800

    ION:move to the node page api for ion pool
    Change-Id: I6d05506167fcecf43e1beffcd0d571555e49981b
    Signed-off-by: xizx <xizx@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1219723
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bingqian Yang <yangbq1@mt.com>
    Submit-Approved: Jira Key

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -73,8 +73,8 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    (1 << (PAGE_SHIFT + pool->order)));
-	mod_zone_page_state(page_zone(page), NR_FILE_PAGES, page_count);
-	mod_zone_page_state(page_zone(page), NR_INACTIVE_FILE, page_count);
+	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, page_count);
+	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, page_count);
 
 	mutex_unlock(&pool->mutex);
 	return 0;
@@ -100,8 +100,8 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    -(1 << (PAGE_SHIFT + pool->order)));
 
-	mod_zone_page_state(page_zone(page), NR_INACTIVE_FILE, -page_count);
-	mod_zone_page_state(page_zone(page), NR_FILE_PAGES, -page_count);
+	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, -page_count);
+	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, -page_count);
 
 	return page;
 }
