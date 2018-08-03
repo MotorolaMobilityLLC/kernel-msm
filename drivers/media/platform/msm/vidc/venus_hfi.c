e6ed34224e6ee3f8577007013ab452623caf3fe1
Paras Nagda <pnagda@codeaurora.org>
2018-08-03 19:31:27 +0530

    msm: vidc: remove redundant init_completion() calls
    Video driver is doing init_completion() for all completion
    events in msm_vidc_initialize_core() and msm_vidc_open().
    So there is no need of calling init_completion() again before
    waiting for completion event and hence remove them.
    
    Mot-CRs-fixed:(CR)
    
    
    
    
    Reviewed-on: https://gerrit.mot.com/1231370
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit d74c0f65a8ca9986d5ccba6cb9b4ef40ac03fab7)
    
    Change-Id: I40177bb6f9c8cb5c72d5bb03d05d74e5f33c04cd
    Reviewed-on: https://gerrit.mot.com/1240861
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Shiya Fu <fusy1@motorola.com>
    SLTApproved: Shiya Fu <fusy1@motorola.com>
    Reviewed-by: Master Yoda

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -937,8 +937,6 @@ static int __core_set_resource(struct venus_hfi_device *device,
 err_create_pkt:
 	return rc;
 }
-
-static DECLARE_COMPLETION(release_resources_done);
 
 static int __alloc_imem(struct venus_hfi_device *device, unsigned long size)
 {
@@ -2173,8 +2171,6 @@ static int venus_hfi_core_init(void *device)
 
 	dev = device;
 	mutex_lock(&dev->lock);
-
-	init_completion(&release_resources_done);
 
 	rc = __load_fw(dev);
 	if (rc) {
@@ -3485,7 +3481,6 @@ static int __response_handler(struct venus_hfi_device *device)
 			break;
 		case HAL_SYS_RELEASE_RESOURCE_DONE:
 			dprintk(VIDC_DBG, "Received SYS_RELEASE_RESOURCE\n");
-			complete(&release_resources_done);
 			break;
 		case HAL_SYS_INIT_DONE:
 			dprintk(VIDC_DBG, "Received SYS_INIT_DONE\n");
