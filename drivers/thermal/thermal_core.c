d9af2428e1e896df7dd645bf87d204e5b78a04dd
zoujc <zoujc@motorola.com>
2018-08-13 14:38:29 +0800

    stable:thermal core sync exit
    Fix the crash issue:
    1. Fix the double free kthread,
    2. Fix free sensor pointer what's using.
    <4>[  687.306284,2] [<ffffff93f9ef8894>] prepare_to_wait_event+0x100/0x118
    <4>[  687.306297,2] [<ffffff93faef54c8>] sensor_sysfs_notify+0xe8/0x11c
    <4>[  687.306306,2] [<ffffff93f9ec6b34>] kthread+0xf0/0x104
    <4>[  687.306316,2] [<ffffff93f9e830f0>] ret_from_fork+0x10/0x20
    <0>[  687.306325,2] Code: f9000080 17ffffe3 f9400684 91002283 (f9000480)
    <4>[  687.306333,2] ---[ end trace 54bcf6ade3f10044 ]---
    
    Change-Id: Iffe7785294c0d66925ee3cc91c601c6d534f201a
    Signed-off-by: zoujc <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1224936
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -380,21 +380,32 @@ static __ref int sensor_sysfs_notify(void *data)
 {
 	int ret = 0;
 	struct sensor_info *sensor = (struct sensor_info *)data;
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
+	sched_setscheduler(current, SCHED_RR, &param);
 
+	set_current_state(TASK_INTERRUPTIBLE);
 	while (!kthread_should_stop()) {
 		if (wait_event_interruptible(
 		   sensor->sysfs_notify_complete.wait,
 		   sensor->sysfs_notify_complete.done || kthread_should_stop()))
 			continue;
-		if (sensor->deregister_active)
-			return ret;
 
 		if (kthread_should_stop())
 			break;
 
+		set_current_state(TASK_RUNNING);
+		/*  Wait the kthread_should_stop to exit,
+		 *  avoid the crash from double free thread or sensor freed.
+		*/
+		if (sensor->deregister_active) {
+			set_current_state(TASK_INTERRUPTIBLE);
+			continue;
+		}
+
 		reinit_completion(&sensor->sysfs_notify_complete);
 		sysfs_notify(&sensor->tz->device.kobj, NULL,
 					THERMAL_UEVENT_DATA);
+		set_current_state(TASK_INTERRUPTIBLE);
 	}
 	return ret;
 }
@@ -2481,6 +2492,7 @@ void thermal_zone_device_unregister(struct thermal_zone_device *tz)
 	flush_work(&tz->sensor.work);
 	tz->sensor.deregister_active = true;
 	complete(&tz->sensor.sysfs_notify_complete);
+	kthread_stop(tz->sensor.sysfs_notify_thread);
 	mutex_lock(&thermal_list_lock);
 	list_del_rcu(&tz->sensor.sensor_list);
 	mutex_unlock(&thermal_list_lock);
