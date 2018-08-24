04c98a1dc1e19c00eda1cd933a45956be1b6a7d9
Shiyong Li <a22381@motorola.com>
2018-08-24 16:18:03 -0700

    sdcardfs: reserve 8 MB for non-media storage
    As media storage (/data/media) is just part of data partition, reserve
    space on non-media storage for the critical programs (e.g. bug2go)
    that don't belong to GID AID_RESERVED_DISK. Set it as 8 MB for now.
    
    P introduced a feature to reserve space for critical system components
    with GID AID_RESERVED_DISK, which ensures system is usable for the user
    to get chance to free up disk space used by abusive apps.
    
    This change is also helpful to reserve some space to avoid the case
    of abusively creating 0 size file (file name will occupy space) to eat
    up storage gradually.
    
    Change-Id: Id60ceece4726e4775db305d0fae24ffd3929001b
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1233103
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zonghua Liu <a17671@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -70,8 +70,8 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	opts->multiuser = false;
 	opts->fs_user_id = 0;
 	vfsopts->gid = 0;
-	/* by default, 0MB is reserved */
-	opts->reserved_mb = 0;
+	/* by default, 8 MB is reserved */
+	opts->reserved_mb = 8;
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
 	opts->default_normal = false;
