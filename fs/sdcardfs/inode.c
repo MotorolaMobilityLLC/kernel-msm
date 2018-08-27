b1ac59c2ad54c1ecd1289b7999b6462098eab4c8
Shiyong Li <a22381@motorola.com>
2018-08-27 13:09:54 -0700

    sdcardfs: check the free space before creating files
    User can create zero byte files even sdcardfs hits the reserved limit
    that will consume blocks for the file name
    and entually use up all memeory
    Check the avaliable free space before creating files.
    
    Change-Id: Ib902844f2e6e6c638184895e3f4fa7282acc8333
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1232981
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -63,7 +63,7 @@ static int sdcardfs_create(struct inode *dir, struct dentry *dentry,
 			 umode_t mode, bool want_excl)
 {
 	int err;
-	struct dentry *lower_dentry;
+	struct dentry *lower_dentry, *parent_dentry;
 	struct vfsmount *lower_dentry_mnt;
 	struct dentry *lower_parent_dentry = NULL;
 	struct path lower_path;
@@ -75,6 +75,15 @@ static int sdcardfs_create(struct inode *dir, struct dentry *dentry,
 		err = -EACCES;
 		goto out_eacces;
 	}
+
+	parent_dentry = dget_parent(dentry);
+	if (!check_min_free_space(parent_dentry, 0, 1)) {
+		pr_err("sdcardfs: No minimum free space.\n");
+		err = -ENOSPC;
+		dput(parent_dentry);
+		goto out_eacces;
+	}
+	dput(parent_dentry);
 
 	/* save current_cred and override it */
 	saved_cred = override_fsids(SDCARDFS_SB(dir->i_sb),
