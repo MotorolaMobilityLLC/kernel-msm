9aa7260d9607ce3d0359fdb8a928cf7d9021961b
houdz1 <houdz1@motorola.com>
2018-08-29 14:21:48 +0800

    mdss_smmu: dump xlog when mdss_smmu run into fault
    Give change to copy xlog when mdss_smmu_fault_handler happens.
    Cherry Pick from https://gerrit.mot.com/#/c/1232921
    
    Change-Id: I5d2c0895eed60804cc669f169854723f78ea47b1
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1233152
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/video/fbdev/msm/mdss_smmu.c
+++ drivers/video/fbdev/msm/mdss_smmu.c
@@ -517,7 +517,10 @@ int mdss_smmu_fault_handler(struct iommu_domain *domain, struct device *dev,
 	} else {
 		pr_err("mdss_smmu: iova:0x%lx flags:0x%x\n",
 			iova, flags);
-		MDSS_XLOG_TOUT_HANDLER("mdp");
+		MDSS_XLOG_TOUT_HANDLER_MMI("mdp", "dsi0_ctrl", "dsi0_phy",
+		    "dsi1_ctrl", "dsi1_phy", "vbif", "vbif_nrt",
+		    "dbg_bus", "vbif_dbg_bus",
+		    "dsi_dbg_bus");
 	}
 end:
 	return -ENODEV;
