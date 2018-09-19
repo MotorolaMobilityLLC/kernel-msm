7c9dd2ee09d3e7c754945c09ac966af5e00a8510
Haijian Ma <mahj8@motorola.com>
2018-09-19 16:59:09 +0800

    power: smb5: Add Factory Commands.
    Add the Factory commands and the lockout for Factory mode.
    
    refer to https://gerrit.mot.com/920848
    
    Change-Id: I8e18ba97d12c734251ea75cb4cac27d0e02cc8e3
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1245606
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Master Yoda
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -349,6 +349,11 @@ struct smb_iio {
 	struct iio_channel	*smb_temp_chan;
 };
 
+struct mmi_params {
+	bool			factory_mode;
+	bool			demo_mode;
+};
+
 struct smb_charger {
 	struct device		*dev;
 	char			*name;
@@ -534,6 +539,10 @@ struct smb_charger {
 
 	/* wireless */
 	int			wireless_vout;
+
+	/* mmi based params */
+	/* Place at end of struct smb_charger as it grows */
+	struct mmi_params	mmi;
 };
 
 int smblib_read(struct smb_charger *chg, u16 addr, u8 *val);
@@ -725,4 +734,7 @@ int smblib_force_vbus_voltage(struct smb_charger *chg, u8 val);
 
 int smblib_init(struct smb_charger *chg);
 int smblib_deinit(struct smb_charger *chg);
+
+void mmi_init(struct smb_charger *chg);
+void mmi_deinit(struct smb_charger *chg);
 #endif /* __SMB5_CHARGER_H */
