a20d54fa07c2a66b72f9f36bed0e3ec55112c011
Haijian Ma <mahj8@motorola.com>
2018-09-22 10:13:56 +0800

    power: smb5: Implement Factory Kill
    Implement Factory kill to power off the device when USBIN is
    removed in factory mode.
    
    refer to  https://gerrit.mot.com/#/c/937858/
    
    Change-Id: Ia4ff0a7dffb9c5addd2c04c9865b537977ea4e9c
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1246116
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -5157,6 +5157,8 @@ void smblib_usb_plugin_hard_reset_locked(struct smb_charger *chg)
 }
 
 #define PL_DELAY_MS	30000
+static int factory_kill_disable;
+module_param(factory_kill_disable, int, 0644);
 void smblib_usb_plugin_locked(struct smb_charger *chg)
 {
 	int rc;
@@ -5196,6 +5198,9 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 		vote(chg->awake_votable, PL_DELAY_VOTER, true, 0);
 		schedule_delayed_work(&chg->pl_enable_work,
 					msecs_to_jiffies(PL_DELAY_MS));
+
+		if (chg->mmi.factory_mode)
+			chg->mmi.factory_kill_armed = true;
 	} else {
 		/* Disable SW Thermal Regulation */
 		rc = smblib_set_sw_thermal_regulation(chg, false);
@@ -5253,6 +5258,12 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 			smblib_err(chg, "Couldn't disable DPDM rc=%d\n", rc);
 
 		smblib_update_usb_type(chg);
+
+		if (chg->mmi.factory_kill_armed && !factory_kill_disable) {
+			smblib_err(chg, "Factory kill power off\n");
+			kernel_power_off();
+		} else
+			chg->mmi.factory_kill_armed = false;
 	}
 
 	if (chg->connector_type == POWER_SUPPLY_CONNECTOR_MICRO_USB)
@@ -7770,8 +7781,6 @@ int smblib_deinit(struct smb_charger *chg)
 
 static struct smb_charger *mmi_chip;
 
-static int factory_kill_disable;
-module_param(factory_kill_disable, int, 0644);
 static int smbchg_reboot(struct notifier_block *nb,
 			 unsigned long event, void *unused)
 {
