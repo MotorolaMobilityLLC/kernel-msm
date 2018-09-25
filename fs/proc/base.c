08de995ec60515acac98f475513c3f66e52fc225
zhaoxp3 <zhaoxp3@motorola.com>
2018-09-25 13:26:27 +0800

    lowmemorykiller: 32bit lmk change
    tune_lmk_para is not supported on moto 32bit system
    porting task's adj tree implementation
    
    Change-Id: Icd5fc4b5e57c6f688064de0af3b81a58c983ebe2
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1245964
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhangqing Huang <huangzq2@motorola.com>
    Reviewed-by: Guolin Wang <wanggl3@mt.com>
    Submit-Approved: Jira Key

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1102,7 +1102,13 @@ static int __set_oom_adj(struct file *file, int oom_adj, bool legacy)
 		}
 	}
 
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	delete_from_adj_tree(task);
+#endif
 	task->signal->oom_score_adj = oom_adj;
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	add_2_adj_tree(task);
+#endif
 	if (!legacy && has_capability_noaudit(current, CAP_SYS_RESOURCE))
 		task->signal->oom_score_adj_min = (short)oom_adj;
 	trace_oom_score_adj_update(task);
@@ -1125,7 +1131,13 @@ static int __set_oom_adj(struct file *file, int oom_adj, bool legacy)
 						task_pid_nr(p), p->comm,
 						p->signal->oom_score_adj, oom_adj,
 						task_pid_nr(task), task->comm);
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+				delete_from_adj_tree(task);
+#endif
 				p->signal->oom_score_adj = oom_adj;
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+				add_2_adj_tree(task);
+#endif
 				if (!legacy && has_capability_noaudit(current, CAP_SYS_RESOURCE))
 					p->signal->oom_score_adj_min = (short)oom_adj;
 			}
