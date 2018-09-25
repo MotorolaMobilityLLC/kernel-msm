7f881386eb7f16ee2a71cc0bd0a6b8e590b84efd
zhaoxp3 <zhaoxp3@motorola.com>
2018-09-25 13:26:27 +0800

    lowmemorykiller: 32bit lmk change
    tune_lmk_para is not supported on moto 32bit system
    porting task's adj tree implementation
    
    Change-Id: Icd5fc4b5e57c6f688064de0af3b81a58c983ebe2
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1245964
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhangqing Huang <huangzq2@motorola.com>
    Reviewed-by: Guolin Wang <wanggl3@lenovo.com>
    Submit-Approved: Jira Key

--- kernel/fork.c
+++ kernel/fork.c
@@ -1395,6 +1395,9 @@ static int copy_signal(unsigned long clone_flags, struct task_struct *tsk)
 	sig->oom_score_adj = current->signal->oom_score_adj;
 	sig->oom_score_adj_min = current->signal->oom_score_adj_min;
 
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	RB_CLEAR_NODE(&sig->adj_node);
+#endif
 	sig->has_child_subreaper = current->signal->has_child_subreaper ||
 				   current->signal->is_child_subreaper;
 
@@ -1984,6 +1987,9 @@ static __latent_entropy struct task_struct *copy_process(
 			p->signal->tty = tty_kref_get(current->signal->tty);
 			list_add_tail(&p->sibling, &p->real_parent->children);
 			list_add_tail_rcu(&p->tasks, &init_task.tasks);
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+			add_2_adj_tree(p);
+#endif
 			attach_pid(p, PIDTYPE_PGID);
 			attach_pid(p, PIDTYPE_SID);
 			__this_cpu_inc(process_counts);
