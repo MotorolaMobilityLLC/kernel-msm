d2dd3690172a605a94ae4b901d167d279b71241b
Weilong Wu <wuwl10@motorola.com>
2018-09-25 10:43:07 +0800

    fingerprint: add goodix FOD driver
    Foles use FOD Tec.
    Add driver for Goodix GW9518 FOD solution.
    
    Change-Id: I71e7ab6eb1eb300107cd0e1e4dc54a85b02926ef
    Signed-off-by: Weilong Wu <wuwl10@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1245929
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

---
+++ drivers/input/misc/fingerprint/platform.c
@@ -0,0 +1,147 @@
+/*
+ * platform indepent driver interface
+ * Copyright (C) 2016 Goodix
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <linux/delay.h>
+#include <linux/workqueue.h>
+#include <linux/of_gpio.h>
+#include <linux/gpio.h>
+#include <linux/regulator/consumer.h>
+#include <linux/timer.h>
+#include <linux/err.h>
+
+#include "gf_spi.h"
+
+#if defined(USE_SPI_BUS)
+#include <linux/spi/spi.h>
+#include <linux/spi/spidev.h>
+#elif defined(USE_PLATFORM_BUS)
+#include <linux/platform_device.h>
+#endif
+
+int gf_parse_dts(struct gf_dev *gf_dev)
+{
+	int rc = 0;
+	struct device *dev = &gf_dev->spi->dev;
+	struct device_node *np = dev->of_node;
+
+
+	gf_dev->pwr_gpio = of_get_named_gpio(np, "fp-gpio-ven", 0);
+	if (gf_dev->pwr_gpio < 0) {
+		pr_err("failed to get pwr gpio!\n");
+		return gf_dev->pwr_gpio;
+	}
+
+	rc = devm_gpio_request(dev, gf_dev->pwr_gpio, "goodix_pwr");
+	if (rc) {
+		pr_err("failed to request pwr gpio, rc = %d\n", rc);
+		goto err_pwr;
+	}
+	gpio_direction_output(gf_dev->pwr_gpio, 1);
+
+	gf_dev->reset_gpio = of_get_named_gpio(np, "fp-gpio-reset", 0);
+	if (gf_dev->reset_gpio < 0) {
+		pr_err("failed to get reset gpio!\n");
+		return gf_dev->reset_gpio;
+	}
+
+	rc = devm_gpio_request(dev, gf_dev->reset_gpio, "goodix_reset");
+	if (rc) {
+		pr_err("failed to request reset gpio, rc = %d\n", rc);
+		goto err_reset;
+	}
+	gpio_direction_output(gf_dev->reset_gpio, 1);
+
+	gf_dev->irq_gpio = of_get_named_gpio(np, "fp-gpio-irq", 0);
+	if (gf_dev->irq_gpio < 0) {
+		pr_err("failed to get irq gpio!\n");
+		return gf_dev->irq_gpio;
+	}
+
+	rc = devm_gpio_request(dev, gf_dev->irq_gpio, "goodix_irq");
+	if (rc) {
+		pr_err("failed to request irq gpio, rc = %d\n", rc);
+		goto err_irq;
+	}
+	gpio_direction_input(gf_dev->irq_gpio);
+
+err_irq:
+	devm_gpio_free(dev, gf_dev->reset_gpio);
+err_reset:
+err_pwr:
+	return rc;
+}
+
+void gf_cleanup(struct gf_dev *gf_dev)
+{
+	pr_info("[info] %s\n", __func__);
+
+	if (gpio_is_valid(gf_dev->irq_gpio)) {
+		gpio_free(gf_dev->irq_gpio);
+		pr_info("remove irq_gpio success\n");
+	}
+	if (gpio_is_valid(gf_dev->reset_gpio)) {
+		gpio_free(gf_dev->reset_gpio);
+		pr_info("remove reset_gpio success\n");
+	}
+}
+
+int gf_power_on(struct gf_dev *gf_dev)
+{
+	int rc = 0;
+
+	if (!gf_dev->power_enabled) {
+		gf_dev->power_enabled = 1;
+		/* TODO: add your power control here */
+	}
+
+	return rc;
+}
+
+int gf_power_off(struct gf_dev *gf_dev)
+{
+	int rc = 0;
+
+	if (gf_dev->power_enabled) {
+		gf_dev->power_enabled = 0;
+		/* TODO: add your power control here */
+	}
+
+	return rc;
+}
+
+int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms)
+{
+	if (!gf_dev) {
+		pr_err("Input buff is NULL.\n");
+		return -ENODEV;
+	}
+	gpio_direction_output(gf_dev->reset_gpio, 1);
+	gpio_set_value(gf_dev->reset_gpio, 0);
+	mdelay(3);
+	gpio_set_value(gf_dev->reset_gpio, 1);
+	mdelay(delay_ms);
+	return 0;
+}
+
+int gf_irq_num(struct gf_dev *gf_dev)
+{
+	if (!gf_dev) {
+		pr_err("Input buff is NULL.\n");
+		return -ENODEV;
+	} else {
+		return gpio_to_irq(gf_dev->irq_gpio);
+	}
+}
+
