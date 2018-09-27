a406fd020fff5780f28850f9952d90d70d17a44e
Paras Nagda <pnagda@codeaurora.org>
2018-09-27 19:58:38 +0530

    msm: vidc: Fix dead lock during long stability run
    During long run stability there is a possibility
    that first thread (T1) which has acquired sync lock
    during streamon, is waiting for ctrl lock to be
    released by second thread (T2) and second thread (T2)
    while performing a set ctrl, has already acquired ctrl lock
    and is waiting for sync lock to be released by T1 there
    by creating a deadlock condition.
    
    With this change, ctrl lock is avoided by keeping the
    operating rate as part of instance variable and not by
    getting it from the control directly.
    
    QC SR: 03717252
    CRs-Fixed: 2319109
    Moto-CRs-Fixed: (CR)
    
    Change-Id: Ie5e3dbcb9007fb2961c8a01f292fcce193f33c15
    Signed-off-by: Paras Nagda <pnagda@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1261841
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/vidc_3x/msm_venc.c
+++ drivers/media/platform/msm/vidc_3x/msm_venc.c
@@ -3627,6 +3627,16 @@ static int try_set_ctrl(struct msm_vidc_inst *inst, struct v4l2_ctrl *ctrl)
 		pdata = &enable;
 		break;
 	case V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE:
+		if ((ctrl->val >> 16) < inst->capability.frame_rate.min ||
+			(ctrl->val >> 16) > inst->capability.frame_rate.max) {
+			dprintk(VIDC_ERR, "Invalid operating rate %u\n",
+				(ctrl->val >> 16));
+			rc = -ENOTSUPP;
+		} else {
+			dprintk(VIDC_DBG, "inst(%pK) operating rate %d\n",
+				inst, ctrl->val >> 16);
+			inst->prop.operating_rate = ctrl->val;
+		}
 		break;
 	case V4L2_CID_MPEG_VIDC_VIDEO_VENC_BITRATE_TYPE:
 	{
@@ -4067,6 +4077,7 @@ int msm_venc_inst_init(struct msm_vidc_inst *inst)
 	inst->buffer_mode_set[OUTPUT_PORT] = HAL_BUFFER_MODE_STATIC;
 	inst->buffer_mode_set[CAPTURE_PORT] = HAL_BUFFER_MODE_STATIC;
 	inst->prop.fps = DEFAULT_FPS;
+	inst->prop.operating_rate = 0;
 	inst->capability.pixelprocess_capabilities = 0;
 	memcpy(&inst->fmts[CAPTURE_PORT], &venc_formats[4],
 						sizeof(struct msm_vidc_format));
