b4fe1317cbf14bb97ce9dc42ee5a2938adf6b209
Bang Nguyen <bangnguyen@motorola.com>
2018-10-03 17:19:43 +0800

    drm/msm/dsi-staGing: add prints for DSI/panel turning off/on
    Add some pr_info to indicate the DSI and panel are turning off
    and on for ESD or black screen investigation.
    
    Change-Id: I2a794de59b7099dfcc28038d62037631d4a53c7a
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1249790
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -6812,6 +6812,7 @@ int dsi_display_prepare(struct dsi_display *display)
 	}
 
 	SDE_EVT32(SDE_EVTLOG_FUNC_ENTRY);
+	pr_info("%s(%s)+\n", __func__, display->drm_conn->name);
 	mutex_lock(&display->display_lock);
 
 	mode = display->panel->cur_mode;
@@ -7367,6 +7368,7 @@ int dsi_display_disable(struct dsi_display *display)
 	}
 
 	SDE_EVT32(SDE_EVTLOG_FUNC_ENTRY);
+	pr_info("%s(%s)+\n", __func__, display->drm_conn->name);
 	mutex_lock(&display->display_lock);
 
 	rc = dsi_display_wake_up(display);
