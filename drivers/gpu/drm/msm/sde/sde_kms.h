64c8922ba157a581d22153647cb5646d0d11c4f0
Bang Nguyen <bangnguyen@motorola.com>
2018-10-03 17:21:32 +0800

    drm/msm/sde: Change display ESD checking interval to 8s
    Change the ESD detection ,
    and recovery interval to 8 second.
    
    Change-Id: Ib3b71e664a24bdb3df76df8eaf3d66e6c4b0a758
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1249854
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/sde/sde_kms.h
+++ drivers/gpu/drm/msm/sde/sde_kms.h
@@ -123,7 +123,7 @@
 #define SDE_KMS_OPS_PREPARE_PLANE_FB		BIT(3)
 
 /* ESD status check interval in miliseconds */
-#define STATUS_CHECK_INTERVAL_MS 5000
+#define STATUS_CHECK_INTERVAL_MS 8000
 
 /**
  * enum sde_kms_smmu_state:	smmu state
