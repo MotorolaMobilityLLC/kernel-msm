9cf240e564a7af3a213278311252de2429126fd6
Bang Nguyen <bangnguyen@motorola.com>
2018-10-03 18:08:46 +0800

    drm/msm/sde: Add DispUtil tool to motUtil Debugfs
    Add the Display Utility(DispUtil) support to the motUtil Debugfs Framework.
    This dispUtil tool will send a command from the motUtil debugfs to the panel
    or it can read any information from panel back.
    The 1st byte (set to 0) will indicate for the displUtil tool.
    
    DispUtil Format:
    
    1st         2nd         3rd    4th  5th       6th
    0=DispUtil  0=Pri Disp 0=write 0=HS 0=DCS     number byte to read
                1=Sec Disp 1=read  1=LP 1=Generic
    
    7th        8th         9th....
    payldLen_u payldLen_l  payload
    
    Change-Id: Ibea6ba630bab543d9751870f192f0c5773b037f4
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1249871
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/sde/sde_connector.h
+++ drivers/gpu/drm/msm/sde/sde_connector.h
@@ -22,6 +22,7 @@
 #include "msm_prop.h"
 #include "sde_kms.h"
 #include "sde_fence.h"
+#include "sde_motUtil.h"
 
 #define SDE_CONNECTOR_NAME_SIZE	16
 
@@ -256,6 +257,18 @@ struct sde_connector_ops {
 	int (*cmd_transfer)(struct drm_connector *connector,
 			void *display, const char *cmd_buf,
 			u32 cmd_buf_len);
+
+	/**
+	 * motUtil_transfer - Convert motUtil data and Transfer command
+	 * 			to the connected display panel
+	 * @display: Pointer to private display handle
+	 * @cmd_buf: Command buffer
+	 * @cmd_buf_len: Command buffer length in bytes
+	 * @motUtil_data: motUtil data information
+	 * Returns: Zero for success, negetive for failure
+	 */
+	int (*motUtil_transfer)(void *display, const char *cmd_buf,
+			u32 cmd_buf_len, struct motUtil *motUtil_data);
 
 	/**
 	 * config_hdr - configure HDR
