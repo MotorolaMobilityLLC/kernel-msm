f58d54875bad6e5cb84dbc3f07eb1d9d3092e8f5
Bang Nguyen <bangnguyen@motorola.com>
2018-10-03 18:08:46 +0800

    drm/msm/sde: Add DispUtil tool to motUtil Debugfs
    Add the Display Utility(DispUtil) support to the motUtil Debugfs Framework.
    This dispUtil tool will send a command from the motUtil debugfs to the panel
    or it can read any information from panel back.
    The 1st byte (set to 0) will indicate for the displUtil tool.
    
    DispUtil Format:
    
    1st         2nd         3rd    4th  5th       6th
    0=DispUtil  0=Pri Disp 0=write 0=HS 0=DCS     number byte to read
                1=Sec Disp 1=read  1=LP 1=Generic
    
    7th        8th         9th....
    payldLen_u payldLen_l  payload
    
    Change-Id: Ibea6ba630bab543d9751870f192f0c5773b037f4
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1339874
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/sde/sde_motUtil.h
+++ drivers/gpu/drm/msm/sde/sde_motUtil.h
@@ -19,6 +19,19 @@
 #include <linux/types.h>
 
 enum sde_motUtil_type {
+        MOTUTIL_DISP_UTIL = 0,
+};
+
+enum sde_motUtil_disp_cmd {
+	DISPUTIL_TYPE = 0,
+	DISPUTIL_PANEL_TYPE,
+	DISPUTIL_CMD_TYPE,
+	DISPUTIL_CMD_XFER_MODE,
+	DISPUTIL_MIPI_CMD_TYPE,
+	DISPUTIL_NUM_BYTE_RD,
+	DISPUTIL_PAYLOAD_LEN_U,
+	DISPUTIL_PAYLOAD_LEN_L,
+	DISPUTIL_PAYLOAD,
 };
 
 struct motUtil {
@@ -31,6 +44,18 @@ struct motUtil {
 	int val;
 	bool te_enable;
 };
+
+#define DISPUTIL_DSI_WRITE      0
+#define DISPUTIL_DSI_READ       1
+
+#define DISPUTIL_DSI_DCS        0
+#define DISPUTIL_DSI_GENERIC    1
+
+#define DISPUTIL_DSI_HS_MODE    0
+#define DISPUTIL_DSI_LP_MODE    1
+
+#define MOTUTIL_MAIN_DISP	0
+#define MOTUTIL_CLI_DISP	1
 
 int sde_debugfs_mot_util_init(struct sde_kms *sde_kms,
 			struct dentry *parent);
