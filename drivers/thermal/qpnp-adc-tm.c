869f7e6e86837f92ea01557d29fc09764c668c35
zoujc <zoujc@motorola.com>
2018-10-10 18:21:52 +0800

    Power:imblance count cause suspend issue.
    1. thermal: qpnp-adc-tm: Improve check for pending tasks before suspend
    Replace wq_cnt with pm_wakeup_event with 2 seconds to prevent platform
    suspend and also add flush workqueue as a part of platform suspend_noirq
    to make sure all work is completed prior to platform suspend.
    
    2. Also avoid channel enable during workqueue for set_trips
    called during workqueue.
    
    Change-Id: I4ced9856ff0cb925f4d6c5aae36c7a15a7333db1
    Signed-off-by: zoujc <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1253325
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/thermal/qpnp-adc-tm.c
+++ drivers/thermal/qpnp-adc-tm.c
@@ -1763,12 +1763,15 @@ static int qpnp_adc_tm_set_trip_temp(void *data, int low_temp, int high_temp)
 		}
 		adc_tm->low_thr = tm_config.high_thr_voltage;
 
-		rc = qpnp_adc_tm_activate_trip_type(adc_tm,
-				ADC_TM_TRIP_HIGH_WARM,
-				THERMAL_TRIP_ACTIVATION_ENABLED);
-		if (rc) {
-			pr_err("adc-tm warm activation failed\n");
-			return rc;
+		/* If there is a pending workqueue, don't enable */
+		if (!(adc_tm->low_thr_triggered)) {
+			rc = qpnp_adc_tm_activate_trip_type(adc_tm,
+					ADC_TM_TRIP_HIGH_WARM,
+					THERMAL_TRIP_ACTIVATION_ENABLED);
+			if (rc) {
+				pr_err("adc-tm warm activation failed\n");
+				return rc;
+			}
 		}
 	} else {
 		rc = qpnp_adc_tm_activate_trip_type(adc_tm,
@@ -1796,12 +1799,15 @@ static int qpnp_adc_tm_set_trip_temp(void *data, int low_temp, int high_temp)
 		}
 		adc_tm->high_thr = tm_config.low_thr_voltage;
 
-		rc = qpnp_adc_tm_activate_trip_type(adc_tm,
-				ADC_TM_TRIP_LOW_COOL,
-				THERMAL_TRIP_ACTIVATION_ENABLED);
-		if (rc) {
-			pr_err("adc-tm cool activation failed\n");
-			return rc;
+		/* If there is a pending workqueue, don't enable */
+		if (!(adc_tm->high_thr_triggered)) {
+			rc = qpnp_adc_tm_activate_trip_type(adc_tm,
+					ADC_TM_TRIP_LOW_COOL,
+					THERMAL_TRIP_ACTIVATION_ENABLED);
+			if (rc) {
+				pr_err("adc-tm cool activation failed\n");
+				return rc;
+			}
 		}
 	} else {
 		rc = qpnp_adc_tm_activate_trip_type(adc_tm,
@@ -1889,7 +1895,6 @@ static void notify_adc_tm_fn(struct work_struct *work)
 {
 	struct qpnp_adc_tm_sensor *adc_tm = container_of(work,
 		struct qpnp_adc_tm_sensor, work);
-	struct qpnp_adc_tm_chip *chip = adc_tm->chip;
 
 	if (adc_tm->thermal_node) {
 		pr_debug("notifying uspace client\n");
@@ -1900,8 +1905,6 @@ static void notify_adc_tm_fn(struct work_struct *work)
 		else
 			notify_clients(adc_tm);
 	}
-
-	atomic_dec(&chip->wq_cnt);
 }
 
 static int qpnp_adc_tm_recalib_request_check(struct qpnp_adc_tm_chip *chip,
@@ -2145,11 +2148,8 @@ static int qpnp_adc_tm_disable_rearm_high_thresholds(
 		return rc;
 	}
 
-	if (!queue_work(chip->sensor[sensor_num].req_wq,
-				&chip->sensor[sensor_num].work)) {
-		/* The item is already queued, reduce the count */
-		atomic_dec(&chip->wq_cnt);
-	}
+	queue_work(chip->sensor[sensor_num].req_wq,
+		&chip->sensor[sensor_num].work);
 
 	return rc;
 }
@@ -2256,11 +2256,8 @@ static int qpnp_adc_tm_disable_rearm_low_thresholds(
 		return rc;
 	}
 
-	if (!queue_work(chip->sensor[sensor_num].req_wq,
-				&chip->sensor[sensor_num].work)) {
-		/* The item is already queued, reduce the count */
-		atomic_dec(&chip->wq_cnt);
-	}
+	queue_work(chip->sensor[sensor_num].req_wq,
+				&chip->sensor[sensor_num].work);
 
 	return rc;
 }
@@ -2324,8 +2321,6 @@ static int qpnp_adc_tm_read_status(struct qpnp_adc_tm_chip *chip)
 
 fail:
 	mutex_unlock(&chip->adc->adc_lock);
-	if (rc < 0)
-		atomic_dec(&chip->wq_cnt);
 
 	return rc;
 }
@@ -2377,10 +2372,6 @@ static int qpnp_adc_tm_hc_read_status(struct qpnp_adc_tm_chip *chip)
 
 fail:
 	mutex_unlock(&chip->adc->adc_lock);
-
-	if (rc < 0 || (!chip->th_info.adc_tm_high_enable &&
-					!chip->th_info.adc_tm_low_enable))
-		atomic_dec(&chip->wq_cnt);
 
 	return rc;
 }
@@ -2491,7 +2482,6 @@ static irqreturn_t qpnp_adc_tm_high_thr_isr(int irq, void *data)
 		}
 	}
 
-	atomic_inc(&chip->wq_cnt);
 	queue_work(chip->high_thr_wq, &chip->trigger_high_thr_work);
 
 	return IRQ_HANDLED;
@@ -2600,7 +2590,6 @@ static irqreturn_t qpnp_adc_tm_low_thr_isr(int irq, void *data)
 		}
 	}
 
-	atomic_inc(&chip->wq_cnt);
 	queue_work(chip->low_thr_wq, &chip->trigger_low_thr_work);
 
 	return IRQ_HANDLED;
@@ -2729,14 +2718,13 @@ static irqreturn_t qpnp_adc_tm_rc_thr_isr(int irq, void *data)
 	}
 
 	if (sensor_low_notify_num) {
-		if (queue_work(chip->low_thr_wq, &chip->trigger_low_thr_work))
-			atomic_inc(&chip->wq_cnt);
+		pm_wakeup_event(chip->dev, 2000);
+		queue_work(chip->low_thr_wq, &chip->trigger_low_thr_work);
 	}
 
 	if (sensor_high_notify_num) {
-		if (queue_work(chip->high_thr_wq,
-				&chip->trigger_high_thr_work))
-			atomic_inc(&chip->wq_cnt);
+		pm_wakeup_event(chip->dev, 2000);
+		queue_work(chip->high_thr_wq, &chip->trigger_high_thr_work);
 	}
 
 	return IRQ_HANDLED;
@@ -3224,7 +3212,6 @@ static int qpnp_adc_tm_probe(struct platform_device *pdev)
 
 	INIT_WORK(&chip->trigger_high_thr_work, qpnp_adc_tm_high_thr_work);
 	INIT_WORK(&chip->trigger_low_thr_work, qpnp_adc_tm_low_thr_work);
-	atomic_set(&chip->wq_cnt, 0);
 
 	if (!chip->adc_tm_hc) {
 		rc = qpnp_adc_tm_initial_setup(chip);
@@ -3331,11 +3318,18 @@ static void qpnp_adc_tm_shutdown(struct platform_device *pdev)
 static int qpnp_adc_tm_suspend_noirq(struct device *dev)
 {
 	struct qpnp_adc_tm_chip *chip = dev_get_drvdata(dev);
+	struct device_node *node = dev->of_node, *child;
+	int i = 0;
 
-	if (atomic_read(&chip->wq_cnt) != 0) {
-		pr_err(
-			"Aborting suspend, adc_tm notification running while suspending\n");
-		return -EBUSY;
+	flush_workqueue(chip->high_thr_wq);
+	flush_workqueue(chip->low_thr_wq);
+
+	for_each_child_of_node(node, child) {
+		if (chip->sensor[i].req_wq) {
+			pr_debug("flushing queue for sensor %d\n", i);
+			flush_workqueue(chip->sensor[i].req_wq);
+		}
+		i++;
 	}
 	return 0;
 }
