9b29567ff9a061958beaafeb6d172168e7763f00
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2018-10-23 13:11:40 +0530

    mmc: host: cmdq: Proceed with other interrupts
    There are instances observed that CQ triggers interrupt for halt
    operation with both halt bit and task-complete bits set in CQIS
    register. Since this task-complete interrupt is a spurious one,
    TQTCN remains zero. In this case, we skip servicing halt interrupt
    and context which is trying to halt CQ gets timeout while waiting
    for halt interrupt.
    
    To cover this scenario, proceed with servicing halt interrupt even
    when it gets the spurious task-complete interrupt.
    
    Change-Id: Ibe4e3b0a1a931ec6eea3f23ae239da50f462bc0b
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1291867
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -1100,7 +1100,7 @@ skip_cqterri:
 		/* read CQTCN and complete the request */
 		comp_status = cmdq_readl(cq_host, CQTCN);
 		if (!comp_status)
-			goto out;
+			goto hac;
 		/*
 		 * The CQTCN must be cleared before notifying req completion
 		 * to upper layers to avoid missing completion notification
@@ -1127,7 +1127,7 @@ skip_cqterri:
 			}
 		}
 	}
-
+hac:
 	if (status & CQIS_HAC) {
 		if (cq_host->ops->post_cqe_halt)
 			cq_host->ops->post_cqe_halt(mmc);
@@ -1138,7 +1138,7 @@ skip_cqterri:
 		complete(&cq_host->halt_comp);
 	}
 
-out:
+
 	return IRQ_HANDLED;
 }
 EXPORT_SYMBOL(cmdq_irq);
