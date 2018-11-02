c7792d91ef80203cd9faae46360ba8c3a43bfaba
Kenny Kessler <kenny.kessler@motorola.com>
2018-11-02 14:38:26 -0500

    usb: dwc3: Delay EXT USB Enable until Resumed.
    When the SPI wakes up from Suspend because of a pending
    tranmission it will call down after the message is decoded in
    greybus.  This happens well before System Resume has occurred.
    Thereby causing contention on the clock enable sequence.
    
    Fix this contention by remembering with a flag that
    we should schedule ext_usb_work at the end of the pm_resume.
    
    Change-Id: Ief92e480c41bdf89d13bba42c71c117f63faa462
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1264292
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-by: Curtis Porter <porterc@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 865610b20cdf6258b370b05467b00870c2dc93eb)
    Reviewed-on: https://gerrit.mot.com/1285422
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Yin-Jun Chen <a7301c@motorola.com>
    Tested-by: Yin-Jun Chen <a7301c@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1316370

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -387,6 +387,7 @@ struct dwc3_msm {
 	bool			mod_vbus_active;
 	enum dwc3_id_state	mod_id_state;
 #endif
+	bool			ext_usb_run;
 };
 
 static struct dwc3_msm *the_chip;
@@ -631,6 +632,13 @@ static void dwc3_ext_usb_enable(struct dwc3_msm *mdwc, bool enable)
 {
 	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
 
+	if (atomic_read(&mdwc->pm_suspended)) {
+		pr_err("%s: Attempting to Run while Suspended!\n", __func__);
+		mdwc->ext_usb_run = true;
+		return;
+	}
+
+	mdwc->ext_usb_run = false;
 	pr_err("%s: ext_enabled %d enable %d\n", __func__,
 	       mdwc->ext_enabled ? 1 : 0, enable ? 1 : 0);
 
@@ -6078,6 +6086,11 @@ static int dwc3_msm_pm_restore(struct device *dev)
 
 	/* kick in otg state machine */
 	queue_work(mdwc->dwc3_wq, &mdwc->resume_work);
+
+	if (mdwc->ext_usb_run) {
+		mdwc->ext_usb_run = false;
+		schedule_work(&mdwc->ext_usb_work);
+	}
 
 	return 0;
 }
