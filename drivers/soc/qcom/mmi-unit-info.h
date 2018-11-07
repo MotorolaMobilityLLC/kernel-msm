97e6046f5cec46a27b1613f4410decf944ee1729
zhangcj1 <zhangcj1@lenovo.com>
2018-11-07 08:41:03 +0800

    modem : river set powerup_reason when modem crash
    when modem crash,modem will slient input sim pin
    so set power reason to modem when modem crash
    
    Change-Id: Ief144e401513be922306acac0199852059c497e5
    Signed-off-by: zhangcj1 <zhangcj1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1265674
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/soc/qcom/mmi-unit-info.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __ARCH_ARM_MACH_MSM_MMI_UNIT_INFO_H
+#define __ARCH_ARM_MACH_MSM_MMI_UNIT_INFO_H
+
+/* set of data provided to the modem over SMEM */
+#define BARCODE_MAX_LEN 65
+#define MACHINE_MAX_LEN 33
+#define CARRIER_MAX_LEN 65
+#define BASEBAND_MAX_LEN 97
+#define MSMHW_MAX_LEN 32
+#define DEVICE_MAX_LEN 33
+#define RADIO_MAX_LEN 33
+#define PU_REASON_MODEM_RESET		0x80000000 /* Bit 31 */
+#define SMEM_KERNEL_RESERVE_SIZE 1024
+struct mmi_unit_info {
+	uint32_t version;
+	uint32_t system_rev;
+	uint32_t system_serial_low;
+	uint32_t system_serial_high;
+	char machine[MACHINE_MAX_LEN];
+	char barcode[BARCODE_MAX_LEN];
+	char carrier[CARRIER_MAX_LEN];
+	char baseband[BASEBAND_MAX_LEN];
+	char device[DEVICE_MAX_LEN];
+	uint32_t radio;
+	uint32_t powerup_reason;
+	char radio_str[RADIO_MAX_LEN];
+};
+
+
+
+#endif
