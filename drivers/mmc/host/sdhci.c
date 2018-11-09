edf1bb0c1e093035decfbc51324e7cb59fd4e9ab
yuedl1 <yuedl1@lenovo.com>
2018-11-09 23:19:46 +0800

    fix CONFIG_SDHCI_DUNPREGS_DEBUG_PANIC
    old config item is invalid . fixed it,if mmc failed
    afterdump sdhci regs, system will panic.
    
    Change-Id: I3537fbc3fb163974c3425e20f853c23a88734a4d
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1266945
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -199,7 +199,7 @@ static void sdhci_dumpregs(struct sdhci_host *host)
 	sdhci_dump_state(host);
 	pr_info(DRIVER_NAME ": ===========================================\n");
 
-#ifdef SDHCI_DUMPREG_DEBUG_PANIC
+#ifdef CONFIG_SDHCI_DUMPREG_DEBUG_PANIC
 	if (mmc_card_mmc(host->mmc->card))
 		BUG_ON(true);
 #endif
