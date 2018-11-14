a2b11658c5ce55d2dddfdbb2821eeed5dcaed9a6
Daniel Helu <dipho@motorola.com>
2018-11-14 14:46:19 -0200

    Fix STS test case CVE_2017_11048
    This commit prevents the kernel to access a null pointer
    causedby the fact that Afinnity does not have the frame
    buffer compression enabled. This was a fix recommend by
    Qcomm.
    
    Change-Id: Iab19729804e5274c9e3aa070ec07233cdbff7f26
    Signed-off-by: Daniel Helu <dipho@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1269094
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -2193,6 +2193,11 @@ static int mdss_mdp_ctl_fbc_enable(int enable,
 
 	fbc = &pdata->fbc;
 
+	if (!fbc->enabled) {
+		pr_debug("FBC not enabled\n");
+		return -EINVAL;
+	}
+
 	if (mixer->num == MDSS_MDP_INTF_LAYERMIXER0 ||
 			mixer->num == MDSS_MDP_INTF_LAYERMIXER1) {
 		pr_debug("Mixer supports FBC.\n");
