57f07ce24e7fd8702a110b29fcd9eeb82d0fa8f2
jixj <jixj@lenovo.com>
2018-11-15 09:51:16 +0800

    security: pfe: add scm failure count statistic
    As pfe key operation is critical for FBE device, add failure statistic
    for better debug. Enforce BUG if seeing 10 time busy failures or
    1 time other failure on UFS (BUG if seeing 1 time for any type of
    failure on EMMC) to avoid wrong encrypted or decrypted
    data, which ends up with hard-debug user data corruption issue.
    
    Propagation from (CR)
    
    Change-Id: I098535aa0efbd58817f3fbbd7fc83cf53221d42e
    Reviewed-on: https://gerrit.mot.com/1269436
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Submit-Approved: Jira Key

--- security/pfe/pfk_ice.c
+++ security/pfe/pfk_ice.c
@@ -67,6 +67,26 @@ enum {
 	ICE_CIPHER_MODE_XTS_256 = 3,
 	ICE_CIPHER_MODE_CBC_256 = 4
 };
+static void qti_pfk_ice_stat_failure(char *type, uint32_t id, int32_t err)
+{
+	static uint32_t set_key_failure, invalidate_key_failure;
+
+	if (id == TZ_ES_CONFIG_SET_ICE_KEY_ID)
+		set_key_failure++;
+	else if (id == TZ_ES_INVALIDATE_ICE_KEY_ID)
+		invalidate_key_failure++;
+	else {
+		pr_warn("%s: unsupported id %d\n", __func__, id);
+		return;
+	}
+
+	pr_warn("%s: failed to call scm %d (%d %d)\n",
+		__func__, id, set_key_failure, invalidate_key_failure);
+	if (err != -EBUSY || !strncmp(type, "sdcc", 4))
+		BUG();
+	BUG_ON((invalidate_key_failure + set_key_failure) > 10);
+}
+
 
 static int set_key(uint32_t index, const uint8_t *key, const uint8_t *salt,
 		unsigned int data_unit)
@@ -151,6 +171,7 @@ int qti_pfk_ice_set_key(uint32_t index, uint8_t *key, uint8_t *salt,
 	ret = set_key(index, key, salt, data_unit);
 	if (ret) {
 		pr_err("%s: Set Key Error: %d\n", __func__, ret);
+		qti_pfk_ice_stat_failure(s_type, TZ_ES_CONFIG_SET_ICE_KEY_ID, ret);
 		if (ret == -EBUSY) {
 			if (qcom_ice_setup_ice_hw((const char *)s_type, false))
 				pr_err("%s: clock disable failed\n", __func__);
@@ -191,8 +212,10 @@ int qti_pfk_ice_invalidate_key(uint32_t index, char *storage_type)
 	}
 
 	ret = clear_key(index);
-	if (ret)
+	if (ret) {
 		pr_err("%s: Invalidate key error: %d\n", __func__, ret);
+		qti_pfk_ice_stat_failure(storage_type, TZ_ES_INVALIDATE_ICE_KEY_ID, ret);
+	}
 
 	if (qcom_ice_setup_ice_hw((const char *)storage_type, false))
 		pr_err("%s: could not disable clocks\n", __func__);
