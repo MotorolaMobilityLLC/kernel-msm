fabf1f8eaba295b8a7548c5195f043f8dcdb81b1
xuwt2 <xuwt2@lenovo.com>
2018-11-15 12:20:29 +0800

    mmc: sdhci_dumpregs should not be invoked when host is off
    sdhci_dumpregs(host) is just used to dump host register for debugging
    it should not be invoked when the host is off
    
    Change-Id: I174c23beb5f4a4d45d39c6d113f1ed17d03d25c2
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1269560
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1828,7 +1828,6 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		if (mrq->data)
 			mrq->data->error = -EIO;
 		host->mrq = NULL;
-		sdhci_dumpregs(host);
 		mmc_request_done(host->mmc, mrq);
 		return;
 	}
