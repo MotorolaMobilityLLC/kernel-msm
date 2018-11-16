da8cff2c9205a7a232844102a24c191a9df53684
Haijian Ma <mahj8@motorola.com>
2018-11-16 14:02:52 +0800

    power: smb5-lib: Only MMI Full
    Only use the MMI charger state machine state
    to send FULL condition.
    
    refer to https://gerrit.mot.com/997762
    
    Change-Id: I7c2e96a6bb556d1d82b6eb4404f08d8aef17046c
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1270045
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -2164,8 +2164,10 @@ int smblib_get_prop_batt_status(struct smb_charger *chg,
 		switch (stat) {
 		case TERMINATE_CHARGE:
 		case INHIBIT_CHARGE:
+#ifdef QCOM_BASE
 			val->intval = POWER_SUPPLY_STATUS_FULL;
 			break;
+#endif
 		default:
 			val->intval = POWER_SUPPLY_STATUS_DISCHARGING;
 			break;
@@ -2182,8 +2184,10 @@ int smblib_get_prop_batt_status(struct smb_charger *chg,
 		break;
 	case TERMINATE_CHARGE:
 	case INHIBIT_CHARGE:
+#ifdef QCOM_BASE
 		val->intval = POWER_SUPPLY_STATUS_FULL;
 		break;
+#endif
 	case DISABLE_CHARGE:
 	case PAUSE_CHARGE:
 		val->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
