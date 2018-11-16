9e0c755478377755ae54975da5e3fba84a115403
Haijian Ma <mahj8@motorola.com>
2018-11-16 10:33:27 +0800

    pmic-voter: Increase number of voting clients.
    Increase the number of clients that can vote for a particular
    votable. Increase in the number of voters calls for an
    increase in the number of votes as well, otherwise we are
    seeing failures where some client votes don't count because
    we have run out of votes.
    
    refer to https://gerrit.mot.com/981852
    
    Change-Id: I4dcc0561fdba65587f4ef21db7980fff7455cbb3
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1269918
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/pmic-voter.c
+++ drivers/power/supply/qcom/pmic-voter.c
@@ -20,7 +20,7 @@
 
 #include <linux/pmic-voter.h>
 
-#define NUM_MAX_CLIENTS		16
+#define NUM_MAX_CLIENTS		32
 #define DEBUG_FORCE_CLIENT	"DEBUG_FORCE_CLIENT"
 
 static DEFINE_SPINLOCK(votable_list_slock);
@@ -362,6 +362,7 @@ int vote(struct votable *votable, const char *client_str, bool enabled, int val)
 	client_id = get_client_id(votable, client_str);
 	if (client_id < 0) {
 		rc = client_id;
+		pr_err("%s client id not found for %s\n", __func__, client_str);
 		goto out;
 	}
 
