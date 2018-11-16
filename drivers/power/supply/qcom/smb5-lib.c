de10d41ea2b3379ba161eaa1b8dc56efd57ba44e
Haijian Ma <mahj8@motorola.com>
2018-11-16 12:34:40 +0800

    smb5-lib: Fix Current Limit Set
    the ICL setting is not taking effect unless it is
    greater then the current settled value.  Fix this
    so that it gets set all the time when the vote
    comes in.
    
    refer to https://gerrit.mot.com/992661
    
    Change-Id: I5f5a2eab2e52a24f56dca914713384e07c5fcb4c
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1270012
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -773,6 +773,7 @@ int smblib_set_usb_suspend(struct smb_charger *chg, bool suspend)
 		vote(chg->icl_irq_disable_votable, USB_SUSPEND_VOTER,
 				true, 0);
 
+	smblib_dbg(chg, PR_MISC, "%s\n", suspend ? "suspend" : "resume");
 	rc = smblib_masked_write(chg, USBIN_CMD_IL_REG, USBIN_SUSPEND_BIT,
 				 suspend ? USBIN_SUSPEND_BIT : 0);
 	if (rc < 0)
@@ -1478,6 +1479,8 @@ int smblib_set_icl_current(struct smb_charger *chg, int icl_ua)
 		pr_err("USB ICL callback in Facory Mode! %d\n", icl_ua);
 		return rc;
 	}
+
+	smblib_dbg(chg, PR_MISC, "%d uA\n", icl_ua);
 
 	if (suspend)
 		return smblib_set_usb_suspend(chg, true);
