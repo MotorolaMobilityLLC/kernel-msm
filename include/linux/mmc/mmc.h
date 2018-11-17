b40082522c454556a3071247dcbb3ca05912091c
yuedl1 <yuedl1@lenovo.com>
2018-11-17 23:10:52 +0800

    support device version in user space
    the Mircon eMMC need device version as minor firmware version.
    so port device version to user space .
    
    Change-Id: I7597a94cb652696dd70cd54536d2ee660a316efe
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1270619
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Chuanchun Zhu <zhucc2@motorola.com>
    Reviewed-by: Xinmin Fu <fuxm1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1365600

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -303,6 +303,7 @@ static inline bool mmc_op_multi(u32 opcode)
 #define EXT_CSD_CACHE_SIZE		249	/* RO, 4 bytes */
 #define EXT_CSD_PWR_CL_DDR_200_360	253	/* RO */
 #define EXT_CSD_FIRMWARE_VERSION	254	/* RO, 8 bytes */
+#define EXT_CSD_DEVICE_VERSION          262     /* RO, 2 bytes */
 #define EXT_CSD_PRE_EOL_INFO		267	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A	268	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_B	269	/* RO */
