0d556ec29cca2c0ca2716a96785fd6edd9a6e04e
Haijian Ma <mahj8@motorola.com>
2018-11-19 18:56:43 +0800

    smb5 : remove useless property
    remove input_voltage_regulation property,
    this property is useless.
    
    Change-Id: I44258e6189687c298c8ba4108118e8cea3364dcb
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1270923
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -3138,6 +3138,7 @@ int smblib_set_prop_dc_current_max(struct smb_charger *chg,
 #endif
 }
 
+#ifdef QCOM_BASE
 int smblib_set_prop_voltage_wls_output(struct smb_charger *chg,
 				    const union power_supply_propval *val)
 {
@@ -3160,6 +3161,7 @@ int smblib_set_prop_voltage_wls_output(struct smb_charger *chg,
 
 	return rc;
 }
+#endif
 
 int smblib_set_prop_dc_reset(struct smb_charger *chg)
 {
@@ -5929,7 +5931,9 @@ irqreturn_t dc_plugin_irq_handler(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
 	struct smb_charger *chg = irq_data->parent_data;
+#ifdef QCOM_BASE
 	union power_supply_propval pval;
+#endif
 	int input_present;
 	bool dcin_present, vbus_present;
 	int rc, wireless_vout = 0;
@@ -5948,7 +5952,7 @@ irqreturn_t dc_plugin_irq_handler(int irq, void *data)
 
 	dcin_present = input_present & INPUT_PRESENT_DC;
 	vbus_present = input_present & INPUT_PRESENT_USB;
-
+#ifdef QCOM_BASE
 	if (dcin_present) {
 		if (!vbus_present && chg->sec_cp_present) {
 			pval.intval = wireless_vout;
@@ -5978,7 +5982,7 @@ irqreturn_t dc_plugin_irq_handler(int irq, void *data)
 
 		vote(chg->dc_suspend_votable, CHG_TERMINATION_VOTER, false, 0);
 	}
-
+#endif
 	power_supply_changed(chg->dc_psy);
 
 	smblib_dbg(chg, PR_WLS, "dcin_present= %d, usbin_present= %d, cp_reason = %d\n",
