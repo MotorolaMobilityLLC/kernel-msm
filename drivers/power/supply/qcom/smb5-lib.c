88508aafc4e9d3d51f4813a610bb2c9a849de5a9
Haijian Ma <mahj8@motorola.com>
2018-11-22 00:19:22 -0600

    Revert "(CR): smb5-lib: fixed torch blinking when usb insert"
    This reverts commit 43c1a2c8f636c55dbc8c6b1a6b53d5577cef1e03.
    
    Change-Id: I97d8598c2e614f90e7102c74a4c018aeebc97951
    Reviewed-on: https://gerrit.mot.com/1272662
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -3260,7 +3260,7 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 
 			if (chg->smb_version == PMI632_SUBTYPE)
 				schgm_flash_torch_priority(chg,
-						TORCH_BOOST_MODE);
+						TORCH_BUCK_MODE);
 
 			data = chg->irq_info[USBIN_UV_IRQ].irq_data;
 			if (data) {
