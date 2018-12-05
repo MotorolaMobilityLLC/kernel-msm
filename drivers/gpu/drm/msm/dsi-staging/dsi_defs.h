e9667ee81cac9df7fa6da2b104846bf8c778e8c9
Bang Nguyen <bangnguyen@motorola.com>
2018-12-05 10:14:49 +0800

    Add the ACL panel param feature
    Add the ACL panel param feature which will be controlled
    from the user-space
    
    Change-Id: I2f70baac1e7eacd948b1ed6209fb432541e90db2
    Reviewed-on: https://gerrit.mot.com/1278525
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shuo Yan <shuoyan@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
@@ -251,6 +251,8 @@ enum dsi_dfps_type {
  * @DSI_CMD_SET_QSYNC_OFF                  Disable qsync mode
  * @DSI_CMD_SET_HBM_ON:                    Turning HBM on
  * @DSI_CMD_SET_HBM_OFF:                   Turning HBM off
+ * @DSI_CMD_SET_ACL_ON:                    Turning ACL on
+ * @DSI_CMD_SET_ACL_OFF:                   Turning ACL off
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -279,6 +281,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_QSYNC_OFF,
 	DSI_CMD_SET_HBM_ON,
 	DSI_CMD_SET_HBM_OFF,
+	DSI_CMD_SET_ACL_ON,
+	DSI_CMD_SET_ACL_OFF,
 	DSI_CMD_SET_MAX
 };
 
