a6ee16c88ce948fff05af213646814c1ab41e8f5
Haijian Ma <mahj8@motorola.com>
2018-12-11 16:36:18 +0800

    power: smb5-lib: Fix Non-Default CC ICL
    Current implementation will ignore the CC line Rp
    resistance if the BC 1.2 detection is PC Port
    and the USB driver sends 500/900 mA as the Current
    limit.  To fix this only listen to the USB driver
    if the CC Rp is Default.
    
    Also a secondary defect was found because 900
    mA was not properly being set from a USB 3.1 port.
    To fix that ensure the the cl_usb is checked to see
    that it is at or below 500 mA prior to locking 500 mA.
    
    refer to https://gerrit.mot.com/1026470
    
    Change-Id: Icb1323379d5701f02f80073a7346dabcd0c08f7a
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1281457
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -9509,7 +9509,8 @@ static void mmi_heartbeat_work(struct work_struct *work)
 		else if (chip->real_charger_type ==
 			 POWER_SUPPLY_TYPE_USB_CDP)
 			cl_usb = 1500;
-		else if (cl_cc == 500)
+		else if ((cl_cc == 500) &&
+			 (cl_usb <= 500))
 			cl_usb = cl_cc;
 		else if (cl_usb <= 0)
 			cl_usb = 500;
