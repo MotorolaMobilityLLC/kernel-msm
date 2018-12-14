ad13db29600338accfadb575ab1cbe13c72ce1b8
Haijian Ma <mahj8@motorola.com>
2018-12-14 15:19:14 +0800

    otg: ensure that OTG is used first
    OTG vbus use internal pmic boost, so it can't work with mods in SRC
    state. so ensure that OTG is used first.
    
    Change-Id: I43ca962bd5d32b87c3ad7cf3a41414090340fc96
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1280801
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -416,6 +416,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(comp_clamp_level),
 	POWER_SUPPLY_ATTR(adapter_cc_mode),
 	POWER_SUPPLY_ATTR(skin_health),
+	POWER_SUPPLY_ATTR(external_vbus),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
