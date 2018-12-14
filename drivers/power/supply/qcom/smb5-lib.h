a48df30870546979a8427dd7befb6bfefb53fdaa
Haijian Ma <mahj8@motorola.com>
2018-12-14 15:19:14 +0800

    otg: ensure that OTG is used first
    OTG vbus use internal pmic boost, so it can't work with mods in SRC
    state. so ensure that OTG is used first.
    
    Change-Id: I43ca962bd5d32b87c3ad7cf3a41414090340fc96
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1280801
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -496,6 +496,7 @@ struct mmi_params {
 	bool			check_ebsrc_vl;
 	int			batt_health;
 	int			max_chrg_temp;
+	int			is_otg_enable;
 };
 
 struct smb_charger {
@@ -519,6 +520,7 @@ struct smb_charger {
 	struct mutex		ps_change_lock;
 	struct mutex		dr_lock;
 	struct mutex		irq_status_lock;
+	struct mutex		otg_lock;
 
 	/* power supplies */
 	struct power_supply		*batt_psy;
@@ -716,6 +718,7 @@ struct smb_charger {
 	struct usbpd		*pd;
 	int			pd_contract_uv;
 	struct delayed_work	pd_contract_work;
+	bool			external_vbus;
 	bool			suspended;
 };
 
