e1de6d0cf85e856144f0f219c65a86f93bbd9c8b
Haijian Ma <mahj8@motorola.com>
2018-12-14 17:21:29 +0800

    smb5: set 3A usb icl when NONE typec mode with QC3.0
    For some reason, the typec mode may not be detected when connect
    to a QC3.0 charger, but we still need to set 3A icl in that case.
    
    refer to https://gerrit.mot.com/1238549
    
    Change-Id: I9522ded5c57bd0f90f149aea8904b0318bb4a0b6
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1283194
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -9491,8 +9491,14 @@ static void mmi_heartbeat_work(struct work_struct *work)
 			cl_cc = 500;
 			break;
 		case POWER_SUPPLY_TYPEC_NONE:
-			if (vbus_present)
-				cl_cc = 500;
+			if (vbus_present) {
+				if (mmi->hvdcp3_con)
+					cl_cc = 3000;
+				else
+					cl_cc = 500;
+			} else
+				cl_cc = 0;
+
 			if (vbus_present &&
 			    !icl_override &&
 			    (apsd_reg == 0)) {
@@ -9503,6 +9509,7 @@ static void mmi_heartbeat_work(struct work_struct *work)
 					smblib_err(chip,
 						   "Fail ICL Over rc%d\n", rc);
 			}
+			break;
 		default:
 			cl_cc = 0;
 			break;
