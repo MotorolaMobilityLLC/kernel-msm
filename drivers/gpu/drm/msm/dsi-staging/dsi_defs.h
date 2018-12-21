264d0651639ac1488fa4b14d9f1c96799a6c8366
houdz1 <houdz1@motorola.com>
2018-12-21 12:58:04 +0800

    Foles: disable Elvss dim of HBM for smd panel
    For DVT2 panel ,the dim is disable default by smd.
    So We do not disable DIM through this code.
    
    Change-Id: I51701a3b0d49deee6bca2a95f2f3efdcdb8f6e70
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1284913
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
@@ -253,6 +253,7 @@ enum dsi_dfps_type {
  * @DSI_CMD_SET_HBM_OFF:                   Turning HBM off
  * @DSI_CMD_SET_ACL_ON:                    Turning ACL on
  * @DSI_CMD_SET_ACL_OFF:                   Turning ACL off
+ * DSI_CMD_SET_HBM_DIM_OFF:		   Turning HBM DIM off
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -283,6 +284,9 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_HBM_OFF,
 	DSI_CMD_SET_ACL_ON,
 	DSI_CMD_SET_ACL_OFF,
+
+	DSI_CMD_SET_HBM_DIM_OFF,
+
 	DSI_CMD_SET_MAX
 };
 
