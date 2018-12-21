9284e83bf9b2f96b28062ae5bac3ed9e93b73051
houdz1 <houdz1@motorola.com>
2018-12-21 12:58:04 +0800

    drm: mipi: disable Elvss dim of HBM for MIPI DSI
    Added 3 MIPI commands to set/get the ELVSS setting information from
    the MIPI DSI panel.
    
    Port from a5b269d7.
    
    Change-Id: I3aa1612c7afc35247f0db86ee1dd8484bae8d7b4
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1413519
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/drm_mipi_dsi.c
+++ drivers/gpu/drm/drm_mipi_dsi.c
@@ -1176,6 +1176,59 @@ int mipi_dsi_driver_register_full(struct mipi_dsi_driver *drv,
 }
 EXPORT_SYMBOL(mipi_dsi_driver_register_full);
 
+int mipi_dsi_dcs_get_elvss_data(struct mipi_dsi_device *dsi)
+{
+            u8 payload_f0[2] = { 0x5A, 0x5A };
+            u8 payload_b0 = 0x07;
+            ssize_t err;
+
+            err = mipi_dsi_dcs_write(dsi, 0xF0, payload_f0, sizeof(payload_f0));
+            err = mipi_dsi_dcs_write(dsi, 0xB0, &payload_b0, sizeof(payload_b0));
+
+            if (err < 0)
+                        return err;
+
+            return 0;
+}
+EXPORT_SYMBOL(mipi_dsi_dcs_get_elvss_data);
+
+int mipi_dsi_dcs_get_elvss_data_1(struct mipi_dsi_device *dsi)
+{
+            u8 payload_f0_1[2] = { 0xA5, 0xA5 };
+            ssize_t err;
+            err = mipi_dsi_dcs_write(dsi, 0xF0, payload_f0_1, sizeof(payload_f0_1));
+
+            if (err < 0)
+                        return err;
+
+            return 0;
+}
+EXPORT_SYMBOL(mipi_dsi_dcs_get_elvss_data_1);
+
+int mipi_dsi_dcs_set_elvss_dim_off(struct mipi_dsi_device *dsi,
+                                                            u8 val)
+{
+            u8 payload_f0[2] = { 0x5A, 0x5A };
+            u8 payload_f0_1[2] = { 0xA5, 0xA5 };
+            u8 payload_b7[2] = { 0x01, 0x5B };
+            u8 payload_b0 = 0x07;
+            ssize_t err;
+
+            pr_info("set elvss data:%d\n", val);
+
+            err = mipi_dsi_dcs_write(dsi, 0xF0, payload_f0, sizeof(payload_f0));
+            err = mipi_dsi_dcs_write(dsi, 0xB7, payload_b7, sizeof(payload_b7));
+            err = mipi_dsi_dcs_write(dsi, 0xB0, &payload_b0, sizeof(payload_b0));
+            err = mipi_dsi_dcs_write(dsi, 0xB7, &val, sizeof(val));
+            err = mipi_dsi_dcs_write(dsi, 0xF0, payload_f0_1, sizeof(payload_f0_1));
+
+            if (err < 0)
+                      return err;
+
+            return 0;
+}
+EXPORT_SYMBOL(mipi_dsi_dcs_set_elvss_dim_off);
+
 /**
  * mipi_dsi_driver_unregister() - unregister a driver for DSI devices
  * @drv: DSI driver structure
