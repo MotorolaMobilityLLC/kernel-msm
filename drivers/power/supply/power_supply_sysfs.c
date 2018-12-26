da408ac3ad1449b461bb743a99c2bdf8e6a16d7d
Haijian Ma <mahj8@motorola.com>
2018-12-26 20:17:39 +0800

    smb5: add vbus_conflict interface for user space
    add vbus_conflict interface for user space to judge if product
    need vbus conflict feature
    
    Change-Id: I27f5c5286b699bb26a5354ac3e22a2d042079aa3
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1287173
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -403,7 +403,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(chip_version),
 	POWER_SUPPLY_ATTR(qc_opti_disable),
 	POWER_SUPPLY_ATTR(therm_icl_limit),
-	POWER_SUPPLY_ATTR(external_vbus),
+	POWER_SUPPLY_ATTR(vbus_conflict),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
