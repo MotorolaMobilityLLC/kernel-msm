723cafd7b478008f52e02d5061c50c3556a0e4df
Tezz <plugmeintezz@gmail.com>
2018-12-26 00:42:05 -0500

    kernel: whitelist stock modules if available

--- kernel/module.c
+++ kernel/module.c
@@ -1145,6 +1145,9 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	if(!strncmp("wlan", mod->name, 4))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
