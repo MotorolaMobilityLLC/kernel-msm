611c33dc27672f206e00978b2e5af31163b506cb
Tezz <plugmeintezz@gmail.com>
2018-12-25 23:23:37 -0500

    cpuidle: don't disable cpuidle when entering suspend
    cpuidle was disabled while entering suspend as part of commit
    8651f97 in order to work around some
    ACPI bugs. However, there's no reason to do this on modern
    platforms. Leaving cpuidle enabled can result in improved power
    consumption if dpm_resume_noirq runs for a significant time.
    
    Change-Id: Ie182785b176f448698c0264eba554d1e315e8a06
    
    Tim Murray authored and flar2 committed on Sep 21, 2016

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -600,7 +600,7 @@ void dpm_resume_noirq(pm_message_t state)
 	async_synchronize_full();
 	dpm_show_time(starttime, state, "noirq");
 	resume_device_irqs();
-	cpuidle_resume();
+//	cpuidle_resume();
 	trace_suspend_resume(TPS("dpm_resume_noirq"), state.event, false);
 }
 
@@ -1112,7 +1112,7 @@ int dpm_suspend_noirq(pm_message_t state)
 	int error = 0;
 
 	trace_suspend_resume(TPS("dpm_suspend_noirq"), state.event, true);
-	cpuidle_pause();
+//	cpuidle_pause();
 	suspend_device_irqs();
 	mutex_lock(&dpm_list_mtx);
 	pm_transition = state;
