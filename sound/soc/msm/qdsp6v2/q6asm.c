429572a67057f0c4ce1ea2bf5ee50d01dda49228
Xiaoyu Ye <benyxy@codeaurora.org>
2019-01-15 15:49:59 -0800

    ASoC: msm: qdsp6v2: add range check for audio port index
    Add range check to make sure the received audio port index
    from ADSP is within the valid range.
    
    depend: 32ba8a8c11e3 ("ASoC: msm: qdspv2: add spin lock to protect ac")
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2019-2324
    CRs-Fixed: 2372292
    
    Change-Id: Ief647df1659f7f349a843f666d8f92f34a9a43be
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1354039
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <Raviv@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1494,6 +1494,13 @@ static int32_t q6asm_srvc_callback(struct apr_client_data *data, void *priv)
 	}
 
 	dir = (data->token & 0x0F);
+	if (dir != IN && dir != OUT) {
+		pr_err("%s: Invalid audio port index: %d\n", __func__, dir);
+		if ((sid > 0 && sid <= SESSION_MAX))
+			spin_unlock_irqrestore(
+				&(session[sid].session_lock), flags);
+		return 0;
+	}
 	port = &ac->port[dir];
 
 	switch (data->opcode) {
