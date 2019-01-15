92f364ea4ec460840926dae813599892d2f08565
houdz1 <houdz1@motorola.com>
2019-01-15 17:34:44 +0800

    msm/dsi-staGing: support for HBM_FOD mode
    FOD: Fingerprint of display.
    
    For tianma panel, there are two hbm mode.
    1. hbm
    2. hbm for FOD(hbm_fod);
    But for SMD panel ,there is only one HBM mode.
    
    Change-Id: I2ffbc563f0af0862209ec993fd44819b49295f3b
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1288506
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -177,6 +177,7 @@ enum acl_state {
 enum hbm_state {
 	HBM_OFF_STATE = 0,
 	HBM_ON_STATE,
+	HBM_FOD_ON_STATE,
 	HBM_STATE_NUM
 };
 
@@ -189,7 +190,7 @@ struct panel_param_val_map {
 struct panel_param {
 	const char *param_name;
 	struct panel_param_val_map *val_map;
-	const u16 val_max;
+	u16 val_max;
 	const u16 default_value;
 	u16 value;
 	bool is_supported;
@@ -251,6 +252,7 @@ struct dsi_panel {
 	bool no_panel_on_read_support;
 
 	bool panel_hbm_dim_off;
+	bool panel_hbm_fod;
 
 	struct panel_param *param_cmds;
 };
