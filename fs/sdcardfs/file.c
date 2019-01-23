4b2ca4ad9588e2e7c2742758a8bd4b805f854e46
jixj <jixj@lenovo.com>
2019-01-23 14:50:11 +0800

    sdcardfs: check if maya config is default value
    Not longer use MAYA storage on P NPI, and SDCARD_FS_PARTIAL_RELATIME/
    SDCARD_FS_DIR_WRITER depend on SDCARD_FS, causing these two configs
    are "n" in out/.../obj/KERNEL_OBJ/.config if we don't set configs in
    defconfig. we use "#ifdef CONFIG_SDCARD_FS_PARTIAL_RELATIME" to enable/
    disable feature in code, un-used code will run and print some error logs
    if we don't enable these two features.
    
    .config
    CONFIG_SDCARD_FS_PARTIAL_RELATIME="n"
    CONFIG_SDCARD_FS_DIR_WRITER="n"
    
    kernel log
    82574.033828: <6> sdcardfs: 102 user.relatime: invalid format
    82574.036783: <6> sdcardfs: 102 user.relatime: invalid format
    82574.060422: <6> sdcardfs: 102 user.relatime: invalid format
    82574.118677: <6> sdcardfs: 102 user.relatime: invalid format
    82574.121654: <6> sdcardfs: 102 user.relatime: invalid format
    82574.168275: <6> sdcardfs: 102 user.relatime: invalid format
    82574.172752: <6> sdcardfs: 102 user.relatime: invalid format
    
    Change-Id: Ib1f8c3743fa5e1ae1ffcad8dfb8f7b46923de8a4
    Reviewed-on: https://gerrit.mot.com/1296500
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -236,10 +236,8 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 	struct dentry *parent = dget_parent(dentry);
 	struct sdcardfs_sb_info *sbi = SDCARDFS_SB(dentry->d_sb);
 	const struct cred *saved_cred = NULL;
-#if defined(CONFIG_SDCARD_FS_DIR_WRITER) || defined(CONFIG_SDCARD_FS_PARTIAL_RELATIME)
 	uid_t writer_uid = current_fsuid().val;
 	struct sdcardfs_inode_data *pd = SDCARDFS_I(d_inode(parent))->data;
-#endif
 
 	/* don't open unhashed/deleted files */
 	if (d_unhashed(dentry)) {
@@ -286,18 +284,15 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 	else
 		sdcardfs_copy_and_fix_attrs(inode, sdcardfs_lower_inode(inode));
 
-#ifdef CONFIG_SDCARD_FS_PARTIAL_RELATIME
 	if (!err && pd->perm < PERM_ANDROID)
 		sdcardfs_update_relatime_flag(lower_file,
 			sdcardfs_lower_inode(inode), writer_uid);
-#endif
-#ifdef CONFIG_SDCARD_FS_DIR_WRITER
+
 	/* update xattr for writing operation under non "Android" folders */
 	if (!err && (lower_file->f_flags & O_ACCMODE) &&
 		pd->perm < PERM_ANDROID)
 		sdcardfs_update_xattr_dirwriter(lower_file->f_path.dentry,
 			writer_uid);
-#endif
 
 out_revert_cred:
 	revert_fsids(saved_cred);
