4e6bda343c2ec0f53034f22c2ccf5c5ea3efceb4
Kenny Kessler <kenny.kessler@motorola.com>
2019-01-24 14:04:42 -0600

    usb/dwc3: msm: Enable all Debug Logs
    Force all logs to print.
    
    The USB DWC3 is very important and tracking dmesg
    is much easier when everything is ON.
    
    Propagated from (CR)
    
    Change-Id: Ic97efd96c8b179c3e163c360b57f3c7ec7a912fa
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1307019
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1321487

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -69,6 +69,11 @@
 /* AHB2PHY read/write waite value */
 #define ONE_READ_WRITE_WAIT 0x11
 
+#undef dev_dbg
+#undef pr_debug
+#define dev_dbg dev_err
+#define pr_debug pr_err
+
 /* cpu to fix usb interrupt */
 static int cpu_to_affin;
 
