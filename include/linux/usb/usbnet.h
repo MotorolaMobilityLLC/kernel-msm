8e7aeaecef070d745db6b54146f5a8c3e6f34f46
Kenny Kessler <kenny.kessler@motorola.com>
2019-01-24 14:40:20 -0600

    net/usb: usbnet: Listen for Panel and set USB Power
    Set the Autosuspend state of the USB Bus when the Panel is OFF.
    Clear the Autosuspend state of the USB Bus when the Panel is ON.
    
    This will allow USB Link using USBNET i.e. RNDIS to enter U3
    when there is no more Packet traffic.  If packet traffic is
    held off long enoguh the System can enter Suspend as well.
    
    Port-of: https://gerrit.mot.com/1307023
    
    Change-Id: I421429b24bd05f3b1f6e4275bdd68b438d2bebd1
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1332195
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/usb/usbnet.h
+++ include/linux/usb/usbnet.h
@@ -82,6 +82,14 @@ struct usbnet {
 #		define EVENT_LINK_CHANGE	11
 #		define EVENT_SET_RX_MODE	12
 #		define EVENT_NO_IP_ALIGN	13
+
+
+#ifdef CONFIG_PANEL_NOTIFICATIONS
+	struct notifier_block 	panel_usb_notifier;
+	struct work_struct	panel_update_work;
+	u32			panel_state;
+	int 			last_panel_state;
+#endif
 };
 
 static inline struct usb_driver *driver_of(struct usb_interface *intf)
