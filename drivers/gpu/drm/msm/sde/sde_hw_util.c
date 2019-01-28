ee1a5039998bee07954a737f6dbdfafe3ea6deb5
houdz1 <houdz1@motorola.com>
2019-01-28 10:38:15 +0800

    pixel: porting the pixel code
    Porting the pixel code.
    Use "CONFIG_IRIS2P_FULL_SUPPORT" to control the code.
    
    Change-Id: I85e2937ebd66979940ff8300e9ff05e7fb3535e3
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1301239
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/sde/sde_hw_util.c
+++ drivers/gpu/drm/msm/sde/sde_hw_util.c
@@ -128,6 +128,9 @@ void sde_set_scaler_v2(struct sde_hw_scaler3_cfg *cfg,
 	cfg->unsharp_mask_blend = scale_v2->unsharp_mask_blend;
 
 	cfg->de.enable = scale_v2->de.enable;
+#if defined(CONFIG_IRIS2P_FULL_SUPPORT)
+	cfg->de.enable = false;
+#endif
 	cfg->de.sharpen_level1 = scale_v2->de.sharpen_level1;
 	cfg->de.sharpen_level2 = scale_v2->de.sharpen_level2;
 	cfg->de.clip = scale_v2->de.clip;
@@ -277,6 +280,9 @@ static void _sde_hw_setup_scaler3_de(struct sde_hw_blk_reg_map *c,
 	u32 sharp_lvl, sharp_ctl, shape_ctl, de_thr;
 	u32 adjust_a, adjust_b, adjust_c;
 
+#if defined(CONFIG_IRIS2P_FULL_SUPPORT)
+	de_cfg->enable = false;
+#endif
 	if (!de_cfg->enable)
 		return;
 
