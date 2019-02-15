4afa7e5408810a6452baca87533c837cd911e1c6
Tezz <t3zz@remainsilent.net>
2019-02-15 02:00:14 -0500

    Update dependencies

--- submodules.sh
+++ submodules.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/rbheromax/AnyKernel2
-git clone https://github.com/rbheromax/gcc-linaro-7.1.1 gcc-linaro-7.1.1
+# git clone https://github.com/rbheromax/gcc-linaro-7.1.1 gcc-linaro-7.1.1
+git clone https://github.com/MotorolaMobilityLLC/motorola-kernel ../kernel -b pie-9.0.0-release-ppk --single-branch
