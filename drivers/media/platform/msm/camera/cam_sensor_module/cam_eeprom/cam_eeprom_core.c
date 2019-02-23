209e0bee472eacdc526a32dd0c4000d556579a27
chenglong1 <chenglong1@lenovo.com>
2019-02-23 16:06:25 +0800

    camera: Fix EEPROM device handle leak.
    When EEPROM read failed, the EEPROM device handle was created,
    but not released. That will cause EEPROM device acquire be rejected
    till phone reboot.
    
    Change-Id: Ife5ea9b4866f33aee137a60328530a3b4c7cb08b
    Signed-off-by: chenglong1 <chenglong1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1313487
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Zilong Zhang <zhangzl7@lenovo.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Chuanxin Guo <guocx4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_core.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_core.c
@@ -925,6 +925,17 @@ power_down:
 memdata_free:
 	vfree(e_ctrl->cal_data.mapdata);
 error:
+	/*Try to release the device handle when EEPROM read failed*/
+	if (e_ctrl->cam_eeprom_state >= CAM_EEPROM_ACQUIRE) {
+		int ret = 0;
+		ret = cam_destroy_device_hdl(e_ctrl->bridge_intf.device_hdl);
+		if (ret < 0)
+			CAM_ERR(CAM_EEPROM, "destroying the device hdl");
+
+		e_ctrl->bridge_intf.device_hdl = -1;
+		e_ctrl->bridge_intf.link_hdl = -1;
+		e_ctrl->bridge_intf.session_hdl = -1;
+	}
 	kfree(power_info->power_setting);
 	kfree(power_info->power_down_setting);
 	power_info->power_setting = NULL;
