ad7d4cc7d1c6b7d5a0eddf16dfea87f95422304d
liujg6 <liujg6@lenovo.com>
2019-02-25 17:18:00 +0800

    arm: check signal is handled on 32bit platform
    refer to 64bit user fault handling, check signal is handled or not
    on 32bit before print unhandled page fault
    
    Change-Id: I0658992629e9966489bc5faae087a449972cf0cb
    Reviewed-on: https://gerrit.mot.com/1313711
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -167,8 +167,9 @@ __do_user_fault(struct task_struct *tsk, unsigned long addr,
 		harden_branch_predictor();
 
 #ifdef CONFIG_DEBUG_USER
-	if (((user_debug & UDBG_SEGV) && (sig == SIGSEGV)) ||
-	    ((user_debug & UDBG_BUS)  && (sig == SIGBUS))) {
+	if (unhandled_signal(tsk, sig) &&
+		(((user_debug & UDBG_SEGV) && (sig == SIGSEGV)) ||
+		((user_debug & UDBG_BUS)  && (sig == SIGBUS)))) {
 		printk(KERN_DEBUG "%s: unhandled page fault (%d) at 0x%08lx, code 0x%03x\n",
 		       tsk->comm, sig, addr, fsr);
 		show_pte(tsk->mm, addr);
