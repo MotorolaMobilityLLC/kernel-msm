9374a1c94a5e8983e8de9b410cea03c3a51979a7
jixj <jixj@lenovo.com>
2019-02-27 14:38:59 +0800

    smb5-lib: use orderly poweroff for factory kill
    The orderly poweroff will attempt to call userspace command,
    and update userspace command to "system/bin/reboot -p", this
    command will sync filesystem before shutdown
    
    Change-Id: I76a8eec9fecd398f40859acbbc5f24d796f13060
    Reviewed-on: https://gerrit.mot.com/1314748
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -386,7 +386,7 @@ void ctrl_alt_del(void)
 		kill_cad_pid(SIGINT, 1);
 }
 
-char poweroff_cmd[POWEROFF_CMD_PATH_LEN] = "/sbin/poweroff";
+char poweroff_cmd[POWEROFF_CMD_PATH_LEN] = "/system/bin/reboot -p";
 static const char reboot_cmd[] = "/sbin/reboot";
 
 static int run_cmd(const char *cmd)
