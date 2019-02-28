959121b43260cd6fe3215c23e39d9310335596a6
Haijian Ma <mahj8@motorola.com>
2019-02-28 16:50:28 +0800

    smb5-lib: fixed mods can't charging with ordinary charger
    base on foles HW design, usbin current = eb current + FCC current,
    so redistribute current to fixed mods can't be charging with ordinary
    charger when phone battery full.
    
    Change-Id: Ic8171a14acf4f3d76566bd377e1a8b8897ae881e
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1315404
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Wei <weiweij@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -9873,17 +9873,18 @@ static void mmi_heartbeat_work(struct work_struct *work)
 	switch (mmi->pres_chrg_step) {
 	case STEP_EB:
 		target_fv = max_fv_mv;
-		if (batt_soc < 100)
-			target_fcc = zone->fcc_norm_ma;
-		else
-			target_fcc = -EINVAL;
 
 		if (cl_usb > 1500) {
 			mmi->cl_ebchg = cl_usb - EB_SPLIT_MA;
-			target_usb = EB_SPLIT_MA;
+			target_usb = cl_usb;
+			if (batt_soc < 100)
+				target_fcc = min(zone->fcc_norm_ma, EB_SPLIT_MA);
+			else
+				target_fcc = -EINVAL;
 		} else {
 			mmi->cl_ebchg = cl_usb;
-			target_usb = -EINVAL;
+			target_usb = cl_usb;
+			target_fcc = -EINVAL;
 		}
 
 		break;
@@ -9899,7 +9900,7 @@ static void mmi_heartbeat_work(struct work_struct *work)
 		if (((cl_usb - EB_SPLIT_MA) >= target_fcc) && eb_chrg_allowed)
 			mmi->cl_ebchg = EB_SPLIT_MA;
 
-		target_usb = cl_usb - mmi->cl_ebchg;
+		target_usb = cl_usb;
 		break;
 	case STEP_FULL:
 		target_fv = max_fv_mv;
@@ -9914,7 +9915,7 @@ static void mmi_heartbeat_work(struct work_struct *work)
 		if (((cl_usb - EB_SPLIT_MA) >= target_fcc) && eb_chrg_allowed)
 			mmi->cl_ebchg = EB_SPLIT_MA;
 
-		target_usb = cl_usb - mmi->cl_ebchg;
+		target_usb = cl_usb;
 		break;
 	case STEP_NONE:
 		target_fv = max_fv_mv;
@@ -9929,7 +9930,7 @@ static void mmi_heartbeat_work(struct work_struct *work)
 	case STEP_DEMO:
 		target_fv = DEMO_MODE_VOLTAGE;
 		target_fcc = zone->fcc_max_ma;
-		target_usb = cl_usb - mmi->cl_ebchg;
+		target_usb = cl_usb;
 		break;
 	default:
 		break;
