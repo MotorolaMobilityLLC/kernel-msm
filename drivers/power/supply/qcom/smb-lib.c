22b3cda8437e6b1d0d36fe9d96759144c8d55538
Kenny Kessler <kenny.kessler@motorola.com>
2019-03-01 11:43:09 -0600

    power: qcom: smb-lib: Use Max an Min Params
    The set function for charge parameters will drop and ignore
    calls that are out of bounds.  This is not necessary as
    in most instances the call is vali but the HW can't support
    it. So stub out the QCOM version and set tot he Max or Min
    when the calls is out of bounds.
    
    Change-Id: I55da55279529d8bd1a9e74aeff1a2bfb5a6ee812
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1316325
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Eyob Tesfu <Eyob.Tesfu@motorola.com>
    Reviewed-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -368,12 +368,25 @@ int smblib_set_charge_param(struct smb_charger *chg,
 		if (rc < 0)
 			return -EINVAL;
 	} else {
+#ifdef QCOM_BASE
 		if (val_u > param->max_u || val_u < param->min_u) {
 			smblib_err(chg, "%s: %d is out of range [%d, %d]\n",
 				param->name, val_u, param->min_u, param->max_u);
 			return -EINVAL;
 		}
-
+#else
+		if (val_u > param->max_u) {
+			smblib_err(chg,
+				   "%s: %d is out of range [%d] hold to max\n",
+				param->name, val_u, param->max_u);
+			val_u = param->max_u;
+		} else if (val_u < param->min_u) {
+			smblib_err(chg,
+				   "%s: %d is out of range [%d] hold to min\n",
+				param->name, val_u, param->min_u);
+			val_u = param->min_u;
+		}
+#endif
 		val_raw = (val_u - param->min_u) / param->step_u;
 	}
 
