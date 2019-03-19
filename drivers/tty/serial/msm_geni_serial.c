ad255b818abfa5be1828319530ae665ac8e08119
Qing Chang <a21550@motorola.com>
2019-03-19 12:45:19 -0500

    tty: msm_geni_serial: Support the match() callback
    The new match() callback for earlycon is to tell whether earlycon and
    the "real" console are the same device.
    
    Change-Id: I8b54d5d9f1d737650d0890fc9f596080c8b1aaf2
    Signed-off-by: Qing Chang <a21550@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1472629
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -149,6 +149,8 @@
 		ipc_log_string(ctx, x); \
 } while (0)
 
+#define MSM_CONSOLE_NAME	"ttyMSM"
+
 #define DMA_RX_BUF_SIZE		(2048)
 #define UART_CONSOLE_RX_WM	(2)
 
@@ -2832,6 +2834,13 @@ static void msm_geni_serial_cancel_rx(struct uart_port *uport)
 						GENI_FORCE_DEFAULT_REG);
 }
 
+static int
+msm_geni_serial_early_console_match(struct console *con, char *name, int idx,
+				    char *options)
+{
+	return (!name || strcmp(MSM_CONSOLE_NAME, name));
+}
+
 static int __init
 msm_geni_serial_earlycon_setup(struct earlycon_device *dev,
 		const char *opt)
@@ -2934,6 +2943,7 @@ msm_geni_serial_earlycon_setup(struct earlycon_device *dev,
 	geni_read_reg_nolog(uport->membase, GENI_SER_M_CLK_CFG);
 
 	dev->con->write = msm_geni_serial_early_console_write;
+	dev->con->match = msm_geni_serial_early_console_match;
 	dev->con->setup = NULL;
 	/*
 	 * Ensure that the early console setup completes before
@@ -2956,7 +2966,7 @@ static void console_unregister(struct uart_driver *drv)
 }
 
 static struct console cons_ops = {
-	.name = "ttyMSM",
+	.name = MSM_CONSOLE_NAME,
 	.write = msm_geni_serial_console_write,
 	.device = uart_console_device,
 	.setup = msm_geni_console_setup,
