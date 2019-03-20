65b091f9303358425121812500ef5d6a84c48b94
Mark Reed <mreed1@motorola.com>
2019-03-20 12:18:10 -0500

    - Disable emergency download mode.
    This issue was raised by the Tracfone security team. This mode
    should be secure, but there is always a possiblity some exploit
    could exist. We can disble for all products.
    
    EDL mode can still be reached via bootloader debug token on
    secure units.
    
    Change-Id: I3f5267c8228c03555b1e2a563a78fe0ceab09f4a
    Reviewed-on: https://gerrit.mot.com/1324417
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Dong-Hua Yan <a18317@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -165,6 +165,8 @@ static bool get_dload_mode(void)
 
 static void enable_emergency_dload_mode(void)
 {
+// IKSECURITY-2018 - Disable EDL mode
+#if 0
 	int ret;
 
 	if (emergency_dload_mode_addr) {
@@ -188,6 +190,7 @@ static void enable_emergency_dload_mode(void)
 	ret = scm_set_dload_mode(SCM_EDLOAD_MODE, 0);
 	if (ret)
 		pr_err("Failed to set secure EDLOAD mode: %d\n", ret);
+#endif
 }
 
 static int dload_set(const char *val, const struct kernel_param *kp)
