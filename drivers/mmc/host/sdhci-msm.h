054a090845ae74be4dd44e6ee8a7130ddaabb7c3
xuwt2 <xuwt2@lenovo.com>
2019-03-21 15:09:43 +0800

    mmc: host: Use clks_on to represent controller clocks
    This change removes redundant variable in sdchi_msm_host called
    controller_clock and instead makes use of clks_on variable only
    to represent status of controller clocks
    
    Change-Id: If8584cc5fadd9010bce8997cffbf4c0e8fcaf3f0
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1324851
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chuanchun Zhu <zhucc2@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -252,7 +252,6 @@ struct sdhci_msm_host {
 	struct device_attribute auto_cmd21_attr;
 	bool is_sdiowakeup_enabled;
 	bool sdio_pending_processing;
-	atomic_t controller_clock;
 	bool use_cdclp533;
 	bool use_updated_dll_reset;
 	bool use_14lpp_dll;
