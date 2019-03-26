30efc51d451ec9fa73e216d0e4a50f0da9801380
zhangjiano <zhangjo@motorola.com>
2019-03-26 11:06:49 +0800

    increase eeprom data size
    GM1 eeprom data total size is 10500, add size to
    12*1024
    
    Change-Id: Iab8e0c118332c361ab6ccd94c6e7837c243874eb
    Reviewed-on: https://gerrit.mot.com/1327036
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xu Ji <jixu@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Jian Zhang <zhangjo@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_cci/cam_cci_dev.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_cci/cam_cci_dev.h
@@ -68,7 +68,7 @@
 /* Max bytes that can be read per CCI read transaction */
 #define CCI_READ_MAX 256
 #define CCI_I2C_READ_MAX_RETRIES 3
-#define CCI_I2C_MAX_READ 8192
+#define CCI_I2C_MAX_READ 12288
 #define CCI_I2C_MAX_WRITE 8192
 #define CCI_I2C_MAX_BYTE_COUNT 65535
 
