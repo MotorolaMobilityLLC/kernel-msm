31e1c5c87127fb99e34c63884fa5975493d1bc0f
Igor Kovalenko <cik009@motorola.com>
2019-03-27 02:41:46 -0500

    net: usbnet: reduce Tx buffer for lower latency
    The most common use case is RNDIS host, which will perform poorly
    on TCP due to ACKs lingering in the USB Tx queue a while.
    
    The queue was originally very short, then was made longer for HS,
    and then was made even longer for SS. At that point it got so long
    that ACKs may chill for ~20ms+. Reduce the Tx depth by 10x, since
    we aren't likely to see huge uplink rates and latency is certainly
    more important. This would match what it was earlier, before the
    queue depth for increased 10x for improved Rx throughput.
    
    Change-Id: Ia0ef67fa6b6ef72b3599a7cac6990032be11a749
    CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/1327858
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -359,7 +359,7 @@ void usbnet_update_max_qlen(struct usbnet *dev)
 	switch (speed) {
 	case USB_SPEED_HIGH:
 		dev->rx_qlen = MAX_QUEUE_MEMORY / dev->rx_urb_size;
-		dev->tx_qlen = MAX_QUEUE_MEMORY / dev->hard_mtu;
+		dev->tx_qlen = MAX_QUEUE_MEMORY / dev->hard_mtu / 10;
 		break;
 	case USB_SPEED_SUPER:
 		/*
@@ -368,7 +368,7 @@ void usbnet_update_max_qlen(struct usbnet *dev)
 		 * work well
 		 */
 		dev->rx_qlen = 5 * MAX_QUEUE_MEMORY / dev->rx_urb_size;
-		dev->tx_qlen = 5 * MAX_QUEUE_MEMORY / dev->hard_mtu;
+		dev->tx_qlen = 5 * MAX_QUEUE_MEMORY / dev->hard_mtu / 10;
 		break;
 	default:
 		dev->rx_qlen = dev->tx_qlen = 4;
