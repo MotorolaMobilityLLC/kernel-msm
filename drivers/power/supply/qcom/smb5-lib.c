927a4b389cabe967d40485efc57717c1233c305a
Kenny Kessler <kenny.kessler@motorola.com>
2019-04-01 13:06:04 -0500

    power: smb5-lib: Add 50/50 EB Charge Split
    Some External Batteries are more then just a battery.
    They might also require the battery internally for a
    feature.  These types of EB will mark themselves with
    Internal Receive flag PARALLEL so that the Phone can
    50/50 split the Charger Power.  This will only happen
    with Turbo Chargers as anything less will lead to slow
    Phone charge times.
    
    Port-of: https://gerrit.mot.com/1283316
    
    Change-Id: I7f29d34f648b7203ff41768171260d1fe707c8f3
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1330828
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -7714,6 +7714,7 @@ end:
 }
 
 #define EB_RCV_NEVER BIT(7)
+#define EB_RCV_PARALLEL BIT(4)
 #define EB_SND_EXT BIT(2)
 #define EB_SND_LOW BIT(1)
 #define EB_SND_NEVER BIT(0)
@@ -7745,7 +7746,9 @@ static void mmi_check_extbat_ability(struct smb_charger *chip, char *able)
 		smblib_err(chip,
 			   "Could not read Receive Params rc = %d\n", rc);
 		*able |= EB_RCV_NEVER;
-	} else if ((ret.intval == POWER_SUPPLY_PTP_INT_RCV_NEVER) ||
+	}  else if (ret.intval == POWER_SUPPLY_PTP_INT_RCV_PARALLEL)
+		*able |= EB_RCV_PARALLEL;
+	else if ((ret.intval == POWER_SUPPLY_PTP_INT_RCV_NEVER) ||
 		   (ret.intval == POWER_SUPPLY_PTP_INT_RCV_UNKNOWN))
 		*able |= EB_RCV_NEVER;
 
@@ -8377,6 +8380,10 @@ void update_charging_limit_modes(struct smb_charger *chip, int batt_soc)
 		chip->mmi.charging_limit_modes = charging_limit_modes;
 }
 
+/* For Testing 50/50 Turbo Charge Split with EB */
+static int force_eb_fifty;
+module_param(force_eb_fifty, int, 0644);
+
 #define CHARGER_DETECTION_DONE 7
 #define SMBCHG_HEARTBEAT_INTERVAL_NS	70000000000
 #define HEARTBEAT_DELAY_MS 60000
@@ -8701,6 +8708,9 @@ static void mmi_heartbeat_work(struct work_struct *work)
 			   !mmi->usbeb_present &&
 			   !mmi->wls_present);
 
+	if (force_eb_fifty)
+		eb_able |= EB_RCV_PARALLEL;
+
 	mmi_get_extbat_out_volt(chip);
 	mmi_get_extbat_out_cl(chip);
 
@@ -8950,6 +8960,9 @@ static void mmi_heartbeat_work(struct work_struct *work)
 
 		if (((cl_usb - EB_SPLIT_MA) >= target_fcc) && eb_chrg_allowed)
 			mmi->cl_ebchg = EB_SPLIT_MA;
+		else if (eb_chrg_allowed && (eb_able & EB_RCV_PARALLEL) &&
+			 (mmi->charger_rate == POWER_SUPPLY_CHARGE_RATE_TURBO))
+			mmi->cl_ebchg = cl_usb / 2; /* 50/50 Split */
 
 		target_usb = cl_usb;
 		break;
@@ -8965,6 +8978,9 @@ static void mmi_heartbeat_work(struct work_struct *work)
 
 		if (((cl_usb - EB_SPLIT_MA) >= target_fcc) && eb_chrg_allowed)
 			mmi->cl_ebchg = EB_SPLIT_MA;
+		else if (eb_chrg_allowed && (eb_able & EB_RCV_PARALLEL) &&
+			 (mmi->charger_rate == POWER_SUPPLY_CHARGE_RATE_TURBO))
+			mmi->cl_ebchg = cl_usb / 2; /* 50/50 Split */
 
 		target_usb = cl_usb;
 		break;
