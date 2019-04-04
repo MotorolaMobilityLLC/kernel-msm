61ef32e75e21b9ff8972c647708f192433fa08f2
a7301c <a7301c@motorola.com>
2019-04-04 11:40:37 -0500

    Revert "(CR): smb5 : remove useless property"
    This reverts commit 2545359d3b12fa4f17c8c4d884d09336a5edd9a9.
    Which has conflict with change
    92fefd8 power: qpnp-smb5: Add VOLTAGE_VPH property
    
    Todo: Please help to bring it in later.
    
    Change-Id: I05453b18302e44c24f724d08102572091342092c
    Signed-off-by: a7301c <a7301c@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -6725,9 +6725,7 @@ irqreturn_t dc_plugin_irq_handler(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
 	struct smb_charger *chg = irq_data->parent_data;
-#ifdef QCOM_BASE
 	union power_supply_propval pval;
-#endif
 	int input_present;
 	bool dcin_present, vbus_present;
 	int rc, wireless_vout = 0;
@@ -6804,9 +6802,9 @@ irqreturn_t dc_plugin_irq_handler(int irq, void *data)
 
 		chg->last_wls_vout = 0;
 	}
-#endif
 	chg->mmi.dc_ebmax_current_ma = chg->mmi.dc_ebmax_current_ma_default;
 	chg->mmi.cl_ebsrc = chg->mmi.dc_ebmax_current_ma_default;
+
 	power_supply_changed(chg->dc_psy);
 
 	smblib_dbg(chg, (PR_WLS | PR_INTERRUPT), "dcin_present= %d, usbin_present= %d, cp_reason = %d\n",
