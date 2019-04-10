2b0f8e346f3a6441a36e93f9d1cc99e02add0d6b
dpxv34 <dpxv34@motorola.com>
2019-04-10 10:52:56 +0530

    Avoid deadlock during AXI restart
    In some rare scenario, when switching to portrait mode,
    AXI restart will be stuck waiting for buf_mgr lock as it
    is suspected to be in deadlock with other function. This
    results in camera to enter into bad state and it can't be
    recovered untill reboot. This lock is newly introduced
    by Qualcom in P. Removing the lock only during AXI restart
    to avoid such bad state of camera.
    
    Change-Id: I290fa5679437be52a02279ce52427418fbb9e458
    Reviewed-on: https://gerrit.mot.com/1334753
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Sumit Jamadar <xkbr38@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -927,7 +927,6 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 	case VIDIOC_MSM_ISP_AXI_RESTART:
 		mutex_lock(&vfe_dev->core_mutex);
 		MSM_ISP_DUAL_VFE_MUTEX_LOCK(vfe_dev);
-		mutex_lock(&vfe_dev->buf_mgr->lock);
 		if (atomic_read(&vfe_dev->error_info.overflow_state)
 			!= HALT_ENFORCED) {
 			rc = msm_isp_stats_restart(vfe_dev);
@@ -938,7 +937,6 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 			pr_err_ratelimited("%s: no AXI restart, halt enforced.\n",
 				__func__);
 		}
-		mutex_unlock(&vfe_dev->buf_mgr->lock);
 		MSM_ISP_DUAL_VFE_MUTEX_UNLOCK(vfe_dev);
 		mutex_unlock(&vfe_dev->core_mutex);
 		break;
