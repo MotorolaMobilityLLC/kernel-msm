a4cc2964b05377603ac16d4d53a6e8a0bb6bf398
Kenny Kessler <kenny.kessler@motorola.com>
2019-04-12 14:16:04 -0500

    net/usb: usbnet: Use Different Panel Notifier
    The Panel notifications mechanisms has changed so switch to
    new method.
    
    Change-Id: I598208db87392c48f89ec5ff9e820eb6086115e8
    Signed-off-by: Kenny Kessler <kenny.kessler@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1336417
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -46,8 +46,8 @@
 #include <linux/kernel.h>
 #include <linux/pm_runtime.h>
 
-#ifdef CONFIG_PANEL_NOTIFICATIONS
-#include <linux/panel_notifier.h>
+#ifdef CONFIG_DRM_MSM
+#include <linux/msm_drm_notify.h>
 #endif
 
 #define DRIVER_VERSION		"22-Aug-2005"
@@ -91,7 +91,7 @@ static int msg_level = -1;
 module_param (msg_level, int, 0);
 MODULE_PARM_DESC (msg_level, "Override default message level");
 
-#ifdef CONFIG_PANEL_NOTIFICATIONS
+#ifdef CONFIG_DRM_MSM
 int last_panel_state = -1;
 #endif
 /*-------------------------------------------------------------------------*/
@@ -1603,8 +1603,8 @@ void usbnet_disconnect (struct usb_interface *intf)
 	if (!dev)
 		return;
 
-#ifdef CONFIG_PANEL_NOTIFICATIONS
-	panel_unregister_notifier(&dev->panel_usb_notifier);
+#ifdef CONFIG_DRM_MSM
+	msm_drm_unregister_client(&dev->panel_usb_notifier);
 #endif
 
 	xdev = interface_to_usbdev (intf);
@@ -1657,7 +1657,7 @@ static struct device_type wwan_type = {
 	.name	= "wwan",
 };
 
-#ifdef CONFIG_PANEL_NOTIFICATIONS
+#ifdef CONFIG_DRM_MSM
 static void panel_usb_work(struct work_struct *work)
 {
 	struct usbnet *dev = container_of(work, struct usbnet,
@@ -1682,19 +1682,32 @@ static int panel_usb_notifier_call(struct notifier_block *nb,
 				   unsigned long event,
 				   void *data)
 {
+	int *blank;
+	struct msm_drm_notifier *evdata = data;
 	struct usbnet *dev = container_of(nb, struct usbnet,
 					  panel_usb_notifier);
 
+	if (!evdata || (evdata->id != 0)) {
+		pr_err("Panel Notifier missing data\n");
+		return 0;
+	}
+
+	blank = evdata->data;
+
 	switch (event) {
-	case PANEL_EVENT_DISPLAY_ON:
-		pr_err("USB Panel ON\n");
-		dev->panel_state = 1;
-		schedule_work(&dev->panel_update_work);
+	case MSM_DRM_EVENT_BLANK:
+		if (blank && (*blank == MSM_DRM_BLANK_UNBLANK)) {
+			pr_err("USB Panel ON\n");
+			dev->panel_state = 1;
+			schedule_work(&dev->panel_update_work);
+		}
 		break;
-	case PANEL_EVENT_PRE_DISPLAY_OFF:
-		pr_err("USB Panel OFF\n");
-		dev->panel_state = 0;
-		schedule_work(&dev->panel_update_work);
+	case MSM_DRM_EARLY_EVENT_BLANK:
+		if (blank && (*blank == MSM_DRM_BLANK_POWERDOWN)) {
+			pr_err("USB Panel OFF\n");
+			dev->panel_state = 0;
+			schedule_work(&dev->panel_update_work);
+		}
 		break;
 	default:
 		break;
@@ -1875,7 +1888,7 @@ usbnet_probe (struct usb_interface *udev, const struct usb_device_id *prod)
 	if (dev->driver_info->flags & FLAG_LINK_INTR)
 		usbnet_link_change(dev, 0, 0);
 
-#ifdef CONFIG_PANEL_NOTIFICATIONS
+#ifdef CONFIG_DRM_MSM
 	INIT_WORK(&dev->panel_update_work, panel_usb_work);
 	if (last_panel_state != -1) {
 		dev->panel_state = last_panel_state;
@@ -1885,7 +1898,7 @@ usbnet_probe (struct usb_interface *udev, const struct usb_device_id *prod)
 		schedule_work(&dev->panel_update_work);
 	}
 	dev->panel_usb_notifier.notifier_call = panel_usb_notifier_call;
-	status = panel_register_notifier(&dev->panel_usb_notifier);
+	status = msm_drm_register_client(&dev->panel_usb_notifier);
 	if (status)
 		pr_err("USBNET Probe: Panel Notifier Failure %d\n", status);
 #endif
