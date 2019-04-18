438ccbd14e3d0f0d9b75955a5d96fb173897d8e4
kunleiz <kunleiz@codeaurora.org>
2019-04-18 18:50:41 +0800

    lsm: check payload size validity before using it as array index
    Payload size validity is not checked before using it in array index.
    Check payload size to avoid out-of-boundary memory.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2019-10512
    CRs-Fixed: 2380702
    
    Change-Id: Ic0b06bb331fc1753ff7543bb218ab12d6a4a3ca8
    Signed-off-by: Kunlei Zhang <kunleiz@codeaurora.org>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1367187
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- include/sound/q6lsm.h
+++ include/sound/q6lsm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, 2019 Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2016, 2019 Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
