b8f73d08296cbb8b0391c5ebe1e5ef4e55ed90cc
daiss1 <daiss1@lenovo.com>
2019-04-18 13:17:10 +0800

    gpu/msm: exchange brghtness high-8-bit and low-8-bit
    Add flag qcom,bklt-dcs-2bytes-0xff0f-enabled
    Enable qcom,bklt-dcs-2bytes-0xff0f-enabled,can set 0xff0f backlight.
    
    Change-Id: I1b8df2026101d7c778fa3c7a035143ebe50ab36d
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1338696
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -612,6 +612,7 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 {
 	int rc = 0;
 	struct mipi_dsi_device *dsi;
+	struct dsi_backlight_config *bl = &panel->bl_config;
 
 	if (!panel || (bl_lvl > 0xffff)) {
 		pr_err("invalid params\n");
@@ -619,6 +620,9 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	}
 
 	dsi = &panel->mipi_device;
+
+       if (bl->bl_2bytes_0xff0f_enable)
+		bl_lvl = ((bl_lvl & 0x000f) << 8) |(bl_lvl >> 4);
 
 	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
@@ -2320,6 +2324,12 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 	} else {
 		panel->bl_config.brightness_default_level = val;
 	}
+
+	panel->bl_config.bl_2bytes_0xff0f_enable = utils->read_bool(utils->data,
+			"qcom,bklt-dcs-2bytes-0xff0f-enabled");
+
+	pr_info("[%s] bl_2bytes_0xff0f_enable=%d\n", panel->name,
+					panel->bl_config.bl_2bytes_0xff0f_enable);
 
 	if (panel->bl_config.type == DSI_BACKLIGHT_PWM) {
 		rc = dsi_panel_parse_bl_pwm_config(panel);
