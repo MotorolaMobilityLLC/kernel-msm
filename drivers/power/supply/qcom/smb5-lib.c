2227a852bc3d6708e819c260d13a54d40e5e6f48
Haijian Ma <mahj8@motorola.com>
2019-06-25 17:58:50 +0800

    smb5-lib: config DCP current to 2A
    As OD, config DCP current
    from 1.5A to 2A
    
    Change-Id: I802fceb5d0d49555677aa0e7afcd13b8c6aa31fd
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1376322
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1502917

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -4353,6 +4353,8 @@ static int get_rp_based_dcp_current(struct smb_charger *chg, int typec_mode)
 		rp_ua = TYPEC_HIGH_CURRENT_UA;
 		break;
 	case POWER_SUPPLY_TYPEC_SOURCE_MEDIUM:
+		rp_ua = TYPEC_MEDIUM_CURRENT_UA;
+		break;
 	case POWER_SUPPLY_TYPEC_SOURCE_DEFAULT:
 	/* fall through */
 	default:
@@ -5078,7 +5080,11 @@ int smblib_get_charge_current(struct smb_charger *chg,
 			current_ua = CDP_CURRENT_UA;
 			break;
 		case DCP_CHARGER_BIT:
+			current_ua = DCP_CURRENT_UA;
+			break;
 		case OCP_CHARGER_BIT:
+			current_ua = OCP_CURRENT_UA;
+			break;
 		case FLOAT_CHARGER_BIT:
 			current_ua = DCP_CURRENT_UA;
 			break;
@@ -5101,7 +5107,11 @@ int smblib_get_charge_current(struct smb_charger *chg,
 			current_ua = CDP_CURRENT_UA;
 			break;
 		case DCP_CHARGER_BIT:
+			current_ua = DCP_CURRENT_UA;
+			break;
 		case OCP_CHARGER_BIT:
+			current_ua = OCP_CURRENT_UA;
+			break;
 		case FLOAT_CHARGER_BIT:
 			current_ua = chg->default_icl_ua;
 			break;
