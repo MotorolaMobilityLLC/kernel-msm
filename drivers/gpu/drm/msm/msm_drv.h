ef708a89d445917fefbaeba04285cf75e7178332
zhanggb <zhanggb@motorola.com>
2019-06-27 19:51:53 +0800

    Display PanelMode: Add CABC control property
    Add CABC control interface for video mode panel. This interface is
    also used by sdc connector property.
    
    Change-Id: Ic92a152c7db63efe2278b60996fe6d6e3fc38ed0
    Reviewed-on: https://gerrit.mot.com/1382393
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    
    Conflicts:
    	drivers/gpu/drm/msm/dsi-staGing/dsi_panel.c
    Change-Id: I2de175013c8fc1693efc3a4327a1540af6f4ff61
    Reviewed-on: https://gerrit.mot.com/1413189
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -203,6 +203,7 @@ enum msm_mdp_conn_property {
 
 	/* MOT feature panel*/
 	CONNECTOR_PROP_HBM,
+	CONNECTOR_PROP_CABC,
 	CONNECTOR_PROP_ACL,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
@@ -321,6 +322,7 @@ enum msm_param_state {
 
 enum msm_param_id {
 	PARAM_HBM_ID = 0,
+	PARAM_CABC_ID,
 	PARAM_ACL_ID,
 	PARAM_ID_NUM
 };
