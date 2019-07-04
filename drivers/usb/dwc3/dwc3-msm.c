abf73bbd118ab39b4a07a951dd3381178a2bcfad
Huosheng Liao <liaohs@motorola.com>
2019-07-04 02:54:08 +0000

    Revert "(CR): dwc3: adjust the error count and reset logic"
    This reverts commit 32da6604a29d187b05f99e898c4b39f00a2c2eaf.
    
    When the usb irq flooding happens, 50 times continous usb
    controller reset for error still causes the soft lockup for
    hung tasks.So reduce it to 5 times reset for controller error.
    when err intterrupt happens more than 5 times , dont do usb reset .
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I8921c95ceb249f797ea65386891730edc851f4e9
    Reviewed-on: https://gerrit.mot.com/1381369
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2269,7 +2269,7 @@ static void dwc3_msm_vbus_draw_work(struct work_struct *w)
 	dwc3_msm_gadget_vbus_draw(mdwc, dwc->vbus_draw);
 }
 
-#define MAX_ERR_CNT 50
+#define MAX_ERR_CNT 5
 static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 							unsigned int value)
 {
@@ -2282,8 +2282,7 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 	switch (event) {
 	case DWC3_CONTROLLER_ERROR_EVENT:
 		/* Avoid a flood of Error events */
-		if ((dwc->err_cnt++ >= MAX_ERR_CNT) &&
-		    (dwc->err_cnt % MAX_ERR_CNT))
+		if (dwc->err_cnt++ >= MAX_ERR_CNT)
 			break;
 
 		dev_info(mdwc->dev,
