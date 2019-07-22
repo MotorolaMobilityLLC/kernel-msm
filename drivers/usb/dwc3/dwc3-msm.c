30a426bb622c09796015c51a02b12c52a23bfb2b
Yonghua Yan <yanyh2@motorola.com>
2019-07-22 13:39:23 +0800

    Revert "(CR): usb: dwc3: Avoid DWC3_CONTROLLER_ERROR flood"
    Apply the QC official patch for this issue.
    
    Mot-CRs-fixed: (CR)
    
    This reverts commit 57a1aff4471c1cf0cfe2bb58512cb62e177bc6d4.
    
    Change-Id: I0640825a75ef04f63468ad2175bae0fc2986a0c6
    Reviewed-on: https://gerrit.mot.com/1389246
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2375,7 +2375,6 @@ static void dwc3_msm_vbus_draw_work(struct work_struct *w)
 	dwc3_msm_gadget_vbus_draw(mdwc, dwc->vbus_draw);
 }
 
-#define MAX_ERR_CNT 5
 static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 							unsigned int value)
 {
@@ -2387,10 +2386,6 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 
 	switch (event) {
 	case DWC3_CONTROLLER_ERROR_EVENT:
-		/* Avoid a flood of Error events */
-		if (dwc->err_cnt++ >= MAX_ERR_CNT)
-			break;
-
 		dev_info(mdwc->dev,
 			"DWC3_CONTROLLER_ERROR_EVENT received, irq cnt %lu\n",
 			dwc->irq_cnt);
@@ -5001,9 +4996,6 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 		}
 	}
 
-	/* Reset Controller Error Count */
-	dwc->err_cnt = 0;
-
 	if (on) {
 		dev_dbg(mdwc->dev, "%s: turn on host\n", __func__);
 
@@ -5167,9 +5159,6 @@ static void dwc3_override_vbus_status(struct dwc3_msm *mdwc, bool vbus_present)
 static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 {
 	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
-
-	/* Reset Controller Error Count */
-	dwc->err_cnt = 0;
 
 	pm_runtime_get_sync(mdwc->dev);
 	dbg_event(0xFF, "StrtGdgt gsync",
