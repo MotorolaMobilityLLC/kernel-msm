c08f26a2d6572dfc75796deb09ec43f10af0c85e
Yonghua Yan <yanyh2@motorola.com>
2019-07-23 21:04:26 +0800

    smb5: handle cc pin floated cable removal
    When use the CC pin floated USB cable to connect to
    PC or charger, typec mode cannot be detected, so it
    cannot use the typec removal when USB cable is removed.
    Since this USB cable make the device function as micro
    usb, so just call the micro usb removal.
    
    Change-Id: I6cf9fbcffa1fe05bade64dfce9309977e782eba4
    Signed-off-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1390306
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1502918
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -5786,7 +5786,8 @@ void smblib_usb_plugin_locked(struct smb_charger *chg)
 		smblib_update_usb_type(chg);
 	}
 
-	if (chg->connector_type == POWER_SUPPLY_CONNECTOR_MICRO_USB)
+	if (chg->connector_type == POWER_SUPPLY_CONNECTOR_MICRO_USB ||
+	    chg->typec_mode == POWER_SUPPLY_TYPEC_NONE)
 		smblib_micro_usb_plugin(chg, vbus_rising);
 
 	vote(chg->temp_change_irq_disable_votable, DEFAULT_VOTER,
