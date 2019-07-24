6d365362e3a4b05a838f7a41e5cc7ca8f3fca9ea
daiss1 <daiss1@lenovo.com>
2019-07-24 17:09:56 +0800

    doha: Modify hbm code,delect goto error
    Modify hbm code
    delect goto error
    
    Change-Id: I4cc241023a83c6e7e655fa14496d03c45c186c43
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1423593
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2850,12 +2850,10 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 			pr_warn("[%s] failed to get bklt gpio, rc=%d\n",
 						panel->name, rc);
 			rc = -EPROBE_DEFER;
-			goto error;
 		} else {
 			pr_warn("[%s] failed to get bklt gpio, rc=%d\n",
 						panel->name, rc);
 			rc = 0;
-			goto error;
 		}
 	}
 
@@ -2867,12 +2865,10 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 			pr_warn("[%s] failed to get hbm gpio, rc=%d\n",
 						panel->name, rc);
 			rc = -EPROBE_DEFER;
-			goto error;
 		} else {
 			pr_warn("[%s]failed to get hbm gpio, rc=%d\n",
 						panel->name, rc);
 			rc = 0;
-			goto error;
 		}
 	}
 
