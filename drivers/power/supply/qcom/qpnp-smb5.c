c609414558d73594ab1cf0a8707de1dc697f7338
Yonghua Yan <yanyh2@motorola.com>
2019-07-26 19:23:24 +0800

    smb5: adjust USBIN OV debounce condition
    Adjust the USBIN OV debounce trigger condition.
    Use chg_done help to decide charge FULL state.
    
    Change-Id: I83d0cae6b85645390580f79ec5435c316f26d534
    Signed-off-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1392701
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1502920
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2018,6 +2018,7 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 			msleep(50);
 			vote(chg->chg_disable_votable, FORCE_RECHARGE_VOTER,
 					false, 0);
+		chg->chg_done = 0;
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		chg->fcc_stepper_enable = val->intval;
