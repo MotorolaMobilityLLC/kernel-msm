393dfbe808bdd11edcfb40fcccf4ccce0fefd47e
Ben Hutchings <ben@decadent.org.uk>
2019-08-03 17:11:42 +0100

    tcp: Clear sk_send_head after purging the write queue
    Denis Andzakovic discovered a potential use-after-free in older kernel
    versions, using syzkaller.  tcp_write_queue_purge() frees all skbs in
    the TCP write queue and can leave sk->sk_send_head pointing to freed
    memory.  tcp_disconnect() clears that pointer after calling
    tcp_write_queue_purge(), but tcp_connect() does not.  It is
    (surprisingly) possible to add to the write queue between
    disconnection and reconnection, so this needs to be done in both
    places.
    
    This bug was introduced by backports of commit 7f582b248d0a ("tcp:
    purge write queue in tcp_connect_init()") and does not exist upstream
    because of earlier changes in commit 75c119afe14f ("tcp: implement
    rb-tree based retransmit queue").  The latter is a major change that's
    not suitable for stable.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2020-0040
    Bug: 143009752
    
    Change-Id: I97a1a1f3f753b950984e48af6c28cfd4a346db8a
    Reported-by: Denis Andzakovic <denis.andzakovic@pulsesecurity.co.nz>
    Bisected-by: Salvatore Bonaccorso <carnil@debian.org>
    Fixes: 7f582b248d0a ("tcp: purge write queue in tcp_connect_init()")
    Cc: <stable@vger.kernel.org> # before 4.15
    Cc: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1498082
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1551,6 +1551,8 @@ struct tcp_fastopen_context {
 	struct rcu_head		rcu;
 };
 
+static inline void tcp_init_send_head(struct sock *sk);
+
 /* write queue abstraction */
 static inline void tcp_write_queue_purge(struct sock *sk)
 {
@@ -1558,6 +1560,7 @@ static inline void tcp_write_queue_purge(struct sock *sk)
 
 	while ((skb = __skb_dequeue(&sk->sk_write_queue)) != NULL)
 		sk_wmem_free_skb(sk, skb);
+	tcp_init_send_head(sk);
 	sk_mem_reclaim(sk);
 	tcp_clear_all_retrans_hints(tcp_sk(sk));
 	tcp_init_send_head(sk);
