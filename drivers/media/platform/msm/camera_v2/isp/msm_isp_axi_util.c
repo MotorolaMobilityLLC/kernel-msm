a53a790cf6763cd3dba27b104e8de716c58464a1
libm1 <libm1@lenovo.com>
2019-08-07 08:53:41 +0800

    msm: camera_v2: reject the late request
    https://gerrit.mot.com/#/c/1390318/ missing some code change from QC patch,
    which lead to camera capture is more often to occur continuous frame drop,
    then camera freeze occur and error pop up with patch.
    So add the missing change follow the QC patch.
    
    CRs-fixed: (CR)
    
    Change-Id: I167ee4d815fb238373af663a92b86e7d6f951d2e
    Signed-off-by: libm1 <libm1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1398136
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Biming Li <libm1@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Jingjing Xiao <xiaojj1@motorola.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -631,8 +631,7 @@ static void msm_isp_update_framedrop_reg(struct msm_vfe_axi_stream *stream_info,
 	if (stream_info->undelivered_request_cnt > 0)
 		stream_info->current_framedrop_period =
 			MSM_VFE_STREAM_STOP_PERIOD;
-	if (stream_info->controllable_output && drop_reconfig == 1)
-		stream_info->current_framedrop_period = 1;
+
 	/*
 	 * re-configure the period pattern, only if it's not already
 	 * set to what we want
