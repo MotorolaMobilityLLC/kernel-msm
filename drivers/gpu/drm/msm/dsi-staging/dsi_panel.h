d9996f8529884b6d447b293115ae8345daa3d377
daiss1 <daiss1@lenovo.com>
2019-08-18 16:26:10 +0800

    gpu: Check the return status of the touch screen
    Check the return status of the touch screen
    Judging the sleeping state of LCD according
    to the return state of touch screen
    
    Change-Id: I4c58d45ad7974437e2ad3606c24678b34ede53ac
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1414553
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -288,6 +288,8 @@ struct dsi_panel {
 	struct panel_hbm hbm_config;
 
 	struct panel_param *param_cmds;
+	bool lcd_not_sleep;
+	bool tp_state_check;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
