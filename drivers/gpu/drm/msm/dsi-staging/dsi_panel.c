649bdec87be7e60eb8bfeb6a4153fb21a66830c6
houdz1 <houdz1@motorola.com>
2019-08-19 17:41:00 +0800

    msm/dsi_panel: set reset_gpio to low when panel power off
    The driver_only code from qcom include the two lines of codes.
    They are removed by 8b99277a25dfc4d0f75a83ce8b56412c15325a9b.
    So we must add the codes back.
    If Some projects can not set reset_gpio to low ,
    we will add label to distinguish.
    
    Change-Id: I70836f1782879619b646031b85bf88eccc4ec18e
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1405553
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -623,6 +623,8 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	if (gpio_is_valid(panel->reset_config.disp_en_gpio))
 		gpio_set_value(panel->reset_config.disp_en_gpio, 0);
 
+	if (gpio_is_valid(panel->reset_config.reset_gpio))
+		gpio_set_value(panel->reset_config.reset_gpio, 0);
 
 	if (gpio_is_valid(panel->reset_config.lcd_mode_sel_gpio))
 		gpio_set_value(panel->reset_config.lcd_mode_sel_gpio, 0);
