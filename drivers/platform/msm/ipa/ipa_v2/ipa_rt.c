49b51907bf7c58febaea1f1947c246f9bcc1e709
Mohammed Javid <mjavid@codeaurora.org>
2019-08-26 16:46:31 +0530

    msm: ipa: Protect ipa Defenderault routing table
    Protect ipa Defenderault routing table from
    addition, deletion and modification once after
    Defenderault rule added by ipa-driver.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2018-13900
    CRs-Fixed: 2287499
    BUG: 119052051
    
    Change-Id: I045d9c29fed23edf796d826e440b81124e1f666a
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1410262
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1501891
    (cherry picked from commit 2f35c0c111e4ea644c80365f651a6a4aea89f66e)

--- drivers/platform/msm/ipa/ipa_v2/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_rt.c
@@ -1062,13 +1062,12 @@ static int __ipa_add_rt_rule(enum ipa_ip_type ip, const char *name,
 		goto error;
 	}
 	/*
-	 * do not allow any rules to be added at end of the "default" routing
+	 * do not allow any rule to be added at "default" routing
 	 * tables
 	 */
 	if (!strncmp(tbl->name, IPA_DFLT_RT_TBL_NAME, IPA_RESOURCE_NAME_MAX) &&
-	    (tbl->rule_cnt > 0) && (at_rear != 0)) {
-		IPAERR("cannot add rule at end of tbl rule_cnt=%d at_rear=%d\n",
-		       tbl->rule_cnt, at_rear);
+	    (tbl->rule_cnt > 0)) {
+		IPAERR("cannot add rules to default rt table\n");
 		goto error;
 	}
 
@@ -1601,6 +1600,11 @@ static int __ipa_mdfy_rt_rule(struct ipa_rt_rule_mdfy *rtrule)
 	if (entry->cookie != IPA_RT_RULE_COOKIE) {
 		IPAERR_RL("bad params\n");
 		goto error;
+	}
+
+	if (!strcmp(entry->tbl->name, IPA_DFLT_RT_TBL_NAME)) {
+		IPAERR_RL("Default tbl rule cannot be modified\n");
+		return -EINVAL;
 	}
 
 	/* Adding check to confirm still
