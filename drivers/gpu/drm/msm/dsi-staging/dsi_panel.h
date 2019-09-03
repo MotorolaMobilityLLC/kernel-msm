82e83bd24bf03904a07323fbf3c6563a4c51731a
wangyq13 <wangyq13@lenovo.com>
2019-09-03 19:42:44 +0800

    drm: dsi-staging: add bklt-dcs-2bytes-0x0fff-enabled feature
    Add bklt-dcs-2bytes-0x0fff-enabled feature.
    Some panels use 0x0fff format to adjust backlight brightness.
    
    Change-Id: Ib0febc1017e9ff552844129095088f8b5c5406da
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1414577
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -115,6 +115,7 @@ struct dsi_backlight_config {
 	enum bl_update_flag bl_update;
 
 	bool bl_2bytes_0xff0f_enable;
+	bool bl_2bytes_0x0fff_enable;
 	bool bl_2bytes_enable;
 	u32 bl_min_level;
 	u32 bl_max_level;
