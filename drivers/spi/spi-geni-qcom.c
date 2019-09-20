703c44f386ad1f8b70f3974ef6a937b0e817e0b2
haojl2 <haojl2@motorola.com>
2019-09-20 16:28:35 +0800

    Revert "driver/spi: do not run force suspend for mods spi"
    Do not force suspend for all spi bus.
    This will not cause power issue, and can fix clock is closed when SPI
    sync message.
    
    This reverts commit 073d0de747e4133e0b27ad0f330862e868031b28.
    
    Change-Id: I75be98ff4f145457151f7e1ab95dcd9c3b7c9a78
    Reviewed-on: https://gerrit.mot.com/1426060
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jialei Hao <haojl2@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -1956,7 +1956,7 @@ static int spi_geni_resume(struct device *dev)
 static int spi_geni_suspend(struct device *dev)
 {
 	int ret = 0;
-
+#if 0
 	if (!pm_runtime_status_suspended(dev)) {
 		struct spi_master *spi = get_spi_master(dev);
 		struct spi_geni_master *geni_mas = spi_master_get_devdata(spi);
@@ -1977,6 +1977,7 @@ static int spi_geni_suspend(struct device *dev)
 			ret = -EBUSY;
 		}
 	}
+#endif
 	return ret;
 }
 #else
