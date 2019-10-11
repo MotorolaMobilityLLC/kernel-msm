7360b99ad282e169196fd4a242e8e7eaabbf8000
Jia He <justin.he@arm.com>
2019-10-11 22:09:36 +0800

    UPSTREAM: arm64: cpufeature: introduce helper cpu_has_hw_af()
    We unconditionally set the HW_AFDBM capability and only enable it on
    CPUs which really have the feature. But sometimes we need to know
    whether this cpu has the capability of HW AF. So decouple AF from
    DBM by a new helper cpu_has_hw_af().
    
    If later we noticed a potential performance issue on this path, we can
    turn it into a static label as with other CPU features.
    Moto-CRs-Fixed:(CR)
    
    Signed-off-by: Jia He <justin.he@arm.com>
    Suggested-by: Suzuki Poulose <Suzuki.Poulose@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 47d7b15b88f96a90694cfc607d0717d62dff6c45)
    Bug: 228114874
    Signed-off-by: Yu Zhao <yuzhao@google.org>
    Change-Id: Ie0c4482c4cc0d23d75ed22bfaedb7e32a448f293
    Reviewed-on: https://gerrit.mot.com/2268682
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -572,6 +572,19 @@ bool is_spectre_bhb_affected(const struct arm64_cpu_capabilities *entry, int sco
 u8 spectre_bhb_loop_affected(int scope);
 void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *__unused);
 
+/* Check whether hardware update of the Access flag is supported */
+static inline bool cpu_has_hw_af(void)
+{
+	u64 mmfr1;
+
+	if (!IS_ENABLED(CONFIG_ARM64_HW_AFDBM))
+		return false;
+
+	mmfr1 = read_cpuid(ID_AA64MMFR1_EL1);
+	return cpuid_feature_extract_unsigned_field(mmfr1,
+						ID_AA64MMFR1_HADBS_SHIFT);
+}
+
 #endif /* __ASSEMBLY__ */
 
 #endif
