cf69009186f48f17f20e6f04f9b32d65087d13d8
Tharun Kumar Merugu <mtharu@codeaurora.org>
2019-10-14 18:18:27 +0530

    msm: adsprpc: Fix integer overflow in refcount of map
    Integer overflow in refcount of map is leading to use after free. Error
    out if refcount reaches INT_MAX
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2019-10585
    CRs-Fixed: 2457975
    
    Change-Id: I21e88361a8e70ef8c5c9593f1fc0ddd2b351a55a
    Acked-by: Himateja Reddy <hmreddy@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1436354
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <Raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -406,6 +406,10 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 			if (va >= map->va &&
 				va + len <= map->va + map->len &&
 				map->fd == fd) {
+				if (map->refs + 1 == INT_MAX) {
+					spin_unlock(&me->hlock);
+					return -ETOOMANYREFS;
+				}
 				map->refs++;
 				match = map;
 				break;
@@ -418,6 +422,10 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 			if (va >= map->va &&
 				va + len <= map->va + map->len &&
 				map->fd == fd) {
+				if (map->refs + 1 == INT_MAX) {
+					spin_unlock(&fl->hlock);
+					return -ETOOMANYREFS;
+				}
 				map->refs++;
 				match = map;
 				break;
