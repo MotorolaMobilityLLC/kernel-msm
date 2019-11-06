45f0e56df36c28d184d96406a7d7a49775535f55
Ryan Lattrel <ryanl@motorola.com>
2019-11-06 17:58:59 -0600

    drivers: power: smb5-lib: add dc in en override
    Add an override for dc_in_en via the property
    POWER_SUPPLY_PROP_PIN_ENABLED on the dc supply.
    
    The wireless charger needs to be able to set this to
    prevent the dc pluGin irq from running when tx_mode
    is enabled via the boost.
    
    Change-Id: I7a05bea4fed0f4d9edaad6580c65f3b9962489ef
    Signed-off-by: Ryan Lattrel <ryanl@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1450500
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Cesar Augusto Marcelino dos Santos <cmsantos@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -735,6 +735,8 @@ int smblib_get_prop_voltage_wls_output(struct smb_charger *chg,
 int smblib_set_prop_voltage_wls_output(struct smb_charger *chg,
 				const union power_supply_propval *val);
 int smblib_set_prop_dc_reset(struct smb_charger *chg);
+int smblib_set_prop_dc_en(struct smb_charger *chg,
+				    const union power_supply_propval *val);
 int smblib_get_prop_usb_present(struct smb_charger *chg,
 				union power_supply_propval *val);
 int smblib_get_prop_usb_online(struct smb_charger *chg,
