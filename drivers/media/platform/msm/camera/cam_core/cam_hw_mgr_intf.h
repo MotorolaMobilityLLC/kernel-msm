79a0b8b2589de791d3f393e0c2cc1bbf133d03a2
shangbl1 <shangbl1@lenovo.com>
2019-11-30 23:16:29 +0800

    camera: revert "isp: Skip reapply of DMI config on bubble recovery"
    keep camera version LA.UM.8.11.r1-01600-NICOBAR.0
    Revert "msm: camera: isp: Skip reapply of DMI config on bubble recovery"
    
    This reverts commit 9f5b927b8eedb2f7ce25751aa4067fc3a2325ca3.
    
    Change-Id: I2702d6e308046e46a3b6541bdbb22c015c06a9fe
    Signed-off-by: shangbl1 <shangbl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1464167
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Reviewed-by: Zilong Zhang <zhangzl7@lenovo.com>
    Reviewed-by: Dawei Wang <wangdw10@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera/cam_core/cam_hw_mgr_intf.h
+++ drivers/media/platform/msm/camera/cam_core/cam_hw_mgr_intf.h
@@ -211,7 +211,6 @@ struct cam_hw_stream_setttings {
  * @num_out_map_entries:   Number of out map entries
  * @priv:                  Private pointer
  * @request_id:            Request ID
- * @reapply                True if reapplying after bubble
  *
  */
 struct cam_hw_config_args {
@@ -223,7 +222,6 @@ struct cam_hw_config_args {
 	void                           *priv;
 	uint64_t                        request_id;
 	bool                            init_packet;
-	bool                            reapply;
 };
 
 /**
