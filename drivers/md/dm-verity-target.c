f7924b65922481087d1665de38b421a8d5111135
Tiago Ganselli <ganselli@motorola.com>
2019-12-02 20:58:09 -0300

    Revert "dm verity: Re-run FEC for each FEC-ed block"
    This commit was originally a partial fix for:
    b804e916 - "Port Non-Continuous Verity to 8998"
    
    Reverting as part of: https://gerrit.mot.com/#/c/1464748/
    
    This reverts commit 2bff1c8e870e8df5c40f8c5d7f5523942f226df1.
    
    Change-Id: Ic6fe2ff60aaa664097edf166184ba3b5bad0a4aa
    Signed-off-by: Tiago Ganselli <ganselli@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1464751
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Rafael Ortolan <rafones@motorola.com>
    Reviewed-by: Marcos Fukai Inoue <marcosi@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -519,7 +519,7 @@ static int verity_verify_io(struct dm_verity_io *io)
 		}
 		else if (verity_fec_decode(v, io, DM_VERITY_BLOCK_TYPE_DATA,
 					   cur_block, NULL, &start) == 0)
-			continue;
+			add_to_verified_cache(io, b);
 		else if (verity_handle_err(v, DM_VERITY_BLOCK_TYPE_DATA,
 					   cur_block))
 			return -EIO;
