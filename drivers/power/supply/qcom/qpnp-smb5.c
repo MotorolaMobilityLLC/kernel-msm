2ee88f4eaa34c601284f1ca5c8830d57e4a2eb7b
Haijian Ma <mahj8@motorola.com>
2019-12-06 17:33:52 +0800

    smb5: disable Type-c FMB function
    Qcom PM7250B uses GPIO9 and GPIO10 for FMB control, and this two gpios
    can be repurposed for other use.
    This two gpios' function would be impacted when HUAWEI P10 cale is inserted
    with FMB enabled. So disable type-c FMB function in case GPIO9 and
    GPIO10 reused for other purpose.
    
    Change-Id: I8a6453067cc24a91d82d39037243631a91ee5b08
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1467621
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1500769
    Reviewed-by: Cesar Augusto Marcelino dos Santos <cmsantos@motorola.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2273,11 +2273,11 @@ static int smb5_configure_typec(struct smb_charger *chg)
 	}
 
 	/*
-	 * Config SCHG_P_TYPEC_TYPE_C_CCOUT_CONTROL to 0x17
+	 * Config SCHG_P_TYPEC_TYPE_C_CCOUT_CONTROL to 0x03
 	 * Enable detection of debug accessory in sink mode
 	 */
 	rc = smblib_masked_write(chg, TYPE_C_DEBUG_ACCESS_SINK_REG,
-				      TYPEC_DEBUG_ACCESS_SINK_MASK, 0x17);
+				      TYPEC_DEBUG_ACCESS_SINK_MASK, 0x03);
 	if (rc < 0) {
 		dev_err(chg->dev,
 			"Couldn't configure TYPE_C_DEBUG_ACCESS_SINK_REG rc=%d\n",
