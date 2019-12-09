b163c580fc5e9df8a35f12ef66f7594f4d74b39d
Hardik Arya <harya@codeaurora.org>
2019-12-09 18:49:03 +0530

    diag: Clearing the mask before switching logGing mode
    Currently there is a possibility of not clearing a mask
    when logGing mode is switched via ioctl. The patch adds
    clearing of mask before switching logGing mode from ioctl.
    CRs-Fixed: 2206931
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2019-10606
    CRs-Fixed: 2192810
    BUG: 142269492
    
    Change-Id: Iaff41e67c963b2333be0cf5853ad657764a71cc1
    Signed-off-by: Hardik Arya <harya@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1469527
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1493947
    Tested-by: Raja Phani K <a16821@motorola.com>
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Sindhu C <a12924@motorola.com>
    SLTApproved: Sindhu C <a12924@motorola.com>

--- drivers/char/diag/diag_masks.c
+++ drivers/char/diag/diag_masks.c
@@ -1807,14 +1807,6 @@ int diag_copy_to_user_msg_mask(char __user *buf, size_t count,
 	if (!mask_info)
 		return -EIO;
 
-	mutex_lock(&driver->diag_maskclear_mutex);
-	if (driver->mask_clear) {
-		DIAG_LOG(DIAG_DEBUG_PERIPHERALS,
-		"diag:%s: count = %zu\n", __func__, count);
-		mutex_unlock(&driver->diag_maskclear_mutex);
-		return -EIO;
-	}
-	mutex_unlock(&driver->diag_maskclear_mutex);
 	mutex_lock(&mask_info->lock);
 	mutex_lock(&driver->msg_mask_lock);
 
