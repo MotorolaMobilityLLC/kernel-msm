5e57132497e3b341bc4bc5890091c1172e606837
huangzq2 <huangzq2@motorola.com>
2019-12-10 10:18:18 +0800

    revert moto changes that caused over-kill while launching camera
    This reverts commit 16463cd4575f55ff62cf48199a7e592492aa3f93.
    
    Change-Id: I9f50a7580f048b2b7b9a0353ddf3e55c2cf2607c
    Reviewed-on: https://gerrit.mot.com/1469298
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -78,8 +78,6 @@ static int enable_lmk = 1;
 module_param_named(enable_lmk, enable_lmk, int, 0644);
 
 static u32 lowmem_debug_level = 1;
-extern int extra_free_kbytes;
-
 static short lowmem_adj[6] = {
 	0,
 	1,
@@ -659,8 +657,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		minfree = lowmem_minfree[i] +
-			  ((extra_free_kbytes * 1024) / PAGE_SIZE);
+		minfree = lowmem_minfree[i];
 		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
 			minfree_count_offset = i;
