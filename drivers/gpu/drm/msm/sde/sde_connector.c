2126a0a0fbc4d051bd53d2a7cdcb85622494c86a
Narender Ankam <nankam@codeaurora.org>
2020-01-06 15:06:10 +0530

    drm/msm/sde: append VIC ID to mode info string
    Append VIC ID of each CEA mode to mode info string.
    
    Change-Id: I48833e69324c7fcea84b6f1905bdd5f42bedb005
    Signed-off-by: Narender Ankam <nankam@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -1999,6 +1999,9 @@ static int sde_connector_populate_mode_info(struct drm_connector *conn,
 
 		sde_kms_info_add_keystr(info, "mode_name", mode->name);
 
+		sde_kms_info_add_keyint(info, "VIC",
+					mode->vic_id);
+
 		sde_kms_info_add_keyint(info, "bit_clk_rate",
 					mode_info.clk_rate);
 
