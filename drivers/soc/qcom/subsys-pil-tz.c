b7e2a7a4f34cc61c628ada4eba4e85a2d1cfde00
duanxl1 <duanxl1@lenovo.com>
2020-01-08 14:44:02 +0800

    expose ssr_reason to ss_ramdump
    expose ssr_reason to ss_ramdump so that
    bug2go can show it in their record.
    
    Change-Id: I1cedf995b61ec10facb85a54b1844abc69d97ce1
    Signed-off-by: duanxl1 <duanxl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1480623
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wen Zeng <zengwen1@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/soc/qcom/subsys-pil-tz.c
+++ drivers/soc/qcom/subsys-pil-tz.c
@@ -45,6 +45,9 @@ struct pil_map_fw_info {
 	phys_addr_t base_addr;
 	struct device *dev;
 };
+static char pil_ssr_reason[MAX_SSR_REASON_LEN];
+static char *ssr_reason = pil_ssr_reason;
+module_param(ssr_reason, charp, S_IRUGO);
 
 /**
  * struct reg_info - regulator info
@@ -805,7 +808,7 @@ static struct pil_reset_ops pil_ops_trusted = {
 static void log_failure_reason(const struct pil_tz_data *d)
 {
 	size_t size;
-	char *smem_reason, reason[MAX_SSR_REASON_LEN];
+	char *smem_reason;
 	const char *name = d->subsys_desc.name;
 
 	if (d->smem_id == -1)
@@ -822,8 +825,8 @@ static void log_failure_reason(const struct pil_tz_data *d)
 		return;
 	}
 
-	strlcpy(reason, smem_reason, min(size, (size_t)MAX_SSR_REASON_LEN));
-	pr_err("%s subsystem failure reason: %s.\n", name, reason);
+	strlcpy(pil_ssr_reason, smem_reason, min((size_t)size, (size_t)sizeof(pil_ssr_reason)));
+	pr_err("%s subsystem failure reason: %s.\n", name, pil_ssr_reason);
 }
 
 static int subsys_shutdown(const struct subsys_desc *subsys, bool force_stop)
