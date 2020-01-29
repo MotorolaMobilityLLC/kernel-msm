b04be3dd5319fc247cef9017abce411f44254321
Qing Chang <a21550@motorola.com>
2020-01-29 11:04:36 -0600

    f2fs: Support Samsung TW feature
    Activate/deactivate Turbo Write during checkpoint/sync if the TW feature
    is enabled.
    
    Reference:
       * mt_sm8250_H2.2.1_T1.1.0.patch, Samsung
    
    Change-Id: I50f9b65a09147b33f4f1e1afe5fe172b980a7b6b
    Signed-off-by: Qing Chang <a21550@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1489723
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -22,6 +22,10 @@
 #include <linux/file.h>
 #include <linux/nls.h>
 
+#if defined(CONFIG_UFSTW)
+#include <linux/ufstw.h>
+#endif
+
 #include "f2fs.h"
 #include "node.h"
 #include "segment.h"
@@ -258,6 +262,10 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 	};
 	unsigned int seq_id = 0;
 
+#if defined(CONFIG_UFSTW)
+	bool turbo_set = false;
+#endif
+
 	if (unlikely(f2fs_readonly(inode->i_sb) ||
 				is_sbi_flag_set(sbi, SBI_CP_DISABLED)))
 		return 0;
@@ -330,6 +338,10 @@ go_write:
 		clear_inode_flag(inode, FI_UPDATE_WRITE);
 		goto out;
 	}
+#if defined(CONFIG_UFSTW)
+	bdev_set_turbo_write(sbi->sb->s_bdev);
+	turbo_set = true;
+#endif
 sync_nodes:
 	atomic_inc(&sbi->wb_sync_req[NODE]);
 	ret = f2fs_fsync_node_pages(sbi, inode, &wbc, atomic, &seq_id);
@@ -376,6 +388,10 @@ flush_out:
 	}
 	f2fs_update_time(sbi, REQ_TIME);
 out:
+#if defined(CONFIG_UFSTW)
+	if (turbo_set)
+		bdev_clear_turbo_write(sbi->sb->s_bdev);
+#endif
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
 	f2fs_trace_ios(NULL, 1);
 	trace_android_fs_fsync_end(inode, start, end - start);
