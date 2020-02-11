8515c21aaaddab48b0d84793e49e78e086c5fe7a
wangyq13 <wangyq13@lenovo.com>
2020-02-11 13:24:55 +0800

    drm: msm: add cabc function interface for LCD panel
    drm/msm:
    Add cabc function interface for LCD panel.
    
    Change-Id: I081644e86ae94300814a7fe893ff65f4f07ce168
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1496326
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shuo Yan <shuoyan@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -199,6 +199,7 @@ enum msm_mdp_conn_property {
 	/* MOT feature panel*/
 	CONNECTOR_PROP_HBM,
 	CONNECTOR_PROP_ACL,
+	CONNECTOR_PROP_CABC,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
 };
@@ -288,6 +289,7 @@ enum msm_param_state {
 enum msm_param_id {
 	PARAM_HBM_ID = 0,
 	PARAM_ACL_ID,
+	PARAM_CABC_ID,
 	PARAM_ID_NUM
 };
 
