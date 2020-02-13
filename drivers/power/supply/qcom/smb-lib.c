83f68e65e9f88142f1c000d9c740b0d37c5eb93f
Haijian Ma <mahj8@motorola.com>
2020-02-13 16:09:07 +0530

    smb: set current based on Charger type
    Changes done based on the CR: (CR)
    
    In factory mode, with USB_SDP plugged if device
    is powered up, input current is seen as 3A and hence
    ICL current get set to 3A irrespetive of Charger type
    plugged in at bootup.
    
    Due to this in Factory mode, "TurboPower Connected"
    toast message is shown at locked screen with USB_SDP plugged
    in at bootup.
    
    To correct this, Charger current should get set based on
    charger type and not based on ICL current.
    
    Change-Id: Iec1b1ea3a179f3ce37eddd4162fd4072ea4e0c41
    Signed-off-by: kumarv <kumarv@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1497608
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3292,12 +3292,15 @@ int smblib_get_charge_current(struct smb_charger *chg,
 		case FLOAT_CHARGER_BIT:
 			current_ua = chg->dcp_current_ua;
 			break;
+		case SDP_CHARGER_BIT:
+			current_ua = SDP_CURRENT_UA;
+			break;
 		default:
 			current_ua = 0;
 			break;
 		}
 
-		*total_current_ua = max(current_ua, val.intval);
+		*total_current_ua = current_ua;
 		return 0;
 	}
 
@@ -3311,6 +3314,9 @@ int smblib_get_charge_current(struct smb_charger *chg,
 		case OCP_CHARGER_BIT:
 		case FLOAT_CHARGER_BIT:
 			current_ua = chg->default_icl_ua;
+			break;
+		case SDP_CHARGER_BIT:
+			current_ua = SDP_CURRENT_UA;
 			break;
 		default:
 			current_ua = 0;
@@ -3330,7 +3336,7 @@ int smblib_get_charge_current(struct smb_charger *chg,
 		break;
 	}
 
-	*total_current_ua = max(current_ua, val.intval);
+	*total_current_ua = current_ua;
 	return 0;
 }
 
