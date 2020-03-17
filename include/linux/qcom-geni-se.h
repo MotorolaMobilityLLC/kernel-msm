17ddfe20de9cd1f2e979dceecd7a86b1d23009af
Prudhvi Yarlagadda <pyarlaga@codeaurora.org>
2020-03-17 17:42:47 +0530

    qcom-geni-se: Make DFS clock table specific to SE of QUP
    Make DFS clock table specific to SE of QUP instead of keeping
    the table specific to QUP common node as the table varies based
    on individual SE.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I84baa22eae8bc757603864be810258d6b3c9c35c
    Signed-off-by: Prudhvi Yarlagadda <pyarlaga@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/1526565
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/qcom-geni-se.h
+++ include/linux/qcom-geni-se.h
@@ -50,6 +50,8 @@ enum se_protocol_types {
  * @geni_pinctrl:	Handle to the pinctrl configuration.
  * @geni_gpio_active:	Handle to the default/active pinctrl state.
  * @geni_gpi_sleep:	Handle to the sleep pinctrl state.
+ * @num_clk_levels:	Number of valid clock levels in clk_perf_tbl.
+ * @clk_perf_tbl:	Table of clock frequency input to Serial Engine clock.
  */
 struct se_geni_rsc {
 	struct device *ctrl_dev;
@@ -69,6 +71,8 @@ struct se_geni_rsc {
 	struct pinctrl_state *geni_gpio_active;
 	struct pinctrl_state *geni_gpio_sleep;
 	int	clk_freq_out;
+	unsigned int num_clk_levels;
+	unsigned long *clk_perf_tbl;
 };
 
 #define PINCTRL_DEFAULT	"default"
