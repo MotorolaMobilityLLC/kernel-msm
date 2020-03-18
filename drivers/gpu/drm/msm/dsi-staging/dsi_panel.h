20d71dbe6c64db32d1fdbddee5846ae2bea3489b
Rajesh Bharathwaj <rajeshbharathwaj@codeaurora.org>
2020-03-17 22:09:17 -0700

    drm:msm:dsi: support additional gpios in display panel
    Adding support for additional gpios for dsi display panel in xr smartviewer
    reference device.
    
    Change-Id: I8816b0244037bf48eee9be62cdcc2fd8bcd84cc6
    Signed-off-by: Rajesh Bharathwaj <rajeshbharathwaj@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -161,6 +161,17 @@ enum dsi_panel_type {
 	DSI_PANEL_TYPE_MAX,
 };
 
+/* Extended Panel config for panels with additional gpios */
+struct dsi_panel_exd_config {
+	int display_1p8_en;
+	int led_5v_en;
+	int switch_power;
+	int led_en1;
+	int led_en2;
+	int oenab;
+	int selab;
+};
+
 struct dsi_panel {
 	const char *name;
 	enum dsi_panel_type type;
@@ -204,6 +215,8 @@ struct dsi_panel {
 	enum dsi_dms_mode dms_mode;
 
 	bool sync_broadcast_en;
+
+	struct dsi_panel_exd_config exd_config;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
