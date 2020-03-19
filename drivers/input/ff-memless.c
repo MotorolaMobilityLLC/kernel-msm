5dde3f0f41e2ab510e8f2fc6e7195035a978b485
Oliver Neukum <oneukum@suse.com>
2020-03-19 09:36:01 +0530

    Input: ff-memless - kill timer in destroy()
    No timer must be left running when the device goes away.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2019-19524
    BUG: 146258053
    
    Signed-off-by: Oliver Neukum <oneukum@suse.com>
    Reported-and-tested-by: syzbot+b6c55daa701fc389e286@syzkaller.appspotmail.com
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/1573726121.17351.3.camel@suse.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    
    Change-Id: Iea9474be3264a5189b1b9bf5820863ef3e32df86
    Signed-off-by: Pachipulusu Bhanu Prakash <bhprakas@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1519493
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Konstantin Makariev <kmakariev@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit b3892c34bf852e1235cc107209b0f8cf5b8be61d)
    Reviewed-on: https://gerrit.mot.com/1537534

--- drivers/input/ff-memless.c
+++ drivers/input/ff-memless.c
@@ -500,6 +500,14 @@ static int ml_ff_upload(struct input_dev *dev,
 static void ml_ff_destroy(struct ff_device *ff)
 {
 	struct ml_device *ml = ff->private;
+	/*
+	 * Even though we stop all playing effects when tearing down
+	 * an input device (via input_device_flush() that calls into
+	 * input_ff_flush() that stops and erases all effects), we
+	 * do not actually stop the timer, and therefore we should
+	 * do it here.
+	 */
+	del_timer_sync(&ml->timer);
 
 	kfree(ml->private);
 }
