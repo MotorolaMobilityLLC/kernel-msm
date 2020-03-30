8ee1caee25dd6c0fbf174c34f8245ea29ece392c
luodw1 <luodw1@motorola.com>
2020-03-30 14:25:56 +0800

    power: Add wakeup source in battery historian logging
    Change-Id: I194ceccd10e455da8c58a2c96e4339557024b142
    Signed-off-by: luodw1 <luodw1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1523878
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -20,6 +20,7 @@
 #include <linux/irq.h>
 #include <linux/interrupt.h>
 #include <linux/irqdesc.h>
+#include <linux/wakeup_reason.h>
 
 #include "power.h"
 
@@ -940,6 +941,7 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 					irq_number, name);
 
 		}
+		log_wakeup_reason(irq_number);
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
 	}
