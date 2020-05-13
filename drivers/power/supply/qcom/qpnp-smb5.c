a1dbc4226532910ae7793c6a4813ef2e6909fcea
chailu1 <chailu1@lenovo.com>
2020-05-13 13:07:52 +0800

    Revert (CR): power: smb5: Clear HDC AICL
    High duty cycleused for AICL/adaptive algorithm
    Qcom default is enable, if disable perhaps cause
    remain voltage while remove USB cable
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I686e503a44de890250b95a1ff0dc7c86c1081979
    Signed-off-by: xuwei9 <xuwei9@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1553135
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -3439,8 +3439,8 @@ static int smb5_init_hw(struct smb5 *chip)
 	 * configuration enable/disable ADB based AICL and Suspend on collapse.
 	 */
 	mask = USBIN_AICL_PERIODIC_RERUN_EN_BIT | USBIN_AICL_ADC_EN_BIT
-		| USBIN_AICL_EN_BIT | SUSPEND_ON_COLLAPSE_USBIN_BIT
-		| USBIN_AICL_HDC_EN_BIT;
+		| USBIN_AICL_EN_BIT | SUSPEND_ON_COLLAPSE_USBIN_BIT;
+
 	val = USBIN_AICL_PERIODIC_RERUN_EN_BIT | USBIN_AICL_EN_BIT;
 	if (!chip->dt.disable_suspend_on_collapse)
 		val |= SUSPEND_ON_COLLAPSE_USBIN_BIT;
