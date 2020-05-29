60b5b7e20f666d167363d983ff5409fd86209a6f
xuwei9 <xuwei9@lenovo.com>
2020-05-29 15:55:50 +0800

    kernel:USB DRP connect debug accessory test
    USB type-c Compliance Test: TD 4.8.2
    DRP Connect Debug Accessory Test
    
    Change-Id: I7ca4de5e6bee392cba24b34d8a474c0b879bcba4
    Signed-off-by: xuwei9 <xuwei9@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1587288
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2842,17 +2842,6 @@ static int smb5_configure_typec(struct smb_charger *chg)
 		return rc;
 	}
 
-	/* Enable detection of unoriented debug accessory in source mode */
-	rc = smblib_masked_write(chg, DEBUG_ACCESS_SRC_CFG_REG,
-				 EN_UNORIENTED_DEBUG_ACCESS_SRC_BIT,
-				 EN_UNORIENTED_DEBUG_ACCESS_SRC_BIT);
-	if (rc < 0) {
-		dev_err(chg->dev,
-			"Couldn't configure TYPE_C_DEBUG_ACCESS_SRC_CFG_REG rc=%d\n",
-				rc);
-		return rc;
-	}
-
 	if (chg->source_current_ma < DEFAULT_SOURCE_CURRENT_MA) {
 		/*advertise default current*/
 		rc = smblib_masked_write(chg, TYPE_C_CURRSRC_CFG_REG,
