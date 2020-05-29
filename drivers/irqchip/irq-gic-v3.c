83598a70cf8e8d1935deceed0e031c0cd6611240
wangjy13 <wangjy13@lenovo.com>
2020-05-29 13:40:36 +0800

    [smith][wangjy13] Add extra debug message for IRQ
    Print more info about IRQ
    When device is waked up frequently, we can get more information
    form this modification.
    
    Change-Id: If48fd7fe15190cb5f6d308293eebd8a0be64e609
    Signed-off-by: wangjy13 <wangjy13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1587027
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xuetao Cheng <chengxta@lenovo.com>
    Submit-Approved: Xiaoxiao Xue <xuexx2@lenovo.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -45,6 +45,8 @@
 
 #include "irq-gic-common.h"
 
+int qrtr_first_msg = 0;
+
 struct redist_region {
 	void __iomem		*redist_base;
 	phys_addr_t		phys_base;
@@ -373,6 +375,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		log_wakeup_reason(irq);
+		qrtr_first_msg = 1; //You could try filter out the irq name by yourself.
 	}
 }
 
