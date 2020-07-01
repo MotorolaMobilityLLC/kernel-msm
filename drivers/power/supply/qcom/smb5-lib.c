0407e83849a3b079d46c0e5910d8f4cd5534517c
mahj8 <mahj8@lenovo.com>
2020-07-01 11:50:34 +0800

    smb5: add stop charGing in STEP FLOAT
    when in STEP FLOAT, charGing will don't stop when current less than
    charGing iterm, so we add stop charGing in STEP FLOAT.
    
    Change-Id: I6a6eecd4655a5f134a811698e58584ac3d9109ee
    Signed-off-by: mahj8 <mahj8@mt.com>
    Reviewed-on: https://gerrit.mot.com/1653743
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -10018,7 +10018,8 @@ static void mmi_heartbeat_work(struct work_struct *work)
 		}
 	} else if ((mmi->pres_chrg_step == STEP_NONE) ||
 		   (mmi->pres_chrg_step == STEP_STOP)) {
-		if (zone->norm_mv && (batt_mv >= zone->norm_mv)) {
+		if (zone->norm_mv
+			&& ((batt_mv + HYST_STEP_MV) >= zone->norm_mv)) {
 			if (zone->fcc_norm_ma)
 				mmi->pres_chrg_step = STEP_NORM;
 			else
@@ -10051,7 +10052,7 @@ static void mmi_heartbeat_work(struct work_struct *work)
 		}
 	} else if (mmi->pres_chrg_step == STEP_NORM) {
 		if (!zone->fcc_norm_ma)
-			mmi->pres_chrg_step = STEP_STOP;
+			mmi->pres_chrg_step = STEP_FLOAT;
 		else if ((batt_soc < 100) ||
 			 (batt_mv + HYST_STEP_MV) < max_fv_mv) {
 			mmi->chrg_taper_cnt = 0;
@@ -10085,7 +10086,9 @@ static void mmi_heartbeat_work(struct work_struct *work)
 				msleep(50);
 			}
 			mmi->pres_chrg_step = STEP_MAX;
-		}
+		} else if (mmi_has_current_tapered(chip, batt_ma,
+						   mmi->chrg_iterm))
+			mmi->pres_chrg_step = STEP_STOP;
 	}
 
 	/* Take State actions */
@@ -10110,9 +10113,9 @@ static void mmi_heartbeat_work(struct work_struct *work)
 	case STEP_FLOAT:
 	case STEP_MAX:
 		if (!zone->norm_mv)
-			target_fv = max_fv_mv;
+			target_fv = max_fv_mv + mmi->vfloat_comp_mv;
 		else
-			target_fv = zone->norm_mv;
+			target_fv = zone->norm_mv + mmi->vfloat_comp_mv;
 		target_fcc = zone->fcc_max_ma;
 		mmi->cl_ebchg = 0;
 
