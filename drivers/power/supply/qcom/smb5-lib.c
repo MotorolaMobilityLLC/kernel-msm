57f90b03b36854ba1fd0e7bb960e37df2cc77997
xuwt2 <xuwt2@lenovo.com>
2020-07-10 15:56:56 +0800

    smb5: Add DCP curr settings
    Read usb_dcp configration from the properity of "mmi.usb_dcp"
    the setting is in vhw.xml.
    Different sku have different requirements for DCP current
    
    Change-Id: Id01a4aa02b69b273a272835054ce1d97b50f3ff9
    Signed-off-by: xuwt2 <xuwt2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1673650
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1771130

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -4404,6 +4404,8 @@ static int get_rp_based_dcp_current(struct smb_charger *chg, int typec_mode)
 	/* fall through */
 	default:
 		rp_ua = DCP_CURRENT_UA;
+		if (chg->usb_dcp_curr_max > 0)
+			rp_ua = chg->usb_dcp_curr_max;
 	}
 
 	return rp_ua;
