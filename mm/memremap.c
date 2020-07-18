2512825a1bdd49c4fa1f38de8add7857524fa876
Yu Zhao <yuzhao@google.com>
2020-07-18 01:46:37 -0600

    BACKPORT: mm: remove superfluous __ClearPageActive()
    To activate a page, mark_page_accessed() always holds a reference on it.
    It either gets a new reference when adding a page to
    lru_pvecs.activate_page or reuses an existing one it previously got when
    it added a page to lru_pvecs.lru_add.  So it doesn't call SetPageActive()
    on a page that doesn't have any reference left.  Therefore, the race is
    impossible these days (I didn't brother to dig into its history).
    
    For other paths, namely reclaim and migration, a reference count is always
    held while calling SetPageActive() on a page.
    
    SetPageSlabPfmemalloc() also uses SetPageActive(), but it's irrelevant to
    LRU pages.
    Moto-CRs-Fixed:(CR)
    
    Change-Id: If818ad43f98960f6b397dae7c2dd555bd4469854
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Cc: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Qian Cai <cai@lca.pw>
    Link: http://lkml.kernel.org/r/20200818184704.3625199-2-yuzhao@google.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 6f4dd8de4835563de9bae797ce1d7a13465a7a7d)
    Bug: 261052246
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Reviewed-on: https://gerrit.mot.com/2491787
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- mm/memremap.c
+++ mm/memremap.c
@@ -419,8 +419,6 @@ void __put_devmap_managed_page(struct page *page)
 	 * holds a reference on the page.
 	 */
 	if (count == 1) {
-		/* Clear Active bit in case of parallel mark_page_accessed */
-		__ClearPageActive(page);
 		__ClearPageWaiters(page);
 
 		mem_cgroup_uncharge(page);
