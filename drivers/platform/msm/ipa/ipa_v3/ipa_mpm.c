649263d3b6e23d09d78f6ec9b178ae72598c9611
Praveen Kurapati <pkurapat@codeaurora.org>
2020-07-20 23:30:43 +0530

    msm: ipa3: Add change to avoid negative index access
    During caching of bw reg dereg, there is a chance to access
    negative index values of cache. Add changes to overcome it.
    
    Change-Id: I646aa3c35cd5a49e46334a3b09e610448779f72a
    Signed-off-by: Praveen Kurapati <pkurapat@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_mpm.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_mpm.c
@@ -3534,8 +3534,9 @@ int ipa3_qmi_reg_dereg_for_bw(bool bw_reg, int bw_reg_dereg_type)
 			}
 			IPA_MPM_DBG("QMI BW regst success from %d",
 				ipa_mpm_ctx->bw_reg_dereg_cache[
-					ipa_mpm_ctx->cache_index -
-					1].bw_reg_dereg_type);
+					(ipa_mpm_ctx->cache_index -
+					1) % IPA_MAX_BW_REG_DEREG_CACHE].
+					bw_reg_dereg_type);
 		} else {
 			IPA_MPM_DBG("bw_change to %d no-op, teth_count = %d",
 				bw_reg,
@@ -3556,8 +3557,9 @@ int ipa3_qmi_reg_dereg_for_bw(bool bw_reg, int bw_reg_dereg_type)
 			}
 			IPA_MPM_DBG("QMI BW De-regst success %d",
 				ipa_mpm_ctx->bw_reg_dereg_cache[
-					ipa_mpm_ctx->cache_index -
-					1].bw_reg_dereg_type);
+					(ipa_mpm_ctx->cache_index -
+					1) % IPA_MAX_BW_REG_DEREG_CACHE].
+					bw_reg_dereg_type);
 		} else {
 			IPA_MPM_DBG("bw_change to %d no-op, teth_count = %d",
 				bw_reg,
