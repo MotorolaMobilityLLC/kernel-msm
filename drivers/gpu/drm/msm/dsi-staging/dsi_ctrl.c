691468b85d45b44c35f07cd16ecc612cf42087a3
Lei Chen <chenlei@codeaurora.org>
2020-07-24 13:31:43 +0800

    disp: msm: Subtract DSI interrupt count after interrupt was destroyed
    DSI interrupt may be destroyed before it is disabled, it will cause
    the interrupt count to be not cleared, so subtract DSI interrupt count
    in disable function even it was destroyed.
    
    Change-Id: I430b0281957db588c7405d5775d0c10f2f498b36
    Signed-off-by: Lei Chen <chenlei@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
@@ -2632,8 +2632,7 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 {
 	unsigned long flags;
 
-	if (!dsi_ctrl || dsi_ctrl->irq_info.irq_num == -1 ||
-			intr_idx >= DSI_STATUS_INTERRUPT_COUNT)
+	if (!dsi_ctrl || intr_idx >= DSI_STATUS_INTERRUPT_COUNT)
 		return;
 
 	spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
@@ -2645,7 +2644,8 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 					dsi_ctrl->irq_info.irq_stat_mask);
 
 			/* don't need irq if no lines are enabled */
-			if (dsi_ctrl->irq_info.irq_stat_mask == 0)
+			if (dsi_ctrl->irq_info.irq_stat_mask == 0 &&
+				dsi_ctrl->irq_info.irq_num != -1)
 				disable_irq_nosync(dsi_ctrl->irq_info.irq_num);
 		}
 
