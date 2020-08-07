c6732189d8d0738d787203b3972ec2176cf8c3db
mahj8 <mahj8@lenovo.com>
2020-08-07 17:00:08 +0800

    drivers: qcom: Support debug accessory in sink mode
    Huawai P10 5A cable is considered a debug accessory cable in sink mode, so
    charging in such conditions needs to be configured by setting proper PMIC
    registers.
    
    Porter from: https://gerrit.mot.com/#/c/1447696/
    
    Change-Id: I1fd9fa5ec7330995e21505c3b543ab1a13ba7576
    Signed-off-by: mahj8 <mahj8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1717168
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2701,6 +2701,18 @@ static int smb5_configure_typec(struct smb_charger *chg)
 		}
 	}
 
+	 /* config 0x154A to 0x03
+	 Enable detection of debug accessory in sink mode*/
+	rc = smblib_masked_write(chg, TYPE_C_DEBUG_ACCESS_SINK_REG,
+					TYPEC_DEBUG_ACCESS_SINK_MASK,
+					0x03);
+	if (rc < 0) {
+		dev_err(chg->dev,
+			"Couldn't configure TYPE_C_DEBUG_ACCESS_SINK_REG rc=%d\n",
+				rc);
+		return rc;
+	}
+
 	if (chg->chg_param.smb_version != PMI632_SUBTYPE) {
 		/*
 		 * Enable detection of unoriented debug
