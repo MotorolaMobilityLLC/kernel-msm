3300df7843edb3138dfb22856b41ab3bd66ed609
Ram Prakash Gupta <rampraka@codeaurora.org>
2020-08-28 09:46:16 +0530

    scsi: ufs: Remove duplicate code
    Remove duplicate code from ufs driver.
    
    Change-Id: I2db52681c74957cd7f1247d2f6a0e37e0bf2dcc0
    Signed-off-by: Ram Prakash Gupta <rampraka@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -6145,9 +6145,6 @@ static int ufshcd_slave_alloc(struct scsi_device *sdev)
 	/* WRITE_SAME command is not supported*/
 	sdev->no_write_same = 1;
 
-	/* WRITE_SAME command is not supported */
-	sdev->no_write_same = 1;
-
 	ufshcd_set_queue_depth(sdev);
 
 	ufshcd_get_lu_power_on_wp_status(hba, sdev);
