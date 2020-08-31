832959a17704333431acce1e547456ec13ecfed2
Nirmal Abraham <nabrah@codeaurora.org>
2020-08-31 10:38:19 +0530

    fbdev: msm: Fix compilation error for user build
    CONFIG_DEGUG_FS is disabled in user build. This
    change resolves the compilation errors which
    occurred when CONFIG_DEBUG_FS flag is disabled.
    
    Change-Id: I2c5c5121178fc78eec2524ee1088579ceb020adb
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -4085,9 +4085,9 @@ static ssize_t mdss_mdp_misr_show(struct device *dev,
 		pr_err("Invalid ctl structure\n");
 		return -EINVAL;
 	}
-
+#ifdef CONFIG_DEBUG_FS
 	ret = mdss_dump_misr_data(&buf, PAGE_SIZE);
-
+#endif
 	return ret;
 }
 
