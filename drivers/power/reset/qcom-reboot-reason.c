2e8498b5472bb92429d8a8e36523a7fb2dd5f45c
xiaoy3 <xiaoy3@motorola.com>
2020-09-18 16:44:15 +0800

    Store moto extra restart reason
    After cold reset is introduced, the phone is cold reset instead of
    warm reset, such as panic. In this case, the power up reason saved
    in shared memory will be lost. Consequently, the power up reason on
    next boot up time will be normal boot instead of correct ones. To
    resolve this issue, we can use the two spared pmic registers which
    are power lost safe to save the restart reason for these cases.
    
    Change-Id: I12fc240798fc59abeb70b6769563abd6c626e43c
    Signed-off-by: xiaoy3 <xiaoy3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1754176
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -14,10 +14,18 @@
 #include <linux/of_address.h>
 #include <linux/nvmem-consumer.h>
 
+#define RESET_EXTRA_POST_PANIC_REASON  (BIT(4) | BIT(5))
+#define RESET_EXTRA_POST_PMICWDT_REASON        BIT(5)
+#define RESET_EXTRA_POST_WDT_REASON    BIT(4)
+#define RESET_EXTRA_PANIC_REASON       BIT(3)
+#define RESET_EXTRA_REBOOT_BL_REASON   BIT(2)
+
 struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
 	struct nvmem_cell *nvmem_cell;
+	struct notifier_block panic_nb;
+	struct nvmem_cell *nvmem_oem_cell;
 };
 
 struct poweroff_reason {
@@ -35,6 +43,27 @@ static struct poweroff_reason reasons[] = {
 	{}
 };
 
+static struct poweroff_reason extra_reasons[] = {
+	{ "post-wdt",		RESET_EXTRA_POST_WDT_REASON},
+	{ "post-pmicwdt",	RESET_EXTRA_POST_PMICWDT_REASON},
+	{ "post-panic",		RESET_EXTRA_POST_PANIC_REASON},
+	{ "bootloader",		RESET_EXTRA_REBOOT_BL_REASON},
+	{}
+};
+
+static int moto_reboot_reason_panic(struct notifier_block *this,
+		unsigned long event, void *ptr)
+{
+	struct qcom_reboot_reason *reboot = container_of(this,
+		struct qcom_reboot_reason, panic_nb);
+	unsigned char val = RESET_EXTRA_PANIC_REASON;
+
+	nvmem_cell_write(reboot->nvmem_oem_cell, &val,
+			sizeof(val));
+
+	return NOTIFY_OK;
+}
+
 static int qcom_reboot_reason_reboot(struct notifier_block *this,
 				     unsigned long event, void *ptr)
 {
@@ -48,6 +77,15 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 	for (reason = reasons; reason->cmd; reason++) {
 		if (!strcmp(cmd, reason->cmd)) {
 			nvmem_cell_write(reboot->nvmem_cell,
+					 &reason->pon_reason,
+					 sizeof(reason->pon_reason));
+			break;
+		}
+	}
+
+	for (reason = extra_reasons; reason->cmd; reason++) {
+		if (!strcmp(cmd, reason->cmd)) {
+			nvmem_cell_write(reboot->nvmem_oem_cell,
 					 &reason->pon_reason,
 					 sizeof(reason->pon_reason));
 			break;
@@ -72,6 +110,16 @@ static int qcom_reboot_reason_probe(struct platform_device *pdev)
 	if (IS_ERR(reboot->nvmem_cell))
 		return PTR_ERR(reboot->nvmem_cell);
 
+	reboot->nvmem_oem_cell = nvmem_cell_get(reboot->dev, "extra_restart_reason");
+
+	if (IS_ERR(reboot->nvmem_oem_cell))
+		return PTR_ERR(reboot->nvmem_oem_cell);
+
+	reboot->panic_nb.notifier_call = moto_reboot_reason_panic;
+	reboot->panic_nb.priority = INT_MAX;
+	atomic_notifier_chain_register(&panic_notifier_list,
+				       &reboot->panic_nb);
+
 	reboot->reboot_nb.notifier_call = qcom_reboot_reason_reboot;
 	reboot->reboot_nb.priority = 255;
 	register_reboot_notifier(&reboot->reboot_nb);
@@ -85,6 +133,8 @@ static int qcom_reboot_reason_remove(struct platform_device *pdev)
 {
 	struct qcom_reboot_reason *reboot = platform_get_drvdata(pdev);
 
+	atomic_notifier_chain_unregister(&panic_notifier_list,
+					 &reboot->panic_nb);
 	unregister_reboot_notifier(&reboot->reboot_nb);
 
 	return 0;
