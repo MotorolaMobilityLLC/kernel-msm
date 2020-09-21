fa9d9713c6a37e97178d964dafc76aec8f374ad8
a17671 <a17671@motorola.com>
2020-09-21 11:26:26 +0800

    scsi/ufs:Add Toshiba KioXia HPB driver
    Add HPB 1.0 driver from Toshiba Kioxia
    Since it is a device controlled mode HPB,
    Flag needs to be added to coexist with
    Current Samsung HPB/TW driver
    
    Reference:
    	M1CCL04-118_HPB10_DeviceControlMode.zip KioXia
    
    Change-Id: I46a31e0c2ff9b6da9d23638f398f2224436dc8b8
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1755653
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

---
+++ drivers/scsi/ufs/ufshpb_toshiba_if.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Motorola Mobility LLC
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _UFSHPB_TOSHIBA_IF_H_
+#define _UFSHPB_TOSHIBA_IF_H_
+#include "ufs_quirks.h"
+
+#define UFSHCD_DEVICE_HPB_SUPPORT_BIT_POSITION 7
+#define UFSHCD_DEVICE_HPB_SUPPORT_BIT UFS_BIT(UFSHCD_DEVICE_HPB_SUPPORT_BIT_POSITION)
+#if defined(CONFIG_UFSHPB_TOSHIBA)
+static inline bool ufshcd_is_hpb_supported(struct ufs_hba *hba)
+{
+	return ((!!(hba->dev_info.d_ext_ufs_feature_sup & UFSHCD_DEVICE_HPB_SUPPORT_BIT)) &&
+			(!!(hba->dev_info.quirks &UFS_DEVICE_QUIRK_HPB_DEVICECONTROL))) ;
+}
+#else
+static inline bool ufshcd_is_hpb_supported(struct ufs_hba *hba) {}
+#endif
+struct ufshcd_lrb;
+void ufshcd_init_hpb(struct ufs_hba *hba);
+void ufshpb_prep_fn_toshiba(struct ufs_hba *hba, struct ufshcd_lrb *lrbp);
+void ufshpb_rsp_upiu_toshiba(struct ufs_hba *hba, struct ufshcd_lrb *lrbp);
+void ufshpb_release_toshiba(struct ufs_hba *hba, int state);
+
+#endif /* End of Header */
