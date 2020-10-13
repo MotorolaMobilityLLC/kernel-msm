584ca73cbe3b9118c8036e8cfeb2218e6ea67773
a17671 <a17671@motorola.com>
2020-10-13 18:07:47 +0800

    ufs:Add Samsung UFS3.1 drivers headers
    Header file changes for Samsung
    HPB_FS and the UFSFEATURE_31
    
    Change-Id: I972820a59f5c70916f0f636796a8703c15ab482d
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1773953
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3144,6 +3144,10 @@ enum {
 
 	/* filesystem does not support filling holes */
 	DIO_SKIP_HOLES	= 0x02,
+#ifdef CONFIG_FS_HPB
+	/* HPB FLAG */
+	DIO_HPB_IO	= 0x10,
+#endif
 };
 
 void dio_end_io(struct bio *bio);
