549dae5182759b07392059ccdb60d49324aa2d38
zhangly19 <zhangly19@lenovo.com>
2020-11-03 08:40:16 +0800

    Revert "(CR) power: Add wakeup source in battery historian logGing"
    This reverts commit 3a7c2ddda918ea0ba239c51d74984a499c392942.
    
    This change has been included in new base .so revert it
    
    Change-Id: I50210a029c2f69b8561a7cae2f4b87b190b8a842
    Reviewed-on: https://gerrit.mot.com/1791620
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: gaoyx9 <gaoyx9@motorola.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -22,7 +22,6 @@
 #include <linux/irq.h>
 #include <linux/interrupt.h>
 #include <linux/irqdesc.h>
-#include <linux/wakeup_reason.h>
 
 #include "power.h"
 
@@ -947,7 +946,6 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 		log_irq_wakeup_reason(irq_number);
 		pr_warn("%s: %d triggered %s\n", __func__, irq_number, name);
 
-		log_wakeup_reason(irq_number);
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
 	}
