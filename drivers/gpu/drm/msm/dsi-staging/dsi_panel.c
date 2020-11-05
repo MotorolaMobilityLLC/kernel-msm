d2b880f63e0ed57b637c3cf5b6d53eff0ca74051
Althaf Neelanchirayil <aneelanc@codeaurora.org>
2020-11-05 15:20:00 +0530

    disp: msm: add inverted display brightness value property
    In general the brightness value, the first parameter is low byte and
    the second is high byte, but some DDIC invert, so add inverted display
    brightness value property.
    
    Change-Id: Ic635eb745709d756f093738348e471a544a2eeb1
    Signed-off-by: Althaf Neelanchirayil <aneelanc@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -620,6 +620,9 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 
 	dsi = &panel->mipi_device;
 
+	if (panel->bl_config.bl_inverted_dbv)
+		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
+
 	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
@@ -2320,6 +2323,9 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 	} else {
 		panel->bl_config.brightness_default_level = val;
 	}
+
+	panel->bl_config.bl_inverted_dbv = utils->read_bool(utils->data,
+		"qcom,mdss-dsi-bl-inverted-dbv");
 
 	if (panel->bl_config.type == DSI_BACKLIGHT_PWM) {
 		rc = dsi_panel_parse_bl_pwm_config(panel);
