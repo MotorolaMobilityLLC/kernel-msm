e87026dc0fb91910c9097b180f431cfa5d541803
yangbq1 <yangbq2@motorola.com>
2020-11-09 02:09:55 +0800

    Revert "(CR): ION: Improve cache accounting"
    This reverts commit 2cfd0e99a1810edac8462d54baee0636203a1ee1.
    In R, there's an item added in /proc/meminfo:
    KReclaimable include global_node_page_state(NR_KERNEL_MISC_RECLAIMABLE)
    Also, getCachedSize in AMS will also calculate this item.
    The changes in previous Android version will make ion pool pages
    not include.
    Thus, we need revert the change from R
    
    Change-Id: I1041c9c721c9d73ddb0558bc07dbcc1ad2aad3d1
    Signed-off-by: yangbq1 <yangbq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1797018
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shunde Jiang <jiangsd2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -48,8 +48,6 @@ static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 
 static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
-	int page_count = 1 << pool->order;
-
 	mutex_lock(&pool->mutex);
 	if (PageHighMem(page)) {
 		list_add_tail(&page->lru, &pool->high_items);
@@ -60,11 +58,8 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 	}
 
 	nr_total_pages += 1 << pool->order;
-	/*mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
-							1 << pool->order);*/
-	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, page_count);
-	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, page_count);
-
+	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
+							1 << pool->order);
 	mutex_unlock(&pool->mutex);
 	return 0;
 }
@@ -72,7 +67,6 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 {
 	struct page *page;
-	int page_count = 1 << pool->order;
 
 	if (high) {
 		BUG_ON(!pool->high_count);
@@ -86,12 +80,8 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 
 	list_del(&page->lru);
 	nr_total_pages -= 1 << pool->order;
-	/*mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
-							-(1 << pool->order));*/
-
-	mod_node_page_state(page_pgdat(page), NR_INACTIVE_FILE, -page_count);
-	mod_node_page_state(page_pgdat(page), NR_FILE_PAGES, -page_count);
-
+	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
+							-(1 << pool->order));
 	return page;
 }
 
