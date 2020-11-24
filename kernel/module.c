fdcee94a52a73f9aa06d495e8eac86f413a2d8bb
litong <litong20@lenovo.com>
2020-11-24 17:53:40 +0800

    fix the error of "__symbol_put " undefined in touchscreen
    TP:
    The function is not exported by Google, and we have fixed the error in
    touchscreen in (CR).
    
    
    Change-Id: I9b39ac3fadbc833f7e30958dae386799d0153479
    Reviewed-on: https://gerrit.mot.com/1810061
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- kernel/module.c
+++ kernel/module.c
@@ -1086,8 +1086,6 @@ void __symbol_put(const char *symbol)
 	module_put(owner);
 	preempt_enable();
 }
-/* MMI_STOPSHIP: touchscreen: export the symbol for temp rebase */
-EXPORT_SYMBOL(__symbol_put);
 
 /* Note this assumes addr is a function, which it currently always is. */
 void symbol_put_addr(void *addr)
@@ -2322,8 +2320,6 @@ fail:
 	preempt_enable();
 	return NULL;
 }
-/* MMI_STOPSHIP: touchscreen: export the symbol for temp rebase */
-EXPORT_SYMBOL(__symbol_get);
 
 /*
  * Ensure that an exported symbol [global namespace] does not already exist
