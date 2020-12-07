9d3ab46d8f72600e486be6a0efb12be479564641
luodw1 <luodw1@motorola.com>
2020-12-07 10:05:34 +0800

    debug: fix pcie device wakeup reason is unknown issue
    For battery life issue,the battery historian can't statistics pcie
    device wakeup number and wakeup reason, show reason is "unknown", now
    add the wakeup reason to facilitate analysis battery life issue.
    
    Change-Id: I3810bcad5ca121250ce62c9e58cb1a4baea4f364
    Signed-off-by: luodw1 <luodw1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1821650
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/pci/controller/pci-msm-msi.c
+++ drivers/pci/controller/pci-msm-msi.c
@@ -11,6 +11,9 @@
 #include <linux/of_irq.h>
 #include <linux/of_pci.h>
 #include <linux/pci.h>
+#include <linux/wakeup_reason.h>
+
+extern int gic_resume_irq;
 
 struct msm_msi_irq {
 	unsigned int hwirq; /* MSI controller hwirq */
@@ -49,6 +52,13 @@ static void msm_msi_handler(struct irq_desc *desc)
 
 	msi = irq_desc_get_handler_data(desc);
 	virq = irq_find_mapping(msi->inner_domain, irq_desc_get_irq(desc));
+
+	if (gic_resume_irq) {
+		if(irq_desc_get_irq(desc) == gic_resume_irq) {
+			log_irq_wakeup_reason(virq);
+		}
+		gic_resume_irq = 0;
+	}
 
 	generic_handle_irq(virq);
 
