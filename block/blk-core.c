3189d005d0f40ec2ac9e24bf55d15a99cc8aca86
a17671 <a17671@motorola.com>
2020-12-07 13:16:37 +0800

    ufs:Add Samsung UFS HPB driver panic fix
    root cause from Samsung:
    Interrupt context called blk_get_request when task_lock is used.
    To avoid this issue, we added another block layer API that
    Does not create io_context. i
    io_context is related to task that creates IO,
    so this change can prevent irrelevent io_context to be created.
    Operating request is no problem without io_context as well.
    
    Change-Id: Idecd9fcc7af1c49742a401c0b52511c930325422
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1820427
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- block/blk-core.c
+++ block/blk-core.c
@@ -1616,6 +1616,41 @@ static struct request *blk_old_get_request(struct request_queue *q,
 	rq->bio = rq->biotail = NULL;
 	return rq;
 }
+/* flags: BLK_MQ_REQ_PREEMPT and/or BLK_MQ_REQ_NOWAIT. */
+struct request *blk_old_get_request_no_ioc(struct request_queue *q,
+                               unsigned int op, blk_mq_req_flags_t flags)
+{
+       struct request *rq;
+       gfp_t gfp_mask = flags & BLK_MQ_REQ_NOWAIT ? GFP_ATOMIC : GFP_NOIO;
+       int ret = 0;
+
+       WARN_ON_ONCE(q->mq_ops);
+
+       ret = blk_queue_enter(q, flags);
+       if (ret)
+               return ERR_PTR(ret);
+       spin_lock_irq(q->queue_lock);
+       rq = get_request(q, op, NULL, flags, gfp_mask);
+       if (IS_ERR(rq)) {
+               spin_unlock_irq(q->queue_lock);
+               blk_queue_exit(q);
+               return rq;
+       }
+
+       /* q->queue_lock is unlocked at this point */
+       rq->__data_len = 0;
+       rq->__sector = (sector_t) -1;
+#ifdef CONFIG_PFK
+       rq->__dun = 0;
+#endif
+       rq->bio = rq->biotail = NULL;
+
+       if (!IS_ERR(rq) && q->initialize_rq_fn)
+               q->initialize_rq_fn(rq);
+
+       return rq;
+}
+EXPORT_SYMBOL(blk_old_get_request_no_ioc);
 
 /**
  * blk_get_request - allocate a request
