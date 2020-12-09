ae508a2f297668600ff0baf786f5a23c70e6aa24
Ravikishore Pampana <rpampana@codeaurora.org>
2020-12-09 14:56:23 +0530

    msm: camera: reqmgr: Correct the condition check for dual camera
    In dual camera sync, either of camera sof notification can come
    first to crm irrespective of their csid sof arrival time stamp.
    CRM should handle this scenario by proper checks of csid time
    stamps and request id.
    
    Change-Id: I1a1535a43aa812bb3c3e49188721821fbadb471c
    Signed-off-by: Ravikishore Pampana <rpampana@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.c
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.c
@@ -1152,10 +1152,15 @@ static int __cam_req_mgr_check_sync_req_is_ready(
 		 * event of sync link is skipped, so we also need to
 		 * skip this SOF event.
 		 */
-		if (req_id >= sync_req_id) {
-			CAM_DBG(CAM_CRM,
-				"Timing issue, the sof event of link %x is delayed",
-				link->link_hdl);
+		if (req_id > sync_req_id) {
+			CAM_INFO(CAM_CRM,
+				"Timing issue, the sof event delayed of link %x sof ts:0x%x sync link handle:0x%x sync sof:0x%x req:%lld sync reqid:%lld",
+				link->link_hdl,
+				link->sof_timestamp,
+				link->sync_link->link_hdl,
+				sync_link->sof_timestamp,
+				req_id,
+				sync_req_id);
 			return -EAGAIN;
 		}
 	}
