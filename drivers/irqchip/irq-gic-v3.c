313d570030b48d605e2646b1ef44f28f5f4ecca2
sunyue5 <sunyue5@motorola.com>
2021-01-06 12:28:14 +0800

    QRTR: Print the first qrtr message after resume
    The interrupt glink-native indicates other subsystems send IPC
    message to APSS and wake up APSS. We usually need to check
    d/ipc_logGing/[subsys]/log_cont to find which subsystems wakes
    up the APSS
    With printing the first qrtr message after resume, kernel log
    will help us to identify which subsystem wakes the APSS
    Refer to:
    DCN: KBA-200427080824, Glink/ipcc irq wakeup debug guide 
    
    Change-Id: I490bf59f74b8a4c5291677ec7699f1df77eb9f1c
    Signed-off-by: sunyue5 <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1844274
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -47,6 +47,8 @@
 
 #include "irq-gic-common.h"
 
+int qrtr_first_msg = 0;
+
 struct redist_region {
 	void __iomem		*redist_base;
 	phys_addr_t		phys_base;
@@ -471,6 +473,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		qrtr_first_msg = 1;
 	}
 }
 
