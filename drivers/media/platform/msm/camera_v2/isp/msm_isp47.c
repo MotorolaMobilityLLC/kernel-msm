01962896e59bad9c400dbdaa4fb579bdc7fc1959
Monica Mabuchi <monicam@motorola.com>
2021-01-08 14:28:48 -0300

    Fix for preview lag in camera after Zoom
    reverting the commmit below that caused the regression in camera
    Revert "msm: camera: Add 12 bit pix_format support in vfe fetch engine"
    
    This reverts commit d766ad875f65591d9ff3fa96e5c52e6c958b268a.
    
    Change-Id: Idaee72eeb8ef0b0154d610d0287893b1878204fe
    Signed-off-by: Monica Mabuchi <monicam@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1847434
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Cleber Kawamorita <cleberk@motorola.com>
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1291,10 +1291,6 @@ void msm_vfe47_cfg_fetch_engine(struct vfe_device *vfe_dev,
 		case V4L2_PIX_FMT_P16GBRG10:
 		case V4L2_PIX_FMT_P16GRBG10:
 		case V4L2_PIX_FMT_P16RGGB10:
-		case V4L2_PIX_FMT_P16BGGR12:
-		case V4L2_PIX_FMT_P16GBRG12:
-		case V4L2_PIX_FMT_P16GRBG12:
-		case V4L2_PIX_FMT_P16RGGB12:
 			main_unpack_pattern = 0xB210;
 			break;
 		default:
