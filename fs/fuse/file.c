b0ff72f0d2913dba3cb31d86591b689bb3a79c3b
Alessio Balsini <balsini@google.com>
2021-01-25 17:05:31 +0000

    FROMLIST: fuse: Introduce passthrough for mmap
    Enabling FUSE passthrough for mmap-ed operations not only affects
    performance, but has also been shown as mandatory for the correct
    functioning of FUSE passthrough.
    yanwu noticed [1] that a FUSE file with passthrough enabled may suffer
    data inconsistencies if the same file is also accessed with mmap. What
    happens is that read/write operations are directly applied to the lower
    file system (and its cache), while mmap-ed operations are affecting the
    FUSE cache.
    
    Extend the FUSE passthrough implementation to also handle memory-mapped
    FUSE file, to both fix the cache inconsistencies and extend the
    passthrough performance benefits to mmap-ed operations.
    
    [1] https://lore.kernel.org/lkml/20210119110654.11817-1-wu-yan@tcl.com/
    
    Mot-CRs-fixed: (CR)
    Bug: 168023149
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-9-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: Ifad4698b0380f6e004c487940ac6907b9a9f2964
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Reviewed-on: https://gerrit.mot.com/2014523
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2330,6 +2330,9 @@ static int fuse_file_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct fuse_file *ff = file->private_data;
 
+	if (ff->passthrough.filp)
+		return fuse_passthrough_mmap(file, vma);
+
 	if (ff->open_flags & FOPEN_DIRECT_IO) {
 		/* Can't provide the coherency needed for MAP_SHARED */
 		if (vma->vm_flags & VM_MAYSHARE)
