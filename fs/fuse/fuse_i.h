e1af7cee69ac544cf13356be86ee8e2b5da16c46
Alessio Balsini <balsini@google.com>
2021-01-25 17:02:28 +0000

    FROMLIST: fuse: Definitions and ioctl for passthrough
    Expose the FUSE_PASSTHROUGH interface to user space and declare all the
    basic data structures and functions as the skeleton on top of which the
    FUSE passthrough functionality will be built.
    
    As part of this, introduce the new FUSE passthrough ioctl, which allows
    the FUSE daemon to specify a direct connection between a FUSE file and a
    lower file system file. Such ioctl requires user space to pass the file
    descriptor of one of its opened files through the fuse_passthrough_out
    data structure introduced in this patch. This structure includes extra
    fields for possible future extensions.
    Also, add the passthrough functions for the set-up and tear-down of the
    data structures and locks that will be used both when fuse_conns and
    fuse_files are created/deleted.
    
    Mot-CRs-fixed: (CR)
    Bug: 168023149
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-4-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I732532581348adadda5b5048a9346c2b0868d539
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Reviewed-on: https://gerrit.mot.com/2014518
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -165,6 +165,14 @@ enum {
 struct fuse_conn;
 struct fuse_release_args;
 
+/**
+ * Reference to lower filesystem file for read/write operations handled in
+ * passthrough mode
+ */
+struct fuse_passthrough {
+	struct file *filp;
+};
+
 /** FUSE specific file data */
 struct fuse_file {
 	/** Fuse connection for this file */
@@ -209,6 +217,9 @@ struct fuse_file {
 		u64 version;
 
 	} readdir;
+
+	/** Container for data related to the passthrough functionality */
+	struct fuse_passthrough passthrough;
 
 	/** RB node to be linked on fuse_conn->polled_files */
 	struct rb_node polled_node;
@@ -726,6 +737,9 @@ struct fuse_conn {
 	/* Do not show mount options */
 	unsigned int no_mount_options:1;
 
+	/** Passthrough mode for read/write IO */
+	unsigned int passthrough:1;
+
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
 
@@ -761,6 +775,12 @@ struct fuse_conn {
 
 	/** List of device instances belonging to this connection */
 	struct list_head devices;
+
+	/** IDR for passthrough requests */
+	struct idr passthrough_req;
+
+	/** Protects passthrough_req */
+	spinlock_t passthrough_req_lock;
 };
 
 static inline struct fuse_conn *get_fuse_conn_super(struct super_block *sb)
@@ -1110,5 +1130,12 @@ unsigned int fuse_len_args(unsigned int numargs, struct fuse_arg *args);
  */
 u64 fuse_get_unique(struct fuse_iqueue *fiq);
 void fuse_free_conn(struct fuse_conn *fc);
+
+/* passthrough.c */
+int fuse_passthrough_open(struct fuse_dev *fud,
+			  struct fuse_passthrough_out *pto);
+int fuse_passthrough_setup(struct fuse_conn *fc, struct fuse_file *ff,
+			   struct fuse_open_out *openarg);
+void fuse_passthrough_release(struct fuse_passthrough *passthrough);
 
 #endif /* _FS_FUSE_I_H */
