5c7c54a975e8ce96df9b57d2cb77ad9589236a10
Yuchang Guo <guoyc1@lenovo.com>
2021-02-02 18:46:46 +0530

    msm: ADSPRPC: Awake PM with a timeout
    Awake PM with a timeout in kernel soon after response from DSP to
    keep the system awake and allow the ioctl call reach user space from
    kernel space.
    
    QC CR2561780
    
    Change-Id: If518e17ec300565af2d16feca0524e741790ba7f
    Signed-off-by: Jeya R <jeyr@codeaurora.org>
    Acked-by: Maitreyi Gupta <maitreyi@qti.qualcomm.com>
    Reviewed-on: https://gerrit.mot.com/1873901
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Nan Wu <wunan3@motorola.com>
    Reviewed-by: Wen Xie <xiewen3@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/char/adsprpc_shared.h
+++ drivers/char/adsprpc_shared.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -246,6 +246,7 @@ enum fastrpc_control_type {
 	FASTRPC_CONTROL_SMMU		=	2,
 	FASTRPC_CONTROL_KALLOC		=	3,
 	FASTRPC_CONTROL_WAKELOCK	=	4,
+	FASTRPC_CONTROL_PM			=	5,
 };
 
 struct fastrpc_ctrl_latency {
@@ -258,7 +259,11 @@ struct fastrpc_ctrl_kalloc {
 };
 
 struct fastrpc_ctrl_wakelock {
-	uint32_t enable;	/* wakelock control enable */
+	uint32_t enable;	/* timeout(in ms) for PM to keep system awake */
+};
+
+struct fastrpc_ctrl_pm {
+	uint32_t timeout;	/* wakelock control enable */
 };
 
 struct fastrpc_ioctl_control {
@@ -267,6 +272,7 @@ struct fastrpc_ioctl_control {
 		struct fastrpc_ctrl_latency lp;
 		struct fastrpc_ctrl_kalloc kalloc;
 		struct fastrpc_ctrl_wakelock wp;
+		struct fastrpc_ctrl_pm pm;
 	};
 };
 
