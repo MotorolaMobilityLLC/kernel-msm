6d5dd3c001736ad93d2d32e61065b0b2fa541c63
Mike SU <suzh1@lenovo.com>
2021-02-05 09:57:57 +0800

    (CR) Make NCM driver compatible with windows
    1. Fix NTP-32 support
    When connecting a CDC-NCM gadget to an host that uses the NTP-32 mode,
    or that relies on the default CRC setting, the current implementation gets
    confused, and does not expect the correct signature for its packets.
    Fix this, by ensuring that the ndp_sign member in the f_ncm structure
    always contain a valid value.
    https://lore.kernel.org/patchwork/patch/1062690/
    
    2. Add OS descriptor support
    To be able to use the default USB class drivers available in Microsoft
    Windows, we need to add OS descriptors to the exported USB gadget to
    tell the OS that we are compatible with the built-in drivers.
    Copy the OS descriptor support from f_rndis into f_ncm. As a result,
    using the WINNCM compatible ID, the UsbNcm driver is loaded on
    enumeration without the need for a custom driver or inf file.
    https://lore.kernel.org/patchwork/patch/1062691/
    
    3. Use "ncm0" interface name
    The default interfacename "usb0" is confused. Use "ncm0" better.
    
    Change-Id: I36d085c519f8205d7f9e0a1d9c669d3fb6623483
    Reviewed-on: https://gerrit.mot.com/1872002
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>
    Reviewed-by: Zonghua Liu <a17671@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit edda0121735f5436ef2287379fb6b57325ef04f4)
    Reviewed-on: https://gerrit.mot.com/1916170
    Reviewed-by: Xuejin Wang <wangxjn@motorola.com>
    Tested-by: Mike SU <suzh1@mt.com>

--- drivers/usb/gadget/function/u_ncm.h
+++ drivers/usb/gadget/function/u_ncm.h
@@ -20,6 +20,12 @@ struct f_ncm_opts {
 	struct net_device		*net;
 	bool				bound;
 
+	/* Begin MOTODESK-2239 Add OS descriptor support. suzh1 2021-02-05 */
+	struct config_group		*ncm_interf_group;
+	struct usb_os_desc		ncm_os_desc;
+	char				ncm_ext_compat_id[16];
+	/* End MOTODESK-2239 */
+
 	/*
 	 * Read/write access to configfs attributes is handled by configfs.
 	 *
