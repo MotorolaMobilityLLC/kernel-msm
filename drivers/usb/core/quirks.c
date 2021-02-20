e5f264aa50d3b5aa51fa8479a9eea968534a88d8
tanhua1 <tanhua1@lenovo.com>
2021-02-20 17:27:33 +0800

    usb: add UGREEN cm136 type-c dock to quirks list
    UGREEN cm136 type-c dock with LPM issue, DP will not work
    after resume from low power mode, add USB_QUIRK_RESET_RESUME
    flag into the quirks list to disable the autosuspend for
    this device.
    
    Change-Id: Id918da5045ab09a31ce2c74cbe44de9ad55b943d
    Signed-off-by: tanhua1 <tanhua1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1883372
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zonghua Liu <a17671@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: tanhua1 <tanhua1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2100446

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -482,6 +482,10 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x1c75, 0x0204), .driver_info =
 			USB_QUIRK_CONFIG_INTF_STRINGS },
 
+	/* VIA Labs */
+	{ USB_DEVICE(0x2109, 0x2817), .driver_info = USB_QUIRK_RESET_RESUME },
+	{ USB_DEVICE(0x2109, 0x0817), .driver_info = USB_QUIRK_RESET_RESUME },
+
 	/* Acer C120 LED Projector */
 	{ USB_DEVICE(0x1de1, 0xc102), .driver_info = USB_QUIRK_NO_LPM },
 
