287ebcf3121bce160b70687ca9b221cd792583c3
yuedl1 <yuedl1@lenovo.com>
2021-03-17 15:16:15 +0800

    Revert "(CR) fs: support samsung's turbo write and HPB features"
    Based on kernel5.4, samsung provide a new  TW/HPB driver to motorola. so
    revert old TW/HPB drvier.
    This reverts commit 29d92809b0cccc46d41a9c02e296fffcd84c04c9.
    
    Change-Id: Icb37079f10afbb854025350168c09446870e0caf
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1903501
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- fs/ext4/page-io.c
+++ fs/ext4/page-io.c
@@ -344,10 +344,6 @@ void ext4_io_submit(struct ext4_io_submit *io)
 		int io_op_flags = io->io_wbc->sync_mode == WB_SYNC_ALL ?
 				  REQ_SYNC : 0;
 		io->io_bio->bi_write_hint = io->io_end->inode->i_write_hint;
-#ifdef CONFIG_FS_HPB
-		if(ext4_test_inode_state(io->io_end->inode, EXT4_STATE_HPB))
-			io_op_flags |= REQ_HPB_PREFER;
-#endif
 		bio_set_op_attrs(io->io_bio, REQ_OP_WRITE, io_op_flags);
 		submit_bio(io->io_bio);
 	}
