67c0fbb3356e3f26e013f8b6849a95496ecb1342
Jeya R <jeyr@codeaurora.org>
2021-03-18 14:04:49 +0530

    msm: adsprpc: overflow vulnerability by race condition in adsprpc driver
    Create local copy of current->comm to avoid the possibility of modification
    in race condition.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2021-30295
    CRs-Fixed: 2902200
    Bug: 182501529
    
    Change-Id: Ie10f6577ed7edb9279a36039348e7a1ad25239f9
    Acked-by: Nishant Chaubey <chaubey@qti.qualcomm.com>
    Signed-off-by: Jeya R <jeyr@codeaurora.org>
    Signed-off-by: Gajjala Chakradhar <gajjalac@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2037832
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 573f26b6af1b789075fc9dd7f005c52695df5b9e)

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3622,22 +3622,26 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 {
 	int err = 0, buf_size = 0;
 	char strpid[PID_SIZE];
+	char cur_comm[TASK_COMM_LEN];
 
+	memcpy(cur_comm, current->comm, TASK_COMM_LEN);
+	cur_comm[TASK_COMM_LEN-1] = '\0';
 	fl->tgid = current->tgid;
 	snprintf(strpid, PID_SIZE, "%d", current->pid);
-	buf_size = strlen(current->comm) + strlen("_") + strlen(strpid) + 1;
+	buf_size = strlen(cur_comm) + strlen("_") + strlen(strpid) + 1;
 	fl->debug_buf = kzalloc(buf_size, GFP_KERNEL);
 	if (!fl->debug_buf) {
 		err = -ENOMEM;
 		return err;
 	}
-	snprintf(fl->debug_buf, UL_SIZE, "%.10s%s%d",
-			current->comm, "_", current->pid);
+	snprintf(fl->debug_buf, buf_size, "%.10s%s%d",
+			cur_comm, "_", current->pid);
 	fl->debugfs_file = debugfs_create_file(fl->debug_buf, 0644,
 					debugfs_root, fl, &debugfs_fops);
 	if (!fl->debugfs_file)
 		pr_warn("Error: %s: %s: failed to create debugfs file %s\n",
-				current->comm, __func__, fl->debug_buf);
+				cur_comm, __func__, fl->debug_buf);
+
 	return err;
 }
 
