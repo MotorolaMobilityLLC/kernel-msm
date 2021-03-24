f2d8d25c3feec003b8c626d65da7b62c570abacd
dengwei1 <dengwei1@motorola.com>
2021-03-24 13:46:29 +0800

    guamp: emmc add ffu
    Porting from (CR)
    emmc add ffu
    support
    
    Change-Id: I1e736ef7989d3af1664afa0d967d17f7ced79b4b
    Signed-off-by: dengwei1 <dengwei1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1934932
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Wang <wangwang1@mt.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1961314
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Reviewed-by: Leandro Castelli Silverio <leandros@motorola.com>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -239,6 +239,9 @@ static inline bool mmc_op_multi(u32 opcode)
  */
 
 #define EXT_CSD_CMDQ_MODE_EN		15	/* R/W */
+#define EXT_CSD_FFU_STATUS		26	/* R */
+#define EXT_CSD_MODE_OPERATION_CODES	29	/* W */
+#define EXT_CSD_MODE_CONFIG		30	/* R/W */
 #define EXT_CSD_FLUSH_CACHE		32      /* W */
 #define EXT_CSD_CACHE_CTRL		33      /* R/W */
 #define EXT_CSD_POWER_OFF_NOTIFICATION	34	/* R/W */
@@ -301,8 +304,12 @@ static inline bool mmc_op_multi(u32 opcode)
 #define EXT_CSD_PRE_EOL_INFO		267	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A	268	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_B	269	/* RO */
+#define EXT_CSD_NUM_OF_FW_SEC_PROG	302	/* RO, 4 bytes */
 #define EXT_CSD_CMDQ_DEPTH		307	/* RO */
 #define EXT_CSD_CMDQ_SUPPORT		308	/* RO */
+#define EXT_CSD_FFU_ARG			487	/* RO, 4 bytes */
+#define EXT_CSD_OPERATION_CODE_TIMEOUT	491	/* RO */
+#define EXT_CSD_FFU_FEATURES		492	/* RO */
 #define EXT_CSD_SUPPORTED_MODE		493	/* RO */
 #define EXT_CSD_TAG_UNIT_SIZE		498	/* RO */
 #define EXT_CSD_DATA_TAG_SUPPORT	499	/* RO */
