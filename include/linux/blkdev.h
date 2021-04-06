538d43f6d81ba19f7c704f2648d4b7b01ae5dad2
Can Guo <cang@codeaurora.org>
2021-04-06 03:31:49 -0700

    block: pm: Fix possible unbalanced nr_pending
    When someone calls blk_get_request() and blk_put_request() without invoking
    blk_execute_rq() in between, nr_pending will be unbalanced. Fix this by
    adding a flag, namely the RQF_PM_ADDED, to track the usage of nr_pending,
    so that blk_pm_put_request() won't touch nr_pending if the flag is not set.
    Extra care should be given to blk_pm_requeue_request(), because blk-flush.c
    inserts flush requests into queue without calling blk_pm_add_request(), if
    the flush requests get requeued by LLD, blk_pm_requeue_request() should
    anyways decrease nr_pending in this situation to keep it balanced.
    
    Change-Id: I7baa2049ac532e4fd22a0ce358133ba88266a314
    Signed-off-by: Ziqi Chen <ziqichen@codeaurora.org>
    Signed-off-by: Can Guo <cang@codeaurora.org>
    Reviewed-on: https://gerrit.mot.com/2030823
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Rafael Ortolan <rafones@motorola.com>
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -119,6 +119,8 @@ typedef __u32 __bitwise req_flags_t;
 /* Look at ->special_vec for the actual data payload instead of the
    bio chain. */
 #define RQF_SPECIAL_PAYLOAD	((__force req_flags_t)(1 << 18))
+/* increased nr_pending for this request */
+#define RQF_PM_ADDED		((__force req_flags_t)(1 << 19))
 
 /* flags that prevent us from merging requests: */
 #define RQF_NOMERGE_FLAGS \
