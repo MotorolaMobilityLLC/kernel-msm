26c653cf8d739de7fdc17655570443cf079b56cc
zhangly19 <zhangly19@lenovo.com>
2021-05-11 10:25:22 +0800

    fix  r1.0_00026  rebase error
    ++++++++++
    ld.lld: error: undefined symbol: smblib_get_prop_usb_voltage_max
    >>> referenced by qpnp-smb5.c:939
    ++++++++++
    
    move "#ifdef QCOM_BASE"  to correct location
    
    Change-Id: I099e7f607e190d3ec3ce17a7a1ede8a7bd1c0005
    Reviewed-on: https://gerrit.mot.com/1945042
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Lianlu Chen <chenll4@lenovo.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -3491,7 +3491,7 @@ int smblib_get_prop_usb_voltage_max_design(struct smb_charger *chg,
 {
 	switch (chg->real_charger_type) {
 	case QTI_POWER_SUPPLY_TYPE_USB_HVDCP:
-#ifdef QCOM_BASE
+
 		if (chg->qc2_unsupported_voltage == QC2_NON_COMPLIANT_9V) {
 			val->intval = MICRO_5V;
 			break;
@@ -3522,6 +3522,7 @@ int smblib_get_prop_usb_voltage_max(struct smb_charger *chg,
 {
 	switch (chg->real_charger_type) {
 	case QTI_POWER_SUPPLY_TYPE_USB_HVDCP:
+#ifdef QCOM_BASE
 		if (chg->qc2_unsupported_voltage == QC2_NON_COMPLIANT_9V) {
 			val->intval = MICRO_5V;
 			break;
