184f77ae8a4a657ff62e5170b102acfc301362f6
mahj8 <mahj8@lenovo.com>
2021-05-20 12:57:41 +0800

    smb5: add main charging on-off IIO prop
    1. add main charging on-off IIO prop
    2. define more mmi IIO prop for charger pump
    
    Change-Id: Iaf49f816b53d64bb61e44fcb82fc25d8d7dd72dd
    Signed-off-by: mahj8 <mahj8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1940727
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qcom/smb5-iio.h
+++ drivers/power/supply/qcom/smb5-iio.h
@@ -201,6 +201,7 @@ static const struct smb5_iio_prop_channels smb5_chans_pmic[] = {
 	SMB5_CHAN_ACTIVITY("battery_force_recharge", FORCE_RECHARGE)
 	SMB5_CHAN_ACTIVITY("battery_fcc_stepper_enable", FCC_STEPPER_ENABLE)
 	SMB5_CHAN_INDEX("usb_typec_accessory_mode", TYPEC_ACCESSORY_MODE)
+	SMB5_CHAN_INDEX("charging_enabled", CHARGING_ENABLED)
 };
 
 struct iio_channel **get_ext_channels(struct device *dev,
