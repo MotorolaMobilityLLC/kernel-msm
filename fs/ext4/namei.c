44b74a5a975a70079b7c068fcd22db72f78de393
Daniel Rosenberg <drosen@google.com>
2021-05-22 00:41:32 +0000

    BACKPORT: ext4: fix no-key deletion for encrypt+casefold
    [ Upstream commit 63e7f1289389c8dff3c766f01ac1cc1c874b2ba5 ]
    
    commit 471fbbea7ff7 ("ext4: handle casefolding with encryption") is
    missing a few checks for the encryption key which are needed to
    support deleting enrypted casefolded files when the key is not
    present.
    
    This bug made it impossible to delete encrypted+casefolded directories
    without the encryption key, due to errors like:
    
        W         : EXT4-fs warning (device vdc): __ext4fs_dirhash:270: inode #49202: comm Binder:378_4: Siphash requires key
    
    Repro steps in kvm-xfstests test appliance:
          mkfs.ext4 -F -E encoding=utf8 -O encrypt /dev/vdc
          mount /vdc
          mkdir /vdc/dir
          chattr +F /vdc/dir
          keyid=$(head -c 64 /dev/zero | xfs_io -c add_enckey /vdc | awk '{print $NF}')
          xfs_io -c "set_encpolicy $keyid" /vdc/dir
          for i in `seq 1 100`; do
              mkdir /vdc/dir/$i
          done
          xfs_io -c "rm_enckey $keyid" /vdc
          rm -rf /vdc/dir # fails with the bug
    
    Fixes: 471fbbea7ff7 ("ext4: handle casefolding with encryption")
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Link: https://lore.kernel.org/r/20210522004132.2142563-1-drosen@google.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Change-Id: Idbf9a46ebac38df02603c13206b41459762d2b8b

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1389,7 +1389,8 @@ int ext4_fname_setup_ci_filename(struct inode *dir, const struct qstr *iname,
 	struct dx_hash_info *hinfo = &name->hinfo;
 	int len;
 
-	if (!needs_casefold(dir)) {
+	if (!IS_CASEFOLDED(dir) || !dir->i_sb->s_encoding ||
+	    (IS_ENCRYPTED(dir) && !fscrypt_has_encryption_key(dir))) {
 		cf_name->name = NULL;
 		return 0;
 	}
@@ -1440,7 +1441,8 @@ static bool ext4_match(struct inode *parent,
 #endif
 
 #ifdef CONFIG_UNICODE
-	if (needs_casefold(parent)) {
+	if (parent->i_sb->s_encoding && IS_CASEFOLDED(parent) &&
+	    (!IS_ENCRYPTED(parent) || fscrypt_has_encryption_key(parent))) {
 		if (fname->cf_name.name) {
 			struct qstr cf = {.name = fname->cf_name.name,
 					  .len = fname->cf_name.len};
