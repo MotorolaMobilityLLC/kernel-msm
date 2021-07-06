ad70ce42170428776a0da7c6e0d33696d52c0406
Vaibhav Agrawal <vagrawa@codeaurora.org>
2021-07-06 17:31:06 +0530

    mmc: host: Add recovery_finish crypto vops
    Define recovery_finish crypto vops to support
    MMC CQE recovery handler path.
    
    Test: Initiate CQE recovery, which calls into crypto
    specific recovery method. If the crypto vops are
    registered on the device, this flow does not lead to
    a null pointer dereference with this fix included.
    
    Change-Id: I6dfaf52a303b9ffa71b6b65779f8e657d6ca051a
    Signed-off-by: Vaibhav Agrawal <vagrawa@codeaurora.org>

--- drivers/mmc/host/cqhci-crypto-qti.h
+++ drivers/mmc/host/cqhci-crypto-qti.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2020 - 2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _UFSHCD_CRYPTO_QTI_H
@@ -27,5 +27,7 @@ int cqhci_crypto_qti_prep_desc(struct cqhci_host *host,
 				u64 *ice_ctx);
 
 int cqhci_crypto_qti_reset(struct cqhci_host *host);
+
+int cqhci_crypto_qti_recovery_finish(struct cqhci_host *host);
 
 #endif /* _UFSHCD_ICE_QTI_H */
