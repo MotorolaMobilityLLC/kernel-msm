5139e8a7dfa9c4e76bfc80d760b131b212a1464c
Kaushal Hooda <quic_khooda@quicinc.com>
2021-07-20 17:31:18 +0530

    net: qrtr: Prevent wakeups using service id based filter
    Filter data from adsp by using service id so that it will not
    have wake up capable packets. Service id can be read from
    DT file and wakeup could be avoided for corresponding packets.
    
    Change-Id: Ibbb5f74bb153cdd333f8037b498c366d4f2ac5f8
    Signed-off-by: Sarannya S <quic_sarannya@quicinc.com>
    Signed-off-by: Kaushal Hooda <quic_khooda@quicinc.com>

--- net/qrtr/smd.c
+++ net/qrtr/smd.c
@@ -64,7 +64,8 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 	struct qrtr_smd_dev *qdev;
 	u32 net_id;
 	bool rt;
-	int rc;
+	int rc, size;
+	u32 *svc_arr = NULL;
 	pr_info("%s:Entered\n", __func__);
 
 	qdev = devm_kzalloc(&rpdev->dev, sizeof(*qdev), GFP_KERNEL);
@@ -81,7 +82,18 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 
 	rt = of_property_read_bool(rpdev->dev.of_node, "qcom,low-latency");
 
-	rc = qrtr_endpoint_register(&qdev->ep, net_id, rt);
+	size = of_property_count_u32_elems(rpdev->dev.of_node, "qcom,non-wake-svc");
+
+	if (size > 0) {
+		if (size > MAX_NON_WAKE_SVC_LEN)
+			size = MAX_NON_WAKE_SVC_LEN;
+		svc_arr = kmalloc_array(size, sizeof(u32), GFP_KERNEL);
+
+		of_property_read_u32_array(rpdev->dev.of_node, "qcom,non-wake-svc",
+					   svc_arr, size);
+	}
+	rc = qrtr_endpoint_register(&qdev->ep, net_id, rt, svc_arr);
+
 	if (rc)
 		return rc;
 
