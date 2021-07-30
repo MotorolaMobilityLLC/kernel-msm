4a3b90ac240454d91b3add249a24bd07026b46a4
xiangming1 <xiangming1@lenovo.com>
2021-07-30 18:15:48 +0800

    Add logic to support different bdwlan at Rogue
    For Rugue, it will have PRC/NA/ROW SKU, and we need add logic to
    support loading different BDF file for each SKU
    
    Change-Id: Ibe2e3ec70f812b263d96be9534bca3fa5f7ff2ff
    Signed-off-by: xiangming1 <xiangming1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2029156
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ningkai Zhuang <zhuangnk1@mt.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/net/wireless/cnss2/qmi.c
+++ drivers/net/wireless/cnss2/qmi.c
@@ -13,6 +13,14 @@
 #include "qmi.h"
 #include "genl.h"
 
+// BEGIN Support loading different bdwlan.elf
+#include <linux/bootconfig.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#define BOOTCONFIG_FULLPATH "/proc/bootconfig"
+static char bootconfig_tmp[256];
+//END Support loading different bdwlan.elf
+
 #define WLFW_SERVICE_INS_ID_V01		1
 #define WLFW_CLIENT_ID			0x4b4e454c
 #define BDF_FILE_NAME_PREFIX		"bdwlan"
@@ -60,6 +68,153 @@ void cnss_ignore_qmi_failure(bool ignore)
 #define CNSS_QMI_ASSERT() do { } while (0)
 void cnss_ignore_qmi_failure(bool ignore) { }
 #endif
+// BEGIN Support loading different bdwlan.elf
+#define NV_EPA "epa"
+#define NV_IPA "ipa"
+#define MOTO_STRING_LEN 32
+static char device_ptr[MOTO_STRING_LEN] = {0};
+static char radio_ptr[MOTO_STRING_LEN] = {0};
+
+typedef struct moto_product {
+	char hw_device[32];
+	char hw_radio[32];
+	char nv_name[64];
+} moto_product;
+
+static moto_product products_list[] = {
+	{"hiphic",	"PRC",	NV_IPA},
+	{"hiphi",	"all",	NV_EPA},
+	/* Terminator */
+	{{0}, {0}, {0}},
+};
+
+
+static inline ssize_t kernel_read_stub(struct file *file, void *addr, size_t count)
+{
+	loff_t pos = 0;
+	return kernel_read(file, addr, count, &pos);
+}
+
+static int cnss_get_bootarg(const char *bootconfig_key, char **bootconfig_val)
+{
+	struct file *filep = NULL;
+	char *xbc_buf = NULL;
+	int bytes = 0;
+	char *start_bootdevice = NULL;
+	int rc = 0;
+
+	filep = filp_open(BOOTCONFIG_FULLPATH, O_RDONLY, 0600);
+	if (IS_ERR_OR_NULL(filep)) {
+		rc = PTR_ERR(filep);
+		pr_err("opening (%s) errno=%d\n", BOOTCONFIG_FULLPATH, rc);
+		return rc;
+	}
+
+	xbc_buf = kzalloc(XBC_DATA_MAX, GFP_KERNEL);
+	if (!xbc_buf) {
+		pr_err("alloc len %d memory fail\n", XBC_DATA_MAX);
+		filp_close(filep, NULL);
+		return -ENOMEM;
+	}
+
+	bytes = kernel_read_stub(filep, (void *) xbc_buf, XBC_DATA_MAX);
+	pr_debug("read bootconfig bytes %d\n", bytes);
+	if (bytes <= 0) {
+		pr_err("fail read bytes %d\n", bytes);
+		kfree(xbc_buf);
+		filp_close(filep, NULL);
+		return -EIO;
+	}
+
+	start_bootdevice = strstr(xbc_buf, bootconfig_key);
+	if (start_bootdevice) {
+		char *p = start_bootdevice + strlen(bootconfig_key);
+		int i = 0;
+
+		memset(bootconfig_tmp, 0, sizeof(bootconfig_tmp));
+		while (p && *p && (*p != '\n') && (*p != '"')
+				&& (i < sizeof(bootconfig_tmp))) {
+			bootconfig_tmp[i] = *p;
+			i++;
+			p++;
+		}
+		*bootconfig_val = bootconfig_tmp;
+		pr_info("bootconfig key %s%s\"\n", bootconfig_key, *bootconfig_val);
+	} else {
+		pr_err("cannot find key %s\n", bootconfig_key);
+		rc = -ENOENT;
+	}
+
+	kfree(xbc_buf);
+	filp_close(filep, NULL);
+
+	return rc;
+}
+
+static int is_void_product(moto_product *entry)
+{
+	return entry && !strlen(entry->hw_device) && !strlen(entry->hw_radio);
+}
+
+static int num_of_products(moto_product *list)
+{
+	int num = 0;
+	if (!list) return 0;
+	while (!is_void_product(list + num)) num++;
+	return num;
+}
+
+static int get_moto_device()
+{
+        char *bootdevice = NULL;
+        int rc = 0;
+        rc = cnss_get_bootarg("androidboot.device = \"", &bootdevice);
+        if (rc || !bootdevice){
+            cnss_pr_err("device string is error");
+            return -ENOMEM;
+        }else{
+            strlcpy(device_ptr, bootdevice,MOTO_STRING_LEN);
+            return 0;
+        }
+}
+
+static int get_moto_radio()
+{
+        char *radiodevice = NULL;
+        int rc = 0;
+        rc = cnss_get_bootarg("androidboot.radio = \"", &radiodevice);
+        if (rc || !radiodevice){
+            cnss_pr_err("radio string is error");
+            return -ENOMEM;
+        }else{
+            strlcpy(radio_ptr, radiodevice,MOTO_STRING_LEN);
+            return 0;
+        }
+}
+
+static int selectFileNameByProduct(char *filename)
+{
+	int i, num, ret = 0;
+        if(get_moto_radio() != 0 || get_moto_device() != 0){
+              cnss_pr_err("device or radio not present ");
+        }
+
+	num = num_of_products(products_list);
+	for (i = 0; i < num; i++) {
+		if (strncmp(device_ptr, (products_list+i)->hw_device, strlen((products_list+i)->hw_device)) == 0) {
+			if(strncmp(radio_ptr, (products_list+i)->hw_radio, strlen((products_list+i)->hw_radio)) == 0 ||
+				strncmp((products_list+i)->hw_radio, "all", strlen((products_list+i)->hw_radio)) == 0) {
+				sprintf(filename, "%s.%s.%s", ELF_BDF_FILE_NAME,
+					(products_list+i)->hw_device, (products_list+i)->nv_name);
+				ret = 1;
+				break;
+			}
+		}
+	}
+
+        return ret;
+}
+// END Support loading different bdwlan.elf
 
 static char *cnss_qmi_mode_to_str(enum cnss_driver_mode mode)
 {
@@ -546,8 +701,11 @@ static int cnss_get_bdf_file_name(struct cnss_plat_data *plat_priv,
 
 	switch (bdf_type) {
 	case CNSS_BDF_ELF:
+			// Support loading different bdwlan.elf
+		if (selectFileNameByProduct(filename) > 0)
+			return ret;
 		/* Board ID will be equal or less than 0xFF in GF mask case */
-		if (plat_priv->board_info.board_id == 0xFF) {
+		else if (plat_priv->board_info.board_id == 0xFF) {
 			if (plat_priv->chip_info.chip_id & CHIP_ID_GF_MASK)
 				snprintf(filename_tmp, filename_len,
 					 ELF_BDF_FILE_NAME_GF);
