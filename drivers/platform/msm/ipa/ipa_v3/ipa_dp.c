a3c2c824b5267a000fd87fdd50d0b47fc93db0c8
Ilia Lin <ilialin@codeaurora.org>
2021-08-26 11:14:33 +0300

    ipa: Null persistent pointers after free
    Assign NULL to pointers that may be used later
    after calling kfree on them.
    
    Change-Id: I3298eb484c92ee2373f0bc41aae8ae45fb373cf0
    Signed-off-by: Ilia Lin <ilialin@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -1297,10 +1297,12 @@ fail_repl:
 	ep->sys->repl_hdlr = ipa3_replenish_rx_cache;
 	ep->sys->repl->capacity = 0;
 	kfree(ep->sys->repl);
+	ep->sys->repl = NULL;
 fail_page_recycle_repl:
 	if (ep->sys->page_recycle_repl) {
 		ep->sys->page_recycle_repl->capacity = 0;
 		kfree(ep->sys->page_recycle_repl);
+		ep->sys->page_recycle_repl = NULL;
 	}
 fail_gen2:
 	ipa_pm_deregister(ep->sys->pm_hdl);
@@ -2703,6 +2705,7 @@ static void ipa3_cleanup_rx(struct ipa3_sys_context *sys)
 
 		kfree(sys->repl->cache);
 		kfree(sys->repl);
+		sys->repl = NULL;
 	}
 	if (sys->page_recycle_repl) {
 		for (i = 0; i < sys->page_recycle_repl->capacity; i++) {
@@ -2721,6 +2724,7 @@ static void ipa3_cleanup_rx(struct ipa3_sys_context *sys)
 		}
 		kfree(sys->page_recycle_repl->cache);
 		kfree(sys->page_recycle_repl);
+		sys->page_recycle_repl = NULL;
 	}
 }
 
