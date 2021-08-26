3a9c5960d9516b1d0615e163085bbf25cc88891b
Rohan Sethi <rohsethi@codeaurora.org>
2021-08-26 14:34:17 +0530

    msm: kgsl: Add timeline traces
    Add some traces to make it easier to debug and track timeline behavior.
    
    Change-Id: Ic0dedbadbfe623c675616a0d2fdd800b49f14705
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rohan Sethi <rohsethi@codeaurora.org>

--- drivers/gpu/msm/kgsl_drawobj.c
+++ drivers/gpu/msm/kgsl_drawobj.c
@@ -904,6 +904,7 @@ int kgsl_drawobj_add_timeline(struct kgsl_device_private *dev_priv,
 			goto err;
 		}
 
+		trace_kgsl_drawobj_timeline(val.timeline, val.seqno);
 		timelineobj->timelines[i].seqno = val.seqno;
 
 		src += cmd.timelines_size;
