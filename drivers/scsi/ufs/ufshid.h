856a4bb6f9c1b5dc2b72b0d05366db30452dbf28
zhucc2 <zhucc2@motorola.com>
2021-10-08 12:51:14 +0800

    ufs: Add Micron UFS HID support
    As Micron said, Micron UFS HID could reuse SamSung HID driver, but need
    change some flags and attibutes definitions.
    
    Change-Id: If485cd01b6b69f1f14947fe84520c190262d966f
    Reviewed-on: https://gerrit.mot.com/2085180
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Dongliang Yue <yuedl1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/scsi/ufs/ufshid.h
+++ drivers/scsi/ufs/ufshid.h
@@ -103,6 +103,20 @@ enum {
 	HID_LEV_RED	= 3,
 };
 
+#if defined(CONFIG_MICRON_UFSHID)
+enum {
+	HID_LEV_GREEN_MICRON  = 0,
+	HID_LEV_RED_MICRON   = 1,
+};
+
+enum {
+	HID_PROG_IDLE		= 0,
+	HID_PROG_ONGOING	= 1,
+	HID_PROG_STOP	= 2, //stopped by Host
+	HID_PROG_COMPLETE	= 3,
+};
+#endif
+
 struct ufshid_dev {
 	struct ufsf_feature *ufsf;
 
