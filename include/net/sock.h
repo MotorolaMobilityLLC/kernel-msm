30b18ac4789f1104775775be7deded3c87c9ce9d
Eric Dumazet <edumazet@google.com>
2021-11-15 09:11:48 -0800

    net: make sock_inuse_add() available
    commit d477eb9004845cb2dc92ad5eed79a437738a868a upstream.
    
    MPTCP hard codes it, let us instead provide this helper.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    [ cherry-pick from amazon-linux amazon-5.15.y/mainline ]
    Link: https://github.com/amazonlinux/linux/commit/7154d8eaac16
    Signed-off-by: Xiangyu Chen <xiangyu.chen@windriver.com>
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1472,11 +1472,21 @@ static inline void sock_prot_inuse_add(const struct net *net,
 {
 	this_cpu_add(net->core.prot_inuse->val[prot->inuse_idx], val);
 }
+
+static inline void sock_inuse_add(const struct net *net, int val)
+{
+	this_cpu_add(*net->core.sock_inuse, val);
+}
+
 int sock_prot_inuse_get(struct net *net, struct proto *proto);
 int sock_inuse_get(struct net *net);
 #else
 static inline void sock_prot_inuse_add(const struct net *net,
 				       const struct proto *prot, int val)
+{
+}
+
+static inline void sock_inuse_add(const struct net *net, int val)
 {
 }
 #endif
