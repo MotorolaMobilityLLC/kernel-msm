6d6be628eab90b6e7bb1a604895f85d67bb75f2f
Todd Kjos <tkjos@google.com>
2021-11-22 16:03:46 +0530

    ANDROID: xt_quota2: set usersize in xt_match registration object
    Expicitly set what is visible to userspace
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2021-0961
    Bug: 196046570
    
    Test: passed netd test suites
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: Iacec0ef8ae290e01f1b60508d8abcd40a3653c83
    Signed-off-by: Sandeep Ganpat Satpute <sandepgs@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2123616
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ling Jin <lingjin@motorola.com>
    Submit-Approved: Jira Key

--- net/netfilter/xt_quota2.c
+++ net/netfilter/xt_quota2.c
@@ -348,6 +348,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 	{
@@ -358,6 +359,7 @@ static struct xt_match quota_mt2_reg[] __read_mostly = {
 		.match      = quota_mt2,
 		.destroy    = quota_mt2_destroy,
 		.matchsize  = sizeof(struct xt_quota_mtinfo2),
+		.usersize   = offsetof(struct xt_quota_mtinfo2, master),
 		.me         = THIS_MODULE,
 	},
 };
