1bf6908b25c02c12aaac28e5b2637aaff2bb614d
Sayali Patil <quic_sayapati@quicinc.com>
2021-11-26 11:15:11 +0530

    ion: util: Add permission settings for CDSP
    Give CDSP memory execute permssion
    
    Change-Id: I90ec6dcd8cb8c2b4fe77e1666d641b78ab2fe1f3
    Signed-off-by: Sayali Patil <quic_sayapati@quicinc.com>

--- drivers/staging/android/ion/ion_secure_util.c
+++ drivers/staging/android/ion/ion_secure_util.c
@@ -175,6 +175,8 @@ int ion_hyp_assign_sg(struct sg_table *sgt, int *dest_vm_list,
 		if (dest_vm_list[i] == VMID_CP_SEC_DISPLAY ||
 		    dest_vm_list[i] == VMID_CP_DSP_EXT)
 			dest_perms[i] = PERM_READ;
+		else if (dest_vm_list[i] == VMID_CP_CDSP)
+			dest_perms[i] = PERM_READ | PERM_WRITE | PERM_EXEC;
 		else if (dest_vm_list[i] == VMID_CP_CAMERA_ENCODE) {
 			j = i;
 			dest_perms[i] = PERM_READ | PERM_WRITE;
@@ -305,6 +307,8 @@ int ion_hyp_assign_from_flags(u64 base, u64 size, unsigned long flags)
 		if (vmids[i] == VMID_CP_SEC_DISPLAY ||
 		    vmids[i] == VMID_CP_DSP_EXT)
 			modes[i] = PERM_READ;
+		else if (vmids[i] == VMID_CP_CDSP)
+			modes[i] = PERM_READ | PERM_WRITE | PERM_EXEC;
 		else
 			modes[i] = PERM_READ | PERM_WRITE;
 
