96b481c46728804b0bb5d4348e45aa6908a0ddee
zhuwg1 <zhuwg1@motorola.com>
2021-11-29 17:58:10 +0800

    haptics: Restore produce a vibration in boot time
    This reverts commit 7014fc14de986103f2cbb9a398dbc2297099f88a.
    
    Change-Id: I77ebfa77d860ef64ba56255aca49d897426d99a4
    Reviewed-on: https://gerrit.mot.com/2129875
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: <zhuwg1@motorola.com>

--- drivers/input/misc/qcom-hv-haptics.c
+++ drivers/input/misc/qcom-hv-haptics.c
@@ -27,6 +27,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/qpnp/qpnp-pbs.h>
+#include <linux/workqueue.h>
 #include <linux/of_gpio.h>
 
 #include <linux/soc/qcom/battery_charger.h>
@@ -336,6 +337,7 @@
 #define is_between(val, min, max)	\
 	(((min) <= (max)) && ((min) <= (val)) && ((val) <= (max)))
 
+#define VIBRATE_TIMEMS_ON_BOOTING 	1000
 #define HAPTIC_SHOW_MAX_SIZE		50
 
 enum hap_status_sel {
@@ -596,6 +598,7 @@ struct haptics_chip {
 	struct regulator		*hpwr_vreg;
 	struct hrtimer			hbst_off_timer;
 	struct notifier_block		hboost_nb;
+	struct delayed_work		vibrate_work;
 	int				fifo_empty_irq;
 	int             		haptic_context_gpio;
 	u32				long_gain_reduced;
@@ -5194,6 +5197,62 @@ restore:
 	return rc;
 }
 
+static void haptics_vibrate_work_routine(struct work_struct *work)
+{
+	struct haptics_chip *chip = container_of(work, struct haptics_chip, vibrate_work.work);
+
+	haptics_enable_play(chip, false);
+	haptics_enable_hpwr_vreg(chip, false);
+}
+
+static int haptics_produce_vibration(struct haptics_chip *chip)
+{
+	int rc;
+	u8 amplitude;
+	u32 vmax_mv = chip->config.vmax_mv;
+	bool unlock_flag = 0;
+
+	mutex_lock(&chip->play.lock);
+
+	/* Stop other mode playing if there is any */
+	rc = haptics_enable_play(chip, false);
+	if (rc < 0) {
+		dev_err(chip->dev, "Stop playing failed, rc=%d\n", rc);
+		goto unlock;
+	}
+
+	rc = haptics_set_vmax_mv(chip, vmax_mv);
+	if (rc < 0)
+		goto unlock;
+
+	amplitude = get_direct_play_max_amplitude(chip);
+	rc = haptics_set_direct_play(chip, amplitude);
+	if (rc < 0)
+		goto unlock;
+
+	rc = haptics_enable_hpwr_vreg(chip, true);
+	if (rc < 0)
+		goto unlock;
+
+	chip->play.pattern_src = DIRECT_PLAY;
+	rc = haptics_enable_play(chip, true);
+	if (rc < 0)
+		goto unlock;
+
+	schedule_delayed_work(&chip->vibrate_work, msecs_to_jiffies(VIBRATE_TIMEMS_ON_BOOTING));
+	unlock_flag = 1;
+
+unlock:
+	/* Disable play in case it's not been disabled */
+	if( !unlock_flag) {
+		haptics_enable_play(chip, false);
+		rc = haptics_enable_hpwr_vreg(chip, false);
+	}
+	mutex_unlock(&chip->play.lock);
+
+	return rc;
+}
+
 static int haptics_start_lra_calibrate(struct haptics_chip *chip)
 {
 	int rc;
@@ -5550,6 +5609,12 @@ static int haptics_probe(struct platform_device *pdev)
 	if (rc < 0)
 		dev_err(chip->dev, "Creating debugfs failed, rc=%d\n", rc);
 #endif
+
+	INIT_DELAYED_WORK(&chip->vibrate_work, haptics_vibrate_work_routine);
+	rc = haptics_produce_vibration(chip);
+	if(rc < 0)
+		dev_err(chip->dev, "haptics produce vibration failed, rc=%d\n", rc);
+
 	return 0;
 destroy_ff:
 	input_ff_destroy(chip->input_dev);
