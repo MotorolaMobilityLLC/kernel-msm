0494ba54ad83f17d17ac27fb790e64cb6545017b
tinno18 <tinno18@motorola.com>
2021-12-02 17:33:05 +0800

    hawaii OLED: visionox ft3519 touch drvier bringup
    hawaii OLED: 1st LCD
    ft3519 touch drvier bringup
    
    Change-Id: I8a67c0d8ae7aead0e76906b8c7110d4c580ca3f9
    Reviewed-on: https://gerrit.mot.com/2133191
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2136692
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_common.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_common.h
@@ -2,7 +2,7 @@
  *
  * FocalTech fts TouchScreen driver.
  *
- * Copyright (c) 2012-2019, Focaltech Ltd. All rights reserved.
+ * Copyright (c) 2012-2020, Focaltech Ltd. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -36,7 +36,7 @@
 /*****************************************************************************
 * Macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_VERSION      "Focaltech V3.1 20190807"
+#define FTS_DRIVER_VERSION                  "Focaltech V3.3.5 20210514"
 
 #define BYTE_OFF_0(x)           (u8)((x) & 0xFF)
 #define BYTE_OFF_8(x)           (u8)(((x) >> 8) & 0xFF)
@@ -54,7 +54,11 @@
 #define FTS_CHIP_IDC            ((FTS_CHIP_TYPE & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
 #define FTS_HID_SUPPORTTED      ((FTS_CHIP_TYPE & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
 
-#define FTS_CHIP_TYPE_MAPPING {{0x81, 0x54, 0x52, 0x54, 0x52, 0x00, 0x00, 0x54, 0x5C}}
+#define FTS_MAX_CHIP_IDS        8
+
+#define FTS_CHIP_TYPE_MAPPING {{0x89, 0x54, 0x52, 0x54, 0x52, 0x54, 0x5B, 0x54, 0x5E}}
+
+
 
 #define FILE_NAME_LENGTH                    128
 #define ENABLE                              1
@@ -67,6 +71,7 @@
 #define FTS_CMD_READ_ID                     0x90
 #define FTS_CMD_READ_ID_LEN                 4
 #define FTS_CMD_READ_ID_LEN_INCELL          1
+#define FTS_CMD_READ_FW_CONF                0xA8
 /*register address*/
 #define FTS_REG_INT_CNT                     0x8F
 #define FTS_REG_FLOW_WORK_CNT               0x91
@@ -88,7 +93,6 @@
 #define FTS_REG_IDE_PARA_STATUS             0xB6
 #define FTS_REG_GLOVE_MODE_EN               0xC0
 #define FTS_REG_COVER_MODE_EN               0xC1
-#define FTS_REG_REPORT_RATE                 0x88
 #define FTS_REG_CHARGER_MODE_EN             0x8B
 #define FTS_REG_GESTURE_EN                  0xD0
 #define FTS_REG_GESTURE_OUTPUT_ADDRESS      0xD3
@@ -100,15 +104,14 @@
 #define FTS_SYSFS_ECHO_OFF(buf)     (buf[0] == '0')
 
 #define kfree_safe(pbuf) do {\
-	if (pbuf) {\
-		kfree(pbuf);\
-		pbuf = NULL;\
-	}\
+    if (pbuf) {\
+        kfree(pbuf);\
+        pbuf = NULL;\
+    }\
 } while(0)
 
 /*****************************************************************************
-*  Alternative mode (When something goes wrong,
-* the modules may be able to solve the problem.)
+*  Alternative mode (When something goes wrong, the modules may be able to solve the problem.)
 *****************************************************************************/
 /*
  * point report check
@@ -120,21 +123,27 @@
 * Global variable or extern global variabls/functions
 *****************************************************************************/
 struct ft_chip_t {
-	u64 type;
-	u8 chip_idh;
-	u8 chip_idl;
-	u8 rom_idh;
-	u8 rom_idl;
-	u8 pb_idh;
-	u8 pb_idl;
-	u8 bl_idh;
-	u8 bl_idl;
+    u16 type;
+    u8 chip_idh;
+    u8 chip_idl;
+    u8 rom_idh;
+    u8 rom_idl;
+    u8 pb_idh;
+    u8 pb_idl;
+    u8 bl_idh;
+    u8 bl_idl;
+};
+
+struct ft_chip_id_t {
+    u16 type;
+    u16 chip_ids[FTS_MAX_CHIP_IDS];
 };
 
 struct ts_ic_info {
-	bool is_incell;
-	bool hid_supported;
-	struct ft_chip_t ids;
+    bool is_incell;
+    bool hid_supported;
+    struct ft_chip_t ids;
+    struct ft_chip_id_t cid;
 };
 
 /*****************************************************************************
@@ -142,15 +151,15 @@ struct ts_ic_info {
 *****************************************************************************/
 #if FTS_DEBUG_EN
 #define FTS_DEBUG(fmt, args...) do { \
-	printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
+    printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 
 #define FTS_FUNC_ENTER() do { \
-	printk("[FTS_TS]%s: Enter\n", __func__); \
+    printk("[FTS_TS]%s: Enter\n", __func__); \
 } while (0)
 
 #define FTS_FUNC_EXIT() do { \
-	printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
+    printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
 } while (0)
 #else /* #if FTS_DEBUG_EN*/
 #define FTS_DEBUG(fmt, args...)
@@ -159,10 +168,10 @@ struct ts_ic_info {
 #endif
 
 #define FTS_INFO(fmt, args...) do { \
-	printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
+    printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 
 #define FTS_ERROR(fmt, args...) do { \
-	printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
+    printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 #endif /* __LINUX_FOCALTECH_COMMON_H__ */
--- drivers/input/touchscreen/focaltech_touch/focaltech_flash.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_flash.h
@@ -1,5 +1,5 @@
 /************************************************************************
-* Copyright (C) 2012-2019, Focaltech Systems (R)£¬All Rights Reserved.
+* Copyright (c) 2012-2020, Focaltech Systems (R)£¬All Rights Reserved.
 *
 * File Name: focaltech_flash.h
 *
@@ -26,7 +26,7 @@
 #define FTS_ROMBOOT_CMD_SET_PRAM_ADDR_LEN           4
 #define FTS_ROMBOOT_CMD_WRITE                       0xAE
 #define FTS_ROMBOOT_CMD_START_APP                   0x08
-#define FTS_DELAY_PRAMBOOT_START                    10
+#define FTS_DELAY_PRAMBOOT_START                    100
 #define FTS_ROMBOOT_CMD_ECC                         0xCC
 #define FTS_PRAM_SADDR                              0x000000
 #define FTS_DRAM_SADDR                              0xD00000
@@ -34,6 +34,7 @@
 #define FTS_CMD_READ                                0x03
 #define FTS_CMD_READ_DELAY                          1
 #define FTS_CMD_READ_LEN                            4
+#define FTS_CMD_READ_LEN_SPI                        6
 #define FTS_CMD_FLASH_TYPE                          0x05
 #define FTS_CMD_FLASH_MODE                          0x09
 #define FLASH_MODE_WRITE_FLASH_VALUE                0x0A
@@ -44,7 +45,7 @@
 #define FTS_REASE_APP_DELAY                         1350
 #define FTS_ERASE_SECTOR_DELAY                      60
 #define FTS_RETRIES_REASE                           50
-#define FTS_RETRIES_DELAY_REASE                     200
+#define FTS_RETRIES_DELAY_REASE                     400
 #define FTS_CMD_FLASH_STATUS                        0x6A
 #define FTS_CMD_FLASH_STATUS_LEN                    2
 #define FTS_CMD_FLASH_STATUS_NOP                    0x0000
@@ -53,13 +54,16 @@
 #define FTS_CMD_FLASH_STATUS_WRITE_OK               0x1000
 #define FTS_CMD_ECC_INIT                            0x64
 #define FTS_CMD_ECC_CAL                             0x65
-#define FTS_CMD_ECC_CAL_LEN                         6
+#define FTS_CMD_ECC_CAL_LEN                         7
 #define FTS_RETRIES_ECC_CAL                         10
 #define FTS_RETRIES_DELAY_ECC_CAL                   50
 #define FTS_CMD_ECC_READ                            0x66
 #define FTS_CMD_DATA_LEN                            0xB0
 #define FTS_CMD_APP_DATA_LEN_INCELL                 0x7A
 #define FTS_CMD_DATA_LEN_LEN                        4
+#define FTS_CMD_SET_WFLASH_ADDR                     0xAB
+#define FTS_CMD_SET_RFLASH_ADDR                     0xAC
+#define FTS_LEN_SET_ADDR                            4
 #define FTS_CMD_WRITE                               0xBF
 #define FTS_RETRIES_WRITE                           100
 #define FTS_RETRIES_DELAY_WRITE                     1
@@ -71,7 +75,7 @@
 #define FTS_FLASH_PACKET_LENGTH                     32     /* max=128 */
 #define FTS_MAX_LEN_ECC_CALC                        0xFFFE /* must be even */
 #define FTS_MIN_LEN                                 0x120
-#define FTS_MAX_LEN_FILE                            (128 * 1024)
+#define FTS_MAX_LEN_FILE                            (256 * 1024)
 #define FTS_MAX_LEN_APP                             (64 * 1024)
 #define FTS_MAX_LEN_SECTOR                          (4 * 1024)
 #define FTS_CONIFG_VENDORID_OFF                     0x04
@@ -103,23 +107,27 @@
 #define FTS_APP_INFO_OFFSET                         0x100
 
 enum FW_STATUS {
-	FTS_RUN_IN_ERROR,
-	FTS_RUN_IN_APP,
-	FTS_RUN_IN_ROM,
-	FTS_RUN_IN_PRAM,
-	FTS_RUN_IN_BOOTLOADER,
+    FTS_RUN_IN_ERROR,
+    FTS_RUN_IN_APP,
+    FTS_RUN_IN_ROM,
+    FTS_RUN_IN_PRAM,
+    FTS_RUN_IN_BOOTLOADER,
 };
 
 enum FW_FLASH_MODE {
-	FLASH_MODE_APP,
-	FLASH_MODE_LIC,
-	FLASH_MODE_PARAM,
-	FLASH_MODE_ALL,
+    FLASH_MODE_APP,
+    FLASH_MODE_LIC,
+    FLASH_MODE_PARAM,
+    FLASH_MODE_ALL,
 };
 
 enum ECC_CHECK_MODE {
-	ECC_CHECK_MODE_XOR,
-	ECC_CHECK_MODE_CRC16,
+    ECC_CHECK_MODE_XOR,
+    ECC_CHECK_MODE_CRC16,
+};
+
+enum UPGRADE_SPEC {
+    UPGRADE_SPEC_V_1_0 = 0x0100,
 };
 
 /*****************************************************************************
@@ -127,71 +135,74 @@ enum ECC_CHECK_MODE {
 *****************************************************************************/
 /* IC info */
 struct upgrade_func {
-	u64 ctype[FTX_MAX_COMPATIBLE_TYPE];
-	u32 fwveroff;
-	u32 fwcfgoff;
-	u32 appoff;
-	u32 licoff;
-	u32 paramcfgoff;
-	u32 paramcfgveroff;
-	u32 paramcfg2off;
-	int pram_ecc_check_mode;
-	int fw_ecc_check_mode;
-	bool new_return_value_from_ic;
-	bool appoff_handle_in_ic;
-	bool is_reset_register_BC;
-	bool read_boot_id_need_reset;
-	bool hid_supported;
-	bool pramboot_supported;
-	u8 *pramboot;
-	u32 pb_length;
-	int (*init)(u8 *, u32);
-	int (*upgrade)(u8 *, u32);
-	int (*get_hlic_ver)(u8 *);
-	int (*lic_upgrade)(u8 *, u32);
-	int (*param_upgrade)(u8 *, u32);
-	int (*force_upgrade)(u8 *, u32);
+    u16 ctype[FTS_MAX_COMPATIBLE_TYPE];
+    u32 fwveroff;
+    u32 fwcfgoff;
+    u32 appoff;
+    u32 licoff;
+    u32 paramcfgoff;
+    u32 paramcfgveroff;
+    u32 paramcfg2off;
+    int pram_ecc_check_mode;
+    int fw_ecc_check_mode;
+    int upgspec_version;
+    bool new_return_value_from_ic;
+    bool appoff_handle_in_ic;
+    bool is_reset_register_BC;
+    bool read_boot_id_need_reset;
+    bool hid_supported;
+    bool pramboot_supported;
+    u8 *pramboot;
+    u32 pb_length;
+    int (*init)(u8 *, u32);
+    int (*write_pramboot_private)(void);
+    int (*upgrade)(u8 *, u32);
+    int (*get_hlic_ver)(u8 *);
+    int (*lic_upgrade)(u8 *, u32);
+    int (*param_upgrade)(u8 *, u32);
+    int (*force_upgrade)(u8 *, u32);
 };
 
 struct upgrade_setting_nf {
-	u8 rom_idh;
-	u8 rom_idl;
-	u16 reserved;
-	u32 app2_offset;
-	u32 ecclen_max;
-	u8 eccok_val;
-	u8 upgsts_boot;
-	u8 delay_init;
-	bool spi_pe;
-	bool half_length;
-	bool fd_check;
-	bool drwr_support;
+    u8 rom_idh;
+    u8 rom_idl;
+    u16 reserved;
+    u32 app2_offset;
+    u32 ecclen_max;
+    u8 eccok_val;
+    u8 upgsts_boot;
+    u8 delay_init;
+    bool spi_pe;
+    bool half_length;
+    bool fd_check;
+    bool drwr_support;
 };
 
 struct upgrade_module {
-	int id;
-	char vendor_name[MAX_MODULE_VENDOR_NAME_LEN];
-	u8 *fw_file;
-	u32 fw_len;
+    int id;
+    char vendor_name[MAX_MODULE_VENDOR_NAME_LEN];
+    u8 *fw_file;
+    u32 fw_len;
 };
 
 struct fts_upgrade {
-	struct fts_ts_data *ts_data;
-	struct upgrade_module *module_info;
-	struct upgrade_func *func;
-	struct upgrade_setting_nf *setting_nf;
-	int module_id;
-	bool fw_from_request;
-	u8 *fw;
-	u32 fw_length;
-	u8 *lic;
-	u32 lic_length;
+    struct fts_ts_data *ts_data;
+    struct upgrade_module *module_info;
+    struct upgrade_func *func;
+    struct upgrade_setting_nf *setting_nf;
+    int module_id;
+    bool fw_from_request;
+    u8 *fw;
+    u32 fw_length;
+    u8 *lic;
+    u32 lic_length;
 };
 
 /*****************************************************************************
 * Global variable or extern global variabls/functions
 *****************************************************************************/
-extern struct upgrade_func upgrade_func_ft5452;
+extern struct upgrade_func upgrade_func_ft5452i;
+
 
 /*****************************************************************************
 * Static function prototypes
@@ -201,5 +212,6 @@ int fts_fwupg_enter_into_boot(void);
 int fts_fwupg_erase(u32 delay);
 int fts_fwupg_ecc_cal(u32 saddr, u32 len);
 int fts_flash_write_buf(u32 saddr, u8 *buf, u32 len, u32 delay);
+int fts_flash_read_buf(u32 saddr, u8 *buf, u32 len);
 int fts_fwupg_upgrade(struct fts_upgrade *upg);
 #endif
