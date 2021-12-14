e37abf4f0f310a9a787e10c514bb7cb9df2af01e
huaqin87 <huaqin87@motorola.com>
2021-12-14 10:47:02 +0800

    Rhode 5GP:kernel: qc3p wt6760 isp download function
    charging qc3+ wt6670
    import blank firmware upgrade function
    
    Change-Id: Ib85b26b8189d7e51b3f31e3bc657dd18d1e2d631
    Signed-off-by: huaqin87 <huaqin87@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2142642
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/i2c/busses/i2c-msm-geni.c
+++ drivers/i2c/busses/i2c-msm-geni.c
@@ -78,6 +78,10 @@
 
 #define MAX_SE	20
 
+#if IS_ENABLED(CONFIG_QCOM_I2C_NACK_ERR_WA)
+u8 i2c_wt6670_dl_mode = 0;
+#endif
+
 enum i2c_se_mode {
 	UNINITIALIZED,
 	FIFO_SE_DMA,
@@ -327,6 +331,15 @@ static irqreturn_t geni_i2c_irq(int irq, void *dev)
 	u32 dma = readl_relaxed(gi2c->base + SE_GENI_DMA_MODE_EN);
 	struct i2c_msg *cur = gi2c->cur;
 
+#if IS_ENABLED(CONFIG_QCOM_I2C_NACK_ERR_WA)
+	//wt6670 download mode i2c addr
+	if(cur[0].addr == 0x2B){
+		i2c_wt6670_dl_mode = 1;
+	} else if(cur[0].addr == 0x35){
+		i2c_wt6670_dl_mode = 0;
+	}
+#endif
+
 	if (!cur) {
 		geni_se_dump_dbg_regs(&gi2c->i2c_rsc, gi2c->base, gi2c->ipcl);
 		GENI_SE_ERR(gi2c->ipcl, false, gi2c->dev, "Spurious irq\n");
@@ -337,9 +350,15 @@ static irqreturn_t geni_i2c_irq(int irq, void *dev)
 		    (dm_rx_st & (DM_I2C_CB_ERR)) ||
 		    (m_stat & M_CMD_CANCEL_EN) ||
 		    (m_stat & M_CMD_ABORT_EN)) {
-
+#if IS_ENABLED(CONFIG_QCOM_I2C_NACK_ERR_WA)
+		if(i2c_wt6670_dl_mode == 0){
+			if (m_stat & M_GP_IRQ_1_EN)
+				geni_i2c_err(gi2c, I2C_NACK);
+		}
+#else
 		if (m_stat & M_GP_IRQ_1_EN)
 			geni_i2c_err(gi2c, I2C_NACK);
+#endif
 		if (m_stat & M_GP_IRQ_3_EN)
 			geni_i2c_err(gi2c, I2C_BUS_PROTO);
 		if (m_stat & M_GP_IRQ_4_EN)
@@ -1026,6 +1045,14 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 
 	gi2c->err = 0;
 
+#if IS_ENABLED(CONFIG_QCOM_I2C_NACK_ERR_WA)
+	//wt6670 download mode i2c addr
+	if(msgs[0].addr == 0x2B){
+		i2c_wt6670_dl_mode = 1;
+	} else if( msgs[0].addr == 0x35){
+		i2c_wt6670_dl_mode = 0;
+	}
+#endif
 	/* Client to respect system suspend */
 	if (!pm_runtime_enabled(gi2c->dev)) {
 		GENI_SE_ERR(gi2c->ipcl, false, gi2c->dev,
@@ -1224,6 +1251,15 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 		if (gi2c->err) {
 			GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
 				"i2c error :%d\n", gi2c->err);
+#if IS_ENABLED(CONFIG_QCOM_I2C_NACK_ERR_WA)
+			if(i2c_wt6670_dl_mode == 1){
+				if(gi2c->err == gi2c_log[I2C_NACK].err) {
+					GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,"wt6670 nack continue! :%d\n", gi2c->err);
+					continue;
+				}
+			}
+
+#endif
 			break;
 		}
 	}
