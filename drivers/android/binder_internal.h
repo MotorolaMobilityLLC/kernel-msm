241cc4d790ae91865af70dcdf026d15b0f06ea90
Murilo Alves <alvesm@motorola.com>
2021-12-22 11:16:06 -0300

    Revert "(CR): binder: BINDER_FREEZE ioctl"
    This reverts commit b69bc86c01cef6c6828f5861e3f5e0877505c908.
    Freeze feature should be disabled both on upper and lower layers.
    
    Change-Id: Ibaa36bb72ad6219d2699e21b14c2c0a59d94df88
    Signed-off-by: Murilo Alves <alvesm@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2152151
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/android/binder_internal.h
+++ drivers/android/binder_internal.h
@@ -394,17 +394,8 @@ enum binder_prio_state {
  *                        (protected by binder_deferred_lock)
  * @deferred_work:        bitmap of deferred work to perform
  *                        (protected by binder_deferred_lock)
- * @outstanding_txns:     number of transactions to be transmitted before
- *                        processes in freeze_wait are woken up
- *                        (protected by @inner_lock)
  * @is_dead:              process is dead and awaiting free
  *                        when outstanding transactions are cleaned up
- *                        (protected by @inner_lock)
- * @is_frozen:            process is frozen and unable to service
- *                        binder transactions
- *                        (protected by @inner_lock)
- * @freeze_wait:          waitqueue of processes waiting for all outstanding
- *                        transactions to be processed
  *                        (protected by @inner_lock)
  * @todo:                 list of work for this process
  *                        (protected by @inner_lock)
@@ -446,10 +437,7 @@ struct binder_proc {
 	struct task_struct *tsk;
 	struct hlist_node deferred_work_node;
 	int deferred_work;
-	int outstanding_txns;
 	bool is_dead;
-	bool is_frozen;
-	wait_queue_head_t freeze_wait;
 
 	struct list_head todo;
 	struct binder_stats stats;
