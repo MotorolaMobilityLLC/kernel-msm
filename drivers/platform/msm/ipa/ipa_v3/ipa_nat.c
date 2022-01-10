b19893d43cd9f415f9766f33b3df9f18a0c23879
Himansu Nayak <quic_himansu@quicinc.com>
2022-01-10 23:25:40 +0530

    msm: ipa3: Fix to validate the NAT table entries during NAT table init
    During NAT table Initialization parameter pass from HLOS, if max/zero 
    table entries passed it was leading to out of bound read. Adding checks 
    to validate the table entries before passing to NAT table parameter.
    
    Change-Id: I85ae3272e6c72eefd241c3b26f3bfc02a636fd75
    Signed-off-by: Himansu Nayak <quic_himansu@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_nat.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_nat.c
@@ -1364,8 +1364,9 @@ int ipa3_nat_init_cmd(
 		goto bail;
 	}
 
-	if (init->table_entries == 0) {
-		IPAERR_RL("Table entries is zero\n");
+	if (init->table_entries == 0 ||
+		init->table_entries == U16_MAX) {
+		IPAERR_RL("Table entries is %d\n", init->table_entries);
 		result = -EPERM;
 		goto bail;
 	}
