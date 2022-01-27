eb64a0e475d2c6c80ff9a4b4a6dc54eb32de2fdc
liaohs <liaohs@lenovo.com>
2022-01-27 16:51:48 +0800

    Revert "i2c: i2c-msm-geni: Do i2c transfer if GENI IOS in proper state"
    GENI IOs read register - Allows a mean to SW for reading current GENI IOs
    DATA might not reflect current bus status due to synchronization delay.
    Let's revert it and monitor I2C crash issue some time . 
    
    This reverts commit 5ad2f9de06597b1c2ce204cff653b53fb596c5f4.
    
    Change-Id: Ie4c9788976a0054dc7c945623b4d0562568f331d
    Reviewed-on: https://gerrit.mot.com/2180331
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Reviewed-by: Minghui Wei <weimh1@motorola.com>
    Submit-Approved: Jira Key

--- drivers/i2c/busses/i2c-msm-geni.c
+++ drivers/i2c/busses/i2c-msm-geni.c
@@ -138,7 +138,6 @@ struct geni_i2c_dev {
 	bool first_resume;
 	bool gpi_reset;
 	bool disable_dma_mode;
-	bool prev_cancel_pending; //Halt cancel till IOS in good state
 };
 
 static struct geni_i2c_dev *gi2c_dev_dbg[MAX_SE];
@@ -244,43 +243,6 @@ static void geni_i2c_err(struct geni_i2c_dev *gi2c, int err)
 	geni_se_dump_dbg_regs(&gi2c->i2c_rsc, gi2c->base, gi2c->ipcl);
 err_ret:
 	gi2c->err = gi2c_log[err].err;
-}
-
-static int do_pending_cancel(struct geni_i2c_dev *gi2c)
-{
-	int timeout = 0;
-	u32 geni_ios = 0;
-
-	geni_ios = geni_read_reg_nolog(gi2c->base, SE_GENI_IOS);
-	if ((geni_ios & 0x3) != 0x3) {
-		GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
-			"%s: Can't do pending cancel, IOS bad state: 0x%x\n",
-			__func__, geni_ios);
-		return -EINVAL;
-	}
-
-	if (gi2c->se_mode == GSI_ONLY) {
-		dmaengine_terminate_all(gi2c->tx_c);
-		gi2c->cfg_sent = 0;
-	} else {
-		reinit_completion(&gi2c->xfer);
-		geni_cancel_m_cmd(gi2c->base);
-		timeout = wait_for_completion_timeout(&gi2c->xfer, HZ);
-		if (!timeout) {
-			GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
-				"%s:Pending Cancel failed\n", __func__);
-			reinit_completion(&gi2c->xfer);
-			geni_abort_m_cmd(gi2c->base);
-			timeout = wait_for_completion_timeout(&gi2c->xfer, HZ);
-			if (!timeout)
-				GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
-				"%s:Abort failed\n", __func__);
-		}
-	}
-	gi2c->prev_cancel_pending = false;
-	GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
-			"%s: Pending Cancel done\n", __func__);
-	return timeout;
 }
 
 static int geni_i2c_prepare(struct geni_i2c_dev *gi2c)
@@ -978,8 +940,6 @@ static int geni_i2c_gsi_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[],
 		timeout = wait_for_completion_timeout(&gi2c->xfer,
 						gi2c->xfer_timeout);
 		if (!timeout) {
-			u32 geni_ios = 0;
-
 			GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
 				"I2C gsi xfer timeout:%u flags:%d addr:0x%x\n",
 				gi2c->xfer_timeout, gi2c->cur->flags,
@@ -987,15 +947,6 @@ static int geni_i2c_gsi_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[],
 			geni_se_dump_dbg_regs(&gi2c->i2c_rsc, gi2c->base,
 						gi2c->ipcl);
 			gi2c->err = -ETIMEDOUT;
-
-			/* WAR: Set flag to mark cancel pending if IOS stuck */
-			geni_ios = geni_read_reg_nolog(gi2c->base, SE_GENI_IOS);
-			if ((geni_ios & 0x3) != 0x3) { //SCL:b'1, SDA:b'0
-				GENI_SE_DBG(gi2c->ipcl, true, gi2c->dev,
-					"%s: IO lines not in good state\n", __func__);
-				gi2c->prev_cancel_pending = true;
-				goto geni_i2c_gsi_cancel_pending;
-			}
 		}
 geni_i2c_err_prep_sg:
 		if (gi2c->err) {
@@ -1018,7 +969,6 @@ geni_i2c_err_prep_sg:
 			/* Resend cfg tre for every new message on shared se */
 			gi2c->cfg_sent = 0;
 
-geni_i2c_gsi_cancel_pending:
 		if (msgs[i].flags & I2C_M_RD)
 			geni_se_iommu_unmap_buf(rx_dev, &gi2c->rx_ph,
 				msgs[i].len, DMA_FROM_DEVICE);
@@ -1070,13 +1020,6 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 			pm_runtime_set_suspended(gi2c->dev);
 			return ret;
 		}
-	}
-
-	// WAR : Complete previous pending cancel cmd
-	if (gi2c->prev_cancel_pending) {
-		ret = do_pending_cancel(gi2c);
-		if (ret)
-			return ret; //Don't perform xfer is cancel failed
 	}
 
 	GENI_SE_DBG(gi2c->ipcl, false, gi2c->dev,
@@ -1196,20 +1139,9 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 		timeout = wait_for_completion_timeout(&gi2c->xfer,
 						gi2c->xfer_timeout);
 		if (!timeout) {
-			u32 geni_ios = 0;
-
 			GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
 				"I2C xfer timeout: %d\n", gi2c->xfer_timeout);
 			geni_i2c_err(gi2c, GENI_TIMEOUT);
-
-			/* WAR: Set flag to mark cancel pending if IOS bad */
-			geni_ios = geni_read_reg_nolog(gi2c->base, SE_GENI_IOS);
-			if ((geni_ios & 0x3) != 0x3) { //SCL:b'1, SDA:b'0
-				GENI_SE_DBG(gi2c->ipcl, true, gi2c->dev,
-					"%s: IO lines not in good state\n", __func__);
-				gi2c->prev_cancel_pending = true;
-				goto geni_i2c_txn_ret;
-			}
 		}
 
 		if (gi2c->err) {
