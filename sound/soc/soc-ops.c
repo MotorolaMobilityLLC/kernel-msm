ab8e92749be0e27ac5431d801cff78212d2113a3
Mark Brown <broonie@kernel.org>
2022-01-28 19:24:43 +0000

    UPSTREAM: ASoC: ops: Check for negative values before reading them
    The controls allow inputs to be specified as negative but our manipulating
    them into register fields need to be done on unsigned variables so the
    checks for negative numbers weren't taking effect properly. Do the checks
    for negative values on the variable in the ABI struct rather than on our
    local unsigned copy.
    
    Bug: 451815973
    Bug: 463585154
    (cherry picked from commit 1601033da2dd2052e0489137f7788a46a8fcd82f)
    Change-Id: Ib48f6fab0ef1ed9ba61044c5f7e6c08a17865e11
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20220128192443.3504823-1-broonie@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Xiao-wei Zhang <zhangxiaowei4@oppo.com>
    (cherry picked from commit 925ccff1f9adfde5d993b10120a38614a170b415)

--- sound/soc/soc-ops.c
+++ sound/soc/soc-ops.c
@@ -327,12 +327,12 @@ int snd_soc_put_volsw(struct snd_kcontrol *kcontrol,
 	if (sign_bit)
 		mask = BIT(sign_bit + 1) - 1;
 
+	if (ucontrol->value.integer.value[0] < 0)
+		return -EINVAL;
 	val = ucontrol->value.integer.value[0];
 	if (mc->platform_max && val > mc->platform_max)
 		return -EINVAL;
 	if (val > max - min)
-		return -EINVAL;
-	if (val < 0)
 		return -EINVAL;
 	val = (val + min) & mask;
 	if (invert)
@@ -340,12 +340,12 @@ int snd_soc_put_volsw(struct snd_kcontrol *kcontrol,
 	val_mask = mask << shift;
 	val = val << shift;
 	if (snd_soc_volsw_is_stereo(mc)) {
+		if (ucontrol->value.integer.value[1] < 0)
+			return -EINVAL;
 		val2 = ucontrol->value.integer.value[1];
 		if (mc->platform_max && val2 > mc->platform_max)
 			return -EINVAL;
 		if (val2 > max - min)
-			return -EINVAL;
-		if (val2 < 0)
 			return -EINVAL;
 		val2 = (val2 + min) & mask;
 		if (invert)
@@ -441,12 +441,12 @@ int snd_soc_put_volsw_sx(struct snd_kcontrol *kcontrol,
 	int ret;
 	unsigned int val, val_mask;
 
+	if (ucontrol->value.integer.value[0] < 0)
+		return -EINVAL;
 	val = ucontrol->value.integer.value[0];
 	if (mc->platform_max && val > mc->platform_max)
 		return -EINVAL;
 	if (val > max)
-		return -EINVAL;
-	if (val < 0)
 		return -EINVAL;
 	val_mask = mask << shift;
 	val = (val + min) & mask;
