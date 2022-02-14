5245a85a3c3324f1a2229c5a7971e9471bb1bc68
liuyc20 <liuyc20@lenovo.com>
2022-02-14 15:24:02 +0800

    usb: remove LowPowMode when usb into host mode
    01-25 15:40:18.596     0     0 I hub 3-0 : 1.0: USB hub found
    01-25 15:40:18.597     0     0 I hub 3-0 : 1.0: 1 port detected
    01-25 15:40:18.602     0     0 E msm_dwc3_perf_vote_update: latency updated to: 61
    01-25 15:40:19.834     0     0 E msm-dwc3 4e00000.ssusb: DWC3-msm runtime idle
    01-25 15:40:19.834     0     0 E msm-dwc3 4e00000.ssusb: DWC3-msm runtime suspend
    typc-c headset connect failed sometimes,from above log,
    the dwc3 into suspend mode when usb in host mode
    
    QC case : 05701211
    
    Change-Id: Id6c6f6bd9d49bfc303e329186491427115dc9d72
    Signed-off-by: liuyc20 <liuyc20@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2190504
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4544,6 +4544,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
+#if 0
 static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
@@ -4561,6 +4562,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_enable_usb2_hardware_lpm(udev);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4924,7 +4926,6 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
-	hub_set_initial_usb2_lpm_policy(udev);
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
