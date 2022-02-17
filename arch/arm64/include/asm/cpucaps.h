64eda7ef8c1993195c30d641150d65622833f6f9
James Morse <james.morse@arm.com>
2022-02-17 12:04:29 +0000

    KVM: arm64: Add templates for BHB mitigation sequences
    KVM writes the Spectre-v2 mitigation template at the beGinning of each
    griffin when a CPU requires a specific sequence to run.
    
    Because the template is copied, it can not be modified by the alternatives
    at runtime. As the KVM template code is intertwined with the bp-hardening
    callbacks, all templates must have a bp-hardening callback.
    
    Add templates for calling ARCH_WORKAROUND_3 and one for each value of K
    in the brancy-loop. Identify these sequences by a new parameter
    template_start, and add a copy of install_bp_hardening_cb() that is able to
    install them.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2022-23960
    Bug: 215557547
    
    Change-Id: I139034d513f4cd3dd3b89fb7594f52bd440196da
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Gajjala Chakradhar <gajjalac@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2359724
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit a558137cd00ff991d392bd768a40ef23d1e33e41)

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -56,8 +56,9 @@
 #define ARM64_WORKAROUND_1188873		35
 #define ARM64_WORKAROUND_1542418		36
 #define ARM64_WORKAROUND_1542419		37
+#define ARM64_SPECTRE_BHB			38
 
-/* kabi: reserve 38 - 62 for future cpu capabilities */
+/* kabi: reserve 39 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
 
 #endif /* __ASM_CPUCAPS_H */
