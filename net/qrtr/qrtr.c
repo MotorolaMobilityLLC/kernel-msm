74a6d3da19dd9d58cc649895f87cbe49ee27ef42
Tony Truong <quic_truong@quicinc.com>
2022-03-16 16:11:12 -0700

    net: qrtr: get svc_id before queueing sk_buff
    In qrtr_endpoint_post, getting svc_id based on sk_buff->cb after sk_buff
    has been queued to the endpoint leads to a potential use-after-free
    scenario. To avoid this race condition, get the service ID via
    sk_buff->cb before queueing the sk_buff.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2022-33292
    Bug: 250627197
    CRs-Fixed: 3152855
    
    Change-Id: I53205fdffc08fd6dc48fd158c7fe5966f38aa978
    Signed-off-by: Tony Truong <quic_truong@quicinc.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2601336
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -929,6 +929,7 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 	/* All control packets and non-local destined data packets should be
 	 * queued to the worker for forwarding handling.
 	 */
+	svc_id = qrtr_get_service_id(cb->src_node, cb->src_port);
 	if (cb->type != QRTR_TYPE_DATA || cb->dst_node != qrtr_local_nid) {
 		skb_queue_tail(&node->rx_queue, skb);
 		kthread_queue_work(&node->kworker, &node->read_data);
@@ -942,7 +943,6 @@ int qrtr_endpoint_post(struct qrtr_endpoint *ep, const void *data, size_t len)
 
 
 		if (node->nid == 5) {
-			svc_id = qrtr_get_service_id(cb->src_node, cb->src_port);
 			if (svc_id > 0){
 				for (i = 0; i < node->no_wake_svc.size; i++) {
 					if (svc_id == node->no_wake_svc.arr[i]) {
