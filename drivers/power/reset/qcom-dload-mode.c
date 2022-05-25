db8622945056e4c52af1ff3b5cf6fc90b01d3066
qianlong <qianl2@motorola.com>
2022-05-25 15:17:27 +0800

    Add debug recovery panic
    When set cmdline "qcom-dload-mode.sys_restart_mode=recovery_panic",
    the kernel will enter into ramdump when it recovery reset.
    
    Change-Id: I2b778013bbaff1daa53bb290d6dac2da6c6ff7ac
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2279329
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2344519

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -269,8 +269,14 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 
 	pr_info("%s: sys_restart_mode [%s]\n", __func__, sys_restart_mode);
 	if (!strcmp(sys_restart_mode, "panic")) {
-		/* Trigger a real panic on 8450 */
+		/* Trigger a real panic on debug setting */
 		BUG();
+	}
+	if (!strcmp(sys_restart_mode, "recovery_panic")) {
+		if (!strcmp(cmd, "recovery")) {
+			/* Trigger a real panic on debug setting */
+			BUG();
+		}
 	}
 
 	/* Clean shutdown, disable dump mode to allow normal restart */
