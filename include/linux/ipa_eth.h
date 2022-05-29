19b7e6c0644c5cdc4e3cff7942a38ac1b2da6fa8
Eliad Ben Yishay <quic_ebenyish@quicinc.com>
2022-05-29 10:49:36 +0300

    kernel-msm: ipa_eth: Dynamic IPA NTN3 ETH client moderation configuration
    * Giving flexibility in moderation configuration for clients
    
    Change-Id: Ic072d2682289e1ed0dfda62f67d5402a134a5d53
    Signed-off-by: Eliad Ben Yishay <quic_ebenyish@quicinc.com>

--- include/linux/ipa_eth.h
+++ include/linux/ipa_eth.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _IPA_ETH_H_
@@ -73,10 +75,13 @@ enum ipa_eth_pipe_direction {
  *
  * @bar_addr: bar PA to access NTN register
  * @tail_ptr_offs: tail ptr offset
+ * @ioc_mod_threshold: Descriptors # per interrupt request from
+ * NTN3 HW via descriptor bit as part of the protocol.
  */
 struct ipa_eth_ntn_setup_info {
 	phys_addr_t bar_addr;
 	phys_addr_t tail_ptr_offs;
+	uint16_t ioc_mod_threshold;
 };
 
 /**
@@ -111,6 +116,7 @@ struct ipa_eth_realtek_setup_info {
 	u8 queue_number;
 	phys_addr_t dest_tail_ptr_offs;
 };
+
 
 /**
  * struct ipa_eth_buff_smmu_map -  IPA iova->pa SMMU mapping
