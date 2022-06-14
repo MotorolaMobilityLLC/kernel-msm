7f47e4f7b418aa61779aa13e98dcb0a141558de7
duanxl1 <duanxl1@lenovo.com>
2022-06-14 19:24:52 +0800

    ssr: expand ssr_reason for subsystem_ramdump
    expose ssr_reason for subsystem_ramdump, so that bug2go
    can show the ssr reason
    
    Change-Id: I54b8c3d98ec77f103baaf11b18850ef7ccacb193
    Signed-off-by: duanxl1 <duanxl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2300572
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wen Zeng <zengwen1@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Reviewed-by: Jianqi Yang <yangj@motorola.com>
    Submit-Approved: Jira Key

--- drivers/remoteproc/qcom_q6v5.c
+++ drivers/remoteproc/qcom_q6v5.c
@@ -20,6 +20,10 @@
 
 #define Q6V5_PANIC_DELAY_MS	200
 
+static char qcom_ssr_reason[256];
+static char *ssr_reason = qcom_ssr_reason;
+module_param(ssr_reason, charp, S_IRUGO);
+
 /**
  * qcom_q6v5_prepare() - reinitialize the qcom_q6v5 context before start
  * @q6v5:	reference to qcom_q6v5 context to be reinitialized
@@ -118,6 +122,8 @@ static irqreturn_t q6v5_wdog_interrupt(int irq, void *data)
 		q6v5->rproc->recovery_disabled ?
 		"disabled and lead to device crash" :
 		"enabled and kick reovery process");
+	memset(qcom_ssr_reason, 0, sizeof(qcom_ssr_reason));
+	strlcpy(qcom_ssr_reason, msg, min((size_t)len, (size_t)sizeof(qcom_ssr_reason)));
 
 	if (q6v5->rproc->recovery_disabled) {
 		schedule_work(&q6v5->crash_handler);
@@ -147,6 +153,9 @@ static irqreturn_t q6v5_fatal_interrupt(int irq, void *data)
 		dev_err(q6v5->dev, "fatal error received: %s\n", msg);
 	else
 		dev_err(q6v5->dev, "fatal error without message\n");
+
+	memset(qcom_ssr_reason, 0, sizeof(qcom_ssr_reason));
+	strlcpy(qcom_ssr_reason, msg, min((size_t)len, (size_t)sizeof(qcom_ssr_reason)));
 
 	q6v5->running = false;
 	trace_rproc_qcom_event(dev_name(q6v5->dev), "q6v5_fatal", msg);
