a899018f2c01d2307c9c940d1bda0b95b8c61fbb
Haijian Ma <mahj8@motorola.com>
2022-06-19 17:00:25 +0800

    USB: add adb force enable logic
    For some debug situations, we need to force adb function
    enabled without charger notification. So we add the forcing
    logic. And it is controlled by a dts
    configuration("qcom,force-adb-enable")
    
    Change-Id: I1282190a7478630c0cd77dacb392b68a18ad9157
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2305694
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3081069
    Reviewed-by: Yuecai Liu <liuyc20@motorola.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -6537,6 +6537,13 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 					    PM_QOS_DEFAULT_VALUE);
 
 	mdwc->force_disconnect = false;
+
+	if (of_property_read_bool(node,
+					"qcom,force-adb-enable")) {
+		dev_err(mdwc->dev, "%s: force usb start device mode\n", __func__);
+		dwc3_start_stop_device(mdwc, true);
+	}
+
 	return 0;
 
 put_dwc3:
