5f4c911b197850bf4fc51579636789455cae3d94
Yonghua Yan <yanyh2@motorola.com>
2022-07-18 01:55:31 +0800

    dwc3-msm-core: Enable debug log for usb issue analysis
    Print the pr_debug and dev debug log to Kernel log for usb issue
    analysis in dwc3 controller driver.
    
    Change-Id: I37fd7f3ba5e0514159111e8392c4a6f68a7cc8f3
    Signed-off-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2335892
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jianqi Yang <yangj@motorola.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -51,6 +51,11 @@
 #include "xhci.h"
 #include "debug-ipc.h"
 
+#undef dev_dbg
+#undef pr_debug
+#define dev_dbg dev_info
+#define pr_debug pr_info
+
 #define NUM_LOG_PAGES   12
 
 /* time out to wait for USB cable status notification (in ms)*/
