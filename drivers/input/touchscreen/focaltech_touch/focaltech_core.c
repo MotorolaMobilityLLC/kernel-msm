74694b904cb7a34a6bd9fe89316572a9a60e9df6
hq_yexiuyon.wb_tmp <yexiuyong.wb@huaqin.com>
2022-08-04 18:19:52 +0800

    TP: The TP  ft8057 close build kernel
    Modify:
          fix TP for ft8057 , add TP fix for factory
    
    Change-Id: I34aec889e04b44ab61b1dbb300391d4e02a41605
    Signed-off-by: hq_yexiuyon.wb_tmp <yexiuyong.wb@huaqin.com>
    Reviewed-on: https://gerrit.mot.com/2354323
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Wang Wang <wangwang1@lenovo.com>
    Submit-Approved: Jira Key

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -70,7 +70,7 @@
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_NAME                     "fts_ts"
+#define FTS_DRIVER_NAME                     "fts_tp"
 #define INTERVAL_READ_REG                   200  /* unit:ms */
 #define TIMEOUT_READ_REG                    1000 /* unit:ms */
 #if FTS_POWER_SOURCE_CUST_EN
