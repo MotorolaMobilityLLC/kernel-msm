3f654648cb8a3926df6648fcac98e0574ae9afaa
Haijian Ma <mahj8@motorola.com>
2022-08-25 15:20:47 +0800

    battery: add setting cycle count power supply
    add setting cycle count power supply
    for battery long cycle debug
    
    Change-Id: Ibb1be39503689216afb94061e2edd49e053a58ad
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2370973
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2762579
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1238,6 +1238,22 @@ static int battery_psy_set_charge_current(struct battery_chg_dev *bcdev,
 	return rc;
 }
 
+static int battery_psy_set_cycle_count(struct battery_chg_dev *bcdev,
+					int CycleCount)
+{
+	int rc;
+
+	rc = write_property_id(bcdev, &bcdev->psy_list[PSY_TYPE_BATTERY],
+				BATT_CYCLE_COUNT, CycleCount);
+	if (rc < 0) {
+		pr_err("Failed to set cycle count %d, rc=%d\n", CycleCount, rc);
+	} else {
+		pr_err("Set CycleCount to %d\n", CycleCount);
+	}
+
+	return rc;
+}
+
 static int battery_psy_get_prop(struct power_supply *psy,
 		enum power_supply_property prop,
 		union power_supply_propval *pval)
@@ -1299,6 +1315,7 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_STATUS:
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
+	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
 	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
 		if (bcdev->combo_batt_psy) {
@@ -1324,6 +1341,13 @@ static int battery_psy_set_prop(struct power_supply *psy,
 	switch (prop) {
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
 		return battery_psy_set_charge_current(bcdev, pval->intval);
+	case POWER_SUPPLY_PROP_CYCLE_COUNT:
+		if (bcdev->combo_batt_psy) {
+			return power_supply_set_property(bcdev->combo_batt_psy,
+						prop, pval);
+		} else {
+			return battery_psy_set_cycle_count(bcdev, pval->intval);
+		}
 	default:
 		return -EINVAL;
 	}
@@ -1336,6 +1360,7 @@ static int battery_psy_prop_is_writeable(struct power_supply *psy,
 {
 	switch (prop) {
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
+	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 		return 1;
 	default:
 		break;
