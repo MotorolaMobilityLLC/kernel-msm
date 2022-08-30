45cadc8d5886a96cb69eabf87738cb1f92cdad29
xuxj12 <xuxj12@motorola.com>
2022-08-30 13:33:29 +0800

    Add the judgment of whether the wls_fw_name pointer is NULL.
    It is necessary to determine whether the wls_fw_name pointer is NULL
    to prevent access to illegal addresses.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I85399843887f5d8aab7a2c8728927d1f7e7c2710
    Signed-off-by: xuxj <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2778643
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -2407,7 +2407,7 @@ static int battery_chg_parse_dt(struct battery_chg_dev *bcdev)
 	else
 		bcdev->num_usb_ports = 1;
 
-	if (strstr(bcdev->wls_fw_name, "cps"))
+	if (bcdev->wls_fw_name && strstr(bcdev->wls_fw_name, "cps"))
 		bcdev->wls_fw_vendor = WLS_CPS;
 
 	rc = read_property_id(bcdev, pst, BATT_CHG_CTRL_LIM_MAX);
