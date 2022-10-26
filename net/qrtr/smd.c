625e3bd86868a2fcc1f49344c41cdf44005b3111
YangYi <yangyi31@motorola.com>
2022-10-26 11:27:31 +0800

    Penang: resolve kasan panic
    Penang:
    KASAN: slab-out-of-bounds in qrtr_endpoint_register
    
    Change-Id: I7542a0f2af0ceb0fdd04291b86795ba942ffe8d7
    Signed-off-by: YangYi <yangyi31@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2413704
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuchang Guo <guoyc1@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- net/qrtr/smd.c
+++ net/qrtr/smd.c
@@ -64,8 +64,9 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 	struct qrtr_smd_dev *qdev;
 	u32 net_id;
 	bool rt;
-	int rc, size;
-	u32 *svc_arr = NULL;
+	int rc;
+	size_t size;
+	struct qrtr_array svc_arr = {NULL, 0};
 	pr_info("%s:Entered\n", __func__);
 
 	qdev = devm_kzalloc(&rpdev->dev, sizeof(*qdev), GFP_KERNEL);
@@ -87,13 +88,16 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 	if (size > 0) {
 		if (size > MAX_NON_WAKE_SVC_LEN)
 			size = MAX_NON_WAKE_SVC_LEN;
-		svc_arr = kmalloc_array(size, sizeof(u32), GFP_KERNEL);
+		svc_arr.size = size;
+		svc_arr.arr = kmalloc_array(size, sizeof(u32), GFP_KERNEL);
+		if (!svc_arr.arr)
+			return -ENOMEM;
 
 		of_property_read_u32_array(rpdev->dev.of_node, "qcom,non-wake-svc",
-					   svc_arr, size);
+				svc_arr.arr, size);
 	}
-	rc = qrtr_endpoint_register(&qdev->ep, net_id, rt, svc_arr);
-	kfree(svc_arr);
+	rc = qrtr_endpoint_register(&qdev->ep, net_id, rt, &svc_arr);
+	kfree(svc_arr.arr);
 
 	if (rc)
 		return rc;
