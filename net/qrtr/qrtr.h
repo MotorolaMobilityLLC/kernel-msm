03eaf87cfb7e8619aa2e76d83b4ffb7e1676bf48
YangYi <yangyi31@motorola.com>
2022-10-26 11:27:31 +0800

    Penang: resolve kasan panic
    Penang:
    KASAN: slab-out-of-bounds in qrtr_endpoint_register
    
    Change-Id: I7542a0f2af0ceb0fdd04291b86795ba942ffe8d7
    Signed-off-by: YangYi <yangyi31@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2413704
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuchang Guo <guoyc1@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- net/qrtr/qrtr.h
+++ net/qrtr/qrtr.h
@@ -28,8 +28,20 @@ struct qrtr_endpoint {
 	struct qrtr_node *node;
 };
 
+/**
+ * struct qrtr_array - array with size
+ * @arr: elements in the array
+ * @size: number of elements
+ *
+ * An array with its size provided.
+ */
+struct qrtr_array {
+	u32 *arr;
+	size_t size;
+};
+
 int qrtr_endpoint_register(struct qrtr_endpoint *ep, unsigned int net_id,
-			   bool rt, u32 *svc_arr);
+			   bool rt, struct qrtr_array *svc_arr);
 
 void qrtr_endpoint_unregister(struct qrtr_endpoint *ep);
 
