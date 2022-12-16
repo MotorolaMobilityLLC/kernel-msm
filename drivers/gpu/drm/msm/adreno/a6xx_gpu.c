452baad1d3da197285b08fe4e85b130f6d295be9
Akhil P Oommen <quic_akhilpo@quicinc.com>
2022-12-16 22:33:14 +0530

    drm/msm/a6xx: Avoid gx gbit halt during rpm suspend
    [ Upstream commit f4a75b5933c998e60fd812a7680e0971eb1c7cee ]
    
    As per the downstream driver, gx gbif halt is required only during
    recovery sequence. So lets avoid it during regular rpm suspend.
    
    Signed-off-by: Akhil P Oommen <quic_akhilpo@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/515279/
    Link: https://lore.kernel.org/r/20221216223253.1.Ice9c47bfeb1fddb8dc377a3491a043a3ee7fca7d@changeid
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Stable-dep-of: f561db72a663 ("drm/msm/a6xx: Fix stale rpmh votes from GPU")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gpu.c
@@ -1178,6 +1178,12 @@ static void a6xx_recover(struct msm_gpu *gpu)
 	if (hang_debug)
 		a6xx_dump(gpu);
 
+	/*
+	 * To handle recovery specific sequences during the rpm suspend we are
+	 * about to trigger
+	 */
+	a6xx_gpu->hung = true;
+
 	/* Halt SQE first */
 	gpu_write(gpu, REG_A6XX_CP_SQE_CNTL, 3);
 
@@ -1191,6 +1197,7 @@ static void a6xx_recover(struct msm_gpu *gpu)
 	gpu->funcs->pm_resume(gpu);
 
 	msm_gpu_hw_init(gpu);
+	a6xx_gpu->hung = false;
 }
 
 static const char *a6xx_uche_fault_block(struct msm_gpu *gpu, u32 mid)
