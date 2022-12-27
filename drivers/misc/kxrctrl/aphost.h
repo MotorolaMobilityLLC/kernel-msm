5b284b6c49bf364757cb5d9ea7efa91a3fc79af7
Wei Tan <quic_weitan@quicinc.com>
2022-12-27 16:07:19 +0800

    misc: Add Nordic driver sleep mode support
    Add HMD nordic driver suspend and sleep
    mode when standlone or bonded with
    remote controllers.
    
    Change-Id: I0dda7e5d474341f8636b78a07ff70f5a164dd082
    Signed-off-by: Wei Tan <quic_weitan@quicinc.com>

--- drivers/misc/kxrctrl/aphost.h
+++ drivers/misc/kxrctrl/aphost.h
@@ -109,6 +109,7 @@ typedef enum _requestType_t
 	getRightJoyStickProductNameRequest,
 	getLeftJoyStickFwVersionRequest,
 	getRightJoyStickFwVersionRequest,
+	setControllerSleepMode = 12,
 	invalidRequest,
 } requestType_t;
 
