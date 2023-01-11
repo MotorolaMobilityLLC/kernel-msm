BayLibre/android_kernel_common

ANDROID: vendor_hooks:vendor hook for control memory dirty rate
When the IO pressure increases or the system performs dirty
page balancing, the frame rate of the foreground application
may become unstable. Therefore, a hook point is added to limit
the buffer IO rate from the source.

Bug: 262189942
Change-Id: I5214d611a388c5e8d87dc44ffde86ead1834ddff
Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

include/trace/hooks/mm.h
@@ -159,6 +159,9 @@ DECLARE_HOOK(android_vh_kmalloc_slab,
 DECLARE_HOOK(android_vh_madvise_cold_or_pageout,
 	TP_PROTO(struct vm_area_struct *vma, bool *allow_shared),
 	TP_ARGS(vma, allow_shared));
+DECLARE_RESTRICTED_HOOK(android_rvh_mi_io_limit,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused), 1);
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */

MotorolaMobilityLLC/kernel-msm

include/trace/hooks/mm.h
@@ -159,6 +159,9 @@ DECLARE_HOOK(android_vh_kmalloc_slab,
 DECLARE_HOOK(android_vh_madvise_cold_or_pageout,
 	TP_PROTO(struct vm_area_struct *vma, bool *allow_shared),
 	TP_ARGS(vma, allow_shared));
+DECLARE_RESTRICTED_HOOK(android_rvh_ctl_dirty_rate,
+	TP_PROTO(void *unused),
+	TP_ARGS(unused), 1);
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */