8cd6c9670d4ff60a5516568df43177f651d9388b
Naini Singh <quic_nainsing@quicinc.com>
2023-02-08 11:25:05 +0530

    drivers: remoteproc: Add support for adsp/cdsp/mpss remoteproc for Crow
    Add support to handle adsp/cdsp/mpss lifecycle in the
    remoteproc's q6v5_pas driver for Crow SoC.
    
    Change-Id: I49ca3028239475e1eec24cfa6be7b1048f6206e3
    Signed-off-by: Naini Singh <quic_nainsing@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1273,6 +1273,20 @@ static const struct adsp_data kalama_adsp_resource = {
 	.ssctl_id = 0x14,
 };
 
+static const struct adsp_data crow_adsp_resource = {
+	.crash_reason_smem = 423,
+	.firmware_name = "adsp.mdt",
+	.pas_id = 1,
+	.minidump_id = 5,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "lpass",
+	.sysmon_name = "adsp",
+	.qmp_name = "adsp",
+	.ssctl_id = 0x14,
+};
+
 static const struct adsp_data khaje_adsp_resource = {
 	.crash_reason_smem = 423,
 	.firmware_name = "adsp.mdt",
@@ -1391,6 +1405,20 @@ static const struct adsp_data kalama_cdsp_resource = {
 	.ssctl_id = 0x17,
 };
 
+static const struct adsp_data crow_cdsp_resource = {
+	.crash_reason_smem = 601,
+	.firmware_name = "cdsp.mdt",
+	.pas_id = 18,
+	.minidump_id = 7,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "cdsp",
+	.sysmon_name = "cdsp",
+	.qmp_name = "cdsp",
+	.ssctl_id = 0x17,
+};
+
 static const struct adsp_data khaje_cdsp_resource = {
 	.crash_reason_smem = 601,
 	.firmware_name = "cdsp.mdt",
@@ -1455,6 +1483,21 @@ static const struct adsp_data kalama_mpss_resource = {
 	.qmp_name = "modem",
 	.ssctl_id = 0x12,
 	.dma_phys_below_32b = true,
+};
+
+static const struct adsp_data crow_mpss_resource = {
+	.crash_reason_smem = 421,
+	.firmware_name = "modem.mdt",
+	.pas_id = 4,
+	.free_after_auth_reset = true,
+	.minidump_id = 3,
+	.uses_elf64 = true,
+	.has_aggre2_clk = false,
+	.auto_boot = false,
+	.ssr_name = "mpss",
+	.sysmon_name = "modem",
+	.qmp_name = "modem",
+	.ssctl_id = 0x12,
 };
 
 static const struct adsp_data cinder_mpss_resource = {
@@ -1861,6 +1904,9 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,kona-adsp-pas", .data = &kona_adsp_resource},
 	{ .compatible = "qcom,kona-cdsp-pas", .data = &kona_cdsp_resource},
 	{ .compatible = "qcom,kona-slpi-pas", .data = &kona_slpi_resource},
+	{ .compatible = "qcom,crow-adsp-pas", .data = &crow_adsp_resource},
+	{ .compatible = "qcom,crow-cdsp-pas", .data = &crow_cdsp_resource},
+	{ .compatible = "qcom,crow-modem-pas", .data = &crow_mpss_resource},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adsp_of_match);
