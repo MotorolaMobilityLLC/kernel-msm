727625472f2f1a0e92f870ae9fbe71d9502861b6
Kamal Agrawal <quic_kamaagra@quicinc.com>
2023-02-13 16:51:37 +0530

    msm: kgsl: Make sure that pool pages don't have any extra references
    Before putting a page back in the pool be sure that it doesn't have
    any additional references that would be a signal that somebody else
    is looking at the page and that it would be a bad idea to keep it
    around and run the risk of accidentally handing it to a different
    process.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2023-21666
    Bug: 271879644
    CRs-Fixed: 3400780
    
    Change-Id: Ic0dedbad0cf2ffb34b76ad23e393c5a911114b82
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2578144
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <asm/cacheflush.h>
@@ -61,6 +62,15 @@ _kgsl_get_pool_from_order(unsigned int order)
 static void
 _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 {
+	/*
+	 * Sanity check to make sure we don't re-pool a page that
+	 * somebody else has a reference to.
+	 */
+	if (WARN_ON_ONCE(unlikely(page_count(p) > 1))) {
+		__free_pages(p, pool->pool_order);
+		return;
+	}
+
 	kgsl_zero_page(p, pool->pool_order);
 
 	spin_lock(&pool->list_lock);
