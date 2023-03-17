44a94ece47267e0a8ad9bd6bbf557f712c77e6d9
Paul Lawrence <paullawrence@google.com>
2023-03-17 12:09:16 -0700

    ANDROID: fuse: Support errors from fuse daemon in canonical path
    Previously errors from the daemon in FUSE_CANONICAL_PATH were simply
    ignored. In order to block inotifys, it is useful to be able to return
    errors from this opcode.
    
    Bug: 238619640
    Test: inotify no longer works on /storage/emulated/0/Android/media but
          does on child folders
    Change-Id: I1c65814f4ad0ccef330bca9764c2db15c71bf2be
    Signed-off-by: Paul Lawrence <paullawrence@google.com>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -102,10 +102,13 @@ static inline int fsnotify_file(struct file *file, __u32 mask)
 	if (mask & FS_OPEN) {
 		if (path->dentry->d_op &&
 		    path->dentry->d_op->d_canonical_path) {
-			struct path lower_path;
+			struct path lower_path = {0};
 			int ret;
 
 			path->dentry->d_op->d_canonical_path(path, &lower_path);
+			if (IS_ERR(lower_path.dentry))
+				return PTR_ERR(lower_path.dentry);
+
 			ret = fsnotify_parent(lower_path.dentry, mask,
 					      &lower_path, FSNOTIFY_EVENT_PATH);
 			path_put(&lower_path);
