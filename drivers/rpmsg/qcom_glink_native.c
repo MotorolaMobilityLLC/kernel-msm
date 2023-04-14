1ed0601c96f854154c4cdd937de6e2595a690db8
Chris Lew <quic_clew@quicinc.com>
2023-04-14 12:52:42 -0700

    rpmsg: glink: Tie rpdev lifetime to remote open/close
    Currently the rpdev lifetime has a mix of references depending on
    whether an endpoint creation is initated from the remote or local side.
    This leads to a confusing refcount for when the rpdev needs to be
    released.
    
    Additionally, there are some users who register/unregister an rpmsg
    driver to simulate a glink open and close. With the mixed reference
    count, the rpdev gets decoupled from the channel in destroy ept and the
    device is never unregistered until the transport is restarted.
    
    The rpdev lifetime should be tied to the remote's open and close
    commands. The rx_open_ack that is sent to the remote can be moved to the
    rx_open command handling instead of in the endpoint creation.
    
    This allows the device to be created independently from the local
    drivers state. It will remain if the local drivers are unregistered, and
    can be reprobed when they are re-registered.
    
    Change-Id: If37ba3a8b76db10fd729c0515718c0b747b1790b
    Signed-off-by: Chris Lew <quic_clew@quicinc.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1611,8 +1611,6 @@ static struct glink_channel *qcom_glink_create_local(struct qcom_glink *glink,
 	if (!ret)
 		goto err_timeout;
 
-	qcom_glink_send_open_ack(glink, channel);
-
 	return channel;
 
 err_timeout:
@@ -1640,8 +1638,6 @@ static int qcom_glink_create_remote(struct qcom_glink *glink,
 	int ret;
 
 	CH_INFO(channel, "\n");
-
-	qcom_glink_send_open_ack(glink, channel);
 
 	ret = qcom_glink_send_open_req(glink, channel);
 	if (ret)
@@ -2096,12 +2092,12 @@ static int qcom_glink_rx_open(struct qcom_glink *glink, unsigned int rcid,
 	struct rpmsg_device *rpdev;
 	bool create_device = false;
 	struct device_node *node;
-	int lcid;
+	int cid;
 	int ret;
 	unsigned long flags;
 
 	spin_lock_irqsave(&glink->idr_lock, flags);
-	idr_for_each_entry(&glink->lcids, channel, lcid) {
+	idr_for_each_entry(&glink->rcids, channel, cid) {
 		if (!strcmp(channel->name, name))
 			break;
 	}
@@ -2127,6 +2123,7 @@ static int qcom_glink_rx_open(struct qcom_glink *glink, unsigned int rcid,
 	spin_unlock_irqrestore(&glink->idr_lock, flags);
 
 	complete_all(&channel->open_req);
+	qcom_glink_send_open_ack(glink, channel);
 
 	if (create_device) {
 		rpdev = kzalloc(sizeof(*rpdev), GFP_KERNEL);
@@ -2218,7 +2215,6 @@ static void qcom_glink_rx_close(struct qcom_glink *glink, unsigned int rcid)
 
 static void qcom_glink_rx_close_ack(struct qcom_glink *glink, unsigned int lcid)
 {
-	struct rpmsg_channel_info chinfo;
 	struct glink_channel *channel;
 	unsigned long flags;
 
@@ -2234,15 +2230,9 @@ static void qcom_glink_rx_close_ack(struct qcom_glink *glink, unsigned int lcid)
 	channel->lcid = 0;
 	spin_unlock_irqrestore(&glink->idr_lock, flags);
 
-	/* Decouple the potential rpdev from the channel */
-	if (channel->rpdev) {
-		strscpy(chinfo.name, channel->name, sizeof(chinfo.name));
-		chinfo.src = RPMSG_ADDR_ANY;
-		chinfo.dst = RPMSG_ADDR_ANY;
-
-		rpmsg_unregister_device(glink->dev, &chinfo);
-	}
-	channel->rpdev = NULL;
+	/* Reinit any variables that are important to endpoint creation */
+	reinit_completion(&channel->open_ack);
+	channel->channel_ready = false;
 
 	kref_put(&channel->refcount, qcom_glink_channel_release);
 }
