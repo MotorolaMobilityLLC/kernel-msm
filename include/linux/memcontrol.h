63d2ea804004465effc8ee2754fda9cb1694a522
Yosry Ahmed <yosryahmed@google.com>
2023-04-21 17:40:19 +0000

    UPSTREAM: memcg: remove mem_cgroup_flush_stats_atomic()
    Previous patches removed all callers of mem_cgroup_flush_stats_atomic().
    Remove the function and simplify the code.
    
    Link: https://lkml.kernel.org/r/20230421174020.2994750-5-yosryahmed@google.com
    Change-Id: Ieef476cf2c0aacb48af4e82e4bb96e5cc69292c5
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Acked-by: Shakeel Butt <shakeelb@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 35822fdae3bf509532b0954088070f17de51ff15)
    Bug: 322544714
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1050,7 +1050,6 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 void mem_cgroup_flush_stats(void);
-void mem_cgroup_flush_stats_atomic(void);
 void mem_cgroup_flush_stats_ratelimited(void);
 
 void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
@@ -1490,10 +1489,6 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 static inline void mem_cgroup_flush_stats(void)
-{
-}
-
-static inline void mem_cgroup_flush_stats_atomic(void)
 {
 }
 
