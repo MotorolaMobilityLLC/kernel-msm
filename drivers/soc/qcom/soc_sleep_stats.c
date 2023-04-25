a4a732169cd23a35429d767e61f267123fe7d5bc
Tushar Nimkar <quic_tnimkar@quicinc.com>
2023-04-25 11:51:40 +0530

    soc: qcom: soc_sleep_stats: Rearrange reg assignment
    This change assign reg irrespective of CONFIG_DEBUGFS
    enabled to allow using same from exported APIs.
    
    Change-Id: I6036ca603cf4823acac337b6686fc72fb4bea07b
    Signed-off-by: Tushar Nimkar <quic_tnimkar@quicinc.com>

--- drivers/soc/qcom/soc_sleep_stats.c
+++ drivers/soc/qcom/soc_sleep_stats.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2011-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -123,35 +123,26 @@ static u64 deep_sleep_last_exited_time;
 uint64_t get_aosd_sleep_exit_time(void)
 {
 	int i;
-	uint32_t offset;
 	u64 last_exited_at;
 	u32 count;
 	static u32 saved_deep_sleep_count;
 	u32 s_type = 0;
 	char stat_type[5] = {0};
 	struct stats_prv_data *drv = gdata;
-	void __iomem *reg;
 
 	for (i = 0; i < drv->config->num_records; i++) {
-		offset = STAT_TYPE_ADDR + (i * sizeof(struct sleep_stats));
-
-		if (drv[i].config->appended_stats_avail)
-			offset += i * sizeof(struct appended_stats);
-
-		reg = drv[i].reg + offset;
-
-		s_type = readl_relaxed(reg);
+		s_type = readl_relaxed(drv[i].reg);
 		memcpy(stat_type, &s_type, sizeof(u32));
 		strim(stat_type);
 
 		if (!memcmp((const void *)stat_type, (const void *)"aosd", 4)) {
-			count = readl_relaxed(reg + COUNT_ADDR);
+			count = readl_relaxed(drv[i].reg + COUNT_ADDR);
 
 			if (saved_deep_sleep_count == count)
 				deep_sleep_last_exited_time = 0;
 			else {
 				saved_deep_sleep_count = count;
-				last_exited_at = readq_relaxed(reg + LAST_EXITED_AT_ADDR);
+				last_exited_at = readq_relaxed(drv[i].reg + LAST_EXITED_AT_ADDR);
 				deep_sleep_last_exited_time = last_exited_at;
 			}
 			break;
@@ -481,7 +472,7 @@ static struct dentry *create_debugfs_entries(void __iomem *reg,
 {
 	struct dentry *root;
 	char stat_type[sizeof(u32) + 1] = {0};
-	u32 offset, type, key;
+	u32 type, key;
 	int i;
 #if IS_ENABLED(CONFIG_QCOM_SMEM)
 	const char *name;
@@ -491,13 +482,6 @@ static struct dentry *create_debugfs_entries(void __iomem *reg,
 	root = debugfs_create_dir("qcom_sleep_stats", NULL);
 
 	for (i = 0; i < prv_data[0].config->num_records; i++) {
-		offset = STAT_TYPE_ADDR + (i * sizeof(struct sleep_stats));
-
-		if (prv_data[0].config->appended_stats_avail)
-			offset += i * sizeof(struct appended_stats);
-
-		prv_data[i].reg = reg + offset;
-
 		type = readl_relaxed(prv_data[i].reg);
 		memcpy(stat_type, &type, sizeof(u32));
 		strim(stat_type);
@@ -555,6 +539,7 @@ static int soc_sleep_stats_probe(struct platform_device *pdev)
 	u32 name;
 	void __iomem *reg;
 #endif
+	u32 offset;
 
 	config = device_get_match_data(&pdev->dev);
 	if (!config)
@@ -581,8 +566,15 @@ static int soc_sleep_stats_probe(struct platform_device *pdev)
 	if (!prv_data)
 		return -ENOMEM;
 
-	for (i = 0; i < config->num_records; i++)
+	for (i = 0; i < config->num_records; i++) {
 		prv_data[i].config = config;
+		offset = STAT_TYPE_ADDR + (i * sizeof(struct sleep_stats));
+
+		if (prv_data[0].config->appended_stats_avail)
+			offset += i * sizeof(struct appended_stats);
+
+		prv_data[i].reg = reg_base + offset;
+	}
 
 	if (!config->ddr_offset_addr)
 		goto skip_ddr_stats;
