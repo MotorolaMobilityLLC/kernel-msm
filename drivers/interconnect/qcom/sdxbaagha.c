c45824ee4ba49b3df2f27880481675eb5a60fbea
Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>
2023-05-04 19:53:39 +0530

    interconnect: qcom: sdxbaagha: Update qos flags for gem_noc masters
    Updated qos mode to bypass for qnm_pcie and qnm_snoc_sf masters.
    
    Change-Id: Ieff89352504618d8f881b64308b3d67e8e4d10d5
    Signed-off-by: Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>

--- drivers/interconnect/qcom/sdxbaagha.c
+++ drivers/interconnect/qcom/sdxbaagha.c
@@ -424,8 +424,8 @@ static struct qcom_icc_qosbox qnm_pcie_qos = {
 	.offsets = { 0x2c000 },
 	.config = &(struct qos_config) {
 		.prio = 0,
-		.urg_fwd = 0,
-		.prio_fwd_disable = 1,
+		.urg_fwd = 1,
+		.prio_fwd_disable = 0,
 	},
 };
 
@@ -446,8 +446,8 @@ static struct qcom_icc_qosbox qnm_snoc_sf_qos = {
 	.offsets = { 0x2b000 },
 	.config = &(struct qos_config) {
 		.prio = 0,
-		.urg_fwd = 0,
-		.prio_fwd_disable = 1,
+		.urg_fwd = 1,
+		.prio_fwd_disable = 0,
 	},
 };
 
