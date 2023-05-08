c36c3f711b9cf0f5373d4aaed8225fe76bf78bb0
Arun Prakash <quic_app@quicinc.com>
2023-05-08 15:59:35 +0530

    rpmsg: glink: Fix possible deadlock scenario
    Glink rx packet processing will happen from both irq and thread context
    by calling qcom_glink_native_rx() API. So qcom_glink_native_rx() can
    be called from irq context after thread acquires the same spinlock
    which leads to deadlock issue.
    
    Use spin_lock_irqsave() & spin_unlock_irqrestore() to avoid the
    possible deadlock.
    
    Change-Id: Ibb47e79e63dfe22a3924fe6988075c351eb0ef57
    Signed-off-by: Arun Prakash <quic_app@quicinc.com>
    Signed-off-by: Meenu Raja Sundaram <quic_mrajasun@quicinc.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2016-2017, Linaro Ltd
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/idr.h>
@@ -1030,6 +1031,7 @@ static void qcom_glink_handle_intent_req(struct qcom_glink *glink,
 static int qcom_glink_rx_defer(struct qcom_glink *glink, size_t extra)
 {
 	struct glink_defer_cmd *dcmd;
+	unsigned long flags;
 
 	extra = ALIGN(extra, 8);
 
@@ -1046,9 +1048,9 @@ static int qcom_glink_rx_defer(struct qcom_glink *glink, size_t extra)
 
 	qcom_glink_rx_peak(glink, &dcmd->msg, 0, sizeof(dcmd->msg) + extra);
 
-	spin_lock(&glink->rx_lock);
+	spin_lock_irqsave(&glink->rx_lock, flags);
 	list_add_tail(&dcmd->node, &glink->rx_queue);
-	spin_unlock(&glink->rx_lock);
+	spin_unlock_irqrestore(&glink->rx_lock, flags);
 
 	schedule_work(&glink->rx_work);
 	qcom_glink_rx_advance(glink, sizeof(dcmd->msg) + extra);
@@ -1162,7 +1164,7 @@ static int qcom_glink_rx_data(struct qcom_glink *glink, size_t avail)
 
 	/* Handle message when no fragments remain to be received */
 	if (!left_size) {
-		spin_lock(&channel->recv_lock);
+		spin_lock_irqsave(&channel->recv_lock, flags);
 		if (channel->ept.cb) {
 			ret = channel->ept.cb(channel->ept.rpdev,
 					intent->data,
@@ -1180,7 +1182,7 @@ static int qcom_glink_rx_data(struct qcom_glink *glink, size_t avail)
 		} else {
 			CH_ERR(channel, "callback not present\n");
 		}
-		spin_unlock(&channel->recv_lock);
+		spin_unlock_irqrestore(&channel->recv_lock, flags);
 
 		if (qcom_glink_is_wakeup(true)) {
 			pr_info("%s[%d:%d] %s: wakeup packet size:%d\n",
@@ -1271,7 +1273,7 @@ static int qcom_glink_rx_data_zero_copy(struct qcom_glink *glink, size_t avail)
 
 	intent->data = data;
 	intent->offset = len;
-	spin_lock(&channel->recv_lock);
+	spin_lock_irqsave(&channel->recv_lock, flags);
 	if (channel->ept.cb) {
 		ret = channel->ept.cb(channel->ept.rpdev, intent->data,
 				intent->offset,
@@ -1286,7 +1288,7 @@ static int qcom_glink_rx_data_zero_copy(struct qcom_glink *glink, size_t avail)
 	} else {
 		CH_ERR(channel, "callback not present\n");
 	}
-	spin_unlock(&channel->recv_lock);
+	spin_unlock_irqrestore(&channel->recv_lock, flags);
 
 	if (qcom_glink_is_wakeup(true)) {
 		pr_info("%s[%d:%d] %s: wakeup packet size:%d\n", channel->name,
@@ -1379,10 +1381,11 @@ static void qcom_glink_handle_intent(struct qcom_glink *glink,
 static int qcom_glink_rx_open_ack(struct qcom_glink *glink, unsigned int lcid)
 {
 	struct glink_channel *channel;
+	unsigned long flags;
 
-	spin_lock(&glink->idr_lock);
+	spin_lock_irqsave(&glink->idr_lock, flags);
 	channel = idr_find(&glink->lcids, lcid);
-	spin_unlock(&glink->idr_lock);
+	spin_unlock_irqrestore(&glink->idr_lock, flags);
 	if (!channel) {
 		dev_err(glink->dev, "Invalid open ack packet\n");
 		return -EINVAL;
