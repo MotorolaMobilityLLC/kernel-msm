d8776c131c7d12cd6c0abadc7a4a54168aed8a97
xiewen3 <xiewen3@motorola.com>
2023-05-16 22:50:54 +0800

    LUR: Revert "BACKPORT: mm: multi-gen LRU: groundwork"
    The LRU function is not compatible with GKI,
    causing GKI to fail to start, so revert.
    
    This reverts commit 82f8143939a9ab52528733433ebfb8f22df2224d.
    
    Change-Id: I7df728d1ad77b59070a61fb90575fdf348532e2a
    Reviewed-on: https://gerrit.mot.com/2609763
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2509,8 +2509,7 @@ static void __split_huge_page_tail(struct page *head, int tail,
 			 (1L << PG_workingset) |
 			 (1L << PG_locked) |
 			 (1L << PG_unevictable) |
-			 (1L << PG_dirty) |
-			 LRU_GEN_MASK | LRU_REFS_MASK));
+			 (1L << PG_dirty)));
 
 	/* ->mapping in first tail page is compound_mapcount */
 	VM_BUG_ON_PAGE(tail > 2 && page_tail->mapping != TAIL_MAPPING,
