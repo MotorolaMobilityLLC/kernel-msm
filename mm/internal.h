a2a799eac18dab9c6357abbad182475004a686a6
xiewen3 <xiewen3@motorola.com>
2023-05-16 22:47:13 +0800

    LUR: Revert "BACKPORT: mm: multi-gen LRU: exploit locality in rmap"
    The LRU function is not compatible with GKI, 
    causing GKI to fail to start, so revert.
    
    This reverts commit 574d1e5ff72493a5b3b7b1a47251418e81aa1369.
    
    Change-Id: I909f487156f7da759b5531bec06a74b729e3b392
    Reviewed-on: https://gerrit.mot.com/2609761
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- mm/internal.h
+++ mm/internal.h
@@ -35,7 +35,6 @@
 void page_writeback_init(void);
 
 vm_fault_t do_swap_page(struct vm_fault *vmf);
-void activate_page(struct page *page);
 
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
 extern struct vm_area_struct *get_vma(struct mm_struct *mm,
