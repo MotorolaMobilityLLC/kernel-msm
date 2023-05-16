bbbe5112397100eac056bab9e592b94edb8a61d3
xiewen3 <xiewen3@motorola.com>
2023-05-16 23:00:13 +0800

    LUR: Revert "BACKPORT: mm: use self-explanatory macros rather than "2""
    The LRU function is not compatible with GKI, 
    causing GKI to fail to start, so revert.
    
    This reverts commit 4e6324f47996243c54f49591465a0743d586127d.
    
    Change-Id: Iae8f79f63d50424acce595fedcd6ccc01df02d21
    Reviewed-on: https://gerrit.mot.com/2609773
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -293,8 +293,6 @@ static inline int is_active_lru(enum lru_list lru)
 	return (lru == LRU_ACTIVE_ANON || lru == LRU_ACTIVE_FILE);
 }
 
-#define ANON_AND_FILE 2
-
 struct zone_reclaim_stat {
 	/*
 	 * The pageout code in vmscan.c keeps track of how many of the
@@ -304,8 +302,8 @@ struct zone_reclaim_stat {
 	 *
 	 * The anon LRU stats live in [0], file LRU stats in [1]
 	 */
-	unsigned long		recent_rotated[ANON_AND_FILE];
-	unsigned long		recent_scanned[ANON_AND_FILE];
+	unsigned long		recent_rotated[2];
+	unsigned long		recent_scanned[2];
 };
 
 struct lruvec {
@@ -426,8 +424,6 @@ enum zone_type {
 };
 
 #ifndef __GENERATING_BOUNDS_H
-
-#define ASYNC_AND_SYNC 2
 
 struct zone {
 	/* Read-mostly fields */
@@ -552,8 +548,8 @@ struct zone {
 #if defined CONFIG_COMPACTION || defined CONFIG_CMA
 	/* pfn where compaction free scanner should start */
 	unsigned long		compact_cached_free_pfn;
-	/* pfn where compaction migration scanner should start */
-	unsigned long		compact_cached_migrate_pfn[ASYNC_AND_SYNC];
+	/* pfn where async and sync compaction migration scanner should start */
+	unsigned long		compact_cached_migrate_pfn[2];
 	unsigned long		compact_init_migrate_pfn;
 	unsigned long		compact_init_free_pfn;
 #endif
