f970c63c00ae8d51b98aacac1c3ed964becf277e
xiewen3 <xiewen3@motorola.com>
2023-05-16 22:53:44 +0800

    LUR: Revert "BACKPORT: mm: x86, arm64: add arch_has_hw_pte_young()"
    The LRU function is not compatible with GKI, 
    causing GKI to fail to start, so revert.
    
    This reverts commit 5adf37f31d207aac909d816b51be3d92a9d2c8ca.
    
    Change-Id: I92db4936ceae197ccab03de584ccc9637d4d065d
    Reviewed-on: https://gerrit.mot.com/2609765
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -894,7 +894,13 @@ static inline void update_mmu_cache(struct vm_area_struct *vma,
  * page after fork() + CoW for pfn mappings. We don't always have a
  * hardware-managed access flag on arm64.
  */
-#define arch_has_hw_pte_young		cpu_has_hw_af
+static inline bool arch_faults_on_old_pte(void)
+{
+	WARN_ON(preemptible());
+
+	return !cpu_has_hw_af();
+}
+#define arch_faults_on_old_pte arch_faults_on_old_pte
 
 #endif /* !__ASSEMBLY__ */
 
