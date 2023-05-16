b9be4bdd4dc1a7e1ccadd134768d03b6b17f4358
xiewen3 <xiewen3@motorola.com>
2023-05-16 22:48:58 +0800

    LUR: Revert "BACKPORT: mm: multi-gen LRU: minimal implementation"
    The LRU function is not compatible with GKI,
    causing GKI to fail to start, so revert.
    
    This reverts commit bdab47a49135e8ded0e5158717bc3a29dbf79e8c.
    
    Change-Id: I48190e01afcb59dae18d87a1ca785fcec73b2730
    Reviewed-on: https://gerrit.mot.com/2609762
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -21,7 +21,6 @@
 #include <linux/vmalloc.h>
 #include <linux/swap_slots.h>
 #include <linux/huge_mm.h>
-#include <linux/mm_inline.h>
 
 #include <asm/pgtable.h>
 #include "internal.h"
@@ -410,7 +409,6 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 	struct page *found_page = NULL, *new_page = NULL;
 	struct swap_info_struct *si;
 	int err;
-	void *shadow;
 	*new_page_allocated = false;
 
 	do {
@@ -467,13 +465,10 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 		__SetPageLocked(new_page);
 		__SetPageSwapBacked(new_page);
 		err = add_to_swap_cache(new_page, entry,
-					gfp_mask & GFP_RECLAIM_MASK, &shadow);
+					gfp_mask & GFP_RECLAIM_MASK, NULL);
 		if (likely(!err)) {
 			/* Initiate read into locked page */
-			if (!lru_gen_enabled())
-				SetPageWorkingset(new_page);
-			else if (shadow)
-				lru_gen_refault(new_page, shadow);
+			SetPageWorkingset(new_page);
 			lru_cache_add_anon(new_page);
 			*new_page_allocated = true;
 			return new_page;
