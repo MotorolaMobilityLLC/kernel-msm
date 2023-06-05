4c89dc49583cf1158bb90596225233f7f3a338b8
Praveen koya <quic_pkoya@quicinc.com>
2023-06-05 10:01:35 +0530

    soc: qcom: add out of bound check for AON fifo
    Add out of bound check while parsing the SPI
    slave-to-master fifo.
    
    Change-Id: I14f707307fa277b2f8a7b543d3cc5e9ebac885db
    Signed-off-by: Praveen koya <quic_pkoya@quicinc.com>

--- drivers/soc/qcom/slatecom_spi.c
+++ drivers/soc/qcom/slatecom_spi.c
@@ -433,18 +433,23 @@ void slatecom_slatedown_handler(void)
 }
 EXPORT_SYMBOL(slatecom_slatedown_handler);
 
-static void parse_fifo(uint8_t *data, union slatecom_event_data_type *event_data)
+static void parse_fifo(uint8_t *data, uint16_t data_len, union slatecom_event_data_type *event_data)
 {
 	uint16_t p_len;
 	uint16_t event_id;
 	void *evnt_data;
 
 	while (*data != '\0') {
-
+		if (data_len < HED_EVENT_ID_LEN)
+			break;
 		event_id = *((uint16_t *) data);
 		data = data + HED_EVENT_ID_LEN;
+		data_len = data_len - HED_EVENT_ID_LEN;
+		if (data_len < HED_EVENT_SIZE_LEN)
+			break;
 		p_len = *((uint16_t *) data);
 		data = data + HED_EVENT_SIZE_LEN;
+		data_len = data_len - HED_EVENT_SIZE_LEN;
 
 		if (event_id == 0x0001) {
 			evnt_data = kmalloc(p_len, GFP_KERNEL);
@@ -458,10 +463,12 @@ static void parse_fifo(uint8_t *data, union slatecom_event_data_type *event_data
 			}
 		} else if (event_id == 0xc8) {
 			data = data + 12;
+			data_len = data_len - 12;
 			pr_err("Packet Received = 0x%X, len = %u\n", event_id, p_len);
 		}
 
 		data = data + p_len;
+		data_len = data_len - p_len;
 	}
 	if (!list_empty(&pr_lst_hd))
 		queue_work(wq, &input_work);
@@ -575,7 +582,8 @@ static void send_back_notification(uint32_t slav_status_reg,
 			if (!ret) {
 				augmnt_fifo((uint8_t *)ptr,
 					master_fifo_used*SLATE_SPI_WORD_SIZE);
-				parse_fifo((uint8_t *)ptr, &event_data);
+				parse_fifo((uint8_t *)ptr,
+					master_fifo_used*SLATE_SPI_WORD_SIZE, &event_data);
 			}
 			kfree(ptr);
 		}
