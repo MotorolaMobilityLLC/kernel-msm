1257ffdde8a315a21ac7a7ab4c7acd755a70d3d1
Greg Kroah-Hartman <gregkh@google.com>
2023-06-13 18:55:00 +0000

    Revert "ASoC: fsl_micfil: Fix error handler with pm_runtime_enable"
    This reverts commit 3a4ff14b0ecbe8dd05eb7ddcf42c849f459d53dd.
    
    It breaks the build due to previous abi preservations.  Will be brought
    back at a later time.
    
    Bug: 161946584
    Change-Id: I334bb523efe52dc3d868123cbbf204ba94cb1505
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- sound/soc/fsl/fsl_micfil.c
+++ sound/soc/fsl/fsl_micfil.c
@@ -763,7 +763,7 @@ static int fsl_micfil_probe(struct platform_device *pdev)
 	ret = devm_snd_dmaengine_pcm_register(&pdev->dev, NULL, 0);
 	if (ret) {
 		dev_err(&pdev->dev, "failed to pcm register\n");
-		goto err_pm_disable;
+		return ret;
 	}
 
 	ret = devm_snd_soc_register_component(&pdev->dev, &fsl_micfil_component,
@@ -771,20 +771,9 @@ static int fsl_micfil_probe(struct platform_device *pdev)
 	if (ret) {
 		dev_err(&pdev->dev, "failed to register component %s\n",
 			fsl_micfil_component.name);
-		goto err_pm_disable;
 	}
 
 	return ret;
-
-err_pm_disable:
-	pm_runtime_disable(&pdev->dev);
-
-	return ret;
-}
-
-static void fsl_micfil_remove(struct platform_device *pdev)
-{
-	pm_runtime_disable(&pdev->dev);
 }
 
 static int __maybe_unused fsl_micfil_runtime_suspend(struct device *dev)
@@ -845,7 +834,6 @@ static const struct dev_pm_ops fsl_micfil_pm_ops = {
 
 static struct platform_driver fsl_micfil_driver = {
 	.probe = fsl_micfil_probe,
-	.remove_new = fsl_micfil_remove,
 	.driver = {
 		.name = "fsl-micfil-dai",
 		.pm = &fsl_micfil_pm_ops,
