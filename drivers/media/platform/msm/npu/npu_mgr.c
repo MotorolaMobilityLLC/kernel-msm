6a68fa38c031e4840022a9e2fdc06896f4898264
Jilai Wang <quic_jilaiw@quicinc.com>
2023-06-22 11:41:21 -0400

    msm: npu: Fix use after free issue
    There is possibility that network will be used after free.
    This change is to fix this issue.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2023-33114
    CRs-Fixed: 3520999
    Change-Id: I25eaf33f2a641127f13ec20df5da29d2b2923828
    Signed-off-by: Jilai Wang <quic_jilaiw@quicinc.com>
    Sigend-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2760383
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/npu/npu_mgr.c
+++ drivers/media/platform/msm/npu/npu_mgr.c
@@ -1593,6 +1593,13 @@ int32_t npu_host_unload_network(struct npu_client *client,
 		return -EINVAL;
 	}
 
+	if (network->is_unloading) {
+		NPU_ERR("network is unloading\n");
+		network_put(network);
+		mutex_unlock(&host_ctx->lock);
+		return -EINVAL;
+	}
+
 	if (!network->is_active) {
 		pr_err("network is not active\n");
 		network_put(network);
@@ -1604,6 +1611,8 @@ int32_t npu_host_unload_network(struct npu_client *client,
 		pr_err("fw in error state, skip unload network in fw\n");
 		goto free_network;
 	}
+
+	network->is_unloading = true;
 
 	pr_debug("Unload network %lld\n", network->id);
 	/* prepare IPC packet for UNLOAD */
@@ -1706,6 +1715,12 @@ int32_t npu_host_exec_network(struct npu_client *client,
 
 	if (atomic_inc_return(&host_ctx->network_execute_cnt) == 1)
 		npu_notify_cdsprm_cxlimit_activity(npu_dev, true);
+
+	if (network->is_unloading) {
+		NPU_ERR("network is unloading\n");
+		ret = -EINVAL;
+		goto exec_v2_done;
+	}
 
 	if (!network->is_active) {
 		pr_err("network is not active\n");
