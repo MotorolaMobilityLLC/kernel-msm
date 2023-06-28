aa3db88c88acd84f18f00851e5d443e3313e0427
Minghao Xue <quic_mingxue@quicinc.com>
2023-06-28 14:09:00 +0800

    drivers: qcom: support a high resolution MPM timer
    Add support for a high resolution MPM timer, which has 19.2 MHZ frequency.
    
    Change-Id: I56d8e6e2dd67c58097094790cca4d771c3731cd3
    Signed-off-by: Minghao Xue <quic_mingxue@quicinc.com>

--- drivers/soc/qcom/boot_stats.c
+++ drivers/soc/qcom/boot_stats.c
@@ -51,6 +51,7 @@ struct boot_stats {
 static void __iomem *mpm_counter_base;
 static uint32_t mpm_counter_freq;
 static struct boot_stats __iomem *boot_stats;
+static void __iomem *mpm_hr_counter_base;
 
 #ifdef CONFIG_MSM_BOOT_TIME_MARKER
 
@@ -156,6 +157,25 @@ unsigned long long msm_timer_get_sclk_ticks(void)
 	return t1;
 }
 EXPORT_SYMBOL(msm_timer_get_sclk_ticks);
+
+unsigned long long msm_hr_timer_get_sclk_ticks(void)
+{
+	unsigned long long tl, th;
+	void __iomem *sclk_tick_high;
+	void __iomem *sclk_tick_low;
+
+	if (!mpm_hr_counter_base)
+		return -EINVAL;
+
+	sclk_tick_high = mpm_hr_counter_base + 0xc;
+	sclk_tick_low = mpm_hr_counter_base + 0x8;
+
+	tl = readl_relaxed(sclk_tick_low);
+	th = readl_relaxed(sclk_tick_high);
+
+	return (th << 32) | tl;
+}
+EXPORT_SYMBOL(msm_hr_timer_get_sclk_ticks);
 
 static void _destroy_boot_marker(const char *name)
 {
@@ -474,7 +494,7 @@ static void exit_bootkpi(void)
 
 static int mpm_parse_dt(void)
 {
-	struct device_node *np_imem, *np_mpm2;
+	struct device_node *np_imem, *np_mpm2, *np_mpm_hr;
 
 	np_imem = of_find_compatible_node(NULL, NULL,
 				"qcom,msm-imem-boot_stats");
@@ -507,8 +527,24 @@ static int mpm_parse_dt(void)
 	} else
 		goto err2;
 
-	return 0;
+	/* qcom,mpm-hr-counter is not mandatory */
+	np_mpm_hr = of_find_compatible_node(NULL, NULL,
+				"qcom,mpm-hr-counter");
+	if (!np_mpm_hr) {
+		pr_info("mpm_hr_counter: can't find DT node\n");
+		return 0;
+	}
 
+	if (of_get_address(np_mpm_hr, 0, NULL, NULL)) {
+		mpm_hr_counter_base = of_iomap(np_mpm_hr, 0);
+		if (!mpm_hr_counter_base) {
+			pr_err("mpm_hr_counter: cant map counter base\n");
+			of_node_put(np_mpm_hr);
+		}
+	} else
+		of_node_put(np_mpm_hr);
+
+	return 0;
 err2:
 	of_node_put(np_mpm2);
 err1:
@@ -552,6 +588,8 @@ static int __init boot_stats_init(void)
 	} else {
 		iounmap(boot_stats);
 		iounmap(mpm_counter_base);
+		if (mpm_hr_counter_base)
+			iounmap(mpm_hr_counter_base);
 	}
 
 	return 0;
@@ -564,6 +602,8 @@ static void __exit boot_stats_exit(void)
 		exit_bootkpi();
 		iounmap(boot_stats);
 		iounmap(mpm_counter_base);
+		if (mpm_hr_counter_base)
+			iounmap(mpm_hr_counter_base);
 	}
 }
 module_exit(boot_stats_exit)
