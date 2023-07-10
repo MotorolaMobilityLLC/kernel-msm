e59eb6c8390dea14b33f25c0bb2848d44a13c302
Manish Pandey <quic_mapa@quicinc.com>
2023-07-10 15:02:28 +0530

    mmc: sdhci-msm: Populate cpu mask dynamically
    Currently cpu mask are populated from device tree
    nodes for assigning the cpu mask to qos_cpu_group.
    It may happen that some cpus are faulty or purposely
    fused and hence won't be enumerated. Also with Logical
    contiguous CPU numbering introduced, one to one mapping
    between physical and logical cpu won't be applicable
    anymore. Hence we need to populate the cpu mask dynamically.
    
    Fix it by populating cpu mask dynamically and then assigning
    valid cpu mask to qos_cpu_group.
    
    Change-Id: I26712366c6e659342590ec3700b29708cc2107e1
    Co-developed-by: Nitin Rawat <quic_nitirawa@quicinc.com>
    Signed-off-by: Nitin Rawat <quic_nitirawa@quicinc.com>
    Signed-off-by: Manish Pandey <quic_mapa@quicinc.com>

--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -30,6 +30,14 @@
 
 #define MMC_CAP2_CLK_SCALE      (1 << 28)       /* Allow dynamic clk scaling */
 
+/* CPU Clusters Info */
+enum cpu_cluster_info {
+	SILVER_CORE,
+	GOLD_CORE,
+	GOLD_PRIME_CORE,
+	MAX_NUM_CLUSTERS,
+};
+
 enum dev_state {
 	DEV_SUSPENDING = 1,
 	DEV_SUSPENDED,
