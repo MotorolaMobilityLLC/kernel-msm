794aa4575f50ef2c075b86726feb8d0a6b73ebab
Kui Gao <quic_kuig@quicinc.com>
2023-07-13 07:11:34 -0400

    soc: qcom: hab: add post function to handle probe arrived too soon
    avoid race condition by register device driver later
    
    Change-Id: I6fa53b91a9fbb546c9471fa98d31afbddb48f597
    Signed-off-by: Kui Gao <quic_kuig@quicinc.com>

--- drivers/soc/qcom/hab/hyp_stub.c
+++ drivers/soc/qcom/hab/hyp_stub.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include "hab.h"
 
@@ -15,3 +16,5 @@ void hab_hypervisor_unregister(void)
 {
 	hab_hypervisor_unregister_common();
 }
+
+int hab_hypervisor_register_post(void) { return 0; }
