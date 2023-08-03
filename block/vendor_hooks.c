BayLibre/android_kernel_common

ANDROID: vendor_hooks: Add hook for blk request
We want to use this hook to record time and blk size when blk request end

Bug: 292581151
Change-Id: I5083d24175725afd8e26c17a0a23c624432d2172
Signed-off-by: Li Chong <lichong1@oppo.com>

block/vendor_hooks.c
@@ -15,6 +15,7 @@
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_alloc_rqs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rq_qos_done);
 
 /*
  * For type visibility

include/trace/hooks/block.h
@@ -19,6 +19,7 @@
 struct blk_mq_tags;
 struct blk_mq_alloc_data;
 struct blk_mq_tag_set;
+struct request;
 
 DECLARE_HOOK(android_vh_blk_alloc_rqs,
 	TP_PROTO(size_t *rq_size, struct blk_mq_tag_set *set,
@@ -29,6 +30,10 @@ DECLARE_HOOK(android_vh_blk_rq_ctx_init,
 	TP_PROTO(struct request *rq, struct blk_mq_tags *tags,
 		struct blk_mq_alloc_data *data, u64 alloc_time_ns),
 	TP_ARGS(rq, tags, data, alloc_time_ns));
+
+DECLARE_HOOK(android_vh_rq_qos_done,
+        TP_PROTO(struct request *rq),
+        TP_ARGS(rq));
 
 #endif /* _TRACE_HOOK_BLOCK_H */
 

MotorolaMobilityLLC/kernel-msm

block/vendor_hooks.c
@@ -15,6 +15,7 @@
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_alloc_rqs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_account_io_done);
 
 /*
  * For type visibility

include/trace/hooks/block.h
@@ -19,6 +19,7 @@
 struct blk_mq_tags;
 struct blk_mq_alloc_data;
 struct blk_mq_tag_set;
+struct request;
 
 DECLARE_HOOK(android_vh_blk_alloc_rqs,
 	TP_PROTO(size_t *rq_size, struct blk_mq_tag_set *set,
@@ -29,6 +30,10 @@ DECLARE_HOOK(android_vh_blk_rq_ctx_init,
 	TP_PROTO(struct request *rq, struct blk_mq_tags *tags,
 		struct blk_mq_alloc_data *data, u64 alloc_time_ns),
 	TP_ARGS(rq, tags, data, alloc_time_ns));
+
+DECLARE_HOOK(android_vh_blk_account_io_done,
+        TP_PROTO(struct request *rq),
+        TP_ARGS(rq));
 
 #endif /* _TRACE_HOOK_BLOCK_H */
 