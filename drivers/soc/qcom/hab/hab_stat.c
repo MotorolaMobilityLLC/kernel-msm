834dd6aee7860e679fc81899e11b576e6c886f0c
lixiang <quic_lixian@quicinc.com>
2023-08-23 13:07:01 +0800

    soc: qcom: hab: introduce sync import and async unimport
    Introduce following changes to HAB driver:
    1. new HAB protocol -- synchronous import
    2. new HAB protocol -- asynchronous unimport
    3. unexport will fail if the target memory has not been unimported
    4. new reclaim list to store the export descriptor when exporter exits
    without unimport from importer
    
    The import call now sends HAB_PAYLOAD_TYPE_IMPORT to the remote.
    The exporter now sends HAB_PAYLOAD_TYPE_IMPORT_ACK to the importer
    when it receives the import request.
    
    importer                exporter
    ---------------------------------------------------------------
                            export memory
                            prepare memory locally
    send import       -->
    wait import ack         receive the import with the requested exp id
    wait import ack   <--   send(import ack with requested exp_desc)
    receive ack
    extract exp_desc
    import memory
    
    Meanwhile, the EXPORT and EXPORT_ACK types of message are removed.
    Export no longer sends the exp descriptor to the importer in advance.
    The export descriptor is only locally prepared.
    
    The reason behind it is we are trying to hide the exported memory as
    long as possible and NOT exposing it to the remote side in advance.
    Otherwise, the export_desc sent in advance may be invalid and importer
    will never know.
    
    The newly added reclaim list is where we keep the exported memory
    descriptor when exporter from userspace suddenly crashes and tries to
    clean up relevant resources. We have to keep the memory to prevent from
    the memory getting freed when the importer is still using it.
    
    The HAB driver with the new protocol is compatible with current HAB.
    
    Change-Id: I5d3fd240c7f4c7a03da279401cb6ef027f021ba6
    Signed-off-by: lixiang <quic_lixian@quicinc.com>

--- drivers/soc/qcom/hab/hab_stat.c
+++ drivers/soc/qcom/hab/hab_stat.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include "hab.h"
 #include "hab_grantable.h"
@@ -123,14 +123,14 @@ static int print_ctx_total_expimp(struct uhab_context *ctx,
 	int exim_size = 0;
 	int ret = 0;
 
-	read_lock(&ctx->exp_lock);
+	read_lock_bh(&ctx->exp_lock);
 	list_for_each_entry(exp, &ctx->exp_whse, node) {
 		pfn_table =	(struct compressed_pfns *)exp->payload;
 		exim_size = get_pft_tbl_total_size(pfn_table);
 		exp_total += exim_size;
 		exp_cnt++;
 	}
-	read_unlock(&ctx->exp_lock);
+	read_unlock_bh(&ctx->exp_lock);
 
 	spin_lock_bh(&ctx->imp_lock);
 	list_for_each_entry(exp, &ctx->imp_whse, node) {
@@ -151,7 +151,7 @@ static int print_ctx_total_expimp(struct uhab_context *ctx,
 	else
 		return 0;
 
-	read_lock(&ctx->exp_lock);
+	read_lock_bh(&ctx->exp_lock);
 	hab_stat_buffer_print(buf, size, "export[expid:vcid:size]: ");
 	list_for_each_entry(exp, &ctx->exp_whse, node) {
 		pfn_table =	(struct compressed_pfns *)exp->payload;
@@ -161,7 +161,7 @@ static int print_ctx_total_expimp(struct uhab_context *ctx,
 			exp->vcid_local, exim_size);
 	}
 	hab_stat_buffer_print(buf, size, "\n");
-	read_unlock(&ctx->exp_lock);
+	read_unlock_bh(&ctx->exp_lock);
 
 	spin_lock_bh(&ctx->imp_lock);
 	hab_stat_buffer_print(buf, size, "import[expid:vcid:size]: ");
@@ -215,6 +215,37 @@ int hab_stat_show_expimp(struct hab_driver *driver,
 	/* print pchannel status, drvlock is not required */
 	if (pchan_count > 0)
 		ret = hab_stat_log(pchans, pchan_count, buf, size);
+
+	return ret;
+}
+
+int hab_stat_show_reclaim(struct hab_driver *driver, char *buf, int size)
+{
+	struct export_desc *exp = NULL;
+	struct compressed_pfns *pfn_table = NULL;
+	int exim_size = 0;
+	int ret = 0;
+	size_t total_size = 0, total_num = 0;
+
+	ret = strscpy(buf, "", size);
+	ret = hab_stat_buffer_print(buf, size, "export[expid:vcid:size:pchan]:\n");
+
+	spin_lock(&hab_driver.reclaim_lock);
+	list_for_each_entry(exp, &hab_driver.reclaim_list, node) {
+		pfn_table = (struct compressed_pfns *)exp->payload;
+		exim_size = get_pft_tbl_total_size(pfn_table);
+		total_size += exim_size;
+		total_num++;
+		ret = hab_stat_buffer_print(buf, size, "[%d:%x:%d:%s] ",
+			exp->export_id,
+			exp->vcid_local,
+			exim_size,
+			exp->pchan->name);
+		ret = hab_stat_buffer_print(buf, size, "\n");
+	}
+	spin_unlock(&hab_driver.reclaim_lock);
+
+	ret = hab_stat_buffer_print(buf, size, "total: %u, size %u\n", total_num, total_size);
 
 	return ret;
 }
