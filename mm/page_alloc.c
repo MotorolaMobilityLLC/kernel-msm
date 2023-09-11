35fa35373d6ede0e0c471463a3655fad15fd1104
Johannes Weiner <hannes@cmpxchg.org>
2023-09-11 14:11:08 -0400

    BACKPORT: mm: page_alloc: fix CMA and HIGHATOMIC landing on the wrong buddy list
    Commit 4b23a68f9536 ("mm/page_alloc: protect PCP lists with a spinlock")
    bypasses the pcplist on lock contention and returns the page directly to
    the buddy list of the page's migratetype.
    
    For pages that don't have their own pcplist, such as CMA and HIGHATOMIC,
    the migratetype is temporarily updated such that the page can hitch a ride
    on the MOVABLE pcplist.  Their true type is later reassessed when flushing
    in free_pcppages_bulk().  However, when lock contention is detected after
    the type was already overridden, the bypass will then put the page on the
    wrong buddy list.
    
    Once on the MOVABLE buddy list, the page becomes eligible for fallbacks
    and even stealing.  In the case of HIGHATOMIC, otherwise ineligible
    allocations can dip into the highatomic reserves.  In the case of CMA, the
    page can be lost from the CMA region permanently.
    
    Use a separate pcpmigratetype variable for the pcplist override.  Use the
    original migratetype when going directly to the buddy.  This fixes the bug
    and should make the intentions more obvious in the code.
    
    Originally sent here to address the HIGHATOMIC case:
    https://lore.kernel.org/lkml/20230821183733.106619-4-hannes@cmpxchg.org/
    
    Changelog updated in response to the CMA-specific bug report.
    
    [mgorman@techsingularity.net: updated changelog]
    Link: https://lkml.kernel.org/r/20230911181108.GA104295@cmpxchg.org
    Fixes: 4b23a68f9536 ("mm/page_alloc: protect PCP lists with a spinlock")
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reported-by: Joe Liu <joe.liu@mediatek.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bug: 302075941
    (cherry picked from commit 7b086755fb8cdbb6b3e45a1bbddc00e7f9b1dc03)
    Change-Id: I5e62c941e0a91db551a56f4b8c1ae14f87fd2bc3
    Signed-off-by: Lecopzer Chen <lecopzer.chen@mediatek.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3606,7 +3606,7 @@ void free_unref_page(struct page *page, unsigned int order)
 	struct per_cpu_pages *pcp;
 	struct zone *zone;
 	unsigned long pfn = page_to_pfn(page);
-	int migratetype;
+	int migratetype, pcpmigratetype;
 	bool pcp_skip_cma_pages = false;
 	bool skip_free_unref_page = false;
 
@@ -3621,11 +3621,11 @@ void free_unref_page(struct page *page, unsigned int order)
 	/*
 	 * We only track unmovable, reclaimable movable, and CMA on pcp lists.
 	 * Place ISOLATE pages on the isolated list because they are being
-	 * offlined but treat HIGHATOMIC as movable pages so we can get those
-	 * areas back if necessary. Otherwise, we may have to free
+	 * offlined but treat HIGHATOMIC and CMA as movable pages so we can
+	 * get those areas back if necessary. Otherwise, we may have to free
 	 * excessively into the page allocator
 	 */
-	migratetype = get_pcppage_migratetype(page);
+	migratetype = pcpmigratetype = get_pcppage_migratetype(page);
 	if (unlikely(migratetype >= MIGRATE_PCPTYPES)) {
 		trace_android_vh_pcplist_add_cma_pages_bypass(migratetype,
 			&pcp_skip_cma_pages);
@@ -3634,14 +3634,14 @@ void free_unref_page(struct page *page, unsigned int order)
 			free_one_page(page_zone(page), page, pfn, order, migratetype, FPI_NONE);
 			return;
 		}
-		migratetype = MIGRATE_MOVABLE;
+		pcpmigratetype = MIGRATE_MOVABLE;
 	}
 
 	zone = page_zone(page);
 	pcp_trylock_prepare(UP_flags);
 	pcp = pcp_spin_trylock_irqsave(zone_per_cpu_pageset(zone), flags);
 	if (pcp) {
-		free_unref_page_commit(zone, pcp, page, pfn, migratetype, order);
+		free_unref_page_commit(zone, pcp, page, pfn, pcpmigratetype, order);
 		pcp_spin_unlock_irqrestore(pcp, flags);
 	} else {
 		free_one_page(zone, page, pfn, order, migratetype, FPI_NONE);
