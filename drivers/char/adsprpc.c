69583dc6d24b3374f3cee5fd6f58d3be2e5e5776
Santosh Sakore <quic_ssakore@quicinc.com>
2023-09-15 12:33:04 +0530

    msm: adsprpc : Fix use after free in fastrpc_internal_mem_unmap
    Thread 1 can make a to call fastrpc_mmap_create under internal mem map
    and release fl->map_mutex. Thread 2 can make call to internal mem unmap,
    acquire fl->map_mutex and get same map though fastrpc_mmap_remove.
    Thread 1 fail in fastrpc_mem_map_to_dsp jumps to bail and do map free.
    Thread 2 still holds same map which can lead use after free. Serialize
    fastrpc internal mem map and unmap.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2023-43514
    CRs-Fixed: 3613254
    Bug: 303101664
    Change-Id: I54a3602914b43fc67635c0de193bd21aa13daaa3
    Signed-off-by: Santosh Sakore <quic_ssakore@quicinc.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2839049
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -5242,6 +5242,7 @@ static int fastrpc_internal_mem_map(struct fastrpc_file *fl,
 	int err = 0;
 	struct fastrpc_mmap *map = NULL;
 
+	mutex_lock(&fl->internal_map_mutex);
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
 		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
@@ -5280,6 +5281,7 @@ bail:
 			mutex_unlock(&fl->map_mutex);
 		}
 	}
+	mutex_unlock(&fl->internal_map_mutex);
 	return err;
 }
 
@@ -5290,6 +5292,7 @@ static int fastrpc_internal_mem_unmap(struct fastrpc_file *fl,
 	struct fastrpc_mmap *map = NULL;
 	size_t map_size = 0;
 
+	mutex_lock(&fl->internal_map_mutex);
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
 		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
@@ -5336,6 +5339,7 @@ bail:
 			mutex_unlock(&fl->map_mutex);
 		}
 	}
+	mutex_unlock(&fl->internal_map_mutex);
 	return err;
 }
 
