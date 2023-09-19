9df252ab209c2b15c32ba83fe4c9bb294f47a273
Anand Kulkarni <quic_anankulk@quicinc.com>
2023-09-19 12:57:28 +0530

    msm: adsprpc: Handle UAF in process shell memory
    Added flag to indicate memory used
    in process initialization. And, this memory
    would not removed in internal unmap to avoid
    UAF or double free.
    
    Change-Id: Ie470fe58ac334421d186feb41fa67bd24bb5efea
    Acked-by: Deepika Singh <dsi@qti.qualcomm.com>
    Signed-off-by: Anand Kulkarni <quic_anankulk@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -4085,6 +4085,8 @@ static int fastrpc_init_create_static_process(struct fastrpc_file *fl,
 			mutex_lock(&fl->map_mutex);
 			err = fastrpc_mmap_create(fl, -1, NULL, 0, init->mem,
 				 init->memlen, ADSP_MMAP_REMOTE_HEAP_ADDR, &mem);
+			if (mem)
+				mem->is_filemap = true;
 			mutex_unlock(&fl->map_mutex);
 			if (err)
 				goto bail;
