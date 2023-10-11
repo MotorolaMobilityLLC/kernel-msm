1c454ffbbd2d5ab700feccb264ff144e07fa5e78
wuqing5 <wuqing5@motorola.com>
2023-10-11 18:00:39 +0800

    ssr: expand ssr_reason for subsystem_ramdump
    porting from (CR)
    expose ssr_reason for subtype_ramdump, so that bug2go can show the ssr
    reason
    
    Change-Id: If3fcdc84a8df8107f80deb53e4ea3d1ccb4254ff
    Reviewed-on: https://gerrit.mot.com/2764492
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiaoliang Duan <duanxl1@motorola.com>
    Reviewed-by: Wen Zeng <zengwen1@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/remoteproc/qcom_q6v5.c
+++ drivers/remoteproc/qcom_q6v5.c
@@ -21,6 +21,9 @@
 
 #define Q6V5_PANIC_DELAY_MS	200
 
+static char qcom_ssr_reason[256];
+static char *ssr_reason = qcom_ssr_reason;
+module_param(ssr_reason, charp, S_IRUGO);
 /**
  * qcom_q6v5_prepare() - reinitialize the qcom_q6v5 context before start
  * @q6v5:	reference to qcom_q6v5 context to be reinitialized
@@ -120,6 +123,8 @@ static irqreturn_t q6v5_wdog_interrupt(int irq, void *data)
 		q6v5->rproc->recovery_disabled ?
 		"disabled and lead to device crash" :
 		"enabled and kick reovery process");
+	memset(qcom_ssr_reason, 0, sizeof(qcom_ssr_reason));
+	strlcpy(qcom_ssr_reason, msg, min((size_t)len, (size_t)sizeof(qcom_ssr_reason)));
 
 	if (q6v5->ssr_subdev)
 		qcom_notify_early_ssr_clients(q6v5->ssr_subdev);
@@ -150,6 +155,9 @@ static irqreturn_t q6v5_fatal_interrupt(int irq, void *data)
 	} else {
 		dev_err(q6v5->dev, "fatal error without message\n");
 	}
+
+	memset(qcom_ssr_reason, 0, sizeof(qcom_ssr_reason));
+	strlcpy(qcom_ssr_reason, msg, min((size_t)len, (size_t)sizeof(qcom_ssr_reason)));
 
 	q6v5->running = false;
 	dev_err(q6v5->dev, "rproc recovery state: %s\n",
