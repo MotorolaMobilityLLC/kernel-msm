5119fe75b92382e36b47a5c7ce32719e58adfb18
Xiaojun Ji <jixj@motorola.com>
2023-10-12 16:21:32 +0800

    Revert "mm/madvise: support both pid and pidfd for process_madvise"
    Mot-CRs-fixed: (CR)
    
    it only has 5 parameters in libc, currently has 6 parameters in kernel to
    cause it return -EINVAL in "flags" check
    
    libc defined:
    ssize_t process_madvise(int __pid_fd, const struct iovec* __iov, size_t __count, int __advice, unsigned __flags) __INTRODUCED_IN(31);
    
    This reverts commit 306d0c3e29cad7c8e41e1bdfc53a3ceac5d2ad66.
    
    Change-Id: If69151ae97498f4db210fbe4d16bf53ad4abf42b
    Reviewed-on: https://gerrit.mot.com/2765786
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhangqing Huang <huangzq2@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -874,7 +874,7 @@ asmlinkage long sys_munlockall(void);
 asmlinkage long sys_mincore(unsigned long start, size_t len,
 				unsigned char __user * vec);
 asmlinkage long sys_madvise(unsigned long start, size_t len, int behavior);
-asmlinkage long sys_process_madvise(int which, pid_t upid,
+asmlinkage long sys_process_madvise(int pidfd,
 		const struct iovec __user *vec, unsigned long vlen,
 		int behavior, unsigned long flags);
 asmlinkage long sys_remap_file_pages(unsigned long start, unsigned long size,
