973868e1fc52d330b929a0c7628866c10f13d2f0
Aniket Randive <quic_arandive@quicinc.com>
2023-10-30 14:52:47 +0530

    serial: msm-geni-serial: Add NULL check before freeing of buffer
    Allocation of DMA buffer for RX will happen in msm_geni_uart_gsi_xfer_rx
    function. In some cases if we didn't receive any RX packet and if we got
    the port closeup request then we will deallocate the DMA buffer which is
    not allocated yet.
    Hence, add a NULL check before freeing of buffer.
    
    Change-Id: I2896cfc04dba82d34199e43952e4463bb901aa96
    Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>
    Signed-off-by: Zahir Shabbir Khan <quic_zshabbir@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2174,6 +2174,7 @@ static int msm_geni_uart_gsi_xfer_rx(struct uart_port *uport)
 			for (k = i; k > 0; k--) {
 				geni_se_common_iommu_free_buf(rx_dev, &msm_port->dma_addr[k - 1],
 						msm_port->rx_gsi_buf[k - 1], DMA_RX_BUF_SIZE);
+				msm_port->rx_gsi_buf[k - 1] = NULL;
 			}
 			msm_geni_deallocate_chan(uport);
 			return -EIO;
@@ -2217,6 +2218,7 @@ exit_gsi_xfer_rx:
 	for (i = 0; i < NUM_RX_BUF; i++) {
 		geni_se_common_iommu_free_buf(rx_dev, &msm_port->dma_addr[i],
 					      msm_port->rx_gsi_buf[i], DMA_RX_BUF_SIZE);
+		msm_port->rx_gsi_buf[i] = NULL;
 	}
 	msm_geni_deallocate_chan(uport);
 	msm_port->gsi_rx_done = false;
@@ -3829,10 +3831,14 @@ static void msm_geni_serial_shutdown(struct uart_port *uport)
 					     uport->dev,
 					     "%s:GSI DMA-Rx ch\n", __func__);
 				for (i = 0; i < 4; i++) {
-					geni_se_common_iommu_free_buf(rx_dev,
-								      &msm_port->dma_addr[i],
-								      msm_port->rx_gsi_buf[i],
-								      DMA_RX_BUF_SIZE);
+					if (msm_port->dma_addr[i]) {
+						geni_se_common_iommu_free_buf(rx_dev,
+								&msm_port->dma_addr[i],
+								msm_port->rx_gsi_buf[i],
+								DMA_RX_BUF_SIZE);
+						msm_port->rx_gsi_buf[i] = NULL;
+					}
+
 				}
 				UART_LOG_DBG(msm_port->ipc_log_misc,
 					     uport->dev, "%s:Unmap buf done\n",
