bbb77126708bec8acb252e54cb8e65453c4778b7
Peter Zijlstra <peterz@infradead.org>
2023-12-15 10:12:18 +0100

    BACKPORT: x86/cfi,bpf: Fix BPF JIT call
    The current BPF call convention is __nocfi, except when it calls !JIT things,
    then it calls regular C functions.
    
    It so happens that with FineIBT the __nocfi and C calling conventions are
    incompatible. Specifically __nocfi will call at func+0, while FineIBT will have
    endbr-poison there, which is not a valid indirect target. Causing #CP.
    
    Notably this only triggers on IBT enabled hardware, which is probably why this
    hasn't been reported (also, most people will have JIT on anyway).
    
    Implement proper CFI prologues for the BPF JIT codegen and drop __nocfi for
    x86.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20231215092707.345270396@infradead.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Bug: 452811419
    Change-Id: I84f6d7464da9d3d6ffa64a8ae6c8972121d70c3f
    (cherry picked from commit 4f9087f16651aca4a5f32da840a53f6660f0579a)
    [liwei: discard the including of <asm/cfi.h> because it already exists
     and fix some minor conflicts]
    Signed-off-by: wei li <sirius.liwei@honor.corp-partner.google.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -29,6 +29,7 @@
 #include <linux/rcupdate_trace.h>
 #include <linux/static_call.h>
 #include <linux/memcontrol.h>
+#include <linux/cfi.h>
 #include <linux/android_kabi.h>
 
 struct bpf_verifier_env;
@@ -1209,7 +1210,11 @@ struct bpf_dispatcher {
 	ANDROID_KABI_RESERVE(1);
 };
 
-static __always_inline __nocfi unsigned int bpf_dispatcher_nop_func(
+#ifndef __bpfcall
+#define __bpfcall __nocfi
+#endif
+
+static __always_inline __bpfcall unsigned int bpf_dispatcher_nop_func(
 	const void *ctx,
 	const struct bpf_insn *insnsi,
 	bpf_func_t bpf_func)
@@ -1299,7 +1304,7 @@ int arch_prepare_bpf_dispatcher(void *image, void *buf, s64 *funcs, int num_func
 
 #define DEFINE_BPF_DISPATCHER(name)					\
 	__BPF_DISPATCHER_SC(name);					\
-	noinline __nocfi unsigned int bpf_dispatcher_##name##_func(	\
+	noinline __bpfcall unsigned int bpf_dispatcher_##name##_func(	\
 		const void *ctx,					\
 		const struct bpf_insn *insnsi,				\
 		bpf_func_t bpf_func)					\
@@ -1444,6 +1449,9 @@ struct bpf_prog_aux {
 	struct bpf_kfunc_desc_tab *kfunc_tab;
 	struct bpf_kfunc_btf_tab *kfunc_btf_tab;
 	u32 size_poke_tab;
+#ifdef CONFIG_FINEIBT
+	struct bpf_ksym ksym_prefix;
+#endif
 	struct bpf_ksym ksym;
 	const struct bpf_prog_ops *ops;
 	struct bpf_map **used_maps;
