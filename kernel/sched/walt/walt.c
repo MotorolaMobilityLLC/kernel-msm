c0cacb553cc9f78b96db8fdf14d0f3e850e9c8e1
yuedl1 <yuedl1@lenovo.com>
2024-01-19 10:48:07 +0800

    [3/12]:optimize scheduling in walt
    1. optimize trace in walt.
    2. export binder ux type set function for further optimization.
    3. restore default exec limit for mvp task.
    Mot-CR-Fixed:(CR)
    
    Change-Id: I2e463cb257410beaaf342f42587ea811b9059de6
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2859408
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3039025
    Signed-off-by: Yiqiang Feng <fengyq4@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3089753
    Reviewed-by: Zhizhong Ge <gezz1@motorola.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -5334,7 +5334,7 @@ static void walt_do_sched_yield(void *unused, struct rq *rq)
 	// Moto wangwang: don't deactivate mvp tasks when moto_sched enabled.
 	if (unlikely(!moto_sched_enabled) && !list_empty(&wts->mvp_list) && wts->mvp_list.next) {
 		if (!pipeline_in_progress() || !walt_pipeline_low_latency_task(curr))
-			walt_cfs_deactivate_mvp_task(rq, curr);
+			walt_cfs_deactivate_mvp_task(rq, curr, 3); // Moto huangzq2: debugging enhancement.
 	}
 
 	if (per_cpu(rt_task_arrival_time, cpu_of(rq)))
