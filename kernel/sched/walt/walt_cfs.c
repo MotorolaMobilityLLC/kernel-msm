fe887f1c9de3d5bd077bdd31aa8936d307f9b9ff
huangzq2 <huangzq2@motorola.com>
2024-01-26 13:50:13 +0800

    [7/12]: Optimize cpu selection in walt
    Optimize cpu selection in walt
    Mot-CR-Fixed:(CR)
    
    Change-Id: I763ab7a8fa10df7cea13ddf40cadfde183f38c79
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2866521
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2866852
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3039104
    Signed-off-by: Yiqiang Feng <fengyq4@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3089786
    Reviewed-by: Zhizhong Ge <gezz1@motorola.com>

--- kernel/sched/walt/walt_cfs.c
+++ kernel/sched/walt/walt_cfs.c
@@ -430,15 +430,16 @@ retry:
 
 #if IS_ENABLED(CONFIG_SCHED_MOTO_UNFAIR)
 			// Moto huangzq2: skip long exec mvp tasks like top app or kswapd.
-			if (wrq->num_mvp_tasks > 0) {
+			if (likely(moto_sched_enabled) && wrq->num_mvp_tasks > 0) {
 				struct walt_task_struct *rq_wts = list_first_entry(&wrq->mvp_tasks, struct walt_task_struct, mvp_list);
 				if (rq_wts != NULL && (rq_wts->mvp_prio == UX_PRIO_TOPAPP || rq_wts->mvp_prio == UX_PRIO_KSWAPD)) {
 					continue;
 				}
 			}
 
-			// Moto huangzq2: select least mvp cpu for all taks.
-			if (wrq->num_mvp_tasks < mvp_min_tasks) {
+			if (likely(moto_sched_enabled)
+					&& (wrq->num_mvp_tasks < mvp_min_tasks // least mvp tasks, select it.
+							|| (wrq->num_mvp_tasks == mvp_min_tasks && i == prev_cpu))) { // same mvp tasks but prev_cpu, also select it.
 				mvp_min_tasks = wrq->num_mvp_tasks;
 				least_mvp_cpu = i;
 			}
@@ -1030,7 +1031,11 @@ int walt_find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 		goto fail;
 
 #if IS_ENABLED(CONFIG_SCHED_MOTO_UNFAIR)
-	fbt_env.mvp_prio = walt_get_mvp_task_prio(p);
+	// TODO: Mark it here becuase of potential overhead of walt_get_mvp_task_prio,
+	// pls unmark it once we have further optimization based on current mvp_prio,
+	// for example, select big core on top app.
+	// fbt_env.mvp_prio = walt_get_mvp_task_prio(p);
+	fbt_env.mvp_prio = WALT_NOT_MVP;
 #endif
 	fbt_env.is_rtg = is_rtg;
 	fbt_env.start_cpu = start_cpu;
