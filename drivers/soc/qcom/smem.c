5bb7f067aceb7970d911741a2f34a989c82146be
Sarannya S <quic_sarannya@quicinc.com>
2024-02-15 14:41:17 +0530

    soc: qcom: smem: Add boundary checks for partitions
    Add condition check to make sure that the end address
    of private entry does not go out of partition.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2024-23368
    Bug: 3522299
    
    Change-Id: I88b3c69d86d90905b214c13a8c632b134b487a49
    Signed-off-by: Sarannya S <quic_sarannya@quicinc.com>
    Signed-off-by: Pranav Mahesh Phansalkar <quic_pphansal@quicinc.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2947890
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/soc/qcom/smem.c
+++ drivers/soc/qcom/smem.c
@@ -100,6 +100,17 @@
 	 (((void *)(ptr) + (size)) >= (void *)(ptr)) &&	\
 	 (((void *)(ptr) + (size)) <= (void *)(end)))
 
+/* Entry range check
+ * ptr >= start : Checks if ptr is greater than the start of access region
+ * ptr + size >= ptr: Check for integer overflow (On 32bit system where ptr
+ * and size are 32bits, ptr + size can wrap around to be a small integer)
+ * ptr + size <= end: Checks if ptr+size is less than the end of access region
+ */
+#define IN_PARTITION_RANGE(ptr, size, start, end)		\
+	(((void *)(ptr) >= (void *)(start)) &&			\
+	 (((void *)(ptr) + (size)) >= (void *)(ptr)) &&	\
+	 (((void *)(ptr) + (size)) <= (void *)(end)))
+
 /**
   * struct smem_proc_comm - proc_comm communication struct (legacy)
   * @command:	current command to be executed
