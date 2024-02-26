6f767760f14da438479c357b83dfea1e0d645047
yuedl1 <yuedl1@lenovo.com>
2024-02-26 13:51:40 +0800

    [9/12]: remove moto_sched changes when disabled
    remove moto_sched changes when disabled
    Mot-CR-Fixed:(CR)
    
    Change-Id: I1df907d491569ad2cc8a17e9a69bd9b52329fa0c
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2888884
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: yuedl1 <yuedl1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3039115
    Signed-off-by: Yiqiang Feng <fengyq4@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3089788
    Reviewed-by: Zhizhong Ge <gezz1@motorola.com>

--- kernel/sched/walt/walt_cfs.c
+++ kernel/sched/walt/walt_cfs.c
@@ -1582,8 +1582,12 @@ static void walt_cfs_check_preempt_wakeup(void *unused, struct rq *rq, struct ta
 	skip_mvp = wrq->skip_mvp;
 	walt_cfs_account_mvp_runtime(rq, c);
 #if IS_ENABLED(CONFIG_SCHED_MOTO_UNFAIR)
-	resched = ((skip_mvp != wrq->skip_mvp) || (wrq->mvp_tasks.next != &wts_c->mvp_list) ||
+	if (likely(moto_sched_enabled))
+		resched = ((skip_mvp != wrq->skip_mvp) || (wrq->mvp_tasks.next != &wts_c->mvp_list) ||
 			(wts_p->mvp_prio > wts_c->mvp_prio)) && (wrq->mvp_tasks.next == &wts_p->mvp_list); // Moto wangwang: fix preemption issue.
+	else
+		resched = (skip_mvp != wrq->skip_mvp) || (wrq->mvp_tasks.next != &wts_c->mvp_list) ||
+			(wts_p->mvp_prio > wts_c->mvp_prio);
 #else
 	resched = (skip_mvp != wrq->skip_mvp) || (wrq->mvp_tasks.next != &wts_c->mvp_list) ||
 			(wts_p->mvp_prio > wts_c->mvp_prio);
