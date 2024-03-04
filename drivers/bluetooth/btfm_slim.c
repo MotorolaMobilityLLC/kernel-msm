e8454888801fa664edd9a2a025213551f332831f
Satish Kumar Kodishala <quic_skodisha@quicinc.com>
2024-03-04 20:44:55 +0530

    Delay after port closure when usecase ended for wcn6x5x
    Delay after port closure when usecase ended for
    WCN6x5x. This would give some time for SB to be
    in right state before next usecase starts and helps
    in quick SHO scenarios.
    
    CRs-Fixed: 3749420
    Moto-CRs-fixed: (CR)
    Change-Id: I5efab4f3985bb3614f16510646cb3387dff3b4b1
    Reviewed-on: https://gerrit.mot.com/2934182
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiaoyang Wang <wangxy52@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/bluetooth/btfm_slim.c
+++ drivers/bluetooth/btfm_slim.c
@@ -243,7 +243,10 @@ int btfm_slim_disable_ch(struct btfmslim *btfmslim, struct btfmslim_ch *ch,
 		chipset_ver == QCA_APACHE_SOC_ID_0100 ||
 		chipset_ver == QCA_APACHE_SOC_ID_0110 ||
 		chipset_ver == QCA_APACHE_SOC_ID_0120 ||
-		chipset_ver == QCA_APACHE_SOC_ID_0121)) {
+		chipset_ver == QCA_APACHE_SOC_ID_0121 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0100 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0110 ||
+		chipset_ver == QCA_MOSELLE_SOC_ID_0120)) {
 		BTFMSLIM_INFO("SB reset needed after all ports disabled, sleeping");
 		msleep(DELAY_FOR_PORT_OPEN_MS);
 	}
