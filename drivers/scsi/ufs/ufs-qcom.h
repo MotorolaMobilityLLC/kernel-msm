0de39c7f2a9fa3229394fd42d46adc279860dfab
Zhiye Li <lizy90@motorola.com>
2024-03-28 16:12:02 +0800

    Fix compile errors
    8475/8450/7450/6450/7435 U: Upmerge
    to r2.0.1.r1_00012.0, have the compile error:
    msm-kernel/drivers/scsi/ufs/vendor/ufsfeature.h:53:2:
    error: redefinition of enumerator 'UFSHCD_EH_IN_PROGRESS'
    
    Need to remove the redefinition UFSHCD_EH_IN_PROGRESS
    
    Change-Id: I2864b1191af4f9edb622ecae32db7b3e246523e2
    Signed-off-by: Zhiye Li <lizy90@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2922235
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dongliang Yue <yuedl1@motorola.com>
    Reviewed-by: Zonghua Liu <a17671@motorola.com>
    Reviewed-by: Chuanchun Zhu <zhucc2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Liyun Zhang <zhangl19@motorola.com>
    (cherry picked from commit 69f888f2b53cd184d5db6857c6f06e4a44ebf914)

--- drivers/scsi/ufs/ufs-qcom.h
+++ drivers/scsi/ufs/ufs-qcom.h
@@ -43,16 +43,6 @@
 #if defined(CONFIG_SCSI_SKHID)
 #define IS_SKHYNIX_DEVICE(mfrid)   (0 == strncasecmp(mfrid,"SKHYNIX", sizeof("SKHYNIX")))
 #define IS_HYNIX_DEVICE(mfrid)   (0 == strncasecmp(mfrid,"HYNIX", sizeof("HYNIX")))
-
-#if defined(CONFIG_SCSI_UFS_HID)
-/* UFSHCD error handling flags */
-enum {
-	UFSHCD_EH_IN_PROGRESS = (1 << 0),		/* ufshcd.c */
-};
-
-#define ufshcd_eh_in_progress(h) \
-        ((h)->eh_flags & UFSHCD_EH_IN_PROGRESS)         /* ufshcd.c */
-#endif
 #endif
 
 /* vendor specific pre-defined parameters */
