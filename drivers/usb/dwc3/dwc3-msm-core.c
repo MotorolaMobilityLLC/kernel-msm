6121e9b3524c2006fbc6a5d051288a32d7b7ff65
shujie <hq135@motorola.com>
2024-04-01 18:48:26 +0800

    usb:  check the pm qos latency config
    Details:
    1.Add Qualcomm patch to fix the low probability of system crash caused by plug-in OTG detection
    2.fix Qcom  patch, CR:3720437
    During enabling the perf mode for the controller, it checked
    the pm qos latency is configured or not.If it is not configure
    then it return before calling cpu_latency_qos_add_request .So
    during disable perf mode if for that controiller if it is not
    configure then putting the check so that it return before the
    cpu_latency_qos_remove_request because there is no requiremen
    as pm_qos_latency is not configure.
    
    Change-Id: Id5e448c5f76ee035e171e3d3d96909bc03e110e0
    Signed-off-by: shujie <hq135@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2923891
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Ying Shen <shenying@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -6826,6 +6826,7 @@ static void msm_dwc3_perf_vote_enable(struct dwc3_msm *mdwc, bool enable)
 	} else {
 		if (!mdwc->pm_qos_latency_cfg)
 			return;
+
 		cancel_delayed_work_sync(&mdwc->perf_vote_work);
 		msm_dwc3_perf_vote_update(mdwc, false);
 		cpu_latency_qos_remove_request(&mdwc->pm_qos_req_dma);
