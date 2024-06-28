df3ffc17cf9eef6fb9be5bf2480ea05e4bf5807e
Andrei Vagin <avagin@google.com>
2024-06-28 02:10:12 +0000

    UPSTREAM: seccomp: release task filters when the task exits
    Previously, seccomp filters were released in release_task(), which
    required the process to exit and its zombie to be collected. However,
    exited threads/processes can't trigger any seccomp events, making it
    more logical to release filters upon task exits.
    
    This adjustment simplifies scenarios where a parent is tracing its child
    process. The parent process can now handle all events from a seccomp
    listening descriptor and then call wait to collect a child zombie.
    
    seccomp_filter_release takes the siglock to avoid races with
    seccomp_sync_threads. There was an idea to bypass taking the lock by
    checking PF_EXITING, but it can be set without holding siglock if
    threads have SIGNAL_GROUP_EXIT. This means it can happen concurently
    with seccomp_filter_release.
    
    This change also fixes another minor problem. Suppose that a group
    leader installs the new filter without SECCOMP_FILTER_FLAG_TSYNC, exits,
    and becomes a zombie. Without this change, SECCOMP_FILTER_FLAG_TSYNC
    from any other thread can never succeed, seccomp_can_sync_threads() will
    check a zombie leader and is_ancestor() will fail.
    
    Bug: 511308737
    Bug: 513030729
    
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrei Vagin <avagin@google.com>
    Link: https://lore.kernel.org/r/20240628021014.231976-3-avagin@google.com
    Reviewed-by: Tycho Andersen <tandersen@netflix.com>
    Signed-off-by: Kees Cook <kees@kernel.org>
    (cherry picked from commit bfafe5efa9754ebc991750da0bcca2a6694f3ed3)
    
    Change-Id: Ib5a900d55867d23c2eaff0d9aa143051553a295a
    Signed-off-by: Yujia Chen <chenyujia3@xiaomi.corp-partner.google.com>
    (cherry picked from commit 5346453405bf12d7ed6003f45dd47b71744fe1be)

--- kernel/exit.c
+++ kernel/exit.c
@@ -277,7 +277,6 @@ repeat:
 	}
 
 	write_unlock_irq(&tasklist_lock);
-	seccomp_filter_release(p);
 	proc_flush_pid(thread_pid);
 	put_pid(thread_pid);
 	release_thread(p);
@@ -832,6 +831,8 @@ void __noreturn do_exit(long code)
 
 	io_uring_files_cancel();
 	exit_signals(tsk);  /* sets PF_EXITING */
+
+	seccomp_filter_release(tsk);
 
 	trace_android_vh_exit_check(current);
 
