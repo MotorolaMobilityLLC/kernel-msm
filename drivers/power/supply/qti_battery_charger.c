06dd6174bd91fb1d6b8975a68fdcb2c1a738db6e
litong20 <litong20@lenovo.com>
2024-08-16 18:22:17 +0800

    exposing the number of thermal levels
    exposing the number of thermal levels for a board via
    POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX property which is based on
    the maximum allowed charging current and the number of thermal
    mitigation levels or thermal mitigation step specified in the device
    tree.
    
    Change-Id: Iac8ab0a3d2eb85f9423777dda4212afe3afcbb91
    Reviewed-on: https://gerrit.mot.com/3060203
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3416593
    Reviewed-by: Obi-Wan Kenobi

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1635,6 +1635,12 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_TEMP:
 		pval->intval = DIV_ROUND_CLOSEST((int)pst->prop[prop_id], 10);
 		break;
+	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT:
+		pval->intval = bcdev->curr_thermal_level;
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
+		pval->intval = bcdev->num_thermal_levels;
+		break;
 	default:
 		pval->intval = pst->prop[prop_id];
 		break;
