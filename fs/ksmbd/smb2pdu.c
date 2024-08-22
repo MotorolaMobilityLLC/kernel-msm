13ac5554fdbeee68d57b0e620290a56c0bf6e67d
ChenXiaoSong <chenxiaosong@kylinos.cn>
2024-08-22 08:20:51 +0000

    smb/server: fix potential null-ptr-deref of lease_ctx_info in smb2_open()
    commit 4e8771a3666c8f216eefd6bd2fd50121c6c437db upstream.
    
    null-ptr-deref will occur when (req_op_level == SMB2_OPLOCK_LEVEL_LEASE)
    and parse_lease_state() return NULL.
    
    Fix this by check if 'lease_ctx_info' is NULL.
    
    Additionally, remove the redundant parentheses in
    parse_durable_handle_context().
    
    Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    [ Drop the parentheses clean-up since the parentheses was introduced by
      c8efcc786146 ("ksmbd: add support for durable handles v1/v2") in v6.9
      Minor context change fixed ]
    Signed-off-by: Jianqi Ren <jianqi.ren.cn@windriver.com>
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ksmbd/smb2pdu.c
+++ fs/ksmbd/smb2pdu.c
@@ -3230,7 +3230,7 @@ int smb2_open(struct ksmbd_work *work)
 			goto err_out1;
 		}
 	} else {
-		if (req_op_level == SMB2_OPLOCK_LEVEL_LEASE) {
+		if (req_op_level == SMB2_OPLOCK_LEVEL_LEASE && lc) {
 			/*
 			 * Compare parent lease using parent key. If there is no
 			 * a lease that has same parent key, Send lease break
