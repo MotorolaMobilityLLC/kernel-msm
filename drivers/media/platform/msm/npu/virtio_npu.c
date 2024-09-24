5a5e5770583b307e93b4393037be2b18df5e89f4
gaowang <quic_gaowang@quicinc.com>
2024-09-24 16:19:44 +0800

    msm: virtio_npu: Fix use-after-free issue in virt_npu_map_buf
    address the security CR of virtio_npu driver.
    
    Signed-off-by: gaowang <quic_gaowang@quicinc.com>
    Signed-off-by: Jilai Wang <quic_jilaiw@quicinc.com>
    
    Mot-CRs-fixed: (CR)
    CVE-fixed: CVE-2025-21458
    CRs Fixed: QC-CR#3928169
    Signed-off-by: Sipra Patel <siprap@motorola.com>
    
    Change-Id: Iefc5630931cd67555e881c24be0f0ece603d28d2
    Reviewed-on: https://gerrit.mot.com/3453900
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit c1b8f89783af445ef8b957ebff744813805f735a)

--- drivers/media/platform/msm/npu/virtio_npu.c
+++ drivers/media/platform/msm/npu/virtio_npu.c
@@ -898,10 +898,12 @@ static int32_t virt_npu_map_buf(struct npu_client *client,
 	struct npu_ion_buf *ion_buf = NULL;
 	int rc = 0;
 
+	mutex_lock(&npu_dev->lock);
 	ion_buf = npu_alloc_npu_ion_buffer(client, buf_hdl, size);
 	if (!ion_buf) {
 		NPU_ERR("fail to alloc npu_ion_buffer\n");
 		rc = -ENOMEM;
+		mutex_unlock(&npu_dev->lock);
 		return rc;
 	}
 
@@ -910,7 +912,8 @@ static int32_t virt_npu_map_buf(struct npu_client *client,
 		NPU_ERR("dma_buf_get failed %d\n", ion_buf->fd);
 		rc = -ENOMEM;
 		ion_buf->dma_buf = NULL;
-		goto map_end;
+		mutex_unlock(&npu_dev->lock);
+		return rc;
 	}
 
 	ion_buf->attachment = dma_buf_attach(ion_buf->dma_buf,
@@ -919,7 +922,8 @@ static int32_t virt_npu_map_buf(struct npu_client *client,
 		NPU_ERR("failed to map attachment\n");
 		rc = -ENOMEM;
 		ion_buf->attachment = NULL;
-		goto map_end;
+		mutex_unlock(&npu_dev->lock);
+		return rc;
 	}
 
 	ion_buf->attachment->dma_map_attrs = DMA_ATTR_SKIP_CPU_SYNC;
@@ -930,15 +934,16 @@ static int32_t virt_npu_map_buf(struct npu_client *client,
 		NPU_ERR("npu dma_buf_map_attachment failed\n");
 		rc = -ENOMEM;
 		ion_buf->table = NULL;
-		goto map_end;
+		mutex_unlock(&npu_dev->lock);
+		return rc;
 	}
 
 	ion_buf->size = ion_buf->dma_buf->size;
+	mutex_unlock(&npu_dev->lock);
 
 	rc = virt_npu_mmap(client, 0, ion_buf->table->sgl,
 		ion_buf->table->nents, size, &ion_buf->iova);
 
-map_end:
 	if (rc)
 		virt_npu_unmap_buf(client, buf_hdl, 0);
 	else
