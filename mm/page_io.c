e55b8361d2d0c1afcf1b49ae9bcc8ecb665cc054
Qun-Wei Lin <qun-wei.lin@mediatek.com>
2024-10-01 10:12:53 +0800

    FROMLIST: mm: Split SWP_SYNCHRONOUS_IO into separate read and write flags
    This patch splits the SWP_SYNCHRONOUS_IO flag into __SWP_READ_SYNCHRONOUS_IO
    and __SWP_WRITE_SYNCHRONOUS_IO.
    
    These changes are motivated by the need to better accommodate certain
    swap devices that support synchronous read operations but asynchronous write
    operations.
    
    The existing SWP_SYNCHRONOUS_IO flag is not sufficient for these devices,
    as they enforce synchronous behavior for both read and write operations.
    
    Bug: 368512254
    Bug: 491161822
    Bug: 504423387
    
    Change-Id: I778053e7f7d7d8f0Montanad66bc1acf44b98ab5c636
    Signed-off-by: Qun-Wei Lin <qun-wei.lin@mediatek.com>
    Link: https://lore.kernel.org/lkml/20241011091133.28173-1-qun-wei.lin@mediatek.com/

--- mm/page_io.c
+++ mm/page_io.c
@@ -380,14 +380,14 @@ void __swap_writepage(struct page *page, struct writeback_control *wbc)
 	VM_BUG_ON_PAGE(!PageSwapCache(page), page);
 	/*
 	 * ->flags can be updated non-atomicially (scan_swap_map_slots),
-	 * but that will never affect SWP_FS_OPS, so the data_race
+	 * but that will never affect __SWP_WRITE_SYNCHRONOUS_IO, so the data_race
 	 * is safe.
 	 */
 	sis_flags = data_race(sis->flags);
 	trace_android_vh_swap_writepage(&sis_flags, page);
 	if (sis_flags & SWP_FS_OPS)
 		swap_writepage_fs(page, wbc);
-	else if (sis_flags & SWP_SYNCHRONOUS_IO)
+	else if (sis_flags & __SWP_WRITE_SYNCHRONOUS_IO)
 		swap_writepage_bdev_sync(page, wbc, sis);
 	else
 		swap_writepage_bdev_async(page, wbc, sis);
