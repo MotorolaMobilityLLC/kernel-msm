ddc8dfabc36f7c85b0a3330c450fbc063c19c099
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2024-10-08 21:08:18 -0700

    ANDROID: GKI: Add padding after init_task structure
    To reduce static memory allocated in the task_struct to accommodate
    vendor data, the move to a dynamically sized task_struct is being made.
    As part of this effort, add padding after the init_task structure.
    This has to be done because init_task is statically allocated and hence
    will not benefit from the dynamic addition of size to all task_structs.
    
    Bug: 233921394
    Bug: 504423387
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    (cherry picked from 1827ee5e5a2de323359c04153e268a7288061eda)
    Bug: 489588860
    Signed-off-by: Jihan LIN <linjihan@xiaomi.com>
    Change-Id: I737f951d57a5c19ff6474d4243a22f5f9cfbfa8b

--- init/init_task.c
+++ init/init_task.c
@@ -218,6 +218,9 @@ struct task_struct init_task
 };
 EXPORT_SYMBOL(init_task);
 
+u64 vendor_data_pad[CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX / sizeof(u64)];
+EXPORT_SYMBOL_GPL(vendor_data_pad);
+
 /*
  * Initial thread structure. Alignment of this is handled by a special
  * linker map entry.
