05458c4874e99fe3734f413defb6b2508f8671d2
Jianan Huang <huangjianan@xiaomi.com>
2024-10-15 14:47:56 +0800

    ANDROID: mm: add vendor hook to trace shrink_node
    This is to adjust parameters between different memcgs to achieve
    more accurate memory reclamation.
    
    Bug: 373540729
    Change-Id: Ifb97a144c057555c5f9181f357fa146f9509be3e
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    (cherry picked from commit 9d6f981a89e6e289f114270e2f1738b2b6fdd2ab)

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -86,6 +86,9 @@ DECLARE_HOOK(android_vh_page_referenced_check_bypass,
 DECLARE_HOOK(android_vh_folio_referenced_check_bypass,
 	TP_PROTO(struct folio *folio, s8 priority, unsigned long nr_to_scan, int lru, bool *bypass),
 	TP_ARGS(folio, priority, nr_to_scan, lru, bypass));
+DECLARE_HOOK(android_vh_shrink_node,
+	TP_PROTO(pg_data_t *pgdat, struct mem_cgroup *memcg),
+	TP_ARGS(pgdat, memcg));
 DECLARE_HOOK(android_vh_should_memcg_bypass,
 	TP_PROTO(struct mem_cgroup *memcg, int priority, bool *bypass),
 	TP_ARGS(memcg, priority, bypass));
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6606,6 +6606,7 @@ static void shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 	struct lruvec *target_lruvec;
 	bool reclaimable = false;
 
+	trace_android_vh_shrink_node(pgdat, sc->target_mem_cgroup);
 	if (lru_gen_enabled() && global_reclaim(sc)) {
 		lru_gen_shrink_node(pgdat, sc);
 		return;
