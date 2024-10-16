a89ab2fe4959c0a5676dbb6e2c2bb5d1af97f32e
Nathan Chancellor <nathan@kernel.org>
2024-10-16 09:12:52 -0700

    ANDROID: GKI: Put vendor_data_pad behind CONFIG_GKI_DYNAMIC_TASK_STRUCT_SIZE
    vendor_data_pad uses CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX, which is
    only defined when CONFIG_GKI_DYNAMIC_TASK_STRUCT_SIZE is enabled,
    resulting in build failures when it is not:
    
      In file included from arch/arm/kernel/asm-offsets.c:12:
      In file included from include/linux/mm.h:1120:
      In file included from include/linux/huge_mm.h:8:
      In file included from include/linux/fs.h:33:
      In file included from include/linux/percpu-rwsem.h:7:
      In file included from include/linux/rcuwait.h:6:
      In file included from include/linux/sched/signal.h:9:
      include/linux/sched/task.h:59:28: error: use of undeclared identifier 'CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX'
         59 | extern u64 vendor_data_pad[CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX / sizeof(u64)];
            |                            ^
      1 error generated.
    
    Place all instances of vendor_data_pad behind
    CONFIG_GKI_DYNAMIC_TASK_STRUCT_SIZE to address the build failures.
    
    Fixes: 1827ee5e5a2de3 ("ANDROID: GKI: Add padding after init_task structure")
    Fixes: 5e9a8cb7144f7d ("ANDROID: GKI: Add to task_struct size via cmdline")
    Change-Id: I7f5dcdd0aaffc5af3063a1b2ca4c1a3517043378
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    (cherry picked from commit: 2282ad09eeb92f2dd89ab98e413b0b16aff8ff35)
    Bug: 489588860
    Bug: 504423387
    Signed-off-by: Jihan LIN <linjihan@xiaomi.com>

--- init/init_task.c
+++ init/init_task.c
@@ -218,8 +218,10 @@ struct task_struct init_task
 };
 EXPORT_SYMBOL(init_task);
 
+#ifdef CONFIG_GKI_DYNAMIC_TASK_STRUCT_SIZE
 u64 vendor_data_pad[CONFIG_GKI_TASK_STRUCT_VENDOR_SIZE_MAX / sizeof(u64)];
 EXPORT_SYMBOL_GPL(vendor_data_pad);
+#endif
 
 /*
  * Initial thread structure. Alignment of this is handled by a special
