19a801015f6d317ae3c44786c42a74e90d161e24
Greg Kroah-Hartman <gregkh@google.com>
2024-11-10 10:53:49 +0000

    Revert "cgroup: Make operations on the cgroup root_list RCU safe"
    This reverts commit de77545c72c428484fa16384dfa7d17f820e6d0c which is
    commit d23b5c577715892c87533b13923306acc6243f93 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ice50054375d4c4358d0b4ef95b2201b72154fd73
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -1335,7 +1335,7 @@ static void cgroup_exit_root_id(struct cgroup_root *root)
 
 void cgroup_free_root(struct cgroup_root *root)
 {
-	kfree_rcu(root, rcu);
+	kfree(root);
 }
 
 static void cgroup_destroy_root(struct cgroup_root *root)
@@ -1368,7 +1368,7 @@ static void cgroup_destroy_root(struct cgroup_root *root)
 	spin_unlock_irq(&css_set_lock);
 
 	if (!list_empty(&root->root_list)) {
-		list_del_rcu(&root->root_list);
+		list_del(&root->root_list);
 		cgroup_root_count--;
 	}
 
@@ -1414,6 +1414,7 @@ current_cgns_cgroup_from_root(struct cgroup_root *root)
 	}
 	rcu_read_unlock();
 
+	BUG_ON(!res);
 	return res;
 }
 
@@ -1423,6 +1424,7 @@ static struct cgroup *cset_cgroup_from_root(struct css_set *cset,
 {
 	struct cgroup *res = NULL;
 
+	lockdep_assert_held(&cgroup_mutex);
 	lockdep_assert_held(&css_set_lock);
 
 	if (cset == &init_css_set) {
@@ -1448,9 +1450,7 @@ static struct cgroup *cset_cgroup_from_root(struct css_set *cset,
 
 /*
  * Return the cgroup for "task" from the given hierarchy. Must be
- * called with css_set_lock held to prevent task's groups from being modified.
- * Must be called with either cgroup_mutex or rcu read lock to prevent the
- * cgroup root from being destroyed.
+ * called with cgroup_mutex and css_set_lock held.
  */
 struct cgroup *task_cgroup_from_root(struct task_struct *task,
 				     struct cgroup_root *root)
@@ -1989,7 +1989,7 @@ void init_cgroup_root(struct cgroup_fs_context *ctx)
 	struct cgroup_root *root = ctx->root;
 	struct cgroup *cgrp = &root->cgrp;
 
-	INIT_LIST_HEAD_RCU(&root->root_list);
+	INIT_LIST_HEAD(&root->root_list);
 	atomic_set(&root->nr_cgrps, 1);
 	cgrp->root = root;
 	init_cgroup_housekeeping(cgrp);
@@ -2071,7 +2071,7 @@ int cgroup_setup_root(struct cgroup_root *root, u16 ss_mask)
 	 * care of subsystems' refcounts, which are explicitly dropped in
 	 * the failure exit path.
 	 */
-	list_add_rcu(&root->root_list, &cgroup_roots);
+	list_add(&root->root_list, &cgroup_roots);
 	cgroup_root_count++;
 
 	/*
