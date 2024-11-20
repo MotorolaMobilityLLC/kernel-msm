660ba2395cc45a69d11224ae9fc867a26810f579
David Wei <dw@davidwei.uk>
2024-11-20 14:14:51 -0800

    BACKPORT: io_uring: add io_local_work_pending()
    In preparation for adding a new llist of tw to retry due to hitting the
    tw limit, add a helper io_local_work_pending(). This function returns
    true if there is any local tw pending. For now it only checks
    ctx->work_llist.
    
    Bug: 297114355
    (cherry picked from commit 40cfe553240b32333b42652370ef5232e6ac59e1)
    [akailash@: Fix merge conflict in io_has_work function ]
    Signed-off-by: David Wei <dw@davidwei.uk>
    Reviewed-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/20241120221452.3762588-2-dw@davidwei.uk
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: I701e7df01072051be1c2f0967f056cc8a2923e99

--- io_uring/io_uring.h
+++ io_uring/io_uring.h
@@ -313,9 +313,14 @@ static inline int io_run_task_work(void)
 	return 0;
 }
 
+static inline bool io_local_work_pending(struct io_ring_ctx *ctx)
+{
+	return !llist_empty(&ctx->work_llist);
+}
+
 static inline bool io_task_work_pending(struct io_ring_ctx *ctx)
 {
-	return task_work_pending(current) || !llist_empty(&ctx->work_llist);
+	return task_work_pending(current) || io_local_work_pending(ctx);
 }
 
 static inline void io_tw_lock(struct io_ring_ctx *ctx, struct io_tw_state *ts)
