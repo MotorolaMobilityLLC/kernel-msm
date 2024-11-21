0ca328d8ce261fdeafbdc70f46cf498e7b765223
Greg Kroah-Hartman <gregkh@google.com>
2024-11-21 01:27:21 +0000

    Revert "net: Add l3mdev index to flow struct and avoid oif reset for port devices"
    This reverts commit 5bcde9b4302120d79370448fdb9356b7220b5e9f which is
    commit 40867d74c374b235e14d839f3a77f26684feefe5 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ib6817b2009874f281e1c810e1a88fafe31473a8a
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -2285,7 +2285,6 @@ static int ip_route_input_slow(struct sk_buff *skb, __be32 daddr, __be32 saddr,
 	/*
 	 *	Now we are ready to route packet.
 	 */
-	fl4.flowi4_l3mdev = 0;
 	fl4.flowi4_oif = 0;
 	fl4.flowi4_iif = dev->ifindex;
 	fl4.flowi4_mark = skb->mark;
@@ -2762,7 +2761,8 @@ struct rtable *ip_route_output_key_hash_rcu(struct net *net, struct flowi4 *fl4,
 		res->fi = NULL;
 		res->table = NULL;
 		if (fl4->flowi4_oif &&
-		    (ipv4_is_multicast(fl4->daddr) || !fl4->flowi4_l3mdev)) {
+		    (ipv4_is_multicast(fl4->daddr) ||
+		    !netif_index_is_l3_master(net, fl4->flowi4_oif))) {
 			/* Apparently, routing tables are wrong. Assume,
 			 * that the destination is on link.
 			 *
