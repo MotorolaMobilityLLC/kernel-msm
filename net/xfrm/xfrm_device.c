c88098018cce9d5877a2ba564542c8ca3a33925b
Greg Kroah-Hartman <gregkh@google.com>
2025-01-03 15:21:18 +0000

    Revert "xfrm: rename xfrm_state_offload struct to allow reuse"
    This reverts commit e4426baa9aa3a485c4fea4909b34c1d562dccba7 which is
    commit 87e0a94e60ea2e29be9dec6bc146fbc9861a4055 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I6e16542895bf05cd0b1b29aabf262f07cfac4f88
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -225,7 +225,7 @@ int xfrm_dev_state_add(struct net *net, struct xfrm_state *x,
 	int err;
 	struct dst_entry *dst;
 	struct net_device *dev;
-	struct xfrm_dev_offload *xso = &x->xso;
+	struct xfrm_state_offload *xso = &x->xso;
 	xfrm_address_t *saddr;
 	xfrm_address_t *daddr;
 
