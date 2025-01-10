51102da2d7cce21d9fc40dd74c18ad467c783f54
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-01-10 15:54:22 +0200

    media: i2c: ccs: Set the device's runtime PM status correctly in probe
    commit 80704d14f1bd3628f578510e0a88b66824990ef6 upstream.
    
    Set the device's runtime PM status to suspended in probe error paths where
    it was previously set to active.
    
    Fixes: 9447082ae666 ("[media] smiapp: Implement power-on and power-off sequences without runtime PM")
    Cc: stable@vger.kernel.org # for >= v5.15
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/i2c/ccs/ccs-core.c
+++ drivers/media/i2c/ccs/ccs-core.c
@@ -3651,6 +3651,7 @@ static int ccs_probe(struct i2c_client *client)
 out_disable_runtime_pm:
 	pm_runtime_put_noidle(&client->dev);
 	pm_runtime_disable(&client->dev);
+	pm_runtime_set_suspended(&client->dev);
 
 out_media_entity_cleanup:
 	media_entity_cleanup(&sensor->src->sd.entity);
