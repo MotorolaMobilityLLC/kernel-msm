fbdaa768660eaeeb88a29ad96910362f17a4b272
Alison Venancio <alisonv@motorola.com>
2025-01-14 13:53:40 -0400

    - bronco - Remove debug logs
    Removed all debug logs and stopship flag from the code
    without change the logic introduced on (CR)
    
    Change-Id: Ie1d21abfdc57d97fa83790d0aedd32025d8cf5d7
    Reviewed-on: https://gerrit.mot.com/3204027
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hugo Cuffa <hcuffa@motorola.com>
    Reviewed-by: Levy Gabriel <levy@motorola.com>
    Reviewed-by: Vitor Hugo Vasconcelos de Oliveira <vhdo@motorola.com>
    Reviewed-by: Carlos Pinho <cpinho@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit f3e6724912937c7f784b3be4e480b9fb9bb0c3d2)
    Reviewed-on: https://gerrit.mot.com/3707159
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Ravindranath Sattaru <sravindr@motorola.com>

--- drivers/thermal/qcom/qcom-spmi-temp-alarm.c
+++ drivers/thermal/qcom/qcom-spmi-temp-alarm.c
@@ -275,13 +275,9 @@ static int qpnp_tm_get_temp(void *data, int *temp)
 		ret = iio_read_channel_processed(chip->adc, &mili_celsius);
 		if (ret < 0)
 			return ret;
-		/* MMI_STOPSHIP <debug abnormal QC sensor> : tsens report abnormal value. */
-		if (mili_celsius / 1000 > 145) {
-			pr_info("%s: %s last=%d, temp=%d, ret=%d\n", __func__,
-				chip->tz_dev->type, chip->temp, mili_celsius, ret);
-		} else {
+
+		if (mili_celsius / 1000 <= 145)
 			chip->temp = mili_celsius;
-		}
 	}
 
 	*temp = chip->temp;
