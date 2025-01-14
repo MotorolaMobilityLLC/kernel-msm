6e81024d46b65581bb0cf3d331cdd289f31542ae
Greg Kroah-Hartman <gregkh@google.com>
2025-01-14 17:34:52 +0000

    Revert "sched/deadline: Move bandwidth accounting into {en,de}queue_dl_entity"
    This reverts commit 01ecd269755e5dc2b556897b7cf45e2eca300f43 which is
    commit 2f7a0f58948d8231236e2facecc500f1930fb996 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ie724cbe53de5cdd50b845e692322d6a193a344d0
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2242,10 +2242,6 @@ extern const u32		sched_prio_to_wmult[40];
  * MOVE - paired with SAVE/RESTORE, explicitly does not preserve the location
  *        in the runqueue.
  *
- * NOCLOCK - skip the update_rq_clock() (avoids double updates)
- *
- * MIGRATION - p->on_rq == TASK_ON_RQ_MIGRATING (used for DEADLINE)
- *
  * ENQUEUE_HEAD      - place at front of runqueue (tail if not specified)
  * ENQUEUE_REPLENISH - CBS (replenish runtime and postpone deadline)
  * ENQUEUE_MIGRATED  - the task was migrated during wakeup
@@ -2256,7 +2252,6 @@ extern const u32		sched_prio_to_wmult[40];
 #define DEQUEUE_SAVE		0x02 /* Matches ENQUEUE_RESTORE */
 #define DEQUEUE_MOVE		0x04 /* Matches ENQUEUE_MOVE */
 #define DEQUEUE_NOCLOCK		0x08 /* Matches ENQUEUE_NOCLOCK */
-#define DEQUEUE_MIGRATING	0x100 /* Matches ENQUEUE_MIGRATING */
 
 #define ENQUEUE_WAKEUP		0x01
 #define ENQUEUE_RESTORE		0x02
@@ -2271,7 +2266,6 @@ extern const u32		sched_prio_to_wmult[40];
 #define ENQUEUE_MIGRATED	0x00
 #endif
 #define ENQUEUE_INITIAL		0x80
-#define ENQUEUE_MIGRATING	0x100
 
 #define ENQUEUE_WAKEUP_SYNC	0x80
 
