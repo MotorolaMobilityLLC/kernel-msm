a7305641bf54b04014c044bb22a02e6bf119ff5e
Quentin Perret <qperret@google.com>
2025-01-30 13:35:41 +0000

    ANDROID: KVM: arm64: Configure size of pKVM linear map on the cmdline
    We currently reserve enough memory for pKVM to construct a stage-1
    page-table that maps all of memory at page granularity. However, in
    practice the amount of memory actually mapped into EL2 stage-1 is quite
    small as it mainly consists of the hypervisor itself and guest metadata
    (kvm, kvm_vcpu and related, plus stage-2 page-tables).
    
    To avoid the waste, introduce a new kernel cmdline parameter that
    defines how much memory can be mapped in total into the hypervisor
    linear map at any point in time. This value should be set conservatively,
    as allocation failures in the hypervisor are l(CR) to be fatal.
    
    Bug: 273700914
    Bug: 239674049
    Change-Id: I979fa07fa14566793f990d7e1ad9c8c3644cba9a
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -559,11 +559,16 @@ static inline unsigned long __hyp_pgtable_moveable_regs_pages(void)
 	return res;
 }
 
+extern u64 kvm_nvhe_sym(hyp_lm_size_mb);
+
 static inline unsigned long hyp_s1_pgtable_pages(void)
 {
 	unsigned long res;
 
-	res = __hyp_pgtable_moveable_regs_pages();
+	if (!kvm_nvhe_sym(hyp_lm_size_mb))
+		res = __hyp_pgtable_moveable_regs_pages();
+	else
+		res = __hyp_pgtable_max_pages(kvm_nvhe_sym(hyp_lm_size_mb) * SZ_1M / PAGE_SIZE);
 
 	/* Allow 1 GiB for private mappings */
 	res += __hyp_pgtable_max_pages(SZ_1G >> PAGE_SHIFT);
