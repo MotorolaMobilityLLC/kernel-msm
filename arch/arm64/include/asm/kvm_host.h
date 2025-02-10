0e8d89fd9f05232aaca733ba5c322d0bc4b3aec1
Mark Rutland <mark.rutland@arm.com>
2025-02-10 19:52:21 +0000

    BACKPORT: KVM: arm64: Remove VHE host restore of CPACR_EL1.ZEN
    [ Upstream commit 459f059be702056d91537b99a129994aa6ccdd35 ]
    
    When KVM is in VHE mode, the host kernel tries to save and restore the
    configuration of CPACR_EL1.ZEN (i.e. CPTR_EL2.ZEN when HCR_EL2.E2H=1)
    across kvm_arch_vcpu_load_fp() and kvm_arch_vcpu_put_fp(), since the
    configuration may be clobbered by hyp when running a vCPU. This logic is
    currently redundant.
    
    The VHE hyp code unconditionally configures CPTR_EL2.ZEN to 0b01 when
    returning to the host, permitting host kernel usage of SVE.
    
    Now that the host eagerly saves and unbinds its own FPSIMD/SVE/SME
    state, there's no need to save/restore the state of the EL0 SVE trap.
    The kernel can safely save/restore state without trapping, as described
    above, and will restore userspace state (including trap controls) before
    returning to userspace.
    
    Remove the redundant logic.
    
    Bug: 411040189
    Change-Id: I43bf5587223aae54caf9389eb3de17f155043d96
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Tested-by: Mark Brown <broonie@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Fuad Tabba <tabba@google.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20250210195226.1215254-4-mark.rutland@arm.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    [Rework for refactoring of where the flags are stored -- broonie]
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -849,8 +849,6 @@ struct kvm_vcpu_arch {
 /* pKVM host vcpu state is dirty, needs resync (nVHE-only) */
 #define PKVM_HOST_STATE_DIRTY	__vcpu_single_flag(iflags, BIT(7))
 
-/* SVE enabled for host EL0 */
-#define HOST_SVE_ENABLED	__vcpu_single_flag(sflags, BIT(0))
 /* SME enabled for EL0 */
 #define HOST_SME_ENABLED	__vcpu_single_flag(sflags, BIT(1))
 /* Physical CPU not in supported_cpus */
