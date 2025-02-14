5f5832ce3dd0faa5222af419501e46978934fc50
Will Deacon <will@kernel.org>
2025-02-14 13:37:24 +0000

    FROMLIST: BACKPORT: KVM: arm64: Fix tcr_el2 initialisation in hVHE mode
    When not running in VHE mode, cpu_prepare_hyp_mode() computes the value
    of TCR_EL2 using the host's TCR_EL1 settings as a starting point. For
    nVHE, this amounts to masking out everything apart from the TG0, SH0,
    ORGN0, IRGN0 and T0SZ fields before setting the RES1 bits, shifting the
    IPS field down to the PS field and setting DS if LPA2 is enabled.
    
    Unfortunately, for hVHE, things go slightly wonky: EPD1 is correctly set
    to disable walks via TTBR1_EL2 but then the T1SZ and IPS fields are
    corrupted when we mistakenly attempt to initialise the PS and DS fields
    in their E2H=0 positions. Furthermore, many fields are retained from
    TCR_EL1 which should not be propagated to TCR_EL2. Notably, this means
    we can end up with A1 set despite not initialising TTBR1_EL2 at all.
    This has been shown to cause unexpected translation faults at EL2 with
    pKVM due to TLB invalidation not taking effect when running with a
    non-zero ASID.
    
    Fix the TCR_EL2 initialisation code to set PS and DS only when E2H=0,
    masking out HD, HA and A1 when E2H=1.
    
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Fixes: ad744e8cb346 ("arm64: Allow arm64_sw.hvhe on command line")
    Signed-off-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20250214133724.13179-1-will@kernel.org
    Bug: 395497376
    Signed-off-by: Will Deacon <willdeacon@google.com>
    [willdeacon@: Drop LPA2 and PS parts (PS is handled in hyp-init.S)]
    Change-Id: I9e7bf8917dea73d97051e1d35374ab00f6477957

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1859,12 +1859,12 @@ static void __init cpu_prepare_hyp_mode(int cpu, u32 hyp_va_bits)
 
 	tcr = read_sysreg(tcr_el1);
 	if (cpus_have_final_cap(ARM64_KVM_HVHE)) {
+		tcr &= ~(TCR_HD | TCR_HA | TCR_A1 | TCR_T0SZ_MASK);
 		tcr |= TCR_EPD1_MASK;
 	} else {
 		tcr &= TCR_EL2_MASK;
 		tcr |= TCR_EL2_RES1;
 	}
-	tcr &= ~TCR_T0SZ_MASK;
 	tcr |= TCR_T0SZ(hyp_va_bits);
 	params->tcr_el2 = tcr;
 
