d57b8f6c1b8964753f53cca2391caf183479b45a
Bruno Oliveira <brunosoe@motorola.com>
2025-03-05 13:37:14 -0300

    Fix EGL throttling at IO intensive apps
    Some devices had some freezes at gpu intensive apps l(CR) youtube and
        camera. Found out that BufferQueueProducer fence was "Throttling
        EGL Production". This is a known issue where IO smmu
        pm_runtime_set_autosuspend_delay is the root cause. At SR
        07705448 qcom agreed to reduce this delay on arm-smmu to 8 ms
        which is lower than the time needed to render a frame at 120 fps.
    After this change the "Throttling EGL Production" message is gone and
        there is no freeze at those apps anymore.
    
    Change-Id: I17e3c69178507ab2147aa4e2f4ef0ace62707d88
    Reviewed-on: https://gerrit.mot.com/3300470
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Rafael Ortolan <rafones@motorola.com>
    Reviewed-by: Gilberto Gambugge Neto <gambugge@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit f2a822978a0d4e22750aa5628a21bb20093970d4)
    Reviewed-on: https://gerrit.mot.com/3710778
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Ravindranath Sattaru <sravindr@motorola.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -2402,7 +2402,7 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 	 * to 5-10sec worth of reprogramming the context bank, while
 	 * the system appears to be locked up to the user.
 	 */
-	pm_runtime_set_autosuspend_delay(smmu->dev, 20);
+	pm_runtime_set_autosuspend_delay(smmu->dev, 8);
 	pm_runtime_use_autosuspend(smmu->dev);
 
 out_power_off:
