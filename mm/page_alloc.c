591c9630c152d4d7638c59e4d81414479a559128
Martin Liu <liumartin@google.com>
2025-03-08 03:46:00 +0000

    BACKPORT: FROMGIT: mm/page_alloc: add trace event for per-zone watermark setup
    Patch series "Add tracepoints for lowmem reserves, watermarks and
    totalreserve_pages", v2.
    
    This patchset introduces tracepoints to track changes in the lowmem
    reserves, watermarks and totalreserve_pages. This helps to track
    the exact timing of such changes and understand their relation to
    reclaim activities.
    
    The tracepoints added are:
    
    mm_setup_per_zone_lowmem_reserve
    mm_setup_per_zone_wmarks
    mm_calculate_totalreserve_pagesi
    
    This patch (of 3):
    
    This commit introduces the `mm_setup_per_zone_wmarks` trace event,
    which provides detailed insights into the kernel's per-zone watermark
    configuration, offering precise timing and the ability to correlate
    watermark changes with specific kernel events.
    
    While `/proc/zoneinfo` provides some information about zone watermarks,
    this trace event offers:
    
    1. The ability to link watermark changes to specific kernel events and
       logic.
    
    2. The ability to capture rapid or short-lived changes in watermarks
       that may be missed by user-space polling
    
    3. Diagnosing unexpected kswapd activity or excessive direct reclaim
       triggered by rapidly changing watermarks.
    
    Link: https://lkml.kernel.org/r/20250308034606.2036033-1-liumartin@google.com
    Link: https://lkml.kernel.org/r/20250308034606.2036033-2-liumartin@google.com
    Signed-off-by: Martin Liu <liumartin@google.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Martin Liu <liumartin@google.com>
    Cc: "Masami Hiramatsu (Google)" <mhiramat@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Bug: 396115949
    (cherry picked from commit 8c02048d1c6126527f15752a5e0849dc49cefeeb
     https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git mm-unstable)
    Change-Id: I7e326e78542abb6fa5f3ccbe5d61a59f42d7cf2f
    Signed-off-by: Martin Liu <liumartin@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9070,6 +9070,7 @@ static void __setup_per_zone_wmarks(void)
 		zone->_watermark[WMARK_LOW]  = min_wmark_pages(zone) + tmp;
 		zone->_watermark[WMARK_HIGH] = low_wmark_pages(zone) + tmp;
 		zone->_watermark[WMARK_PROMO] = high_wmark_pages(zone) + tmp;
+		trace_mm_setup_per_zone_wmarks(zone);
 
 		spin_unlock_irqrestore(&zone->lock, flags);
 	}
