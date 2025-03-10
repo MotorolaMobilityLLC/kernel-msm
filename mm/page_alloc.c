796911eaf2f4e859d528863a80a905464ebda3c0
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2025-03-10 10:28:55 +0200

    mm/page_alloc: fix memory accept before watermarks gets initialized
    commit 800f1059c99e2b39899bdc67a7593a7bea6375d8 upstream.
    
    Watermarks are initialized during the postcore initcall.  Until then, all
    watermarks are set to zero.  This causes cond_accept_memory() to
    incorrectly skip memory acceptance because a watermark of 0 is always met.
    
    This can lead to a premature OOM on boot.
    
    To ensure progress, accept one MAX_ORDER page if the watermark is zero.
    
    Link: https://lkml.kernel.org/r/20250310082855.2587122-1-kirill.shutemov@linux.intel.com
    Fixes: dcdfdd40fa82 ("mm: Add support for unaccepted memory")
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Tested-by: Farrah Chen <farrah.chen@intel.com>
    Reported-by: Farrah Chen <farrah.chen@intel.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Pankaj Gupta <pankaj.gupta@amd.com>
    Cc: Ashish Kalra <ashish.kalra@amd.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: "Edgecombe, Rick P" <rick.p.edgecombe@intel.com>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: "M(CR) Rapoport (IBM)" <rppt@kernel.org>
    Cc: Thomas Lendacky <thomas.lendacky@amd.com>
    Cc: <stable@vger.kernel.org>	[6.5+]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6653,7 +6653,7 @@ static bool try_to_accept_memory_one(struct zone *zone)
 
 static bool cond_accept_memory(struct zone *zone, unsigned int order)
 {
-	long to_accept;
+	long to_accept, wmark;
 	bool ret = false;
 
 	if (!has_unaccepted_memory())
@@ -6662,8 +6662,18 @@ static bool cond_accept_memory(struct zone *zone, unsigned int order)
 	if (list_empty(&zone->unaccepted_pages))
 		return false;
 
+	wmark = high_wmark_pages(zone);
+
+	/*
+	 * Watermarks have not been initialized yet.
+	 *
+	 * Accepting one MAX_ORDER page to ensure progress.
+	 */
+	if (!wmark)
+		return try_to_accept_memory_one(zone);
+
 	/* How much to accept to get to high watermark? */
-	to_accept = high_wmark_pages(zone) -
+	to_accept = wmark -
 		    (zone_page_state(zone, NR_FREE_PAGES) -
 		    __zone_watermark_unusable_free(zone, order, 0) -
 		    zone_page_state(zone, NR_UNACCEPTED));
