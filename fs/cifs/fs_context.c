ebf2776b3bd2480799a9a2a01a2dac30ae03d54a
Murad Masimov <m.masimov@mt-integration.ru>
2025-03-11 17:22:03 +0300

    cifs: Fix integer overflow while processing acregmax mount option
    [ Upstream commit 7489161b1852390b4413d57f2457cd40b34da6cc ]
    
    User-provided mount parameter acregmax of type u32 is intended to have
    an upper limit, but before it is validated, the value is converted from
    seconds to jiffies which can lead to an integer overflow.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Fixes: 5780464614f6 ("cifs: Add new parameter "acregmax" for distinct file and directory metadata timeout")
    Signed-off-by: Murad Masimov <m.masimov@mt-integration.ru>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/cifs/fs_context.c
+++ fs/cifs/fs_context.c
@@ -1055,11 +1055,11 @@ static int smb3_fs_context_parse_param(struct fs_context *fc,
 		ctx->got_wsize = true;
 		break;
 	case Opt_acregmax:
-		ctx->acregmax = HZ * result.uint_32;
-		if (ctx->acregmax > CIFS_MAX_ACTIMEO) {
+		if (result.uint_32 > CIFS_MAX_ACTIMEO / HZ) {
 			cifs_errorf(fc, "acregmax too large\n");
 			goto cifs_parse_mount_err;
 		}
+		ctx->acregmax = HZ * result.uint_32;
 		break;
 	case Opt_acdirmax:
 		ctx->acdirmax = HZ * result.uint_32;
