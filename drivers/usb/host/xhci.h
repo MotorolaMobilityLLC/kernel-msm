9bc9bad782445399e4f87716060005c6a465ba8e
Greg Kroah-Hartman <gregkh@google.com>
2025-03-14 14:20:19 +0000

    Revert "usb: xhci: Add timeout argument in address_device USB HCD callback"
    This reverts commit 5fed21083b4517a5792eec7d3b63a41222860806 which is
    commit a769154c7cac037914ba375ae88aae55b2c853e0 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I57882437946b468a9e81e1dc8e6467a66f3306ff
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -816,8 +816,6 @@ struct xhci_command {
 	struct completion		*completion;
 	union xhci_trb			*command_trb;
 	struct list_head		cmd_list;
-	/* xHCI command response timeout in milliseconds */
-	unsigned int			timeout_ms;
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
@@ -1574,11 +1572,8 @@ struct xhci_td {
 	unsigned int		num_trbs;
 };
 
-/*
- * xHCI command default timeout value in milliseconds.
- * USB 3.2 spec, section 9.2.6.1
- */
-#define XHCI_CMD_DEFAULT_TIMEOUT	5000
+/* xHCI command default timeout value */
+#define XHCI_CMD_DEFAULT_TIMEOUT	(5 * HZ)
 
 /* command descriptor */
 struct xhci_cd {
