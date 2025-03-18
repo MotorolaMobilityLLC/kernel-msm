2179e01663bcc5076c5ddc98e16e42d202f5d4a3
Florian Westphal <fw@strlen.de>
2025-03-18 23:15:22 +0100

    netfilter: nf_tables: allow clone callbacks to sleep
    commit fa23e0d4b756d25829e124d6b670a4c6bbd4bf7e upstream.
    
    Sven Auhagen reports transaction failures with following error:
      ./main.nft:13:1-26: Error: Could not process rule: Cannot allocate memory
      percpu: allocation failed, size=16 align=8 atomic=1, atomic alloc failed, no space left
    
    This points to failing pcpu allocation with GFP_ATOMIC flag.
    However, transactions happen from user context and are allowed to sleep.
    
    One case where we can call into percpu allocator with GFP_ATOMIC is
    nft_counter expression.
    
    Normally this happens from control plane, so this could use GFP_KERNEL
    instead.  But one use case, element insertion from packet path,
    needs to use GFP_ATOMIC allocations (nft_dynset expression).
    
    At this time, .clone callbacks always use GFP_ATOMIC for this reason.
    
    Add gfp_t argument to the .clone function and pass GFP_KERNEL or
    GFP_ATOMIC flag depending on context, this allows all clone memory
    allocations to sleep for the normal (transaction) case.
    
    Cc: Sven Auhagen <sven.auhagen@voleatech.de>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -407,7 +407,7 @@ struct nft_expr_info;
 
 int nft_expr_inner_parse(const struct nft_ctx *ctx, const struct nlattr *nla,
 			 struct nft_expr_info *info);
-int nft_expr_clone(struct nft_expr *dst, struct nft_expr *src);
+int nft_expr_clone(struct nft_expr *dst, struct nft_expr *src, gfp_t gfp);
 void nft_expr_destroy(const struct nft_ctx *ctx, struct nft_expr *expr);
 int nft_expr_dump(struct sk_buff *skb, unsigned int attr,
 		  const struct nft_expr *expr, bool reset);
@@ -936,7 +936,7 @@ struct nft_expr_ops {
 						struct nft_regs *regs,
 						const struct nft_pktinfo *pkt);
 	int				(*clone)(struct nft_expr *dst,
-						 const struct nft_expr *src);
+						 const struct nft_expr *src, gfp_t gfp);
 	unsigned int			size;
 
 	int				(*init)(const struct nft_ctx *ctx,
