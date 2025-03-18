dadecdf6611633e959c7b7348cc722f77487b637
Andrey Vatoropin <a.vatoropin@crpt.ru>
2025-03-18 13:42:18 +0000

    fs/ntfs3: handle hdr_first_de() return value
    [ Upstream commit af5cab0e5b6f8edb0be51a9f47f3f620e0b4fd70 ]
    
    The hdr_first_de() function returns a pointer to a struct NTFS_DE. This
    pointer may be NULL. To handle the NULL error effectively, it is important
    to implement an error handler. This will help manage potential errors
    consistently.
    
    Additionally, error handling for the return value already exists at other
    points where this function is called.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Fixes: 82cae269cfa9 ("fs/ntfs3: Add initialization of super block")
    Signed-off-by: Andrey Vatoropin <a.vatoropin@crpt.ru>
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ntfs3/index.c
+++ fs/ntfs3/index.c
@@ -2166,6 +2166,10 @@ static int indx_get_entry_to_replace(struct ntfs_index *indx,
 
 		e = hdr_first_de(&n->index->ihdr);
 		fnd_push(fnd, n, e);
+		if (!e) {
+			err = -EINVAL;
+			goto out;
+		}
 
 		if (!de_is_last(e)) {
 			/*
@@ -2187,6 +2191,10 @@ static int indx_get_entry_to_replace(struct ntfs_index *indx,
 
 	n = fnd->nodes[level];
 	te = hdr_first_de(&n->index->ihdr);
+	if (!te) {
+		err = -EINVAL;
+		goto out;
+	}
 	/* Copy the candidate entry into the replacement entry buffer. */
 	re = kmalloc(le16_to_cpu(te->size) + sizeof(u64), GFP_NOFS);
 	if (!re) {
