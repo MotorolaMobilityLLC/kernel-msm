8128968b0ca60e4d5a70baa22f80ec8743dabcd6
Steven Rostedt <rostedt@goodmis.org>
2025-03-23 15:21:51 -0400

    tracing: Do not use PERF enums when perf is not defined
    commit 8eb1518642738c6892bd629b46043513a3bf1a6a upstream.
    
    An update was made to up the module ref count when a synthetic event is
    registered for both trace and perf events. But if perf is not configured
    in, the perf enums used will cause the kernel to fail to build.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Douglas Raillard <douglas.raillard@arm.com>
    Link: https://lore.kernel.org/20250323152151.528b5ced@batman.local.home
    Fixes: 21581dd4e7ff ("tracing: Ensure module defining synth event cannot be unloaded while tracing")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202503232230.TeREVy8R-lkp@intel.com/
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/trace/trace_events_synth.c
+++ kernel/trace/trace_events_synth.c
@@ -863,8 +863,10 @@ static int synth_event_reg(struct trace_event_call *call,
 	int ret;
 
 	switch (type) {
-	case TRACE_REG_REGISTER:
+#ifdef CONFIG_PERF_EVENTS
 	case TRACE_REG_PERF_REGISTER:
+#endif
+	case TRACE_REG_REGISTER:
 		if (!try_module_get(event->mod))
 			return -EBUSY;
 		break;
@@ -875,8 +877,10 @@ static int synth_event_reg(struct trace_event_call *call,
 	ret = trace_event_reg(call, type, data);
 
 	switch (type) {
-	case TRACE_REG_UNREGISTER:
+#ifdef CONFIG_PERF_EVENTS
 	case TRACE_REG_PERF_UNREGISTER:
+#endif
+	case TRACE_REG_UNREGISTER:
 		module_put(event->mod);
 		break;
 	default:
