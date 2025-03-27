dc0d9f5c1a5796c000f072d9441c3b11a73b2201
Xunjie Xu <xuxj12@motorola.com>
2025-03-27 10:48:29 +0800

    charger: check wireless charger fw update status
    Twice enable fw update will failed by adsp crash.
    Need avoid triggle fw update more times
    
    Change-Id: If35b2f3cce4db4ee862a520ca8f01137725d2ea6
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3325871
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wentao Xu <xuwt2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -2122,6 +2122,9 @@ static ssize_t wireless_fw_force_update_store(struct class *c,
 	if (kstrtobool(buf, &val) || !val)
 		return -EINVAL;
 
+	if (wls_fw_udating)
+		return -EBUSY;
+
 	rc = wireless_fw_update(bcdev, true);
 	if (rc < 0)
 		return rc;
@@ -2141,6 +2144,9 @@ static ssize_t wireless_fw_update_store(struct class *c,
 
 	if (kstrtobool(buf, &val) || !val)
 		return -EINVAL;
+
+	if (wls_fw_udating)
+		return -EBUSY;
 
 	do{
 		rc = wireless_fw_update(bcdev, false);
