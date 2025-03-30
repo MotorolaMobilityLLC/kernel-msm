d31230d32e25e1c3eda0406e3c1d7415462ad7f1
pengzhongcui <pengzhongcui@xiaomi.corp-partner.google.com>
2025-03-30 16:32:27 +0800

    ANDROID: vendor_hook: Add hook is to optimize the time consumption of shrink slab.
    one Vendor hook add:
        android_vh_do_shrink_slab_ex
    
    Add vendor hook point in do_shrink_slab to optimize for user
    experience related threads and time-consuming shrinkers.
    
    Bug: 407420219
    
    Change-Id: I5ee29988eebb53da503f729564946b12deb1d981
    Signed-off-by: pengzhongcui <pengzhongcui@xiaomi.corp-partner.google.com>

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -21,6 +21,10 @@ DECLARE_HOOK(android_vh_shrink_slab_bypass,
 DECLARE_HOOK(android_vh_do_shrink_slab,
 	TP_PROTO(struct shrinker *shrinker, long *freeable),
 	TP_ARGS(shrinker, freeable));
+DECLARE_HOOK(android_vh_do_shrink_slab_ex,
+	TP_PROTO(struct shrink_control *shrinkctl, struct shrinker *shrinker,
+	        long *freeable, int priority),
+	TP_ARGS(shrinkctl, shrinker, freeable, priority));
 DECLARE_HOOK(android_vh_shrink_node_memcgs,
 	TP_PROTO(struct mem_cgroup *memcg, bool *skip),
 	TP_ARGS(memcg, skip));
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -816,6 +816,7 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 
 	freeable = shrinker->count_objects(shrinker, shrinkctl);
 	trace_android_vh_do_shrink_slab(shrinker, &freeable);
+	trace_android_vh_do_shrink_slab_ex(shrinkctl, shrinker, &freeable, priority);
 	if (freeable == 0 || freeable == SHRINK_EMPTY)
 		return freeable;
 
