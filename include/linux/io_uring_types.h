ae96d49ee17e74a9e475629ea20423f0fc9be794
Greg Kroah-Hartman <gregkh@google.com>
2025-04-14 17:15:39 +0000

    ANDROID: GKI: allow abi break in struct io_ring_ctx
    In commit 46b1b3d81a7e ("io_uring/kbuf: use vm_insert_pages() for
    mmap'ed pbuf ring"), a structure was removed from struct io_ring_ctx.
    This structure is private to the io_uring core code, but exposed as a
    pointer to it is in a global structure used elsewhere.  Because of this,
    the abi checker, and CRC checker, notices the change and causes an abi
    break to be reported.
    
    Fix thsi up by allowing this break to be updated to the .stg file and
    document it as being ok.
    
    Change-Id: I8c4c182112f521910399be07285c21d3aac70711
    Fixes: 46b1b3d81a7e ("io_uring/kbuf: use vm_insert_pages() for mmap'ed pbuf ring")
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/linux/io_uring_types.h
+++ include/linux/io_uring_types.h
@@ -326,6 +326,10 @@ struct io_ring_ctx {
 
 	struct list_head	io_buffers_cache;
 
+#ifdef __GENKSYMS__
+	struct hlist_head	io_buf_list;
+#endif
+
 	/* Keep this last, we don't need it for the fast path */
 	struct wait_queue_head		poll_wq;
 	struct io_restriction		restrictions;
