34291cbd27664a7ce3330fc2d0d987bfb11c9a55
Xiaolei Wang <xiaolei.wang@windriver.com>
2025-04-30 17:20:42 +0800

    remoteproc: core: Cleanup acquired resources when rproc_handle_resources() fails in rproc_attach()
    commit 7692c9fbedd9087dc9050903f58095915458d9b1 upstream.
    
    When rproc->state = RPROC_DETACHED and rproc_attach() is used
    to attach to the remote processor, if rproc_handle_resources()
    returns a failure, the resources allocated by imx_rproc_prepare()
    should be released, otherwise the following memory leak will occur.
    
    Since almost the same thing is done in imx_rproc_prepare() and
    rproc_resource_cleanup(), Function rproc_resource_cleanup() is able
    to deal with empty lists so it is better to fix the "goto" statements
    in rproc_attach(). replace the "unprepare_device" goto statement with
    "clean_up_resources" and get rid of the "unprepare_device" label.
    
    unreferenced object 0xffff0000861c5d00 (size 128):
    comm "kworker/u12:3", pid 59, jiffies 4294893509 (age 149.220s)
    hex dump (first 32 bytes):
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ................
    00 00 02 88 00 00 00 00 00 00 10 00 00 00 00 00 ............
    backtrace:
     [<00000000f949fe18>] slab_post_alloc_hook+0x98/0x37c
     [<00000000adbfb3e7>] __kmem_cache_alloc_node+0x138/0x2e0
     [<00000000521c0345>] kmalloc_trace+0x40/0x158
     [<000000004e330a49>] rproc_mem_entry_init+0x60/0xf8
     [<000000002815755e>] imx_rproc_prepare+0xe0/0x180
     [<0000000003f61b4e>] rproc_boot+0x2ec/0x528
     [<00000000e7e994ac>] rproc_add+0x124/0x17c
     [<0000000048594076>] imx_rproc_probe+0x4ec/0x5d4
     [<00000000efc298a1>] platform_probe+0x68/0xd8
     [<00000000110be6fe>] really_probe+0x110/0x27c
     [<00000000e245c0ae>] __driver_probe_device+0x78/0x12c
     [<00000000f61f6f5e>] driver_probe_device+0x3c/0x118
     [<00000000a7874938>] __device_attach_driver+0xb8/0xf8
     [<0000000065319e69>] bus_for_each_drv+0x84/0xe4
     [<00000000db3eb243>] __device_attach+0xfc/0x18c
     [<0000000072e4e1a4>] device_initial_probe+0x14/0x20
    
    Fixes: 10a3d4079eae ("remoteproc: imx_rproc: move memory parsing to rproc_ops")
    Suggested-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20250430092043.1819308-2-xiaolei.wang@windriver.com
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1615,7 +1615,7 @@ static int rproc_attach(struct rproc *rproc)
 	ret = rproc_set_rsc_table(rproc);
 	if (ret) {
 		dev_err(dev, "can't load resource table: %d\n", ret);
-		goto unprepare_device;
+		goto clean_up_resources;
 	}
 
 	/* reset max_notifyid */
@@ -1632,7 +1632,7 @@ static int rproc_attach(struct rproc *rproc)
 	ret = rproc_handle_resources(rproc, rproc_loading_handlers);
 	if (ret) {
 		dev_err(dev, "Failed to process resources: %d\n", ret);
-		goto unprepare_device;
+		goto clean_up_resources;
 	}
 
 	/* Allocate carveout resources associated to rproc */
@@ -1651,7 +1651,6 @@ static int rproc_attach(struct rproc *rproc)
 
 clean_up_resources:
 	rproc_resource_cleanup(rproc);
-unprepare_device:
 	/* release HW resources if needed */
 	rproc_unprepare_device(rproc);
 disable_iommu:
