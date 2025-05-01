822895b9a0eb87acdf8c0736ccc128095be017f6
Fuad Tabba <tabba@google.com>
2025-05-01 13:21:50 +0100

    ANDROID: KVM: arm64: Move __deactivate_fpsimd_traps() to switch.h
    Move __deactivate_fpsimd_traps() to the shared switch header, instead of
    having separate implementations in the vhe/nvhe switch.c files.
    Subsequent patches will remove all specific implementations from
    switch.c and include switch.h in other files.
    
    Bug: 411040189
    Change-Id: I42c545e939b230366fbd9ad8e41a614193169bce
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/vhe/switch.c
+++ arch/arm64/kvm/hyp/vhe/switch.c
@@ -163,16 +163,6 @@ void deactivate_traps_vhe_put(struct kvm_vcpu *vcpu)
 	local_irq_restore(flags);
 }
 
-static void __deactivate_fpsimd_traps(struct kvm_vcpu *vcpu)
-{
-	u64 reg = CPACR_EL1_FPEN_EL0EN | CPACR_EL1_FPEN_EL1EN;
-
-	if (vcpu_has_sve(vcpu))
-		reg |= CPACR_EL1_ZEN_EL0EN | CPACR_EL1_ZEN_EL1EN;
-
-	sysreg_clear_set(cpacr_el1, 0, reg);
-}
-
 static const exit_handler_fn hyp_exit_handlers[] = {
 	[0 ... ESR_ELx_EC_MAX]		= NULL,
 	[ESR_ELx_EC_CP15_32]		= kvm_hyp_handle_cp15_32,
