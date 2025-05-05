171168359305adafca512864505d2c291a84facd
Greg Kroah-Hartman <gregkh@google.com>
2025-05-05 12:26:46 +0000

    Revert "blk-cgroup: support to track if policy is online"
    This reverts commit 5d5ea4f957d548f5a256c3933f1fbe4ed84280bc which is
    commit dfd6200a095440b663099d8d42f1efb0175a1ce3 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: Ibd89626cc871d0f045806ebbcad500ee6fa1dfbd
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -192,7 +192,6 @@ static struct blkcg_gq *blkg_alloc(struct blkcg *blkcg, struct request_queue *q,
 		blkg->pd[i] = pd;
 		pd->blkg = blkg;
 		pd->plid = i;
-		pd->online = false;
 	}
 
 	return blkg;
@@ -290,11 +289,8 @@ static struct blkcg_gq *blkg_create(struct blkcg *blkcg,
 		for (i = 0; i < BLKCG_MAX_POLS; i++) {
 			struct blkcg_policy *pol = blkcg_policy[i];
 
-			if (blkg->pd[i]) {
-				if (pol->pd_online_fn)
-					pol->pd_online_fn(blkg->pd[i]);
-				blkg->pd[i]->online = true;
-			}
+			if (blkg->pd[i] && pol->pd_online_fn)
+				pol->pd_online_fn(blkg->pd[i]);
 		}
 	}
 	blkg->online = true;
@@ -394,11 +390,8 @@ static void blkg_destroy(struct blkcg_gq *blkg)
 	for (i = 0; i < BLKCG_MAX_POLS; i++) {
 		struct blkcg_policy *pol = blkcg_policy[i];
 
-		if (blkg->pd[i] && blkg->pd[i]->online) {
-			if (pol->pd_offline_fn)
-				pol->pd_offline_fn(blkg->pd[i]);
-			blkg->pd[i]->online = false;
-		}
+		if (blkg->pd[i] && pol->pd_offline_fn)
+			pol->pd_offline_fn(blkg->pd[i]);
 	}
 
 	blkg->online = false;
@@ -1377,7 +1370,6 @@ retry:
 		blkg->pd[pol->plid] = pd;
 		pd->blkg = blkg;
 		pd->plid = pol->plid;
-		pd->online = false;
 	}
 
 	/* all allocated, init in the same order */
@@ -1385,11 +1377,9 @@ retry:
 		list_for_each_entry_reverse(blkg, &q->blkg_list, q_node)
 			pol->pd_init_fn(blkg->pd[pol->plid]);
 
-	list_for_each_entry_reverse(blkg, &q->blkg_list, q_node) {
-		if (pol->pd_online_fn)
+	if (pol->pd_online_fn)
+		list_for_each_entry_reverse(blkg, &q->blkg_list, q_node)
 			pol->pd_online_fn(blkg->pd[pol->plid]);
-		blkg->pd[pol->plid]->online = true;
-	}
 
 	__set_bit(pol->plid, q->blkcg_pols);
 	ret = 0;
@@ -1451,7 +1441,7 @@ void blkcg_deactivate_policy(struct request_queue *q,
 
 		spin_lock(&blkcg->lock);
 		if (blkg->pd[pol->plid]) {
-			if (blkg->pd[pol->plid]->online && pol->pd_offline_fn)
+			if (pol->pd_offline_fn)
 				pol->pd_offline_fn(blkg->pd[pol->plid]);
 			pol->pd_free_fn(blkg->pd[pol->plid]);
 			blkg->pd[pol->plid] = NULL;
