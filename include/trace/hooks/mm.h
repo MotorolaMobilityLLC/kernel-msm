335b59db6fb3873572763d5dd32aa8d078a20a1f
Minchan Kim <minchan@google.com>
2025-05-06 19:57:28 +0000

    ANDROID: adjust check_flags parameter
    Along with I746f4fbc20df5cf394d7644ff2cd6f25916c9790,
    it also needs to adjust check_flags.
    
    Bug: 416087944
    Bug: 417601220
    Change-Id: I2871ab305963da00fcc8b0a7755bd8db7859e4c5
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 8f5494d90cac148e269da2937456243ed2d5906f)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -441,6 +441,9 @@ DECLARE_HOOK(android_vh_free_unref_page_list_bypass,
 DECLARE_HOOK(android_vh_free_pages_prepare_init,
 	TP_PROTO(struct page *page, int nr_pages, bool *init),
 	TP_ARGS(page, nr_pages, init));
+DECLARE_HOOK(android_vh_free_one_page_flag_check,
+	TP_PROTO(unsigned long *flags),
+	TP_ARGS(flags));
 DECLARE_HOOK(android_vh_post_alloc_hook,
 	TP_PROTO(struct page *page, unsigned int order, bool *init),
 	TP_ARGS(page, order, init));
