b0b23d780258ee0a57cfa318d8c774e0545aaf64
jianhua hao <haojianhua1@xiaomi.com>
2025-05-06 10:42:25 +0800

    ANDROID: mm: export __pte_offset_map/unuse_swap_pte/read_swap_cache_async
    Export __pte_offset_map facilitate retrieving the corresponding PTE using PMD and an address.
    Add and export unuse_swap_pte to facilitate releasing the PTE resources corresponding to pages preloaded via swapin.
    Export read_swap_cache_async to facilitate asynchronously reading pages from the swap partition using PTE-prefetch scanning.
    
    Bug: 415852480
    
    Change-Id: Ie200656ec97b087936ca98c06b0a370f547d5d0a
    Signed-off-by: jianhua hao <haojianhua1@xiaomi.com>
    (cherry picked from commit 88cb3505ebf4d9eb1dd0d3c63403727eb4b239bd)

--- mm/pgtable-generic.c
+++ mm/pgtable-generic.c
@@ -305,6 +305,7 @@ nomap:
 	rcu_read_unlock();
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(__pte_offset_map);
 
 pte_t *pte_offset_map_nolock(struct mm_struct *mm, pmd_t *pmd,
 			     unsigned long addr, spinlock_t **ptlp)
