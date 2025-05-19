ddca8b8a6681c8869842697fd359d93b549d8e15
Greg Kroah-Hartman <gregkh@google.com>
2025-05-19 12:53:17 +0000

    Revert "selftests/bpf: extend changes_pkt_data with cases w/o subprograms"
    This reverts commit 04789af756a4a43e72986185f66f148e65b32fed which is
    commit 04789af756a4a43e72986185f66f148e65b32fed upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I454badded1d44b44beded5b1382c581caf57fc03
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- tools/testing/selftests/bpf/progs/changes_pkt_data.c
+++ tools/testing/selftests/bpf/progs/changes_pkt_data.c
@@ -4,35 +4,22 @@
 #include <bpf/bpf_helpers.h>
 
 __noinline
-long changes_pkt_data(struct __sk_buff *sk)
+long changes_pkt_data(struct __sk_buff *sk, __u32 len)
 {
-	return bpf_skb_pull_data(sk, 0);
+	return bpf_skb_pull_data(sk, len);
 }
 
 __noinline __weak
-long does_not_change_pkt_data(struct __sk_buff *sk)
+long does_not_change_pkt_data(struct __sk_buff *sk, __u32 len)
 {
 	return 0;
 }
 
-SEC("?tc")
-int main_with_subprogs(struct __sk_buff *sk)
+SEC("tc")
+int dummy(struct __sk_buff *sk)
 {
-	changes_pkt_data(sk);
-	does_not_change_pkt_data(sk);
-	return 0;
-}
-
-SEC("?tc")
-int main_changes(struct __sk_buff *sk)
-{
-	bpf_skb_pull_data(sk, 0);
-	return 0;
-}
-
-SEC("?tc")
-int main_does_not_change(struct __sk_buff *sk)
-{
+	changes_pkt_data(sk, 0);
+	does_not_change_pkt_data(sk, 0);
 	return 0;
 }
 
