7eaee1f07490cccb313525ffeeb03363b49d60fb
Chungkai Mei <chungkai@google.com>
2025-05-19 03:12:34 +0000

    ANDROID: vendor_hook: add trace_android_rvh_setscheduler_prio
    To modify priority of specific tasks, add the vendor hook in __setscheduler_prio
    
    Bug: 426090643
    Bug: 409176857
    Change-Id: Id5a2309378f1a8c3ecc1de71c20f44f73b3f7557
    Signed-off-by: Chungkai Mei <chungkai@google.com>
    (cherry picked from commit ba26a9d32c606abc46d181e593219e945b1b7dfd)
    Signed-off-by: Gina Ko <yichunko@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7191,6 +7191,7 @@ static void __setscheduler_prio(struct task_struct *p, int prio)
 		p->sched_class = &fair_sched_class;
 
 	p->prio = prio;
+	trace_android_rvh_setscheduler_prio(p);
 }
 
 #ifdef CONFIG_RT_MUTEXES
