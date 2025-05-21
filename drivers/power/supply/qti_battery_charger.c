269e9c7e7c0caf4e1976d7db733da33e41844421
Xunjie Xu <xuxj12@motorola.com>
2025-05-21 14:56:43 +0800

    battery: add setting cycle count power supply
    add setting cycle count power supply
    new_bhd will save and update it
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I7dbe3111bf3b95fdbf99c001e30799ae94e05da9
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3402754
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1533,6 +1533,22 @@ static int battery_psy_set_charge_current(struct battery_chg_dev *bcdev,
 	return rc;
 }
 
+static int battery_psy_set_cycle_count(struct battery_chg_dev *bcdev,
+					int CycleCount)
+{
+	int rc;
+
+	rc = write_property_id(bcdev, &bcdev->psy_list[PSY_TYPE_BATTERY],
+				BATT_CYCLE_COUNT, CycleCount);
+	if (rc < 0) {
+		pr_err("Failed to set cycle count %d, rc=%d\n", CycleCount, rc);
+	} else {
+		pr_err("Set CycleCount to %d\n", CycleCount);
+	}
+
+	return rc;
+}
+
 static int battery_psy_get_prop(struct power_supply *psy,
 		enum power_supply_property prop,
 		union power_supply_propval *pval)
@@ -1592,6 +1608,8 @@ static int battery_psy_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_END_THRESHOLD:
 		return battery_psy_set_charge_end_threshold(bcdev,
 								pval->intval);
+	case POWER_SUPPLY_PROP_CYCLE_COUNT:
+		return battery_psy_set_cycle_count(bcdev, pval->intval);
 	default:
 		return -EINVAL;
 	}
@@ -1605,6 +1623,7 @@ static int battery_psy_prop_is_writeable(struct power_supply *psy,
 	switch (prop) {
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_START_THRESHOLD:
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_END_THRESHOLD:
+	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 		return 1;
 	default:
 		break;
