acb9c6f82ec5c69c91d7a95f18c47496d81b662b
Xunjie Xu <xuxj12@motorola.com>
2025-05-21 14:51:14 +0800

    haptic: Add new node to support tcmd
    Create new node enable_play and vmax_mv_debug.
    Used to check for normal vibration function.
    Mot-CRs-fixed: (CR)
    
    Change-Id: I9e9595e0646d0b1a33f02414a317cbec59d94d2e
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3403094
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/input/misc/qcom-hv-haptics.c
+++ drivers/input/misc/qcom-hv-haptics.c
@@ -805,6 +805,7 @@ struct haptics_hw_config {
 	struct brake_cfg	brake;
 	struct brake_cfg	swr_brake;
 	u32			vmax_mv;
+	u32			vmax_mv_debug;
 	u32			t_lra_us;
 	u32			cl_t_lra_us;
 	u32			lra_min_mohms;
@@ -5372,6 +5373,7 @@ static int haptics_parse_dt(struct haptics_chip *chip)
 		rc = -EINVAL;
 		goto free_pbs;
 	}
+	config->vmax_mv_debug = 0;
 
 	config->fifo_empty_thresh = chip->fifo_info->fifo_empty_threshold;
 	of_property_read_u32(node, "qcom,fifo-empty-threshold",
@@ -6297,6 +6299,62 @@ static int haptics_auto_brake_manual_config(struct haptics_chip *chip)
 	return rc;
 }
 
+static int haptics_start_play(struct haptics_chip *chip, bool enable)
+{
+	int rc;
+	u8 amplitude;
+	u32 vmax_mv = (chip->config.vmax_mv_debug == 0 ?
+	    chip->config.vmax_mv : chip->config.vmax_mv_debug);
+
+	mutex_lock(&chip->play.lock);
+	if (enable) {
+		rc = haptics_runtime_resume_get(chip);
+		if (rc < 0)
+			goto unlock;
+		/* Stop other mode playing if there is any */
+		rc = haptics_enable_play(chip, false);
+		if (rc < 0) {
+			dev_err(chip->dev, "Stop playing failed, rc=%d\n", rc);
+			goto unlock;
+		}
+
+		rc = haptics_set_vmax_mv(chip, vmax_mv);
+		if (rc < 0)
+			goto unlock;
+
+		amplitude = get_direct_play_max_amplitude(chip);
+		rc = haptics_set_direct_play(chip, amplitude);
+		if (rc < 0)
+			goto unlock;
+
+		rc = haptics_enable_hpwr_vreg(chip, true);
+		if (rc < 0)
+			goto unlock;
+
+		rc = haptics_wait_hboost_ready(chip);
+		if (rc < 0)
+			goto unlock;
+
+		chip->play.pattern_src = DIRECT_PLAY;
+		rc = haptics_enable_play(chip, true);
+		if (rc < 0)
+			goto unlock;
+
+		mutex_unlock(&chip->play.lock);
+		return rc;
+	}
+
+unlock:
+	/* Disable play in case it's not been disabled */
+	haptics_enable_play(chip, false);
+	rc = haptics_enable_hpwr_vreg(chip, false);
+	haptics_runtime_autosuspend_put(chip);
+
+	mutex_unlock(&chip->play.lock);
+
+	return rc;
+}
+
 #define AUTO_BRAKE_CAL_POLLING_COUNT	10
 #define AUTO_BRAKE_CAL_POLLING_STEP_US	20000
 #define AUTO_BRAKE_CAL_WAIT_MS		800
@@ -6716,6 +6774,62 @@ static ssize_t swr_play_store(const struct class *c,
 }
 static CLASS_ATTR_RW(swr_play);
 
+static ssize_t vmax_mv_debug_store(const struct class *c,
+		const struct class_attribute *attr, const char *buf, size_t count)
+{
+	struct haptics_chip *chip = container_of(c,
+			struct haptics_chip, hap_class);
+	int val;
+
+	if (kstrtoint(buf, 0, &val))
+		return -EINVAL;
+
+	if (val >= 0) {
+	    dev_err(chip->dev, "change vmax_mv %d -> %d\n",
+	            chip->config.vmax_mv, val);
+	    chip->config.vmax_mv_debug = val;
+	} else {
+	    chip->config.vmax_mv_debug = 0;
+	}
+	return count;
+}
+
+static ssize_t vmax_mv_debug_show(const struct class *c,
+		const struct class_attribute *attr, char *buf)
+{
+	struct haptics_chip *chip = container_of(c,
+			struct haptics_chip, hap_class);
+
+	return scnprintf(buf, PAGE_SIZE, "%d mv\n", chip->config.vmax_mv_debug);
+}
+static CLASS_ATTR_RW(vmax_mv_debug);
+
+static ssize_t enable_play_store(const struct class *c,
+		const struct class_attribute *attr, const char *buf, size_t count)
+{
+	struct haptics_chip *chip = container_of(c,
+			struct haptics_chip, hap_class);
+	int val;
+	int rc;
+
+	if (kstrtoint(buf, 0, &val))
+		return -EINVAL;
+
+	if (val) {
+		rc = haptics_start_play(chip, true);
+		if (rc < 0)
+			return rc;
+	} else {
+		rc = haptics_start_play(chip, false);
+		if (rc < 0)
+			return rc;
+	}
+	return count;
+}
+static CLASS_ATTR_WO(enable_play);
+
+
+
 static struct attribute *hap_class_attrs[] = {
 	&class_attr_lra_calibration.attr,
 	&class_attr_lra_frequency_hz.attr,
@@ -6724,6 +6838,8 @@ static struct attribute *hap_class_attrs[] = {
 	&class_attr_primitive_duration.attr,
 	&class_attr_visense_enabled.attr,
 	&class_attr_swr_play.attr,
+	&class_attr_enable_play.attr,
+	&class_attr_vmax_mv_debug.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(hap_class);
