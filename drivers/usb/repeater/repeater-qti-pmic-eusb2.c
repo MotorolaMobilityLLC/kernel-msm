2920359bf77c489342e60f57889e8bbea4ef5578
Xunjie Xu <xuxj12@motorola.com>
2025-05-21 14:54:30 +0800

    usb: add repeater parameter update log
    Add log to show host/device usb parameter
    update info
    Mot-CRs-fixed: (CR)
    
    Change-Id: If646287d50765e6625a14c860c0d07cfc0a71920
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3402750
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/repeater/repeater-qti-pmic-eusb2.c
+++ drivers/usb/repeater/repeater-qti-pmic-eusb2.c
@@ -174,9 +174,9 @@ static void eusb2_repeater_update_seq(struct eusb2_repeater *er,
 {
 	int i;
 
-	dev_dbg(er->ur.dev, "param override seq count:%d\n", cnt);
+	dev_info(er->ur.dev, "param override seq count:%d\n", cnt);
 	for (i = 0; i < cnt; i = i+2) {
-		dev_dbg(er->ur.dev, "write 0x%02x to 0x%02x\n",
+		dev_info(er->ur.dev, "write 0x%02x to 0x%02x\n",
 						seq[i], seq[i+1]);
 		eusb2_repeater_reg_write(er, seq[i+1], seq[i]);
 	}
