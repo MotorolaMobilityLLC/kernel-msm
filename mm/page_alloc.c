1421762d235e4d8725f02d6438bd9c82cfe22e32
Kalesh Singh <kaleshsingh@google.com>
2025-05-27 21:09:07 -0700

    Revert "BACKPORT: mm: page_alloc: batch vmstat updates in expand()"
    This reverts commit bc1e3097e35e33a3355e2b78c0bd960ee6aef2cb.
    
    Change-Id: Ia2ac7d24bf2eb0931bcfff2bec0852654cd4d5a2
    Bug: 420771453
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1519,7 +1519,6 @@ static inline void expand(struct zone *zone, struct page *page,
 	int low, int high, int migratetype)
 {
 	unsigned long size = 1 << high;
-	unsigned long nr_added = 0;
 
 	while (high > low) {
 		high--;
@@ -1537,9 +1536,7 @@ static inline void expand(struct zone *zone, struct page *page,
 
 		__add_to_free_list(&page[size], zone, high, migratetype, false);
 		set_buddy_order(&page[size], high);
-		nr_added += size;
 	}
-	account_freepages(zone, nr_added, migratetype);
 }
 
 static void check_new_page_bad(struct page *page)
