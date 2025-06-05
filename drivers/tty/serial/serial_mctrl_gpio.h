9436587b9b527322e526066857d4d1471fe6bd0c
Greg Kroah-Hartman <gregkh@google.com>
2025-06-05 13:19:40 +0000

    Revert "serial: mctrl_gpio: split disable_ms into sync and no_sync APIs"
    This reverts commit 68435c1fa3db696db4f480385db9e50e26691d0d which is
    commit 1bd2aad57da95f7f2d2bb52f7ad15c0f4993a685 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I5a1f6ecb6b0139b48d9cbfeff2efde4362d6f3b0
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/tty/serial/serial_mctrl_gpio.h
+++ drivers/tty/serial/serial_mctrl_gpio.h
@@ -87,16 +87,9 @@ void mctrl_gpio_free(struct device *dev, struct mctrl_gpios *gpios);
 void mctrl_gpio_enable_ms(struct mctrl_gpios *gpios);
 
 /*
- * Disable gpio interrupts to report status line changes, and block until
- * any corresponding IRQ is processed
+ * Disable gpio interrupts to report status line changes.
  */
-void mctrl_gpio_disable_ms_sync(struct mctrl_gpios *gpios);
-
-/*
- * Disable gpio interrupts to report status line changes, and return
- * immediately
- */
-void mctrl_gpio_disable_ms_no_sync(struct mctrl_gpios *gpios);
+void mctrl_gpio_disable_ms(struct mctrl_gpios *gpios);
 
 /*
  * Enable gpio wakeup interrupts to enable wake up source.
@@ -155,11 +148,7 @@ static inline void mctrl_gpio_enable_ms(struct mctrl_gpios *gpios)
 {
 }
 
-static inline void mctrl_gpio_disable_ms_sync(struct mctrl_gpios *gpios)
-{
-}
-
-static inline void mctrl_gpio_disable_ms_no_sync(struct mctrl_gpios *gpios)
+static inline void mctrl_gpio_disable_ms(struct mctrl_gpios *gpios)
 {
 }
 
