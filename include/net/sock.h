d00a702c063053e9986e603fd9e76d07d1051239
Michal Luczaj <mhal@rbox.co>
2025-06-09 19:08:03 +0200

    net: Fix TOCTOU issue in sk_is_readable()
    [ Upstream commit 2660a544fdc0940bba15f70508a46cf9a6491230 ]
    
    sk->sk_prot->sock_is_readable is a valid function pointer when sk resides
    in a sockmap. After the last sk_psock_put() (which usually happens when
    socket is removed from sockmap), sk->sk_prot gets restored and
    sk->sk_prot->sock_is_readable becomes NULL.
    
    This makes sk_is_readable() racy, if the value of sk->sk_prot is reloaded
    after the initial check. Which in turn may lead to a null pointer
    dereference.
    
    Ensure the function pointer does not turn NULL after the check.
    
    Fixes: 8934ce2fd081 ("bpf: sockmap redirect ingress support")
    Suggested-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Michal Luczaj <mhal@rbox.co>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://patch.msgid.link/20250609-skisreadable-toctou-v1-1-d0dfb2d62c37@rbox.co
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2827,8 +2827,11 @@ int sock_bind_add(struct sock *sk, struct sockaddr *addr, int addr_len);
 
 static inline bool sk_is_readable(struct sock *sk)
 {
-	if (sk->sk_prot->sock_is_readable)
-		return sk->sk_prot->sock_is_readable(sk);
+	const struct proto *prot = READ_ONCE(sk->sk_prot);
+
+	if (prot->sock_is_readable)
+		return prot->sock_is_readable(sk);
+
 	return false;
 }
 #endif	/* _SOCK_H */
