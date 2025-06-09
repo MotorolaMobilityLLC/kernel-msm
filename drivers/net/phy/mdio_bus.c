3a5d134fe5b543959ab69e3bf8595e59c415b23d
Jakub Raczynski <j.raczynski@samsung.com>
2025-06-09 17:31:46 +0200

    net/mdiobus: Fix potential out-of-bounds read/write access
    [ Upstream commit 0e629694126ca388916f059453a1c36adde219c4 ]
    
    When using publicly available tools l(CR) 'mdio-tools' to read/write data
    from/to network interface and its PHY via mdiobus, there is no verification of
    parameters passed to the ioctl and it accepts any mdio address.
    Currently there is support for 32 addresses in kernel via PHY_MAX_ADDR define,
    but it is possible to pass higher value than that via ioctl.
    While read/write operation should generally fail in this case,
    mdiobus provides stats array, where wrong address may allow out-of-bounds
    read/write.
    
    Fix that by adding address verification before read/write operation.
    While this excludes this access from any statistics, it improves security of
    read/write operation.
    
    Fixes: 080bb352fad00 ("net: phy: Maintain MDIO device and bus statistics")
    Signed-off-by: Jakub Raczynski <j.raczynski@samsung.com>
    Reported-by: Wenjing Shan <wenjing.shan@samsung.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/phy/mdio_bus.c
+++ drivers/net/phy/mdio_bus.c
@@ -754,6 +754,9 @@ int __mdiobus_read(struct mii_bus *bus, int addr, u32 regnum)
 
 	WARN_ON_ONCE(!mutex_is_locked(&bus->mdio_lock));
 
+	if (addr >= PHY_MAX_ADDR)
+		return -ENXIO;
+
 	if (bus->read)
 		retval = bus->read(bus, addr, regnum);
 	else
@@ -782,6 +785,9 @@ int __mdiobus_write(struct mii_bus *bus, int addr, u32 regnum, u16 val)
 	int err;
 
 	WARN_ON_ONCE(!mutex_is_locked(&bus->mdio_lock));
+
+	if (addr >= PHY_MAX_ADDR)
+		return -ENXIO;
 
 	if (bus->write)
 		err = bus->write(bus, addr, regnum, val);
