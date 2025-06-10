BayLibre/android_kernel_common

ANDROID: mm: add vendor hooks to adjust memory reclamation
Add vendor hooks to adjust page reclamation operations based on the
running task and kernel memory pressure. Determine if file pages should
be skipped in response to file faults and memory pressure.

Bug: 350865007
Change-Id: I39c223876ef82af443407234e35f39bcbbeb17cb
Signed-off-by: Yang Yang <yang.yang@vivo.com>

drivers/android/vendor_hooks.c
@@ -357,6 +357,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_enter);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_direct_reclaim_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mm_may_oom_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_vmscan_kswapd_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_folio_list);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_inode_lru_isolate);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_invalidate_mapping_pagevec);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_customize_alloc_gfp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpu_capacity_show);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shmem_swapin_folio);

MotorolaMobilityLLC/kernel-msm

 Conflicts:
	drivers/android/vendor_hooks.c
	include/trace/hooks/vmscan.h
	mm/vmscan.c
	replace folio with page
(cherry picked from commit b0807745d4495c1614bcdbc4e5394d7bcabbf698)
Signed-off-by: Hailong Liu <liuhailong@oppo.com>

drivers/android/vendor_hooks.c
@@ -425,6 +425,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_thermal_stats);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_proc_transaction);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_new_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_del_ref);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_page_list);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_inode_lru_isolate);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_invalidate_mapping_pagevec);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_mapcount_pages);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_traversal_lruvec);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_page_mapcount);