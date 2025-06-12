83b66db4966e5a6f3e6cfd70b3232167a03f0667
Xunjie Xu <xuxj12@motorola.com>
2025-06-12 11:20:25 +0800

    dwc3: don't call dwc3_msm_resume() when exit LPM
    problems will arise in below case:
    plug out earphone:
    A thread					B thread
    msm_dwc3_pwr_irq //in_lpm is true
    				      ->      	dwc3_msm_runtime_resume //set in_lpm to false
    msm_dwc3_pwr_irq_thread //in_lpm is false
    dwc3_pwr_event_handler//no clear resume_pending
    
    PM resume:
    dwc3_core_complete
    dwc3_resume_work -> resume_pending is true
    dwc3_msm_resume(mdwc) ->  set WAIT_FOR_LPM bit
    
    plug in earphone:
    dwc3_otg_sm_work -> return due to wait WAIT_FOR_LPM
    Mot-CRs-fixed: (CR)
    
    Change-Id: I0428e0134d784724d69f21173318f1167fe8e802
    Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427224
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -4969,6 +4969,7 @@ skip_update:
 	if (mdwc->resume_pending) {
 		pm_runtime_resume(mdwc->dev);
 		mdwc->resume_pending = false;
+		dev_info(mdwc->dev, "%s: set resume_pending to false\n", __func__);
 	}
 
 	if (atomic_read(&mdwc->pm_suspended)) {
@@ -5048,7 +5049,7 @@ static irqreturn_t msm_dwc3_pwr_irq(int irq, void *data)
 {
 	struct dwc3_msm *mdwc = data;
 
-	dev_dbg(mdwc->dev, "%s received %d\n", __func__, irq);
+	dev_info(mdwc->dev, "%s received %d\n", __func__, irq);
 	/*
 	 * When in Low Power Mode, can't read PWR_EVNT_IRQ_STAT_REG to acertain
 	 * which interrupts have been triggered, as the clocks are disabled.
@@ -5058,8 +5059,13 @@ static irqreturn_t msm_dwc3_pwr_irq(int irq, void *data)
 	 */
 	if (atomic_read(&mdwc->in_lpm)) {
 		/* set this to call dwc3_msm_resume() */
+		dev_info(mdwc->dev, "%s resume_pending=%d, set resume_pending\n", __func__, mdwc->resume_pending);
 		mdwc->resume_pending = true;
 		return IRQ_WAKE_THREAD;
+	} else {
+		/* don't set this to call dwc3_msm_resume()when exit LPM */
+		dev_info(mdwc->dev, "%s resume_pending=%d, clean resume_pending\n", __func__, mdwc->resume_pending);
+		mdwc->resume_pending = false;
 	}
 
 	dwc3_pwr_event_handler(mdwc);
