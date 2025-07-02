5eca15bbae4f1cd57cc5ed2370b4f6a2b7b7ed2a
Greg Kroah-Hartman <gregkh@google.com>
2025-07-02 06:49:02 +0000

    Revert "net: Rename ->stream_memory_read to ->sock_is_readable"
    This reverts commit c83455d7de0baa77989ae68b8c6f2a4f33965763 which is
    commit 7b50ecfcc6cdfe87488576bc3ed443dc8d083b90 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I867c006a61134ba40e667c3df3074cef4f38e8dd
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -482,7 +482,10 @@ static bool tcp_stream_is_readable(struct sock *sk, int target)
 {
 	if (tcp_epollin_ready(sk, target))
 		return true;
-	return sk_is_readable(sk);
+
+	if (sk->sk_prot->stream_memory_read)
+		return sk->sk_prot->stream_memory_read(sk);
+	return false;
 }
 
 /*
