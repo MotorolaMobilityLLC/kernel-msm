9ed28524197391df2812ea630af20d333ec2ef78
Nikita Ioffe <ioffe@google.com>
2025-07-04 12:04:53 +0000

    ANDROID: kvm: arm64: add per_cpu/cpuX/trace file
    The trace interface was present in android14-6.1 kernel, and is used by
    perfetto (although perfetto can work without it), so we should keep it.
    
    Bug: 428904926
    Test: presubmit
    Change-Id: I51cc82324b3ef1ad8a801ae54f427eaf8790acd2
    Signed-off-by: N(CR) Ioffe <ioffe@google.com>

--- arch/arm64/kvm/hyp_trace.c
+++ arch/arm64/kvm/hyp_trace.c
@@ -862,6 +862,8 @@ int hyp_trace_init_tracefs(void)
 				    (void *)cpu, &hyp_trace_pipe_fops);
 		tracefs_create_file("trace_pipe_raw", TRACEFS_MODE_READ, per_cpu_dir,
 				    (void *)cpu, &hyp_trace_raw_fops);
+		tracefs_create_file("trace", TRACEFS_MODE_WRITE, per_cpu_dir,
+				    (void *)cpu, &hyp_trace_fops);
 	}
 
 	hyp_trace_init_event_tracefs(root);
