0a5cce0f2d1e5e4889b96c244ac23185bb5f1b59
Suren Baghdasaryan <surenb@google.com>
2025-07-09 16:52:30 -0700

    Revert "ANDROID: fixup dma_buf struct to avoid ABI breakage"
    Revert submission 3680024
    
    Reason for revert: replacing with a fixed version
    
    Reverted changes: /q/submissionid:3680024
    
    Bug: 430499939
    Change-Id: I994b93b56b734441b6ecebf90b777a7a6f54f1ab
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -535,11 +535,6 @@ struct dma_buf {
 	} *sysfs_entry;
 #endif
 
-	ANDROID_KABI_RESERVE(1);
-	ANDROID_KABI_RESERVE(2);
-};
-
-struct dma_buf_ext {
 	/**
 	 * @num_unique_refs:
 	 *
@@ -547,17 +542,9 @@ struct dma_buf_ext {
 	 */
 	atomic64_t num_unique_refs;
 
-	/*
-	 * dma_buf can have a reservation object after it, so keep this member
-	 * at the end of this structure.
-	 */
-	struct dma_buf dmabuf;
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
 };
-
-static inline struct dma_buf_ext *get_dmabuf_ext(struct dma_buf *dmabuf)
-{
-	return container_of(dmabuf, struct dma_buf_ext, dmabuf);
-}
 
 /**
  * struct dma_buf_attach_ops - importer operations for an attachment
