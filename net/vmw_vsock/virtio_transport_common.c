b72c6cabf92184a66356354a4de5b381b5dbc040
Will Deacon <willdeacon@google.com>
2025-07-21 20:44:42 +0100

    Revert "ANDROID: Make vsock virtio packet buff size configurable"
    This reverts commit f465fb7e1af29c5f6da1cd77b44f5d02e0ad5eeb.
    
    Now that vsock is more careful with its page allocations, overriding
    this option doesn't achieve an awful lot.
    
    Bug: 421244320
    Bug: 149494272
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I3ff4e92407f97f547528da459c1dad3519eb8b04

--- net/vmw_vsock/virtio_transport_common.c
+++ net/vmw_vsock/virtio_transport_common.c
@@ -29,10 +29,6 @@
 static void virtio_transport_cancel_close_work(struct vsock_sock *vsk,
 					       bool cancel_timeout);
 
-uint virtio_transport_max_vsock_pkt_buf_size = 64 * 1024;
-module_param(virtio_transport_max_vsock_pkt_buf_size, uint, 0444);
-EXPORT_SYMBOL_GPL(virtio_transport_max_vsock_pkt_buf_size);
-
 static const struct virtio_transport *
 virtio_transport_get_ops(struct vsock_sock *vsk)
 {
