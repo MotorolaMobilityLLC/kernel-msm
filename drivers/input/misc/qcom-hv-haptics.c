a20fae41e3df9c6bfe2f182cdf89416112565ce4
Xiaojun Ji <jixj@motorola.com>
2025-07-22 17:14:18 +0800

    Fix the build error after rebase
    reference https://gerrit.mot.com/#/c/3450407/
    
    Change-Id: I2ea85a7e51a73eb502e8b8e0650873123932be6d
    Reviewed-on: https://gerrit.mot.com/3473632
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/input/misc/qcom-hv-haptics.c
+++ drivers/input/misc/qcom-hv-haptics.c
@@ -5741,8 +5741,12 @@ static int richtap_file_mmap(struct file *filp, struct vm_area_struct *vma)
 	int ret = 0;
 
 	//only accept PROT_READ, PROT_WRITE and MAP_SHARED from the API of mmap
+#if 0
 	vm_flags_t vm_flags = calc_vm_prot_bits(PROT_READ|PROT_WRITE, 0) |
 		calc_vm_flag_bits(MAP_SHARED);
+#else
+	vm_flags_t vm_flags = VM_READ | VM_WRITE | VM_SHARED;
+#endif
 	vm_flags |= current->mm->def_flags | VM_MAYREAD |
 		VM_MAYWRITE | VM_MAYEXEC | VM_SHARED | VM_MAYSHARE;
 	if (vma && (pgprot_val(vma->vm_page_prot) != pgprot_val(vm_get_page_prot(vm_flags))))
