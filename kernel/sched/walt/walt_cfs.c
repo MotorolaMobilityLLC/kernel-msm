1d9e8bbe0fa4d89e9be347c86050ed2a4d28c528
Xiaojun Ji <jixj@motorola.com>
2025-07-23 10:33:24 +0800

    fixup! (CR) Fix skip_mvp issue which we picked moto changes
    Change-Id: I25593bfb0382046cc4cb74fedaf52e524c11de94
    Reviewed-on: https://gerrit.mot.com/3474874
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Zhangqing Huang <huangzq2@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- kernel/sched/walt/walt_cfs.c
+++ kernel/sched/walt/walt_cfs.c
@@ -1586,8 +1586,8 @@ static void walt_cfs_check_preempt_wakeup(void *unused, struct rq *rq, struct ta
 	walt_cfs_account_mvp_runtime(rq, c);
 #if IS_ENABLED(CONFIG_SCHED_MOTO_UNFAIR)
 	if (likely(moto_sched_enabled))
-		resched = (skip_mvp != wrq->skip_mvp) || (((wrq->mvp_tasks.next != &wts_c->mvp_list) ||
-			(wts_p->mvp_prio > wts_c->mvp_prio)) && (wrq->mvp_tasks.next == &wts_p->mvp_list)); // Moto wangwang: fix preemption issue.
+		resched = (skip_mvp != wrq->skip_mvp) || ((wrq->mvp_tasks.next != &wts_c->mvp_list) && (wrq->mvp_tasks.next == &wts_p->mvp_list)) ||
+			(wts_p->mvp_prio > wts_c->mvp_prio); // Moto wangwang: fix preemption issue.
 	else
 		resched = (skip_mvp != wrq->skip_mvp) || (wrq->mvp_tasks.next != &wts_c->mvp_list) ||
 			(wts_p->mvp_prio > wts_c->mvp_prio);
