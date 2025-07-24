210c5398a75dbf549f17d2113d4bb4efe421dc84
Zhang Lixu <lixu.zhang@intel.com>
2025-07-24 11:36:37 -0400

    iio: hid-sensor-prox: Restore lost scale assignments
    [ Upstream commit 83ded7cfaccccd2f4041769c313b58b4c9e265ad ]
    
    The variables `scale_pre_decml`, `scale_post_decml`, and `scale_precision`
    were assigned in commit d68c592e02f6 ("iio: hid-sensor-prox: Fix scale not
    correct issue"), but due to a merge conflict in
    commit 9c15db92a8e5 ("Merge tag 'iio-for-5.13a' of
    https://git.kernel.org/pub/scm/linux/kernel/git/jic23/iio into staging-next"),
    these assignments were lost.
    
    Add back lost assignments and replace `st->prox_attr` with
    `st->prox_attr[0]` because commit 596ef5cf654b ("iio: hid-sensor-prox: Add
    support for more channels") changed `prox_attr` to an array.
    
    Cc: stable@vger.kernel.org # 5.13+
    Fixes: 9c15db92a8e5 ("Merge tag 'iio-for-5.13a' of https://git.kernel.org/pub/scm/linux/kernel/git/jic23/iio into staging-next")
    Signed-off-by: Zhang Lixu <lixu.zhang@intel.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Link: https://patch.msgid.link/20250331055022.1149736-2-lixu.zhang@intel.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    [ changed st->prox_attr[0] array access to st->prox_attr single struct member ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iio/light/hid-sensor-prox.c
+++ drivers/iio/light/hid-sensor-prox.c
@@ -226,6 +226,11 @@ static int prox_parse_report(struct platform_device *pdev,
 	dev_dbg(&pdev->dev, "prox %x:%x\n", st->prox_attr.index,
 			st->prox_attr.report_id);
 
+	st->scale_precision = hid_sensor_format_scale(hsdev->usage,
+						      &st->prox_attr,
+						      &st->scale_pre_decml,
+						      &st->scale_post_decml);
+
 	return ret;
 }
 
