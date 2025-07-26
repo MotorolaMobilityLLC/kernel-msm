ea01b44a96b4a97aa1fddf353e0e7a657b03404f
Neill Kapron <nkapron@google.com>
2025-07-26 14:57:33 +0000

    ANDROID: bpf: do not fail to load if log is full
    Upstream commit 973c7a0d8a38 ("bpf: fix precision backtracking
    instruction iteration") slightly changes the logic in the verifier which
    results in the verifier log growing. This results in the log being too
    small when loading the filterPowerSupplyEvents BPF program in Android,
    and therefore causing the program loading to fail. Because this
    program is labeled 'critical', a load failure forces a boot loop.
    
    This BPF program exists on the vendor partition, and therefore we must
    maintain the GRF/ treble boundary and modify the kernel logic.
    
    The kernel's bpf log logic is refactored in the 6.4 kernel and
    acknowledges the shortcomings of the existing approach which causes the
    program load to fail. Instead of backporting the significant changes,
    this change simply ignores the fact that the log is full.
    
    For more information see commit 121664093803 ("bpf: Switch BPF verifier
    log to be a rotating log by default")
    
    Bug: 432207940
    Bug: 433641053
    Test: verify pixel 6 boots on a 5.10 kernel including commit 973c7a0d8a38
    Change-Id: I35c3d2074dd9b39e44bfdbaf66fa56ec917df0a6
    Signed-off-by: Neill Kapron <nkapron@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -12743,8 +12743,14 @@ skip_full_check:
 	env->verification_time = ktime_get_ns() - start_time;
 	print_verification_stats(env);
 
-	if (log->level && bpf_verifier_log_full(log))
-		ret = -ENOSPC;
+	// ANDROID: Do not fail to load if log buffer passed in from userspace
+	// is too small. The bpf log logic is refactored in the 6.4 kernel
+	// acknowledging the shortcomings of this approch. Instead of backporting
+	// the significant changes, simply ignore the fact that the log is full.
+	// For more information see commit 121664093803: bpf: Switch BPF verifier
+	// log to be a rotating log by default
+	//if (log->level && bpf_verifier_log_full(log))
+	//	ret = -ENOSPC;
 	if (log->level && !log->ubuf) {
 		ret = -EFAULT;
 		goto err_release_maps;
