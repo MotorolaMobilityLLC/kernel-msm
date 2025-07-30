85c95989fa7fe5c8045b1217af9256f2758abacc
Daniel Borkmann <daniel@iogearbox.net>
2025-07-31 01:47:30 +0200

    bpf: Add cookie object to bpf maps
    [ Upstream commit 12df58ad294253ac1d8df0c9bb9cf726397a671d ]
    
    Add a cookie to BPF maps to uniquely identify BPF maps for the timespan
    when the node is up. This is different to comparing a pointer or BPF map
    id which could get rolled over and reused.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/r/20250730234733.530041-1-daniel@iogearbox.net
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -35,6 +35,7 @@
 #include <linux/rcupdate_trace.h>
 #include <linux/memcontrol.h>
 #include <linux/trace_events.h>
+#include <linux/cookie.h>
 #include <net/netfilter/nf_bpf_link.h>
 
 #include <net/tcx.h>
@@ -50,6 +51,7 @@
 #define BPF_OBJ_FLAG_MASK   (BPF_F_RDONLY | BPF_F_WRONLY)
 
 DEFINE_PER_CPU(int, bpf_prog_active);
+DEFINE_COOKIE(bpf_map_cookie);
 static DEFINE_IDR(prog_idr);
 static DEFINE_SPINLOCK(prog_idr_lock);
 static DEFINE_IDR(map_idr);
@@ -1252,6 +1254,10 @@ static int map_create(union bpf_attr *attr)
 			       sizeof(attr->map_name));
 	if (err < 0)
 		goto free_map;
+
+	preempt_disable();
+	map->cookie = gen_cookie_next(&bpf_map_cookie);
+	preempt_enable();
 
 	atomic64_set(&map->refcnt, 1);
 	atomic64_set(&map->usercnt, 1);
