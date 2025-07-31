65d483ef6d27ea0da1f8db8e787c5738fe0b0421
Chao Yu <chao@kernel.org>
2025-07-31 14:03:38 +0800

    FROMGIT: f2fs: fix condition in __allow_reserved_blocks()
    If reserve_root mount option is not assigned, __allow_reserved_blocks()
    will return false, it's not correct, fix it.
    
    Fixes: 7e65be49ed94 ("f2fs: add reserved blocks for root user")
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit e75ce117905d2830976a289e718470f3230fa30a https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git dev)
    
    Bug: 440276987
    Change-Id: Ie24d0d0b1b8bd81751f2210a138a4a7bc0914a12
    Signed-off-by: panchuang <panchuang@vivo.corp-partner.google.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2252,8 +2252,6 @@ static inline bool __allow_reserved_blocks(struct f2fs_sb_info *sbi,
 {
 	if (!inode)
 		return true;
-	if (!test_opt(sbi, RESERVE_ROOT))
-		return false;
 	if (IS_NOQUOTA(inode))
 		return true;
 	if (uid_eq(F2FS_OPTION(sbi).s_resuid, current_fsuid()))
@@ -2274,7 +2272,7 @@ static inline unsigned int get_available_block_count(struct f2fs_sb_info *sbi,
 	avail_user_block_count = sbi->user_block_count -
 					sbi->current_reserved_blocks;
 
-	if (!__allow_reserved_blocks(sbi, inode, cap))
+	if (test_opt(sbi, RESERVE_ROOT) && !__allow_reserved_blocks(sbi, inode, cap))
 		avail_user_block_count -= F2FS_OPTION(sbi).root_reserved_blocks;
 
 	if (F2FS_IO_ALIGNED(sbi))
