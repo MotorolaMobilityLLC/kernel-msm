de6e1f545841868bdbdb187218c43e8d257621ae
Filipe Manana <fdmanana@suse.com>
2025-08-06 12:11:31 +0100

    btrfs: fix race between setting last_dir_index_offset and inode logging
    [ Upstream commit 59a0dd4ab98970086fd096281b1606c506ff2698 ]
    
    At inode_logged() if we find that the inode was not logged before we
    update its ->last_dir_index_offset to (u64)-1 with the goal that the
    next directory log operation will see the (u64)-1 and then figure out
    it must check what was the index of the last logged dir index key and
    update ->last_dir_index_offset to that key's offset (this is done in
    update_last_dir_index_offset()).
    
    This however has a possibility for a time window where a race can happen
    and lead to directory logging skipping dir index keys that should be
    logged. The race happens l(CR) this:
    
    1) Task A calls inode_logged(), sees ->logged_trans as 0 and then checks
       that the inode item was logged before, but before it sets the inode's
       ->last_dir_index_offset to (u64)-1...
    
    2) Task B is at btrfs_log_inode() which calls inode_logged() early, and
       that has set ->last_dir_index_offset to (u64)-1;
    
    3) Task B then enters log_directory_changes() which calls
       update_last_dir_index_offset(). There it sees ->last_dir_index_offset
       is (u64)-1 and that the inode was logged before (ctx->logged_before is
       true), and so it searches for the last logged dir index key in the log
       tree and it finds that it has an offset (index) value of N, so it sets
       ->last_dir_index_offset to N, so that we can skip index keys that are
       less than or equal to N (later at process_dir_items_leaf());
    
    4) Task A now sets ->last_dir_index_offset to (u64)-1, undoing the update
       that task B just did;
    
    5) Task B will now skip every index key when it enters
       process_dir_items_leaf(), since ->last_dir_index_offset is (u64)-1.
    
    Fix this by making inode_logged() not touch ->last_dir_index_offset and
    initializing it to 0 when an inode is loaded (at btrfs_alloc_inode()) and
    then having update_last_dir_index_offset() treat a value of 0 as meaning
    we must check the log tree and update with the index of the last logged
    index key. This is fine since the minimum possible value for
    ->last_dir_index_offset is 1 (BTRFS_DIR_START_INDEX - 1 = 2 - 1 = 1).
    This also simplifies the management of ->last_dir_index_offset and now
    all accesses to it are done under the inode's log_mutex.
    
    Fixes: 0f8ce49821de ("btrfs: avoid inode logging during rename and link when possible")
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -8525,6 +8525,7 @@ struct inode *btrfs_alloc_inode(struct super_block *sb)
 	ei->last_sub_trans = 0;
 	ei->logged_trans = 0;
 	ei->delalloc_bytes = 0;
+	/* new_delalloc_bytes and last_dir_index_offset are in a union. */
 	ei->new_delalloc_bytes = 0;
 	ei->defrag_bytes = 0;
 	ei->disk_i_size = 0;
