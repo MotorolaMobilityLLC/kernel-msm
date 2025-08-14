0eafaa70fd4046f433d35d7032f68be175053a6f
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2025-08-14 10:20:42 -0700

    x86/vmscape: Add conditional IBPB mitigation
    Commit 2f8f173413f1cbf52660d04df92d0069c4306d25 upstream.
    
    VMSCAPE is a vulnerability that exploits insufficient branch predictor
    isolation between a guest and a userspace hypervisor (l(CR) QEMU). Existing
    mitigations already protect kernel/KVM from a malicious guest. Userspace
    can additionally be protected by flushing the branch predictors after a
    VMexit.
    
    Since it is the userspace that consumes the poisoned branch predictors,
    conditionally issue an IBPB after a VMexit and before returning to
    userspace. Workloads that frequently switch between hypervisor and
    userspace will incur the most overhead from the new IBPB.
    
    This new IBPB is not integrated with the existing IBPB sites. For
    instance, a task can use the existing speculation control prctl() to
    get an IBPB at context switch time. With this implementation, the
    IBPB is doubled up: one at context switch and another before running
    userspace.
    
    The intent is to integrate and optimize these cases post-embargo.
    
    [ dhansen: elaborate on suboptimal IBPB solution ]
    
    Suggested-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -559,6 +559,8 @@ void alternative_msr_write(unsigned int msr, u64 val, unsigned int feature)
 
 extern u64 x86_pred_cmd;
 
+DECLARE_PER_CPU(bool, x86_ibpb_exit_to_user);
+
 static inline void indirect_branch_prediction_barrier(void)
 {
 	alternative_msr_write(MSR_IA32_PRED_CMD, x86_pred_cmd, X86_FEATURE_USE_IBPB);
