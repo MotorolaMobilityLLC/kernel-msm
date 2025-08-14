3e0ca3c721c2319d5580d4c8773c30e07aa7b808
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2025-08-14 10:20:42 -0700

    x86/vmscape: Add conditional IBPB mitigation
    Commit 2f8f173413f1cbf52660d04df92d0069c4306d25 upstream.
    
    VMSCAPE is a vulnerability that exploits insufficient branch predictor
    isolation between a guest and a userspace hypervisor (l(CR) QEMU). Existing
    mitigations already protect kernel/KVM from a malicious guest. Userspace
    can additionally be protected by flushing the branch predictors after a
    VMexit.
    
    Since it is the userspace that consumes the poisoned branch predictors,
    conditionally issue an IBPB after a VMexit and before returning to
    userspace. Workloads that frequently switch between hypervisor and
    userspace will incur the most overhead from the new IBPB.
    
    This new IBPB is not integrated with the existing IBPB sites. For
    instance, a task can use the existing speculation control prctl() to
    get an IBPB at context switch time. With this implementation, the
    IBPB is doubled up: one at context switch and another before running
    userspace.
    
    The intent is to integrate and optimize these cases post-embargo.
    
    [ dhansen: elaborate on suboptimal IBPB solution ]
    
    Suggested-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -60,6 +60,14 @@ EXPORT_SYMBOL_GPL(x86_spec_ctrl_base);
 DEFINE_PER_CPU(u64, x86_spec_ctrl_current);
 EXPORT_SYMBOL_GPL(x86_spec_ctrl_current);
 
+/*
+ * Set when the CPU has run a potentially malicious guest. An IBPB will
+ * be needed to before running userspace. That IBPB will flush the branch
+ * predictor content.
+ */
+DEFINE_PER_CPU(bool, x86_ibpb_exit_to_user);
+EXPORT_PER_CPU_SYMBOL_GPL(x86_ibpb_exit_to_user);
+
 u64 x86_pred_cmd __ro_after_init = PRED_CMD_IBPB;
 EXPORT_SYMBOL_GPL(x86_pred_cmd);
 
