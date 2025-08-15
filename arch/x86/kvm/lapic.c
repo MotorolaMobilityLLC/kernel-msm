32d7c145e8667457b87640b6696cd475aabfe097
Sean Christopherson <seanjc@google.com>
2025-08-14 17:25:23 -0700

    KVM: x86: Plumb in the vCPU to kvm_x86_ops.hwapic_isr_update()
    [ Upstream commit 76bce9f10162cd4b36ac0b7889649b22baf70ebd ]
    
    Pass the target vCPU to the hwapic_isr_update() vendor hook so that VMX
    can defer the update until after nested VM-Exit if an EOI for L1's vAPIC
    occurs while L2 is active.
    
    Note, commit d39850f57d21 ("KVM: x86: Drop @vcpu parameter from
    kvm_x86_ops.hwapic_isr_update()") removed the parameter with the
    justification that doing so "allows for a decent amount of (future)
    cleanup in the APIC code", but it's not at all clear what cleanup was
    intended, or if it was ever realized.
    
    No functional change intended.
    
    Cc: stable@vger.kernel.org
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Tested-by: Chao Gao <chao.gao@intel.com>
    Link: https://lore.kernel.org/r/20241128000010.4051275-2-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [sean: account for lack of kvm_x86_call(), drop vmx/x86_ops.h change]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -750,7 +750,7 @@ static inline void apic_set_isr(int vec, struct kvm_lapic *apic)
 	 * just set SVI.
 	 */
 	if (unlikely(apic->apicv_active))
-		static_call_cond(kvm_x86_hwapic_isr_update)(vec);
+		static_call_cond(kvm_x86_hwapic_isr_update)(apic->vcpu, vec);
 	else {
 		++apic->isr_count;
 		BUG_ON(apic->isr_count > MAX_APIC_VECTOR);
@@ -795,7 +795,7 @@ static inline void apic_clear_isr(int vec, struct kvm_lapic *apic)
 	 * and must be left alone.
 	 */
 	if (unlikely(apic->apicv_active))
-		static_call_cond(kvm_x86_hwapic_isr_update)(apic_find_highest_isr(apic));
+		static_call_cond(kvm_x86_hwapic_isr_update)(apic->vcpu, apic_find_highest_isr(apic));
 	else {
 		--apic->isr_count;
 		BUG_ON(apic->isr_count < 0);
@@ -2772,7 +2772,7 @@ void kvm_lapic_reset(struct kvm_vcpu *vcpu, bool init_event)
 	if (apic->apicv_active) {
 		static_call_cond(kvm_x86_apicv_post_state_restore)(vcpu);
 		static_call_cond(kvm_x86_hwapic_irr_update)(vcpu, -1);
-		static_call_cond(kvm_x86_hwapic_isr_update)(-1);
+		static_call_cond(kvm_x86_hwapic_isr_update)(vcpu, -1);
 	}
 
 	vcpu->arch.apic_arb_prio = 0;
@@ -3072,7 +3072,7 @@ int kvm_apic_set_state(struct kvm_vcpu *vcpu, struct kvm_lapic_state *s)
 	if (apic->apicv_active) {
 		static_call_cond(kvm_x86_apicv_post_state_restore)(vcpu);
 		static_call_cond(kvm_x86_hwapic_irr_update)(vcpu, apic_find_highest_irr(apic));
-		static_call_cond(kvm_x86_hwapic_isr_update)(apic_find_highest_isr(apic));
+		static_call_cond(kvm_x86_hwapic_isr_update)(vcpu, apic_find_highest_isr(apic));
 	}
 	kvm_make_request(KVM_REQ_EVENT, vcpu);
 	if (ioapic_in_kernel(vcpu->kvm))
