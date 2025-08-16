6bbda87df7a116aa9629f8d4b67a0e03690879a4
Lukas Wunner <lukas@wunner.de>
2025-08-15 23:50:20 -0400

    PCI/ACPI: Fix runtime PM ref imbalance on Hot-Plug Capable ports
    [ Upstream commit 6cff20ce3b92ffbf2fc5eb9e5a030b3672aa414a ]
    
    pci_bridge_d3_possible() is called from both pcie_portdrv_probe() and
    pcie_portdrv_remove() to determine whether runtime power management shall
    be enabled (on probe) or disabled (on remove) on a PCIe port.
    
    The underlying assumption is that pci_bridge_d3_possible() always returns
    the same value, else a runtime PM reference imbalance would occur.  That
    assumption is not given if the PCIe port is inaccessible on remove due to
    hot-unplug:  pci_bridge_d3_possible() calls pciehp_is_native(), which
    accesses Config Space to determine whether the port is Hot-Plug Capable.
    An inaccessible port returns "all ones", which is converted to "all
    zeroes" by pcie_capability_read_dword().  Hence the port no longer seems
    Hot-Plug Capable on remove even though it was on probe.
    
    The resulting runtime PM ref imbalance causes warning messages such as:
    
      pcieport 0000:02:04.0: Runtime PM usage count underflow!
    
    Avoid the Config Space access (and thus the runtime PM ref imbalance) by
    caching the Hot-Plug Capable bit in struct pci_dev.
    
    The struct already contains an "is_hotplug_bridge" flag, which however is
    not only set on Hot-Plug Capable PCIe ports, but also Conventional PCI
    Hot-Plug bridges and ACPI slots.  The flag identifies bridges which are
    allocated additional MMIO and bus number resources to allow for hierarchy
    expansion.
    
    The kernel is somewhat sloppily using "is_hotplug_bridge" in a number of
    places to identify Hot-Plug Capable PCIe ports, even though the flag
    encompasses other devices.  Subsequent commits replace these occurrences
    with the new flag to clearly delineate Hot-Plug Capable PCIe ports from
    other kinds of hotplug bridges.
    
    Document the existing "is_hotplug_bridge" and the new "is_pciehp" flag
    and document the (non-obvious) requirement that pci_bridge_d3_possible()
    always returns the same value across the entire lifetime of a bridge,
    including its hot-removal.
    
    Fixes: 5352a44a561d ("PCI: pciehp: Make pciehp_is_native() stricter")
    Reported-by: Laurent Bigonville <bigon@bigon.be>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=220216
    Reported-by: Mario Limonciello <mario.limonciello@amd.com>
    Closes: https://lore.kernel.org/r/20250609020223.269407-3-superm1@kernel.org/
    Link: https://lore.kernel.org/all/20250620025535.3425049-3-superm1@kernel.org/T/#u
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Rafael J. Wysocki <rafael@kernel.org>
    Cc: stable@vger.kernel.org # v4.18+
    Link: https://patch.msgid.link/fe5dcc3b2e62ee1df7905d746bde161eb1b3291c.1752390101.git.lukas@wunner.de
    [ changed "recent enough PCIe ports" comment to "some PCIe ports" ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -3065,8 +3065,12 @@ static const struct dmi_system_id bridge_d3_blacklist[] = {
  * pci_bridge_d3_possible - Is it possible to put the bridge into D3
  * @bridge: Bridge to check
  *
- * This function checks if it is possible to move the bridge to D3.
- * Currently we only allow D3 for recent enough PCIe ports and Thunderbolt.
+ * Currently we only allow D3 for some PCIe ports and for Thunderbolt.
+ *
+ * Return: Whether it is possible to move the bridge to D3.
+ *
+ * The return value is guaranteed to be constant across the entire lifetime
+ * of the bridge, including its hot-removal.
  */
 bool pci_bridge_d3_possible(struct pci_dev *bridge)
 {
