30a1c1a0774bdfcb36b3edba65977a0666d9893f
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2025-08-20 17:04:00 -0400

    Bluetooth: hci_event: Detect if HCI_EV_NUM_COMP_PKTS is unbalanced
    [ Upstream commit 15bf2c6391bafb14a3020d06ec0761bce0803463 ]
    
    This attempts to detect if HCI_EV_NUM_COMP_PKTS contain an unbalanced
    (more than currently considered outstanding) number of packets otherwise
    it could cause the hcon->sent to underflow and loop around breaking the
    tracking of the outstanding packets pending acknowledgment.
    
    Fixes: f42809185896 ("Bluetooth: Simplify num_comp_pkts_evt function")
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -4392,7 +4392,17 @@ static void hci_num_comp_pkts_evt(struct hci_dev *hdev, void *data,
 		if (!conn)
 			continue;
 
-		conn->sent -= count;
+		/* Check if there is really enough packets outstanding before
+		 * attempting to decrease the sent counter otherwise it could
+		 * underflow..
+		 */
+		if (conn->sent >= count) {
+			conn->sent -= count;
+		} else {
+			bt_dev_warn(hdev, "hcon %p sent %u < count %u",
+				    conn, conn->sent, count);
+			conn->sent = 0;
+		}
 
 		switch (conn->type) {
 		case ACL_LINK:
