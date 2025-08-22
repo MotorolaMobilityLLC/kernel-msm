99696ed53fde88f7f0023901ed0f34849ac9c64f
Richard Zhu <hongxing.zhu@nxp.com>
2025-08-22 16:27:13 -0400

    PCI: imx6: Add IMX8MM_EP and IMX8MP_EP fixed 256-byte BAR 4 in epc_features
    [ Upstream commit 399444a87acdea5d21c218bc8e9b621fea1cd218 ]
    
    For IMX8MM_EP and IMX8MP_EP, add fixed 256-byte BAR 4 and reserved BAR 5
    in imx8m_pcie_epc_features.
    
    Fixes: 75c2f26da03f ("PCI: imx6: Add i.MX PCIe EP mode support")
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
    [bhelgaas: add details in subject]
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20250708091003.2582846-3-hongxing.zhu@nxp.com
    [ Adapted BAR configuration to use reserved_bar bitmap and bar_fixed_size ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -1043,7 +1043,10 @@ static const struct pci_epc_features imx8m_pcie_epc_features = {
 	.linkup_notifier = false,
 	.msi_capable = true,
 	.msix_capable = false,
-	.reserved_bar = 1 << BAR_1 | 1 << BAR_3,
+	.reserved_bar = 1 << BAR_1 | 1 << BAR_3 | 1 << BAR_5,
+	.bar_fixed_size = {
+		[BAR_4] = SZ_256,
+	},
 	.align = SZ_64K,
 };
 
