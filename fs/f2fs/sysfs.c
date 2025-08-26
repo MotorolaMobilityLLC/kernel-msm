455f7e1124485bd58c59d7a781669f87ffb7a919
Chakradhar Gajjala <gajjalac@motorola.com>
2025-08-26 14:25:00 +0530

    Merge source code with GKI android14-6.1-2025-05_r5 for Velar
    PROPAGATED_from (CR)
    
    Merge source code with GKI from android14-6.1-2025-05_r2
    to android14-6.1-2025-05_r5
    
    ACK Merge list:
    [From] d32955d FROMGIT: f2fs: support to disable linear lookup fallback by Chao Yu
    [To]   66e758f UPSTREAM: af_unix: Fix uninit-value in __unix_walk_scc() by Shigeru Yoshida
    
    Change-Id: I15c9dcdab219eef3913b2b56cd7f4bc37001750d
    Signed-off-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3529870
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Navya Vijayan <navya@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 1674cb74984c14254b1f4d32c135da35ba2b0894)
    Reviewed-on: https://gerrit.mot.com/3564269
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Andrea dos Santos <andreads@motorola.com>
    Submit-Approved: Andrea dos Santos <andreads@motorola.com>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -235,6 +235,13 @@ static ssize_t encoding_show(struct f2fs_attr *a,
 	return sysfs_emit(buf, "(none)\n");
 }
 
+static ssize_t encoding_flags_show(struct f2fs_attr *a,
+		struct f2fs_sb_info *sbi, char *buf)
+{
+	return sysfs_emit(buf, "%x\n",
+		le16_to_cpu(F2FS_RAW_SUPER(sbi)->s_encoding_flags));
+}
+
 static ssize_t mounted_time_sec_show(struct f2fs_attr *a,
 		struct f2fs_sb_info *sbi, char *buf)
 {
@@ -918,6 +925,7 @@ F2FS_GENERAL_RO_ATTR(features);
 F2FS_GENERAL_RO_ATTR(current_reserved_blocks);
 F2FS_GENERAL_RO_ATTR(unusable);
 F2FS_GENERAL_RO_ATTR(encoding);
+F2FS_GENERAL_RO_ATTR(encoding_flags);
 F2FS_GENERAL_RO_ATTR(mounted_time_sec);
 F2FS_GENERAL_RO_ATTR(main_blkaddr);
 F2FS_GENERAL_RO_ATTR(pending_discard);
@@ -969,6 +977,9 @@ F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, compress_percent, compress_percent);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, compress_watermark, compress_watermark);
 #endif
 F2FS_FEATURE_RO_ATTR(pin_file);
+#ifdef CONFIG_UNICODE
+F2FS_FEATURE_RO_ATTR(linear_lookup);
+#endif
 
 /* For ATGC */
 F2FS_RW_ATTR(ATGC_INFO, atgc_management, atgc_candidate_ratio, candidate_ratio);
@@ -1057,6 +1068,7 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(reserved_blocks),
 	ATTR_LIST(current_reserved_blocks),
 	ATTR_LIST(encoding),
+	ATTR_LIST(encoding_flags),
 	ATTR_LIST(mounted_time_sec),
 #ifdef CONFIG_F2FS_STAT_FS
 	ATTR_LIST(cp_foreground_calls),
@@ -1129,6 +1141,9 @@ static struct attribute *f2fs_feat_attrs[] = {
 	ATTR_LIST(compression),
 #endif
 	ATTR_LIST(pin_file),
+#ifdef CONFIG_UNICODE
+	ATTR_LIST(linear_lookup),
+#endif
 	NULL,
 };
 ATTRIBUTE_GROUPS(f2fs_feat);
