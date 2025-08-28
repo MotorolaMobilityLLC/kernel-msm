44bc1ea6b1866c2e9fe84ba4431af0daed9dced3
Sebastian Ene <sebastianene@google.com>
2025-08-28 07:42:01 +0000

    ANDROID: KVM: Fix ToCToU issue when admitting pages in memcache
    Use a local copy of the host provided data to prevent a time
    of check time of use when we give pages to the hypervisor
    memcache.
    
    Bug: 441436872
    Change-Id: I548e2721dc324124cf11f56cd3eb497a8600cdd7
    Signed-off-by: Sebastian Ene <sebastianene@google.com>
    Signed-off-by: Keir Fraser <keirf@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -221,15 +221,17 @@ void __repudiate_host_page(void *addr, unsigned long order,
 
 int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 {
+	struct kvm_hyp_memcache tmp = *host_mc;
+
 	if (!kvm_iommu_ops)
 		return -EINVAL;
 
 	/* Paired with smp_wmb() in kvm_iommu_init() */
 	smp_rmb();
 
-	while (host_mc->nr_pages) {
-		unsigned long order = FIELD_GET(~PAGE_MASK, host_mc->head);
-		phys_addr_t phys = host_mc->head & PAGE_MASK;
+	while (tmp.nr_pages) {
+		unsigned long order = FIELD_GET(~PAGE_MASK, tmp.head);
+		phys_addr_t phys = tmp.head & PAGE_MASK;
 		struct hyp_pool *pool = &iommu_system_pool;
 		u64 nr_pages;
 		void *addr;
@@ -238,16 +240,18 @@ int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 		    !IS_ALIGNED(phys, PAGE_SIZE << order))
 			return -EINVAL;
 
-		addr = admit_host_page(host_mc, order);
+		addr = admit_host_page(&tmp, order);
 		if (!addr)
 			return -EINVAL;
+		*host_mc = tmp;
 
 		if (kvm_iommu_donate_from_cma(phys, order)) {
 			hyp_spin_lock(&__block_pools_lock);
 			pool = __get_empty_block_pool(phys);
 			hyp_spin_unlock(&__block_pools_lock);
 			if (!pool) {
-				__repudiate_host_page(addr, order, host_mc);
+				__repudiate_host_page(addr, order, &tmp);
+				*host_mc = tmp;
 				return -EBUSY;
 			}
 		} else {
