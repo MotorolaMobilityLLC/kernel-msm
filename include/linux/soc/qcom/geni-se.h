34f5c8fad959e5c4a328fc82088308e473f9f319
qianlong <qianl2@motorola.com>
2025-08-28 14:44:45 +0800

    kernel sync with produ-sm8635-b1
    PROPAGATED_from (CR)
    
    Some watchdog met after for kernel upgrade from upgrade 6.1.84 up to 6.1.134.
    But same upgrade with produ-sm8635-b1 no watchdog. so compare different
    sync with produ-sm8635-b1 try fix watchdog issue.
    
    Change-Id: Ia355995cddd655999f1cb6ce142e310cf5447def
    Signed-off-by: qianlong <qianl2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3529539
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiang Nie <nieqiang@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 83ed56e389e28327f07e6e3ba1cd27e3884dd6d4)
    Reviewed-on: https://gerrit.mot.com/3542522
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Navya Vijayan <navya@motorola.com>
    (cherry picked from commit 1bf90bd4ab8e2ecae41bda83bb745e52c7dfe890)
    Reviewed-on: https://gerrit.mot.com/3564267
    Reviewed-by: Obi-Wan Kenobi
    Submit-Approved: Andrea dos Santos <andreads@motorola.com>
    Reviewed-by: Andrea dos Santos <andreads@motorola.com>

--- include/linux/soc/qcom/geni-se.h
+++ include/linux/soc/qcom/geni-se.h
@@ -88,15 +88,11 @@ struct geni_se {
 #define SE_GENI_M_IRQ_STATUS		0x610
 #define SE_GENI_M_IRQ_EN		0x614
 #define SE_GENI_M_IRQ_CLEAR		0x618
-#define SE_GENI_M_IRQ_EN_SET		0x61c
-#define SE_GENI_M_IRQ_EN_CLEAR		0x620
 #define SE_GENI_S_CMD0			0x630
 #define SE_GENI_S_CMD_CTRL_REG		0x634
 #define SE_GENI_S_IRQ_STATUS		0x640
 #define SE_GENI_S_IRQ_EN		0x644
 #define SE_GENI_S_IRQ_CLEAR		0x648
-#define SE_GENI_S_IRQ_EN_SET		0x64c
-#define SE_GENI_S_IRQ_EN_CLEAR		0x650
 #define SE_GENI_TX_FIFOn		0x700
 #define SE_GENI_RX_FIFOn		0x780
 #define SE_GENI_TX_FIFO_STATUS		0x800
@@ -120,9 +116,6 @@ struct geni_se {
 
 /* GENI_FORCE_DEFAULT_REG fields */
 #define FORCE_DEFAULT	BIT(0)
-
-/* GENI_OUTPUT_CTRL fields */
-#define GENI_IO_MUX_0_EN		BIT(0)
 
 /* GENI_STATUS fields */
 #define M_GENI_CMD_ACTIVE		BIT(0)
@@ -240,9 +233,6 @@ struct geni_se {
 /* SE_GENI_IOS fields */
 #define IO2_DATA_IN			BIT(1)
 #define RX_DATA_IN			BIT(0)
-
-/* SE_GENI_M_GP_LENGTH and SE_GENI_S_GP_LENGTH fields */
-#define GP_LENGTH			GENMASK(31, 0)
 
 /* SE_DMA_TX_IRQ_STAT Register fields */
 #define TX_DMA_DONE			BIT(0)
