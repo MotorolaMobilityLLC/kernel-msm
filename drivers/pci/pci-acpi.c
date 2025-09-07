90a78a30a30d9bdb8bc677d021930b8e577c4643
Greg Kroah-Hartman <gregkh@google.com>
2025-09-07 06:50:10 +0000

    Revert "PCI/ACPI: Fix runtime PM ref imbalance on Hot-Plug Capable ports"
    This reverts commit 827f7b4fe05120431083ec367b6f36ff045fa52e which is
    commit 6cff20ce3b92ffbf2fc5eb9e5a030b3672aa414a upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I0bdbaefb2c64a1186640bd5eaf98b522447db982
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -815,11 +815,13 @@ int pci_acpi_program_hp_params(struct pci_dev *dev)
 bool pciehp_is_native(struct pci_dev *bridge)
 {
 	const struct pci_host_bridge *host;
+	u32 slot_cap;
 
 	if (!IS_ENABLED(CONFIG_HOTPLUG_PCI_PCIE))
 		return false;
 
-	if (!bridge->is_pciehp)
+	pcie_capability_read_dword(bridge, PCI_EXP_SLTCAP, &slot_cap);
+	if (!(slot_cap & PCI_EXP_SLTCAP_HPC))
 		return false;
 
 	if (pcie_ports_native)
--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -3020,12 +3020,8 @@ static const struct dmi_system_id bridge_d3_blacklist[] = {
  * pci_bridge_d3_possible - Is it possible to put the bridge into D3
  * @bridge: Bridge to check
  *
- * Currently we only allow D3 for some PCIe ports and for Thunderbolt.
- *
- * Return: Whether it is possible to move the bridge to D3.
- *
- * The return value is guaranteed to be constant across the entire lifetime
- * of the bridge, including its hot-removal.
+ * This function checks if it is possible to move the bridge to D3.
+ * Currently we only allow D3 for recent enough PCIe ports and Thunderbolt.
  */
 bool pci_bridge_d3_possible(struct pci_dev *bridge)
 {
