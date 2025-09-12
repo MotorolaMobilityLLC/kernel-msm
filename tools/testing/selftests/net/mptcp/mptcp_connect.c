b590519747fd81635e5f913972cfb7b4a6b48d8a
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-09-12 14:25:52 +0200

    selftests: mptcp: avoid spurious errors on TCP disconnect
    commit 8708c5d8b3fb3f6d5d3b9e6bfe01a505819f519a upstream.
    
    The disconnect test-case, with 'plain' TCP sockets generates spurious
    errors, e.g.
    
      07 ns1 TCP   -> ns1 (dead:beef:1::1:10006) MPTCP
      read: Connection reset by peer
      read: Connection reset by peer
      (duration   155ms) [FAIL] client exit code 3, server 3
    
      netns ns1-FloSdv (listener) socket stat for 10006:
      TcpActiveOpens                  2                  0.0
      TcpPassiveOpens                 2                  0.0
      TcpEstabResets                  2                  0.0
      TcpInSegs                       274                0.0
      TcpOutSegs                      276                0.0
      TcpOutRsts                      3                  0.0
      TcpExtPruneCalled               2                  0.0
      TcpExtRcvPruned                 1                  0.0
      TcpExtTCPPureAcks               104                0.0
      TcpExtTCPRcvCollapsed           2                  0.0
      TcpExtTCPBacklogCoalesce        42                 0.0
      TcpExtTCPRcvCoalesce            43                 0.0
      TcpExtTCPChallengeACK           1                  0.0
      TcpExtTCPFromZeroWindowAdv      42                 0.0
      TcpExtTCPToZeroWindowAdv        41                 0.0
      TcpExtTCPWantZeroWindowAdv      13                 0.0
      TcpExtTCPOrigDataSent           164                0.0
      TcpExtTCPDelivered              165                0.0
      TcpExtTCPRcvQDrop               1                  0.0
    
    In the failing scenarios (TCP -> MPTCP), the involved sockets are
    actually plain TCP ones, as fallbacks for passive sockets at 2WHS time
    cause the MPTCP listeners to actually create 'plain' TCP sockets.
    
    Similar to commit 218cc166321f ("selftests: mptcp: avoid spurious errors
    on disconnect"), the root cause is in the user-space bits: the test
    program tries to disconnect as soon as all the pending data has been
    spooled, generating an RST. If such option reaches the peer before the
    connection has reached the closed status, the TCP socket will report an
    error to the user-space, as per protocol specification, causing the
    above failure. Note that it looks l(CR) this issue got more visible since
    the "tcp: receiver changes" series from commit 06baf9bfa6ca ("Merge
    branch 'tcp-receiver-changes'").
    
    Address the issue by explicitly waiting for the TCP sockets (-t) to
    reach a closed status before performing the disconnect. More precisely,
    the test program now waits for plain TCP sockets or TCP subflows in
    addition to the MPTCP sockets that were already monitored.
    
    While at it, use 'ss' with '-n' to avoid resolving service names, which
    is not needed here.
    
    Fixes: 218cc166321f ("selftests: mptcp: avoid spurious errors on disconnect")
    Cc: stable@vger.kernel.org
    Suggested-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Reviewed-by: Geliang Tang <geliang@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20250912-net-mptcp-fix-sft-connect-v1-3-d40e77cbbf02@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/testing/selftests/net/mptcp/mptcp_connect.c
+++ tools/testing/selftests/net/mptcp/mptcp_connect.c
@@ -1234,7 +1234,7 @@ void xdisconnect(int fd)
 	else
 		xerror("bad family");
 
-	strcpy(cmd, "ss -M | grep -q ");
+	strcpy(cmd, "ss -Mnt | grep -q ");
 	cmdlen = strlen(cmd);
 	if (!inet_ntop(addr.ss_family, raw_addr, &cmd[cmdlen],
 		       sizeof(cmd) - cmdlen))
@@ -1244,7 +1244,7 @@ void xdisconnect(int fd)
 
 	/*
 	 * wait until the pending data is completely flushed and all
-	 * the MPTCP sockets reached the closed status.
+	 * the sockets reached the closed status.
 	 * disconnect will bypass/ignore/drop any pending data.
 	 */
 	for (i = 0; ; i += msec_sleep) {
