f1a0000789f28183ab2a2aa689e96500a1cdb04d
liuyc20 <liuyc20@lenovo.com>
2025-09-16 14:10:46 +0800

    usbaudio: Add a delay of 5-6ms at each control message handling for usbaudio
    Solve the problem of potential silence when playing with certain headphones
    quirk_flags |= QUIRK_FLAG_CTL_MSG_DELAY_5M
    -->Add a delay of 5-6ms at each control message handling
    
    Change-Id: I688ef635be38acb600b781e8ebe255bf6ee23a27
    Signed-off-by: liuyc20 <liuyc20@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3557352
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- sound/usb/usb_audio_qmi_svc.c
+++ sound/usb/usb_audio_qmi_svc.c
@@ -993,6 +993,7 @@ static void uaudio_connect(struct snd_usb_audio *chip)
 
 	uadev[chip->card->number].chip = chip;
 	uadev[chip->card->number].sb = sb;
+	uadev[chip->card->number].chip->quirk_flags |= QUIRK_FLAG_CTL_MSG_DELAY_5M;
 }
 
 static void uaudio_disconnect(struct snd_usb_audio *chip)
