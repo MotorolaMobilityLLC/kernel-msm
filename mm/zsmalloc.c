28b4823ccb502d0d2c2a1c48ed8b85518fb2f83f
Linus Torvalds <torvalds@linux-foundation.org>
2025-09-29 17:17:22 +0000

    minmax: make generic MIN() and MAX() macros available everywhere
    [ Upstream commit 1a251f52cfdc417c84411a056bc142cbd77baef4 ]
    
    This just standardizes the use of MIN() and MAX() macros, with the very
    traditional semantics.  The goal is to use these for C constant
    expressions and for top-level / static initializers, and so be able to
    simplify the min()/max() macros.
    
    These macro names were used by various kernel code - they are very
    traditional, after all - and all such users have been fixed up, with a
    few different approaches:
    
     - trivial duplicated macro definitions have been removed
    
       Note that 'trivial' here means that it's obviously kernel code that
       already included all the major kernel headers, and thus gets the new
       generic MIN/MAX macros automatically.
    
     - non-trivial duplicated macro definitions are guarded with #ifndef
    
       This is the "yes, they define their own versions, but no, the include
       situation is not entirely obvious, and maybe they don't get the
       generic version automatically" case.
    
     - strange use case #1
    
       A couple of drivers decided that the way they want to describe their
       versioning is with
    
    	#define MAJ 1
    	#define MIN 2
    	#define DRV_VERSION __stringify(MAJ) "." __stringify(MIN)
    
       which adds zero value and I just did my Alexander the Great
       impersonation, and rewrote that pointless Gordian knot as
    
    	#define DRV_VERSION "1.2"
    
       instead.
    
     - strange use case #2
    
       A couple of drivers thought that it's a good idea to have a random
       'MIN' or 'MAX' define for a value or index into a table, rather than
       the traditional macro that takes arguments.
    
       These values were re-written as C enum's instead. The new
       function-line macros only expand when followed by an open
       parenthesis, and thus don't clash with enum use.
    
    Happily, there weren't really all that many of these cases, and a lot of
    users already had the pattern of using '#ifndef' guarding (or in one
    case just using '#undef MIN') before defining their own private version
    that does the same thing. I left such cases alone.
    
    Cc: David Laight <David.Laight@aculab.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Eliav Farber <farbere@amazon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -119,8 +119,6 @@
 #define ISOLATED_BITS	5
 #define MAGIC_VAL_BITS	8
 
-#define MAX(a, b) ((a) >= (b) ? (a) : (b))
-
 #define ZS_MAX_PAGES_PER_ZSPAGE	(_AC(CONFIG_ZSMALLOC_CHAIN_SIZE, UL))
 
 /* ZS_MIN_ALLOC_SIZE must be multiple of ZS_ALIGN */
