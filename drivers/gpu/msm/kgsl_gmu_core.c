399e7530e61660273a341e0e3faadebe008d8fac
Aditya Sherawat <asherawa@qti.qualcomm.com>
2025-09-30 06:48:08 -0700

    kgsl: Check for errors from mapping sg lists in the iommu
    Error returns from iommu_map_sg() were being ignored. Check for the result
    of this API correctly.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2025-47397
    
    Change-Id: I92cf4d4ce5f7a4937097b06c34406cf16ad174ef
    Signed-off-by: Lynus Vaz <lvaz@qti.qualcomm.com>
    Signed-off-by: Aditya Sherawat <asherawa@qti.qualcomm.com>
    Signed-off-by: Ashutosh Verma <ashverma@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3624115
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Sipra Patel <siprap@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3750327
    Reviewed-by: Navya Vijayan <navya@motorola.com>

--- drivers/gpu/msm/kgsl_gmu_core.c
+++ drivers/gpu/msm/kgsl_gmu_core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/iopoll.h>
@@ -186,7 +186,7 @@ int gmu_core_timed_poll_check(struct kgsl_device *device,
 int gmu_core_map_memdesc(struct iommu_domain *domain, struct kgsl_memdesc *memdesc,
 		u64 gmuaddr, int attrs)
 {
-	size_t mapped;
+	ssize_t mapped;
 
 	if (!memdesc->pages) {
 		mapped = iommu_map_sg(domain, gmuaddr, memdesc->sgt->sgl,
@@ -205,5 +205,8 @@ int gmu_core_map_memdesc(struct iommu_domain *domain, struct kgsl_memdesc *memde
 		sg_free_table(&sgt);
 	}
 
-	return mapped == 0 ? -ENOMEM : 0;
+	if (!mapped)
+		mapped = -ENOMEM;
+
+	return (mapped < 0) ? mapped : 0;
 }
