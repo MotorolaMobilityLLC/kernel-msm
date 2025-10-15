3d001347313b876ed7026e98ad54b3a6921d7ffb
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2025-10-15 11:19:34 +0200

    sched: Remove never used code in mm_cid_get()
    [ Upstream commit 53abe3e1c154628cc74e33a1bfcd865656e433a5 ]
    
    Clang is not happy with set but unused variable (this is visible
    with `make W=1` build:
    
      kernel/sched/sched.h:3744:18: error: variable 'cpumask' set but not used [-Werror,-Wunused-but-set-variable]
    
    It seems l(CR) the variable was never used along with the assignment
    that does not have side effects as far as I can see.  Remove those
    altogether.
    
    Fixes: 223baf9d17f2 ("sched: Fix performance regression introduced by mm_cid")
    Signed-off-by: Perry Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Eric Biggers <ebiggers@kernel.org>
    Reviewed-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3435,11 +3435,9 @@ end:
 static inline int mm_cid_get(struct rq *rq, struct mm_struct *mm)
 {
 	struct mm_cid __percpu *pcpu_cid = mm->pcpu_cid;
-	struct cpumask *cpumask;
 	int cid;
 
 	lockdep_assert_rq_held(rq);
-	cpumask = mm_cidmask(mm);
 	cid = __this_cpu_read(pcpu_cid->cid);
 	if (mm_cid_is_valid(cid)) {
 		mm_cid_snapshot_time(rq, mm);
