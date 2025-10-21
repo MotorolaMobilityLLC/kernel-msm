b1f6b0c5fa68586ad95a007574b8e8457ca41661
Yonghua Yan <yanyh2@motorola.com>
2025-10-21 15:49:42 +0800

    Add delay work to register panel
    As the panel register failing in probe will break the battery
    charger driver probe and register power supply in the case that
    there is no active panel in board test or the panel driver is
    loaded after battery charger driver.
    
    In order to avoid this issue, add a delay work to do the panel
    register and retry before it reaches the times predefined.
    
    Change-Id: I43ee347349f05a2650f41ec67a3a8b04d728086e
    Signed-off-by: Yonghua Yan <yanyh2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3599362
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -304,10 +304,12 @@ struct battery_chg_dev {
 	struct completion		fw_update_ack;
 	struct psy_state		psy_list[PSY_TYPE_MAX];
 	struct dentry			*debugfs_dir;
-        void                            *notifier_cookie[BC_NOTIFY_PANEL_MAX];
-        bool                            notify_vote[BC_NOTIFY_PANEL_MAX];
+	void				*notifier_cookie[BC_NOTIFY_PANEL_MAX];
+	bool				notify_vote[BC_NOTIFY_PANEL_MAX];
+	struct delayed_work		panel_register_work;
+	int				panel_register_retry_cnt;
 	/* extcon for VBUS/ID notification for USB for micro USB */
-	struct extcon_dev		*extcon;
+        struct extcon_dev               *extcon;
 	u32				*thermal_levels;
 	const char			*wls_fw_name;
 	int				curr_thermal_level;
@@ -3016,6 +3018,32 @@ static int register_extcon_conn_type(struct battery_chg_dev *bcdev)
 	return rc;
 }
 
+#define BC_PANEL_REGISTER_DELAY_MS 500
+#define BC_PANEL_REGISTER_RETRY_CNT 20
+static void battery_chg_panel_register_work(struct work_struct *work)
+{
+	struct battery_chg_dev *bcdev = container_of(work,
+					struct battery_chg_dev,
+					panel_register_work.work);
+	int rc;
+	int i;
+
+	for (i = 0; i < BC_NOTIFY_PANEL_MAX && i < ARRAY_SIZE(notify_panel_cfg); i++) {
+		if (bcdev->notifier_cookie[i])
+			continue;
+		rc = battery_chg_register_panel_notifier(bcdev, &notify_panel_cfg[i]);
+		if (rc == -EPROBE_DEFER &&
+		    bcdev->panel_register_retry_cnt < BC_PANEL_REGISTER_RETRY_CNT) {
+			schedule_delayed_work(&bcdev->panel_register_work,
+					msecs_to_jiffies(BC_PANEL_REGISTER_DELAY_MS));
+			bcdev->panel_register_retry_cnt++;
+		} else if (rc < 0) {
+			dev_err(bcdev->dev, "Error in registering panel %s, rc=%d\n",
+				notify_panel_cfg[i].panel_name, rc);
+		}
+	}
+}
+
 static int battery_chg_probe(struct platform_device *pdev)
 {
 	struct battery_chg_dev *bcdev;
@@ -3069,13 +3097,8 @@ static int battery_chg_probe(struct platform_device *pdev)
 	INIT_WORK(&bcdev->subsys_up_work, battery_chg_subsys_up_work);
 	INIT_WORK(&bcdev->usb_type_work, battery_chg_update_usb_type_work);
 	INIT_WORK(&bcdev->battery_check_work, battery_chg_check_status_work);
+	INIT_DELAYED_WORK(&bcdev->panel_register_work, battery_chg_panel_register_work);
 	bcdev->dev = dev;
-
-	for (i = 0; i < BC_NOTIFY_PANEL_MAX && i < ARRAY_SIZE(notify_panel_cfg); i++) {
-		rc = battery_chg_register_panel_notifier(bcdev, &notify_panel_cfg[i]);
-		if (rc < 0)
-			goto reg_error;
-	}
 
 	client_data.id = MSG_OWNER_BC;
 	client_data.name = "battery_charger";
@@ -3089,7 +3112,7 @@ static int battery_chg_probe(struct platform_device *pdev)
 		if (rc != -EPROBE_DEFER)
 			dev_err(dev, "Error in registering with pmic_glink %d\n",
 				rc);
-		goto reg_error;
+		return rc;
 	}
 
 	down_write(&bcdev->state_sem);
@@ -3180,6 +3203,7 @@ static int battery_chg_probe(struct platform_device *pdev)
 	}
 
 	schedule_work(&bcdev->usb_type_work);
+	schedule_delayed_work(&bcdev->panel_register_work, msecs_to_jiffies(0));
 
 	rc = get_charge_control_en(bcdev);
 	if (rc < 0)
@@ -3198,11 +3222,6 @@ error:
 	complete(&bcdev->ack);
 	unregister_reboot_notifier(&bcdev->reboot_notifier);
 	pmic_glink_unregister_client(bcdev->client);
-reg_error:
-	for (i = 0; i < BC_NOTIFY_PANEL_MAX; i++) {
-		if (bcdev->notifier_cookie[i])
-			panel_event_notifier_unregister(bcdev->notifier_cookie[i]);
-	}
 	return rc;
 }
 
@@ -3216,10 +3235,13 @@ static void battery_chg_remove(struct platform_device *pdev)
 	bcdev->initialized = false;
 	up_write(&bcdev->state_sem);
 
+	cancel_delayed_work(&bcdev->panel_register_work);
 	for (i = 0; i < BC_NOTIFY_PANEL_MAX; i++) {
-		if (bcdev->notifier_cookie[i])
-			panel_event_notifier_unregister(bcdev->notifier_cookie[i]);
+		if (!bcdev->notifier_cookie[i])
+			continue;
+		panel_event_notifier_unregister(bcdev->notifier_cookie[i]);
 	}
+	bcdev->panel_register_retry_cnt = 0;
 
 	device_init_wakeup(bcdev->dev, false);
 	debugfs_remove_recursive(bcdev->debugfs_dir);
