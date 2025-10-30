2e6c705e4b9603f1212670e786cacd3fac21fa2f
liuyc20 <liuyc20@lenovo.com>
2025-10-30 11:50:47 +0800

    USB: gsi_bind success then return 0
    fix the ramdump issue if enable the usb rndis function,
    As gsi_bind success,but not return and continue exec rndis_deregister
    so the memory issue happen.
    
    ail = 0),
     | magic = 0,
     | owner_cpu = 0,
     | owner = 0x0)))
     -013 |rndis_indicate_status_msg()  @usb_f_gsi  |  params = 0xFFFFFF8861013980 // gsi->params
     -014 |rndis_signal_connect()  @usb_f_gsi
     -015 |gsi_rndis_open(inline)
     -015 |gsi_handle_data_alt(inline)
     |  gsi = 0xFFFFFF80443D9800 -> (
     | params = 0xFFFFFF8861013980 -> (
     -015 |gsi_set_alt()  @usb_f_gsi
     -016 |set_config()
     -017 |composite_setup()
     -018 |android_setup()
     -019 |dwc3_ep0_delegate_req(inline)
     -019 |dwc3_ep0_set_config(inline)
     -019 |dwc3_ep0_std_request(inline)
     -019 |dwc3_ep0_inspect_setup(inline)
     -019 |dwc3_ep0_xfer_complete(inline)
     -019 |dwc3_ep0_interrupt()
     -020 |dwc3_endpoint_interrupt(inline)
     -020 |dwc3_process_event_entry(inline)
     -020 |dwc3_process_event_buf(inline)
     -020 |dwc3_thread_interrupt()
     -021 |irq_thread_fn()
     -022 |irq_thread()
     -023 |kthread()
     -024 |ret_from_fork(asm)
      --- |end of frame
    
    Change-Id: I7295ba8458fdb99d9209c59eb6a3a57436c17dde
    Signed-off-by: liuyc20 <liuyc20@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3615713
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -3502,6 +3502,9 @@ static int gsi_bind(struct usb_configuration *c, struct usb_function *f)
 	if (status)
 		goto dereg_rndis;
 
+	/* MMI_STOPSHIP <usb>: fix ramdump issue when usb rndis function*/
+	return 0;
+
 dereg_rndis:
 	rndis_deregister(gsi->params);
 fail:
