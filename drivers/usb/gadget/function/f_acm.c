5c379ba33a096caf05f940ab6a1514ef36e58ac4
Greg Kroah-Hartman <gregkh@google.com>
2025-10-31 08:11:19 +0000

    Revert "usb: gadget: f_acm: Refactor bind path to use __free()"
    This reverts commit e348d18fb0124b662cfefb3001733b49da428215 which is
    commit 47b2116e54b4a854600341487e8b55249e926324 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I1a9c6edfc49fd7b083034c7a245c88a42f415d8b
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/gadget/function/f_acm.c
+++ drivers/usb/gadget/function/f_acm.c
@@ -11,14 +11,11 @@
 
 /* #define VERBOSE_DEBUG */
 
-#include <linux/cleanup.h>
 #include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
-
-#include <linux/usb/gadget.h>
 
 #include "u_serial.h"
 
@@ -615,7 +612,6 @@ acm_bind(struct usb_configuration *c, struct usb_function *f)
 	struct usb_string	*us;
 	int			status;
 	struct usb_ep		*ep;
-	struct usb_request	*request __free(free_usb_request) = NULL;
 
 	/* REVISIT might want instance-specific strings to help
 	 * distinguish instances ...
@@ -633,7 +629,7 @@ acm_bind(struct usb_configuration *c, struct usb_function *f)
 	/* allocate instance-specific interface IDs, and patch descriptors */
 	status = usb_interface_id(c, f);
 	if (status < 0)
-		return status;
+		goto fail;
 	acm->ctrl_id = status;
 	acm_iad_descriptor.bFirstInterface = status;
 
@@ -642,38 +638,40 @@ acm_bind(struct usb_configuration *c, struct usb_function *f)
 
 	status = usb_interface_id(c, f);
 	if (status < 0)
-		return status;
+		goto fail;
 	acm->data_id = status;
 
 	acm_data_interface_desc.bInterfaceNumber = status;
 	acm_union_desc.bSlaveInterface0 = status;
 	acm_call_mgmt_descriptor.bDataInterface = status;
 
+	status = -ENODEV;
+
 	/* allocate instance-specific endpoints */
 	ep = usb_ep_autoconfig(cdev->gadget, &acm_fs_in_desc);
 	if (!ep)
-		return -ENODEV;
+		goto fail;
 	acm->port.in = ep;
 
 	ep = usb_ep_autoconfig(cdev->gadget, &acm_fs_out_desc);
 	if (!ep)
-		return -ENODEV;
+		goto fail;
 	acm->port.out = ep;
 
 	ep = usb_ep_autoconfig(cdev->gadget, &acm_fs_notify_desc);
 	if (!ep)
-		return -ENODEV;
+		goto fail;
 	acm->notify = ep;
 
 	/* allocate notification */
-	request = gs_alloc_req(ep,
-			       sizeof(struct usb_cdc_notification) + 2,
-			       GFP_KERNEL);
-	if (!request)
-		return -ENODEV;
+	acm->notify_req = gs_alloc_req(ep,
+			sizeof(struct usb_cdc_notification) + 2,
+			GFP_KERNEL);
+	if (!acm->notify_req)
+		goto fail;
 
-	request->complete = acm_cdc_notify_complete;
-	request->context = acm;
+	acm->notify_req->complete = acm_cdc_notify_complete;
+	acm->notify_req->context = acm;
 
 	/* support all relevant hardware speeds... we expect that when
 	 * hardware is dual speed, all bulk-capable endpoints work at
@@ -690,9 +688,7 @@ acm_bind(struct usb_configuration *c, struct usb_function *f)
 	status = usb_assign_descriptors(f, acm_fs_function, acm_hs_function,
 			acm_ss_function, acm_ss_function);
 	if (status)
-		return status;
-
-	acm->notify_req = no_free_ptr(request);
+		goto fail;
 
 	dev_dbg(&cdev->gadget->dev,
 		"acm ttyGS%d: IN/%s OUT/%s NOTIFY/%s\n",
@@ -700,6 +696,14 @@ acm_bind(struct usb_configuration *c, struct usb_function *f)
 		acm->port.in->name, acm->port.out->name,
 		acm->notify->name);
 	return 0;
+
+fail:
+	if (acm->notify_req)
+		gs_free_req(acm->notify, acm->notify_req);
+
+	ERROR(cdev, "%s/%p: can't bind, err %d\n", f->name, f, status);
+
+	return status;
 }
 
 static void acm_unbind(struct usb_configuration *c, struct usb_function *f)
