bd86bf071e5587bdfccc6eb8f1c55675393c8403
Abishek K V-ABISHEKK <abishekk@motorola.com>
2025-11-18 14:21:01 +0530

    : Merge source code with GKI android15-6.6-2025-04_r29 for Orion
    Merge source code with GKI from android15-6.6-2025-04_r25
    to android15-6.6-2025-04_r29
    
    Merge list:
    eb97177 UPSTREAM: tls: make sure to abort the stream if headers are bogus by Jakub Kicinski
    9a903d6 ANDROID: ABI: Update pixel symbol list by Richard Chang
    fbc0e15 ANDROID: fix ABI breakage for trace_array extensions by Richard Chang
    71560fb UPSTREAM: tracing: Allow creating instances with specified system events by Steven Rostedt (Google)
    73f4141 ANDROID: ABI: Update oplus symbol list by LongPing Wei
    7480af4 ANDROID: KVM: arm64: Prevent memory sharing outside of the RAM regions by Sebastian Ene
    c36031b UPSTREAM: tls: handle data disappearing from under the TLS ULP by Jakub Kicinski
    bf34b84 UPSTREAM: tls: fix handling of zero-length records on the rx_list by Jakub Kicinski
    c594dc5 UPSTREAM: vsock: Do not allow binding to VMADDR_PORT_ANY by Budimir Markovic
    3a68b86 UPSTREAM: xfrm: interface: fix use-after-free after changing collect_md xfrm interface by Eyal Birger
    6bc0f27 ANDROID: KVM: arm64: size overflow checks for mem_protect HVCs by Vincent Donnefort
    13dbff8 ANDROID: KVM: Fix ToCToU issue when admitting pages in memcache by Sebastian Ene
    5a585c1 ANDROID: GKI: update symbol list for xiaomi by liuzenglin
    dbeb723 ANDROID: GKI: Update xiaomi symbol list. by yujiaochen
    c3d9a51 ANDROID: KVM: arm64: Fix CPU type when reading trace_pdesc by Vincent Donnefort
    e6992f0 ANDROID: KVM: arm64: Don't copy pvmfw pages more than once by Sebastian Ene
    8c91b36 BACKPORT: FROMGIT: f2fs: add sysfs entry for effective lookup mode by Daniel Lee
    550f584 BACKPORT: FROMGIT: f2fs: add lookup_mode mount option by Daniel Lee
    2f34f62 ANDROID: KVM: Enforce hyp requests alignment by Sebastian Ene
    5b8315e UPSTREAM: eventpoll: don't decrement ep refcount while still holding the ep mutex by Linus Torvalds
    
    Change-Id: I3bc289bda15a097eabbb92f0374f3661ac94f509
    Signed-off-by: Abishek K V <abishekk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3651297
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Navya Vijayan <navya@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit 37df175e0c3df7b841624ba4ea215367e37b26a9)
    Reviewed-on: https://gerrit.mot.com/3692195
    Reviewed-by: Hao Li <lihao24@motorola.com>
    Submit-Approved: Hao Li <lihao24@motorola.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -442,6 +442,10 @@ int __pkvm_guest_relinquish_to_host(struct pkvm_hyp_vcpu *vcpu,
 		goto end;
 
 	WARN_ON(host_stage2_set_owner_locked(data.pa, PAGE_SIZE, PKVM_ID_HOST));
+
+	if (pkvm_ipa_range_has_pvmfw(vm, ipa, ipa + PAGE_SIZE))
+		vm->kvm.arch.pkvm.pvmfw_load_addr = PVMFW_INVALID_LOAD_ADDR;
+
 end:
 	guest_unlock_component(vm);
 	host_unlock_component();
@@ -1057,7 +1061,10 @@ static int ___host_check_page_state_range(u64 addr, u64 size,
 		.desired	= state,
 		.get_page_state	= host_get_mmio_page_state,
 	};
-	u64 end = addr + size;
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
 
 	hyp_assert_lock_held(&host_mmu.lock);
 
@@ -1087,7 +1094,10 @@ static int __host_check_page_state_range(u64 addr, u64 size,
 {
 	struct memblock_region *reg;
 	struct kvm_mem_range range;
-	u64 end = addr + size;
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
 
 	/* Can't check the state of both MMIO and memory regions at once */
 	reg = find_mem_range(addr, &range);
@@ -1100,6 +1110,14 @@ static int __host_check_page_state_range(u64 addr, u64 size,
 static int __host_set_page_state_range(u64 addr, u64 size,
 				       enum pkvm_page_state state)
 {
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
+
+	if (!range_is_memory(addr, end))
+		return -EPERM;
+
 	if (hyp_phys_to_page(addr)->host_state & PKVM_NOPAGE) {
 		int ret = host_stage2_idmap_locked(addr, size, PKVM_HOST_MEM_PROT, true);
 
@@ -1398,6 +1416,10 @@ static int __guest_check_page_state_range(struct pkvm_hyp_vm *vm, u64 addr,
 		.desired	= state,
 		.get_page_state	= guest_get_page_state,
 	};
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
 
 	hyp_assert_lock_held(&vm->pgtable_lock);
 	return check_page_state_range(&vm->pgt, addr, size, &d);
@@ -2600,8 +2622,12 @@ static int guest_get_valid_pte(struct pkvm_hyp_vm *vm, u64 pfn, u64 ipa,
 	size_t size = PAGE_SIZE << order;
 	u64 phys = hyp_pfn_to_phys(pfn);
 	u32 level;
+	u64 end;
 
 	if (order && size != PMD_SIZE)
+		return -EINVAL;
+
+	if (check_add_overflow(phys, size, &end))
 		return -EINVAL;
 
 	WARN_ON(kvm_pgtable_get_leaf(&vm->pgt, ipa, pte, &level));
