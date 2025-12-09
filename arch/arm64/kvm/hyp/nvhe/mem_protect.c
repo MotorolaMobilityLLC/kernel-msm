dc1c8ffe9496e6ee9cc4d1d5e892f2497f0f3248
Abishek K V <abishekk@motorola.com>
2025-12-09 14:56:23 +0530

    Merge source code with GKI android14-6.1-2025-03_r14 for Velar
    Merge source code with GKI from android14-6.1-2025-03_r12
    to android14-6.1-2025-03_r14
    
    Merge list:
    0cc282c UPSTREAM: tls: make sure to abort the stream if headers are bogus by Jakub Kicinski
    67bac638 ANDROID: KVM: arm64: Check PGD alignment when creating a pVM by Quentin Perret
    58deda0 ANDROID: f2fs: fix incorrect merge resolution in f2fs_trace_rw_file_path() by Sandeep Dhavale
    f4c242f UPSTREAM: eventpoll: don't decrement ep refcount while still holding the ep mutex by Linus Torvalds
    1f0c6db ANDROID: KVM: arm64: size overflow checks for mem_protect HVCs by Vincent Donnefort
    c3100f2 UPSTREAM: tls: fix handling of zero-length records on the rx_list by Jakub Kicinski
    b788051 UPSTREAM: vsock: Do not allow binding to VMADDR_PORT_ANY by Budimir Markovic
    4e75b52 UPSTREAM: xfrm: interface: fix use-after-free after changing collect_md xfrm interface by Eyal Birger
    af6806c ANDROID: KVM: arm64: Fix CPU type when reading trace_pdesc by Vincent Donnefort
    40db4d71 ANDROID: KVM: arm64: Don't copy pvmfw pages more than once by Sebastian Ene
    a5ed59a8 UPSTREAM: f2fs: fix to avoid atomicity corruption of atomic file
    
    Change-Id: I57258edd54680cda32e87391dfe3fd98c3228611
    Signed-off-by: Abishek K V <abishekk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3703875
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Navya Vijayan <navya@motorola.com>
    Submit-Approved: Jira Key

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -210,7 +210,7 @@ static void *guest_s2_zalloc_pages_exact(size_t size)
 {
 	void *addr = hyp_alloc_pages(&current_vm->pool, get_order(size));
 
-	WARN_ON(size != (PAGE_SIZE << get_order(size)));
+	WARN_ON(!addr || size != (PAGE_SIZE << get_order(size)));
 	hyp_split_page(hyp_virt_to_page(addr));
 
 	return addr;
@@ -366,6 +366,9 @@ int __pkvm_guest_relinquish_to_host(struct pkvm_hyp_vcpu *vcpu,
 	if (!ret && data.pa) {
 		WARN_ON(host_stage2_set_owner_locked(data.pa, PAGE_SIZE, PKVM_ID_HOST));
 		WARN_ON(kvm_pgtable_stage2_unmap(&vm->pgt, ipa, PAGE_SIZE));
+
+		if (pkvm_ipa_range_has_pvmfw(vm, ipa, ipa + PAGE_SIZE))
+			vm->kvm.arch.pkvm.pvmfw_load_addr = PVMFW_INVALID_LOAD_ADDR;
 	}
 
 	guest_unlock_component(vm);
@@ -1015,6 +1018,10 @@ static int __host_check_page_state_range(u64 addr, u64 size,
 		.desired	= state,
 		.get_page_state	= host_get_page_state,
 	};
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
 
 	hyp_assert_lock_held(&host_mmu.lock);
 	return check_page_state_range(&host_mmu.pgt, addr, size, &d);
@@ -1305,6 +1312,10 @@ static int __guest_check_page_state_range(struct pkvm_hyp_vcpu *vcpu, u64 addr,
 		.desired	= state,
 		.get_page_state	= guest_get_page_state,
 	};
+	u64 end;
+
+	if (check_add_overflow(addr, size, &end))
+		return -EINVAL;
 
 	hyp_assert_lock_held(&vm->lock);
 	return check_page_state_range(&vm->pgt, addr, size, &d);
