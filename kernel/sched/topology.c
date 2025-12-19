30abf7f95abe2170cc0277bd095607a634ccc5e9
Qais Yousef <qyousef@google.com>
2025-12-19 14:25:28 +0000

    ANDROID: Export active_load_balance_cpu_stop and plist_add/del
    and sched_energy_present static key to allow implementing push load
    balance from upstream
    
    	https://lore.kernel.org/lkml/20250302210539.1563190-6-vincent.guittot@linaro.org/
    
    Bug: 437347237
    Change-Id: I812f0bcf86b77eec18a5bbe60c1e175488778568
    Signed-off-by: Qais Yousef <qyousef@google.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -212,6 +212,7 @@ sd_parent_degenerate(struct sched_domain *sd, struct sched_domain *parent)
 
 #if defined(CONFIG_ENERGY_MODEL) && defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL)
 DEFINE_STATIC_KEY_FALSE(sched_energy_present);
+EXPORT_SYMBOL_GPL(sched_energy_present);
 static unsigned int sysctl_sched_energy_aware = 1;
 static DEFINE_MUTEX(sched_energy_mutex);
 static bool sched_energy_update;
