66ca513670bac83207d9c8385be01914612595f8
ying zuxin <11154159@vivo.com>
2025-12-24 10:46:53 +0800

    ANDROID: cgroup: Add vendor hooks for cpuset
    These two vendor hooks allow us to capture relevant information when
    a cpuset CSS comes online, and to set the corresponding task flags
    based on that information when a task is attached to the cpuset.
    
    Bug: 471555377
    Change-Id: I395a834d8b8938bce748e890a761bfb0cfc59374
    Signed-off-by: ying zuxin <11154159@vivo.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2681,6 +2681,8 @@ static void cpuset_attach_task(struct cpuset *cs, struct task_struct *task)
 
 	cpuset_change_task_nodemask(task, &cpuset_attach_nodemask_to);
 	cpuset_update_task_spread_flags(cs, task);
+
+	trace_android_vh_cpuset_attach_task(&cs->css, task);
 }
 
 static void cpuset_attach(struct cgroup_taskset *tset)
@@ -3354,6 +3356,7 @@ static int cpuset_css_online(struct cgroup_subsys_state *css)
 out_unlock:
 	mutex_unlock(&cpuset_mutex);
 	cpus_read_unlock();
+	trace_android_vh_cpuset_css_online(css);
 	return 0;
 }
 
