4f6d388f63fc0114bcefbbc5fd724253ace99a4f
wuxj3 <wuxj3@lenovo.com>
2026-01-08 09:30:01 +0800

    Equator: defer the msm_sharedmem_probe upon alloc failures
    if alloc failed return EPROBE_DEFER ,
    then alloc memory later again
    
    Change-Id: I664652ddaa4a7d36cbe3ee2337acd819c3b93a92
    
    Reviewed-on: https://gerrit.mot.com/3712900
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/uio/msm_sharedmem/msm_sharedmem.c
+++ drivers/uio/msm_sharedmem/msm_sharedmem.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023,2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define DRIVER_NAME "msm_sharedmem"
@@ -178,8 +178,10 @@ static int msm_sharedmem_probe(struct platform_device *pdev)
 
 		shared_mem = dma_alloc_coherent(&pdev->dev, shared_mem_tot_sz,
 					&shared_mem_pyhsical, GFP_KERNEL);
-		if (shared_mem == NULL)
-			return -ENOMEM;
+		if (shared_mem == NULL) {
+			pr_err_ratelimited("shared mem allocation failed..deferring probe\n");
+			return -EPROBE_DEFER;
+		}
 
 		if (guard_memory)
 			shared_mem_pyhsical += SZ_4K;
