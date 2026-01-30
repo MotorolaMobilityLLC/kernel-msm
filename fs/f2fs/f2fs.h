e3439fc5d17189626db520474868ef88dbc18c79
Chao Yu <chao@kernel.org>
2026-01-30 21:28:08 +0800

    BACKPORT: UPSTREAM: f2fs: fix lock priority inversion issue
    If userspace thread has held f2fs rw semaphore, due to its low priority,
    it could be runnable or preempted state for long time, during the time,
    it will block high priority thread which is trying to grab the same rw
    semaphore, e.g. cp_rwsem, io_rwsem...
    
    To fix such issue, let's detect thread's priority when it tries to grab
    f2fs_rwsem lock, if the priority is lower than a priority threshold, let's
    uplift the priority before it enters into critical region of lock, and
    restore the priority after it leaves from critical region.
    
    Meanwhile, introducing two new sysfs nodes:
    - /sys/fs/f2fs/<disk>/adjust_lock_priority, it is used to control whether
    the functionality is enable or not.
    ==========     ==================
    Flag_Value     Flag_Description
    ==========     ==================
    0x00000000     Disabled (default)
    0x00000001     cp_rwsem
    0x00000002     node_change
    0x00000004     node_write
    0x00000008     gc_lock
    0x00000010     cp_global
    0x00000020     io_rwsem
    ==========     ==================
    - /sys/fs/f2fs/<disk>/lock_duration_priority, it is used to control
    priority threshold.
    
    Bug: 408202989
    Bug: 487801842
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 07de55cbf5762cb4a7e9e0db7aba5c10c8cfe079)
    [Chao: fix the conflict (relocate some codes from checkpoint.c to lock.c)]
    Signed-off-by: Chao Yu <chaseyu@google.com>
    Cherrypick-From: https://android-review.googlesource.com/q/commit:12840a12ec3d3ac3d0530b0bbbb1da5421f2efbb
    Merged-In: Icde023cdc46dd7e9be16635cc6f047c9101051fa
    Change-Id: Icde023cdc46dd7e9be16635cc6f047c9101051fa

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -148,6 +148,7 @@ enum f2fs_lock_name {
 	LOCK_NAME_GC_LOCK,
 	LOCK_NAME_CP_GLOBAL,
 	LOCK_NAME_IO_RWSEM,
+	LOCK_NAME_MAX,
 };
 
 /*
@@ -1337,7 +1338,10 @@ struct f2fs_time_stat {
 
 struct f2fs_lock_context {
 	struct f2fs_time_stat ts;
+	int orig_nice;
+	int new_nice;
 	bool lock_trace;
+	bool need_restore;
 };
 
 struct f2fs_gc_control {
@@ -1463,6 +1467,8 @@ enum {
 
 /* a threshold of maximum elapsed time in critical region to print tracepoint */
 #define MAX_LOCK_ELAPSED_TIME		500
+
+#define F2FS_DEFAULT_TASK_PRIORITY		(DEFAULT_PRIO)
 
 static inline int f2fs_test_bit(unsigned int nr, char *addr);
 static inline void f2fs_set_bit(unsigned int nr, char *addr);
@@ -1866,6 +1872,12 @@ struct f2fs_sb_info {
 
 	/* max elapsed time threshold in critical region that lock covered */
 	unsigned long long max_lock_elapsed_time;
+
+	/* enable/disable to adjust task priority in critical region covered by lock */
+	unsigned int adjust_lock_priority;
+
+	/* adjust priority for task which is in critical region covered by lock */
+	unsigned int lock_duration_priority;
 
 #ifdef CONFIG_F2FS_FS_COMPRESSION
 	struct kmem_cache *page_array_slab;	/* page array entry */
