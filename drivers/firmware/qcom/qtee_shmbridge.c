ac9d014eb05648e88a8a73ab77ce6abec59c290e
xiewen3@motorola.com <xiewen3@motorola.com>
2026-03-04 18:02:33 +0800

    firmware: qcom: qcom_tzmem: Increase shmbridge pool size to prevent allocation failures
    Increase initial_size from 256KB to 1MB and max_size from 4MB to 8MB
    to reduce the frequency of pool expansion. Each pool expansion requires
    allocating contiguous physical memory (order=7, 512KB) via __get_free_pages,
    which can fail due to memory fragmentation when there are no available
    512KB+ contiguous pages in the buddy allocator.
    
    Also add error logging in qcom_tzmem_pool_add_memory() to help diagnose
    allocation failures by printing the requested size, allocation order,
    pool total size, and pool available size.
    
    Bug: page allocation failure: order:7, mode:0xcc0(GFP_KERNEL)
    
    Change-Id: Icce0a75cfe5794a4e70fc8395abec0df604ec182
    Signed-off-by: xiewen3@motorola.com <xiewen3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3790752
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/firmware/qcom/qtee_shmbridge.c
+++ drivers/firmware/qcom/qtee_shmbridge.c
@@ -100,9 +100,9 @@ int qtee_shmbridge_driver_init(void)
 	struct qcom_tzmem_pool_config pool_config;
 
 	memset(&pool_config, 0, sizeof(pool_config));
-	pool_config.initial_size = SZ_512K;
+	pool_config.initial_size = SZ_1M;
 	pool_config.policy = QCOM_TZMEM_POLICY_ON_DEMAND;
-	pool_config.max_size = SZ_4M;
+	pool_config.max_size = SZ_8M;
 	pool_config.is_cached = true;
 
 	shmbridge_pool = qcom_tzmem_pool_new(&pool_config);
