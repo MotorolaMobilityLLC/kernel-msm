72aabe434faafa80affbc48998b19ec7957cc9bd
daiyang5 <daiyang5@xiaomi.com>
2026-03-09 20:17:49 +0800

    ANDROID: vendor_hook: Added hooks and a function to handle special pages
    The following hooks are added to handle special pages:
    
    android_vh_filemap_adjust_folio_flags
      Invoked before adding a folio to the LRU. It allows adjusting the
      active flag to decide whether the folio should be inserted into
      the active or inactive list.
    
    trace_android_vh_folio_skip_activate
      Allows determining whether a folio should bypass the remaining
      steps in activate_locked().
    
    trace_android_vh_folio_trylock_clear_bypass
      Allows determining whether a folio should be added to the
      active list.
    
    Bug: 491161822
    Bug: 504423387
    
    Change-Id: Ia204713966fff9609d76dc4f608bc4a2605251c6
    Signed-off-by: daiyang5 <daiyang5@xiaomi.com>

--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -108,6 +108,12 @@ DECLARE_HOOK(android_vh_remove_mapping_failed,
 DECLARE_HOOK(android_vh_handle_trylock_failed_folio,
 	TP_PROTO(struct list_head *folio_list),
 	TP_ARGS(folio_list));
+DECLARE_HOOK(android_vh_folio_skip_activate,
+	TP_PROTO(struct folio *folio, bool *skip),
+	TP_ARGS(folio, skip));
+DECLARE_HOOK(android_vh_folio_trylock_clear_bypass,
+	TP_PROTO(struct folio *folio, bool *bypass),
+	TP_ARGS(folio, bypass));
 DECLARE_HOOK(android_vh_folio_trylock_set,
 	TP_PROTO(struct folio *folio),
 	TP_ARGS(folio));
