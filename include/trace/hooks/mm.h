d872dd17f10366fa9439ea70f667fbd7cefa8f89
daiyang5 <daiyang5@xiaomi.com>
2026-03-09 20:06:20 +0800

    ANDROID: Add vendor hook for tracking folios during readahead
    Add vendor hook android_vh_readahead_add_folio before adding a folio to
    the page cache during readahead, enabling vendor modules to monitor and
    optimize readahead pages.
    
    Bug: 491161822
    Bug: 504423387
    
    Change-Id: I5cca4fdb646da04993b832a53e9f6afa1de53ae5
    Signed-off-by: daiyang5 <daiyang5@xiaomi.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -708,6 +708,9 @@ DECLARE_HOOK(android_vh_folio_add_file_rmap,
 	TP_PROTO(struct folio *folio, struct page *page, int nr_pages,
 		 int level),
 	TP_ARGS(folio, page, nr_pages, level));
+DECLARE_HOOK(android_vh_readahead_add_folio,
+	TP_PROTO(struct folio *folio, struct address_space *mapping),
+	TP_ARGS(folio, mapping));
 DECLARE_HOOK(android_vh_folio_remove_rmap,
 	TP_PROTO(struct folio *folio, struct page *page, int nr_pages,
 		 int level),
