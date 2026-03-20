29f0c6db359b138f42a327f0d0456c45214eac90
Bart Van Assche <bvanassche@google.com>
2026-03-20 13:19:36 -0700

    ANDROID: block: Introduce the android_rvh_blk_mq_has_request() vendor hook
    Change the return value of blk_mq_has_request() from 'false' into 'true'
    to make the caller iterate over all requests instead of only over one
    request.
    
    Bug: 479780487
    Bug: 495560253
    Change-Id: I2bb757362e32ec57543b5990aeea9b3ee1856a11
    Signed-off-by: Bart Van Assche <bvanassche@google.com>
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -3545,11 +3545,13 @@ struct rq_iter_data {
 static bool blk_mq_has_request(struct request *rq, void *data)
 {
 	struct rq_iter_data *iter_data = data;
+	bool ret = false;
 
 	if (rq->mq_hctx != iter_data->hctx)
 		return true;
 	iter_data->has_rq = true;
-	return false;
+	trace_android_rvh_blk_mq_has_request(iter_data->hctx, rq, &ret);
+	return ret;
 }
 
 static bool blk_mq_hctx_has_requests(struct blk_mq_hw_ctx *hctx)
