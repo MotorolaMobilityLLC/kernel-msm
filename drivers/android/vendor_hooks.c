080be6080aeb202c14a6293504bb761d4646ae08
Joshua Liu <josliu@google.com>
2026-03-23 11:29:20 +0000

    ANDROID: add read_swap_cache_async_timeout vendor hook
    Enable vendor implementation for schedule_timeout_uninterruptible
    
    Bug: 498452657
    
    Change-Id: Ia3f11ed83e5375fd4bf8ba2cbb1ad3d557e8ded4
    Signed-off-by: Joshua Liu <josliu@google.com>
    (cherry picked from commit af90e40a302bda636c6c1003ce95b0a8b61811e4)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -719,3 +719,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_curr_lazy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lock_delay_schedule);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lock_task_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lock_task_exit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_read_swap_cache_async_timeout);
