ece8174c8aecbc1516fecff77e8ea124f399a0f9
Joshua Liu <josliu@google.com>
2026-03-23 11:29:20 +0000

    ANDROID: add read_swap_cache_async_timeout vendor hook
    Enable vendor implementation for schedule_timeout_uninterruptible
    
    Bug: 498452657
    
    Change-Id: Ia3f11ed83e5375fd4bf8ba2cbb1ad3d557e8ded4
    Signed-off-by: Joshua Liu <josliu@google.com>
    (cherry picked from commit af90e40a302bda636c6c1003ce95b0a8b61811e4)

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -428,6 +428,7 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 	struct folio *folio;
 	struct page *page;
 	void *shadow = NULL;
+	size_t count = 0;
 
 	*new_page_allocated = false;
 	si = get_swap_device(entry);
@@ -436,6 +437,7 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 
 	for (;;) {
 		int err;
+		bool skip = false;
 		/*
 		 * First check the swap cache.  Since this is normally
 		 * called after swap_cache_get_folio() failed, re-calling
@@ -486,6 +488,10 @@ struct page *__read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 		 * __read_swap_cache_async(), which has set SWAP_HAS_CACHE
 		 * in swap_map, but not yet added its page to swap cache.
 		 */
+		trace_android_rvh_read_swap_cache_async_timeout(&count, &skip);
+		if (skip)
+			continue;
+
 		schedule_timeout_uninterruptible(1);
 	}
 
