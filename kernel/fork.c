bcc10039d97efd256d7f20b6b62abc1f2993494b
T.J. Mercier <tjmercier@google.com>
2026-03-27 13:00:43 -0700

    ANDROID: Clear task dmabuf_info on unshare(CLONE_FILES)
    unshare_files is called by exec, but not by the unshare syscall which
    calls unshare_fd directly. If a process calls unshare with CLONE_FILES,
    the kernel fails to clear the task's dmabuf_info despite the new partial
    sharing of dmabuf accounting info (shared MM, unshared files struct)
    which is unsupported. Subsequent reads of /proc/<pid>/dmabuf_rss (and
    friends) for the task that called unshare will return some value when
    they should return nothing.
    
    Fix this by clearing the task's dmabuf_info pointer in ksys_unshare in
    addition to unshare_files. This cannot be done in a single place in
    unshare_fd because later errors could prevent the swap of a task's files
    struct from occurring after unshare_fd is called.
    
    Also move the put_dmabuf_info call outside of the task_lock critical
    section.
    
    Bug: 461520624
    Fixes: 09725f5bd79d ("ANDROID: Don't use current pointer for per-process dmabuf accounting")
    Change-Id: Ibbd92c8974689206985a2ac9e8beeb5b21fa7dce
    Signed-off-by: T.J. Mercier <tjmercier@google.com>
    (cherry picked from commit 2d8e5eb61eae0fc9a486f5710b078c30efd0364f)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -3456,6 +3456,7 @@ int ksys_unshare(unsigned long unshare_flags)
 	struct files_struct *new_fd = NULL;
 	struct cred *new_cred = NULL;
 	struct nsproxy *new_nsproxy = NULL;
+	struct task_dma_buf_info *dmabuf_info = NULL;
 	int do_sysvsem = 0;
 	int err;
 
@@ -3540,10 +3541,22 @@ int ksys_unshare(unsigned long unshare_flags)
 			spin_unlock(&fs->lock);
 		}
 
-		if (new_fd)
+		if (new_fd) {
 			swap(current->files, new_fd);
 
+			/*
+			 * This is a new partial sharing relationship for the current task, since we
+			 * have a new files_struct (and the MM might still be shared). Since partial
+			 * sharing is not supported for dmabuf accounting, we need to remove the
+			 * accounting info from the task. Leave the mm->dmabuf_info so any existing
+			 * accounting can be unaccounted properly.
+			 */
+			dmabuf_info = current->dmabuf_info;
+			current->dmabuf_info = NULL;
+		}
+
 		task_unlock(current);
+		put_dmabuf_info(dmabuf_info);
 
 		if (new_cred) {
 			/* Install the new user namespace */
@@ -3584,6 +3597,7 @@ int unshare_files(void)
 {
 	struct task_struct *task = current;
 	struct files_struct *old, *copy = NULL;
+	struct task_dma_buf_info *dmabuf_info;
 	int error;
 
 	error = unshare_fd(CLONE_FILES, &copy);
@@ -3595,16 +3609,17 @@ int unshare_files(void)
 	task->files = copy;
 
 	/*
-	 * This is a new partial sharing relationship for task, since we have a new
-	 * files_struct (but the MM is still used). Since partial sharing is not
-	 * supported for dmabuf accounting, we need to remove the accounting info
-	 * from the task. Leave the mm->dmabuf_info so any existing accounting can
-	 * be unaccounted properly. The fixup for this new files_struct happens
-	 * externally with appropriate locking.
+	 * This is a new partial sharing relationship for the current task, since we have a new
+	 * files_struct (and the MM might still be shared). Since partial sharing is not
+	 * supported for dmabuf accounting, we need to remove the accounting info from the task.
+	 * Leave the mm->dmabuf_info so any existing accounting can be unaccounted properly. For
+	 * execs where we also have a new MM, the fixup for this new files_struct happens externally
+	 * with appropriate locking in dma_buf_begin_new_exec.
 	 */
-	put_dmabuf_info(task->dmabuf_info);
+	dmabuf_info = task->dmabuf_info;
 	task->dmabuf_info = NULL;
 	task_unlock(task);
+	put_dmabuf_info(dmabuf_info);
 	put_files_struct(old);
 	return 0;
 }
