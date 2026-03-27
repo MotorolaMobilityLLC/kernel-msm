55f832b81cb455652e8770840f1f9ca2a6b7e1c7
Henry Dong <donghenry@google.com>
2026-03-27 16:53:42 -0700

    ANDROID: vendor_hooks: Add hook for sync mmap readahead
    Add vendor hooks to precisely control sync readahead based on
    various factors, such as filesystem type, file type, process
    priority, and other considerations.
    
    Bug: 496697696
    Bug: 500664194
    Change-Id: I1df4c16065eab4d7fbe4d6e27fa1c56bcb405b5f
    Signed-off-by: Henry Dong <donghenry@google.com>
    (cherry picked from commit b07ffa32679e789eb5ab8bbb336f0cb07454e117)

--- mm/filemap.c
+++ mm/filemap.c
@@ -3262,6 +3262,11 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 	struct file *fpin = NULL;
 	unsigned long vm_flags = vmf->vma->vm_flags;
 	unsigned int mmap_miss;
+	bool skip = false;
+
+	trace_android_vh_do_sync_mmap_readahead(vmf, &skip);
+	if (skip)
+		return fpin;
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	/* Use the readahead code, even if readahead is disabled */
