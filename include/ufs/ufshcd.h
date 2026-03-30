a4d3ab82862558d33ce135365ba0468e1a2647f4
Bart Van Assche <bvanassche@acm.org>
2026-03-30 10:23:51 -0700

    ANDROID: ufs: core: Introduce ufshcd_mcq_poll_n_cqe_lock()
    Introduce a new function for processing completions that accepts an
    upper limit for the number of completions to poll. Tell
    ufshcd_mcq_poll_cqe_lock() to poll at most hwq->max_entries. This is
    sufficient to poll all pending completions since there are never more
    than hwq->max_entries - 1 completions on a completion queue. This patch
    prepares for reducing the interrupt latency.
    
    Reviewed-by: Peter Wang <peter.wang@mediatek.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Bug: 493400699
    Bug: 500704598
    Bug: 503029970
    Change-Id: Id39333024cf6447cb7112b21a768ebe838b89795
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -1321,6 +1321,9 @@ void ufshcd_schedule_eh_work(struct ufs_hba *hba);
 void ufshcd_mcq_config_mac(struct ufs_hba *hba, u32 max_active_cmds);
 u32 ufshcd_mcq_read_cqis(struct ufs_hba *hba, int i);
 void ufshcd_mcq_write_cqis(struct ufs_hba *hba, u32 val, int i);
+unsigned long ufshcd_mcq_poll_n_cqe_lock(struct ufs_hba *hba,
+					 struct ufs_hw_queue *hwq,
+					 unsigned int max_compl);
 unsigned long ufshcd_mcq_poll_cqe_lock(struct ufs_hba *hba,
 					 struct ufs_hw_queue *hwq);
 void ufshcd_mcq_make_queues_operational(struct ufs_hba *hba);
