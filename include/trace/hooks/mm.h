779fc63168a2ad93fdb5953b8e3880befffd2652
Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>
2026-04-21 20:46:03 +0800

    ANDROID: vendor_hooks: Add vh_folios_put_direct_free
    I added a vendor hook "vh_folios_put_refs_direct_free_extent" in 'commit
    afdc0cbbc106 ("ANDROID: vendor_hooks: Add vh_folios_put_refs_direct_free_extent")',
    while the hook name is too long and causes a build error when used:
    
    ERROR: modpost: too long symbol "__tracepoint_android_vh_folios_put_refs_direct_free_extent".
    
    The root cause is that in `scripts/mod/modpost.c`, exported symbol names
    are checked against `MODULE_NAME_LEN`.
    
    Since vendor hooks cannot be renamed, I submitted a new vendor hook at the
    same call site.
    
    Bug: 504935225
    Bug: 505230691
    Change-Id: Ic9bc72066790df04633ef82e7eec483bd0d10bc0
    Signed-off-by: Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>
    (cherry picked from commit e7b7ca435f6a2168a441d39306d83d9a2137a6bf)

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -728,6 +728,10 @@ DECLARE_HOOK(android_vh_folio_remove_rmap,
 	TP_PROTO(struct folio *folio, struct page *page, int nr_pages,
 		 int level),
 	TP_ARGS(folio, page, nr_pages, level));
+DECLARE_HOOK(android_vh_folios_put_direct_free,
+	TP_PROTO(struct folio *folio, unsigned int nr_refs,
+		 struct lruvec **lruvec, unsigned long flags, bool *direct_free),
+	TP_ARGS(folio, nr_refs, lruvec, flags, direct_free));
 DECLARE_HOOK(android_vh_folios_put_refs_direct_free_extent,
 	TP_PROTO(struct folio *folio, unsigned int nr_refs,
 		 struct lruvec **lruvec, unsigned long flags, bool *direct_free),
